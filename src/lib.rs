14d7742c81168e027812741b800d7d572f902479
pkusensei <ZW_08@outlook.com>
2024-07-27 18:36:18 -0400

    16 3sum closest

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,26 @@
-pub fn roman_to_int(s: &str) -> i32 {
-    let mut iter = s.chars().rev().peekable();
-    let mut res = 0;
-    while let Some(ch) = iter.next() {
-        res += decode(ch);
-        match ch {
-            'V' | 'X' if iter.peek().is_some_and(|c| *c == 'I') => {
-                res -= decode('I');
-                iter.next();
-            }
+pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
+    let mut nums = nums;
+    nums.sort_unstable();
+    let mut closest: i32 = nums[0..3].iter().sum();
 
-            'L' | 'C' if iter.peek().is_some_and(|c| *c == 'X') => {
-                res -= decode('X');
-                iter.next();
-            }
+    for i in 0..nums.len() - 2 {
+        let mut left = i + 1;
+        let mut right = nums.len() - 1;
 
-            'D' | 'M' if iter.peek().is_some_and(|c| *c == 'C') => {
-                res -= decode('C');
-                iter.next();
+        while left < right {
+            let curr = nums[i] + nums[left] + nums[right];
+            if (curr - target).abs() < (closest - target).abs() {
+                closest = curr
             }
-            _ => (),
+            match curr.cmp(&target) {
+                std::cmp::Ordering::Less => left += 1,
+                std::cmp::Ordering::Equal => return closest,
+                std::cmp::Ordering::Greater => right -= 1,
+            }
         }
     }
-    res
-}
 
-const fn decode(ch: char) -> i32 {
-    match ch {
-        'I' => 1,
-        'V' => 5,
-        'X' => 10,
-        'L' => 50,
-        'C' => 100,
-        'D' => 500,
-        'M' => 1000,
-        _ => unreachable!(),
-    }
+    closest
 }
 
 #[cfg(test)]
@@ -44,11 +30,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(roman_to_int("III"), 3);
-        debug_assert_eq!(roman_to_int("LVIII"), 58);
-        debug_assert_eq!(roman_to_int("MCMXCIV"), 1994);
+        debug_assert_eq!(three_sum_closest(vec![-1, 2, 1, -4], 1), 2);
+        debug_assert_eq!(three_sum_closest(vec![0, 0, 0], 1), 0);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            three_sum_closest(vec![4, 0, 5, -5, 3, 3, 0, -4, -5], -2),
+            -2
+        )
+    }
 }
