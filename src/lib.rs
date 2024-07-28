3dbe6f49f7d3702be9713c624e17c1b307653421
pkusensei <ZW_08@outlook.com>
2024-07-28 11:40:09 -0400

    22 paren pairs

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,30 @@
-use std::collections::{HashMap, VecDeque};
-
-pub fn second_minimum(n: i32, edges: Vec<[i32; 2]>, time: i32, change: i32) -> i32 {
-    let edges: HashMap<i32, Vec<i32>> =
-        edges.into_iter().fold(HashMap::new(), |mut acc, [n1, n2]| {
-            acc.entry(n1).or_default().push(n2);
-            acc.entry(n2).or_default().push(n1);
-            acc
-        });
-    bfs(n, &edges, time, change)
+pub fn generate_parenthesis(n: i32) -> Vec<String> {
+    solve(n, 1, 0)
+        .into_iter()
+        .map(|s| format!("({s}"))
+        .filter(|s| s.len() == n as usize * 2)
+        .collect()
 }
 
-fn bfs(n: i32, edges: &HashMap<i32, Vec<i32>>, time: i32, change: i32) -> i32 {
-    let mut dist1: Vec<_> = (0..n).map(|_| -1).collect();
-    let mut dist2 = dist1.clone();
-    let mut queue = VecDeque::new();
-    dist1[0] = 0;
-    // (node, frequency)
-    queue.push_back((1, 1));
-
-    while let Some((node, freq)) = queue.pop_front() {
-        let time_taken = {
-            let past_time = if freq == 1 {
-                dist1[node as usize - 1]
-            } else {
-                dist2[node as usize - 1]
-            };
-            if (past_time / change) & 1 == 1 {
-                // run into red light
-                change * (past_time / change + 1) + time
-            } else {
-                past_time + time
-            }
-        };
-        for &neighbor in edges[&node].iter() {
-            if dist1[neighbor as usize - 1] == -1 {
-                dist1[neighbor as usize - 1] = time_taken;
-                queue.push_back((neighbor, 1));
-            } else if dist2[neighbor as usize - 1] == -1
-                && dist1[neighbor as usize - 1] != time_taken
-            {
-                if neighbor == n {
-                    return time_taken;
-                }
-                dist2[neighbor as usize - 1] = time_taken;
-                queue.push_back((neighbor, 2));
-            }
-        }
+fn solve(n: i32, left: i32, right: i32) -> Vec<String> {
+    if left == n && right < n {
+        solve(n, left, right + 1)
+            .into_iter()
+            .map(|s| format!("){s}"))
+            .collect()
+    } else if left < n && right < n && left >= right {
+        solve(n, left + 1, right)
+            .into_iter()
+            .map(|s| format!("({s}"))
+            .chain(
+                solve(n, left, right + 1)
+                    .into_iter()
+                    .map(|s| format!("){s}")),
+            )
+            .collect()
+    } else {
+        vec!["".to_string()]
     }
-    0
 }
 
 #[cfg(test)]
@@ -56,11 +33,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            second_minimum(5, vec![[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]], 3, 5),
-            13
-        );
-        debug_assert_eq!(second_minimum(2, vec![[1, 2]], 3, 2), 11)
+        {
+            let mut ans = generate_parenthesis(3);
+            ans.sort_unstable();
+            debug_assert_eq!(ans, ["((()))", "(()())", "(())()", "()(())", "()()()"])
+        }
+        {
+            let mut ans = generate_parenthesis(1);
+            ans.sort_unstable();
+            debug_assert_eq!(ans, ["()"])
+        }
     }
 
     #[test]
