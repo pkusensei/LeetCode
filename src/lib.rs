7caf438e416826482d208d1b501909250f392b2d
pkusensei <ZW_08@outlook.com>
2024-07-28 08:05:24 -0400

    2045 bfs with a twitch

--- src/lib.rs
+++ src/lib.rs
@@ -1,123 +1,68 @@
-pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-    let mut nums = nums;
-    nums.sort_unstable();
-    k_sum(&nums, target as _, 0, 4)
+use std::collections::{HashMap, VecDeque};
+
+pub fn second_minimum(n: i32, edges: Vec<[i32; 2]>, time: i32, change: i32) -> i32 {
+    let edges: HashMap<i32, Vec<i32>> =
+        edges.into_iter().fold(HashMap::new(), |mut acc, [n1, n2]| {
+            acc.entry(n1).or_default().push(n2);
+            acc.entry(n2).or_default().push(n1);
+            acc
+        });
+    bfs(n, &edges, time, change)
 }
 
-fn k_sum(nums: &[i32], target: i64, start: usize, k: i32) -> Vec<Vec<i32>> {
-    let mut res = vec![];
-    if start == nums.len() {
-        return res;
-    }
+fn bfs(n: i32, edges: &HashMap<i32, Vec<i32>>, time: i32, change: i32) -> i32 {
+    let mut dist1: Vec<_> = (0..n).map(|_| -1).collect();
+    let mut dist2 = dist1.clone();
+    let mut queue = VecDeque::new();
+    dist1[0] = 0;
+    // (node, frequency)
+    queue.push_back((1, 1));
 
-    let average = target / k as i64;
-    if nums[start] as i64 > average || nums.last().is_some_and(|&n| (n as i64) < average) {
-        return res;
-    }
-
-    if k == 2 {
-        return two_sum(nums, target, start);
-    }
-
-    for i in start..nums.len() {
-        if i == start || nums.get(i - 1).is_some_and(|&n| n != nums[i]) {
-            for subset in k_sum(nums, target - nums[i] as i64, i + 1, k - 1) {
-                let mut v = vec![nums[i]];
-                v.extend(subset);
-                res.push(v)
+    while let Some((node, freq)) = queue.pop_front() {
+        let time_taken = {
+            let past_time = if freq == 1 {
+                dist1[node as usize - 1]
+            } else {
+                dist2[node as usize - 1]
+            };
+            if (past_time / change) & 1 == 1 {
+                // run into red light
+                change * (past_time / change + 1) + time
+            } else {
+                past_time + time
+            }
+        };
+        for &neighbor in edges[&node].iter() {
+            if dist1[neighbor as usize - 1] == -1 {
+                dist1[neighbor as usize - 1] = time_taken;
+                queue.push_back((neighbor, 1));
+            } else if dist2[neighbor as usize - 1] == -1
+                && dist1[neighbor as usize - 1] != time_taken
+            {
+                if neighbor == n {
+                    return time_taken;
+                }
+                dist2[neighbor as usize - 1] = time_taken;
+                queue.push_back((neighbor, 2));
             }
         }
     }
-
-    res
-}
-
-fn two_sum(nums: &[i32], target: i64, start: usize) -> Vec<Vec<i32>> {
-    let mut res = vec![];
-    let (mut left, mut right) = (start, nums.len() - 1);
-    while left < right {
-        let curr = nums[left] + nums[right];
-        if (curr as i64) < target || (left > start && nums[left] == nums[left - 1]) {
-            // curr is smaller || skip repeating numbers
-            left += 1
-        } else if curr as i64 > target || (right < nums.len() - 1 && nums[right] == nums[right + 1])
-        {
-            right -= 1;
-        } else {
-            res.push(vec![nums[left], nums[right]]);
-            left += 1;
-            right -= 1
-        }
-    }
-    res
+    0
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
     fn basics() {
-        {
-            let mut res: Vec<_> = four_sum(vec![1, 0, -1, 0, -2, 2], 0)
-                .into_iter()
-                .map(|mut v| {
-                    v.sort_unstable();
-                    v
-                })
-                .collect();
-            res.sort_unstable();
-            debug_assert_eq!(
-                res,
-                vec![vec![-2, -1, 1, 2], vec![-2, 0, 0, 2], vec![-1, 0, 0, 1]]
-            )
-        };
-        {
-            let mut res: Vec<_> = four_sum(vec![2, 2, 2, 2, 2], 8)
-                .into_iter()
-                .map(|mut v| {
-                    v.sort_unstable();
-                    v
-                })
-                .collect();
-            res.sort_unstable();
-            debug_assert_eq!(res, vec![vec![2, 2, 2, 2]])
-        };
+        debug_assert_eq!(
+            second_minimum(5, vec![[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]], 3, 5),
+            13
+        );
+        debug_assert_eq!(second_minimum(2, vec![[1, 2]], 3, 2), 11)
     }
 
     #[test]
-    fn test() {
-        {
-            let mut res: Vec<_> = four_sum(vec![-2, -1, -1, 1, 1, 2, 2], 0)
-                .into_iter()
-                .map(|mut v| {
-                    v.sort_unstable();
-                    v
-                })
-                .collect();
-            res.sort_unstable();
-            debug_assert_eq!(res, vec![vec![-2, -1, 1, 2], vec![-1, -1, 1, 1]])
-        };
-        {
-            let mut res: Vec<_> = four_sum(
-                vec![
-                    -1000000000,
-                    -1000000000,
-                    1000000000,
-                    -1000000000,
-                    -1000000000,
-                ],
-                294967296,
-            )
-            .into_iter()
-            .map(|mut v| {
-                v.sort_unstable();
-                v
-            })
-            .collect();
-            res.sort_unstable();
-            debug_assert_eq!(res, Vec::<Vec<_>>::new())
-        };
-    }
+    fn test() {}
 }
