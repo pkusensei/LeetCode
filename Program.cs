1f85cc6d4f15eecf04516315dba84ed7acb1eca9
pkusensei <ZW_08@outlook.com>
2024-07-28 16:42:47 -0400

    25 reverse chunk of linked list

--- Program.cs
+++ Program.cs
@@ -1,32 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text;
+﻿using System.Text;
 
 Test1();
 Test2();
-Test3();
 
 
 void Test1()
 {
-    ListNode n = new(1, new(2, new(3, new(4))));
-    var ans = SwapPairs(n);
-    Console.WriteLine(ans); // [2,1,4,3]
+    ListNode n = new(1, new(2, new(3, new(4, new(5)))));
+    var ans = ReverseKGroup(n, 2);
+    Console.WriteLine(ans); // [2,1,4,3,5]
 }
 
 void Test2()
 {
-    Console.WriteLine(SwapPairs(null));
-}
-
-void Test3()
-{
-    Console.WriteLine(SwapPairs(new(1))); // []
+    ListNode n = new(1, new(2, new(3, new(4, new(5)))));
+    var ans = ReverseKGroup(n, 3);
+    Console.WriteLine(ans); // [3,2,1,4,5]
 }
 
 
-ListNode SwapPairs(ListNode head)
+ListNode ReverseKGroup(ListNode head, int k)
 {
+    if (k < 2) { return head; }
+
     ListNode lst = new(0, head);
     (ListNode node, int step) fast = (lst, 0);
     (ListNode node, int step) slow = (lst, 0);
@@ -36,28 +32,48 @@ ListNode SwapPairs(ListNode head)
         fast.node = fast.node.next;
         fast.step += 1;
 
-        if (slow.step + 2 < fast.step)
+        if (slow.step + k < fast.step)
         {
             slow.node = slow.node.next;
             slow.step += 1;
         }
 
-        if ((fast.step & 1) == 0 && fast.node is not null)
+        if (fast.step % k == 0 && fast.node is not null)
         {
-            // slow prev fast 
-            //  0    1    2    3
-            //  0    2    1    3
-            var prev = slow.node.next;
-            prev.next = fast.node.next;
-            fast.node.next = prev;
-            slow.node.next = fast.node;
-            fast.node = slow.node.next.next;
+            // slow first     fast next
+            //  0    1    2    3    4
+            //  0    3    2    1    4
+            var next = fast.node.next;
+            var first = slow.node.next;
+            var (left, right) = ReverseK(first, fast.node, k);
+            slow.node.next = left;
+            right.next = next;
+            fast.node = right;
         }
     }
-
     return lst.next;
 }
 
+(ListNode, ListNode) ReverseK(ListNode left, ListNode right, int k)
+{
+    if (k == 2) { return SwapPair(left); }
+
+    var first = left;
+    left = left.next;
+    (left, right) = ReverseK(left, right, k - 1);
+    first.next = null;
+    right.next = first;
+    return (left, first);
+}
+
+(ListNode, ListNode) SwapPair(ListNode head)
+{
+    var left = head.next;
+    var right = head;
+    left.next = right;
+    right.next = null;
+    return (left, right);
+}
 
 class ListNode
 {
