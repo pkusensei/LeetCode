9c5463ece7dfc1e1d0a5ec097bb9c918e91839b9
pkusensei <ZW_08@outlook.com>
2024-07-29 08:18:28 -0400

    34 range of element

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,18 @@
-pub fn longest_valid_parentheses(s: &str) -> i32 {
-    // ")()())" => [0, 1, 1, 1, 1]
-    let mut bits: Vec<bool> = (0..s.len()).map(|_| false).collect();
-    let mut stack = vec![];
-    for (idx, ch) in s.bytes().enumerate() {
-        if stack.is_empty() || ch == b'(' {
-            stack.push((idx, ch));
-        } else if ch == b')' {
-            if let Some(&(i, b'(')) = stack.last() {
-                stack.pop();
-                bits[i] = true;
-                bits[idx] = true;
-            } else {
-                stack.push((idx, ch))
-            }
-        }
+pub fn search_range(nums: &[i32], target: i32) -> Vec<i32> {
+    if nums.is_empty() {
+        return vec![-1, -1];
     }
-    let mut consecutive = 0;
-    let mut res = 0;
-    for b in bits {
-        if b {
-            consecutive += 1;
-            res = res.max(consecutive);
-        } else {
-            consecutive = 0
+    let left = nums.partition_point(|&n| n < target);
+
+    if left == nums.len() || target != nums[left] {
+        vec![-1, -1]
+    } else {
+        let mut curr = left;
+        while nums.get(curr).is_some_and(|&n| n == target) {
+            curr += 1;
         }
+        vec![left as i32, curr as i32 - 1]
     }
-    res
 }
 
 #[cfg(test)]
@@ -34,9 +21,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(longest_valid_parentheses("(()"), 2);
-        debug_assert_eq!(longest_valid_parentheses(")()())"), 4);
-        debug_assert_eq!(longest_valid_parentheses(""), 0);
+        debug_assert_eq!(search_range(&[5, 7, 7, 8, 8, 10], 8), [3, 4]);
+        debug_assert_eq!(search_range(&[5, 7, 7, 8, 8, 10], 6), [-1, -1]);
+        debug_assert_eq!(search_range(&[], 0), [-1, -1]);
     }
 
     #[test]
