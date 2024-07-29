f7bdc3bf47682f0c7ae5653226a1b9068de7901d
pkusensei <ZW_08@outlook.com>
2024-07-29 19:53:12 -0400

    40 combination sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,31 @@
-use std::collections::HashSet;
-
-pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
     let mut nums = candidates;
-    nums.sort_unstable_by_key(|&n| std::cmp::Reverse(n));
-    solve(&nums, target).into_iter().collect()
+    nums.sort_unstable();
+    let mut res = vec![];
+    backtrace(&nums, target, &mut res, &mut vec![], 0);
+    res
 }
 
-fn solve(nums: &[i32], target: i32) -> HashSet<Vec<i32>> {
-    match nums {
-        [] => HashSet::new(),
-        [head, tail @ ..] => match target.cmp(head) {
-            std::cmp::Ordering::Less => solve(tail, target),
-            std::cmp::Ordering::Equal => {
-                let mut res = solve(tail, target);
-                res.insert(vec![*head]);
-                res
+fn backtrace(
+    nums: &[i32],
+    target: i32,
+    result: &mut Vec<Vec<i32>>,
+    curr: &mut Vec<i32>,
+    start: usize,
+) {
+    if target == 0 {
+        result.push(curr.clone());
+    } else {
+        for i in start..nums.len() {
+            if target < nums[i] {
+                break;
             }
-            std::cmp::Ordering::Greater => {
-                let mut res = solve(tail, target);
-                res.extend(solve(tail, target - head).into_iter().map(|mut v| {
-                    v.push(*head);
-                    v
-                }));
-                res.extend(solve(nums, target - head).into_iter().map(|mut v| {
-                    v.push(*head);
-                    v
-                }));
-                res
+            if i == start || nums[i] != nums[i - 1] {
+                curr.push(nums[i]);
+                backtrace(nums, target - nums[i], result, curr, i + 1);
+                curr.pop();
             }
-        },
+        }
     }
 }
 
@@ -39,14 +36,14 @@ mod tests {
     #[test]
     fn basics() {
         debug_assert_eq!(
-            combination_sum(vec![2, 3, 6, 7], 7),
-            [vec![2, 2, 3], vec![7]]
+            combination_sum2(vec![10, 1, 2, 7, 6, 1, 5], 8),
+            [vec![1, 1, 6], vec![1, 2, 5], vec![1, 7], vec![2, 6]]
         );
         debug_assert_eq!(
-            combination_sum(vec![2, 3, 5], 8),
-            [vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]]
+            combination_sum2(vec![2, 5, 2, 1, 2], 5),
+            [vec![1, 2, 2], vec![5]]
         );
-        debug_assert_eq!(combination_sum(vec![2], 1), Vec::<Vec<i32>>::new())
+        debug_assert_eq!(combination_sum2(vec![2], 1), Vec::<Vec<i32>>::new())
     }
 
     #[test]
