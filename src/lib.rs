761b2e38eeb3c2a1c8b108fed2820bd7b134555d
pkusensei <ZW_08@outlook.com>
2024-07-29 20:39:56 -0400

    41 first missing positive integer

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,16 @@
-pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-    let mut nums = candidates;
-    nums.sort_unstable();
-    let mut res = vec![];
-    backtrace(&nums, target, &mut res, &mut vec![], 0);
-    res
-}
-
-fn backtrace(
-    nums: &[i32],
-    target: i32,
-    result: &mut Vec<Vec<i32>>,
-    curr: &mut Vec<i32>,
-    start: usize,
-) {
-    if target == 0 {
-        result.push(curr.clone());
-    } else {
-        for i in start..nums.len() {
-            if target < nums[i] {
-                break;
-            }
-            if i == start || nums[i] != nums[i - 1] {
-                curr.push(nums[i]);
-                backtrace(nums, target - nums[i], result, curr, i + 1);
-                curr.pop();
-            }
+pub fn first_missing_positive(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let mut seen: Vec<_> = (0..=n).map(|_| false).collect();
+    for &num in nums {
+        if (1..=n).contains(&(num as usize)) {
+            seen[num as usize] = true;
         }
     }
+    seen.into_iter()
+        .enumerate()
+        .skip(1)
+        .find_map(|(i, b)| if !b { Some(i as i32) } else { None })
+        .unwrap_or(n as i32 + 1)
 }
 
 #[cfg(test)]
@@ -35,15 +19,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            combination_sum2(vec![10, 1, 2, 7, 6, 1, 5], 8),
-            [vec![1, 1, 6], vec![1, 2, 5], vec![1, 7], vec![2, 6]]
-        );
-        debug_assert_eq!(
-            combination_sum2(vec![2, 5, 2, 1, 2], 5),
-            [vec![1, 2, 2], vec![5]]
-        );
-        debug_assert_eq!(combination_sum2(vec![2], 1), Vec::<Vec<i32>>::new())
+        debug_assert_eq!(first_missing_positive(&[1, 2, 0]), 3);
+        debug_assert_eq!(first_missing_positive(&[3, 4, -1, 1]), 2);
+        debug_assert_eq!(first_missing_positive(&[7, 8, 9, 11, 12]), 1)
     }
 
     #[test]
