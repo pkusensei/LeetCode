33659d97f2dbbf5b0f4d3c627dc8fb3994e3f21c
pkusensei <ZW_08@outlook.com>
2024-08-01 08:15:59 -0400

    2678

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,9 @@
-pub fn is_match(s: &str, p: &str) -> bool {
-    let haystack: Vec<_> = s.chars().collect();
-    let pattern: Vec<_> = p.chars().collect();
-    solve(&haystack, &pattern, 0, 0, None, 0)
-}
-
-fn solve(
-    haystack: &[char],
-    pattern: &[char],
-    h_idx: usize,
-    p_idx: usize,
-    last_star: Option<usize>,
-    last_match: usize,
-) -> bool {
-    if h_idx == haystack.len() {
-        return pattern[p_idx..].iter().all(|&c| c == '*');
-    }
-
-    if h_idx < haystack.len() {
-        if pattern
-            .get(p_idx)
-            .is_some_and(|&p| p == '?' || haystack[h_idx] == p)
-        {
-            return solve(
-                haystack,
-                pattern,
-                h_idx + 1,
-                p_idx + 1,
-                last_star,
-                last_match,
-            );
-        } else if pattern.get(p_idx).is_some_and(|&p| p == '*') {
-            return solve(haystack, pattern, h_idx, p_idx + 1, Some(p_idx), h_idx);
-        } else if let Some(star) = last_star {
-            return solve(
-                haystack,
-                pattern,
-                last_match,
-                star + 1,
-                last_star,
-                last_match + 1,
-            );
-        }
-    }
-    false
+pub fn count_seniors(details: &[&str]) -> i32 {
+    details
+        .iter()
+        .filter_map(|line| line[11..13].parse::<i32>().ok())
+        .filter(|&n| n > 60)
+        .count() as _
 }
 
 #[cfg(test)]
@@ -51,9 +12,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(is_match("aa", "a"), false);
-        debug_assert_eq!(is_match("aa", "*"), true);
-        debug_assert_eq!(is_match("cb", "?a"), false)
+        debug_assert_eq!(
+            count_seniors(&["7868190130M7522", "5303914400F9211", "9273338290F4010"]),
+            2
+        );
+        debug_assert_eq!(count_seniors(&["1313579440F2036", "2921522980M5644"]), 0)
     }
 
     #[test]
