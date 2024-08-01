74ce5a367f9c4b22bd4043facbac845e9cacad84
pkusensei <ZW_08@outlook.com>
2024-08-01 18:18:28 -0400

    46 permutations of unique numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
-pub fn jump(nums: &[i32]) -> i32 {
-    let mut res = 0;
-    let (mut slow, mut fast) = (0, 0);
-    while fast < nums.len() - 1 {
-        let long_jump = (slow..=fast)
-            .map(|n| n + nums[n] as usize)
-            .max()
-            .unwrap_or_default();
-        slow = fast + 1;
-        fast = long_jump;
-        res += 1;
+pub fn permute(nums: &[i32]) -> Vec<Vec<i32>> {
+    match nums {
+        [head] => vec![vec![*head]],
+        [head, tail @ ..] => {
+            let mut res = vec![];
+            for mut candid in permute(tail) {
+                for i in 0..candid.len() {
+                    let mut v = candid.clone();
+                    v.insert(i, *head);
+                    res.push(v);
+                }
+                candid.push(*head);
+                res.push(candid)
+            }
+            res
+        }
+        [] => unreachable!(),
     }
-    res
 }
 
 #[cfg(test)]
@@ -19,8 +24,19 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(jump(&[2, 3, 1, 1, 4]), 2);
-        debug_assert_eq!(jump(&[2, 3, 0, 1, 4]), 2)
+        // debug_assert_eq!(
+        //     permute(&[1, 2, 3]),
+        //     vec![
+        //         vec![1, 2, 3],
+        //         vec![1, 3, 2],
+        //         vec![2, 1, 3],
+        //         vec![2, 3, 1],
+        //         vec![3, 1, 2],
+        //         vec![3, 2, 1]
+        //     ]
+        // );
+        debug_assert_eq!(permute(&[0, 1]), vec![vec![0, 1], vec![1, 0]]);
+        debug_assert_eq!(permute(&[1]), vec![vec![1]]);
     }
 
     #[test]
