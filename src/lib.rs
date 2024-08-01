4609ee02a156e63412301330e7692e3722122931
pkusensei <ZW_08@outlook.com>
2024-08-01 17:52:51 -0400

    45 greedy jump

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
-pub fn count_seniors(details: &[&str]) -> i32 {
-    details
-        .iter()
-        .filter_map(|line| line[11..13].parse::<i32>().ok())
-        .filter(|&n| n > 60)
-        .count() as _
+pub fn jump(nums: &[i32]) -> i32 {
+    let mut res = 0;
+    let (mut slow, mut fast) = (0, 0);
+    while fast < nums.len() - 1 {
+        let long_jump = (slow..=fast)
+            .map(|n| n + nums[n] as usize)
+            .max()
+            .unwrap_or_default();
+        slow = fast + 1;
+        fast = long_jump;
+        res += 1;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -12,11 +19,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            count_seniors(&["7868190130M7522", "5303914400F9211", "9273338290F4010"]),
-            2
-        );
-        debug_assert_eq!(count_seniors(&["1313579440F2036", "2921522980M5644"]), 0)
+        debug_assert_eq!(jump(&[2, 3, 1, 1, 4]), 2);
+        debug_assert_eq!(jump(&[2, 3, 0, 1, 4]), 2)
     }
 
     #[test]
