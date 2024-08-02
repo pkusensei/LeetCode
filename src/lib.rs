6baf1bbed210f977d001dd92db7634b35ed70fce
pkusensei <ZW_08@outlook.com>
2024-08-02 14:27:28 -0400

    51 brute force n-queens

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,51 @@
-pub fn my_pow(x: f64, n: i32) -> f64 {
-    solve(x, n.into())
+pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
+    match n {
+        1 => vec![vec!["Q".to_string()]],
+        4..=9 => {
+            let boards = solve(n as _, 0, vec![]);
+            boards
+                .into_iter()
+                .map(|board| {
+                    board
+                        .into_iter()
+                        .map(|col| {
+                            let mut s: Vec<_> = vec!['.'; n as _];
+                            s[col] = 'Q';
+                            s.into_iter().collect()
+                        })
+                        .collect()
+                })
+                .collect()
+        }
+        _ => vec![],
+    }
 }
 
-fn solve(x: f64, n: i64) -> f64 {
-    match (x, n) {
-        (_, 0) | (1.0, _) => 1.0,
-        (-1.0, _) => {
-            if n & 1 == 0 {
-                1.0
-            } else {
-                -1.0
-            }
-        }
-        (0.0, _) => 0.0,
-        _ => {
-            if n < 0 {
-                solve(1.0 / x, -n)
-            } else if n & 1 == 0 {
-                solve(x * x, n >> 1)
-            } else {
-                x * solve(x * x, n >> 1)
-            }
+fn solve(n: usize, row: usize, board: Vec<usize>) -> Vec<Vec<usize>> {
+    // board: Vec<usize>
+    // index as row, board[row] == col
+    if row == n {
+        return vec![board];
+    }
+
+    let mut res = vec![];
+    for col in 0..n {
+        if check(&board, row, col) {
+            let mut temp = board.clone();
+            temp.push(col);
+            res.extend(solve(n, row + 1, temp))
         }
     }
+    res
+}
+
+fn check(board: &[usize], row: usize, col: usize) -> bool {
+    row == board.len()
+        && board
+            .iter()
+            .copied()
+            .enumerate()
+            .all(|(r, c)| c != col && r.abs_diff(row) != c.abs_diff(col))
 }
 
 #[cfg(test)]
@@ -31,9 +54,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(my_pow(2.0000, 10), 1024.0000);
-        // debug_assert_eq!(my_pow(2.1000, 3), 9.26100);
-        debug_assert_eq!(my_pow(2.00000, -2), 0.25000);
+        debug_assert_eq!(
+            solve_n_queens(4),
+            [
+                [".Q..", "...Q", "Q...", "..Q."],
+                ["..Q.", "Q...", "...Q", ".Q.."]
+            ]
+        );
+        debug_assert_eq!(solve_n_queens(1), [["Q"]]);
     }
 
     #[test]
