2300f4832f1b586ad829f97f20e9a07e113bd5f0
pkusensei <ZW_08@outlook.com>
2024-08-02 10:17:44 -0400

    50 pow(x, n)

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,28 @@
-pub fn group_anagrams<'a>(strs: &[&'a str]) -> Vec<Vec<&'a str>> {
-    use std::collections::HashMap;
-    let res: HashMap<i128, Vec<_>> = strs.iter().fold(HashMap::new(), |mut acc, &s| {
-        let hash = s.bytes().fold(1, |acc, b| acc * letter_to_prime(b));
-        acc.entry(hash).or_default().push(s);
-        acc
-    });
-    res.into_values().collect()
+pub fn my_pow(x: f64, n: i32) -> f64 {
+    solve(x, n.into())
 }
 
-const fn letter_to_prime(byte: u8) -> i128 {
-    match byte {
-        b'a' => 2,
-        b'b' => 3,
-        b'c' => 5,
-        b'd' => 7,
-        b'e' => 11,
-        b'f' => 13,
-        b'g' => 17,
-        b'h' => 19,
-        b'i' => 23,
-        b'j' => 29,
-        b'k' => 31,
-        b'l' => 37,
-        b'm' => 41,
-        b'n' => 43,
-        b'o' => 47,
-        b'p' => 53,
-        b'q' => 59,
-        b'r' => 61,
-        b's' => 67,
-        b't' => 71,
-        b'u' => 73,
-        b'v' => 79,
-        b'w' => 83,
-        b'x' => 89,
-        b'y' => 97,
-        b'z' => 101,
-        _ => unreachable!(),
+fn solve(x: f64, n: i64) -> f64 {
+    match (x, n) {
+        (_, 0) | (1.0, _) => 1.0,
+        (-1.0, _) => {
+            if n & 1 == 0 {
+                1.0
+            } else {
+                -1.0
+            }
+        }
+        (0.0, _) => 0.0,
+        _ => {
+            if n < 0 {
+                solve(1.0 / x, -n)
+            } else if n & 1 == 0 {
+                solve(x * x, n >> 1)
+            } else {
+                x * solve(x * x, n >> 1)
+            }
+        }
     }
-}
-
-#[allow(unused)]
-fn f1<'a>(strs: &[&'a str]) -> Vec<Vec<&'a str>> {
-    use std::collections::BTreeMap;
-    fn count(s: &str) -> BTreeMap<u8, i32> {
-        s.bytes().fold(BTreeMap::new(), |mut acc, b| {
-            *acc.entry(b).or_insert(0) += 1;
-            acc
-        })
-    }
-
-    let res: BTreeMap<BTreeMap<u8, i32>, Vec<&str>> =
-        strs.iter().fold(BTreeMap::new(), |mut acc, s| {
-            let m = count(s);
-            acc.entry(m).or_default().push(s);
-            acc
-        });
-
-    res.into_values().collect()
 }
 
 #[cfg(test)]
@@ -66,12 +31,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        // debug_assert_eq!(
-        //     group_anagrams(&["eat", "tea", "tan", "ate", "nat", "bat"]),
-        //     [vec!["bat"], vec!["nat", "tan"], vec!["ate", "eat", "tea"]]
-        // );
-        debug_assert_eq!(group_anagrams(&[""]), [[""]]);
-        debug_assert_eq!(group_anagrams(&["a"]), [["a"]]);
+        debug_assert_eq!(my_pow(2.0000, 10), 1024.0000);
+        // debug_assert_eq!(my_pow(2.1000, 3), 9.26100);
+        debug_assert_eq!(my_pow(2.00000, -2), 0.25000);
     }
 
     #[test]
