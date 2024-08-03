3bd8d177093cd25c2cc1d66ee7d8b8a7b6d18c3f
pkusensei <ZW_08@outlook.com>
2024-08-03 15:34:28 -0400

    70 Fibonacci numbers in disguise

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,14 @@
-pub fn my_sqrt(x: i32) -> i32 {
-    match x {
-        0 => 0,
-        1..4 => 1,
-        _ => {
-            let (mut left, mut right) = (1 as u64, (x / 2) as u64);
-            while left <= right {
-                let mid = (right - left) / 2 + left;
-                match (mid * mid).cmp(&(x as u64)) {
-                    std::cmp::Ordering::Less => left = mid + 1,
-                    std::cmp::Ordering::Equal => return mid as _,
-                    std::cmp::Ordering::Greater => right = mid - 1,
-                }
-            }
-            right as _
-        }
+pub fn climb_stairs(n: i32) -> i32 {
+    if n < 2 {
+        return 1;
     }
+    let mut dp = vec![0; n as usize];
+    dp[0] = 1;
+    dp[1] = 2;
+    for i in 2..(n as usize) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    *dp.last().unwrap()
 }
 
 #[cfg(test)]
@@ -23,14 +17,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(my_sqrt(4), 2);
-        debug_assert_eq!(my_sqrt(8), 2);
-        debug_assert_eq!(my_sqrt(9), 3);
-        debug_assert_eq!(my_sqrt(12), 3);
+        debug_assert_eq!(climb_stairs(2), 2);
+        debug_assert_eq!(climb_stairs(3), 3);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(my_sqrt(2147395599), 46339);
-    }
+    fn test() {}
 }
