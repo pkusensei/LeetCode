f71677c930a145cd53c5db4f3a23f9ca48ea60ad
pkusensei <ZW_08@outlook.com>
2024-08-03 13:06:02 -0400

    66 vec of digits + 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,23 @@
-pub fn is_number(s: &str) -> bool {
-    let mut iter = s.trim().bytes().peekable();
-    let mut prev = None;
-    let mut seen_e = false;
-    let mut seen_point = false;
-    let mut seen_plus_minus = false;
-    while let Some(byte) = iter.next() {
-        match byte {
-            b'0'..=b'9' => prev = Some(byte),
-            b'+' | b'-' => {
-                if prev.is_some_and(|b| b != b'e') || seen_plus_minus || iter.peek().is_none() {
-                    return false;
-                }
-                prev = Some(byte);
-                seen_plus_minus = true
-            }
-            b'E' | b'e' => match prev {
-                None => return false,
-                Some(b) => {
-                    if !b.is_ascii_digit() || seen_e || iter.peek().is_none() {
-                        return false;
-                    }
-                    prev = Some(byte.to_ascii_lowercase());
-                    seen_e = true;
-                    seen_plus_minus = false
-                }
-            },
-            b'.' => {
-                if seen_e || seen_point {
-                    return false;
-                }
-                match prev {
-                    None => {
-                        if !iter
-                            .peek()
-                            .is_some_and(|&b| b.is_ascii_digit() || b == b'e')
-                        {
-                            return false;
-                        }
-                    }
-                    Some(b) => {
-                        if !b.is_ascii_digit() && b != b'+' && b != b'-' {
-                            return false;
-                        }
-                        if (b == b'+' || b == b'-')
-                            && !iter.peek().is_some_and(|p| p.is_ascii_digit())
-                        {
-                            return false;
-                        }
-                    }
-                }
-                seen_point = true;
-                seen_plus_minus = false;
-            }
-            _ => return false,
-        }
+pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    if digits.is_empty() {
+        return vec![1];
     }
-    true
+
+    digits.reverse();
+        let mut carry = 1;
+        for v in digits.iter_mut() {
+            if carry == 0 {
+                break;
+            }
+            let sum = *v + carry;
+            carry = sum / 10;
+            *v = sum % 10;
+        }
+    if carry > 0 {
+        digits.push(carry)
+    };
+    digits.reverse();
+    digits
 }
 
 #[cfg(test)]
@@ -64,13 +26,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(is_number("0"));
-        debug_assert!(!is_number("e"));
-        debug_assert!(!is_number("."));
+        debug_assert_eq!(plus_one(vec![1, 2, 3]), [1, 2, 4]);
+        debug_assert_eq!(plus_one(vec![4, 3, 2, 1]), [4, 3, 2, 2]);
+        debug_assert_eq!(plus_one(vec![9]), [1, 0]);
     }
 
     #[test]
-    fn test() {
-        debug_assert!(!is_number("+."))
-    }
+    fn test() {}
 }
