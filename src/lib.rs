f897982fc3818dd381001223e99a62cf79b9a66f
pkusensei <ZW_08@outlook.com>
2024-08-03 12:50:44 -0400

    65 validate f64 the edgy way

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,61 @@
-use std::collections::{BinaryHeap, HashMap};
-
-pub fn min_path_sum(grid: &[&[i32]]) -> i32 {
-    let row = grid.len();
-    let col = grid.first().map(|r| r.len()).unwrap_or_default();
-    if row * col == 0 {
-        return 0;
-    }
-
-    let mut dp = vec![vec![i32::MAX; col + 1]; row + 1];
-    dp[row][col - 1] = 0;
-    dp[row - 1][col] = 0;
-    for r in (0..row).rev() {
-        for c in (0..col).rev() {
-            dp[r][c] = grid[r][c] + dp[r][c + 1].min(dp[r + 1][c]);
-        }
-    }
-    dp[0][0]
-
-    // dijkstra(grid, (row - 1, col - 1))
-}
-
-#[allow(unused)]
-fn dijkstra(grid: &[&[i32]], goal: (usize, usize)) -> i32 {
-    let mut dist = HashMap::new();
-    for r in 0..=goal.0 {
-        for c in 0..=goal.1 {
-            dist.insert((r, c), i32::MAX);
-        }
-    }
-    let mut heap = BinaryHeap::new();
-    let start = Node {
-        row: 0,
-        col: 0,
-        cost: grid[0][0],
-    };
-    heap.push(start);
-    while let Some(Node { row, col, cost }) = heap.pop() {
-        if (row, col) == goal {
-            return cost;
-        }
-        if dist.get(&(row, col)).is_some_and(|&c| c < cost) {
-            continue;
-        }
-        for (r, c) in step(row, col, goal) {
-            let node = Node {
-                row: r,
-                col: c,
-                cost: cost + grid[r][c],
-            };
-            if dist.get(&(r, c)).is_some_and(|&cost| cost > node.cost) {
-                heap.push(node);
-                dist.insert((r, c), node.cost);
+pub fn is_number(s: &str) -> bool {
+    let mut iter = s.trim().bytes().peekable();
+    let mut prev = None;
+    let mut seen_e = false;
+    let mut seen_point = false;
+    let mut seen_plus_minus = false;
+    while let Some(byte) = iter.next() {
+        match byte {
+            b'0'..=b'9' => prev = Some(byte),
+            b'+' | b'-' => {
+                if prev.is_some_and(|b| b != b'e') || seen_plus_minus || iter.peek().is_none() {
+                    return false;
+                }
+                prev = Some(byte);
+                seen_plus_minus = true
             }
+            b'E' | b'e' => match prev {
+                None => return false,
+                Some(b) => {
+                    if !b.is_ascii_digit() || seen_e || iter.peek().is_none() {
+                        return false;
+                    }
+                    prev = Some(byte.to_ascii_lowercase());
+                    seen_e = true;
+                    seen_plus_minus = false
+                }
+            },
+            b'.' => {
+                if seen_e || seen_point {
+                    return false;
+                }
+                match prev {
+                    None => {
+                        if !iter
+                            .peek()
+                            .is_some_and(|&b| b.is_ascii_digit() || b == b'e')
+                        {
+                            return false;
+                        }
+                    }
+                    Some(b) => {
+                        if !b.is_ascii_digit() && b != b'+' && b != b'-' {
+                            return false;
+                        }
+                        if (b == b'+' || b == b'-')
+                            && !iter.peek().is_some_and(|p| p.is_ascii_digit())
+                        {
+                            return false;
+                        }
+                    }
+                }
+                seen_point = true;
+                seen_plus_minus = false;
+            }
+            _ => return false,
         }
     }
-    0
-}
-
-fn step(row: usize, col: usize, goal: (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
-    [(row + 1, col), (row, col + 1)]
-        .into_iter()
-        .filter(move |&p| p.0 <= goal.0 && p.1 <= goal.1)
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Node {
-    row: usize,
-    col: usize,
-    cost: i32,
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.cost.cmp(&self.cost)
-    }
+    true
 }
 
 #[cfg(test)]
@@ -88,10 +64,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_path_sum(&[&[1, 3, 1], &[1, 5, 1], &[4, 2, 1]]), 7);
-        debug_assert_eq!(min_path_sum(&[&[1, 2, 3], &[4, 5, 6]]), 12);
+        debug_assert!(is_number("0"));
+        debug_assert!(!is_number("e"));
+        debug_assert!(!is_number("."));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert!(!is_number("+."))
+    }
 }
