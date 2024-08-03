e96490142b7558a46db5978dd7b276a98e09ceca
pkusensei <ZW_08@outlook.com>
2024-08-03 17:42:14 -0400

    72 edit distance between 2 strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
-pub fn simplify_path(path: &str) -> String {
-        let mut stack = vec![];
-        for s in path.split('/').filter(|s| !s.is_empty()) {
-            match s {
-                "." => (),
-                ".." => {
-                    stack.pop();
-                }
-                _ => stack.push(s),
+pub fn min_distance(word1: &str, word2: &str) -> i32 {
+    if word1.is_empty() {
+        return word2.len() as _;
+    }
+    if word2.is_empty() {
+        return word1.len() as _;
+    }
+    if word1 == word2 {
+        return 0;
+    }
+    let row = word1.len();
+    let col = word2.len();
+    let mut dp = vec![vec![0; col + 1]; row + 1];
+
+    // First row and col means
+    // The dist between string of length x and empty string is x
+    for (j, v) in dp[0].iter_mut().enumerate() {
+        *v = j;
+    }
+    for (i, v) in dp.iter_mut().enumerate() {
+        v[0] = i
+    }
+    let w1 = word1.as_bytes();
+    let w2 = word2.as_bytes();
+    for i in 1..=row {
+        for j in 1..=col {
+            if w1[i - 1] == w2[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                // replacing i-1 with j-1
+                // delete i-1 from word1
+                // insert j-1 into word1
+                dp[i][j] = 1 + dp[i - 1][j - 1].min(dp[i - 1][j]).min(dp[i][j - 1]);
             }
         }
-        format!("/{}", stack.join("/"))
+    }
+    dp[row][col] as _
 }
 
 #[cfg(test)]
@@ -18,14 +43,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(simplify_path("/home/"), "/home");
-        debug_assert_eq!(simplify_path("/home//foo/"), "/home/foo");
-        debug_assert_eq!(
-            simplify_path("/home/user/Documents/../Pictures"),
-            "/home/user/Pictures"
-        );
-        debug_assert_eq!(simplify_path("/../"), "/");
-        debug_assert_eq!(simplify_path("/.../a/../b/c/../d/./"), "/.../b/d");
+        debug_assert_eq!(min_distance("horse", "ros"), 3);
+        debug_assert_eq!(min_distance("intention", "execution"), 5);
     }
 
     #[test]
