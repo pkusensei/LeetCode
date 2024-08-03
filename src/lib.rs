2f1dab4650c2668b605308ed4d3fc669cbcacde5
pkusensei <ZW_08@outlook.com>
2024-08-03 19:27:26 -0400

    74 binary search matrix

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,15 @@
-pub fn set_zeroes(matrix: &mut [Vec<i32>]) {
+pub fn search_matrix(matrix: &[&[i32]], target: i32) -> bool {
     let row = matrix.len();
     let col = matrix.first().map(|v| v.len()).unwrap_or_default();
     if row * col == 0 {
-        return;
+        return false;
     }
-
-    let row_flag = matrix[0].iter().any(|&n| n == 0);
-    let col_flag = (0..row).any(|r| matrix[r][0] == 0);
-
-    for i in 1..row {
-        for j in 1..col {
-            if matrix[i][j] == 0 {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
-            }
-        }
-    }
-    for i in 1..row {
-        for j in 1..col {
-            if matrix[i][0] == 0 || matrix[0][j] == 0 {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-    if row_flag {
-        for v in matrix[0].iter_mut() {
-            *v = 0
-        }
-    }
-    if col_flag {
-        for v in matrix.iter_mut() {
-            v[0] = 0
-        }
+    let first_col: Vec<i32> = matrix.iter().map(|v| v[0]).collect();
+    match first_col.binary_search(&target) {
+        Ok(_) => true,
+        Err(0) => false, // smaller than first element
+        Err(idx) => matrix[idx - 1].binary_search(&target).is_ok(),
+        // bigger then matrix[idx-1][0]
     }
 }
 
@@ -41,24 +19,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        {
-            let mut v = vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]];
-            set_zeroes(&mut v);
-            debug_assert_eq!(v, [[1, 0, 1], [0, 0, 0], [1, 0, 1]]);
-        }
-        {
-            let mut v = vec![vec![0, 1, 2, 0], vec![3, 4, 5, 2], vec![1, 3, 1, 5]];
-            set_zeroes(&mut v);
-            debug_assert_eq!(v, [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]);
-        }
+        debug_assert!(search_matrix(
+            &[&[1, 3, 5, 7], &[10, 11, 16, 20], &[23, 30, 34, 60]],
+            3
+        ));
+        debug_assert!(!search_matrix(
+            &[&[1, 3, 5, 7], &[10, 11, 16, 20], &[23, 30, 34, 60]],
+            14
+        ));
     }
 
     #[test]
-    fn test() {
-        {
-            let mut v = vec![vec![1, 0]];
-            set_zeroes(&mut v);
-            debug_assert_eq!(v, [[0, 0]]);
-        }
-    }
+    fn test() {}
 }
