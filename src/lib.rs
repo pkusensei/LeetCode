8a24d5fd52c8ac2af88963d4449c53c7e9821089
pkusensei <ZW_08@outlook.com>
2024-08-03 11:06:12 -0400

    63 dp count unique paths with obstacles

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
-pub fn unique_paths(m: i32, n: i32) -> i32 {
-    let mut dp = vec![1; n as usize];
-    for _ in 1..m {
-        for j in 1..n {
-            dp[j as usize] += dp[j as usize - 1];
+pub fn unique_paths_with_obstacles(obstacle_grid: &[&[i32]]) -> i32 {
+    let row = obstacle_grid.len();
+    let col = obstacle_grid.first().map(|r| r.len()).unwrap_or_default();
+    if row * col == 0 || obstacle_grid[0][0] == 1 || obstacle_grid[row - 1][col - 1] == 1 {
+        return 0;
+    }
+
+    let mut dp = vec![vec![0; col]; row];
+    dp[0][0] = 1;
+    for i in 0..row {
+        for j in 0..col {
+            if obstacle_grid[i][j] == 1 {
+                dp[i][j] = 0;
+            } else {
+                if i > 0 {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if j > 0 {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
         }
     }
-    *dp.last().unwrap()
+    dp[row - 1][col - 1]
 }
 
 #[cfg(test)]
@@ -14,8 +30,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(unique_paths(3, 7), 28);
-        debug_assert_eq!(unique_paths(3, 2), 3);
+        debug_assert_eq!(
+            unique_paths_with_obstacles(&[&[0, 0, 0], &[0, 1, 0], &[0, 0, 0]]),
+            2
+        );
+        debug_assert_eq!(unique_paths_with_obstacles(&[&[0, 1], &[0, 0]]), 1);
     }
 
     #[test]
