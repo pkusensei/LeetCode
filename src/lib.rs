7399d1e8aa28d4beeb561985d92430eaac20c8cd
pkusensei <ZW_08@outlook.com>
2024-08-02 21:41:22 -0400

    60 generate nth permutation of all without brute force

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,26 @@
-pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
-    let (mut row_min, mut row_max) = (0, n as usize - 1);
-    let (mut col_min, mut col_max) = (0, n as usize - 1);
+pub fn get_permutation(n: i32, k: i32) -> String {
+    let digits: Vec<_> = (1..=n)
+        .map(|n| char::from_digit(n as _, 10).unwrap())
+        .collect();
+    solve(digits, n, k - 1) // 0 indexed
+}
 
-    let (mut row, mut col) = (0, 0);
-    let mut res = vec![vec![0; n as usize]; n as usize];
-    let mut curr = 1;
-    loop {
-        while col < col_max {
-            res[row][col] = curr;
-            col += 1;
-            curr += 1
-        }
-        res[row][col] = curr;
-        curr += 1;
-        if curr > n * n {
-            break;
-        }
-
-        row_min += 1;
-        row = row_min;
-        while row < row_max {
-            res[row][col] = curr;
-            curr += 1;
-            row += 1;
-        }
-        res[row][col] = curr;
-        curr += 1;
-        if curr > n * n {
-            break;
-        }
-
-        col_max -= 1;
-        col = col_max;
-        while col > col_min {
-            res[row][col] = curr;
-            curr += 1;
-            col -= 1;
-        }
-        res[row][col] = curr;
-        curr += 1;
-        if curr > n * n {
-            break;
-        }
-
-        row_max -= 1;
-        row = row_max;
-        while row > row_min {
-            res[row][col] = curr;
-            curr += 1;
-            row -= 1;
-        }
-        res[row][col] = curr;
-        curr += 1;
-        if curr > n * n {
-            break;
-        }
-        col_min += 1;
-        col = col_min
+fn solve(mut digits: Vec<char>, n: i32, k: i32) -> String {
+    if n == 1 {
+        return digits[0].to_string();
     }
-    res
+    let group = factorial(n - 1);
+    let idx = k / group;
+    let ch = digits.remove(idx as _);
+    format!("{}{}", ch, solve(digits, n - 1, k % group))
+}
+
+const fn factorial(n: i32) -> i32 {
+    if n < 2 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
 }
 
 #[cfg(test)]
@@ -67,8 +29,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(generate_matrix(3), [[1, 2, 3], [8, 9, 4], [7, 6, 5]]);
-        debug_assert_eq!(generate_matrix(1), [[1]]);
+        debug_assert_eq!(get_permutation(3, 3), "213");
+        debug_assert_eq!(get_permutation(4, 9), "2314");
+        debug_assert_eq!(get_permutation(3, 1), "123");
     }
 
     #[test]
