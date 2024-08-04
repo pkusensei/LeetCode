a7c4fe0b005fdacc8c424554c0f45e853d280ee3
pkusensei <ZW_08@outlook.com>
2024-08-04 08:24:07 -0400

    1508 sort subarray sums with priority queue/binary heap

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,46 @@
-pub fn sort_colors(nums: &mut [i32]) {
-    if nums.len() < 2 {
-        return;
-    }
-    let (mut left, mut right) = (0, nums.len() - 1);
-    let mut i = 0;
-    while left < right && i <= right {
-        if nums[i] == 0 {
-            nums.swap(i, left);
-            left += 1;
-            i += 1;
-        } else if nums[i] == 2 {
-            nums.swap(i, right);
-            right -= 1;
-        } else {
-            i += 1
+pub fn range_sum(nums: &[i32], _n: i32, left: i32, right: i32) -> i32 {
+    const MODULUS: i32 = 10i32.pow(9) + 7;
+
+    let mut queue: std::collections::BinaryHeap<_> = nums
+        .iter()
+        .copied()
+        .enumerate()
+        .map(|(i, num)| Pair { num, end: i })
+        .collect();
+    let mut res = 0;
+    for i in 1..=right {
+        // or 0..right; but later: left - 1 <= i
+        let Some(Pair { num, end }) = queue.pop() else {
+            return res;
+        };
+        if left <= i {
+            res = (res + num) % MODULUS;
         }
+        if let Some(&n) = nums.get(end + 1) {
+            queue.push(Pair {
+                num: num + n,
+                end: end + 1,
+            });
+        }
+    }
+    res
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Pair {
+    num: i32,
+    end: usize,
+}
+
+impl PartialOrd for Pair {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Pair {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.num.cmp(&self.num).then(self.end.cmp(&other.end))
     }
 }
 
@@ -24,16 +50,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        {
-            let mut v = [2, 0, 2, 1, 1, 0];
-            sort_colors(&mut v);
-            debug_assert_eq!(v, [0, 0, 1, 1, 2, 2]);
-        }
-        {
-            let mut v = [2, 0, 1];
-            sort_colors(&mut v);
-            debug_assert_eq!(v, [0, 1, 2]);
-        }
+        debug_assert_eq!(range_sum(&[1, 2, 3, 4], 4, 1, 2), 3);
+        debug_assert_eq!(range_sum(&[1, 2, 3, 4], 4, 1, 5), 13);
+        debug_assert_eq!(range_sum(&[1, 2, 3, 4], 4, 3, 4), 6);
+        debug_assert_eq!(range_sum(&[1, 2, 3, 4], 4, 1, 10), 50);
     }
 
     #[test]
