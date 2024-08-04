d2b609439fad8887ee854ff1b4ab3fbae68acb95
pkusensei <ZW_08@outlook.com>
2024-08-04 19:15:25 -0400

    81 binary search vec with dup items

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,29 @@
-pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    if nums.len() <= 2 {
-        return nums.len() as _;
+pub fn search(nums: &[i32], target: i32) -> bool {
+    let pivot =
+        nums.windows(2)
+            .enumerate()
+            .find_map(|(idx, w)| if w[0] > w[1] { Some(idx + 1) } else { None });
+    if let Some(pivot) = pivot {
+        binary_search(&nums[0..pivot], target) || binary_search(&nums[pivot..], target)
+    } else {
+        binary_search(nums, target)
     }
-    let (mut slow, mut fast) = (0, 2);
-    while fast < nums.len() {
-        if nums[slow] == nums[fast] {
-            nums.remove(fast);
-        } else {
-            slow += 1;
-            fast += 1
+}
+
+fn binary_search(nums: &[i32], target: i32) -> bool {
+    let (mut left, mut right) = (0, nums.len() - 1);
+    while left <= right {
+        let mid = (right - left) / 2 + left;
+        match nums[mid].cmp(&target) {
+            std::cmp::Ordering::Less | std::cmp::Ordering::Greater if left == right => {
+                return false
+            }
+            std::cmp::Ordering::Less => left = mid + 1,
+            std::cmp::Ordering::Equal => return true,
+            std::cmp::Ordering::Greater => right = mid,
         }
     }
-    nums.len() as _
+    false
 }
 
 #[cfg(test)]
@@ -20,10 +32,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(remove_duplicates(&mut vec![1, 1, 1, 2, 2, 3]), 5);
-        debug_assert_eq!(remove_duplicates(&mut vec![0, 0, 1, 1, 1, 1, 2, 3, 3]), 7);
+        debug_assert!(search(&[2, 5, 6, 0, 0, 1, 2], 0));
+        debug_assert!(!search(&[2, 5, 6, 0, 0, 1, 2], 3));
+        debug_assert!(search(&[1, 0, 1, 1, 1], 0));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert!(search(&[2, 2, 2, 3, 2, 2, 2], 3))
+    }
 }
