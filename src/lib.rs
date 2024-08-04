0749ba08f85a1bd56ebbeb7044415dec076104e0
pkusensei <ZW_08@outlook.com>
2024-08-04 10:09:26 -0400

    77 generate all combinations

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,20 @@
-use std::collections::HashMap;
-
-pub fn min_window(s: &str, t: &str) -> String {
-    let (s1, s2) = (s.len(), t.len());
-    if s1 < s2 {
-        return String::new();
+pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+    if k == 1 {
+        (1..=n).map(|n| vec![n]).collect()
+    } else if k < n {
+        // use n
+        combine(n - 1, k - 1)
+            .into_iter()
+            .map(|mut v| {
+                v.push(n);
+                v
+            })
+            .chain(combine(n - 1, k)) // skip n
+            .collect()
+    } else {
+        // k == n
+        vec![(1..=n).collect()]
     }
-    if s == t {
-        return s.to_string();
-    }
-
-    let needle = t.bytes().fold(HashMap::new(), |mut acc, b| {
-        *acc.entry(b).or_insert(0) += 1;
-        acc
-    });
-    let hay = s.as_bytes();
-    let mut pairs = vec![];
-    let (mut left, mut right) = (0, t.len());
-    let mut window = hay[left..right].iter().fold(HashMap::new(), |mut acc, &b| {
-        if needle.contains_key(&b) {
-            *acc.entry(b).or_insert(0) += 1;
-        }
-        acc
-    });
-    right = expand_right(&mut window, &needle, right, hay);
-    if contains(&window, &needle) {
-        pairs.push((left, right));
-    }
-    while left + t.len() < right {
-        if needle.contains_key(&hay[left]) {
-            if let Some(v) = window.get_mut(&hay[left]) {
-                *v -= 1
-            }
-            left += 1;
-            // if contains == true, expand_right does nothing
-            right = expand_right(&mut window, &needle, right, hay);
-            if contains(&window, &needle) {
-                pairs.push((left, right));
-            }
-        } else {
-            left += 1;
-            if contains(&window, &needle) {
-                pairs.push((left, right));
-            }
-        }
-    }
-    pairs
-        .into_iter()
-        .min_by_key(|(l, r)| r - l)
-        .map(|(l, r)| s[l..r].to_string())
-        .unwrap_or_default()
-}
-
-fn expand_right(
-    window: &mut HashMap<u8, i32>,
-    needle: &HashMap<u8, i32>,
-    mut right: usize,
-    hay: &[u8],
-) -> usize {
-    while !contains(window, needle) && right < hay.len() {
-        if needle.contains_key(&hay[right]) {
-            *window.entry(hay[right]).or_insert(0) += 1;
-        }
-        right += 1
-    }
-    right
-}
-
-fn contains(window: &HashMap<u8, i32>, needle: &HashMap<u8, i32>) -> bool {
-    window.len() == needle.len()
-        && needle
-            .iter()
-            .all(|(k, v)| window.get(k).is_some_and(|count| count >= v))
 }
 
 #[cfg(test)]
@@ -79,13 +23,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_window("ADOBECODEBANC", "ABC"), "BANC");
-        debug_assert_eq!(min_window("a", "a"), "a");
-        debug_assert_eq!(min_window("a", "aa"), "");
+        // debug_assert_eq!(
+        //     combine(4, 2),
+        //     [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+        // );
+        debug_assert_eq!(combine(1, 1), [[1]])
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(min_window("ab", "A"), "")
-    }
+    fn test() {}
 }
