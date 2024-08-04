f78446704a7ce907906946128f84c118f71ccd4c
pkusensei <ZW_08@outlook.com>
2024-08-03 20:01:39 -0400

    75 Dutch National Flag problem; sort 0s, 1s, and 2s in place

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
-pub fn search_matrix(matrix: &[&[i32]], target: i32) -> bool {
-    let row = matrix.len();
-    let col = matrix.first().map(|v| v.len()).unwrap_or_default();
-    if row * col == 0 {
-        return false;
-    }
-    let first_col: Vec<i32> = matrix.iter().map(|v| v[0]).collect();
-    match first_col.binary_search(&target) {
-        Ok(_) => true,
-        Err(0) => false, // smaller than first element
-        Err(idx) => matrix[idx - 1].binary_search(&target).is_ok(),
-        // bigger then matrix[idx-1][0]
-    }
+pub fn sort_colors(nums: &mut [i32]) {
+        if nums.len() < 2 {
+            return;
+        }
+        let (mut left, mut right) = (0, nums.len() - 1);
+        let mut i = 0;
+        while left < right && i <= right {
+            if nums[i] == 0 {
+                nums.swap(i, left);
+                left += 1;
+                i += 1;
+            } else if nums[i] == 2 {
+                nums.swap(i, right);
+                right -= 1;
+            } else {
+                i += 1
+            }
+        }
 }
 
 #[cfg(test)]
@@ -19,14 +24,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(search_matrix(
-            &[&[1, 3, 5, 7], &[10, 11, 16, 20], &[23, 30, 34, 60]],
-            3
-        ));
-        debug_assert!(!search_matrix(
-            &[&[1, 3, 5, 7], &[10, 11, 16, 20], &[23, 30, 34, 60]],
-            14
-        ));
+        {
+            let mut v = [2, 0, 2, 1, 1, 0];
+            sort_colors(&mut v);
+            debug_assert_eq!(v, [0, 0, 1, 1, 2, 2]);
+        }
+        {
+            let mut v = [2, 0, 1];
+            sort_colors(&mut v);
+            debug_assert_eq!(v, [0, 1, 2]);
+        }
     }
 
     #[test]
