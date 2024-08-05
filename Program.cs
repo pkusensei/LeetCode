87ec7344039aaac1bcc5cf01b39281dc6804d0e5
pkusensei <ZW_08@outlook.com>
2024-08-04 21:48:47 -0400

    83 dedup sorted linked list

--- Program.cs
+++ Program.cs
@@ -8,41 +8,36 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    ListNode n = DeleteDuplicates(ListNode.Make([1, 2, 3, 3, 4, 4, 5]));
-    var a = "[1,2,5]";
+    ListNode n = DeleteDuplicates(ListNode.Make([1, 1, 2]));
+    var a = "[1,2]";
     Debug.Assert(n.ToString() == a, $"Output: {n}\nExpect: {a}");
 }
 
 void Test2()
 {
-    ListNode n = DeleteDuplicates(ListNode.Make([1, 1, 1, 2, 3]));
-    var a = "[2,3]";
+    ListNode n = DeleteDuplicates(ListNode.Make([1, 1, 2, 3, 3]));
+    var a = "[1,2,3]";
     Debug.Assert(n.ToString() == a, $"Output: {n}\nExpect: {a}");
 }
 
 
 ListNode DeleteDuplicates(ListNode head)
 {
-    ListNode dummy = new(0, head);
-    var (slow, fast) = (dummy, dummy.next);
-    while (fast is not null)
-    {
-        while (fast.next is not null && fast.val == fast.next.val)
-        {
-            fast = fast.next;
-        }
+        if (head is null) { return head; }
 
-        if (slow.next == fast)
+        var curr = head;
+        while (curr is not null && curr.next is not null)
         {
-            slow = slow.next;
+            if (curr.val == curr.next.val)
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            {
+                curr = curr.next;
+            }
         }
-        else
-        {
-            slow.next = fast.next;
-        }
-        fast = fast.next;
-    }
-    return dummy.next;
+        return head;
 }
 
 class ListNode : IEnumerable<int>
