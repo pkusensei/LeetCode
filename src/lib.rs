a708d94b34e3f14387170d9531c20de13af9a876
pkusensei <ZW_08@outlook.com>
2024-08-04 20:27:40 -0400

    2053 kth unique string in array

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,26 @@
-pub fn search(nums: &[i32], target: i32) -> bool {
-    let pivot =
-        nums.windows(2)
-            .enumerate()
-            .find_map(|(idx, w)| if w[0] > w[1] { Some(idx + 1) } else { None });
-    if let Some(pivot) = pivot {
-        binary_search(&nums[0..pivot], target) || binary_search(&nums[pivot..], target)
-    } else {
-        binary_search(nums, target)
-    }
-}
-
-fn binary_search(nums: &[i32], target: i32) -> bool {
-    let (mut left, mut right) = (0, nums.len() - 1);
-    while left <= right {
-        let mid = (right - left) / 2 + left;
-        match nums[mid].cmp(&target) {
-            std::cmp::Ordering::Less | std::cmp::Ordering::Greater if left == right => {
-                return false
-            }
-            std::cmp::Ordering::Less => left = mid + 1,
-            std::cmp::Ordering::Equal => return true,
-            std::cmp::Ordering::Greater => right = mid,
-        }
-    }
-    false
+pub fn kth_distinct(arr: &[&str], k: i32) -> String {
+    let mut strs: Vec<_> = arr
+        .iter()
+        .enumerate()
+        .fold(std::collections::HashMap::new(), |mut acc, (idx, s)| {
+            acc.entry(s).or_insert((idx, 0)).1 += 1;
+            acc
+        })
+        .into_iter()
+        .filter_map(
+            |(s, (idx, count))| {
+                if count == 1 {
+                    Some((s, idx))
+                } else {
+                    None
+                }
+            },
+        )
+        .collect();
+    strs.sort_unstable_by_key(|(_s, i)| *i);
+    strs.get(k as usize - 1)
+        .map(|(s, _i)| s.to_string())
+        .unwrap_or_default()
 }
 
 #[cfg(test)]
@@ -32,13 +29,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(search(&[2, 5, 6, 0, 0, 1, 2], 0));
-        debug_assert!(!search(&[2, 5, 6, 0, 0, 1, 2], 3));
-        debug_assert!(search(&[1, 0, 1, 1, 1], 0));
+        debug_assert_eq!(kth_distinct(&["d", "b", "c", "b", "c", "a"], 2), "a");
+        debug_assert_eq!(kth_distinct(&["aaa", "aa", "a"], 1), "aaa");
+        debug_assert_eq!(kth_distinct(&["a", "b", "a"], 3), "");
     }
 
     #[test]
-    fn test() {
-        debug_assert!(search(&[2, 2, 2, 3, 2, 2, 2], 3))
-    }
+    fn test() {}
 }
