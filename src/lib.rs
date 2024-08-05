b7d1654e9b000623973e35922f6a0671bbefda00
pkusensei <ZW_08@outlook.com>
2024-08-05 10:42:33 -0400

    85 expand on 84 to find max rectangle in matrix

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
-pub fn largest_rectangle_area(heights: &[i32]) -> i32 {
-    let it = heights.iter().copied().enumerate();
-    let prevs = smallers(heights, it.clone());
-    let mut nexts = smallers(heights, it.clone().rev());
+pub fn maximal_rectangle(matrix: &[&[char]]) -> i32 {
+    let row = matrix.len();
+    let col = matrix.first().map(|r| r.len()).unwrap_or_default();
+    if row * col == 0 {
+        return 0;
+    }
+
+    let (_, area) = matrix.iter().fold((vec![0; col], 0), |(nums, area), line| {
+        let nums: Vec<_> = nums
+            .into_iter()
+            .zip(line.iter())
+            .map(|(n, ch)| if *ch == '0' { 0 } else { n + 1 })
+            .collect();
+        let area = area.max(largest_rectangle_area(&nums));
+        (nums, area)
+    });
+    area
+}
+
+fn largest_rectangle_area(nums: &[i32]) -> i32 {
+    let it = nums.iter().copied().enumerate();
+    let prevs = smallers(nums, it.clone());
+    let mut nexts = smallers(nums, it.clone().rev());
     nexts.reverse();
 
     it.fold(0, |acc, (idx, num)| {
         let left = prevs[idx].unwrap_or(-1);
-        let right = nexts[idx].unwrap_or(heights.len() as i32);
+        let right = nexts[idx].unwrap_or(nums.len() as i32);
         acc.max((right - left - 1) * num)
     })
 }
@@ -30,13 +49,19 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(largest_rectangle_area(&[2, 1, 5, 6, 2, 3]), 10);
-        debug_assert_eq!(largest_rectangle_area(&[2, 4]), 4);
+        debug_assert_eq!(
+            maximal_rectangle(&[
+                &['1', '0', '1', '0', '0'],
+                &['1', '0', '1', '1', '1'],
+                &['1', '1', '1', '1', '1'],
+                &['1', '0', '0', '1', '0']
+            ]),
+            6
+        );
+        debug_assert_eq!(maximal_rectangle(&[&['0']]), 0);
+        debug_assert_eq!(maximal_rectangle(&[&['1']]), 1);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(largest_rectangle_area(&[1, 1]), 2);
-        debug_assert_eq!(largest_rectangle_area(&[2, 3]), 4);
-    }
+    fn test() {}
 }
