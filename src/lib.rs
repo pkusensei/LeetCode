067ecdf5f32b4ad97e585df1a8d495d17170fd86
pkusensei <ZW_08@outlook.com>
2024-08-05 15:42:50 -0400

    87 dfs and dp; scramble strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,51 @@
-pub fn maximal_rectangle(matrix: &[&[char]]) -> i32 {
-    let row = matrix.len();
-    let col = matrix.first().map(|r| r.len()).unwrap_or_default();
-    if row * col == 0 {
-        return 0;
+use std::collections::HashMap;
+
+pub fn is_scramble(s1: &str, s2: &str) -> bool {
+    dfs(s1, s2, &mut HashMap::new())
+}
+
+fn dfs<'a>(s1: &'a str, s2: &'a str, seen: &mut HashMap<(&'a str, &'a str), bool>) -> bool {
+    if let Some(&res) = seen.get(&(s1, s2)) {
+        return res;
+    }
+    if s1.len() != s2.len() {
+        seen.insert((s1, s2), false);
+        return false;
+    }
+    if s1 == s2 {
+        seen.insert((s1, s2), true);
+        return true;
+    }
+    if !is_anagram(s1, s2) {
+        seen.insert((s1, s2), false);
+        return false;
     }
 
-    let (_, area) = matrix.iter().fold((vec![0; col], 0), |(nums, area), line| {
-        let nums: Vec<_> = nums
-            .into_iter()
-            .zip(line.iter())
-            .map(|(n, ch)| if *ch == '0' { 0 } else { n + 1 })
-            .collect();
-        let area = area.max(largest_rectangle_area(&nums));
-        (nums, area)
-    });
-    area
-}
-
-fn largest_rectangle_area(nums: &[i32]) -> i32 {
-    let it = nums.iter().copied().enumerate();
-    let prevs = smallers(nums, it.clone());
-    let mut nexts = smallers(nums, it.clone().rev());
-    nexts.reverse();
-
-    it.fold(0, |acc, (idx, num)| {
-        let left = prevs[idx].unwrap_or(-1);
-        let right = nexts[idx].unwrap_or(nums.len() as i32);
-        acc.max((right - left - 1) * num)
-    })
-}
-
-fn smallers(nums: &[i32], it: impl Iterator<Item = (usize, i32)>) -> Vec<Option<i32>> {
-    let mut stack = vec![];
-    let mut res = Vec::with_capacity(nums.len());
-    for (idx, num) in it {
-        while stack.last().is_some_and(|&i| nums[i] >= num) {
-            stack.pop();
+    let size = s1.len();
+    for length in 1..size {
+        let res = (dfs(&s1[..length], &s2[..length], seen)
+            && dfs(&s1[length..], &s2[length..], seen))
+            || (dfs(&s1[..length], &s2[size - length..], seen)
+                && dfs(&s1[length..], &s2[..size - length], seen));
+        if res {
+            seen.insert((s1, s2), true);
+            return true;
         }
-        res.push(stack.last().copied().map(|i| i as i32));
-        stack.push(idx);
     }
-    res
+
+    seen.insert((s1, s2), false);
+    false
+}
+
+fn is_anagram(s1: &str, s2: &str) -> bool {
+    let mut count = [0; 26];
+    for b in s1.bytes() {
+        count[(b - b'a') as usize] += 1;
+    }
+    for b in s2.bytes() {
+        count[(b - b'a') as usize] -= 1;
+    }
+    count.into_iter().all(|c| c == 0)
 }
 
 #[cfg(test)]
@@ -49,17 +54,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            maximal_rectangle(&[
-                &['1', '0', '1', '0', '0'],
-                &['1', '0', '1', '1', '1'],
-                &['1', '1', '1', '1', '1'],
-                &['1', '0', '0', '1', '0']
-            ]),
-            6
-        );
-        debug_assert_eq!(maximal_rectangle(&[&['0']]), 0);
-        debug_assert_eq!(maximal_rectangle(&[&['1']]), 1);
+        debug_assert!(is_scramble("great", "rgeat"));
+        debug_assert!(!is_scramble("abcde", "caebd"));
+        debug_assert!(is_scramble("a", "a"));
     }
 
     #[test]
