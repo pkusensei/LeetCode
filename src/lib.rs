7dd380d19e1fcafd89eb2230732286c120ee1369
pkusensei <ZW_08@outlook.com>
2024-08-05 20:07:24 -0400

    91 decode int array into letters

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,42 @@
-pub fn subsets_with_dup(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-    let size = 1 << nums.len();
-    nums.sort_unstable();
-    let mut res = std::collections::HashSet::new();
-    for mask in size..size * 2 {
-        let mut bismask = format!("{:b}", mask);
-        bismask.remove(0);
-        let curr = (0..nums.len())
-            .filter_map(|i| {
-                if bismask.as_bytes()[i] == b'1' {
-                    Some(nums[i])
-                } else {
-                    None
-                }
-            })
-            .collect();
-        res.insert(curr);
+use std::collections::HashMap;
+
+pub fn num_decodings(s: &str) -> i32 {
+    solve(s, &mut HashMap::new())
+}
+
+fn solve<'a>(s: &'a str, seen: &mut HashMap<&'a str, i32>) -> i32 {
+    if let Some(res) = seen.get(s) {
+        return *res;
     }
-    res.into_iter().collect()
+
+    let res = match s.len() {
+        0 => unreachable!(),
+        1 => {
+            if (b'1'..=b'9').contains(&s.as_bytes()[0]) {
+                1
+            } else {
+                0
+            }
+        }
+        2 => {
+            let double = s
+                .parse::<u8>()
+                .map(|num| if (10..=26).contains(&num) { 1 } else { 0 })
+                .unwrap_or(0);
+            let singles = solve(&s[..1], seen) * solve(&s[1..], seen);
+            double + singles
+        }
+        _ => {
+            let single = solve(&s[..1], seen);
+            let double = s[..2]
+                .parse::<u8>()
+                .map(|num| if (10..=26).contains(&num) { 1 } else { 0 })
+                .unwrap_or(0);
+            single * solve(&s[1..], seen) + double * solve(&s[2..], seen)
+        }
+    };
+    seen.insert(s, res);
+    res
 }
 
 #[cfg(test)]
@@ -25,18 +45,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            subsets_with_dup(vec![1, 2, 2]),
-            [
-                vec![],
-                vec![1],
-                vec![1, 2],
-                vec![1, 2, 2],
-                vec![2],
-                vec![2, 2]
-            ]
-        );
-        debug_assert_eq!(subsets_with_dup(vec![0]), [vec![], vec![0]]);
+        // debug_assert_eq!(num_decodings("12"), 2);
+        debug_assert_eq!(num_decodings("226"), 3);
+        debug_assert_eq!(num_decodings("06"), 0);
     }
 
     #[test]
