1ac4aaa7753c5f5b398245d3c2ea54fc94b1e913
pkusensei <ZW_08@outlook.com>
2024-08-05 20:27:36 -0400

    3016 taps to type letter on numpad; smh an array is slower than hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,13 @@
-use std::collections::HashMap;
-
-pub fn num_decodings(s: &str) -> i32 {
-    solve(s, &mut HashMap::new())
-}
-
-fn solve<'a>(s: &'a str, seen: &mut HashMap<&'a str, i32>) -> i32 {
-    if let Some(res) = seen.get(s) {
-        return *res;
-    }
-
-    let res = match s.len() {
-        0 => unreachable!(),
-        1 => {
-            if (b'1'..=b'9').contains(&s.as_bytes()[0]) {
-                1
-            } else {
-                0
-            }
-        }
-        2 => {
-            let double = s
-                .parse::<u8>()
-                .map(|num| if (10..=26).contains(&num) { 1 } else { 0 })
-                .unwrap_or(0);
-            let singles = solve(&s[..1], seen) * solve(&s[1..], seen);
-            double + singles
-        }
-        _ => {
-            let single = solve(&s[..1], seen);
-            let double = s[..2]
-                .parse::<u8>()
-                .map(|num| if (10..=26).contains(&num) { 1 } else { 0 })
-                .unwrap_or(0);
-            single * solve(&s[1..], seen) + double * solve(&s[2..], seen)
-        }
-    };
-    seen.insert(s, res);
-    res
+pub fn minimum_pushes(s: &str) -> i32 {
+    let mut freq = s.bytes().fold([0; 26], |mut acc, b| {
+        acc[(b - b'a') as usize] += 1;
+        acc
+    });
+    freq.sort_unstable_by_key(|&count| std::cmp::Reverse(count));
+    freq.chunks(8)
+        .enumerate()
+        .map(|(idx, w)| w.iter().sum::<i32>() * (idx as i32 + 1))
+        .sum()
 }
 
 #[cfg(test)]
@@ -45,9 +16,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        // debug_assert_eq!(num_decodings("12"), 2);
-        debug_assert_eq!(num_decodings("226"), 3);
-        debug_assert_eq!(num_decodings("06"), 0);
+        debug_assert_eq!(minimum_pushes("abcde"), 5);
+        debug_assert_eq!(minimum_pushes("xyzxyzxyzxyz"), 12);
+        debug_assert_eq!(minimum_pushes("aabbccddeeffgghhiiiiii"), 24);
     }
 
     #[test]
