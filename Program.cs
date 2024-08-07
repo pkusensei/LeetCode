f5f2e9bcbf2819b98de44df5ccc46a5913477fc1
pkusensei <ZW_08@outlook.com>
2024-08-07 17:20:49 -0400

    104 depth of binary tree

--- Program.cs
+++ Program.cs
@@ -2,79 +2,31 @@
 
 Test1();
 Test2();
-Test3();
 Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
     TreeNode n = TreeNode.Make([3, 9, 20, null, null, 15, 7]);
-    var s = ZigzagLevelOrder(n);
-    var a = "[[3],[20,9],[15,7]]";
-    Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
+    var s = MaxDepth(n);
+    var a = 3;
+    Debug.Assert(s == a, $"Expect: {s}\nOutput: {a}");
 }
 
 void Test2()
 {
-    TreeNode n = TreeNode.Make([1]);
-    var s = ZigzagLevelOrder(n);
-    var a = "[1]";
-    Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
+    TreeNode n = TreeNode.Make([1, null, 2]);
+    var s = MaxDepth(n);
+    var a = 2;
+    Debug.Assert(s == a, $"Expect: {s}\nOutput: {a}");
 }
 
-void Test3()
+int MaxDepth(TreeNode root)
 {
-    TreeNode n = TreeNode.Make([1, 2, null, 3, null, 4, null, 5]);
-    var s = ZigzagLevelOrder(n);
-    var a = "[[1],[2],[3],[4],[5]]";
-    Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
+    return Dfs(root, 1);
 }
 
-IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+int Dfs(TreeNode root, int depth)
 {
-    var res = new List<IList<int>>();
-    if (root is null) { return res; }
-
-    var curr_level = 0;
-    var curr = new List<int>();
-    foreach (var (node, level) in InorderFlatten(root))
-    {
-        if (level == curr_level)
-        {
-            if (node is not null) { curr.Add(node.val); }
-        }
-        else
-        {
-            if ((curr_level & 1) == 1) { curr.Reverse(); }
-            res.Add(curr);
-            curr_level = level;
-            curr = [];
-            if (node is not null) { curr.Add(node.val); }
-        }
-    }
-    if (curr.Count > 0)
-    {
-        if ((curr_level & 1) == 1) { curr.Reverse(); }
-        res.Add(curr);
-    }
-    return res;
-}
-
-IEnumerable<(TreeNode node, int level)> InorderFlatten(TreeNode root)
-{
-    var queue = new Queue<(TreeNode, int)>();
-    queue.Enqueue((root, 0));
-    while (queue.TryDequeue(out var item))
-    {
-        var (node, level) = item;
-        if (node is not null)
-        {
-            yield return item;
-            queue.Enqueue((node.left, level + 1));
-            queue.Enqueue((node.right, level + 1));
-        }
-        else
-        {
-            yield return item;
-        }
-    }
+    if (root is null) { return depth - 1; }
+    return Math.Max(Dfs(root.left, depth + 1), Dfs(root.right, depth + 1));
 }
