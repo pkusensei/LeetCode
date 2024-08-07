0e2afce9234336479aa8aeb0fd889a8ea0aa248b
pkusensei <ZW_08@outlook.com>
2024-08-07 10:01:45 -0400

    97 dp interleaving strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,27 @@
-pub fn number_to_words(mut num: i32) -> String {
-        if num == 0 {
-            DIGITS[0].to_string()
-        } else {
-            let mut res = vec![];
-            let mut order = 0;
-            while num > 0 {
-                let curr = num % 1000;
-                if let Some(s) = solve_3(curr, order) {
-                    res.push(s);
-                }
-                num /= 1000;
-                order += 1;
-            }
-            res.reverse();
-            res.join(" ")
-        }
-}
-
-#[rustfmt::skip]
-const DIGITS: [&str; 20] = [
-    "Zero", "One", "Two", "Three", "Four",
-    "Five", "Six", "Seven", "Eight", "Nine",
-    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
-    "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen",
-];
-
-const TENS: [&str; 9] = [
-    "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety",
-];
-
-const ORDERS: [&str; 3] = ["Thousand", "Million", "Billion"];
-
-fn solve_3(num: i32, order: i32) -> Option<String> {
-    if num == 0 {
-        None
-    } else {
-        let mut res = {
-            let digit = num / 100;
-            if digit == 0 {
-                String::new()
-            } else {
-                format!("{} Hundred", DIGITS[digit as usize])
-            }
-        };
-        let double = num % 100;
-        if (1..=19).contains(&double) {
-            if !res.is_empty() {
-                res.push(' ');
-            }
-            res.push_str(DIGITS[double as usize]);
-        } else if double > 0 {
-            if !res.is_empty() {
-                res.push(' ');
-            }
-            let tens = double / 10;
-            res.push_str(TENS[tens as usize - 1]);
-            let single = double % 10;
-            if single > 0 {
-                res.push(' ');
-                res.push_str(DIGITS[single as usize]);
-            }
-        }
-        if order > 0 {
-            res.push(' ');
-            res.push_str(ORDERS[order as usize - 1]);
-        }
-        Some(res)
+pub fn is_interleave(s1: &str, s2: &str, s3: &str) -> bool {
+    if s1.len() + s2.len() != s3.len() {
+        return false;
     }
+
+    let mut dp = vec![vec![false; s2.len() + 1]; s1.len() + 1];
+    dp[0][0] = true;
+    for (i1, (b1, b3)) in s1.bytes().zip(s3.bytes()).enumerate() {
+        dp[i1 + 1][0] = dp[i1][0] && b1 == b3;
+    }
+    for (i2, (b2, b3)) in s2.bytes().zip(s3.bytes()).enumerate() {
+        dp[0][i2 + 1] = dp[0][i2] && b2 == b3;
+    }
+
+    for (i1, b1) in s1.bytes().enumerate().map(|(i, b)| (i + 1, b)) {
+        for (i2, b2) in s2.bytes().enumerate().map(|(i, b)| (i + 1, b)) {
+            // start with s3[1] because
+            // dp[0][..] and dp[..][0] have compared s1[0] and s2[0] against s3[0]
+            // s3[0] option is already exhausted
+            let b3 = s3.as_bytes()[i1 + i2 - 1];
+            dp[i1][i2] = (dp[i1 - 1][i2] && b1 == b3) || (dp[i1][i2 - 1] && b2 == b3);
+        }
+    }
+    dp[s1.len()][s2.len()]
 }
 
 #[cfg(test)]
@@ -75,20 +30,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(number_to_words(123), "One Hundred Twenty Three");
-        debug_assert_eq!(
-            number_to_words(12345),
-            "Twelve Thousand Three Hundred Forty Five"
-        );
-        debug_assert_eq!(
-            number_to_words(1234567),
-            "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
-        );
+        debug_assert_eq!(is_interleave("aabcc", "dbbca", "aadbbcbcac"), true);
+        debug_assert_eq!(is_interleave("aabcc", "dbbca", "aadbbbaccc"), false);
+        debug_assert_eq!(is_interleave("", "", ""), true);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(number_to_words(100), "One Hundred");
-        debug_assert_eq!(number_to_words(1000), "One Thousand");
-    }
+    fn test() {}
 }
