e2027bbd87711e51d35c14ed40df7c29b44f1c2e
pkusensei <ZW_08@outlook.com>
2024-08-06 21:49:54 -0400

    273 int to english words

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,71 @@
-pub fn restore_ip_addresses(s: &str) -> Vec<String> {
-    parse(s, 4).into_iter().map(|v| v.join(".")).collect()
+pub fn number_to_words(mut num: i32) -> String {
+        if num == 0 {
+            DIGITS[0].to_string()
+        } else {
+            let mut res = vec![];
+            let mut order = 0;
+            while num > 0 {
+                let curr = num % 1000;
+                if let Some(s) = solve_3(curr, order) {
+                    res.push(s);
+                }
+                num /= 1000;
+                order += 1;
+            }
+            res.reverse();
+            res.join(" ")
+        }
 }
 
-fn parse(s: &str, sec_count: usize) -> Vec<Vec<&str>> {
-    if !(sec_count..=sec_count * 3).contains(&s.len()) {
-        return vec![];
-    }
-    match sec_count {
-        0 => unreachable!(),
-        1 => {
-            if s.len() > 1 && s.starts_with('0') {
-                vec![]
-            } else if s.parse::<u8>().is_ok() {
-                vec![vec![s]]
+#[rustfmt::skip]
+const DIGITS: [&str; 20] = [
+    "Zero", "One", "Two", "Three", "Four",
+    "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+    "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen",
+];
+
+const TENS: [&str; 9] = [
+    "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety",
+];
+
+const ORDERS: [&str; 3] = ["Thousand", "Million", "Billion"];
+
+fn solve_3(num: i32, order: i32) -> Option<String> {
+    if num == 0 {
+        None
+    } else {
+        let mut res = {
+            let digit = num / 100;
+            if digit == 0 {
+                String::new()
             } else {
-                vec![]
+                format!("{} Hundred", DIGITS[digit as usize])
+            }
+        };
+        let double = num % 100;
+        if (1..=19).contains(&double) {
+            if !res.is_empty() {
+                res.push(' ');
+            }
+            res.push_str(DIGITS[double as usize]);
+        } else if double > 0 {
+            if !res.is_empty() {
+                res.push(' ');
+            }
+            let tens = double / 10;
+            res.push_str(TENS[tens as usize - 1]);
+            let single = double % 10;
+            if single > 0 {
+                res.push(' ');
+                res.push_str(DIGITS[single as usize]);
             }
         }
-        _ => {
-            let mut res = vec![];
-            for i in 1..s.len() {
-                let first = parse(&s[..i], 1);
-                let rest = parse(&s[i..], sec_count - 1);
-                if !first.is_empty() && !rest.is_empty() {
-                    for v1 in &first {
-                        for v2 in &rest {
-                            let mut v = v1.clone();
-                            v.extend(v2);
-                            res.push(v);
-                        }
-                    }
-                }
-            }
-            res
+        if order > 0 {
+            res.push(' ');
+            res.push_str(ORDERS[order as usize - 1]);
         }
+        Some(res)
     }
 }
 
@@ -43,23 +75,20 @@ mod tests {
 
     #[test]
     fn basics() {
-        // debug_assert_eq!(
-        //     restore_ip_addresses("25525511135"),
-        //     ["255.255.11.135", "255.255.111.35"]
-        // );
-        debug_assert_eq!(restore_ip_addresses("0000"), ["0.0.0.0"]);
+        debug_assert_eq!(number_to_words(123), "One Hundred Twenty Three");
         debug_assert_eq!(
-            restore_ip_addresses("101023"),
-            [
-                "1.0.10.23",
-                "1.0.102.3",
-                "10.1.0.23",
-                "10.10.2.3",
-                "101.0.2.3"
-            ]
+            number_to_words(12345),
+            "Twelve Thousand Three Hundred Forty Five"
+        );
+        debug_assert_eq!(
+            number_to_words(1234567),
+            "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
         );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(number_to_words(100), "One Hundred");
+        debug_assert_eq!(number_to_words(1000), "One Thousand");
+    }
 }
