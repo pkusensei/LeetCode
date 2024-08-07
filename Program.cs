014ed4c043ce897db2adcf83d1ee906d6ee5c63c
pkusensei <ZW_08@outlook.com>
2024-08-07 17:12:54 -0400

    103 zigzag level order of bonary tree

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-// Test1();
+Test1();
 Test2();
 Test3();
 Console.WriteLine("!!Test Passed!!");
@@ -8,15 +8,15 @@ Console.WriteLine("!!Test Passed!!");
 void Test1()
 {
     TreeNode n = TreeNode.Make([3, 9, 20, null, null, 15, 7]);
-    var s = LevelOrder(n);
-    var a = "[[3],[9,20],[15,7]]";
+    var s = ZigzagLevelOrder(n);
+    var a = "[[3],[20,9],[15,7]]";
     Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
 }
 
 void Test2()
 {
     TreeNode n = TreeNode.Make([1]);
-    var s = LevelOrder(n);
+    var s = ZigzagLevelOrder(n);
     var a = "[1]";
     Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
 }
@@ -24,12 +24,12 @@ void Test2()
 void Test3()
 {
     TreeNode n = TreeNode.Make([1, 2, null, 3, null, 4, null, 5]);
-    var s = LevelOrder(n);
+    var s = ZigzagLevelOrder(n);
     var a = "[[1],[2],[3],[4],[5]]";
     Debug.Assert(s.ToString() == a, $"Expect: {s}\nOutput: {a}");
 }
 
-IList<IList<int>> LevelOrder(TreeNode root)
+IList<IList<int>> ZigzagLevelOrder(TreeNode root)
 {
     var res = new List<IList<int>>();
     if (root is null) { return res; }
@@ -44,13 +44,18 @@ IList<IList<int>> LevelOrder(TreeNode root)
         }
         else
         {
-            curr_level = level;
+            if ((curr_level & 1) == 1) { curr.Reverse(); }
             res.Add(curr);
+            curr_level = level;
             curr = [];
             if (node is not null) { curr.Add(node.val); }
         }
     }
-    if (curr.Count > 0) { res.Add(curr); }
+    if (curr.Count > 0)
+    {
+        if ((curr_level & 1) == 1) { curr.Reverse(); }
+        res.Add(curr);
+    }
     return res;
 }
 
