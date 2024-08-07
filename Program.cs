93c77004f2ab518d8e2a8b92d9e23a3a6485a13b
pkusensei <ZW_08@outlook.com>
2024-08-07 19:44:40 -0400

    106 binary tree from in and post order

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = BuildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]);
+    var n = BuildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]);
     var a = "[3,9,20,null,null,15,7]";
     Debug.Assert(n.ToString() == a, $"Expect: {n}\nOutput: {a}");
 }
@@ -18,21 +18,21 @@ void Test2()
     Debug.Assert(n.ToString() == a, $"Expect: {n}\nOutput: {a}");
 }
 
-TreeNode BuildTree(int[] preorder, int[] inorder)
+TreeNode BuildTree(int[] inorder, int[] postorder)
 {
-    if (preorder.Length == 0 || inorder.Length == 0) { return null; }
-    var root = new TreeNode(preorder[0]);
-    if (preorder.Length == 1) { return root; }
+    if (postorder.Length == 0 || inorder.Length == 0) { return null; }
+    var root = new TreeNode(postorder.Last());
+    if (postorder.Length == 1) { return root; }
 
-    var root_idx = Array.FindIndex(inorder, num => num == preorder[0]);
+    var root_idx = Array.FindIndex(inorder, num => num == root.val);
 
     var left_in = inorder.Take(root_idx).ToArray();
     var right_in = inorder.Skip(root_idx + 1).ToArray();
-    var left_pre = preorder.Skip(1).Where(num => left_in.Contains(num)).ToArray();
-    var right_pre = preorder.Skip(1).Where(num => right_in.Contains(num)).ToArray();
+    var left_post = postorder.SkipLast(1).Where(num => left_in.Contains(num)).ToArray();
+    var right_post = postorder.SkipLast(1).Where(num => right_in.Contains(num)).ToArray();
 
-    root.left = BuildTree(left_pre, left_in);
-    root.right = BuildTree(right_pre, right_in);
+    root.left = BuildTree(left_in, left_post);
+    root.right = BuildTree(right_in, right_post);
 
     return root;
 }
