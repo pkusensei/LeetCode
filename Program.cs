ad59d4a7a2592dedd1423b88285c0d7487a46ecd
pkusensei <ZW_08@outlook.com>
2024-08-08 18:21:18 -0400

    109 binary tree from linked list using 2 ptrs; no extra array

--- Program.cs
+++ Program.cs
@@ -6,21 +6,44 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = SortedArrayToBST([-10, -3, 0, 5, 9]);
+    var n = SortedListToBST(ListNode.Make([-10, -3, 0, 5, 9]));
     var a = "[0,-3,9,-10,null,5]";
     Debug.Assert(n.ToString() == a, $"Expect: {n}\nOutput: {a}");
 }
 
 void Test2()
 {
-    var n = SortedArrayToBST([1, 3]);
-    var a = "[3,1]";
-    Debug.Assert(n.ToString() == a, $"Expect: {n}\nOutput: {a}");
+    var n = SortedListToBST(ListNode.Make([]));
+    Debug.Assert(n is null);
 }
 
-TreeNode SortedArrayToBST(int[] nums)
+TreeNode SortedListToBST(ListNode head)
 {
-    return Solve(nums, 0, nums.Length);
+    if (head is null) { return null; }
+    if (head.next is null) { return new(head.val); }
+
+    ListNode dummy = new(0, head);
+    (var slow, var fast) = (dummy, dummy);
+    var step = 0;
+    while (fast.next is not null)
+    {
+        fast = fast.next;
+        step += 1;
+        if (step == 2)
+        {
+            slow = slow.next;
+            step = 0;
+        }
+    }
+    var temp = slow.next;
+    slow.next = null;
+    var root = new TreeNode()
+    {
+        val = temp.val,
+        left = SortedListToBST(dummy.next),
+        right = SortedListToBST(temp.next)
+    };
+    return root;
 }
 
 TreeNode Solve(int[] nums, int left, int right)
