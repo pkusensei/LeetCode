4f43dc6221df9c77387f1ae04f4689eec4f460fe
pkusensei <ZW_08@outlook.com>
2024-08-08 20:41:34 -0400

    840 magic squares in grid

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,43 @@
-pub fn spiral_matrix_iii(rows: i32, cols: i32, r_start: i32, c_start: i32) -> Vec<Vec<i32>> {
-        const SIDE: i32 = 2;
-        let mut turn = 1;
-        let mut res = Vec::with_capacity((rows * cols) as usize);
-        let mut dir = Dir::East;
-        let mut curr = [r_start, c_start];
-        res.push(curr);
-        let mut step = 0;
-        let mut side = 0;
-
-        while res.len() < (rows * cols) as usize {
-            curr = dir.walk(curr);
-            if (0..rows).contains(&curr[0]) && (0..cols).contains(&curr[1]) {
-                res.push(curr);
-            }
-            step += 1;
-            if step == turn {
-                step = 0;
-                side += 1;
-                dir = dir.turn();
-                if side == SIDE {
-                    side = 0;
-                    turn += 1
-                }
+pub fn num_magic_squares_inside(grid: &[&[i32]]) -> i32 {
+    let row = grid.len();
+    let col = grid.first().map(|r| r.len()).unwrap_or_default();
+    if row < 3 || col < 3 {
+        return 0;
+    }
+    let mut res = 0;
+    for r in 0..=row - 3 {
+        for c in 0..=col - 3 {
+            if check(grid, r, c) {
+                res += 1;
             }
         }
-        res.into_iter().map(|v| v.into()).collect()
+    }
+    res
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Dir {
-    East,
-    South,
-    West,
-    North,
-}
-
-impl Dir {
-    fn turn(self) -> Self {
-        match self {
-            Dir::East => Self::South,
-            Dir::South => Self::West,
-            Dir::West => Self::North,
-            Dir::North => Self::East,
+fn check(grid: &[&[i32]], row: usize, col: usize) -> bool {
+    let mut nums = [0; 9];
+    for r in row..row + 3 {
+        for c in col..col + 3 {
+            if !(1..=9).contains(&grid[r][c]) {
+                return false;
+            }
+            nums[grid[r][c] as usize - 1] += 1;
         }
     }
-
-    fn walk(self, [row, col]: [i32; 2]) -> [i32; 2] {
-        match self {
-            Dir::East => [row, col + 1],
-            Dir::South => [row + 1, col],
-            Dir::West => [row, col - 1],
-            Dir::North => [row - 1, col],
-        }
+    if !nums.into_iter().all(|n| n == 1) {
+        return false;
     }
+
+    let sum: i32 = grid[row][col..col + 3].iter().sum();
+
+    grid[row + 1][col..col + 3].iter().sum::<i32>() == sum
+        && grid[row + 2][col..col + 3].iter().sum::<i32>() == sum
+        && grid[row][col] + grid[row + 1][col] + grid[row + 2][col] == sum
+        && grid[row][col + 1] + grid[row + 1][col + 1] + grid[row + 2][col + 1] == sum
+        && grid[row][col + 2] + grid[row + 1][col + 2] + grid[row + 2][col + 2] == sum
+        && grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2] == sum
+        && grid[row][col + 2] + grid[row + 1][col + 1] + grid[row + 2][col] == sum
 }
 
 #[cfg(test)]
@@ -62,22 +47,21 @@ mod tests {
     #[test]
     fn basics() {
         debug_assert_eq!(
-            spiral_matrix_iii(1, 4, 0, 0),
-            [[0, 0], [0, 1], [0, 2], [0, 3]]
+            num_magic_squares_inside(&[&[4, 3, 8, 4], &[9, 5, 1, 9], &[2, 7, 6, 2]]),
+            1
         );
-        #[rustfmt::skip]
-        debug_assert_eq!(
-            spiral_matrix_iii(5, 6, 1, 4),
-            [
-                [1, 4], [1, 5], [2, 5], [2, 4], [2, 3], [1, 3],
-                [0, 3], [0, 4], [0, 5], [3, 5], [3, 4], [3, 3], 
-                [3, 2], [2, 2], [1, 2], [0, 2], [4, 5], [4, 4], 
-                [4, 3], [4, 2], [4, 1], [3, 1], [2, 1], [1, 1],
-                [0, 1], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]
-            ]
-        );
+        debug_assert_eq!(num_magic_squares_inside(&[&[8]]), 0);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            num_magic_squares_inside(&[&[5, 5, 5], &[5, 5, 5], &[5, 5, 5]]),
+            0
+        );
+        debug_assert_eq!(
+            num_magic_squares_inside(&[&[10, 3, 5], &[1, 6, 11], &[7, 9, 2]]),
+            0
+        );
+    }
 }
