f03b1abebe2582fce6f2975823c666d8b11fa1af
pkusensei <ZW_08@outlook.com>
2024-08-09 08:23:19 -0400

    113 sum path of binary tree

--- Program.cs
+++ Program.cs
@@ -7,33 +7,45 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = TreeNode.Make([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]);
-    Debug.Assert(HasPathSum(n, 22));
+    var n = TreeNode.Make([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1]);
+    var r = PathSum(n, 22);
+    IList<IList<int>> a = [[5, 4, 11, 2], [5, 8, 4, 5]];
+    Debug.Assert(r == a, $"Output: {r}\nExpect: {a}");
 }
 
 void Test2()
 {
     var n = TreeNode.Make([1, 2, 3]);
-    Debug.Assert(!HasPathSum(n, 5));
-    // var s = MinDepth(n);
-    // var a = 5;
-    // Debug.Assert(s == a, $"Output: {s}\nExpected:{a}");
+    var r = PathSum(n, 5);
+    List<IList<int>> a = [];
+    Debug.Assert(r == a, $"Output: {r}\nExpect: {a}");
 }
 
 void Test3()
 {
     var n = TreeNode.Make([]);
-    Debug.Assert(!HasPathSum(n, 0));
+    var r = PathSum(n, 1);
+    List<IList<int>> a = [];
+    Debug.Assert(r == a, $"Output: {r}\nExpect: {a}");
 }
 
-bool HasPathSum(TreeNode root, int targetSum)
+IList<IList<int>> PathSum(TreeNode root, int targetSum)
 {
-    if (root is null) { return false; }
+    if (root is null) { return []; }
 
     var target = targetSum - root.val;
-    if (root.left is null && root.right is null)
+    if (root.left is null && root.right is null && target == 0)
     {
-        return target == 0;
+        return [[root.val]];
     }
-    return HasPathSum(root.left, target) || HasPathSum(root.right, target);
+    var res = new List<IList<int>>();
+    foreach (var item in PathSum(root.left, target).Concat(PathSum(root.right, target)))
+    {
+        if (item is not null)
+        {
+            item.Insert(0, root.val);
+            res.Add(item);
+        }
+    }
+    return res;
 }
