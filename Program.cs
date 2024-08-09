7a09ae7871167641bedab1b3b9e2558094e212af
pkusensei <ZW_08@outlook.com>
2024-08-08 20:08:13 -0400

    112 sum of path in binary tree

--- Program.cs
+++ Program.cs
@@ -2,47 +2,38 @@
 
 Test1();
 Test2();
+Test3();
 Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = TreeNode.Make([3, 9, 20, null, null, 15, 7]);
-    var s = MinDepth(n);
-    var a = 2;
-    Debug.Assert(s == a, $"Output: {s}\nExpected:{a}");
+    var n = TreeNode.Make([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]);
+    Debug.Assert(HasPathSum(n, 22));
 }
 
 void Test2()
 {
-    var n = TreeNode.Make([2, null, 3, null, 4, null, 5, null, 6]);
-    var s = MinDepth(n);
-    var a = 5;
-    Debug.Assert(s == a, $"Output: {s}\nExpected:{a}");
+    var n = TreeNode.Make([1, 2, 3]);
+    Debug.Assert(!HasPathSum(n, 5));
+    // var s = MinDepth(n);
+    // var a = 5;
+    // Debug.Assert(s == a, $"Output: {s}\nExpected:{a}");
 }
 
-int MinDepth(TreeNode root)
+void Test3()
 {
-    if (root is null) { return 0; }
-
-    var queue = new Queue<(TreeNode, int)>();
-    queue.Enqueue((root, 1));
-    while (queue.TryDequeue(out var item))
-    {
-        (var node, int depth) = item;
-        if (node.left is null && node.right is null)
-        {
-            return depth;
-        }
-        if (node.left is not null)
-        {
-            queue.Enqueue((node.left, depth + 1));
-        }
-        if (node.right is not null)
-        {
-            queue.Enqueue((node.right, depth + 1));
-        }
-
-    }
-    return 0;
+    var n = TreeNode.Make([]);
+    Debug.Assert(!HasPathSum(n, 0));
 }
 
+bool HasPathSum(TreeNode root, int targetSum)
+{
+    if (root is null) { return false; }
+
+    var target = targetSum - root.val;
+    if (root.left is null && root.right is null)
+    {
+        return target == 0;
+    }
+    return HasPathSum(root.left, target) || HasPathSum(root.right, target);
+}
