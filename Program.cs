c37d54e88d14ad68d6238829edba8a184b540331
pkusensei <ZW_08@outlook.com>
2024-08-09 14:13:42 -0400

    117 link nodes on same level in imperfect binary tree

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-// Test1();
+Test1();
 Test2();
 Test3();
 Test4();
@@ -8,9 +8,9 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = Node.Make([1, 2, 3, 4, 5, 6, 7]);
+    var n = Node.Make([1, 2, 3, 4, 5, null, 7]);
     var s = Connect(n);
-    var a = "[1,#,2,3,#,4,5,6,7,#]";
+    var a = "[1,#,2,3,#,4,5,7,#]";
     Debug.Assert(s.ToString() == a, $"Output: {n}\nExpect: {a}");
 }
 
@@ -40,23 +40,26 @@ void Test4()
 
 Node Connect(Node root)
 {
-    if (root is null) { return root; }
-    (Node node, int level) curr = (null, -1);
-    var queue = new Queue<(Node node, int level)>();
-    queue.Enqueue((root, 0));
-    while (queue.TryDequeue(out var item))
-    {
-        if (curr.level == item.level)
+        if (root is null) { return root; }
+        (Node node, int level) curr = (null, -1);
+        var queue = new Queue<(Node node, int level)>();
+        queue.Enqueue((root, 0));
+        while (queue.TryDequeue(out var item))
         {
-            curr.node.next = item.node;
+            if (curr.level == item.level)
+            {
+                curr.node.next = item.node;
+            }
+            (var left, var right) = (item.node.left, item.node.right);
+            curr = item;
+            if (left is not null)
+            {
+                queue.Enqueue((left, curr.level + 1));
+            }
+            if (right is not null)
+            {
+                queue.Enqueue((right, curr.level + 1));
+            }
         }
-        (var left, var right) = (item.node.left, item.node.right);
-        curr = item;
-        if (left is not null)
-        {
-            queue.Enqueue((left, curr.level + 1));
-            queue.Enqueue((right, curr.level + 1));
-        }
-    }
-    return root;
+        return root;
 }
