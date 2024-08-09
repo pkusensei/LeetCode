5cd60e609a6ec13be2bf104629c817264c2d46ca
pkusensei <ZW_08@outlook.com>
2024-08-09 19:40:13 -0400

    122 sliding window to find sum of posotive deltas

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,14 @@ pub fn max_profit(prices: &[i32]) -> i32 {
     if prices.len() < 2 {
         return 0;
     }
-    let mut buy = prices[0];
-    let mut profit = 0;
-    for &num in &prices[1..] {
-        // save current minimum
-        buy = buy.min(num);
-        // for all bigger numbers on the right
-        profit = profit.max(num - buy);
-    }
-    profit
+    prices
+        .windows(2)
+        .filter_map(|w| if w[0] < w[1] { Some(w[1] - w[0]) } else { None })
+        .sum()
+    // for all price pairs
+    // buy at low and sell at high
+    // since sell-then-buy can happen on same day
+    // this effectively means buy-hold-sell
 }
 
 #[cfg(test)]
@@ -19,7 +18,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_profit(&[7, 1, 5, 3, 6, 4]), 5);
+        debug_assert_eq!(max_profit(&[7, 1, 5, 3, 6, 4]), 7);
+        debug_assert_eq!(max_profit(&[1, 2, 3, 4, 5]), 4);
         debug_assert_eq!(max_profit(&[7, 6, 4, 3, 1]), 0);
     }
 
