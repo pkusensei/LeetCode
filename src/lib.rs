304fc4e6c01d26d708b1caaeac10614569de7e3b
pkusensei <ZW_08@outlook.com>
2024-08-11 15:00:21 -0400

    127 length of shortest path of converting strings in 126

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,19 @@ pub fn find_ladders(begin_word: &str, end_word: &str, word_list: &[&str]) -> Vec
         .collect()
 }
 
+pub fn ladder_length(begin_word: &str, end_word: &str, word_list: &[&str]) -> i32 {
+    if !word_list.contains(&end_word) {
+        return 0;
+    }
+    let graph = build_graph(begin_word, word_list);
+    let dist = bfs_length(begin_word, &graph);
+    dist.get(end_word)
+        .copied()
+        .flatten()
+        .map(|n| n + 1)
+        .unwrap_or_default()
+}
+
 fn bfs<'a>(
     begin_word: &'a str,
     graph: &HashMap<&str, HashSet<&'a str>>,
@@ -41,6 +54,32 @@ fn bfs<'a>(
         }
     }
     parents
+}
+
+fn bfs_length<'a>(
+    begin_word: &'a str,
+    graph: &HashMap<&'a str, HashSet<&'a str>>,
+) -> HashMap<&'a str, Option<i32>> {
+    let mut dist = HashMap::from([(begin_word, Some(0))]);
+    for &n in graph.keys().filter(|&&k| k != begin_word) {
+        dist.insert(n, None);
+    }
+    let mut queue = VecDeque::from([begin_word]);
+
+    while let Some(curr) = queue.pop_front() {
+        if !graph.contains_key(curr) {
+            continue;
+        }
+        for &neighbor in graph[curr].iter() {
+            if dist[neighbor].is_none() {
+                dist.insert(neighbor, dist[curr].map(|n| n + 1));
+                queue.push_back(neighbor);
+            } else if dist[curr].map(|n| n + 1) < dist[neighbor] {
+                dist.insert(neighbor, dist[curr].map(|n| n + 1));
+            }
+        }
+    }
+    dist
 }
 
 fn build_path<'a>(
@@ -102,14 +141,28 @@ mod tests {
             find_ladders("hit", "cog", &["hot", "dot", "dog", "lot", "log"]),
             Vec::<Vec<String>>::new()
         );
-        debug_assert_eq!(find_ladders("a", "c", &["a", "b", "c"]), [["a", "c"]]);
+
+        debug_assert_eq!(
+            ladder_length("hit", "cog", &["hot", "dot", "dog", "lot", "log", "cog"]),
+            5
+        );
+        debug_assert_eq!(
+            ladder_length("hit", "cog", &["hot", "dot", "dog", "lot", "log"]),
+            0
+        );
     }
 
     #[test]
     fn test() {
+        debug_assert_eq!(find_ladders("a", "c", &["a", "b", "c"]), [["a", "c"]]);
         debug_assert_eq!(
             find_ladders("hot", "dog", &["hot", "dog"]),
             Vec::<Vec<String>>::new()
         );
+
+        debug_assert_eq!(
+            ladder_length("hit", "cog", &["hot", "dot", "tog", "cog"]),
+            0
+        )
     }
 }
