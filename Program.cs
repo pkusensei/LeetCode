2b92de340e1b691dc4eb6d527e43693c21f1bb63
pkusensei <ZW_08@outlook.com>
2024-08-11 17:30:39 -0400

    129 sum root-to-leaf path as int in binary tree

--- Program.cs
+++ Program.cs
@@ -2,65 +2,46 @@
 
 Test1();
 Test2();
-Test3();
-Test4();
-Test5();
+
 Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
     var n = TreeNode.Make([1, 2, 3]);
-    var s = MaxPathSum(n);
-    var a = 6;
+    var s = SumNumbers(n);
+    var a = 25;
     Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
 }
 
 void Test2()
 {
-    var n = TreeNode.Make([-10, 9, 20, null, null, 15, 7]);
-    var s = MaxPathSum(n);
-    var a = 42;
-    Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
-}
-
-void Test3()
-{
-    var n = TreeNode.Make([1, 2]);
-    var s = MaxPathSum(n);
-    var a = 3;
-    Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
-}
-
-void Test4()
-{
-    var n = TreeNode.Make([2, -1]);
-    var s = MaxPathSum(n);
-    var a = 2;
-    Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
-}
-
-void Test5()
-{
-    var n = TreeNode.Make([1, -2, -3, 1, 3, -2, null, -1]);
-    var s = MaxPathSum(n);
-    var a = 3;
+    var n = TreeNode.Make([4, 9, 0, 5, 1]);
+    var s = SumNumbers(n);
+    var a = 1026;
     Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
 }
 
 
-int MaxPathSum(TreeNode root)
+int SumNumbers(TreeNode root)
 {
-    return Solve(root).treeSum;
+    return Solve(root, 0);
 }
 
-(int branchSum, int treeSum) Solve(TreeNode root)
+int Solve(TreeNode root, int curr)
 {
-    if (root is null) { return (0, int.MinValue); }
+    if (root.left is null && root.right is null)
+    {
+        return curr * 10 + root.val;
+    }
 
-    (var lb, var lt) = Solve(root.left);
-    (var rb, var rt) = Solve(root.right);
-    (lb, rb) = (Math.Max(0, lb), Math.Max(0, rb));
-
-    var treeSum = Math.Max(Math.Max(lt, rt), root.val + lb + rb);
-    return (Math.Max(lb, rb) + root.val, treeSum);
+    var res = 0;
+    if (root.left is not null)
+    {
+        res += Solve(root.left, curr * 10 + root.val);
+    }
+    if (root.right is not null)
+    {
+        res += Solve(root.right, curr * 10 + root.val);
+    }
+    return res;
 }
