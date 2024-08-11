e2051f72d0c7552eb0e622d92867fc68417c6f8b
pkusensei <ZW_08@outlook.com>
2024-08-11 14:39:45 -0400

    126 shortest path to convert one string to another

--- src/lib.rs
+++ src/lib.rs
@@ -1,117 +1,88 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
-pub fn min_days(grid: &[&[i32]]) -> i32 {
-    let ones: i32 = grid.iter().flat_map(|row| row.iter()).sum();
-    if ones == 0 {
-        return 0;
+pub fn find_ladders(begin_word: &str, end_word: &str, word_list: &[&str]) -> Vec<Vec<String>> {
+    if !word_list.contains(&end_word) {
+        return vec![];
     }
-    if ones == 1 {
-        return 1;
-    }
-
-    for (y, row) in grid.iter().enumerate() {
-        for (x, num) in row.iter().enumerate() {
-            if *num == 0 {
-                continue;
-            }
-            let (island, graph) = find_island((y, x), grid);
-            if island.len() < ones as usize {
-                return 0;
-            }
-            let ap = find_articulation_points(island, graph);
-            if !ap.is_empty() {
-                return 1;
-            } else {
-                return 2;
-            }
-        }
-    }
-    0
+    let graph = build_graph(begin_word, word_list);
+    let parents = bfs(begin_word, &graph);
+    build_path(&parents, vec![end_word], end_word)
+        .into_iter()
+        .map(|v| v.into_iter().map(|s| s.to_string()).collect())
+        .collect()
 }
 
-type Coord = (usize, usize);
+fn bfs<'a>(
+    begin_word: &'a str,
+    graph: &HashMap<&str, HashSet<&'a str>>,
+) -> HashMap<&'a str, Vec<&'a str>> {
+    let mut parents = HashMap::from([(begin_word, vec![])]);
+    let mut dist = HashMap::from([(begin_word, 0)]);
+    for &n in graph.keys().filter(|&&k| k != begin_word) {
+        dist.insert(n, i32::MIN);
+    }
+    let mut queue = VecDeque::from([begin_word]);
 
-fn find_island((row, col): Coord, grid: &[&[i32]]) -> (HashSet<Coord>, HashMap<Coord, Vec<Coord>>) {
-    let mut queue = VecDeque::from([(row, col)]);
-    let mut seen = HashSet::from([(row, col)]);
-    let mut graph: HashMap<Coord, Vec<_>> = HashMap::new();
     while let Some(curr) = queue.pop_front() {
-        let neighbors = neighbors(curr).filter(|&(row, col)| {
-            grid.get(row)
-                .is_some_and(|row| row.get(col).is_some_and(|&n| n == 1))
-        });
-        let v = graph.entry(curr).or_default();
-        for n in neighbors {
-            v.push(n);
-            if seen.insert(n) {
-                queue.push_back(n);
+        if !graph.contains_key(curr) {
+            continue;
+        }
+        for &neighbor in graph[curr].iter() {
+            if dist[neighbor] == i32::MIN {
+                dist.insert(neighbor, dist[curr] + 1);
+                parents.entry(neighbor).or_default().push(curr);
+                queue.push_back(neighbor);
+            } else if dist[curr] + 1 == dist[neighbor] {
+                parents.entry(neighbor).or_default().push(curr);
+            } else if dist[curr] + 1 < dist[neighbor] {
+                dist.insert(neighbor, dist[curr] + 1);
+                parents.insert(neighbor, vec![curr]);
             }
         }
     }
-    (seen, graph)
+    parents
 }
 
-fn find_articulation_points(
-    coords: HashSet<Coord>,
-    graph: HashMap<Coord, Vec<Coord>>,
-) -> HashSet<Coord> {
-    let mut ap = HashSet::new();
-    // since we are considering only one island
-    if let Some(node) = coords.into_iter().next() {
-        dfs(
-            &graph,
-            node,
-            None,
-            &mut HashMap::new(),
-            &mut HashMap::new(),
-            &mut ap,
-            0,
-        );
+fn build_path<'a>(
+    parents: &HashMap<&str, Vec<&'a str>>,
+    mut path: Vec<&'a str>,
+    curr: &'a str,
+) -> Vec<Vec<&'a str>> {
+    let mut res = vec![];
+    if parents.get(curr).is_none() || parents[curr].is_empty() {
+        if path.len() > 1 {
+            path.reverse();
+            res.push(path);
+        }
+    } else {
+        for &p in parents[curr].iter() {
+            let mut c = path.clone();
+            c.push(p);
+            res.extend(build_path(parents, c, p))
+        }
     }
-    ap // should also consider isolated 1s
+    res
 }
 
-fn dfs(
-    graph: &HashMap<Coord, Vec<Coord>>,
-    curr: Coord,
-    parent: Option<Coord>,
-    lowest: &mut HashMap<Coord, i32>,
-    disc_time: &mut HashMap<Coord, i32>,
-    ap: &mut HashSet<Coord>,
-    time: i32,
-) {
-    let mut children = 0;
-    lowest.entry(curr).or_insert(time);
-    disc_time.entry(curr).or_insert(time);
-
-    for &neighbor in graph[&curr].iter() {
-        if let Some(&v) = disc_time.get(&neighbor) {
-            if parent.is_none() || parent.is_some_and(|p| p != neighbor) {
-                lowest.insert(curr, lowest[&curr].min(v));
-            }
-        } else {
-            children += 1;
-            dfs(graph, neighbor, Some(curr), lowest, disc_time, ap, time + 1);
-            lowest.insert(curr, lowest[&curr].min(lowest[&neighbor]));
-            if parent.is_some() && lowest[&neighbor] >= disc_time[&curr] {
-                ap.insert(curr);
+fn build_graph<'a>(begin_word: &'a str, list: &[&'a str]) -> HashMap<&'a str, HashSet<&'a str>> {
+    let mut graph: HashMap<&str, HashSet<_>> = HashMap::new();
+    for (i, &a) in std::iter::once(&begin_word).chain(list.iter()).enumerate() {
+        for &b in list.iter().skip(i) {
+            if is_adjacent(a, b) {
+                graph.entry(a).or_default().insert(b);
+                graph.entry(b).or_default().insert(a);
             }
         }
     }
-    if parent.is_none() && children > 1 {
-        ap.insert(curr);
-    }
+    graph
 }
 
-fn neighbors((a, b): Coord) -> impl Iterator<Item = Coord> {
-    [
-        (a.saturating_sub(1), b),
-        (a + 1, b),
-        (a, b.saturating_sub(1)),
-        (a, b + 1),
-    ]
-    .into_iter()
-    .filter(move |p| *p != (a, b))
+fn is_adjacent(a: &str, b: &str) -> bool {
+    a.bytes()
+        .zip(b.bytes())
+        .filter(|&(b1, b2)| b1 != b2)
+        .count()
+        == 1
 }
 
 #[cfg(test)]
@@ -120,31 +91,25 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_days(&[&[0, 1, 1, 0], &[0, 1, 1, 0], &[0, 0, 0, 0]]), 2);
-        debug_assert_eq!(min_days(&[&[1, 1]]), 2);
         debug_assert_eq!(
-            min_days(&[
-                &[1, 1, 0, 1, 1],
-                &[1, 1, 1, 1, 1],
-                &[1, 1, 0, 1, 1],
-                &[1, 1, 0, 1, 1]
-            ]),
-            1
+            find_ladders("hit", "cog", &["hot", "dot", "dog", "lot", "log", "cog"]),
+            [
+                ["hit", "hot", "dot", "dog", "cog"],
+                ["hit", "hot", "lot", "log", "cog"],
+            ]
         );
         debug_assert_eq!(
-            min_days(&[
-                &[1, 1, 0, 1, 1],
-                &[1, 1, 1, 1, 1],
-                &[1, 1, 0, 1, 1],
-                &[1, 1, 1, 1, 1]
-            ]),
-            2
+            find_ladders("hit", "cog", &["hot", "dot", "dog", "lot", "log"]),
+            Vec::<Vec<String>>::new()
         );
-        debug_assert_eq!(min_days(&[&[1, 1], &[1, 0]]), 1);
+        debug_assert_eq!(find_ladders("a", "c", &["a", "b", "c"]), [["a", "c"]]);
     }
 
     #[test]
     fn test() {
-        debug_assert_eq!(min_days(&[&[0, 0, 0], &[0, 1, 0], &[0, 0, 0]]), 1);
+        debug_assert_eq!(
+            find_ladders("hot", "dog", &["hot", "dog"]),
+            Vec::<Vec<String>>::new()
+        );
     }
 }
