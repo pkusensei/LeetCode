a33d25bc40efff160dbca941f5692173c7d0920c
pkusensei <ZW_08@outlook.com>
2024-08-11 20:14:49 -0400

    131 partition string into palindromes

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,54 @@
-use std::collections::{HashSet, VecDeque};
-
-pub fn solve(board: &mut [Vec<char>]) {
-    let row = board.len();
-    let col = board.first().map(|r| r.len()).unwrap_or_default();
-    if row * col == 0 {
-        return;
-    }
-
-    let mut region = HashSet::new();
-    for (y, r) in board.iter().enumerate() {
-        for (x, &ch) in r.iter().enumerate() {
-            if ch == 'O' && (x == 0 || x == col - 1 || y == 0 || y == row - 1) {
-                region.extend(bfs(board, (y, x)));
-            }
-        }
-    }
-    for r in board.iter_mut() {
-        for c in r.iter_mut() {
-            *c = 'X';
-        }
-    }
-    for (row, col) in region {
-        board[row][col] = 'O'
-    }
+pub fn partition(s: &str) -> Vec<Vec<String>> {
+    solve(s, 0)
+        .into_iter()
+        .map(|v| v.into_iter().rev().map(|s| s.to_string()).collect())
+        .collect()
 }
 
-fn bfs(board: &[Vec<char>], curr: Coord) -> HashSet<Coord> {
-    let mut seen = HashSet::from([curr]);
-    let mut queue = VecDeque::from([curr]);
-    while let Some(coord) = queue.pop_front() {
-        for neighbor in neighbors(coord).filter(|&(row, col)| {
-            board
-                .get(row)
-                .is_some_and(|r| r.get(col).is_some_and(|&c| c == 'O'))
-        }) {
-            if seen.insert(neighbor) {
-                queue.push_back(neighbor);
-            }
+fn solve(s: &str, start: usize) -> Vec<Vec<&str>> {
+    if start + 1 == s.len() {
+        return vec![vec![&s[start..]]];
+    }
+
+    let mut res = vec![];
+    for end in start + 1..s.len() {
+        let curr = &s[start..end];
+        if is_palindrome(curr) {
+            let temp = solve(s, end).into_iter().map(|mut v| {
+                v.push(curr);
+                v
+            });
+            res.extend(temp);
         }
     }
-    seen
+    if is_palindrome(&s[start..]) {
+        res.push(vec![&s[start..]])
+    }
+    res
 }
 
-type Coord = (usize, usize);
-
-fn neighbors((a, b): Coord) -> impl Iterator<Item = Coord> {
-    [
-        (a.saturating_sub(1), b),
-        (a + 1, b),
-        (a, b.saturating_sub(1)),
-        (a, b + 1),
-    ]
-    .into_iter()
-    .filter(move |&p| p != (a, b))
+fn is_palindrome(s: &str) -> bool {
+    if s.len() < 2 {
+        return true;
+    }
+    s.bytes()
+        .rev()
+        .zip(s.bytes().take(s.len() / 2 + 1))
+        .all(|(b1, b2)| b1 == b2)
 }
+
+// type Coord = (usize, usize);
+
+// fn neighbors((a, b): Coord) -> impl Iterator<Item = Coord> {
+//     [
+//         (a.saturating_sub(1), b),
+//         (a + 1, b),
+//         (a, b.saturating_sub(1)),
+//         (a, b + 1),
+//     ]
+//     .into_iter()
+//     .filter(move |&p| p != (a, b))
+// }
 
 #[cfg(test)]
 mod tests {
@@ -61,50 +56,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        {
-            let mut board = vec![
-                vec!['X', 'X', 'X', 'X'],
-                vec!['X', 'O', 'O', 'X'],
-                vec!['X', 'X', 'O', 'X'],
-                vec!['X', 'O', 'X', 'X'],
-            ];
-            solve(&mut board);
-            debug_assert_eq!(
-                board,
-                [
-                    ['X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X'],
-                    ['X', 'O', 'X', 'X'],
-                ]
-            );
-        }
-        {
-            let mut board = vec![vec!['X']];
-            solve(&mut board);
-            debug_assert_eq!(board, [['X']]);
-        }
+        debug_assert_eq!(partition("aab"), [vec!["a", "a", "b"], vec!["aa", "b"]]);
+        debug_assert_eq!(partition("a"), [["a"]]);
     }
 
     #[test]
     fn test() {
-        {
-            let mut board = vec![
-                vec!['X', 'O', 'X', 'O', 'X', 'O'],
-                vec!['O', 'X', 'O', 'X', 'O', 'X'],
-                vec!['X', 'O', 'X', 'O', 'X', 'O'],
-                vec!['O', 'X', 'O', 'X', 'O', 'X'],
-            ];
-            solve(&mut board);
-            debug_assert_eq!(
-                board,
-                [
-                    ['X', 'O', 'X', 'O', 'X', 'O'],
-                    ['O', 'X', 'X', 'X', 'X', 'X'],
-                    ['X', 'X', 'X', 'X', 'X', 'O'],
-                    ['O', 'X', 'O', 'X', 'O', 'X']
-                ]
-            )
-        }
+        debug_assert_eq!(partition("bb"), [vec!["b", "b"], vec!["bb"]]);
+        debug_assert_eq!(partition("cdd"), [vec!["c", "d", "d"], vec!["c", "dd"]]);
     }
 }
