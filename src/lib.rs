0ec0168e0bfbc0ce405e9e14f1643bc16d4c0fbd
pkusensei <ZW_08@outlook.com>
2024-08-12 16:49:16 -0400

    137 find single int out of triples

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 pub fn single_number(nums: &[i32]) -> i32 {
-    nums.iter().fold(0, |acc, num| acc ^ num)
+    fn solve(nums: &[i32]) -> i32 {
+        let (mut ones, mut twos) = (0, 0);
+        for &num in nums {
+            ones ^= num & !twos;
+            twos ^= num & !ones;
+        }
+        ones
+    }
+
+    let mut ans = 0;
+    // count 1s on every bit
+    // count%3 reveals the bit from the single
+    for i in 0..32 {
+        let sum = nums.iter().fold(0, |acc, &num| {
+            let n = (num >> i) & 1;
+            acc + n
+        });
+        ans |= (sum % 3) << i;
+    }
+    ans
 }
 
 // fn is_palindrome(s: &str) -> bool {
@@ -31,9 +50,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(single_number(&[2, 2, 1]), 1);
-        debug_assert_eq!(single_number(&[4, 1, 2, 1, 2]), 4);
-        debug_assert_eq!(single_number(&[1]), 1);
+        debug_assert_eq!(single_number(&[2, 2, 3, 2]), 3);
+        debug_assert_eq!(single_number(&[0, 1, 0, 1, 0, 1, 99]), 99);
     }
 
     #[test]
