bcd2832feba6d3b3ba7769ed6d3e16115db13cb3
pkusensei <ZW_08@outlook.com>
2024-08-13 19:20:44 -0400

    142 find the start of a loop in linked list

--- Program.cs
+++ Program.cs
@@ -9,19 +9,45 @@ Console.WriteLine("!!Test Passed!!");
 void Test1()
 {
     var n = ListNode.Make([3, 2, 0, -4], 1);
-    Debug.Assert(HasCycle(n));
+    var s = DetectCycle(n);
+    Debug.Assert(s.val == 2);
 }
 
 void Test2()
 {
     var n = ListNode.Make([1, 2], 0);
-    Debug.Assert(HasCycle(n));
+    var s = DetectCycle(n);
+    Debug.Assert(s.val == 1);
 }
 
 void Test3()
 {
     var n = ListNode.Make([1], -1);
-    Debug.Assert(!HasCycle(n));
+    var s = DetectCycle(n);
+    Debug.Assert(s is null);
+}
+
+ListNode DetectCycle(ListNode head)
+{
+    if (head is null) { return null; }
+
+    (var slow, var fast) = (head, head);
+    var hasCycle = false;
+    while (fast is not null && fast.next is not null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) { hasCycle = true; break; }
+    }
+    if (!hasCycle) { return null; }
+
+    slow = head;
+    while (slow != fast)
+    {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
 }
 
 bool HasCycle(ListNode head)
