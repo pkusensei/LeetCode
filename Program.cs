7e10559962b007f1674ccab7d09f2794cc629cbc
pkusensei <ZW_08@outlook.com>
2024-08-14 14:19:00 -0400

    147 insertion sort with linked list

--- Program.cs
+++ Program.cs
@@ -3,81 +3,50 @@ using System.Text;
 
 Test1();
 Test2();
-Test3();
-Test4();
 Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var cache = new LRUCache(2);
-    cache.Put(1, 1); // cache is {1=1}
-    cache.Put(2, 2); // cache is {1=1, 2=2}
-    Debug.Assert(cache.Get(1) == 1);    // return 1
-    cache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-    Debug.Assert(cache.Get(2) == -1);    // returns -1 (not found)
-    cache.Put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-    Debug.Assert(cache.Get(1) == -1);    // return -1 (not found)
-    Debug.Assert(cache.Get(3) == 3);    // return 3
-    Debug.Assert(cache.Get(4) == 4);    // return 4
+    var n = ListNode.Make([4, 2, 1, 3]);
+    var s = InsertionSortList(n);
+    var a = "[1,2,3,4]";
+    Debug.Assert(s.ToString() == a, $"Output:{s}\nExpect:{a}");
 }
 
 void Test2()
 {
+    var n = ListNode.Make([-1, 5, 3, 4, 0]);
+    var s = InsertionSortList(n);
+    var a = "[-1,0,3,4,5]";
+    Debug.Assert(s.ToString() == a, $"Output:{s}\nExpect:{a}");
 }
 
-void Test3()
+ListNode InsertionSortList(ListNode head)
 {
-}
-
-void Test4()
-{
-}
-
-public class LRUCache
-{
-    public LinkedList<(int k, int v)> List { get; }
-    public Dictionary<int, LinkedListNode<(int k, int v)>> Map { get; }
-    public int Capacity { get; }
-    public int Count { get; private set; }
-
-    public LRUCache(int capacity)
+    if (head is null) { return null; }
+    (ListNode dummy1, ListNode dummy2) = (new(0, head), new(0, head.next));
+    head.next = null;
+    while (dummy2.next is not null)
     {
-        List = [];
-        Map = [];
-        Capacity = capacity;
-        Count = 0;
-    }
+        var curr = dummy2.next;
+        dummy2.next = curr.next;
 
-    public int Get(int key)
-    {
-        if (Map.TryGetValue(key, out var node))
+        if (curr.val < dummy1.next.val)
         {
-            List.Remove(node);
-            List.AddFirst(node);
-            return node.Value.v;
-        }
-        return -1;
-    }
-
-    public void Put(int key, int value)
-    {
-        if (Map.TryGetValue(key, out var node))
-        {
-            List.Remove(node);
-            List.AddFirst((key, value));
-            Map[key] = List.First;
+            curr.next = dummy1.next;
+            dummy1.next = curr;
         }
         else
         {
-            Count += 1;
-            if (Count > Capacity)
+            (var prev, var next) = (dummy1, dummy1.next);
+            while (next is not null && next.val < curr.val)
             {
-                var lru = List.Last;
-                List.RemoveLast();
-                Map.Remove(lru.Value.k);
+                prev = next;
+                next = next.next;
             }
-            List.AddFirst((key, value));
-            Map.Add(key, List.First);
+            curr.next = next;
+            prev.next = curr;
         }
     }
+    return dummy1.next;
 }
