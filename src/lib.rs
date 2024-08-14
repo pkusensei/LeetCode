5de9722718ab0542bdc460cbb946af7a57bcdf32
pkusensei <ZW_08@outlook.com>
2024-08-14 19:25:09 -0400

    152 max subarray product

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,25 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn reverse_words(s: &str) -> String {
-    s.split_whitespace().rev().collect::<Vec<_>>().join(" ")
+pub fn max_product(nums: &[i32]) -> i32 {
+    // NOTE: cast to f64 for extreme test cases
+    if nums.len() < 2 {
+        return nums.first().copied().unwrap_or(0);
+    }
+    let (mut curr_min, mut curr_max) = (nums[0], nums[0]);
+    let mut res = curr_max;
+    for &num in &nums[1..] {
+        if num == 0 {
+            (curr_min, curr_max) = (1, 1);
+            res = res.max(0)
+        } else {
+            let temp = num.max(curr_max * num).max(curr_min * num);
+            curr_min = num.min(curr_max * num).min(curr_min * num);
+            curr_max = temp;
+            res = res.max(curr_max)
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -13,9 +30,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(reverse_words("the sky is blue"), "blue is sky the");
-        debug_assert_eq!(reverse_words("  hello world  "), "world hello");
-        debug_assert_eq!(reverse_words("a good   example"), "example good a");
+        debug_assert_eq!(max_product(&[2, 3, -2, 4]), 6);
+        debug_assert_eq!(max_product(&[-2, 0, -1]), 0);
     }
 
     #[test]
