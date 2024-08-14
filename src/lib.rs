bdc6778cda1de937c23a13f248ab405740c0084a
pkusensei <ZW_08@outlook.com>
2024-08-14 17:36:15 -0400

    149 y = kx + b; find line with most points

--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,35 @@
 mod helper;
 
+use std::collections::{HashMap, HashSet};
+
 #[allow(unused_imports)]
 use helper::*;
 
-use std::collections::HashMap;
-
-pub fn smallest_distance_pair(nums: &[i32], mut k: i32) -> i32 {
-    let max_num = nums.iter().max().copied().unwrap_or(nums[0]);
-    let mut diff = vec![0; 1 + max_num as usize];
-    for (i, x) in nums.iter().enumerate() {
-        for y in nums.iter().skip(i + 1) {
-            diff[x.abs_diff(*y) as usize] += 1;
-        }
-    }
-    for dist in 0..=max_num {
-        k -= diff[dist as usize];
-        if k <= 0 {
-            return dist;
-        }
-    }
-    -1
-}
-
-fn binary_search_dp(mut nums: Vec<i32>, k: i32) -> i32 {
-    fn count_pairs(
-        nums: &[i32],
-        prefix_count: &[usize],
-        value_count: &HashMap<i32, i32>,
-        max_dist: i32,
-    ) -> i32 {
-        let (mut count, mut idx) = (0, 0);
-        let size = nums.len();
-
-        while idx < size {
-            let curr = nums[idx];
-            let curr_count = value_count[&curr];
-            let pairs_with_larger =
-                prefix_count[(curr + max_dist) as usize] - prefix_count[curr as usize];
-            let pairs_with_same = curr_count * (curr_count - 1) / 2;
-            count += pairs_with_larger as i32 * curr_count + pairs_with_same;
-
-            while idx + 1 < size && nums[idx] == nums[idx + 1] {
-                idx += 1;
-            }
-            idx += 1;
-        }
-
-        count
+pub fn max_points(points: &[[i32; 2]]) -> i32 {
+    if points.len() <= 2 {
+        return points.len() as i32;
     }
 
-    let size = nums.len();
-    nums.sort_unstable();
-    let max_num = nums.last().copied().unwrap_or(nums[0]);
-    let max_dist = 2 * max_num;
-    let mut prefix_count = vec![0; max_dist as usize];
-    let value_count = nums.iter().fold(HashMap::new(), |mut acc, &num| {
-        *acc.entry(num).or_insert(0) += 1;
-        acc
-    });
-
-    let mut idx = 0;
-    for value in 0..max_dist {
-        while idx < size && nums[idx] <= value {
-            idx += 1;
-        }
-        prefix_count[value as usize] = idx;
-    }
-
-    let (mut low, mut high) = (0, max_num);
-    while low < high {
-        let mid = (high - low) / 2 + low;
-        let count = count_pairs(&nums, &prefix_count, &value_count, mid);
-        if count < k {
-            low = mid + 1;
-        } else {
-            high = mid;
+    let mut lines = HashMap::new();
+    for (i, a) in points.iter().enumerate() {
+        for b in points.iter().skip(i + 1) {
+            let (x0, y0) = (a[0], a[1]);
+            let (x1, y1) = (b[0], b[1]);
+            let key = if x0 == x1 {
+                format!("n/a,{x0}")
+            } else {
+                let slope = (y1 - y0) as f64 / (x1 - x0) as f64;
+                let b = y0 as f64 - x0 as f64 * slope;
+                format!("{slope},{b}")
+            };
+            lines
+                .entry(key.clone())
+                .or_insert(HashSet::new())
+                .insert((x0, y0));
+            lines.get_mut(&key).unwrap().insert((x1, y1));
         }
     }
-    low
-}
-
-fn binary_search_sliding_window(mut nums: Vec<i32>, k: i32) -> i32 {
-    fn count_pairs(nums: &[i32], max_dist: i32) -> i32 {
-        let (mut count, mut left) = (0, 0);
-        let size = nums.len();
-        for right in 0..size {
-            while nums[right] - nums[left] > max_dist {
-                left += 1;
-            }
-            count += right - left;
-        }
-        count as i32
-    }
-
-    let size = nums.len();
-    nums.sort_unstable();
-
-    let mut low = 0;
-    let mut high = nums[size - 1] - nums[0];
-    while low < high {
-        let mid = (high - low) / 2 + low;
-        let count = count_pairs(&nums, mid);
-        if count < k {
-            low = mid + 1
-        } else {
-            high = mid;
-        }
-    }
-    low
+    lines.into_values().map(|s| s.len()).max().unwrap_or(2) as i32
 }
 
 #[cfg(test)]
@@ -116,17 +38,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(smallest_distance_pair(&[1, 3, 1], 1), 0);
-        debug_assert_eq!(smallest_distance_pair(&[1, 1, 1], 2), 0);
-        debug_assert_eq!(smallest_distance_pair(&[1, 6, 1], 3), 5);
-
-        debug_assert_eq!(binary_search_dp(vec![1, 3, 1], 1), 0);
-        debug_assert_eq!(binary_search_dp(vec![1, 1, 1], 2), 0);
-        debug_assert_eq!(binary_search_dp(vec![1, 6, 1], 3), 5);
-
-        debug_assert_eq!(binary_search_sliding_window(vec![1, 3, 1], 1), 0);
-        debug_assert_eq!(binary_search_sliding_window(vec![1, 1, 1], 2), 0);
-        debug_assert_eq!(binary_search_sliding_window(vec![1, 6, 1], 3), 5);
+        debug_assert_eq!(max_points(&[[1, 1], [2, 2], [3, 3]]), 3);
+        debug_assert_eq!(
+            max_points(&[[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]),
+            4
+        );
     }
 
     #[test]
