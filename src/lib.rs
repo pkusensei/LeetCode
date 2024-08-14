8d7f117f5279a27d0b74c5208804562320bfabfe
pkusensei <ZW_08@outlook.com>
2024-08-14 10:27:50 -0400

    719 binary search and sliding window to find int pair with kth distance

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,108 @@
-pub fn word_break(s: &str, word_dict: &[&str]) -> Vec<String> {
-    let mut res = vec![];
-    solve(s, word_dict, 0, &mut res, &mut vec![]);
-    res.into_iter().map(|v| v.join(" ")).collect()
-}
+use std::collections::HashMap;
 
-fn solve<'a>(
-    s: &str,
-    word_dict: &[&'a str],
-    start: usize,
-    res: &mut Vec<Vec<&'a str>>,
-    curr: &mut Vec<&'a str>,
-) {
-    if start == s.len() {
-        res.push(curr.clone());
-    }
-    for word in word_dict {
-        if s[start..].strip_prefix(word).is_some() {
-            curr.push(word);
-            solve(s, word_dict, start + word.len(), res, curr);
-            curr.pop();
+pub fn smallest_distance_pair(nums: &[i32], mut k: i32) -> i32 {
+    let max_num = nums.iter().max().copied().unwrap_or(nums[0]);
+    let mut diff = vec![0; 1 + max_num as usize];
+    for (i, x) in nums.iter().enumerate() {
+        for y in nums.iter().skip(i + 1) {
+            diff[x.abs_diff(*y) as usize] += 1;
         }
     }
+    for dist in 0..=max_num {
+        k -= diff[dist as usize];
+        if k <= 0 {
+            return dist;
+        }
+    }
+    -1
+}
+
+fn binary_search_dp(mut nums: Vec<i32>, k: i32) -> i32 {
+    fn count_pairs(
+        nums: &[i32],
+        prefix_count: &[usize],
+        value_count: &HashMap<i32, i32>,
+        max_dist: i32,
+    ) -> i32 {
+        let (mut count, mut idx) = (0, 0);
+        let size = nums.len();
+
+        while idx < size {
+            let curr = nums[idx];
+            let curr_count = value_count[&curr];
+            let pairs_with_larger =
+                prefix_count[(curr + max_dist) as usize] - prefix_count[curr as usize];
+            let pairs_with_same = curr_count * (curr_count - 1) / 2;
+            count += pairs_with_larger as i32 * curr_count + pairs_with_same;
+
+            while idx + 1 < size && nums[idx] == nums[idx + 1] {
+                idx += 1;
+            }
+            idx += 1;
+        }
+
+        count
+    }
+
+    let size = nums.len();
+    nums.sort_unstable();
+    let max_num = nums.last().copied().unwrap_or(nums[0]);
+    let max_dist = 2 * max_num;
+    let mut prefix_count = vec![0; max_dist as usize];
+    let value_count = nums.iter().fold(HashMap::new(), |mut acc, &num| {
+        *acc.entry(num).or_insert(0) += 1;
+        acc
+    });
+
+    let mut idx = 0;
+    for value in 0..max_dist {
+        while idx < size && nums[idx] <= value {
+            idx += 1;
+        }
+        prefix_count[value as usize] = idx;
+    }
+
+    let (mut low, mut high) = (0, max_num);
+    while low < high {
+        let mid = (high - low) / 2 + low;
+        let count = count_pairs(&nums, &prefix_count, &value_count, mid);
+        if count < k {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    low
+}
+
+fn binary_search_sliding_window(mut nums: Vec<i32>, k: i32) -> i32 {
+    fn count_pairs(nums: &[i32], max_dist: i32) -> i32 {
+        let (mut count, mut left) = (0, 0);
+        let size = nums.len();
+        for right in 0..size {
+            while nums[right] - nums[left] > max_dist {
+                left += 1;
+            }
+            count += right - left;
+        }
+        count as i32
+    }
+
+    let size = nums.len();
+    nums.sort_unstable();
+
+    let mut low = 0;
+    let mut high = nums[size - 1] - nums[0];
+    while low < high {
+        let mid = (high - low) / 2 + low;
+        let count = count_pairs(&nums, mid);
+        if count < k {
+            low = mid + 1
+        } else {
+            high = mid;
+        }
+    }
+    low
 }
 
 // fn is_palindrome(s: &str) -> bool {
@@ -52,25 +134,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            word_break("catsanddog", &["cats", "dog", "sand", "and", "cat"]),
-            ["cats and dog", "cat sand dog"]
-        );
-        debug_assert_eq!(
-            word_break("catsandog", &["cats", "dog", "sand", "and", "cat"]),
-            [""; 0]
-        );
-        debug_assert_eq!(
-            word_break(
-                "pineapplepenapple",
-                &["apple", "pen", "applepen", "pine", "pineapple"]
-            ),
-            [
-                "pine apple pen apple",
-                "pineapple pen apple",
-                "pine applepen apple"
-            ]
-        );
+        debug_assert_eq!(smallest_distance_pair(&[1, 3, 1], 1), 0);
+        debug_assert_eq!(smallest_distance_pair(&[1, 1, 1], 2), 0);
+        debug_assert_eq!(smallest_distance_pair(&[1, 6, 1], 3), 5);
+
+        debug_assert_eq!(binary_search_dp(vec![1, 3, 1], 1), 0);
+        debug_assert_eq!(binary_search_dp(vec![1, 1, 1], 2), 0);
+        debug_assert_eq!(binary_search_dp(vec![1, 6, 1], 3), 5);
+
+        debug_assert_eq!(binary_search_sliding_window(vec![1, 3, 1], 1), 0);
+        debug_assert_eq!(binary_search_sliding_window(vec![1, 1, 1], 2), 0);
+        debug_assert_eq!(binary_search_sliding_window(vec![1, 6, 1], 3), 5);
     }
 
     #[test]
