53b64a1868d89d4bb3eb82e6e1613f209e7e6cb1
pkusensei <ZW_08@outlook.com>
2024-08-14 17:54:30 -0400

    150 postfix arithmetics

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,28 @@
 mod helper;
 
-use std::collections::{HashMap, HashSet};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_points(points: &[[i32; 2]]) -> i32 {
-    if points.len() <= 2 {
-        return points.len() as i32;
+pub fn eval_rpn(tokens: &[&str]) -> i32 {
+    fn arith_op(stack: &mut Vec<i32>, func: fn(i32, i32) -> i32) {
+        let (a, b) = (stack.pop().unwrap(), stack.pop().unwrap());
+        stack.push(func(b, a))
     }
 
-    let mut lines = HashMap::new();
-    for (i, a) in points.iter().enumerate() {
-        for b in points.iter().skip(i + 1) {
-            let (x0, y0) = (a[0], a[1]);
-            let (x1, y1) = (b[0], b[1]);
-            let key = if x0 == x1 {
-                format!("n/a,{x0}")
-            } else {
-                let slope = (y1 - y0) as f64 / (x1 - x0) as f64;
-                let b = y0 as f64 - x0 as f64 * slope;
-                format!("{slope},{b}")
-            };
-            lines
-                .entry(key.clone())
-                .or_insert(HashSet::new())
-                .insert((x0, y0));
-            lines.get_mut(&key).unwrap().insert((x1, y1));
+    let mut stack = vec![];
+    for token in tokens {
+        match *token {
+            "+" => arith_op(&mut stack, |a, b| a + b),
+            "-" => arith_op(&mut stack, |a, b| a - b),
+            "*" => arith_op(&mut stack, |a, b| a * b),
+            "/" => arith_op(&mut stack, |a, b| a / b),
+            _ => {
+                let num = token.parse::<i32>().unwrap_or(1);
+                stack.push(num);
+            }
         }
     }
-    lines.into_values().map(|s| s.len()).max().unwrap_or(2) as i32
+    stack[0]
 }
 
 #[cfg(test)]
@@ -38,10 +31,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_points(&[[1, 1], [2, 2], [3, 3]]), 3);
+        debug_assert_eq!(eval_rpn(&["2", "1", "+", "3", "*"]), 9);
+        debug_assert_eq!(eval_rpn(&["4", "13", "5", "/", "+"]), 6);
         debug_assert_eq!(
-            max_points(&[[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]),
-            4
+            eval_rpn(&["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]),
+            22
         );
     }
 
