515acf8593fbf1900b377809aa6e950ab555af2d
pkusensei <ZW_08@outlook.com>
2024-08-15 08:22:11 -0400

    160 find intersection of 2 linked lists

--- Program.cs
+++ Program.cs
@@ -7,57 +7,21 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = ListNode.Make([4, 2, 1, 3]);
-    var s = SortList(n);
-    var a = "[1,2,3,4]";
-    Debug.Assert(s.ToString() == a, $"Output:{s}\nExpect:{a}");
 }
 
 void Test2()
 {
-    var n = ListNode.Make([-1, 5, 3, 4, 0]);
-    var s = SortList(n);
-    var a = "[-1,0,3,4,5]";
-    Debug.Assert(s.ToString() == a, $"Output:{s}\nExpect:{a}");
 }
 
-ListNode SortList(ListNode head)
+ListNode GetIntersectionNode(ListNode headA, ListNode headB)
 {
-    if (head is null || head.next is null) { return head; }
-    (var slow, var fast) = (head, head);
-    ListNode prev = null;
-    while (fast is not null && fast.next is not null)
-    {
-        prev = slow;
-        fast = fast.next.next;
-        slow = slow.next;
-    }
-    prev.next = null;
-    var left = SortList(head);
-    var right = SortList(slow);
-    return Merge(left, right);
-}
+    if (headA is null || headB is null) { return null; }
 
-ListNode Merge(ListNode left, ListNode right)
-{
-    ListNode dummy = new(0);
-    var curr = dummy;
-
-    while (left is not null && right is not null)
+    (var a, var b) = (headA, headB);
+    while (a != b)
     {
-        if (left.val < right.val)
-        {
-            curr.next = left;
-            left = left.next;
-        }
-        else
-        {
-            curr.next = right;
-            right = right.next;
-        }
-        curr = curr.next;
+        a = a is null ? headB : a.next;
+        b = b is null ? headA : b.next;
     }
-    if (left is not null) { curr.next = left; }
-    if (right is not null) { curr.next = right; }
-    return dummy.next;
+    return a;
 }
