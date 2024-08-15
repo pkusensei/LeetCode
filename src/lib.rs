42cfd7e7f23d98c3bd7565bd9ba04a7667d8f955
pkusensei <ZW_08@outlook.com>
2024-08-14 20:22:53 -0400

    154 binary search min int in rotated sorted array with dup values

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,16 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_min(nums: &[i32]) -> i32 {
-    if nums.len() < 2 {
-        return nums[0];
+pub fn find_min(mut nums: Vec<i32>) -> i32 {
+    while nums.len() > 1 && nums.first() == nums.last() {
+        nums.pop();
     }
     let (mut low, mut high) = (0, nums.len() - 1);
     while low < high {
         let mid = (high - low) / 2 + low;
         match nums[mid].cmp(&nums[high]) {
             std::cmp::Ordering::Less => high = mid,
-            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Equal => high -= 1,
             std::cmp::Ordering::Greater => low = mid + 1,
         }
     }
@@ -25,11 +25,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(find_min(&[3, 4, 5, 1, 2]), 1);
-        debug_assert_eq!(find_min(&[4, 5, 6, 7, 0, 1, 2]), 0);
-        debug_assert_eq!(find_min(&[11, 13, 15, 17]), 11);
+        debug_assert_eq!(find_min(vec![1, 3, 5]), 1);
+        debug_assert_eq!(find_min(vec![2, 2, 2, 0, 1]), 0);
+        debug_assert_eq!(find_min(vec![1, 1]), 1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(find_min(vec![1, 1, 3]), 1)
+    }
 }
