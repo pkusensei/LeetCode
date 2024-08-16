84ad1ed87b0b84f169467b463f7dcd0a4b39fa3a
pkusensei <ZW_08@outlook.com>
2024-08-16 10:42:04 -0400

    168 number to 26-base

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,17 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn two_sum(numbers: &[i32], target: i32) -> Vec<i32> {
-    let (mut i, mut j) = (0, numbers.len() - 1);
-    while i < j {
-        match (numbers[i] + numbers[j]).cmp(&target) {
-            std::cmp::Ordering::Less => i += 1,
-            std::cmp::Ordering::Equal => return vec![(i + 1) as i32, (j + 1) as i32],
-            std::cmp::Ordering::Greater => j -= 1,
-        }
+pub fn convert_to_title(column_number: i32) -> String {
+    let mut chs = vec![];
+    let mut num = column_number;
+    while num > 0 {
+        chs.push((num - 1) % 26);
+        num = (num - 1) / 26;
     }
-    vec![]
+    chs.reverse();
+    chs.into_iter()
+        .map(|n| char::from(n as u8 + b'A'))
+        .collect()
 }
 
 #[cfg(test)]
@@ -21,11 +22,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(two_sum(&[2, 7, 11, 15], 9), [1, 2]);
-        debug_assert_eq!(two_sum(&[2, 3, 4], 6), [1, 3]);
-        debug_assert_eq!(two_sum(&[-1, 0], -1), [1, 2]);
+        // debug_assert_eq!(convert_to_title(1), "A");
+        // debug_assert_eq!(convert_to_title(28), "AB");
+        debug_assert_eq!(convert_to_title(701), "ZY");
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(convert_to_title(52), "AZ");
+    }
 }
