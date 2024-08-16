13d856a6a319d3471800ea8fe30adb2da90c83d1
pkusensei <ZW_08@outlook.com>
2024-08-16 09:34:00 -0400

    166 fraction number to string

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,42 @@
 mod helper;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_distance(arrays: &[&[i32]]) -> i32 {
-    if arrays.len() < 2 {
-        return 0;
+pub fn fraction_to_decimal(numerator: i32, denominator: i32) -> String {
+    if numerator == 0 {
+        return "0".to_string();
     }
-    let curr_min = arrays[0][0];
-    let curr_max = arrays[0].last().copied().unwrap_or(curr_min);
-    arrays
-        .iter()
-        .skip(1)
-        .fold((curr_min, curr_max, 0), |(curr_min, curr_max, res), arr| {
-            let temp_min = arr[0];
-            let temp_max = arr.last().copied().unwrap_or(temp_min);
-            let res = res
-                .max(temp_max.abs_diff(curr_min) as i32)
-                .max(curr_max.abs_diff(temp_min) as i32);
-            (curr_min.min(temp_min), curr_max.max(temp_max), res)
-        })
-        .2
-}
 
-// pub fn fraction_to_decimal(numerator: i32, denominator: i32) -> String {
-//     todo!()
-// }
+    let mut res = if numerator.is_negative() != denominator.is_negative() {
+        "-".to_string()
+    } else {
+        String::new()
+    };
+    let (numerator, denominator) = ((numerator as i64).abs(), (denominator as i64).abs());
+    let int = numerator / denominator;
+    res = format!("{res}{int}");
+    let mut remainder = numerator % denominator;
+    if remainder == 0 {
+        return res;
+    }
+    res.push('.');
+    let mut fracts = HashMap::new();
+    while remainder > 0 {
+        if let Some(&i) = fracts.get(&remainder) {
+            res.insert(i, '(');
+            res.push(')');
+            break;
+        }
+        fracts.insert(remainder, res.len());
+        remainder *= 10;
+        res = format!("{res}{}", remainder / denominator);
+        remainder %= denominator;
+    }
+    res
+}
 
 #[cfg(test)]
 mod tests {
@@ -33,23 +44,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_distance(&[&[1, 2, 3], &[4, 5], &[1, 2, 3]]), 4);
-        debug_assert_eq!(max_distance(&[&[1], &[1]]), 0);
-        // debug_assert_eq!(fraction_to_decimal(1, 2), "0.5");
-        // debug_assert_eq!(fraction_to_decimal(2, 1), "2");
-        // debug_assert_eq!(fraction_to_decimal(4, 333), "0.(012)");
+        debug_assert_eq!(fraction_to_decimal(1, 2), "0.5");
+        debug_assert_eq!(fraction_to_decimal(2, 1), "2");
+        debug_assert_eq!(fraction_to_decimal(4, 333), "0.(012)");
     }
 
     #[test]
     fn test() {
+        debug_assert_eq!(fraction_to_decimal(7, -12), "-0.58(3)");
         debug_assert_eq!(
-            max_distance(&[
-                &[-8, -7, -7, -5, 1, 1, 3, 4],
-                &[-2],
-                &[-10, -10, -7, 0, 1, 3],
-                &[2]
-            ]),
-            14
+            fraction_to_decimal(-1, -2147483648),
+            "0.0000000004656612873077392578125"
         );
+        debug_assert_eq!(fraction_to_decimal(-2147483648, 1), "-2147483648");
     }
 }
