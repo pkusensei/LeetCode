82c93b1253fdc684441722de384ebcc9d279baeb
pkusensei <ZW_08@outlook.com>
2024-08-16 08:03:12 -0400

    624 find max gap between sorted arrays

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,29 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn compare_version(version1: &str, version2: &str) -> i32 {
-    let (mut it1, mut it2) = (version1.split('.'), version2.split('.'));
-    loop {
-        match (it1.next(), it2.next()) {
-            (None, None) => break,
-            (Some(s1), Some(s2)) => {
-                let n1 = s1.parse::<u32>().unwrap();
-                let n2 = s2.parse::<u32>().unwrap();
-                if n1 > n2 {
-                    return 1;
-                }
-                if n1 < n2 {
-                    return -1;
-                }
-            }
-            (Some(s1), None) => {
-                let n1 = s1.parse::<u32>().unwrap();
-                if n1 > 0 {
-                    return 1;
-                }
-            }
-            (None, Some(s2)) => {
-                let n2 = s2.parse::<u32>().unwrap();
-                if n2 > 0 {
-                    return -1;
-                }
-            }
-        }
+pub fn max_distance(arrays: &[&[i32]]) -> i32 {
+    if arrays.len() < 2 {
+        return 0;
     }
-    0
+    let curr_min = arrays[0][0];
+    let curr_max = arrays[0].last().copied().unwrap_or(curr_min);
+    arrays
+        .iter()
+        .skip(1)
+        .fold((curr_min, curr_max, 0), |(curr_min, curr_max, res), arr| {
+            let temp_min = arr[0];
+            let temp_max = arr.last().copied().unwrap_or(temp_min);
+            let res = res
+                .max(temp_max.abs_diff(curr_min) as i32)
+                .max(curr_max.abs_diff(temp_min) as i32);
+            (curr_min.min(temp_min), curr_max.max(temp_max), res)
+        })
+        .2
 }
+
+// pub fn fraction_to_decimal(numerator: i32, denominator: i32) -> String {
+//     todo!()
+// }
 
 #[cfg(test)]
 mod tests {
@@ -41,11 +33,23 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(compare_version("1.2", "1.10"), -1);
-        debug_assert_eq!(compare_version("1.01", "1.001"), 0);
-        debug_assert_eq!(compare_version("1.0", "1.0.0.0"), 0);
+        debug_assert_eq!(max_distance(&[&[1, 2, 3], &[4, 5], &[1, 2, 3]]), 4);
+        debug_assert_eq!(max_distance(&[&[1], &[1]]), 0);
+        // debug_assert_eq!(fraction_to_decimal(1, 2), "0.5");
+        // debug_assert_eq!(fraction_to_decimal(2, 1), "2");
+        // debug_assert_eq!(fraction_to_decimal(4, 333), "0.(012)");
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            max_distance(&[
+                &[-8, -7, -7, -5, 1, 1, 3, 4],
+                &[-2],
+                &[-10, -10, -7, 0, 1, 3],
+                &[2]
+            ]),
+            14
+        );
+    }
 }
