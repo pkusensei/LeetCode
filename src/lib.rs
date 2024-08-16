b888d1ed860614281817f354ce0a52a401cf4ebc
pkusensei <ZW_08@outlook.com>
2024-08-16 13:41:13 -0400

    174 minimum int to pass thru matrix while maintaining positive

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,35 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub const fn trailing_zeroes(n: i32) -> i32 {
-    if n < 5 {
+pub fn calculate_minimum_hp(grid: &[&[i32]]) -> i32 {
+    let (row, col) = (grid.len(), grid.first().map(|r| r.len()).unwrap_or(0));
+    if row * col < 1 {
         return 0;
     }
-    let mut div = 5;
-    let mut count = 0;
-    while n / div > 0 {
-        count += n / div;
-        div *= 5;
+
+    let mut dp = vec![vec![1; col]; row];
+    dp[row - 1][col - 1] = 1.max(1 - grid[row - 1][col - 1]);
+
+    for (j, &num) in grid[row - 1].iter().enumerate().rev() {
+        if j == col - 1 {
+            continue;
+        }
+        dp[row - 1][j] = 1.max(dp[row - 1][j + 1] - num);
     }
-    count
+
+    for i in (0..row - 1).rev() {
+        dp[i][col - 1] = 1.max(dp[i + 1][col - 1] - grid[i][col - 1]);
+    }
+
+    for i in (0..row - 1).rev() {
+        for j in (0..col - 1).rev() {
+            let right = 1.max(dp[i][j + 1] - grid[i][j]);
+            let down = 1.max(dp[i + 1][j] - grid[i][j]);
+            dp[i][j] = right.min(down);
+        }
+    }
+
+    dp[0][0]
 }
 
 #[cfg(test)]
@@ -22,14 +40,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(trailing_zeroes(3), 0);
-        debug_assert_eq!(trailing_zeroes(5), 1);
-        debug_assert_eq!(trailing_zeroes(0), 0);
+        debug_assert_eq!(
+            calculate_minimum_hp(&[&[-2, -3, 3], &[-5, -10, 1], &[10, 30, -5]]),
+            7
+        );
+        debug_assert_eq!(calculate_minimum_hp(&[&[0]]), 1);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(trailing_zeroes(30), 7);
-        debug_assert_eq!(trailing_zeroes(51), 12);
-    }
+    fn test() {}
 }
