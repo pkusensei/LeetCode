7ef18b54468cbf4d1da1f091d926617cdda6dd54
pkusensei <ZW_08@outlook.com>
2024-08-16 11:03:22 -0400

    171 26-base int to 10-base

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,10 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-// Boyer–Moore majority vote algorithm
-pub fn majority_element(nums: &[i32]) -> i32 {
-    nums.iter()
-        .fold((0, 0), |(count, res), &num| {
-            let temp = if count == 0 { num } else { res };
-            let count = if temp == num { count + 1 } else { count - 1 };
-            (count, temp)
-        })
-        .1
+pub fn title_to_number(column_title: &str) -> i32 {
+    column_title
+        .bytes()
+        .fold(0, |acc, byte| acc * 26 + (byte - b'A' + 1) as i32)
 }
 
 #[cfg(test)]
@@ -20,8 +15,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(majority_element(&[3, 2, 3]), 3);
-        debug_assert_eq!(majority_element(&[2, 2, 1, 1, 1, 2, 2]), 2);
+        debug_assert_eq!(title_to_number("A"), 1);
+        debug_assert_eq!(title_to_number("AB"), 28);
+        debug_assert_eq!(title_to_number("ZY"), 701);
     }
 
     #[test]
