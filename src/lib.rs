6d5ad8b7521b539c52f7be6a2d617d249f3e7272
pkusensei <ZW_08@outlook.com>
2024-08-17 14:19:19 -0400

    188 buy and sell stocks 4

--- src/lib.rs
+++ src/lib.rs
@@ -3,36 +3,18 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_points(points: &[&[i32]]) -> i64 {
-    let (row, col) = (points.len(), points.first().map(|r| r.len()).unwrap_or(0));
-    if row * col == 0 {
-        return 0;
-    }
-    if row == 1 {
-        return points[0].iter().max().copied().unwrap_or(0) as _;
-    }
-    if col == 1 {
-        return (0..row).map(|n| i64::from(points[n][0])).sum();
-    }
-    let mut prev: Vec<i64> = points[0].iter().copied().map(i64::from).collect();
-    for r in points.iter().skip(1) {
-        let mut left_max = vec![prev[0]];
-        for x in 1..col {
-            left_max.push((left_max[x - 1] - 1).max(prev[x]));
+// https://hashnode.com/post/best-time-to-buy-and-sell-stocks-a-comprehensive-guide-cko8xg43m0oz194s114kz71pn
+pub fn max_profit(k: i32, prices: &[i32]) -> i32 {
+    let mut profit_after_buy = vec![i32::MIN; k as usize + 1];
+    let mut profit_after_sell = vec![0; k as usize + 1];
+
+    for &num in prices.iter() {
+        for j in (1..=k as usize).rev() {
+            profit_after_buy[j] = profit_after_buy[j].max(profit_after_sell[j - 1] - num);
+            profit_after_sell[j] = profit_after_sell[j].max(profit_after_buy[j] + num);
         }
-        let mut right_max = vec![0; col - 1];
-        right_max.push(prev[col - 1]);
-        for x in (0..=col - 2).rev() {
-            right_max[x] = (right_max[x + 1] - 1).max(prev[x]);
-        }
-        let curr = r
-            .iter()
-            .enumerate()
-            .map(|(x, &num)| num as i64 + left_max[x].max(right_max[x]))
-            .collect();
-        prev = curr;
     }
-    prev.into_iter().max().unwrap_or(0)
+    profit_after_sell[k as usize]
 }
 
 #[cfg(test)]
@@ -41,12 +23,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_points(&[&[1, 2, 3], &[1, 5, 1], &[3, 1, 1]]), 9);
-        debug_assert_eq!(max_points(&[&[1, 5], &[2, 3], &[4, 2]]), 11);
+        debug_assert_eq!(max_profit(2, &[2, 4, 1]), 2);
+        debug_assert_eq!(max_profit(2, &[3, 2, 6, 5, 0, 3]), 7);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(max_points(&[&[1], &[2], &[3]]), 6)
-    }
+    fn test() {}
 }
