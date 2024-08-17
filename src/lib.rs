6d7443d87f3864bd650db332ef615918291c2231
pkusensei <ZW_08@outlook.com>
2024-08-17 17:43:45 -0400

    189 slice::rotate_right

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,11 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-// https://hashnode.com/post/best-time-to-buy-and-sell-stocks-a-comprehensive-guide-cko8xg43m0oz194s114kz71pn
-pub fn max_profit(k: i32, prices: &[i32]) -> i32 {
-    let mut profit_after_buy = vec![i32::MIN; k as usize + 1];
-    let mut profit_after_sell = vec![0; k as usize + 1];
-
-    for &num in prices.iter() {
-        for j in (1..=k as usize).rev() {
-            profit_after_buy[j] = profit_after_buy[j].max(profit_after_sell[j - 1] - num);
-            profit_after_sell[j] = profit_after_sell[j].max(profit_after_buy[j] + num);
-        }
-    }
-    profit_after_sell[k as usize]
+pub fn rotate_right(nums: &mut [i32], k: i32) {
+    let k = k as usize % nums.len();
+    nums.reverse();
+    nums[..k].reverse();
+    nums[k..].reverse();
 }
 
 #[cfg(test)]
@@ -23,8 +16,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_profit(2, &[2, 4, 1]), 2);
-        debug_assert_eq!(max_profit(2, &[3, 2, 6, 5, 0, 3]), 7);
+        {
+            let mut nums = vec![1, 2, 3, 4, 5, 6, 7];
+            rotate_right(&mut nums, 3);
+            debug_assert_eq!(nums, [5, 6, 7, 1, 2, 3, 4]);
+        }
+        {
+            let mut nums = vec![-1, -100, 3, 99];
+            rotate_right(&mut nums, 2);
+            debug_assert_eq!(nums, [3, 99, -1, -100]);
+        }
     }
 
     #[test]
