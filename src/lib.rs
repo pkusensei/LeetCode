3b60bb8f12162441af3fb0e078ffcf91ee14255d
pkusensei <ZW_08@outlook.com>
2024-08-17 18:06:53 -0400

    190 u32::reverse_bits

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn rotate_right(nums: &mut [i32], k: i32) {
-    let k = k as usize % nums.len();
-    nums.reverse();
-    nums[..k].reverse();
-    nums[k..].reverse();
+pub fn reverse_bits(mut x: u32) -> u32 {
+    let mut res = 0;
+    for _ in 0..32 {
+        res <<= 1;
+        res += x & 1;
+        x >>= 1;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -16,16 +19,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        {
-            let mut nums = vec![1, 2, 3, 4, 5, 6, 7];
-            rotate_right(&mut nums, 3);
-            debug_assert_eq!(nums, [5, 6, 7, 1, 2, 3, 4]);
-        }
-        {
-            let mut nums = vec![-1, -100, 3, 99];
-            rotate_right(&mut nums, 2);
-            debug_assert_eq!(nums, [3, 99, -1, -100]);
-        }
+        debug_assert_eq!(
+            reverse_bits(0b00000010100101000001111010011100),
+            0b00111001011110000010100101000000
+        );
+        debug_assert_eq!(
+            reverse_bits(0b11111111111111111111111111111101),
+            0b10111111111111111111111111111111
+        );
     }
 
     #[test]
