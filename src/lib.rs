3dee2b02347412812d1b172e1426f904c82f9224
pkusensei <ZW_08@outlook.com>
2024-08-18 16:39:59 -0400

    207 dfs to detect cycle in graph

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,50 @@
 mod helper;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_isomorphic(s: &str, t: &str) -> bool {
-    if s.len() < 2 {
-        return true;
-    }
+pub fn can_finish(num_courses: i32, prerequisites: &[[i32; 2]]) -> bool {
+    let graph: HashMap<i32, Vec<_>> = prerequisites.iter().fold(HashMap::new(), |mut acc, p| {
+        acc.entry(p[0]).or_default().push(p[1]);
+        acc
+    });
 
-    let mut m1 = HashMap::new();
-    let mut m2 = HashMap::new();
-    for (b1, b2) in s.bytes().zip(t.bytes()) {
-        if m1.get(&b1).is_some_and(|&seen| seen != b2) {
+    let (mut seen, mut path) = (HashSet::new(), HashSet::new());
+    for node in 0..num_courses {
+        if has_cycle(node, &graph, &mut seen, &mut path) {
             return false;
         }
-        if m2.get(&b2).is_some_and(|&seen| seen != b1) {
-            return false;
-        }
-        m1.insert(b1, b2);
-        m2.insert(b2, b1);
     }
     true
+}
+
+fn has_cycle(
+    node: i32,
+    graph: &HashMap<i32, Vec<i32>>,
+    seen: &mut HashSet<i32>,
+    path: &mut HashSet<i32>,
+) -> bool {
+    if path.contains(&node) {
+        return true;
+    }
+    if seen.contains(&node) {
+        return false;
+    }
+
+    seen.insert(node);
+    path.insert(node);
+
+    if let Some(neighbors) = graph.get(&node) {
+        for &neighbor in neighbors {
+            if has_cycle(neighbor, graph, seen, path) {
+                return true;
+            }
+        }
+    }
+    path.remove(&node);
+    false
 }
 
 #[cfg(test)]
@@ -31,13 +53,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(is_isomorphic("egg", "add"));
-        debug_assert!(!is_isomorphic("foo", "bar"));
-        debug_assert!(is_isomorphic("paper", "title"));
+        debug_assert!(can_finish(2, &[[1, 0]]));
+        debug_assert!(!can_finish(2, &[[1, 0], [0, 1]]));
     }
 
     #[test]
     fn test() {
-        debug_assert!(!is_isomorphic("badc", "baba"));
+        debug_assert!(can_finish(5, &[[1, 4], [2, 4], [3, 1], [3, 2]]));
     }
 }
