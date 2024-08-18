b0d5e539fe4a5e0492f799cf2954a6c60b5a835e
pkusensei <ZW_08@outlook.com>
2024-08-18 17:38:20 -0400

    208 implement a trie / prefix tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,116 @@
 mod helper;
 
-use std::collections::{HashMap, HashSet};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn can_finish(num_courses: i32, prerequisites: &[[i32; 2]]) -> bool {
-    let graph: HashMap<i32, Vec<_>> = prerequisites.iter().fold(HashMap::new(), |mut acc, p| {
-        acc.entry(p[0]).or_default().push(p[1]);
-        acc
-    });
-
-    let (mut seen, mut path) = (HashSet::new(), HashSet::new());
-    for node in 0..num_courses {
-        if has_cycle(node, &graph, &mut seen, &mut path) {
-            return false;
-        }
-    }
-    true
+#[derive(Debug, Default)]
+struct Trie {
+    nodes: [Option<Node>; 26],
 }
 
-fn has_cycle(
-    node: i32,
-    graph: &HashMap<i32, Vec<i32>>,
-    seen: &mut HashSet<i32>,
-    path: &mut HashSet<i32>,
-) -> bool {
-    if path.contains(&node) {
-        return true;
-    }
-    if seen.contains(&node) {
-        return false;
+impl Trie {
+    fn new() -> Self {
+        Default::default()
     }
 
-    seen.insert(node);
-    path.insert(node);
+    fn insert(&mut self, word: &str) {
+        let bytes = word.as_bytes();
+        let idx = (bytes[0] - b'a') as usize;
+        if self.nodes[idx].is_none() {
+            self.nodes[idx] = Some(Node::new(bytes));
+        } else {
+            let Some(n) = self.nodes[idx].as_mut() else {
+                return;
+            };
+            n.insert(bytes);
+        }
+    }
 
-    if let Some(neighbors) = graph.get(&node) {
-        for &neighbor in neighbors {
-            if has_cycle(neighbor, graph, seen, path) {
-                return true;
+    fn search(&self, word: &str) -> bool {
+        let bytes = word.as_bytes();
+        let idx = (bytes[0] - b'a') as usize;
+        if let Some(n) = self.nodes[idx].as_ref() {
+            n.search(bytes)
+        } else {
+            false
+        }
+    }
+
+    fn starts_with(&self, prefix: &str) -> bool {
+        let bytes = prefix.as_bytes();
+        let idx = (bytes[0] - b'a') as usize;
+        if let Some(n) = self.nodes[idx].as_ref() {
+            n.starts_with(bytes)
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct Node {
+    val: u8,
+    is_end: bool,
+    children: [Option<Box<Node>>; 26],
+}
+
+impl Node {
+    fn new(bytes: &[u8]) -> Self {
+        let mut node = Self {
+            val: bytes[0],
+            is_end: false,
+            children: Default::default(),
+        };
+        if bytes.len() > 1 {
+            node.insert(bytes)
+        } else {
+            node.is_end = true;
+        }
+        node
+    }
+
+    fn insert(&mut self, bytes: &[u8]) {
+        debug_assert_eq!(self.val, bytes[0]);
+        if bytes.len() > 1 {
+            let child = bytes[1];
+            if let Some(ref mut n) = self.children[(child - b'a') as usize] {
+                n.insert(&bytes[1..])
+            } else {
+                let node = Self::new(&bytes[1..]);
+                self.children[(child - b'a') as usize] = Some(Box::new(node))
+            }
+        } else {
+            self.is_end = true;
+        }
+    }
+
+    fn search(&self, bytes: &[u8]) -> bool {
+        debug_assert_eq!(self.val, bytes[0]);
+        if bytes.len() == 1 {
+            self.is_end
+        } else {
+            let child = (bytes[1] - b'a') as usize;
+            if let Some(ref n) = self.children[child] {
+                n.search(&bytes[1..])
+            } else {
+                false
             }
         }
     }
-    path.remove(&node);
-    false
+
+    fn starts_with(&self, bytes: &[u8]) -> bool {
+        debug_assert_eq!(self.val, bytes[0]);
+        if bytes.len() == 1 {
+            true
+        } else {
+            let child = (bytes[1] - b'a') as usize;
+            if let Some(ref n) = self.children[child] {
+                n.starts_with(&bytes[1..])
+            } else {
+                false
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -53,12 +119,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(can_finish(2, &[[1, 0]]));
-        debug_assert!(!can_finish(2, &[[1, 0], [0, 1]]));
+        let mut trie = Trie::new();
+        trie.insert("apple");
+        debug_assert!(trie.search("apple")); // return True
+        debug_assert!(!trie.search("app")); // return False
+        debug_assert!(trie.starts_with("app")); // return True
+        trie.insert("app");
+        debug_assert!(trie.search("app")); // return True
     }
 
     #[test]
-    fn test() {
-        debug_assert!(can_finish(5, &[[1, 4], [2, 4], [3, 1], [3, 2]]));
-    }
+    fn test() {}
 }
