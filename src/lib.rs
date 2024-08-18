7851f949d9dd1fb1f6926bc60ad573023a8fe5c3
pkusensei <ZW_08@outlook.com>
2024-08-18 08:38:31 -0400

    201 bitwise and for range of ints

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,27 @@
 mod helper;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_islands(grid: &[&[char]]) -> i32 {
-    let mut seen = HashSet::new();
-    let mut res = 0;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, &ch) in row.iter().enumerate() {
-            if ch == '1' && seen.insert((x, y)) {
-                find_cluster((x, y), &mut seen, grid);
-                res += 1
-            }
+pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+    fn bit_shift(mut left: i32, mut right: i32) -> i32 {
+        let mut shift = 0;
+        while left < right {
+            left >>= 1;
+            right >>= 1;
+            shift += 1;
         }
+        left << shift
     }
-    res
-}
 
-fn find_cluster(coord: Coord, seen: &mut HashSet<Coord>, grid: &[&[char]]) {
-    let mut queue = VecDeque::from([coord]);
-    while let Some(coord) = queue.pop_front() {
-        for (x, y) in neighbors(coord) {
-            if grid
-                .get(y)
-                .is_some_and(|r| r.get(x).is_some_and(|&c| c == '1'))
-                && seen.insert((x, y))
-            {
-                queue.push_back((x, y))
-            }
-        }
+    if left == 0 {
+        return 0;
+    }
+    let (left_p, right_p) = (left.ilog2(), right.ilog2());
+    if left_p == right_p {
+        (left..=right).fold(left, |acc, n| acc & n)
+    } else {
+        0
     }
 }
 
@@ -40,24 +31,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            num_islands(&[
-                &['1', '1', '1', '1', '0'],
-                &['1', '1', '0', '1', '0'],
-                &['1', '1', '0', '0', '0'],
-                &['0', '0', '0', '0', '0'],
-            ]),
-            1
-        );
-        debug_assert_eq!(
-            num_islands(&[
-                &['1', '1', '0', '0', '0'],
-                &['1', '1', '0', '0', '0'],
-                &['0', '0', '1', '0', '0'],
-                &['0', '0', '0', '1', '1'],
-            ]),
-            3
-        );
+        debug_assert_eq!(range_bitwise_and(5, 7), 4);
+        debug_assert_eq!(range_bitwise_and(0, 0), 0);
+        debug_assert_eq!(range_bitwise_and(1, i32::MAX), 0);
     }
 
     #[test]
