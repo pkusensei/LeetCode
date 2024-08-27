cbe0e99f70f5f90c9ef673d05093d91542fa91b2
pkusensei <ZW_08@outlook.com>
2024-08-26 21:04:20 -0400

    1514 dijkstra but find the max product

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,61 +1,75 @@
 mod helper;
 
+use std::collections::{BinaryHeap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn nearest_palindromic(n: &str) -> String {
-    let num: u64 = n.parse().unwrap();
-    let a = search_left(num);
-    let b = search_right(num);
-    if a.abs_diff(num) <= b.abs_diff(num) {
-        a.to_string()
-    } else {
-        b.to_string()
-    }
+pub fn max_probability(n: i32, edges: &[[i32; 2]], succ_prob: &[f64], start: i32, end: i32) -> f64 {
+    let graph: HashMap<i32, Vec<(i32, f64)>> =
+        edges
+            .iter()
+            .zip(succ_prob)
+            .fold(HashMap::new(), |mut acc, (&[n1, n2], &prob)| {
+                acc.entry(n1).or_default().push((n2, prob.log2()));
+                acc.entry(n2).or_default().push((n1, prob.log2()));
+                acc
+            });
+    dijkstra(n, &graph, start, end)
+        .map(|p| 2f64.powf(p))
+        .unwrap_or(0.0)
 }
 
-fn search_left(num: u64) -> u64 {
-    let (mut left, mut right) = (0, num);
-    let mut res = 0;
-    while left <= right {
-        let mid = (right - left) / 2 + left;
-        let p = build(mid);
-        if p < num {
-            res = p;
-            left = mid + 1
-        } else {
-            right = mid - 1;
+fn dijkstra(n: i32, graph: &HashMap<i32, Vec<(i32, f64)>>, start: i32, end: i32) -> Option<f64> {
+    let mut dist: Vec<_> = (0..n).map(|_| f64::MIN).collect();
+    dist[start as usize] = 0.0;
+    let mut heap = BinaryHeap::from([State {
+        node: start,
+        prob: 0.0,
+    }]);
+    while let Some(State { node, prob }) = heap.pop() {
+        if node == end {
+            return Some(prob);
+        }
+        if prob < dist[node as usize] {
+            continue;
+        }
+        if let Some(adj) = graph.get(&node) {
+            for edge in adj {
+                let next = State {
+                    node: edge.0,
+                    prob: prob + edge.1,
+                };
+                if next.prob > dist[next.node as usize] {
+                    heap.push(next);
+                    dist[next.node as usize] = next.prob
+                }
+            }
         }
     }
-    res
+    None
 }
 
-fn search_right(num: u64) -> u64 {
-    let (mut left, mut right) = (num, u64::MAX);
-    let mut res = 0;
-    while left <= right {
-        let mid = (right - left) / 2 + left;
-        let p = build(mid);
-        if p > num {
-            res = p;
-            right = mid - 1
-        } else {
-            left = mid + 1
-        }
-    }
-    res
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct State {
+    node: i32,
+    prob: f64,
 }
 
-fn build(num: u64) -> u64 {
-    let mut v: Vec<_> = num.to_string().chars().collect();
-    let size = v.len();
-    v.truncate((size + 1) / 2);
-    let mut half = v.clone();
-    if size & 1 == 1 {
-        half.pop();
+impl Eq for State {}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
-    v.extend(half.into_iter().rev());
-    v.into_iter().collect::<String>().parse().unwrap()
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.prob
+            .partial_cmp(&other.prob)
+            .unwrap_or(self.node.cmp(&other.node))
+    }
 }
 
 #[cfg(test)]
@@ -64,12 +78,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(nearest_palindromic("123"), "121");
-        debug_assert_eq!(nearest_palindromic("1"), "0");
+        debug_assert_eq!(
+            max_probability(3, &[[0, 1], [1, 2], [0, 2]], &[0.5, 0.5, 0.2], 0, 2),
+            0.25000
+        );
+        debug_assert_eq!(
+            max_probability(3, &[[0, 1], [1, 2], [0, 2]], &[0.5, 0.5, 0.3], 0, 2),
+            0.30000
+        );
+        debug_assert_eq!(max_probability(3, &[[0, 1]], &[0.5], 0, 2), 0.0);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(nearest_palindromic("1213"), "1221");
-    }
+    fn test() {}
 }
