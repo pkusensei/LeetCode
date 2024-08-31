030890d7381f42bc84d42d57df1b5aa9f0eef577
pkusensei <ZW_08@outlook.com>
2024-08-31 12:10:06 -0400

    229 Boyer-Moore Voting Algorithm; again

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,16 +3,41 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn summary_ranges(nums: &[i32]) -> Vec<String> {
-    nums.chunk_by(|&a, &b| a + 1 == b)
-        .map(|ch| {
-            if ch.len() == 1 {
-                format!("{}", ch[0])
-            } else {
-                format!("{}->{}", ch[0], *ch.last().unwrap())
-            }
-        })
-        .collect()
+pub fn majority_element(nums: &[i32]) -> Vec<i32> {
+    //Boyer-Moore Voting Algorithm
+    let (mut c1, mut n1, mut c2, mut n2) = (0, 0, 0, 0);
+    for &num in nums {
+        if num == n1 {
+            c1 += 1
+        } else if num == n2 {
+            c2 += 1
+        } else if c1 == 0 {
+            n1 = num;
+            c1 += 1
+        } else if c2 == 0 {
+            n2 = num;
+            c2 += 1
+        } else {
+            c1 -= 1;
+            c2 -= 1
+        }
+    }
+    let (mut c1, mut c2) = (0, 0);
+    for &num in nums {
+        if num == n1 {
+            c1 += 1
+        } else if num == n2 {
+            c2 += 1;
+        }
+    }
+    let mut res = vec![];
+    if c1 > nums.len() / 3 {
+        res.push(n1)
+    }
+    if c2 > nums.len() / 3 {
+        res.push(n2)
+    }
+    res
 }
 
 #[cfg(test)]
@@ -21,13 +46,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(summary_ranges(&[0, 1, 2, 4, 5, 7]), ["0->2", "4->5", "7"]);
-        debug_assert_eq!(
-            summary_ranges(&[0, 2, 3, 4, 6, 8, 9]),
-            ["0", "2->4", "6", "8->9"]
-        );
+        debug_assert_eq!(majority_element(&[3, 2, 3]), [3]);
+        debug_assert_eq!(majority_element(&[1]), [1]);
+        debug_assert_eq!(majority_element(&[1, 2]), [1, 2]);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(majority_element(&[0, 0, 0]), [0]);
+    }
 }
