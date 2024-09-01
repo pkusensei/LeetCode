9c1f87cfdbdf5e8c0b2ee549ef60667471ae03bc
pkusensei <ZW_08@outlook.com>
2024-08-31 20:22:52 -0400

    2022 1d array to 2d

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,41 +3,14 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn majority_element(nums: &[i32]) -> Vec<i32> {
-    //Boyer-Moore Voting Algorithm
-    let (mut c1, mut n1, mut c2, mut n2) = (0, 0, 0, 0);
-    for &num in nums {
-        if num == n1 {
-            c1 += 1
-        } else if num == n2 {
-            c2 += 1
-        } else if c1 == 0 {
-            n1 = num;
-            c1 += 1
-        } else if c2 == 0 {
-            n2 = num;
-            c2 += 1
-        } else {
-            c1 -= 1;
-            c2 -= 1
-        }
+pub fn construct2_d_array(original: &[i32], m: i32, n: i32) -> Vec<Vec<i32>> {
+    if (m * n) as usize != original.len() {
+        return vec![];
     }
-    let (mut c1, mut c2) = (0, 0);
-    for &num in nums {
-        if num == n1 {
-            c1 += 1
-        } else if num == n2 {
-            c2 += 1;
-        }
-    }
-    let mut res = vec![];
-    if c1 > nums.len() / 3 {
-        res.push(n1)
-    }
-    if c2 > nums.len() / 3 {
-        res.push(n2)
-    }
-    res
+    original
+        .chunks_exact(n as usize)
+        .map(|w| w.to_vec())
+        .collect()
 }
 
 #[cfg(test)]
@@ -46,13 +19,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(majority_element(&[3, 2, 3]), [3]);
-        debug_assert_eq!(majority_element(&[1]), [1]);
-        debug_assert_eq!(majority_element(&[1, 2]), [1, 2]);
+        debug_assert_eq!(construct2_d_array(&[1, 2, 3, 4], 2, 2), [[1, 2], [3, 4]]);
+        debug_assert_eq!(construct2_d_array(&[1, 2, 3], 1, 3), [[1, 2, 3]]);
+        debug_assert!(construct2_d_array(&[1, 2], 1, 1).is_empty());
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(majority_element(&[0, 0, 0]), [0]);
-    }
+    fn test() {}
 }
