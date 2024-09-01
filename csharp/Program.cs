c1e133930a8d451b7a703540626dbda5453629c5
pkusensei <ZW_08@outlook.com>
2024-09-01 08:06:55 -0400

    230 kth element in binary search tree

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -6,25 +6,39 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = Tree.TreeNode.Make([4, 2, 7, 1, 3, 6, 9]);
-    var s = InvertTree(n);
-    var a = "[4,7,2,9,6,3,1]";
-    Debug.Assert(s.ToString() == a, $"Output: {s}\n Expected: {a}");
+    var n = Tree.TreeNode.Make([3, 1, 4, null, 2]);
+    var s = KthSmallest(n, 1);
+    var a = 1;
+    Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
 }
 
 void Test2()
 {
-    var n = Tree.TreeNode.Make([2, 1, 3]);
-    var s = InvertTree(n);
-    var a = "[2,3,1]";
-    Debug.Assert(s.ToString() == a, $"Output: {s}\n Expected: {a}");
+    var n = Tree.TreeNode.Make([5, 3, 6, 2, 4, null, null, 1]);
+    var s = KthSmallest(n, 3);
+    var a = 3;
+    Debug.Assert(s == a, $"Output: {s}\nExpect: {a}");
 }
 
-Tree.TreeNode InvertTree(Tree.TreeNode root)
+int KthSmallest(Tree.TreeNode root, int k)
 {
-    if (root is null) { return null; }
-    (var left, var right) = (InvertTree(root.right), InvertTree(root.left));
-    root.left = left;
-    root.right = right;
-    return root;
+    var stack = new Stack<Tree.TreeNode>();
+    var curr = root;
+    var i = 0;
+    while (stack.Count > 0 || curr is not null)
+    {
+        if (curr is not null)
+        {
+            stack.Push(curr);
+            curr = curr.left;
+        }
+        else
+        {
+            var temp = stack.Pop();
+            i += 1;
+            if (i == k) { return temp.val; }
+            curr = temp.right;
+        }
+    }
+    return 0;
 }
