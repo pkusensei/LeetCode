0cfbadac5432ffe41b764c6ec5e9f37f0ce8b2cb
pkusensei <ZW_08@outlook.com>
2024-09-01 18:33:24 -0400

    236 lowest common ancestor in binary tree

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,26 +8,28 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var n = TreeNode.Make([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
-    var s = LowestCommonAncestor(n, new(2), new(8));
-    var a = 6;
+    var n = TreeNode.Make([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+    var s = LowestCommonAncestor(n, new(5), new(1));
+    var a = 3;
     Debug.Assert(s.val == a, $"Output: {s}\nExpect: {a}");
 }
 
 void Test2()
 {
-    var n = TreeNode.Make([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
-    var s = LowestCommonAncestor(n, new(2), new(4));
-    var a = 2;
+    var n = TreeNode.Make([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+    var s = LowestCommonAncestor(n, new(5), new(4));
+    var a = 5;
     Debug.Assert(s.val == a, $"Output: {s}\nExpect: {a}");
 }
 
 TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
 {
-    var curr = root;
-    var diffp = p.val - curr.val;
-    var diffq = q.val - root.val;
-    if (diffp == 0 || diffq == 0 || (diffp < 0) != (diffq < 0)) { return curr; }
-    if (diffp < 0) { return LowestCommonAncestor(curr.left, p, q); }
-    else { return LowestCommonAncestor(curr.right, p, q); }
+        if (root is null) { return null; }
+        if (root.val == p.val || root.val == q.val) { return root; }
+
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        if (left is not null && right is not null) { return root; }
+        else if (left is not null) { return left; }
+        else { return right; }
 }
