8e0e041a5735811050eef91f7c2c5be12f10ad99
pkusensei <ZW_08@outlook.com>
2024-09-02 12:28:35 -0400

    260 find two diff ints in array of paired ints

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,10 +3,22 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub const fn add_digits(num: i32) -> i32 {
-    // https://en.wikipedia.org/wiki/Digital_root#Using_the_floor_function
-    const BASE: i32 = 10;
-    num - (BASE - 1) * ((num - 1) / (BASE - 1))
+pub fn single_number(nums: &[i32]) -> Vec<i32> {
+    let mask = nums.iter().skip(1).fold(nums[0], |acc, &num| acc ^ num);
+    let mut i = 0;
+    while (mask >> i) & 1 == 0 {
+        i += 1;
+    }
+    let bit = 1 << i;
+    let a = nums
+        .iter()
+        .filter(|&num| num & bit != 0)
+        .fold(0, |acc, num| acc ^ num);
+    // let b = nums
+    //     .iter()
+    //     .filter(|&num| num & bit == 0)
+    //     .fold(0, |acc, num| acc ^ num);
+    vec![a, mask ^ a]
 }
 
 #[cfg(test)]
@@ -15,10 +27,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(add_digits(38), 2);
-        debug_assert_eq!(add_digits(0), 0);
+        debug_assert_eq!(single_number(&[1, 2, 1, 3, 2, 5]), [3, 5]);
+        debug_assert_eq!(single_number(&[-1, 0]), [-1, 0]);
+        debug_assert_eq!(single_number(&[0, 1]), [1, 0]);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(single_number(&[1, 1, 0, -2147483648]), [-2147483648, 0])
+    }
 }
