800fcafc66033d603e55fa803f60add3a4f2bdff
pkusensei <ZW_08@outlook.com>
2024-09-02 10:47:49 -0400

    241 do + - * in expr with random precedence

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,21 +1,43 @@
 mod helper;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn search_matrix(matrix: &[&[i32]], target: i32) -> bool {
-    let first_col: Vec<_> = matrix.iter().map(|r| r[0]).collect();
-    match first_col.binary_search(&target) {
-        Ok(_) => true,
-        Err(y) if y == 0 => false,
-        Err(y1) => {
-            let last_col: Vec<_> = matrix.iter().map(|r| *r.last().unwrap()).take(y1).collect();
-            match last_col.binary_search(&target) {
-                Ok(_) => true,
-                Err(y2) => (y2..y1).any(|y| matrix[y].binary_search(&target).is_ok()),
+pub fn diff_ways_to_compute(expression: &str) -> Vec<i32> {
+    solve(expression, &mut HashMap::new())
+}
+
+fn solve<'a>(expr: &'a str, seen: &mut HashMap<&'a str, Vec<i32>>) -> Vec<i32> {
+    if let Some(v) = seen.get(expr) {
+        return v.clone();
+    }
+    if let Ok(r) = expr.parse() {
+        seen.insert(expr, vec![r]);
+        return vec![r];
+    }
+    let mut res = vec![];
+    for (idx, b) in expr
+        .bytes()
+        .enumerate()
+        .filter(|(_, b)| b"+-*".contains(&b))
+    {
+        let left = solve(&expr[..idx], seen);
+        let right = solve(&expr[idx + 1..], seen);
+        for r1 in left {
+            for r2 in &right {
+                match b {
+                    b'+' => res.push(r1 + r2),
+                    b'-' => res.push(r1 - r2),
+                    b'*' => res.push(r1 * r2),
+                    _ => (),
+                }
             }
         }
     }
+    seen.insert(expr, res.clone());
+    res
 }
 
 #[cfg(test)]
@@ -24,26 +46,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(search_matrix(
-            &[
-                &[1, 4, 7, 11, 15],
-                &[2, 5, 8, 12, 19],
-                &[3, 6, 9, 16, 22],
-                &[10, 13, 14, 17, 24],
-                &[18, 21, 23, 26, 30]
-            ],
-            5
-        ));
-        debug_assert!(!search_matrix(
-            &[
-                &[1, 4, 7, 11, 15],
-                &[2, 5, 8, 12, 19],
-                &[3, 6, 9, 16, 22],
-                &[10, 13, 14, 17, 24],
-                &[18, 21, 23, 26, 30]
-            ],
-            20
-        ))
+        // debug_assert_eq!(diff_ways_to_compute(&"2-1-1"), [0, 2]);
+        debug_assert_eq!(diff_ways_to_compute(&"2*3-4*5"), [-34, -14, -10, -10, 10]);
     }
 
     #[test]
