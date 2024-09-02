8f498f1a6891358f1eb3285f5217b16873894228
pkusensei <ZW_08@outlook.com>
2024-09-02 18:00:49 -0400

    279 any natual number can be the sum of at most 4 int squares

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,22 +3,34 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn h_index(nums: &[i32]) -> i32 {
-    let (mut left, mut right) = (0, nums.len() - 1);
-    let mut res = 0;
-    while left <= right {
-        let mid = (right - left) / 2 + left;
-        if nums.len() - mid <= nums[mid] as usize {
-            res = nums.len() - mid;
-            if mid == 0 {
-                break;
-            }
-            right = mid - 1
+pub fn num_squares(n: i32) -> i32 {
+    let candidates: Vec<_> = (1..).map(|v| v * v).take_while(|&v| v <= n).collect();
+    if candidates.last().is_some_and(|&v| v == n) {
+        1
+    } else if two_sum(&candidates, n) {
+        2
+    } else if three_sum(&candidates, n) {
+        3
+    } else {
+        4
+    }
+}
+
+fn two_sum(nums: &[i32], n: i32) -> bool {
+    let mut map = std::collections::HashSet::new();
+    for &num in nums.iter() {
+        let comp = n - num;
+        if map.contains(&comp) || comp == num {
+            return true;
         } else {
-            left = mid + 1;
+            map.insert(num);
         }
     }
-    res as i32
+    false
+}
+
+fn three_sum(nums: &[i32], n: i32) -> bool {
+    nums.iter().any(|&num| two_sum(nums, n - num))
 }
 
 #[cfg(test)]
@@ -27,12 +39,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(h_index(&[0, 1, 3, 5, 6]), 3);
-        debug_assert_eq!(h_index(&[1, 2, 100]), 2);
+        debug_assert_eq!(num_squares(12), 3);
+        debug_assert_eq!(num_squares(13), 2);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(h_index(&[1]), 1);
-    }
+    fn test() {}
 }
