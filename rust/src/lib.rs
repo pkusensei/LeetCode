bf6f20ea70c4717a50849b193b194a58c0886792
pkusensei <ZW_08@outlook.com>
2024-09-02 20:31:30 -0400

    1945 str -> u8 array -> add digits

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,34 +3,30 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_squares(n: i32) -> i32 {
-    let candidates: Vec<_> = (1..).map(|v| v * v).take_while(|&v| v <= n).collect();
-    if candidates.last().is_some_and(|&v| v == n) {
-        1
-    } else if two_sum(&candidates, n) {
-        2
-    } else if three_sum(&candidates, n) {
-        3
-    } else {
-        4
-    }
-}
-
-fn two_sum(nums: &[i32], n: i32) -> bool {
-    let mut map = std::collections::HashSet::new();
-    for &num in nums.iter() {
-        let comp = n - num;
-        if map.contains(&comp) || comp == num {
-            return true;
-        } else {
-            map.insert(num);
+pub fn get_lucky(s: &str, k: i32) -> i32 {
+    let mut num: i32 = s
+        .bytes()
+        .map(|b| {
+            let n = i32::from(b - b'a' + 1);
+            if n >= 10 {
+                n % 10 + n / 10
+            } else {
+                n
+            }
+        })
+        .sum();
+    for _ in 0..k - 1 {
+        if num < 10 {
+            return num;
+        }
+        let mut temp = num;
+        num = 0;
+        while temp > 0 {
+            num += temp % 10;
+            temp /= 10;
         }
     }
-    false
-}
-
-fn three_sum(nums: &[i32], n: i32) -> bool {
-    nums.iter().any(|&num| two_sum(nums, n - num))
+    num
 }
 
 #[cfg(test)]
@@ -39,8 +35,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(num_squares(12), 3);
-        debug_assert_eq!(num_squares(13), 2);
+        debug_assert_eq!(get_lucky("iiii", 1), 36);
+        debug_assert_eq!(get_lucky("leetcode", 2), 6);
+        debug_assert_eq!(get_lucky("zbax", 2), 8);
     }
 
     #[test]
