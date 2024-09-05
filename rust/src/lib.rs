1f99c717a2583fca35cf869fc4277786829f18cb
Zx Wang <ZW_08@outlook.com>
2024-09-05 13:44:27 -0400

    309 buy and sell with cooldown

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,50 +3,15 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-#[derive(Debug, Clone)]
-struct NumArray {
-    tree: Vec<i32>,
-    n: usize,
-}
-
-impl NumArray {
-    fn new(nums: Vec<i32>) -> Self {
-        let n = nums.len();
-        let mut tree = vec![0; n * 2];
-        tree[n..].copy_from_slice(&nums);
-        for i in (1..n).rev() {
-            tree[i] = tree[i << 1] + tree[i << 1 | 1] // i*2+1
-        }
-        Self { tree, n }
+pub fn max_profit(prices: &[i32]) -> i32 {
+    let (mut buy, mut sell, mut cool) = (i32::MIN, 0, 0);
+    for &num in prices.iter() {
+        let b = buy.max(cool - num);
+        let s = sell.max(buy + num);
+        let c = sell.max(cool);
+        (buy, sell, cool) = (b, s, c)
     }
-
-    fn update(&mut self, index: i32, val: i32) {
-        let mut idx = index as usize;
-        idx += self.n;
-        self.tree[idx] = val;
-        while idx > 1 {
-            self.tree[idx >> 1] = self.tree[idx] + self.tree[idx ^ 1];
-            idx >>= 1;
-        }
-    }
-
-    fn sum_range(&self, left: i32, right: i32) -> i32 {
-        let (mut left, mut right) = (left as usize + self.n, right as usize + self.n);
-        let mut res = 0;
-        while left <= right {
-            if left & 1 == 1 {
-                res += self.tree[left];
-                left += 1;
-            }
-            if right & 1 == 0 {
-                res += self.tree[right];
-                right -= 1;
-            }
-            left >>= 1;
-            right >>= 1;
-        }
-        res
-    }
+    sell
 }
 
 #[cfg(test)]
@@ -57,10 +22,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut arr = NumArray::new(vec![1, 3, 5]);
-        debug_assert_eq!(arr.sum_range(0, 2), 9); // return 1 + 3 + 5 = 9
-        arr.update(1, 2); // nums = [1, 2, 5]
-        debug_assert_eq!(arr.sum_range(0, 2), 8); // return 1 + 2 + 5 = 8
+        debug_assert_eq!(max_profit(&[1, 2, 3, 0, 2]), 3);
+        debug_assert_eq!(max_profit(&[1]), 0);
     }
 
     #[test]
