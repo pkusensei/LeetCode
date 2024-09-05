7d10014499b6d34f108bbbad91e3c5456e1183ce
Zx Wang <ZW_08@outlook.com>
2024-09-05 15:10:59 -0400

    310 find roots in graph/tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,17 +1,39 @@
 mod helper;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_profit(prices: &[i32]) -> i32 {
-    let (mut buy, mut sell, mut cool) = (i32::MIN, 0, 0);
-    for &num in prices.iter() {
-        let b = buy.max(cool - num);
-        let s = sell.max(buy + num);
-        let c = sell.max(cool);
-        (buy, sell, cool) = (b, s, c)
+pub fn find_min_height_trees(n: i32, edges: &[[i32; 2]]) -> Vec<i32> {
+    if n == 1 {
+        return vec![0];
     }
-    sell
+    let mut n = n as usize;
+    let mut graph: HashMap<i32, Vec<_>> = edges.iter().fold(HashMap::new(), |mut acc, e| {
+        acc.entry(e[0]).or_default().push(e[1]);
+        acc.entry(e[1]).or_default().push(e[0]);
+        acc
+    });
+    let mut leaf: Vec<_> = graph
+        .iter()
+        .filter_map(|(&k, v)| if v.len() == 1 { Some(k) } else { None })
+        .collect();
+    while n > 2 {
+        n -= leaf.len();
+        let mut new_leaf = vec![];
+        for node in leaf {
+            let neighbor = graph[&node][0];
+            if let Some(v) = graph.get_mut(&neighbor) {
+                v.retain(|&num| num != node);
+                if v.len() == 1 {
+                    new_leaf.push(neighbor)
+                }
+            }
+        }
+        leaf = new_leaf;
+    }
+    leaf
 }
 
 #[cfg(test)]
@@ -22,8 +44,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_profit(&[1, 2, 3, 0, 2]), 3);
-        debug_assert_eq!(max_profit(&[1]), 0);
+        debug_assert_eq!(find_min_height_trees(4, &[[1, 0], [1, 2], [1, 3]]), [1]);
+        sort_eq(
+            find_min_height_trees(6, &[[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]),
+            [3, 4],
+        );
     }
 
     #[test]
