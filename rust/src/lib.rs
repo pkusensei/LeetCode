b64bcb56687c086f1c6de6a0f7cc1dcf7d9fd61c
pkusensei <ZW_08@outlook.com>
2024-09-07 12:16:23 -0400

    321 a mix of 1673 and 1754 with minor tweaks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,46 +3,38 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-// pub fn max_number(nums1: &[i32], nums2: &[i32], k: i32) -> Vec<i32> {
-//     todo!()
-// }
+pub fn max_number(nums1: &[i32], nums2: &[i32], k: i32) -> Vec<i32> {
+    let k = k as usize;
+    let mut res = vec![];
+    for k1 in 0..=nums1.len() {
+        let k2 = k.saturating_sub(k1).min(nums2.len());
+        if k1 + k2 != k {
+            continue;
+        }
+        let v1 = most_competitive(nums1, k1, |a, b| a > b);
+        let v2 = most_competitive(nums2, k2, |a, b| a > b);
+        res.push(largest_merge(&v1, &v2));
+    }
+    res.into_iter().max().unwrap()
+}
 
 // 1673
-pub fn most_competitive(nums: &[i32], k: i32) -> Vec<i32> {
-    fn with_binary_heap(nums: &[i32], k: i32) -> Vec<i32> {
-        use std::{cmp::Reverse, collections::BinaryHeap};
-
-        let mut queue = BinaryHeap::new();
-        let mut res = vec![];
-        for (i, &num) in nums.iter().enumerate() {
-            queue.push(Reverse((num, i)));
-            if i >= nums.len() - k as usize {
-                let Some(Reverse((num, idx))) = queue.pop() else {
-                    break;
-                };
-                res.push(num);
-                while queue.peek().is_some_and(|&Reverse((_n, j))| j < idx) {
-                    // pop any number eligible but to the left of (num, idx)
-                    queue.pop();
-                }
-            }
-        }
-        res
+pub fn most_competitive(nums: &[i32], k: usize, pred: fn(i32, i32) -> bool) -> Vec<i32> {
+    if k == 0 {
+        return vec![];
     }
-
-    let k = k as usize;
-    // monotonically increasing stack
+    // monotonic stack
     let mut stack = Vec::with_capacity(k);
     for (i, &num) in nums.iter().enumerate() {
         while stack
             .last()
-            .is_some_and(|&n| num < n && nums.len() - i > k - stack.len())
+            .is_some_and(|&n| pred(num, n) && nums.len() - i > k - stack.len())
         {
             // nums.len() - i => amount of num left to choose from
             // k - stack.len() => amount of num missing in res
             stack.pop();
         }
-        // Only keeps the smallest k numbers
+        // Only keeps the largest k numbers
         if stack.len() < k {
             stack.push(num);
         }
@@ -51,8 +43,10 @@ pub fn most_competitive(nums: &[i32], k: i32) -> Vec<i32> {
 }
 
 // 1754
-pub fn largest_merge(word1: &str, word2: &str) -> String {
-    let (mut s1, mut s2) = (word1.as_bytes(), word2.as_bytes());
+pub fn largest_merge<T>(mut s1: &[T], mut s2: &[T]) -> Vec<T>
+where
+    T: Copy + PartialOrd,
+{
     let mut res = Vec::with_capacity(s1.len() + s2.len());
     while !s1.is_empty() && !s2.is_empty() {
         if s1 > s2 {
@@ -65,7 +59,7 @@ pub fn largest_merge(word1: &str, word2: &str) -> String {
     }
     res.extend_from_slice(s1);
     res.extend_from_slice(s2);
-    res.into_iter().map(char::from).collect()
+    res
 }
 
 #[cfg(test)]
@@ -77,19 +71,23 @@ mod tests {
     #[test]
     fn basics() {
         // 1673
-        debug_assert_eq!(most_competitive(&[3, 5, 2, 6], 2), [2, 6]);
-        debug_assert_eq!(most_competitive(&[2, 4, 3, 3, 5, 4, 9, 6], 4), [2, 3, 3, 4]);
+        debug_assert_eq!(most_competitive(&[3, 5, 2, 6], 2, |a, b| a < b), [2, 6]);
+        debug_assert_eq!(
+            most_competitive(&[2, 4, 3, 3, 5, 4, 9, 6], 4, |a, b| a < b),
+            [2, 3, 3, 4]
+        );
 
         // 1754
-        debug_assert_eq!(largest_merge("cabaa", "bcaaa"), "cbcabaaaaa");
-        debug_assert_eq!(largest_merge("abcabc", "abdcaba"), "abdcabcabcaba");
+        debug_assert_eq!(largest_merge(b"cabaa", b"bcaaa"), b"cbcabaaaaa");
+        debug_assert_eq!(largest_merge(b"abcabc", b"abdcaba"), b"abdcabcabcaba");
 
-        // debug_assert_eq!(
-        //     max_number(&[3, 4, 6, 5], &[9, 1, 2, 5, 8, 3], 5),
-        //     [9, 8, 6, 5, 3]
-        // );
-        // debug_assert_eq!(max_number(&[6, 7], &[6, 0, 4], 5), [6, 7, 6, 0, 4]);
-        // debug_assert_eq!(max_number(&[3, 9], &[8, 9], 3), [9, 8, 9]);
+        // 321
+        debug_assert_eq!(
+            max_number(&[3, 4, 6, 5], &[9, 1, 2, 5, 8, 3], 5),
+            [9, 8, 6, 5, 3]
+        );
+        debug_assert_eq!(max_number(&[6, 7], &[6, 0, 4], 5), [6, 7, 6, 0, 4]);
+        debug_assert_eq!(max_number(&[3, 9], &[8, 9], 3), [9, 8, 9]);
     }
 
     #[test]
