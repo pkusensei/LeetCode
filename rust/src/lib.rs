d70d7f6da97aa4aa80271a2e625def2693b3c879
pkusensei <ZW_08@outlook.com>
2024-09-06 20:16:05 -0400

    316 remove dup chars from string and produce smallest lexico string

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,15 +3,63 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_smaller(nums: &[i32]) -> Vec<i32> {
-    let mut sorted = Vec::with_capacity(nums.len());
-    let mut res = Vec::with_capacity(nums.len());
-    for &num in nums.iter().rev() {
-        let i = sorted.partition_point(|&n| n < num);
-        res.push(i as i32);
-        sorted.insert(i, num);
+pub fn remove_duplicate_letters(s: &str) -> String {
+    let mut count = s.bytes().fold([0; 26], |mut acc, b| {
+        acc[usize::from(b - b'a')] += 1;
+        acc
+    });
+    let mut seen = [false; 26]; // track what's in stack
+    let mut stack = vec![];
+    for b in s.bytes() {
+        let idx = usize::from(b - b'a');
+        count[idx] -= 1;
+        if seen[idx] {
+            continue;
+        }
+        while stack
+            .last()
+            .is_some_and(|&v| b < v && count[usize::from(v - b'a')] > 0)
+        {
+            // pop "bigger" chars unless this is their final occurrence
+            let Some(v) = stack.pop() else {
+                break;
+            };
+            seen[usize::from(v - b'a')] = false
+        }
+        stack.push(b);
+        seen[idx] = true;
     }
-    res.reverse();
+    stack.into_iter().map(char::from).collect()
+    // solve(s.as_bytes().to_vec())
+    //     .into_iter()
+    //     .map(char::from)
+    //     .collect()
+}
+
+fn solve(mut s: Vec<u8>) -> Vec<u8> {
+    if s.is_empty() {
+        return s;
+    }
+    let mut count = s.iter().fold([0; 26], |mut acc, &b| {
+        acc[usize::from(b - b'a')] += 1;
+        acc
+    });
+    let (mut idx, mut byte) = (0, s[0]);
+    for (i, &b) in s.iter().enumerate() {
+        if b < byte {
+            idx = i;
+            byte = b;
+        }
+        let c = usize::from(b - b'a');
+        count[c] -= 1;
+        if count[c] == 0 {
+            break;
+        }
+    }
+    let mut rest = s.split_off(idx + 1);
+    rest.retain(|&b| b != byte);
+    let mut res = solve(rest);
+    res.insert(0, byte);
     res
 }
 
@@ -23,9 +71,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(count_smaller(&[5, 2, 6, 1]), &[2, 1, 1, 0]);
-        debug_assert_eq!(count_smaller(&[-1]), [0]);
-        debug_assert_eq!(count_smaller(&[-1, -1]), [0, 0])
+        debug_assert_eq!(remove_duplicate_letters("bcabc"), "abc");
+        debug_assert_eq!(remove_duplicate_letters("cbacdcbc"), "acdb");
     }
 
     #[test]
