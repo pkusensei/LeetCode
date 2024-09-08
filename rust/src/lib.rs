ce881af3d3eca7d58594e7c03de9b5e5d97bcb8c
pkusensei <ZW_08@outlook.com>
2024-09-08 13:04:04 -0400

    327 count of range sums that fit in [a..b]; prefix sum and merge sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,8 +3,39 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub const fn is_power_of_three(n: i32) -> bool {
-    n >= 1 && 3i32.pow(19) % n == 0
+pub fn count_range_sum(nums: &[i32], lower: i32, upper: i32) -> i32 {
+    let n = nums.len();
+    let mut prefix = Vec::with_capacity(n);
+    prefix.push(nums[0].into());
+    for (i, &v) in nums.iter().enumerate().skip(1) {
+        prefix.push(prefix[i - 1] + i64::from(v));
+    }
+    count_merge_sort(&mut prefix, lower, upper)
+        + nums.iter().filter(|&n| (lower..=upper).contains(n)).count() as i32
+}
+
+fn count_merge_sort(values: &mut [i64], lower: i32, upper: i32) -> i32 {
+    let mid = values.len() / 2;
+    if mid == 0 {
+        return 0; // exclude when len <= 1
+    }
+    let mut count = count_merge_sort(&mut values[..mid], lower, upper)
+        + count_merge_sort(&mut values[mid..], lower, upper);
+    let (mut low, mut high) = (mid, mid);
+    for &num in values[..mid].iter() {
+        while values.get(low).is_some_and(|&v| v - num < i64::from(lower)) {
+            low += 1 // the first index that lower <= delta
+        }
+        while values
+            .get(high)
+            .is_some_and(|&v| v - num <= i64::from(upper))
+        {
+            high += 1 // the last index that delta <= upper
+        }
+        count += (high - low) as i32;
+    }
+    values.sort(); // slice is partially sorted
+    count
 }
 
 #[cfg(test)]
@@ -15,13 +46,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(is_power_of_three(27));
-        debug_assert!(!is_power_of_three(0));
-        debug_assert!(!is_power_of_three(-1));
+        debug_assert_eq!(count_range_sum(&[-2, 5, -1], -2, 2), 3);
+        debug_assert_eq!(count_range_sum(&[0], 0, 0), 1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(count_range_sum(&[0, -3, -3, 1, 1, 2], 3, 5), 2);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(i1: I1, i2: I2)
