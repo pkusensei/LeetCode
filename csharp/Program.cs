250c37a302e704db787b5147135bf30165786377
pkusensei <ZW_08@outlook.com>
2024-09-08 13:41:40 -0400

    328 reorder linked list by odd and even indices

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,59 +8,33 @@ Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var a = ListNode.Make([1, 2, 3]);
-    var b = SplitListToParts(a, 5);
-    var c = "[[1],[2],[3],[],[]]";
-    Debug.Assert(b.Print() == c, $"Output: {b.Print()}\nExpect: {c}");
+    var a = ListNode.Make([1, 2, 3, 4, 5]);
+    var b = OddEvenList(a);
+    var c = "[1,3,5,2,4]";
+    Debug.Assert(b.ToString() == c, $"Output: {b}\nExpect: {c}");
 }
 
 void Test2()
 {
-    var a = ListNode.Make([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    var b = SplitListToParts(a, 3);
-    var c = "[[1,2,3,4],[5,6,7],[8,9,10]]";
-    Debug.Assert(b.Print() == c, $"Output: {b.Print()}\nExpect: {c}");
+    var a = ListNode.Make([2, 1, 3, 5, 6, 4, 7]);
+    var b = OddEvenList(a);
+    var c = "[2,3,6,7,1,5,4]";
+    Debug.Assert(b.ToString() == c, $"Output: {b}\nExpect: {c}");
 }
 
-ListNode[] SplitListToParts(ListNode head, int k)
+ListNode OddEvenList(ListNode head)
 {
-    var count = 0;
-    var curr = head;
-    while (curr is not null)
+    if (head is null) { return null; }
+    var h2 = head.next;
+    (var curr1, var curr2) = (head, h2);
+    while (curr2 is not null && curr2.next is not null)
     {
-        curr = curr.next;
-        count += 1;
-    }
-    var div = count / k;
-    var rem = count % k;
-    var nums = new int[k];
-    Array.Fill(nums, div);
-    for (int i = 0; i < rem; i++)
-    {
-        nums[i] += 1;
-    }
-    var res = new ListNode[k];
-    Array.Fill(res, null);
-    curr = head;
-    for (int i = 0; i < k; i++)
-    {
-        (var a, var b) = Take(curr, nums[i]);
-        res[i] = a;
-        curr = b;
-    }
-    return res;
-}
+        curr1.next = curr2.next;
+        curr1 = curr1.next;
 
-(ListNode, ListNode) Take(ListNode head, int n)
-{
-    if (head is null) { return (null, null); }
-    var prev = head;
-    var curr = head;
-    for (int i = 0; i < n; i++)
-    {
-        prev = curr;
-        curr = curr.next;
+        curr2.next = curr1.next;
+        curr2 = curr2.next;
     }
-    prev.next = null;
-    return (head, curr);
+    curr1.next = h2;
+    return head;
 }
