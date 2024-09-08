a79441da1a9a9c77d40a2a03395b0c3382b3522b
pkusensei <ZW_08@outlook.com>
2024-09-07 20:39:28 -0400

    725 split linked list

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -4,49 +4,63 @@ using Tree;
 
 Test1();
 Test2();
-Test3();
-Test4();
 Console.WriteLine("!!Test Passed!!");
 
 void Test1()
 {
-    var a = ListNode.Make([4, 2, 8]);
-    var b = TreeNode.Make([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-    Debug.Assert(IsSubPath(a, b));
+    var a = ListNode.Make([1, 2, 3]);
+    var b = SplitListToParts(a, 5);
+    var c = "[[1],[2],[3],[],[]]";
+    Debug.Assert(b.Print() == c, $"Output: {b.Print()}\nExpect: {c}");
 }
 
 void Test2()
 {
-    var a = ListNode.Make([1, 4, 2, 6]);
-    var b = TreeNode.Make([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-    Debug.Assert(IsSubPath(a, b));
+    var a = ListNode.Make([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    var b = SplitListToParts(a, 3);
+    var c = "[[1,2,3,4],[5,6,7],[8,9,10]]";
+    Debug.Assert(b.Print() == c, $"Output: {b.Print()}\nExpect: {c}");
 }
 
-void Test3()
+ListNode[] SplitListToParts(ListNode head, int k)
 {
-    var a = ListNode.Make([1, 4, 2, 6, 8]);
-    var b = TreeNode.Make([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-    Debug.Assert(!IsSubPath(a, b));
+    var count = 0;
+    var curr = head;
+    while (curr is not null)
+    {
+        curr = curr.next;
+        count += 1;
+    }
+    var div = count / k;
+    var rem = count % k;
+    var nums = new int[k];
+    Array.Fill(nums, div);
+    for (int i = 0; i < rem; i++)
+    {
+        nums[i] += 1;
+    }
+    var res = new ListNode[k];
+    Array.Fill(res, null);
+    curr = head;
+    for (int i = 0; i < k; i++)
+    {
+        (var a, var b) = Take(curr, nums[i]);
+        res[i] = a;
+        curr = b;
+    }
+    return res;
 }
 
-void Test4()
+(ListNode, ListNode) Take(ListNode head, int n)
 {
-    var a = ListNode.Make([4, 1]);
-    var b = TreeNode.Make([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-    Debug.Assert(!IsSubPath(a, b));
-}
-
-
-bool IsSubPath(ListNode head, TreeNode root)
-{
-    if (root is null) { return false; }
-    return Dfs(head, root) || IsSubPath(head, root.left) || IsSubPath(head, root.right);
-}
-
-bool Dfs(ListNode head, TreeNode root)
-{
-    if (head is null) { return true; }
-    if (root is null) { return false; }
-    if (head.val != root.val) { return false; }
-    return Dfs(head.next, root.left) || Dfs(head.next, root.right);
+    if (head is null) { return (null, null); }
+    var prev = head;
+    var curr = head;
+    for (int i = 0; i < n; i++)
+    {
+        prev = curr;
+        curr = curr.next;
+    }
+    prev.next = null;
+    return (head, curr);
 }
