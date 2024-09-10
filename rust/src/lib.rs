ca180cb20169bd0bb75af85cb1c47d62f2712ecf
pkusensei <ZW_08@outlook.com>
2024-09-10 19:09:32 -0400

    355 well he's already ruined it

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,36 +1,71 @@
 mod helper;
 
+use std::collections::{HashMap, HashSet};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_envelopes(envelopes: &mut [[i32; 2]]) -> i32 {
-    envelopes.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then(b[1].cmp(&a[1])));
-    let mut values = vec![envelopes[0][1]];
-    // longest increasing sequence
-    for &[_, num] in envelopes.iter() {
-        if let Err(i) = values.binary_search(&num) {
-            if i == values.len() {
-                values.push(num)
-            } else {
-                values[i] = num;
-            }
-        }
-    }
-    values.len() as i32
+#[derive(Debug, Clone, Default)]
+struct Twitter {
+    tweets: Vec<Tweet>,
+    follows: HashMap<i32, HashSet<i32>>,
 }
 
-fn with_dp(envelopes: &mut [[i32; 2]]) -> i32 {
-    envelopes.sort_unstable();
-    let n = envelopes.len();
-    let mut dp = vec![1; n];
-    for i in 0..n {
-        for j in 0..i {
-            if envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1] {
-                dp[i] = dp[i].max(dp[j] + 1)
-            }
+impl Twitter {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn post_tweet(&mut self, user_id: i32, tweet_id: i32) {
+        let tweet = Tweet {
+            author: user_id,
+            tweet_id,
+        };
+        self.tweets.push(tweet);
+        self.follows
+            .entry(user_id)
+            .or_insert(HashSet::from([user_id])); // First tweet
+    }
+
+    fn get_news_feed(&mut self, user_id: i32) -> Vec<i32> {
+        if let Some(v) = self.follows.get(&user_id) {
+            self.tweets
+                .iter()
+                .rev()
+                .filter_map(|t| {
+                    if v.contains(&t.author) {
+                        Some(t.tweet_id)
+                    } else {
+                        None
+                    }
+                })
+                .take(10)
+                .collect()
+        } else {
+            self.follows.insert(user_id, HashSet::from([user_id]));
+            vec![]
         }
     }
-    dp.into_iter().max().unwrap()
+
+    fn follow(&mut self, follower_id: i32, followee_id: i32) {
+        self.follows
+            .entry(follower_id)
+            .or_insert(HashSet::from([follower_id]))
+            .insert(followee_id);
+    }
+
+    fn unfollow(&mut self, follower_id: i32, followee_id: i32) {
+        self.follows
+            .entry(follower_id)
+            .or_insert(HashSet::from([follower_id]))
+            .remove(&followee_id);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Tweet {
+    author: i32,
+    tweet_id: i32,
 }
 
 #[cfg(test)]
@@ -41,8 +76,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_envelopes(&mut [[5, 4], [6, 4], [6, 7], [2, 3]]), 3);
-        debug_assert_eq!(max_envelopes(&mut [[1, 1], [1, 1], [1, 1]]), 1);
+        let mut twitter = Twitter::new();
+        twitter.post_tweet(1, 5); // User 1 posts a new tweet (id = 5).
+        debug_assert_eq!(twitter.get_news_feed(1), [5]); // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+        twitter.follow(1, 2); // User 1 follows user 2.
+        twitter.post_tweet(2, 6); // User 2 posts a new tweet (id = 6).
+        debug_assert_eq!(twitter.get_news_feed(1), [6, 5]); // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+        twitter.unfollow(1, 2); // User 1 unfollows user 2.
+        debug_assert_eq!(twitter.get_news_feed(1), [5]); // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
     }
 
     #[test]
