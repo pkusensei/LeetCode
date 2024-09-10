454eb0e5a7c556b2b71cb55b64f0319aa0923947
Zx Wang <ZW_08@outlook.com>
2024-09-10 14:40:17 -0400

    352 update consecutive int intervals using BTreeSet

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,24 +1,40 @@
 mod helper;
 
-use std::{collections::HashMap, iter};
+use std::collections::BTreeSet;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn intersect(nums1: &[i32], nums2: &[i32]) -> Vec<i32> {
-    let [m1, m2] = [nums1, nums2].map(|nums| {
-        nums.iter().fold(HashMap::new(), |mut acc, &num| {
-            *acc.entry(num).or_insert(0) += 1;
-            acc
-        })
-    });
-    let mut res = vec![];
-    for (k, v1) in m1.into_iter() {
-        if let Some(&v2) = m2.get(&k) {
-            res.extend(iter::repeat(k).take(v1.min(v2)))
+#[derive(Debug, Clone)]
+struct SummaryRanges {
+    data: BTreeSet<[i32; 2]>,
+}
+
+impl SummaryRanges {
+    fn new() -> Self {
+        Self {
+            data: BTreeSet::new(),
         }
     }
-    res
+
+    fn add_num(&mut self, value: i32) {
+        let mut range = [value; 2];
+        let left = self.data.range(..range).last().copied().unwrap_or(range);
+        let right = self.data.range(range..).next().copied().unwrap_or(range);
+        if left[1] + 1 >= value {
+            range = [left[0], value.max(left[1])];
+            self.data.remove(&left);
+        }
+        if right[0] - 1 <= value && range[1] < right[1] {
+            range = [range[0].min(right[0]), right[1]];
+            self.data.remove(&right);
+        }
+        self.data.insert(range);
+    }
+
+    fn get_intervals(&self) -> Vec<Vec<i32>> {
+        self.data.iter().map(|d| d.to_vec()).collect()
+    }
 }
 
 #[cfg(test)]
@@ -29,12 +45,43 @@ mod tests {
 
     #[test]
     fn basics() {
-        sort_eq(intersect(&[1, 2, 2, 1], &[2, 2]), [2, 2]);
-        sort_eq(intersect(&[4, 9, 5], &[9, 4, 9, 8, 4]), [4, 9]);
+        let mut range = SummaryRanges::new();
+        range.add_num(1); // arr = [1]
+        debug_assert_eq!(range.get_intervals(), [[1, 1]]);
+        range.add_num(3); // arr = [1, 3]
+        debug_assert_eq!(range.get_intervals(), [[1, 1], [3, 3]]);
+        range.add_num(7); // arr = [1, 3, 7]
+        debug_assert_eq!(range.get_intervals(), [[1, 1], [3, 3], [7, 7]]);
+        range.add_num(2); // arr = [1, 2, 3, 7]
+        debug_assert_eq!(range.get_intervals(), [[1, 3], [7, 7]]);
+        range.add_num(6); // arr = [1, 2, 3, 6, 7]
+        debug_assert_eq!(range.get_intervals(), [[1, 3], [6, 7]]);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        let mut range = SummaryRanges::new();
+        range.add_num(6);
+        debug_assert_eq!(range.get_intervals(), [[6, 6]]);
+        range.add_num(6);
+        debug_assert_eq!(range.get_intervals(), [[6, 6]]);
+        range.add_num(0);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [6, 6]]);
+        range.add_num(4);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 6]]);
+        range.add_num(8);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 6], [8, 8]]);
+        range.add_num(7);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 8]]);
+        range.add_num(6);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 8]]);
+        range.add_num(4);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 8]]);
+        range.add_num(7);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 4], [6, 8]]);
+        range.add_num(5);
+        debug_assert_eq!(range.get_intervals(), [[0, 0], [4, 8]]);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(i1: I1, i2: I2)
