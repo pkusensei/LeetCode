0c98e415fed5b29a378a5e572a41a03356bf132b
Zx Wang <ZW_08@outlook.com>
2024-09-10 11:06:18 -0400

    345 reverse vowels

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,13 +3,25 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn reverse_string(s: &mut [char]) {
+pub fn reverse_vowels(s: &str) -> String {
+    const VOWELS: &[u8; 10] = b"AEIOUaeiou";
+    let mut s: Vec<_> = s.bytes().collect();
     let (mut left, mut right) = (0, s.len() - 1);
     while left < right {
-        s.swap(left, right);
-        left += 1;
-        right -= 1;
+        while left < s.len() && !VOWELS.contains(&s[left]) {
+            left += 1;
+        }
+        while 0 < right && !VOWELS.contains(&s[right]) {
+            right -= 1;
+        }
+        if left < s.len() && left < right {
+            // usize guarantees 0<=right
+            s.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
     }
+    s.into_iter().map(char::from).collect()
 }
 
 #[cfg(test)]
@@ -19,10 +31,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        debug_assert_eq!(reverse_vowels("hello"), "holle");
+        debug_assert_eq!(reverse_vowels("leetcode"), "leotcede");
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(reverse_vowels(".,"), ".,");
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(i1: I1, i2: I2)
