0c1902cd9ce9ea94ba0c7fa6420e7993dfa4b321
pkusensei <ZW_08@outlook.com>
2024-09-11 09:58:10 -0400

    363 max rectangle whose sum is <= k in matrix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,18 +3,66 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_numbers_with_unique_digits(n: i32) -> i32 {
-    // [1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851]
-    if n < 1 {
-        1
-    } else {
-        let mut res = 9;
-        let mut curr = 9;
-        for _ in 1..n {
-            res *= curr;
-            curr -= 1;
+pub fn max_sum_submatrix(matrix: &[&[i32]], k: i32) -> i32 {
+    let (rows, cols) = get_dimensions(matrix);
+    let prefix = NumMatrix::new(matrix);
+    let mut res = i32::MIN;
+    for row1 in 0..rows {
+        for col1 in 0..cols {
+            for row2 in row1..rows {
+                for col2 in col1..cols {
+                    let v = prefix.sum_region(row1, col1, row2, col2);
+                    if v == k {
+                        return k;
+                    }
+                    if v <= k {
+                        res = res.max(v)
+                    }
+                }
+            }
         }
-        res + count_numbers_with_unique_digits(n - 1)
+    }
+    res
+}
+
+#[derive(Debug, Clone)]
+struct NumMatrix {
+    prefix: Vec<Vec<i32>>,
+}
+
+// LC304
+impl NumMatrix {
+    // https://www.researchgate.net/profile/Michael-Jones-66/publication/3940582_Rapid_Object_Detection_using_a_Boosted_Cascade_of_Simple_Features/links/0f31753b419c639337000000/Rapid-Object-Detection-using-a-Boosted-Cascade-of-Simple-Features.pdf
+    // 2.1 Integral Image
+    fn new<T: AsRef<[i32]>>(matrix: &[T]) -> Self {
+        let (rows, cols) = get_dimensions(&matrix);
+        let mut row_sum = vec![vec![0; cols]; rows + 1];
+        let mut prefix = vec![vec![0; cols + 1]; rows + 1];
+        for (y, row) in matrix.iter().enumerate() {
+            for (x, &num) in row.as_ref().iter().enumerate() {
+                row_sum[y + 1][x] = row_sum[y][x] + num;
+                prefix[y + 1][x + 1] = prefix[y + 1][x] + row_sum[y + 1][x];
+            }
+        }
+        Self { prefix }
+    }
+
+    fn sum_region(&self, row1: usize, col1: usize, row2: usize, col2: usize) -> i32 {
+        let (row2, col2) = (row2 + 1, col2 + 1);
+        self.prefix[row2][col2] + self.prefix[row1][col1]
+            - (self.prefix[row1][col2] + self.prefix[row2][col1])
+        //  In prefix sum matrix
+        //                  col1
+        //                   v
+        //          0  0  0  0  0  ...
+        //          0  n  n  n  n  ...
+        // row1 ->  0  n  n [*] n  ...
+        //          0  n  n  n [c]  ...
+        //          0  n  n  n  n  ...
+        // Starred point [*] corresponds to
+        // (row1, col1) in original matrix i.e [c]
+        // Notice that prefix matrix has extra 0s
+        // (row2, col2) needs +1
     }
 }
 
@@ -26,9 +74,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(count_numbers_with_unique_digits(2), 91);
-        debug_assert_eq!(count_numbers_with_unique_digits(0), 1);
-        debug_assert_eq!(count_numbers_with_unique_digits(4), 5275);
+        // debug_assert_eq!(max_sum_submatrix(&[&[1, 0, 1], &[0, -2, 3]], 2), 2);
+        debug_assert_eq!(max_sum_submatrix(&[&[2, 2, -1]], 3), 3);
     }
 
     #[test]
