af2a8952b232cda56943d6e6a9e07612327b0faf
pkusensei <ZW_08@outlook.com>
2024-09-11 10:28:50 -0400

    365 jug water with bfs or gcd

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,69 +1,49 @@
 mod helper;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_sum_submatrix(matrix: &[&[i32]], k: i32) -> i32 {
-    let (rows, cols) = get_dimensions(matrix);
-    let prefix = NumMatrix::new(matrix);
-    let mut res = i32::MIN;
-    for row1 in 0..rows {
-        for col1 in 0..cols {
-            for row2 in row1..rows {
-                for col2 in col1..cols {
-                    let v = prefix.sum_region(row1, col1, row2, col2);
-                    if v == k {
-                        return k;
-                    }
-                    if v <= k {
-                        res = res.max(v)
-                    }
-                }
-            }
+pub fn can_measure_water(x: i32, y: i32, target: i32) -> bool {
+    // bfs(x, y, target, 0, 0)
+
+    // if x and y are nonzero integers and g = gcd(x,y),
+    // then there exist integers a and b such that ax+by=g
+    x + y == target || (x + y > target && target % gcd(x, y) == 0)
+}
+
+fn bfs(x: i32, y: i32, target: i32, jx: i32, jy: i32) -> bool {
+    let mut queue = VecDeque::from([(jx, jy)]);
+    let mut seen = HashSet::new();
+    while let Some((cx, cy)) = queue.pop_front() {
+        if !seen.insert((cx, cy)) {
+            continue;
         }
+        if cx.abs_diff(cy) == target as u32 || cx + cy == target {
+            return true;
+        }
+        queue.extend(next(x, y, cx, cy))
+    }
+    false
+}
+
+fn next(x: i32, y: i32, jx: i32, jy: i32) -> Vec<(i32, i32)> {
+    let mut res = vec![(x, jy), (jx, y), (0, jy), (jx, 0)];
+    let (dx, dy) = (x - jx, y - jy);
+    // x -> y
+    if jx > dy {
+        res.push((jx - dy, y));
+    } else {
+        res.push((0, jy + jx));
+    }
+    // y -> x
+    if jy > dx {
+        res.push((x, jy - dx));
+    } else {
+        res.push((jx + jy, 0));
     }
     res
-}
-
-#[derive(Debug, Clone)]
-struct NumMatrix {
-    prefix: Vec<Vec<i32>>,
-}
-
-// LC304
-impl NumMatrix {
-    // https://www.researchgate.net/profile/Michael-Jones-66/publication/3940582_Rapid_Object_Detection_using_a_Boosted_Cascade_of_Simple_Features/links/0f31753b419c639337000000/Rapid-Object-Detection-using-a-Boosted-Cascade-of-Simple-Features.pdf
-    // 2.1 Integral Image
-    fn new<T: AsRef<[i32]>>(matrix: &[T]) -> Self {
-        let (rows, cols) = get_dimensions(&matrix);
-        let mut row_sum = vec![vec![0; cols]; rows + 1];
-        let mut prefix = vec![vec![0; cols + 1]; rows + 1];
-        for (y, row) in matrix.iter().enumerate() {
-            for (x, &num) in row.as_ref().iter().enumerate() {
-                row_sum[y + 1][x] = row_sum[y][x] + num;
-                prefix[y + 1][x + 1] = prefix[y + 1][x] + row_sum[y + 1][x];
-            }
-        }
-        Self { prefix }
-    }
-
-    fn sum_region(&self, row1: usize, col1: usize, row2: usize, col2: usize) -> i32 {
-        let (row2, col2) = (row2 + 1, col2 + 1);
-        self.prefix[row2][col2] + self.prefix[row1][col1]
-            - (self.prefix[row1][col2] + self.prefix[row2][col1])
-        //  In prefix sum matrix
-        //                  col1
-        //                   v
-        //          0  0  0  0  0  ...
-        //          0  n  n  n  n  ...
-        // row1 ->  0  n  n [*] n  ...
-        //          0  n  n  n [c]  ...
-        //          0  n  n  n  n  ...
-        // Starred point [*] corresponds to
-        // (row1, col1) in original matrix i.e [c]
-        // Notice that prefix matrix has extra 0s
-        // (row2, col2) needs +1
-    }
 }
 
 #[cfg(test)]
@@ -74,8 +54,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        // debug_assert_eq!(max_sum_submatrix(&[&[1, 0, 1], &[0, -2, 3]], 2), 2);
-        debug_assert_eq!(max_sum_submatrix(&[&[2, 2, -1]], 3), 3);
+        debug_assert!(can_measure_water(3, 5, 4));
+        debug_assert!(!can_measure_water(2, 6, 5));
+        debug_assert!(can_measure_water(1, 2, 3));
     }
 
     #[test]
