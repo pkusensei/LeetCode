62aa81187790583125a42fae8d6fca731de41321
pkusensei <ZW_08@outlook.com>
2024-09-12 07:48:59 -0400

    375 minimum cost to cover all guesses

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,13 +3,27 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_consistent_strings(allowed: &str, words: &[&str]) -> i32 {
-    let a = as_bits(allowed);
-    words.iter().filter(|s| (as_bits(s) | a) == a).count() as _
+pub fn get_money_amount(n: i32) -> i32 {
+    let n = n as usize;
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    solve(&mut dp, 1, n)
 }
 
-fn as_bits(s: &str) -> i32 {
-    s.bytes().fold(0, |acc, b| acc | (1 << (b - b'a')))
+fn solve(dp: &mut [Vec<i32>], start: usize, end: usize) -> i32 {
+    if start >= end {
+        return 0;
+    }
+    if dp[start][end] != 0 {
+        return dp[start][end];
+    }
+    let mut res = i32::MAX;
+    for i in start..=end {
+        // for each choice i, find worst outcome => i + max(..i-1, i+1..)
+        // but with strategy, find best of worst outcomes => min(all i)
+        res = res.min(i as i32 + solve(dp, start, i - 1).max(solve(dp, i + 1, end)));
+    }
+    dp[start][end] = res;
+    res
 }
 
 #[cfg(test)]
@@ -20,18 +34,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            count_consistent_strings("ab", &["ad", "bd", "aaab", "baa", "badab"]),
-            2
-        );
-        debug_assert_eq!(
-            count_consistent_strings("abc", &["a", "b", "c", "ab", "ac", "bc", "abc"]),
-            7
-        );
-        debug_assert_eq!(
-            count_consistent_strings("cad", &["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"]),
-            4
-        );
+        debug_assert_eq!(get_money_amount(10), 16);
+        debug_assert_eq!(get_money_amount(1), 0);
+        debug_assert_eq!(get_money_amount(2), 1);
     }
 
     #[test]
