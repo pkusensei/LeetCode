27a568f42ddfa48f8d6a7210b1edf66be4f79eac
pkusensei <ZW_08@outlook.com>
2024-09-11 20:13:46 -0400

    1684 count strings whose chars fall in specific set

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,67 +1,15 @@
 mod helper;
 
-use std::collections::{BinaryHeap, HashSet};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn k_smallest_pairs(nums1: &[i32], nums2: &[i32], k: i32) -> Vec<[i32; 2]> {
-    let k = k as usize;
-    let mut res = Vec::with_capacity(k);
-    let mut heap = BinaryHeap::from([Pair {
-        i1: 0,
-        i2: 0,
-        n1: nums1[0],
-        n2: nums2[0],
-    }]);
-    let mut seen = HashSet::new();
-    while res.len() < k && !heap.is_empty() {
-        let p @ Pair { i1, i2, n1, n2 } = heap.pop().unwrap();
-        if !seen.insert(p) {
-            continue;
-        }
-        res.push([n1, n2]);
-        if i1 + 1 < nums1.len() {
-            heap.push(Pair {
-                i1: i1 + 1,
-                i2,
-                n1: nums1[i1 + 1],
-                n2,
-            });
-        }
-        if i2 + 1 < nums2.len() {
-            heap.push(Pair {
-                i1,
-                i2: i2 + 1,
-                n1,
-                n2: nums2[i2 + 1],
-            });
-        }
-    }
-    res
+pub fn count_consistent_strings(allowed: &str, words: &[&str]) -> i32 {
+    let a = as_bits(allowed);
+    words.iter().filter(|s| (as_bits(s) | a) == a).count() as _
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Pair {
-    i1: usize,
-    i2: usize,
-    n1: i32,
-    n2: i32,
-}
-
-impl PartialOrd for Pair {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Pair {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (other.n1 + other.n2)
-            .cmp(&(self.n1 + self.n2))
-            .then(other.n1.cmp(&self.n1))
-            .then(other.n2.cmp(&self.n2))
-    }
+fn as_bits(s: &str) -> i32 {
+    s.bytes().fold(0, |acc, b| acc | (1 << (b - b'a')))
 }
 
 #[cfg(test)]
@@ -73,23 +21,21 @@ mod tests {
     #[test]
     fn basics() {
         debug_assert_eq!(
-            k_smallest_pairs(&[1, 7, 11], &[2, 4, 6], 3),
-            [[1, 2], [1, 4], [1, 6]]
+            count_consistent_strings("ab", &["ad", "bd", "aaab", "baa", "badab"]),
+            2
         );
         debug_assert_eq!(
-            k_smallest_pairs(&[1, 1, 2], &[1, 2, 3], 2),
-            [[1, 1], [1, 1]]
+            count_consistent_strings("abc", &["a", "b", "c", "ab", "ac", "bc", "abc"]),
+            7
+        );
+        debug_assert_eq!(
+            count_consistent_strings("cad", &["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"]),
+            4
         );
     }
 
     #[test]
-    #[rustfmt::skip]
-    fn test() {
-        debug_assert_eq!(
-        k_smallest_pairs(&[1, 2, 4, 5, 6], &[3, 5, 7, 9], 20),
-        [[1,3],[2,3],[1,5],[2,5],[4,3],[1,7],[5,3],[2,7],[4,5],[6,3],[1,9],[5,5],[2,9],[4,7],[6,5],[5,7],[4,9],[6,7],[5,9],[6,9]]
-        )
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(i1: I1, i2: I2)
