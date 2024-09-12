65087352619a2a3331e7dca954516fd42d77a45d
Zx Wang <ZW_08@outlook.com>
2024-09-12 13:17:07 -0400

    378 find kth smallest int in sorted matrix with binary heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,19 +1,36 @@
 mod helper;
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet},
+};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn combination_sum4(nums: &[i32], target: i32) -> i32 {
-    let mut dp = vec![0; 1 + target as usize];
-    dp[0] = 1;
-    for n in 1..=target {
-        for &num in nums.iter() {
-            if num <= n {
-                dp[n as usize] += dp[(n - num) as usize]
-            }
+pub fn kth_smallest(matrix: &[&[i32]], mut k: i32) -> i32 {
+    let n = matrix.len();
+    if n == 1 {
+        return matrix[0][0];
+    }
+    let mut heap = BinaryHeap::from([Reverse((matrix[0][0], 0, 0))]);
+    let mut seen = HashSet::new();
+    let mut res = 0;
+    while k > 0 && !heap.is_empty() {
+        let Reverse(curr @ (num, row, col)) = heap.pop().unwrap();
+        if !seen.insert(curr) {
+            continue;
+        }
+        k -= 1;
+        res = num;
+        if let Some(r) = matrix.get(row + 1) {
+            heap.push(Reverse((r[col], row + 1, col)));
+        }
+        if let Some(&n) = matrix[row].get(col + 1) {
+            heap.push(Reverse((n, row, col + 1)));
         }
     }
-    *dp.last().unwrap()
+    res
 }
 
 #[cfg(test)]
@@ -24,13 +41,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(combination_sum4(&[1, 2, 3], 4), 7);
-        debug_assert_eq!(combination_sum4(&[9], 3), 0);
+        debug_assert_eq!(
+            kth_smallest(&[&[1, 5, 9], &[10, 11, 13], &[12, 13, 15]], 8),
+            13
+        );
+        debug_assert_eq!(kth_smallest(&[&[-5]], 1), -5);
     }
 
     #[test]
     fn test() {
-        debug_assert_eq!(combination_sum4(&[2, 1, 3], 35), 1132436852);
+        debug_assert_eq!(kth_smallest(&[&[1, 2], &[1, 3]], 2), 1)
     }
 
     #[allow(dead_code)]
