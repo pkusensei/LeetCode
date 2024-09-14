9791571fa9dc34affca6c56952569b855e6925e7
pkusensei <ZW_08@outlook.com>
2024-09-13 20:45:50 -0400

    392 str is subsequence of another str

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,33 +3,18 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_subarray(nums: &[i32]) -> i32 {
-    let (mut curr, mut curr_c) = (0, 0);
-    let (mut prev, mut prev_c) = (0, 0);
-    for &num in nums.iter() {
-        match curr.cmp(&num) {
-            std::cmp::Ordering::Less => {
-                curr = num;
-                curr_c = 1
-            }
-            std::cmp::Ordering::Equal => curr_c += 1,
-            std::cmp::Ordering::Greater => {
-                if curr > prev {
-                    prev = curr;
-                    prev_c = curr_c;
-                } else if curr == prev {
-                    prev_c = prev_c.max(curr_c);
-                }
-                curr = 0;
-                curr_c = 0;
-            }
+pub fn is_subsequence(s: &str, t: &str) -> bool {
+    let (s, t) = (s.as_bytes(), t.as_bytes());
+    let (mut si, mut ti) = (0, 0);
+    while si < s.len() && ti < t.len() {
+        if s[si] == t[ti] {
+            si += 1;
+            ti += 1
+        } else {
+            ti += 1
         }
     }
-    match curr.cmp(&prev) {
-        std::cmp::Ordering::Less => prev_c,
-        std::cmp::Ordering::Equal => curr_c.max(prev_c),
-        std::cmp::Ordering::Greater => curr_c,
-    }
+    si == s.len()
 }
 
 #[cfg(test)]
@@ -40,19 +25,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(longest_subarray(&[1, 2, 3, 3, 2, 2]), 2);
-        debug_assert_eq!(longest_subarray(&[1, 2, 3, 4]), 1);
+        debug_assert!(is_subsequence("abc", "ahbgdc"));
+        debug_assert!(!is_subsequence("axc", "ahbgdc"));
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(
-            longest_subarray(&[
-                311155, 311155, 311155, 311155, 311155, 311155, 311155, 311155, 201191, 311155
-            ]),
-            8
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(i1: I1, i2: I2)
