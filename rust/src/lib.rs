93b7def5d9d604af7d6d4dc0a4f440a6f4a76b0e
pkusensei <ZW_08@outlook.com>
2024-09-15 16:15:54 -0400

    406 reconstruct a queue with height and blocking count

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,19 +3,25 @@ mod helper;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn to_hex(num: i32) -> String {
-    if num == 0 {
-        return "0".to_string();
+pub fn reconstruct_queue(mut people: Vec<[i32; 2]>) -> Vec<[i32; 2]> {
+    // sort order: h increasing
+    // with the same h, pop the one with smaller k first
+    // so that [h, 0] gets inserted before [h, n]
+    people.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then(b[1].cmp(&a[1])));
+    let n = people.len();
+    let mut res = Vec::with_capacity(n);
+    while let Some([h, k]) = people.pop() {
+        if res.is_empty() || k == 0 {
+            res.insert(0, [h, k]);
+            continue;
+        }
+        if (k as usize) >= res.len() {
+            res.push([h, k]);
+        } else {
+            res.insert(k as usize, [h, k]);
+        }
     }
-    const B: &[u8] = b"0123456789abcdef";
-    let mut num = num as u32;
-    let mut res = vec![];
-    while num > 0 {
-        let i = (num % 16) as usize;
-        res.push(B[i]);
-        num /= 16;
-    }
-    res.into_iter().rev().map(char::from).collect()
+    res
 }
 
 #[cfg(test)]
@@ -26,14 +32,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(to_hex(26), "1a");
-        debug_assert_eq!(to_hex(-1), "ffffffff");
+        debug_assert_eq!(
+            reconstruct_queue(vec![[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]),
+            [[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]]
+        );
+        debug_assert_eq!(
+            reconstruct_queue(vec![[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]]),
+            [[4, 0], [5, 0], [2, 2], [3, 2], [1, 4], [6, 0]]
+        );
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(to_hex(13), "d");
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
