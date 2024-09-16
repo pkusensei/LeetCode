25123b10d73a9be07620a4b533bda029388b717f
pkusensei <ZW_08@outlook.com>
2024-09-16 13:38:29 -0400

    419 number of islands

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,40 +5,35 @@ use std::collections::{HashSet, VecDeque};
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn pacific_atlantic(heights: &[&[i32]]) -> Vec<[i32; 2]> {
-    let (rows, cols) = get_dimensions(heights);
-    let (mut p_set, mut a_set) = (HashSet::new(), HashSet::new());
-    for x in 0..cols {
-        bfs(heights, (x, 0), &mut p_set);
-        bfs(heights, (x, rows - 1), &mut a_set);
+pub fn count_battleships(board: &[&[char]]) -> i32 {
+    let mut seen = HashSet::new();
+    let mut res = 0;
+    for (y, r) in board.iter().enumerate() {
+        for (x, &ch) in r.iter().enumerate() {
+            if ch == 'X' && bfs(board, (x, y), &mut seen) {
+                res += 1
+            }
+        }
     }
-    for y in 0..rows {
-        bfs(heights, (0, y), &mut p_set);
-        bfs(heights, (cols - 1, y), &mut a_set);
-    }
-    p_set
-        .intersection(&a_set)
-        .map(|c| [c.1 as i32, c.0 as i32])
-        .collect()
+    res
 }
 
-fn bfs(heights: &[&[i32]], curr: Coord, seen: &mut HashSet<Coord>) {
+fn bfs(board: &[&[char]], curr: Coord, seen: &mut HashSet<Coord>) -> bool {
     if seen.contains(&curr) {
-        return;
+        return false;
     }
     let mut queue = VecDeque::from([curr]);
     while let Some(c) = queue.pop_front() {
         if !seen.insert(c) {
             continue;
         }
-        for (nx, ny) in
-            neighbors(c).filter(|&(x, y)| heights.get(y).is_some_and(|r| r.get(x).is_some()))
-        {
-            if heights[ny][nx] >= heights[c.1][c.0] {
-                queue.push_back((nx, ny));
-            }
-        }
+        queue.extend(neighbors(c).filter(|&(x, y)| {
+            board
+                .get(y)
+                .is_some_and(|r| r.get(x).is_some_and(|&ch| ch == 'X'))
+        }));
     }
+    true
 }
 
 #[cfg(test)]
@@ -49,17 +44,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        sort_eq(
-            pacific_atlantic(&[
-                &[1, 2, 2, 3, 5],
-                &[3, 2, 3, 4, 4],
-                &[2, 4, 5, 3, 1],
-                &[6, 7, 1, 4, 5],
-                &[5, 1, 1, 2, 4],
+        debug_assert_eq!(
+            count_battleships(&[
+                &['X', '.', '.', 'X'],
+                &['.', '.', '.', 'X'],
+                &['.', '.', '.', 'X']
             ]),
-            [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]],
+            2
         );
-        debug_assert_eq!(pacific_atlantic(&[&[1]]), [[0, 0]]);
+        debug_assert_eq!(count_battleships(&[&['.']]), 0);
     }
 
     #[test]
