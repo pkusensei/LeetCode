e278a2e62e43cb75024172e56f1df48c0d1fe1fe
Zx Wang <ZW_08@outlook.com>
2024-09-17 13:53:40 -0400

    424 longest char-repeating str after replacement

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,43 +1,33 @@
 mod helper;
 
-use std::{collections::HashMap, iter};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn original_digits(s: &str) -> String {
-    let mut letters = s.bytes().fold(HashMap::new(), |mut acc, b| {
-        *acc.entry(b).or_insert(0) += 1;
-        acc
-    });
-    let mut res = vec![];
-    res.extend(remove_word(&mut letters, b'z', b"zero", b'0'));
-    res.extend(remove_word(&mut letters, b'w', b"two", b'2'));
-    res.extend(remove_word(&mut letters, b'u', b"four", b'4'));
-    res.extend(remove_word(&mut letters, b'x', b"six", b'6'));
-    res.extend(remove_word(&mut letters, b'g', b"eight", b'8'));
-    res.extend(remove_word(&mut letters, b'o', b"one", b'1'));
-    res.extend(remove_word(&mut letters, b'h', b"three", b'3'));
-    res.extend(remove_word(&mut letters, b'f', b"five", b'5'));
-    res.extend(remove_word(&mut letters, b'v', b"seven", b'7'));
-    res.extend(remove_word(&mut letters, b'i', b"nine", b'9'));
-
-    res.sort_unstable();
-    res.into_iter().map(char::from).collect()
-}
-
-fn remove_word(letters: &mut HashMap<u8, i32>, unique: u8, seq: &[u8], digit: u8) -> Vec<u8> {
-    let mut res = vec![];
-    if let Some(&count) = letters.get(&unique) {
-        res.extend(iter::repeat(digit).take(count as usize));
-        letters.remove(&b'z');
-        for &b in seq.iter() {
-            if let Some(v) = letters.get_mut(&b) {
-                *v -= count;
-            }
+pub fn character_replacement(s: &str, k: i32) -> i32 {
+    let (s, n) = (s.as_bytes(), s.len());
+    let (mut left, mut right) = (0, 0);
+    let mut letters = [0; 26];
+    let mut max_count = 1;
+    while right < n && right - left < (max_count + k) as usize {
+        let i = usize::from(s[right] - b'A');
+        letters[i] += 1;
+        max_count = max_count.max(letters[i]);
+        right += 1
+    }
+    while right < n {
+        let i = usize::from(s[right] - b'A');
+        right += 1;
+        letters[i] += 1;
+        let temp = letters[i];
+        if temp > max_count {
+            max_count = temp;
+            // expand window size
+            continue;
         }
-    };
-    res
+        letters[usize::from(s[left] - b'A')] -= 1;
+        left += 1
+    }
+    letters.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -48,12 +38,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(original_digits("owoztneoer"), "012");
-        debug_assert_eq!(original_digits("fviefuro"), "45");
+        debug_assert_eq!(character_replacement("ABAB", 2), 4);
+        debug_assert_eq!(character_replacement("AABABBA", 1), 4);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(character_replacement("AAAA", 0), 4);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
