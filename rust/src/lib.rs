f0f795c02f28bd81ead0578a78b88c146488f80a
Zx Wang <ZW_08@outlook.com>
2024-09-19 11:50:52 -0400

    440 kth number in lexico order; wrote a trie but didn't use it :)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,37 @@
 mod helper;
+mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
 
 pub fn find_kth_number(n: i32, k: i32) -> i32 {
-    let (mut res, mut count) = (0, 0);
-    for i in 1..=9 {
-        dfs(i, n as _, &mut res, &mut count, k);
+    let (mut res, mut k) = (1, i64::from(k - 1));
+    let n = i64::from(n);
+    while k > 0 {
+        let steps = calc_steps(n, res, res + 1);
+        if steps <= k {
+            // skip the whole tree rooted by res
+            // and jump to (res+1)
+            k -= steps;
+            res += 1;
+        } else {
+            k -= 1;
+            res *= 10;
+        }
     }
-    res
+    res as _
+}
+
+fn calc_steps(n: i64, mut n1: i64, mut n2: i64) -> i64 {
+    let mut steps = 0;
+    while n1 <= n {
+        // n2>n  ==> n2 is out side of path n1->n
+        // n2<=n ==> we want to reach n2 first
+        steps += (n + 1).min(n2) - n1;
+        n1 *= 10;
+        n2 *= 10;
+    }
+    steps
 }
 
 fn dfs(curr: i64, n: i64, res: &mut i32, count: &mut i32, k: i32) {
@@ -39,7 +62,9 @@ mod tests {
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(find_kth_number(13, 9), 5);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
---
+++ rust/src/trie.rs
@@ -0,0 +1,101 @@
+#[derive(Debug, Clone)]
+pub struct Trie<const N: usize> {
+    data: [Option<Box<Trie<N>>>; N],
+    is_end: bool,
+}
+
+impl<const N: usize> Default for Trie<N> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[allow(dead_code)]
+impl<const N: usize> Trie<N> {
+    pub const fn new() -> Self {
+        Self {
+            data: [const { None }; N],
+            is_end: false,
+        }
+    }
+
+    pub fn insert<T, I, F>(&mut self, input: I, index_of: F)
+    where
+        I: IntoIterator<Item = T>,
+        F: FnMut(T) -> usize,
+    {
+        self.insert_impl(input.into_iter(), index_of)
+    }
+
+    fn insert_impl<T, I, F>(&mut self, mut it: I, mut index_of: F)
+    where
+        I: Iterator<Item = T>,
+        F: FnMut(T) -> usize,
+    {
+        if let Some(v) = it.next() {
+            let idx = index_of(v);
+            if let Some(n) = self.data.get_mut(idx).and_then(|opt| opt.as_mut()) {
+                n.insert(it, index_of);
+            } else {
+                let mut node = Box::new(Self::new());
+                node.insert(it, index_of);
+                self.data[idx] = Some(node);
+            }
+        } else {
+            self.is_end = true;
+        }
+    }
+
+    pub fn check<T, I, F>(&self, input: I, index_of: F) -> bool
+    where
+        I: IntoIterator<Item = T>,
+        F: FnMut(T) -> usize,
+    {
+        self.check_impl(input.into_iter(), index_of)
+    }
+
+    fn check_impl<T, I, F>(&self, mut it: I, mut index_of: F) -> bool
+    where
+        I: Iterator<Item = T>,
+        F: FnMut(T) -> usize,
+    {
+        if let Some(v) = it.next() {
+            let idx = index_of(v);
+            if let Some(node) = self.data.get(idx).and_then(|n| n.as_ref()) {
+                return node.check(it, index_of);
+            } else {
+                return false;
+            }
+        }
+        self.is_end
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let mut trie = Trie::<26>::new();
+        let func = |&b| usize::from(b - b'a');
+        trie.insert(b"abcde", func);
+        debug_assert!(trie.check(b"abcde", func));
+        debug_assert!(!trie.check(b"abc", func));
+        trie.insert(b"abc", func);
+        debug_assert!(trie.check(b"abc", func));
+        debug_assert!(!trie.check(b"abcd", func));
+        debug_assert!(!trie.check(b"abcdef", func));
+        debug_assert!(trie.check(b"abcde", func));
+
+        debug_assert!(!trie.check(b"abdef", func));
+        trie.insert(b"abdef", func);
+        debug_assert!(trie.check(b"abdef", func));
+        debug_assert!(trie.check(b"abcde", func));
+
+        debug_assert!(!trie.check(b"x", func));
+        trie.insert(b"xyz", func);
+        debug_assert!(trie.check(b"xyz", func));
+        debug_assert!(!trie.check(b"x", func));
+    }
+}
