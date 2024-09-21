72d4793fe0225044248eec8a48f0076d449f0d68
pkusensei <ZW_08@outlook.com>
2024-09-21 11:02:21 -0400

    458 poor pigs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,41 +4,13 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn circular_array_loop(nums: &mut [i32]) -> bool {
-    let n = nums.len();
-    if n < 2 {
-        return false;
+pub fn poor_pigs(buckets: i32, minutes_to_die: i32, minutes_to_test: i32) -> i32 {
+    let attempt = minutes_to_test / minutes_to_die;
+    // (1 + attempt).pow(x) >= buckets
+    let mut res = 0;
+    while (1 + attempt).pow(res) < buckets {
+        res += 1;
     }
-    for i in 0..nums.len() {
-        if nums[i] == 0 {
-            continue;
-        }
-        let (mut slow, mut fast) = (i, next(nums, i));
-        while nums[i] * nums[fast] > 0 && nums[i] * nums[next(nums, fast)] > 0 {
-            if slow == fast {
-                if slow == next(nums, slow) {
-                    break;
-                }
-                return true;
-            }
-            slow = next(nums, slow);
-            fast = next(nums, next(nums, fast));
-        }
-
-        let dir = i32::from(nums[i] > 0); // direction
-        let mut curr = i;
-        while dir * nums[curr] > 0 {
-            let temp = next(nums, curr);
-            nums[curr] = 0;
-            curr = temp
-        }
-    }
-    false
-}
-
-const fn next(nums: &[i32], start: usize) -> usize {
-    let n = nums.len() as i32;
-    let res = ((start as i32 + nums[start] % n) + n) % n;
     res as _
 }
 
@@ -50,9 +22,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(circular_array_loop(&mut [2, -1, 1, 2, 2]));
-        debug_assert!(!circular_array_loop(&mut [-1, -2, -3, -4, -5, 6]));
-        debug_assert!(circular_array_loop(&mut [1, -1, 5, 1, 4]));
+        debug_assert_eq!(poor_pigs(4, 15, 15), 2);
+        debug_assert_eq!(poor_pigs(4, 15, 30), 2);
     }
 
     #[test]
