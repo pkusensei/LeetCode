8dd78c73a61e3e5cee66b506d29866266b17c8d1
pkusensei <ZW_08@outlook.com>
2024-09-23 11:33:01 -0400

    475 binary search to find radius so that two arrays overlap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,57 +1,25 @@
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_max_form(strs: &[&str], m: i32, n: i32) -> i32 {
-    // let strs: Vec<_> = strs.iter().map(|s| count_zero_one(s)).collect();
-    // dfs(&strs, m, n, &mut HashMap::new())
-    let (m, n) = (m as usize, n as usize);
-    let mut dp = vec![vec![0; 1 + m]; 1 + n];
-    for [zero, one] in strs.iter().map(|s| count_zero_one(s).map(|n| n as usize)) {
-        for ones in (one..=n).rev() {
-            for zeros in (zero..=m).rev() {
-                dp[ones][zeros] = dp[ones][zeros].max(1 + dp[ones - one][zeros - zero]);
+pub fn find_radius(houses: &[i32], heaters: &mut [i32]) -> i32 {
+    heaters.sort_unstable();
+    let n = heaters.len();
+    let mut res = 0;
+    for h in houses.iter() {
+        if let Err(i) = heaters.binary_search(h) {
+            if i == n {
+                res = res.max(h - heaters[n - 1]);
+            } else if i == 0 {
+                res = res.max(heaters[0] - h);
+            } else {
+                res = res.max((h - heaters[i - 1]).min(heaters[i] - h));
             }
         }
     }
-    dp[n][m]
-}
-
-fn dfs(
-    strs: &[[i32; 2]],
-    zeros: i32,
-    ones: i32,
-    seen: &mut HashMap<(usize, i32, i32), i32>,
-) -> i32 {
-    if let Some(&v) = seen.get(&(strs.len(), zeros, ones)) {
-        return v;
-    }
-    match strs {
-        [] => 0,
-        [head, tail @ ..] => {
-            let mut res = dfs(tail, zeros, ones, seen);
-            if zeros - head[0] >= 0 && ones - head[1] >= 0 {
-                res = res.max(1 + dfs(tail, zeros - head[0], ones - head[1], seen))
-            }
-            seen.insert((strs.len(), zeros, ones), res);
-            res
-        }
-    }
-}
-
-fn count_zero_one(s: &str) -> [i32; 2] {
-    s.bytes().fold([0, 0], |mut acc, b| {
-        if b == b'0' {
-            acc[0] += 1
-        } else {
-            acc[1] += 1
-        }
-        acc
-    })
+    res
 }
 
 #[cfg(test)]
@@ -62,8 +30,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(find_max_form(&["10", "0001", "111001", "1", "0"], 5, 3), 4);
-        debug_assert_eq!(find_max_form(&["10", "0", "1"], 1, 1), 2);
+        debug_assert_eq!(find_radius(&[1, 2, 3], &mut [2]), 1);
+        debug_assert_eq!(find_radius(&[1, 2, 3, 4], &mut [1, 4]), 1);
+        debug_assert_eq!(find_radius(&[1, 5], &mut [2]), 3);
     }
 
     #[test]
