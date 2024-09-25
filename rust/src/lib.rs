4d14adc2a1c608e20554065ee8dd7de2dcf34da5
pkusensei <ZW_08@outlook.com>
2024-09-25 15:25:26 -0400

    493 merge sort to find reverse pairs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,14 +4,43 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn construct_rectangle(area: i32) -> [i32; 2] {
-    let upper = (area as f64).sqrt().trunc() as i32;
-    for w in (1..=upper).rev() {
-        if area % w == 0 {
-            return [area / w, w];
+pub fn reverse_pairs(nums: &mut [i32]) -> i32 {
+    merge_sort(nums)
+}
+
+fn merge_sort(nums: &mut [i32]) -> i32 {
+    let n = nums.len();
+    let mut res = 0;
+    if n < 2 {
+        return res;
+    }
+
+    let mid = n / 2;
+    res += merge_sort(&mut nums[..mid]);
+    res += merge_sort(&mut nums[mid..]);
+
+    let mut right = mid;
+    for left in 0..mid {
+        while right < n && i64::from(nums[left]) > 2 * i64::from(nums[right]) {
+            right += 1;
+        }
+        res += (right - mid) as i32;
+    }
+    let (mut left, mut right) = (0, mid);
+    let mut sorted = Vec::with_capacity(n);
+    while left < mid && right < n {
+        if nums[left] <= nums[right] {
+            sorted.push(nums[left]);
+            left += 1
+        } else {
+            sorted.push(nums[right]);
+            right += 1
         }
     }
-    [area, 1]
+    sorted.extend_from_slice(&nums[left..mid]);
+    sorted.extend_from_slice(&nums[right..]);
+    nums.copy_from_slice(&sorted);
+    res
 }
 
 #[cfg(test)]
@@ -22,9 +51,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(construct_rectangle(4), [2, 2]);
-        debug_assert_eq!(construct_rectangle(37), [37, 1]);
-        debug_assert_eq!(construct_rectangle(122122), [427, 286]);
+        debug_assert_eq!(reverse_pairs(&mut [1, 3, 2, 3, 1]), 2);
+        debug_assert_eq!(reverse_pairs(&mut [2, 4, 3, 5, 1]), 3);
     }
 
     #[test]
