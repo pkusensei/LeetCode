124ff4b334557e269268edba07475d49596ef383
pkusensei <ZW_08@outlook.com>
2024-09-25 12:58:08 -0400

    491 backtracking non-decreasing subseq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,64 +1,43 @@
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_min_step(board: &str, hand: &str) -> i32 {
-    let board: Vec<_> = board.bytes().collect();
-    let mut hand: Vec<_> = hand.bytes().collect();
-    hand.sort_unstable();
-    let mut queue = VecDeque::from([(board, hand, 0)]);
-    let mut seen = HashSet::new();
-    while let Some((board, hand, dist)) = queue.pop_front() {
-        if board.is_empty() {
-            return dist;
-        }
-        if !seen.insert((board.clone(), hand.clone())) {
-            continue;
-        }
-        for i1 in 0..board.len() {
-            for i2 in 0..hand.len() {
-                if i2 > 0 && hand[i2 - 1] == hand[i2] {
-                    continue;
-                }
-                if i1 > 0 && board[i1 - 1] == hand[i2] {
-                    continue;
-                }
-                let pick = board[i1] == hand[i2] || (i1 > 0 && board[i1 - 1] == board[i1]); // && board[i1] != hand[i2]
-                if pick {
-                    let mut new_board = board.clone();
-                    new_board.insert(i1, hand[i2]);
-                    let mut new_hand = hand.clone();
-                    new_hand.remove(i2);
-                    new_board = process(new_board, i1);
-                    queue.push_back((new_board, new_hand, dist + 1));
-                }
-            }
-        }
-    }
-    -1
+pub fn find_subsequences(nums: &[i32]) -> Vec<Vec<i32>> {
+    let mut res = vec![];
+    let mut curr = vec![];
+    dfs(nums, &mut curr, &mut res);
+    res
 }
 
-fn process(mut board: Vec<u8>, idx: usize) -> Vec<u8> {
-    let n = board.len();
-    if idx >= n {
-        return board;
-    }
-    let (mut left, mut right) = (idx, idx);
-    while left > 0 && board[left - 1] == board[idx] {
-        left -= 1;
-    }
-    while right + 1 < n && board[right + 1] == board[idx] {
-        right += 1;
-    }
-    if right - left + 1 >= 3 {
-        board.drain(left..=right);
-        process(board, left)
-    } else {
-        board
+fn dfs(nums: &[i32], curr: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+    match nums {
+        [] => {
+            if curr.len() > 1 {
+                res.push(curr.clone());
+            }
+        }
+        [head, tail @ ..] => match curr.last().copied() {
+            None => {
+                curr.push(*head);
+                dfs(tail, curr, res);
+                curr.pop();
+                dfs(tail, curr, res);
+            }
+            Some(v) if v <= *head => {
+                curr.push(*head);
+                dfs(tail, curr, res);
+                curr.pop();
+                if v < *head {
+                    // say curr is [1,2], head is [2]
+                    // above has already checked [1,2,2]
+                    // skip the non-pick case
+                    dfs(tail, curr, res);
+                }
+            }
+            _ => dfs(tail, curr, res),
+        },
     }
 }
 
@@ -70,15 +49,24 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(find_min_step("WRRBBW", "RB"), -1);
-        debug_assert_eq!(find_min_step("WWRRBBWW", "WRBRW"), 2);
-        debug_assert_eq!(find_min_step("G", "GGGGG"), 2);
+        sort_eq(
+            find_subsequences(&[4, 6, 7, 7]),
+            [
+                vec![4, 6],
+                vec![4, 6, 7],
+                vec![4, 6, 7, 7],
+                vec![4, 7],
+                vec![4, 7, 7],
+                vec![6, 7],
+                vec![6, 7, 7],
+                vec![7, 7],
+            ],
+        );
+        debug_assert_eq!(find_subsequences(&[4, 4, 3, 2, 1]), [[4, 4]]);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(find_min_step("RRWWRRBBRR", "WB"), 2);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
