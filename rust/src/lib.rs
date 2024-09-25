3b3a601c8bdfc4f5027fc7ee50a510755ddd7db9
pkusensei <ZW_08@outlook.com>
2024-09-25 09:55:42 -0400

    2416 sum of prefix scores of strs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,29 +4,100 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn predict_the_winner(nums: &[i32]) -> bool {
-    dfs(nums, 0, 0, true)
+pub fn sum_prefix_scores(words: &[&str]) -> Vec<i32> {
+    let mut indices: Vec<_> = (0..words.len()).collect();
+    indices.sort_unstable_by(|&a, &b| words[a].cmp(words[b]));
+    let lengths = common_prefix_lengths(words, &indices);
+    calc_scores(words, &indices, &lengths)
+    // with_trie(words)
 }
 
-fn dfs(nums: &[i32], p1: i32, p2: i32, turn1: bool) -> bool {
-    match nums {
-        [] => p1 >= p2,
-        &[num] => {
-            if turn1 {
-                p1 + num >= p2
-            } else {
-                p1 >= p2 + num
+// map words to indices
+// sort indices by the words they point to
+// get common prefix between "adjacent" words
+// for (i, j) in indices => ([i], [j]) in words
+// the further removed, the less common [i] and [j] are
+// accumulate common([i], [j]) before it becomes 0 eventually
+fn common_prefix_lengths(words: &[&str], indices: &[usize]) -> Vec<i32> {
+    let n = words.len();
+    let mut res = Vec::with_capacity(n);
+    res.push(0);
+    for w in indices.windows(2) {
+        let (a, b) = (words[w[0]], words[w[1]]);
+        res.push(a.bytes().zip(b.bytes()).take_while(|(x, y)| x == y).count() as i32);
+    }
+    res
+}
+
+fn calc_scores(words: &[&str], indices: &[usize], lengths: &[i32]) -> Vec<i32> {
+    let n = words.len();
+    let mut res = vec![0; n];
+    for (idx, &word_idx) in indices.iter().enumerate() {
+        let word_length = words[word_idx].len() as i32;
+        res[word_idx] += word_length;
+        let mut j = idx + 1;
+        let mut common_length = word_length;
+        while j < n {
+            common_length = common_length.min(lengths[j]);
+            if common_length == 0 {
+                break;
             }
+            res[word_idx] += common_length;
+            res[indices[j]] += common_length;
+            j += 1
         }
-        &[first, .., last] => {
-            if turn1 {
-                let a = dfs(&nums[1..], p1 + first, p2, !turn1);
-                let b = dfs(&nums[..nums.len() - 1], p1 + last, p2, !turn1);
-                a || b
-            } else {
-                let a = dfs(&nums[1..], p1, p2 + first, !turn1);
-                let b = dfs(&nums[..nums.len() - 1], p1, p2 + last, !turn1);
-                a && b
+    }
+    res
+}
+
+fn with_trie(words: &[&str]) -> Vec<i32> {
+    let mut trie = Trie::new();
+    for s in words.iter() {
+        trie.add(s.as_bytes());
+    }
+    words.iter().map(|s| trie.count(s.as_bytes())).collect()
+}
+
+#[derive(Debug, Clone)]
+struct Trie {
+    data: [Option<Box<Trie>>; 26],
+    count: i32,
+}
+
+impl Trie {
+    const fn new() -> Self {
+        Self {
+            data: [const { None }; 26],
+            count: 0,
+        }
+    }
+
+    fn add(&mut self, s: &[u8]) {
+        let [b, tail @ ..] = s else {
+            return;
+        };
+        let idx = usize::from(b - b'a');
+        if let Some(node) = self.data.get_mut(idx).and_then(|n| n.as_mut()) {
+            node.count += 1;
+            node.add(tail);
+        } else {
+            let mut node = Self::new();
+            node.count += 1;
+            node.add(tail);
+            self.data[idx] = Some(Box::new(node));
+        }
+    }
+
+    fn count(&self, s: &[u8]) -> i32 {
+        match s {
+            [] => self.count,
+            [b, tail @ ..] => {
+                let idx = usize::from(b - b'a');
+                if let Some(node) = self.data.get(idx).and_then(|n| n.as_ref()) {
+                    self.count + node.count(tail)
+                } else {
+                    self.count
+                }
             }
         }
     }
@@ -40,15 +111,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(!predict_the_winner(&[1, 5, 2]));
-        debug_assert!(predict_the_winner(&[1, 5, 233, 7]));
-        debug_assert!(!predict_the_winner(&[1, 3, 1]));
+        debug_assert_eq!(sum_prefix_scores(&["abc", "ab", "bc", "b"]), [5, 4, 3, 2]);
+        debug_assert_eq!(sum_prefix_scores(&["abcd"]), [4]);
     }
 
     #[test]
-    fn test() {
-        debug_assert!(predict_the_winner(&[1, 567, 1, 1]));
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
