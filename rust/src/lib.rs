4ad90a97063980007df14f5415761d9c0909ab8a
Zx Wang <ZW_08@outlook.com>
2024-09-26 14:21:06 -0400

    506 relative rank in array

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,21 +4,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn convert_to_base7(num: i32) -> String {
-    if num == 0 {
-        return "0".to_string();
-    }
-    let is_negative = num.is_negative();
-    let mut num = num.abs();
-    let mut res = vec![];
-    while num > 0 {
-        res.push((num % 7) as u8 + b'0');
-        num /= 7;
-    }
-    if is_negative {
-        res.push(b'-');
-    }
-    res.into_iter().rev().map(char::from).collect()
+pub fn find_relative_ranks(score: &[i32]) -> Vec<String> {
+    let mut nums: Vec<_> = score.iter().copied().enumerate().collect();
+    nums.sort_unstable_by_key(|p| std::cmp::Reverse(p.1));
+    let mut res: Vec<_> = nums
+        .into_iter()
+        .enumerate()
+        .map(|(rank, (idx, _))| {
+            let s = match rank {
+                0 => "Gold Medal".to_string(),
+                1 => "Silver Medal".to_string(),
+                2 => "Bronze Medal".to_string(),
+                n => (n + 1).to_string(),
+            };
+            (idx, s)
+        })
+        .collect();
+    res.sort_unstable_by_key(|p| p.0);
+    res.into_iter().map(|p| p.1).collect()
 }
 
 #[cfg(test)]
@@ -29,8 +32,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(convert_to_base7(100), "202");
-        debug_assert_eq!(convert_to_base7(-7), "-10");
+        debug_assert_eq!(
+            find_relative_ranks(&[5, 4, 3, 2, 1]),
+            ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+        );
+        debug_assert_eq!(
+            find_relative_ranks(&[10, 3, 8, 9, 4]),
+            ["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"]
+        );
     }
 
     #[test]
