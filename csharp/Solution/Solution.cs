f3ca8ab1a19d59729156cc0924e60638e907da84
Zx Wang <ZW_08@outlook.com>
2024-09-26 11:24:31 -0400

    501 find most frequent int in binary search tree

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,31 +5,36 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode DeleteNode(TreeNode root, int key)
+    List<int> res = [];
+    int currNum = 0;
+    int currStreak = 0;
+    int maxStreak = 0;
+
+    public int[] FindMode(TreeNode root)
     {
-        if (root is null) { return null; }
-        if (root.val < key) { root.right = DeleteNode(root.right, key); }
-        else if (root.val > key) { root.left = DeleteNode(root.left, key); }
-        else
-        {
-            switch ((root.left is null, root.right is null))
-            {
-                case (true, true): return null;
-                case (true, false): return root.right;
-                case (false, true): return root.left;
-                default:
-                    root.val = InorderSuccessor(ref root.right, root.right);
-                    root.right = DeleteNode(root.right, root.val);
-                    break;
-            }
-        }
-        return root;
+        Dfs(root);
+        return [.. res];
     }
 
-    static int InorderSuccessor(ref TreeNode pos, TreeNode node)
+    void Dfs(TreeNode node)
     {
-        if (node.left is not null) { return InorderSuccessor(ref node.left, node.left); }
-        pos = node.right;
-        return node.val; // node is dropped
+        if (node is null) { return; }
+        Dfs(node.left);
+        if (currNum == node.val)
+        {
+            currStreak += 1;
+        }
+        else
+        {
+            currStreak = 1;
+            currNum = node.val;
+        }
+        if (currStreak > maxStreak)
+        {
+            res.Clear();
+            maxStreak = currStreak;
+        }
+        if (currStreak == maxStreak) { res.Add(node.val); }
+        Dfs(node.right);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -1,3 +1,4 @@
+using Solution;
 using Solution.LList;
 using Solution.Tree;
 
@@ -11,37 +12,29 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = TreeNode.Make([5, 3, 6, 2, 4, null, 7]);
-        var b = sol.DeleteNode(a, 3);
-        var c = "[5,4,6,2,null,null,7]";
-        Assert.AreEqual(c, b.ToString());
+        var a = TreeNode.Make([1, null, 2, 2]);
+        var b = sol.FindMode(a);
+        var c = "[2]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([5, 3, 6, 2, 4, null, 7]);
-        var b = sol.DeleteNode(a, 0);
-        var c = "[5,3,6,2,4,null,7]";
-        Assert.AreEqual(c, b.ToString());
+        var a = TreeNode.Make([0]);
+        var b = sol.FindMode(a);
+        var c = "[0]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-        var a = TreeNode.Make([5, 3, 6, 2, 4, null, 7]);
-        var b = sol.DeleteNode(a, 5);
-        var c = "[6,3,7,2,4]";
-        Assert.AreEqual(c, b.ToString());
     }
 
     [TestMethod]
     public void TestMethod4()
     {
-        var a = TreeNode.Make([1,null,2]);
-        var b = sol.DeleteNode(a, 1);
-        var c = "[2]";
-        Assert.AreEqual(c, b.ToString());
     }
 
 }
