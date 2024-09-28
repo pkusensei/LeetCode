9c994b7cc74a3a5b5657e8c062bd95389c947f45
pkusensei <ZW_08@outlook.com>
2024-09-28 15:19:47 -0400

    529 bfs and minesweeper

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,34 +1,47 @@
 mod helper;
 mod trie;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
-use rand::Rng;
 
-#[derive(Debug, Clone)]
-struct Solution {
-    prefix: Vec<i32>,
-}
-
-impl Solution {
-    fn new(w: Vec<i32>) -> Self {
-        let mut prefix = Vec::with_capacity(w.len());
-        prefix.push(w[0]);
-        for (i, &num) in w.iter().enumerate().skip(1) {
-            prefix.push(prefix[i - 1] + num);
+pub fn update_board(mut board: Vec<Vec<char>>, click: [i32; 2]) -> Vec<Vec<char>> {
+    let [row, col] = click;
+    if board[row as usize][col as usize] == 'M' {
+        // 'M' or 'E'
+        board[row as usize][col as usize] = 'X';
+        return board;
+    }
+    let mut mines = HashSet::new();
+    for (y, r) in board.iter().enumerate() {
+        for (x, &ch) in r.iter().enumerate() {
+            if ch == 'M' {
+                mines.insert([y, x]);
+            }
         }
-        Self { prefix }
     }
-
-    fn pick_index(&self) -> i32 {
-        let mut rng = rand::thread_rng();
-        // 0 is unreachable while last value is
-        // using 0..last tilted the distribution left
-        let num = rng.gen_range(1..=*self.prefix.last().unwrap());
-        self.prefix
-            .binary_search(&num)
-            .unwrap_or_else(std::convert::identity) as _
+    let (rows, cols) = get_dimensions(&board);
+    let mut queue = VecDeque::from([(row, col)]);
+    while let Some((c_row, c_col)) = queue.pop_front() {
+        if board[c_row as usize][c_col as usize] != 'E' {
+            continue;
+        }
+        let neighbors: Vec<_> = around(c_row, c_col)
+            .filter(|(r, c)| *r < rows && *c < cols)
+            .collect();
+        let count = neighbors
+            .iter()
+            .filter(|(r, c)| mines.contains(&[*r, *c]))
+            .count();
+        if count > 0 {
+            board[c_row as usize][c_col as usize] = char::from(count as u8 + b'0');
+        } else {
+            board[c_row as usize][c_col as usize] = 'B';
+            queue.extend(neighbors.into_iter().map(|(a, b)| (a as i32, b as i32)));
+        }
     }
+    board
 }
 
 #[cfg(test)]
@@ -38,7 +51,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        debug_assert_eq!(
+            update_board(
+                vec![
+                    vec!['E', 'E', 'E', 'E', 'E'],
+                    vec!['E', 'E', 'M', 'E', 'E'],
+                    vec!['E', 'E', 'E', 'E', 'E'],
+                    vec!['E', 'E', 'E', 'E', 'E']
+                ],
+                [3, 0]
+            ),
+            [
+                ['B', '1', 'E', '1', 'B'],
+                ['B', '1', 'M', '1', 'B'],
+                ['B', '1', '1', '1', 'B'],
+                ['B', 'B', 'B', 'B', 'B']
+            ]
+        );
+        debug_assert_eq!(
+            update_board(
+                vec![
+                    vec!['B', '1', 'E', '1', 'B'],
+                    vec!['B', '1', 'M', '1', 'B'],
+                    vec!['B', '1', '1', '1', 'B'],
+                    vec!['B', 'B', 'B', 'B', 'B']
+                ],
+                [1, 2]
+            ),
+            [
+                ['B', '1', 'E', '1', 'B'],
+                ['B', '1', 'X', '1', 'B'],
+                ['B', '1', '1', '1', 'B'],
+                ['B', 'B', 'B', 'B', 'B']
+            ]
+        )
+    }
 
     #[test]
     fn test() {}
