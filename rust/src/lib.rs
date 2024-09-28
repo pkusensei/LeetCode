2f31542b1e6128756981cfbd6bc93952f5c0b077
pkusensei <ZW_08@outlook.com>
2024-09-28 18:23:23 -0400

    532 pairs in array whose diff is k

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,47 +1,36 @@
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn update_board(mut board: Vec<Vec<char>>, click: [i32; 2]) -> Vec<Vec<char>> {
-    let [row, col] = click;
-    if board[row as usize][col as usize] == 'M' {
-        // 'M' or 'E'
-        board[row as usize][col as usize] = 'X';
-        return board;
-    }
-    let mut mines = HashSet::new();
-    for (y, r) in board.iter().enumerate() {
-        for (x, &ch) in r.iter().enumerate() {
-            if ch == 'M' {
-                mines.insert([y, x]);
+pub fn find_pairs(nums: &mut [i32], k: i32) -> i32 {
+    // nums.sort_unstable();
+    // let mut res = std::collections::HashSet::new();
+    // for (i, &num) in nums.iter().enumerate() {
+    //     if let Ok(j) = nums[1 + i..].binary_search(&(num + k)) {
+    //         res.insert([num, nums[j + i + 1]]);
+    //     }
+    // }
+    // res.len() as _
+    let dict = nums
+        .iter()
+        .fold(std::collections::HashMap::new(), |mut acc, &num| {
+            *acc.entry(num).or_insert(0) += 1;
+            acc
+        });
+    let mut res = 0;
+    for (&num, &count) in dict.iter() {
+        if dict.contains_key(&(num + k)) {
+            if k > 0 {
+                res += 1;
+            }
+            if k == 0 && count > 1 {
+                res += 1
             }
         }
     }
-    let (rows, cols) = get_dimensions(&board);
-    let mut queue = VecDeque::from([(row, col)]);
-    while let Some((c_row, c_col)) = queue.pop_front() {
-        if board[c_row as usize][c_col as usize] != 'E' {
-            continue;
-        }
-        let neighbors: Vec<_> = around(c_row, c_col)
-            .filter(|(r, c)| *r < rows && *c < cols)
-            .collect();
-        let count = neighbors
-            .iter()
-            .filter(|(r, c)| mines.contains(&[*r, *c]))
-            .count();
-        if count > 0 {
-            board[c_row as usize][c_col as usize] = char::from(count as u8 + b'0');
-        } else {
-            board[c_row as usize][c_col as usize] = 'B';
-            queue.extend(neighbors.into_iter().map(|(a, b)| (a as i32, b as i32)));
-        }
-    }
-    board
+    res
 }
 
 #[cfg(test)]
@@ -52,44 +41,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            update_board(
-                vec![
-                    vec!['E', 'E', 'E', 'E', 'E'],
-                    vec!['E', 'E', 'M', 'E', 'E'],
-                    vec!['E', 'E', 'E', 'E', 'E'],
-                    vec!['E', 'E', 'E', 'E', 'E']
-                ],
-                [3, 0]
-            ),
-            [
-                ['B', '1', 'E', '1', 'B'],
-                ['B', '1', 'M', '1', 'B'],
-                ['B', '1', '1', '1', 'B'],
-                ['B', 'B', 'B', 'B', 'B']
-            ]
-        );
-        debug_assert_eq!(
-            update_board(
-                vec![
-                    vec!['B', '1', 'E', '1', 'B'],
-                    vec!['B', '1', 'M', '1', 'B'],
-                    vec!['B', '1', '1', '1', 'B'],
-                    vec!['B', 'B', 'B', 'B', 'B']
-                ],
-                [1, 2]
-            ),
-            [
-                ['B', '1', 'E', '1', 'B'],
-                ['B', '1', 'X', '1', 'B'],
-                ['B', '1', '1', '1', 'B'],
-                ['B', 'B', 'B', 'B', 'B']
-            ]
-        )
+        debug_assert_eq!(find_pairs(&mut [3, 1, 4, 1, 5], 2), 2);
+        debug_assert_eq!(find_pairs(&mut [1, 2, 3, 4, 5], 1), 4);
+        debug_assert_eq!(find_pairs(&mut [1, 3, 1, 5, 4], 0), 1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(find_pairs(&mut [-1, 0, 0, 1, 0, 0, -1], 1), 2);
+        debug_assert_eq!(find_pairs(&mut [1, 1, 1, 2, 2], 1), 1);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
