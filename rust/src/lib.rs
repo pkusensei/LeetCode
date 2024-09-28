6302d808575c69208b2746079a9870993936e00a
pkusensei <ZW_08@outlook.com>
2024-09-27 21:52:47 -0400

    524 longest subseq of str found in array

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,92 +4,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-#[derive(Debug, Clone)]
-struct MyCircularDeque {
-    data: Box<[i32]>,
-    cap: usize,
-    size: usize,
-    front: usize,
-    back: usize,
+pub fn find_longest_word(s: &str, dictionary: &mut [&str]) -> String {
+    dictionary.sort_unstable_by(|a, b| b.len().cmp(&a.len()).then(a.cmp(b)));
+    for word in dictionary.iter() {
+        if is_subseq(s.as_bytes(), word.as_bytes()) {
+            return word.to_string();
+        }
+    }
+    String::new()
 }
 
-impl MyCircularDeque {
-    fn new(k: i32) -> Self {
-        let cap = k as usize;
-        Self {
-            data: vec![0; cap].into_boxed_slice(),
-            cap,
-            size: 0,
-            front: 0,
-            back: cap - 1,
+fn is_subseq(hay: &[u8], needle: &[u8]) -> bool {
+    if hay.len() < needle.len() {
+        return false;
+    }
+    let (mut i1, mut i2) = (0, 0);
+    while i1 < hay.len() && i2 < needle.len() {
+        if hay[i1] == needle[i2] {
+            i2 += 1
         }
+        i1 += 1;
     }
-
-    fn insert_front(&mut self, value: i32) -> bool {
-        if self.is_full() {
-            false
-        } else {
-            self.front = self.front.checked_sub(1).unwrap_or(self.cap - 1);
-            self.data[self.front] = value;
-            self.size += 1;
-            true
-        }
-    }
-
-    fn insert_last(&mut self, value: i32) -> bool {
-        if self.is_full() {
-            false
-        } else {
-            self.back = (self.back + 1) % self.cap;
-            self.data[self.back] = value;
-            self.size += 1;
-            true
-        }
-    }
-
-    fn delete_front(&mut self) -> bool {
-        if self.is_empty() {
-            false
-        } else {
-            self.front = (self.front + 1) % self.cap;
-            self.size -= 1;
-            true
-        }
-    }
-
-    fn delete_last(&mut self) -> bool {
-        if self.is_empty() {
-            false
-        } else {
-            self.back = self.back.checked_sub(1).unwrap_or(self.cap - 1);
-            self.size -= 1;
-            true
-        }
-    }
-
-    fn get_front(&self) -> i32 {
-        if self.is_empty() {
-            -1
-        } else {
-            self.data[self.front]
-        }
-    }
-
-    fn get_rear(&self) -> i32 {
-        if self.is_empty() {
-            -1
-        } else {
-            self.data[self.back]
-        }
-    }
-
-    fn is_empty(&self) -> bool {
-        0 == self.size
-    }
-
-    fn is_full(&self) -> bool {
-        self.size == self.cap
-    }
+    i2 == needle.len()
 }
 
 #[cfg(test)]
@@ -100,20 +36,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut q = MyCircularDeque::new(3);
-        debug_assert!(q.insert_last(1)); // return True
-        debug_assert!(q.insert_last(2)); // return True
-        debug_assert!(q.insert_front(3)); // return True
-        debug_assert!(!q.insert_front(4)); // return False, the queue is full.
-        debug_assert_eq!(q.get_rear(), 2); // return 2
-        debug_assert!(q.is_full()); // return True
-        debug_assert!(q.delete_last()); // return True
-        debug_assert!(q.insert_front(4)); // return True
-        debug_assert_eq!(q.get_front(), 4); // return 4
+        debug_assert_eq!(
+            find_longest_word("abpcplea", &mut ["ale", "apple", "monkey", "plea"]),
+            "apple"
+        );
+        debug_assert_eq!(find_longest_word("abpcplea", &mut ["a", "b", "c"]), "a");
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(find_longest_word("aaa", &mut ["aaa", "aa", "a"]), "aaa");
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
