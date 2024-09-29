d990581a511fbdd179b7200795a8cba790a90cd9
pkusensei <ZW_08@outlook.com>
2024-09-28 20:24:33 -0400

    535 shorten url and restore

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,33 +4,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_pairs(nums: &mut [i32], k: i32) -> i32 {
-    // nums.sort_unstable();
-    // let mut res = std::collections::HashSet::new();
-    // for (i, &num) in nums.iter().enumerate() {
-    //     if let Ok(j) = nums[1 + i..].binary_search(&(num + k)) {
-    //         res.insert([num, nums[j + i + 1]]);
-    //     }
-    // }
-    // res.len() as _
-    let dict = nums
-        .iter()
-        .fold(std::collections::HashMap::new(), |mut acc, &num| {
-            *acc.entry(num).or_insert(0) += 1;
-            acc
-        });
-    let mut res = 0;
-    for (&num, &count) in dict.iter() {
-        if dict.contains_key(&(num + k)) {
-            if k > 0 {
-                res += 1;
-            }
-            if k == 0 && count > 1 {
-                res += 1
-            }
-        }
+#[derive(Debug, Clone)]
+struct Codec {
+    data: Vec<String>,
+}
+
+impl Codec {
+    fn new() -> Self {
+        Self { data: vec![] }
     }
-    res
+
+    // Encodes a URL to a shortened URL.
+    fn encode(&mut self, s: String) -> String {
+        self.data.push(s);
+        (self.data.len() - 1).to_string()
+    }
+
+    // Decodes a shortened URL to its original URL.
+    fn decode(&self, s: String) -> String {
+        let i = s.parse::<usize>().unwrap();
+        self.data[i].clone()
+    }
 }
 
 #[cfg(test)]
@@ -40,17 +34,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        debug_assert_eq!(find_pairs(&mut [3, 1, 4, 1, 5], 2), 2);
-        debug_assert_eq!(find_pairs(&mut [1, 2, 3, 4, 5], 1), 4);
-        debug_assert_eq!(find_pairs(&mut [1, 3, 1, 5, 4], 0), 1);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        debug_assert_eq!(find_pairs(&mut [-1, 0, 0, 1, 0, 0, -1], 1), 2);
-        debug_assert_eq!(find_pairs(&mut [1, 1, 1, 2, 2], 1), 1);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
