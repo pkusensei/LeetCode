b9e47d9a1115ad11e7bd8007cc7fe50d3be0ba49
pkusensei <ZW_08@outlook.com>
2024-09-29 18:54:15 -0400

    546 3d dp; remove boxes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,30 +4,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn update_matrix(mut mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let (rows, cols) = get_dimensions(&mat);
-    let mut queue = std::collections::VecDeque::new();
-    for y in 0..rows {
-        for x in 0..cols {
-            if mat[y][x] > 0 {
-                mat[y][x] = -1
-            } else {
-                queue.push_back((y, x, 0));
-            }
+pub fn remove_boxes(boxes: &[i32]) -> i32 {
+    let n = boxes.len();
+    let mut dp = vec![vec![vec![0; n]; n]; n];
+    solve(boxes, &mut dp, 0, n - 1, 0)
+}
+
+fn solve(nums: &[i32], dp: &mut [Vec<Vec<i32>>], left: usize, right: usize, count: usize) -> i32 {
+    if left > right {
+        return 0;
+    }
+    if dp[left][right][count] > 0 {
+        return dp[left][right][count];
+    }
+    let (mut c_left, mut c_count) = (left, count);
+    while c_left < right && nums[c_left] == nums[c_left + 1] {
+        c_left += 1;
+        c_count += 1;
+    }
+    // [1,1,1,2,2,1..]
+    // remove [1,1,1] and calculate [2,2,1...]
+    let mut res = ((c_count + 1) * (c_count + 1)) as i32 + solve(nums, dp, c_left + 1, right, 0);
+
+    for i in c_left + 1..=right {
+        if nums[i] == nums[c_left] {
+            // find all chance to merge
+            // [1,1,1,2,2,1,1..] can be seen as
+            // [2,2] + [1,1,1,1,1..]
+            //          ^^^^^ to carry over this information
+            //                add count to the next recursion
+            res = res
+                .max(solve(nums, dp, c_left + 1, i - 1, 0) + solve(nums, dp, i, right, c_count + 1))
         }
     }
-    while let Some((row, col, dist)) = queue.pop_front() {
-        if mat[row][col] > 0 {
-            continue;
-        }
-        mat[row][col] = dist;
-        for (nr, nc) in
-            neighbors((row, col)).filter(|&(r, c)| r < rows && c < cols && mat[r][c] < 0)
-        {
-            queue.push_back((nr, nc, dist + 1));
-        }
-    }
-    mat
+    dp[left][right][count] = res;
+    res
 }
 
 #[cfg(test)]
@@ -38,14 +49,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            update_matrix(vec![vec![0, 0, 0], vec![0, 1, 0], vec![0, 0, 0]]),
-            [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-        );
-        debug_assert_eq!(
-            update_matrix(vec![vec![0, 0, 0], vec![0, 1, 0], vec![1, 1, 1]]),
-            [[0, 0, 0], [0, 1, 0], [1, 2, 1]]
-        );
+        debug_assert_eq!(remove_boxes(&[1, 3, 2, 2, 2, 3, 4, 3, 1]), 23);
+        debug_assert_eq!(remove_boxes(&[1, 1, 1]), 9);
+        debug_assert_eq!(remove_boxes(&[1]), 1);
     }
 
     #[test]
