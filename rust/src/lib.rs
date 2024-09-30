0b11edc7f91eb4a33332ecd9a367ac0ece85ee95
pkusensei <ZW_08@outlook.com>
2024-09-30 19:04:48 -0400

    560 subarray with sum of k

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,11 +4,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn reverse_words(s: &str) -> String {
-    s.split_whitespace()
-        .map(|s| s.chars().rev().collect::<String>())
-        .collect::<Vec<_>>()
-        .join(" ")
+pub fn subarray_sum(nums: &[i32], k: i32) -> i32 {
+    let mut prefix = std::collections::HashMap::new();
+    prefix.insert(0, 1);
+    let mut res = 0;
+    let mut sum = 0;
+    for &num in nums.iter() {
+        sum += num;
+        if let Some(&v) = prefix.get(&(sum - k)) {
+            res += v;
+        }
+        *prefix.entry(sum).or_insert(0) += 1;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -19,15 +27,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            reverse_words("Let's take LeetCode contest"),
-            "s'teL ekat edoCteeL tsetnoc"
-        );
-        debug_assert_eq!(reverse_words("Mr Ding"), "rM gniD");
+        debug_assert_eq!(subarray_sum(&[1, 1, 1], 2), 2);
+        debug_assert_eq!(subarray_sum(&[1, 2, 3], 3), 2);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(subarray_sum(&[1, -1, 0], 0), 3);
+        debug_assert_eq!(subarray_sum(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0), 55);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
