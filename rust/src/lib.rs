6c4cf4ad1e56cf21e98e835f72503e5180403c48
pkusensei <ZW_08@outlook.com>
2024-09-30 12:07:29 -0400

    552 T_T

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,38 +4,64 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-#[derive(Debug, Clone)]
-struct CustomStack {
-    data: Vec<i32>,
-    cap: usize,
+const MOD: i32 = 1_000_000_007;
+
+pub fn check_record(n: i32) -> i32 {
+    bottom_up(n)
+    // let n = n as usize;
+    // // dp[absent][late][1..=n]
+    // let mut dp = vec![vec![vec![-1; 1 + n]; 3]; 2];
+    // top_down(&mut dp, n, 0, 0)
 }
 
-impl CustomStack {
-    fn new(cap: i32) -> Self {
-        let cap = cap as usize;
-        Self {
-            data: Vec::with_capacity(cap),
-            cap,
-        }
+// stack overflow
+fn top_down(dp: &mut [Vec<Vec<i32>>], n: usize, abs: usize, late: usize) -> i32 {
+    if abs >= 2 || late >= 3 {
+        return 0;
     }
-
-    fn push(&mut self, x: i32) {
-        if self.data.len() < self.cap {
-            self.data.push(x);
-        }
+    if n == 0 {
+        return 1;
     }
-
-    fn pop(&mut self) -> i32 {
-        self.data.pop().unwrap_or(-1)
+    if dp[abs][late][n] > -1 {
+        return dp[abs][late][n];
     }
+    // 'P' => clears late records, but keeps absent
+    let mut res = top_down(dp, n - 1, abs, 0) % MOD;
+    // 'A' => clears late records, but keeps absent
+    res = (res + top_down(dp, n - 1, abs + 1, 0)) % MOD;
+    // 'L' => consecutive late
+    res = (res + top_down(dp, n - 1, abs, late + 1)) % MOD;
+    dp[abs][late][n] = res;
+    res
+}
 
-    fn increment(&mut self, k: i32, val: i32) {
-        for i in 0..k as usize {
-            if let Some(v) = self.data.get_mut(i) {
-                *v += val;
+fn bottom_up(n: i32) -> i32 {
+    // dp[abs][late]
+    let mut dp_curr = [[0; 3]; 2];
+    let mut dp_next = dp_curr;
+    dp_curr[0][0] = 1;
+    for _ in 0..n {
+        for abs in 0..2 {
+            for lat in 0..3 {
+                // 'P'
+                dp_next[abs][0] = (dp_next[abs][0] + dp_curr[abs][lat]) % MOD;
+                if abs == 0 {
+                    // 'A'
+                    dp_next[1][0] = (dp_next[1][0] + dp_curr[0][lat]) % MOD;
+                }
+                if lat < 2 {
+                    // 'L'
+                    dp_next[abs][lat + 1] = (dp_next[abs][lat + 1] + dp_curr[abs][lat]) % MOD;
+                }
             }
         }
+        dp_curr = dp_next;
+        dp_next = Default::default();
     }
+    dp_curr
+        .into_iter()
+        .flat_map(|r| r.into_iter())
+        .fold(0, |acc, n| (acc + n) % MOD)
 }
 
 #[cfg(test)]
@@ -46,19 +72,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut stk = CustomStack::new(3); // Stack is Empty []
-        stk.push(1); // stack becomes [1]
-        stk.push(2); // stack becomes [1, 2]
-        debug_assert_eq!(stk.pop(), 2); // return 2 --> Return top of the stack 2, stack becomes [1]
-        stk.push(2); // stack becomes [1, 2]
-        stk.push(3); // stack becomes [1, 2, 3]
-        stk.push(4); // stack still [1, 2, 3], Do not add another elements as size is 4
-        stk.increment(5, 100); // stack becomes [101, 102, 103]
-        stk.increment(2, 100); // stack becomes [201, 202, 103]
-        debug_assert_eq!(stk.pop(), 103); // return 103 --> Return top of the stack 103, stack becomes [201, 202]
-        debug_assert_eq!(stk.pop(), 202); // return 202 --> Return top of the stack 202, stack becomes [201]
-        debug_assert_eq!(stk.pop(), 201); // return 201 --> Return top of the stack 201, stack becomes []
-        debug_assert_eq!(stk.pop(), -1); // return -1 --> Stack is empty return -1.
+        debug_assert_eq!(check_record(2), 8);
+        debug_assert_eq!(check_record(1), 3);
+        debug_assert_eq!(check_record(10101), 183236316);
     }
 
     #[test]
