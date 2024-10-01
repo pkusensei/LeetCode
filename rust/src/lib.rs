0eac23c928900fcb32e8a027b1f471314002c5c5
Zx Wang <ZW_08@outlook.com>
2024-10-01 12:54:41 -0400

    575 distribute candies

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,32 +4,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_inclusion(s1: &str, s2: &str) -> bool {
-    let (n1, n2) = (s1.len(), s2.len());
-    if n1 > n2 {
-        return false;
-    }
-    let c1 = count(s1.as_bytes());
-    let mut c2 = count(s2[..n1].as_bytes());
-    if c1 == c2 {
-        return true;
-    }
-    let s2 = s2.as_bytes();
-    for i in 1..=n2 - n1 {
-        c2[usize::from(s2[i - 1] - b'a')] -= 1;
-        c2[usize::from(s2[i + n1 - 1] - b'a')] += 1;
-        if c1 == c2 {
-            return true;
-        }
-    }
-    false
-}
-
-fn count(s: &[u8]) -> [i16; 26] {
-    s.iter().fold([0; 26], |mut acc, &b| {
-        acc[usize::from(b - b'a')] += 1;
-        acc
-    })
+pub fn distribute_candies(candy_type: &[i32]) -> i32 {
+    let counts = candy_type
+        .iter()
+        .fold(std::collections::HashSet::new(), |mut acc, &n| {
+            acc.insert(n);
+            acc
+        });
+    let c = candy_type.len() / 2;
+    c.min(counts.len()) as _
 }
 
 #[cfg(test)]
@@ -39,15 +22,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        debug_assert!(check_inclusion("ab", "eidbaooo"));
-        debug_assert!(!check_inclusion("ab", "eidboaoo"));
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        debug_assert!(check_inclusion("adc", "dcda"))
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
