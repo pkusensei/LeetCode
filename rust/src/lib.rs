a9b151403b3f0e86b95c388ffd63cd9465e85f84
Zx Wang <ZW_08@outlook.com>
2024-10-01 14:25:37 -0400

    576 amount of paths out of bounds

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,15 +4,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn distribute_candies(candy_type: &[i32]) -> i32 {
-    let counts = candy_type
-        .iter()
-        .fold(std::collections::HashSet::new(), |mut acc, &n| {
-            acc.insert(n);
-            acc
-        });
-    let c = candy_type.len() / 2;
-    c.min(counts.len()) as _
+const MOD: i32 = 1_000_000_007;
+
+pub fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32 {
+    // dp[move][row][col]
+    // 1) ^^^^  ^^^  ^^^  the three variables that might change during each turn of recursion
+    // 2) exit conditions
+    // 3) actually still (1), how to break up into smaller (sub)problems
+    let mut dp = vec![vec![vec![-1; n as usize]; m as usize]; 1 + max_move as usize];
+    dfs(&mut dp, m, n, max_move, start_row, start_column)
+}
+
+fn dfs(dp: &mut [Vec<Vec<i32>>], m: i32, n: i32, moves: i32, row: i32, col: i32) -> i32 {
+    if moves < 0 {
+        return 0;
+    }
+    if !(0..m).contains(&row) || !(0..n).contains(&col) {
+        return 1;
+    }
+    if dp[moves as usize][row as usize][col as usize] > -1 {
+        return dp[moves as usize][row as usize][col as usize];
+    }
+    let res = [
+        dfs(dp, m, n, moves - 1, row - 1, col),
+        dfs(dp, m, n, moves - 1, row + 1, col),
+        dfs(dp, m, n, moves - 1, row, col - 1),
+        dfs(dp, m, n, moves - 1, row, col + 1),
+    ]
+    .into_iter()
+    .fold(0, |acc, n| (acc + n) % MOD);
+    dp[moves as usize][row as usize][col as usize] = res;
+    res
 }
 
 #[cfg(test)]
@@ -22,7 +44,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        debug_assert_eq!(find_paths(2, 2, 2, 0, 0), 6);
+        debug_assert_eq!(find_paths(1, 3, 3, 0, 1), 12);
+    }
 
     #[test]
     fn test() {}
