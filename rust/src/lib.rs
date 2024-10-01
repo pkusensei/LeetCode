5ad654f5041eca0f2dec0e9ce31bfa72a9646efa
pkusensei <ZW_08@outlook.com>
2024-09-30 20:57:19 -0400

    1497 pairs in array are divisible by k

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,9 +4,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn array_pair_sum(nums: &mut [i32]) -> i32 {
-    nums.sort_unstable();
-    nums.iter().step_by(2).sum()
+pub fn can_arrange(arr: &[i32], k: i32) -> bool {
+    // let counts = arr
+    //     .iter()
+    //     .fold(std::collections::HashMap::new(), |mut acc, &num| {
+    //         *acc.entry((num % k + k) % k).or_insert(0) += 1;
+    //         acc
+    //     });
+    // for &num in arr.iter() {
+    //     let remainder = (num % k + k) % k;
+    //     if remainder == 0 {
+    //         if counts.get(&0).is_some_and(|v| v & 1 == 1) {
+    //             return false;
+    //         }
+    //     } else if counts.get(&remainder) != counts.get(&(k - remainder)) {
+    //         return false;
+    //     }
+    // }
+    let counts = arr.iter().fold(vec![0; k as usize], |mut acc, &num| {
+        acc[((num % k + k) % k) as usize] += 1;
+        acc
+    });
+    if counts[0] & 1 > 0 {
+        return false;
+    }
+    for i in 1..=k / 2 {
+        if counts[i as usize] != counts[(k - i) as usize] {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -17,12 +44,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(array_pair_sum(&mut [1, 4, 3, 2]), 4);
-        debug_assert_eq!(array_pair_sum(&mut [6, 2, 6, 5, 1, 2]), 9);
+        debug_assert!(can_arrange(&[1, 2, 3, 4, 5, 10, 6, 7, 8, 9], 5));
+        debug_assert!(can_arrange(&[1, 2, 3, 4, 5, 6], 7));
+        debug_assert!(!can_arrange(&[1, 2, 3, 4, 5, 6], 10));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert!(!can_arrange(&[5, 5, 1, 2, 3, 4], 10));
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
