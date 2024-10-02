31a64c8f81787c4d9ac37eb7e548f0538be037a4
pkusensei <ZW_08@outlook.com>
2024-10-02 16:18:02 -0400

    590 postorder traversal of n-ary trees; ii

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,22 +5,24 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<int> Preorder(Node root)
+    public IList<int> Postorder(Node root)
     {
         List<int> res = [];
         if (root is null) { return res; }
-        var st = new Stack<Node>(); st.Push(root);
+        var st = new Stack<Node>();
+        st.Push(root);
         while (st.TryPop(out var node))
         {
             if (node is not null)
             {
                 res.Add(node.val);
-                foreach (var item in node.children.Reverse())
+                foreach (var item in node.children)
                 {
                     st.Push(item);
                 }
             }
         }
+        res.Reverse();
         return res;
     }
 }
