acd16c67157265c2f07f4bc1bea69ec2504595f2
pkusensei <ZW_08@outlook.com>
2024-10-02 13:28:18 -0400

    589 preorder traversal of n-ary tree using stack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,26 +5,22 @@ namespace Solution;
 
 public class Solution
 {
-    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    public IList<int> Preorder(Node root)
     {
-        return (root is null, subRoot is null)
-        switch
+        List<int> res = [];
+        if (root is null) { return res; }
+        var st = new Stack<Node>(); st.Push(root);
+        while (st.TryPop(out var node))
         {
-            (true, true) => true,
-            (false, true) or (true, false) => false,
-            _ => Dfs(root, subRoot) || IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot),
-        };
-
-    }
-
-    static bool Dfs(TreeNode a, TreeNode b)
-    {
-        return (a is null, b is null)
-        switch
-        {
-            (true, true) => true,
-            (false, true) or (true, false) => false,
-            _ => a.val == b.val && Dfs(a.left, b.left) && Dfs(a.right, b.right),
-        };
+            if (node is not null)
+            {
+                res.Add(node.val);
+                foreach (var item in node.children.Reverse())
+                {
+                    st.Push(item);
+                }
+            }
+        }
+        return res;
     }
 }
