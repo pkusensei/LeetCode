f9aab91f395ff510e0a7a07159b8738f0828683d
pkusensei <ZW_08@outlook.com>
2024-10-03 06:59:16 -0400

    591 didn't actually do this

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,27 +4,71 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_subarray(nums: &[i32], p: i32) -> i32 {
-    let sum: i32 = nums.iter().fold(0, |acc, &n| (acc + n) % p);
-    let target = sum % p;
-    if target == 0 {
-        return 0;
-    };
-    let mut map = std::collections::HashMap::from([(0, -1)]);
-    let (mut curr, mut res) = (0, nums.len() as i32);
-    for (i, &num) in nums.iter().enumerate() {
-        curr = (curr + num) % p;
-        let v = (curr - target).rem_euclid(p);
-        if let Some(j) = map.get(&v) {
-            res = res.min(i as i32 - j);
+pub fn is_valid(code: &str) -> bool {
+    let (s, n) = (code.as_bytes(), code.len());
+    if s[0] != b'<' || !s.iter().last().is_some_and(|&b| b == b'>') {
+        return false;
+    }
+    let mut idx = 0;
+    let mut stack = vec![];
+    let mut has_tag = false;
+    while idx < n {
+        let mut is_end = false;
+        if stack.is_empty() && has_tag {
+            return false;
         }
-        map.insert(curr, i as i32);
+        if s[idx] == b'<' {
+            idx = if !stack.is_empty() && s[idx + 1] == b'!' {
+                let Some(close) = code[idx + 1..].find("]]>").map(|v| v + idx + 1) else {
+                    return false;
+                };
+                if !is_valid_cdata(&code[idx + 2..close]) {
+                    return false;
+                }
+                close
+            } else {
+                if s[idx + 1] == b'/' {
+                    idx += 1;
+                    is_end = true;
+                }
+                let Some(close) = code[idx + 1..].find('>').map(|v| v + idx + 1) else {
+                    return false;
+                };
+                if !is_valid_tagname(&code[idx + 1..close], is_end, &mut stack, &mut has_tag) {
+                    return false;
+                }
+                close
+            };
+        }
+        idx += 1
     }
-    if res == nums.len() as i32 {
-        -1
+    stack.is_empty()
+}
+
+fn is_valid_tagname<'a>(
+    s: &'a str,
+    is_end: bool,
+    stack: &mut Vec<&'a str>,
+    has_tag: &mut bool,
+) -> bool {
+    if !(1..=9).contains(&s.len()) || !s.bytes().all(|b| b.is_ascii_uppercase()) {
+        return false;
+    }
+    if is_end {
+        if stack.last().is_some_and(|v| *v == s) {
+            stack.pop();
+        } else {
+            return false;
+        }
     } else {
-        res
+        stack.push(s);
+        *has_tag = true;
     }
+    true
+}
+
+fn is_valid_cdata(s: &str) -> bool {
+    s.find("[CDATA[").is_some_and(|v| v == 0)
 }
 
 #[cfg(test)]
@@ -35,13 +79,21 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_subarray(&[3, 1, 4, 2], 6), 1);
-        debug_assert_eq!(min_subarray(&[6, 3, 5, 2], 9), 2);
-        debug_assert_eq!(min_subarray(&[1, 2, 3], 3), 0);
+        debug_assert!(is_valid(
+            "<DIV>This is the first line <![CDATA[<div>]]></DIV>"
+        ));
+        debug_assert!(is_valid(
+            "<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>"
+        ));
+        debug_assert!(!is_valid("<A>  <B> </A>   </B>"));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert!(!is_valid(
+            "<TRUe><![CDATA[123123]]]]><![CDATA[>123123]]></TRUe>"
+        ));
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
