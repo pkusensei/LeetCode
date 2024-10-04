559cd7e687b1c2ae58060955bafb8fcd6755559c
pkusensei <ZW_08@outlook.com>
2024-10-04 11:54:56 -0400

    621 task scheduler with forced idle gaps

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,25 +1,53 @@
 mod helper;
 mod trie;
 
+use std::collections::{BinaryHeap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn triangle_number(nums: &mut [i32]) -> i32 {
-    let n = nums.len();
-    if n < 3 {
-        return 0;
-    }
-    nums.sort_unstable();
+pub fn least_interval(tasks: &[char], n: i32) -> i32 {
+    // with_pq(tasks, n)
+    let counts = tasks.iter().fold([0; 26], |mut acc, &ch| {
+        acc[(u32::from(ch) - u32::from('A')) as usize] += 1;
+        acc
+    });
+    let max = *counts.iter().max().unwrap();
+    let max_count = counts.iter().filter(|&&n| n == max).count() as i32;
+    ((max - 1) * (n + 1) + max_count).max(tasks.len() as i32)
+    // ^^^^^^^ with gap/idle ^^^^^^^  vs  ^^^^^ no gap ^^^^^
+    // Reagrding the gap situation
+    // A cycle is (1+n) steps, with (max-1) cycles
+    // And amount of max_count that's lingering
+}
+
+fn with_pq(tasks: &[char], n: i32) -> i32 {
+    let mut chs: BinaryHeap<_> = tasks
+        .iter()
+        .fold(HashMap::with_capacity(26), |mut acc, &ch| {
+            *acc.entry(ch).or_insert(0) += 1;
+            acc
+        })
+        .into_values()
+        .collect();
+
     let mut res = 0;
-    for i in 0..n - 2 {
-        if nums[i] == 0 {
-            continue;
+    let mut temp = Vec::with_capacity(1 + n as usize);
+    while !chs.is_empty() {
+        let mut cycle = n + 1;
+        let mut count = 0;
+        while cycle > 0 {
+            cycle -= 1;
+            let Some(v) = chs.pop() else {
+                break;
+            };
+            if v > 1 {
+                temp.push(v - 1);
+            }
+            count += 1;
         }
-        for j in i + 1..n - 1 {
-            let side = nums[i] + nums[j];
-            let k = nums[j + 1..].partition_point(|&v| v < side);
-            res += k as i32;
-        }
+        chs.extend(temp.drain(..));
+        res += if chs.is_empty() { count } else { n + 1 };
     }
     res
 }
@@ -32,8 +60,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(triangle_number(&mut [2, 2, 3, 4]), 3);
-        debug_assert_eq!(triangle_number(&mut [4, 2, 3, 4]), 4);
+        debug_assert_eq!(least_interval(&['A', 'A', 'A', 'B', 'B', 'B'], 2), 8);
+        debug_assert_eq!(least_interval(&['A', 'C', 'A', 'B', 'D', 'B'], 1), 6);
+        debug_assert_eq!(least_interval(&['A', 'A', 'A', 'B', 'B', 'B'], 3), 10);
     }
 
     #[test]
