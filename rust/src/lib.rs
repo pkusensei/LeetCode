10213562c927370769869007f5d36dc24ceaabc0
pkusensei <ZW_08@outlook.com>
2024-10-05 13:11:53 -0400

    636 exclusive time of funtions with stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,17 +4,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn judge_square_sum(c: i32) -> bool {
-    let mut right = f64::from(c).sqrt().trunc() as i32;
-    let mut left = 0;
-    while left <= right {
-        match (left * left + right * right).cmp(&c) {
-            std::cmp::Ordering::Less => left += 1,
-            std::cmp::Ordering::Equal => return true,
-            std::cmp::Ordering::Greater => right -= 1,
+pub fn exclusive_time(n: i32, logs: &[&str]) -> Vec<i32> {
+    let mut states: Vec<_> = logs.iter().map(|s| parse(s).unwrap()).collect();
+    states.sort_unstable_by_key(|s| s.1);
+    let n = n as usize;
+    let mut stack: Vec<(usize, i32, bool)> = Vec::with_capacity(n / 2);
+    let mut res = vec![0; n];
+    for state in states {
+        if state.2 {
+            if let Some(prev) = stack.last_mut() {
+                res[prev.0] += state.1 - prev.1; // pause prev
+            }
+            stack.push(state);
+        } else {
+            let Some((i, s, _)) = stack.pop() else {
+                break;
+            };
+            debug_assert_eq!(i, state.0);
+            res[i] += state.1 - s + 1;
+            if let Some(prev) = stack.last_mut() {
+                prev.1 = state.1 + 1; // resume
+            }
         }
     }
-    false
+    res
+}
+
+fn parse(s: &str) -> Option<(usize, i32, bool)> {
+    let mut it = s.split(':');
+    let id = it.next().map(|s| s.parse().ok())??; // ???
+    let start = it.next() == Some("start");
+    let time = it.next().map(|s| s.parse().ok())??;
+    Some((id, time, start))
 }
 
 #[cfg(test)]
@@ -23,10 +44,24 @@ mod tests {
 
     use super::*;
 
+    #[rustfmt::skip]
     #[test]
     fn basics() {
-        debug_assert!(judge_square_sum(5));
-        debug_assert!(!judge_square_sum(3));
+        debug_assert_eq!(
+            exclusive_time(2, &["0:start:0", "1:start:2", "1:end:5", "0:end:6"]),
+            [3, 4]
+        );
+
+        debug_assert_eq!(
+            exclusive_time(1,
+                &["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"]),
+            [8]
+        );
+        debug_assert_eq!(
+            exclusive_time(2, 
+                &["0:start:0","0:start:2","0:end:5","1:start:6","1:end:6","0:end:7"]),
+            [7,1]
+        );
     }
 
     #[test]
