cf9c0d4fd80a463f6d70611d5961fce5b1f52791
pkusensei <ZW_08@outlook.com>
2024-10-05 08:04:30 -0400

    630 finish courses before ddl

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,33 +1,36 @@
 mod helper;
 mod trie;
 
+use std::collections::BinaryHeap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_inclusion(s1: &str, s2: &str) -> bool {
-    let n = s1.len();
-    if n > s2.len() {
-        return false;
-    }
-    let mut counts = s1.bytes().fold([0; 26], |mut acc, b| {
-        acc[usize::from(b - b'a')] += 1;
-        acc
-    });
-    for b in s2[..n].bytes() {
-        counts[usize::from(b - b'a')] -= 1;
-    }
-    if counts.iter().all(|&n| n == 0) {
-        return true;
-    }
-    let s2 = s2.as_bytes();
-    for i in 1..=s2.len() - n {
-        counts[usize::from(s2[i - 1] - b'a')] += 1;
-        counts[usize::from(s2[i + n - 1] - b'a')] -= 1;
-        if counts.iter().all(|&n| n == 0) {
-            return true;
+pub fn schedule_course(courses: &mut [[i32; 2]]) -> i32 {
+    courses.sort_unstable_by_key(|v| v[1]);
+    let mut total = 0;
+    let mut heap = BinaryHeap::new();
+    for v in courses.iter() {
+        // Expand v as [dur, ddl]
+        // Every dur that's pushed onto the heap has passed the check
+        // total + dur <= ddl
+        // Since array is sorted, every upcoming ddl must be bigger
+        // Whenever a false check shows up
+        // Try find a bigger __used__ dur on heap and swap it out
+        // Thus reducing total
+        if total + v[0] <= v[1] {
+            total += v[0];
+            heap.push(v[0]);
+        } else if heap.peek().is_some_and(|&n| n > v[0]) {
+            let Some(prev) = heap.pop() else {
+                continue;
+            };
+            total -= prev;
+            total += v[0];
+            heap.push(v[0]);
         }
     }
-    false
+    heap.len() as _
 }
 
 #[cfg(test)]
@@ -38,12 +41,19 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(check_inclusion("ab", "eidbaooo"));
-        debug_assert!(!check_inclusion("ab", "eidboaoo"));
+        debug_assert_eq!(
+            schedule_course(&mut [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]]),
+            3
+        );
+        debug_assert_eq!(schedule_course(&mut [[1, 2]]), 1);
+        debug_assert_eq!(schedule_course(&mut [[3, 2], [4, 3]]), 0);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(schedule_course(&mut [[1, 2], [2, 3]]), 2);
+        debug_assert_eq!(schedule_course(&mut [[5, 5], [4, 6], [2, 6]]), 2);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
