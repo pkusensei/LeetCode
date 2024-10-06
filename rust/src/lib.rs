7df29d122e468c9ce24e885c1afda979f03f308c
pkusensei <ZW_08@outlook.com>
2024-10-06 13:19:56 -0400

    643 max average subarray

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,53 +4,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn solve_equation(equation: &str) -> String {
-    let Some(((ax, an), (bx, bn))) = equation.split_once('=').map(|(a, b)| (parse(a), parse(b)))
-    else {
-        return "No solution".to_string();
-    };
-    match (ax - bx, bn - an) {
-        (0, 0) => "Infinite solutions".to_string(),
-        (0, _) => "No solution".to_string(),
-        _ => format!("x={}", (bn - an) / (ax - bx)),
-    }
-}
-
-fn parse(s: &str) -> (i32, i32) {
-    let (s, n) = (s.as_bytes(), s.len());
-    let mut idx = 0;
-    let mut sign = 1;
-    let mut factor = 0;
-    let mut num = 0;
-    while idx < n {
-        match s[idx] {
-            b'+' => idx += 1,
-            b'-' => {
-                sign = -1;
-                idx += 1
-            }
-            b'x' => {
-                factor += sign;
-                sign = 1;
-                idx += 1;
-            }
-            _ => {
-                let mut temp = 0;
-                while idx < n && s[idx].is_ascii_digit() {
-                    temp = temp * 10 + i32::from(s[idx] - b'0');
-                    idx += 1;
-                }
-                if s.get(idx).is_some_and(|&b| b == b'x') {
-                    factor += sign * temp;
-                    idx += 1;
-                } else {
-                    num += sign * temp;
-                }
-                sign = 1;
-            }
-        }
-    }
-    (factor, num)
+pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+    nums.windows(k as usize)
+        .map(|w| f64::from(w.iter().sum::<i32>()) / f64::from(k))
+        .max_by(|a, b| a.total_cmp(b))
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -60,11 +18,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        debug_assert_eq!(solve_equation("x+5-3+x=6+x-2"), "x=2");
-        debug_assert_eq!(solve_equation("x=x"), "Infinite solutions");
-        debug_assert_eq!(solve_equation("2x=x"), "x=0");
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
