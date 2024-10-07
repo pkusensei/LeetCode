a4a6e97710729c720c0092d6aad93acc566746ed
pkusensei <ZW_08@outlook.com>
2024-10-07 11:21:43 -0400

    649 ban the next voter

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,22 +1,39 @@
 mod helper;
 mod trie;
 
+use std::collections::VecDeque;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn replace_words(dictionary: &[&str], sentence: &str) -> String {
-    sentence
-        .split_ascii_whitespace()
-        .map(|word| replace(dictionary, word).unwrap_or(word))
-        .collect::<Vec<_>>()
-        .join(" ")
-}
-
-fn replace<'a>(dict: &[&'a str], word: &str) -> Option<&'a str> {
-    dict.iter()
-        .filter(|w| word.starts_with(*w))
-        .min_by_key(|w| w.len())
-        .copied()
+pub fn predict_party_victory(s: &str) -> String {
+    let n = s.len();
+    let (mut ds, mut rs) = (VecDeque::new(), VecDeque::new());
+    for (i, b) in s.bytes().enumerate() {
+        if b == b'D' {
+            ds.push_back(i);
+        } else {
+            rs.push_back(i);
+        }
+    }
+    while !ds.is_empty() || !rs.is_empty() {
+        let Some(d) = ds.pop_front() else {
+            return "Radiant".into();
+        };
+        let Some(r) = rs.pop_front() else {
+            return "Dire".into();
+        };
+        if d < r {
+            ds.push_back(d + n);
+        } else {
+            rs.push_back(r + n);
+        }
+    }
+    if ds.is_empty() {
+        "Radiant".into()
+    } else {
+        "Dire".into()
+    }
 }
 
 #[cfg(test)]
@@ -27,21 +44,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            replace_words(
-                &["cat", "bat", "rat"],
-                "the cattle was rattled by the battery"
-            ),
-            "the cat was rat by the bat"
-        );
-        debug_assert_eq!(
-            replace_words(&["a", "b", "c"], "aadsfasf absbs bbab cadsfafs"),
-            "a a b c"
-        );
+        debug_assert_eq!(predict_party_victory("RD".into()), "Radiant");
+        debug_assert_eq!(predict_party_victory("RDD".into()), "Dire");
+        debug_assert_eq!(predict_party_victory("DDRRR".into()), "Dire");
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(predict_party_victory("DRRDRDRDRDDRDRDR".into()), "Radiant");
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
