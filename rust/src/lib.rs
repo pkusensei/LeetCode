c591fe8e6eb73f64ca69822e2cc716560bafd87a
pkusensei <ZW_08@outlook.com>
2024-10-07 15:52:53 -0400

    658 find k closest ints to x in array

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,21 +1,58 @@
 mod helper;
 mod trie;
 
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn judge_circle(moves: &str) -> bool {
-    moves.len() & 1 == 0
-        && moves.bytes().fold([0, 0], |mut acc, b| {
-            match b {
-                b'U' => acc[0] += 1,
-                b'D' => acc[0] -= 1,
-                b'R' => acc[1] += 1,
-                b'L' => acc[1] -= 1,
-                _ => (),
+pub fn find_closest_elements(arr: &[i32], k: i32, x: i32) -> Vec<i32> {
+    let n = arr.len();
+    let k = k as usize;
+    let pivot = arr.partition_point(|&n| n < x);
+    if pivot == 0 {
+        arr.iter().copied().take(k).collect()
+    } else if pivot == n {
+        arr.iter().rev().copied().take(k).rev().collect()
+    } else {
+        let (mut left, mut right) = (pivot - 1, pivot);
+        let mut heap = BinaryHeap::new();
+        heap.push((Reverse((arr[left].abs_diff(x), arr[left])), left));
+        heap.push((Reverse((arr[right].abs_diff(x), arr[right])), right));
+        let mut res = Vec::with_capacity(k);
+        while res.len() < k {
+            let Some((Reverse((_, num)), idx)) = heap.pop() else {
+                break;
             };
-            acc
-        }) == [0, 0]
+            if idx < pivot {
+                res.insert(0, num);
+                if idx > 0 {
+                    left = idx - 1;
+                    heap.push((Reverse((arr[left].abs_diff(x), arr[left])), left));
+                }
+            } else {
+                res.push(num);
+                if idx < n - 1 {
+                    right = idx + 1;
+                    heap.push((Reverse((arr[right].abs_diff(x), arr[right])), right));
+                }
+            }
+        }
+        res
+    }
+}
+
+fn with_binary_search(arr: &[i32], k: usize, x: i32) -> &[i32] {
+    let (mut left, mut right) = (0, arr.len() - k);
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if x.abs_diff(arr[mid]) <= x.abs_diff(arr[mid + k]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    &arr[left..(left + k)]
 }
 
 #[cfg(test)]
@@ -26,8 +63,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(judge_circle("UD"));
-        debug_assert!(!judge_circle("LL"));
+        debug_assert_eq!(with_binary_search(&[1, 2, 3, 4, 5], 4, 3), [1, 2, 3, 4]);
+        debug_assert_eq!(with_binary_search(&[1, 1, 2, 3, 4, 5], 4, -1), [1, 1, 2, 3]);
     }
 
     #[test]
