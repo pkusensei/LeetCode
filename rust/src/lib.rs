533468f2badf30425472f8f0da3ac460b9e4a7b7
Zx Wang <ZW_08@outlook.com>
2024-10-10 11:11:15 -0400

    685 find redudant edge in directed graph

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,43 +1,51 @@
 mod helper;
 mod trie;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_redundant_connection(edges: &[[i32; 2]]) -> Vec<i32> {
-    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
-    let mut seen = HashSet::new();
+pub fn find_redundant_directed_connection(edges: &[[i32; 2]]) -> Vec<i32> {
+    let mut parents: HashMap<i32, i32> = HashMap::new();
+    let mut cycle = None;
+    let mut two_parents = vec![];
     for edge in edges.iter() {
-        seen.clear();
-        if graph.get(&edge[0]).is_some_and(|v| !v.is_empty())
-            && graph.get(&edge[1]).is_some_and(|v| !v.is_empty())
-            && dfs(&graph, &mut seen, edge[0], edge[1])
-        {
-            return edge.to_vec();
+        if let Some(&p1) = parents.get(&edge[1]) {
+            // two parents
+            two_parents.push([p1, edge[1]]);
+            two_parents.push([edge[0], edge[1]]);
+            if cycle.is_some() {
+                return two_parents[0].to_vec();
+            }
+        } else {
+            parents.insert(edge[1], edge[0]);
         }
-        graph.entry(edge[0]).or_default().push(edge[1]);
-        graph.entry(edge[1]).or_default().push(edge[0]);
-    }
-    edges[0].to_vec() // unreachable!()
-}
-
-fn dfs(graph: &HashMap<i32, Vec<i32>>, seen: &mut HashSet<i32>, from: i32, to: i32) -> bool {
-    if !seen.insert(from) {
-        return false;
-    }
-    if from == to {
-        return true;
-    }
-    if let Some(v) = graph.get(&from) {
-        for &n in v.iter() {
-            if dfs(graph, seen, n, to) {
-                return true;
+        if cycle.is_none() && find_root(&parents, edge[0]).is_none() {
+            cycle = Some(edge);
+            if !two_parents.is_empty() {
+                return two_parents[0].to_vec();
             }
         }
     }
-    false
+    cycle
+        .map(|v| v.to_vec())
+        .or(two_parents.get(1).map(|v| v.to_vec()))
+        .unwrap_or_default()
+}
+
+fn find_root(parents: &HashMap<i32, i32>, node: i32) -> Option<i32> {
+    if !parents.contains_key(&node) {
+        return Some(node); // root has no parent
+    };
+    let mut curr = node;
+    while let Some(&p) = parents.get(&curr) {
+        curr = p;
+        if curr == node {
+            return None; // a cycle
+        }
+    }
+    Some(curr)
 }
 
 #[cfg(test)]
@@ -48,9 +56,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(find_redundant_connection(&[[1, 2], [1, 3], [2, 3]]), [2, 3]);
         debug_assert_eq!(
-            find_redundant_connection(&[[1, 2], [2, 3], [3, 4], [4, 1], [1, 5]]),
+            find_redundant_directed_connection(&[[1, 2], [1, 3], [2, 3]]),
+            [2, 3]
+        );
+        debug_assert_eq!(
+            find_redundant_directed_connection(&[[1, 2], [2, 3], [3, 4], [4, 1], [1, 5]]),
             [4, 1]
         );
     }
