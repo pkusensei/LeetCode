015b5302f4365751aa27b09625bd1daa15719f9e
pkusensei <ZW_08@outlook.com>
2024-10-11 19:42:20 -0400

    695 area of largest island

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,20 +1,40 @@
 mod helper;
 mod trie;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn has_alternating_bits(mut n: i32) -> bool {
-    let mut prev = -1;
-    while n > 0 {
-        let curr = n & 1;
-        if curr == prev {
-            return false;
+pub fn max_area_of_island(grid: &[&[i32]]) -> i32 {
+    let mut res = 0;
+    let mut seen = HashSet::new();
+    for (y, row) in grid.iter().enumerate() {
+        for (x, &num) in row.iter().enumerate() {
+            if num == 1 && !seen.contains(&(x, y)) {
+                res = res.max(bfs(grid, x, y, &mut seen));
+            }
         }
-        prev = curr;
-        n >>= 1;
     }
-    true
+    res
+}
+
+fn bfs(grid: &[&[i32]], x: usize, y: usize, seen: &mut HashSet<(usize, usize)>) -> i32 {
+    let mut res = 0;
+    seen.insert((x, y));
+    let mut queue = VecDeque::from([(x, y)]);
+    while let Some((x, y)) = queue.pop_front() {
+        res += 1;
+        for (nx, ny) in neighbors((x, y)).filter(|&(nx, ny)| {
+            grid.get(ny)
+                .is_some_and(|r| r.get(nx).is_some_and(|&num| num == 1))
+        }) {
+            if seen.insert((nx, ny)) {
+                queue.push_back((nx, ny));
+            }
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -25,15 +45,24 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(has_alternating_bits(5));
-        debug_assert!(!has_alternating_bits(7));
-        debug_assert!(!has_alternating_bits(11));
+        debug_assert_eq!(
+            max_area_of_island(&[
+                &[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                &[0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                &[0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+                &[0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+                &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+            ]),
+            6
+        );
+        debug_assert_eq!(max_area_of_island(&[&[0, 0, 0, 0, 0, 0, 0, 0]]), 0)
     }
 
     #[test]
-    fn test() {
-        debug_assert!(!has_alternating_bits(4));
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
