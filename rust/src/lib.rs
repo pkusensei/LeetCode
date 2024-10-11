95d7eee93da0fddef86fdebbedd70909ca0557f7
pkusensei <ZW_08@outlook.com>
2024-10-11 13:42:17 -0400

    689 max sum of 3 non-overlapping subarrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,33 +1,52 @@
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn knight_probability(n: i32, k: i32, row: i32, col: i32) -> f64 {
-    #[rustfmt::skip]
-    const DELTAS: [[i32; 2]; 8] = [[2, 1],[1, 2],[2, -1],[-1, 2],
-                                   [-2, 1],[1, -2],[-2, -1],[-1, -2],];
-    let mut queue = HashMap::from([([row, col], 1.0)]);
-    let mut res = 1.0;
-    for _ in 0..k {
-        res = 0.0;
-        let mut next = HashMap::new();
-        for (&[r, c], &p) in queue.iter() {
-            for (nr, nc) in DELTAS
-                .map(|d| (r + d[0], c + d[1]))
-                .into_iter()
-                .filter(|(nr, nc)| (0..n).contains(nr) && (0..n).contains(nc))
-            {
-                *next.entry([nr, nc]).or_insert(0.0) += p / 8.0;
-                res += p / 8.0;
+pub fn max_sum_of_three_subarrays(nums: &[i32], k: i32) -> Vec<i32> {
+        let k = k as usize;
+        // let n = nums.len();
+        // let mut sums = Vec::with_capacity(n - k);
+        // let mut temp: i32 = nums[..k].iter().sum();
+        // sums.push(temp);
+        // for i in 1..=n - k {
+        //     temp -= nums[i - 1];
+        //     temp += nums[i + k - 1];
+        //     sums.push(temp);
+        // }
+        let sums: Vec<i32> = nums.windows(k).map(|w| w.iter().sum()).collect();
+        let mut max_i = 0;
+        let mut left_max = Vec::with_capacity(sums.len());
+        for (i, &num) in sums.iter().enumerate() {
+            if num > sums[max_i] {
+                max_i = i;
+            }
+            left_max.push(max_i);
+        }
+
+        max_i = sums.len() - 1;
+        let mut right_max = Vec::with_capacity(sums.len());
+        for (i, &num) in sums.iter().enumerate().rev() {
+            if num >= sums[max_i] {
+                max_i = i;
+            }
+            right_max.push(max_i);
+        }
+        right_max.reverse();
+
+        let mut res = [0; 3];
+        let mut curr = 0;
+        for idx in k..sums.len() - k {
+            let left = sums[left_max[idx - k]];
+            let right = sums[right_max[idx + k]];
+            let temp = sums[idx] + left + right;
+            if temp > curr {
+                res = [left_max[idx - k], idx, right_max[idx + k]];
+                curr = temp
             }
         }
-        queue = next
-    }
-    res
+        res.map(|i| i as i32).to_vec()
 }
 
 #[cfg(test)]
@@ -38,12 +57,23 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(knight_probability(3, 2, 0, 0), 0.06250);
-        debug_assert_eq!(knight_probability(1, 0, 0, 0), 1.00000);
+        debug_assert_eq!(
+            max_sum_of_three_subarrays(&[1, 2, 1, 2, 6, 7, 5, 1], 2),
+            [0, 3, 5]
+        );
+        debug_assert_eq!(
+            max_sum_of_three_subarrays(&[1, 2, 1, 2, 1, 2, 1, 2, 1], 2),
+            [0, 2, 4]
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            max_sum_of_three_subarrays(&[4, 5, 10, 6, 11, 17, 4, 11, 1, 3], 1),
+            [4, 5, 7]
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
