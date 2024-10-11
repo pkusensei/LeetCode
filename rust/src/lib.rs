58dba5a61c8522639d3b959361adf4ace90d5335
pkusensei <ZW_08@outlook.com>
2024-10-11 18:33:57 -0400

    692 top k frequent strs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,38 +1,32 @@
 mod helper;
 mod trie;
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_stickers(stickers: &[&str], target: &str) -> i32 {
-    let n = target.len();
-    // Each letter has 2 states => 2^n
-    let mut dp = vec![-1; 1 << n];
-    dp[0] = 0; // no sticker needed
-    for mask in 0..1 << n {
-        if dp[mask] == -1 {
-            // This mask is not reached in previous loop(s)
-            // i.e not possible to build from stickers
-            continue;
-        }
-        for stick in stickers.iter() {
-            let mut curr = mask;
-            for b1 in stick.bytes() {
-                for (idx, b2) in target.bytes().enumerate() {
-                    if (curr >> idx) & 1 == 0 && b2 == b1 {
-                        curr |= 1 << idx;
-                        // use current b1 and break into next loop
-                        break;
-                    }
-                }
-            }
-            // curr is reachable; record min value
-            if dp[curr] == -1 || dp[curr] > dp[mask] + 1 {
-                dp[curr] = 1 + dp[mask];
-            }
-        }
+pub fn top_k_frequent(words: &[&str], k: i32) -> Vec<String> {
+    let mut heap: BinaryHeap<_> = words
+        .iter()
+        .fold(HashMap::new(), |mut acc, s| {
+            *acc.entry(*s).or_insert(0) += 1;
+            acc
+        })
+        .into_iter()
+        .map(|(s, count)| (count, Reverse(s)))
+        .collect();
+    let mut res = Vec::with_capacity(k as usize);
+    while res.len() < k as usize {
+        let Some((_, Reverse(s))) = heap.pop() else {
+            break;
+        };
+        res.push(s.to_string());
     }
-    *dp.last().unwrap()
+    res
 }
 
 #[cfg(test)]
@@ -43,8 +37,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_stickers(&["with", "example", "science"], "thehat"), 3);
-        debug_assert_eq!(min_stickers(&["notice", "possible"], "basicbasic"), -1);
+        debug_assert_eq!(
+            top_k_frequent(&["i", "love", "leetcode", "i", "love", "coding"], 2),
+            ["i", "love"]
+        );
+        debug_assert_eq!(
+            top_k_frequent(
+                &["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+                4
+            ),
+            ["the", "is", "sunny", "day"]
+        );
     }
 
     #[test]
