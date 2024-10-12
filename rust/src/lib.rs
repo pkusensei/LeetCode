80f30d867467789c232393cace95728e051de50c
pkusensei <ZW_08@outlook.com>
2024-10-11 20:50:26 -0400

    2406 overlapping intervals

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,40 +1,40 @@
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, BinaryHeap},
+};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_area_of_island(grid: &[&[i32]]) -> i32 {
-    let mut res = 0;
-    let mut seen = HashSet::new();
-    for (y, row) in grid.iter().enumerate() {
-        for (x, &num) in row.iter().enumerate() {
-            if num == 1 && !seen.contains(&(x, y)) {
-                res = res.max(bfs(grid, x, y, &mut seen));
-            }
-        }
+pub fn min_groups(intervals: &[[i32; 2]]) -> i32 {
+    let mut map = BTreeMap::new();
+    for v in intervals.iter() {
+        *map.entry(v[0]).or_insert(0) += 1;
+        *map.entry(v[1] + 1).or_insert(0) -= 1;
+    }
+    let (mut res, mut curr) = (0, 0);
+    for v in map.into_values() {
+        curr += v;
+        res = res.max(curr);
     }
     res
 }
 
-fn bfs(grid: &[&[i32]], x: usize, y: usize, seen: &mut HashSet<(usize, usize)>) -> i32 {
-    let mut res = 0;
-    seen.insert((x, y));
-    let mut queue = VecDeque::from([(x, y)]);
-    while let Some((x, y)) = queue.pop_front() {
-        res += 1;
-        for (nx, ny) in neighbors((x, y)).filter(|&(nx, ny)| {
-            grid.get(ny)
-                .is_some_and(|r| r.get(nx).is_some_and(|&num| num == 1))
-        }) {
-            if seen.insert((nx, ny)) {
-                queue.push_back((nx, ny));
-            }
+fn with_pq(intervals: &mut [[i32; 2]]) -> usize {
+    intervals.sort_unstable();
+    let mut heap = BinaryHeap::new();
+    for &[start, end] in intervals.iter() {
+        // Ideally (without overlapping) an end should pop before next start
+        // If it misses this chance, it has an overlap and stays in heap
+        if heap.peek().is_some_and(|&Reverse(soon)| soon < start) {
+            heap.pop();
         }
+        heap.push(Reverse(end));
     }
-    res
+    heap.len()
 }
 
 #[cfg(test)]
@@ -45,20 +45,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            max_area_of_island(&[
-                &[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                &[0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                &[0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-                &[0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
-            ]),
-            6
-        );
-        debug_assert_eq!(max_area_of_island(&[&[0, 0, 0, 0, 0, 0, 0, 0]]), 0)
+        debug_assert_eq!(with_pq(&mut [[5, 10], [6, 8], [1, 5], [2, 3], [1, 10]]), 3);
+        debug_assert_eq!(with_pq(&mut [[1, 3], [5, 6], [8, 10], [11, 13]]), 1);
     }
 
     #[test]
