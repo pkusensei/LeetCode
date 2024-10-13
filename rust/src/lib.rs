1c04f1c94a8400e5be2263adb570b67834b55122
pkusensei <ZW_08@outlook.com>
2024-10-13 18:53:33 -0400

    707 singly linked list

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,28 +5,93 @@ mod trie;
 use helper::*;
 
 #[derive(Debug, Clone)]
-struct MyHashMap {
-    data: Vec<i32>,
+struct MyLinkedList {
+    head: Option<Box<Node>>,
 }
 
-impl MyHashMap {
+impl MyLinkedList {
     fn new() -> Self {
-        Self {
-            data: vec![-1; 1_000_001],
+        Self { head: None }
+    }
+
+    fn get(&self, index: i32) -> i32 {
+        let mut count = 0;
+        let mut curr = self.head.as_ref();
+        while curr.is_some() && count < index {
+            count += 1;
+            curr = curr.and_then(|n| n.next.as_ref());
+        }
+        curr.map(|n| n.val).unwrap_or(-1)
+    }
+
+    fn add_at_head(&mut self, val: i32) {
+        let mut node = Node { val, next: None };
+        node.next = self.head.take();
+        self.head = Some(Box::new(node))
+    }
+
+    fn add_at_tail(&mut self, val: i32) {
+        let node = Node { val, next: None };
+        match self.head.as_mut() {
+            None => self.head = Some(Box::new(node)),
+            Some(mut curr) => {
+                while let Some(ref mut next) = curr.next {
+                    curr = next
+                }
+                curr.next = Some(Box::new(node));
+            }
         }
     }
 
-    fn put(&mut self, key: i32, value: i32) {
-        self.data[key as usize] = value;
+    fn add_at_index(&mut self, mut index: i32, val: i32) {
+        if index <= 0 {
+            self.add_at_head(val);
+        }
+
+        let mut curr = &mut self.head;
+        let mut count = 0;
+        while let Some(ref mut n) = curr {
+            if count + 1 == index {
+                let node = Node {
+                    val,
+                    next: n.next.take(),
+                };
+                n.next = Some(Box::new(node));
+                return;
+            }
+            curr = &mut n.next;
+            count += 1;
+        }
     }
 
-    fn remove(&mut self, key: i32) {
-        self.data[key as usize] = -1;
+    fn delete_at_index(&mut self, mut index: i32) {
+        let Some(ref mut head) = self.head else {
+            return;
+        };
+        if index == 0 {
+            let n = head.next.take();
+            self.head = n;
+            return;
+        }
+        let mut curr = &mut self.head;
+        let mut count = 0;
+        while let Some(ref mut n) = curr {
+            if count + 1 == index {
+                let Some(mut del) = n.next.take() else {
+                    return;
+                };
+                n.next = del.next.take();
+            }
+            curr = &mut n.next;
+            count += 1;
+        }
     }
+}
 
-    fn get(&self, key: i32) -> i32 {
-        self.data[key as usize]
-    }
+#[derive(Debug, Clone)]
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
 }
 
 #[cfg(test)]
@@ -37,15 +102,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut m = MyHashMap::new();
-        m.put(1, 1); // The map is now [[1,1]]
-        m.put(2, 2); // The map is now [[1,1], [2,2]]
-        debug_assert_eq!(m.get(1), 1); // return 1, The map is now [[1,1], [2,2]]
-        debug_assert_eq!(m.get(3), -1); // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
-        m.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
-        debug_assert_eq!(m.get(2), 1); // return 1, The map is now [[1,1], [2,1]]
-        m.remove(2); // remove the mapping for 2, The map is now [[1,1]]
-        debug_assert_eq!(m.get(2), -1); // return -1 (i.e., not found), The map is now [[1,1]] // return False, (already removed)
+        let mut myLinkedList = MyLinkedList::new();
+        myLinkedList.add_at_head(1);
+        myLinkedList.add_at_tail(3);
+        myLinkedList.add_at_index(1, 2); // linked list becomes 1->2->3
+        debug_assert_eq!(myLinkedList.get(1), 2); // return 2
+        myLinkedList.delete_at_index(1); // now the linked list is 1->3
+        debug_assert_eq!(myLinkedList.get(1), 3); // return 3
     }
 
     #[test]
