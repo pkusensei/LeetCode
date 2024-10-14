c8ed21be5fd3a68331c3bf636e2f03257c4ef2bc
pkusensei <ZW_08@outlook.com>
2024-10-14 11:13:37 -0400

    714 buy and sell with a fee

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,50 +4,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_subarray_product_less_than_k(nums: &[i32], k: i32) -> i32 {
-    if k <= 1 {
-        return 0;
+pub fn max_profit(prices: &[i32], fee: i32) -> i32 {
+    // Greedy; for every num there are two states
+    // Do something => buy or sell
+    // Hold => do nothing
+    let mut buy = -prices[0];
+    let mut sell = 0;
+    for &num in prices.iter().skip(1) {
+        let temp = buy;
+        buy = buy.max(sell - num);
+        sell = sell.max(temp + num - fee);
     }
-    let (mut left, mut res) = (0, 0);
-    let mut curr = 1;
-    for (right, num) in nums.iter().enumerate() {
-        curr *= num;
-        while curr >= k {
-            curr /= nums[left];
-            left += 1;
-        }
-        res += right - left + 1;
-    }
-    res as _
-}
-
-fn with_log_binary_search(nums: &[i32], k: i32) -> i32 {
-    if k == 0 {
-        return 0;
-    }
-    let log_k = f64::from(k).ln();
-    let n = 1 + nums.len();
-    let mut prefix = Vec::with_capacity(n);
-    prefix.push(0.0); // ln(1)
-    for &num in nums.iter() {
-        prefix.push(f64::from(num).ln() + *prefix.last().unwrap());
-    }
-    let mut res = 0;
-    for idx in 0..n {
-        let (mut left, mut right) = (idx + 1, n);
-        while left < right {
-            let mid = left + (right - left) / 2;
-            // want to find mid such that product of [idx..mid] is k
-            // i.e sum of ln([idx])..ln([mid]) == ln(k)
-            if prefix[mid] - prefix[idx] < log_k - 1_e-9 {
-                left = 1 + mid;
-            } else {
-                right = mid;
-            }
-        }
-        res += left - idx - 1;
-    }
-    res as _
+    sell.max(buy)
 }
 
 #[cfg(test)]
@@ -58,8 +26,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(with_log_binary_search(&[10, 5, 2, 6], 100), 8);
-        debug_assert_eq!(with_log_binary_search(&[1, 2, 3], 0), 0);
+        debug_assert_eq!(max_profit(&[1, 3, 2, 8, 4, 9], 2), 8);
+        debug_assert_eq!(max_profit(&[1, 3, 7, 5, 10, 3], 3), 6);
     }
 
     #[test]
