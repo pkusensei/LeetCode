4801cd4481d32a45888e65f9160e08ccd2c8d7e6
Zx Wang <ZW_08@outlook.com>
2024-10-15 10:33:44 -0400

    721 DSU union find

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,16 +5,81 @@ namespace Solution;
 
 public class Solution
 {
-    public int Search(int[] nums, int target)
+    public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts)
     {
-        var (left, right) = (0, nums.Length - 1);
-        while (left <= right)
+        DSU dsu = new(accounts.Count);
+
+        var groups = new Dictionary<string, int>();
+        foreach (var (idx, acc) in accounts.Select((acc, i) => (i, acc)))
         {
-            var mid = left + (right - left) / 2;
-            if (nums[mid] == target) { return mid; }
-            else if (nums[mid] < target) { left = mid + 1; }
-            else { right = mid - 1; }
+            var size = acc.Count;
+            foreach (var em in acc.Skip(1))
+            {
+                if (groups.TryGetValue(em, out var i))
+                {
+                    dsu.UnionBySize(idx, i);
+                }
+                else
+                {
+                    groups.Add(em, idx);
+                }
+            }
         }
-        return -1;
+
+        var components = new Dictionary<int, List<string>>();
+        foreach (var (em, group) in groups)
+        {
+            var rep = dsu.FindRepr(group);
+            if (!components.TryGetValue(rep, out List<string> value))
+            {
+                value = ([]);
+                components.Add(rep, value);
+            }
+            value.Add(em);
+        }
+
+        List<IList<string>> res = [];
+        foreach (var (group, comp) in components)
+        {
+            comp.Sort(StringComparer.Ordinal);
+            comp.Insert(0, accounts[group][0]);
+            res.Add(comp);
+        }
+        return res;
+    }
+}
+
+class DSU
+{
+    public int[] Reps { get; private set; }
+    public int[] Size { get; private set; }
+    public DSU(int n)
+    {
+        Reps = Enumerable.Range(0, n).ToArray();
+        Size = Reps[..];
+    }
+
+    public int FindRepr(int x)
+    {
+        if (x == Reps[x]) { return x; }
+        Reps[x] = FindRepr(Reps[x]); // compression
+        return Reps[x];
+    }
+
+    public void UnionBySize(int a, int b)
+    {
+        var (rep_a, rep_b) = (FindRepr(a), FindRepr(b));
+        if (rep_a == rep_b) { return; }
+
+        if (Size[rep_a] >= Size[rep_b])
+        {
+            Size[rep_a] += Size[rep_b];
+            Reps[rep_b] = Reps[rep_a];
+        }
+        else
+        {
+            Size[rep_b] += Size[rep_a];
+            Reps[rep_a] = Reps[rep_b];
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -13,18 +13,18 @@ public class UnitTest
     public void TestMethod1()
     {
         // var a = TreeNode.Make([4, 2, 7, 1, 3]);
-        var b = sol.Search([-1, 0, 3, 5, 9, 12], target: 9);
-        var c = 4;
-        Assert.AreEqual(c, b);
+        var b = sol.AccountsMerge([["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["John", "johnsmith@mail.com", "john00@mail.com"], ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]]);
+        List<List<string>> c = [["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"], ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]];
+        Assert.AreEqual(c.Print(), b.Print());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
         // var a = TreeNode.Make([40, 20, 60, 10, 30, 50, 70]);
-        var b = sol.Search([-1, 0, 3, 5, 9, 12], target: 2);
-        var c = -1;
-        Assert.AreEqual(c, b);
+        var b = sol.AccountsMerge([["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"], ["Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"], ["Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"], ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"], ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"]]);
+        List<List<string>> c = [["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"], ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"], ["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"], ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"], ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"]];
+        Assert.AreEqual(c.Print(), b.Print());
     }
 
     [TestMethod]
