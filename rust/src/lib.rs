d114e3919e79e4a1e9ba03369625375e0498be7d
Zx Wang <ZW_08@outlook.com>
2024-10-15 12:23:52 -0400

    724 index in array where left and right sums equal

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,66 +4,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn remove_comments(source: &[&str]) -> Vec<String> {
-    let mut res = vec![];
-    let mut in_block: Option<String> = None;
-    for line in source.iter() {
-        let mut s = *line;
-        while !s.is_empty() {
-            if let Some(ref mut r) = in_block {
-                let Some(end) = s.find(r#"*/"#) else {
-                    break;
-                };
-                s = &s[2 + end..];
-                let line = s.find(r#"//"#);
-                let block = s.find(r#"/*"#);
-                match (line, block) {
-                    (Some(a), None) => {
-                        r.push_str(&s[..a]);
-                        res.push(in_block.take().unwrap_or_default());
-                        break;
-                    }
-                    (Some(a), Some(b)) if a < b => {
-                        r.push_str(&s[..a]);
-                        res.push(in_block.take().unwrap_or_default());
-                        break;
-                    }
-                    (None, None) => {
-                        r.push_str(s);
-                        res.push(in_block.take().unwrap_or_default());
-                        break;
-                    }
-                    (_, Some(a)) => {
-                        r.push_str(&s[..a]);
-                        s = &s[2 + a..];
-                    }
-                }
-            } else {
-                let line = s.find(r#"//"#);
-                let block = s.find(r#"/*"#);
-                match (line, block) {
-                    (Some(a), None) => {
-                        res.push(s[..a].to_string());
-                        break;
-                    }
-                    (Some(a), Some(b)) if a < b => {
-                        res.push(s[..a].to_string());
-                        break;
-                    }
-                    (None, None) => {
-                        res.push(s.to_string());
-                        break;
-                    }
-                    (_, Some(a)) => {
-                        let r = s[..a].to_string();
-                        in_block = Some(r);
-                        s = &s[2 + a..];
-                    }
-                }
-            }
+pub fn pivot_index(nums: &[i32]) -> i32 {
+    let sum: i32 = nums.iter().sum();
+    let mut left = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        if sum - num - left == left {
+            return idx as _;
         }
+        left += num
     }
-    res.into_iter().filter(|s| !s.is_empty()).collect()
+    -1
+}
+
+fn with_prefix(nums: &[i32]) -> i32 {
+    let prefix = nums.iter().scan(0, |sum, num| {
+        *sum += num;
+        Some(*sum)
+    });
+    let suffix = nums.iter().rev().scan(0, |sum, num| {
+        *sum += num;
+        Some(*sum)
+    });
+    let (prefix, suffix) = (prefix.collect::<Vec<_>>(), suffix.collect::<Vec<_>>());
+    // dbg!(prefix);
+    // dbg!(suffix);
+    prefix
+        .into_iter()
+        .zip(suffix.into_iter().rev())
+        .position(|(a, b)| a == b)
+        .map(|i| i as i32)
+        .unwrap_or(-1)
 }
 
 #[cfg(test)]
@@ -74,26 +44,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            remove_comments(&[
-                "/*Test program */",
-                "int main()",
-                "{ ",
-                "  // variable declaration ",
-                "int a, b, c;",
-                "/* This is a test",
-                "   multiline  ",
-                "   comment for ",
-                "   testing */",
-                "a = b + c;",
-                "}"
-            ]),
-            ["int main()", "{ ", "  ", "int a, b, c;", "a = b + c;", "}"]
-        );
-        debug_assert_eq!(
-            remove_comments(&["a/*comment", "line", "more_comment*/b"]),
-            ["ab"]
-        );
+        debug_assert_eq!(with_prefix(&[1, 7, 3, 6, 5, 6]), 3);
+        debug_assert_eq!(with_prefix(&[1, 2, 3]), -1);
+        debug_assert_eq!(with_prefix(&[2, 1, -1]), 0);
     }
 
     #[test]
