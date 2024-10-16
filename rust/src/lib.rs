dfb91b50b240e88e449df4585526b5a3265fdef7
pkusensei <ZW_08@outlook.com>
2024-10-16 18:47:51 -0400

    740 delete and earn; house robber wit preprocessing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,20 +1,30 @@
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn daily_temperatures(temperatures: &[i32]) -> Vec<i32> {
-    let mut stack: Vec<(usize, i32)> = vec![];
-    let mut res = vec![0; temperatures.len()];
-    for (idx, &num) in temperatures.iter().enumerate() {
-        while stack.last().is_some_and(|&(_, n)| n < num) {
-            let Some((i, _)) = stack.pop() else {
-                break;
-            };
-            res[i] = (idx - i) as i32;
-        }
-        stack.push((idx, num));
+pub fn delete_and_earn(nums: &mut [i32]) -> i32 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    nums.sort_unstable();
+    let (min, max) = (nums[0], *nums.last().unwrap());
+    let map: HashMap<_, _> = nums
+        .chunk_by(|a, b| a == b)
+        .map(|ch| (ch[0], ch.iter().sum()))
+        .collect();
+
+    // and house robber
+    let mut prev = 0;
+    let mut res = 0;
+    for num in min..=max {
+        let val = map.get(&num).copied().unwrap_or(0);
+        let temp = res;
+        res = res.max(prev + val);
+        prev = temp;
     }
     res
 }
@@ -27,12 +37,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            daily_temperatures(&[73, 74, 75, 71, 69, 72, 76, 73]),
-            [1, 1, 4, 2, 1, 1, 0, 0]
-        );
-        debug_assert_eq!(daily_temperatures(&[30, 40, 50, 60]), [1, 1, 1, 0]);
-        debug_assert_eq!(daily_temperatures(&[30, 60, 90]), [1, 1, 0]);
+        debug_assert_eq!(delete_and_earn(&mut [3, 4, 2]), 6);
+        debug_assert_eq!(delete_and_earn(&mut [2, 2, 3, 3, 3, 4]), 9);
     }
 
     #[test]
