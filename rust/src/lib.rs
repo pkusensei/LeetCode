d10434dc3fe2252b4a283337e64ce186165ee74c
Zx Wang <ZW_08@outlook.com>
2024-10-17 11:37:42 -0400

    744 binary search to find bigger char

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,42 +1,12 @@
 mod helper;
 mod trie;
 
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap},
-};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn network_delay_time(times: &[[i32; 3]], n: i32, k: i32) -> i32 {
-    let graph: HashMap<i32, Vec<[i32; 2]>> = times.iter().fold(HashMap::new(), |mut acc, v| {
-        acc.entry(v[0]).or_default().push([v[1], v[2]]);
-        acc
-    });
-    let Some(next) = graph.get(&k) else { return -1 };
-    let mut nodes = HashMap::new();
-    let mut heap = BinaryHeap::from([(Reverse(0), k)]);
-    for v in next {
-        heap.push((Reverse(v[1]), v[0]));
-    }
-    while let Some((Reverse(dist), node)) = heap.pop() {
-        if nodes.get(&node).is_some_and(|&v| v <= dist) {
-            continue;
-        }
-        nodes.insert(node, dist);
-        let Some(next) = graph.get(&node) else {
-            continue;
-        };
-        for v in next {
-            heap.push((Reverse(v[1] + dist), v[0]));
-        }
-    }
-    if nodes.len() == n as usize {
-        nodes.into_values().max().unwrap_or(-1)
-    } else {
-        -1
-    }
+pub fn next_greatest_letter(letters: &[char], target: char) -> char {
+    let i = letters.partition_point(|&ch| ch <= target);
+    letters.get(i).copied().unwrap_or(letters[0])
 }
 
 #[cfg(test)]
@@ -47,12 +17,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            network_delay_time(&[[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2),
-            2
-        );
-        debug_assert_eq!(network_delay_time(&[[1, 2, 1]], 2, 1), 1);
-        debug_assert_eq!(network_delay_time(&[[1, 2, 1]], 2, 2), -1)
+        debug_assert_eq!(next_greatest_letter(&['c', 'f', 'j'], 'a'), 'c');
+        debug_assert_eq!(next_greatest_letter(&['c', 'f', 'j'], 'c'), 'f');
+        debug_assert_eq!(next_greatest_letter(&['x', 'x', 'y', 'y'], 'z'), 'x');
     }
 
     #[test]
