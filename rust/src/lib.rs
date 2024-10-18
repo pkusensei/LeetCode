4ca8d79038f62e8737e0d6c5946e9d1bb743c536
pkusensei <ZW_08@outlook.com>
2024-10-18 13:23:04 -0400

    756 backtrack, priority heap, and bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,33 +1,63 @@
 mod helper;
 mod trie;
 
+use std::collections::{BinaryHeap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn reach_number(target: i32) -> i32 {
-    let target = target.abs();
-    let mut curr = 0;
-    let mut num = 0;
-    while curr < target {
-        num += 1;
-        curr += num;
+pub fn pyramid_transition(bottom: String, allowed: &[&str]) -> bool {
+    let dict: HashMap<[u8; 2], Vec<u8>> = allowed.iter().fold(HashMap::new(), |mut acc, s| {
+        let s = s.as_bytes();
+        acc.entry([s[0], s[1]]).or_default().push(s[2]);
+        acc
+    });
+    let start = bottom.into_bytes();
+    let mut heap = BinaryHeap::from([Line(start)]);
+    while let Some(Line(prev)) = heap.pop() {
+        if prev.len() == 1 {
+            return true;
+        }
+        let mut res = vec![];
+        build(&prev, &dict, &mut vec![], &mut res);
+        heap.extend(res.into_iter().map(Line));
     }
-    let delta = curr - target;
-    if delta & 1 == 0 {
-        // delta == 0 => num
-        // delta is even => find number in [1..=num] and flip its sign
-        num
-    } else if num & 1 == 0 {
-        // num is even, but delta is odd
-        // (num+1) is odd
-        // Find an odd number in [1..=1+num] to offset delta
-        num + 1
-    } else {
-        // delta is odd, num is odd
-        // curr and target are both odd
-        // Flipping any odd number instead makes curr even
-        // Has to flip _two_ numbers
-        num + 2
+    false
+}
+
+fn build(
+    prev: &[u8],
+    dict: &HashMap<[u8; 2], Vec<u8>>,
+    curr: &mut Vec<u8>,
+    res: &mut Vec<Vec<u8>>,
+) {
+    let Some((window, _)) = prev.split_at_checked(2) else {
+        res.push(curr.clone());
+        return;
+    };
+    let Some(bytes) = dict.get(window) else {
+        return;
+    };
+    for &b in bytes.iter() {
+        curr.push(b);
+        build(&prev[1..], dict, curr, res);
+        curr.pop();
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Line(Vec<u8>);
+
+impl PartialOrd for Line {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// Pop shorter first
+impl Ord for Line {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.0.len().cmp(&self.0.len())
     }
 }
 
@@ -39,8 +69,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(reach_number(2), 3);
-        debug_assert_eq!(reach_number(3), 2);
+        debug_assert!(pyramid_transition(
+            "BCD".into(),
+            &["BCC", "CDE", "CEA", "FFF"]
+        ));
+        debug_assert!(!pyramid_transition(
+            "AAAA".into(),
+            &["AAB", "AAC", "BCD", "BBE", "DEF"]
+        ));
     }
 
     #[test]
