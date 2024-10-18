54cfe9f4d85fdbd28a08c4450bdafd02c8f94cb5
pkusensei <ZW_08@outlook.com>
2024-10-18 08:38:28 -0400

    752 bfs with dead paths

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,37 +1,45 @@
 mod helper;
 mod trie;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_max_or_subsets(nums: &[i32]) -> i32 {
-    let val = nums.iter().fold(0, |acc, n| acc | n);
-    let space = 1 << nums.len(); // predetermined choise space 2**n
-    let mut res = 0;
-    for mask in 0..space {
-        // Each mask is a preset selection of numbers
-        let mut temp = 0;
-        for (i, num) in nums.iter().enumerate() {
-            if (mask >> i) & 1 == 1 {
-                // current num is picked
-                temp |= num;
-                if temp == val {
-                    res += 1;
-                    break; // val is already the max temp could reach
-                }
+pub fn open_lock(deadends: &[&str], target: &str) -> i32 {
+    let ends: HashSet<_> = deadends.iter().map(|s| s.as_bytes()).collect();
+    let target = target.as_bytes();
+    let start = [b'0'; 4];
+    let mut queue = VecDeque::from([(start, 0)]);
+    let mut seen = HashSet::from([start]);
+    while let Some((curr, dist)) = queue.pop_front() {
+        if ends.contains(curr.as_ref()) {
+            continue;
+        }
+        if curr == target {
+            return dist;
+        }
+        for next in step(curr) {
+            if seen.insert(next) {
+                queue.push_back((next, 1 + dist));
             }
         }
     }
-    res
-    // backtrack(nums, val, 0)
+    -1
 }
 
-// smh this is faster
-fn backtrack(nums: &[i32], val: i32, curr: i32) -> i32 {
-    match nums {
-        [] => (curr == val).into(),
-        [head, tail @ ..] => backtrack(tail, val, curr) + backtrack(tail, val, curr | head),
-    }
+fn step(start: [u8; 4]) -> impl Iterator<Item = [u8; 4]> {
+    (0..8).map(move |n| {
+        let mut curr = start;
+        let idx = n / 2;
+        let b = curr[idx] - b'0';
+        if n & 1 == 0 {
+            curr[idx] = b'0' + b.checked_sub(1).unwrap_or(9);
+        } else {
+            curr[idx] = b'0' + (b + 1) % 10;
+        }
+        curr
+    })
 }
 
 #[cfg(test)]
@@ -42,9 +50,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(count_max_or_subsets(&[3, 1]), 2);
-        debug_assert_eq!(count_max_or_subsets(&[2, 2, 2]), 7);
-        debug_assert_eq!(count_max_or_subsets(&[3, 2, 1, 5]), 6);
+        // debug_assert_eq!(
+        //     open_lock(&["0201", "0101", "0102", "1212", "2002"], "0202"),
+        //     6
+        // );
+        debug_assert_eq!(open_lock(&["8888"], "0009"), 1);
+        debug_assert_eq!(
+            open_lock(
+                &["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"],
+                "8888"
+            ),
+            -1
+        );
     }
 
     #[test]
