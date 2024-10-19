5bb473684b0d451babcf7477236753397ed2a8a5
pkusensei <ZW_08@outlook.com>
2024-10-19 13:49:30 -0400

    767 same as 1405 but only check last entered char

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,22 +1,40 @@
 mod helper;
 mod trie;
 
+use std::collections::{BinaryHeap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_toeplitz_matrix(matrix: &[&[i32]]) -> bool {
-    for (y, row) in matrix.iter().enumerate() {
-        for (x, &n1) in row.iter().enumerate() {
-            if matrix
-                .get(1 + y)
-                .and_then(|r| r.get(1 + x))
-                .is_some_and(|&n2| n2 != n1)
-            {
-                return false;
+pub fn reorganize_string(s: &str) -> String {
+    let mut heap: BinaryHeap<_> = s
+        .bytes()
+        .fold(HashMap::new(), |mut acc, b| {
+            *acc.entry(b).or_insert(0) += 1;
+            acc
+        })
+        .into_iter()
+        .map(|(b, count)| (count, b))
+        .collect();
+    let mut res = vec![];
+    while let Some((count, b)) = heap.pop() {
+        if res.last().is_some_and(|&v| v == b) {
+            let Some((c, b2)) = heap.pop() else {
+                return String::new(); // not able to deplete all bytes
+            };
+            res.push(b2);
+            if c > 1 {
+                heap.push((c - 1, b2));
+            }
+            heap.push((count, b));
+        } else {
+            res.push(b);
+            if count > 1 {
+                heap.push((count - 1, b));
             }
         }
     }
-    true
+    String::from_utf8(res).unwrap()
 }
 
 #[cfg(test)]
@@ -27,12 +45,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(is_toeplitz_matrix(&[
-            &[1, 2, 3, 4],
-            &[5, 1, 2, 3],
-            &[9, 5, 1, 2]
-        ]));
-        debug_assert!(!is_toeplitz_matrix(&[&[1, 2], &[2, 2]]));
+        debug_assert_eq!(reorganize_string("aab"), "aba");
+        debug_assert_eq!(reorganize_string("aaab"), "");
     }
 
     #[test]
