e227d85f875faaefeb1c6e3e60a817580c55dd56
pkusensei <ZW_08@outlook.com>
2024-10-19 12:06:08 -0400

    1545 it really is confusing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,39 +1,73 @@
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn partition_labels(s: &str) -> Vec<i32> {
-    let (s, n) = (s.as_bytes(), s.len());
-    let lasts = s
-        .iter()
-        .enumerate()
-        .rev()
-        .fold(HashMap::new(), |mut acc, (i, &b)| {
-            acc.entry(b).or_insert(i);
-            acc
-        });
-    let mut res = vec![];
-    let mut left = 0;
-    while left < n {
-        let mut last = lasts[&s[left]];
-        for (right, b) in s.iter().enumerate().skip(left) {
-            if right >= last {
-                res.push((right - left + 1) as i32);
-                left = 1 + right;
-                break;
+pub fn find_kth_bit(n: i32, k: i32) -> char {
+    let mut curr = vec![0u8];
+    for _ in 0..n {
+        let mut inv = curr.clone();
+        for v in inv.iter_mut() {
+            *v = 1 - *v;
+        }
+        curr = curr
+            .into_iter()
+            .chain(std::iter::once(1))
+            .chain(inv.into_iter().rev())
+            .collect();
+    }
+    (b'0' + curr[k as usize - 1]).into()
+}
+
+fn recurse(n: i32, k: i32) -> char {
+    if n == 1 {
+        return '0';
+    }
+    let len = 1 << n;
+    match k.cmp(&(len / 2)) {
+        std::cmp::Ordering::Less => recurse(n - 1, k),
+        std::cmp::Ordering::Equal => '1',
+        std::cmp::Ordering::Greater => {
+            if '0' == find_kth_bit(n - 1, len - k) {
+                '1'
             } else {
-                let temp = lasts[b];
-                if temp > last {
-                    last = temp
-                }
+                '0'
             }
         }
     }
-    res
+}
+
+fn divide_and_conquer(n: i32, mut k: i32) -> char {
+    let mut invert = 0;
+    let mut len = 1 << n - 1;
+    while k > 1 {
+        if k == len / 2 + 1 {
+            return if invert & 1 == 0 { '0' } else { '1' };
+        }
+        if k > len / 2 + 1 {
+            k = len + 1 - k;
+            invert += 1;
+        }
+        len /= 2;
+    }
+    if invert & 1 == 0 {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn bit_magic(_n: i32, k: i32) -> char {
+    let pos_in_section = k & -k;
+    let in_inverted_sec = (((k / pos_in_section) >> 1) & 1) == 1;
+    let original_bit_is_one = (k & 1) == 0;
+    match (in_inverted_sec, original_bit_is_one) {
+        (true, true) => '0',
+        (true, false) => '1',
+        (false, true) => '1',
+        (false, false) => '0',
+    }
 }
 
 #[cfg(test)]
@@ -44,8 +78,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(partition_labels("ababcbacadefegdehijhklij"), [9, 7, 8]);
-        debug_assert_eq!(partition_labels("eccbbbbdec"), [10]);
+        debug_assert_eq!(bit_magic(3, 1), '0');
+        debug_assert_eq!(bit_magic(4, 11), '1');
     }
 
     #[test]
