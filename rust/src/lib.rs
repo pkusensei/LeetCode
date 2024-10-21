c8c25c2e37102f6af6f2c9a6e5c21c8c8c0740c1
pkusensei <ZW_08@outlook.com>
2024-10-21 14:29:40 -0400

    787 dijkstra with length limit

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,93 +1,37 @@
 mod helper;
 mod trie;
 
-use core::f64;
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn kth_smallest_prime_fraction(arr: &[i32], k: i32) -> Vec<i32> {
-    let mut heap: BinaryHeap<_> = arr
-        .iter()
-        .skip(1)
-        .map(|&n| (Reverse(Fraction { num: 1, den: n }), 0usize))
-        .collect();
-    let mut res = [0; 2];
-    for _ in 0..k {
-        let Some((Reverse(curr), idx)) = heap.pop() else {
-            break;
-        };
-        res = [curr.num, curr.den];
-        let Some(&next_num) = arr.get(1 + idx) else {
-            continue;
-        };
-        if next_num >= curr.den {
+pub fn find_cheapest_price(n: i32, flights: &[[i32; 3]], src: i32, dst: i32, k: i32) -> i32 {
+    let graph: HashMap<i32, Vec<[i32; 2]>> = flights.iter().fold(HashMap::new(), |mut acc, v| {
+        acc.entry(v[0]).or_default().push([v[1], v[2]]);
+        acc
+    });
+    let mut heap = BinaryHeap::from([(Reverse((0, 0)), src)]);
+    let mut stops = vec![i32::MAX; n as usize];
+    while let Some((Reverse((cost, stop)), curr)) = heap.pop() {
+        if stop > 1 + k || stop > stops[curr as usize] {
             continue;
         }
-        heap.push((
-            Reverse(Fraction {
-                num: next_num,
-                ..curr
-            }),
-            1 + idx,
-        ));
-    }
-    res.to_vec()
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Fraction {
-    num: i32,
-    den: i32,
-}
-
-impl PartialOrd for Fraction {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-// a/b - c/d == (ad - bc)/bd
-impl Ord for Fraction {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.num * other.den).cmp(&(self.den * other.num))
-    }
-}
-
-fn with_binary_search(arr: &[i32], k: i32) -> [i32; 2] {
-    let n = arr.len();
-    let (mut left, mut right) = (0.0, 1.0);
-    while left < right {
-        let mid = left + (right - left) / 2.0;
-        let mut max_fraction = 0.0;
-        let mut total_smaller_frac = 0;
-        let (mut num_idx, mut den_idx) = (0, 0);
-        let mut i2 = 1; // denominator
-        for i1 in 0..n - 1 {
-            while i2 < n && f64::from(arr[i1]) >= mid * f64::from(arr[i2]) {
-                i2 += 1; // this is the counter of bigger fractions
-            }
-            total_smaller_frac += n - i2; // hence this n-i2
-            if i2 == n {
-                // with current numerator, every fraction is bigger
-                // no need to do another bigger numerator
-                break;
-            }
-            let fraction = f64::from(arr[i1]) / f64::from(arr[i2]);
-            if fraction > max_fraction {
-                num_idx = i1;
-                den_idx = i2;
-                max_fraction = fraction;
-            }
+        stops[curr as usize] = stop;
+        if dst == curr {
+            return cost;
         }
-        match total_smaller_frac.cmp(&(k as usize)) {
-            std::cmp::Ordering::Less => right = mid,
-            std::cmp::Ordering::Equal => return [arr[num_idx], arr[den_idx]],
-            std::cmp::Ordering::Greater => left = mid,
+        let Some(nodes) = graph.get(&curr) else {
+            continue;
+        };
+        for next in nodes.iter() {
+            heap.push((Reverse((cost + next[1], 1 + stop)), next[0]));
         }
     }
-    [0; 2]
+    -1
 }
 
 #[cfg(test)]
@@ -98,12 +42,56 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(with_binary_search(&[1, 2, 3, 5], 3), [2, 5]);
-        debug_assert_eq!(with_binary_search(&[1, 7], 1), [1, 7]);
+        debug_assert_eq!(
+            find_cheapest_price(
+                4,
+                &[
+                    [0, 1, 100],
+                    [1, 2, 100],
+                    [2, 0, 100],
+                    [1, 3, 600],
+                    [2, 3, 200]
+                ],
+                0,
+                3,
+                1
+            ),
+            700
+        );
+        debug_assert_eq!(
+            find_cheapest_price(3, &[[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 1),
+            200
+        );
+        debug_assert_eq!(
+            find_cheapest_price(3, &[[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 0),
+            500
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            find_cheapest_price(4, &[[0, 1, 1], [0, 2, 5], [1, 2, 1], [2, 3, 1]], 0, 3, 1),
+            6
+        );
+        debug_assert_eq!(
+            find_cheapest_price(
+                4,
+                &[
+                    [0, 3, 59],
+                    [2, 0, 83],
+                    [2, 3, 32],
+                    [0, 2, 97],
+                    [3, 1, 16],
+                    [1, 3, 16]
+                ],
+                3,
+                0,
+                3
+            ),
+            -1
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
