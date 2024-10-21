111162a1805566cbfce3753ba34a70b6e22f4839
pkusensei <ZW_08@outlook.com>
2024-10-21 11:37:32 -0400

    785 bipartite graph

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,22 +6,66 @@ use std::collections::HashSet;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn letter_case_permutation(s: &str) -> Vec<String> {
-    let n = s.len();
-    let total = 1 << n;
-    let mut res = HashSet::new();
-    for mask in 0..total {
-        let mut curr = vec![];
-        for (i, b) in s.bytes().enumerate() {
-            if (mask >> i) & 1 == 1 {
-                curr.push(b.to_ascii_uppercase());
-            } else {
-                curr.push(b.to_ascii_lowercase());
+pub fn is_bipartite(graph: &[&[i32]]) -> bool {
+    let n = graph.len();
+    let (mut set1, mut set2) = (HashSet::new(), HashSet::new());
+    let mut seen = vec![false; n];
+    for n in 0..n {
+        if !seen[n] && !dfs(graph, n as i32, &mut set1, &mut set2, &mut seen) {
+            return false;
+        }
+    }
+    true
+}
+
+fn dfs(
+    graph: &[&[i32]],
+    curr: i32,
+    set1: &mut HashSet<i32>,
+    set2: &mut HashSet<i32>,
+    seen: &mut [bool],
+) -> bool {
+    if set2.contains(&curr) {
+        return false;
+    }
+    set1.insert(curr);
+    seen[curr as usize] = true;
+    for &n in graph[curr as usize].iter() {
+        if seen[n as usize] && set1.contains(&n) {
+            return false;
+        }
+        // switch affliation
+        if !seen[n as usize] && !dfs(graph, n, set2, set1, seen) {
+            return false;
+        }
+    }
+    true
+}
+
+fn bfs(graph: &[&[i32]]) -> bool {
+    let n = graph.len();
+    let mut colors = vec![-1; n];
+    for root in 0..n {
+        if colors[root] > -1 {
+            continue;
+        }
+        let mut queue = std::collections::VecDeque::from([root]);
+        colors[root] = 1;
+        while let Some(node) = queue.pop_front() {
+            let curr = colors[node];
+            for &next in graph[node].iter() {
+                let v = &mut colors[next as usize];
+                if *v == curr {
+                    return false;
+                }
+                if *v == -1 {
+                    *v = 1 - curr;
+                    queue.push_back(next as usize);
+                }
             }
         }
-        res.insert(String::from_utf8(curr).unwrap());
     }
-    res.into_iter().collect()
+    true
 }
 
 #[cfg(test)]
@@ -32,15 +76,26 @@ mod tests {
 
     #[test]
     fn basics() {
-        sort_eq(
-            letter_case_permutation("a1b2"),
-            ["a1b2", "a1B2", "A1b2", "A1B2"],
-        );
-        sort_eq(letter_case_permutation("3z4"), ["3z4", "3Z4"]);
+        debug_assert!(!bfs(&[&[1, 2, 3], &[0, 2], &[0, 1, 3], &[0, 2]]));
+        debug_assert!(bfs(&[&[1, 3], &[0, 2], &[1, 3], &[0, 2]]));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert!(bfs(&[&[1], &[0, 3], &[3], &[1, 2]]));
+        debug_assert!(!bfs(&[
+            &[],
+            &[2, 4, 6],
+            &[1, 4, 8, 9],
+            &[7, 8],
+            &[1, 2, 8, 9],
+            &[6, 9],
+            &[1, 5, 7, 8, 9],
+            &[3, 6, 9],
+            &[2, 3, 4, 6, 9],
+            &[2, 4, 5, 6, 7, 8]
+        ]))
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
