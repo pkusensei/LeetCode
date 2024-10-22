d3bede032a7e5afe88f3069872b48f8957164c34
Zx Wang <ZW_08@outlook.com>
2024-10-22 12:33:49 -0400

    795 apparently it doesn't let me slide

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,50 +4,22 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn valid_tic_tac_toe(board: &[&str]) -> bool {
-        let board: Vec<&[u8]> = board.iter().map(|s| s.as_bytes()).collect();
-        let (xcount, ocount) =
-            board
-                .iter()
-                .flat_map(|row| row.iter())
-                .fold((0, 0), |(xcount, ocount), &b| {
-                    if b == b'X' {
-                        (1 + xcount, ocount)
-                    } else if b == b'O' {
-                        (xcount, 1 + ocount)
-                    } else {
-                        (xcount, ocount)
-                    }
-                });
-        if !(0..=1).contains(&(xcount - ocount)) {
-            return false;
+pub fn num_subarray_bounded_max(nums: &[i32], left: i32, right: i32) -> i32 {
+    let mut res = 0;
+    let mut count = 0;
+    let mut i1 = 0;
+    for (i2, &num) in nums.iter().enumerate() {
+        if (left..=right).contains(&num) {
+            count = i2 - i1 + 1;
+            res += count
+        } else if num < left {
+            res += count
+        } else {
+            i1 = 1 + i2;
+            count = 0
         }
-        if check(&board, b'X') && xcount != 1 + ocount {
-            return false;
-        }
-        if check(&board, b'O') && xcount != ocount {
-            return false;
-        }
-        true
-}
-
-fn check(board: &[&[u8]], byte: u8) -> bool {
-    if board.iter().any(|s| s.iter().all(|&b| b == byte)) {
-        return true;
     }
-    if (0..3)
-        .map(|i| [board[0][i], board[1][i], board[2][i]])
-        .any(|col| col.into_iter().all(|b| byte == b))
-    {
-        return true;
-    }
-    if (0..3).map(|i| board[i][i]).all(|b| byte == b) {
-        return true;
-    }
-    if (0..3).map(|i| board[i][2 - i]).all(|b| byte == b) {
-        return true;
-    }
-    false
+    res as _
 }
 
 #[cfg(test)]
@@ -58,13 +30,21 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(!valid_tic_tac_toe(&["O  ", "   ", "   "]));
-        debug_assert!(!valid_tic_tac_toe(&["XOX", " X ", "   "]));
-        debug_assert!(valid_tic_tac_toe(&["XOX", "O O", "XOX"]));
+        debug_assert_eq!(num_subarray_bounded_max(&[2, 1, 4, 3], 2, 3), 3);
+        debug_assert_eq!(num_subarray_bounded_max(&[2, 9, 2, 5, 6], 2, 8), 7);
+        debug_assert_eq!(
+            num_subarray_bounded_max(&[73, 55, 36, 5, 55, 14, 9, 7, 72, 52], 32, 69),
+            22
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            num_subarray_bounded_max(&[45, 2, 49, 6, 55, 73, 27, 17, 4, 71], 17, 33),
+            5
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
