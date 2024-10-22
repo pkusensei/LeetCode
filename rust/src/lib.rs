2bd9ac29dc127079f50a861fb2e57848f86fa747
Zx Wang <ZW_08@outlook.com>
2024-10-22 13:06:16 -0400

    797 bfs all paths

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,8 +4,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn rotate_string(s: &str, goal: &str) -> bool {
-    s.len() == goal.len() && format!("{s}{s}").contains(goal)
+pub fn all_paths_source_target(graph: &[&[i32]]) -> Vec<Vec<i32>> {
+    let n = graph.len() as i32;
+    let mut queue = std::collections::VecDeque::from([vec![0]]);
+    let mut res = vec![];
+    while let Some(path) = queue.pop_front() {
+        let Some(&last) = path.last() else {
+            break;
+        };
+        if last == n - 1 {
+            res.push(path);
+            continue;
+        }
+        for &next in graph[last as usize].iter() {
+            if !path.contains(&next) {
+                let mut p = path.clone();
+                p.push(next);
+                queue.push_back(p);
+            }
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -16,8 +35,20 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(rotate_string("abcde", "cdeab"));
-        debug_assert!(!rotate_string("abcde", "abced"));
+        sort_eq(
+            all_paths_source_target(&[&[1, 2], &[3], &[3], &[]]),
+            [vec![0, 1, 3], vec![0, 2, 3]],
+        );
+        sort_eq(
+            all_paths_source_target(&[&[4, 3, 1], &[3, 2, 4], &[3], &[4], &[]]),
+            [
+                vec![0, 4],
+                vec![0, 3, 4],
+                vec![0, 1, 3, 4],
+                vec![0, 1, 2, 3, 4],
+                vec![0, 1, 4],
+            ],
+        );
     }
 
     #[test]
