62834566d7d1074667ff43498ee62a511360c6f8
pkusensei <ZW_08@outlook.com>
2024-10-23 12:38:15 -0400

    801 min swaps to make increasing arrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,20 +4,57 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn champagne_tower(poured: i32, query_row: i32, query_glass: i32) -> f64 {
-    let r = query_row as usize;
-    let mut dp = vec![vec![0.0; 102]; 2 + r];
-    dp[0][0] = f64::from(poured);
-    for row in 0..=r {
-        for col in 0..=row {
-            let v = (dp[row][col] - 1.0) * 0.5;
-            if v > 0.0 {
-                dp[1 + row][col] += v;
-                dp[1 + row][1 + col] += v;
-            }
+pub fn min_swap(nums1: &[i32], nums2: &[i32]) -> i32 {
+    // dfs(&nums1[1..], &nums2[1..], 0, nums1[0], nums2[0]).unwrap_or(-1)
+
+    let (mut swap, mut skip) = (1, 0); // [skip, swap]
+    for (w1, w2) in nums1.windows(2).zip(nums2.windows(2)) {
+        if w1[0] >= w2[1] || w2[0] >= w1[1] {
+            // For window
+            // ...[3, 5]...
+            // ...[2, 3]...
+            // If [2]<->[3] is swapped, [5]<->[3] has to as well
+            // keep skip the same
+            swap += 1 // mark this [5]<->[3] swap
+        } else if w1[0] >= w1[1] || w2[0] >= w2[1] {
+            // Have to swap
+            // skip = swap to count possible previous swap
+            std::mem::swap(&mut swap, &mut skip);
+            swap += 1;
+        } else {
+            skip = skip.min(swap);
+            swap = 1 + skip;
         }
     }
-    dp[r][query_glass as usize].min(1.0)
+    swap.min(skip)
+}
+
+fn dfs(nums1: &[i32], nums2: &[i32], count: i32, prev1: i32, prev2: i32) -> Option<i32> {
+    match (nums1, nums2) {
+        ([], []) => Some(count),
+        ([h1, t1 @ ..], [h2, t2 @ ..]) => {
+            let (h1, h2) = (*h1, *h2);
+            if prev1 >= h1 || prev2 >= h2 {
+                // must switch
+                if prev1 < h2 && prev2 < h1 {
+                    dfs(t1, t2, 1 + count, h2, h1)
+                } else {
+                    None // not possible; terminate this path
+                }
+            } else if prev1 < h2 && prev2 < h1 {
+                let switch = dfs(t1, t2, 1 + count, h2, h1);
+                let skip = dfs(t1, t2, count, h1, h2);
+                match (switch, skip) {
+                    (Some(a), Some(b)) => Some(a.min(b)),
+                    (None, None) => None,
+                    _ => switch.or(skip),
+                }
+            } else {
+                dfs(t1, t2, count, h1, h2)
+            }
+        }
+        _ => unreachable!(),
+    }
 }
 
 #[cfg(test)]
@@ -28,13 +65,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(champagne_tower(1, 1, 1), 0.00000);
-        debug_assert_eq!(champagne_tower(2, 1, 1), 0.50000);
-        debug_assert_eq!(champagne_tower(100_000_009, 33, 17), 1.00000);
+        debug_assert_eq!(min_swap(&[1, 3, 5, 4], &[1, 2, 3, 7]), 1);
+        debug_assert_eq!(min_swap(&[0, 3, 5, 8, 9], &[2, 1, 4, 6, 9]), 1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(min_swap(&[0, 4, 4, 5, 9], &[0, 1, 6, 8, 10]), 1);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
