14d35c343b76b1029f2277419f8d6eb1e00b64e3
Zx Wang <ZW_08@outlook.com>
2024-10-24 11:11:17 -0400

    808 serving soup and probability

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,28 +1,36 @@
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_increase_keeping_skyline(grid: &[&[i32]]) -> i32 {
-    let n = grid.len();
-    if n < 2 {
-        return 0;
+pub fn soup_servings(n: i32) -> f64 {
+    if n > 4800 {
+        return 1.0;
     }
-    let rows: Vec<i32> = grid
-        .iter()
-        .map(|v| v.iter().copied().max().unwrap_or(0))
-        .collect();
-    let cols: Vec<i32> = (0..n)
-        .map(|i| grid.iter().map(|v| v[i]).max().unwrap_or(0))
-        .collect();
-    let mut res = 0;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, &n) in row.iter().enumerate() {
-            res += rows[y].min(cols[x]) - n;
+    dfs(n, n, &mut HashMap::new())
+}
+
+fn dfs(a: i32, b: i32, seen: &mut HashMap<[i32; 2], f64>) -> f64 {
+    match (a <= 0, b <= 0) {
+        (true, true) => 0.5,
+        (true, false) => 1.0,
+        (false, true) => 0.0,
+        _ => {
+            if let Some(&v) = seen.get(&[a, b]) {
+                return v;
+            }
+            let res = 0.25
+                * (dfs(a - 100, b, seen)
+                    + dfs(a - 75, b - 25, seen)
+                    + dfs(a - 50, b - 50, seen)
+                    + dfs(a - 25, b - 75, seen));
+            seen.insert([a, b], res);
+            res
         }
     }
-    res
 }
 
 #[cfg(test)]
@@ -33,19 +41,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            max_increase_keeping_skyline(&[
-                &[3, 0, 8, 4],
-                &[2, 4, 5, 7],
-                &[9, 2, 6, 3],
-                &[0, 3, 1, 0]
-            ]),
-            35
-        );
-        debug_assert_eq!(
-            max_increase_keeping_skyline(&[&[0, 0, 0], &[0, 0, 0], &[0, 0, 0]]),
-            0
-        );
+        debug_assert_eq!(soup_servings(50), 0.625);
+        debug_assert_eq!(soup_servings(100), 0.71875);
     }
 
     #[test]
