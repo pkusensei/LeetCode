6635dba7a2ae5d9cf2ea894f7a3adfb057d48fd9
pkusensei <ZW_08@outlook.com>
2024-10-25 17:59:50 -0400

    refactor the ref trie implementation

--- rust/src/trie.rs
+++ rust/src/trie.rs
@@ -1,44 +1,40 @@
 #[derive(Debug, Clone)]
-pub struct Trie<const N: usize> {
-    data: [Option<Box<Trie<N>>>; N],
+pub struct Trie<F, const N: usize> {
+    data: [Option<Box<Trie<F, N>>>; N],
     is_end: bool,
-}
-
-impl<const N: usize> Default for Trie<N> {
-    fn default() -> Self {
-        Self::new()
-    }
+    index_of: F,
 }
 
 #[allow(dead_code)]
-impl<const N: usize> Trie<N> {
-    pub const fn new() -> Self {
+impl<F, const N: usize> Trie<F, N> {
+    pub const fn new(index_of: F) -> Self {
         Self {
             data: [const { None }; N],
             is_end: false,
+            index_of,
         }
     }
 
-    pub fn insert<T, I, F>(&mut self, input: I, index_of: F)
+    pub fn insert<T, I>(&mut self, input: I)
     where
         I: IntoIterator<Item = T>,
-        F: FnMut(T) -> usize,
+        F: Fn(T) -> usize + Clone,
     {
-        self.insert_impl(input.into_iter(), index_of)
+        self.insert_impl(input.into_iter())
     }
 
-    fn insert_impl<T, I, F>(&mut self, mut it: I, mut index_of: F)
+    fn insert_impl<T, I>(&mut self, mut it: I)
     where
         I: Iterator<Item = T>,
-        F: FnMut(T) -> usize,
+        F: Fn(T) -> usize + Clone,
     {
         if let Some(v) = it.next() {
-            let idx = index_of(v);
+            let idx = (self.index_of)(v);
             if let Some(n) = self.data.get_mut(idx).and_then(|opt| opt.as_mut()) {
-                n.insert(it, index_of);
+                n.insert(it);
             } else {
-                let mut node = Box::new(Self::new());
-                node.insert(it, index_of);
+                let mut node = Box::new(Self::new(self.index_of.clone()));
+                node.insert(it);
                 self.data[idx] = Some(node);
             }
         } else {
@@ -46,23 +42,23 @@ impl<const N: usize> Trie<N> {
         }
     }
 
-    pub fn check<T, I, F>(&self, input: I, index_of: F) -> bool
+    pub fn check<T, I>(&self, input: I) -> bool
     where
         I: IntoIterator<Item = T>,
-        F: FnMut(T) -> usize,
+        F: Fn(T) -> usize,
     {
-        self.check_impl(input.into_iter(), index_of)
+        self.check_impl(input.into_iter())
     }
 
-    fn check_impl<T, I, F>(&self, mut it: I, mut index_of: F) -> bool
+    fn check_impl<T, I>(&self, mut it: I) -> bool
     where
         I: Iterator<Item = T>,
-        F: FnMut(T) -> usize,
+        F: Fn(T) -> usize,
     {
         if let Some(v) = it.next() {
-            let idx = index_of(v);
+            let idx = (self.index_of)(v);
             if let Some(node) = self.data.get(idx).and_then(|n| n.as_ref()) {
-                return node.check(it, index_of);
+                return node.check(it);
             } else {
                 return false;
             }
@@ -78,25 +74,24 @@ mod tests {
     #[test]
     #[ignore = "trie not changed"]
     fn basics() {
-        let mut trie = Trie::<26>::new();
-        let func = |&b| usize::from(b - b'a');
-        trie.insert(b"abcde", func);
-        debug_assert!(trie.check(b"abcde", func));
-        debug_assert!(!trie.check(b"abc", func));
-        trie.insert(b"abc", func);
-        debug_assert!(trie.check(b"abc", func));
-        debug_assert!(!trie.check(b"abcd", func));
-        debug_assert!(!trie.check(b"abcdef", func));
-        debug_assert!(trie.check(b"abcde", func));
+        let mut trie: Trie<_, 26> = Trie::new(|b: &u8| usize::from(b - b'a'));
+        trie.insert(b"abcde");
+        debug_assert!(trie.check(b"abcde",));
+        debug_assert!(!trie.check(b"abc",));
+        trie.insert(b"abc");
+        debug_assert!(trie.check(b"abc",));
+        debug_assert!(!trie.check(b"abcd",));
+        debug_assert!(!trie.check(b"abcdef",));
+        debug_assert!(trie.check(b"abcde",));
 
-        debug_assert!(!trie.check(b"abdef", func));
-        trie.insert(b"abdef", func);
-        debug_assert!(trie.check(b"abdef", func));
-        debug_assert!(trie.check(b"abcde", func));
+        debug_assert!(!trie.check(b"abdef",));
+        trie.insert(b"abdef");
+        debug_assert!(trie.check(b"abdef",));
+        debug_assert!(trie.check(b"abcde",));
 
-        debug_assert!(!trie.check(b"x", func));
-        trie.insert(b"xyz", func);
-        debug_assert!(trie.check(b"xyz", func));
-        debug_assert!(!trie.check(b"x", func));
+        debug_assert!(!trie.check(b"x",));
+        trie.insert(b"xyz");
+        debug_assert!(trie.check(b"xyz",));
+        debug_assert!(!trie.check(b"x",));
     }
 }
