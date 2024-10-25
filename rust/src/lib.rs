fab38973b5520436aee8696696b69ef1b47c4ba5
pkusensei <ZW_08@outlook.com>
2024-10-24 20:46:56 -0400

    1233 trie but with substrings

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,22 +1,58 @@
 mod helper;
 mod trie;
 
+use std::collections::HashSet;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_triangle_area(points: &[[i32; 2]]) -> f64 {
-    let n = points.len();
-    let mut res = 0f64;
-    for i1 in 0..n {
-        for i2 in i1..n {
-            for i3 in i2..n {
-                let (a, b, c) = (points[i1], points[i2], points[i3]);
-                let r = a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1]);
-                res = res.max(0.5 * f64::from(r.abs()));
+pub fn remove_subfolders(mut folder: Vec<&str>) -> Vec<&str> {
+    folder.sort_unstable_by_key(|s| s.len());
+    let mut dels = HashSet::new();
+    for (i1, needle) in folder.iter().enumerate() {
+        if dels.contains(&i1) {
+            continue;
+        }
+        for (i2, hay) in folder.iter().enumerate().skip(i1) {
+            if dels.contains(&i2) {
+                continue;
+            }
+            if hay.strip_prefix(needle).is_some_and(|v| v.starts_with('/')) {
+                dels.insert(i2);
             }
         }
     }
-    res
+    folder
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, s)| if dels.contains(&i) { None } else { Some(s) })
+        .collect()
+}
+
+fn with_trie(mut folder: Vec<&str>) -> Vec<&str> {
+    folder.sort_unstable_by_key(|s| s.len());
+    let mut trie = Trie::default();
+    folder.into_iter().filter(|s| trie.insert(s)).collect()
+}
+
+#[derive(Debug, Clone, Default)]
+struct Trie<'a> {
+    nodes: std::collections::HashMap<&'a str, Trie<'a>>,
+    is_end: bool,
+}
+
+impl<'a> Trie<'a> {
+    fn insert(&mut self, s: &'a str) -> bool {
+        let mut curr = self;
+        for seg in s.split('/').skip(1) {
+            curr = curr.nodes.entry(seg).or_default();
+            if curr.is_end {
+                return false; // has seen prefix
+            }
+        }
+        curr.is_end = true;
+        true
+    }
 }
 
 #[cfg(test)]
@@ -27,15 +63,43 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            largest_triangle_area(&[[0, 0], [0, 1], [1, 0], [0, 2], [2, 0]]),
-            2.0
+        sort_eq(
+            with_trie(vec!["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"]),
+            ["/a", "/c/d", "/c/f"],
         );
-        debug_assert_eq!(largest_triangle_area(&[[1, 0], [0, 0], [0, 1]]), 0.5);
+        sort_eq(with_trie(vec!["/a", "/a/b/c", "/a/b/d"]), ["/a"]);
+        sort_eq(
+            with_trie(vec!["/a/b/c", "/a/b/ca", "/a/b/d"]),
+            ["/a/b/c", "/a/b/ca", "/a/b/d"],
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        sort_eq(
+            with_trie(vec![
+                "/aa/ab/ac/ad",
+                "/aa/aq/ay",
+                "/bf/bv/cd/ch/cj",
+                "/bf/bg",
+                "/aa/aq/ar",
+                "/bf",
+                "/aa/ab/aj/an/ao",
+                "/aa/aq/ay/az",
+                "/aa/aq/ay/bc",
+                "/bf/bg/bh/bi/bj",
+                "/bf/bv/bw/ca/cc",
+                "/bf/bg/bh/bl",
+            ]),
+            [
+                "/aa/ab/ac/ad",
+                "/aa/ab/aj/an/ao",
+                "/aa/aq/ar",
+                "/aa/aq/ay",
+                "/bf",
+            ],
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
