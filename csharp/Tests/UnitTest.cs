5da6711733740cd88985276aa6782a68fc63caa8
pkusensei <ZW_08@outlook.com>
2024-10-26 11:01:14 -0400

    2458 heights of binary tree with subtree skipped

--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,24 +12,28 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = ListNode.Make([0, 1, 2, 3]);
-        var b = sol.NumComponents(a, [0, 1, 3]);
-        var c = 2;
-        Assert.AreEqual(c, b);
+        var a = TreeNode.Make([1, 3, 4, 2, null, 6, 5, null, null, null, null, null, 7]);
+        var b = sol.TwoLargestCousins(a, [4]);
+        var c = "[2]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = ListNode.Make([0, 1, 2, 3, 4]);
-        var b = sol.NumComponents(a, [0, 3, 1, 4]);
-        var c = 2;
-        Assert.AreEqual(c, b);
+        var a = TreeNode.Make([5, 8, 9, 2, 1, 3, 7, 4, 6]);
+        var b = sol.TwoLargestCousins(a, [3, 2, 4, 8]);
+        var c = "[3,2,3,2]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([1, null, 5, 3, null, 2, 4]);
+        var b = sol.TwoLargestCousins(a, [3, 5, 4, 2, 4]);
+        var c = "[1,0,3,3,3]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
