726e278707240776226237e3663fcddd305d3387
Zx Wang <ZW_08@outlook.com>
2024-10-29 11:03:35 -0400

    847 shortest path to visit all nodes in undirected unweighted graph with multi-source bfs and bit masking

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,81 +5,34 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_moves(grid: &[&[i32]]) -> i32 {
-    let (rows, cols) = get_dimensions(grid);
-    // (0..rows)
-    //     .map(|r| dfs(grid, cols, r, 0, &mut vec![vec![-1; cols]; rows]))
-    //     .max()
-    //     .unwrap_or(0)
-    // with_bfs(grid, rows, cols)
-    let (mut prev, mut curr) = (vec![1; rows], vec![0; rows]);
-    let mut res = 0;
-    for c in 1..cols {
-        for r in 0..rows {
-            for nr in r.saturating_sub(1)..=r + 1 {
-                if grid
-                    .get(nr)
-                    .is_some_and(|v| v.get(c - 1).is_some_and(|&num| num < grid[r][c]))
-                    && prev[nr] > 0
-                {
-                    curr[r] = curr[r].max(1 + prev[nr]);
-                }
-            }
-            res = res.max(curr[r] - 1);
-        }
-        prev = curr;
-        curr = vec![0; rows];
+pub fn shortest_path_length(graph: &[&[i32]]) -> i32 {
+    let n = graph.len();
+    let mut visited = vec![vec![false; n]; 1 << n];
+    for i in 0..n {
+        visited[1 << i][i] = true;
     }
-    res
-}
-
-fn dfs(grid: &[&[i32]], cols: usize, r: usize, c: usize, dp: &mut [Vec<i32>]) -> i32 {
-    if c == cols - 1 {
-        return 0;
-    }
-    if dp[r][c] > -1 {
-        return dp[r][c];
-    }
-    let val = grid[r][c];
-    let mut res = 0;
-    for nr in r.saturating_sub(1)..=r + 1 {
-        if grid
-            .get(nr)
-            .is_some_and(|v| v.get(1 + c).is_some_and(|&num| num > val))
-        {
-            res = res.max(1 + dfs(grid, cols, nr, 1 + c, dp));
-        }
-    }
-    dp[r][c] = res;
-    res
-}
-
-fn with_bfs(grid: &[&[i32]], rows: usize, cols: usize) -> i32 {
-    let mut visited = vec![vec![false; cols]; rows];
-    // interesting choice
-    let mut queue: std::collections::VecDeque<_> = (0..rows).map(|r| (r, 0, 0)).collect();
-    let mut res = 0;
+    // multi-source BFS
+    let mut queue: std::collections::VecDeque<_> = (0..n).map(|node| (1 << node, node)).collect();
+    let mut count = 0;
     while !queue.is_empty() {
-        let mut n = queue.len();
-        while n > 0 {
-            n -= 1;
-            let Some((r, c, count)) = queue.pop_front() else {
+        for _ in 0..queue.len() {
+            let Some((mask, node)) = queue.pop_front() else {
                 break;
             };
-            res = res.max(count);
-            for nr in r.saturating_sub(1)..=r + 1 {
-                if grid
-                    .get(nr)
-                    .is_some_and(|v| v.get(1 + c).is_some_and(|&num| num > grid[r][c]))
-                    && !visited[nr][1 + c]
-                {
-                    visited[nr][1 + c] = true;
-                    queue.push_back((nr, 1 + c, 1 + count));
+            if mask == (1 << n) - 1 {
+                return count;
+            }
+            for &neighbor in graph[node].iter() {
+                let nmask = mask | (1 << neighbor);
+                if !visited[nmask][neighbor as usize] {
+                    visited[nmask][neighbor as usize] = true;
+                    queue.push_back((nmask, neighbor as usize));
                 }
             }
         }
+        count += 1;
     }
-    res
+    -1
 }
 
 #[cfg(test)]
@@ -90,16 +43,11 @@ mod tests {
 
     #[test]
     fn basics() {
+        debug_assert_eq!(shortest_path_length(&[&[1, 2, 3], &[0], &[0], &[0]]), 4);
         debug_assert_eq!(
-            max_moves(&[
-                &[2, 4, 3, 5],
-                &[5, 4, 9, 3],
-                &[3, 4, 2, 11],
-                &[10, 9, 13, 15]
-            ]),
-            3
+            shortest_path_length(&[&[1], &[0, 2, 4], &[1, 3, 4], &[2], &[1, 2]]),
+            4
         );
-        debug_assert_eq!(max_moves(&[&[3, 2, 4], &[2, 1, 9], &[1, 1, 7]]), 0);
     }
 
     #[test]
