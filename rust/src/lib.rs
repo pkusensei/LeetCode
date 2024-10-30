f7476dc0b67047f33249ccdb50b8b4736fe163ef
pkusensei <ZW_08@outlook.com>
2024-10-30 12:16:23 -0400

    855 BTreeSet and biggest interval

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,30 +2,61 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::BTreeSet;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn k_similarity(s1: String, s2: &str) -> i32 {
-    let (mut s1, s2) = (s1.into_bytes(), s2.as_bytes());
-    dfs(&mut s1, s2, 0)
+#[derive(Debug, Clone)]
+struct ExamRoom {
+    nums: BTreeSet<i32>,
+    n: i32,
 }
 
-fn dfs(s1: &mut [u8], s2: &[u8], start: usize) -> i32 {
-    if start == s1.len() - 1 {
-        0
-    } else if s1[start] == s2[start] {
-        dfs(s1, s2, 1 + start)
-    } else {
-        let mut res = i32::MAX;
-        for idx in 1 + start..s1.len() {
-            if s1[idx] == s2[idx] || s2[start] != s1[idx] {
+impl ExamRoom {
+    fn new(n: i32) -> Self {
+        Self {
+            nums: BTreeSet::new(),
+            n,
+        }
+    }
+
+    fn seat(&mut self) -> i32 {
+        if self.nums.is_empty() {
+            self.nums.insert(0);
+            return 0;
+        }
+        let (mut width, mut res) = (0, 0);
+        if let Some(&v) = self.nums.first() {
+            if v > 0 {
+                width = v;
+                res = 0;
+            }
+        }
+        let mut prev = -1;
+        for &num in self.nums.iter() {
+            if prev == -1 {
+                prev = num;
                 continue;
             }
-            s1.swap(start, idx);
-            res = res.min(1 + dfs(s1, s2, 1 + start));
-            s1.swap(start, idx);
+            if (num - prev) / 2 > width {
+                width = (num - prev) / 2;
+                res = prev + 1 + (num - 1 - prev - 1) / 2; // this trips me up
+            }
+            prev = num;
         }
+        if let Some(&v) = self.nums.last() {
+            if self.n - 1 - v > width {
+                // width = self.n - 1 - v;
+                res = self.n - 1;
+            }
+        }
+        self.nums.insert(res);
         res
+    }
+
+    fn leave(&mut self, p: i32) {
+        self.nums.remove(&p);
     }
 }
 
@@ -37,9 +68,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(k_similarity("ab".into(), "ba"), 1);
-        debug_assert_eq!(k_similarity("abc".into(), "bca"), 2);
-        debug_assert_eq!(k_similarity("abccaacceecdeea".into(), "bcaacceeccdeaae"), 9);
+        let mut room = ExamRoom::new(10);
+        debug_assert_eq!(room.seat(), 0); // return 0, no one is in the room, then the student sits at seat number 0.
+        debug_assert_eq!(room.seat(), 9); // return 9, the student sits at the last seat number 9.
+        debug_assert_eq!(room.seat(), 4); // return 4, the student sits at the last seat number 4.
+        debug_assert_eq!(room.seat(), 2); // return 2, the student sits at the last seat number 2.
+        room.leave(4);
+        debug_assert_eq!(room.seat(), 5); // return 5, the student sits at the last seat number 5.
     }
 
     #[test]
