a039c447f8f452c583c242061448269405df4ccb
pkusensei <ZW_08@outlook.com>
2024-10-29 21:22:51 -0400

    1671 longest inc and dec subseq; longest mountain array

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,22 +5,76 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn car_fleet(target: i32, position: &[i32], speed: &[i32]) -> i32 {
-    let mut pairs: Vec<_> = position
-        .iter()
-        .copied()
-        .zip(speed.iter().copied())
-        .collect();
-    pairs.sort_unstable_by_key(|&(pos, _)| std::cmp::Reverse(pos));
-    let mut stack = vec![];
-    for (pos, speed) in pairs {
-        let t = f64::from(target - pos) / f64::from(speed);
-        if stack.last().is_some_and(|&v| v >= t) {
-            continue; // catches up and stays in fleet
+pub fn minimum_mountain_removals(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let (mut inc, mut dec) = (vec![1; n], vec![1; n]);
+    for i1 in 1..n {
+        for i2 in 0..i1 {
+            if nums[i2] < nums[i1] {
+                inc[i1] = inc[i1].max(1 + inc[i2]);
+            }
         }
-        stack.push(t);
     }
-    stack.len() as _
+    for i1 in (0..n).rev() {
+        for i2 in 1 + i1..n {
+            if nums[i1] > nums[i2] {
+                dec[i1] = dec[i1].max(1 + dec[i2]);
+            }
+        }
+    }
+    let mount = inc
+        .into_iter()
+        .zip(dec)
+        .filter_map(|(a, b)| {
+            if a > 1 && b > 1 {
+                Some(a + b - 1)
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap_or(1);
+    nums.len() as i32 - mount
+}
+
+fn with_binary_search(nums: &mut [i32]) -> i32 {
+    let inc = lis(nums);
+    nums.reverse();
+    let mut dec = lis(nums);
+    dec.reverse();
+
+    let mount = inc
+        .into_iter()
+        .zip(dec)
+        .filter_map(|(a, b)| {
+            if a > 1 && b > 1 {
+                Some(a + b - 1)
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap_or(1);
+    nums.len() as i32 - mount
+}
+
+fn lis(nums: &[i32]) -> Vec<i32> {
+    let n = nums.len();
+    let mut res = vec![1; n]; // track length of LIS by each index
+    let mut lis = vec![nums[0]]; // track LIS
+    for (idx, &num) in nums.iter().enumerate().skip(1) {
+        // binary search to find num in lis
+        let i = lis.partition_point(|&v| v < num);
+        if i == lis.len() {
+            // num is bigger than all LIS
+            lis.push(num);
+        } else {
+            // update num into LIS
+            lis[i] = num;
+        }
+        res[idx] = lis.len() as i32;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -31,15 +85,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(car_fleet(12, &[10, 8, 0, 5, 3], &[2, 4, 1, 1, 3],), 3);
-        debug_assert_eq!(car_fleet(10, &[3], &[3],), 1);
-        debug_assert_eq!(car_fleet(100, &[0, 2, 4], &[4, 2, 1],), 1);
+        debug_assert_eq!(with_binary_search(&mut [1, 3, 1]), 0);
+        debug_assert_eq!(with_binary_search(&mut [2, 1, 1, 5, 6, 2, 3, 1]), 3);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(car_fleet(10, &[0, 4, 2], &[2, 1, 3]), 1);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
