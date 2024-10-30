af2836b6f70636ba3fbdc6b7e9b8cfac156ad7ea
pkusensei <ZW_08@outlook.com>
2024-10-30 16:51:10 -0400

    858 extend that mirror

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,37 +2,19 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::BinaryHeap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn mincost_to_hire_workers(quality: &[i32], wage: &[i32], k: i32) -> f64 {
-    let mut pairs: Vec<_> = quality
-        .iter()
-        .zip(wage.iter())
-        .map(|(&q, &w)| (q, f64::from(w) / f64::from(q)))
-        .collect();
-    pairs.sort_unstable_by(|a, b| a.1.total_cmp(&b.1));
-    // want lowest wage/quality ratio
-    // and keep lowest quality in heap
-    let mut heap = BinaryHeap::new();
-    let mut res = f64::MAX;
-    let mut quality = 0;
-    for curr in pairs.into_iter() {
-        heap.push(curr.0);
-        quality += curr.0;
-        if heap.len() > k as usize {
-            let Some(top) = heap.pop() else {
-                continue;
-            };
-            quality -= top;
-        }
-        if heap.len() == k as usize {
-            res = res.min(f64::from(quality) * curr.1);
-        }
+pub fn mirror_reflection(p: i32, q: i32) -> i32 {
+    let gcd = gcd(p, q);
+    let lcm = p * q / gcd;
+    // the first corner that ray hits
+    let (a, b) = (lcm / p, lcm / q);
+    match (a & 1, b & 1) {
+        (1, 1) => 1,
+        (0, 1) => 0,
+        _ => 2,
     }
-    res
 }
 
 #[cfg(test)]
@@ -43,14 +25,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        float_eq(
-            mincost_to_hire_workers(&[10, 20, 5], &[70, 50, 30], 2),
-            105.0,
-        );
-        float_eq(
-            mincost_to_hire_workers(&[3, 1, 10, 10, 1], &[4, 8, 2, 2, 7], 3),
-            30.66667,
-        );
+        debug_assert_eq!(mirror_reflection(2, 1), 2);
+        debug_assert_eq!(mirror_reflection(3, 1), 1);
     }
 
     #[test]
@@ -70,12 +46,12 @@ mod tests {
     }
 
     #[allow(dead_code)]
-    fn float_eq<T1, T2>(a: T1, b: T2) -> bool
+    fn float_eq<T1, T2>(a: T1, b: T2)
     where
         T1: Into<f64>,
         T2: Into<f64>,
     {
         const EP: f64 = 1e-5;
-        (<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() <= EP
+        debug_assert!((<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() <= EP);
     }
 }
