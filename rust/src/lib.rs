bb125894cd157c8162840f30b954b9a957d0f556
pkusensei <ZW_08@outlook.com>
2024-10-30 12:32:21 -0400

    856 score of ()'s

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,62 +2,35 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::BTreeSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-#[derive(Debug, Clone)]
-struct ExamRoom {
-    nums: BTreeSet<i32>,
-    n: i32,
+pub fn score_of_parentheses(s: &str) -> i32 {
+    dfs(s)
 }
 
-impl ExamRoom {
-    fn new(n: i32) -> Self {
-        Self {
-            nums: BTreeSet::new(),
-            n,
+fn dfs(s: &str) -> i32 {
+    if s.is_empty() {
+        return 0;
+    }
+    if s == "()" {
+        return 1;
+    }
+    let mut open = 0;
+    for (idx, b) in s.bytes().enumerate() {
+        match b {
+            b'(' => open += 1,
+            _ => open -= 1,
+        }
+        if open == 0 {
+            if idx == s.len() - 1 {
+                return 2 * dfs(&s[1..s.len() - 1]);
+            } else {
+                return dfs(&s[..=idx]) + dfs(&s[1 + idx..]);
+            }
         }
     }
-
-    fn seat(&mut self) -> i32 {
-        if self.nums.is_empty() {
-            self.nums.insert(0);
-            return 0;
-        }
-        let (mut width, mut res) = (0, 0);
-        if let Some(&v) = self.nums.first() {
-            if v > 0 {
-                width = v;
-                res = 0;
-            }
-        }
-        let mut prev = -1;
-        for &num in self.nums.iter() {
-            if prev == -1 {
-                prev = num;
-                continue;
-            }
-            if (num - prev) / 2 > width {
-                width = (num - prev) / 2;
-                res = prev + 1 + (num - 1 - prev - 1) / 2; // this trips me up
-            }
-            prev = num;
-        }
-        if let Some(&v) = self.nums.last() {
-            if self.n - 1 - v > width {
-                // width = self.n - 1 - v;
-                res = self.n - 1;
-            }
-        }
-        self.nums.insert(res);
-        res
-    }
-
-    fn leave(&mut self, p: i32) {
-        self.nums.remove(&p);
-    }
+    0
 }
 
 #[cfg(test)]
@@ -68,13 +41,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut room = ExamRoom::new(10);
-        debug_assert_eq!(room.seat(), 0); // return 0, no one is in the room, then the student sits at seat number 0.
-        debug_assert_eq!(room.seat(), 9); // return 9, the student sits at the last seat number 9.
-        debug_assert_eq!(room.seat(), 4); // return 4, the student sits at the last seat number 4.
-        debug_assert_eq!(room.seat(), 2); // return 2, the student sits at the last seat number 2.
-        room.leave(4);
-        debug_assert_eq!(room.seat(), 5); // return 5, the student sits at the last seat number 5.
+        debug_assert_eq!(score_of_parentheses("()"), 1);
+        debug_assert_eq!(score_of_parentheses("(())"), 2);
+        debug_assert_eq!(score_of_parentheses("()()"), 2);
     }
 
     #[test]
