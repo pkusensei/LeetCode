44d0380a4b70e059a40222bfda5272d2fea34968
pkusensei <ZW_08@outlook.com>
2024-10-31 04:07:57 -0400

    863 nodes with k distance from target node

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,33 +5,34 @@ namespace Solution;
 
 public class Solution
 {
-    public bool LemonadeChange(int[] bills)
+    public IList<int> DistanceK(TreeNode root, TreeNode target, int k)
     {
-        var (five, ten) = (0, 0);
-        foreach (var n in bills)
+        List<int> res = [];
+        if (root is null) { return res; }
+
+        Dictionary<TreeNode, TreeNode> parents = [];
+        FindParent(root, null, parents);
+        Dfs(target, k, res, parents, []);
+        return res;
+    }
+
+    static void Dfs(TreeNode node, int k, List<int> nums, Dictionary<TreeNode, TreeNode> parents, HashSet<int> seen)
+    {
+        if (node is null || !seen.Add(node.val)) { return; }
+        if (k == 0) { nums.Add(node.val); return; }
+        Dfs(node.left, k - 1, nums, parents, seen);
+        Dfs(node.right, k - 1, nums, parents, seen);
+        if (parents.TryGetValue(node, out var p))
         {
-            switch (n)
-            {
-                case 5: five += 1; break;
-                case 10:
-                    if (five > 0)
-                    {
-                        five -= 1; ten += 1;
-                        break;
-                    }
-                    else { return false; }
-                case 20:
-                    if (five > 0 && ten > 0)
-                    {
-                        five -= 1; ten -= 1;
-                        break;
-                    }
-                    else if (five >= 3) { five -= 3; break; }
-                    else { return false; }
-                default:
-                    break;
-            }
+            Dfs(p, k - 1, nums, parents, seen);
         }
-        return true;
+    }
+
+    static void FindParent(TreeNode node, TreeNode parent, Dictionary<TreeNode, TreeNode> parents)
+    {
+        if (node is null) { return; }
+        parents.Add(node, parent);
+        FindParent(node.left, node, parents);
+        FindParent(node.right, node, parents);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,18 +12,27 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        Assert.IsTrue(sol.LemonadeChange([5, 5, 5, 10, 20]));
+        var a = TreeNode.Make([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+        var b = sol.DistanceK(a, a.left, 2);
+        var c = "[7,4,1]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        Assert.IsFalse(sol.LemonadeChange([5, 5, 10, 10, 20]));
+        var a = TreeNode.Make([1]);
+        var b = sol.DistanceK(a, a, 3);
+        Assert.AreEqual(0, b.Count);
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([0, 1, null, 3, 2]);
+        var b = sol.DistanceK(a, a.left.right, 1);
+        var c = "[1]";
+        Assert.AreEqual(c, b.Print());
     }
 
     [TestMethod]
