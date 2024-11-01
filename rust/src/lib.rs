aa451472b15f13f483b76bf4e08a3df3a0e9fe8e
pkusensei <ZW_08@outlook.com>
2024-11-01 08:30:37 -0400

    869 this feels like cheating; const/constexpr ftw

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,11 +5,29 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn make_fancy_string(s: &str) -> String {
-    s.as_bytes()
-        .chunk_by(|a, b| a == b)
-        .flat_map(|ch| ch.iter().take(2).copied().map(char::from))
-        .collect()
+pub fn reordered_power_of2(n: i32) -> bool {
+    const ALL: [[u8; 10]; 31] = preprocess();
+    let x = get_digits(n);
+    ALL.iter().any(|&v| v == x)
+}
+
+const fn preprocess() -> [[u8; 10]; 31] {
+    let mut res = [[0; 10]; 31];
+    let mut x = 0;
+    while x < 31 {
+        res[x as usize] = get_digits(2i32.pow(x));
+        x += 1;
+    }
+    res
+}
+
+const fn get_digits(mut n: i32) -> [u8; 10] {
+    let mut res = [0; 10];
+    while n > 0 {
+        res[(n % 10) as usize] += 1;
+        n /= 10;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -20,9 +38,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(make_fancy_string("leeetcode"), "leetcode");
-        debug_assert_eq!(make_fancy_string("aaabaaaa"), "aabaa");
-        debug_assert_eq!(make_fancy_string("aab"), "aab");
+        debug_assert!(reordered_power_of2(1));
+        debug_assert!(!reordered_power_of2(10));
+        debug_assert!(reordered_power_of2(46));
     }
 
     #[test]
