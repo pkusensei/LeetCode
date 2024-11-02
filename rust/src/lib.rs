db0673b196f3be8f1e6bd8c7276760472061dec7
pkusensei <ZW_08@outlook.com>
2024-11-02 16:40:37 -0400

    886 hatred flows both ways; dfs and coloring graph

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn projection_area(grid: &[&[i32]]) -> i32 {
-    let n = grid.len();
-    let xy = grid
-        .iter()
-        .map(|r| r.iter().filter(|&&v| v > 0).count())
-        .sum::<usize>() as i32;
-    let xz = grid
-        .iter()
-        .map(|r| r.iter().max().unwrap_or(&0))
-        .sum::<i32>();
-    let yz = (0..n)
-        .map(|c| grid.iter().map(|row| row[c]).max().unwrap_or(0))
-        .sum::<i32>();
-    xy + xz + yz
+pub fn possible_bipartition(n: i32, dislikes: &[[i32; 2]]) -> bool {
+    let n = n as usize;
+    let graph = dislikes.iter().fold(vec![vec![]; 1 + n], |mut acc, v| {
+        acc[v[0] as usize].push(v[1] as usize);
+        acc[v[1] as usize].push(v[0] as usize);
+        acc
+    });
+    let mut colors = vec![-1; 1 + n];
+    for i in 1..n {
+        if colors[i] < 0 && !dfs(&graph, &mut colors, i, 0) {
+            return false;
+        }
+    }
+    true
+}
+
+fn dfs(graph: &[Vec<usize>], colors: &mut [i32], curr: usize, color: i32) -> bool {
+    if colors[curr] > -1 {
+        return colors[curr] == color;
+    }
+    colors[curr] = color;
+    for &next in graph[curr].iter() {
+        if !dfs(graph, colors, next, 1 - color) {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -29,9 +42,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(projection_area(&[&[1, 2], &[3, 4]]), 17);
-        debug_assert_eq!(projection_area(&[&[2]]), 5);
-        debug_assert_eq!(projection_area(&[&[1, 0], &[0, 2]]), 8);
+        debug_assert!(possible_bipartition(4, &[[1, 2], [1, 3], [2, 4]]));
+        debug_assert!(!possible_bipartition(3, &[[1, 2], [1, 3], [2, 3]]));
     }
 
     #[test]
