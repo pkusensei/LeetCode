f4008feb1d5902139784e8fca2c98dd1bb936f3e
pkusensei <ZW_08@outlook.com>
2024-11-02 14:56:14 -0400

    882 but also try walk edges as much as possible

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,20 +2,45 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::VecDeque;
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_rescue_boats(people: &mut [i32], limit: i32) -> i32 {
-    people.sort_unstable();
-    let mut queue: VecDeque<i32> = people.iter().copied().collect();
+pub fn reachable_nodes(edges: &[[i32; 3]], max_moves: i32, n: i32) -> i32 {
+    let n = n as usize;
+    let mut graph = vec![vec![]; n];
+    for e in edges {
+        graph[e[0] as usize].push((e[1] as usize, e[2]));
+        graph[e[1] as usize].push((e[0] as usize, e[2]));
+    }
+    let mut heap = BinaryHeap::from([(Reverse(0), 0)]);
+    let mut dist = vec![1 + max_moves; 1 + n];
+    dist[0] = 0;
+    let mut used = HashMap::new(); // or edge length travelled
     let mut res = 0;
-    while let Some(end) = queue.pop_back() {
-        res += 1;
-        if queue.front().is_some_and(|&v| v + end <= limit) {
-            queue.pop_front();
+    while let Some((Reverse(moves), curr)) = heap.pop() {
+        if moves > dist[curr] {
+            continue;
         }
+        res += 1;
+        for &(next, weight) in graph[curr].iter() {
+            used.insert((curr, next), weight.min(max_moves - moves));
+            let total = moves + weight + 1; // total dist from start 0
+            if total < dist[next].min(1 + max_moves) {
+                heap.push((Reverse(total), next));
+                dist[next] = total;
+            }
+        }
+    }
+    for e in edges {
+        res += e[2].min(
+            used.get(&(e[0] as usize, e[1] as usize)).unwrap_or(&0)
+                + used.get(&(e[1] as usize, e[0] as usize)).unwrap_or(&0),
+        )
     }
     res
 }
@@ -28,13 +53,35 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(num_rescue_boats(&mut [1, 2], 3), 1);
-        debug_assert_eq!(num_rescue_boats(&mut [3, 2, 2, 1], 3), 3);
-        debug_assert_eq!(num_rescue_boats(&mut [3, 5, 3, 4], 5), 4);
+        debug_assert_eq!(
+            reachable_nodes(&[[0, 1, 10], [0, 2, 1], [1, 2, 2]], 6, 3),
+            13
+        );
+        debug_assert_eq!(
+            reachable_nodes(&[[0, 1, 4], [1, 2, 6], [0, 2, 8], [1, 3, 1]], 10, 4),
+            23
+        );
+        debug_assert_eq!(
+            reachable_nodes(
+                &[[1, 2, 4], [1, 4, 5], [1, 3, 1], [2, 3, 4], [3, 4, 5]],
+                17,
+                5
+            ),
+            1
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            reachable_nodes(
+                &[[2, 4, 2], [3, 4, 5], [2, 3, 1], [0, 2, 1], [0, 3, 5]],
+                14,
+                5
+            ),
+            18
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
