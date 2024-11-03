b2906fd1c28e73a40d85f6b5cb57c974d478f6c1
pkusensei <ZW_08@outlook.com>
2024-11-02 20:28:56 -0400

    888 one swap to make arrays with equal sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,46 +5,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn super_egg_drop(k: i32, n: i32) -> i32 {
-    let (n, k) = (n as usize, k as usize);
-    // dfs(n, k, &mut vec![vec![-1; 1 + n]; 1 + k])
-    let mut dp = vec![0; 1 + k];
-    let mut moves = 0;
-    while dp[k] < n {
-        // rev to avoid duplicate counting ??
-        for i in (1..=k).rev() {
-            // With one more move, the highest floor reachable is
-            // dp[m][i] = 1 + dp[m-1][i-1] + dp[m-1][i]
-            // 1 + prev case when egg breaks + prev when egg is intact
-            dp[i] = 1 + dp[i - 1] + dp[i];
-        }
-        moves += 1;
-    }
-    moves
-}
-
-fn dfs(n: usize, k: usize, dp: &mut [Vec<i32>]) -> i32 {
-    if n < 2 || k == 1 {
-        return n as i32;
-    }
-    if dp[k][n] > -1 {
-        return dp[k][n];
-    }
-    let (mut left, mut right) = (1, n);
-    let mut res = i32::MAX;
-    while left <= right {
-        let mid = left + (right - left) / 2;
-        let low = dfs(mid - 1, k - 1, dp); // egg broken
-        let high = dfs(n - mid, k, dp);
-        res = res.min(1 + low.max(high));
-        if low < high {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+pub fn fair_candy_swap(alice_sizes: &[i32], bob_sizes: &[i32]) -> Vec<i32> {
+    let sa: i32 = alice_sizes.iter().sum();
+    let sb: i32 = bob_sizes.iter().sum();
+    let delta = sa - sb;
+    let b: std::collections::HashSet<_> = bob_sizes.iter().copied().collect();
+    for &a in alice_sizes.iter() {
+        if b.contains(&(a - delta / 2)) {
+            return vec![a, a - delta / 2];
         }
     }
-    dp[k][n] = res;
-    res
+    vec![]
 }
 
 #[cfg(test)]
@@ -55,15 +26,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(super_egg_drop(1, 2), 2);
-        debug_assert_eq!(super_egg_drop(2, 6), 3);
-        debug_assert_eq!(super_egg_drop(3, 14), 4);
+        debug_assert_eq!(fair_candy_swap(&[1, 1], &[2, 2]), [1, 2]);
+        debug_assert_eq!(fair_candy_swap(&[1, 2], &[2, 3]), [1, 2]);
+        debug_assert_eq!(fair_candy_swap(&[2], &[1, 3]), [2, 3]);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(super_egg_drop(2, 2), 2);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
