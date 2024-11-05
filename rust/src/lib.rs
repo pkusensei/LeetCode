15f393aeb1bc78b5ab4a297fa0d2bc26068376d0
pkusensei <ZW_08@outlook.com>
2024-11-04 20:23:30 -0500

    2914 substr with only 1 or 0 and even length

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,33 +5,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_subarray_mins(arr: &[i32]) -> i32 {
-    const MOD: i32 = 1_000_000_007;
-    let n = arr.len();
-    let mut dp = vec![0; n];
-    let mut stack = vec![];
-    for (idx, &num) in arr.iter().enumerate() {
-        while stack.last().is_some_and(|&i| arr[i] >= num) {
-            stack.pop();
-        }
-        if let Some(&i) = stack.last() {
-            // ..i..idx where [i] < [idx]
-            // take current num for [..idx]
-            // add in all that account for [..i]
-            // Consider [3,1,5,2]
-            // dp[1] is the result for all with [3,1]
-            // When current num == 2, dp[3] is
-            // dp[1] this is all subarrays that starts with [3,1] or [1]
-            // (3-1) * 2 is result for [5,2]
-            dp[idx] = (dp[i] + (idx - i) as i32 * num) % MOD;
-        } else {
-            // current num is the min
-            // each past subarray should be counted in
-            dp[idx] = (1 + idx) as i32 * num % MOD;
-        }
-        stack.push(idx);
-    }
-    dp.into_iter().fold(0, |acc, v| (acc + v) % MOD)
+pub fn min_changes(s: &str) -> i32 {
+    s.as_bytes()
+        .chunks_exact(2)
+        .filter(|ch| ch[0] != ch[1])
+        .count() as _
 }
 
 #[cfg(test)]
@@ -42,8 +20,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(sum_subarray_mins(&[3, 1, 2, 4]), 17);
-        debug_assert_eq!(sum_subarray_mins(&[11, 81, 94, 43, 3]), 444);
+        debug_assert_eq!(min_changes("1001"), 2);
+        debug_assert_eq!(min_changes("10"), 1);
+        debug_assert_eq!(min_changes("0000"), 0);
     }
 
     #[test]
