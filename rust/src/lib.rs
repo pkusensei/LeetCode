cad12e70e4c1eaedc30ec76c01d35bd60c06c337
pkusensei <ZW_08@outlook.com>
2024-11-04 20:04:22 -0500

    907 monotonic stack to sum min values in all subarrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,22 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn superpalindromes_in_range(left: &str, right: &str) -> i32 {
-        const ROOTS: [u64; 70] = [
-            1, 2, 3, 11, 22, 101, 111, 121, 202, 212, 1001, 1111, 2002, 10001, 10101, 10201, 11011,
-            11111, 11211, 20002, 20102, 100001, 101101, 110011, 111111, 200002, 1000001, 1001001,
-            1002001, 1010101, 1011101, 1012101, 1100011, 1101011, 1102011, 1110111, 1111111, 2000002,
-            2001002, 10000001, 10011001, 10100101, 10111101, 11000011, 11011011, 11100111, 11111111,
-            20000002, 100000001, 100010001, 100020001, 100101001, 100111001, 100121001, 101000101,
-            101010101, 101020101, 101101101, 101111101, 110000011, 110010011, 110020011, 110101011,
-            110111011, 111000111, 111010111, 111101111, 111111111, 200000002, 200010002,
-        ];
-        let a = left.parse().unwrap();
-        let b = right.parse().unwrap();
-        ROOTS
-            .into_iter()
-            .filter(|n| (a..=b).contains(&(n * n)))
-            .count() as _
+pub fn sum_subarray_mins(arr: &[i32]) -> i32 {
+    const MOD: i32 = 1_000_000_007;
+    let n = arr.len();
+    let mut dp = vec![0; n];
+    let mut stack = vec![];
+    for (idx, &num) in arr.iter().enumerate() {
+        while stack.last().is_some_and(|&i| arr[i] >= num) {
+            stack.pop();
+        }
+        if let Some(&i) = stack.last() {
+            // ..i..idx where [i] < [idx]
+            // take current num for [..idx]
+            // add in all that account for [..i]
+            // Consider [3,1,5,2]
+            // dp[1] is the result for all with [3,1]
+            // When current num == 2, dp[3] is
+            // dp[1] this is all subarrays that starts with [3,1] or [1]
+            // (3-1) * 2 is result for [5,2]
+            dp[idx] = (dp[i] + (idx - i) as i32 * num) % MOD;
+        } else {
+            // current num is the min
+            // each past subarray should be counted in
+            dp[idx] = (1 + idx) as i32 * num % MOD;
+        }
+        stack.push(idx);
+    }
+    dp.into_iter().fold(0, |acc, v| (acc + v) % MOD)
 }
 
 #[cfg(test)]
@@ -31,8 +42,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(superpalindromes_in_range("4", "1000"), 4);
-        debug_assert_eq!(superpalindromes_in_range("1", "2"), 1);
+        debug_assert_eq!(sum_subarray_mins(&[3, 1, 2, 4]), 17);
+        debug_assert_eq!(sum_subarray_mins(&[11, 81, 94, 43, 3]), 444);
     }
 
     #[test]
