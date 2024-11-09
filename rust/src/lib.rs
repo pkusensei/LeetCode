be2393cfbab7d00e2bdf17c4f310491aab5441d5
pkusensei <ZW_08@outlook.com>
2024-11-09 16:34:19 -0500

    944 find unsorted colomns

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,84 +5,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn shortest_superstring(words: &[&str]) -> String {
-    let n = words.len();
-    let mut overlap = vec![vec![0; n]; n];
-    for (i1, a) in words.iter().enumerate() {
-        for (i2, b) in words.iter().enumerate().skip(1 + i1) {
-            overlap[i1][i2] = get_overlap(a, b);
-            overlap[i2][i1] = get_overlap(b, a);
-        }
-    }
-    let (dp, parent) = enumerate(n, &overlap);
-    let perm = build(n, dp, parent);
-    let mut res = words[perm[0]].to_string();
-    for w in perm.windows(2) {
-        let over = overlap[w[0]][w[1]];
-        res.push_str(&words[w[1]][over..]);
-    }
-    res
-}
-
-fn build(n: usize, dp: Vec<Vec<usize>>, parent: Vec<Vec<Option<usize>>>) -> Vec<usize> {
-    let mut perm = Vec::with_capacity(n);
-    let mut mask = (1 << n) - 1;
-    // Find last added str
-    let mut p = (0..n).max_by_key(|&i| dp[mask][i]);
-    let mut seen = vec![false; n];
-    while let Some(v) = p {
-        perm.push(v);
-        seen[v] = true;
-        let p2 = parent[mask][v];
-        mask ^= 1 << v;
-        p = p2;
-    }
-    perm.reverse();
-    // add strs with no overlaps
-    perm.extend((0..n).filter(|&i| !seen[i]));
-    perm
-}
-
-fn enumerate(n: usize, overlap: &[Vec<usize>]) -> (Vec<Vec<usize>>, Vec<Vec<Option<usize>>>) {
-    // dp[mask][i] => mask which ends with i
-    let mut dp = vec![vec![0; n]; 1 << n];
-    // parent[mask][i] => previous i which leads to mask with i
-    let mut parent = vec![vec![None; n]; 1 << n];
-    for mask in 0..1 << n {
-        // try this end bit
-        for bit in 0..n {
-            if (mask >> bit) & 1 == 0 {
-                continue;
-            }
-            let pmask = mask ^ (1 << bit);
-            if pmask == 0 {
-                continue; // avoid parent->parent loop
-            }
-            for pi in 0..n {
-                if (pmask >> pi) & 1 == 0 {
-                    continue;
-                }
-                // For each possible parent, find its overlap
-                let val = dp[pmask][pi] + overlap[pi][bit];
-                if val > dp[mask][bit] {
-                    dp[mask][bit] = val;
-                    parent[mask][bit] = Some(pi);
-                }
-            }
-        }
-    }
-    (dp, parent)
-}
-
-fn get_overlap(a: &str, b: &str) -> usize {
-    let mut n = a.len().min(b.len());
-    while n > 0 {
-        if a.ends_with(&b[..n]) {
-            return n;
-        }
-        n -= 1;
-    }
-    0
+pub fn min_deletion_size(strs: &[&str]) -> i32 {
+    let n = strs[0].len();
+    (0..n)
+        .filter(|&i| {
+            strs.windows(2)
+                .any(|w| w[0].as_bytes()[i] > w[1].as_bytes()[i])
+        })
+        .count() as _
 }
 
 #[cfg(test)]
@@ -93,14 +23,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            shortest_superstring(&["alex", "loves", "leetcode"]),
-            "leetcodealexloves"
-        );
-        debug_assert_eq!(
-            shortest_superstring(&["catg", "ctaagt", "gcta", "ttca", "atgcatc"]),
-            "gctaagttcatgcatc"
-        );
+        debug_assert_eq!(min_deletion_size(&["cba", "daf", "ghi"]), 1);
+        debug_assert_eq!(min_deletion_size(&["a", "b"]), 0);
+        debug_assert_eq!(min_deletion_size(&["zyx", "wvu", "tsr"]), 3);
     }
 
     #[test]
