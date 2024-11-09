3c2ca78b1df46b9c286d5fb40f8152a9c666d6ef
pkusensei <ZW_08@outlook.com>
2024-11-09 13:56:33 -0500

    943 bitmask, dp and backtracking in a big mishmash

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,84 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn di_string_match(s: &str) -> Vec<i32> {
-    let n = s.len();
-    let mut res = Vec::with_capacity(1 + n);
-    let (mut low, mut high) = (0, n as i32);
-    for b in s.bytes() {
-        if b == b'D' {
-            res.push(high);
-            high -= 1;
-        } else {
-            res.push(low);
-            low += 1;
+pub fn shortest_superstring(words: &[&str]) -> String {
+    let n = words.len();
+    let mut overlap = vec![vec![0; n]; n];
+    for (i1, a) in words.iter().enumerate() {
+        for (i2, b) in words.iter().enumerate().skip(1 + i1) {
+            overlap[i1][i2] = get_overlap(a, b);
+            overlap[i2][i1] = get_overlap(b, a);
         }
     }
-    res.push(low);
+    let (dp, parent) = enumerate(n, &overlap);
+    let perm = build(n, dp, parent);
+    let mut res = words[perm[0]].to_string();
+    for w in perm.windows(2) {
+        let over = overlap[w[0]][w[1]];
+        res.push_str(&words[w[1]][over..]);
+    }
     res
+}
+
+fn build(n: usize, dp: Vec<Vec<usize>>, parent: Vec<Vec<Option<usize>>>) -> Vec<usize> {
+    let mut perm = Vec::with_capacity(n);
+    let mut mask = (1 << n) - 1;
+    // Find last added str
+    let mut p = (0..n).max_by_key(|&i| dp[mask][i]);
+    let mut seen = vec![false; n];
+    while let Some(v) = p {
+        perm.push(v);
+        seen[v] = true;
+        let p2 = parent[mask][v];
+        mask ^= 1 << v;
+        p = p2;
+    }
+    perm.reverse();
+    // add strs with no overlaps
+    perm.extend((0..n).filter(|&i| !seen[i]));
+    perm
+}
+
+fn enumerate(n: usize, overlap: &[Vec<usize>]) -> (Vec<Vec<usize>>, Vec<Vec<Option<usize>>>) {
+    // dp[mask][i] => mask which ends with i
+    let mut dp = vec![vec![0; n]; 1 << n];
+    // parent[mask][i] => previous i which leads to mask with i
+    let mut parent = vec![vec![None; n]; 1 << n];
+    for mask in 0..1 << n {
+        // try this end bit
+        for bit in 0..n {
+            if (mask >> bit) & 1 == 0 {
+                continue;
+            }
+            let pmask = mask ^ (1 << bit);
+            if pmask == 0 {
+                continue; // avoid parent->parent loop
+            }
+            for pi in 0..n {
+                if (pmask >> pi) & 1 == 0 {
+                    continue;
+                }
+                // For each possible parent, find its overlap
+                let val = dp[pmask][pi] + overlap[pi][bit];
+                if val > dp[mask][bit] {
+                    dp[mask][bit] = val;
+                    parent[mask][bit] = Some(pi);
+                }
+            }
+        }
+    }
+    (dp, parent)
+}
+
+fn get_overlap(a: &str, b: &str) -> usize {
+    let mut n = a.len().min(b.len());
+    while n > 0 {
+        if a.ends_with(&b[..n]) {
+            return n;
+        }
+        n -= 1;
+    }
+    0
 }
 
 #[cfg(test)]
@@ -30,9 +93,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(di_string_match("IDID"), [0, 4, 1, 3, 2]);
-        debug_assert_eq!(di_string_match("III"), [0, 1, 2, 3]);
-        debug_assert_eq!(di_string_match("DDI"), [3, 2, 0, 1]);
+        debug_assert_eq!(
+            shortest_superstring(&["alex", "loves", "leetcode"]),
+            "leetcodealexloves"
+        );
+        debug_assert_eq!(
+            shortest_superstring(&["catg", "ctaagt", "gcta", "ttca", "atgcatc"]),
+            "gctaagttcatgcatc"
+        );
     }
 
     #[test]
