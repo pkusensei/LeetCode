d86ae50ae7cae5f0028cffca6cfc75e483817fa7
pkusensei <ZW_08@outlook.com>
2024-11-08 21:34:53 -0500

    3133 min array end that keeps x's bit pattern

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,29 +2,54 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_area_rect(points: &[[i32; 2]]) -> i32 {
-    let points: HashSet<_> = points.iter().map(|p| [p[0], p[1]]).collect();
-    let mut res = None::<i32>;
-    for (i, &p1) in points.iter().enumerate() {
-        let [x1, y1] = p1;
-        for &p2 in points.iter().skip(1 + i) {
-            let [x2, y2] = p2;
-            if x1 != x2 && y1 != y2 && points.contains(&[x1, y2]) && points.contains(&[x2, y1]) {
-                let temp = ((x1 - x2) * (y1 - y2)).abs();
-                if let Some(ref mut v) = res {
-                    *v = (*v).min(temp)
-                } else {
-                    res = Some(temp);
-                }
-            }
+pub fn min_end(n: i32, x: i32) -> i64 {
+    // fit bits of (n-1) onto "empty" bits of x
+    // as if counting up from x for 0..n times
+    let mut bits = format!("{:b}", x).into_bytes();
+    let fill = format!("{:b}", n - 1).into_bytes();
+    let mut idx = bits.len() - 1;
+    for b in fill.into_iter().rev() {
+        while idx > 0 && bits[idx] != b'0' {
+            idx -= 1;
+        }
+        if idx > 0 {
+            bits[idx] = b;
+            idx -= 1;
+        } else {
+            bits.insert(0, b);
         }
     }
-    res.unwrap_or(0)
+    let mut res = 0;
+    for b in bits.into_iter() {
+        res <<= 1;
+        res |= i64::from(b - b'0');
+    }
+    res
+}
+
+fn consecutive_or(n: i32, x: i32) -> i64 {
+    let mut res = i64::from(x);
+    for _ in 1..n {
+        res = (res + 1) | i64::from(x);
+    }
+    res
+}
+
+fn bitmask(n: i32, x: i32) -> i64 {
+    let mut res: i64 = x.into();
+    let mut mask: i64 = 1;
+    let mut n: i64 = (n - 1).into();
+    while n > 0 {
+        if mask & i64::from(x) == 0 {
+            res |= (n & 1) * mask;
+            n >>= 1;
+        }
+        mask <<= 1;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -35,11 +60,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(min_area_rect(&[[1, 1], [1, 3], [3, 1], [3, 3], [2, 2]]), 4);
-        debug_assert_eq!(
-            min_area_rect(&[[1, 1], [1, 3], [3, 1], [3, 3], [4, 1], [4, 3]]),
-            2
-        );
+        debug_assert_eq!(bitmask(3, 4), 6);
+        debug_assert_eq!(bitmask(2, 7), 15);
     }
 
     #[test]
