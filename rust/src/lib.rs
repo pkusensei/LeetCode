e13ef6079e43fb67bc3bc6d15da7635f20926f7f
pkusensei <ZW_08@outlook.com>
2024-11-11 12:36:58 -0500

    960 longest increasing seq, but spanning multiple rows

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,33 +5,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn prime_sub_operation(nums: &[i32]) -> bool {
-    const PRIMES: [i32; 168] = [
-        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
-        97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
-        191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-        283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
-        401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
-        509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619,
-        631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743,
-        751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
-        877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
-    ];
-    let mut prev = 0;
-    'out: for &num in nums.iter() {
-        if num <= prev {
-            return false;
-        }
-        let i = PRIMES.partition_point(|&v| v < num);
-        for p in PRIMES[..i].iter().rev() {
-            if num - p > prev {
-                prev = num - p;
-                continue 'out;
+pub fn min_deletion_size(strs: &[&str]) -> i32 {
+    let n = strs[0].len();
+    let mut dp = vec![1; n];
+    for i1 in 1..n {
+        for i2 in 0..i1 {
+            if strs.iter().all(|s| s.as_bytes()[i1] >= s.as_bytes()[i2]) {
+                dp[i1] = dp[i1].max(1 + dp[i2])
             }
         }
-        prev = num;
     }
-    true
+    n as i32 - dp.into_iter().max().unwrap_or(1)
 }
 
 #[cfg(test)]
@@ -42,9 +26,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(prime_sub_operation(&[4, 9, 6, 10]));
-        debug_assert!(prime_sub_operation(&[6, 8, 11, 12]));
-        debug_assert!(!prime_sub_operation(&[5, 8, 3]));
+        debug_assert_eq!(min_deletion_size(&["babca", "bbazb"]), 3);
+        debug_assert_eq!(min_deletion_size(&["edcba"]), 4);
+        debug_assert_eq!(min_deletion_size(&["ghi", "def", "abc"]), 0);
     }
 
     #[test]
