80cc1ee2b80946505f9bcaf4e1060ea6ddad88a4
pkusensei <ZW_08@outlook.com>
2024-11-10 20:59:21 -0500

    2601 subtract by biggest prime

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,41 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn prison_after_n_days(mut cells: Vec<i32>, n: i32) -> Vec<i32> {
-    let mut map = std::collections::HashMap::new();
-    let mut idx = 0;
-    while idx <= n {
-        let num = to_num(&cells);
-        if let Some(prev) = map.get(&num) {
-            let period = idx - prev; // is 14
-            let count = (n - idx) / period;
-            idx += count * period - 1; // no idea why -1 is needed
-            map.clear();
-        } else {
-            if idx == n {
-                return cells;
-            }
-            map.insert(num, idx);
-            let mut next = cells.clone();
-            (next[0], next[7]) = (0, 0);
-            for i in 1..=6 {
-                next[i] = (cells[i - 1] == cells[i + 1]).into();
-            }
-            cells = next;
+pub fn prime_sub_operation(nums: &[i32]) -> bool {
+    const PRIMES: [i32; 168] = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+        97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
+        191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+        283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+        401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
+        509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619,
+        631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743,
+        751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
+        877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+    ];
+    let mut prev = 0;
+    'out: for &num in nums.iter() {
+        if num <= prev {
+            return false;
         }
-        idx += 1;
-    }
-    cells
-}
-
-fn to_num(bits: &[i32]) -> u8 {
-    let mut res = 0;
-    for (i, &b) in bits.iter().enumerate() {
-        if b > 0 {
-            res |= 1 << i;
+        let i = PRIMES.partition_point(|&v| v < num);
+        for p in PRIMES[..i].iter().rev() {
+            if num - p > prev {
+                prev = num - p;
+                continue 'out;
+            }
         }
+        prev = num;
     }
-    res
+    true
 }
 
 #[cfg(test)]
@@ -50,14 +42,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            prison_after_n_days(vec![0, 1, 0, 1, 1, 0, 0, 1], 7),
-            [0, 0, 1, 1, 0, 0, 0, 0]
-        );
-        debug_assert_eq!(
-            prison_after_n_days(vec![1, 0, 0, 1, 0, 0, 1, 0], 1000000000),
-            [0, 0, 1, 1, 1, 1, 1, 0]
-        );
+        debug_assert!(prime_sub_operation(&[4, 9, 6, 10]));
+        debug_assert!(prime_sub_operation(&[6, 8, 11, 12]));
+        debug_assert!(!prime_sub_operation(&[5, 8, 3]));
     }
 
     #[test]
