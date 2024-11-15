faf72c25d24263422649c4de693f310c1ce73726
pkusensei <ZW_08@outlook.com>
2024-11-15 10:26:04 -0500

    975 trace backwards

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,33 +5,66 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_length_of_shortest_subarray(arr: &[i32]) -> i32 {
+pub fn odd_even_jumps(arr: &[i32]) -> i32 {
     let n = arr.len();
-    let mut right = n - 1;
-    for (idx, &num) in arr.iter().enumerate().rev() {
-        if num <= arr[right] {
-            right = idx;
+    let mut dp = vec![[-1; 2]; n];
+    (0..n).map(|i| dfs(arr, i, 1, &mut dp)).sum()
+}
+
+fn dfs(nums: &[i32], idx: usize, odd: usize, dp: &mut [[i32; 2]]) -> i32 {
+    if idx >= nums.len() - 1 {
+        return 1;
+    }
+    if dp[idx][odd] > -1 {
+        return dp[idx][odd];
+    }
+    let res = if odd == 1 {
+        if let Some(jump) = nums[1 + idx..]
+            .iter()
+            .enumerate()
+            .filter(|(_, &v)| v >= nums[idx])
+            .min_by_key(|(_, &v)| v)
+            .map(|(i, _)| i + 1 + idx)
+        {
+            dfs(nums, jump, 1 - odd, dp)
         } else {
-            break;
+            0
         }
-    }
-    let mut left = 0;
-    for (idx, &num) in arr.iter().enumerate() {
-        if num >= arr[left] {
-            left = idx
-        } else {
-            break;
+    } else if let Some(jump) = nums[1 + idx..]
+        .iter()
+        .enumerate()
+        .filter(|(_, &v)| v <= nums[idx])
+        .max_by(|(i1, v1), (i2, v2)| v1.cmp(v2).then(i2.cmp(i1)))
+        .map(|(i, _)| i + 1 + idx)
+    {
+        dfs(nums, jump, 1 - odd, dp)
+    } else {
+        0
+    };
+    dp[idx][odd] = res;
+    res
+}
+
+fn with_btreemap(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let [mut odd, mut even] = [0, 1].map(|_| vec![false; n]);
+    odd[n - 1] = true;
+    even[n - 1] = true;
+    let mut res = 1;
+    let mut map = std::collections::BTreeMap::from([(nums[n - 1], n - 1)]);
+    for (idx, &num) in nums.iter().enumerate().rev().skip(1) {
+        if let Some((_, &i)) = map.range(num..).next() {
+            odd[idx] = even[i];
         }
-    }
-    let mut res = right;
-    let mut high = right;
-    for low in 0..=left {
-        while high < n && arr[low] > arr[high] {
-            high += 1;
+        if let Some((_, &i)) = map.range(..=num).next_back() {
+            even[idx] = odd[i];
         }
-        res = res.min(high.saturating_sub(1 + low));
+        if odd[idx] {
+            res += 1;
+        }
+        map.insert(num, idx);
     }
-    res as i32
+    res
 }
 
 #[cfg(test)]
@@ -42,25 +75,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            find_length_of_shortest_subarray(&[1, 2, 3, 10, 4, 2, 3, 5]),
-            3
-        );
-        debug_assert_eq!(find_length_of_shortest_subarray(&[5, 4, 3, 2, 1]), 4);
-        debug_assert_eq!(find_length_of_shortest_subarray(&[1, 2, 3]), 0);
+        debug_assert_eq!(with_btreemap(&[10, 13, 12, 14, 15]), 2);
+        debug_assert_eq!(with_btreemap(&[2, 3, 1, 1, 4]), 3);
+        debug_assert_eq!(with_btreemap(&[5, 1, 3, 4, 2]), 3);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(
-            find_length_of_shortest_subarray(&[1, 2, 3, 10, 0, 7, 8, 9]),
-            2
-        );
-        debug_assert_eq!(
-            find_length_of_shortest_subarray(&[16, 10, 0, 3, 22, 1, 14, 7, 1, 12, 15]),
-            8
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
