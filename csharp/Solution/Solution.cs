e1682e537047bf4395915a18408e20f5e06a9e29
pkusensei <ZW_08@outlook.com>
2024-11-17 14:13:29 -0500

    993 cousins in binary tree

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,91 +5,22 @@ namespace Solution;
 
 public class Solution
 {
-    public int ShortestSubarray(int[] nums, int k)
+    public bool IsCousins(TreeNode root, int x, int y)
     {
-        var prefix = new long[1 + nums.Length];
-        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
-        {
-            prefix[1 + idx] = prefix[idx] + num;
-        }
-        List<(int idx, long num)> queue = [];
-        var res = 1 + nums.Length;
-        foreach (var (idx, sum) in prefix.Select((n, i) => (i, n)))
-        {
-            while (queue.Count > 0 && queue.Last().num >= sum)
-            {
-                queue.RemoveAt(queue.Count - 1);
-            }
-            while (queue.Count > 0 && queue.First().num + k <= sum)
-            {
-                res = Math.Min(res, idx - queue.First().idx);
-                queue.RemoveAt(0);
-            }
-            queue.Add((idx, sum));
-        }
-        return res < 1 + nums.Length ? res : -1;
-    }
+        List<(int depth, int parent)> nodes = new(2);
+        Dfs(root, -1, 0);
+        return nodes[0].depth == nodes[1].depth && nodes[0].parent != nodes[1].parent;
 
-    public int WithPriorityQueue(int[] nums, int k)
-    {
-        var res = 1 + nums.Length;
-        // element => int idx
-        // priority => long sum 
-        PriorityQueue<int, long> pq = new();
-        long sum = 0;
-        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
+        void Dfs(TreeNode node, int parent, int depth)
         {
-            sum += num;
-            if (sum >= k) { res = Math.Min(res, 1 + idx); }
-
-            while (pq.TryPeek(out int i, out long peek) && sum - peek >= k)
+            if (node is null) { return; }
+            if (node.val == x || node.val == y)
             {
-                pq.Dequeue();
-                res = Math.Min(res, idx - i);
+                nodes.Add((depth, parent));
+                if (nodes.Count == 2) { return; }
             }
-            pq.Enqueue(idx, sum);
-        }
-        return res < 1 + nums.Length ? res : -1;
-    }
-
-    public int WithMonotonicStack(int[] nums, int k)
-    {
-        List<(long num, int idx)> stack = [(0, -1)];
-        long sum = 0;
-        var res = int.MaxValue;
-        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
-        {
-            sum += num;
-            while (stack.Count > 0 && stack.Last().num >= sum)
-            {
-                stack.RemoveAt(stack.Count - 1);
-            }
-            stack.Add((sum, idx));
-            var candidate = BinarySearch(sum - k);
-            if (candidate >= 0)
-            {
-                res = Math.Min(res, idx - stack[candidate].idx);
-            }
-        }
-        return res < 1 + nums.Length ? res : -1;
-
-        int BinarySearch(long target)
-        {
-            int left = 0;
-            int right = stack.Count - 1;
-            while (left <= right)
-            {
-                var mid = left + (right - left) / 2;
-                if (stack[mid].num <= target)
-                {
-                    left = 1 + mid;
-                }
-                else
-                {
-                    right = mid - 1;
-                }
-            }
-            return right;
+            Dfs(node.left, node.val, 1 + depth);
+            Dfs(node.right, node.val, 1 + depth);
         }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,23 +10,23 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 1 }, 1, 1)]
-    [DataRow(new[] { 1, 2 }, 4, -1)]
-    [DataRow(new[] { 2, -1, 2 }, 3, 3)]
-    public void TestMethod1(int[] nums, int k, int ans)
+    public void TestMethod1()
     {
-        Assert.AreEqual(ans, sol.WithMonotonicStack(nums, k));
+        var a = TreeNode.Make([1, 2, 3, 4]);
+        Assert.IsFalse(sol.IsCousins(a, 4, 3));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-
+        var a = TreeNode.Make([1, 2, 3, null, 4, null, 5]);
+        Assert.IsTrue(sol.IsCousins(a, 5, 4));
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-
+        var a = TreeNode.Make([1, 2, 3, null, 4]);
+        Assert.IsFalse(sol.IsCousins(a, 2, 3));
     }
 }
