47a099924cecbc44977c07a8c4f7f2a759ca4de4
pkusensei <ZW_08@outlook.com>
2024-11-17 18:35:06 -0500

    995 no I would happily go with my brute force

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,38 +5,95 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn oranges_rotting(grid: &mut [&mut [i32]]) -> i32 {
-    let mut queue = std::collections::VecDeque::new();
-    for (y, row) in grid.iter().enumerate() {
-        for (x, &v) in row.iter().enumerate() {
-            if v == 2 {
-                queue.push_back((x, y));
-            }
-        }
-    }
+pub fn min_k_bit_flips(nums: &mut [i32], k: i32) -> i32 {
+    let n = nums.len();
+    let k = k as usize;
+    let mut idx = 0;
     let mut res = 0;
-    while !queue.is_empty() {
-        let n = queue.len();
+    while idx <= n - k {
+        if nums[idx] == 1 {
+            idx += 1;
+            continue;
+        }
+        for v in nums[idx..].iter_mut().take(k) {
+            *v = 1 - *v;
+        }
         res += 1;
-        for _ in 0..n {
-            let Some((x, y)) = queue.pop_front() else {
-                continue;
-            };
-            for (nx, ny) in neighbors((x, y)) {
-                if let Some(v) = grid.get_mut(ny).and_then(|r| r.get_mut(nx)) {
-                    if *v == 1 {
-                        *v = 2;
-                        queue.push_back((nx, ny));
-                    }
-                }
+        idx += 1;
+    }
+    if nums.iter().all(|&v| v == 1) {
+        res
+    } else {
+        -1
+    }
+}
+
+fn with_array(nums: &[i32], k: i32) -> i32 {
+    let (n, k) = (nums.len(), k as usize);
+    let mut flipped = vec![false; n];
+    let mut valid_flips = 0;
+    let mut res = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        if idx >= k && flipped[idx - k] {
+            valid_flips -= 1;
+        }
+        if num == valid_flips & 1 {
+            if idx + k > n {
+                return -1;
             }
+            valid_flips += 1;
+            flipped[idx] = true;
+            res += 1;
         }
     }
-    if grid.iter().any(|r| r.iter().any(|&v| v == 1)) {
-        -1
-    } else {
-        (res - 1).max(0)
+    res
+}
+
+fn with_deque(nums: &[i32], k: i32) -> i32 {
+    let (n, k) = (nums.len(), k as usize);
+    let mut flip_queue = std::collections::VecDeque::with_capacity(k);
+    let mut flipped = 0;
+    let mut res = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        if idx >= k {
+            let Some(v) = flip_queue.pop_front() else {
+                break;
+            };
+            flipped ^= v;
+        }
+        if num == flipped {
+            if idx + k > n {
+                return -1;
+            }
+            flip_queue.push_back(1);
+            flipped ^= 1;
+            res += 1;
+        } else {
+            flip_queue.push_back(0);
+        }
     }
+    res
+}
+
+fn with_constant_space(nums: &mut [i32], k: i32) -> i32 {
+    let (n, k) = (nums.len(), k as usize);
+    let mut curr = 0;
+    let mut res = 0;
+    for idx in 0..n {
+        if idx >= k && nums[idx - k] == 2 {
+            curr -= 1;
+            // nums[idx - k] -= 2; // restores original state
+        }
+        if curr & 1 == nums[idx] {
+            if idx + k > n {
+                return -1;
+            }
+            nums[idx] = 2;
+            curr += 1;
+            res += 1;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -47,15 +104,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            oranges_rotting(&mut [&mut [2, 1, 1], &mut [1, 1, 0], &mut [0, 1, 1]]),
-            4
-        );
-        debug_assert_eq!(
-            oranges_rotting(&mut [&mut [2, 1, 1], &mut [0, 1, 1], &mut [1, 0, 1]]),
-            -1
-        );
-        debug_assert_eq!(oranges_rotting(&mut [&mut [0, 2]]), 0);
+        debug_assert_eq!(with_constant_space(&mut [0, 1, 0], 1), 2);
+        debug_assert_eq!(with_constant_space(&mut [1, 1, 0], 2), -1);
+        debug_assert_eq!(with_constant_space(&mut [0, 0, 0, 1, 0, 1, 1, 0], 3), 3);
     }
 
     #[test]
