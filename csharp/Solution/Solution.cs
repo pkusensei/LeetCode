c82c6a3dc93daa2c95b0444d01785930eeba19bb
pkusensei <ZW_08@outlook.com>
2024-11-17 08:36:22 -0500

    862 shortest subarray with sum >= k

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,30 +5,91 @@ namespace Solution;
 
 public class Solution
 {
-    public string SmallestFromLeaf(TreeNode root)
+    public int ShortestSubarray(int[] nums, int k)
     {
-        var res = "";
-        Backtrack(root, [], ref res);
-        return res;
+        var prefix = new long[1 + nums.Length];
+        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
+        {
+            prefix[1 + idx] = prefix[idx] + num;
+        }
+        List<(int idx, long num)> queue = [];
+        var res = 1 + nums.Length;
+        foreach (var (idx, sum) in prefix.Select((n, i) => (i, n)))
+        {
+            while (queue.Count > 0 && queue.Last().num >= sum)
+            {
+                queue.RemoveAt(queue.Count - 1);
+            }
+            while (queue.Count > 0 && queue.First().num + k <= sum)
+            {
+                res = Math.Min(res, idx - queue.First().idx);
+                queue.RemoveAt(0);
+            }
+            queue.Add((idx, sum));
+        }
+        return res < 1 + nums.Length ? res : -1;
     }
 
-    static void Backtrack(TreeNode node, List<char> curr, ref string res)
+    public int WithPriorityQueue(int[] nums, int k)
     {
-        if (node is null) { return; }
-        curr.Insert(0, (char)('a' + node.val));
-        if (node.left is null && node.right is null)
+        var res = 1 + nums.Length;
+        // element => int idx
+        // priority => long sum 
+        PriorityQueue<int, long> pq = new();
+        long sum = 0;
+        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
         {
-            var s = string.Join(null, curr);
-            if (res.Length == 0 || s.CompareTo(res) < 0)
+            sum += num;
+            if (sum >= k) { res = Math.Min(res, 1 + idx); }
+
+            while (pq.TryPeek(out int i, out long peek) && sum - peek >= k)
             {
-                res = s;
+                pq.Dequeue();
+                res = Math.Min(res, idx - i);
+            }
+            pq.Enqueue(idx, sum);
+        }
+        return res < 1 + nums.Length ? res : -1;
+    }
+
+    public int WithMonotonicStack(int[] nums, int k)
+    {
+        List<(long num, int idx)> stack = [(0, -1)];
+        long sum = 0;
+        var res = int.MaxValue;
+        foreach (var (idx, num) in nums.Select((n, i) => (i, n)))
+        {
+            sum += num;
+            while (stack.Count > 0 && stack.Last().num >= sum)
+            {
+                stack.RemoveAt(stack.Count - 1);
+            }
+            stack.Add((sum, idx));
+            var candidate = BinarySearch(sum - k);
+            if (candidate >= 0)
+            {
+                res = Math.Min(res, idx - stack[candidate].idx);
             }
         }
-        else
+        return res < 1 + nums.Length ? res : -1;
+
+        int BinarySearch(long target)
         {
-            Backtrack(node.left, curr, ref res);
-            Backtrack(node.right, curr, ref res);
+            int left = 0;
+            int right = stack.Count - 1;
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                if (stack[mid].num <= target)
+                {
+                    left = 1 + mid;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return right;
         }
-        curr.RemoveAt(0);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,26 +10,23 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(new[] { 1 }, 1, 1)]
+    [DataRow(new[] { 1, 2 }, 4, -1)]
+    [DataRow(new[] { 2, -1, 2 }, 3, 3)]
+    public void TestMethod1(int[] nums, int k, int ans)
     {
-        var a = TreeNode.Make([0, 1, 2, 3, 4, 3, 4]);
-        var b = sol.SmallestFromLeaf(a);
-        Assert.AreEqual("dba", b);
+        Assert.AreEqual(ans, sol.WithMonotonicStack(nums, k));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([25, 1, 3, 1, 3, 0, 2]);
-        var b = sol.SmallestFromLeaf(a);
-        Assert.AreEqual("adz", b);
+
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-        var a = TreeNode.Make([2, 2, 1, null, 1, 0, null, 0]);
-        var b = sol.SmallestFromLeaf(a);
-        Assert.AreEqual("abc", b);
+
     }
 }
