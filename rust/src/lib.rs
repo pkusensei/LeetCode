d93a2b6929f7cb342db3fd1e75c0460e18698b0d
pkusensei <ZW_08@outlook.com>
2024-11-18 11:54:15 -0500

    1000 dp and work backwards

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,50 +5,79 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_rook_captures(board: [[char; 8]; 8]) -> i32 {
-    let mut start = [0, 0];
-    for (y, row) in board.iter().enumerate() {
-        for (x, &ch) in row.iter().enumerate() {
-            if ch == 'R' {
-                start = [x, y];
+pub fn merge_stones(stones: &[i32], k: i32) -> i32 {
+    let (n, k) = (stones.len(), k as usize);
+    if (n - 1) % (k - 1) > 0 {
+        return -1;
+    }
+    let mut prefix = Vec::with_capacity(n);
+    for &num in stones.iter() {
+        prefix.push(num + prefix.last().unwrap_or(&0));
+    }
+    let mut dp = vec![vec![vec![-1; 1 + k]; n]; n];
+    dfs(0, n - 1, 1, &prefix, k, &mut dp)
+}
+
+fn dfs(
+    i1: usize,
+    i2: usize,
+    piles: usize,
+    prefix: &[i32],
+    k: usize,
+    dp: &mut [Vec<Vec<i32>>],
+) -> i32 {
+    const INF: i32 = i32::MAX / 4;
+    if i1 == i2 {
+        // merge [i..i] into one is noop
+        // otherwise it is impossible
+        return if piles == 1 { 0 } else { INF };
+    }
+    if dp[i1][i2][piles] > -1 {
+        return dp[i1][i2][piles];
+    }
+    let res = if piles == 1 {
+        // any merged pile was k piles before merge
+        dfs(i1, i2, k, prefix, k, dp)
+            + if i1 == 0 {
+                prefix[i2]
+            } else {
+                prefix[i2] - prefix[i1 - 1]
+            }
+    } else {
+        // or try each combination
+        (i1..i2)
+            .map(|t| dfs(i1, t, 1, prefix, k, dp) + dfs(1 + t, i2, piles - 1, prefix, k, dp))
+            .min()
+            .unwrap_or(INF)
+    };
+    dp[i1][i2][piles] = res;
+    res
+}
+
+fn bottom_up(stones: &[i32], k: i32) -> i32 {
+    let (n, k) = (stones.len(), k as usize);
+    if (n - 1) % (k - 1) > 0 {
+        return -1;
+    }
+    let mut prefix = Vec::with_capacity(1 + n);
+    prefix.push(0);
+    for &num in stones.iter() {
+        prefix.push(num + prefix.last().unwrap_or(&0));
+    }
+    let mut dp = vec![vec![0; n]; n];
+    for len in k..=n {
+        for start in 0..=n - len {
+            let end = start + len - 1;
+            dp[start][end] = i32::MAX;
+            for i in (start..end).step_by(k - 1) {
+                dp[start][end] = dp[start][end].min(dp[start][i] + dp[1 + i][end]);
+            }
+            if (end - start) % (k - 1) == 0 {
+                dp[start][end] += prefix[1 + end] - prefix[start];
             }
         }
     }
-    let [x, y] = start;
-    let mut res = 0;
-    for i in (0..y).rev() {
-        if board[i][x] == 'p' {
-            res += 1;
-            break;
-        } else if board[i][x] == 'B' {
-            break;
-        }
-    }
-    for i in y..8 {
-        if board[i][x] == 'p' {
-            res += 1;
-            break;
-        } else if board[i][x] == 'B' {
-            break;
-        }
-    }
-    for i in (0..x).rev() {
-        if board[y][i] == 'p' {
-            res += 1;
-            break;
-        } else if board[y][i] == 'B' {
-            break;
-        }
-    }
-    for i in x..8 {
-        if board[y][i] == 'p' {
-            res += 1;
-            break;
-        } else if board[y][i] == 'B' {
-            break;
-        }
-    }
-    res
+    dp[0][n - 1]
 }
 
 #[cfg(test)]
@@ -59,45 +88,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            num_rook_captures([
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', 'p', '.', '.', '.', '.'],
-                ['.', '.', '.', 'R', '.', '.', '.', 'p'],
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', 'p', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.']
-            ]),
-            3
-        );
-        debug_assert_eq!(
-            num_rook_captures([
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', 'p', 'p', 'p', 'p', 'p', '.', '.'],
-                ['.', 'p', 'p', 'B', 'p', 'p', '.', '.'],
-                ['.', 'p', 'B', 'R', 'B', 'p', '.', '.'],
-                ['.', 'p', 'p', 'B', 'p', 'p', '.', '.'],
-                ['.', 'p', 'p', 'p', 'p', 'p', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.']
-            ]),
-            0
-        );
-        debug_assert_eq!(
-            num_rook_captures([
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', 'p', '.', '.', '.', '.'],
-                ['.', '.', '.', 'p', '.', '.', '.', '.'],
-                ['p', 'p', '.', 'R', '.', 'p', 'B', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', 'B', '.', '.', '.', '.'],
-                ['.', '.', '.', 'p', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.', '.']
-            ]),
-            3
-        );
+        debug_assert_eq!(bottom_up(&[3, 2, 4, 1], 2), 20);
+        debug_assert_eq!(bottom_up(&[3, 2, 4, 1], 3), -1);
+        debug_assert_eq!(bottom_up(&[3, 5, 1, 2, 6], 3), 25);
     }
 
     #[test]
