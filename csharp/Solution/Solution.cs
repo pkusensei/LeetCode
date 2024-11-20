c5f77c69ec1b1bb23a21d5a60b40a9f38e69dd14
pkusensei <ZW_08@outlook.com>
2024-11-20 15:52:22 -0500

    1025 max diff between ancestor and leaf in binary tree

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,17 +5,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int SumRootToLeaf(TreeNode root)
+    public int MaxAncestorDiff(TreeNode root)
     {
         if (root is null) { return 0; }
-        return Dfs(root, 0);
+        return Dfs(root, root.val, root.val);
     }
 
-    static int Dfs(TreeNode node, int curr)
+    static int Dfs(TreeNode node, int min, int max)
     {
-        if (node is null) { return 0; }
-        curr = (curr << 1) | node.val;
-        if (node.left is null && node.right is null) { return curr; }
-        return Dfs(node.left, curr) + Dfs(node.right, curr);
+        if (node is null) { return max - min; }
+        min = Math.Min(min, node.val);
+        max = Math.Max(max, node.val);
+        return Math.Max(Math.Max(max - min, Dfs(node.left, min, max)), Dfs(node.right, min, max));
     }
 }
