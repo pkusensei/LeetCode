96f5cd73c33ed019ea38bee39c8eee3d30ce81a1
pkusensei <ZW_08@outlook.com>
2024-11-20 15:52:22 -0500

    1026 max diff between ancestor and leaf in binary tree

--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -9,19 +9,26 @@ public class UnitTest
 {
     readonly Solution.Solution sol = new();
 
-    [TestMethod]
-    [DataRow(new int[] { 1, 0, 1, 0, 1, 0, 1 }, 22)]
-    [DataRow(new int[] { 0 }, 0)]
-    public void TestMethod1(int[] nums, int c)
+    (int exp, int act) Run(IList<int?> nums, int c)
     {
         var a = TreeNode.Make(nums);
-        var b = sol.SumRootToLeaf(a);
-        Assert.AreEqual(c, b);
+        var b = sol.MaxAncestorDiff(a);
+        return (c, b);
     }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var t = Run([8, 3, 10, 1, 6, null, 14, null, null, 4, 7, 13], 7);
+        Assert.AreEqual(t.exp, t.act);
+    }
+
 
     [TestMethod]
     public void TestMethod2()
     {
+        var t = Run([1, null, 2, null, 0, 3], 3);
+        Assert.AreEqual(t.exp, t.act);
     }
 
     [TestMethod]
