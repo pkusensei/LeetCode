29168c0e38ca542ce831e52d8e8f01bf13089d1e
pkusensei <ZW_08@outlook.com>
2024-11-20 15:31:54 -0500

    1025 apparently odd numbers are a problem

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,22 +5,8 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn video_stitching(clips: &mut [[i32; 2]], time: i32) -> i32 {
-    let time = time as usize;
-    clips.sort_unstable();
-    let mut dp = vec![101; 1 + time];
-    dp[0] = 0;
-    for clip in clips.iter() {
-        let [a, b] = [clip[0], clip[1]].map(|v| v as usize);
-        for i in 1 + a..=b.min(time) {
-            dp[i] = dp[i].min(1 + dp[a])
-        }
-    }
-    if dp[time] == 101 {
-        -1
-    } else {
-        dp[time]
-    }
+pub fn divisor_game(n: i32) -> bool {
+    n & 1 == 0
 }
 
 #[cfg(test)]
@@ -31,41 +17,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            video_stitching(&mut [[0, 2], [4, 6], [8, 10], [1, 9], [1, 5], [5, 9]], 10),
-            3
-        );
-        debug_assert_eq!(video_stitching(&mut [[0, 1], [1, 2]], 5), -1);
-        debug_assert_eq!(
-            video_stitching(
-                &mut [
-                    [0, 1],
-                    [6, 8],
-                    [0, 2],
-                    [5, 6],
-                    [0, 4],
-                    [0, 3],
-                    [6, 7],
-                    [1, 3],
-                    [4, 7],
-                    [1, 4],
-                    [2, 5],
-                    [2, 6],
-                    [3, 4],
-                    [4, 5],
-                    [5, 7],
-                    [6, 9]
-                ],
-                9
-            ),
-            3
-        );
+        debug_assert!(divisor_game(2));
+        debug_assert!(!divisor_game(3));
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(video_stitching(&mut [[0, 4], [2, 8]], 5), 2)
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
