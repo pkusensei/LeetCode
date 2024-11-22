69b8ca1bab2a4bd03757c1e43de6e800c45113f8
pkusensei <ZW_08@outlook.com>
2024-11-22 09:20:05 -0500

    1035 longest common subseq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,43 +2,22 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn color_border(mut grid: Vec<Vec<i32>>, row: i32, col: i32, color: i32) -> Vec<Vec<i32>> {
-        let (rows, cols) = get_dimensions(&grid);
-        let [row, col] = [row, col].map(|v| v as usize);
-        let mark = grid[row][col];
-        let mut seen = vec![vec![false; cols]; rows];
-        seen[row][col] = true;
-        let mut front = HashSet::new();
-        let mut queue = VecDeque::from([[row, col]]);
-        while let Some([r, c]) = queue.pop_front() {
-            if r == 0 || r == rows - 1 || c == 0 || c == cols - 1 {
-                front.insert([r, c]);
-            }
-            for (nr, nc) in neighbors((r, c)) {
-                match grid.get(nr).and_then(|row| row.get(nc)) {
-                    None => {
-                        front.insert([r, c]);
-                    }
-                    Some(&v) if v == mark && !seen[nr][nc] => {
-                        queue.push_back([nr, nc]);
-                        seen[nr][nc] = true;
-                    }
-                    Some(&v) if v == mark => (),
-                    _ => {
-                        front.insert([r, c]);
-                    }
-                }
+pub fn max_uncrossed_lines(nums1: &[i32], nums2: &[i32]) -> i32 {
+    let (n1, n2) = (nums1.len(), nums2.len());
+    let mut dp = vec![vec![0; 1 + n2]; 1 + n1];
+    for i1 in 1..=n1 {
+        for i2 in 1..=n2 {
+            if nums1[i1 - 1] == nums2[i2 - 1] {
+                dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1];
+            } else {
+                dp[i1][i2] = dp[i1 - 1][i2].max(dp[i1][i2 - 1])
             }
         }
-        for [r, c] in front.into_iter() {
-            grid[r][c] = color;
-        }
-        grid
+    }
+    dp[n1][n2]
 }
 
 #[cfg(test)]
@@ -49,17 +28,14 @@ mod tests {
 
     #[test]
     fn basics() {
+        debug_assert_eq!(max_uncrossed_lines(&[1, 4, 2], &[1, 2, 4]), 2);
         debug_assert_eq!(
-            color_border(vec![vec![1, 1], vec![1, 2]], 0, 0, 3),
-            [[3, 3], [3, 2]]
+            max_uncrossed_lines(&[2, 5, 1, 2, 5], &[10, 5, 2, 1, 5, 2]),
+            3
         );
         debug_assert_eq!(
-            color_border(vec![vec![1, 2, 2], vec![2, 3, 2]], 0, 1, 3),
-            [[1, 3, 3], [2, 3, 3]]
-        );
-        debug_assert_eq!(
-            color_border(vec![vec![1, 1, 1], vec![1, 1, 1], vec![1, 1, 1]], 1, 1, 2),
-            [[2, 2, 2], [2, 1, 2], [2, 2, 2]]
+            max_uncrossed_lines(&[1, 3, 7, 1, 7, 5], &[1, 9, 2, 5, 1]),
+            2
         );
     }
 
