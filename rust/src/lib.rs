4d11cb34a4f258ef7ae73964609e8bb90f7e1d94
pkusensei <ZW_08@outlook.com>
2024-11-22 17:18:00 -0500

    1037 shoelace formula

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,64 +2,14 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_escape_possible(blocked: &[[i32; 2]], source: [i32; 2], target: [i32; 2]) -> bool {
-    let blocks: HashSet<_> = blocked.iter().map(|v| [v[0], v[1]]).collect();
-    bfs(source, target, &blocks) && bfs(target, source, &blocks)
-    // dfs(source, target, &blocks, &mut HashSet::new())
-    //     && dfs(target, source, &blocks, &mut HashSet::new())
-}
-
-const LIMIT: i32 = 1_000_000;
-
-fn bfs(source: [i32; 2], target: [i32; 2], blocks: &HashSet<[i32; 2]>) -> bool {
-    let mut queue = VecDeque::from([source]);
-    let mut seen = HashSet::from([source]);
-    while let Some([x, y]) = queue.pop_front() {
-        for next in [[0, 1], [0, -1], [1, 0], [-1, 0]].map(|[dx, dy]| [dx + x, dy + y]) {
-            if next.iter().any(|v| !(0..LIMIT).contains(v)) {
-                continue;
-            }
-            if blocks.contains(&next) || !seen.insert(next) {
-                continue;
-            }
-            if next == target {
-                return true;
-            }
-            queue.push_back(next);
-        }
-        if seen.len() == 20_000 {
-            return true;
-        }
-    }
-    false
-}
-
-// stack overflow
-fn dfs(
-    source: [i32; 2],
-    target: [i32; 2],
-    blocks: &HashSet<[i32; 2]>,
-    seen: &mut HashSet<[i32; 2]>,
-) -> bool {
-    if source.iter().any(|v| !(0..LIMIT).contains(v)) {
-        return false;
-    }
-    if blocks.contains(&source) || !seen.insert(source) {
-        return false;
-    }
-    if source == target || seen.len() > 20_000 {
-        return true;
-    }
-    let [x, y] = source;
-    dfs([x - 1, y], target, blocks, seen)
-        || dfs([x + 1, y], target, blocks, seen)
-        || dfs([x, y - 1], target, blocks, seen)
-        || dfs([x, y + 1], target, blocks, seen)
+pub fn is_boomerang(points: [[i32; 2]; 3]) -> bool {
+    points[0][0] * (points[1][1] - points[2][1])
+        + points[1][0] * (points[2][1] - points[0][1])
+        + points[2][0] * (points[0][1] - points[1][1])
+        != 0
 }
 
 #[cfg(test)]
@@ -70,8 +20,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert!(!is_escape_possible(&[[0, 1], [1, 0]], [0, 0], [0, 2]));
-        debug_assert!(is_escape_possible(&[], [0, 0], [999999, 999999]));
+        debug_assert!(is_boomerang([[1, 1], [2, 3], [3, 2]]));
+        debug_assert!(!is_boomerang([[1, 1], [2, 2], [3, 3]]));
     }
 
     #[test]
