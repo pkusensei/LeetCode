494c8457f3ab0975a00c95b5c854daf3499680a8
pkusensei <ZW_08@outlook.com>
2024-11-25 14:45:49 -0500

    1046 a heap, a fine heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,53 +2,20 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-const P: i64 = 127;
-const MOD: i64 = 1_000_000_007;
-
-pub fn longest_dup_substring(s: &str) -> String {
-    let n = s.len();
-    let [mut prefix, mut pows] = [0, 1].map(|_| Vec::with_capacity(n));
-    prefix.push(0);
-    pows.push(1);
-    for b in s.bytes() {
-        prefix.push((prefix.last().unwrap_or(&0) * P + i64::from(b)) % MOD);
-        pows.push((pows.last().unwrap_or(&1) * P) % MOD);
-    }
-
-    let (mut left, mut right) = (0, n);
-    let mut res = "";
-    while left < right {
-        let mid = left + (right - left) / 2;
-        if let Some(v) = check(s, mid, &prefix, pows[mid]) {
-            if v.len() > res.len() {
-                res = v;
-            }
-            left = mid + 1
-        } else {
-            right = mid
+pub fn last_stone_weight(stones: Vec<i32>) -> i32 {
+    let mut heap = std::collections::BinaryHeap::from(stones);
+    while heap.len() > 1 {
+        let (Some(a), Some(b)) = (heap.pop(), heap.pop()) else {
+            break;
+        };
+        if a != b {
+            heap.push(a.abs_diff(b) as i32);
         }
     }
-    res.to_string()
-}
-
-fn check<'a>(s: &'a str, mid: usize, prefix: &[i64], pow: i64) -> Option<&'a str> {
-    let n = s.len();
-    let mut map = HashMap::new();
-    for i1 in 0..=n - mid {
-        let a = (prefix[i1 + mid] - prefix[i1] * pow).rem_euclid(MOD);
-        if let Some(&i2) = map.get(&a) {
-            if s[i1..i1 + mid] == s[i2..i2 + mid] {
-                return Some(&s[i1..i1 + mid]);
-            }
-        }
-        map.insert(a, i1);
-    }
-    None
+    heap.pop().unwrap_or(0)
 }
 
 #[cfg(test)]
@@ -59,18 +26,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(longest_dup_substring("banana"), "ana");
-        debug_assert_eq!(longest_dup_substring("abcd"), "");
+        debug_assert_eq!(last_stone_weight([2, 7, 4, 1, 8, 1].into()), 1);
+        debug_assert_eq!(last_stone_weight([1].into()), 1);
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(longest_dup_substring("aa"), "a");
-        debug_assert_eq!(
-            longest_dup_substring("nnpxouomcofdjuujloanjimymadkuepightrfodmauhrsy"),
-            "ma"
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
