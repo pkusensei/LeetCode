9c2b0fd605db196c79b70711d811daa0d591fc2c
Zx Wang <ZW_08@outlook.com>
2024-11-26 13:27:24 -0500

    1071 gcd of strs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,46 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn smallest_equivalent_string(s1: &str, s2: &str, base_str: &str) -> String {
-    let mut dsu = DSU::new();
-    for (a, b) in s1.bytes().zip(s2.bytes()) {
-        dsu.union((a - b'a').into(), (b - b'a').into());
-    }
-    base_str
-        .bytes()
-        .map(|b| char::from(dsu.find((b - b'a').into()) as u8 + b'a'))
-        .collect()
-}
-
-#[derive(Debug, Clone, Copy)]
-struct DSU {
-    parent: [usize; 26],
-}
-
-impl DSU {
-    fn new() -> Self {
-        let mut parent = [0; 26];
-        for (i, v) in parent.iter_mut().enumerate() {
-            *v = i
+pub fn gcd_of_strings(str1: &str, str2: &str) -> String {
+    let (n1, n2) = (str1.len(), str2.len());
+    let n = n1.min(n2);
+    let mut res = "";
+    for len in 1..=n {
+        if n1 % len > 0 || n2 % len > 0 {
+            continue;
         }
-        Self { parent }
-    }
-
-    fn find(&mut self, x: usize) -> usize {
-        if self.parent[x] != x {
-            self.parent[x] = self.find(self.parent[x])
-        }
-        self.parent[x]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        match rx.cmp(&ry) {
-            std::cmp::Ordering::Less => self.parent[ry] = rx,
-            std::cmp::Ordering::Equal => (),
-            std::cmp::Ordering::Greater => self.parent[rx] = ry,
+        let prefix = &str1[..len];
+        if [str1, str2].into_iter().all(|s| {
+            s.as_bytes()
+                .chunks_exact(len)
+                .all(|w| w == prefix.as_bytes())
+        }) {
+            res = prefix;
         }
     }
+    res.into()
 }
 
 #[cfg(test)]
@@ -55,15 +33,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(
-            smallest_equivalent_string("parker", "morris", "parser"),
-            "makkek"
-        );
-        debug_assert_eq!(smallest_equivalent_string("hello", "world", "hold"), "hdld");
-        debug_assert_eq!(
-            smallest_equivalent_string("leetcode", "programs", "sourcecode"),
-            "aauaaaaada"
-        );
+        debug_assert_eq!(gcd_of_strings("ABCABC", "ABC"), "ABC");
+        debug_assert_eq!(gcd_of_strings("ABABAB", "ABAB"), "AB");
+        debug_assert_eq!(gcd_of_strings("LEET", "CODE"), "");
     }
 
     #[test]
