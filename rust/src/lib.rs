58917411fb53d90016f61d34b36b5af7db786118
Zx Wang <ZW_08@outlook.com>
2024-11-26 12:02:43 -0500

    1054 no consecutive dups

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,27 +2,40 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{BinaryHeap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn prev_perm_opt1(mut arr: Vec<i32>) -> Vec<i32> {
-    let Some(i1) =
-        arr.windows(2)
-            .enumerate()
-            .rev()
-            .find_map(|(i, w)| if w[0] > w[1] { Some(i) } else { None })
-    else {
-        return arr;
-    };
-        let mut i2 = 1 + i1;
-        let n = arr.len();
-        for i in 1 + i1..n {
-            if (1 + arr[i2]..arr[i1]).contains(&arr[i]) {
-                i2 = i
+pub fn rearrange_barcodes(barcodes: &[i32]) -> Vec<i32> {
+    let mut heap: BinaryHeap<_> = barcodes
+        .iter()
+        .fold(HashMap::new(), |mut acc, &num| {
+            *acc.entry(num).or_insert(0) += 1;
+            acc
+        })
+        .into_iter()
+        .map(|(num, count)| (count, num))
+        .collect();
+    let mut res = Vec::with_capacity(barcodes.len());
+    while let Some((count, num)) = heap.pop() {
+        if res.last().is_some_and(|&v| v == num) {
+            let Some((c, n)) = heap.pop() else {
+                break;
+            };
+            res.push(n);
+            if c > 1 {
+                heap.push((c - 1, n));
+            }
+            heap.push((count, num));
+        } else {
+            res.push(num);
+            if count > 1 {
+                heap.push((count - 1, num));
             }
         }
-        arr.swap(i1, i2);
-    arr
+    }
+    res
 }
 
 #[cfg(test)]
@@ -33,15 +46,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(prev_perm_opt1(vec![3, 2, 1]), [3, 1, 2]);
-        debug_assert_eq!(prev_perm_opt1(vec![1, 1, 5]), [1, 1, 5]);
-        debug_assert_eq!(prev_perm_opt1(vec![1, 9, 4, 6, 7]), [1, 7, 4, 6, 9]);
+        debug_assert_eq!(rearrange_barcodes(&[1, 1, 1, 2, 2, 2]), [2, 1, 2, 1, 2, 1]);
+        debug_assert_eq!(
+            rearrange_barcodes(&[1, 1, 1, 1, 2, 2, 3, 3]),
+            [1, 3, 1, 2, 1, 3, 2, 1]
+        );
     }
 
     #[test]
-    fn test() {
-        debug_assert_eq!(prev_perm_opt1(vec![3, 1, 1, 3]), [1, 3, 1, 3]);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
