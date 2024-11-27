86dcb3a2cd4166946d7b0fe02db438e1e57fd46f
Zx Wang <ZW_08@outlook.com>
2024-11-27 10:34:43 -0500

    1080 insufficient nodes in binary tree

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,17 +6,22 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode BstToGst(TreeNode root)
+    public TreeNode SufficientSubset(TreeNode root, int limit)
     {
-        Dfs(root, 0);
+        var n = Dfs(root, 0, limit);
+        if (n < limit) { return null; }
         return root;
     }
 
-    static int Dfs(TreeNode node, int num)
+    static int Dfs(TreeNode node, int curr, int limit)
     {
-        if (node is null) { return num; }
-        var v = Dfs(node.right, num);
-        node.val += v;
-        return Dfs(node.left, node.val);
+        if (node is null) { return int.MinValue; }
+        var sum = node.val + curr;
+        if (node.left is null && node.right is null) { return sum; }
+        var left = Dfs(node.left, sum, limit);
+        var right = Dfs(node.right, sum, limit);
+        node.left = left < limit ? null : node.left;
+        node.right = right < limit ? null : node.right;
+        return Math.Max(left, right);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,21 +12,32 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = TreeNode.Make([4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8]);
-        var b = sol.BstToGst(a);
-        Assert.AreEqual("[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]", b.ToString());
+        var a = TreeNode.Make(new int[] { 1, 2, 3, 4, -99, -99, 7, 8, 9, -99, -99, 12, 13, -99, 14 });
+        var b = sol.SufficientSubset(a, 1);
+        Assert.AreEqual("[1,2,3,4,null,null,7,8,9,null,14]", b.ToString());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([0, null, 1]);
-        var b = sol.BstToGst(a);
-        Assert.AreEqual("[1,null,1]", b.ToString());
+        var a = TreeNode.Make([5, 4, 8, 11, null, 17, 4, 7, 1, null, null, 5, 3]);
+        var b = sol.SufficientSubset(a, 22);
+        Assert.AreEqual("[5,4,8,11,null,17,4,7,null,null,null,5]", b.ToString());
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([1, 2, -3, -5, null, 4, null]);
+        var b = sol.SufficientSubset(a, -1);
+        Assert.AreEqual("[1,null,-3,4]", b.ToString());
+    }
+
+    [TestMethod]
+    public void TestMethod4()
+    {
+        var a = TreeNode.Make(new int[] { 10, 5, 10 });
+        var b = sol.SufficientSubset(a, 21);
+        Assert.IsTrue(b is null);
     }
 }
