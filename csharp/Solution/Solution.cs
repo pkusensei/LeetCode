9f2627ef96bec857769c352f7b183499d9752fdb
pkusensei <ZW_08@outlook.com>
2024-11-30 11:01:24 -0500

    1123 as LC865

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -4,29 +4,50 @@ using Solution.Tree;
 
 namespace Solution;
 
-public class H2O
+public class Solution
 {
-    int count = 0;
-    readonly SemaphoreSlim hy = new(1, 1);
-    readonly SemaphoreSlim ox = new(0, 1);
-
-    public H2O() { }
-
-    public void Hydrogen(Action releaseHydrogen)
+    public TreeNode LcaDeepestLeaves(TreeNode root)
     {
-        hy.Wait();
-        releaseHydrogen();
-        count += 1;
-        if (count == 1) { hy.Release(); }
-        else { ox.Release(); }
+        Dictionary<TreeNode, int> depths = [];
+        var depth = Depth(root, 0);
+        // return Find(root);
+
+        return Dfs(root, 0).node;
+
+        int Depth(TreeNode node, int depth)
+        {
+            if (node is null) { return depth - 1; }
+            var left = Depth(node.left, 1 + depth);
+            var right = Depth(node.right, 1 + depth);
+            var d = Math.Max(left, right);
+            depths.Add(node, d);
+            return d;
+        }
+
+        TreeNode Find(TreeNode node)
+        {
+            if (node is null) { return null; }
+            if (depths[node] == depth)
+            {
+                var left = node.left is not null && depths[node.left] == depth;
+                var right = node.right is not null && depths[node.right] == depth;
+                return (left, right) switch
+                {
+                    (true, false) => Find(node.left),
+                    (false, true) => Find(node.right),
+                    _ => node,
+                };
+            }
+            return null;
+        }
     }
 
-    public void Oxygen(Action releaseOxygen)
+    static (TreeNode node, int depth) Dfs(TreeNode node, int depth)
     {
-        ox.Wait();
-        // releaseOxygen() outputs "O". Do not change or remove this line.
-        releaseOxygen();
-        count = 0;
-        hy.Release();
+        if (node is null) { return (node, depth); }
+        var left = Dfs(node.left, 1 + depth);
+        var right = Dfs(node.right, 1 + depth);
+        if (left.depth == right.depth) { return (node, left.depth); }
+        return left.depth > right.depth ? left : right;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -7,21 +7,30 @@ namespace Tests;
 [TestClass]
 public class UnitTest
 {
-    // readonly Solution.Solution sol = new();
+    readonly Solution.Solution sol = new();
 
     [TestMethod]
     public void TestMethod1()
     {
+        var a = TreeNode.Make([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+        var b = sol.LcaDeepestLeaves(a);
+        Assert.AreEqual("[2,7,4]", b.ToString());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        var a = TreeNode.Make(new int[] { 1 });
+        var b = sol.LcaDeepestLeaves(a);
+        Assert.AreEqual("[1]", b.ToString());
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([0, 1, 3, null, 2]);
+        var b = sol.LcaDeepestLeaves(a);
+        Assert.AreEqual("[2]", b.ToString());
     }
 
     [TestMethod]
