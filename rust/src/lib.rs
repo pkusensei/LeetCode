c7116245684b126be1cfa5b863df57db3d8f8eca
pkusensei <ZW_08@outlook.com>
2024-11-30 16:49:51 -0500

    1129 bfs with alternating colors

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,20 +2,48 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::VecDeque;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_equiv_domino_pairs(dominoes: &[[i32; 2]]) -> i32 {
-    let mut map = std::collections::HashMap::new();
-    let mut res = 0;
-    for d in dominoes {
-        let a = d[0].min(d[1]);
-        let b = d[0].max(d[1]);
-        if let Some(v) = map.get_mut(&[a, b]) {
-            res += *v;
-            *v += 1;
-        } else {
-            map.insert([a, b], 1);
+pub fn shortest_alternating_paths(
+    n: i32,
+    red_edges: &[[i32; 2]],
+    blue_edges: &[[i32; 2]],
+) -> Vec<i32> {
+    let n = n as usize;
+    let (mut reds, mut blues) = (vec![vec![false; n]; n], vec![vec![false; n]; n]);
+    for v in red_edges.iter() {
+        reds[v[0] as usize][v[1] as usize] = true;
+    }
+    for v in blue_edges.iter() {
+        blues[v[0] as usize][v[1] as usize] = true;
+    }
+    let mut res = vec![-1; n];
+    // (node, dist, color) => 0:red 1:blue
+    let mut queue = VecDeque::from([(0, 0, 0), (0, 0, 1)]);
+    let mut seen = vec![vec![false; n]; 2];
+    seen[0][0] = true;
+    seen[1][0] = true;
+    while let Some((node, dist, color)) = queue.pop_front() {
+        if res[node] == -1 {
+            res[node] = dist
+        }
+        if color == 0 {
+            for (next, b) in blues[node].iter().enumerate() {
+                if *b && !seen[1][next] {
+                    seen[1][next] = true;
+                    queue.push_back((next, 1 + dist, 1));
+                }
+            }
+        } else if color == 1 {
+            for (next, b) in reds[node].iter().enumerate() {
+                if *b && !seen[0][next] {
+                    seen[0][next] = true;
+                    queue.push_back((next, 1 + dist, 0));
+                }
+            }
         }
     }
     res
@@ -29,15 +57,31 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(num_equiv_domino_pairs(&[[1, 2], [2, 1], [3, 4], [5, 6]]), 1);
         debug_assert_eq!(
-            num_equiv_domino_pairs(&[[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]),
-            3
+            shortest_alternating_paths(3, &[[0, 1], [1, 2]], &[]),
+            [0, 1, -1]
+        );
+        debug_assert_eq!(
+            shortest_alternating_paths(3, &[[0, 1]], &[[2, 1]]),
+            [0, 1, -1]
         );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        debug_assert_eq!(
+            shortest_alternating_paths(3, &[[0, 1], [0, 2]], &[[1, 0]]),
+            [0, 1, 1]
+        );
+        debug_assert_eq!(
+            shortest_alternating_paths(
+                5,
+                &[[0, 1], [1, 2], [2, 3], [3, 4]],
+                &[[1, 2], [2, 3], [3, 1]]
+            ),
+            [0, 1, 2, 3, 7]
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
