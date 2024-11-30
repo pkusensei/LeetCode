96affe66b2d303bb9b8715a0ac8e5c613709f7c3
pkusensei <ZW_08@outlook.com>
2024-11-29 20:09:53 -0500

    2097 Hierholzer’s algorithm and Eulerian path

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,22 +2,49 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_depth_after_split(seq: &str) -> Vec<i32> {
-    let mut res = Vec::with_capacity(seq.len());
-    let mut depth = 0;
-    for b in seq.bytes() {
-        if b == b'(' {
-            res.push(depth & 1);
-            depth += 1;
-        } else {
-            depth -= 1;
-            res.push(depth & 1);
-        }
+pub fn valid_arrangement(pairs: &[[i32; 2]]) -> Vec<Vec<i32>> {
+    let mut graph = pairs
+        .iter()
+        .fold(HashMap::<i32, Vec<_>>::new(), |mut acc, v| {
+            acc.entry(v[0]).or_default().push(v[1]);
+            acc
+        });
+    let start = find_ends(pairs).map(|v| v[0]).unwrap_or(pairs[0][0]);
+    let mut res = vec![];
+    dfs(&mut graph, start, &mut res);
+    res.reverse();
+    res.windows(2).map(|w| w.to_vec()).collect()
+}
+
+fn dfs(graph: &mut HashMap<i32, Vec<i32>>, curr: i32, path: &mut Vec<i32>) {
+    while let Some(next) = graph.get_mut(&curr).and_then(|v| v.pop()) {
+        dfs(graph, next, path);
     }
-    res
+    path.push(curr);
+}
+
+fn find_ends(pairs: &[[i32; 2]]) -> Option<[i32; 2]> {
+    let counts = pairs.iter().fold(HashMap::new(), |mut acc, v| {
+        acc.entry(v[0]).or_insert((0, 0)).0 += 1;
+        acc.entry(v[1]).or_insert((0, 0)).1 += 1;
+        acc
+    });
+    let start = counts
+        .iter()
+        .find_map(|(&k, &v)| if v.0 - v.1 == 1 { Some(k) } else { None });
+    let end = counts
+        .iter()
+        .find_map(|(&k, &v)| if v.0 - v.1 == -1 { Some(k) } else { None });
+    if let (Some(a), Some(b)) = (start, end) {
+        Some([a, b])
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -28,8 +55,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        debug_assert_eq!(max_depth_after_split("(()())"), [0, 1, 1, 1, 1, 0]);
-        debug_assert_eq!(max_depth_after_split("()(())()"), [0, 0, 0, 1, 1, 0, 0, 0]);
+        debug_assert_eq!(
+            valid_arrangement(&[[5, 1], [4, 5], [11, 9], [9, 4]]),
+            [[11, 9], [9, 4], [4, 5], [5, 1]]
+        );
+        debug_assert_eq!(
+            valid_arrangement(&[[1, 3], [3, 2], [2, 1]]),
+            [[1, 3], [3, 2], [2, 1]]
+        );
+        debug_assert_eq!(
+            valid_arrangement(&[[1, 2], [1, 3], [2, 1]]),
+            [[1, 2], [2, 1], [1, 3]]
+        );
     }
 
     #[test]
