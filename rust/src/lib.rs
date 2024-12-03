a2fce2740b294d7593009e3c8897c1f1f71113c2
pkusensei <ZW_08@outlook.com>
2024-12-03 18:40:45 -0500

    2109 add spaces to str

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,25 +5,22 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_characters(words: &[&str], chars: &str) -> i32 {
-    let target = count(chars);
-    words
-        .iter()
-        .filter_map(|s| {
-            if count(s).into_iter().zip(target).all(|(a, b)| a <= b) {
-                Some(s.len())
-            } else {
-                None
-            }
-        })
-        .sum::<usize>() as i32
-}
-
-fn count(s: &str) -> [u8; 26] {
-    s.bytes().fold([0; 26], |mut acc, b| {
-        acc[usize::from(b - b'a')] += 1;
-        acc
-    })
+pub fn add_spaces(s: String, spaces: &[i32]) -> String {
+    // let mut s = s.into_bytes();
+    // for i in spaces.iter().rev() {
+    //     s.insert(*i as usize, b' ');
+    // }
+    // String::from_utf8(s).unwrap()
+    let mut res = Vec::with_capacity(s.len() + spaces.len());
+    let mut i1 = 0;
+    for (i2, b) in s.bytes().enumerate() {
+        if spaces.get(i1).is_some_and(|&v| v as usize == i2) {
+            res.push(b' ');
+            i1 += 1;
+        }
+        res.push(b);
+    }
+    String::from_utf8(res).unwrap()
 }
 
 #[cfg(test)]
@@ -34,10 +31,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(count_characters(&["cat", "bt", "hat", "tree"], "atach"), 6);
         assert_eq!(
-            count_characters(&["hello", "world", "leetcode"], "welldonehoneyr"),
-            10
+            add_spaces("LeetcodeHelpsMeLearn".into(), &[8, 13, 15]),
+            "Leetcode Helps Me Learn"
+        );
+        assert_eq!(
+            add_spaces("icodeinpython".into(), &[1, 5, 7, 9]),
+            "i code in py thon"
+        );
+        assert_eq!(
+            add_spaces("spacing".into(), &[0, 1, 2, 3, 4, 5, 6]),
+            " s p a c i n g"
         );
     }
 
