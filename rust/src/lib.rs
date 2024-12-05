e7fcbc8bb8cfc4671285e1057e09bd354a0eac8c
Zx Wang <ZW_08@outlook.com>
2024-12-05 12:27:23 -0500

    1172 keep track of vacancies with BTreeSet

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,28 +5,68 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn can_change(start: &str, target: &str) -> bool {
-    let [start, target] =
-        [start, target].map(|v| v.bytes().enumerate().filter(|&(_i, b)| b != b'_'));
-    if start.clone().count() != target.clone().count() {
-        return false;
-    }
-    for (s, t) in start.zip(target) {
-        match (s.1, t.1) {
-            (b'L', b'L') => {
-                if s.0 < t.0 {
-                    return false;
-                }
-            }
-            (b'R', b'R') => {
-                if s.0 > t.0 {
-                    return false;
-                }
-            }
-            _ => return false,
+#[derive(Debug, Clone, Default)]
+struct DinnerPlates {
+    cap: usize,
+    stacks: Vec<Vec<i32>>,
+    slots: std::collections::BTreeSet<usize>,
+}
+
+impl DinnerPlates {
+    fn new(capacity: i32) -> Self {
+        Self {
+            cap: capacity as usize,
+            ..Default::default()
         }
     }
-    true
+
+    fn push(&mut self, val: i32) {
+        if let Some(&i) = self.slots.iter().next() {
+            self.stacks[i].push(val);
+            if self.stacks[i].len() == self.cap {
+                self.slots.remove(&i);
+            }
+        } else {
+            let v = vec![val];
+            self.stacks.push(v);
+            if self.cap > 1 {
+                self.slots.insert(self.stacks.len() - 1);
+            }
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        let Some((idx, st)) = self
+            .stacks
+            .iter_mut()
+            .enumerate()
+            .rfind(|(_i, st)| !st.is_empty())
+        else {
+            return -1;
+        };
+        let res = st.pop().unwrap_or(-1);
+        if st.is_empty() {
+            self.slots.retain(|&i| i < idx);
+            self.stacks.truncate(idx);
+        } else {
+            self.slots.insert(idx);
+        }
+        res
+    }
+
+    fn pop_at_stack(&mut self, index: i32) -> i32 {
+        let idx = index as usize;
+        if self.stacks.len() <= idx {
+            return -1;
+        }
+        if self.stacks.len() - 1 == idx {
+            return self.pop();
+        }
+        if self.stacks[idx].len() == self.cap {
+            self.slots.insert(idx);
+        }
+        self.stacks[idx].pop().unwrap_or(-1)
+    }
 }
 
 #[cfg(test)]
@@ -37,9 +77,22 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(can_change("_L__R__R_", "L______RR"));
-        assert!(!can_change("R_L_", "__LR"));
-        assert!(!can_change("_R", "R_"));
+        let mut D = DinnerPlates::new(2); // Initialize with capacity = 2
+        D.push(1);
+        D.push(2);
+        D.push(3);
+        D.push(4);
+        D.push(5);
+        assert_eq!(D.pop_at_stack(0), 2);
+        D.push(20);
+        D.push(21);
+        assert_eq!(D.pop_at_stack(0), 20);
+        assert_eq!(D.pop_at_stack(2), 21);
+        assert_eq!(D.pop(), 5);
+        assert_eq!(D.pop(), 4);
+        assert_eq!(D.pop(), 3);
+        assert_eq!(D.pop(), 1);
+        assert_eq!(D.pop(), -1);
     }
 
     #[test]
