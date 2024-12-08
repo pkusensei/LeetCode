e44c2c0007113ca0db79b2915542e560fa277f0f
pkusensei <ZW_08@outlook.com>
2024-12-08 12:25:49 -0500

    1200 pair with min diff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,60 +2,24 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::cmp::Reverse;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_two_events(events: &mut [[i32; 3]]) -> i32 {
-    events.sort_unstable_by_key(|v| v[0]);
-    let n = events.len();
-    let mut suffix = Vec::with_capacity(n);
-    let mut curr = 0;
-    for e in events.iter().rev() {
-        curr = curr.max(e[2]);
-        suffix.push(curr);
-    }
-    suffix.reverse();
-    let mut res = 0;
-    for e in events.iter() {
-        let i = events.partition_point(|v| v[0] <= e[1]);
-        let temp = e[2] + suffix.get(i).unwrap_or(&0);
-        res = res.max(temp);
-    }
-    res
-}
-
-fn with_heap(events: &mut [[i32; 3]]) -> i32 {
-    events.sort_unstable_by_key(|v| v[0]);
-    let mut heap = std::collections::BinaryHeap::new();
-    let mut res = 0;
-    let mut temp = 0; // record previous max value
-    for e in events.iter() {
-        while heap.peek().is_some_and(|&(Reverse(end), _v)| end < e[0]) {
-            let (_, val) = heap.pop().unwrap();
-            temp = temp.max(val);
-        }
-        res = res.max(e[2] + temp);
-        heap.push((Reverse(e[1]), e[2]));
-    }
-    res
-}
-
-fn greedy(events: &[[i32; 3]]) -> i32 {
-    let mut times = Vec::with_capacity(2 * events.len());
-    for e in events.iter() {
-        times.push([e[0], 1, e[2]]);
-        times.push([1 + e[1], 0, e[2]]); // 0 so that sorting put end first
-    }
-    times.sort_unstable();
-    let mut res = 0;
-    let mut temp = 0;
-    for t in times {
-        if t[1] == 1 {
-            res = res.max(t[2] + temp);
-        } else {
-            temp = temp.max(t[2]);
+pub fn minimum_abs_difference(arr: &mut [i32]) -> Vec<Vec<i32>> {
+    arr.sort_unstable();
+    let mut delta = i32::MAX;
+    let mut res = vec![];
+    for w in arr.windows(2) {
+        let d = w[1] - w[0];
+        match d.cmp(&delta) {
+            std::cmp::Ordering::Less => {
+                delta = d;
+                res = vec![vec![w[0], w[1]]];
+            }
+            std::cmp::Ordering::Equal => {
+                res.push(vec![w[0], w[1]]);
+            }
+            std::cmp::Ordering::Greater => (),
         }
     }
     res
@@ -69,9 +33,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(greedy(&mut [[1, 3, 2], [4, 5, 2], [2, 4, 3]]), 4);
-        assert_eq!(greedy(&mut [[1, 3, 2], [4, 5, 2], [1, 5, 5]]), 5);
-        assert_eq!(greedy(&mut [[1, 5, 3], [1, 5, 1], [6, 6, 5]]), 8);
+        assert_eq!(
+            minimum_abs_difference(&mut [4, 2, 1, 3]),
+            [[1, 2], [2, 3], [3, 4]]
+        );
+        assert_eq!(minimum_abs_difference(&mut [1, 3, 6, 10, 15]), [[1, 3]]);
+        assert_eq!(
+            minimum_abs_difference(&mut [3, 8, -10, 23, 19, -4, -14, 27]),
+            [[-14, -10], [19, 23], [23, 27]]
+        );
     }
 
     #[test]
