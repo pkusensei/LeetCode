88f02b85eef6406e91873bf68b9b8a419a1194b4
pkusensei <ZW_08@outlook.com>
2024-12-08 21:14:30 -0500

    3152 subarray with alternating parity

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,70 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn nth_ugly_number(n: i32, a: i32, b: i32, c: i32) -> i32 {
-    let [n, a, b, c] = [n, a, b, c].map(i64::from);
-    let ab = a * b / gcd(a, b);
-    let ac = a * c / gcd(a, c);
-    let bc = b * c / gcd(c, b);
-    let abc = ab * c / gcd(ab, c);
-    let mut left = 1;
-    let mut right = 2_000_000_000;
-    while left < right {
-        let mid = left + (right - left) / 2;
-        let count = mid / a + mid / b + mid / c - mid / ab - mid / ac - mid / bc + mid / abc;
-        if count < n {
-            left = 1 + mid
-        } else {
-            right = mid
-        }
+pub fn is_array_special(nums: &[i32], queries: &[[i32; 2]]) -> Vec<bool> {
+    let mut prefix = Vec::with_capacity(nums.len());
+    prefix.push(0);
+    for (i, &num) in nums.iter().enumerate().skip(1) {
+        prefix.push(prefix.last().unwrap_or(&0) + i32::from(num & 1 == nums[i - 1] & 1));
     }
-    left as i32
+    let mut res = Vec::with_capacity(queries.len());
+    for q in queries.iter() {
+        // No increase in "abnormal" numbers
+        res.push(prefix[q[1] as usize] == prefix[q[0] as usize]);
+    }
+    res
+}
+
+fn with_binary_search(nums: &[i32], queries: &[[i32; 2]]) -> Vec<bool> {
+    fn bs(indices: &[usize], start: usize, end: usize) -> bool {
+        let mut left = 0;
+        let mut right = indices.len() - 1;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            let i = indices[mid];
+            if i < start {
+                left = 1 + mid;
+            } else if i > end {
+                right = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        false
+    }
+
+    let indices: Vec<_> = nums
+        .windows(2)
+        .enumerate()
+        .filter_map(|(i, w)| {
+            if w[0] & 1 == w[1] & 1 {
+                Some(1 + i)
+            } else {
+                None
+            }
+        })
+        .collect();
+    queries
+        .iter()
+        .map(|q| !bs(&indices, 1 + q[0] as usize, q[1] as usize))
+        .collect()
+}
+
+fn sliding_window(nums: &[i32], queries: &[[i32; 2]]) -> Vec<bool> {
+    let n = nums.len();
+    let mut reach = Vec::with_capacity(n);
+    let mut end = 0;
+    for start in 0..n {
+        end = start.max(end);
+        while end < n - 1 && nums[end] & 1 != nums[1 + end] & 1 {
+            end += 1;
+        }
+        reach.push(end);
+    }
+    queries
+        .iter()
+        .map(|q| q[1] as usize <= reach[q[0] as usize])
+        .collect()
 }
 
 #[cfg(test)]
@@ -33,9 +79,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(nth_ugly_number(3, 2, 3, 5), 4);
-        assert_eq!(nth_ugly_number(4, 2, 3, 4), 6);
-        assert_eq!(nth_ugly_number(5, 2, 11, 13), 10);
+        assert_eq!(sliding_window(&[3, 4, 1, 2, 6], &[[0, 4]]), [false]);
+        assert_eq!(
+            sliding_window(&[4, 3, 1, 6], &[[0, 2], [2, 3]]),
+            [false, true]
+        );
     }
 
     #[test]
