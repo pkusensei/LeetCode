7fc92a7431c73f5264851fd9a5b4dda9901eb8dd
pkusensei <ZW_08@outlook.com>
2024-12-11 19:05:57 -0500

    2558 priority heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,36 +5,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn die_simulator(n: i32, roll_max: [i32; 6]) -> i32 {
-    const MOD: i64 = 1_000_000_007;
-    let n = n as usize;
-    let mut dp = Vec::with_capacity(n);
-    dp.push([1i64; 6]);
-    for dice in 1..n {
-        dp.push([0; 6]);
-        for face in 0..6 {
-            // Without constraints
-            // current dice could attach all face to previous one
-            // i.e whichever previous face is, this face could be picked
-            let mut curr: i64 = dp[dice - 1].iter().sum();
-            match (roll_max[face] as usize).cmp(&dice) {
-                std::cmp::Ordering::Greater => (),
-                // roll_max[face] == current_dice => this face cannot be used
-                std::cmp::Ordering::Equal => curr -= 1,
-                std::cmp::Ordering::Less => {
-                    // For all face i!= current_face
-                    // Consider the situation
-                    // that previous roll_max[face] dices all rolled current face
-                    // dp[dice -1 -max[face]][i] must be eliminated
-                    for i in (0..6).filter(|&i| i != face) {
-                        curr -= dp[dice - 1 - roll_max[face] as usize][i];
-                    }
-                }
-            }
-            dp[dice][face] = curr.rem_euclid(MOD);
-        }
+pub fn pick_gifts(gifts: Vec<i32>, k: i32) -> i64 {
+    let mut heap = std::collections::BinaryHeap::from(gifts);
+    for _ in 0..k {
+        let mut v = heap.pop().unwrap();
+        v = (v as f64).sqrt().floor() as i32;
+        heap.push(v);
     }
-    dp[n - 1].iter().fold(0, |acc, v| (acc + v) % MOD) as _
+    heap.into_iter().map(i64::from).sum()
 }
 
 #[cfg(test)]
@@ -45,9 +23,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(die_simulator(2, [1, 1, 2, 2, 2, 3]), 34);
-        assert_eq!(die_simulator(2, [1, 1, 1, 1, 1, 1]), 30);
-        assert_eq!(die_simulator(3, [1, 1, 1, 2, 2, 3]), 181);
+        assert_eq!(pick_gifts([25, 64, 9, 4, 100].into(), 4), 29);
+        assert_eq!(pick_gifts([1, 1, 1, 1].into(), 4), 4);
     }
 
     #[test]
