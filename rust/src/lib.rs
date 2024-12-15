48c43220553ad57aa7ada21e8c8f52ac40a7cff3
pkusensei <ZW_08@outlook.com>
2024-12-14 20:06:03 -0500

    1792 tried to avoid writing Ord like this

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,57 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn odd_cells(m: i32, n: i32, indices: &[[i32; 2]]) -> i32 {
-    let [mut rows, mut cols] = [m, n].map(|v| vec![0; v as usize]);
-    for v in indices.iter() {
-        rows[v[0] as usize] += 1;
-        cols[v[1] as usize] += 1;
+pub fn max_average_ratio(classes: &[[i32; 2]], extra_students: i32) -> f64 {
+    let mut heap: std::collections::BinaryHeap<_> = classes
+        .iter()
+        .map(|v| Class {
+            pass: v[0],
+            total: v[1],
+        })
+        .collect();
+    for _ in 0..extra_students {
+        let Some(mut c) = heap.pop() else {
+            continue;
+        };
+        c.pass += 1;
+        c.total += 1;
+        heap.push(c);
     }
-    let mut res = 0;
-    for row in rows.into_iter() {
-        for col in cols.iter() {
-            res += (row + col) & 1;
-        }
+    heap.into_iter()
+        .map(|c| f64::from(c.pass) / f64::from(c.total))
+        .sum::<f64>()
+        / classes.len() as f64
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Class {
+    pass: i32,
+    total: i32,
+}
+
+impl PartialEq for Class {
+    fn eq(&self, other: &Self) -> bool {
+        self.pass * other.total == self.total * other.pass
     }
-    res
+}
+
+impl Eq for Class {}
+
+impl PartialOrd for Class {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+// This is the most obvious and dirtiest way to write it
+impl Ord for Class {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (f64::from(1 + self.pass) / f64::from(1 + self.total)
+            - f64::from(self.pass) / f64::from(self.total))
+        .total_cmp(
+            &(f64::from(1 + other.pass) / f64::from(1 + other.total)
+                - f64::from(other.pass) / f64::from(other.total)),
+        )
+    }
 }
 
 #[cfg(test)]
@@ -28,14 +66,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(odd_cells(2, 3, &[[0, 1], [1, 1]]), 6);
-        assert_eq!(odd_cells(2, 2, &[[1, 1], [0, 0]]), 0);
+        // float_eq(max_average_ratio(&[[1, 2], [3, 5], [2, 2]], 2), 0.78333);
+        float_eq(
+            max_average_ratio(&[[2, 4], [3, 9], [4, 5], [2, 10]], 4),
+            0.53485,
+        );
     }
 
     #[test]
-    fn test() {
-        assert_eq!(odd_cells(48, 37, &[[40, 5]]), 83)
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
@@ -57,8 +96,9 @@ mod tests {
         T2: Into<f64> + Copy + Debug,
     {
         const EP: f64 = 1e-5;
-        if (<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() > EP {
-            dbg!(a, b);
-        }
+        assert!(
+            (<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() <= EP,
+            "left = {a:?}, right = {b:?}",
+        );
     }
 }
