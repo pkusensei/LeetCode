7c33e50654437bd5f0c09fc5d895af2916f08826
pkusensei <ZW_08@outlook.com>
2024-12-16 13:57:58 -0500

    1263 another 2d grid; (box_pos, player_pos)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,29 +2,95 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::{HashSet, VecDeque};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_final_state(nums: &[i32], k: i32, multiplier: i32) -> Vec<i32> {
-    let mut heap: BinaryHeap<_> = nums
-        .iter()
-        .enumerate()
-        .map(|(i, &v)| Reverse((v, i)))
-        .collect();
-    for _ in 0..k {
-        let Some(Reverse((mut v, i))) = heap.pop() else {
-            continue;
-        };
-        v *= multiplier;
-        heap.push(Reverse((v, i)));
+pub fn min_push_box(grid: &[&[char]]) -> i32 {
+    let (walls, [carton, start, target], borders) = build(grid);
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+    for pos in immediate(carton, borders) {
+        if !walls.contains(&pos) && reachable(&walls, carton, start, pos, borders) {
+            queue.push_back((carton, pos, 0));
+            seen.insert([carton, pos]);
+        }
     }
-    let mut res = vec![0; nums.len()];
-    for Reverse((v, i)) in heap.into_iter() {
-        res[i] = v;
+    while let Some((carton, pos, push)) = queue.pop_front() {
+        if carton == target {
+            return push;
+        }
+        let dx = carton[0] - pos[0];
+        let dy = carton[1] - pos[1];
+        let new_carton = [carton[0] + dx, carton[1] + dy];
+        if inside(new_carton, borders) && !walls.contains(&new_carton) {
+            for new_pos in immediate(new_carton, borders) {
+                if !walls.contains(&new_pos)
+                    && reachable(&walls, new_carton, pos, new_pos, borders)
+                    && seen.insert([new_carton, new_pos])
+                {
+                    queue.push_back((new_carton, new_pos, 1 + push));
+                }
+            }
+        }
     }
-    res
+    -1
+}
+
+fn reachable(
+    walls: &HashSet<[i8; 2]>,
+    carton: [i8; 2],
+    start: [i8; 2],
+    goal: [i8; 2],
+    borders: [i8; 2],
+) -> bool {
+    let mut queue = VecDeque::from([start]);
+    let mut seen = HashSet::from([start]);
+    while let Some(node) = queue.pop_front() {
+        if goal == node {
+            return true;
+        }
+        for neighbor in immediate(node, borders) {
+            if !walls.contains(&neighbor) && neighbor != carton && seen.insert(neighbor) {
+                queue.push_back(neighbor);
+            }
+        }
+    }
+    false
+}
+
+fn immediate([x, y]: [i8; 2], borders: [i8; 2]) -> impl Iterator<Item = [i8; 2]> {
+    const DELTAS: [[i8; 2]; 4] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    DELTAS
+        .map(|[dx, dy]| [x + dx, y + dy])
+        .into_iter()
+        .filter(move |&p| inside(p, borders))
+}
+
+fn inside([x, y]: [i8; 2], [xmax, ymax]: [i8; 2]) -> bool {
+    (0..xmax).contains(&x) && (0..ymax).contains(&y)
+}
+
+fn build(grid: &[&[char]]) -> (HashSet<[i8; 2]>, [[i8; 2]; 3], [i8; 2]) {
+    let [mut start, mut target, mut carton] = [[0, 0]; 3];
+    let (rows, cols) = get_dimensions(grid);
+    let mut walls = HashSet::new();
+    for (row, r) in grid.iter().enumerate() {
+        for (col, &ch) in r.iter().enumerate() {
+            let curr = [col as i8, row as i8];
+            match ch {
+                '#' => {
+                    walls.insert(curr);
+                }
+                'B' => carton = curr,
+                'S' => start = curr,
+                'T' => target = curr,
+                _ => (),
+            }
+        }
+    }
+    (walls, [carton, start, target], [cols as i8, rows as i8])
 }
 
 #[cfg(test)]
@@ -35,12 +101,55 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(get_final_state(&[2, 1, 3, 5, 6], 5, 2), [8, 4, 6, 5, 6]);
-        assert_eq!(get_final_state(&[1, 2], 3, 4), [16, 8]);
+        assert_eq!(
+            min_push_box(&[
+                &['#', '#', '#', '#', '#', '#'],
+                &['#', 'T', '#', '#', '#', '#'],
+                &['#', '.', '.', 'B', '.', '#'],
+                &['#', '.', '#', '#', '.', '#'],
+                &['#', '.', '.', '.', 'S', '#'],
+                &['#', '#', '#', '#', '#', '#']
+            ]),
+            3
+        );
+        assert_eq!(
+            min_push_box(&[
+                &['#', '#', '#', '#', '#', '#'],
+                &['#', 'T', '#', '#', '#', '#'],
+                &['#', '.', '.', 'B', '.', '#'],
+                &['#', '#', '#', '#', '.', '#'],
+                &['#', '.', '.', '.', 'S', '#'],
+                &['#', '#', '#', '#', '#', '#']
+            ]),
+            -1
+        );
+        assert_eq!(
+            min_push_box(&[
+                &['#', '#', '#', '#', '#', '#'],
+                &['#', 'T', '.', '.', '#', '#'],
+                &['#', '.', '#', 'B', '.', '#'],
+                &['#', '.', '.', '.', '.', '#'],
+                &['#', '.', '.', '.', 'S', '#'],
+                &['#', '#', '#', '#', '#', '#']
+            ]),
+            5
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            min_push_box(&[
+                &['#', '.', '.', '#', '#', '#', '#', '#'],
+                &['#', '.', '.', 'T', '#', '.', '.', '#'],
+                &['#', '.', '.', '.', '#', 'B', '.', '#'],
+                &['#', '.', '.', '.', '.', '.', '.', '#'],
+                &['#', '.', '.', '.', '#', '.', 'S', '#'],
+                &['#', '.', '.', '#', '#', '#', '#', '#']
+            ]),
+            7
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
@@ -64,7 +173,7 @@ mod tests {
         const EP: f64 = 1e-5;
         assert!(
             (<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() <= EP,
-            "left = {a:?}, right = {b:?}",
+            "left = {a:?}, right = {b:?}"
         );
     }
 }
