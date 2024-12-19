f369940825dca3f491e0c0b55691cb17da8d9919
pkusensei <ZW_08@outlook.com>
2024-12-18 19:59:18 -0500

    769 with monotonic stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn final_prices(prices: &[i32]) -> Vec<i32> {
-    let n = prices.len();
-    let [mut stack, mut res] = [0; 2].map(|_| Vec::with_capacity(n));
-    for &num in prices.iter().rev() {
-        while stack.last().is_some_and(|&v| v > num) {
-            stack.pop();
+pub fn max_chunks_to_sorted(arr: &[i32]) -> i32 {
+    let mut stack = vec![];
+    for &num in arr.iter() {
+        match stack.last() {
+            None => stack.push(num),
+            Some(&v) if v < num => stack.push(num),
+            Some(&temp) => {
+                while stack.last().is_some_and(|&v| v > num) {
+                    stack.pop();
+                }
+                stack.push(temp);
+            }
         }
-        res.push(num - stack.last().unwrap_or(&0));
-        stack.push(num);
     }
-    res.reverse();
-    res
+    stack.len() as _
 }
 
 #[cfg(test)]
@@ -27,13 +30,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(final_prices(&[8, 4, 6, 2, 3]), [4, 2, 4, 2, 3]);
-        assert_eq!(final_prices(&[1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
-        assert_eq!(final_prices(&[10, 1, 1, 6]), [9, 0, 1, 6]);
+        assert_eq!(max_chunks_to_sorted(&[4, 3, 2, 1, 0]), 1);
+        assert_eq!(max_chunks_to_sorted(&[1, 0, 2, 3, 4]), 4);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(max_chunks_to_sorted(&[1, 2, 0, 3]), 2);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
