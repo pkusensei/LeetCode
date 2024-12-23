50b138de129466f984997153ff84d37d88b16fc6
pkusensei <ZW_08@outlook.com>
2024-12-23 15:16:43 -0500

    1284 use bits to model a small binary matrix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,25 +2,63 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn smallest_divisor(nums: &[i32], threshold: i32) -> i32 {
-    let mut left = 1;
-    let mut right = nums.iter().copied().max().unwrap_or(i32::MAX);
-    while left < right {
-        let mid = left + (right - left) / 2;
-        let curr: i32 = nums
-            .iter()
-            .map(|num| num / mid + i32::from(num % mid > 0))
-            .sum();
-        if curr > threshold {
-            left = mid + 1
-        } else {
-            right = mid
+pub fn min_flips(mat: Vec<Vec<i32>>) -> i32 {
+    let (rows, cols) = get_dimensions(&mat);
+    let mask = to_num(mat);
+    let mut queue = VecDeque::from([(mask, 0)]);
+    let mut seen = HashSet::from([mask]);
+    while let Some((mask, step)) = queue.pop_front() {
+        if mask.count_ones() == 0 {
+            return step;
+        }
+        for row in 0..rows {
+            for col in 0..cols {
+                let mut next = mask;
+                let idx = row * cols + col;
+                next ^= 1 << idx;
+                for (nr, nc) in neighbors((row, col)) {
+                    if (0..rows).contains(&nr) && (0..cols).contains(&nc) {
+                        let i = nr * cols + nc;
+                        next ^= 1 << i;
+                    }
+                }
+                if seen.insert(next) {
+                    queue.push_back((next, 1 + step));
+                }
+            }
         }
     }
-    left
+    -1
+}
+
+fn to_num(grid: Vec<Vec<i32>>) -> i32 {
+    let mut res = 0;
+    for r in grid {
+        for num in r {
+            res <<= 1;
+            res |= num;
+        }
+    }
+    res
+}
+
+fn to_grid(mut num: i32, rows: usize, cols: usize) -> Vec<Vec<i32>> {
+    let mut res = vec![];
+    for _ in 0..rows {
+        let mut curr = Vec::with_capacity(cols);
+        for _ in 0..cols {
+            curr.push(num & 1);
+            num >>= 1;
+        }
+        curr.reverse();
+        res.push(curr);
+    }
+    res
 }
 
 #[cfg(test)]
@@ -31,8 +69,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(smallest_divisor(&[1, 2, 5, 9], 6), 5);
-        assert_eq!(smallest_divisor(&[44, 22, 33, 11, 1], 5), 44);
+        assert_eq!(min_flips(vec![vec![0, 0], vec![0, 1]]), 3);
+        assert_eq!(min_flips(vec![vec![0]]), 0);
+        assert_eq!(min_flips(vec![vec![1, 0, 0], vec![1, 0, 0]]), -1);
     }
 
     #[test]
