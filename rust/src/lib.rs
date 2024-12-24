8785c7e02ca35a8489ba13c7facdfc3112f2c0b7
pkusensei <ZW_08@outlook.com>
2024-12-24 16:21:19 -0500

    1289 min falling path with non-zero shifts

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn remove_covered_intervals(intervals: &mut [[i32; 2]]) -> i32 {
-    intervals.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then(b[1].cmp(&a[1])));
-    let mut prev = [-1, -1];
-    let mut res = 0;
-    for int in intervals.iter() {
-        if prev[0] <= int[0] && int[1] <= prev[1] {
-            continue;
-        }
-        prev = [int[0], int[1]];
-        res += 1;
+pub fn min_falling_path_sum(grid: &[&[i32]]) -> i32 {
+    let n = grid.len();
+    if n == 1 {
+        return grid[0][0];
     }
-    res
+    let [(mut i1, mut n1), (mut _i2, mut n2)] = find_2_mins(grid[0]);
+    for row in grid.iter().skip(1) {
+        let mut dp = Vec::with_capacity(n);
+        for (idx, &num) in row.iter().enumerate() {
+            if idx == i1 {
+                dp.push(num + n2);
+            } else {
+                dp.push(num + n1);
+            }
+        }
+        [(i1, n1), (_i2, n2)] = find_2_mins(&dp);
+    }
+    n1
+}
+
+fn find_2_mins(row: &[i32]) -> [(usize, i32); 2] {
+    let (mut i1, mut n1) = (0, i32::MAX);
+    let (mut i2, mut n2) = (0, i32::MAX);
+    for (idx, &num) in row.iter().enumerate() {
+        if num < n1 {
+            i2 = i1;
+            n2 = n1;
+            n1 = num;
+            i1 = idx
+        } else if num < n2 {
+            n2 = num;
+            i2 = idx;
+        }
+    }
+    [(i1, n1), (i2, n2)]
 }
 
 #[cfg(test)]
@@ -27,12 +50,31 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(remove_covered_intervals(&mut [[1, 4], [3, 6], [2, 8]]), 2);
-        assert_eq!(remove_covered_intervals(&mut [[1, 4], [2, 3]]), 1);
+        assert_eq!(
+            min_falling_path_sum(&[&[1, 2, 3], &[4, 5, 6], &[7, 8, 9]]),
+            13
+        );
+        assert_eq!(min_falling_path_sum(&[&[7]]), 7);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            min_falling_path_sum(&[
+                &[50, -18, -38, 39, -20, -37, -61, 72, 22, 79],
+                &[82, 26, 30, -96, -1, 28, 87, 94, 34, -89],
+                &[55, -50, 20, 76, -50, 59, -58, 85, 83, -83],
+                &[39, 65, -68, 89, -62, -53, 74, 2, -70, -90],
+                &[1, 57, -70, 83, -91, -32, -13, 49, -11, 58],
+                &[-55, 83, 60, -12, -90, -37, -36, -27, -19, -6],
+                &[76, -53, 78, 90, 70, 62, -81, -94, -32, -57],
+                &[-32, -85, 81, 25, 80, 90, -24, 10, 27, -55],
+                &[39, 54, 39, 34, -45, 17, -2, -61, -81, 85],
+                &[-77, 65, 76, 92, 21, 68, 78, -13, 39, 22]
+            ]),
+            -807
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
