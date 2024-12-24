56624d7468714868f8a96f86186ea019030b9fa7
pkusensei <ZW_08@outlook.com>
2024-12-24 17:09:45 -0500

    1291 int with sequential digits

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,41 +5,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_falling_path_sum(grid: &[&[i32]]) -> i32 {
-    let n = grid.len();
-    if n == 1 {
-        return grid[0][0];
-    }
-    let [(mut i1, mut n1), (mut _i2, mut n2)] = find_2_mins(grid[0]);
-    for row in grid.iter().skip(1) {
-        let mut dp = Vec::with_capacity(n);
-        for (idx, &num) in row.iter().enumerate() {
-            if idx == i1 {
-                dp.push(num + n2);
-            } else {
-                dp.push(num + n1);
+pub fn sequential_digits(low: i32, high: i32) -> Vec<i32> {
+    const SEQ: &str = "123456789";
+    let n1 = 1 + low.ilog10() as usize;
+    let n2 = (1 + high.ilog10() as usize).min(9);
+    let mut res = vec![];
+    for len in n1..=n2 {
+        for i in 0..=9 - len {
+            let v = SEQ[i..i + len].parse::<i32>().unwrap();
+            if (low..=high).contains(&v) {
+                res.push(v);
             }
         }
-        [(i1, n1), (_i2, n2)] = find_2_mins(&dp);
     }
-    n1
-}
-
-fn find_2_mins(row: &[i32]) -> [(usize, i32); 2] {
-    let (mut i1, mut n1) = (0, i32::MAX);
-    let (mut i2, mut n2) = (0, i32::MAX);
-    for (idx, &num) in row.iter().enumerate() {
-        if num < n1 {
-            i2 = i1;
-            n2 = n1;
-            n1 = num;
-            i1 = idx
-        } else if num < n2 {
-            n2 = num;
-            i2 = idx;
-        }
-    }
-    [(i1, n1), (i2, n2)]
+    res.sort_unstable();
+    res
 }
 
 #[cfg(test)]
@@ -50,30 +30,23 @@ mod tests {
 
     #[test]
     fn basics() {
+        assert_eq!(sequential_digits(100, 300), [123, 234]);
         assert_eq!(
-            min_falling_path_sum(&[&[1, 2, 3], &[4, 5, 6], &[7, 8, 9]]),
-            13
+            sequential_digits(1000, 13000),
+            [1234, 2345, 3456, 4567, 5678, 6789, 12345]
         );
-        assert_eq!(min_falling_path_sum(&[&[7]]), 7);
     }
 
     #[test]
     fn test() {
         assert_eq!(
-            min_falling_path_sum(&[
-                &[50, -18, -38, 39, -20, -37, -61, 72, 22, 79],
-                &[82, 26, 30, -96, -1, 28, 87, 94, 34, -89],
-                &[55, -50, 20, 76, -50, 59, -58, 85, 83, -83],
-                &[39, 65, -68, 89, -62, -53, 74, 2, -70, -90],
-                &[1, 57, -70, 83, -91, -32, -13, 49, -11, 58],
-                &[-55, 83, 60, -12, -90, -37, -36, -27, -19, -6],
-                &[76, -53, 78, 90, 70, 62, -81, -94, -32, -57],
-                &[-32, -85, 81, 25, 80, 90, -24, 10, 27, -55],
-                &[39, 54, 39, 34, -45, 17, -2, -61, -81, 85],
-                &[-77, 65, 76, 92, 21, 68, 78, -13, 39, 22]
-            ]),
-            -807
-        );
+            sequential_digits(10, 1000000000),
+            [
+                12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345,
+                3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678,
+                456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789
+            ]
+        )
     }
 
     #[allow(dead_code)]
