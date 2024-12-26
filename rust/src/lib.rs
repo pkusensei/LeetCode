5a7559d2062f28eb6ad3d83b82ae84b1b31cb5e4
pkusensei <ZW_08@outlook.com>
2024-12-26 12:11:47 -0500

    1300 binary search inside another binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,14 +5,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn replace_elements(arr: &[i32]) -> Vec<i32> {
-    let mut res = Vec::with_capacity(arr.len());
-    let mut curr = -1;
-    for &num in arr.iter().rev() {
-        res.push(curr);
-        curr = curr.max(num);
+pub fn find_best_value(arr: &mut [i32], target: i32) -> i32 {
+    let n = arr.len();
+    arr.sort_unstable();
+    let mut prefix = Vec::with_capacity(n);
+    for &num in arr.iter() {
+        prefix.push(num + prefix.last().unwrap_or(&0));
     }
-    res.reverse();
+    let mut left = 0;
+    let mut right = arr[n - 1];
+    let mut curr_diff = u32::MAX;
+    let mut res = i32::MAX;
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        let i = arr.partition_point(|&v| v <= mid);
+        let sum = if i > 0 {
+            prefix[i - 1] + mid * (n - i) as i32
+        } else {
+            mid * n as i32
+        };
+        if sum <= target {
+            left = 1 + mid;
+        } else {
+            right = mid - 1;
+        }
+        if sum.abs_diff(target) < curr_diff {
+            curr_diff = sum.abs_diff(target);
+            res = mid;
+        }
+        if sum.abs_diff(target) == curr_diff {
+            res = res.min(mid);
+        }
+    }
     res
 }
 
@@ -24,11 +48,12 @@ mod tests {
 
     #[test]
     fn basics() {
+        assert_eq!(find_best_value(&mut [4, 9, 3], 10), 3);
+        assert_eq!(find_best_value(&mut [2, 3, 5], 10), 5);
         assert_eq!(
-            replace_elements(&[17, 18, 5, 4, 6, 1]),
-            [18, 6, 6, 6, 1, -1]
+            find_best_value(&mut [60864, 25176, 27249, 21296, 20204], 56803),
+            11361
         );
-        assert_eq!(replace_elements(&[400]), [-1]);
     }
 
     #[test]
