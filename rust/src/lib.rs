988a51928c82061325deb309944777d1ee2a8d35
pkusensei <ZW_08@outlook.com>
2024-12-27 20:20:35 -0500

    1309 str mapping to int

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,73 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_solvable(words: &[&str], result: &str) -> bool {
-    let (coeffs, nonzeros) = build(words, result);
-    backtrack(&coeffs, &nonzeros, &mut [false; 10], &mut [0; 10], 0, 0)
-}
-
-fn backtrack(
-    coeffs: &[i32],
-    nonzeros: &[bool],
-    used: &mut [bool; 10],
-    values: &mut [i32; 10], // at most 10 unique chars
-    idx: usize,
-    curr: i32,
-) -> bool {
-    if idx == coeffs.len() {
-        return curr == 0;
-    }
-    // Pruning on potential result
-    let max: i32 = coeffs[idx..]
-        .iter()
-        .map(|&v| if v > 0 { 9 * v } else { 0 })
-        .sum();
-    let min: i32 = coeffs[idx..]
-        .iter()
-        .map(|&v| if v > 0 { 0 } else { 9 * v })
-        .sum();
-    // sum cannot ever reach 0 by current config
-    if curr + max < 0 || curr + min > 0 {
-        return false;
-    }
-    let temp = values[idx];
-    let start = if nonzeros[idx] { 1 } else { 0 };
-    for digit in start..=9 {
-        if !used[digit as usize] {
-            used[digit as usize] = true;
-            values[idx] = digit;
-            let next = curr + digit * coeffs[idx];
-            if backtrack(coeffs, nonzeros, used, values, 1 + idx, next) {
-                return true;
-            }
-            used[digit as usize] = false;
+pub fn freq_alphabets(s: &str) -> String {
+    let (s, n) = (s.as_bytes(), s.len());
+    let mut res = vec![];
+    let mut i = n - 1;
+    loop {
+        let b = if s[i] == b'#' {
+            i -= 2;
+            10 * (s[i] - b'0') + s[i + 1] - b'0'
+        } else {
+            s[i] - b'0'
+        };
+        res.push(b + b'a' - 1);
+        if i == 0 {
+            break;
         }
+        i -= 1
     }
-    values[idx] = temp;
-    false
-}
-
-fn build(words: &[&str], result: &str) -> (Vec<i32>, Vec<bool>) {
-    let mut coeffs = [(0, false); 26];
-    for word in words.iter() {
-        process(word, 1, &mut coeffs);
-    }
-    process(result, -1, &mut coeffs);
-    // coeffs.sort_by_key(|(coeff, _)| std::cmp::Reverse(coeff.abs()));
-    coeffs.into_iter().filter(|(coeff, _)| *coeff != 0).unzip()
-}
-
-fn process(word: &str, sign: i32, coeffs: &mut [(i32, bool); 26]) {
-    let mut pow = 1;
-    for (idx, b) in word.bytes().enumerate().rev() {
-        let ch = usize::from(b - b'A');
-        coeffs[ch].0 += sign * pow;
-        pow *= 10;
-        // leading chars can't be zero
-        if word.len() > 1 && idx == 0 {
-            coeffs[ch].1 = true;
-        }
-    }
+    res.into_iter().rev().map(|b| b as char).collect()
 }
 
 #[cfg(test)]
@@ -82,9 +33,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(is_solvable(&["SEND", "MORE"], "MONEY"));
-        assert!(is_solvable(&["SIX", "SEVEN", "SEVEN"], "TWENTY"));
-        assert!(!is_solvable(&["LEET", "CODE"], "POINT"));
+        assert_eq!(freq_alphabets("10#11#12"), "jkab");
+        assert_eq!(freq_alphabets("1326#"), "acz");
     }
 
     #[test]
