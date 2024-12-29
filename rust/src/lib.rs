6dccf0912559cf3414644223a2907c252bd5350c
pkusensei <ZW_08@outlook.com>
2024-12-28 21:21:07 -0500

    1639 pick-unpick in disguise

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,10 +5,47 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn decompress_rl_elist(nums: &[i32]) -> Vec<i32> {
-    nums.chunks_exact(2)
-        .flat_map(|c| std::iter::repeat(c[1]).take(c[0] as usize))
-        .collect()
+pub fn num_ways(words: &[&str], target: &str) -> i32 {
+    let (wn, tn) = (words[0].len(), target.len());
+    let mut freq: Vec<_> = std::iter::repeat([0; 26]).take(wn).collect();
+    for word in words.iter() {
+        for (i, b) in word.bytes().enumerate() {
+            freq[i][usize::from(b - b'a')] += 1;
+        }
+    }
+    let mut prev = vec![0; 1 + tn];
+    prev[0] = 1;
+    for fi in 1..=wn {
+        let mut curr = prev.clone();
+        for ti in 1..=tn {
+            let pos = usize::from(target.as_bytes()[ti - 1] - b'a');
+            curr[ti] += freq[fi - 1][pos] * prev[ti - 1];
+            curr[ti] %= MOD;
+        }
+        prev = curr;
+    }
+    prev[tn] as _
+    // dfs(&freq, target.as_bytes(), 0, 0, &mut vec![vec![-1; tn]; wn]) as _
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn dfs(freq: &[[i64; 26]], target: &[u8], fi: usize, ti: usize, memo: &mut [Vec<i64>]) -> i64 {
+    if ti == target.len() {
+        return 1;
+    }
+    if freq.len() - fi < target.len() - ti {
+        return 0;
+    }
+    if memo[fi][ti] > -1 {
+        return memo[fi][ti];
+    }
+    let byte = usize::from(target[ti] - b'a');
+    let mut res = dfs(freq, target, 1 + fi, ti, memo);
+    res += freq[fi][byte] * dfs(freq, target, 1 + fi, 1 + ti, memo);
+    res %= MOD;
+    memo[fi][ti] = res;
+    res
 }
 
 #[cfg(test)]
@@ -18,7 +55,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(num_ways(&["acca", "bbbb", "caca"], "aba"), 6);
+        assert_eq!(num_ways(&["abba", "baab"], "bab"), 4);
+    }
 
     #[test]
     fn test() {}
