97c8202ca0fc1780f9310f413c2f04d35265331a
pkusensei <ZW_08@outlook.com>
2024-12-30 17:06:04 -0500

    1325 remove leaf with target value

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,28 +6,12 @@ namespace Solution;
 
 public class Solution
 {
-    public int SumEvenGrandparent(TreeNode root)
+    public TreeNode RemoveLeafNodes(TreeNode root, int target)
     {
-        if (root is null) { return 0; }
-        var res = SumSubtree(root);
-        res += SumEvenGrandparent(root.left) + SumEvenGrandparent(root.right);
-        return res;
-
-        static int SumSubtree(TreeNode node)
-        {
-            if (node is null || (node.val & 1) == 1) { return 0; }
-            var res = 0;
-            if (node.left is not null)
-            {
-                res += node.left.left is null ? 0 : node.left.left.val;
-                res += node.left.right is null ? 0 : node.left.right.val;
-            }
-            if (node.right is not null)
-            {
-                res += node.right.left is null ? 0 : node.right.left.val;
-                res += node.right.right is null ? 0 : node.right.right.val;
-            }
-            return res;
-        }
+        if (root is null) { return null; }
+        root.left = RemoveLeafNodes(root.left, target);
+        root.right = RemoveLeafNodes(root.right, target);
+        if (root.left is null && root.right is null && root.val == target) { return null; }
+        return root;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,20 +12,22 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = TreeNode.Make([6, 7, 8, 2, 7, 1, 3, 9, null, 1, 4, null, null, null, 5]);
-        Assert.AreEqual(18, sol.SumEvenGrandparent(a));
+        var a = TreeNode.Make([1, 2, 3, 2, null, 2, 4]);
+        Assert.AreEqual("[1,null,3,null,4]", sol.RemoveLeafNodes(a, 2).ToString());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([1]);
-        Assert.AreEqual(0, sol.SumEvenGrandparent(a));
+        var a = TreeNode.Make([1, 3, 3, 3, 2]);
+        Assert.AreEqual("[1,3,null,null,2]", sol.RemoveLeafNodes(a, 3).ToString());
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([1, 2, null, 2, null, 2]);
+        Assert.AreEqual("[1]", sol.RemoveLeafNodes(a, 2).ToString());
     }
 
     [TestMethod]
