8ce4130555e26323dccd868f949c6d124f353434
pkusensei <ZW_08@outlook.com>
2024-12-30 11:07:08 -0500

    1319 dsu

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,56 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_flips(mut a: i32, mut b: i32, mut c: i32) -> i32 {
-    let mut res = 0;
-    while [a, b, c].iter().any(|&v| v > 0) {
-        if c & 1 == 0 {
-            res += (a & 1) + (b & 1);
-        } else {
-            res += match (a & 1, b & 1) {
-                (1, 0) | (0, 1) => 0,
-                (0, 0) => 1,
-                _ => 0,
-            };
-        }
-        [a, b, c] = [a, b, c].map(|v| v >> 1);
+pub fn make_connected(n: i32, connections: &[[i32; 2]]) -> i32 {
+    let n = n as usize;
+    if connections.len() + 1 < n {
+        return -1;
     }
-    res
+    let mut dsu = DSU::new(n);
+    for e in connections.iter() {
+        dsu.union(e[0] as usize, e[1] as usize);
+    }
+    dsu.count as i32 - 1
+}
+
+#[derive(Debug, Clone)]
+struct DSU {
+    parent: Vec<usize>,
+    rank: Vec<i32>,
+    count: usize,
+}
+
+impl DSU {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            rank: vec![1; n],
+            count: n,
+        }
+    }
+
+    pub fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x])
+        }
+        self.parent[x]
+    }
+
+    pub fn union(&mut self, x: usize, y: usize) {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return;
+        }
+        match self.rank[rx].cmp(&self.rank[ry]) {
+            std::cmp::Ordering::Less => self.parent[rx] = ry,
+            std::cmp::Ordering::Equal => {
+                self.parent[ry] = rx;
+                self.rank[rx] += 1;
+            }
+            std::cmp::Ordering::Greater => self.parent[ry] = rx,
+        }
+        self.count -= 1;
+    }
 }
 
 #[cfg(test)]
@@ -30,15 +65,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_flips(2, 6, 5), 3);
-        assert_eq!(min_flips(4, 2, 7), 1);
-        assert_eq!(min_flips(1, 2, 3), 0);
+        assert_eq!(make_connected(4, &[[0, 1], [0, 2], [1, 2]]), 1);
+        assert_eq!(
+            make_connected(6, &[[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]),
+            2
+        );
+        assert_eq!(make_connected(6, &[[0, 1], [0, 2], [0, 3], [1, 2]]), -1);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(min_flips(8, 3, 5), 3);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
