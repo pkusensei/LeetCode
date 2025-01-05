cba43a411e134902c9cb2d9192a6682dbf1f4f97
pkusensei <ZW_08@outlook.com>
2025-01-05 13:36:14 -0500

    1333 filter and sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,12 +5,25 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn remove_palindrome_sub(s: &str) -> i32 {
-    if s.bytes().zip(s.bytes().rev()).all(|(a, b)| a == b) {
-        1
-    } else {
-        2
-    }
+pub fn filter_restaurants(
+    restaurants: Vec<[i32; 5]>,
+    vegan_friendly: i32,
+    max_price: i32,
+    max_distance: i32,
+) -> Vec<i32> {
+    let mut res: Vec<_> = restaurants
+        .into_iter()
+        .filter_map(|v| {
+            let [id, rating, vf, price, dist] = v;
+            if vf >= vegan_friendly && price <= max_price && dist <= max_distance {
+                Some([id, rating])
+            } else {
+                None
+            }
+        })
+        .collect();
+    res.sort_unstable_by(|a, b| b[1].cmp(&a[1]).then(b[0].cmp(&a[0])));
+    res.into_iter().map(|[id, _]| id).collect()
 }
 
 #[cfg(test)]
@@ -21,9 +34,51 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(remove_palindrome_sub("ababa"), 1);
-        assert_eq!(remove_palindrome_sub("abb"), 2);
-        assert_eq!(remove_palindrome_sub("baabb"), 2);
+        assert_eq!(
+            filter_restaurants(
+                vec![
+                    [1, 4, 1, 40, 10],
+                    [2, 8, 0, 50, 5],
+                    [3, 8, 1, 30, 4],
+                    [4, 10, 0, 10, 3],
+                    [5, 1, 1, 15, 1]
+                ],
+                1,
+                50,
+                10
+            ),
+            [3, 1, 5]
+        );
+        assert_eq!(
+            filter_restaurants(
+                vec![
+                    [1, 4, 1, 40, 10],
+                    [2, 8, 0, 50, 5],
+                    [3, 8, 1, 30, 4],
+                    [4, 10, 0, 10, 3],
+                    [5, 1, 1, 15, 1]
+                ],
+                0,
+                50,
+                10
+            ),
+            [4, 3, 2, 1, 5]
+        );
+        assert_eq!(
+            filter_restaurants(
+                vec![
+                    [1, 4, 1, 40, 10],
+                    [2, 8, 0, 50, 5],
+                    [3, 8, 1, 30, 4],
+                    [4, 10, 0, 10, 3],
+                    [5, 1, 1, 15, 1]
+                ],
+                0,
+                30,
+                3
+            ),
+            [4, 5]
+        );
     }
 
     #[test]
