28ae5033ad3c5bdc1f9e32cd560d300144f21a12
pkusensei <ZW_08@outlook.com>
2025-01-06 15:06:58 -0500

    1350 binary search to find negative numbers

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,37 +5,13 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_students(seats: &[&[char]]) -> i32 {
-    let (rows, cols) = get_dimensions(seats);
-    let valid: Vec<_> = seats
-        .iter()
+pub fn count_negatives(grid: &[&[i32]]) -> i32 {
+    grid.iter()
         .map(|row| {
-            row.iter()
-                .fold(0, |acc, &ch| acc << 1 | usize::from(ch == '.'))
+            let i = row.partition_point(|&v| v >= 0);
+            (row.len() - i) as i32
         })
-        .collect();
-    let mut prev = vec![-1; 1 << cols];
-    prev[0] = 0; // start with a "virtual" row
-    for row in 1..=rows {
-        let mut curr = vec![-1; 1 << cols];
-        for (mask, curr_val) in curr.iter_mut().enumerate() {
-            // no '#' seats
-            // no adjacent seats
-            if mask & valid[row - 1] != mask || mask & (mask >> 1) > 0 {
-                continue;
-            }
-            for (prev_mask, &prev_val) in prev.iter().enumerate() {
-                // upper right
-                // upper left
-                // prev_val is valid state
-                if (mask >> 1) & prev_mask == 0 && mask & (prev_mask >> 1) == 0 && prev_val != -1 {
-                    *curr_val = (*curr_val).max(prev_val + mask.count_ones() as i32);
-                }
-            }
-        }
-        prev = curr;
-    }
-    prev.into_iter().max().unwrap()
+        .sum()
 }
 
 #[cfg(test)]
@@ -47,33 +23,15 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            max_students(&[
-                &['#', '.', '#', '#', '.', '#'],
-                &['.', '#', '#', '#', '#', '.'],
-                &['#', '.', '#', '#', '.', '#']
+            count_negatives(&[
+                &[4, 3, 2, -1],
+                &[3, 2, 1, -1],
+                &[1, 1, -1, -2],
+                &[-1, -1, -2, -3]
             ]),
-            4
+            8
         );
-        assert_eq!(
-            max_students(&[
-                &['.', '#'],
-                &['#', '#'],
-                &['#', '.'],
-                &['#', '#'],
-                &['.', '#']
-            ]),
-            3
-        );
-        assert_eq!(
-            max_students(&[
-                &['#', '.', '.', '.', '#'],
-                &['.', '#', '.', '#', '.'],
-                &['.', '.', '#', '.', '.'],
-                &['.', '#', '.', '#', '.'],
-                &['#', '.', '.', '.', '#']
-            ]),
-            10
-        );
+        assert_eq!(count_negatives(&[&[3, 2], &[1, 0]]), 0);
     }
 
     #[test]
