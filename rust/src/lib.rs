83b1ef7f6d7fba49eec99e03158f399682220b60
pkusensei <ZW_08@outlook.com>
2025-01-06 13:05:09 -0500

    1348 count tweets in time intervals

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,21 +2,63 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{BTreeMap, HashMap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_steps(s: &str, t: &str) -> i32 {
-    let [a, b] = [s, t].map(|v| {
-        v.bytes().fold([0i32; 26], |mut acc, b| {
-            acc[usize::from(b - b'a')] += 1;
-            acc
-        })
-    });
-    (a.into_iter()
-        .zip(b)
-        .map(|(a, b)| a.abs_diff(b))
-        .sum::<u32>()
-        / 2) as i32
+#[derive(Debug, Clone, Default)]
+struct TweetCounts {
+    data: HashMap<String, BTreeMap<i32, i32>>,
+}
+
+impl TweetCounts {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn record_tweet(&mut self, tweet_name: String, time: i32) {
+        *self
+            .data
+            .entry(tweet_name)
+            .or_default()
+            .entry(time)
+            .or_insert(0) += 1;
+    }
+
+    fn get_tweet_counts_per_frequency(
+        &self,
+        freq: String,
+        tweet_name: String,
+        mut start_time: i32,
+        end_time: i32,
+    ) -> Vec<i32> {
+        let mut res = vec![];
+        let Some(map) = self.data.get(&tweet_name) else {
+            return res;
+        };
+
+        fn next(freq: &str, start: i32, end: i32) -> Option<[i32; 2]> {
+            if start > end {
+                return None;
+            }
+            let interval = match freq {
+                "minute" => 60,
+                "hour" => 60 * 60,
+                "day" => 24 * 60 * 60,
+                _ => return None,
+            };
+            let end = end.min(start + interval - 1);
+            Some([start, end])
+        }
+
+        while let Some([start, end]) = next(&freq, start_time, end_time) {
+            let curr: i32 = map.range(start..=end).map(|(_k, v)| v).sum();
+            res.push(curr);
+            start_time = 1 + end;
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -27,9 +69,23 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_steps("bab", "aba"), 1);
-        assert_eq!(min_steps("leetcode", "practice"), 5);
-        assert_eq!(min_steps("anagram", "mangaar"), 0);
+        let mut tc = TweetCounts::new();
+        tc.record_tweet("tweet3".into(), 0); // New tweet "tweet3" at time 0
+        tc.record_tweet("tweet3".into(), 60); // New tweet "tweet3" at time 60
+        tc.record_tweet("tweet3".into(), 10); // New tweet "tweet3" at time 10
+        assert_eq!(
+            tc.get_tweet_counts_per_frequency("minute".into(), "tweet3".into(), 0, 59),
+            [2]
+        ); // return [2]; chunk [0,59] had 2 tweets
+        assert_eq!(
+            tc.get_tweet_counts_per_frequency("minute".into(), "tweet3".into(), 0, 60),
+            [2, 1]
+        ); // return [2,1]; chunk [0,59] had 2 tweets, chunk [60,60] had 1 tweet
+        tc.record_tweet("tweet3".into(), 120); // New tweet "tweet3" at time 120
+        assert_eq!(
+            tc.get_tweet_counts_per_frequency("hour".into(), "tweet3".into(), 0, 210),
+            [4]
+        ); // return [4]; chunk [0,210] had 4 tweets
     }
 
     #[test]
