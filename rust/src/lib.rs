e3960d952b87e62895d62abcbf023dd0196353e3
pkusensei <ZW_08@outlook.com>
2025-01-06 21:24:48 -0500

    1408 find all substrs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,32 +2,114 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::HashMap;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_events(events: &mut [[i32; 2]]) -> i32 {
-    let n = events.len();
-    let max_day = events.iter().map(|e| e[1]).max().unwrap_or(100_000);
-    events.sort_unstable();
-    let mut res = 0;
-    let mut idx = 0;
-    let mut heap = BinaryHeap::<Reverse<i32>>::new();
-    for d in 1..=max_day {
-        // remove completed events
-        while heap.peek().is_some_and(|v| v.0 < d) {
-            heap.pop();
+pub fn string_matching(words: &[&str]) -> Vec<String> {
+    let mut res = std::collections::HashSet::new();
+    for (i, a) in words.iter().enumerate() {
+        for b in words.iter().skip(1 + i) {
+            if a.len() != b.len() {
+                if a.contains(b) {
+                    res.insert(b);
+                }
+                if b.contains(a) {
+                    res.insert(a);
+                }
+            }
         }
-        // push any event that starts on d
-        while idx < n && events[idx][0] == d {
-            heap.push(Reverse(events[idx][1]));
-            idx += 1;
+    }
+    res.into_iter().map(|s| s.to_string()).collect()
+}
+
+fn with_kmp(words: &[&str]) -> Vec<String> {
+    fn compute_lps(s: &str) -> Vec<usize> {
+        let (s, n) = (s.as_bytes(), s.len());
+        let mut res = vec![0; n];
+        let (mut idx, mut len) = (1, 0);
+        while idx < n {
+            if s[idx] == s[len] {
+                len += 1;
+                res[idx] = len;
+                idx += 1;
+            } else if len > 0 {
+                len = res[len - 1];
+            } else {
+                idx += 1;
+            }
         }
-        // select one event ending on d
-        if !heap.is_empty() {
-            heap.pop();
-            res += 1;
+        res
+    }
+    fn is_sub_of(needle: &str, hay: &str, lps: &[usize]) -> bool {
+        let [mut hay_idx, mut needle_idx] = [0; 2];
+        let [hay, needle] = [hay, needle].map(|v| v.as_bytes());
+        while hay_idx < hay.len() {
+            if hay[hay_idx] == needle[needle_idx] {
+                hay_idx += 1;
+                needle_idx += 1;
+                if needle_idx == needle.len() {
+                    return true;
+                }
+            } else if needle_idx > 0 {
+                needle_idx = lps[needle_idx - 1];
+            } else {
+                hay_idx += 1;
+            }
+        }
+        false
+    }
+
+    let mut res = vec![];
+    for a in words.iter() {
+        let lps = compute_lps(a);
+        for b in words.iter() {
+            if a == b {
+                continue;
+            }
+            if is_sub_of(a, b, &lps) {
+                res.push(a.to_string());
+                break;
+            }
+        }
+    }
+    res
+}
+
+fn with_suffix_trie(words: &[&str]) -> Vec<String> {
+    #[derive(Debug, Clone, Default)]
+    struct Trie {
+        freq: i32,
+        nodes: HashMap<u8, Trie>,
+    }
+    impl Trie {
+        fn insert(&mut self, s: &str) {
+            let mut curr = self;
+            for b in s.bytes() {
+                curr = curr.nodes.entry(b).or_default();
+                curr.freq += 1;
+            }
+        }
+        fn find(&self, s: &str) -> bool {
+            let mut curr = self;
+            for b in s.bytes() {
+                curr = &curr.nodes[&b];
+            }
+            curr.freq > 1
+        }
+    }
+
+    let mut root = Trie::default();
+    for word in words.iter() {
+        for i in 0..word.len() {
+            root.insert(&word[i..]);
+        }
+    }
+    let mut res = vec![];
+    for word in words.iter() {
+        if root.find(word) {
+            res.push(word.to_string());
         }
     }
     res
@@ -41,15 +123,33 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_events(&mut [[1, 2], [2, 3], [3, 4]]), 3);
-        assert_eq!(max_events(&mut [[1, 2], [2, 3], [3, 4], [1, 2]]), 4);
+        sort_eq(
+            string_matching(&["mass", "as", "hero", "superhero"]),
+            ["as", "hero"],
+        );
+        sort_eq(string_matching(&["leetcode", "et", "code"]), ["et", "code"]);
+        assert!(string_matching(&["blue", "green", "bu"]).is_empty());
+
+        sort_eq(
+            with_kmp(&["mass", "as", "hero", "superhero"]),
+            ["as", "hero"],
+        );
+        sort_eq(with_kmp(&["leetcode", "et", "code"]), ["et", "code"]);
+        assert!(with_kmp(&["blue", "green", "bu"]).is_empty());
+
+        sort_eq(
+            with_suffix_trie(&["mass", "as", "hero", "superhero"]),
+            ["as", "hero"],
+        );
+        sort_eq(
+            with_suffix_trie(&["leetcode", "et", "code"]),
+            ["et", "code"],
+        );
+        assert!(with_suffix_trie(&["blue", "green", "bu"]).is_empty());
     }
 
     #[test]
-    fn test() {
-        assert_eq!(max_events(&mut [[1, 2], [1, 2], [3, 3], [1, 5], [1, 5]]), 5);
-        assert_eq!(max_events(&mut [[1, 4], [4, 4], [2, 2], [3, 4], [1, 1]]), 4);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
