53727de1ba92e0776a5c88dc83b1d194046bb9ae
Zx Wang <ZW_08@outlook.com>
2025-01-09 15:40:58 -0500

    1382 balance binary search tree; DSW algo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,10 +6,82 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+    public TreeNode BalanceBST(TreeNode root)
     {
-        if (original is null) { return null; }
-        if (object.ReferenceEquals(original, target)) { return cloned; }
-        return GetTargetCopy(original.left, cloned.left, target) ?? GetTargetCopy(original.right, cloned.right, target);
+        List<int> vals = [];
+        Inorder(root);
+        return Construct([.. vals]);
+
+        void Inorder(TreeNode node)
+        {
+            if (node is null) { return; }
+            Inorder(node.left);
+            vals.Add(node.val);
+            Inorder(node.right);
+        }
+
+        static TreeNode Construct(int[] vals)
+        {
+            if (vals.Length == 0) { return null; }
+            if (vals.Length == 1) { return new(vals[0]); }
+            var node = vals[vals.Length / 2];
+            var left = Construct(vals[..(vals.Length / 2)]);
+            var right = Construct(vals[(1 + vals.Length / 2)..]);
+            return new(node, left, right);
+        }
+    }
+
+    public TreeNode WithDSW(TreeNode root)
+    {
+        if (root is null) { return null; }
+        TreeNode vinehead = new(0, null, root);
+        var curr = vinehead;
+        while (curr.right is not null)
+        {
+            if (curr.right.left is not null) { RotateRight(curr, curr.right); }
+            else { curr = curr.right; }
+        }
+        var count = 0;
+        curr = vinehead.right;
+        while (curr is not null)
+        {
+            curr = curr.right;
+            count += 1;
+        }
+        var depth = (int)Math.Pow(2, Math.Floor(Math.Log2(1 + count))) - 1;
+        MakeRotations(vinehead, count - depth);
+        while (depth > 1)
+        {
+            depth /= 2;
+            MakeRotations(vinehead, depth);
+        }
+        return vinehead.right;
+
+        static void RotateRight(TreeNode parent, TreeNode node)
+        {
+            TreeNode temp = node.left;
+            node.left = temp.right;
+            temp.right = node;
+            parent.right = temp;
+        }
+
+        static void RotateLeft(TreeNode parent, TreeNode node)
+        {
+            TreeNode temp = node.right;
+            node.right = temp.left;
+            temp.left = node;
+            parent.right = temp;
+        }
+
+        static void MakeRotations(TreeNode vinehead, int count)
+        {
+            var curr = vinehead;
+            for (int i = 0; i < count; i++)
+            {
+                var temp = curr.right;
+                RotateLeft(curr, temp);
+                curr = curr.right;
+            }
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,11 +12,17 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
+        var a = TreeNode.Make([1, null, 2, null, 3, null, 4, null, null]);
+        Assert.AreEqual("[3,2,4,1]", sol.BalanceBST(a).ToString());
+        Assert.AreEqual("[3,2,4,1]", sol.WithDSW(a).ToString());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        var a = TreeNode.Make([2, 1, 3]);
+        Assert.AreEqual("[2,1,3]", sol.BalanceBST(a).ToString());
+        Assert.AreEqual("[2,1,3]", sol.WithDSW(a).ToString());
     }
 
     [TestMethod]
