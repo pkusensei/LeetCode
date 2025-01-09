8e0d0f8c232660be6e69d76c46345f16e0c20ec7
Zx Wang <ZW_08@outlook.com>
2025-01-09 13:15:15 -0500

    1379 object.ReferenceEquals

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,43 +6,10 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxSumBST(TreeNode root)
+    public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
     {
-        var min_sum = int.MinValue;
-        Dfs(root);
-        return min_sum < 0 ? 0 : min_sum;
-
-        State? Dfs(TreeNode node)
-        {
-            if (node is null) { return null; }
-            (var left, var right) = (Dfs(node.left), Dfs(node.right));
-            State? res = null;
-            if (!left.HasValue && !right.HasValue)
-            {
-                res = new State(true, node.val, node.val, node.val);
-            }
-            else if (left.HasValue && !right.HasValue)
-            {
-                bool is_bst = left.Value.IsBst && left.Value.MaxLeft < node.val;
-                res = new State(is_bst, left.Value.Sum + node.val, node.val, left.Value.MinRight);
-            }
-            else if (!left.HasValue && right.HasValue)
-            {
-                bool is_bst = right.Value.IsBst && node.val < right.Value.MinRight;
-                res = new State(is_bst, right.Value.Sum + node.val, right.Value.MaxLeft, node.val);
-            }
-            else
-            {
-                bool is_bst = left.Value.IsBst && left.Value.MaxLeft < node.val
-                    && right.Value.IsBst && node.val < right.Value.MinRight;
-                res = new State(is_bst, left.Value.Sum + right.Value.Sum + node.val,
-                                right.Value.MaxLeft, left.Value.MinRight);
-            }
-            if (res.Value.IsBst) { min_sum = Math.Max(min_sum, res.Value.Sum); }
-            return res;
-        }
+        if (original is null) { return null; }
+        if (object.ReferenceEquals(original, target)) { return cloned; }
+        return GetTargetCopy(original.left, cloned.left, target) ?? GetTargetCopy(original.right, cloned.right, target);
     }
-
 }
-
-public readonly record struct State(bool IsBst, int Sum, int MaxLeft, int MinRight);
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,22 +12,16 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = TreeNode.Make([1, 4, 3, 2, 4, 2, 5, null, null, null, null, null, null, 4, 6]);
-        Assert.AreEqual(20, sol.MaxSumBST(a));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([4, 3, null, 1, 2]);
-        Assert.AreEqual(2, sol.MaxSumBST(a));
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-        var a = TreeNode.Make([-4, -2, -5]);
-        Assert.AreEqual(0, sol.MaxSumBST(a));
     }
 
     [TestMethod]
