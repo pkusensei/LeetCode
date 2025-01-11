07d23259c07a196d62913eda21c0eda65055d983
pkusensei <ZW_08@outlook.com>
2025-01-10 20:05:52 -0500

    1400 construct k palindromes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,50 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn has_valid_path(grid: &[&[i32]]) -> bool {
-    let [rows, cols] = get_dimensions(grid).map(|v| v as i32);
-    let mut queue = std::collections::VecDeque::from([[0, 0]]);
-    let mut seen = vec![vec![false; cols as usize]; rows as usize];
-    seen[0][0] = true;
-    while let Some([row, col]) = queue.pop_front() {
-        if row == rows - 1 && col == cols - 1 {
-            return true;
-        }
-        for [dr, dc] in dir(grid[row as usize][col as usize]) {
-            let nr = dr + row;
-            let nc = dc + col;
-            if (0..rows).contains(&nr)
-                && (0..cols).contains(&nc)
-                && is_connected([row, col], [nr, nc], grid)
-                && !seen[nr as usize][nc as usize]
-            {
-                seen[nr as usize][nc as usize] = true;
-                queue.push_back([nr, nc]);
-            }
-        }
+pub fn can_construct(s: &str, k: i32) -> bool {
+    if s.len() < k as usize {
+        return false;
     }
-    false
-}
-
-fn is_connected(prev: [i32; 2], next: [i32; 2], grid: &[&[i32]]) -> bool {
-    for [dr, dc] in dir(grid[next[0] as usize][next[1] as usize]) {
-        if next[0] + dr == prev[0] && next[1] + dc == prev[1] {
-            return true;
-        }
-    }
-    false
-}
-
-const fn dir(num: i32) -> [[i32; 2]; 2] {
-    match num {
-        1 => [[0, -1], [0, 1]],
-        2 => [[1, 0], [-1, 0]],
-        3 => [[0, -1], [1, 0]],
-        4 => [[0, 1], [1, 0]],
-        5 => [[0, -1], [-1, 0]],
-        6 => [[0, 1], [-1, 0]],
-        _ => unreachable!(),
-    }
+    s.bytes()
+        .fold([0; 26], |mut acc, b| {
+            acc[usize::from(b - b'a')] += 1;
+            acc
+        })
+        .into_iter()
+        .map(|v| v & 1)
+        .sum::<i32>()
+        <= k
 }
 
 #[cfg(test)]
@@ -59,15 +28,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(has_valid_path(&[&[2, 4, 3], &[6, 5, 2]]));
-        assert!(!has_valid_path(&[&[1, 2, 1], &[1, 2, 1]]));
-        assert!(!has_valid_path(&[&[1, 1, 2]]))
+        assert!(can_construct("annabelle", 2));
+        assert!(!can_construct("leetcode", 3));
+        assert!(can_construct("true", 4));
     }
 
     #[test]
-    fn test() {
-        assert!(has_valid_path(&[&[4, 1], &[6, 1]]))
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
