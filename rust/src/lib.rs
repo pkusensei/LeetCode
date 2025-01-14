cf91c865ba39a98ea66101adf181ae1f39072aa9
Zx Wang <ZW_08@outlook.com>
2025-01-14 13:22:12 -0500

    1432 edge cases...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,9 +5,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Vec<bool> {
-    let max = candies.iter().copied().max().unwrap();
-    candies.iter().map(|v| v + extra_candies >= max).collect()
+pub fn max_diff(num: i32) -> i32 {
+    let mut digits = vec![];
+    let mut x = num;
+    while x > 0 {
+        digits.push(x % 10);
+        x /= 10;
+    }
+    digits.reverse();
+    if digits.len() == 1 {
+        8
+    } else {
+        let a = to_big(&digits);
+        let b = to_small(digits);
+        a - b
+    }
+}
+
+fn to_small(digits: Vec<i32>) -> i32 {
+    let (x, y) = if digits[0] > 1 {
+        (digits[0], 1)
+    } else {
+        // digits[0] == 1
+        let Some(&v) = digits.iter().find(|&&v| v > 1) else {
+            return digits.iter().fold(0, |acc, d| acc * 10 + d);
+        };
+        (v, 0)
+    };
+    digits
+        .into_iter()
+        .fold(0, |acc, d| acc * 10 + if d == x { y } else { d })
+}
+
+fn to_big(digits: &[i32]) -> i32 {
+    let Some(&v) = digits.iter().find(|&&v| v < 9) else {
+        return digits.iter().fold(0, |acc, d| acc * 10 + d);
+    };
+    digits
+        .iter()
+        .fold(0, |acc, &d| acc * 10 + if d == v { 9 } else { d })
 }
 
 #[cfg(test)]
@@ -18,19 +54,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            kids_with_candies(&[2, 3, 5, 1, 3], 3),
-            [true, true, true, false, true]
-        );
-        assert_eq!(
-            kids_with_candies(&[4, 2, 1, 1, 2], 1),
-            [true, false, false, false, false]
-        );
-        assert_eq!(kids_with_candies(&[12, 1, 12], 10), [true, false, true]);
+        assert_eq!(max_diff(555), 888);
+        assert_eq!(max_diff(9), 8);
+
+        assert_eq!(max_diff(10000), 80000);
+        assert_eq!(max_diff(9288), 8700);
+        assert_eq!(max_diff(111), 888);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(max_diff(123456), 820000);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
