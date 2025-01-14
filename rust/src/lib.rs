5c8810413576ce8b03bc77516f6888023b512a31
pkusensei <ZW_08@outlook.com>
2025-01-13 19:15:37 -0500

    count common prefixes of two arrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,33 +2,20 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{BTreeMap, BTreeSet, HashMap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn display_table(orders: &[[&str; 3]]) -> Vec<Vec<String>> {
-    let mut table_food = BTreeMap::<u16, HashMap<_, _>>::new();
-    let mut foods = BTreeSet::new();
-    for ord in orders.iter() {
-        *table_food
-            .entry(ord[1].parse().unwrap_or(0))
-            .or_default()
-            .entry(ord[2])
-            .or_insert(0) += 1;
-        foods.insert(ord[2]);
-    }
-    let mut res = vec![];
-    let mut row = vec!["Table".to_string()];
-    row.extend(foods.iter().map(|s| s.to_string()));
-    res.push(row);
-    for (k, v) in table_food.into_iter() {
-        row = vec![k.to_string()];
-        for food in foods.iter() {
-            let num = v.get(food).copied().unwrap_or(0).to_string();
-            row.push(num);
-        }
-        res.push(row);
+pub fn find_the_prefix_common_array(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut res = Vec::with_capacity(a.len());
+    let [mut mask1, mut mask2] = [0u64; 2];
+    // let mut map = std::collections::HashMap::with_capacity(a.len());
+    for (&x, &y) in a.iter().zip(b.iter()) {
+        mask1 |= 1 << x;
+        mask2 |= 1 << y;
+        res.push((mask1 & mask2).count_ones() as i32);
+        //     *map.entry(x).or_insert(0) += 1;
+        //     *map.entry(y).or_insert(0) += 1;
+        //     res.push(map.values().filter(|&&v| v > 1).count() as i32);
     }
     res
 }
@@ -42,45 +29,12 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            display_table(&[
-                ["David", "3", "Ceviche"],
-                ["Corina", "10", "Beef Burrito"],
-                ["David", "3", "Fried Chicken"],
-                ["Carla", "5", "Water"],
-                ["Carla", "5", "Ceviche"],
-                ["Rous", "3", "Ceviche"]
-            ]),
-            [
-                ["Table", "Beef Burrito", "Ceviche", "Fried Chicken", "Water"],
-                ["3", "0", "2", "1", "0"],
-                ["5", "0", "1", "0", "1"],
-                ["10", "1", "0", "0", "0"]
-            ]
+            find_the_prefix_common_array(&[1, 3, 2, 4], &[3, 1, 2, 4]),
+            [0, 2, 3, 4]
         );
         assert_eq!(
-            display_table(&[
-                ["James", "12", "Fried Chicken"],
-                ["Ratesh", "12", "Fried Chicken"],
-                ["Amadeus", "12", "Fried Chicken"],
-                ["Adam", "1", "Canadian Waffles"],
-                ["Brianna", "1", "Canadian Waffles"]
-            ]),
-            [
-                ["Table", "Canadian Waffles", "Fried Chicken"],
-                ["1", "2", "0"],
-                ["12", "0", "3"]
-            ]
-        );
-        assert_eq!(
-            display_table(&[
-                ["Laura", "2", "Bean Burrito"],
-                ["Jhon", "2", "Beef Burrito"],
-                ["Melissa", "2", "Soda"]
-            ]),
-            [
-                ["Table", "Bean Burrito", "Beef Burrito", "Soda"],
-                ["2", "1", "1", "1"]
-            ]
+            find_the_prefix_common_array(&[2, 3, 1], &[3, 1, 2]),
+            [0, 1, 3]
         );
     }
 
