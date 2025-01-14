cbf4c84691fc5fdd0326427029443d0992b83c6e
Zx Wang <ZW_08@outlook.com>
2025-01-14 10:38:14 -0500

    1422 one pass prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,55 +5,22 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_of_arrays(n: i32, m: i32, k: i32) -> i32 {
-    let [n, m] = [n, m].map(|v| v as usize);
-    // dfs(
-    //     n,
-    //     m,
-    //     k,
-    //     0,
-    //     &mut vec![vec![vec![-1; 1 + m]; 1 + k as usize]; 1 + n],
-    // )
-    let k = k as usize;
-    let mut dp = vec![vec![vec![0i64; 1 + k]; 1 + m]; 1 + n];
-    for max in 1..=m {
-        dp[1][max][1] = 1;
-    }
-    for len in 2..=n {
-        for max in 1..=m {
-            for cost in 1..=k {
-                dp[len][max][cost] += dp[len - 1][max][cost] * (max as i64);
-                for prev_max in 1..max {
-                    dp[len][max][cost] += dp[len - 1][prev_max][cost - 1];
-                }
-                dp[len][max][cost] %= 1_000_000_007;
-            }
-        }
-    }
-    (1..=m).fold(0, |acc, max| (acc + dp[n][max][k]) % 1_000_000_007) as i32
-}
-
-fn dfs(n: usize, m: usize, k: i32, prev: usize, memo: &mut [Vec<Vec<i32>>]) -> i32 {
-    if n == 0 {
-        return i32::from(0 == k);
-    }
-    if k < 0 {
-        return 0;
-    }
-    if memo[n][k as usize][prev] > -1 {
-        return memo[n][k as usize][prev];
-    }
-    let mut res = 0;
-    for curr in 1..=m {
-        if prev < curr {
-            res += dfs(n - 1, m, k - 1, curr, memo);
+pub fn max_score(s: String) -> i32 {
+    let [mut zeros, mut ones] = [0, 0];
+    let mut best = i32::MIN;
+    let n = s.len();
+    for b in s.bytes().take(n - 1) {
+        if b == b'1' {
+            ones += 1
         } else {
-            res += dfs(n - 1, m, k, prev, memo);
-        }
-        res %= 1_000_000_007;
+            zeros += 1
+        };
+        best = best.max(zeros - ones);
     }
-    memo[n][k as usize][prev] = res;
-    res
+    if s.bytes().last() == Some(b'1') {
+        ones += 1;
+    }
+    best + ones
 }
 
 #[cfg(test)]
@@ -63,11 +30,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(num_of_arrays(2, 3, 1), 6);
-        assert_eq!(num_of_arrays(5, 2, 3), 0);
-        assert_eq!(num_of_arrays(9, 1, 1), 1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
