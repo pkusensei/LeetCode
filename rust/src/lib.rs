0e0366e24039c7b24ae7538c8d60304f513e976a
pkusensei <ZW_08@outlook.com>
2025-01-15 12:58:58 -0500

    1443 a myriad of bfs code vs dfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,41 +2,66 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{HashMap, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_triplets(arr: &[i32]) -> i32 {
-    let n = arr.len();
-    let mut prefix = Vec::with_capacity(n);
-    for &num in arr.iter() {
-        prefix.push(num ^ prefix.last().unwrap_or(&0));
-    }
-    let mut res = 0;
-    for left in 0..n - 1 {
-        for right in 1 + left..n {
-            let p = if left > 0 {
-                prefix[right] ^ prefix[left - 1]
-            } else {
-                prefix[right]
-            };
-            if p == 0 {
-                res += right - left;
-            }
-        }
-    }
-    res as _
+pub fn min_time(n: i32, edges: &[[i32; 2]], has_apple: &[bool]) -> i32 {
+    let adj = edges
+        .iter()
+        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, e| {
+            acc.entry(e[0]).or_default().push(e[1]);
+            acc.entry(e[1]).or_default().push(e[0]);
+            acc
+        });
+    dfs(&adj, has_apple, 0, -1)
+    // let mut must_visit = has_apple.to_vec();
+    // let mut queue = VecDeque::from([0]);
+    // let mut seen = vec![false; n as usize];
+    // seen[0] = true;
+    // let mut prevs = HashMap::new();
+    // let mut targets = vec![];
+    // while let Some(node) = queue.pop_front() {
+    //     if must_visit[node as usize] {
+    //         must_visit[node as usize] = false;
+    //         targets.push(node);
+    //     }
+    //     if must_visit.iter().all(|&b| !b) {
+    //         break;
+    //     }
+    //     for &next in adj[&node].iter() {
+    //         if !seen[next as usize] {
+    //             seen[next as usize] = true;
+    //             queue.push_back(next);
+    //             prevs.insert(next, node);
+    //         }
+    //     }
+    // }
+    // seen = vec![false; n as usize];
+    // seen[0] = true;
+    // let mut res = 0;
+    // while let Some(mut node) = targets.pop() {
+    //     while !seen[node as usize] {
+    //         res += 1;
+    //         seen[node as usize] = true;
+    //         node = prevs[&node];
+    //     }
+    // }
+    // 2 * res
 }
 
-fn with_map(arr: &[i32]) -> i32 {
-    let mut map = std::collections::HashMap::from([(0, vec![-1])]);
-    let mut curr = 0;
+fn dfs(adj: &HashMap<i32, Vec<i32>>, has_apple: &[bool], node: i32, prev: i32) -> i32 {
     let mut res = 0;
-    for (idx, &num) in arr.iter().enumerate() {
-        curr ^= num;
-        if let Some(prev) = map.get(&curr) {
-            res += prev.iter().map(|p| idx as i32 - p - 1).sum::<i32>();
+    let Some(v) = adj.get(&node) else { return 0 };
+    for &next in v.iter() {
+        if next == prev {
+            continue;
         }
-        map.entry(curr).or_default().push(idx as i32);
+        let time = dfs(adj, has_apple, next, node);
+        if time > 0 || has_apple[next as usize] {
+            res += time + 2;
+        }
     }
     res
 }
@@ -49,11 +74,30 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(count_triplets(&[2, 3, 1, 6, 7]), 4);
-        assert_eq!(count_triplets(&[1, 1, 1, 1, 1]), 10);
-
-        assert_eq!(with_map(&[2, 3, 1, 6, 7]), 4);
-        assert_eq!(with_map(&[1, 1, 1, 1, 1]), 10);
+        assert_eq!(
+            min_time(
+                7,
+                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                &[false, false, true, false, true, true, false]
+            ),
+            8
+        );
+        assert_eq!(
+            min_time(
+                7,
+                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                &[false, false, true, false, false, true, false]
+            ),
+            6
+        );
+        assert_eq!(
+            min_time(
+                7,
+                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                &[false, false, false, false, false, false, false]
+            ),
+            0
+        );
     }
 
     #[test]
