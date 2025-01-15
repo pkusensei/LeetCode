f02473902a033ec25709e8daa40089d094e352b9
pkusensei <ZW_08@outlook.com>
2025-01-15 13:55:00 -0500

    1444 cut into valid pieces

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,67 +2,110 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashMap, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_time(n: i32, edges: &[[i32; 2]], has_apple: &[bool]) -> i32 {
-    let adj = edges
-        .iter()
-        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, e| {
-            acc.entry(e[0]).or_default().push(e[1]);
-            acc.entry(e[1]).or_default().push(e[0]);
-            acc
-        });
-    dfs(&adj, has_apple, 0, -1)
-    // let mut must_visit = has_apple.to_vec();
-    // let mut queue = VecDeque::from([0]);
-    // let mut seen = vec![false; n as usize];
-    // seen[0] = true;
-    // let mut prevs = HashMap::new();
-    // let mut targets = vec![];
-    // while let Some(node) = queue.pop_front() {
-    //     if must_visit[node as usize] {
-    //         must_visit[node as usize] = false;
-    //         targets.push(node);
-    //     }
-    //     if must_visit.iter().all(|&b| !b) {
-    //         break;
-    //     }
-    //     for &next in adj[&node].iter() {
-    //         if !seen[next as usize] {
-    //             seen[next as usize] = true;
-    //             queue.push_back(next);
-    //             prevs.insert(next, node);
-    //         }
-    //     }
-    // }
-    // seen = vec![false; n as usize];
-    // seen[0] = true;
-    // let mut res = 0;
-    // while let Some(mut node) = targets.pop() {
-    //     while !seen[node as usize] {
-    //         res += 1;
-    //         seen[node as usize] = true;
-    //         node = prevs[&node];
-    //     }
-    // }
-    // 2 * res
-}
-
-fn dfs(adj: &HashMap<i32, Vec<i32>>, has_apple: &[bool], node: i32, prev: i32) -> i32 {
-    let mut res = 0;
-    let Some(v) = adj.get(&node) else { return 0 };
-    for &next in v.iter() {
-        if next == prev {
-            continue;
-        }
-        let time = dfs(adj, has_apple, next, node);
-        if time > 0 || has_apple[next as usize] {
-            res += time + 2;
+pub fn ways(pizza: &[&str], k: i32) -> i32 {
+    let grid: Vec<_> = pizza.iter().map(|v| v.as_bytes()).collect();
+    let [rows, cols] = get_dimensions(&grid);
+    let k = k as usize;
+    // dfs(
+    //     &grid,
+    //     [0, 0],
+    //     k,
+    //     &mut vec![vec![vec![-1; 1 + k]; cols]; rows],
+    // )
+    let mut apples = vec![vec![0; 1 + cols]; 1 + rows];
+    for r in (0..rows).rev() {
+        for c in (0..cols).rev() {
+            apples[r][c] = i32::from(grid[r][c] == b'A') + apples[1 + r][c] + apples[r][1 + c]
+                - apples[1 + r][1 + c];
         }
     }
+    let mut dp = vec![vec![vec![0; 1 + k]; cols]; rows];
+    for r in 0..rows {
+        for c in 0..cols {
+            if apples[r][c] > 0 {
+                dp[r][c][1] = 1;
+            }
+        }
+    }
+    for pieces in 2..=k {
+        for r in 0..rows {
+            for c in 0..cols {
+                for cut in 1 + r..rows {
+                    if apples[r][c] - apples[cut][c] > 0 {
+                        dp[r][c][pieces] += dp[cut][c][pieces - 1];
+                        dp[r][c][pieces] %= MOD;
+                    }
+                }
+                for cut in 1 + c..cols {
+                    if apples[r][c] - apples[r][cut] > 0 {
+                        dp[r][c][pieces] += dp[r][cut][pieces - 1];
+                        dp[r][c][pieces] %= MOD;
+                    }
+                }
+            }
+        }
+    }
+    dp[0][0][k]
+}
+
+const MOD: i32 = 1_000_000_007;
+
+fn dfs(grid: &[&[u8]], [row, col]: [usize; 2], k: usize, memo: &mut [Vec<Vec<i32>>]) -> i32 {
+    let [rows, cols] = get_dimensions(grid);
+    if k <= 1 {
+        for r in row..rows {
+            for c in col..cols {
+                if grid[r][c] == b'A' {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    if memo[row][col][k] > -1 {
+        return memo[row][col][k];
+    }
+    let mut res = 0;
+    let mut valid = false;
+    'outer: for cut in 1 + row..rows {
+        if valid {
+            res += dfs(grid, [cut, col], k - 1, memo);
+            res %= MOD;
+            continue;
+        }
+        for r in row..cut {
+            for c in col..cols {
+                if grid[r][c] == b'A' {
+                    valid = true;
+                    res += dfs(grid, [cut, col], k - 1, memo);
+                    res %= MOD;
+                    continue 'outer;
+                }
+            }
+        }
+    }
+    valid = false;
+    'outer: for cut in 1 + col..cols {
+        if valid {
+            res += dfs(grid, [row, cut], k - 1, memo);
+            res %= MOD;
+            continue;
+        }
+        for r in row..rows {
+            for c in col..cut {
+                if grid[r][c] == b'A' {
+                    valid = true;
+                    res += dfs(grid, [row, cut], k - 1, memo);
+                    res %= MOD;
+                    continue 'outer;
+                }
+            }
+        }
+    }
+    memo[row][col][k] = res;
     res
 }
 
@@ -74,30 +117,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            min_time(
-                7,
-                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
-                &[false, false, true, false, true, true, false]
-            ),
-            8
-        );
-        assert_eq!(
-            min_time(
-                7,
-                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
-                &[false, false, true, false, false, true, false]
-            ),
-            6
-        );
-        assert_eq!(
-            min_time(
-                7,
-                &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
-                &[false, false, false, false, false, false, false]
-            ),
-            0
-        );
+        assert_eq!(ways(&["A..", "AAA", "..."], 3), 3);
+        assert_eq!(ways(&["A..", "AA.", "..."], 3), 1);
+        assert_eq!(ways(&["A..", "A..", "..."], 1), 1);
     }
 
     #[test]
