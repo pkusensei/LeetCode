5c651310bd7688f4d6e57a5832a45b1a87890979
pkusensei <ZW_08@outlook.com>
2025-01-15 16:22:21 -0500

    1448 nodes bigger than all previous nodes in binary tree

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,82 +6,17 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode BalanceBST(TreeNode root)
+    public int GoodNodes(TreeNode root)
     {
-        List<int> vals = [];
-        Inorder(root);
-        return Construct([.. vals]);
-
-        void Inorder(TreeNode node)
-        {
-            if (node is null) { return; }
-            Inorder(node.left);
-            vals.Add(node.val);
-            Inorder(node.right);
-        }
-
-        static TreeNode Construct(int[] vals)
-        {
-            if (vals.Length == 0) { return null; }
-            if (vals.Length == 1) { return new(vals[0]); }
-            var node = vals[vals.Length / 2];
-            var left = Construct(vals[..(vals.Length / 2)]);
-            var right = Construct(vals[(1 + vals.Length / 2)..]);
-            return new(node, left, right);
-        }
+        if (root is null) { return 0; }
+        return Dfs(root, root.val);
     }
 
-    public TreeNode WithDSW(TreeNode root)
+    static int Dfs(TreeNode node, int max)
     {
-        if (root is null) { return null; }
-        TreeNode vinehead = new(0, null, root);
-        var curr = vinehead;
-        while (curr.right is not null)
-        {
-            if (curr.right.left is not null) { RotateRight(curr, curr.right); }
-            else { curr = curr.right; }
-        }
-        var count = 0;
-        curr = vinehead.right;
-        while (curr is not null)
-        {
-            curr = curr.right;
-            count += 1;
-        }
-        var depth = (int)Math.Pow(2, Math.Floor(Math.Log2(1 + count))) - 1;
-        MakeRotations(vinehead, count - depth);
-        while (depth > 1)
-        {
-            depth /= 2;
-            MakeRotations(vinehead, depth);
-        }
-        return vinehead.right;
-
-        static void RotateRight(TreeNode parent, TreeNode node)
-        {
-            TreeNode temp = node.left;
-            node.left = temp.right;
-            temp.right = node;
-            parent.right = temp;
-        }
-
-        static void RotateLeft(TreeNode parent, TreeNode node)
-        {
-            TreeNode temp = node.right;
-            node.right = temp.left;
-            temp.left = node;
-            parent.right = temp;
-        }
-
-        static void MakeRotations(TreeNode vinehead, int count)
-        {
-            var curr = vinehead;
-            for (int i = 0; i < count; i++)
-            {
-                var temp = curr.right;
-                RotateLeft(curr, temp);
-                curr = curr.right;
-            }
-        }
+        if (node is null) { return 0; }
+        var res = node.val >= max ? 1 : 0;
+        max = Math.Max(max, node.val);
+        return res + Dfs(node.left, max) + Dfs(node.right, max);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,22 +12,22 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        var a = TreeNode.Make([1, null, 2, null, 3, null, 4, null, null]);
-        Assert.AreEqual("[3,2,4,1]", sol.BalanceBST(a).ToString());
-        Assert.AreEqual("[3,2,4,1]", sol.WithDSW(a).ToString());
+        var a = TreeNode.Make([3, 1, 4, 3, null, 1, 5]);
+        Assert.AreEqual(4, sol.GoodNodes(a));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        var a = TreeNode.Make([2, 1, 3]);
-        Assert.AreEqual("[2,1,3]", sol.BalanceBST(a).ToString());
-        Assert.AreEqual("[2,1,3]", sol.WithDSW(a).ToString());
+        var a = TreeNode.Make([3, 3, null, 4, 2]);
+        Assert.AreEqual(3, sol.GoodNodes(a));
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        var a = TreeNode.Make([1]);
+        Assert.AreEqual(1, sol.GoodNodes(a));
     }
 
     [TestMethod]
