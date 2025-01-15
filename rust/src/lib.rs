2c90e9f92b85e4bdb141d04ff516729f57c6e07e
pkusensei <ZW_08@outlook.com>
2025-01-15 10:31:58 -0500

    1439 missing drain_sorted or into_sorted_iter on BinaryHeap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,60 +2,36 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
-    let mut count = num2.count_ones();
-    let mut bits = vec![];
-    let mut x = num1;
-    while x > 0 {
-        bits.push(x & 1);
-        x >>= 1;
-    }
-    bits.reverse();
-    for bit in bits.iter_mut() {
-        if (*bit) == 1 && count > 0 {
-            (*bit) = -1;
-            count -= 1;
+pub fn kth_smallest(mat: &[&[i32]], k: i32) -> i32 {
+    let mut heap: BinaryHeap<_> = mat[0].iter().map(|&v| Reverse(v)).collect();
+    for row in mat.iter().skip(1) {
+        let mut next: BinaryHeap<_> = heap
+            .into_iter()
+            .flat_map(|Reverse(prev)| row.iter().map(move |v| Reverse(prev + v)))
+            .collect();
+        heap = BinaryHeap::with_capacity(k as usize);
+        let mut k = k;
+        while let Some(v) = next.pop() {
+            heap.push(v);
+            k -= 1;
+            if 0 == k {
+                break;
+            }
         }
     }
-    for bit in bits.iter_mut().rev() {
-        if *bit == 0 && count > 0 {
-            (*bit) = 1;
-            count -= 1;
-        }
-    }
-    while count > 0 {
-        bits.insert(0, 1);
-        count -= 1;
-    }
-    let num = bits
-        .into_iter()
-        .fold(0, |acc, bit| (acc << 1) | i32::from(bit == 1));
-    num ^ num1
-}
-
-fn solve(num1: i32, num2: i32) -> i32 {
-    // build res as close to num1 as possible
     let mut res = 0;
-    let count = num2.count_ones();
-    let mut set_bits = 0;
-    let mut curr_bit = 31;
-    fn is_set(x: i32, bit: i32) -> bool {
-        (x & (1 << bit)) != 0
-    }
-    fn set(x: i32, bit: i32) -> i32 {
-        x | (1 << bit)
-    }
-    while set_bits < count {
-        // case 1) curr_bit is set in num1
-        // case 2) all remaining bits must be set
-        if is_set(num1, curr_bit) || count - set_bits > curr_bit as u32 {
-            res = set(res, curr_bit);
-            set_bits += 1;
-        }
-        curr_bit -= 1;
+    let mut k = k;
+    while k > 0 {
+        let Some(Reverse(v)) = heap.pop() else {
+            return res;
+        };
+        res = v;
+        k -= 1;
     }
     res
 }
@@ -68,16 +44,29 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimize_xor(3, 5), 3);
-        assert_eq!(minimize_xor(1, 12), 3);
-
-        assert_eq!(solve(3, 5), 3);
-        assert_eq!(solve(1, 12), 3);
+        assert_eq!(kth_smallest(&[&[1, 10, 10], &[1, 4, 5], &[2, 3, 6]], 7), 9);
+        assert_eq!(kth_smallest(&[&[1, 3, 11], &[2, 4, 6]], 5), 7);
+        assert_eq!(kth_smallest(&[&[1, 3, 11], &[2, 4, 6]], 9), 17);
     }
 
     #[test]
     fn test() {
-        assert_eq!(minimize_xor(65, 84), 67);
+        assert_eq!(
+            kth_smallest(
+                &[
+                    &[6, 9, 32, 34, 43, 45],
+                    &[5, 10, 18, 21, 40, 42],
+                    &[15, 16, 22, 33, 50, 50],
+                    &[14, 27, 29, 31, 33, 39],
+                    &[1, 11, 19, 25, 45, 50],
+                    &[3, 9, 17, 19, 26, 30],
+                    &[3, 14, 26, 29, 41, 43],
+                    &[12, 23, 25, 26, 40, 46]
+                ],
+                47
+            ),
+            75
+        );
     }
 
     #[allow(dead_code)]
