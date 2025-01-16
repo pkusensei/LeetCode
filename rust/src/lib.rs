402a0ca724c0c1979070e01a25c63c7500d3c598
Zx Wang <ZW_08@outlook.com>
2025-01-16 15:25:20 -0500

    1463 pick cherries with 2 robots

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,29 +5,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_if_prerequisite(
-    num_courses: i32,
-    prerequisites: &[[i32; 2]],
-    queries: &[[i32; 2]],
-) -> Vec<bool> {
-    let n = num_courses as usize;
-    let mut graph = prerequisites
-        .iter()
-        .fold(vec![vec![false; n]; n], |mut acc, p| {
-            acc[p[0] as usize][p[1] as usize] = true;
-            acc
-        });
-    for mid in 0..n {
-        for src in 0..n {
-            for target in 0..n {
-                graph[src][target] = graph[src][target] || (graph[src][mid] && graph[mid][target]);
+pub fn cherry_pickup(grid: &[&[i32]]) -> i32 {
+    let [rows, cols] = get_dimensions(grid);
+    dfs(
+        grid,
+        0,
+        0,
+        cols - 1,
+        &mut vec![vec![vec![-1; cols]; cols]; rows],
+    )
+}
+
+fn dfs(grid: &[&[i32]], row: usize, col1: usize, col2: usize, memo: &mut [Vec<Vec<i32>>]) -> i32 {
+    let [rows, cols] = get_dimensions(grid);
+    if row >= rows {
+        return 0;
+    }
+    if memo[row][col1][col2] > -1 {
+        return memo[row][col1][col2];
+    }
+    let mut res = grid[row][col1] + grid[row][col2];
+    let mut curr = 0;
+    for c1 in col1.saturating_sub(1)..=(1 + col1).min(cols - 1) {
+        for c2 in col2.saturating_sub(1)..=(1 + col2).min(cols - 1) {
+            if c1 < c2 {
+                curr = curr.max(dfs(grid, 1 + row, c1, c2, memo));
             }
         }
     }
-    queries
-        .iter()
-        .map(|q| graph[q[0] as usize][q[1] as usize])
-        .collect()
+    res += curr;
+    memo[row][col1][col2] = res;
+    res
 }
 
 #[cfg(test)]
@@ -39,30 +47,23 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            check_if_prerequisite(2, &[[1, 0]], &[[0, 1], [1, 0]]),
-            [false, true]
+            cherry_pickup(&[&[3, 1, 1], &[2, 5, 1], &[1, 5, 5], &[2, 1, 1]]),
+            24
         );
         assert_eq!(
-            check_if_prerequisite(2, &[], &[[1, 0], [0, 1]]),
-            [false, false]
-        );
-        assert_eq!(
-            check_if_prerequisite(3, &[[1, 2], [1, 0], [2, 0]], &[[1, 0], [1, 2]]),
-            [true, true]
+            cherry_pickup(&[
+                &[1, 0, 0, 0, 0, 0, 1],
+                &[2, 0, 0, 0, 0, 3, 0],
+                &[2, 0, 9, 0, 0, 0, 0],
+                &[0, 3, 0, 5, 4, 0, 0],
+                &[1, 0, 2, 3, 0, 0, 6]
+            ]),
+            28
         );
     }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            check_if_prerequisite(
-                5,
-                &[[0, 1], [1, 2], [2, 3], [3, 4]],
-                &[[0, 4], [4, 0], [1, 3], [3, 0]]
-            ),
-            [true, false, true, false]
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
