13b4d28e01e6d5761c642651c6ae64ba3382b669
Zx Wang <ZW_08@outlook.com>
2025-01-16 11:34:18 -0500

    1458 max dot product between 2 arrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,51 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_vowels(s: String, k: i32) -> i32 {
-    const VOWELS: &[u8] = b"aeiou";
-    let (n, k) = (s.len(), k as usize);
-    let s = s.into_bytes();
-    let mut curr = s[..k].iter().filter(|&b| VOWELS.contains(b)).count();
-    let mut res = curr;
-    for i in 1..=n - k {
-        curr -= usize::from(VOWELS.contains(&s[i - 1]));
-        curr += usize::from(VOWELS.contains(&s[i + k - 1]));
-        res = res.max(curr);
+pub fn max_dot_product(nums1: &[i32], nums2: &[i32]) -> i32 {
+    let [n1, n2] = [nums1, nums2].map(|v| v.len());
+    // dfs(nums1, nums2, 0, 0, &mut vec![vec![None; n2]; n1]).unwrap()
+    let mut dp = vec![vec![0; 1 + n2]; 1 + n1];
+    let mut res = i32::MIN;
+    for (i1, v1) in nums1.iter().enumerate() {
+        for (i2, v2) in nums2.iter().enumerate() {
+            res = res.max(v1 * v2);
+            dp[1 + i1][1 + i2] = dp[1 + i1][1 + i2]
+                .max(dp[i1][1 + i2])
+                .max(dp[1 + i1][i2])
+                .max(dp[i1][i2] + v1 * v2);
+        }
     }
-    res as _
+    if res >= 0 {
+        // initial value in dp is set to 0
+        // max res is max (v1*v2)
+        // this check ensures that init 0 does not offset negative max values
+        res.max(dp[n1][n2])
+    } else {
+        res
+    }
+}
+
+fn dfs(
+    nums1: &[i32],
+    nums2: &[i32],
+    i1: usize,
+    i2: usize,
+    memo: &mut [Vec<Option<i32>>],
+) -> Option<i32> {
+    if i1 >= nums1.len() || i2 >= nums2.len() {
+        return None;
+    }
+    if let Some(v) = memo[i1][i2] {
+        return Some(v);
+    }
+    let mut res = Some(nums1[i1] * nums2[i2]);
+    let pick = nums1[i1] * nums2[i2] + dfs(nums1, nums2, 1 + i1, 1 + i2, memo).unwrap_or(0);
+    let skip_i2 = dfs(nums1, nums2, i1, 1 + i2, memo);
+    let skip_i1 = dfs(nums1, nums2, 1 + i1, i2, memo);
+    let skip = dfs(nums1, nums2, 1 + i1, 1 + i2, memo);
+    res = res.max(Some(pick)).max(skip_i1).max(skip_i2).max(skip);
+    memo[i1][i2] = res;
+    res
 }
 
 #[cfg(test)]
@@ -26,10 +59,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_dot_product(&[2, 1, -2, 5], &[3, 0, -6]), 18);
+        assert_eq!(max_dot_product(&[3, -2], &[2, -6, 7]), 21);
+        assert_eq!(max_dot_product(&[-1, -1], &[1, 1]), -1);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(max_dot_product(&[-5, -1, -2], &[3, 3, 5, 5]), -3);
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
