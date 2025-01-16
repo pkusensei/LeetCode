9f401faba4798d3ae367e0d30b20c4130564942f
Zx Wang <ZW_08@outlook.com>
2025-01-16 15:33:09 -0500

    1464 slice::select_nth_unstable

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,37 +5,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn cherry_pickup(grid: &[&[i32]]) -> i32 {
-    let [rows, cols] = get_dimensions(grid);
-    dfs(
-        grid,
-        0,
-        0,
-        cols - 1,
-        &mut vec![vec![vec![-1; cols]; cols]; rows],
-    )
-}
-
-fn dfs(grid: &[&[i32]], row: usize, col1: usize, col2: usize, memo: &mut [Vec<Vec<i32>>]) -> i32 {
-    let [rows, cols] = get_dimensions(grid);
-    if row >= rows {
-        return 0;
+pub fn max_product(mut nums: Vec<i32>) -> i32 {
+    if nums.len() > 2 {
+        nums.select_nth_unstable_by(2, |a, b| b.cmp(&a));
     }
-    if memo[row][col1][col2] > -1 {
-        return memo[row][col1][col2];
-    }
-    let mut res = grid[row][col1] + grid[row][col2];
-    let mut curr = 0;
-    for c1 in col1.saturating_sub(1)..=(1 + col1).min(cols - 1) {
-        for c2 in col2.saturating_sub(1)..=(1 + col2).min(cols - 1) {
-            if c1 < c2 {
-                curr = curr.max(dfs(grid, 1 + row, c1, c2, memo));
-            }
-        }
-    }
-    res += curr;
-    memo[row][col1][col2] = res;
-    res
+    (nums[0] - 1) * (nums[1] - 1)
 }
 
 #[cfg(test)]
@@ -45,22 +19,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            cherry_pickup(&[&[3, 1, 1], &[2, 5, 1], &[1, 5, 5], &[2, 1, 1]]),
-            24
-        );
-        assert_eq!(
-            cherry_pickup(&[
-                &[1, 0, 0, 0, 0, 0, 1],
-                &[2, 0, 0, 0, 0, 3, 0],
-                &[2, 0, 9, 0, 0, 0, 0],
-                &[0, 3, 0, 5, 4, 0, 0],
-                &[1, 0, 2, 3, 0, 0, 6]
-            ]),
-            28
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
