5e1bf3a5bf7c2482f1a295cc14bc271c4b7d3d05
pkusensei <ZW_08@outlook.com>
2025-01-17 14:25:29 -0500

    1476

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,64 +5,25 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost(houses: &[i32], cost: &[&[i32]], m: i32, n: i32, target: i32) -> i32 {
-    let [m, n] = [m, n].map(|v| v as usize);
-    // m, n, target
-    let mut memo = vec![vec![vec![None; 1 + target as usize]; 1 + n]; m];
-    dfs(houses, cost, m, target, 0, 0, &mut memo).unwrap_or(-1)
+struct SubrectangleQueries {
+    data: Vec<Vec<i32>>,
 }
 
-fn dfs(
-    houses: &[i32],
-    cost: &[&[i32]],
-    m: usize,
-    target: i32,
-    idx: usize,
-    prev: usize, // color of previous house
-    memo: &mut [Vec<Vec<Option<i32>>>],
-) -> Option<i32> {
-    if target < 0 {
-        return None;
+impl SubrectangleQueries {
+    fn new(rectangle: Vec<Vec<i32>>) -> Self {
+        Self { data: rectangle }
     }
-    if idx >= m {
-        return if target == 0 { Some(0) } else { None };
-    }
-    if let Some(v) = memo[idx][prev][target as usize] {
-        return if v == -1 { None } else { Some(v) };
-    }
-    if houses[idx] > 0 {
-        return dfs(
-            houses,
-            cost,
-            m,
-            target - i32::from(houses[idx] as usize != prev),
-            1 + idx,
-            houses[idx] as usize,
-            memo,
-        );
-    }
-    let mut res = i32::MAX;
-    // 1+cidx=>color
-    // c=>cost
-    for (cidx, &c) in cost[idx].iter().enumerate() {
-        if let Some(v) = dfs(
-            houses,
-            cost,
-            m,
-            target - i32::from(1 + cidx != prev),
-            1 + idx,
-            1 + cidx,
-            memo,
-        ) {
-            res = res.min(c + v);
+
+    fn update_subrectangle(&mut self, row1: i32, col1: i32, row2: i32, col2: i32, new_value: i32) {
+        for r in row1..=row2 {
+            for c in col1..=col2 {
+                self.data[r as usize][c as usize] = new_value;
+            }
         }
     }
-    if res == i32::MAX {
-        memo[idx][prev][target as usize] = Some(-1);
-        None
-    } else {
-        memo[idx][prev][target as usize] = Some(res);
-        Some(res)
+
+    fn get_value(&self, row: i32, col: i32) -> i32 {
+        self.data[row as usize][col as usize]
     }
 }
 
@@ -73,38 +34,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            min_cost(
-                &[0, 0, 0, 0, 0],
-                &[&[1, 10], &[10, 1], &[10, 1], &[1, 10], &[5, 1]],
-                5,
-                2,
-                3
-            ),
-            9
-        );
-        assert_eq!(
-            min_cost(
-                &[0, 2, 1, 2, 0],
-                &[&[1, 10], &[10, 1], &[10, 1], &[1, 10], &[5, 1]],
-                5,
-                2,
-                3
-            ),
-            11
-        );
-        assert_eq!(
-            min_cost(
-                &[3, 1, 2, 3],
-                &[&[1, 1, 1], &[1, 1, 1], &[1, 1, 1], &[1, 1, 1]],
-                4,
-                3,
-                3
-            ),
-            -1
-        )
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
