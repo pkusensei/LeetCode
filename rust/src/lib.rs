aa7172eeb2d08dc8bc9f60af5528b3074638f962
pkusensei <ZW_08@outlook.com>
2025-01-17 18:39:35 -0500

    1486

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,42 +5,8 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-struct TreeAncestor {
-    jump: Vec<Vec<i32>>,
-    max_pow: usize,
-}
-
-impl TreeAncestor {
-    fn new(n: i32, parent: Vec<i32>) -> Self {
-        let n = n as usize;
-        let max_pow = 1 + n.ilog2() as usize;
-        let mut jump = vec![vec![0; n]; max_pow];
-        jump[0] = parent;
-        for p in 1..max_pow {
-            for idx in 0..n {
-                let pre = jump[p - 1][idx];
-                jump[p][idx] = if pre == -1 {
-                    -1
-                } else {
-                    jump[p - 1][pre as usize]
-                };
-            }
-        }
-        Self { jump, max_pow }
-    }
-
-    fn get_kth_ancestor(&self, mut node: i32, mut k: i32) -> i32 {
-        let mut max_pow = self.max_pow;
-        while k > 0 && node > -1 {
-            if k >= (1 << max_pow) {
-                node = self.jump[max_pow][node as usize];
-                k -= 1 << max_pow;
-            } else {
-                max_pow -= 1;
-            }
-        }
-        node
-    }
+pub fn xor_operation(n: i32, start: i32) -> i32 {
+    (0..n).map(|i| start + 2 * i).fold(0, |acc, v| acc ^ v)
 }
 
 #[cfg(test)]
@@ -50,12 +16,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        let ta = TreeAncestor::new(7, vec![-1, 0, 0, 1, 1, 2, 2]);
-        assert_eq!(ta.get_kth_ancestor(3, 1), 1); // returns 1 which is the parent of 3
-        assert_eq!(ta.get_kth_ancestor(5, 2), 0); // returns 0 which is the grandparent of 5
-        assert_eq!(ta.get_kth_ancestor(6, 3), -1); // returns -1 because there is no such ancestor
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
