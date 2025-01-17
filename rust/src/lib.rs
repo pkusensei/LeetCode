1653844f888d0823b1c5e3f36ea751f41bea1def
pkusensei <ZW_08@outlook.com>
2025-01-17 12:33:42 -0500

    1470 intersperse

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,60 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_probability(balls: &[i32]) -> f64 {
-    let n = balls.len();
-    let sum = balls.iter().sum();
-    let [mut box1, mut box2] = [0, 1].map(|_| vec![0; n]);
-    dfs(balls, sum, &mut box1, &mut box2, 0, 0, 0) / perm(balls)
-}
-
-fn dfs(
-    balls: &[i32],
-    sum: i32,
-    box1: &mut [i32],
-    box2: &mut [i32],
-    idx: usize,
-    sum1: i32,
-    sum2: i32,
-) -> f64 {
-    if sum1 > sum / 2 || sum2 > sum / 2 {
-        return 0.0;
-    }
-    if idx >= balls.len() {
-        let [c1, c2] = [&box1, &box2].map(|v| v.iter().filter(|&&v| v > 0).count());
-        return if c1 == c2 {
-            perm(box1) * perm(box2)
-        } else {
-            0.0
-        };
-    }
-    let mut res = 0.0;
-    for curr in 0..=balls[idx] {
-        box1[idx] = curr;
-        box2[idx] = balls[idx] - curr;
-        res += dfs(
-            balls,
-            sum,
-            box1,
-            box2,
-            1 + idx,
-            curr + sum1,
-            balls[idx] - curr + sum2,
-        )
-    }
-    res
-}
-
-fn perm(vals: &[i32]) -> f64 {
-    let mut res = 1.0;
-    let mut count = 0;
-    for &v in vals.iter() {
-        for curr in 1..=v {
-            count += 1;
-            res *= f64::from(count) / f64::from(curr);
-        }
-    }
-    res
+// 1 2 3 4
+// 5 6 7 8
+// 1 5 2 6 3 7 4 8
+pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+    let n = n as usize;
+    nums[..n]
+        .iter()
+        .zip(nums[n..].iter())
+        .flat_map(|(a, b)| [*a, *b])
+        .collect()
 }
 
 #[cfg(test)]
@@ -68,16 +24,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        float_eq(get_probability(&[1, 1]), 1.0);
-        float_eq(get_probability(&[2, 1, 1]), 0.66667);
-        float_eq(get_probability(&[1, 2, 1, 2]), 0.60000);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        float_eq(get_probability(&[6, 6, 6, 6, 6, 6]), 0.90327);
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
