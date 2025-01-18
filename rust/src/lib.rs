4c5160fabad6817ce1755210d1fbf885d41e5530
pkusensei <ZW_08@outlook.com>
2025-01-18 18:30:49 -0500

    1499 heap and monotonic queue

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,30 +2,43 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{BinaryHeap, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_subseq(mut nums: Vec<i32>, target: i32) -> i32 {
-    const MOD: i32 = 1_000_000_007;
-    let n = nums.len();
-    let mut pow = vec![1; n];
-    for i in 1..n {
-        pow[i] = 2 * pow[i - 1] % MOD;
-    }
-    nums.sort_unstable();
-    let mut res = 0;
-    let [mut left, mut right] = [0, n - 1];
-    while left <= right {
-        if nums[left] + nums[right] > target {
-            let Some(v) = right.checked_sub(1) else {
-                break;
-            };
-            right = v;
-        } else {
-            res += pow[right - left];
-            res %= MOD;
-            left += 1;
+// yi + yj + |xi - xj|
+// yj+xj + yi-xi
+pub fn find_max_value_of_equation(points: &[[i32; 2]], k: i32) -> i32 {
+    // [yi-xi, xi]
+    let mut heap: BinaryHeap<[i32; 2]> = BinaryHeap::new();
+    let mut res = i32::MIN;
+    for p in points.iter() {
+        while heap.peek().is_some_and(|v| p[0] - v[1] > k) {
+            heap.pop();
         }
+        if let Some(v) = heap.peek() {
+            res = res.max(p[1] + p[0] + v[0]);
+        }
+        heap.push([p[1] - p[0], p[0]]);
+    }
+    res
+}
+
+fn with_deque(points: &[[i32; 2]], k: i32) -> i32 {
+    let mut queue = VecDeque::<[i32; 2]>::new();
+    let mut res = 0;
+    for p in points.iter() {
+        while queue.front().is_some_and(|v| p[0] - v[1] > k) {
+            queue.pop_front();
+        }
+        if let Some(v) = queue.front() {
+            res = res.max(p[0] + p[1] + v[0]);
+        }
+        while queue.back().is_some_and(|v| v[0] <= p[1] - p[0]) {
+            queue.pop_back();
+        }
+        queue.push_back([p[1] - p[0], p[0]]);
     }
     res
 }
@@ -37,24 +50,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            find_max_value_of_equation(&[[1, 3], [2, 0], [5, 10], [6, -10]], 1),
+            4
+        );
+        assert_eq!(find_max_value_of_equation(&[[0, 0], [3, 0], [9, 2]], 3), 3);
+
+        assert_eq!(with_deque(&[[1, 3], [2, 0], [5, 10], [6, -10]], 1), 4);
+        assert_eq!(with_deque(&[[0, 0], [3, 0], [9, 2]], 3), 3);
+    }
 
     #[test]
-    fn test() {
-        assert_eq!(num_subseq(vec![1], 1), 0);
-        assert_eq!(
-            num_subseq(
-                vec![
-                    27, 21, 14, 2, 15, 1, 19, 8, 12, 24, 21, 8, 12, 10, 11, 30, 15, 18, 28, 14, 26,
-                    9, 2, 24, 23, 11, 7, 12, 9, 17, 30, 9, 28, 2, 14, 22, 19, 19, 27, 6, 15, 12,
-                    29, 2, 30, 11, 20, 30, 21, 20, 2, 22, 6, 14, 13, 19, 21, 10, 18, 30, 2, 20, 28,
-                    22
-                ],
-                31
-            ),
-            688052206
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
