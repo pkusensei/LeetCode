e8b47984001f4cdb7c191286301188350064b8fd
pkusensei <ZW_08@outlook.com>
2025-01-18 18:35:59 -0500

    1502 arithmetic seq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,45 +2,14 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{BinaryHeap, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-// yi + yj + |xi - xj|
-// yj+xj + yi-xi
-pub fn find_max_value_of_equation(points: &[[i32; 2]], k: i32) -> i32 {
-    // [yi-xi, xi]
-    let mut heap: BinaryHeap<[i32; 2]> = BinaryHeap::new();
-    let mut res = i32::MIN;
-    for p in points.iter() {
-        while heap.peek().is_some_and(|v| p[0] - v[1] > k) {
-            heap.pop();
-        }
-        if let Some(v) = heap.peek() {
-            res = res.max(p[1] + p[0] + v[0]);
-        }
-        heap.push([p[1] - p[0], p[0]]);
-    }
-    res
-}
-
-fn with_deque(points: &[[i32; 2]], k: i32) -> i32 {
-    let mut queue = VecDeque::<[i32; 2]>::new();
-    let mut res = 0;
-    for p in points.iter() {
-        while queue.front().is_some_and(|v| p[0] - v[1] > k) {
-            queue.pop_front();
-        }
-        if let Some(v) = queue.front() {
-            res = res.max(p[0] + p[1] + v[0]);
-        }
-        while queue.back().is_some_and(|v| v[0] <= p[1] - p[0]) {
-            queue.pop_back();
-        }
-        queue.push_back([p[1] - p[0], p[0]]);
-    }
-    res
+pub fn can_make_arithmetic_progression(mut arr: Vec<i32>) -> bool {
+    arr.sort_unstable();
+    let mut it = arr.windows(2).map(|w| w[1] - w[0]);
+    let curr = it.next().unwrap_or(0);
+    it.all(|v| v == curr)
 }
 
 #[cfg(test)]
@@ -50,16 +19,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            find_max_value_of_equation(&[[1, 3], [2, 0], [5, 10], [6, -10]], 1),
-            4
-        );
-        assert_eq!(find_max_value_of_equation(&[[0, 0], [3, 0], [9, 2]], 3), 3);
-
-        assert_eq!(with_deque(&[[1, 3], [2, 0], [5, 10], [6, -10]], 1), 4);
-        assert_eq!(with_deque(&[[0, 0], [3, 0], [9, 2]], 3), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
