50357b7e9e6241b382f3619c974e2b53feecb2b2
pkusensei <ZW_08@outlook.com>
2025-01-18 10:36:17 -0500

    1489 DSU, Kruskal algo, and minimum spanning tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,33 +2,100 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{BTreeSet, HashMap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn avoid_flood(rains: &[i32]) -> Vec<i32> {
-    let mut res = vec![-1; rains.len()];
-    let mut dry = BTreeSet::new();
-    let mut wet = HashMap::new();
-    for (idx, &num) in rains.iter().enumerate() {
-        if num == 0 {
-            dry.insert(idx);
-        } else if let Some(prev) = wet.get_mut(&num) {
-            let Some(&i) = dry.range(1 + *prev..).next() else {
-                return vec![];
-            };
-            res[i] = num;
-            dry.remove(&i);
-            *prev = idx;
-        } else {
-            wet.insert(num, idx);
+pub fn find_critical_and_pseudo_critical_edges(n: i32, edges: &[[i32; 3]]) -> Vec<Vec<i32>> {
+    let mut edges: Vec<_> = edges
+        .iter()
+        .enumerate()
+        .map(|(i, e)| (i, e[0] as usize, e[1] as usize, e[2]))
+        .collect();
+    // idx a b weight
+    edges.sort_by_key(|e| e.3);
+    let n = n as usize;
+    let weight = build_mst(n, &edges, None, None);
+    let [mut c, mut p] = [0, 1].map(|_| vec![]);
+    for e in edges.iter() {
+        let exc = build_mst(n, &edges, None, Some(e));
+        let inc = build_mst(n, &edges, Some(e), None);
+        if exc > weight {
+            c.push(e.0 as i32);
+        } else if inc == weight {
+            // else keyword is necessary
+            p.push(e.0 as i32);
         }
     }
-    for d in dry {
-        res[d] = 1;
+    vec![c, p]
+}
+
+fn build_mst(
+    n: usize,
+    edges: &[(usize, usize, usize, i32)],
+    inc: Option<&(usize, usize, usize, i32)>,
+    exc: Option<&(usize, usize, usize, i32)>,
+) -> i32 {
+    let mut dsu = DSU::new(n);
+    let mut weight = 0;
+    if let Some(e) = inc {
+        dsu.union(e.1, e.2);
+        weight += e.3;
     }
-    res
+    for e in edges {
+        if exc.is_some_and(|v| v.0 == e.0) {
+            continue;
+        }
+        if dsu.union(e.1, e.2) {
+            weight += e.3;
+        }
+    }
+    if dsu.size == 1 {
+        // forms a tree
+        weight
+    } else {
+        i32::MAX
+    }
+}
+
+#[derive(Debug, Clone)]
+struct DSU {
+    parent: Vec<usize>,
+    ranks: Vec<i32>,
+    size: usize,
+}
+
+impl DSU {
+    pub fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            ranks: vec![1; n],
+            size: n,
+        }
+    }
+
+    pub fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    pub fn union(&mut self, x: usize, y: usize) -> bool {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return false;
+        }
+        match self.ranks[rx].cmp(&self.ranks[ry]) {
+            std::cmp::Ordering::Less => self.parent[rx] = ry,
+            std::cmp::Ordering::Equal => {
+                self.parent[ry] = rx;
+                self.ranks[rx] += 1;
+            }
+            std::cmp::Ordering::Greater => self.parent[ry] = rx,
+        }
+        self.size -= 1;
+        true
+    }
 }
 
 #[cfg(test)]
@@ -39,18 +106,47 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(avoid_flood(&[1, 2, 3, 4]), [-1; 4]);
-        assert_eq!(avoid_flood(&[1, 2, 0, 0, 2, 1]), [-1, -1, 2, 1, -1, -1]);
-        assert!(avoid_flood(&[1, 2, 0, 1, 2]).is_empty());
+        assert_eq!(
+            find_critical_and_pseudo_critical_edges(
+                5,
+                &[
+                    [0, 1, 1],
+                    [1, 2, 1],
+                    [2, 3, 2],
+                    [0, 3, 2],
+                    [0, 4, 3],
+                    [3, 4, 3],
+                    [1, 4, 6]
+                ]
+            ),
+            [vec![0, 1], vec![2, 3, 4, 5]]
+        );
+        assert_eq!(
+            find_critical_and_pseudo_critical_edges(
+                4,
+                &[[0, 1, 1], [1, 2, 1], [2, 3, 1], [0, 3, 1]]
+            ),
+            [vec![], vec![0, 1, 2, 3]]
+        );
     }
 
     #[test]
     fn test() {
         assert_eq!(
-            avoid_flood(&[1, 0, 2, 0, 3, 0, 2, 0, 0, 0, 1, 2, 3]),
-            [-1, 1, -1, 2, -1, 3, -1, 2, 1, 1, -1, -1, -1]
+            find_critical_and_pseudo_critical_edges(
+                6,
+                &[
+                    [0, 1, 1],
+                    [1, 2, 1],
+                    [0, 2, 1],
+                    [2, 3, 4],
+                    [3, 4, 2],
+                    [3, 5, 2],
+                    [4, 5, 2]
+                ]
+            ),
+            [vec![3], vec![0, 1, 2, 4, 5, 6]]
         );
-        assert!(avoid_flood(&[0, 1, 1]).is_empty());
     }
 
     #[allow(dead_code)]
