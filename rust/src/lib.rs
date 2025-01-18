0f2f7030f62c48b8169d329e8f7d3b9af9d0eca3
pkusensei <ZW_08@outlook.com>
2025-01-18 18:07:40 -0500

    1498 count subseqs that sum up to k

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_path_crossing(path: String) -> bool {
-    let mut pos = [0, 0];
-    let mut seen = std::collections::HashSet::from([pos]);
-    let mut res = false;
-    for b in path.bytes() {
-        match b {
-            b'N' => pos[1] += 1,
-            b'S' => pos[1] -= 1,
-            b'E' => pos[0] += 1,
-            _ => pos[0] -= 1,
+pub fn num_subseq(mut nums: Vec<i32>, target: i32) -> i32 {
+    const MOD: i32 = 1_000_000_007;
+    let n = nums.len();
+    let mut pow = vec![1; n];
+    for i in 1..n {
+        pow[i] = 2 * pow[i - 1] % MOD;
+    }
+    nums.sort_unstable();
+    let mut res = 0;
+    let [mut left, mut right] = [0, n - 1];
+    while left <= right {
+        if nums[left] + nums[right] > target {
+            let Some(v) = right.checked_sub(1) else {
+                break;
+            };
+            right = v;
+        } else {
+            res += pow[right - left];
+            res %= MOD;
+            left += 1;
         }
-        res |= !seen.insert(pos);
     }
     res
 }
@@ -31,7 +40,21 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(num_subseq(vec![1], 1), 0);
+        assert_eq!(
+            num_subseq(
+                vec![
+                    27, 21, 14, 2, 15, 1, 19, 8, 12, 24, 21, 8, 12, 10, 11, 30, 15, 18, 28, 14, 26,
+                    9, 2, 24, 23, 11, 7, 12, 9, 17, 30, 9, 28, 2, 14, 22, 19, 19, 27, 6, 15, 12,
+                    29, 2, 30, 11, 20, 30, 21, 20, 2, 22, 6, 14, 13, 19, 21, 10, 18, 30, 2, 20, 28,
+                    22
+                ],
+                31
+            ),
+            688052206
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
