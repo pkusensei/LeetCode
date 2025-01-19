54d7cc488f1657e1bfcb8ea0e211117b6b6e58a9
pkusensei <ZW_08@outlook.com>
2025-01-19 11:58:07 -0500

    1507

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,29 +5,29 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_integer(num: String, k: i32) -> String {
-    let mut s = num.into_bytes();
-    let n = s.len();
-    let mut k = k as usize;
-    for left in 0..n - 1 {
-        if k == 0 {
-            break;
-        }
-        let right = s
-            .iter()
-            .enumerate()
-            .skip(left)
-            .take(1 + k)
-            .min_by_key(|(_i, v)| **v)
-            .map(|(i, _v)| i)
-            .unwrap_or(left);
-        if right == left {
-            continue;
-        }
-        s[left..=right].rotate_right(1);
-        k -= right - left;
-    }
-    String::from_utf8(s).unwrap()
+pub fn reformat_date(date: String) -> String {
+    let m = std::collections::HashMap::from([
+        ("Jan", "01"),
+        ("Feb", "02"),
+        ("Mar", "03"),
+        ("Apr", "04"),
+        ("May", "05"),
+        ("Jun", "06"),
+        ("Jul", "07"),
+        ("Aug", "08"),
+        ("Sep", "09"),
+        ("Oct", "10"),
+        ("Nov", "11"),
+        ("Dec", "12"),
+    ]);
+    let mut it = date.split_ascii_whitespace();
+    let d = it
+        .next()
+        .and_then(|s| s[..s.len() - 2].parse().ok())
+        .unwrap_or(1);
+    let m = it.next().and_then(|s| m.get(s)).unwrap_or(&"01");
+    let y = it.next().unwrap_or("1900");
+    format!("{y}-{m}-{d:02}")
 }
 
 #[cfg(test)]
@@ -37,11 +37,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(min_integer("4321".into(), 4), "1342");
-        assert_eq!(min_integer("100".into(), 1), "010");
-        assert_eq!(min_integer("36789".into(), 1000), "36789");
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
