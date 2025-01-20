4566ad3dd8ccbaddc64395797fdd2ee0a4dc621b
pkusensei <ZW_08@outlook.com>
2025-01-20 10:35:52 -0500

    1528 shuffle str

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,8 +5,12 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_number_operations(target: &[i32]) -> i32 {
-    target[0] + target.windows(2).map(|w| 0.max(w[1] - w[0])).sum::<i32>()
+pub fn restore_string(s: String, indices: Vec<i32>) -> String {
+    let mut res = vec![0; s.len()];
+    for (b, i) in s.bytes().zip(indices) {
+        res[i as usize] = b;
+    }
+    String::from_utf8(res).unwrap()
 }
 
 #[cfg(test)]
@@ -16,11 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(min_number_operations(&[1, 2, 3, 2, 1]), 3);
-        assert_eq!(min_number_operations(&[3, 1, 1, 2]), 4);
-        assert_eq!(min_number_operations(&[3, 1, 5, 4, 2]), 7);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
