384929b7fa13e14e85de919d4d40d3ebbd95f6ac
pkusensei <ZW_08@outlook.com>
2025-01-21 09:16:21 -0500

    1540 shifting letters; reading comprehension

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-// 0 0 0 0 # # #
-// # # # 0 0 0 0
-// Robot2 can take either all #'s on top row or all on bottom row
-// Thus for each i, curr = max(top_prefix[1+i..n], down_prefix[i])
-// Then min(all curr)
-pub fn grid_game(grid: [&[i32]; 2]) -> i64 {
-    let n = grid[0].len();
-    let [mut top, mut down] = [0, 1].map(|_| Vec::with_capacity(n));
-    for idx in 0..n {
-        top.push(i64::from(grid[0][idx]) + top.last().unwrap_or(&0));
-        down.push(i64::from(grid[1][idx]) + down.last().unwrap_or(&0));
-    }
-    let mut res = i64::MAX;
-    for idx in 0..n {
-        let curr = (top[n - 1] - top[idx]).max(if idx > 0 { down[idx - 1] } else { 0 });
-        res = res.min(curr);
-    }
-    res
+pub fn can_convert_string(s: &str, t: &str, k: i32) -> bool {
+    s.len() == t.len()
+        && s.bytes()
+            .zip(t.bytes())
+            .filter_map(|(a, b)| match a.cmp(&b) {
+                std::cmp::Ordering::Less => Some(i32::from(b - a)),
+                std::cmp::Ordering::Equal => None,
+                std::cmp::Ordering::Greater => Some(i32::from(b + 26 - a)),
+            })
+            .fold(std::collections::HashMap::new(), |mut acc, diff| {
+                acc.entry(diff).and_modify(|v| *v += 26).or_insert(diff);
+                acc
+            })
+            .into_values()
+            .all(|v| v <= k)
 }
 
 #[cfg(test)]
@@ -33,13 +30,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(grid_game([&[2, 5, 4], &[1, 5, 1]]), 4);
-        assert_eq!(grid_game([&[3, 3, 1], &[8, 5, 2]]), 4);
-        assert_eq!(grid_game([&[1, 3, 1, 15], &[1, 3, 3, 1]]), 7);
+        assert!(can_convert_string("input", "ouput", 9));
+        assert!(!can_convert_string("abc", "bcd", 10));
+        assert!(can_convert_string("aab", "bbb", 27));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert!(!can_convert_string("jicfxaa", "ocxltbp", 15));
+        assert!(!can_convert_string("atmtxzjkz", "tvbtjhvjd", 35));
+        assert!(can_convert_string("iqssxdlb", "dyuqrwyr", 40));
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
