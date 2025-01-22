fc1afe5b81ad94ae1576246ef681ad5181f9265b
pkusensei <ZW_08@outlook.com>
2025-01-22 18:52:16 -0500

    1582

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,78 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_num_edges_to_remove(n: i32, edges: &[[i32; 3]]) -> i32 {
-    let [type1, type2, type3] =
-        edges
-            .iter()
-            .fold([const { vec![] }; 3], |[mut t1, mut t2, mut t3], e| {
-                match e[0] {
-                    1 => t1.push([e[1], e[2]]),
-                    2 => t2.push([e[1], e[2]]),
-                    _ => t3.push([e[1], e[2]]),
-                };
-                [t1, t2, t3]
-            });
-    let dsu = DSU::new(n as usize);
-    let (dsu, v3) = connect(dsu, type3);
-    let (d1, v1) = connect(dsu.clone(), type1);
-    let (d2, v2) = connect(dsu, type2);
-    if d1.n > 1 || d2.n > 1 {
-        -1
-    } else {
-        v1 + v2 + v3
-    }
-}
-
-fn connect(mut dsu: DSU, edges: Vec<[i32; 2]>) -> (DSU, i32) {
-    let mut res = 0;
-    for [x, y] in edges {
-        if !dsu.union(x as usize - 1, y as usize - 1) {
-            res += 1;
-        }
-    }
-    (dsu, res)
-}
-
-#[derive(Debug, Clone)]
-struct DSU {
-    parent: Vec<usize>,
-    rank: Vec<i32>,
-    n: usize,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            rank: vec![1; n],
-            n,
-        }
-    }
-
-    fn find(&mut self, x: usize) -> usize {
-        if self.parent[x] != x {
-            self.parent[x] = self.find(self.parent[x]);
-        }
-        self.parent[x]
-    }
-
-    fn union(&mut self, x: usize, y: usize) -> bool {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            return false;
-        }
-        match self.rank[rx].cmp(&self.rank[ry]) {
-            std::cmp::Ordering::Less => self.parent[rx] = ry,
-            std::cmp::Ordering::Equal => {
-                self.rank[rx] += 1;
-                self.parent[ry] = rx;
+pub fn num_special(mat: Vec<Vec<i32>>) -> i32 {
+    use std::collections::HashMap;
+    let mut rmap = HashMap::new();
+    let mut cmap = HashMap::new();
+    let mut ps = vec![];
+    for (r, row) in mat.iter().enumerate() {
+        for (c, &v) in row.iter().enumerate() {
+            if v == 1 {
+                *rmap.entry(r).or_insert(0) += 1;
+                *cmap.entry(c).or_insert(0) += 1;
+                ps.push([r, c]);
             }
-            std::cmp::Ordering::Greater => self.parent[ry] = rx,
         }
-        self.n -= 1;
-        true
     }
+    ps.into_iter()
+        .filter(|[r, c]| rmap[r] == 1 && cmap[c] == 1)
+        .count() as _
 }
 
 #[cfg(test)]
