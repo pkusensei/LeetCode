1aaa646ae4a298f1a474e5308e81aed9f8a0bec7
pkusensei <ZW_08@outlook.com>
2025-01-22 12:39:46 -0500

    1572 sum of diagonals

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,41 +5,13 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_of_ways(nums: &[i32]) -> i32 {
-    let table = build(nums.len());
-    dfs(nums, &table) as i32 - 1
-}
-
-const MOD: i64 = 1_000_000_007;
-
-fn dfs(nums: &[i32], table: &[Vec<i64>]) -> i64 {
-    let n = nums.len();
-    if n < 3 {
-        return 1;
-    }
-    let root = nums[0];
-    let [left, right] = nums
-        .iter()
-        .fold([vec![], vec![]], |[mut left, mut right], &num| {
-            match num.cmp(&root) {
-                std::cmp::Ordering::Less => left.push(num),
-                std::cmp::Ordering::Equal => (),
-                std::cmp::Ordering::Greater => right.push(num),
-            };
-            [left, right]
-        });
-    (dfs(&left, table) * dfs(&right, table) % MOD) * table[n - 1][left.len()] % MOD
-}
-
-fn build(n: usize) -> Vec<Vec<i64>> {
-    let mut res = vec![vec![0; n]; n];
-    for (i, row) in res.iter_mut().enumerate() {
-        row[0] = 1;
-        row[i] = 1;
-    }
-    for row in 2..n {
-        for col in 1..row {
-            res[row][col] = (res[row - 1][col - 1] + res[row - 1][col]) % MOD;
+pub fn diagonal_sum(mat: Vec<Vec<i32>>) -> i32 {
+    let n = mat.len();
+    let mut res = 0;
+    for (i, row) in mat.iter().enumerate() {
+        res += row[i] + row[n - 1 - i];
+        if i == n - 1 - i {
+            res -= row[i];
         }
     }
     res
@@ -52,20 +24,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {
-        assert_eq!(num_of_ways(&[2, 1, 3]), 1);
-        assert_eq!(num_of_ways(&[3, 4, 5, 1, 2]), 5);
-        assert_eq!(num_of_ways(&[1, 2, 3]), 0);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(num_of_ways(&[3, 1, 2, 5, 4, 6]), 19);
-        assert_eq!(
-            num_of_ways(&[9, 4, 2, 1, 3, 6, 5, 7, 8, 14, 11, 10, 12, 13, 16, 15, 17, 18]),
-            216212978
-        );
-    }
+    fn test() {}
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
