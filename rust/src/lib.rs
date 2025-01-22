7b9c9f56351f8a2a6b7fe723dc6cd03fea992024
pkusensei <ZW_08@outlook.com>
2025-01-22 12:31:30 -0500

    1569 build n choose k table

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,156 +2,47 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashMap, HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn tarjan(grid: &[&[i32]]) -> i32 {
-    let ones: i32 = grid.iter().flat_map(|row| row.iter()).sum();
-    if ones == 0 {
-        return 0;
-    }
-    if ones == 1 {
+pub fn num_of_ways(nums: &[i32]) -> i32 {
+    let table = build(nums.len());
+    dfs(nums, &table) as i32 - 1
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn dfs(nums: &[i32], table: &[Vec<i64>]) -> i64 {
+    let n = nums.len();
+    if n < 3 {
         return 1;
     }
-    for (y, row) in grid.iter().enumerate() {
-        for (x, num) in row.iter().enumerate() {
-            if *num == 0 {
-                continue;
-            }
-            let (island, graph) = find_island([y, x], grid);
-            if island.len() < ones as usize {
-                return 0;
-            }
-            let ap = find_articulation_points(island, graph);
-            if !ap.is_empty() {
-                return 1;
-            } else {
-                return 2;
-            }
-        }
-    }
-    0
-}
-
-fn find_island([row, col]: Coord, grid: &[&[i32]]) -> (HashSet<Coord>, HashMap<Coord, Vec<Coord>>) {
-    let mut queue = VecDeque::from([[row, col]]);
-    let mut seen = HashSet::from([[row, col]]);
-    let mut graph: HashMap<Coord, Vec<_>> = HashMap::new();
-    while let Some(curr) = queue.pop_front() {
-        let neighbors = neighbors(curr).filter(|&[row, col]| {
-            grid.get(row)
-                .is_some_and(|row| row.get(col).is_some_and(|&n| n == 1))
+    let root = nums[0];
+    let [left, right] = nums
+        .iter()
+        .fold([vec![], vec![]], |[mut left, mut right], &num| {
+            match num.cmp(&root) {
+                std::cmp::Ordering::Less => left.push(num),
+                std::cmp::Ordering::Equal => (),
+                std::cmp::Ordering::Greater => right.push(num),
+            };
+            [left, right]
         });
-        let v = graph.entry(curr).or_default();
-        for n in neighbors {
-            v.push(n);
-            if seen.insert(n) {
-                queue.push_back(n);
-            }
-        }
-    }
-    (seen, graph)
+    (dfs(&left, table) * dfs(&right, table) % MOD) * table[n - 1][left.len()] % MOD
 }
 
-fn find_articulation_points(
-    coords: HashSet<Coord>,
-    graph: HashMap<Coord, Vec<Coord>>,
-) -> HashSet<Coord> {
-    let mut ap = HashSet::new();
-    // since we are considering only one island
-    if let Some(node) = coords.into_iter().next() {
-        dfs(
-            &graph,
-            node,
-            None,
-            &mut HashMap::new(),
-            &mut HashMap::new(),
-            &mut ap,
-            0,
-        );
+fn build(n: usize) -> Vec<Vec<i64>> {
+    let mut res = vec![vec![0; n]; n];
+    for (i, row) in res.iter_mut().enumerate() {
+        row[0] = 1;
+        row[i] = 1;
     }
-    ap // should also consider isolated 1s
-}
-
-fn dfs(
-    graph: &HashMap<Coord, Vec<Coord>>,
-    curr: Coord,
-    parent: Option<Coord>,
-    lowest: &mut HashMap<Coord, i32>,
-    disc_time: &mut HashMap<Coord, i32>,
-    ap: &mut HashSet<Coord>,
-    time: i32,
-) {
-    let mut children = 0;
-    lowest.entry(curr).or_insert(time);
-    disc_time.entry(curr).or_insert(time);
-
-    for &neighbor in graph[&curr].iter() {
-        if let Some(&v) = disc_time.get(&neighbor) {
-            if parent.is_none() || parent.is_some_and(|p| p != neighbor) {
-                lowest.insert(curr, lowest[&curr].min(v));
-            }
-        } else {
-            children += 1;
-            dfs(graph, neighbor, Some(curr), lowest, disc_time, ap, time + 1);
-            lowest.insert(curr, lowest[&curr].min(lowest[&neighbor]));
-            if parent.is_some() && lowest[&neighbor] >= disc_time[&curr] {
-                ap.insert(curr);
-            }
+    for row in 2..n {
+        for col in 1..row {
+            res[row][col] = (res[row - 1][col - 1] + res[row - 1][col]) % MOD;
         }
     }
-    if parent.is_none() && children > 1 {
-        ap.insert(curr);
-    }
-}
-
-pub fn brute_force(mut grid: Vec<Vec<i32>>) -> i32 {
-    fn count(grid: &[Vec<i32>]) -> i32 {
-        let [rows, cols] = get_dimensions(&grid);
-        let mut seen = vec![vec![false; cols]; rows];
-        let mut res = 0;
-        for (row, r) in grid.iter().enumerate() {
-            for (col, &v) in r.iter().enumerate() {
-                if v == 1 && !seen[row][col] {
-                    res += 1;
-                    seen[row][col] = true;
-                    let mut queue = VecDeque::from([[row, col]]);
-                    while let Some([row, col]) = queue.pop_front() {
-                        for [nr, nc] in neighbors([row, col]).filter(|&[nr, nc]| {
-                            grid.get(nr)
-                                .is_some_and(|r| r.get(nc).is_some_and(|&v| v == 1))
-                        }) {
-                            if !seen[nr][nc] {
-                                seen[nr][nc] = true;
-                                queue.push_back([nr, nc]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        res
-    }
-
-    let [rows, cols] = get_dimensions(&grid);
-    if count(&grid) != 1 {
-        return 0;
-    }
-    for r in 0..rows {
-        for c in 0..cols {
-            if grid[r][c] != 1 {
-                continue;
-            }
-            grid[r][c] = 0;
-            if count(&grid) != 1 {
-                return 1;
-            }
-            grid[r][c] = 1;
-        }
-    }
-    2
+    res
 }
 
 #[cfg(test)]
@@ -161,10 +52,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(num_of_ways(&[2, 1, 3]), 1);
+        assert_eq!(num_of_ways(&[3, 4, 5, 1, 2]), 5);
+        assert_eq!(num_of_ways(&[1, 2, 3]), 0);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(num_of_ways(&[3, 1, 2, 5, 4, 6]), 19);
+        assert_eq!(
+            num_of_ways(&[9, 4, 2, 1, 3, 6, 5, 7, 8, 14, 11, 10, 12, 13, 16, 15, 17, 18]),
+            216212978
+        );
+    }
 
     #[allow(dead_code)]
     fn sort_eq<T1, T2, I1, I2>(mut i1: I1, mut i2: I2)
