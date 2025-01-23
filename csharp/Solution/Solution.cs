a24240dd5d1f6385f8c9e6e04c59c47d658f3ee8
pkusensei <ZW_08@outlook.com>
2025-01-23 13:17:26 -0500

    1593 backtrack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,24 +6,25 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindLengthOfShortestSubarray(int[] arr)
+    public int MaxUniqueSplit(string s)
     {
-        var right = arr.Length - 1;
-        for (int i = arr.Length - 1; i > 0; i -= 1)
-        {
-            if (arr[i] >= arr[i - 1]) { right = i - 1; } else { break; }
-        }
-        var res = right;
-        var left = 0;
-        while (left < right && (left == 0 || arr[left - 1] <= arr[left]))
-        {
-            while (right < arr.Length && arr[left] > arr[right])
-            {
-                right += 1;
-            }
-            res = Math.Min(res, right - left - 1);
-            left += 1;
-        }
+        HashSet<string> set = [];
+        var res = 0;
+        Backtrack(s);
         return res;
+
+        void Backtrack(string s)
+        {
+            if (set.Count + s.Length <= res) { return; }
+            if (s.Length == 0) { res = Math.Max(res, set.Count); }
+            for (int i = 1; i <= s.Length; i++)
+            {
+                if (set.Add(s[..i]))
+                {
+                    Backtrack(s[i..]);
+                    set.Remove(s[..i]);
+                }
+            }
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 1, 2, 3, 10, 4, 2, 3, 5 }, 3)]
-    [DataRow(new[] { 5, 4, 3, 2, 1 }, 4)]
-    [DataRow(new[] { 1, 2, 3 }, 0)]
-    public void TestMethod1(int[] nums, int exp)
+    [DataRow("ababccc", 5)]
+    [DataRow("aba", 2)]
+    [DataRow("aa", 1)]
+    public void TestMethod1(string s, int exp)
     {
-        Assert.AreEqual(exp, sol.FindLengthOfShortestSubarray(nums));
+        Assert.AreEqual(exp, sol.MaxUniqueSplit(s));
     }
 
     [TestMethod]
