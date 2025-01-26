af93288c004b64ac1c0de343b3e42131c783a86e
pkusensei <ZW_08@outlook.com>
2025-01-26 11:40:40 -0500

    2127 cycles and chains

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,32 +2,109 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::{HashMap, VecDeque};
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_effort_path(heights: Vec<Vec<i32>>) -> i32 {
-    let [rows, cols] = get_dimensions(&heights);
-    let mut dists = vec![vec![i32::MAX; cols]; rows];
-    dists[0][0] = 0;
-    let mut heap = BinaryHeap::from([(Reverse(0), [0, 0])]);
-    while let Some((Reverse(cost), [r, c])) = heap.pop() {
-        if r == rows - 1 && c == cols - 1 {
-            return cost;
-        }
-        if cost > dists[r][c] {
+pub fn maximum_invitations(favorite: Vec<i32>) -> i32 {
+    let n = favorite.len();
+    let innodes = favorite
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (i, &v)| {
+            acc[v as usize].push(i);
+            acc
+        });
+    let mut seen = vec![false; n];
+    // Cycle a->b->c->d->e->a
+    // Chains a->b->c<->f<-g<-h
+    // Multiple 2-chain groups can be all fit in
+    // a->b->c<->d<-e + f->g<->h<-i<-j + ...
+    let [mut cycle_len, mut chain_len] = [0, 0];
+    for node in 0..n {
+        if seen[node] {
             continue;
         }
-        for [nr, nc] in neighbors([r, c]).filter(|&[nr, nc]| nr < rows && nc < cols) {
-            let next_cost = (heights[r][c] - heights[nr][nc]).abs().max(cost);
-            if next_cost < dists[nr][nc] {
-                dists[nr][nc] = next_cost;
-                heap.push((Reverse(next_cost), [nr, nc]));
+        let mut curr = node;
+        let mut dist = 0;
+        let mut dists = HashMap::new();
+        while !seen[curr] {
+            seen[curr] = true;
+            dists.insert(curr, dist);
+            dist += 1;
+            let next = favorite[curr] as usize;
+            if let Some(prev) = dists.get(&next) {
+                let d = dist - prev;
+                cycle_len = cycle_len.max(d);
+                if d == 2 {
+                    let mut inchain = vec![false; n];
+                    inchain[curr] = true;
+                    inchain[next] = true;
+                    chain_len +=
+                        bfs(&innodes, &mut inchain, curr) + bfs(&innodes, &mut inchain, next);
+                }
+            }
+            curr = next;
+        }
+    }
+    cycle_len.max(chain_len)
+}
+
+fn bfs(innodes: &[Vec<usize>], inchain: &mut [bool], start: usize) -> i32 {
+    let mut queue = VecDeque::from([(start, 0)]);
+    let mut res = 0;
+    while let Some((curr, dist)) = queue.pop_front() {
+        res = res.max(dist);
+        for &next in innodes[curr].iter() {
+            if !inchain[next] {
+                inchain[next] = true;
+                queue.push_back((next, 1 + dist));
             }
         }
     }
-    -1
+    1 + res
+}
+
+pub fn with_topo_sort(favorite: Vec<i32>) -> i32 {
+    let n = favorite.len();
+    let mut indegs = favorite.iter().fold(vec![0; n], |mut acc, &fav| {
+        acc[fav as usize] += 1;
+        acc
+    });
+    let mut queue: VecDeque<_> = indegs
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &v)| if v == 0 { Some(i) } else { None })
+        .collect();
+    let mut depth = vec![1; n];
+    while let Some(node) = queue.pop_front() {
+        let fav = favorite[node] as usize;
+        depth[fav] = depth[fav].max(1 + depth[node]);
+        indegs[fav] -= 1;
+        if indegs[fav] == 0 {
+            queue.push_back(fav);
+        }
+    }
+    let [mut cycle_len, mut chain_len] = [0, 0];
+    for node in 0..n {
+        if indegs[node] == 0 {
+            continue;
+        }
+        let mut curr = node;
+        let mut curr_len = 0;
+        while indegs[curr] > 0 {
+            indegs[curr] = 0;
+            curr_len += 1;
+            curr = favorite[curr] as usize;
+        }
+        if curr_len == 2 {
+            chain_len += depth[node] + depth[favorite[node] as usize];
+        } else {
+            cycle_len = cycle_len.max(curr_len);
+        }
+    }
+    cycle_len.max(chain_len)
 }
 
 #[cfg(test)]
@@ -47,10 +124,28 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(maximum_invitations(vec![2, 2, 1, 2]), 3);
+        assert_eq!(maximum_invitations(vec![1, 2, 0]), 3);
+        assert_eq!(maximum_invitations(vec![3, 0, 1, 4, 1]), 4);
+
+        assert_eq!(with_topo_sort(vec![2, 2, 1, 2]), 3);
+        assert_eq!(with_topo_sort(vec![1, 2, 0]), 3);
+        assert_eq!(with_topo_sort(vec![3, 0, 1, 4, 1]), 4);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            maximum_invitations(vec![1, 0, 3, 2, 5, 6, 7, 4, 9, 8, 11, 10, 11, 12, 10]),
+            11
+        );
+
+        assert_eq!(
+            with_topo_sort(vec![1, 0, 3, 2, 5, 6, 7, 4, 9, 8, 11, 10, 11, 12, 10]),
+            11
+        );
+    }
 
     #[allow(dead_code)]
     fn float_eq<T1, T2>(a: T1, b: T2)
