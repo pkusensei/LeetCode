6fe8b1b3894bb3bcf120108265e16b18b571d350
Zx Wang <ZW_08@outlook.com>
2025-01-28 14:30:44 -0500

    1673 monotonic stack and keep k elements in stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,67 +2,21 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::VecDeque;
-
 #[allow(unused_imports)]
 use helper::*;
 
-#[derive(Debug, Clone, Default)]
-struct FrontMiddleBackQueue {
-    fst: VecDeque<i32>,
-    snd: VecDeque<i32>,
-}
-
-impl FrontMiddleBackQueue {
-    fn new() -> Self {
-        Default::default()
-    }
-
-    fn push_front(&mut self, val: i32) {
-        self.fst.push_front(val);
-        if self.fst.len() > self.snd.len() {
-            self.snd.push_front(self.fst.pop_back().unwrap());
+pub fn most_competitive(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let k = k as usize;
+    let n = nums.len();
+    let mut res = Vec::with_capacity(k);
+    for (idx, &num) in nums.iter().enumerate() {
+        while res.last().is_some_and(|&v| v > num) && res.len() + (n - idx - 1) >= k {
+            res.pop();
         }
+        res.push(num);
     }
-
-    fn push_middle(&mut self, val: i32) {
-        if self.fst.len() < self.snd.len() {
-            self.fst.push_back(val);
-        } else {
-            self.snd.push_front(val);
-        }
-    }
-
-    fn push_back(&mut self, val: i32) {
-        self.snd.push_back(val);
-        if self.fst.len() + 1 < self.snd.len() {
-            self.fst.push_back(self.snd.pop_front().unwrap());
-        }
-    }
-
-    fn pop_front(&mut self) -> i32 {
-        let res = self.fst.pop_front().or_else(||self.snd.pop_front()).unwrap_or(-1);
-        if self.fst.len() + 1 < self.snd.len() {
-            self.fst.push_back(self.snd.pop_front().unwrap());
-        }
-        res
-    }
-
-    fn pop_middle(&mut self) -> i32 {
-        if self.fst.len() == self.snd.len() {
-            self.fst.pop_back().unwrap_or(-1)
-        } else {
-            self.snd.pop_front().unwrap_or(-1)
-        }
-    }
-
-    fn pop_back(&mut self) -> i32 {
-        let res = self.snd.pop_back().unwrap_or(-1);
-        if self.fst.len() > self.snd.len() {
-            self.snd.push_front(self.fst.pop_back().unwrap());
-        }
-        res
-    }
+    res.truncate(k);
+    res
 }
 
 #[cfg(test)]
@@ -82,18 +36,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        let mut q = FrontMiddleBackQueue::new();
-        q.push_front(1); // [1]
-        q.push_back(2); // [1, 2]
-        q.push_middle(3); // [1, 3, 2]
-        q.push_middle(4); // [1, 4, 3, 2]
-        assert_eq!(q.pop_front(), 1); // return 1 -> [4, 3, 2]
-        assert_eq!(q.pop_middle(), 3); // return 3 -> [4, 2]
-        assert_eq!(q.pop_middle(), 4); // return 4 -> [2]
-        assert_eq!(q.pop_back(), 2); // return 2 -> []
-        assert_eq!(q.pop_front(), -1); // return -1
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
