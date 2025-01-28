073684b3eb96abef9ce411698a6d65d9c88a3f04
Zx Wang <ZW_08@outlook.com>
2025-01-28 13:18:23 -0500

    1669 remove range of nodes from linked list

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,55 +6,29 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<bool> CheckIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries)
+    public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
     {
-        bool[,] pre = new bool[numCourses, numCourses];
-        foreach (var item in prerequisites)
+        var curr = list1;
+        var count = 0;
+        while (count < a - 1)
         {
-            pre[item[0], item[1]] = true;
+            count += 1;
+            curr = curr.next;
         }
-        // Why this loop sequence makes a difference
-        for (int mid = 0; mid < numCourses; mid++)
+        var prev = curr;
+        while (count < b)
         {
-            for (int src = 0; src < numCourses; src++)
-            {
-                for (int tgt = 0; tgt < numCourses; tgt++)
-                {
-                    pre[src, tgt] = pre[src, tgt] || (pre[src, mid] && pre[mid, tgt]);
-                }
-            }
+            count += 1;
+            curr = curr.next;
         }
-        return [.. queries.Select(q => pre[q[0], q[1]])];
-    }
-
-    public IList<bool> WithTopoSort(int numCourses, int[][] prerequisites, int[][] queries)
-    {
-        var indegs = new int[numCourses];
-        var adj = Enumerable.Range(0, numCourses).Select(_ => new List<int>()).ToList();
-        foreach (var edge in prerequisites)
+        var last = curr.next;
+        prev.next = list2;
+        curr = list2;
+        while (curr.next is not null)
         {
-            adj[edge[0]].Add(edge[1]);
-            indegs[edge[1]] += 1;
+            curr = curr.next;
         }
-        var queue = new Queue<int>();
-        for (int i = 0; i < numCourses; i++)
-        {
-            if (indegs[i] == 0) { queue.Enqueue(i); }
-        }
-        var all_preqs = Enumerable.Range(0, numCourses).Select(_ => new HashSet<int>()).ToList();
-        while (queue.TryDequeue(out var curr))
-        {
-            foreach (var next in adj[curr])
-            {
-                all_preqs[next].Add(curr);
-                foreach (var preq in all_preqs[curr])
-                {
-                    all_preqs[next].Add(preq);
-                }
-                indegs[next] -= 1;
-                if (indegs[next] == 0) { queue.Enqueue(next); }
-            }
-        }
-        return [.. queries.Select(q => all_preqs[q[1]].Contains(q[0]))];
+        curr.next = last;
+        return list1;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,13 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(new[] { 10, 1, 13, 6, 9, 5 }, 3, 4, new[] { 1000000, 1000001, 1000002 }, "[10,1,13,1000000,1000001,1000002,5]")]
+    [DataRow(new[] { 0, 1, 2, 3, 4, 5, 6 }, 2, 5, new[] { 1000000, 1000001, 1000002, 1000003, 1000004 }, "[0,1,1000000,1000001,1000002,1000003,1000004,6]")]
+    public void TestMethod1(int[] n1, int a, int b, int[] n2, string exp)
     {
+        var list1 = ListNode.Make(n1);
+        var list2 = ListNode.Make(n2);
+        Assert.AreEqual(exp, sol.MergeInBetween(list1, a, b, list2).ToString());
     }
 
     [TestMethod]
