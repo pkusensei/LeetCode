c45fd02067036277473af0673bd02189e9c67265
Zx Wang <ZW_08@outlook.com>
2025-01-28 13:45:38 -0500

    1670 2 deques to pop middle

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,17 +2,67 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::VecDeque;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_repeating(sequence: String, word: String) -> i32 {
-    let mut res = 0;
-    let mut curr = word.clone();
-    while sequence.contains(&curr) {
-        curr.push_str(&word);
-        res += 1;
+#[derive(Debug, Clone, Default)]
+struct FrontMiddleBackQueue {
+    fst: VecDeque<i32>,
+    snd: VecDeque<i32>,
+}
+
+impl FrontMiddleBackQueue {
+    fn new() -> Self {
+        Default::default()
     }
-    res
+
+    fn push_front(&mut self, val: i32) {
+        self.fst.push_front(val);
+        if self.fst.len() > self.snd.len() {
+            self.snd.push_front(self.fst.pop_back().unwrap());
+        }
+    }
+
+    fn push_middle(&mut self, val: i32) {
+        if self.fst.len() < self.snd.len() {
+            self.fst.push_back(val);
+        } else {
+            self.snd.push_front(val);
+        }
+    }
+
+    fn push_back(&mut self, val: i32) {
+        self.snd.push_back(val);
+        if self.fst.len() + 1 < self.snd.len() {
+            self.fst.push_back(self.snd.pop_front().unwrap());
+        }
+    }
+
+    fn pop_front(&mut self) -> i32 {
+        let res = self.fst.pop_front().or_else(||self.snd.pop_front()).unwrap_or(-1);
+        if self.fst.len() + 1 < self.snd.len() {
+            self.fst.push_back(self.snd.pop_front().unwrap());
+        }
+        res
+    }
+
+    fn pop_middle(&mut self) -> i32 {
+        if self.fst.len() == self.snd.len() {
+            self.fst.pop_back().unwrap_or(-1)
+        } else {
+            self.snd.pop_front().unwrap_or(-1)
+        }
+    }
+
+    fn pop_back(&mut self) -> i32 {
+        let res = self.snd.pop_back().unwrap_or(-1);
+        if self.fst.len() > self.snd.len() {
+            self.snd.push_front(self.fst.pop_back().unwrap());
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -32,7 +82,18 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        let mut q = FrontMiddleBackQueue::new();
+        q.push_front(1); // [1]
+        q.push_back(2); // [1, 2]
+        q.push_middle(3); // [1, 3, 2]
+        q.push_middle(4); // [1, 4, 3, 2]
+        assert_eq!(q.pop_front(), 1); // return 1 -> [4, 3, 2]
+        assert_eq!(q.pop_middle(), 3); // return 3 -> [4, 2]
+        assert_eq!(q.pop_middle(), 4); // return 4 -> [2]
+        assert_eq!(q.pop_back(), 2); // return 2 -> []
+        assert_eq!(q.pop_front(), -1); // return -1
+    }
 
     #[test]
     fn test() {}
