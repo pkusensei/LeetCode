59af4b1f55b42fa81ef017b37d0eeb5e34edaa75
pkusensei <ZW_08@outlook.com>
2025-01-29 13:17:04 -0500

    1684

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,44 +5,12 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_incompatibility(nums: &mut [i32], k: i32) -> i32 {
-    let (n, k) = (nums.len(), k as usize);
-    let mut groups = vec![Vec::with_capacity(n / k); k];
-    nums.sort_unstable();
-    let mut res = i32::MAX;
-    dfs(nums, 0, 0, &mut groups, n / k, 0, &mut res);
-    if res == i32::MAX {
-        -1
-    } else {
-        res
-    }
-}
-
-fn dfs(
-    nums: &[i32],
-    k: usize,
-    idx: usize,
-    groups: &mut [Vec<i32>],
-    group_len: usize,
-    curr: i32,
-    res: &mut i32,
-) {
-    let n = nums.len();
-    if idx >= n {
-        *res = (*res).min(curr);
-        return;
-    }
-    let val = nums[idx];
-    for i in 0..groups.len().min(1 + k) {
-        if groups[i].len() < group_len && groups[i].last().is_none_or(|&v| v < val) {
-            let temp = val - groups[i].last().copied().unwrap_or(val) + curr;
-            if curr < *res {
-                groups[i].push(val);
-                dfs(nums, k.max(1 + i), 1 + idx, groups, group_len, temp, res);
-                groups[i].pop();
-            }
-        }
-    }
+pub fn count_consistent_strings(allowed: String, words: Vec<String>) -> i32 {
+    let allowed: std::collections::HashSet<_> = allowed.into_bytes().into_iter().collect();
+    words
+        .into_iter()
+        .map(|s| i32::from(s.into_bytes().into_iter().all(|b| allowed.contains(&b))))
+        .sum()
 }
 
 #[cfg(test)]
@@ -62,11 +30,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_incompatibility(&mut [1, 2, 1, 4], 2), 4);
-        assert_eq!(minimum_incompatibility(&mut [6, 3, 8, 1, 3, 1, 2, 2], 4), 6);
-        assert_eq!(minimum_incompatibility(&mut [5, 3, 3, 6, 3, 3], 3), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
