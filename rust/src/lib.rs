aaef6f5bc1e62cb435b4ebb043ae1934b70ffa30
pkusensei <ZW_08@outlook.com>
2025-01-29 17:23:03 -0500

    1687 this sliding window + dp...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,45 +5,92 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn stone_game_vi(alice_values: Vec<i32>, bob_values: Vec<i32>) -> i32 {
-    let mut heap: std::collections::BinaryHeap<_> = alice_values
-        .into_iter()
-        .zip(bob_values)
-        .map(|(a, b)| Pair { a, b })
-        .collect();
-    let [mut s1, mut s2] = [0, 0];
-    let mut turn = true;
-    while let Some(Pair { a, b }) = heap.pop() {
-        if turn {
-            s1 += a;
-        } else {
-            s2 += b;
+pub fn box_delivering(
+    boxes: &[[i32; 2]],
+    _ports_count: i32,
+    mut max_boxes: i32,
+    mut max_weight: i32,
+) -> i32 {
+    let n = boxes.len();
+    let [mut right, mut last_right] = [0, 0];
+    let mut need = 0; // trips needed to do [left..=right]
+    let mut dp = vec![200_000; 1 + n];
+    dp[0] = 0;
+    for (left, b) in boxes.iter().enumerate() {
+        while right < n && max_boxes > 0 && max_weight >= boxes[right][1] {
+            max_boxes -= 1;
+            max_weight -= boxes[right][1];
+            if right == 0 || boxes[right][0] != boxes[right - 1][0] {
+                last_right = right; // [last_right..=right] of same port
+                need += 1; // new port adds one trip
+            }
+            right += 1;
         }
-        turn = !turn;
+        // take as many as possible
+        dp[right] = dp[right].min(dp[left] + 1 + need);
+        // save one trip/need, but carry less weight
+        dp[last_right] = dp[last_right].min(dp[left] + need);
+        // move left ptr forward; this box was shipped during previous trip
+        max_boxes += 1;
+        max_weight += b[1];
+        // this box is of a different port
+        if left == n - 1 || b[0] != boxes[left + 1][0] {
+            need -= 1;
+        }
     }
-    match s1.cmp(&s2) {
-        std::cmp::Ordering::Less => -1,
-        std::cmp::Ordering::Equal => 0,
-        std::cmp::Ordering::Greater => 1,
-    }
+    dp[n]
+
+    // let prefix = boxes.iter().fold(Vec::with_capacity(n), |mut acc, b| {
+    //     acc.push(b[1] + acc.last().unwrap_or(&0));
+    //     acc
+    // });
+    // dfs(
+    //     boxes,
+    //     &prefix,
+    //     max_boxes as _,
+    //     max_weight,
+    //     0,
+    //     &mut vec![-1; n],
+    // )
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Pair {
-    a: i32,
-    b: i32,
-}
-
-impl PartialOrd for Pair {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+// TLEs
+fn dfs(
+    boxes: &[[i32; 2]],
+    prefix: &[i32],
+    max_boxes: usize,
+    max_weight: i32,
+    idx: usize,
+    memo: &mut [i32],
+) -> i32 {
+    let n = boxes.len();
+    if idx >= n {
+        return 0;
     }
-}
-
-impl Ord for Pair {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.a + self.b).cmp(&(other.a + other.b))
+    if memo[idx] > -1 {
+        return memo[idx];
     }
+    let mut res = i32::MAX;
+    let mut port_count = 1;
+    let mut curr_port = boxes[idx][0];
+    let mut end = 0;
+    for (i, b) in boxes.iter().enumerate().skip(idx).take(max_boxes) {
+        let curr_weight = prefix[i] - if idx > 0 { prefix[idx - 1] } else { 0 };
+        if curr_weight > max_weight {
+            break;
+        }
+        if b[0] != curr_port {
+            // new port
+            curr_port = b[0];
+            res = res.min(2 + port_count - 1 + dfs(boxes, prefix, max_boxes, max_weight, i, memo));
+            port_count += 1;
+        }
+        end = i;
+    }
+    // take as many as possible
+    res = res.min(2 + port_count - 1 + dfs(boxes, prefix, max_boxes, max_weight, 1 + end, memo));
+    memo[idx] = res;
+    res
 }
 
 #[cfg(test)]
@@ -63,7 +110,17 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(box_delivering(&[[1, 1], [2, 1], [1, 1]], 2, 3, 3), 4);
+        assert_eq!(
+            box_delivering(&[[1, 2], [3, 3], [3, 1], [3, 1], [2, 4]], 3, 3, 6),
+            6
+        );
+        assert_eq!(
+            box_delivering(&[[1, 4], [1, 2], [2, 1], [2, 1], [3, 2], [3, 4]], 3, 6, 7),
+            6
+        );
+    }
 
     #[test]
     fn test() {}
