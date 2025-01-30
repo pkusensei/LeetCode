234302e4c8a745c4d28514766753910fc8e2e98a
Zx Wang <ZW_08@outlook.com>
2025-01-30 11:09:09 -0500

    1691 LIS after sorting

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,39 +5,25 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn stone_game_vii(stones: &[i32]) -> i32 {
-    let n = stones.len();
-    let prefix = stones.iter().fold(Vec::with_capacity(n), |mut acc, &num| {
-        acc.push(num + acc.last().unwrap_or(&0));
-        acc
-    });
-    let mut dp = vec![vec![0; n]; n];
-    for left in (0..n).rev() {
-        for right in 1 + left..n {
-            let remove_left = prefix[right] - prefix[left];
-            let remove_right = prefix[right - 1] - if left > 0 { prefix[left - 1] } else { 0 };
-            dp[left][right] =
-                (remove_left - dp[1 + left][right]).max(remove_right - dp[left][right - 1]);
+pub fn max_height(cuboids: &mut [[i32; 3]]) -> i32 {
+    let n = cuboids.len();
+    for c in cuboids.iter_mut() {
+        c.sort_unstable();
+    }
+    cuboids.sort_unstable();
+    let mut dp: Vec<i32> = cuboids.iter().map(|c| c[2]).collect();
+    for i1 in 1..n {
+        for i2 in 0..i1 {
+            if cuboids[i2]
+                .iter()
+                .zip(cuboids[i1].iter())
+                .all(|(a, b)| a <= b)
+            {
+                dp[i1] = dp[i1].max(cuboids[i1][2] + dp[i2])
+            }
         }
     }
-    dp[0][n - 1]
-    // dfs(&prefix, 0, n - 1, &mut vec![vec![-1; n]; n])
-}
-
-fn dfs(prefix: &[i32], left: usize, right: usize, memo: &mut [Vec<i32>]) -> i32 {
-    if left == right {
-        return 0;
-    }
-    if memo[left][right] > -1 {
-        return memo[left][right];
-    }
-    let remove_left = prefix[right] - prefix[left];
-    let remove_right = prefix[right - 1] - if left > 0 { prefix[left - 1] } else { 0 };
-    // max (curr_score - opponent_score)
-    let res = (remove_left - dfs(prefix, 1 + left, right, memo))
-        .max(remove_right - dfs(prefix, left, right - 1, memo));
-    memo[left][right] = res;
-    res
+    dp.into_iter().max().unwrap_or(cuboids[n - 1][2])
 }
 
 #[cfg(test)]
@@ -58,8 +44,22 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(stone_game_vii(&[5, 3, 1, 4, 2]), 6);
-        assert_eq!(stone_game_vii(&[7, 90, 5, 1, 100, 10, 10, 2]), 122);
+        assert_eq!(
+            max_height(&mut [[50, 45, 20], [95, 37, 53], [45, 23, 12]]),
+            190
+        );
+        assert_eq!(max_height(&mut [[38, 25, 45], [76, 35, 3]]), 76);
+        assert_eq!(
+            max_height(&mut [
+                [7, 11, 17],
+                [7, 17, 11],
+                [11, 7, 17],
+                [11, 17, 7],
+                [17, 7, 11],
+                [17, 11, 7]
+            ]),
+            102
+        );
     }
 
     #[test]
