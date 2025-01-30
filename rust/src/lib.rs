09f59bd1c6322d9a48f54e2e5f3aae836dc3a970
Zx Wang <ZW_08@outlook.com>
2025-01-30 14:30:40 -0500

    1701

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_students(students: Vec<i32>, sandwiches: Vec<i32>) -> i32 {
-    let mut ones: i32 = students.iter().sum();
-    let mut zeros = students.len() as i32 - ones;
-    for num in sandwiches {
-        if num == 0 && zeros > 0 {
-            zeros -= 1;
-        } else if num == 1 && ones > 0 {
-            ones -= 1;
+pub fn average_waiting_time(customers: &[[i32; 2]]) -> f64 {
+    let mut chef = 0;
+    let mut wait = 0;
+    for c in customers.iter() {
+        let [a, t] = [0, 1].map(|i| i64::from(c[i]));
+        if a >= chef {
+            chef = a + t;
+            wait += t
         } else {
-            break;
+            chef += t;
+            wait += chef - a;
         }
     }
-    zeros + ones
+    wait as f64 / customers.len() as f64
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fmt::Debug;
 
     use super::*;
 
@@ -36,22 +36,29 @@ mod tests {
         };
     }
 
+    const EP: f64 = 1e-5;
+    #[allow(unused_macros)]
+    macro_rules! float_eq {
+        ($a:expr, $b:expr) => {
+            let (left, right) = ($a, $b);
+            assert!(
+                (left - right).abs() <= EP,
+                "left = {:?}, right = {:?}",
+                left,
+                right
+            );
+        };
+    }
+
     #[test]
-    fn basics() {}
+    fn basics() {
+        float_eq!(average_waiting_time(&[[1, 2], [2, 5], [4, 3]]), 5.0);
+        float_eq!(
+            average_waiting_time(&[[5, 2], [5, 4], [10, 3], [20, 1]]),
+            3.25
+        );
+    }
 
     #[test]
     fn test() {}
-
-    #[allow(dead_code)]
-    fn float_eq<T1, T2>(a: T1, b: T2)
-    where
-        T1: Into<f64> + Copy + Debug,
-        T2: Into<f64> + Copy + Debug,
-    {
-        const EP: f64 = 1e-5;
-        assert!(
-            (<T1 as Into<f64>>::into(a) - <T2 as Into<f64>>::into(b)).abs() <= EP,
-            "left = {a:?}, right = {b:?}"
-        );
-    }
 }
