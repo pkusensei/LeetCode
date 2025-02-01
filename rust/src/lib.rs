239334ed7841323d0f64b8f2d4694976b03a5623
pkusensei <ZW_08@outlook.com>
2025-01-31 21:34:23 -0500

    1716 arithmetic seq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,55 +2,12 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{BTreeSet, HashMap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(target: &[i32], arr: &[i32]) -> i32 {
-    // Each num in target is unique
-    // Map them to their indices
-    // Thus their indices should form an increasing subseq in arr
-    // LCS => LIS
-    let indices = target
-        .iter()
-        .enumerate()
-        .fold(HashMap::new(), |mut acc, (i, &num)| {
-            acc.insert(num, i);
-            acc
-        });
-    let mut lis = vec![];
-    for num in arr.iter() {
-        let Some(&idx) = indices.get(num) else {
-            continue; // ignore num not in target
-        };
-        let Err(pos) = lis.binary_search(&idx) else {
-            continue;
-        };
-        if let Some(v) = lis.get_mut(pos) {
-            *v = idx;
-        } else {
-            lis.push(idx);
-        }
-    }
-    (target.len() - lis.len()) as _
-    // let mut stack = BTreeSet::new();
-    // for &num in arr.iter() {
-    //     let Some(&idx) = indices.get(&num) else {
-    //         continue; // ignore num not in target
-    //     };
-    //     // If current idx is bigger than all in stack
-    //     if stack.is_empty() || stack.last().is_some_and(|&v| v < idx) {
-    //         // if stack.last().is_none_or(|&v| v < idx) {
-    //         stack.insert(idx);
-    //         continue;
-    //     }
-    //     // else replace the closest bigger value
-    //     let key = *stack.range(idx..).next().unwrap();
-    //     stack.remove(&key);
-    //     stack.insert(idx);
-    // }
-    // (target.len() - stack.len()) as i32
+pub fn total_money(n: i32) -> i32 {
+    let seq = n / 7;
+    21 * seq + 7 * (1 + seq) * seq / 2 + (1..=n % 7).map(|v| v + seq).sum::<i32>()
 }
 
 #[cfg(test)]
@@ -83,13 +40,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_operations(&[5, 1, 3], &[9, 4, 2, 3, 4]), 2);
-        assert_eq!(
-            min_operations(&[6, 4, 8, 1, 3, 2], &[4, 7, 6, 2, 3, 8, 6, 1]),
-            3
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
