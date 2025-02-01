075e58028580abb77ee10cb0d605a3dbe8afd1a1
pkusensei <ZW_08@outlook.com>
2025-01-31 19:04:53 -0500

    3151

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,27 +5,8 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn ways_to_split(nums: &[i32]) -> i32 {
-    let n = nums.len();
-    let prefix = nums.iter().fold(Vec::with_capacity(n), |mut acc, &num| {
-        acc.push(num + acc.last().unwrap_or(&0));
-        acc
-    });
-    let [mut right1, mut right2] = [0, 0];
-    let mut res = 0;
-    for left in 0..n - 2 {
-        right1 = (1 + left).max(right1);
-        while right1 < n - 1 && prefix[right1] < 2 * prefix[left] {
-            right1 += 1;
-        }
-        right2 = right2.max(right1);
-        while right2 < n - 1 && prefix[n - 1] - prefix[right2] >= prefix[right2] - prefix[left] {
-            right2 += 1;
-        }
-        res += right2 - right1;
-        res %= 1_000_000_007;
-    }
-    res as _
+pub fn is_array_special(nums: Vec<i32>) -> bool {
+    nums.windows(2).all(|w| w[0] & 1 != w[1] & 1)
 }
 
 #[cfg(test)]
@@ -58,11 +39,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(ways_to_split(&[1, 1, 1]), 1);
-        assert_eq!(ways_to_split(&[1, 2, 2, 2, 5, 0]), 3);
-        assert_eq!(ways_to_split(&[3, 2, 1]), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
