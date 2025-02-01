19a1b39e9944a9c3ec86c24619eb9af4ecfe8a3d
Zx Wang <ZW_08@outlook.com>
2025-02-01 09:28:01 -0500

    1717 double stacks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,9 +5,29 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn total_money(n: i32) -> i32 {
-    let seq = n / 7;
-    21 * seq + 7 * (1 + seq) * seq / 2 + (1..=n % 7).map(|v| v + seq).sum::<i32>()
+pub fn maximum_gain(s: &str, x: i32, y: i32) -> i32 {
+    let mut st1 = vec![];
+    let &[mut left, mut right] = if x > y { b"ab" } else { b"ba" };
+    let mut res = 0;
+    for b in s.bytes() {
+        if b == right && st1.last().is_some_and(|&v| v == left) {
+            st1.pop();
+            res += x.max(y);
+        } else {
+            st1.push(b);
+        }
+    }
+    std::mem::swap(&mut left, &mut right);
+    let mut st2 = vec![];
+    for b in st1 {
+        if b == right && st2.last().is_some_and(|&v| v == left) {
+            st2.pop();
+            res += x.min(y)
+        } else {
+            st2.push(b);
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -40,7 +60,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(maximum_gain("cdbcbbaaabab", 4, 5), 19);
+        assert_eq!(maximum_gain("aabbaaxybbaabb", 5, 4), 20);
+    }
 
     #[test]
     fn test() {}
