8bc8d4a26e212a1cd3b13b4d77ff2b74e32fc3fd
pkusensei <ZW_08@outlook.com>
2025-02-02 18:02:09 -0500

    1736 edge cases

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,54 +2,30 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn ways_to_fill_array(queries: &[[i32; 2]]) -> Vec<i32> {
-    const PRIMES: [i32; 25] = [
-        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
-        97,
-    ];
-    let mut memo = HashMap::new();
+pub fn maximum_time(time: String) -> String {
+    let (h, m) = time.split_once(':').unwrap();
+    let h = h.as_bytes();
     let mut res = vec![];
-    for q in queries.iter() {
-        let mut count = 1;
-        let [n, mut k] = [q[0], q[1]];
-        for prime in PRIMES {
-            let mut curr = 0;
-            while k % prime == 0 {
-                k /= prime;
-                curr += 1;
-            }
-            count *= n_choose_k(n - 1 + curr, curr, &mut memo);
-            count %= MOD;
-        }
-        if k != 1 {
-            count *= i64::from(n);
-            count %= MOD;
-        }
-        res.push(count as i32);
+    match (h[0], h[1]) {
+        (b'?', b'?') => res.extend_from_slice(b"23"),
+        (b'?', b'0'..b'4') => res.extend([b'2', h[1]]),
+        (b'?', b'4'..=b'9') => res.extend([b'1', h[1]]),
+        (b'2', b'?') => res.extend_from_slice(b"23"),
+        (_, b'?') => res.extend([h[0], b'9']),
+        _ => res.extend_from_slice(h),
     }
-    res
-}
-
-const MOD: i64 = 1_000_000_007;
-
-fn n_choose_k(n: i32, k: i32, memo: &mut HashMap<[i32; 2], i64>) -> i64 {
-    if let Some(&v) = memo.get(&[n, k]) {
-        return v;
+    res.push(b':');
+    let m = m.as_bytes();
+    match (m[0], m[1]) {
+        (b'?', b'?') => res.extend_from_slice(b"59"),
+        (b'?', b'0'..=b'9') => res.extend([b'5', m[1]]),
+        (_, b'?') => res.extend([m[0], b'9']),
+        _ => res.extend_from_slice(m),
     }
-    let res = if k > n {
-        0
-    } else if k == 0 || k == n {
-        1
-    } else {
-        (n_choose_k(n - 1, k - 1, memo) + n_choose_k(n - 1, k, memo)) % MOD
-    };
-    memo.insert([n, k], res);
-    res
+    String::from_utf8(res).unwrap()
 }
 
 #[cfg(test)]
@@ -82,16 +58,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            ways_to_fill_array(&[[2, 6], [5, 1], [73, 660]]),
-            [4, 1, 50734910]
-        );
-        assert_eq!(
-            ways_to_fill_array(&[[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]),
-            [1, 2, 3, 10, 5]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
