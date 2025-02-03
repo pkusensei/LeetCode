37d8c5239c9ac2ff1ee2c3ce31a2ba791883341f
pkusensei <ZW_08@outlook.com>
2025-02-03 12:31:06 -0500

    1743 dfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,23 +2,37 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_balls(low_limit: i32, high_limit: i32) -> i32 {
-    (low_limit..=high_limit)
-        .fold(std::collections::HashMap::new(), |mut acc, mut num| {
-            let mut curr = 0;
-            while num > 0 {
-                curr += num % 10;
-                num /= 10;
-            }
-            *acc.entry(curr).or_insert(0) += 1;
+pub fn restore_array(adjacent_pairs: &[[i32; 2]]) -> Vec<i32> {
+    let adj = adjacent_pairs
+        .iter()
+        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, e| {
+            acc.entry(e[0]).or_default().push(e[1]);
+            acc.entry(e[1]).or_default().push(e[0]);
             acc
-        })
-        .into_values()
-        .max()
-        .unwrap()
+        });
+    let Some(start) = adj
+        .iter()
+        .find_map(|(&k, v)| if v.len() == 1 { Some(k) } else { None })
+    else {
+        return vec![];
+    };
+    let mut res = vec![start];
+    dfs(&adj, start, None, &mut res);
+    res
+}
+
+fn dfs(adj: &HashMap<i32, Vec<i32>>, node: i32, prev: Option<i32>, res: &mut Vec<i32>) {
+    for &next in adj[&node].iter() {
+        if prev.is_none_or(|v| v != next) {
+            res.push(next);
+            dfs(adj, next, Some(node), res);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -51,7 +65,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(restore_array(&[[2, 1], [3, 4], [3, 2]]), [1, 2, 3, 4]);
+    }
 
     #[test]
     fn test() {}
