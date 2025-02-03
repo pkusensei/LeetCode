fdf4ed6f735a470241fe5e076ac1915e13f6c013
pkusensei <ZW_08@outlook.com>
2025-02-03 18:50:17 -0500

    1751 pick unpick

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_length(s: String) -> i32 {
-    let mut s = s.as_str();
-    let mut right = s.len() - 1;
-    while right > 0 && s.as_bytes()[0] == s.as_bytes()[right] {
-        let b = s.as_bytes()[0];
-        s = s.trim_matches(char::from(b));
-        if s.is_empty() {
-            break;
+pub fn max_value(events: &mut [[i32; 3]], k: i32) -> i32 {
+    let (n, k) = (events.len(), k as usize);
+    events.sort_unstable_by_key(|e| e[0]); // sort by start
+    let nexts: Vec<_> = (0..n)
+        .map(|idx| events.partition_point(|e| e[0] <= events[idx][1]))
+        .collect();
+    let mut dp = vec![vec![0; 1 + n]; 1 + k];
+    for idx in (0..n).rev() {
+        for count in 1..=k {
+            let next = nexts[idx];
+            let pick = events[idx][2] + dp[count - 1][next];
+            let skip = dp[count][1 + idx];
+            dp[count][idx] = pick.max(skip)
         }
-        right = s.len() - 1
     }
-    s.len() as _
+    dp[k][0]
+    // dfs(events, 0, k, &mut vec![vec![-1; 1 + k]; n])
+}
+
+fn dfs(events: &[[i32; 3]], idx: usize, k: usize, memo: &mut [Vec<i32>]) -> i32 {
+    let n = events.len();
+    if k == 0 || idx >= n {
+        return 0;
+    }
+    if memo[idx][k] > -1 {
+        return memo[idx][k];
+    }
+    let mut res = dfs(events, 1 + idx, k, memo); // skip
+    let end = events[idx][1];
+    let val = events[idx][2];
+    let next = events.partition_point(|v| v[0] <= end);
+    res = res.max(val + dfs(events, next, k - 1, memo));
+    memo[idx][k] = res;
+    res
 }
 
 #[cfg(test)]
@@ -49,8 +71,29 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_value(&mut [[1, 2, 4], [3, 4, 3], [2, 3, 1]], 2), 7);
+        assert_eq!(max_value(&mut [[1, 2, 4], [3, 4, 3], [2, 3, 10]], 2), 10);
+        assert_eq!(
+            max_value(&mut [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]], 3),
+            9
+        );
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            max_value(
+                &mut [
+                    [11, 17, 56],
+                    [24, 40, 53],
+                    [5, 62, 67],
+                    [66, 69, 84],
+                    [56, 89, 15]
+                ],
+                2
+            ),
+            151
+        );
+    }
 }
