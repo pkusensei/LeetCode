80d754855f9cdd5f409ac67b3536b4195f899c7a
pkusensei <ZW_08@outlook.com>
2025-02-07 16:30:01 -0500

    1814 num-reverse(num)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,28 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn are_sentences_similar(sentence1: String, sentence2: String) -> bool {
-    let s1: Vec<_> = sentence1.split_whitespace().collect();
-    let s2: Vec<_> = sentence2.split_whitespace().collect();
-    let (n1, n2) = (s1.len(), s2.len());
-    let [mut prefix, mut suffix] = [0, 0];
-    let [mut i1, mut i2] = [0, 0];
-    while s1.get(i1).zip(s2.get(i2)).is_some_and(|(&a, &b)| a == b) {
-        prefix += 1;
-        i1 += 1;
-        i2 += 1;
+pub fn count_nice_pairs(nums: &[i32]) -> i32 {
+    nums.iter()
+        .fold(std::collections::HashMap::new(), |mut acc, &num| {
+            *acc.entry(num - reverse(num)).or_insert(0i64) += 1;
+            acc
+        })
+        .into_values()
+        .fold(0, |acc, v| (acc + v * (v - 1) / 2) % 1_000_000_007) as _
+}
+
+const fn reverse(mut num: i32) -> i32 {
+    let mut res = 0;
+    while num > 0 {
+        res = 10 * res + num % 10;
+        num /= 10;
     }
-    i1 = n1 - 1;
-    i2 = n2 - 1;
-    while s1.get(i1).zip(s2.get(i2)).is_some_and(|(&a, &b)| a == b) {
-        suffix += 1;
-        if i1 == 0 || i2 == 0 {
-            break;
-        }
-        i1 -= 1;
-        i2 -= 1;
-    }
-    n1.min(n2) <= prefix + suffix
+    res
 }
 
 #[cfg(test)]
