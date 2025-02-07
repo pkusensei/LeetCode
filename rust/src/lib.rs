cb543e1c0ab01dfaed61ab3d41cd211c5e66a4d7
pkusensei <ZW_08@outlook.com>
2025-02-07 16:14:32 -0500

    1813

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,9 +5,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn square_is_white(coordinates: String) -> bool {
-    let s = coordinates.as_bytes();
-    (s[0] - b'a') & 1 == (s[1] - b'0') & 1
+pub fn are_sentences_similar(sentence1: String, sentence2: String) -> bool {
+    let s1: Vec<_> = sentence1.split_whitespace().collect();
+    let s2: Vec<_> = sentence2.split_whitespace().collect();
+    let (n1, n2) = (s1.len(), s2.len());
+    let [mut prefix, mut suffix] = [0, 0];
+    let [mut i1, mut i2] = [0, 0];
+    while s1.get(i1).zip(s2.get(i2)).is_some_and(|(&a, &b)| a == b) {
+        prefix += 1;
+        i1 += 1;
+        i2 += 1;
+    }
+    i1 = n1 - 1;
+    i2 = n2 - 1;
+    while s1.get(i1).zip(s2.get(i2)).is_some_and(|(&a, &b)| a == b) {
+        suffix += 1;
+        if i1 == 0 || i2 == 0 {
+            break;
+        }
+        i1 -= 1;
+        i2 -= 1;
+    }
+    n1.min(n2) <= prefix + suffix
 }
 
 #[cfg(test)]
