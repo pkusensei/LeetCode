a06a2ba4428ddd7d67ab82f25dcc1ea2bf66cd16
Zx Wang <ZW_08@outlook.com>
2025-02-08 10:14:36 -0500

    1822 signum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,39 +2,11 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_different_subsequence_gc_ds(nums: &[i32]) -> i32 {
-    const fn gcd(a: i32, b: i32) -> i32 {
-        if a == 0 {
-            b
-        } else {
-            gcd(b % a, a)
-        }
-    }
-
-    let set: HashSet<_> = nums.iter().copied().collect();
-    let max = nums.iter().copied().max().unwrap();
-    let mut res = 0;
-    // candidate gcd x
-    for x in 1..=max {
-        let mut curr = 0;
-        // scan thru seq of multiples of x
-        // test whether the seq's intersection with nums has gcd==x
-        for num in (x..=max).step_by(x as usize) {
-            if set.contains(&num) {
-                curr = gcd(curr, num);
-                if x == curr {
-                    break;
-                }
-            }
-        }
-        res += i32::from(curr == x);
-    }
-    res
+pub fn array_sign(nums: Vec<i32>) -> i32 {
+    nums.into_iter().map(|v| v.signum()).product()
 }
 
 #[cfg(test)]
@@ -67,10 +39,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_different_subsequence_gc_ds(&[6, 10, 3]), 5);
-        assert_eq!(count_different_subsequence_gc_ds(&[5, 15, 40, 5, 6]), 7);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
