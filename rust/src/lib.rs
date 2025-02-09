ccf8b57fab3d5342342a08e5019723255109e588
pkusensei <ZW_08@outlook.com>
2025-02-09 14:05:00 -0500

    1848

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,32 +5,12 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn closest_room(rooms: &mut [[i32; 2]], queries: &[[i32; 2]]) -> Vec<i32> {
-    let n = rooms.len();
-    // sort by size, then by id
-    rooms.sort_unstable_by(|a, b| a[1].cmp(&b[1]).then(a[0].cmp(&b[0])));
-    let mut res = vec![];
-    for q in queries.iter() {
-        let [pref, min] = [q[0], q[1]];
-        let i = rooms.partition_point(|v| v[1] < min);
-        if i == n {
-            res.push(-1);
-            continue;
+pub fn get_min_distance(nums: Vec<i32>, target: i32, start: i32) -> i32 {
+    let mut res = i32::MAX;
+    for (i, &num) in (0..).zip(nums.iter()) {
+        if num == target {
+            res = res.min((i - start).abs());
         }
-        let mut curr = 0;
-        let mut diff = u32::MAX;
-        for r in rooms[i..].iter() {
-            let id = r[0];
-            match id.abs_diff(pref).cmp(&diff) {
-                std::cmp::Ordering::Less => {
-                    curr = id;
-                    diff = id.abs_diff(pref);
-                }
-                std::cmp::Ordering::Equal => curr = curr.min(id),
-                std::cmp::Ordering::Greater => (),
-            }
-        }
-        res.push(curr);
     }
     res
 }
@@ -65,19 +45,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            closest_room(&mut [[2, 2], [1, 2], [3, 2]], &[[3, 1], [3, 3], [5, 2]]),
-            [3, -1, 3]
-        );
-        assert_eq!(
-            closest_room(
-                &mut [[1, 4], [2, 3], [3, 5], [4, 1], [5, 2]],
-                &[[2, 3], [2, 4], [2, 5]]
-            ),
-            [2, 1, 3]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
