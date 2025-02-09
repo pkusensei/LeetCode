09dd96e8a18e86e6508a843a96d71cc4534e9a55
pkusensei <ZW_08@outlook.com>
2025-02-09 13:17:11 -0500

    1844

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,26 +5,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_building(n: i32, mut rests: Vec<[i32; 2]>) -> i32 {
-    rests.extend([[1, 0], [n, n - 1]]);
-    rests.sort_unstable();
-    let len = rests.len();
-    for idx in 0..len - 1 {
-        let dist = rests[idx + 1][0] - rests[idx][0];
-        rests[idx + 1][1] = rests[idx + 1][1].min(rests[idx][1] + dist);
+pub fn replace_digits(s: String) -> String {
+    let mut s = s.into_bytes();
+    let n = s.len();
+    for idx in (1..n).step_by(2) {
+        let b = s[idx - 1];
+        s[idx] = b + (s[idx] - b'0');
     }
-    for idx in (0..len - 1).rev() {
-        let dist = rests[idx + 1][0] - rests[idx][0];
-        rests[idx][1] = rests[idx][1].min(rests[idx + 1][1] + dist);
-    }
-    let mut res = 0;
-    for idx in 0..len - 1 {
-        let dist = rests[idx + 1][0] - rests[idx][0];
-        let h1 = rests[idx][1];
-        let h2 = rests[idx + 1][1];
-        res = res.max(h1.max(h2) + (dist - (h1 - h2).abs()) / 2);
-    }
-    res
+    String::from_utf8(s).unwrap()
 }
 
 #[cfg(test)]
@@ -57,11 +45,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_building(5, vec![[2, 1], [4, 1]]), 2);
-        assert_eq!(max_building(6, vec![]), 5);
-        assert_eq!(max_building(10, vec![[5, 3], [2, 5], [7, 4], [10, 3]]), 5);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
