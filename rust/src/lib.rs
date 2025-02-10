5113f1641e22ea70854d4962e063e9930ae23c5a
pkusensei <ZW_08@outlook.com>
2025-02-10 11:09:14 -0500

    1860

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sort_sentence(s: String) -> String {
-    let mut v: Vec<_> = s
-        .split_whitespace()
-        .map(|s| {
-            let n = s.len();
-            (s.as_bytes()[n - 1], &s[..n - 1])
-        })
-        .collect();
-    v.sort_unstable();
-    v.into_iter().map(|(_, s)| s).collect::<Vec<_>>().join(" ")
+pub fn mem_leak(mut memory1: i32, mut memory2: i32) -> Vec<i32> {
+    let mut n = 1;
+    loop {
+        if memory1 >= memory2 && memory1 >= n {
+            memory1 -= n;
+        } else if memory2 >= n {
+            memory2 -= n
+        } else {
+            return vec![n, memory1, memory2];
+        }
+        n += 1;
+    }
 }
 
 #[cfg(test)]
