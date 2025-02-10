68dbfd5b4980091d99c4377031b4f9f2797e03e9
pkusensei <ZW_08@outlook.com>
2025-02-10 15:55:58 -0500

    1866 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,42 +2,41 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct FindSumPairs {
-    nums1: Vec<i32>,
-    nums2: Vec<i32>,
-    map: HashMap<i32, i32>,
+pub fn rearrange_sticks(n: i32, k: i32) -> i32 {
+    let [n, k] = [n, k].map(|v| v as usize);
+    let mut dp = vec![vec![0; 1 + k]; 1 + n];
+    dp[0][0] = 1;
+    for i1 in 1..=n {
+        for i2 in 1..=i1.min(k) {
+            dp[i1][i2] = (dp[i1 - 1][i2 - 1] + (i1 as i64 - 1) * dp[i1 - 1][i2]) % MOD;
+        }
+    }
+    dp[n][k] as _
+    // dfs(n, k, &mut vec![vec![-1; 1 + k]; 1 + n]) as _
 }
 
-impl FindSumPairs {
-    fn new(nums1: Vec<i32>, nums2: Vec<i32>) -> Self {
-        let map = nums2.iter().fold(HashMap::new(), |mut acc, &num| {
-            *acc.entry(num).or_insert(0) += 1;
-            acc
-        });
-        Self { nums1, nums2, map }
-    }
+const MOD: i64 = 1_000_000_007;
 
-    fn add(&mut self, index: i32, val: i32) {
-        let idx = index as usize;
-        self.map.entry(self.nums2[idx]).and_modify(|v| *v -= 1);
-        self.nums2[idx] += val;
-        *self.map.entry(self.nums2[idx]).or_insert(0) += 1;
+fn dfs(n: usize, k: usize, memo: &mut [Vec<i64>]) -> i64 {
+    if k == 0 {
+        return 0;
     }
-
-    fn count(&self, tot: i32) -> i32 {
-        let mut res = 0;
-        for &n1 in self.nums1.iter() {
-            if let Some(v) = self.map.get(&(tot - n1)) {
-                res += v;
-            }
-        }
-        res
+    if n <= k {
+        return 1;
     }
+    if memo[n][k] > -1 {
+        return memo[n][k];
+    }
+    // (n-1, k-1) => (n, k) => Put nth stick at last
+    let mut res = dfs(n - 1, k - 1, memo);
+    // Put any other stick at last
+    res += (n as i64 - 1) * dfs(n - 1, k, memo) % MOD;
+    res %= MOD;
+    memo[n][k] = res;
+    res
 }
 
 #[cfg(test)]
@@ -70,7 +69,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(rearrange_sticks(3, 2), 3);
+        assert_eq!(rearrange_sticks(5, 5), 1);
+        assert_eq!(rearrange_sticks(20, 11), 647427950);
+    }
 
     #[test]
     fn test() {}
