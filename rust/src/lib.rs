78f372b2d77f5292a42786a075636e16d8353508
pkusensei <ZW_08@outlook.com>
2025-02-11 18:57:40 -0500

    1884 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,53 +2,41 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_skips(dist: &[i32], speed: i32, hours_before: i32) -> i32 {
-    let n = dist.len();
-    let mut dp = vec![vec![f64::INFINITY; 1 + n]; 1 + n];
-    dp[0][0] = 0.0;
-    for idx in 1..=n {
-        for k in 0..=idx {
-            let time = f64::from(dist[idx - 1]) / f64::from(speed);
-            if k < idx {
-                dp[idx][k] = (dp[idx - 1][k] + time - 1e-9).ceil(); // take rest
-            }
-            if k > 0 {
-                dp[idx][k] = dp[idx][k].min(dp[idx - 1][k - 1] + time); // skip rest
-            }
-        }
+pub fn two_egg_drop(n: i32) -> i32 {
+    let mut res = 1;
+    while res * (res + 1) / 2 < n {
+        res += 1;
     }
-    for k in 0..=n {
-        if dp[n][k] <= f64::from(hours_before) {
-            return k as _;
-        }
-    }
-    // let speed = i64::from(speed);
-    // let mut memo = vec![vec![-1; 1 + n]; n];
-    // for k in 0..=n {
-    //     if dfs(dist, speed, n as i32 - 1, k, &mut memo) <= i64::from(hours_before) * speed {
-    //         return k as _;
-    //     }
-    // }
-    -1
+    res
+    // dfs(n, 2, &mut HashMap::new())
 }
 
-fn dfs(dist: &[i32], speed: i64, idx: i32, k: usize, memo: &mut [Vec<i64>]) -> i64 {
-    if idx < 0 {
-        return 0;
+fn dfs(floors: i32, eggs: i32, memo: &mut HashMap<[i32; 2], i32>) -> i32 {
+    if floors <= 1 {
+        return floors;
     }
-    if memo[idx as usize][k] > -1 {
-        return memo[idx as usize][k];
+    if eggs <= 1 {
+        return floors;
     }
-    let mut res = (dfs(dist, speed, idx - 1, k, memo) + i64::from(dist[idx as usize]) + speed - 1)
-        / speed
-        * speed;
-    if k > 0 {
-        res = res.min(dfs(dist, speed, idx - 1, k - 1, memo) + i64::from(dist[idx as usize]));
+    if let Some(&v) = memo.get(&[floors, eggs]) {
+        return v;
     }
-    memo[idx as usize][k] = res;
+    let mut res = i32::MAX;
+    for f in 1..=floors {
+        // For each floor between 0 and floors
+        // The max of
+        // (0..f) one egg broken
+        // (f..floors) no egg broken
+        let curr = dfs(f - 1, eggs - 1, memo).max(dfs(floors - f, eggs, memo));
+        res = res.min(curr);
+    }
+    res += 1;
+    memo.insert([floors, eggs], res);
     res
 }
 
@@ -83,9 +71,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_skips(&[1, 3, 2], 4, 2), 1);
-        assert_eq!(min_skips(&[7, 3, 5, 5], 2, 10), 2);
-        assert_eq!(min_skips(&[7, 3, 5, 5], 1, 10), -1);
+        assert_eq!(two_egg_drop(2), 2);
+        assert_eq!(two_egg_drop(100), 14);
     }
 
     #[test]
