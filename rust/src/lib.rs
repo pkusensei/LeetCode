141f57a0d45d0688e0aa268ede994626492f4abb
pkusensei <ZW_08@outlook.com>
2025-02-10 19:22:33 -0500

    1871 sliding window + dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,31 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_speed_on_time(dist: &[i32], hour: f64) -> i32 {
-    let mut left = 1;
-    let mut right = 10_000_000;
-    if count(dist, right) > hour {
-        return -1;
-    }
-    while left < right {
-        let mid = left + (right - left) / 2;
-        if count(dist, mid) <= hour {
-            right = mid;
-        } else {
-            left = 1 + mid;
+pub fn can_reach(s: &str, min_jump: i32, max_jump: i32) -> bool {
+    let (s, n) = (s.as_bytes(), s.len());
+    let [minj, maxj] = [min_jump, max_jump].map(|v| v as usize);
+    let mut dp = vec![false; n];
+    dp[0] = true;
+    let mut count = 0;
+    // sliding window [i-maxj..i-minj]
+    for idx in minj..n {
+        if dp[idx - minj] {
+            count += 1;
         }
+        if idx > maxj && dp[idx - maxj - 1] {
+            count -= 1;
+        }
+        dp[idx] = count > 0 && s[idx] == b'0';
     }
-    left
-}
-
-fn count(dist: &[i32], mid: i32) -> f64 {
-    let n = dist.len();
-    let mut res = 0.0;
-    let v = f64::from(mid);
-    for d in dist.iter().take(n - 1) {
-        res += (f64::from(*d) / v).ceil();
-    }
-    res + f64::from(dist[n - 1]) / v
+    dp[n - 1]
 }
 
 #[cfg(test)]
@@ -63,9 +55,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_speed_on_time(&[1, 3, 2], 6.0), 1);
-        assert_eq!(min_speed_on_time(&[1, 3, 2], 2.7), 3);
-        assert_eq!(min_speed_on_time(&[1, 3, 2], 1.9), -1);
+        assert!(can_reach("011010", 2, 3));
+        assert!(!can_reach("01101110", 2, 3));
     }
 
     #[test]
