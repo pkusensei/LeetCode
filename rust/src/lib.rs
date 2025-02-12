79c6f4b4685f03e6453bed0b67908ab69eb317bd
pkusensei <ZW_08@outlook.com>
2025-02-12 17:50:30 -0500

    1899

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,40 +2,31 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_removals(s: &str, p: &str, removable: &[i32]) -> i32 {
-    let mut left = 0;
-    let mut right = removable.len();
-    while left < right {
-        let mid = left + (right + 1 - left) / 2;
-        let del: HashSet<_> = removable[..mid].iter().copied().collect();
-        if is_subseq(s.as_bytes(), p.as_bytes(), &del) {
-            left = mid
-        } else {
-            right = mid - 1;
+pub fn merge_triplets(triplets: &[[i32; 3]], target: [i32; 3]) -> bool {
+    let del: std::collections::HashSet<_> = triplets
+        .iter()
+        .enumerate()
+        .filter_map(|(i, v)| {
+            if v.iter().zip(target.iter()).any(|(a, b)| *a > *b) {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let n = triplets.len();
+    for (i, &num) in target.iter().enumerate() {
+        if (0..n)
+            .filter(|v| !del.contains(v))
+            .all(|v| triplets[v][i] != num)
+        {
+            return false;
         }
     }
-    left as i32
-}
-
-fn is_subseq(hay: &[u8], needle: &[u8], del: &HashSet<i32>) -> bool {
-    let (n1, n2) = (hay.len(), needle.len());
-    let [mut i1, mut i2] = [0, 0];
-    while i1 < n1 && i2 < n2 {
-        if del.contains(&(i1 as i32)) {
-            i1 += 1;
-            continue;
-        }
-        if i1 < n1 && hay[i1] == needle[i2] {
-            i2 += 1;
-        }
-        i1 += 1;
-    }
-    i2 == n2
+    true
 }
 
 #[cfg(test)]
@@ -69,12 +60,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(maximum_removals("abcacb", "ab", &[3, 1, 0]), 2);
-        assert_eq!(
-            maximum_removals("abcbddddd", "abcd", &[3, 2, 1, 4, 5, 6]),
-            1
-        );
-        assert_eq!(maximum_removals("abcab", "abc", &[0, 1, 2, 3, 4]), 0);
+        assert!(merge_triplets(
+            &[[2, 5, 3], [1, 8, 4], [1, 7, 5]],
+            [2, 7, 5]
+        ));
+        assert!(!merge_triplets(&[[3, 4, 5], [4, 5, 6]], [3, 2, 5]));
+        assert!(merge_triplets(
+            &[[2, 5, 3], [2, 3, 4], [1, 2, 5], [5, 2, 3]],
+            [5, 5, 5]
+        ));
     }
 
     #[test]
