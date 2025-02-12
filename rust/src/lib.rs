91e3ae6355dffa77e8f2ea40bfde0cf15d9bfaf9
pkusensei <ZW_08@outlook.com>
2025-02-12 16:25:18 -0500

    1897 double fold

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,53 +2,18 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{
-    collections::HashMap,
-    ops::{BitAnd, BitOr},
-};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations_to_flip(expression: &str) -> i32 {
-    let mut parens = HashMap::new();
-    let mut stack = vec![];
-    for (idx, b) in expression.bytes().enumerate() {
-        if b == b'(' {
-            stack.push(idx);
-        } else if b == b')' {
-            parens.insert(idx, stack.pop().unwrap());
-        }
-    }
-    let n = expression.len();
-    dfs(expression.as_bytes(), &parens, 0, n - 1)[1]
-}
-
-// [value of exp, cost to flip]
-fn dfs(s: &[u8], parens: &HashMap<usize, usize>, left: usize, right: usize) -> [i32; 2] {
-    if left == right {
-        return [i32::from(s[left] - b'0'), 1];
-    }
-    let mid = *parens.get(&right).unwrap_or(&right); // default to single digit
-    if left == mid {
-        return dfs(s, parens, left + 1, right - 1); // discard ()
-    }
-    let [val1, cost1] = dfs(s, parens, left, mid - 2); // exp before operator
-    let [val2, cost2] = dfs(s, parens, mid, right);
-    let func: fn(i32, i32) -> i32 = if s[mid - 1] == b'|' {
-        i32::bitor
-    } else {
-        i32::bitand
-    };
-    let cost = if val1 + val2 == 1 {
-        1
-        // 1|0 and 0|1 and 1&0 and 0&1 => flip the op
-    } else {
-        // 0|0 and 1&1 => flip either side
-        // 0&0 and 1|1 => flip either side + flip the op
-        cost1.min(cost2) + (val1 ^ i32::from(s[mid - 1] == b'&'))
-    };
-    [func(val1, val2), cost]
+pub fn make_equal(words: Vec<String>) -> bool {
+    let n = words.len();
+    let count = words.iter().fold([0; 26], |acc, s| {
+        s.bytes().fold(acc, |mut acc, b| {
+            acc[usize::from(b - b'a')] += 1;
+            acc
+        })
+    });
+    count.into_iter().all(|v| v % n as i32 == 0)
 }
 
 #[cfg(test)]
@@ -81,11 +46,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_operations_to_flip("1&(0|1)"), 1);
-        assert_eq!(min_operations_to_flip("(0&0)&(0&0&0)"), 3);
-        assert_eq!(min_operations_to_flip("(0|(1|0&1))"), 1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
