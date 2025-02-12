4d1663eda777551961c55f82611196df285915e3
pkusensei <ZW_08@outlook.com>
2025-02-12 08:32:55 -0500

    1886 rotate matrix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,32 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_sum(nums: Vec<i32>) -> i32 {
-    nums.iter()
-        .fold(
-            std::collections::HashMap::<_, Vec<_>>::new(),
-            |mut acc, &n| {
-                let mut curr = 0;
-                let mut num = n;
-                while num > 0 {
-                    curr += num % 10;
-                    num /= 10;
-                }
-                acc.entry(curr).or_default().push(n);
-                acc
-            },
-        )
-        .into_values()
-        .filter_map(|mut v| {
-            if v.len() > 1 {
-                v.sort_unstable_by(|a, b| b.cmp(a));
-                Some(v[0] + v[1])
-            } else {
-                None
+pub fn find_rotation(mut mat: Vec<Vec<i32>>, target: Vec<Vec<i32>>) -> bool {
+    let n = mat.len();
+    for _ in 0..4 {
+        mat.reverse();
+        for r in 0..n {
+            for c in 0..r {
+                (mat[r][c], mat[c][r]) = (mat[c][r], mat[r][c]);
             }
-        })
-        .max()
-        .unwrap_or(-1)
+        }
+        if mat == target {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
