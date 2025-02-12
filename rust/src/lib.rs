9e9e6e57e09c750241060884696bb9d18c7b55e4
pkusensei <ZW_08@outlook.com>
2025-02-11 19:10:14 -0500

    2342

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,42 +2,35 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn two_egg_drop(n: i32) -> i32 {
-    let mut res = 1;
-    while res * (res + 1) / 2 < n {
-        res += 1;
-    }
-    res
-    // dfs(n, 2, &mut HashMap::new())
-}
-
-fn dfs(floors: i32, eggs: i32, memo: &mut HashMap<[i32; 2], i32>) -> i32 {
-    if floors <= 1 {
-        return floors;
-    }
-    if eggs <= 1 {
-        return floors;
-    }
-    if let Some(&v) = memo.get(&[floors, eggs]) {
-        return v;
-    }
-    let mut res = i32::MAX;
-    for f in 1..=floors {
-        // For each floor between 0 and floors
-        // The max of
-        // (0..f) one egg broken
-        // (f..floors) no egg broken
-        let curr = dfs(f - 1, eggs - 1, memo).max(dfs(floors - f, eggs, memo));
-        res = res.min(curr);
-    }
-    res += 1;
-    memo.insert([floors, eggs], res);
-    res
+pub fn maximum_sum(nums: Vec<i32>) -> i32 {
+    nums.iter()
+        .fold(
+            std::collections::HashMap::<_, Vec<_>>::new(),
+            |mut acc, &n| {
+                let mut curr = 0;
+                let mut num = n;
+                while num > 0 {
+                    curr += num % 10;
+                    num /= 10;
+                }
+                acc.entry(curr).or_default().push(n);
+                acc
+            },
+        )
+        .into_values()
+        .filter_map(|mut v| {
+            if v.len() > 1 {
+                v.sort_unstable_by(|a, b| b.cmp(a));
+                Some(v[0] + v[1])
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap_or(-1)
 }
 
 #[cfg(test)]
@@ -70,10 +63,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(two_egg_drop(2), 2);
-        assert_eq!(two_egg_drop(100), 14);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
