260cf64afb66a7a67f689dd3d10241daa094d227
pkusensei <ZW_08@outlook.com>
2025-02-12 12:22:28 -0500

    1893 line sweep

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,46 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_wasted_space(packages: Vec<i32>, mut boxes: Vec<Vec<i32>>) -> i32 {
-    const MAX: usize = 100_000;
-    let mut prefix = packages.iter().fold(vec![0; 1 + MAX], |mut acc, &p| {
-        acc[p as usize] += 1;
-        acc
-    });
-    for i in 1..=MAX {
+pub fn is_covered(ranges: Vec<Vec<i32>>, left: i32, right: i32) -> bool {
+    let mut prefix = vec![0; 1 + right as usize];
+    for v in ranges.iter() {
+        prefix[v[0] as usize] += 1;
+        prefix[1 + v[1] as usize] -= 1;
+    }
+    for i in 1..=right as usize {
         prefix[i] += prefix[i - 1];
     }
-    let sum: i64 = packages.iter().map(|&v| i64::from(v)).sum();
-    let mut res = i64::MAX;
-    for b in boxes.iter_mut() {
-        if let Some(v) = fit(&prefix, b) {
-            res = res.min(v)
-        }
-    }
-    if res == i64::MAX {
-        -1
-    } else {
-        ((res - sum) % 1_000_000_007) as _
-    }
-}
-
-fn fit(prefix: &[i32], boxes: &mut [i32]) -> Option<i64> {
-    boxes.sort_unstable();
-    let mut count = *prefix.last().unwrap();
-    let mut res = 0;
-    for (idx, &right) in boxes.iter().enumerate() {
-        let left = if idx == 0 { 0 } else { boxes[idx - 1] };
-        let window_count = prefix[right as usize] - prefix[left as usize];
-        if window_count > 0 {
-            res += i64::from(right) * i64::from(window_count);
-            count -= window_count;
-        }
-    }
-    if count == 0 {
-        Some(res)
-    } else {
-        None
-    }
+    (left..=right).all(|v| prefix[v as usize] > 0)
 }
 
 #[cfg(test)]
@@ -77,23 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            min_wasted_space(vec![2, 3, 5], vec![vec![4, 8], vec![2, 8]]),
-            6
-        );
-        assert_eq!(
-            min_wasted_space(vec![2, 3, 5], vec![vec![1, 4], vec![2, 3], vec![3, 4]]),
-            -1
-        );
-        assert_eq!(
-            min_wasted_space(
-                vec![3, 5, 8, 10, 11, 12],
-                vec![vec![12], vec![11, 9], vec![10, 5, 14]]
-            ),
-            9
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
