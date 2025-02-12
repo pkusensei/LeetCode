149c6cd34706f2ab7de24a05148457dc695c2580
pkusensei <ZW_08@outlook.com>
2025-02-12 12:10:40 -0500

    1889 prefix sum on counts

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,26 +5,46 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_flips(s: &str) -> i32 {
-    let mut res = i32::MAX;
-    let [mut zero_even, mut zero_odd] = [0, 0];
-    let n = s.len();
-    // Slide a window with length n in total 2n array
-    for (idx, b) in s.bytes().cycle().enumerate().take(2 * n) {
-        // Try fit all zeros onto even idx, ones onto odd idx
-        zero_even += i32::from(b != (b'0' + (idx & 1) as u8));
-        // Try fit all ones onto even idx, zeros onto odd idx
-        zero_odd += i32::from(b != (b'0' + 1 - (idx & 1) as u8));
-        if idx >= n {
-            let left = idx - n; // droppped out of window
-            zero_even -= i32::from(s.as_bytes()[left] != (b'0' + (left & 1) as u8));
-            zero_odd -= i32::from(s.as_bytes()[left] != (b'0' + 1 - (left & 1) as u8));
-        }
-        if idx >= n - 1 {
-            res = res.min(zero_even).min(zero_odd);
+pub fn min_wasted_space(packages: Vec<i32>, mut boxes: Vec<Vec<i32>>) -> i32 {
+    const MAX: usize = 100_000;
+    let mut prefix = packages.iter().fold(vec![0; 1 + MAX], |mut acc, &p| {
+        acc[p as usize] += 1;
+        acc
+    });
+    for i in 1..=MAX {
+        prefix[i] += prefix[i - 1];
+    }
+    let sum: i64 = packages.iter().map(|&v| i64::from(v)).sum();
+    let mut res = i64::MAX;
+    for b in boxes.iter_mut() {
+        if let Some(v) = fit(&prefix, b) {
+            res = res.min(v)
         }
     }
-    res
+    if res == i64::MAX {
+        -1
+    } else {
+        ((res - sum) % 1_000_000_007) as _
+    }
+}
+
+fn fit(prefix: &[i32], boxes: &mut [i32]) -> Option<i64> {
+    boxes.sort_unstable();
+    let mut count = *prefix.last().unwrap();
+    let mut res = 0;
+    for (idx, &right) in boxes.iter().enumerate() {
+        let left = if idx == 0 { 0 } else { boxes[idx - 1] };
+        let window_count = prefix[right as usize] - prefix[left as usize];
+        if window_count > 0 {
+            res += i64::from(right) * i64::from(window_count);
+            count -= window_count;
+        }
+    }
+    if count == 0 {
+        Some(res)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -58,13 +78,23 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_flips("111000"), 2);
-        assert_eq!(min_flips("010"), 0);
-        assert_eq!(min_flips("1110"), 1);
+        assert_eq!(
+            min_wasted_space(vec![2, 3, 5], vec![vec![4, 8], vec![2, 8]]),
+            6
+        );
+        assert_eq!(
+            min_wasted_space(vec![2, 3, 5], vec![vec![1, 4], vec![2, 3], vec![3, 4]]),
+            -1
+        );
+        assert_eq!(
+            min_wasted_space(
+                vec![3, 5, 8, 10, 11, 12],
+                vec![vec![12], vec![11, 9], vec![10, 5, 14]]
+            ),
+            9
+        );
     }
 
     #[test]
-    fn test() {
-        assert_eq!(min_flips("01001001101"), 2);
-    }
+    fn test() {}
 }
