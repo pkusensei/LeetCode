39804158fffaa207d5c0a97368457621ecf8cfa6
pkusensei <ZW_08@outlook.com>
2025-02-12 19:57:56 -0500

    1900 ridiculous

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,21 +2,41 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-    let mut heap: BinaryHeap<_> = nums.iter().map(|&v| Reverse(v as i64)).collect();
-    let mut res = 0;
-    while heap.len() > 1 && heap.peek().is_some_and(|&Reverse(v)| v < k as i64) {
-        let x = heap.pop().unwrap().0;
-        let y = heap.pop().unwrap().0;
-        heap.push(Reverse(2 * x + y));
-        res += 1;
+pub fn earliest_and_latest(n: i32, first_player: i32, second_player: i32) -> Vec<i32> {
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    dfs(
+        n,
+        first_player,
+        n + 1 - second_player,
+        1,
+        &mut min,
+        &mut max,
+    );
+    vec![min, max]
+}
+
+fn dfs(n: i32, mut left: i32, mut right: i32, round: i32, min: &mut i32, max: &mut i32) {
+    if left == right {
+        *min = (*min).min(round);
+        *max = (*max).max(round);
+        return;
     }
-    res
+    if left > right {
+        std::mem::swap(&mut left, &mut right);
+    }
+    for new_left in 1..=left {
+        let mut new_right = left + 1 - new_left;
+        while new_left + new_right <= right.min((1 + n) / 2) {
+            if left + right - new_left - new_right <= n / 2 {
+                dfs((1 + n) / 2, new_left, new_right, 1 + round, min, max);
+            }
+            new_right += 1;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -49,7 +69,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(earliest_and_latest(11, 2, 4), [3, 4]);
+        assert_eq!(earliest_and_latest(5, 1, 5), [1, 1]);
+    }
 
     #[test]
     fn test() {}
