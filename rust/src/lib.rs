c22ee54ada1775dcc49ea357599aa7a26f7ea145
Zx Wang <ZW_08@outlook.com>
2025-02-13 14:10:46 -0500

    1913

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,63 +2,27 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{BTreeSet, HashMap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct MovieRentingSystem {
-    // movie -> [price, shop]
-    data: HashMap<i32, BTreeSet<[i32; 2]>>,
-    // [shop, movie] -> price
-    prices: HashMap<[i32; 2], i32>,
-    // [price, shop, movie]
-    out: BTreeSet<[i32; 3]>,
-}
-
-impl MovieRentingSystem {
-    fn new(_n: i32, entries: Vec<Vec<i32>>) -> Self {
-        let mut data: HashMap<i32, BTreeSet<[i32; 2]>> = HashMap::new();
-        let mut prices = HashMap::new();
-        for e in entries.into_iter() {
-            let [shop, movie, price] = e[..] else {
-                unreachable!()
-            };
-            data.entry(movie).or_default().insert([price, shop]);
-            prices.insert([shop, movie], price);
+pub fn max_product_difference(nums: Vec<i32>) -> i32 {
+    let [mut max1, mut max2] = [i32::MIN; 2];
+    let [mut min1, mut min2] = [i32::MAX; 2];
+    for &num in nums.iter() {
+        if num > max1 {
+            max2 = max1;
+            max1 = num;
+        } else if num > max2 {
+            max2 = num
         }
-        Self {
-            data,
-            prices,
-            out: BTreeSet::new(),
+        if num < min1 {
+            min2 = min1;
+            min1 = num
+        } else if num < min2 {
+            min2 = num
         }
     }
-
-    fn search(&self, movie: i32) -> Vec<i32> {
-        self.data
-            .get(&movie)
-            .map(|set| set.iter().map(|&[_p, s]| s).take(5).collect())
-            .unwrap_or_default()
-    }
-
-    fn rent(&mut self, shop: i32, movie: i32) {
-        let Some(set) = self.data.get_mut(&movie) else {
-            return;
-        };
-        let price = self.prices[&[shop, movie]];
-        set.remove(&[price, shop]);
-        self.out.insert([price, shop, movie]);
-    }
-
-    fn drop(&mut self, shop: i32, movie: i32) {
-        let price = self.prices[&[shop, movie]];
-        self.out.remove(&[price, shop, movie]);
-        self.data.entry(movie).or_default().insert([price, shop]);
-    }
-
-    fn report(&self) -> Vec<Vec<i32>> {
-        self.out.iter().map(|v| vec![v[1], v[2]]).take(5).collect()
-    }
+    max1 * max2 - min1 * min2
 }
 
 #[cfg(test)]
