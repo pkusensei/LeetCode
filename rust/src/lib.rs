e5d3435f5f2f02483a109390fb1db0e65ffa8143
pkusensei <ZW_08@outlook.com>
2025-02-14 10:33:39 -0500

    1920

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,35 +5,9 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn ways_to_build_rooms(prev_room: &[i32]) -> i32 {
-    let n = prev_room.len();
-    let adj =
-        prev_room
-            .iter()
-            .enumerate()
-            .skip(1)
-            .fold(vec![vec![]; n], |mut acc, (prev, &node)| {
-                acc[node as usize].push(prev);
-                acc
-            });
-    let mut size = vec![0; n];
-    dfs(&adj, &mut size, 0);
-    let prod = size.iter().fold(1, |acc, &num| acc * i64::from(num) % MOD);
-    let fact = (1..=n).fold(1, |acc, num| acc * num as i64 % MOD);
-    let inverse = mod_pow(prod, MOD - 2, MOD);
-    (inverse * fact % MOD) as i32
-}
-
-const MOD: i64 = 1_000_000_007;
-
-// size of substree
-fn dfs(adj: &[Vec<usize>], size: &mut [i32], curr: usize) -> i32 {
-    let res = 1 + adj[curr]
-        .iter()
-        .map(|&next| dfs(adj, size, next))
-        .sum::<i32>();
-    size[curr] = res;
-    res
+pub fn build_array(nums: &[i32]) -> Vec<i32> {
+    let n = nums.len();
+    (0..n).map(|i| nums[i] as usize).map(|i| nums[i]).collect()
 }
 
 #[cfg(test)]
@@ -66,10 +40,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(ways_to_build_rooms(&[-1, 0, 1]), 1);
-        assert_eq!(ways_to_build_rooms(&[-1, 0, 0, 1, 2]), 6);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
