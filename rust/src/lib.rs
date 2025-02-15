963c94f19d97b012fa001f6525f80c72dc48e99e
pkusensei <ZW_08@outlook.com>
2025-02-15 17:53:25 -0500

    1931 dfs+memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,28 +5,48 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_palindromic_subsequence(s: &str) -> i32 {
-    let mut prefix = [None; 26];
-    let mut suffix = [0; 26];
-    for (idx, b) in s.bytes().enumerate() {
-        let curr = usize::from(b - b'a');
-        prefix[curr].get_or_insert(idx);
-        suffix[curr] = idx;
+pub fn color_the_grid(m: i32, n: i32) -> i32 {
+    dfs(
+        m,
+        n,
+        0,
+        0,
+        0,
+        0,
+        &mut vec![vec![-1; 1 << (2 * m)]; n as usize],
+    )
+}
+
+fn dfs(m: i32, n: i32, prev_col: i32, row: i32, col: i32, mask: i32, memo: &mut [Vec<i32>]) -> i32 {
+    if col == n {
+        return 1;
     }
+    if row == m {
+        return dfs(m, n, mask, 0, 1 + col, 0, memo);
+    }
+    if row == 0 && memo[col as usize][prev_col as usize] > -1 {
+        // row==0 => starting a new col
+        // But this prev_mask => col combo has been seen
+        return memo[col as usize][prev_col as usize];
+    }
+    // prev_mask  01 01 01 01 01
+    // row number 0  1  2  3  4
+    let left = (prev_col >> ((m - row - 1) * 2)) & 0b11;
+    let up = if row == 0 { 0 } else { mask & 0b11 };
     let mut res = 0;
-    for (left, right) in prefix
-        .into_iter()
-        .zip(suffix)
-        .filter_map(|(left, right)| left.map(|v| (v, right)))
-    {
-        if left < right {
-            res += s[1 + left..right]
-                .bytes()
-                .collect::<std::collections::HashSet<_>>()
-                .len();
+    for color in 1..=3 {
+        if color != left && color != up {
+            res += dfs(m, n, prev_col, 1 + row, col, (mask << 2) | color, memo);
+            res %= 1_000_000_007;
         }
     }
-    res as _
+    if row == 0 {
+        // Recursion works backwards, i.e
+        // Row m-1 is calculated first, then m-2, .. down to 0
+        // When row==0, this whole col is done.
+        memo[col as usize][prev_col as usize] = res;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -60,9 +80,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(count_palindromic_subsequence("aabca"), 3);
-        assert_eq!(count_palindromic_subsequence("adc"), 0);
-        assert_eq!(count_palindromic_subsequence("bbcbaba"), 4);
+        assert_eq!(color_the_grid(1, 1), 3);
+        assert_eq!(color_the_grid(1, 2), 6);
+        assert_eq!(color_the_grid(5, 5), 580986);
     }
 
     #[test]
