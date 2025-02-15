c6007592c024d7e3407d539b0498950a2de7b763
pkusensei <ZW_08@outlook.com>
2025-02-15 18:52:33 -0500

    1932 merge BSTs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -4,23 +4,64 @@ using Solution.Tree;
 
 namespace Solution;
 
-public class ProductOfNumbers
+public class Solution
 {
-    List<int> Nums { get; }
-    public ProductOfNumbers() => Nums = [];
-
-    public void Add(int num)
+    public TreeNode CanMerge(IList<TreeNode> trees)
     {
-        if (num == 0) { Nums.Clear(); }
-        else if (Nums.LastOrDefault() == 0) { Nums.Add(num); }
-        else { Nums.Add(num * Nums.Last()); }
-    }
+        Dictionary<int, TreeNode> val_roots = [];
+        HashSet<int> non_roots = [];
+        // Find all root_val-root and non root vals
+        foreach (var (i, tree) in trees.Select((t, i) => (i, t)))
+        {
+            val_roots.Add(tree.val, tree);
+            var leaves = CollectVals(tree);
+            leaves.RemoveAt(leaves.Count - 1);
+            non_roots.UnionWith(leaves);
+        }
+        var roots = val_roots.Keys.Where(k => !non_roots.Contains(k))
+            .Select(k => val_roots[k]).Take(2).ToList();
+        if (roots.Count != 1) { return null; }
+        var root = roots.Single();
+        TreeNode dummy = new(0, root);
+        HashSet<int> used_roots = [root.val];
+        root = Merge(root, dummy, false);
+        var all_vals = CollectVals(root); // All vals must be merged
+        if (Check(root, 0, 50_001) && all_vals.Count == non_roots.Count + 1) { return root; }
+        return null;
 
-    public int GetProduct(int k)
-    {
-        var i = Nums.Count - k;
-        if (i < 0) { return 0; }
-        if (i == 0) { return Nums.Last(); }
-        return Nums.Last() / Nums[i - 1];
+        static bool Check(TreeNode node, int min, int max)
+        {
+            if (node is null) { return true; }
+            if (node.val <= min || node.val >= max) { return false; }
+            return Check(node.left, min, node.val) && Check(node.right, node.val, max);
+        }
+
+        TreeNode Merge(TreeNode node, TreeNode prev, bool is_left)
+        {
+            if (node is null) { return null; }
+            if (node.left is null && node.right is null)
+            {
+                if (val_roots.TryGetValue(node.val, out var new_root) && used_roots.Add(node.val))
+                {
+                    node = new_root;
+                    if (is_left) { prev.left = new_root; }
+                    else { prev.right = new_root; }
+                }
+            }
+            var left = Merge(node.left, node, true);
+            var right = Merge(node.right, node, false);
+            node.left = left;
+            node.right = right;
+            return node;
+        }
+
+        static List<int> CollectVals(TreeNode node)
+        {
+            if (node is null) { return []; }
+            var res = CollectVals(node.left);
+            res.AddRange(CollectVals(node.right));
+            res.Add(node.val); // Post-order
+            return res;
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,20 +12,32 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
+        List<List<int?>> arrs = [[2, 1], [3, 2, 5], [5, 4]];
+        var trees = arrs.Select(nums => TreeNode.Make(nums)).ToList();
+        Assert.AreEqual("[3,2,5,1,null,4]", sol.CanMerge(trees).ToString());
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        List<List<int?>> arrs = [[5, 3, 8], [3, 2, 6]];
+        var trees = arrs.Select(nums => TreeNode.Make(nums)).ToList();
+        Assert.IsTrue(sol.CanMerge(trees) is null);
     }
 
     [TestMethod]
     public void TestMethod3()
     {
+        List<List<int?>> arrs = [[5, 4], [3]];
+        var trees = arrs.Select(nums => TreeNode.Make(nums)).ToList();
+        Assert.IsTrue(sol.CanMerge(trees) is null);
     }
 
     [TestMethod]
     public void TestMethod4()
     {
+        List<List<int?>> arrs = [[1, null, 3], [3, 1], [4, 2]];
+        var trees = arrs.Select(nums => TreeNode.Make(nums)).ToList();
+        Assert.IsTrue(sol.CanMerge(trees) is null);
     }
 }
