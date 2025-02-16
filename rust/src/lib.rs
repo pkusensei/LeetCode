54cbc06d876ce3fbaf98a87ce64f1e407d3e2449
pkusensei <ZW_08@outlook.com>
2025-02-16 17:36:14 -0500

    1952

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,44 +5,12 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_compatibility_sum(students: &[&[i32]], mentors: &[&[i32]]) -> i32 {
-    let [m, n] = get_dimensions(students);
-    let smasks: Vec<_> = students
-        .iter()
-        .map(|s| s.iter().fold(0, |acc, v| (acc << 1) | (v & 1)))
-        .collect();
-    let mmasks: Vec<_> = mentors
-        .iter()
-        .map(|m| m.iter().fold(0, |acc, v| (acc << 1) | (v & 1)))
-        .collect();
-    let mut memo = vec![vec![-1; 1 << m]; m];
-    dfs(&smasks, &mmasks, n as i32, 0, 0, &mut memo)
-}
-
-fn dfs(
-    smasks: &[i32],
-    mmasks: &[i32],
-    n: i32,
-    idx: usize,
-    mask: usize,
-    memo: &mut [Vec<i32>],
-) -> i32 {
-    if idx >= mmasks.len() {
-        return 0;
-    }
-    if memo[idx][mask] > -1 {
-        return memo[idx][mask];
-    }
+pub fn is_three(n: i32) -> bool {
     let mut res = 0;
-    for (si, stu) in smasks.iter().enumerate() {
-        if mask & (1 << si) > 0 {
-            continue;
-        }
-        let curr: i32 = n - (stu ^ mmasks[idx]).count_ones() as i32;
-        res = res.max(curr + dfs(smasks, mmasks, n, 1 + idx, mask | (1 << si), memo));
+    for v in 1..=n {
+        res += i32::from(n % v == 0);
     }
-    memo[idx][mask] = res;
-    res
+    res == 3
 }
 
 #[cfg(test)]
@@ -75,19 +43,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            max_compatibility_sum(
-                &[&[1, 1, 0], &[1, 0, 1], &[0, 0, 1]],
-                &[&[1, 0, 0], &[0, 0, 1], &[1, 1, 0]]
-            ),
-            8
-        );
-        assert_eq!(
-            max_compatibility_sum(&[&[0, 0], &[0, 0], &[0, 0]], &[&[1, 1], &[1, 1], &[1, 1]]),
-            0
-        )
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
