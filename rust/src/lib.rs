6c5a68ac2910e9c436fb5be8fee0df4fd2e7cb85
pkusensei <ZW_08@outlook.com>
2025-02-17 17:20:14 -0500

    1963

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_stone_sum(piles: Vec<i32>, k: i32) -> i32 {
-    let mut heap = std::collections::BinaryHeap::from(piles);
-    for _ in 0..k {
-        let Some(v) = heap.pop() else {
-            break;
-        };
-        heap.push(v - v / 2);
+pub fn min_swaps(s: String) -> i32 {
+    let mut open = 0;
+    for b in s.bytes() {
+        if b == b'[' {
+            open += 1;
+        } else if open > 0 {
+            open -= 1;
+        }
     }
-    heap.into_iter().sum()
+    // +1: one swap removes 2 mismatches
+    // but in case of single/odd mismatch
+    (1 + open) / 2
 }
 
 #[cfg(test)]
