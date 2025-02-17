247e195b7ae6b4957a1d9f247eff1044e542133d
pkusensei <ZW_08@outlook.com>
2025-02-17 10:29:43 -0500

    1958

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_special_subsequences(nums: &[i32]) -> i32 {
-    const MOD: i64 = 1_000_000_007;
-    let [mut zero, mut one, mut two] = [0; 3];
-    for &num in nums.iter() {
-        match num {
-            0 => zero += zero + 1,  // seq ends on 0
-            1 => one += zero + one, // seq ends on 1
-            _ => two += one + two,  // seq ends on 2
+pub fn check_move(board: Vec<Vec<char>>, r_move: i32, c_move: i32, color: char) -> bool {
+    for [dr, dc] in ALL_DIRS {
+        for step in 1.. {
+            let nr = r_move + step * dr;
+            let nc = c_move + step * dc;
+            if (0..8).contains(&nr) && (0..8).contains(&nc) {
+                if board[nr as usize][nc as usize] == '.' {
+                    break;
+                }
+                if board[nr as usize][nc as usize] == color {
+                    if step > 1 {
+                        return true;
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
         }
-        zero %= MOD;
-        one %= MOD;
-        two %= MOD;
     }
-    two as _
+    false
 }
 
 #[cfg(test)]
@@ -51,11 +58,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_special_subsequences(&[0, 1, 2, 2]), 3);
-        assert_eq!(count_special_subsequences(&[2, 2, 0, 0]), 0);
-        assert_eq!(count_special_subsequences(&[0, 1, 2, 0, 1, 2]), 7);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
