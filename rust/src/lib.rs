57b50c3ee71a8ff00abfed2ab48878c0c86e40de
pkusensei <ZW_08@outlook.com>
2025-02-17 11:52:26 -0500

    1959 dfs/memo; try all subarrays

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,27 +5,35 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_move(board: Vec<Vec<char>>, r_move: i32, c_move: i32, color: char) -> bool {
-    for [dr, dc] in ALL_DIRS {
-        for step in 1.. {
-            let nr = r_move + step * dr;
-            let nc = c_move + step * dc;
-            if (0..8).contains(&nr) && (0..8).contains(&nc) {
-                if board[nr as usize][nc as usize] == '.' {
-                    break;
-                }
-                if board[nr as usize][nc as usize] == color {
-                    if step > 1 {
-                        return true;
-                    }
-                    break;
-                }
-            } else {
-                break;
-            }
+pub fn min_space_wasted_k_resizing(nums: &[i32], k: i32) -> i32 {
+    let (n, k) = (nums.len(), k as usize);
+    dfs(nums, 0, k, &mut vec![vec![-1; n]; 1 + k])
+}
+
+fn dfs(nums: &[i32], idx: usize, k: usize, memo: &mut [Vec<i32>]) -> i32 {
+    let n = nums.len();
+    if idx >= n {
+        return 0;
+    }
+    if memo[k][idx] > -1 {
+        return memo[k][idx];
+    }
+    let mut res = i32::MAX;
+    let mut max = 0;
+    let mut window_sum = 0;
+    for (next, &num) in nums[idx..].iter().enumerate() {
+        max = max.max(num);
+        window_sum += num;
+        let curr = max * (next + 1) as i32 - window_sum;
+        if k > 0 {
+            res = res.min(curr + dfs(nums, idx + next + 1, k - 1, memo));
         }
     }
-    false
+    if k == 0 {
+        res = max * (n - idx) as i32 - window_sum;
+    }
+    memo[k][idx] = res;
+    res
 }
 
 #[cfg(test)]
@@ -58,7 +66,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_space_wasted_k_resizing(&[10, 20], 0), 10);
+        assert_eq!(min_space_wasted_k_resizing(&[10, 20, 30], 1), 10);
+        assert_eq!(min_space_wasted_k_resizing(&[10, 20, 15, 30, 20], 2), 15);
+    }
 
     #[test]
     fn test() {}
