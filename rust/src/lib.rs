22744149aaa987113eedece552d0399127995556
Zx Wang <ZW_08@outlook.com>
2025-02-18 15:26:01 -0500

    1977 precompute longest common prefix to speed up dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,42 +2,78 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_paths(n: i32, roads: &[[i32; 3]]) -> i32 {
+pub fn number_of_combinations(num: &str) -> i32 {
     const MOD: i32 = 1_000_000_007;
-    let n = n as usize;
-    let adj = roads.iter().fold(vec![vec![]; n], |mut acc, road| {
-        let (a, b, t) = (road[0] as usize, road[1] as usize, i64::from(road[2]));
-        acc[a].push((b, t));
-        acc[b].push((a, t));
-        acc
-    });
-    let mut dists = vec![i64::MAX; n];
-    dists[0] = 0;
-    let mut dp = vec![0; n];
-    dp[0] = 1;
-    let mut heap = BinaryHeap::from([(Reverse(0), 0)]);
-    while let Some((Reverse(cost), node)) = heap.pop() {
-        if cost > dists[node] {
-            continue;
-        }
-        for &(next, t) in adj[node].iter() {
-            let nc = t + cost;
-            if nc < dists[next] {
-                dists[next] = nc;
-                heap.push((Reverse(nc), next));
-                dp[next] = dp[node]
-            } else if nc == dists[next] {
-                dp[next] += dp[node];
-                dp[next] %= MOD;
+    let (n, s) = (num.len(), num.as_bytes());
+    if s[0] == b'0' {
+        return 0;
+    }
+    // dfs(num.as_bytes(), 0, 1, &mut vec![vec![-1; n]; n])
+
+    // longest commmon prefix
+    let mut lcp = vec![vec![0; 1 + n]; 1 + n];
+    for i1 in (0..n).rev() {
+        for i2 in (0..n).rev() {
+            if s[i1] == s[i2] {
+                lcp[i1][i2] = 1 + lcp[1 + i1][1 + i2];
             }
         }
     }
-    dp[n - 1]
+    let mut dp = vec![vec![0; 1 + n]; 1 + n];
+    dp[0] = vec![1; 1 + n];
+    for idx in 1..n {
+        if s[idx] == b'0' {
+            continue;
+        }
+        let mut prev = idx.checked_sub(1);
+        let mut sum = 0;
+        for right in idx..n {
+            dp[idx][right] = sum;
+            if let Some(k) = prev {
+                if s[k] > b'0' {
+                    let len = lcp[k][idx];
+                    if len >= right + 1 - idx || s[k + len] < s[idx + len] {
+                        dp[idx][right] += dp[k][idx - 1];
+                        dp[idx][right] %= MOD;
+                    }
+                }
+                sum += dp[k][idx - 1];
+                sum %= MOD;
+                prev = k.checked_sub(1);
+            }
+        }
+    }
+    dp.into_iter().fold(0, |acc, v| (acc + v[n - 1]) % MOD)
+}
+
+// TLE's
+fn dfs(s: &[u8], i1: usize, len: usize, memo: &mut [Vec<i32>]) -> i32 {
+    let n = s.len();
+    if i1 >= n {
+        return 1;
+    }
+    if s[i1] == b'0' {
+        return 0;
+    }
+    if memo[i1][len] > -1 {
+        return memo[i1][len];
+    }
+    let mut res = 0;
+    for i2 in i1 + len..=n {
+        if let Some(prev) = i1.checked_sub(len) {
+            if len < (i2 - i1) || s[prev..i1] < s[i1..i2] {
+                res += dfs(s, i2, i2 - i1, memo);
+            }
+        } else {
+            res += dfs(s, i2, i2 - i1, memo);
+        }
+        res %= 1_000_000_007;
+    }
+    memo[i1][len] = res;
+    res
 }
 
 #[cfg(test)]
@@ -71,81 +107,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            count_paths(
-                7,
-                &[
-                    [0, 6, 7],
-                    [0, 1, 2],
-                    [1, 2, 3],
-                    [1, 3, 3],
-                    [6, 3, 3],
-                    [3, 5, 1],
-                    [6, 5, 1],
-                    [2, 5, 1],
-                    [0, 4, 5],
-                    [4, 6, 2]
-                ]
-            ),
-            4
-        );
-        assert_eq!(count_paths(2, &[[1, 0, 10]]), 1);
+        assert_eq!(number_of_combinations("327"), 2);
+        assert_eq!(number_of_combinations("094"), 0);
+        assert_eq!(number_of_combinations("0"), 0);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            count_paths(
-                12,
-                &[
-                    [1, 0, 2348],
-                    [2, 1, 2852],
-                    [2, 0, 5200],
-                    [3, 1, 12480],
-                    [2, 3, 9628],
-                    [4, 3, 7367],
-                    [4, 0, 22195],
-                    [5, 4, 5668],
-                    [1, 5, 25515],
-                    [0, 5, 27863],
-                    [6, 5, 836],
-                    [6, 0, 28699],
-                    [2, 6, 23499],
-                    [6, 3, 13871],
-                    [1, 6, 26351],
-                    [5, 7, 6229],
-                    [2, 7, 28892],
-                    [1, 7, 31744],
-                    [3, 7, 19264],
-                    [6, 7, 5393],
-                    [2, 8, 31998],
-                    [8, 7, 3106],
-                    [3, 8, 22370],
-                    [8, 4, 15003],
-                    [8, 6, 8499],
-                    [8, 5, 9335],
-                    [8, 9, 5258],
-                    [9, 2, 37256],
-                    [3, 9, 27628],
-                    [7, 9, 8364],
-                    [1, 9, 40108],
-                    [9, 5, 14593],
-                    [2, 10, 45922],
-                    [5, 10, 23259],
-                    [9, 10, 8666],
-                    [10, 0, 51122],
-                    [10, 3, 36294],
-                    [10, 4, 28927],
-                    [11, 4, 25190],
-                    [11, 9, 4929],
-                    [11, 8, 10187],
-                    [11, 6, 18686],
-                    [2, 11, 42185],
-                    [11, 3, 32557],
-                    [1, 11, 45037]
-                ]
-            ),
-            166
-        );
-    }
+    fn test() {}
 }
