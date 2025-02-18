eafe74c0bb1dd015547ac1731074faa8637a4ef1
Zx Wang <ZW_08@outlook.com>
2025-02-18 10:07:47 -0500

    1968 wiggle sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,13 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_obstacle_course_at_each_position(obstacles: &[i32]) -> Vec<i32> {
-    let n = obstacles.len();
-    let mut sorted = Vec::with_capacity(n); // LIS
-    let mut res = Vec::with_capacity(n);
-    for &num in obstacles {
-        let i = sorted.partition_point(|&v| v <= num);
-        res.push(1 + i as i32);
-        if i == sorted.len() {
-            sorted.push(num);
-        } else {
-            sorted[i] = num;
-        }
+pub fn rearrange_array(mut nums: Vec<i32>) -> Vec<i32> {
+    nums.sort_unstable();
+    let n = nums.len();
+    for idx in (1..n).step_by(2) {
+        nums.swap(idx - 1, idx);
     }
-    res
+    nums
 }
 
 #[cfg(test)]
@@ -51,20 +44,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            longest_obstacle_course_at_each_position(&[1, 2, 3, 2]),
-            [1, 2, 3, 3]
-        );
-        assert_eq!(
-            longest_obstacle_course_at_each_position(&[2, 2, 1]),
-            [1, 2, 1]
-        );
-        assert_eq!(
-            longest_obstacle_course_at_each_position(&[3, 1, 5, 6, 4, 2]),
-            [1, 1, 2, 3, 2, 2]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
