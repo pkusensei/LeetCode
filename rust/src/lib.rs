a3183144c090ef67d1b382bb4fc82a36295529dd
pkusensei <ZW_08@outlook.com>
2025-02-19 14:58:57 -0500

    1987 dp; append on previous ends

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,46 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_sessions(tasks: &[i32], session_time: i32) -> i32 {
-    let n = tasks.len();
-    dfs(
-        tasks,
-        session_time,
-        0,
-        0,
-        &mut vec![vec![-1; 1 << n]; session_time as usize],
-    )
-}
-
-fn dfs(tasks: &[i32], session_time: i32, mask: usize, curr: i32, memo: &mut [Vec<i32>]) -> i32 {
-    let n = tasks.len();
-    if mask.count_ones() as usize >= n {
-        return 0;
-    }
-    if memo[curr as usize][mask] > -1 {
-        return memo[curr as usize][mask];
-    }
-    let mut res = n as i32;
-    for idx in 0..n {
-        if mask & (1 << idx) > 0 {
-            continue;
+pub fn number_of_unique_good_subsequences(binary: &str) -> i32 {
+    const MOD: i32 = 1_000_000_007;
+    let [mut end0, mut end1] = [0, 0];
+    let mut zero = 0; // "0"
+    for b in binary.bytes() {
+        if b == b'0' {
+            end0 = (end0 + end1) % MOD;
+            zero = 1;
+        } else {
+            end1 = (1 + end0 + end1) % MOD;
         }
-        let t = tasks[idx];
-        if t <= curr {
-            res = res.min(dfs(tasks, session_time, mask | (1 << idx), curr - t, memo));
-        }
-        res = res.min(
-            1 + dfs(
-                tasks,
-                session_time,
-                mask | (1 << idx),
-                session_time - t,
-                memo,
-            ),
-        );
     }
-    memo[curr as usize][mask] = res;
-    res
+    (end0 + end1 + zero) % MOD
 }
 
 #[cfg(test)]
@@ -78,13 +51,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_sessions(&[1, 2, 3], 3), 2);
-        assert_eq!(min_sessions(&[3, 1, 3, 1, 1], 8), 2);
-        assert_eq!(min_sessions(&[1, 2, 3, 4, 5], 15), 1);
+        assert_eq!(number_of_unique_good_subsequences("001"), 2);
+        assert_eq!(number_of_unique_good_subsequences("11"), 2);
+        assert_eq!(number_of_unique_good_subsequences("101"), 5);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(min_sessions(&[9, 8, 8, 4, 6], 14), 3);
-    }
+    fn test() {}
 }
