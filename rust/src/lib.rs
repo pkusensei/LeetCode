beb8be9c35fa98823ef90dc557cb910499e2724f
pkusensei <ZW_08@outlook.com>
2025-02-19 15:34:14 -0500

    1992 bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_middle_index(nums: &[i32]) -> i32 {
-    let sum: i32 = nums.iter().sum();
-    let mut left = 0;
-    for (i, &num) in nums.iter().enumerate() {
-        if left + num + left == sum {
-            return i as i32;
+pub fn find_farmland(mut land: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let [rows, cols] = get_dimensions(&land);
+    let mut res = vec![];
+    for r in 0..rows {
+        for c in 0..cols {
+            if land[r][c] == 1 {
+                let mut queue = std::collections::VecDeque::from([[r, c]]);
+                land[r][c] = 0;
+                let [mut r1, mut c1] = [i32::MAX; 2];
+                let [mut r2, mut c2] = [i32::MIN; 2];
+                while let Some([r, c]) = queue.pop_front() {
+                    r1 = r1.min(r as i32);
+                    c1 = c1.min(c as i32);
+                    r2 = r2.max(r as i32);
+                    c2 = c2.max(c as i32);
+                    for [nr, nc] in neighbors([r, c]) {
+                        if nr < rows && nc < cols && land[nr][nc] == 1 {
+                            land[nr][nc] = 0;
+                            queue.push_back([nr, nc]);
+                        }
+                    }
+                }
+                res.push(vec![r1, c1, r2, c2]);
+            }
         }
-        left += num;
     }
-    -1
+    res
 }
 
 #[cfg(test)]
@@ -47,9 +64,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(find_middle_index(&[2, 3, -1, 8, 4]), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
