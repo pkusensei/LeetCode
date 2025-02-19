907dff918cb8bd9341b3e3de4145560b1a9ff17d
pkusensei <ZW_08@outlook.com>
2025-02-19 16:10:19 -0500

    1993 had to use std::cell::Cell

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,36 +2,85 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::cell::Cell;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_farmland(mut land: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let [rows, cols] = get_dimensions(&land);
-    let mut res = vec![];
-    for r in 0..rows {
-        for c in 0..cols {
-            if land[r][c] == 1 {
-                let mut queue = std::collections::VecDeque::from([[r, c]]);
-                land[r][c] = 0;
-                let [mut r1, mut c1] = [i32::MAX; 2];
-                let [mut r2, mut c2] = [i32::MIN; 2];
-                while let Some([r, c]) = queue.pop_front() {
-                    r1 = r1.min(r as i32);
-                    c1 = c1.min(c as i32);
-                    r2 = r2.max(r as i32);
-                    c2 = c2.max(c as i32);
-                    for [nr, nc] in neighbors([r, c]) {
-                        if nr < rows && nc < cols && land[nr][nc] == 1 {
-                            land[nr][nc] = 0;
-                            queue.push_back([nr, nc]);
-                        }
-                    }
+struct LockingTree {
+    states: Vec<Cell<i32>>,
+    parent: Vec<i32>,
+    adj: Vec<Vec<usize>>,
+}
+
+impl LockingTree {
+    fn new(parent: Vec<i32>) -> Self {
+        let n = parent.len();
+        let adj = parent
+            .iter()
+            .enumerate()
+            .fold(vec![vec![]; n], |mut acc, (node, &p)| {
+                if p >= 0 {
+                    acc[p as usize].push(node);
                 }
-                res.push(vec![r1, c1, r2, c2]);
-            }
+                acc
+            });
+        Self {
+            states: vec![Cell::new(0); n],
+            parent,
+            adj,
         }
     }
-    res
+
+    fn lock(&mut self, num: i32, user: i32) -> bool {
+        let node = num as usize;
+        if self.states[node].get() == 0 {
+            self.states[node].set(user);
+            return true;
+        }
+        false
+    }
+
+    fn unlock(&mut self, num: i32, user: i32) -> bool {
+        let node = num as usize;
+        if self.states[node].get() == user {
+            self.states[node].set(0);
+            return true;
+        }
+        false
+    }
+
+    fn upgrade(&self, num: i32, user: i32) -> bool {
+        let node = num as usize;
+        if self.states[node].get() > 0 {
+            return false;
+        }
+        // Find potentially locked acestor
+        let mut p = self.parent[node];
+        while p >= 0 {
+            if self.states[p as usize].get() > 0 {
+                return false;
+            }
+            p = self.parent[p as usize];
+        }
+        if self.unlock_desc(node) {
+            self.states[node].set(user);
+            return true;
+        }
+        false
+    }
+
+    fn unlock_desc(&self, node: usize) -> bool {
+        let mut res = false;
+        for &next in self.adj[node].iter() {
+            if self.states[next].get() > 0 {
+                self.states[next].set(0);
+                res |= true
+            }
+            res |= self.unlock_desc(next);
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -64,7 +113,19 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        let mut lt = LockingTree::new(vec![-1, 0, 0, 1, 1, 2, 2]);
+        assert!(lt.lock(2, 2)); // return true because node 2 is unlocked.
+                                // Node 2 will now be locked by user 2.
+        assert!(!lt.unlock(2, 3)); // return false because user 3 cannot unlock a node locked by user 2.
+        assert!(lt.unlock(2, 2)); // return true because node 2 was previously locked by user 2.
+                                  // Node 2 will now be unlocked.
+        assert!(lt.lock(4, 5)); // return true because node 4 is unlocked.
+                                // Node 4 will now be locked by user 5.
+        assert!(lt.upgrade(0, 1)); // return true because node 0 is unlocked and has at least one locked descendant (node 4).
+                                   // Node 0 will now be locked by user 1 and node 4 will now be unlocked.
+        assert!(!lt.lock(0, 1)); // return false because node 0 is already locked.
+    }
 
     #[test]
     fn test() {}
