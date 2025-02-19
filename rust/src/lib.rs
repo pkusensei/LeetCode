fd2520469c1df3c5a6d47e4903b4f22c5644ed48
pkusensei <ZW_08@outlook.com>
2025-02-19 18:20:28 -0500

    1994 dp and bitmask of prime subsets

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,85 +2,48 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::cell::Cell;
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct LockingTree {
-    states: Vec<Cell<i32>>,
-    parent: Vec<i32>,
-    adj: Vec<Vec<usize>>,
-}
-
-impl LockingTree {
-    fn new(parent: Vec<i32>) -> Self {
-        let n = parent.len();
-        let adj = parent
+pub fn number_of_good_subsets(nums: &[i32]) -> i32 {
+    const MOD: i64 = 1_000_000_007;
+    const PRIMES: [i32; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    let mut dp = vec![0; 1 + (1 << 10)]; // total of 10 primes
+    dp[0] = 1;
+    let count = nums.iter().fold([0; 31], |mut acc, &num| {
+        acc[num as usize] += 1;
+        acc
+    });
+    for num in 1..=30 {
+        if count[num as usize] == 0 {
+            continue; // absent
+        }
+        if num == 1 {
+            continue; // special case
+        }
+        if [4, 9, 25].iter().any(|v| num % v == 0) {
+            // 4,9,25 - dupe primes
+            continue;
+        }
+        // Prime factors of this number
+        let mask: usize = PRIMES
             .iter()
             .enumerate()
-            .fold(vec![vec![]; n], |mut acc, (node, &p)| {
-                if p >= 0 {
-                    acc[p as usize].push(node);
-                }
-                acc
-            });
-        Self {
-            states: vec![Cell::new(0); n],
-            parent,
-            adj,
-        }
-    }
-
-    fn lock(&mut self, num: i32, user: i32) -> bool {
-        let node = num as usize;
-        if self.states[node].get() == 0 {
-            self.states[node].set(user);
-            return true;
-        }
-        false
-    }
-
-    fn unlock(&mut self, num: i32, user: i32) -> bool {
-        let node = num as usize;
-        if self.states[node].get() == user {
-            self.states[node].set(0);
-            return true;
-        }
-        false
-    }
-
-    fn upgrade(&self, num: i32, user: i32) -> bool {
-        let node = num as usize;
-        if self.states[node].get() > 0 {
-            return false;
-        }
-        // Find potentially locked acestor
-        let mut p = self.parent[node];
-        while p >= 0 {
-            if self.states[p as usize].get() > 0 {
-                return false;
+            .filter_map(|(i, &p)| if num % p == 0 { Some(1 << i) } else { None })
+            .sum(); // same as fold acc|(1<<i)
+        for subset in 0..1 << 10 {
+            if subset & mask > 0 {
+                continue; // intersects => dupe
             }
-            p = self.parent[p as usize];
+            let new_mask = subset | mask; // Add num into subset
+            dp[new_mask] += dp[subset] * count[num as usize];
+            dp[new_mask] %= MOD;
         }
-        if self.unlock_desc(node) {
-            self.states[node].set(user);
-            return true;
-        }
-        false
     }
-
-    fn unlock_desc(&self, node: usize) -> bool {
-        let mut res = false;
-        for &next in self.adj[node].iter() {
-            if self.states[next].get() > 0 {
-                self.states[next].set(0);
-                res |= true
-            }
-            res |= self.unlock_desc(next);
-        }
-        res
-    }
+    // [1..] to exclude empty subset
+    // 1's can be freely added
+    let res = dp[1..].iter().fold(0, |acc, v| (acc + v) % MOD) * mod_pow(2, count[1], MOD);
+    (res % MOD) as _
 }
 
 #[cfg(test)]
@@ -114,19 +77,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        let mut lt = LockingTree::new(vec![-1, 0, 0, 1, 1, 2, 2]);
-        assert!(lt.lock(2, 2)); // return true because node 2 is unlocked.
-                                // Node 2 will now be locked by user 2.
-        assert!(!lt.unlock(2, 3)); // return false because user 3 cannot unlock a node locked by user 2.
-        assert!(lt.unlock(2, 2)); // return true because node 2 was previously locked by user 2.
-                                  // Node 2 will now be unlocked.
-        assert!(lt.lock(4, 5)); // return true because node 4 is unlocked.
-                                // Node 4 will now be locked by user 5.
-        assert!(lt.upgrade(0, 1)); // return true because node 0 is unlocked and has at least one locked descendant (node 4).
-                                   // Node 0 will now be locked by user 1 and node 4 will now be unlocked.
-        assert!(!lt.lock(0, 1)); // return false because node 0 is already locked.
+        assert_eq!(number_of_good_subsets(&[1, 2, 3, 4]), 6);
+        assert_eq!(number_of_good_subsets(&[4, 2, 3, 15]), 5);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(number_of_good_subsets(&[12, 3]), 1);
+    }
 }
