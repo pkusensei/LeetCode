76a3efde9c6d05087ed3dde98a0712b899600a44
Zx Wang <ZW_08@outlook.com>
2025-02-20 14:09:13 -0500

    2003 building subtree TLE's

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,29 +5,47 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_product(s: &str) -> i32 {
-    backtrack(s.as_bytes(), &mut vec![], &mut vec![])
+pub fn smallest_missing_value_subtree(parents: &[i32], nums: &[i32]) -> Vec<i32> {
+    let n = parents.len();
+    let mut res = vec![1; n];
+    let Some(pos) = nums.iter().position(|&v| v == 1) else {
+        return res;
+    };
+    let adj = parents
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (node, &pa)| {
+            if pa >= 0 {
+                acc[pa as usize].push(node);
+            }
+            acc
+        });
+    let max = *nums.iter().max().unwrap_or(&100_000);
+    let mut seen = vec![false; 2 + max as usize];
+    let mut node = pos as i32;
+    let mut miss = 1;
+    // Only the path from root to nums[node]==1 matters
+    // Mark all values in this path with dfs
+    // And walk upwards towards the root
+    while node > -1 {
+        dfs(&adj, nums, &mut seen, node as usize);
+        while seen[miss as usize] {
+            miss += 1;
+        }
+        res[node as usize] = miss;
+        node = parents[node as usize]
+    }
+    res
 }
 
-fn backtrack(s: &[u8], s1: &mut Vec<u8>, s2: &mut Vec<u8>) -> i32 {
-    match s {
-        [] => {
-            if is_palindrome(s1.as_slice()) && is_palindrome(s2.as_slice()) {
-                return s1.len() as i32 * s2.len() as i32;
-            }
-            return 0;
-        }
-        [head, tail @ ..] => {
-            let skip = backtrack(tail, s1, s2);
-            s1.push(*head);
-            let take1 = backtrack(tail, s1, s2);
-            s1.pop();
-            s2.push(*head);
-            let take2 = backtrack(tail, s1, s2);
-            s2.pop();
-            skip.max(take1).max(take2)
-        }
+fn dfs(adj: &[Vec<usize>], nums: &[i32], seen: &mut [bool], node: usize) {
+    if seen[nums[node] as usize] {
+        return;
     }
+    for &next in adj[node].iter() {
+        dfs(adj, nums, seen, next);
+    }
+    seen[nums[node] as usize] = true;
 }
 
 #[cfg(test)]
@@ -61,9 +79,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_product("leetcodecom"), 9);
-        assert_eq!(max_product("bb"), 1);
-        assert_eq!(max_product("accbcaxxcxx"), 25);
+        assert_eq!(
+            smallest_missing_value_subtree(&[-1, 0, 0, 2], &[1, 2, 3, 4]),
+            [5, 1, 1, 1]
+        );
+        assert_eq!(
+            smallest_missing_value_subtree(&[-1, 0, 1, 0, 3, 3], &[5, 4, 6, 2, 1, 3]),
+            [7, 1, 1, 4, 2, 1]
+        );
+        assert_eq!(
+            smallest_missing_value_subtree(&[-1, 2, 3, 0, 2, 4, 1], &[2, 3, 4, 5, 6, 7, 8]),
+            [1, 1, 1, 1, 1, 1, 1]
+        );
     }
 
     #[test]
