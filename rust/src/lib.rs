b8328f35b5ccb615ce0e239b5bbc589e4c6df59d
pkusensei <ZW_08@outlook.com>
2025-02-19 19:08:46 -0500

    1995 walk backwards

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,45 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_good_subsets(nums: &[i32]) -> i32 {
-    const MOD: i64 = 1_000_000_007;
-    const PRIMES: [i32; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
-    let mut dp = vec![0; 1 + (1 << 10)]; // total of 10 primes
-    dp[0] = 1;
-    let count = nums.iter().fold([0; 31], |mut acc, &num| {
-        acc[num as usize] += 1;
-        acc
-    });
-    for num in 1..=30 {
-        if count[num as usize] == 0 {
-            continue; // absent
-        }
-        if num == 1 {
-            continue; // special case
-        }
-        if [4, 9, 25].iter().any(|v| num % v == 0) {
-            // 4,9,25 - dupe primes
-            continue;
-        }
-        // Prime factors of this number
-        let mask: usize = PRIMES
-            .iter()
-            .enumerate()
-            .filter_map(|(i, &p)| if num % p == 0 { Some(1 << i) } else { None })
-            .sum(); // same as fold acc|(1<<i)
-        for subset in 0..1 << 10 {
-            if subset & mask > 0 {
-                continue; // intersects => dupe
+pub fn count_quadruplets(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    let mut seen = std::collections::HashMap::from([(nums[n - 1], 1)]);
+    let mut res = 0;
+    for (i1, &c) in nums[..n - 1].iter().enumerate().rev() {
+        for (i2, b) in nums[..i1].iter().enumerate().rev() {
+            for a in nums[..i2].iter().rev() {
+                let d = a + b + c;
+                res += seen.get(&d).unwrap_or(&0);
             }
-            let new_mask = subset | mask; // Add num into subset
-            dp[new_mask] += dp[subset] * count[num as usize];
-            dp[new_mask] %= MOD;
         }
+        *seen.entry(c).or_insert(0) += 1;
     }
-    // [1..] to exclude empty subset
-    // 1's can be freely added
-    let res = dp[1..].iter().fold(0, |acc, v| (acc + v) % MOD) * mod_pow(2, count[1], MOD);
-    (res % MOD) as _
+    res
 }
 
 #[cfg(test)]
@@ -76,13 +51,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_good_subsets(&[1, 2, 3, 4]), 6);
-        assert_eq!(number_of_good_subsets(&[4, 2, 3, 15]), 5);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(number_of_good_subsets(&[12, 3]), 1);
-    }
+    fn test() {}
 }
