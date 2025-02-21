9206a2f4333ca8dd18600da5480824d12af04cd0
pkusensei <ZW_08@outlook.com>
2025-02-21 08:38:44 -0500

    2012 prefix and suffix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,10 +5,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
-    operations
+pub fn sum_of_beauties(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let pre_max = nums.iter().fold(Vec::with_capacity(n), |mut acc, &num| {
+        acc.push(num.max(acc.last().copied().unwrap_or(0)));
+        acc
+    });
+    let mut suf_min = nums
         .iter()
-        .map(|s| if s.contains('+') { 1 } else { -1 })
+        .rev()
+        .fold(Vec::with_capacity(n), |mut acc, &num| {
+            acc.push(num.min(acc.last().copied().unwrap_or(i32::MAX)));
+            acc
+        });
+    suf_min.reverse();
+    (1..n - 1)
+        .map(|i| (i, nums[i]))
+        .map(|(i, num)| {
+            if pre_max[i - 1] < num && num < suf_min[1 + i] {
+                2
+            } else if nums[i - 1] < num && num < nums[1 + i] {
+                1
+            } else {
+                0
+            }
+        })
         .sum()
 }
 
@@ -42,7 +63,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(sum_of_beauties(&[1, 2, 3]), 2);
+        assert_eq!(sum_of_beauties(&[2, 4, 6, 4]), 1);
+        assert_eq!(sum_of_beauties(&[3, 2, 1]), 0);
+    }
 
     #[test]
     fn test() {}
