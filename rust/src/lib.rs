987d0676e140aa51517c9787a1954b293d3ed6ce
pkusensei <ZW_08@outlook.com>
2025-02-20 21:02:26 -0500

    2011

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,36 +5,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(nums: &[i32]) -> i32 {
-    let n = nums.len();
-    let mut sorted = nums.to_vec();
-    sorted.sort_unstable();
-    sorted.dedup();
-    let mut res = i32::MAX;
-    for (left, &min) in sorted.iter().enumerate() {
-        let max = n as i32 - 1 + min;
-        let right = sorted.partition_point(|&v| v <= max);
-        let len = right - left;
-        res = res.min((n - len) as i32);
-    }
-    res
-}
-
-pub fn sliding_window(nums: &[i32]) -> i32 {
-    let n = nums.len();
-    let mut sorted = nums.to_vec();
-    sorted.sort_unstable();
-    sorted.dedup();
-    let mut res = i32::MAX;
-    let mut right = 0;
-    for (left, &min) in sorted.iter().enumerate() {
-        while sorted.get(right).is_some_and(|&v| v < min + n as i32) {
-            right += 1;
-        }
-        let len = right - left;
-        res = res.min((n - len) as i32);
-    }
-    res
+pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
+    operations
+        .iter()
+        .map(|s| if s.contains('+') { 1 } else { -1 })
+        .sum()
 }
 
 #[cfg(test)]
@@ -67,15 +42,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_operations(&[4, 2, 5, 3]), 0);
-        assert_eq!(min_operations(&[1, 2, 3, 5, 6]), 1);
-        assert_eq!(min_operations(&[1, 10, 100, 1000]), 3);
-
-        assert_eq!(sliding_window(&[4, 2, 5, 3]), 0);
-        assert_eq!(sliding_window(&[1, 2, 3, 5, 6]), 1);
-        assert_eq!(sliding_window(&[1, 10, 100, 1000]), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
