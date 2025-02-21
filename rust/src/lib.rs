1af53deb3373a41d060bf6f57128aa24c9dc2640
pkusensei <ZW_08@outlook.com>
2025-02-21 11:15:23 -0500

    2014 TLE hell

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,42 +2,57 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct DetectSquares {
-    pts: HashMap<[i32; 2], i32>,
+pub fn longest_subsequence_repeated_k(s: &str, k: i32) -> String {
+    let chs: Vec<_> = s
+        .bytes()
+        .fold([0; 26], |mut acc, b| {
+            acc[usize::from(b - b'a')] += 1;
+            acc
+        })
+        .into_iter()
+        .enumerate()
+        .rev()
+        .flat_map(|(i, count)| std::iter::repeat(b'a' + i as u8).take((count / k) as usize))
+        .collect();
+    let mut res = vec![];
+    backtrack(s.as_bytes(), &chs, k, &mut vec![], &mut res, 0);
+    String::from_utf8(res).unwrap()
 }
 
-impl DetectSquares {
-    fn new() -> Self {
-        Self {
-            pts: HashMap::new(),
+fn backtrack(s: &[u8], chs: &[u8], k: i32, curr: &mut Vec<u8>, res: &mut Vec<u8>, mask: i32) {
+    for (i, &b) in chs.iter().enumerate() {
+        if (mask >> i) & 1 == 1 {
+            continue;
         }
-    }
-
-    fn add(&mut self, point: Vec<i32>) {
-        *self.pts.entry([point[0], point[1]]).or_insert(0) += 1;
-    }
-
-    fn count(&self, point: Vec<i32>) -> i32 {
-        let [x0, y0] = point[..] else {
-            return 0;
-        };
-        let mut res = 0;
-        for &[x1, y1] in self.pts.keys().filter(|&&[x1, y1]| x1 == x0 && y1 != y0) {
-            for &[x2, y2] in self.pts.keys().filter(|&&[x2, y2]| x2 != x0 && y2 == y0) {
-                if y1.abs_diff(y0) == x2.abs_diff(x0) {
-                    res += self.pts[&[x1, y1]]
-                        * self.pts[&[x2, y2]]
-                        * self.pts.get(&[x2, y1]).unwrap_or(&0);
-                }
+        curr.push(b);
+        if find(s, curr, k) {
+            if curr.len() > res.len() {
+                *res = curr.clone();
             }
+            backtrack(s, chs, k, curr, res, mask | (1 << i));
         }
-        res
+        curr.pop();
     }
+}
+
+fn find(s: &[u8], curr: &[u8], mut k: i32) -> bool {
+    let [mut i1, mut i2] = [0, 0];
+    while k > 0 && i1 < s.len() && i2 < curr.len() {
+        if s[i1] == curr[i2] {
+            i1 += 1;
+            i2 += 1;
+            if i2 == curr.len() {
+                k -= 1;
+                i2 = 0;
+            }
+        } else {
+            i1 += 1;
+        }
+    }
+    k <= 0
 }
 
 #[cfg(test)]
@@ -70,8 +85,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(longest_subsequence_repeated_k("letsleetcode", 2), "let");
+        assert_eq!(longest_subsequence_repeated_k("bb", 2), "b");
+        assert_eq!(longest_subsequence_repeated_k("ab", 2), "");
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(longest_subsequence_repeated_k("rehrklqrhrklqrhrklqrhrklqrhrklqrhrklqrhrklqrhrklqrhrklqrhrkloqnrhrklqrhrkilyqrhrklqrphrkliqrherkglqrhzraklqrhrklqrhrlklqrhrklqrxhrklqrhrklkqhrhzorklrq", 22),"rhrklq");
+    }
 }
