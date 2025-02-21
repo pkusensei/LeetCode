1e4375596b85c7dd297de1c1043924be7d1f3dcb
pkusensei <ZW_08@outlook.com>
2025-02-21 14:55:58 -0500

    2018 transpose

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,46 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_difference(nums: Vec<i32>) -> i32 {
-    let mut prev = nums[0];
-    let mut res = i32::MIN;
-    for &num in nums.iter().skip(1) {
-        res = res.max(num - prev);
-        prev = prev.min(num)
+pub fn place_word_in_crossword(board: Vec<Vec<char>>, word: String) -> bool {
+    let [rows, cols] = get_dimensions(&board);
+    let mut transposed = vec![vec![' '; rows]; cols];
+    for r in 0..rows {
+        for c in 0..cols {
+            transposed[c][r] = board[r][c];
+        }
     }
-    if res <= 0 {
-        -1
-    } else {
-        res
+    if find(&board, word.as_bytes()) || find(&transposed, word.as_bytes()) {
+        return true;
     }
+    let mut s = word.into_bytes();
+    s.reverse();
+    find(&board, &s) || find(&transposed, &s)
+}
+
+fn find(board: &[Vec<char>], s: &[u8]) -> bool {
+    let [rows, cols] = get_dimensions(board);
+    let mut count = vec![vec![-1; cols]; rows]; // horizontal match
+    for r in 0..rows {
+        for c in 0..cols {
+            if c == 0 || board[r][c - 1] == '#' || (c > 0 && count[r][c - 1] > -1) {
+                let pos = if c == 0 {
+                    0
+                } else {
+                    1 + count[r][c - 1] as usize
+                };
+                if board[r][c] == ' ' || board[r][c] == char::from(s[pos]) {
+                    if pos == s.len() - 1 {
+                        if c == cols - 1 || board[r][1 + c] == '#' {
+                            return true;
+                        }
+                    } else {
+                        count[r][c] = pos as i32;
+                    }
+                }
+            }
+        }
+    }
+    false
 }
 
 #[cfg(test)]
