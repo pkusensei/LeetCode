4eb8c6681fe79880b66e44022dce20129be16823
pkusensei <ZW_08@outlook.com>
2025-02-22 16:05:42 -0500

    2033 median

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,20 +2,24 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn two_out_of_three(nums1: Vec<i32>, nums2: Vec<i32>, nums3: Vec<i32>) -> Vec<i32> {
-    let [s1, s2, s3] = [nums1, nums2, nums3].map(|v| v.into_iter().collect::<HashSet<_>>());
-    s1.intersection(&s2)
-        .chain(s2.intersection(&s3))
-        .chain(s3.intersection(&s1))
-        .copied()
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect()
+pub fn min_operations(grid: Vec<Vec<i32>>, x: i32) -> i32 {
+    let mut nums: Vec<_> = grid.into_iter().flat_map(|r| r.into_iter()).collect();
+    nums.sort_unstable();
+    let n = nums.len();
+    let med = nums[n / 2];
+    let mut res = 0;
+    for num in nums {
+        let d = (med - num).abs();
+        if d % x == 0 {
+            res += d / x
+        } else {
+            return -1;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
