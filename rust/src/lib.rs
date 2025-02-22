4124515629454d9209bf72bfedf2b36bf98968a2
pkusensei <ZW_08@outlook.com>
2025-02-21 19:55:32 -0500

    2025 prefix sum, binary search, and move diff from right to left

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,26 +2,65 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_consecutive_answers(answer_key: &str, k: i32) -> i32 {
-    slide(answer_key.as_bytes(), k, b'F').max(slide(answer_key.as_bytes(), k, b'T'))
+pub fn ways_to_partition(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let mut prefix = Vec::with_capacity(n);
+    let mut num_diff = Vec::with_capacity(n);
+    for (idx, &num) in nums.iter().enumerate() {
+        prefix.push(i64::from(num) + prefix.last().unwrap_or(&0));
+        if k != num {
+            num_diff.push((idx, i64::from(k - num)));
+        }
+    }
+    let mut res = 0;
+    let mut zero = 0;
+    let mut part_diff = HashMap::<_, Vec<_>>::new();
+    for p in 1..n {
+        let d = prefix[n - 1] - prefix[p - 1] * 2;
+        zero += i32::from(d == 0);
+        part_diff.entry(d).or_default().push(p);
+    }
+    for (idx, nd) in num_diff {
+        let mut curr = 0;
+        if let Some(v) = part_diff.get(&nd) {
+            let i = v.partition_point(|&piv| piv <= idx);
+            curr += v.len() - i;
+        }
+        if let Some(p) = part_diff
+            .get(&-nd)
+            .map(|v| v.partition_point(|&piv| piv <= idx))
+        {
+            curr += p;
+        }
+        res = res.max(curr as i32);
+    }
+    res.max(zero)
 }
 
-fn slide(answer_key: &[u8], k: i32, byte: u8) -> i32 {
-    let mut left = 0;
-    let mut res = 0;
-    let mut missed = 0;
-    for (right, &b) in answer_key.iter().enumerate() {
-        if b == byte {
-            missed += 1;
-        }
-        while missed > k {
-            missed -= i32::from(answer_key[left] == byte);
-            left += 1;
-        }
-        res = res.max((right + 1 - left) as i32);
+pub fn linear_time(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let [mut left, mut right] = [0, 1].map(|_| HashMap::new());
+    let sum = nums.iter().fold(0, |acc, &num| acc + i64::from(num));
+    let mut left_sum = 0;
+    for &num in nums[..n - 1].iter() {
+        left_sum += i64::from(num);
+        *right.entry(sum - 2 * left_sum).or_insert(0) += 1;
+    }
+    let mut res = *right.get(&0).unwrap_or(&0);
+    left_sum = 0;
+    for &num in nums.iter() {
+        res = res.max(
+            left.get(&i64::from(num - k)).unwrap_or(&0)
+                + right.get(&i64::from(k - num)).unwrap_or(&0),
+        );
+        left_sum += i64::from(num);
+        *left.entry(sum - 2 * left_sum).or_insert(0) += 1;
+        *right.entry(sum - 2 * left_sum).or_insert(0) -= 1;
     }
     res
 }
@@ -57,11 +96,49 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_consecutive_answers("TTFF", 2), 4);
-        assert_eq!(max_consecutive_answers("TFFT", 1), 3);
-        assert_eq!(max_consecutive_answers("TTFTTFTT", 1), 5);
+        assert_eq!(ways_to_partition(&[2, -1, 2], 3), 1);
+        assert_eq!(ways_to_partition(&[0, 0, 0], 1), 2);
+        assert_eq!(
+            ways_to_partition(
+                &[22, 4, -25, -20, -15, 15, -16, 7, 19, -10, 0, -13, -14],
+                -33
+            ),
+            4
+        );
+
+        assert_eq!(linear_time(&[2, -1, 2], 3), 1);
+        assert_eq!(linear_time(&[0, 0, 0], 1), 2);
+        assert_eq!(
+            linear_time(
+                &[22, 4, -25, -20, -15, 15, -16, 7, 19, -10, 0, -13, -14],
+                -33
+            ),
+            4
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            ways_to_partition(
+                &[
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30827, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0
+                ],
+                0
+            ),
+            33
+        );
+
+        assert_eq!(
+            linear_time(
+                &[
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30827, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0
+                ],
+                0
+            ),
+            33
+        );
+    }
 }
