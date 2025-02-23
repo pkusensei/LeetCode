5ab43fe1caf0e83ab256fe2147dfc9eb3ffb1b15
pkusensei <ZW_08@outlook.com>
2025-02-23 16:55:48 -0500

    2043

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,55 +5,50 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn kth_smallest_product(nums1: &[i32], nums2: &[i32], k: i64) -> i64 {
-    let [mut a1, mut a2, mut b1, mut b2] = [0; 4].map(|_| vec![]);
-    for &num in nums1.iter() {
-        if num < 0 {
-            a1.push(-i64::from(num));
-        } else {
-            a2.push(i64::from(num));
-        }
-    }
-    for &num in nums2.iter() {
-        if num < 0 {
-            b1.push(-i64::from(num));
-        } else {
-            b2.push(i64::from(num));
-        }
-    }
-    a1.reverse();
-    b1.reverse();
-    let neg_count = (a1.len() * b2.len() + b1.len() * a2.len()) as i64;
-    let [sign, k] = if k > neg_count {
-        [1, k - neg_count]
-    } else {
-        // Want negative number
-        // switch (b1<0, b2>0)
-        std::mem::swap(&mut b1, &mut b2);
-        [-1, neg_count + 1 - k] // might overflow
-    };
-    let [mut left, mut right] = [0, 10i64.pow(10)];
-    while left < right {
-        let mid = left + (right - left) / 2;
-        if k <= count(&a1, &b1, mid) + count(&a2, &b2, mid) {
-            right = mid;
-        } else {
-            left = 1 + mid;
-        }
-    }
-    sign * left
+struct Bank {
+    data: Vec<i64>,
 }
 
-fn count(nums1: &[i64], nums2: &[i64], val: i64) -> i64 {
-    let mut res = 0;
-    let mut i2 = nums2.len();
-    for &num1 in nums1 {
-        while i2 > 0 && num1 * nums2[i2 - 1] > val {
-            i2 -= 1;
-        }
-        res += i2;
+impl Bank {
+    fn new(balance: Vec<i64>) -> Self {
+        Self { data: balance }
     }
-    res as _
+
+    fn transfer(&mut self, account1: i32, account2: i32, money: i64) -> bool {
+        let n = self.data.len() as i32;
+        if (1..=n).contains(&account1) && (1..=n).contains(&account2) {
+            let a1 = account1 as usize - 1;
+            let a2 = account2 as usize - 1;
+            if self.data[a1] >= money {
+                self.data[a1] -= money;
+                self.data[a2] += money;
+                return true;
+            }
+        }
+        false
+    }
+
+    fn deposit(&mut self, account: i32, money: i64) -> bool {
+        let n = self.data.len() as i32;
+        if (1..=n).contains(&account) {
+            let a = account as usize - 1;
+            self.data[a] += money;
+            return true;
+        }
+        false
+    }
+
+    fn withdraw(&mut self, account: i32, money: i64) -> bool {
+        let n = self.data.len() as i32;
+        if (1..=n).contains(&account) {
+            let a = account as usize - 1;
+            if self.data[a] >= money {
+                self.data[a] -= money;
+                return true;
+            }
+        }
+        false
+    }
 }
 
 #[cfg(test)]
@@ -86,14 +81,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(kth_smallest_product(&[2, 5], &[3, 4], 2), 8);
-        assert_eq!(kth_smallest_product(&[-4, -2, 0, 3], &[2, 4], 6), 0);
-        assert_eq!(
-            kth_smallest_product(&[-2, -1, 0, 1, 2], &[-3, -1, 2, 4, 5], 3),
-            -6
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
