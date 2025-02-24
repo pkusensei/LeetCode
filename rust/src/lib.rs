e92d68380808b1d8148b877b22ad0b90cf9a7633
pkusensei <ZW_08@outlook.com>
2025-02-23 20:41:26 -0500

    2047 ifs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,38 +5,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn second_minimum(n: i32, edges: &[[i32; 2]], time: i32, change: i32) -> i32 {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize - 1);
-        acc[a].push(b);
-        acc[b].push(a);
-        acc
-    });
-    let [mut dist1, mut dist2] = [0, 1].map(|_| vec![-1; n]);
-    dist1[0] = 0;
-    let mut queue = std::collections::VecDeque::from([(0, 1)]);
-    while let Some((node, freq)) = queue.pop_front() {
-        let mut time_taken = if freq == 1 { dist1[node] } else { dist2[node] };
-        if (time_taken / change) & 1 == 1 {
-            time_taken = (time_taken / change + 1) * change + time; // red light
-        } else {
-            time_taken += time;
+pub fn count_valid_words(sentence: String) -> i32 {
+    let mut res = 0;
+    for s in sentence.split_ascii_whitespace() {
+        if s.is_empty() {
+            continue;
         }
-        for &next in adj[node].iter() {
-            if dist1[next] == -1 {
-                dist1[next] = time_taken;
-                queue.push_back((next, 1));
-            } else if dist2[next] == -1 && dist1[next] != time_taken {
-                if next == n - 1 {
-                    return time_taken;
-                }
-                dist2[next] = time_taken;
-                queue.push_back((next, 2));
+        if s.bytes().any(|b| b.is_ascii_digit()) {
+            continue;
+        }
+        if s.bytes().filter(|&b| b == b'-').count() > 1 {
+            continue;
+        }
+        if let Some(i) = s.bytes().position(|b| b == b'-') {
+            if i == 0 || i == s.len() - 1 {
+                continue;
+            }
+            if !s.as_bytes()[i - 1].is_ascii_alphabetic()
+                || !s.as_bytes()[i + 1].is_ascii_alphabetic()
+            {
+                continue;
             }
         }
+        if s.bytes().filter(|b| b"!.,".contains(b)).count() > 1 {
+            continue;
+        }
+        if s.contains(['!', '.', ',']) && !s.bytes().last().is_some_and(|b| b"!.,".contains(&b)) {
+            continue;
+        }
+        res += 1;
     }
-    -1
+    res
 }
 
 #[cfg(test)]
@@ -69,13 +68,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            second_minimum(5, &[[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]], 3, 5),
-            13
-        );
-        assert_eq!(second_minimum(2, &[[1, 2]], 3, 2), 11);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
