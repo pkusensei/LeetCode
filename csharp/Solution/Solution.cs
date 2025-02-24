0f66a3b2b4ebd2edb66096fb2fe44b3750353820
pkusensei <ZW_08@outlook.com>
2025-02-24 15:59:05 -0500

    2058

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,42 +6,30 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode ConstructFromPrePost(int[] preorder, int[] postorder)
+    public int[] NodesBetweenCriticalPoints(ListNode head)
     {
-        return Dfs(preorder, postorder);
-
-        TreeNode Dfs(int[] pre, int[] post)
+        var curr = head;
+        List<int> nums = [];
+        while (curr is not null)
         {
-            if (pre.Length == 0) { return null; }
-            TreeNode root = new(pre[0]);
-            if (pre.Length == 1) { return root; }
-            var right_val = post[^2];
-            var right_idx_in_pre = Array.IndexOf(pre, right_val);
-            var right_pre = pre[right_idx_in_pre..];
-            var right_post = post.Where(n => right_pre.Contains(n));
-            root.right = Dfs(right_pre, [.. right_post]);
-            var left_pre = pre[1..right_idx_in_pre];
-            var left_post = post.Where(n => left_pre.Contains(n));
-            root.left = Dfs(left_pre, [.. left_post]);
-            return root;
+            nums.Add(curr.val);
+            curr = curr.next;
         }
-    }
-
-    public TreeNode TwoPtrs(int[] pre, int[] post)
-    {
-        int pre_idx = 0;
-        int post_idx = 0;
-        return Dfs();
-
-        TreeNode Dfs()
+        List<int> points = [];
+        for (int i = 1; i < nums.Count - 1; i++)
         {
-            TreeNode root = new(pre[pre_idx]);
-            pre_idx += 1;
-            if (root.val != post[post_idx]) { root.left = Dfs(); }
-            if (root.val != post[post_idx]) { root.right = Dfs(); }
-            post_idx += 1;
-            return root;
+            if ((nums[i - 1] < nums[i] && nums[i + 1] < nums[i]) || (nums[i - 1] > nums[i] && nums[i + 1] > nums[i]))
+            {
+                points.Add(i);
+            }
         }
+        if (points.Count < 2) { return [-1, -1]; }
+        var min = int.MaxValue;
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            min = Math.Min(min, points[i + 1] - points[i]);
+        }
+        return [min, points.Last() - points[0]];
     }
 }
 
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,13 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 1, 2, 4, 5, 3, 6, 7 }, new[] { 4, 5, 2, 6, 7, 3, 1 }, "[1,2,3,4,5,6,7]")]
-    [DataRow(new[] { 2, 1, 3 }, new[] { 3, 1, 2 }, "[2,null,1,null,3]")]
-    public void TestMethod1(int[] pre, int[] post, string exp)
+    [DataRow(new[] { 3, 1 }, "[-1,-1]")]
+    [DataRow(new[] { 5, 3, 1, 2, 5, 1, 2 }, "[1,3]")]
+    [DataRow(new[] { 1, 3, 2, 2, 3, 2, 2, 2, 7 }, "[3,3]")]
+    public void TestMethod1(int[] nums, string exp)
     {
-        Assert.AreEqual(exp, sol.ConstructFromPrePost(pre, post).ToString());
-        Assert.AreEqual(exp, sol.TwoPtrs(pre, post).ToString());
+        var a = ListNode.Make(nums);
+        Assert.AreEqual(exp, sol.NodesBetweenCriticalPoints(a).Print());
     }
 
     [TestMethod]
