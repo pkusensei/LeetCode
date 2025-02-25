66b4081dab45160f4624d4732450de3667c6ea70
Zx Wang <ZW_08@outlook.com>
2025-02-25 10:53:46 -0500

    2068

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,45 +5,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximal_path_quality(values: &[i32], edges: &[[i32; 3]], max_time: i32) -> i32 {
-    let n = values.len();
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push((b, e[2]));
-        acc[b].push((a, e[2]));
-        acc
-    });
-    let mut res = 0;
-    let mut seen = vec![false; n];
-    seen[0] = true;
-    backtrack(values, &adj, 0, max_time, values[0], &mut seen, &mut res);
-    res
-}
-
-fn backtrack(
-    values: &[i32],
-    adj: &[Vec<(usize, i32)>],
-    node: usize,
-    time: i32,
-    val: i32,
-    seen: &mut [bool],
-    res: &mut i32,
-) {
-    if time < 0 {
-        return;
+pub fn check_almost_equivalent(word1: String, word2: String) -> bool {
+    let mut count = [0i8; 26];
+    for b in word1.bytes() {
+        count[usize::from(b - b'a')] -= 1;
     }
-    if node == 0 {
-        *res = (*res).max(val);
+    for b in word2.bytes() {
+        count[usize::from(b - b'a')] += 1;
     }
-    for &(next, t) in adj[node].iter() {
-        if seen[next] {
-            backtrack(values, adj, next, time - t, val, seen, res);
-        } else {
-            seen[next] = true;
-            backtrack(values, adj, next, time - t, val + values[next], seen, res);
-            seen[next] = false;
-        }
-    }
+    count.into_iter().all(|v| v.abs() <= 3)
 }
 
 #[cfg(test)]
@@ -76,24 +46,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            maximal_path_quality(&[0, 32, 10, 43], &[[0, 1, 10], [1, 2, 15], [0, 3, 10]], 49),
-            75
-        );
-        assert_eq!(
-            maximal_path_quality(&[5, 10, 15, 20], &[[0, 1, 10], [1, 2, 10], [0, 3, 10]], 30),
-            25
-        );
-        assert_eq!(
-            maximal_path_quality(
-                &[1, 2, 3, 4],
-                &[[0, 1, 10], [1, 2, 11], [2, 3, 12], [1, 3, 13]],
-                50
-            ),
-            7
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
