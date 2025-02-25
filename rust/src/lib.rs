afb45334b753704c024f3dbcf64ef3b96ced109b
Zx Wang <ZW_08@outlook.com>
2025-02-25 11:33:13 -0500

    2069 precompute all dirs and idx%len

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_almost_equivalent(word1: String, word2: String) -> bool {
-    let mut count = [0i8; 26];
-    for b in word1.bytes() {
-        count[usize::from(b - b'a')] -= 1;
+struct Robot {
+    idx: usize,
+    robs: Vec<([i32; 2], &'static str)>,
+}
+
+impl Robot {
+    fn new(width: i32, height: i32) -> Self {
+        let mut robs = vec![([0, 0], "South")];
+        robs.extend((1..width).map(|x| ([x, 0], "East")));
+        robs.extend((1..height).map(|y| ([width - 1, y], "North")));
+        robs.extend((0..width - 1).rev().map(|x| ([x, height - 1], "West")));
+        robs.extend((1..height - 1).rev().map(|y| ([0, y], "South")));
+        Self { idx: 0, robs }
     }
-    for b in word2.bytes() {
-        count[usize::from(b - b'a')] += 1;
+
+    fn step(&mut self, num: i32) {
+        self.idx += num as usize
     }
-    count.into_iter().all(|v| v.abs() <= 3)
+
+    fn get_pos(&self) -> Vec<i32> {
+        self.robs[self.idx % self.robs.len()].0.to_vec()
+    }
+
+    fn get_dir(&self) -> String {
+        if self.idx == 0 {
+            "East".to_string()
+        } else {
+            self.robs[self.idx % self.robs.len()].1.to_string()
+        }
+    }
 }
 
 #[cfg(test)]
