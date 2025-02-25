aa0be43482437c1e665e41d8990183431ae9d9ba
pkusensei <ZW_08@outlook.com>
2025-02-24 20:10:55 -0500

    2060 beyond me

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,37 +2,73 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_operations(nums: &[i32], start: i32, goal: i32) -> i32 {
-    let mut queue = VecDeque::from([(start, 0)]);
-    let mut seen = HashSet::from([start]);
-    while let Some((node, step)) = queue.pop_front() {
-        if node == goal {
-            return step;
-        }
-        if !(0..=1000).contains(&node) {
-            continue;
-        }
-        for &num in nums.iter() {
-            let a = num + node;
-            if seen.insert(a) {
-                queue.push_back((a, 1 + step));
-            }
-            let b = -num + node;
-            if seen.insert(b) {
-                queue.push_back((b, 1 + step));
-            }
-            let c = num ^ node;
-            if seen.insert(c) {
-                queue.push_back((c, 1 + step));
-            }
-        }
+pub fn possibly_equals(s1: &str, s2: &str) -> bool {
+    let (n1, n2) = (s1.len(), s2.len());
+    let mut memo = vec![vec![[None; 2000]; 1 + n2]; 1 + n1];
+    dfs(s1.as_bytes(), s2.as_bytes(), 0, 0, 0, &mut memo)
+}
+
+fn dfs(
+    s1: &[u8],
+    s2: &[u8],
+    i1: usize,
+    i2: usize,
+    diff: i32,
+    memo: &mut [Vec<[Option<bool>; 2000]>],
+) -> bool {
+    let (n1, n2) = (s1.len(), s2.len());
+    if i1 == n1 && i2 == n2 {
+        return diff == 0;
     }
-    -1
+    if let Some(v) = memo[i1][i2][(1000 + diff) as usize] {
+        return v;
+    }
+    if s1.get(i1).zip(s2.get(i2)).is_some_and(|(a, b)| a == b)
+        && diff == 0
+        && dfs(s1, s2, 1 + i1, 1 + i2, 0, memo)
+    {
+        memo[i1][i2][(1000 + diff) as usize] = Some(true);
+        return true;
+    }
+    if s1.get(i1).is_some_and(|b| b.is_ascii_alphabetic())
+        && diff > 0
+        && dfs(s1, s2, 1 + i1, i2, diff - 1, memo)
+    {
+        memo[i1][i2][(1000 + diff) as usize] = Some(true);
+        return true;
+    }
+    if s2.get(i2).is_some_and(|b| b.is_ascii_alphabetic())
+        && diff < 0
+        && dfs(s1, s2, i1, 1 + i2, diff + 1, memo)
+    {
+        memo[i1][i2][(1000 + diff) as usize] = Some(true);
+        return true;
+    }
+    let mut num = 0;
+    let mut idx = i1;
+    while s1.get(idx).is_some_and(|b| b.is_ascii_digit()) {
+        num = 10 * num + i32::from(s1[idx] - b'0');
+        if dfs(s1, s2, 1 + idx, i2, diff - num, memo) {
+            memo[i1][i2][(1000 + diff) as usize] = Some(true);
+            return true;
+        }
+        idx += 1;
+    }
+    num = 0;
+    idx = i2;
+    while s2.get(idx).is_some_and(|b| b.is_ascii_digit()) {
+        num = 10 * num + i32::from(s2[idx] - b'0');
+        if dfs(s1, s2, i1, 1 + idx, diff + num, memo) {
+            memo[i1][i2][(1000 + diff) as usize] = Some(true);
+            return true;
+        }
+        idx += 1;
+    }
+    memo[i1][i2][(1000 + diff) as usize] = Some(false);
+    false
 }
 
 #[cfg(test)]
@@ -65,8 +101,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert!(possibly_equals("internationalization", "i18n"));
+        assert!(possibly_equals("l123e", "44"));
+        assert!(!possibly_equals("a5b", "c5b"));
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert!(!possibly_equals("x94", "x14"));
+    }
 }
