3b506c1302fa35c1c13c6165f61273ab5a133753
Zx Wang <ZW_08@outlook.com>
2025-02-27 10:55:40 -0500

    2085

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,54 +2,21 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{
-    iter::{once, repeat},
-    str,
-};
+use std::collections::HashMap;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn k_mirror(k: i32, n: i32) -> i64 {
-    let mut res = 0;
-    let mut s = vec![b'0'];
-    for _ in 0..n {
-        loop {
-            s = gen_mirror_k(k as u8 + b'0', s);
-            let Some(num) = str::from_utf8(&s)
-                .ok()
-                .and_then(|s| i64::from_str_radix(s, k as u32).ok())
-            else {
-                continue;
-            };
-            if is_palindrome(num.to_string().bytes()) {
-                res += num;
-                break;
-            }
-        }
-    }
-    res
-}
-
-fn gen_mirror_k(k: u8, mut s: Vec<u8>) -> Vec<u8> {
-    let n = s.len();
-    let mid = s.len() / 2;
-    for idx in mid..s.len() {
-        if s[idx] + 1 < k {
-            let d = 1 + s[idx];
-            s[idx] = d;
-            s[n - 1 - idx] = d;
-            for i in mid..idx {
-                s[i] = b'0';
-                s[n - 1 - i] = b'0';
-            }
-            return s;
-        }
-    }
-    once(b'1')
-        .chain(repeat(b'0').take(n - 1))
-        .chain(once(b'1'))
-        .collect()
+pub fn count_words(words1: Vec<String>, words2: Vec<String>) -> i32 {
+    let [map1, map2] = [&words1, &words2].map(|v| {
+        v.iter().fold(HashMap::new(), |mut acc, s| {
+            *acc.entry(s.as_str()).or_insert(0) += 1;
+            acc
+        })
+    });
+    map1.into_iter()
+        .filter(|(s, c1)| *c1 == 1 && map2.get(s).is_some_and(|&c2| c2 == 1))
+        .count() as i32
 }
 
 #[cfg(test)]
@@ -82,11 +49,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(k_mirror(2, 5), 25);
-        assert_eq!(k_mirror(3, 7), 499);
-        assert_eq!(k_mirror(7, 17), 20379000);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
