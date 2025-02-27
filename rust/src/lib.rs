af504caf44355cea6469ba091d98561096f04f65
Zx Wang <ZW_08@outlook.com>
2025-02-27 15:52:34 -0500

    2100 prefix dec and suffix inc length

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,19 +2,40 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::cmp::Reverse;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    if nums.len() <= k as usize {
-        return nums;
+pub fn good_days_to_rob_bank(security: &[i32], time: i32) -> Vec<i32> {
+    let n = security.len();
+    let mut decs = Vec::with_capacity(n);
+    decs.push(0);
+    for i in 1..n {
+        if security[i - 1] >= security[i] {
+            decs.push(1 + decs[i - 1]);
+        } else {
+            decs.push(0);
+        }
     }
-    let mut nis: Vec<_> = nums.iter().enumerate().map(|(i, &v)| (i, v)).collect();
-    let (res, _, _) = nis.select_nth_unstable_by_key(k as usize, |&(_i, v)| Reverse(v));
-    res.sort_unstable_by_key(|&(i, _v)| i);
-    res.iter().map(|&(_, v)| v).collect()
+    let mut incs = Vec::with_capacity(n);
+    incs.push(0);
+    for i in (0..n - 1).rev() {
+        if security[i] <= security[1 + i] {
+            incs.push(1 + incs.last().unwrap_or(&0));
+        } else {
+            incs.push(0);
+        }
+    }
+    incs.reverse();
+    (0..)
+        .zip(decs.into_iter().zip(incs))
+        .filter_map(|(i, (dec, inc))| {
+            if dec >= time && inc >= time {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -47,7 +68,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(good_days_to_rob_bank(&[5, 3, 3, 3, 5, 6, 2], 2), [2, 3]);
+        assert_eq!(good_days_to_rob_bank(&[1, 1, 1, 1, 1], 0), [0, 1, 2, 3, 4]);
+        assert!(good_days_to_rob_bank(&[1, 2, 3, 4, 5, 6], 2).is_empty());
+    }
 
     #[test]
     fn test() {}
