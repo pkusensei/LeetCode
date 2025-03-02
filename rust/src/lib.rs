1939438e64f2d371de65f05148576596ac76db88
pkusensei <ZW_08@outlook.com>
2025-03-01 19:35:28 -0500

    2570

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn capitalize_title(title: String) -> String {
-    title
-        .split_ascii_whitespace()
-        .map(|s| {
-            let s = s.to_ascii_lowercase();
-            if s.len() > 2 {
-                let mut v = s[..1].to_uppercase();
-                v.push_str(&s[1..]);
-                v
-            } else {
-                s
+pub fn merge_arrays(nums1: Vec<Vec<i32>>, nums2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let (n1, n2) = (nums1.len(), nums2.len());
+    let [mut i1, mut i2] = [0, 0];
+    let mut res = vec![];
+    while i1 < n1 && i2 < n2 {
+        match nums1[i1][0].cmp(&nums2[i2][0]) {
+            std::cmp::Ordering::Less => {
+                res.push(nums1[i1].clone());
+                i1 += 1;
             }
-        })
-        .collect::<Vec<_>>()
-        .join(" ")
+            std::cmp::Ordering::Equal => {
+                res.push(vec![nums1[i1][0], nums1[i1][1] + nums2[i2][1]]);
+                i1 += 1;
+                i2 += 1;
+            }
+            std::cmp::Ordering::Greater => {
+                res.push(nums2[i2].clone());
+                i2 += 1;
+            }
+        }
+    }
+    while i1 < n1 {
+        res.push(nums1[i1].clone());
+        i1 += 1;
+    }
+    while i2 < n2 {
+        res.push(nums2[i2].clone());
+        i2 += 1;
+    }
+    res
 }
 
 #[cfg(test)]
