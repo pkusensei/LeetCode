2f012984d898890236b0e411e2584df4e8ca7197
pkusensei <ZW_08@outlook.com>
2025-03-02 13:10:49 -0500

    2140 dp pick/unpick

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_moves(mut target: i32, mut max_doubles: i32) -> i32 {
-    let mut res = 0;
-    while target > 1 && max_doubles > 0 {
-        if target & 1 == 1 {
-            target -= 1;
-        } else {
-            target /= 2;
-            max_doubles -= 1;
-        }
-        res += 1;
+pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+    let n = questions.len();
+    let mut dp = vec![0; n];
+    for idx in (0..n).rev() {
+        let [pts, bra] = questions[idx][..] else {
+            unreachable!()
+        };
+        let next = bra as usize + idx + 1;
+        let take = i64::from(pts) + if next < n { dp[next] } else { 0 };
+        let skip = if 1 + idx < n { dp[1 + idx] } else { 0 };
+        dp[idx] = take.max(skip);
     }
-    res += target - 1;
-    res
+    dp[0]
+    // dfs(&questions, 0, &mut vec![-1; n])
+}
+
+fn dfs(questions: &[Vec<i32>], idx: usize, memo: &mut [i64]) -> i64 {
+    if idx >= questions.len() {
+        return 0;
+    }
+    if memo[idx] > -1 {
+        return memo[idx];
+    }
+    let [pts, bra] = questions[idx][..] else {
+        unreachable!()
+    };
+    let take = i64::from(pts) + dfs(questions, 1 + idx + bra as usize, memo);
+    let skip = dfs(questions, 1 + idx, memo);
+    memo[idx] = take.max(skip);
+    memo[idx]
 }
 
 #[cfg(test)]
