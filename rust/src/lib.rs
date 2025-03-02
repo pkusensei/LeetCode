846b58d48539f7e21263601ccd3f8e4b8681e6fd
pkusensei <ZW_08@outlook.com>
2025-03-02 18:09:54 -0500

    2148

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,31 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_ways(corridor: &str) -> i32 {
-    let count = corridor.bytes().filter(|&b| b == b'S').count();
-    if count == 0 || count & 1 == 1 {
+pub fn count_elements(nums: Vec<i32>) -> i32 {
+    let min = *nums.iter().min().unwrap();
+    let max = *nums.iter().max().unwrap();
+    if min == max {
         return 0;
     }
-    let mut p_count = 0;
-    let mut s_count = 0;
-    let mut res = 1;
-    for b in corridor.bytes() {
-        if b == b'S' {
-            if s_count == 2 {
-                res *= i64::from(1 + p_count);
-                p_count = 0;
-                res %= 1_000_000_007;
-                s_count = 1;
-            } else {
-                s_count += 1;
-            }
-        } else {
-            if s_count == 2 {
-                p_count += 1;
-            }
-        }
-    }
-    res as _
+    let n = nums.len();
+    let min_count = nums.iter().filter(|&&v| v == min).count();
+    let max_count = nums.iter().filter(|&&v| v == max).count();
+    (n - min_count - max_count) as _
 }
 
 #[cfg(test)]
@@ -62,11 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_ways("SSPPSPS"), 3);
-        assert_eq!(number_of_ways("PPSPSP"), 1);
-        assert_eq!(number_of_ways("S"), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
