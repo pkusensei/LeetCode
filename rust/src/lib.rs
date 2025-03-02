1790370b6e446f68644644ed3c91798a418dd37a
Zx Wang <ZW_08@outlook.com>
2025-03-02 09:47:44 -0500

    2132 2d prefix sum, thrice

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,36 +2,49 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_palindrome(words: &[&str]) -> i32 {
-    let mut sym = HashMap::new(); // 5 3 3 3
-    let mut asym = HashMap::<[u8; 2], [i32; 2]>::new();
-    for w in words.iter() {
-        let [a, b] = w.as_bytes()[..] else {
-            unreachable!()
-        };
-        if a == b {
-            *sym.entry([a; 2]).or_insert(0) += 1;
-        } else if let Some(v) = asym.get_mut(&[b, a]) {
-            v[1] += 1;
-        } else {
-            asym.entry([a, b]).or_insert([0, 0])[0] += 1;
+pub fn possible_to_stamp(grid: &[&[i32]], stamp_height: i32, stamp_width: i32) -> bool {
+    let [rows, cols] = get_dimensions(grid);
+    let [height, width] = [stamp_height, stamp_width].map(|v| v as usize);
+    // records all empty points
+    let mut prefix = vec![vec![0; 1 + cols]; 1 + rows];
+    // a stamp with bottom-right at [r, c]
+    let mut good = vec![vec![0; cols]; rows];
+    for r in 0..rows {
+        for c in 0..cols {
+            prefix[1 + r][1 + c] =
+                prefix[1 + r][c] + prefix[r][1 + c] - prefix[r][c] + (1 - grid[r][c]);
+            if 1 + r >= height && 1 + c >= width {
+                let y = 1 + r - height;
+                let x = 1 + c - width;
+                good[r][c] += i32::from(
+                    prefix[1 + r][1 + c] - prefix[y][1 + c] - prefix[1 + r][x] + prefix[y][x]
+                        == (width * height) as i32,
+                ); // possible stamps at [r, c]
+            }
         }
     }
-    // Even syms are all added in
-    // Odd syms => keep the max one, take all others as (val-1)
-    let mut res: i32 = sym.values().sum();
-    let odd_count = sym.values().filter(|&&v| v & 1 == 1).count() as i32;
-    if odd_count > 0 {
-        res -= odd_count;
-        res += 1;
+    // The number of stamps to have [r, c] covered
+    let mut covered = vec![vec![0; 1 + cols]; 1 + rows];
+    for r in 0..rows {
+        for c in 0..cols {
+            covered[1 + r][1 + c] =
+                covered[1 + r][c] + covered[r][1 + c] - covered[r][c] + good[r][c];
+        }
     }
-    res += asym.values().map(|v| 2 * v[0].min(v[1])).sum::<i32>();
-    2 * res
+    for r in 0..rows {
+        for c in 0..cols {
+            let y = (r + height).min(rows);
+            let x = (c + width).min(cols);
+            if grid[r][c] == 0 && covered[y][x] - covered[y][c] - covered[r][x] + covered[r][c] == 0
+            {
+                return false; // [r, c] is not covered
+            }
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -64,15 +77,25 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert!(possible_to_stamp(
+            &[
+                &[1, 0, 0, 0],
+                &[1, 0, 0, 0],
+                &[1, 0, 0, 0],
+                &[1, 0, 0, 0],
+                &[1, 0, 0, 0]
+            ],
+            4,
+            3
+        ));
+        assert!(!possible_to_stamp(
+            &[&[1, 0, 0, 0], &[0, 1, 0, 0], &[0, 0, 1, 0], &[0, 0, 0, 1]],
+            2,
+            2
+        ));
+    }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            longest_palindrome(&[
-                "dd", "aa", "bb", "dd", "aa", "dd", "bb", "dd", "aa", "cc", "bb", "cc", "dd", "cc"
-            ]),
-            22
-        );
-    }
+    fn test() {}
 }
