bbbd672b546403adbe7dd6dff4347f4f1d26da03
Zx Wang <ZW_08@outlook.com>
2025-03-02 10:05:43 -0500

    2134 circular => dup array

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_valid(matrix: Vec<Vec<i32>>) -> bool {
-    use std::collections::HashSet;
-    let n = matrix.len();
-    let set: HashSet<_> = (1..=n as i32).collect();
-    for r in matrix.iter() {
-        if r.iter().copied().collect::<HashSet<_>>() != set {
-            return false;
-        }
+pub fn min_swaps(mut nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    let ones: i32 = nums.iter().sum();
+    let window_len = ones as usize;
+    nums.extend_from_within(..);
+    let mut zeros = ones - nums[..window_len].iter().sum::<i32>();
+    let mut res = zeros;
+    for idx in 1..(2 * n - window_len) {
+        zeros -= i32::from(nums[idx - 1] == 0);
+        zeros += i32::from(nums[idx + window_len - 1] == 0);
+        res = res.min(zeros);
     }
-    for c in 0..n {
-        if matrix.iter().map(|r| r[c]).collect::<HashSet<_>>() != set {
-            return false;
-        }
-    }
-    true
+    res
 }
 
 #[cfg(test)]
