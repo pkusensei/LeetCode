2747267952ab4394aa98956de63068a32cd74b23
pkusensei <ZW_08@outlook.com>
2025-03-03 10:09:03 -0500

    2157 running dsu while recording bitmasks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,26 +5,74 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sub_str_hash(s: &str, power: i32, modulo: i32, k: i32, hash_value: i32) -> String {
-    let (bytes, n) = (s.as_bytes(), s.len());
-    let [p, m, hv] = [power, modulo, hash_value].map(i64::from);
-    let k = k as usize;
-    let mut start = 0;
-    let mut curr = 0;
-    let mut p_k = 1;
-    for (idx, &b) in bytes.iter().enumerate().rev() {
-        curr = (p * curr + i64::from(b - b'a' + 1)) % m;
-        if idx + k >= n {
-            // mod_pow(p, k, m) but here m is not prime
-            p_k = p_k * p % m;
+pub fn group_strings(words: &[&str]) -> Vec<i32> {
+    let n = words.len();
+    let mut dsu = DSU::new(n);
+    let mut mask_idx = std::collections::HashMap::new();
+    for (idx, s) in words.iter().enumerate() {
+        let mask = to_mask(s);
+        if let Some(&v) = mask_idx.get(&mask) {
+            dsu.union(v, idx);
         } else {
-            curr = (curr - i64::from(bytes[idx + k] - b'a' + 1) * p_k % m).rem_euclid(m);
+            mask_idx.insert(mask, idx);
         }
-        if curr == hv {
-            start = idx;
+        for bit in 0..26 {
+            if (mask >> bit) & 1 == 1 {
+                let del = mask ^ (1 << bit);
+                if let Some(&v) = mask_idx.get(&del) {
+                    dsu.union(v, idx);
+                } else {
+                    mask_idx.insert(del, idx);
+                }
+            }
         }
     }
-    s[start..start + k].to_string()
+    vec![dsu.count as i32, *dsu.size.iter().max().unwrap_or(&1)]
+}
+
+fn to_mask(s: &str) -> i32 {
+    s.bytes().fold(0, |acc, b| acc | (1 << (b - b'a')))
+}
+
+struct DSU {
+    parent: Vec<usize>,
+    size: Vec<i32>,
+    count: usize,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+            count: n,
+        }
+    }
+
+    fn find(&mut self, v: usize) -> usize {
+        if self.parent[v] != v {
+            self.parent[v] = self.find(self.parent[v]);
+        }
+        self.parent[v]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return;
+        }
+        match self.size[rx].cmp(&self.size[ry]) {
+            std::cmp::Ordering::Less => {
+                self.parent[rx] = ry;
+                self.size[ry] += self.size[rx];
+            }
+            _ => {
+                self.parent[ry] = rx;
+                self.size[rx] += self.size[ry];
+            }
+        }
+        self.count -= 1;
+    }
 }
 
 #[cfg(test)]
@@ -58,10 +106,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(sub_str_hash("leetcode", 7, 20, 2, 0), "ee");
-        assert_eq!(sub_str_hash("fbxzaad", 31, 100, 3, 32), "fbx");
+        assert_eq!(group_strings(&["a", "b", "ab", "cde"]), [2, 3]);
+        assert_eq!(group_strings(&["a", "ab", "abc"]), [1, 3]);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(group_strings(&["qamp", "am", "khdrn"]), [3, 1]);
+    }
 }
