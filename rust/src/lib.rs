7d97112b3505641e6b9cb8fc6b0992fc3d1da97e
pkusensei <ZW_08@outlook.com>
2025-03-03 12:33:31 -0500

    2164

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,43 +5,26 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_difference(nums: &[i32]) -> i64 {
-    use std::cmp::Reverse;
-    use std::collections::BinaryHeap;
-
-    let n = nums.len() / 3;
-    let mut pre_min = Vec::with_capacity(n);
-    let mut curr: i64 = nums[..n].iter().map(|&v| i64::from(v)).sum();
-    pre_min.push(curr);
-    let mut max_heap: BinaryHeap<_> = nums[..n].iter().copied().collect();
-    for idx in 1..=n {
-        let val = nums[idx + n - 1];
-        curr += i64::from(val);
-        max_heap.push(val);
-        let top = max_heap.pop().unwrap();
-        curr -= i64::from(top);
-        pre_min.push(curr);
+pub fn sort_even_odd(nums: Vec<i32>) -> Vec<i32> {
+    let [mut odds, mut evens] = [vec![], vec![]];
+    for (i, &v) in nums.iter().enumerate() {
+        if i & 1 == 1 {
+            odds.push(v);
+        } else {
+            evens.push(v);
+        }
     }
-
-    let mut suf_max = Vec::with_capacity(n);
-    curr = nums[2 * n..].iter().map(|&v| i64::from(v)).sum();
-    suf_max.push(curr);
-    let mut min_heap: BinaryHeap<_> = nums[2 * n..].iter().map(|&v| Reverse(v)).collect();
-    for idx in (n..2 * n).rev() {
-        let val = nums[idx];
-        curr += i64::from(val);
-        min_heap.push(Reverse(val));
-        let Reverse(top) = min_heap.pop().unwrap();
-        curr -= i64::from(top);
-        suf_max.push(curr);
+    odds.sort_unstable_by(|a, b| b.cmp(a));
+    evens.sort_unstable();
+    let mut res: Vec<_> = evens
+        .iter()
+        .zip(odds.iter())
+        .flat_map(|(&a, &b)| [a, b])
+        .collect();
+    if evens.len() > odds.len() {
+        res.push(*evens.last().unwrap());
     }
-    suf_max.reverse();
-    pre_min
-        .into_iter()
-        .zip(suf_max)
-        .map(|(a, b)| a - b)
-        .min()
-        .unwrap()
+    res
 }
 
 #[cfg(test)]
@@ -74,10 +57,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_difference(&[3, 1, 2]), -1);
-        assert_eq!(minimum_difference(&[7, 9, 5, 8, 1, 3]), 1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
