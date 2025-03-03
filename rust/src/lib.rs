60233cb6f1a7d4f15e551a098d5039f219d53c3d
pkusensei <ZW_08@outlook.com>
2025-03-03 08:37:27 -0500

    2155 prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,11 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_final_value(nums: Vec<i32>, original: i32) -> i32 {
-    let set: std::collections::HashSet<_> = nums.into_iter().collect();
-    let mut res = original;
-    while set.contains(&res) {
-        res *= 2;
+pub fn max_score_indices(nums: Vec<i32>) -> Vec<i32> {
+    let mut left_zeros = 0;
+    let mut right_ones: i32 = nums.iter().sum();
+    let mut score = left_zeros + right_ones;
+    let mut res = vec![0];
+    for (idx, &num) in (1..).zip(nums.iter()) {
+        left_zeros += i32::from(num == 0);
+        right_ones -= i32::from(num == 1);
+        let curr = left_zeros + right_ones;
+        match curr.cmp(&score) {
+            std::cmp::Ordering::Less => (),
+            std::cmp::Ordering::Equal => res.push(idx),
+            std::cmp::Ordering::Greater => {
+                score = curr;
+                res = vec![idx];
+            }
+        }
     }
     res
 }
