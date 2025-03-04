9abe8cee57f98eb4f6c2e2e50f6cfcba9993c2af
Zx Wang <ZW_08@outlook.com>
2025-03-04 14:26:32 -0500

    2186

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,28 +5,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_pairs(nums: &[i32], k: i32) -> i64 {
-    const fn gcd(a: i64, b: i64) -> i64 {
-        if b == 0 {
-            a
-        } else {
-            gcd(b, a % b)
-        }
+pub fn min_steps(s: String, t: String) -> i32 {
+    let mut count = [0i32; 26];
+    for b in s.bytes() {
+        count[usize::from(b - b'a')] += 1;
     }
-
-    let k = i64::from(k);
-    let mut count = std::collections::HashMap::new();
-    let mut res = 0;
-    for &num in nums.iter() {
-        let curr = gcd(i64::from(num), k);
-        for (i, v) in count.iter() {
-            if i * curr % k == 0 {
-                res += v;
-            }
-        }
-        *count.entry(curr).or_insert(0) += 1;
+    for b in t.bytes() {
+        count[usize::from(b - b'a')] -= 1;
     }
-    res
+    count.into_iter().map(|v| v.abs()).sum()
 }
 
 #[cfg(test)]
@@ -59,9 +46,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_pairs(&[1, 2, 3, 4, 5], 2), 7);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
