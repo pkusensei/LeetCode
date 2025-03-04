2015f37336475286a2f0e03a21bfbf86053a551e
Zx Wang <ZW_08@outlook.com>
2025-03-04 14:21:39 -0500

    2183 (a*b)%k==0 => gcd(a,k)*gcd(b,k)%k==0

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,26 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_even(num: i32) -> i32 {
-    let mut res = 0;
-    for mut val in 1..=num {
-        let mut curr = 0;
-        while val > 0 {
-            curr += val % 10;
-            val /= 10;
+pub fn count_pairs(nums: &[i32], k: i32) -> i64 {
+    const fn gcd(a: i64, b: i64) -> i64 {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
         }
-        res += i32::from(curr & 1 == 0);
+    }
+
+    let k = i64::from(k);
+    let mut count = std::collections::HashMap::new();
+    let mut res = 0;
+    for &num in nums.iter() {
+        let curr = gcd(i64::from(num), k);
+        for (i, v) in count.iter() {
+            if i * curr % k == 0 {
+                res += v;
+            }
+        }
+        *count.entry(curr).or_insert(0) += 1;
     }
     res
 }
@@ -48,7 +59,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_pairs(&[1, 2, 3, 4, 5], 2), 7);
+    }
 
     #[test]
     fn test() {}
