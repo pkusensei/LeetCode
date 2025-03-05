3c90b9e7c780719931488f6f0c3949321b05044b
pkusensei <ZW_08@outlook.com>
2025-03-05 11:19:43 -0500

    2194

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,34 +5,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_moves_to_make_palindrome(s: String) -> i32 {
-    let n = s.len();
-    let mut s = s.into_bytes();
-    let [mut left, mut right] = [0, n - 1];
-    let mut res = 0;
-    while left < right {
-        if s[left] != s[right] {
-            let temp = right;
-            while left < right && s[left] != s[right] {
-                right -= 1;
-            }
-            if left == right {
-                // s[left] is at the wrong spot
-                s.swap(left, 1 + left);
-                res += 1;
-                right = temp;
-            } else {
-                s[right..=temp].rotate_left(1);
-                res += temp - right;
-                left += 1;
-                right = temp - 1;
-            }
-        } else {
-            left += 1;
-            right -= 1;
+pub fn cells_in_range(s: String) -> Vec<String> {
+    let s = s.as_bytes();
+    let mut res = vec![];
+    for letter in s[0]..=s[3] {
+        for digit in s[1]..=s[4] {
+            res.push(String::from_utf8(vec![letter, digit]).unwrap());
         }
     }
-    res as i32
+    res
 }
 
 #[cfg(test)]
@@ -65,16 +46,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_moves_to_make_palindrome("aabb".into()), 2);
-        assert_eq!(min_moves_to_make_palindrome("letelt".into()), 2);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(
-            min_moves_to_make_palindrome("skwhhaaunskegmdtutlgtteunmuuludii".into()),
-            163
-        );
-    }
+    fn test() {}
 }
