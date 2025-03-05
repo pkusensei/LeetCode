dc3700fe0d523bee74238803b21bb63dd6db22c1
pkusensei <ZW_08@outlook.com>
2025-03-05 10:01:07 -0500

    2192 combine topo sort with dfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,23 +5,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sort_jumbled(mapping: Vec<i32>, mut nums: Vec<i32>) -> Vec<i32> {
-    nums.sort_by_cached_key(|num| {
-        let mut num = *num;
-        if num == 0 {
-            return mapping[0];
+pub fn get_ancestors(n: i32, edges: &[[i32; 2]]) -> Vec<Vec<i32>> {
+    let n = n as usize;
+    let mut adj = vec![vec![]; n];
+    let mut indegs = vec![0; n];
+    for e in edges.iter() {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        adj[b].push(a);
+        indegs[a] += 1;
+    }
+    let mut res = vec![vec![]; n];
+    for (node, &v) in indegs.iter().enumerate() {
+        if v == 0 {
+            dfs(&adj, node, &mut res);
         }
-        let mut digits = vec![];
-        while num > 0 {
-            digits.push(num % 10);
-            num /= 10;
-        }
-        digits
-            .into_iter()
-            .rev()
-            .fold(0, |acc, d| 10 * acc + mapping[d as usize])
-    });
-    nums
+    }
+    res
+}
+
+fn dfs(adj: &[Vec<usize>], node: usize, res: &mut [Vec<i32>]) {
+    if !res[node].is_empty() {
+        return;
+    }
+    let mut curr = vec![];
+    for &next in adj[node].iter() {
+        curr.push(next as i32);
+        dfs(adj, next, res);
+        curr.extend_from_slice(&res[next]);
+    }
+    curr.sort_unstable();
+    curr.dedup();
+    res[node] = curr;
 }
 
 #[cfg(test)]
@@ -54,7 +68,52 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            get_ancestors(
+                8,
+                &[
+                    [0, 3],
+                    [0, 4],
+                    [1, 3],
+                    [2, 4],
+                    [2, 7],
+                    [3, 5],
+                    [3, 6],
+                    [3, 7],
+                    [4, 6]
+                ]
+            ),
+            [
+                vec![],
+                vec![],
+                vec![],
+                vec![0, 1],
+                vec![0, 2],
+                vec![0, 1, 3],
+                vec![0, 1, 2, 3, 4],
+                vec![0, 1, 2, 3]
+            ]
+        );
+        assert_eq!(
+            get_ancestors(
+                5,
+                &[
+                    [0, 1],
+                    [0, 2],
+                    [0, 3],
+                    [0, 4],
+                    [1, 2],
+                    [1, 3],
+                    [1, 4],
+                    [2, 3],
+                    [2, 4],
+                    [3, 4]
+                ]
+            ),
+            [vec![], vec![0], vec![0, 1], vec![0, 1, 2], vec![0, 1, 2, 3]]
+        );
+    }
 
     #[test]
     fn test() {}
