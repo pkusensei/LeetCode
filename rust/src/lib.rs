f7c652313968377afcba3659a5205dc802c1e5bd
pkusensei <ZW_08@outlook.com>
2025-03-05 08:19:05 -0500

    2190

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,40 +5,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_finish_time(tires: &[[i32; 2]], change_time: i32, num_laps: i32) -> i32 {
-    let n = num_laps as usize;
-    let change_time = i64::from(change_time);
-    let mut best = vec![i64::MAX; 1 + n];
-    let mut max_laps = 0;
-    for tire in tires.iter() {
-        let [f, r] = [0, 1].map(|i| i64::from(tire[i]));
-        let mut lap_time = f;
-        let mut total = lap_time;
-        for lap in 1..=n {
-            if lap_time >= f + change_time {
-                break;
-            }
-            max_laps = max_laps.max(lap);
-            best[lap] = best[lap].min(total);
-            lap_time *= r;
-            total += lap_time;
-        }
-    }
-    dfs(n, change_time, &best, max_laps, &mut vec![i64::MAX; 1 + n]) as i32
-}
-
-fn dfs(n: usize, change_time: i64, best: &[i64], max_laps: usize, memo: &mut [i64]) -> i64 {
-    if n == 0 {
-        return -change_time;
-    }
-    if memo[n] < i64::MAX {
-        return memo[n];
-    }
-    for lap in 1..=n.min(max_laps) {
-        memo[n] =
-            memo[n].min(best[lap] + change_time + dfs(n - lap, change_time, best, max_laps, memo));
-    }
-    memo[n]
+pub fn most_frequent(nums: Vec<i32>, key: i32) -> i32 {
+    nums.windows(2)
+        .filter_map(|w| if w[0] == key { Some(w[1]) } else { None })
+        .fold(std::collections::HashMap::new(), |mut acc, num| {
+            *acc.entry(num).or_insert(0) += 1;
+            acc
+        })
+        .into_iter()
+        .max_by_key(|&(_k, v)| v)
+        .map(|(k, _v)| k)
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -71,11 +48,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_finish_time(&[[2, 3], [3, 4]], 5, 4), 21);
-        assert_eq!(minimum_finish_time(&[[1, 10], [2, 2], [3, 4]], 6, 5), 25);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(most_frequent(vec![2, 1, 2, 1, 2, 3], 2), 1);
+    }
 }
