f328ba69e8e055d4ebd70f649c057f94f84d1a3a
Zx Wang <ZW_08@outlook.com>
2025-03-06 09:59:15 -0500

    2203 run dijkstra thrice. thrice

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,40 +2,49 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_missing_and_repeated_values(grid: &[&[i32]]) -> Vec<i32> {
-    let n = grid.len() as i32;
-    let xor = (1..=n.pow(2)).fold(0, |acc, v| acc ^ v);
-    let mut grid_xor = 0;
-    let mut set = std::collections::HashSet::new();
-    let mut dup = 0;
-    for r in grid.iter() {
-        for &v in r.iter() {
-            grid_xor ^= v;
-            if !set.insert(v) {
-                dup = v;
+pub fn minimum_weight(n: i32, edges: &[[i32; 3]], src1: i32, src2: i32, dest: i32) -> i64 {
+    let n = n as usize;
+    let [mut adj, mut adj2] = [0, 1].map(|_| vec![vec![]; n]);
+    for e in edges.iter() {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        adj[a].push((b, e[2]));
+        adj2[b].push((a, e[2]));
+    }
+    let dists1 = dijkstra(&adj, src1 as _);
+    let dists2 = dijkstra(&adj, src2 as _);
+    let dists3 = dijkstra(&adj2, dest as _);
+    dists1
+        .into_iter()
+        .zip(dists2)
+        .zip(dists3)
+        .filter_map(|((a, b), c)| a.checked_add(b).and_then(|v| v.checked_add(c)))
+        .min()
+        .unwrap_or(-1)
+}
+
+fn dijkstra(adj: &[Vec<(usize, i32)>], node: usize) -> Vec<i64> {
+    let n = adj.len();
+    let mut dists = vec![i64::MAX; n];
+    dists[node] = 0;
+    let mut heap = BinaryHeap::from([(Reverse(0), node)]);
+    while let Some((Reverse(cost), node)) = heap.pop() {
+        if cost > dists[node] {
+            continue;
+        }
+        for &(next, weight) in adj[node].iter() {
+            let nc = cost + i64::from(weight);
+            if nc < dists[next] {
+                dists[next] = nc;
+                heap.push((Reverse(nc), next));
             }
         }
     }
-    vec![dup, xor ^ grid_xor ^ dup]
-}
-
-pub fn with_math(grid: &[&[i32]]) -> Vec<i32> {
-    let mut diff = 0;
-    let mut sum_diff = 0;
-    let mut sqr_diff = 0;
-    for r in grid.iter() {
-        for &v in r.iter() {
-            diff += 1;
-            sum_diff += v - diff;
-            sqr_diff += v.pow(2) - diff.pow(2);
-        }
-    }
-    let dup = (sqr_diff / sum_diff + sum_diff) / 2;
-    let missing = (sqr_diff / sum_diff - sum_diff) / 2;
-    vec![dup, missing]
+    dists
 }
 
 #[cfg(test)]
@@ -70,16 +79,26 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            find_missing_and_repeated_values(&[&[1, 3], &[2, 2]]),
-            [2, 4]
+            minimum_weight(
+                6,
+                &[
+                    [0, 2, 2],
+                    [0, 5, 6],
+                    [1, 0, 3],
+                    [1, 4, 5],
+                    [2, 1, 1],
+                    [2, 3, 3],
+                    [2, 3, 4],
+                    [3, 4, 2],
+                    [4, 5, 1]
+                ],
+                0,
+                1,
+                5
+            ),
+            9
         );
-        assert_eq!(
-            find_missing_and_repeated_values(&[&[9, 1, 7], &[8, 9, 2], &[3, 4, 6]]),
-            [9, 5]
-        );
-
-        assert_eq!(with_math(&[&[1, 3], &[2, 2]]), [2, 4]);
-        assert_eq!(with_math(&[&[9, 1, 7], &[8, 9, 2], &[3, 4, 6]]), [9, 5]);
+        assert_eq!(minimum_weight(3, &[[0, 1, 1], [2, 1, 1]], 0, 1, 2), -1);
     }
 
     #[test]
