ab494b1bdbff2c3fb91744caa277c40aa8241aa2
Zx Wang <ZW_08@outlook.com>
2025-03-06 12:35:42 -0500

    2211 move towards each other

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,14 +5,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_hill_valley(mut nums: Vec<i32>) -> i32 {
-    nums.dedup();
-    nums.windows(2)
-        .map(|w| w[1] - w[0])
-        .collect::<Vec<_>>()
-        .windows(2)
-        .filter(|w| w[0].signum() != w[1].signum())
-        .count() as _
+pub fn count_collisions(directions: &str) -> i32 {
+    let mut stack = vec![];
+    let mut res = 0;
+    for b in directions.bytes() {
+        match b {
+            b'S' => {
+                while stack.last().is_some_and(|&v| v == b'R') {
+                    stack.pop();
+                    res += 1;
+                }
+                stack.push(b'S');
+            }
+            b'L' => match stack.last() {
+                Some(b'R') => {
+                    res += 1;
+                    while stack.last().is_some_and(|&v| v == b'R') {
+                        stack.pop();
+                        res += 1;
+                    }
+                    stack.push(b'S');
+                }
+                Some(b'S') => res += 1,
+                _ => (),
+            },
+            _ => stack.push(b),
+        }
+    }
+    res
+}
+
+pub fn towards_middle(s: &str) -> i32 {
+    let mut s = s.trim_start_matches('L');
+    s = s.trim_end_matches('R');
+    s.bytes().map(|b| i32::from(b != b'S')).sum()
 }
 
 #[cfg(test)]
@@ -45,7 +71,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_collisions("RLRSLL"), 5);
+        assert_eq!(count_collisions("LLRR"), 0);
+
+        assert_eq!(towards_middle("RLRSLL"), 5);
+        assert_eq!(towards_middle("LLRR"), 0);
+    }
 
     #[test]
     fn test() {}
