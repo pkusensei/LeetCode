63b99fea1c702a449db4d56228561e79cd8bc3c8
pkusensei <ZW_08@outlook.com>
2025-03-05 20:19:06 -0500

    2965 sum_diff and square_sum_diff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_top(nums: Vec<i32>, k: i32) -> i32 {
-    if nums.len() == 1 {
-        return if k & 1 == 1 { -1 } else { nums[0] };
+pub fn find_missing_and_repeated_values(grid: &[&[i32]]) -> Vec<i32> {
+    let n = grid.len() as i32;
+    let xor = (1..=n.pow(2)).fold(0, |acc, v| acc ^ v);
+    let mut grid_xor = 0;
+    let mut set = std::collections::HashSet::new();
+    let mut dup = 0;
+    for r in grid.iter() {
+        for &v in r.iter() {
+            grid_xor ^= v;
+            if !set.insert(v) {
+                dup = v;
+            }
+        }
     }
-    if k <= 1 {
-        return nums[k as usize];
+    vec![dup, xor ^ grid_xor ^ dup]
+}
+
+pub fn with_math(grid: &[&[i32]]) -> Vec<i32> {
+    let mut diff = 0;
+    let mut sum_diff = 0;
+    let mut sqr_diff = 0;
+    for r in grid.iter() {
+        for &v in r.iter() {
+            diff += 1;
+            sum_diff += v - diff;
+            sqr_diff += v.pow(2) - diff.pow(2);
+        }
     }
-    let mut res = 0;
-    for i in 0..(k as usize - 1).min(nums.len()) {
-        res = res.max(nums[i])
-    }
-    if let Some(&v) = nums.get(k as usize) {
-        res = res.max(v)
-    }
-    res
+    let dup = (sqr_diff / sum_diff + sum_diff) / 2;
+    let missing = (sqr_diff / sum_diff - sum_diff) / 2;
+    vec![dup, missing]
 }
 
 #[cfg(test)]
@@ -52,7 +68,19 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            find_missing_and_repeated_values(&[&[1, 3], &[2, 2]]),
+            [2, 4]
+        );
+        assert_eq!(
+            find_missing_and_repeated_values(&[&[9, 1, 7], &[8, 9, 2], &[3, 4, 6]]),
+            [9, 5]
+        );
+
+        assert_eq!(with_math(&[&[1, 3], &[2, 2]]), [2, 4]);
+        assert_eq!(with_math(&[&[9, 1, 7], &[8, 9, 2], &[3, 4, 6]]), [9, 5]);
+    }
 
     #[test]
     fn test() {}
