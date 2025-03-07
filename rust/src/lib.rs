9aa1de92f485345de6b7e3f18dff6b02d8a1be33
pkusensei <ZW_08@outlook.com>
2025-03-07 18:43:24 -0500

    2222 prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,11 +5,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn triangular_sum(mut nums: Vec<i32>) -> i32 {
-    while nums.len() > 1 {
-        nums = nums.windows(2).map(|w| (w[0] + w[1]) % 10).collect();
+pub fn number_of_ways(s: &str) -> i64 {
+    let prefix = s.bytes().fold(vec![], |mut acc, b| {
+        acc.push(i64::from(b - b'0') + acc.last().unwrap_or(&0));
+        acc
+    });
+    let (s, n) = (s.as_bytes(), s.len());
+    let mut res = 0;
+    for i in 1..n - 1 {
+        res += if s[i] == b'0' {
+            let left = prefix[i - 1];
+            let right = prefix[n - 1] - prefix[i];
+            left * right
+        } else {
+            let left = i as i64 - prefix[i - 1];
+            let right = (n - i - 1) as i64 - (prefix[n - 1] - prefix[i]);
+            left * right
+        }
     }
-    nums[0]
+    res
+}
+
+pub fn one_pass(s: &str) -> i64 {
+    let mut res = 0;
+    let [mut one, mut zero, mut zero_one, mut one_zero] = [0; 4];
+    for b in s.bytes() {
+        if b == b'0' {
+            zero += 1;
+            one_zero += one;
+            res += zero_one;
+        } else {
+            one += 1;
+            zero_one += zero;
+            res += one_zero;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -42,7 +73,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_ways("001101"), 6);
+        assert_eq!(number_of_ways("11100"), 0);
+
+        assert_eq!(one_pass("001101"), 6);
+        assert_eq!(one_pass("11100"), 0);
+    }
 
     #[test]
     fn test() {}
