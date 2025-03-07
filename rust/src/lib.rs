2767b436e2f9725f895950fad8fcc9b7db7bd134
pkusensei <ZW_08@outlook.com>
2025-03-07 10:12:14 -0500

    2213 add and remove ranges

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,41 +2,69 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::BTreeMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn closest_primes(left: i32, right: i32) -> Vec<i32> {
-    let sieve = sieve(right);
-    let mut diff = right;
-    let mut res = vec![-1, -1];
-    for w in sieve.windows(2) {
-        let d = w[1] - w[0];
-        if left <= w[0] && d < diff {
-            diff = d;
-            res = w.to_vec();
-            if diff == 2 {
-                break;
+pub fn longest_repeating(s: String, query_characters: &str, query_indices: &[i32]) -> Vec<i32> {
+    let mut s = s.into_bytes();
+    let mut spans = BTreeMap::new();
+    let mut lengths = BTreeMap::new();
+    let mut left = 0;
+    for w in s.chunk_by(|a, b| a == b) {
+        spans.insert(left, left + w.len() - 1);
+        *lengths.entry(w.len()).or_insert(0) += 1;
+        left += w.len();
+    }
+    let mut res = vec![];
+    for (qi, qch) in query_indices
+        .iter()
+        .zip(query_characters.bytes())
+        .map(|(&qi, b)| (qi as usize, b))
+    {
+        if s[qi] != qch {
+            let (&left, &right) = spans.range(..=qi).next_back().unwrap();
+            spans.remove(&left);
+            let length = right + 1 - left;
+            *lengths.entry(length).or_insert(0) -= 1;
+            if lengths[&length] == 0 {
+                lengths.remove(&length);
             }
+            if left < qi {
+                spans.insert(left, qi - 1);
+                *lengths.entry(qi - left).or_insert(0) += 1;
+            }
+            if qi < right {
+                spans.insert(1 + qi, right);
+                *lengths.entry(right - qi).or_insert(0) += 1;
+            }
+            s[qi] = qch;
+            let [mut left, mut right] = [qi; 2];
+            if qi.checked_sub(1).is_some_and(|i| s[i] == s[qi]) {
+                let item = spans.range(..=qi).next_back().unwrap();
+                left = *item.0;
+                let length = spans.remove(&left).unwrap() + 1 - left;
+                *lengths.entry(length).or_insert(0) -= 1;
+                if lengths[&length] == 0 {
+                    lengths.remove(&length);
+                }
+            }
+            if s.get(1 + qi).is_some_and(|&v| v == s[qi]) {
+                let (&key, _) = spans.range(qi..).next().unwrap();
+                right = spans.remove(&key).unwrap();
+                let length = right + 1 - key;
+                *lengths.entry(length).or_insert(0) -= 1;
+                if lengths[&length] == 0 {
+                    lengths.remove(&length);
+                }
+            }
+            spans.insert(left, right);
+            *lengths.entry(right + 1 - left).or_insert(0) += 1;
         }
+        res.push(*lengths.keys().last().unwrap() as i32);
     }
     res
-}
-
-fn sieve(n: i32) -> Vec<i32> {
-    let mut primes = vec![true; 1 + n as usize];
-    primes[0..2].copy_from_slice(&[false; 2]);
-    for p in 2..=(n as f64).sqrt() as usize {
-        if primes[p] {
-            for i in (p * p..=n as usize).step_by(p) {
-                primes[i] = false;
-            }
-        }
-    }
-    primes
-        .into_iter()
-        .enumerate()
-        .filter_map(|(i, v)| if v { Some(i as i32) } else { None })
-        .collect()
 }
 
 #[cfg(test)]
@@ -70,12 +98,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(closest_primes(10, 19), [11, 13]);
-        assert_eq!(closest_primes(4, 6), [-1, -1]);
+        assert_eq!(
+            longest_repeating("babacc".into(), "bcb", &[1, 3, 3]),
+            [3, 3, 4]
+        );
+        assert_eq!(longest_repeating("abyzz".into(), "aa", &[2, 1]), [2, 3]);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(closest_primes(19, 31), [29, 31]);
-    }
+    fn test() {}
 }
