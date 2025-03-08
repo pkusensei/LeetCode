350cbc72b85474085290164ed38ee56fa276f539
pkusensei <ZW_08@outlook.com>
2025-03-07 19:38:44 -0500

    2379 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,39 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_ways(s: &str) -> i64 {
-    let prefix = s.bytes().fold(vec![], |mut acc, b| {
-        acc.push(i64::from(b - b'0') + acc.last().unwrap_or(&0));
-        acc
-    });
-    let (s, n) = (s.as_bytes(), s.len());
-    let mut res = 0;
-    for i in 1..n - 1 {
-        res += if s[i] == b'0' {
-            let left = prefix[i - 1];
-            let right = prefix[n - 1] - prefix[i];
-            left * right
-        } else {
-            let left = i as i64 - prefix[i - 1];
-            let right = (n - i - 1) as i64 - (prefix[n - 1] - prefix[i]);
-            left * right
+pub fn minimum_recolors(blocks: &str, k: i32) -> i32 {
+    let mut res = k;
+    let k = k as usize;
+    let s = blocks.as_bytes();
+    let mut curr = 0;
+    for (idx, &b) in s.iter().enumerate() {
+        curr += i32::from(b == b'W');
+        if idx >= k {
+            curr -= i32::from(s[idx - k] == b'W');
         }
-    }
-    res
-}
-
-pub fn one_pass(s: &str) -> i64 {
-    let mut res = 0;
-    let [mut one, mut zero, mut zero_one, mut one_zero] = [0; 4];
-    for b in s.bytes() {
-        if b == b'0' {
-            zero += 1;
-            one_zero += one;
-            res += zero_one;
-        } else {
-            one += 1;
-            zero_one += zero;
-            res += one_zero;
+        if idx >= k - 1 {
+            res = res.min(curr);
         }
     }
     res
@@ -74,11 +53,7 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(number_of_ways("001101"), 6);
-        assert_eq!(number_of_ways("11100"), 0);
-
-        assert_eq!(one_pass("001101"), 6);
-        assert_eq!(one_pass("11100"), 0);
+        assert_eq!(minimum_recolors("WBWBBBW", 2), 0);
     }
 
     #[test]
