0b3f85d301b7036ad462cf384d8a92001909bd31
pkusensei <ZW_08@outlook.com>
2025-03-09 14:39:24 -0400

    2251 line sweep

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_rectangles(rectangles: Vec<Vec<i32>>, points: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut recs = vec![vec![]; 101];
-    for rect in rectangles.iter() {
-        let [x, y] = rect[..] else { unreachable!() };
-        recs[y as usize].push(x);
+pub fn full_bloom_flowers(flowers: &[[i32; 2]], people: &[i32]) -> Vec<i32> {
+    let mut map = std::collections::BTreeMap::new();
+    for flo in flowers.iter() {
+        *map.entry(flo[0]).or_insert(0) += 1;
+        *map.entry(1 + flo[1]).or_insert(0) -= 1;
     }
-    for r in recs.iter_mut() {
-        r.sort_unstable_by(|a, b| b.cmp(a));
+    let mut curr = 0;
+    for v in map.values_mut() {
+        curr += *v;
+        *v = curr;
     }
-    points
+    people
         .iter()
-        .map(|p| {
-            let [x, y] = p[..] else { unreachable!() };
-            recs[y as usize..]
-                .iter()
-                .map(|r| r.partition_point(|&v| v <= x))
-                .sum::<usize>() as i32
-        })
+        .map(|&p| map.range(..=p).next_back().map(|i| *i.1).unwrap_or(0))
         .collect()
 }
 
@@ -56,7 +52,12 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            full_bloom_flowers(&[[1, 6], [3, 7], [9, 12], [4, 13]], &[2, 3, 7, 11]),
+            [1, 2, 2, 2]
+        );
+    }
 
     #[test]
     fn test() {}
