e52195bb95c9dfffa16b1f697ec6be05efbd02dd
pkusensei <ZW_08@outlook.com>
2025-03-09 12:11:33 -0400

    2248 count==n

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,37 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_path(parent: &[i32], s: &str) -> i32 {
-    let (s, n) = (s.as_bytes(), s.len());
-    let adj = parent
-        .iter()
-        .enumerate()
-        .fold(vec![vec![]; n], |mut acc, (i, &p)| {
-            if p >= 0 {
-                acc[p as usize].push(i);
-            }
+pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+    let n = nums.len();
+    let mut res: Vec<_> = nums
+        .into_iter()
+        .flatten()
+        .fold(std::collections::HashMap::new(), |mut acc, num| {
+            *acc.entry(num).or_insert(0) += 1;
             acc
-        });
-    let mut res = 0;
-    dfs(&adj, s, 0, &mut res);
+        })
+        .into_iter()
+        .filter_map(|(i, v)| if v == n { Some(i) } else { None })
+        .collect();
+    res.sort_unstable();
     res
-}
-
-fn dfs(adj: &[Vec<usize>], s: &[u8], node: usize, res: &mut i32) -> i32 {
-    let [mut max1, mut max2] = [0, 0];
-    for &next in adj[node].iter() {
-        let temp = dfs(adj, s, next, res);
-        if s[node] != s[next] {
-            if temp > max1 {
-                max2 = max1;
-                max1 = temp;
-            } else if temp > max2 {
-                max2 = temp;
-            }
-        }
-    }
-    *res = (*res).max(1 + max1 + max2);
-    1 + max1
 }
 
 #[cfg(test)]
@@ -68,10 +51,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(longest_path(&[-1, 0, 0, 1, 1, 2], "abacbe"), 3);
-        assert_eq!(longest_path(&[-1, 0, 0, 0], "aabc"), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
