3a23468a234eb323d8884b41e55a6e16703c88ac
pkusensei <ZW_08@outlook.com>
2025-03-08 21:14:30 -0500

    2243 chunks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,33 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_score(scores: &[i32], edges: &[[i32; 2]]) -> i32 {
-    use std::{cmp::Reverse, collections::BinaryHeap};
-    let n = scores.len();
-    let mut heaps = vec![BinaryHeap::with_capacity(4); n];
-    for e in edges.iter() {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        heaps[a].push((Reverse(scores[b]), b));
-        heaps[b].push((Reverse(scores[a]), a));
-        if heaps[a].len() > 3 {
-            heaps[a].pop();
-        }
-        if heaps[b].len() > 3 {
-            heaps[b].pop();
-        }
-    }
-    let mut res = -1;
-    for e in edges.iter() {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        for &(Reverse(s1), n1) in heaps[a].iter() {
-            for &(Reverse(s2), n2) in heaps[b].iter() {
-                if n1 != n2 && n1 != b && n2 != a {
-                    res = res.max(scores[a] + scores[b] + s1 + s2);
+pub fn digit_sum(s: String, k: i32) -> String {
+    let k = k as usize;
+    let mut s: Vec<_> = s.bytes().map(|b| u16::from(b - b'0')).collect();
+    while s.len() > k {
+        s = s
+            .chunks(k)
+            .flat_map(|w| {
+                let mut sum: u16 = w.iter().sum();
+                let mut digits = vec![];
+                while sum > 0 {
+                    digits.push(sum % 10);
+                    sum /= 10;
                 }
-            }
-        }
+                if digits.is_empty() {
+                    digits.push(0);
+                }
+                digits.into_iter().rev()
+            })
+            .collect();
     }
-    res
+    s.into_iter().map(|v| char::from(v as u8 + b'0')).collect()
 }
 
 #[cfg(test)]
@@ -64,19 +58,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            maximum_score(
-                &[5, 2, 9, 8, 4],
-                &[[0, 1], [1, 2], [2, 3], [0, 2], [1, 3], [2, 4]]
-            ),
-            24
-        );
-        assert_eq!(
-            maximum_score(&[9, 20, 6, 4, 11, 12], &[[0, 3], [5, 3], [2, 4], [1, 3]]),
-            -1
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
