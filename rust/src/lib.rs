5db6d71a4101c7047bd73538644cf993572bdd0d
pkusensei <ZW_08@outlook.com>
2025-03-10 15:51:56 -0400

    2279 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,10 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn percentage_letter(s: String, letter: char) -> i32 {
-    let n = s.len();
-    let c = s.chars().filter(|&v| v == letter).count();
-    (100 * c / n) as i32
+pub fn maximum_bags(capacity: Vec<i32>, rocks: Vec<i32>, mut additional_rocks: i32) -> i32 {
+    let mut deltas: Vec<_> = capacity
+        .iter()
+        .zip(rocks.iter())
+        .map(|(c, r)| c - r)
+        .collect();
+    deltas.sort_unstable();
+    let mut res = 0;
+    for &num in deltas.iter() {
+        if additional_rocks >= num {
+            additional_rocks -= num;
+            res += 1;
+        } else {
+            break;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
