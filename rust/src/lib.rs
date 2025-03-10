edd61bdc011bfda9ecd4a5bd0ac56c0b56207715
pkusensei <ZW_08@outlook.com>
2025-03-10 10:51:40 -0400

    2266 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_good_integer(num: String) -> String {
-    num.as_bytes()
-        .windows(3)
-        .filter_map(|w| {
-            if w[0] == w[1] && w[1] == w[2] {
-                Some(w)
-            } else {
-                None
+pub fn count_texts(pressed_keys: &str) -> i32 {
+    let (s, n) = (pressed_keys.as_bytes(), pressed_keys.len());
+    // dfs(s, 0, &mut vec![-1; n])
+    let mut dp = vec![0; 1 + n];
+    dp[n] = 1;
+    for idx in (0..n).rev() {
+        let len = if matches!(s[idx], b'7' | b'9') { 4 } else { 3 };
+        for i in idx..(idx + len).min(n) {
+            if s[i] != s[idx] {
+                break;
             }
-        })
-        .max()
-        .map(|w| w.iter().map(|b| char::from(*b)).collect())
-        .unwrap_or_default()
+            dp[idx] += dp[1 + i];
+            dp[idx] %= MOD;
+        }
+    }
+    dp[0]
+}
+
+const MOD: i32 = 1_000_000_007;
+
+fn dfs(s: &[u8], idx: usize, memo: &mut [i32]) -> i32 {
+    let n = s.len();
+    if idx >= n {
+        return 1;
+    }
+    if memo[idx] > -1 {
+        return memo[idx];
+    }
+    let mut res = 0;
+    let len = if matches!(s[idx], b'7' | b'9') { 4 } else { 3 };
+    for i in idx..(idx + len).min(n) {
+        if s[i] != s[idx] {
+            break;
+        }
+        res += dfs(s, 1 + i, memo);
+        res %= MOD;
+    }
+    memo[idx] = res;
+    res
 }
 
 #[cfg(test)]
@@ -50,7 +76,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_texts("22233"), 8);
+        assert_eq!(
+            count_texts("222222222222222222222222222222222222"),
+            82876089
+        );
+    }
 
     #[test]
     fn test() {}
