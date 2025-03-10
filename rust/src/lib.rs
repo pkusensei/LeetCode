d22582e6152c7a5282fccc160fbb7421f8b2fac0
pkusensei <ZW_08@outlook.com>
2025-03-10 15:41:08 -0400

    2276 apparently O(n) sum tles's

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,19 +2,47 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::BTreeMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_combination(candidates: Vec<i32>) -> i32 {
-    let mut res = 0;
-    for bit in 0..=24 {
-        let mut curr = 0;
-        for &num in candidates.iter() {
-            curr += (num >> bit) & 1
-        }
-        res = res.max(curr)
+#[derive(Default)]
+struct CountIntervals {
+    data: BTreeMap<i32, i32>,
+    count: i32,
+}
+
+impl CountIntervals {
+    fn new() -> Self {
+        Default::default()
     }
-    res
+
+    fn add(&mut self, mut left: i32, mut right: i32) {
+        while let Some((&k, &v)) = self.data.range(left..).next() {
+            if right < k {
+                break;
+            }
+            right = right.max(v);
+            self.data.remove(&k);
+            self.count -= v - k + 1;
+        }
+        while let Some((&k, &v)) = self.data.range(..right).next_back() {
+            if v < left {
+                break;
+            }
+            left = left.min(k);
+            right = right.max(v);
+            self.data.remove(&k);
+            self.count -= v - k + 1;
+        }
+        self.data.insert(left, right);
+        self.count += right - left + 1;
+    }
+
+    fn count(&self) -> i32 {
+        self.count // O(n) sum TLE's
+    }
 }
 
 #[cfg(test)]
@@ -47,7 +75,16 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        let mut countIntervals = CountIntervals::new();
+        countIntervals.add(2, 3); // add [2, 3] to the set of intervals.
+        countIntervals.add(7, 10); // add [7, 10] to the set of intervals.
+        assert_eq!(countIntervals.count(), 6);
+        // the integers 2 and 3 are present in the interval [2, 3].
+        // the integers 7, 8, 9, and 10 are present in the interval [7, 10].
+        countIntervals.add(5, 8); // add [5, 8] to the set of intervals.
+        assert_eq!(countIntervals.count(), 8);
+    }
 
     #[test]
     fn test() {}
