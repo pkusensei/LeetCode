74803827a0085c9d62858d3dd96ec3c8b7290096
pkusensei <ZW_08@outlook.com>
2025-03-10 09:49:30 -0400

    2265 subtree_sum/count == node.val

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,33 +6,19 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode CreateBinaryTree(int[][] descriptions)
+    public int AverageOfSubtree(TreeNode root)
     {
-        Dictionary<int, List<(int val, bool is_left)>> adj = [];
-        HashSet<int> nodes = []; // nodes with parent
-        foreach (var desc in descriptions)
-        {
-            var parent = desc[0];
-            var child = desc[1];
-            bool is_left = desc[2] == 1;
-            if (!adj.TryAdd(parent, [(child, is_left)])) { adj[parent].Add((child, is_left)); }
-            nodes.Add(child);
-        }
-        int root = adj.Keys.Where(k => !nodes.Contains(k)).Single();
-        return Dfs(root);
+        return Dfs(root).res;
 
-        TreeNode Dfs(int val)
+        static (int sum, int count, int res) Dfs(TreeNode node)
         {
-            TreeNode node = new(val);
-            if (adj.TryGetValue(val, out var nodes))
-            {
-                foreach (var item in nodes)
-                {
-                    if (item.is_left) { node.left = Dfs(item.val); }
-                    else { node.right = Dfs(item.val); }
-                }
-            }
-            return node;
+            if (node is null) { return (0, 0, 0); }
+            var left = Dfs(node.left);
+            var right = Dfs(node.right);
+            var sum = node.val + left.sum + right.sum;
+            var count = 1 + left.count + right.count;
+            var curr = sum / count == node.val ? 1 : 0;
+            return (sum, count, curr + left.res + right.res);
         }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,15 +12,15 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        int[][] des = [[20, 15, 1], [20, 17, 0], [50, 20, 1], [50, 80, 0], [80, 19, 1]];
-        Assert.AreEqual("[50,20,80,15,17,19]", sol.CreateBinaryTree(des).ToString());
+        var a = TreeNode.Make([4, 8, 5, 0, 1, null, 6]);
+        Assert.AreEqual(5, sol.AverageOfSubtree(a));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        int[][] des = [[1, 2, 1], [2, 3, 0], [3, 4, 1]];
-        Assert.AreEqual("[1,2,null,null,3,4]", sol.CreateBinaryTree(des).ToString());
+        var a = TreeNode.Make([1]);
+        Assert.AreEqual(1, sol.AverageOfSubtree(a));
     }
 
     [TestMethod]
