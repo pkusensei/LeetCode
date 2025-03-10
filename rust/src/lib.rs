c5ea3b09b5140078b90b136b7cd81a99313ab643
pkusensei <ZW_08@outlook.com>
2025-03-09 21:02:56 -0400

    2259

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,39 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_of_substrings(word: &str, k: i32) -> i64 {
-    fn at_least_k(s: &[u8], k: i32) -> i64 {
-        let n = s.len();
-        let mut res = 0;
-        let mut left = 0;
-        let mut vows = [0; 5];
-        let mut cons = 0;
-        for (right, &b) in s.iter().enumerate() {
-            match b {
-                b'a' => vows[0] += 1,
-                b'e' => vows[1] += 1,
-                b'i' => vows[2] += 1,
-                b'o' => vows[3] += 1,
-                b'u' => vows[4] += 1,
-                _ => cons += 1,
-            }
-            while vows.iter().all(|&v| v > 0) && cons >= k {
-                res += n - right;
-                match s[left] {
-                    b'a' => vows[0] -= 1,
-                    b'e' => vows[1] -= 1,
-                    b'i' => vows[2] -= 1,
-                    b'o' => vows[3] -= 1,
-                    b'u' => vows[4] -= 1,
-                    _ => cons -= 1,
-                }
-                left += 1;
-            }
-        }
-        res as _
+pub fn remove_digit(number: String, digit: char) -> String {
+    let s = number.as_bytes().to_vec();
+    let mut res = vec![];
+    for i in number
+        .char_indices()
+        .filter_map(|(i, c)| if c == digit { Some(i) } else { None })
+    {
+        let mut curr = s.clone();
+        curr.remove(i);
+        res = res.max(curr);
     }
-
-    at_least_k(word.as_bytes(), k) - at_least_k(word.as_bytes(), 1 + k)
+    String::from_utf8(res).unwrap()
 }
 
 #[cfg(test)]
@@ -70,15 +49,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_of_substrings("aeioqq", 1), 0);
-        assert_eq!(count_of_substrings("aeiou", 0), 1);
-        assert_eq!(count_of_substrings("ieaouqqieaouqq", 1), 3);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(count_of_substrings("iqeaouqi", 2), 3);
-        assert_eq!(count_of_substrings("aadieuoh", 1), 2);
-    }
+    fn test() {}
 }
