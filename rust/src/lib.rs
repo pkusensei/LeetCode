6f8691c502c5e787f9c269721b26a4086ceace85
pkusensei <ZW_08@outlook.com>
2025-03-10 12:43:53 -0400

    2271 prefix sum and binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,14 +5,26 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn divisor_substrings(num: i32, k: i32) -> i32 {
-    let s = num.to_string();
-    let k = k as usize;
-    let n = s.len();
+pub fn maximum_white_tiles(tiles: &mut [[i32; 2]], carpet_len: i32) -> i32 {
+    tiles.sort_unstable();
+    let prefix = tiles.iter().fold(vec![], |mut acc, t| {
+        acc.push(t[1] - t[0] + 1 + acc.last().unwrap_or(&0));
+        acc
+    });
     let mut res = 0;
-    for i in 0..=n - k {
-        let val = s[i..i + k].parse::<i32>().unwrap();
-        res += i32::from(val > 0 && num % val == 0);
+    for (idx, tile) in tiles.iter().enumerate() {
+        let [left, right] = tile[..] else {
+            unreachable!()
+        };
+        // Put on left
+        let i = tiles.partition_point(|sp| sp[0] < left + carpet_len);
+        if i == 1 + idx {
+            res = res.max((1 + right - left).min(carpet_len));
+        } else {
+            let mut curr = prefix[i - 2] - if idx > 0 { prefix[idx - 1] } else { 0 };
+            curr += (left + carpet_len).min(1 + tiles[i - 1][1]) - tiles[i - 1][0];
+            res = res.max(curr);
+        }
     }
     res
 }
@@ -47,7 +59,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            maximum_white_tiles(&mut [[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10),
+            9
+        );
+        assert_eq!(maximum_white_tiles(&mut [[10, 11], [1, 1]], 2), 2);
+    }
 
     #[test]
     fn test() {}
