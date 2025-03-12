5dced87faecafa96a8c4ca4d9c724cc97e9e930d
pkusensei <ZW_08@outlook.com>
2025-03-12 15:49:19 -0400

    2305 backtrack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,26 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_path_cost(grid: Vec<Vec<i32>>, move_cost: Vec<Vec<i32>>) -> i32 {
-    let mut prev = grid[0].clone();
-    let n = prev.len();
-    for (r, row) in grid.iter().enumerate().skip(1) {
-        let mut curr = vec![i32::MAX; n];
-        for (c, p) in prev.iter().enumerate() {
-            for i in 0..n {
-                let mc = move_cost[grid[r - 1][c] as usize][i];
-                curr[i] = curr[i].min(p + mc + row[i]);
+pub fn distribute_cookies(cookies: &[i32], k: i32) -> i32 {
+    let k = k as usize;
+    let mut buckets = vec![0; k];
+    backtrack(cookies, &mut buckets)
+}
+
+fn backtrack(cookies: &[i32], buckets: &mut [i32]) -> i32 {
+    match cookies {
+        [] => *buckets.iter().max().unwrap(),
+        [head, tail @ ..] => {
+            let k = buckets.len();
+            let mut res = i32::MAX;
+            for i in 0..k {
+                buckets[i] += head;
+                res = res.min(backtrack(tail, buckets));
+                buckets[i] -= head;
             }
+            res
         }
-        prev = curr
     }
-    prev.into_iter().min().unwrap_or(0)
 }
 
 #[cfg(test)]
@@ -52,20 +58,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            min_path_cost(
-                vec![vec![5, 3], vec![4, 0], vec![2, 1]],
-                vec![
-                    vec![9, 8],
-                    vec![1, 5],
-                    vec![10, 12],
-                    vec![18, 6],
-                    vec![2, 4],
-                    vec![14, 3]
-                ]
-            ),
-            17
-        );
+        assert_eq!(distribute_cookies(&[8, 15, 10, 20, 8], 2), 31);
+        assert_eq!(distribute_cookies(&[6, 1, 3, 2, 2, 4, 1, 2], 3), 7);
     }
 
     #[test]
