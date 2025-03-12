c81139715ced0c776fa69efe04c424cdb01c4702
pkusensei <ZW_08@outlook.com>
2025-03-12 11:05:45 -0400

    2295

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn partition_array(mut nums: Vec<i32>, k: i32) -> i32 {
-    nums.sort_unstable();
-    let mut left = 0;
-    let mut res = 1;
-    for (right, &num) in nums.iter().enumerate() {
-        if num - nums[left] > k {
-            res += 1;
-            left = right
-        }
+pub fn array_change(nums: Vec<i32>, operations: Vec<Vec<i32>>) -> Vec<i32> {
+    use std::collections::HashMap;
+    let n = nums.len();
+    let mut map = nums
+        .iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut acc, (i, &num)| {
+            acc.insert(num, i);
+            acc
+        });
+    for op in operations.iter() {
+        let [a, b] = op[..] else { unreachable!() };
+        let i = map.remove(&a).unwrap();
+        map.insert(b, i);
+    }
+    let mut res = vec![0; n];
+    for (num, i) in map.into_iter() {
+        res[i] = num;
     }
     res
 }
