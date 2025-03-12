b5a395da62824c87e54761fd248ff08875815a6d
pkusensei <ZW_08@outlook.com>
2025-03-12 10:59:01 -0400

    2294 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_max_game(mut nums: Vec<i32>) -> i32 {
-    while nums.len() > 1 {
-        let mut curr = Vec::with_capacity(nums.len() / 2);
-        for (i, ch) in nums.chunks(2).enumerate() {
-            if i & 1 == 0 {
-                curr.push(ch[0].min(ch[1]));
-            } else {
-                curr.push(ch[0].max(ch[1]));
-            }
+pub fn partition_array(mut nums: Vec<i32>, k: i32) -> i32 {
+    nums.sort_unstable();
+    let mut left = 0;
+    let mut res = 1;
+    for (right, &num) in nums.iter().enumerate() {
+        if num - nums[left] > k {
+            res += 1;
+            left = right
         }
-        nums = curr;
     }
-    nums[0]
+    res
 }
 
 #[cfg(test)]
