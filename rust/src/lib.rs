c76b9965bf5dea5f17e338dba42638e4ab9fcc3e
pkusensei <ZW_08@outlook.com>
2025-03-16 12:54:23 -0400

    2359 bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,34 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_groups(grades: Vec<i32>) -> i32 {
-    let n = grades.len();
-    let mut left = 1;
-    let mut right = n;
-    while left < right {
-        let mid = (left + right) / 2;
-        if mid * (1 + mid) / 2 > n {
-            right = mid - 1
-        } else {
-            left = mid
+pub fn closest_meeting_node(edges: &[i32], node1: i32, node2: i32) -> i32 {
+    let a = bfs(edges, node1 as _);
+    let b = bfs(edges, node2 as _);
+    a.into_iter()
+        .zip(b)
+        .enumerate()
+        .filter(|&(_i, (x, y))| x > -1 && y > -1)
+        .min_by_key(|&(_i, (x, y))| x.max(y))
+        .map(|(i, _)| i as i32)
+        .unwrap_or(-1)
+}
+
+fn bfs(edges: &[i32], start: usize) -> Vec<i32> {
+    use std::collections::VecDeque;
+    let n = edges.len();
+    let mut res = vec![-1; n];
+    let mut queue = VecDeque::from([(start, 0)]);
+    res[start] = 0;
+    while let Some((node, dist)) = queue.pop_front() {
+        if edges[node] > -1 {
+            let next = edges[node] as usize;
+            if res[next] == -1 {
+                res[next] = 1 + dist;
+                queue.push_back((next, 1 + dist));
+            }
         }
     }
-    left as i32
+    res
 }
 
 #[cfg(test)]
