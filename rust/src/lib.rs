fe7cf9f23b81e9efa7509b3cbc24081edc7855de
pkusensei <ZW_08@outlook.com>
2025-03-16 16:02:45 -0400

    2367

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,13 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_replacement(nums: &[i32]) -> i64 {
-    let n = nums.len();
-    let mut last = nums[n - 1];
+pub fn arithmetic_triplets(nums: Vec<i32>, diff: i32) -> i32 {
     let mut res = 0;
-    for &num in nums.iter().rev().skip(1) {
-        if num <= last {
-            last = num;
-        } else {
-            let div = num / last + i32::from(num % last > 0);
-            last = num / div;
-            res += i64::from(div - 1);
+    for &num in nums.iter() {
+        if nums.binary_search(&(num + diff)).is_ok()
+            && nums.binary_search(&(num + diff * 2)).is_ok()
+        {
+            res += 1;
         }
     }
     res
@@ -51,13 +47,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_replacement(&[3, 9, 3]), 2);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(minimum_replacement(&[12, 9, 7, 6, 17, 19, 21]), 6);
-        assert_eq!(minimum_replacement(&[2, 10, 20, 19, 1]), 47);
-    }
+    fn test() {}
 }
