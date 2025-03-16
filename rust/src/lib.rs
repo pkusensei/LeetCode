bf5949b556c737b424fe5685b11e551f4f1f8789
pkusensei <ZW_08@outlook.com>
2025-03-16 11:23:58 -0400

    2353 deep down the layers...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,49 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
-    let n = grid.len();
-    let mut res = 0;
-    for row in grid.iter() {
-        for c in 0..n {
-            if row.iter().zip(0..n).all(|(&v1, r)| v1 == grid[r][c]) {
-                res += 1;
-            }
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+#[derive(Default)]
+struct FoodRatings {
+    f_cr: HashMap<String, (String, i32)>,
+    c_r_f: HashMap<String, BTreeMap<i32, BTreeSet<String>>>,
+}
+
+impl FoodRatings {
+    fn new(foods: Vec<String>, cuisines: Vec<String>, ratings: Vec<i32>) -> Self {
+        let mut f_cr = HashMap::new();
+        let mut c_r_f: HashMap<String, BTreeMap<i32, BTreeSet<String>>> = HashMap::new();
+        for (f, (c, r)) in foods.into_iter().zip(cuisines.into_iter().zip(ratings)) {
+            f_cr.insert(f.clone(), (c.clone(), r));
+            c_r_f.entry(c).or_default().entry(r).or_default().insert(f);
         }
+        Self { f_cr, c_r_f }
     }
-    res
+
+    fn change_rating(&mut self, food: String, new_rating: i32) {
+        let (c, old_r) = self.f_cr.remove(&food).unwrap();
+        let r_f = self.c_r_f.get_mut(&c).unwrap();
+        let set = r_f.get_mut(&old_r).unwrap();
+        set.remove(&food);
+        if set.is_empty() {
+            r_f.remove(&old_r);
+        }
+        self.f_cr.insert(food.clone(), (c.clone(), new_rating));
+        self.c_r_f
+            .entry(c)
+            .or_default()
+            .entry(new_rating)
+            .or_default()
+            .insert(food);
+    }
+
+    fn highest_rated(&self, cuisine: String) -> String {
+        self.c_r_f
+            .get(&cuisine)
+            .and_then(|r_f| r_f.values().last())
+            .and_then(|set| set.first())
+            .map(|s| s.to_string())
+            .unwrap_or_default()
+    }
 }
 
 #[cfg(test)]
