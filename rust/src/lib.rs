c10b595ff995088593568a8606715e1716db1e5a
pkusensei <ZW_08@outlook.com>
2025-03-15 20:23:27 -0400

    2338 nope

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,38 +2,28 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashSet},
-};
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct SmallestInfiniteSet {
-    heap: BinaryHeap<Reverse<i32>>,
-    out: HashSet<i32>,
-}
-
-impl SmallestInfiniteSet {
-    fn new() -> Self {
-        Self {
-            heap: (1..=1000).map(Reverse).collect(),
-            out: HashSet::new(),
+pub fn ideal_arrays(n: i32, max_value: i32) -> i32 {
+    use std::collections::HashMap;
+    const MOD: i64 = 1_000_000_007;
+    let mut freq: HashMap<_, i64> = (1..=max_value).map(|v| (v, 1)).collect();
+    let mut res = 0;
+    let mut combi = 1;
+    for k in 0..n {
+        let mut curr = HashMap::new();
+        for (&num, &count) in freq.iter() {
+            for val in (num * 2..=max_value).step_by(num as usize) {
+                *curr.entry(val).or_insert(0) += count;
+            }
         }
+        res += combi * freq.values().sum::<i64>() % MOD;
+        res %= MOD;
+        combi = (combi * i64::from(n - k - 1)) % MOD * mod_pow((1 + k).into(), MOD - 2, MOD) % MOD;
+        freq = curr;
     }
-
-    fn pop_smallest(&mut self) -> i32 {
-        let val = self.heap.pop().unwrap().0;
-        self.out.insert(val);
-        val
-    }
-
-    fn add_back(&mut self, num: i32) {
-        if self.out.remove(&num) {
-            self.heap.push(Reverse(num));
-        }
-    }
+    (res % MOD) as i32
 }
 
 #[cfg(test)]
@@ -66,8 +56,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(ideal_arrays(2, 5), 10);
+        assert_eq!(ideal_arrays(5, 3), 11);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(ideal_arrays(5878, 2900), 465040898);
+    }
 }
