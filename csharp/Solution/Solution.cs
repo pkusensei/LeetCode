7aa06fa43bb0746ab07f2b230b48d2c1fcb70285
pkusensei <ZW_08@outlook.com>
2025-03-17 11:25:13 -0400

    2385 flatten and bfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,6 +6,39 @@ namespace Solution;
 
 public class Solution
 {
-    public bool DivideArray(int[] nums)
-    => nums.GroupBy(x => x).Select(g => g.Count()).All(v => (v & 1) == 0);
+    public int AmountOfTime(TreeNode root, int start)
+    {
+        Dictionary<int, List<int>> adj = [];
+        Dfs(root, null);
+        Queue<(int node, int dist)> queue = [];
+        queue.Enqueue((start, 0));
+        int res = 0;
+        HashSet<int> seen = [start];
+        while (queue.TryDequeue(out var item))
+        {
+            res = Math.Max(res, item.dist);
+            foreach (var next in adj[item.node])
+            {
+                if (seen.Add(next)) { queue.Enqueue((next, 1 + item.dist)); }
+            }
+        }
+        return res;
+
+        void Dfs(TreeNode node, TreeNode parent)
+        {
+            if (node is null) { return; }
+            adj.TryAdd(node.val, []);
+            if (parent is not null) { adj[node.val].Add(parent.val); }
+            if (node.left is not null)
+            {
+                adj[node.val].Add(node.left.val);
+                Dfs(node.left, node);
+            }
+            if (node.right is not null)
+            {
+                adj[node.val].Add(node.right.val);
+                Dfs(node.right, node);
+            }
+        }
+    }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,11 +12,15 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
+        var a = TreeNode.Make([1, 5, 3, null, 4, 10, 6, 9, 2]);
+        Assert.AreEqual(4, sol.AmountOfTime(a, 3));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        var a = TreeNode.Make([1]);
+        Assert.AreEqual(0, sol.AmountOfTime(a, 1));
     }
 
     [TestMethod]
