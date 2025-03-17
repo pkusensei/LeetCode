3479354c1413995bd228de29a39f900651943da2
pkusensei <ZW_08@outlook.com>
2025-03-17 14:53:06 -0400

    2391 prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,32 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn remove_stars(s: String) -> String {
-    let mut res = vec![];
-    for b in s.bytes() {
-        if b == b'*' {
-            res.pop();
-        } else {
-            res.push(b);
+pub fn garbage_collection(garbage: Vec<String>, travel: Vec<i32>) -> i32 {
+    let [mut mcount, mut pcount, mut gcount] = [0; 3];
+    let [mut m_right, mut p_right, mut g_right] = [0; 3];
+    let prefix = travel.iter().fold(vec![0], |mut acc, &num| {
+        acc.push(num + acc.last().unwrap());
+        acc
+    });
+    for (idx, s) in garbage.iter().enumerate() {
+        for b in s.bytes() {
+            match b {
+                b'M' => {
+                    mcount += 1;
+                    m_right = idx;
+                }
+                b'P' => {
+                    pcount += 1;
+                    p_right = idx;
+                }
+                _ => {
+                    gcount += 1;
+                    g_right = idx
+                }
+            }
         }
     }
-    String::from_utf8(res).unwrap()
+    mcount + prefix[m_right] + pcount + prefix[p_right] + gcount + prefix[g_right]
 }
 
 #[cfg(test)]
