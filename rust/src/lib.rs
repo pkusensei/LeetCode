231de711315d7a0abe489271387394216f2a4e78
pkusensei <ZW_08@outlook.com>
2025-03-17 15:25:03 -0400

    2395

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,61 +2,17 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashMap, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn build_matrix(
-    k: i32,
-    row_conditions: &[[i32; 2]],
-    col_conditions: &[[i32; 2]],
-) -> Vec<Vec<i32>> {
-    let k = k as usize;
-    let rseq = topo_sort(k, row_conditions);
-    let cseq = topo_sort(k, col_conditions);
-    if rseq.is_empty() || cseq.is_empty() {
-        return vec![];
-    }
-    let mut res = vec![vec![0; k]; k];
-    for v in 1..=k as i32 {
-        let r = rseq[&v];
-        let c = cseq[&v];
-        res[r][c] = v;
-    }
-    res
-}
-
-fn topo_sort(k: usize, conds: &[[i32; 2]]) -> HashMap<i32, usize> {
-    let mut indegs = vec![0; 1 + k];
-    let mut adj = vec![vec![]; 1 + k];
-    for c in conds {
-        let [a, b] = [0, 1].map(|i| c[i] as usize);
-        indegs[b] += 1;
-        adj[a].push(b);
-    }
-    let mut queue: VecDeque<_> = indegs
-        .iter()
-        .enumerate()
-        .filter_map(|(node, &deg)| {
-            if deg == 0 && node > 0 {
-                Some(node)
-            } else {
-                None
-            }
-        })
-        .collect();
-    let mut res = HashMap::with_capacity(k);
-    while let Some(node) = queue.pop_front() {
-        res.insert(node as i32, res.len());
-        for &next in adj[node].iter() {
-            indegs[next] -= 1;
-            if indegs[next] == 0 {
-                queue.push_back(next);
-            }
+pub fn find_subarrays(nums: Vec<i32>) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    for w in nums.windows(2) {
+        if !seen.insert(w[0] + w[1]) {
+            return true;
         }
     }
-    if res.len() < k { HashMap::new() } else { res }
+    false
 }
 
 #[cfg(test)]
@@ -89,13 +45,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            build_matrix(3, &[[1, 2], [3, 2]], &[[2, 1], [3, 2]]),
-            [[0, 0, 1], [3, 0, 0], [0, 2, 0]]
-        );
-        assert!(build_matrix(3, &[[1, 2], [2, 3], [3, 1], [2, 3]], &[[2, 1]]).is_empty());
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
