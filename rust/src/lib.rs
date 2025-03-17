de3f43deb6e6be0fdc7b00ea50ccb7aa88b97d04
pkusensei <ZW_08@outlook.com>
2025-03-17 15:21:45 -0400

    2392 topo sort, twice

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,35 +2,61 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{HashMap, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn garbage_collection(garbage: Vec<String>, travel: Vec<i32>) -> i32 {
-    let [mut mcount, mut pcount, mut gcount] = [0; 3];
-    let [mut m_right, mut p_right, mut g_right] = [0; 3];
-    let prefix = travel.iter().fold(vec![0], |mut acc, &num| {
-        acc.push(num + acc.last().unwrap());
-        acc
-    });
-    for (idx, s) in garbage.iter().enumerate() {
-        for b in s.bytes() {
-            match b {
-                b'M' => {
-                    mcount += 1;
-                    m_right = idx;
-                }
-                b'P' => {
-                    pcount += 1;
-                    p_right = idx;
-                }
-                _ => {
-                    gcount += 1;
-                    g_right = idx
-                }
+pub fn build_matrix(
+    k: i32,
+    row_conditions: &[[i32; 2]],
+    col_conditions: &[[i32; 2]],
+) -> Vec<Vec<i32>> {
+    let k = k as usize;
+    let rseq = topo_sort(k, row_conditions);
+    let cseq = topo_sort(k, col_conditions);
+    if rseq.is_empty() || cseq.is_empty() {
+        return vec![];
+    }
+    let mut res = vec![vec![0; k]; k];
+    for v in 1..=k as i32 {
+        let r = rseq[&v];
+        let c = cseq[&v];
+        res[r][c] = v;
+    }
+    res
+}
+
+fn topo_sort(k: usize, conds: &[[i32; 2]]) -> HashMap<i32, usize> {
+    let mut indegs = vec![0; 1 + k];
+    let mut adj = vec![vec![]; 1 + k];
+    for c in conds {
+        let [a, b] = [0, 1].map(|i| c[i] as usize);
+        indegs[b] += 1;
+        adj[a].push(b);
+    }
+    let mut queue: VecDeque<_> = indegs
+        .iter()
+        .enumerate()
+        .filter_map(|(node, &deg)| {
+            if deg == 0 && node > 0 {
+                Some(node)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let mut res = HashMap::with_capacity(k);
+    while let Some(node) = queue.pop_front() {
+        res.insert(node as i32, res.len());
+        for &next in adj[node].iter() {
+            indegs[next] -= 1;
+            if indegs[next] == 0 {
+                queue.push_back(next);
             }
         }
     }
-    mcount + prefix[m_right] + pcount + prefix[p_right] + gcount + prefix[g_right]
+    if res.len() < k { HashMap::new() } else { res }
 }
 
 #[cfg(test)]
@@ -63,7 +89,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            build_matrix(3, &[[1, 2], [3, 2]], &[[2, 1], [3, 2]]),
+            [[0, 0, 1], [3, 0, 0], [0, 2, 0]]
+        );
+        assert!(build_matrix(3, &[[1, 2], [2, 3], [3, 1], [2, 3]], &[[2, 1]]).is_empty());
+    }
 
     #[test]
     fn test() {}
