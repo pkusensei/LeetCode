e768d72e819666240dd595dc08d51f5000da09c7
pkusensei <ZW_08@outlook.com>
2025-03-17 19:37:43 -0400

    2400 dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,22 +2,30 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn check_distances(s: &str, distance: &[i32]) -> bool {
-    let mut ids = [None; 26];
-    for (i, b) in s.bytes().enumerate() {
-        let id = usize::from(b - b'a');
-        if let Some(prev) = ids[id] {
-            if (i - prev) as i32 - 1 != distance[id] {
-                return false;
-            }
-        } else {
-            ids[id] = Some(i)
-        }
+pub fn number_of_ways(start_pos: i32, end_pos: i32, k: i32) -> i32 {
+    let diff = (start_pos - end_pos).abs();
+    if diff > k || (diff - k) & 1 == 1 {
+        return 0;
     }
-    true
+    dfs(k, diff, &mut HashMap::new())
+}
+
+fn dfs(k: i32, dist: i32, memo: &mut HashMap<[i32; 2], i32>) -> i32 {
+    if k <= dist {
+        return i32::from(k == dist);
+    }
+    if let Some(&v) = memo.get(&[k, dist]) {
+        return v - 1;
+    }
+    let mut res = 1 + dfs(k - 1, 1 + dist, memo) + dfs(k - 1, (dist - 1).abs(), memo);
+    res %= 1_000_000_007;
+    memo.insert([k, dist], res);
+    res - 1
 }
 
 #[cfg(test)]
@@ -51,12 +59,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(check_distances(
-            "abaccb",
-            &[
-                1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-            ]
-        ));
+        assert_eq!(number_of_ways(1, 2, 3), 3);
+        assert_eq!(number_of_ways(2, 5, 10), 0);
     }
 
     #[test]
