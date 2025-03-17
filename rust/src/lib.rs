539e2418500f50c1e5a99776e843f9993dd64ac9
pkusensei <ZW_08@outlook.com>
2025-03-17 11:12:56 -0400

    2384 use two deques to build palindrome

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_number_of_hours(
-    initial_energy: i32,
-    initial_experience: i32,
-    energy: Vec<i32>,
-    experience: Vec<i32>,
-) -> i32 {
-    let mut res = (energy.iter().sum::<i32>() + 1 - initial_energy).max(0);
-    let mut exp = initial_experience;
-    for &e in experience.iter() {
-        if exp > e {
-            exp += e;
-        } else {
-            let diff = e + 1 - exp;
-            exp += diff + e;
-            res += diff;
+pub fn largest_palindromic(num: &str) -> String {
+    use std::collections::VecDeque;
+    let count = num.bytes().fold([0; 10], |mut acc, b| {
+        acc[usize::from(b - b'0')] += 1;
+        acc
+    });
+    let mut left = VecDeque::new();
+    let mut right = VecDeque::new();
+    for (idx, &c) in count.iter().enumerate().rev() {
+        if c >= 2 {
+            let ch = char::from(idx as u8 + b'0');
+            for _ in 0..c / 2 {
+                left.push_back(ch);
+                right.push_front(ch);
+            }
         }
     }
-    res
+    while left.front().is_some_and(|&v| v == '0') {
+        left.pop_front();
+        right.pop_back();
+    }
+    if let Some(i) = count
+        .iter()
+        .enumerate()
+        .rev()
+        .find_map(|(i, &c)| if c & 1 == 1 { Some(i) } else { None })
+    {
+        let ch = char::from(i as u8 + b'0');
+        left.push_back(ch);
+    }
+    let res: String = left.into_iter().chain(right).collect();
+    if res.is_empty() { "0".into() } else { res }
 }
 
 #[cfg(test)]
@@ -55,7 +69,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(largest_palindromic("444947137"), "7449447");
+        assert_eq!(largest_palindromic("00009"), "9");
+    }
 
     #[test]
     fn test() {}
