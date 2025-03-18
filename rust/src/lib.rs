6ce69e132ee0acdf5a1bb9f5ee00e8fbcd33bba7
Zx Wang <ZW_08@outlook.com>
2025-03-18 10:23:24 -0400

    2402 two heaps

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,39 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_nice_subarray(nums: &[i32]) -> i32 {
-    let mut res = 0;
-    let mut all_bits = 0;
-    let mut left = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        while all_bits & num > 0 {
-            all_bits ^= nums[left];
-            left += 1;
+pub fn most_booked(n: i32, meetings: &mut [[i32; 2]]) -> i32 {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let n = n as usize;
+    meetings.sort_unstable();
+    let mut count = vec![0; n];
+    // min heap by id
+    let mut free: BinaryHeap<_> = (0..n).map(|i| Reverse(i)).collect();
+    // min heap by endtime
+    let mut busy: BinaryHeap<Reverse<(i64, usize)>> = BinaryHeap::new();
+    for meet in meetings.iter() {
+        let [start, end] = [0, 1].map(|i| i64::from(meet[i]));
+        while busy.peek().is_some_and(|Reverse((time, _))| *time <= start) {
+            let Reverse((_, id)) = busy.pop().unwrap();
+            free.push(Reverse(id));
         }
-        all_bits |= num;
-        res = res.max(right + 1 - left);
+        if let Some(Reverse(id)) = free.pop() {
+            count[id] += 1;
+            busy.push(Reverse((i64::from(end), id)));
+        } else {
+            let Reverse((prev, id)) = busy.pop().unwrap();
+            count[id] += 1;
+            busy.push(Reverse((prev + i64::from(end - start), id)));
+        }
     }
-    res as _
+    let mut res = 0;
+    let mut max = 0;
+    for (i, &c) in count.iter().enumerate() {
+        if c > max {
+            res = i as i32;
+            max = c;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -51,10 +71,18 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(longest_nice_subarray(&[1, 3, 8, 48, 10]), 3);
-        assert_eq!(longest_nice_subarray(&[3, 1, 5, 11, 13]), 1);
+        assert_eq!(most_booked(2, &mut [[0, 10], [1, 5], [2, 7], [3, 4]]), 0);
+        assert_eq!(
+            most_booked(3, &mut [[1, 20], [2, 10], [3, 5], [4, 9], [6, 8]]),
+            1
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            most_booked(4, &mut [[18, 19], [3, 12], [17, 19], [2, 13], [7, 10]]),
+            0
+        );
+    }
 }
