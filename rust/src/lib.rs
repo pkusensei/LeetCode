5860c77eec3b844dbfd3c8718adbbece5509aaa6
pkusensei <ZW_08@outlook.com>
2025-03-19 12:44:41 -0400

    2423 edge cases...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,49 +4,28 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-
 use itertools::Itertools;
-use std::collections::HashMap;
 
-pub fn number_of_good_paths(vals: &[i32], edges: &[[i32; 2]]) -> i32 {
-    let n = vals.len();
-    let mut count = vals.iter().map(|&v| HashMap::from([(v, 1)])).collect_vec();
-    let edges = edges
-        .iter()
-        .map(|e| {
-            let [a, b] = [0, 1].map(|i| e[i] as usize);
-            (vals[a].max(vals[b]), a, b)
+pub fn equal_frequency(word: &str) -> bool {
+    let mut it = word
+        .bytes()
+        .fold([0; 26], |mut acc, b| {
+            acc[usize::from(b - b'a')] += 1;
+            acc
         })
-        .sorted_unstable()
-        .collect_vec();
-    let mut dsu = DSU::new(n);
-    let mut res = 0;
-    for &(val, node1, node2) in edges.iter() {
-        let [r1, r2] = [node1, node2].map(|v| dsu.find(v));
-        let [c1, c2] = [r1, r2].map(|v| *count[v].get(&val).unwrap_or(&0));
-        res += c1 * c2;
-        dsu.parent[r2] = r1;
-        count[r1] = HashMap::from([(val, c1 + c2)]);
-    }
-    res + n as i32
-}
-
-struct DSU {
-    parent: Vec<usize>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
+        .into_iter()
+        .filter(|&v| v > 0)
+        .counts()
+        .into_iter();
+    match [it.next(), it.next(), it.next()] {
+        [Some((k1, v1)), Some((k2, v2)), None] => {
+            let mut vals = [[k1, v1], [k2, v2]];
+            vals.sort_unstable();
+            let [[k1, v1], [k2, v2]] = vals;
+            (k1 == 1 && v1 == 1) || (k1 + 1 == k2 && v2 == 1)
         }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v]);
-        }
-        self.parent[v]
+        [Some((k, v)), None, None] => k == 1 || v == 1,
+        _ => false,
     }
 }
 
@@ -81,34 +60,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            number_of_good_paths(&[1, 3, 2, 1, 3], &[[0, 1], [0, 2], [2, 3], [2, 4]]),
-            6
-        );
-        assert_eq!(
-            number_of_good_paths(&[1, 1, 2, 2, 3], &[[0, 1], [1, 2], [2, 3], [2, 4]]),
-            7
-        );
+        assert!(equal_frequency("abcc"));
     }
 
     #[test]
     fn test() {
-        assert_eq!(
-            number_of_good_paths(
-                &[2, 5, 5, 1, 5, 2, 3, 5, 1, 5],
-                &[
-                    [0, 1],
-                    [2, 1],
-                    [3, 2],
-                    [3, 4],
-                    [3, 5],
-                    [5, 6],
-                    [1, 7],
-                    [8, 4],
-                    [9, 7]
-                ]
-            ),
-            20
-        );
+        assert!(equal_frequency("abbcc"));
+        assert!(equal_frequency("zz"));
     }
 }
