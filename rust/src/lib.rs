10ec0f384482ce09977a6eaada7d014d2ba2ab25
pkusensei <ZW_08@outlook.com>
2025-03-19 10:05:10 -0400

    2420 off-by-1's

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,10 +5,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sort_people(names: Vec<String>, heights: Vec<i32>) -> Vec<String> {
-    let mut v: Vec<_> = names.into_iter().zip(heights).collect();
-    v.sort_unstable_by_key(|i| -i.1);
-    v.into_iter().map(|i| i.0).collect()
+pub fn good_indices(nums: &[i32], k: i32) -> Vec<i32> {
+    let mut dec = vec![0];
+    let mut curr = 0;
+    for w in nums.windows(2) {
+        if w[0] >= w[1] {
+            curr += 1;
+        } else {
+            curr = 0;
+        }
+        dec.push(curr);
+    }
+    let mut inc = vec![];
+    curr = 0;
+    for w in nums.windows(2).rev() {
+        if w[0] <= w[1] {
+            curr += 1
+        } else {
+            curr = 0
+        }
+        inc.push(curr);
+    }
+    inc.reverse();
+    inc.push(0);
+    let n = nums.len();
+    let mut res = vec![];
+    if n <= 2 * k as usize {
+        return res;
+    }
+    for i in k as usize..n - k as usize {
+        if dec[i - 1] >= k - 1 && inc[i + 1] >= k - 1 {
+            res.push(i as i32);
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -41,7 +71,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(good_indices(&[2, 1, 1, 1, 3, 4, 1], 2), [2, 3]);
+    }
 
     #[test]
     fn test() {}
