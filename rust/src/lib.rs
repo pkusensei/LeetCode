13ea98f28d6a08cacc5cc015bf86d6d93044f11f
pkusensei <ZW_08@outlook.com>
2025-03-19 09:24:11 -0400

    2418

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,43 +5,10 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_prefix_scores(words: Vec<String>) -> Vec<i32> {
-    let mut trie = Trie::default();
-    for s in words.iter() {
-        trie.insert(s);
-    }
-    words.iter().map(|s| trie.find(s)).collect()
-}
-
-#[derive(Default)]
-struct Trie {
-    nodes: [Option<Box<Trie>>; 26],
-    count: i32,
-}
-
-impl Trie {
-    fn insert(&mut self, s: &str) {
-        let mut curr = self;
-        for b in s.bytes() {
-            let idx = usize::from(b - b'a');
-            curr = curr.nodes[idx].get_or_insert(Box::new(Trie::default()));
-            curr.count += 1;
-        }
-    }
-
-    fn find(&self, s: &str) -> i32 {
-        let mut curr = self;
-        let mut res = curr.count;
-        for b in s.bytes() {
-            let idx = usize::from(b - b'a');
-            let Some(ref node) = curr.nodes[idx] else {
-                return 0;
-            };
-            curr = node;
-            res += curr.count;
-        }
-        res
-    }
+pub fn sort_people(names: Vec<String>, heights: Vec<i32>) -> Vec<String> {
+    let mut v: Vec<_> = names.into_iter().zip(heights).collect();
+    v.sort_unstable_by_key(|i| -i.1);
+    v.into_iter().map(|i| i.0).collect()
 }
 
 #[cfg(test)]
