05185b9849d5800f03167d6267b585b27ac72b01
pkusensei <ZW_08@outlook.com>
2025-03-19 17:55:40 -0400

    2427

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,26 +2,15 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::BTreeMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-// nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff
-// nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff
-pub fn number_of_pairs(nums1: &[i32], nums2: &[i32], diff: i32) -> i64 {
-    let mut res = 0;
-    let mut sorted = Vec::with_capacity(nums1.len());
-    for (&a, &b) in nums1.iter().zip(nums2.iter()) {
-        res += sorted.partition_point(|&v| v <= a - b + diff) as i64;
-        let i = sorted.partition_point(|&v| v < a - b);
-        if i == sorted.len() {
-            sorted.push(a - b);
-        } else {
-            sorted.insert(i, a - b);
-        }
+pub fn common_factors(a: i32, b: i32) -> i32 {
+    const fn gcd(a: i32, b: i32) -> i32 {
+        if a == 0 { b } else { gcd(b % a, a) }
     }
-    res
+    let v = gcd(a, b);
+    (1..=v).filter(|&div| v % div == 0).count() as i32
 }
 
 #[cfg(test)]
@@ -54,10 +43,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_pairs(&[3, 2, 5], &[2, 2, 1], 1), 3);
-        assert_eq!(number_of_pairs(&[3, -1], &[-2, 2], -1), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
