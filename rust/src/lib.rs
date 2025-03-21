717bf27535c4d8dbfa692b2f8d74a9bc9ee2e52d
pkusensei <ZW_08@outlook.com>
2025-03-21 12:00:02 -0400

    2462 two min-heaps

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,31 +5,47 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn make_integer_beautiful(n: i64, target: i32) -> i64 {
+pub fn total_cost(costs: &[i32], k: i32, candidates: i32) -> i64 {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let n = costs.len();
+    let c = candidates as usize;
+    let mut left_heap: BinaryHeap<_> = costs[..c].iter().copied().map(Reverse).collect();
+    let mut right_heap: BinaryHeap<_> = costs[(n - c).max(c)..]
+        .iter()
+        .copied()
+        .map(Reverse)
+        .collect();
     let mut res = 0;
-    let mut num = n;
-    while digit_sum(num) > i64::from(target) {
-        let d = alter(num);
-        res += d;
-        num += d;
-    }
-    res
-}
-
-const fn alter(mut num: i64) -> i64 {
-    let mut pow = 1;
-    while num % 10 == 0 {
-        pow *= 10;
-        num /= 10;
-    }
-    pow * (10 - num % 10)
-}
-
-const fn digit_sum(mut num: i64) -> i64 {
-    let mut res = 0;
-    while num > 0 {
-        res += num % 10;
-        num /= 10;
+    let mut left = c;
+    let mut right = (n - c).max(c) - 1;
+    for _ in 0..k {
+        match [left_heap.peek(), right_heap.peek()] {
+            [Some(&Reverse(a)), Some(&Reverse(b))] if a <= b => {
+                left_heap.pop();
+                res += i64::from(a);
+                if left <= right {
+                    left_heap.push(Reverse(costs[left]));
+                    left += 1;
+                }
+            }
+            [Some(&Reverse(_a)), Some(&Reverse(b))] => {
+                right_heap.pop();
+                res += i64::from(b);
+                if left <= right {
+                    right_heap.push(Reverse(costs[right]));
+                    right -= 1;
+                }
+            }
+            [Some(&Reverse(a)), None] => {
+                left_heap.pop();
+                res += i64::from(a);
+            }
+            [None, Some(&Reverse(b))] => {
+                right_heap.pop();
+                res += i64::from(b);
+            }
+            _ => break,
+        }
     }
     res
 }
@@ -65,8 +81,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(make_integer_beautiful(16, 6), 4);
-        assert_eq!(make_integer_beautiful(467, 6), 33);
+        assert_eq!(total_cost(&[17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4), 11);
+        assert_eq!(total_cost(&[1, 2, 4, 1], 3, 3), 4);
     }
 
     #[test]
