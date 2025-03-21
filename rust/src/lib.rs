254b0913b7f3d1e59fc875a3e110edbdceff8865
pkusensei <ZW_08@outlook.com>
2025-03-21 17:02:06 -0400

    2470

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,36 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn split_message(message: &str, limit: i32) -> Vec<String> {
-    let mut curr = 0;
-    let mut b = 0;
-    while check(message.len() as _, limit as _, b, curr) {
-        b += 1;
-        curr += 1 + b.ilog10(); // length of all indices, i.e all a's
-    }
-    let mut res = vec![];
-    if b > 0 && 3 + 2 * (1 + b.ilog10()) < limit as u32 {
-        let n = message.len();
-        let mut idx = 0;
-        for a in 1..=b {
-            let len = limit as usize - (1 + a.ilog10()) as usize - 3 - (1 + b.ilog10()) as usize;
-            res.push(format!(
-                "{}<{}/{}>",
-                &message[idx..(idx + len).min(n)],
-                a,
-                b
-            ));
-            idx += len;
+pub fn subarray_lcm(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let mut res = 0;
+    for i1 in 0..n {
+        let mut curr = nums[i1];
+        for i2 in i1..n {
+            curr = lcm(curr, nums[i2]);
+            res += i32::from(curr == k);
+            if curr > k {
+                break;
+            }
         }
     }
     res
 }
 
-const fn check(len: u32, limit: u32, b: u32, curr: u32) -> bool {
-    let width = if b == 0 { 1 } else { 1 + b.ilog10() };
-    // len("<b/b>") >= limit => not possible
-    // len + len(all a's) + b*len("</b>") > limit*b => b is too small
-    3 + width * 2 < limit && curr + len + b * (3 + width) > limit * b
+const fn lcm(a: i32, b: i32) -> i32 {
+    const fn gcd(a: i32, b: i32) -> i32 {
+        if a == 0 { b } else { gcd(b % a, a) }
+    }
+    a / gcd(a, b) * b
 }
 
 #[cfg(test)]
@@ -68,29 +59,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            split_message("this is really a very awesome message", 9),
-            [
-                "thi<1/14>",
-                "s i<2/14>",
-                "s r<3/14>",
-                "eal<4/14>",
-                "ly <5/14>",
-                "a v<6/14>",
-                "ery<7/14>",
-                " aw<8/14>",
-                "eso<9/14>",
-                "me<10/14>",
-                " m<11/14>",
-                "es<12/14>",
-                "sa<13/14>",
-                "ge<14/14>"
-            ]
-        );
-        assert_eq!(
-            split_message("short message", 15),
-            ["short mess<1/2>", "age<2/2>"]
-        );
+        assert_eq!(subarray_lcm(&[3, 6, 2, 7, 1], 6), 4);
+        assert_eq!(subarray_lcm(&[3], 2), 0);
     }
 
     #[test]
