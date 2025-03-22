9285bbd0fb3c13dacded1c70364623b0b9926c99
pkusensei <ZW_08@outlook.com>
2025-03-22 09:24:18 -0400

    2472 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,63 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_complete_components(n: i32, edges: &[[i32; 2]]) -> i32 {
-    use std::collections::HashSet;
-    let n = n as usize;
-    let mut dsu = DSU::new(n);
-    for e in edges.iter() {
-        dsu.union(e[0] as _, e[1] as _);
-    }
-    (0..n)
-        .map(|v| dsu.find(v))
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .filter(|&v| {
-            let e = dsu.edges[v];
-            let size = dsu.size[v];
-            size * (size - 1) / 2 == e
-        })
-        .count() as _
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    size: Vec<i32>,
-    edges: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            size: vec![1; n],
-            edges: vec![0; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v]);
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            self.edges[rx] += 1;
-            return;
-        }
-        if self.size[rx] < self.size[ry] {
-            self.parent[rx] = ry;
-            self.size[ry] += self.size[rx];
-            self.edges[ry] += 1 + self.edges[rx];
+pub fn max_palindromes(s: &str, k: i32) -> i32 {
+    let (s, n) = (s.as_bytes(), s.len());
+    let k = k as usize;
+    let mut res = 0;
+    let mut idx = 0;
+    while idx <= n - k {
+        if is_palindrome(&s[idx..idx + k]) {
+            res += 1;
+            idx += k;
+        } else if idx + k + 1 <= n && is_palindrome(&s[idx..idx + k + 1]) {
+            res += 1;
+            idx += k + 1;
         } else {
-            self.parent[ry] = rx;
-            self.size[rx] += self.size[ry];
-            self.edges[rx] += 1 + self.edges[ry];
+            idx += 1;
         }
     }
+    res
 }
 
 #[cfg(test)]
@@ -95,14 +55,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            count_complete_components(6, &[[0, 1], [0, 2], [1, 2], [3, 4]]),
-            3
-        );
-        assert_eq!(
-            count_complete_components(6, &[[0, 1], [0, 2], [1, 2], [3, 4], [3, 5]]),
-            1
-        );
+        assert_eq!(max_palindromes("abaccdbbd", 3), 2);
+        assert_eq!(max_palindromes("adbcda", 2), 0);
     }
 
     #[test]
