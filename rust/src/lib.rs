77a9bb10b43f696337df19686b0b25d67b854a19
pkusensei <ZW_08@outlook.com>
2025-03-22 13:00:32 -0400

    2482

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,36 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn beautiful_partitions(s: &str, k: i32, min_length: i32) -> i32 {
-    let (s, n) = (s.as_bytes(), s.len());
-    if !P.contains(&s[0]) || P.contains(&s[n - 1]) {
-        return 0;
+pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let [rows, cols] = get_dimensions(&grid);
+    let pre_rows: Vec<_> = grid.iter().map(|r| r.iter().sum::<i32>()).collect();
+    let pre_cols: Vec<_> = (0..cols)
+        .map(|c| grid.iter().map(|row| row[c]).sum::<i32>())
+        .collect();
+    let mut res = vec![vec![0; cols]; rows];
+    for r in 0..rows {
+        for c in 0..cols {
+            res[r][c] = pre_rows[r] + pre_cols[c]
+                - (cols as i32 - pre_rows[r])
+                - (rows as i32 - pre_cols[c]);
+        }
     }
-    let [k, len] = [k, min_length].map(|v| v as usize);
-    dfs(s, k - 1, len, len, &mut vec![vec![-1; n]; k])
-}
-
-const P: &[u8] = b"2357";
-
-fn dfs(s: &[u8], k: usize, len: usize, idx: usize, memo: &mut [Vec<i32>]) -> i32 {
-    let n = s.len();
-    if k == 0 {
-        return i32::from(idx <= n);
-    }
-    if idx >= n {
-        return 0;
-    }
-    if memo[k][idx] > -1 {
-        return memo[k][idx];
-    }
-    // skip
-    let mut res = dfs(s, k, len, 1 + idx, memo);
-    // take
-    if P.contains(&s[idx]) && !P.contains(&s[idx - 1]) {
-        res += dfs(s, k - 1, len, idx + len, memo);
-    }
-    res %= 1_000_000_007;
-    memo[k][idx] = res;
     res
 }
 
@@ -68,11 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(beautiful_partitions("23542185131", 3, 2), 3);
-        assert_eq!(beautiful_partitions("23542185131", 3, 3), 1);
-        assert_eq!(beautiful_partitions("3312958", 3, 1), 1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
