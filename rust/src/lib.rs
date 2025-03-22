c958d04e1899b08c85c100a3ac9f17dcd78e5878
pkusensei <ZW_08@outlook.com>
2025-03-22 19:43:50 -0400

    2485

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,35 +5,9 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_palindromes(s: &str) -> i32 {
-    let (s, n) = (s.as_bytes(), s.len());
-    let mut res = 0;
-    for a in b'0'..=b'9' {
-        for b in b'0'..=b'9' {
-            let mut memo = vec![[-1; 5]; n];
-            res += dfs(s, [a, b, b'#', b, a], 0, 0, &mut memo);
-            res %= MOD;
-        }
-    }
-    res
-}
-
-const MOD: i32 = 1_000_000_007;
-
-fn dfs(s: &[u8], pattern: [u8; 5], si: usize, pi: usize, memo: &mut [[i32; 5]]) -> i32 {
-    if pi == 5 || si >= s.len() {
-        return i32::from(pi == 5);
-    }
-    if memo[si][pi] > -1 {
-        return memo[si][pi];
-    }
-    let mut res = dfs(s, pattern, 1 + si, pi, memo);
-    if s[si] == pattern[pi] || pattern[pi] == b'#' {
-        res += dfs(s, pattern, 1 + si, 1 + pi, memo);
-        res %= MOD;
-    }
-    memo[si][pi] = res;
-    res
+pub const fn pivot_integer(n: i32) -> i32 {
+    let v = ((n * n + n) / 2).isqrt();
+    if 2 * v * v == n * n + n { v } else { -1 }
 }
 
 #[cfg(test)]
@@ -66,11 +40,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_palindromes("103301"), 2);
-        assert_eq!(count_palindromes("0000000"), 21);
-        assert_eq!(count_palindromes("9999900000"), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
