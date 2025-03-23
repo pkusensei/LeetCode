68d87ccc89e93d9d1cb88d63043162538df8cd8e
pkusensei <ZW_08@outlook.com>
2025-03-23 14:48:22 -0400

    2500

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,34 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_total_cost(nums1: &[i32], nums2: &[i32]) -> i64 {
-    use std::collections::HashMap;
-    let mut map = HashMap::new();
+pub fn delete_greatest_value(mut grid: Vec<Vec<i32>>) -> i32 {
+    let cols = grid[0].len();
+    for r in grid.iter_mut() {
+        r.sort_unstable();
+    }
     let mut res = 0;
-    let mut equals = 0;
-    for (i, (a, b)) in nums1.iter().zip(nums2.iter()).enumerate() {
-        if a == b {
-            res += i as i64;
-            equals += 1;
-            *map.entry(*a).or_insert(0) += 1;
-        }
+    for c in 0..cols {
+        res += grid.iter().map(|r| r[c]).max().unwrap_or(0);
     }
-    let Some((max_num, max_count)) = map.into_iter().max_by_key(|&(_k, v)| v) else {
-        return 0;
-    };
-    // (equal - max_count) => swaps within equal elements
-    // swaps = max_count - above => necessary swaps with unequal elements
-    let mut swaps = max_count - (equals - max_count);
-    for (i, (&a, &b)) in nums1.iter().zip(nums2.iter()).enumerate() {
-        if swaps <= 0 {
-            break;
-        }
-        if a != b && a != max_num && b != max_num {
-            res += i as i64;
-            swaps -= 1;
-        }
-    }
-    if swaps > 0 { -1 } else { res }
+    res
 }
 
 #[cfg(test)]
@@ -65,11 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_total_cost(&[1, 2, 3, 4, 5], &[1, 2, 3, 4, 5],), 10);
-        assert_eq!(minimum_total_cost(&[2, 2, 2, 1, 3], &[1, 2, 2, 3, 3]), 10);
-        assert_eq!(minimum_total_cost(&[1, 2, 2], &[1, 2, 2]), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
