21e5218bb0364338abb9faf792ccfb1b36c3e543
pkusensei <ZW_08@outlook.com>
2025-03-23 15:30:07 -0400

    2501 overflow checks in C#

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,89 +6,25 @@ namespace Solution;
 
 public class Solution
 {
-    public int MagnificentSets(int n, int[][] edges)
+    public int LongestSquareStreak(int[] nums)
     {
-        var dsu = new DSU(n);
-        List<List<int>> adj = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
-        foreach (var item in edges)
+        HashSet<int> set = [];
+        Array.Sort(nums);
+        int res = -1;
+        foreach (var item in nums)
         {
-            int a = item[0] - 1;
-            int b = item[1] - 1;
-            dsu.Union(a, b);
-            adj[a].Add(b);
-            adj[b].Add(a);
-        }
-        Dictionary<int, int> components = [];
-        for (int node = 0; node < n; node++)
-        {
-            int v = BFS(node);
-            if (v == -1) { return -1; }
-            int root = dsu.Find(node);
-            if (!components.TryAdd(root, v))
+            int num = item;
+            if (!set.Add(num)) { continue; }
+            int count = 0;
+            while (Array.BinarySearch(nums, num) >= 0)
             {
-                components[root] = Math.Max(components[root], v);
+                count += 1;
+                set.Add(num);
+                try { checked { num *= num; } }
+                catch (OverflowException) { break; }
             }
+            if (count > 1) { res = Math.Max(res, count); }
         }
-        return components.Values.Sum();
-
-        int BFS(int start)
-        {
-            Queue<int> queue = [];
-            queue.Enqueue(start);
-            Span<int> seen = stackalloc int[n];
-            seen.Fill(-1);
-            int layer = 0;
-            seen[start] = layer;
-            while (queue.Count > 0)
-            {
-                int c = queue.Count;
-                for (int _ = 0; _ < c; _ += 1)
-                {
-                    int curr = queue.Dequeue();
-                    foreach (var next in adj[curr])
-                    {
-                        if (seen[next] == -1)
-                        {
-                            seen[next] = 1 + layer;
-                            queue.Enqueue(next);
-                        }
-                        else if (seen[next] == layer)
-                        {
-                            return -1;
-                        }
-                    }
-                }
-                layer += 1;
-            }
-            return layer;
-        }
-    }
-}
-
-class DSU
-{
-    public int[] Parent { get; init; }
-    public int[] Rank { get; init; }
-
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)]; // !!
-        Rank = new int[n];
-    }
-
-    public int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public void Union(int x, int y)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry) { return; }
-        if (Rank[rx] < Rank[ry]) { Parent[rx] = ry; }
-        else if (Rank[rx] > Rank[ry]) { Parent[ry] = rx; }
-        else { Rank[rx] += 1; Parent[ry] = rx; }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,24 +10,20 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(new[] { 4, 16, 256, 65536 }, 4)]
+    public void TestMethod1(int[] nums, int exp)
     {
-        int[][] v = [[1, 2], [1, 4], [1, 5], [2, 6], [2, 3], [4, 6]];
-        Assert.AreEqual(4, sol.MagnificentSets(6, v));
+        Assert.AreEqual(exp, sol.LongestSquareStreak(nums));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        int[][] v = [[1, 2], [2, 3], [3, 1]];
-        Assert.AreEqual(-1, sol.MagnificentSets(3, v));
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-        int[][] v = [[67, 29], [13, 29], [77, 29], [36, 29], [82, 29], [54, 29], [57, 29], [53, 29], [68, 29], [26, 29], [21, 29], [46, 29], [41, 29], [45, 29], [56, 29], [88, 29], [2, 29], [7, 29], [5, 29], [16, 29], [37, 29], [50, 29], [79, 29], [91, 29], [48, 29], [87, 29], [25, 29], [80, 29], [71, 29], [9, 29], [78, 29], [33, 29], [4, 29], [44, 29], [72, 29], [65, 29], [61, 29]];
-        Assert.AreEqual(57, sol.MagnificentSets(92, v));
     }
 
     [TestMethod]
