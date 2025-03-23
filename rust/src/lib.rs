c949570732e5535cd30a6263442cbf699c52ae6b
pkusensei <ZW_08@outlook.com>
2025-03-23 14:40:30 -0400

    2499 min swaps

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,30 +5,34 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_jump(stones: &[i32]) -> i32 {
-    let n = stones.len();
-    let mut idx = 0;
+pub fn minimum_total_cost(nums1: &[i32], nums2: &[i32]) -> i64 {
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
     let mut res = 0;
-    let mut prev = 0;
-    while idx < n {
-        res = res.max(stones[idx] - prev);
-        if idx == n - 1 {
+    let mut equals = 0;
+    for (i, (a, b)) in nums1.iter().zip(nums2.iter()).enumerate() {
+        if a == b {
+            res += i as i64;
+            equals += 1;
+            *map.entry(*a).or_insert(0) += 1;
+        }
+    }
+    let Some((max_num, max_count)) = map.into_iter().max_by_key(|&(_k, v)| v) else {
+        return 0;
+    };
+    // (equal - max_count) => swaps within equal elements
+    // swaps = max_count - above => necessary swaps with unequal elements
+    let mut swaps = max_count - (equals - max_count);
+    for (i, (&a, &b)) in nums1.iter().zip(nums2.iter()).enumerate() {
+        if swaps <= 0 {
             break;
         }
-        prev = stones[idx];
-        idx = (idx + 2).min(n - 1);
-    }
-    idx = 1;
-    prev = 0;
-    while idx < n {
-        res = res.max(stones[idx] - prev);
-        if idx == n - 1 {
-            break;
+        if a != b && a != max_num && b != max_num {
+            res += i as i64;
+            swaps -= 1;
         }
-        prev = stones[idx];
-        idx = (idx + 2).min(n - 1);
     }
-    res
+    if swaps > 0 { -1 } else { res }
 }
 
 #[cfg(test)]
@@ -62,8 +66,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_jump(&[0, 2, 5, 6, 7]), 5);
-        assert_eq!(max_jump(&[0, 3, 9]), 9);
+        assert_eq!(minimum_total_cost(&[1, 2, 3, 4, 5], &[1, 2, 3, 4, 5],), 10);
+        assert_eq!(minimum_total_cost(&[2, 2, 2, 1, 3], &[1, 2, 2, 3, 3]), 10);
+        assert_eq!(minimum_total_cost(&[1, 2, 2], &[1, 2, 2]), -1);
     }
 
     #[test]
