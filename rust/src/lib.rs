c0424775fb6367f9ab59a06ef74b271be47f6085
pkusensei <ZW_08@outlook.com>
2025-03-23 11:56:24 -0400

    2496 one-liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,51 +5,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_score(n: i32, roads: Vec<Vec<i32>>) -> i32 {
-    let n = n as usize;
-    let mut dsu = DSU::new(n);
-    for r in roads.iter() {
-        let [a, b] = [0, 1].map(|i| r[i] as usize - 1);
-        dsu.union(a, b, r[2]);
-    }
-    let root = dsu.find(0);
-    dsu.edge[root]
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    edge: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            edge: vec![i32::MAX; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v]);
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize, edge: i32) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            self.edge[rx] = self.edge[rx].min(edge);
-            return;
-        }
-        if self.edge[rx] <= self.edge[ry] {
-            self.parent[ry] = rx;
-            self.edge[rx] = self.edge[rx].min(edge);
-        } else {
-            self.parent[rx] = ry;
-            self.edge[ry] = self.edge[ry].min(edge);
-        }
-    }
+pub fn maximum_value(strs: Vec<String>) -> i32 {
+    strs.iter()
+        .map(|s| s.parse::<i32>().unwrap_or(s.len() as i32))
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
