b12d7eef65d1be9ca5d5cc090258dfe6c8ba32d1
pkusensei <ZW_08@outlook.com>
2025-03-23 09:55:13 -0400

    2488 track balance, but only record counts on one side

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,29 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn append_characters(s: String, t: String) -> i32 {
-    let (t, n) = (t.as_bytes(), t.len());
-    let mut idx = 0;
-    for b in s.bytes() {
-        if t.get(idx).is_some_and(|&v| v == b) {
-            idx += 1
+pub fn count_subarrays(nums: &[i32], k: i32) -> i32 {
+    let Some(ki) = nums.iter().position(|&v| v == k) else {
+        return 0;
+    };
+    let mut curr = 0;
+    let mut res = 0;
+    let mut map = std::collections::HashMap::new();
+    for &num in &nums[ki..] {
+        curr += (num - k).signum();
+        res += i32::from(matches!(curr, 0 | 1));
+        *map.entry(curr).or_insert(0) += 1;
+    }
+    curr = 0;
+    for &num in nums[..ki].iter().rev() {
+        curr += (num - k).signum();
+        if let Some(v) = map.get(&-curr) {
+            res += v;
+        }
+        if let Some(v) = map.get(&(1 - curr)) {
+            res += v;
         }
     }
-    (n - idx) as i32
+    res
 }
 
 #[cfg(test)]
@@ -46,8 +60,21 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_subarrays(&[3, 2, 1, 4, 5], 4), 3);
+        assert_eq!(count_subarrays(&[2, 3, 1], 3), 1);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            count_subarrays(
+                &[
+                    5, 19, 11, 15, 13, 16, 4, 6, 2, 7, 10, 8, 18, 20, 1, 3, 17, 9, 12, 14
+                ],
+                6
+            ),
+            13
+        );
+    }
 }
