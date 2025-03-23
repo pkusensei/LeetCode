e75c42afcb5e650767239b3d9cde800b7ea059a3
pkusensei <ZW_08@outlook.com>
2025-03-23 12:09:29 -0400

    2497 keep at most k neighbors

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,9 +5,29 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_value(strs: Vec<String>) -> i32 {
-    strs.iter()
-        .map(|s| s.parse::<i32>().unwrap_or(s.len() as i32))
+pub fn max_star_sum(vals: Vec<i32>, edges: Vec<Vec<i32>>, k: i32) -> i32 {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let n = vals.len();
+    let k = k as usize;
+    let mut adj = vec![BinaryHeap::with_capacity(1 + k); n];
+    for e in edges.iter() {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        if vals[b] > 0 {
+            adj[a].push(Reverse(vals[b]));
+        }
+        if vals[a] > 0 {
+            adj[b].push(Reverse(vals[a]));
+        }
+        if adj[a].len() > k {
+            adj[a].pop();
+        }
+        if adj[b].len() > k {
+            adj[b].pop();
+        }
+    }
+    adj.into_iter()
+        .enumerate()
+        .map(|(i, heap)| vals[i] + heap.into_iter().map(|v| v.0).sum::<i32>())
         .max()
         .unwrap()
 }
