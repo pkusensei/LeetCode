46a8335aa10b64007f797d2e3ac78fece9d9dad6
pkusensei <ZW_08@outlook.com>
2025-03-24 15:47:23 -0400

    2521

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,55 +2,30 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashSet;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_partitions(nums: &[i32], k: i32) -> i32 {
-    let k = k as usize;
-    let n = nums.len();
-    let sum: usize = nums.iter().map(|&v| v as usize).sum();
-    if sum < 2 * k {
-        return 0;
-    }
-    let total = (0..n).fold(1, |acc, _| acc * 2 % MOD);
-    // let mut memo = vec![vec![-1; k]; 1 + n];
-    // let invalid = (0..k).fold(0, |acc, t| (acc + dfs(nums, t, &mut memo)) % MOD);
-    let mut dp = vec![0; k];
-    dp[0] = 1;
+pub fn distinct_prime_factors(nums: Vec<i32>) -> i32 {
+    let mut res = HashSet::new();
     for &num in nums.iter() {
-        let mut target = k - 1;
-        while target >= num as usize {
-            dp[target] += dp[target - num as usize];
-            dp[target] %= MOD;
-            target -= 1;
-        }
+        res.extend(find(num));
     }
-    let invalid = dp.into_iter().fold(0, |acc, v| (acc + v) % MOD);
-    (total - 2 * invalid).rem_euclid(MOD)
+    res.len() as _
 }
 
-const MOD: i32 = 1_000_000_007;
-
-fn dfs(nums: &[i32], target: usize, memo: &mut [Vec<i32>]) -> i32 {
-    if target == 0 {
-        return 1;
-    }
-    match nums {
-        [] => 0,
-        [head, tail @ ..] => {
-            let n = nums.len();
-            if memo[n][target] > -1 {
-                return memo[n][target];
-            }
-            let mut res = dfs(tail, target, memo); // skip
-            if *head as usize <= target {
-                res += dfs(tail, target - *head as usize, memo);
-                res %= MOD;
-            }
-            memo[n][target] = res;
-            res
+fn find(mut num: i32) -> HashSet<i32> {
+    let mut res = HashSet::new();
+    let mut p = 2;
+    while num > 1 {
+        while num % p == 0 {
+            num /= p;
+            res.insert(p);
         }
+        p += 1;
     }
+    res
 }
 
 #[cfg(test)]
@@ -83,11 +58,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_partitions(&[1, 2, 3, 4], 4), 6);
-        assert_eq!(count_partitions(&[3, 3, 3], 4), 0);
-        assert_eq!(count_partitions(&[6, 6], 4), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
