f78f48e45ebfbcd3a2e078f80fe8ee1ba8e50dec
pkusensei <ZW_08@outlook.com>
2025-03-23 20:17:33 -0400

    2508 tedious

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,30 +5,44 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn smallest_value(n: i32) -> i32 {
-    let mut n = n;
-    while let Some(v) = sum_prime(n) {
-        if n == v {
-            break;
+pub fn is_possible(n: i32, edges: &[[i32; 2]]) -> bool {
+    use itertools::Itertools;
+    use std::collections::HashSet;
+    let n = n as usize;
+    let adj = edges.iter().fold(vec![HashSet::new(); n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize - 1);
+        acc[a].insert(b);
+        acc[b].insert(a);
+        acc
+    });
+    let odds: Vec<_> = adj
+        .iter()
+        .enumerate()
+        .filter(|(_, s)| s.len() & 1 == 1)
+        .collect();
+    match odds.len() {
+        0 => true,
+        2 => {
+            let a = odds[0];
+            let b = odds[1];
+            if !a.1.contains(&b.0) && !b.1.contains(&a.0) {
+                true
+            } else {
+                (0..n)
+                    .any(|i| i != a.0 && i != b.0 && !adj[i].contains(&a.0) && !adj[i].contains(&b.0))
+            }
         }
-        n = v;
-    }
-    n
-}
-
-fn sum_prime(n: i32) -> Option<i32> {
-    if n < 4 {
-        return None;
-    }
-    let mut res = 0;
-    let mut num = n;
-    for p in 2..n {
-        while num % p == 0 {
-            res += p;
-            num /= p;
+        4 => {
+            for v in odds.into_iter().permutations(4) {
+                let [a, b, c, d] = v[..] else { unreachable!() };
+                if !a.1.contains(&b.0) && !c.1.contains(&d.0) {
+                    return true;
+                }
+            }
+            false
         }
+        _ => false,
     }
-    if res > 0 { Some(res) } else { None }
 }
 
 #[cfg(test)]
@@ -62,11 +76,28 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(smallest_value(15), 5);
+        assert!(is_possible(
+            5,
+            &[[1, 2], [2, 3], [3, 4], [4, 2], [1, 4], [2, 5]]
+        ));
+        assert!(is_possible(4, &[[1, 2], [3, 4]]));
+        assert!(!is_possible(4, &[[1, 2], [1, 3], [1, 4]]));
     }
 
     #[test]
     fn test() {
-        assert_eq!(smallest_value(4), 4);
+        assert!(!is_possible(4, &[[1, 2], [2, 3], [3, 4], [4, 1], [1, 3]]));
+        assert!(is_possible(
+            17,
+            &[
+                [11, 12],
+                [11, 8],
+                [4, 2],
+                [15, 6],
+                [6, 11],
+                [2, 11],
+                [12, 16]
+            ]
+        ));
     }
 }
