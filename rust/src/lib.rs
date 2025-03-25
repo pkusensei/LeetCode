2df17134c9b984d628a13b759c479292d5aaecba
pkusensei <ZW_08@outlook.com>
2025-03-24 20:39:33 -0400

    3394 merge intervals; line sweep

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,25 +5,35 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-struct DataStream {
-    value: i32,
-    k: i32,
-    count: i32,
+pub fn check_valid_cuts(_n: i32, rectangles: &[[i32; 4]]) -> bool {
+    let mut xspans = vec![];
+    let mut yspans = vec![];
+    for rec in rectangles.iter() {
+        let [x1, y1, x2, y2] = rec[..] else {
+            unreachable!()
+        };
+        xspans.push([x1, x2]);
+        yspans.push([y1, y2]);
+    }
+    xspans.sort_unstable();
+    yspans.sort_unstable();
+    check(&xspans) || check(&yspans)
 }
 
-impl DataStream {
-    fn new(value: i32, k: i32) -> Self {
-        Self { value, k, count: 0 }
-    }
-
-    fn consec(&mut self, num: i32) -> bool {
-        if num == self.value {
-            self.count += 1;
-        } else {
-            self.count = 0;
+fn check(spans: &[[i32; 2]]) -> bool {
+    let mut curr_end = spans[0][1];
+    let mut count = 0;
+    for sp in spans[1..].iter() {
+        let [start, end] = *sp;
+        if start >= curr_end {
+            count += 1;
         }
-        self.count >= self.k
+        curr_end = curr_end.max(end);
+        if count >= 2 {
+            break;
+        }
     }
+    count >= 2
 }
 
 #[cfg(test)]
@@ -56,8 +66,38 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        // assert!(check_valid_cuts(
+        //     5,
+        //     &[[1, 0, 5, 2], [0, 2, 2, 4], [3, 2, 5, 3], [0, 4, 4, 5]]
+        // ));
+        // assert!(check_valid_cuts(
+        //     4,
+        //     &[[0, 0, 1, 1], [2, 0, 3, 4], [0, 2, 2, 3], [3, 0, 4, 3]]
+        // ));
+        assert!(!check_valid_cuts(
+            4,
+            &[
+                [0, 2, 2, 4],
+                [1, 0, 3, 2],
+                [2, 2, 3, 4],
+                [3, 0, 4, 2],
+                [3, 2, 4, 4]
+            ]
+        ));
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert!(!check_valid_cuts(
+            4,
+            &[
+                [0, 0, 1, 4],
+                [1, 0, 2, 3],
+                [2, 0, 3, 3],
+                [3, 0, 4, 3],
+                [1, 3, 4, 4]
+            ]
+        ));
+    }
 }
