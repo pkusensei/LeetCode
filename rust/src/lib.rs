c4359a3e7a208f406f2f915e6799db416afb8159
Zx Wang <ZW_08@outlook.com>
2025-03-25 14:46:03 -0400

    2541

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,13 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-    for num in nums1.iter() {
-        if nums2.binary_search(num).is_ok() {
-            return *num;
-        }
+pub fn min_operations(nums1: &[i32], nums2: &[i32], k: i32) -> i64 {
+    if k == 0 {
+        return if nums1 == nums2 { 0 } else { -1 };
     }
-    -1
+    if nums1.iter().map(|&v| i64::from(v)).sum::<i64>()
+        != nums2.iter().map(|&v| i64::from(v)).sum::<i64>()
+    {
+        return -1;
+    }
+    let mut res = 0;
+    for (a, b) in nums1.iter().zip(nums2.iter()) {
+        if a % k != b % k {
+            return -1;
+        }
+        res += i64::from((a - b) / k).abs();
+    }
+    res / 2
 }
 
 #[cfg(test)]
@@ -44,7 +54,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_operations(&[4, 3, 1, 4], &[1, 3, 7, 1], 3), 2);
+        assert_eq!(min_operations(&[3, 8, 5, 2], &[2, 4, 1, 6], 1), -1);
+    }
 
     #[test]
     fn test() {}
