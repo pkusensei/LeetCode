e4d47a538d938aad35b25e9dfae213cc956a22f3
pkusensei <ZW_08@outlook.com>
2025-03-25 19:09:37 -0400

    2546 bits

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,11 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_reachable(target_x: i32, target_y: i32) -> bool {
-    // gcd(target_x as _, target_y as _).count_ones() == 1
-    let [mut x, mut y] = [target_x, target_y];
-    while x > 1 && y > 1 {
-        if x < y {
-            if y & 1 == 0 { y /= 2 } else { y -= x }
-        } else {
-            if x & 1 == 0 { x /= 2 } else { x -= y }
-        }
-    }
-    x == 1 || y == 1
+pub fn make_strings_equal(s: String, target: String) -> bool {
+    // 0|0 =0 , 0^0 =0 => all 0's cannot make 1's
+    // 1|1 =1 , 1^1 =0 => eliminate 1's
+    // 1|0 =1 , 1^0 =1 => create 1's
+    s == target || (!s.bytes().all(|b| b == b'0') && !target.bytes().all(|b| b == b'0'))
 }
 
 #[cfg(test)]
