7bb995ec0eb1aba8e05075c53094b2d321ecb8b7
pkusensei <ZW_08@outlook.com>
2025-03-26 18:00:51 -0400

    2556 two dfs runs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,30 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximize_win(prize_positions: &[i32], k: i32) -> i32 {
-    let n = prize_positions.len();
-    let mut dp = vec![0; 1 + n]; // prev largest window
-    let mut res = 0;
-    let mut left = 0;
-    for (right, &num) in prize_positions.iter().enumerate() {
-        while num - prize_positions[left] > k {
-            left += 1;
-        }
-        // record max window until right
-        dp[1 + right] = dp[right].max(right + 1 - left);
-        res = res.max(right + 1 - left + dp[left]);
+pub fn is_possible_to_cut_path(mut grid: Vec<Vec<i32>>) -> bool {
+    let [rows, cols] = get_dimensions(&grid);
+    if (rows == 1 && cols <= 2) || (rows <= 2 && cols == 1) {
+        return false;
     }
-    res as _
+    if dfs(&mut grid, 0, 0) {
+        grid[0][0] = 1;
+        if dfs(&mut grid, 0, 0) {
+            return false;
+        }
+    }
+    true
+}
+
+fn dfs(grid: &mut [Vec<i32>], r: usize, c: usize) -> bool {
+    let [rows, cols] = get_dimensions(grid);
+    if grid[r][c] == 0 {
+        return false;
+    }
+    if r == rows - 1 && c == cols - 1 {
+        return true;
+    }
+    grid[r][c] = 0;
+    (1 + r < rows && dfs(grid, 1 + r, c)) || (1 + c < cols && dfs(grid, r, 1 + c))
 }
 
 #[cfg(test)]
@@ -52,10 +62,20 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(maximize_win(&[1, 1, 2, 2, 3, 3, 5], 2), 7);
-        assert_eq!(maximize_win(&[1, 2, 3, 4], 0), 2);
+        assert!(is_possible_to_cut_path(vec![
+            vec![1, 1, 1],
+            vec![1, 0, 0],
+            vec![1, 1, 1]
+        ]));
+        assert!(!is_possible_to_cut_path(vec![
+            vec![1, 1, 1],
+            vec![1, 0, 1],
+            vec![1, 1, 1]
+        ]));
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert!(is_possible_to_cut_path(vec![vec![1, 1, 1, 1, 1]]));
+    }
 }
