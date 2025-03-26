d75e91d2f1c1abd5eb430c1a399c12311bf35aac
pkusensei <ZW_08@outlook.com>
2025-03-26 16:14:14 -0400

    2555 sliding window and dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn separate_digits(nums: Vec<i32>) -> Vec<i32> {
-    nums.iter()
-        .flat_map(|&v| {
-            let mut num = v;
-            let mut res = vec![];
-            while num > 0 {
-                res.push(num % 10);
-                num /= 10;
-            }
-            res.reverse();
-            res
-        })
-        .collect()
+pub fn maximize_win(prize_positions: &[i32], k: i32) -> i32 {
+    let n = prize_positions.len();
+    let mut dp = vec![0; 1 + n]; // prev largest window
+    let mut res = 0;
+    let mut left = 0;
+    for (right, &num) in prize_positions.iter().enumerate() {
+        while num - prize_positions[left] > k {
+            left += 1;
+        }
+        // record max window until right
+        dp[1 + right] = dp[right].max(right + 1 - left);
+        res = res.max(right + 1 - left + dp[left]);
+    }
+    res as _
 }
 
 #[cfg(test)]
@@ -50,7 +51,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(maximize_win(&[1, 1, 2, 2, 3, 3, 5], 2), 7);
+        assert_eq!(maximize_win(&[1, 2, 3, 4], 0), 2);
+    }
 
     #[test]
     fn test() {}
