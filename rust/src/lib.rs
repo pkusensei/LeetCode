1fceab04251be7c54dd4a54b2788e0967b5fdcde
pkusensei <ZW_08@outlook.com>
2025-03-26 14:59:30 -0400

    2552 twisting my mind

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,59 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn put_marbles(weights: &[i32], k: i32) -> i64 {
-    use itertools::Itertools;
-    let n = weights.len();
-    let k = k as usize;
-    if n == k || k < 2 {
-        return 0;
+//  i1 < i2 < i3 < i4
+// [i1]<[i3]<[i2]<[i4]
+pub fn count_quadruplets(nums: &[i32]) -> i64 {
+    let n = nums.len();
+    // track 132 triplets in dp[i2]
+    let mut count = vec![0; n];
+    let mut res = 0;
+    for x in 2..n {
+        let mut prev_small_pairs = 0;
+        for y in 0..x {
+            if nums[x] > nums[y] {
+                // [x]>[y] => [i3]>[i1]
+                // This count is saved for a potential [i2]
+                prev_small_pairs += 1;
+                // !! semamtics of (x, y) changed !!
+                // [x]>[y] => [i4]>[i2]
+                // Found valid [i4], accumulate result
+                res += count[y];
+            } else if nums[x] < nums[y] {
+                // [x]<[y] => [i3]<[i2]
+                // record the value in dp[i2]
+                count[y] += prev_small_pairs;
+            }
+        }
     }
-    let mut nums = weights
-        .windows(2)
-        .map(|w| i64::from(w[0] + w[1]))
-        .collect_vec();
-    nums.sort_unstable();
-    let min: i64 = nums[..k - 1].iter().sum();
-    let max: i64 = nums.iter().tail(k - 1).sum();
-    max - min
+    res
+}
+
+pub fn with_prefix_sums(nums: &[i32]) -> i64 {
+    let n = nums.len();
+    let mut prefix = vec![vec![0; n]; n];
+    for i2 in 1..n {
+        for i3 in 1 + i2..n {
+            // Count all [i1]<[i3]
+            prefix[i2][i3] = prefix[i2 - 1][i3] + i64::from(nums[i2 - 1] < nums[i3]);
+        }
+    }
+    let mut suffix = vec![vec![0; n]; n];
+    for i3 in (0..n - 1).rev() {
+        for i2 in (0..i3).rev() {
+            // Count all [i2]<[i4]
+            suffix[i3][i2] = suffix[1 + i3][i2] + i64::from(nums[i2] < nums[1 + i3]);
+        }
+    }
+    let mut res = 0;
+    for i2 in 1..n - 2 {
+        for i3 in 1 + i2..n - 1 {
+            if nums[i2] > nums[i3] {
+                res += prefix[i2][i3] * suffix[i3][i2];
+            }
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -53,12 +91,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(put_marbles(&[1, 3, 5, 1], 2), 4);
-        assert_eq!(put_marbles(&[1, 3], 2), 0);
+        assert_eq!(count_quadruplets(&[1, 3, 2, 4, 5]), 2);
+        assert_eq!(count_quadruplets(&[1, 2, 3, 4]), 0);
+
+        assert_eq!(with_prefix_sums(&[1, 3, 2, 4, 5]), 2);
+        assert_eq!(with_prefix_sums(&[1, 2, 3, 4]), 0);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(put_marbles(&[1, 4, 2, 5, 2], 3), 3);
-    }
+    fn test() {}
 }
