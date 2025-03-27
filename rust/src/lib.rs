0dbbb7256d3d63472b15f8decdd3e8670f16a981
Zx Wang <ZW_08@outlook.com>
2025-03-27 11:07:40 -0400

    2562

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,34 +5,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost(basket1: Vec<i32>, basket2: Vec<i32>) -> i64 {
-    use std::{collections::BTreeMap, iter};
-    let mut map = BTreeMap::<i32, i32>::new();
-    for &num in basket1.iter() {
-        *map.entry(num).or_insert(0) += 1;
-    }
-    for &num in basket2.iter() {
-        *map.entry(num).or_insert(0) -= 1;
-    }
-    let mut nums = vec![]; // nums to be swapped
-    for (k, v) in map.iter().map(|(&k, &v)| (k, v.unsigned_abs() as usize)) {
-        if v & 1 == 1 {
-            return -1;
-        }
-        nums.extend(iter::repeat_n(k, v / 2));
-    }
+pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
+    let n = nums.len();
     let mut res = 0;
-    // min of both arrays
-    let min = *map.keys().next().unwrap_or(&10i32.pow(9));
-    // Of all nums to be swapped, try either
-    // 1) smaller num paired with bigger num => take(n/2)
-    //    [4,4] and [3,3] => 2*[3,4] with min cost of 3
-    // 2) pair big num with min element, and swap twice => 2*min
-    //    [1, 3,3] and [1, 4,4]
-    //    1st swap => [4, 3, 3] and [1, 1, 4]
-    //    2nd swap => [4, 1, 3] and [1, 3, 4]
-    for &num in nums.iter().take(nums.len() / 2) {
-        res += i64::from(num.min(2 * min));
+    let mut left = 0;
+    let mut right = n - 1;
+    while left <= right && left < n {
+        if left == right {
+            res += i64::from(nums[left])
+        } else {
+            let v1 = i64::from(nums[left]);
+            let v2 = i64::from(nums[right]);
+            res += v1 * 10i64.pow(1 + v2.ilog10()) + v2;
+        }
+        left += 1;
+        right -= 1;
     }
     res
 }
@@ -67,10 +54,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_cost(vec![4, 2, 2, 2], vec![1, 4, 1, 2]), 1);
-        assert_eq!(min_cost(vec![2, 3, 4, 1], vec![3, 2, 5, 1]), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
