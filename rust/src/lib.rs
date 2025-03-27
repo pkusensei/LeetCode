be975e3338e7e547ce1006d4a42d2b8cec2454c0
pkusensei <ZW_08@outlook.com>
2025-03-26 20:35:00 -0400

    2780 Boyer-Moore Majority Voting Algorithm and prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,30 +5,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_possible_to_cut_path(mut grid: Vec<Vec<i32>>) -> bool {
-    let [rows, cols] = get_dimensions(&grid);
-    if (rows == 1 && cols <= 2) || (rows <= 2 && cols == 1) {
-        return false;
-    }
-    if dfs(&mut grid, 0, 0) {
-        grid[0][0] = 1;
-        if dfs(&mut grid, 0, 0) {
-            return false;
+pub fn minimum_index(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    // let (major, mcount) = with_hashmap(nums);
+    let (major, mcount) = with_boyer_moore(nums);
+    let mut left = 0;
+    let mut right = mcount;
+    for (idx, &num) in (0..).zip(nums.iter()) {
+        left += usize::from(num == major);
+        right -= usize::from(num == major);
+        if left > (1 + idx) / 2 && right > (n - 1 - idx) / 2 {
+            return idx as i32;
         }
     }
-    true
+    -1
 }
 
-fn dfs(grid: &mut [Vec<i32>], r: usize, c: usize) -> bool {
-    let [rows, cols] = get_dimensions(grid);
-    if grid[r][c] == 0 {
-        return false;
+fn with_hashmap(nums: &[i32]) -> (i32, usize) {
+    use itertools::Itertools;
+    let n = nums.len();
+    let count = nums.iter().copied().counts();
+    let (&major, &mcount) = count.iter().find(|(_, v)| **v > n / 2).unwrap();
+    (major, mcount)
+}
+
+fn with_boyer_moore(nums: &[i32]) -> (i32, usize) {
+    let mut count = 0;
+    let mut major = nums[0];
+    for &num in nums {
+        if num == major {
+            count += 1
+        } else {
+            count -= 1
+        }
+        if count == 0 {
+            major = num;
+            count = 1;
+        }
     }
-    if r == rows - 1 && c == cols - 1 {
-        return true;
-    }
-    grid[r][c] = 0;
-    (1 + r < rows && dfs(grid, 1 + r, c)) || (1 + c < cols && dfs(grid, r, 1 + c))
+    (major, nums.iter().filter(|&&v| v == major).count())
 }
 
 #[cfg(test)]
@@ -62,20 +77,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(is_possible_to_cut_path(vec![
-            vec![1, 1, 1],
-            vec![1, 0, 0],
-            vec![1, 1, 1]
-        ]));
-        assert!(!is_possible_to_cut_path(vec![
-            vec![1, 1, 1],
-            vec![1, 0, 1],
-            vec![1, 1, 1]
-        ]));
+        assert_eq!(minimum_index(&[1, 2, 2, 2]), 2);
+        assert_eq!(minimum_index(&[2, 1, 3, 1, 1, 1, 7, 1, 2, 1]), 4);
+        assert_eq!(minimum_index(&[3, 3, 3, 3, 7, 2, 2]), -1);
     }
 
     #[test]
-    fn test() {
-        assert!(is_possible_to_cut_path(vec![vec![1, 1, 1, 1, 1]]));
-    }
+    fn test() {}
 }
