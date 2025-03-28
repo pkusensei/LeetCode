f2da2133b1015bf2cec14bf6791255c18f548475
pkusensei <ZW_08@outlook.com>
2025-03-28 13:14:03 -0400

    2574

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,34 +5,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_the_string(lcp: &[&[i32]]) -> String {
-    let n = lcp.len();
-    let mut res = vec![0; n];
-    let mut curr = b'a' - 1;
-    for (r, row) in lcp.iter().enumerate() {
-        if res[r] > 0 {
-            continue;
-        }
-        curr += 1;
-        if curr > b'z' {
-            return String::new();
-        }
-        for (c, &v) in row.iter().enumerate().skip(r) {
-            if v > 0 {
-                res[c] = curr
-            }
-        }
-    }
-    for r in 0..n {
-        for c in 0..n {
-            let mut v = *lcp.get(1 + r).and_then(|row| row.get(1 + c)).unwrap_or(&0);
-            v = if res[r] == res[c] { 1 + v } else { 0 };
-            if lcp[r][c] != v {
-                return String::new();
-            }
-        }
-    }
-    String::from_utf8(res).unwrap()
+pub fn left_right_difference(nums: Vec<i32>) -> Vec<i32> {
+    let mut prefix = nums.iter().fold(vec![0], |mut acc, &num| {
+        acc.push(num + acc.last().unwrap_or(&0));
+        acc
+    });
+    prefix.pop();
+    let mut suffix = nums.iter().rev().fold(vec![0], |mut acc, &num| {
+        acc.push(num + acc.last().unwrap_or(&0));
+        acc
+    });
+    suffix.pop();
+    suffix.reverse();
+    prefix
+        .into_iter()
+        .zip(suffix)
+        .map(|(a, b)| (a - b).abs())
+        .collect()
 }
 
 #[cfg(test)]
@@ -65,20 +54,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            find_the_string(&[&[4, 0, 2, 0], &[0, 3, 0, 1], &[2, 0, 2, 0], &[0, 1, 0, 1]]),
-            "abab"
-        );
-        assert_eq!(
-            find_the_string(&[&[4, 3, 2, 1], &[3, 3, 2, 1], &[2, 2, 2, 1], &[1, 1, 1, 1]]),
-            "aaaa"
-        );
-        assert_eq!(
-            find_the_string(&[&[4, 3, 2, 1], &[3, 3, 2, 1], &[2, 2, 2, 1], &[1, 1, 1, 3]]),
-            ""
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
