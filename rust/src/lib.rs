a17cad9b9c4807c70a7a9558c741859c494545e0
pkusensei <ZW_08@outlook.com>
2025-03-29 09:50:11 -0400

    2585 dp one by one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,47 +4,26 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
-use std::collections::HashMap;
 
-pub fn find_valid_split(nums: &[i32]) -> i32 {
-    let n = nums.len();
-    let maps = nums.iter().map(|&v| primes(v)).collect_vec();
-    let mut right = HashMap::new();
-    for m in maps.iter() {
-        for (&k, v) in m.iter() {
-            *right.entry(k).or_insert(0) += v;
-        }
-    }
-    let mut left = HashMap::new();
-    for (idx, m) in maps[..n - 1].iter().enumerate() {
-        for (&k, v) in m.iter() {
-            *left.entry(k).or_insert(0) += v;
-            right.entry(k).and_modify(|c| *c -= v);
-            if right[&k] == 0 {
-                right.remove(&k);
+pub fn ways_to_reach_target(target: i32, types: &[[i32; 2]]) -> i32 {
+    let target = target as usize;
+    let mut dp = vec![0; 1 + target];
+    dp[0] = 1;
+    for t in types.iter() {
+        let [count, mark] = [t[0], t[1]];
+        let mut curr = vec![0; 1 + target];
+        for c in 0..=count {
+            for (prev_s, v) in dp.iter().enumerate() {
+                let score = prev_s + (c * mark) as usize;
+                if score <= target {
+                    curr[score] += v;
+                    curr[score] %= 1_000_000_007;
+                }
             }
         }
-        if left.keys().all(|a| !right.contains_key(a)) {
-            return idx as i32;
-        }
+        dp = curr;
     }
-    -1
-}
-
-fn primes(mut num: i32) -> HashMap<i32, i32> {
-    let mut res = HashMap::new();
-    let sq = num.isqrt();
-    for p in 2..=sq {
-        while num % p == 0 {
-            num /= p;
-            *res.entry(p).or_insert(0) += 1;
-        }
-    }
-    if num > 1 {
-        *res.entry(num).or_insert(0) += 1;
-    }
-    res
+    dp[target]
 }
 
 #[cfg(test)]
@@ -78,8 +57,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(find_valid_split(&[4, 7, 8, 15, 3, 5]), 2);
-        assert_eq!(find_valid_split(&[4, 7, 15, 8, 3, 5]), -1);
+        assert_eq!(ways_to_reach_target(6, &[[6, 1], [3, 2], [2, 3]]), 7);
+        assert_eq!(ways_to_reach_target(5, &[[50, 1], [50, 2], [50, 5]]), 4);
     }
 
     #[test]
