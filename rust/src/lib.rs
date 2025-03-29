6aaa8ea89949d19b0157a8af11f53889980c5035
pkusensei <ZW_08@outlook.com>
2025-03-29 19:38:14 -0400

    2603 run two topo sort to prune tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,29 +2,56 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::{HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(mut nums: Vec<i32>, queries: &[i32]) -> Vec<i64> {
-    let n = nums.len();
-    nums.sort_unstable();
-    let prefix = nums.iter().fold(vec![], |mut acc, &num| {
-        acc.push(i64::from(num) + acc.last().unwrap_or(&0));
-        acc
-    });
-    let mut res = vec![];
-    for &q in queries.iter() {
-        let i = nums.partition_point(|&v| v <= q);
-        let q = i64::from(q);
-        if i == 0 {
-            res.push(prefix[n - 1] - q * n as i64);
-        } else {
-            let left = i as i64 * q - prefix[i - 1];
-            let right = prefix[n - 1] - prefix[i - 1] - (n - i) as i64 * q;
-            res.push(left + right);
+pub fn collect_the_coins(coins: &[i32], edges: &[[i32; 2]]) -> i32 {
+    let n = coins.len();
+    let mut degs = vec![0; n];
+    let mut adj = vec![vec![]; n];
+    for e in edges.iter() {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        degs[a] += 1;
+        degs[b] += 1;
+        adj[a].push(b);
+        adj[b].push(a);
+    }
+    let mut queue: VecDeque<_> = (0..n)
+        .filter(|&node| degs[node] == 1 && coins[node] == 0)
+        .collect();
+    let mut del = HashSet::new();
+    while let Some(node) = queue.pop_front() {
+        del.insert(node);
+        for &next in adj[node].iter() {
+            degs[next] -= 1;
+            if degs[next] == 1 && coins[next] == 0 {
+                queue.push_back(next);
+            }
         }
     }
-    res
+    let mut queue: VecDeque<_> = (0..n)
+        .filter_map(|node| {
+            if degs[node] == 1 && coins[node] == 1 {
+                Some((node, 0))
+            } else {
+                None
+            }
+        })
+        .collect();
+    while let Some((node, depth)) = queue.pop_front() {
+        del.insert(node);
+        if depth == 0 {
+            for &next in adj[node].iter() {
+                degs[next] -= 1;
+                if degs[next] == 1 {
+                    queue.push_back((next, 1));
+                }
+            }
+        }
+    }
+    ((n - del.len()) as i32 - 1).max(0) * 2
 }
 
 #[cfg(test)]
@@ -58,8 +85,20 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_operations(vec![3, 1, 6, 8], &[1, 5]), [14, 10]);
-        assert_eq!(min_operations(vec![2, 9, 6, 3], &[10]), [20]);
+        assert_eq!(
+            collect_the_coins(
+                &[1, 0, 0, 0, 0, 1],
+                &[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
+            ),
+            2
+        );
+        assert_eq!(
+            collect_the_coins(
+                &[0, 0, 0, 1, 1, 0, 0, 1],
+                &[[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [5, 6], [5, 7]]
+            ),
+            2
+        );
     }
 
     #[test]
