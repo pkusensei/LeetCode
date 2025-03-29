23324b69d01fa8dc2a8aca3bd260c1a71669de95
pkusensei <ZW_08@outlook.com>
2025-03-29 13:58:56 -0400

    2589 record used slots before end

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn beautiful_subarrays(nums: Vec<i32>) -> i64 {
-    use std::collections::HashMap;
-    let mut prefix = 0;
-    let mut seen = HashMap::from([(0, 1)]);
-    let mut res = 0;
-    for &num in nums.iter() {
-        prefix ^= num;
-        if let Some(v) = seen.get(&(0 ^ prefix)) {
-            res += v
+pub fn find_minimum_time(mut tasks: Vec<[i32; 3]>) -> i32 {
+    tasks.sort_unstable_by_key(|t| t[1]);
+    let max = tasks.last().unwrap()[1];
+    let mut slots = vec![false; 1 + max as usize];
+    for t in tasks.iter() {
+        let [tstart, tend, mut tdur] = [t[0], t[1], t[2]];
+        let count = slots[tstart as usize..=tend as usize]
+            .iter()
+            .filter(|&&v| v)
+            .count() as i32;
+        tdur -= tdur.min(count);
+        let mut idx = tend;
+        while tdur > 0 && idx >= tstart {
+            if !slots[idx as usize] {
+                slots[idx as usize] = true;
+                tdur -= 1;
+            }
+            idx -= 1;
         }
-        *seen.entry(prefix).or_insert(0) += 1;
     }
-    res
+    slots.into_iter().map(i32::from).sum()
 }
 
 #[cfg(test)]
@@ -50,8 +58,20 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(find_minimum_time(vec![[2, 3, 1], [4, 5, 1], [1, 5, 2]]), 2);
+        assert_eq!(find_minimum_time(vec![[1, 3, 2], [2, 5, 3], [5, 6, 2]]), 4);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            find_minimum_time(vec![[10, 16, 3], [10, 20, 5], [1, 12, 4], [8, 11, 2]]),
+            6
+        );
+        assert_eq!(
+            find_minimum_time(vec![[1, 10, 7], [4, 11, 1], [3, 19, 7], [10, 15, 2]]),
+            8
+        );
+    }
 }
