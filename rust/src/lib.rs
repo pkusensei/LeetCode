64a44ffd126fe93b2c5fe3797779c9643c872c1a
pkusensei <ZW_08@outlook.com>
2025-03-28 22:08:10 -0400

    2818 such a long one; next greater with mono stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,11 +4,69 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
 
-pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
-    let rem = time % (n - 1);
-    let round = time / (n - 1);
-    if round & 1 == 1 { n - rem } else { 1 + rem }
+pub fn maximum_score(nums: &[i32], k: i32) -> i32 {
+    const MOD: i64 = 1_000_000_007;
+    let n = nums.len();
+    let scores = nums.iter().map(|&v| p_score(v)).collect_vec();
+    let mut right_greater = vec![n as i64; n];
+    let mut stack = vec![];
+    for (idx, &num) in scores.iter().enumerate() {
+        while stack.last().is_some_and(|&i| scores[i] < num) {
+            let i = stack.pop().unwrap();
+            right_greater[i] = idx as i64;
+        }
+        stack.push(idx);
+    }
+    let mut left_greater = vec![-1_i64; n];
+    stack.clear();
+    for (idx, &num) in scores.iter().enumerate().rev() {
+        while stack.last().is_some_and(|&i| scores[i] <= num) {
+            let i = stack.pop().unwrap();
+            left_greater[i] = idx as i64;
+        }
+        stack.push(idx);
+    }
+    let ranges = (0..)
+        .zip(left_greater.into_iter().zip(right_greater))
+        .map(|(i, (left, right))| (i - left) * (right - i))
+        .collect_vec();
+    let mut k = i64::from(k);
+    let mut res = 1;
+    for (idx, &num) in nums
+        .iter()
+        .enumerate()
+        .sorted_unstable_by(|a, b| b.1.cmp(a.1))
+    {
+        if k == 0 {
+            break;
+        }
+        let count = ranges[idx].min(k);
+        k -= count;
+        res *= mod_pow(num.into(), count, MOD);
+        res %= MOD;
+    }
+    res as i32
+}
+
+fn p_score(mut num: i32) -> i32 {
+    if num < 2 {
+        0
+    } else {
+        let mut res = 0;
+        let sq = num.isqrt();
+        for p in 2..=sq {
+            let mut temp = true;
+            while num % p == 0 {
+                num /= p;
+                res += i32::from(temp);
+                temp = false;
+            }
+        }
+        res += i32::from(num >= 2);
+        res
+    }
 }
 
 #[cfg(test)]
@@ -41,7 +99,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(maximum_score(&[19, 12, 14, 6, 10, 18], 3), 4788);
+        assert_eq!(maximum_score(&[8, 3, 9, 3, 8], 2), 81);
+    }
 
     #[test]
     fn test() {}
