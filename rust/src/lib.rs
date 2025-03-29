e99c8f37a5ae847ed8787127949b8decec613d20
pkusensei <ZW_08@outlook.com>
2025-03-29 09:58:46 -0400

    2586 one-liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,25 +5,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn ways_to_reach_target(target: i32, types: &[[i32; 2]]) -> i32 {
-    let target = target as usize;
-    let mut dp = vec![0; 1 + target];
-    dp[0] = 1;
-    for t in types.iter() {
-        let [count, mark] = [t[0], t[1]];
-        let mut curr = vec![0; 1 + target];
-        for c in 0..=count {
-            for (prev_s, v) in dp.iter().enumerate() {
-                let score = prev_s + (c * mark) as usize;
-                if score <= target {
-                    curr[score] += v;
-                    curr[score] %= 1_000_000_007;
-                }
-            }
-        }
-        dp = curr;
-    }
-    dp[target]
+pub fn vowel_strings(words: Vec<String>, left: i32, right: i32) -> i32 {
+    words[left as usize..=right as usize]
+        .iter()
+        .filter(|s| {
+            s.bytes().next().is_some_and(|b| b"aeiou".contains(&b))
+                && s.bytes().last().is_some_and(|b| b"aeiou".contains(&b))
+        })
+        .count() as i32
 }
 
 #[cfg(test)]
@@ -56,10 +45,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(ways_to_reach_target(6, &[[6, 1], [3, 2], [2, 3]]), 7);
-        assert_eq!(ways_to_reach_target(5, &[[50, 1], [50, 2], [50, 5]]), 4);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
