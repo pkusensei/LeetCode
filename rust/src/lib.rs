e3996c3d9a74fcf16158ec1446f76e106f158620
pkusensei <ZW_08@outlook.com>
2025-03-29 20:23:48 -0400

    763 last occurrence of elements

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,56 +2,36 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn collect_the_coins(coins: &[i32], edges: &[[i32; 2]]) -> i32 {
-    let n = coins.len();
-    let mut degs = vec![0; n];
-    let mut adj = vec![vec![]; n];
-    for e in edges.iter() {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        degs[a] += 1;
-        degs[b] += 1;
-        adj[a].push(b);
-        adj[b].push(a);
-    }
-    let mut queue: VecDeque<_> = (0..n)
-        .filter(|&node| degs[node] == 1 && coins[node] == 0)
-        .collect();
-    let mut del = HashSet::new();
-    while let Some(node) = queue.pop_front() {
-        del.insert(node);
-        for &next in adj[node].iter() {
-            degs[next] -= 1;
-            if degs[next] == 1 && coins[next] == 0 {
-                queue.push_back(next);
+pub fn partition_labels(s: &str) -> Vec<i32> {
+    let n = s.len();
+    // last occurrence of byte
+    let latest = s
+        .bytes()
+        .enumerate()
+        .rev()
+        .fold([n; 26], |mut acc, (i, b)| {
+            let pos = usize::from(b - b'a');
+            if acc[pos] == n {
+                acc[pos] = i;
             }
+            acc
+        });
+    let mut end = 0;
+    let mut prev = -1;
+    let mut res = vec![];
+    for (idx, b) in s.bytes().enumerate() {
+        let last = latest[usize::from(b - b'a')];
+        end = end.max(last);
+        if end == idx {
+            res.push(end as i32 - prev);
+            prev = end as i32;
+            end = 1 + idx;
         }
     }
-    let mut queue: VecDeque<_> = (0..n)
-        .filter_map(|node| {
-            if degs[node] == 1 && coins[node] == 1 {
-                Some((node, 0))
-            } else {
-                None
-            }
-        })
-        .collect();
-    while let Some((node, depth)) = queue.pop_front() {
-        del.insert(node);
-        if depth == 0 {
-            for &next in adj[node].iter() {
-                degs[next] -= 1;
-                if degs[next] == 1 {
-                    queue.push_back((next, 1));
-                }
-            }
-        }
-    }
-    ((n - del.len()) as i32 - 1).max(0) * 2
+    res
 }
 
 #[cfg(test)]
@@ -85,20 +65,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            collect_the_coins(
-                &[1, 0, 0, 0, 0, 1],
-                &[[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
-            ),
-            2
-        );
-        assert_eq!(
-            collect_the_coins(
-                &[0, 0, 0, 1, 1, 0, 0, 1],
-                &[[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [5, 6], [5, 7]]
-            ),
-            2
-        );
+        assert_eq!(partition_labels("ababcbacadefegdehijhklij"), [9, 7, 8]);
+        assert_eq!(partition_labels("eccbbbbdec"), [10]);
     }
 
     #[test]
