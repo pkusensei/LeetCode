758a96cb1cc7a9952841c48925f270b3ba1cf5fb
pkusensei <ZW_08@outlook.com>
2025-03-30 15:35:31 -0400

    2612 dsu and bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,62 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn mice_and_cheese(reward1: &[i32], reward2: &[i32], k: i32) -> i32 {
-    use std::collections::BinaryHeap;
-    let mut heap: BinaryHeap<_> = reward1
-        .iter()
-        .zip(reward2.iter())
-        .map(|(a, b)| a - b)
-        .collect();
-    let mut res: i32 = reward2.iter().sum::<i32>();
-    for _ in 0..k {
-        let Some(diff) = heap.pop() else {
-            break;
-        };
-        res += diff
+pub fn min_reverse_operations(n: i32, p: i32, banned: &[i32], k: i32) -> Vec<i32> {
+    use std::collections::VecDeque;
+    let mut dsu = DSU::new(2 + n as usize);
+    dsu.union(p as usize, 2 + p as usize);
+    for &b in banned.iter() {
+        dsu.union(b as usize, 2 + b as usize);
+    }
+    let mut res = vec![-1; n as usize];
+    res[p as usize] = 0;
+    let mut queue = VecDeque::from([p]);
+    let mut step = 0;
+    while !queue.is_empty() {
+        step += 1;
+        let len = queue.len();
+        for _ in 0..len {
+            let i = queue.pop_front().unwrap();
+            let min = (i - k + 1).max(k - i - 1);
+            let max = (i + k - 1).min(2 * n - k - i - 1);
+            let mut root = dsu.find(min as usize);
+            // Banned idx would be in the same group/component (??)
+            while root <= max as usize {
+                res[root] = step;
+                queue.push_back(root as i32);
+                dsu.union(root, 2 + max as usize);
+                root = dsu.find(2 + root);
+            }
+        }
     }
     res
+}
+
+struct DSU {
+    parent: Vec<usize>,
+}
+
+impl DSU {
+    pub fn new(size: usize) -> Self {
+        Self {
+            parent: (0..size).collect(),
+        }
+    }
+
+    pub fn find(&mut self, v: usize) -> usize {
+        if self.parent[v] != v {
+            self.parent[v] = self.find(self.parent[v]);
+        }
+        self.parent[v]
+    }
+
+    pub fn union(&mut self, x: usize, y: usize) {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        // smh removing rank vec is the key
+        if rx != ry {
+            self.parent[rx] = ry;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -53,9 +94,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(mice_and_cheese(&[1, 1, 3, 4], &[4, 4, 1, 1], 2), 15);
+        assert_eq!(min_reverse_operations(4, 0, &[1, 2], 4), [0, -1, -1, 1]);
+        assert_eq!(min_reverse_operations(4, 2, &[0, 1, 3], 1), [-1, -1, 0, -1]);
+        assert_eq!(
+            min_reverse_operations(5, 0, &[2, 4], 3),
+            [0, -1, -1, -1, -1]
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(min_reverse_operations(4, 0, &[], 4), [0, -1, -1, 1]);
+        assert_eq!(min_reverse_operations(5, 0, &[], 2), [0, 1, 2, 3, 4]);
+    }
 }
