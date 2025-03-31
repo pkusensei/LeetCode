87f557c7745caa3ebc29b0bd32d8ebd680201254
pkusensei <ZW_08@outlook.com>
2025-03-31 15:40:01 -0400

    2653 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,47 +5,30 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_total_price(n: i32, edges: &[[i32; 2]], price: &[i32], trips: &[[i32; 2]]) -> i32 {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push(b);
-        acc[b].push(a);
-        acc
-    });
-    let mut freq = vec![0; n];
-    for t in trips.iter() {
-        count(&adj, t[1] as usize, t[0] as usize, n, &mut freq);
-    }
-    let [a, b] = dfs(&adj, price, &freq, 0, n);
-    a.min(b)
-}
-
-fn dfs(adj: &[Vec<usize>], price: &[i32], freq: &[i32], node: usize, prev: usize) -> [i32; 2] {
-    let mut full = freq[node] * price[node];
-    let mut half = full / 2;
-    for &next in adj[node].iter() {
-        if prev != next {
-            let [temp_full, temp_half] = dfs(adj, price, freq, next, node);
-            full += temp_full.min(temp_half);
-            half += temp_full;
+pub fn get_subarray_beauty(nums: &[i32], k: i32, x: i32) -> Vec<i32> {
+    let k = k as usize;
+    let mut res = vec![];
+    let mut freq = [0; 50];
+    'outer: for (idx, &num) in nums.iter().enumerate() {
+        if num < 0 {
+            freq[(num + 50) as usize] += 1;
+        }
+        if idx >= k && nums[idx - k] < 0 {
+            freq[(nums[idx - k] + 50) as usize] -= 1;
+        }
+        if idx >= k - 1 {
+            let mut count = 0;
+            for (val, f) in (-50..).zip(freq.iter()) {
+                count += f;
+                if count >= x {
+                    res.push(val);
+                    continue 'outer;
+                }
+            }
+            res.push(0);
         }
     }
-    [full, half]
-}
-
-fn count(adj: &[Vec<usize>], goal: usize, node: usize, prev: usize, freq: &mut [i32]) -> bool {
-    if node == goal {
-        freq[node] += 1;
-        return true;
-    }
-    for &next in adj[node].iter() {
-        if prev != next && count(adj, goal, next, node, freq) {
-            freq[node] += 1;
-            return true;
-        }
-    }
-    false
+    res
 }
 
 #[cfg(test)]
@@ -79,15 +62,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimum_total_price(2, &[[0, 1]], &[2, 2], &[[0, 0]]), 1);
+        assert_eq!(get_subarray_beauty(&[1, -1, -3, -2, 3], 3, 2), [-1, -2, -2]);
         assert_eq!(
-            minimum_total_price(
-                4,
-                &[[0, 1], [1, 2], [1, 3]],
-                &[2, 2, 10, 6],
-                &[[0, 3], [2, 1], [2, 3]]
-            ),
-            23
+            get_subarray_beauty(&[-1, -2, -3, -4, -5], 2, 2),
+            [-1, -2, -3, -4]
+        );
+        assert_eq!(
+            get_subarray_beauty(&[-3, 1, 2, -3, 0, -3], 2, 1),
+            [-3, 0, -3, -3, -3]
         );
     }
 
