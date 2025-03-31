3e4cfb76ededaa04521441ea8fd3fdf38bef70e4
pkusensei <ZW_08@outlook.com>
2025-03-31 12:15:39 -0400

    2642 dijkstra

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,51 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_prefix_score(nums: &[i32]) -> Vec<i64> {
-    let mut res = vec![];
-    let mut max = 0;
-    let mut prefix = 0;
-    for num in nums.iter().map(|&v| i64::from(v)) {
-        max = max.max(num);
-        prefix += num + max;
-        res.push(prefix);
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+struct Graph {
+    adj: Vec<Vec<i32>>,
+}
+
+impl Graph {
+    fn new(n: i32, edges: Vec<Vec<i32>>) -> Self {
+        let n = n as usize;
+        let mut adj = vec![vec![0; n]; n];
+        for e in edges {
+            let [a, b, c] = e[..] else { unreachable!() };
+            adj[a as usize][b as usize] = c;
+        }
+        Self { adj }
     }
-    res
+
+    fn add_edge(&mut self, edge: Vec<i32>) {
+        let [a, b, c] = edge[..] else { unreachable!() };
+        self.adj[a as usize][b as usize] = c;
+    }
+
+    fn shortest_path(&self, node1: i32, node2: i32) -> i32 {
+        let mut dists = vec![i32::MAX; self.adj.len()];
+        let [n1, n2] = [node1, node2].map(|v| v as usize);
+        let mut heap = BinaryHeap::from([(Reverse(0), n1)]);
+        while let Some((Reverse(dist), node)) = heap.pop() {
+            if node == n2 {
+                return dist;
+            }
+            if dists[node] < dist {
+                continue;
+            }
+            for (next, &val) in self.adj[node].iter().enumerate() {
+                if val > 0 {
+                    let nd = dist + val;
+                    if nd < dists[next] {
+                        dists[next] = nd;
+                        heap.push((Reverse(nd), next));
+                    }
+                }
+            }
+        }
+        -1
+    }
 }
 
 #[cfg(test)]
@@ -48,11 +83,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(find_prefix_score(&[2, 3, 7, 5, 10]), [4, 10, 24, 36, 56]);
-        assert_eq!(
-            find_prefix_score(&[1, 1, 2, 4, 8, 16]),
-            [2, 4, 8, 16, 32, 64]
+        let mut g = Graph::new(
+            4,
+            vec![vec![0, 2, 5], vec![0, 1, 2], vec![1, 2, 1], vec![3, 0, 3]],
         );
+        assert_eq!(g.shortest_path(3, 2), 6);
+        assert_eq!(g.shortest_path(0, 3), -1);
+        g.add_edge(vec![1, 3, 4]);
+        assert_eq!(g.shortest_path(0, 3), 6);
     }
 
     #[test]
