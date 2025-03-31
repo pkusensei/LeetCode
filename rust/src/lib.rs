c7ffa35e5251d414c575463eefae666c622d44f4
pkusensei <ZW_08@outlook.com>
2025-03-31 19:17:15 -0400

    2654 O(N^2)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,30 +5,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_subarray_beauty(nums: &[i32], k: i32, x: i32) -> Vec<i32> {
-    let k = k as usize;
-    let mut res = vec![];
-    let mut freq = [0; 50];
-    'outer: for (idx, &num) in nums.iter().enumerate() {
-        if num < 0 {
-            freq[(num + 50) as usize] += 1;
-        }
-        if idx >= k && nums[idx - k] < 0 {
-            freq[(nums[idx - k] + 50) as usize] -= 1;
-        }
-        if idx >= k - 1 {
-            let mut count = 0;
-            for (val, f) in (-50..).zip(freq.iter()) {
-                count += f;
-                if count >= x {
-                    res.push(val);
-                    continue 'outer;
-                }
+pub fn min_operations(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let ones = nums.iter().filter(|&&v| v == 1).count();
+    if ones > 0 {
+        return (n - ones) as i32;
+    }
+    let mut res = n;
+    for (left, &a) in nums.iter().enumerate() {
+        let mut val = a;
+        for (right, &b) in nums[left..].iter().enumerate() {
+            val = gcd(val, b);
+            if val == 1 {
+                res = res.min(right);
+                break;
             }
-            res.push(0);
         }
     }
-    res
+    if res == n { -1 } else { (res + n - 1) as i32 }
+}
+
+const fn gcd(a: i32, b: i32) -> i32 {
+    if a == 0 { b } else { gcd(b % a, a) }
 }
 
 #[cfg(test)]
@@ -62,17 +60,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(get_subarray_beauty(&[1, -1, -3, -2, 3], 3, 2), [-1, -2, -2]);
-        assert_eq!(
-            get_subarray_beauty(&[-1, -2, -3, -4, -5], 2, 2),
-            [-1, -2, -3, -4]
-        );
-        assert_eq!(
-            get_subarray_beauty(&[-3, 1, 2, -3, 0, -3], 2, 1),
-            [-3, 0, -3, -3, -3]
-        );
+        assert_eq!(min_operations(&[2, 6, 3, 4]), 4);
+        assert_eq!(min_operations(&[2, 10, 6, 14]), -1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(min_operations(&[1, 1]), 0);
+    }
 }
