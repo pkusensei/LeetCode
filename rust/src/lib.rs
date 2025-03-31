44b9b1774bcc4dd7795350596094e6217a08fe81
pkusensei <ZW_08@outlook.com>
2025-03-31 10:57:46 -0400

    2639

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,36 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_visited_cells(grid: &[&[i32]]) -> i32 {
-    use std::{cmp::Reverse, collections::BinaryHeap};
-    let [rows, cols] = get_dimensions(grid);
-    let mut dp = vec![vec![i64::MAX; cols]; rows];
-    dp[0][0] = 1;
-    let mut heap = BinaryHeap::from([Reverse((1, 0, 0))]);
-    while let Some(Reverse((dist, r, c))) = heap.pop() {
-        if r == rows - 1 && c == cols - 1 {
-            return dist as i32;
-        }
-        for k in 1 + c..(c + grid[r][c] as usize + 1).min(cols) {
-            if dp[r][k] > 1 + dist {
-                dp[r][k] = 1 + dist;
-                heap.push(Reverse((1 + dist, r, k)));
-                if r == rows - 1 && k == cols - 1 {
-                    return 1 + dist as i32;
-                }
-            }
-        }
-        for k in 1 + r..(r + grid[r][c] as usize + 1).min(rows) {
-            if dp[k][c] > 1 + dist {
-                dp[k][c] = 1 + dist;
-                heap.push(Reverse((1 + dist, k, c)));
-                if k == rows - 1 && c == cols - 1 {
-                    return 1 + dist as i32;
-                }
-            }
-        }
+pub fn find_column_width(grid: Vec<Vec<i32>>) -> Vec<i32> {
+        let cols = grid[0].len();
+        (0..cols)
+            .map(|c| grid.iter().map(|r| width(r[c])).max().unwrap())
+            .collect()
+}
+
+fn width(num: i32) -> i32 {
+    match num.cmp(&0) {
+        std::cmp::Ordering::Less => 2 + num.abs().ilog10() as i32,
+        std::cmp::Ordering::Equal => 1,
+        std::cmp::Ordering::Greater => 1 + num.abs().ilog10() as i32,
     }
-    -1
 }
 
 #[cfg(test)]
@@ -67,17 +50,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            minimum_visited_cells(&[&[3, 4, 2, 1], &[4, 2, 3, 1], &[2, 1, 0, 0], &[2, 4, 0, 0]]),
-            4
-        );
-        assert_eq!(
-            minimum_visited_cells(&[&[3, 4, 2, 1], &[4, 2, 1, 1], &[2, 1, 1, 0], &[3, 4, 1, 0]]),
-            3
-        );
-        assert_eq!(minimum_visited_cells(&[&[2, 1, 0], &[1, 0, 0]]), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
