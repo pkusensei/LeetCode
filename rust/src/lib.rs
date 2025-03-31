1a036dfea32aa144ebf38527795762286c801a04
pkusensei <ZW_08@outlook.com>
2025-03-30 20:05:30 -0400

    2615 could not find a better way than LC2121

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,36 +2,39 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn diagonal_prime(nums: Vec<Vec<i32>>) -> i32 {
-    let n = nums.len();
-    let mut res = None;
-    for i in 0..n {
-        let a = nums[i][i];
-        let b = nums[i][n - i - 1];
-        for p in [a, b] {
-            if res.is_none_or(|v| v < p) && is_prime(p) {
-                res = Some(p)
-            }
+pub fn distance(nums: &[i32]) -> Vec<i64> {
+    let prefs =
+        (0..)
+            .zip(nums.iter())
+            .fold(HashMap::<_, Vec<[i64; 2]>>::new(), |mut acc, (i, &num)| {
+                let v = acc.entry(num).or_default();
+                let last = v.last().map(|[_, p]| p).unwrap_or(&0);
+                v.push([i, i + last]);
+                acc
+            });
+    let mut res = vec![];
+    for (idx, num) in (0..).zip(nums.iter()) {
+        let Some(prefix) = prefs.get(num) else {
+            unreachable!()
+        };
+        if prefix.len() <= 1 {
+            res.push(0);
+            continue;
         }
+        let Ok(i) = prefix.binary_search_by_key(&idx, |&[i, _]| i) else {
+            unreachable!()
+        };
+        let left = if i > 0 { prefix[i - 1][1] } else { 0 };
+        let right = prefix.last().unwrap()[1] - prefix[i][1];
+        let len = prefix.len() as i64;
+        res.push(i as i64 * idx - left + right - (len - 1 - i as i64) * idx);
     }
-    res.unwrap_or(0)
-}
-
-fn is_prime(num: i32) -> bool {
-    if num < 2 {
-        false
-    } else {
-        let sq = num.isqrt();
-        for p in 2..=sq {
-            if num % p == 0 {
-                return false;
-            }
-        }
-        true
-    }
+    res
 }
 
 #[cfg(test)]
@@ -65,7 +68,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert!(is_prime(2))
+        assert_eq!(distance(&[1, 3, 1, 1, 2]), [5, 0, 3, 4, 0]);
+        assert_eq!(distance(&[0, 5, 3]), [0, 0, 0]);
     }
 
     #[test]
