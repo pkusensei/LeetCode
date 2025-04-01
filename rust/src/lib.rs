8028c9907e04e7a088cc62cd801d5adb1f0151b0
Zx Wang <ZW_08@outlook.com>
2025-04-01 12:30:11 -0400

    2663 ugly greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,54 +2,34 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap, HashSet},
-};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_cost(start: &[i32], target: &[i32], special_roads: &[[i32; 5]]) -> i32 {
-    let [sx, sy] = [start[0], start[1]];
-    let [tx, ty] = [target[0], target[1]];
-    let mut dists = HashMap::new();
-    dists.insert([sx, sy], 0);
-    dists.insert([tx, ty], sx.abs_diff(tx) + sy.abs_diff(ty));
-    let mut roads = HashMap::<_, HashMap<_, _>>::new();
-    let mut heap = BinaryHeap::from([(Reverse(0), sx, sy)]);
-    let mut vertices = HashSet::from([[tx, ty]]);
-    for r in special_roads.iter() {
-        let [x1, y1, x2, y2, c] = r[..] else {
-            unreachable!()
-        };
-        let v = roads
-            .entry([x1, y1])
-            .or_default()
-            .entry([x2, y2])
-            .or_insert(c);
-        *v = (*v).min(c);
-        vertices.extend([[x1, y1], [x2, y2]]);
-    }
-    while let Some((Reverse(dist), x, y)) = heap.pop() {
-        if dists.get(&[x, y]).is_some_and(|&v| v < dist) {
-            continue;
+pub fn smallest_beautiful_string(s: &str, k: i32) -> String {
+    let (s, n) = (s.as_bytes(), s.len());
+    let k = k as u8 + b'a' - 1;
+    let mut res = s.to_vec();
+    for idx in (0..n).rev() {
+        res[idx] += 1;
+        while !check(&res, idx) {
+            res[idx] += 1;
         }
-        if x == tx && y == ty {
-            return dist as i32;
-        }
-        for vert in vertices.iter() {
-            let mut d = dist + x.abs_diff(vert[0]) + y.abs_diff(vert[1]);
-            if let Some(&c) = roads.get(&[x, y]).and_then(|v| v.get(vert)) {
-                d = d.min(dist + c as u32);
+        if res[idx] <= k {
+            for i in 1 + idx..n {
+                res[i] = b'a';
+                while !check(&res, i) {
+                    res[i] += 1;
+                }
             }
-            if dists.get(vert).is_none_or(|&v| v > d) {
-                dists.insert(*vert, d);
-                heap.push((Reverse(d), vert[0], vert[1]));
-            }
+            return String::from_utf8(res).unwrap();
         }
     }
-    dists[&[tx, ty]] as i32
+    String::new()
+}
+
+fn check(v: &[u8], idx: usize) -> bool {
+    idx.checked_sub(1).is_none_or(|i| v[i] != v[idx])
+        && idx.checked_sub(2).is_none_or(|i| v[i] != v[idx])
 }
 
 #[cfg(test)]
@@ -83,36 +63,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            minimum_cost(&[1, 1], &[4, 5], &[[1, 2, 3, 3, 2], [3, 4, 4, 5, 1]],),
-            5
-        );
-        assert_eq!(
-            minimum_cost(
-                &[3, 2],
-                &[5, 7],
-                &[
-                    [5, 7, 3, 2, 1],
-                    [3, 2, 3, 4, 4],
-                    [3, 3, 5, 5, 5],
-                    [3, 4, 5, 6, 6]
-                ]
-            ),
-            7
-        );
-        assert_eq!(
-            minimum_cost(
-                &[1, 1],
-                &[10, 4],
-                &[
-                    [4, 2, 1, 1, 3],
-                    [1, 2, 7, 4, 4],
-                    [10, 3, 6, 1, 2],
-                    [6, 1, 1, 2, 3]
-                ]
-            ),
-            8
-        );
+        assert_eq!(smallest_beautiful_string("abcz", 26), "abda");
+        assert_eq!(smallest_beautiful_string("dc", 4), "");
     }
 
     #[test]
