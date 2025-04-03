8fc74a19621ad650b8f45f1f1dc6f1d6333797c9
Zx Wang <ZW_08@outlook.com>
2025-04-03 11:19:41 -0400

    2707 trie + dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,59 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn buy_choco(prices: Vec<i32>, money: i32) -> i32 {
-    let [mut min1, mut min2] = [money; 2];
-    for &num in prices.iter() {
-        if num < min1 {
-            min2 = min1;
-            min1 = num
-        } else if num < min2 {
-            min2 = num;
+pub fn min_extra_char(s: &str, dictionary: &[&str]) -> i32 {
+    let mut trie = Trie::default();
+    let mut max_len = 0; // of word in dict
+    for w in dictionary.iter() {
+        trie.add(w.bytes());
+        max_len = max_len.max(w.len());
+    }
+    let n = s.len();
+    let mut dp = vec![n; 1 + n];
+    dp[0] = 0;
+    for end in 1..=n {
+        for left in end.saturating_sub(max_len)..end {
+            let len = end - left;
+            let curr = if trie.find(s[left..end].bytes(), len) {
+                dp[left]
+            } else {
+                dp[left] + len
+            };
+            dp[end] = dp[end].min(curr);
         }
     }
-    let res = money - min1 - min2;
-    if res < 0 { money } else { res }
+    dp[n] as i32
+}
+
+#[derive(Default)]
+struct Trie {
+    nodes: [Option<Box<Trie>>; 26],
+    len: usize,
+}
+
+impl Trie {
+    fn add(&mut self, it: impl Iterator<Item = u8>) {
+        let mut curr = self;
+        let mut len = 0;
+        for b in it {
+            let idx = usize::from(b - b'a');
+            curr = curr.nodes[idx].get_or_insert(Box::new(Trie::default()));
+            len += 1;
+        }
+        curr.len = len;
+    }
+
+    fn find(&self, it: impl Iterator<Item = u8>, len: usize) -> bool {
+        let mut curr = self;
+        for b in it {
+            let idx = usize::from(b - b'a');
+            let Some(ref v) = curr.nodes[idx] else {
+                break;
+            };
+            curr = v;
+        }
+        curr.len == len
+    }
 }
 
 #[cfg(test)]
@@ -49,7 +90,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            min_extra_char("leetscode", &["leet", "code", "leetcode"]),
+            1
+        );
+        assert_eq!(min_extra_char("sayhelloworld", &["hello", "world"]), 3);
+    }
 
     #[test]
     fn test() {}
