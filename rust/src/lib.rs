a2076e81dab1f69baa5c7024d4d33307cc98aa39
Zx Wang <ZW_08@outlook.com>
2025-04-03 14:22:04 -0400

    2712 prefix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,30 +5,35 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn difference_of_distinct_values(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    use std::collections::HashSet;
-    let [rows, cols] = get_dimensions(&grid);
-    let mut res = vec![vec![0; cols]; rows];
-    for r in 0..rows {
-        for c in 0..cols {
-            let mut a = HashSet::new();
-            let mut b = HashSet::new();
-            let mut cr = r;
-            let mut cc = c;
-            while cr > 0 && cc > 0 {
-                a.insert(grid[cr - 1][cc - 1]);
-                cr -= 1;
-                cc -= 1;
-            }
-            cr = r;
-            cc = c;
-            while cr + 1 < rows && cc + 1 < cols {
-                b.insert(grid[1 + cr][1 + cc]);
-                cr += 1;
-                cc += 1;
-            }
-            res[r][c] = a.len().abs_diff(b.len()) as i32;
+pub fn minimum_cost(s: &str) -> i64 {
+    let n = s.len();
+    let mut prefix = vec![0_i64];
+    let mut prev = s.as_bytes()[0];
+    for (i, b) in s.bytes().enumerate().skip(1) {
+        // i is the length of previous substr
+        if prev == b {
+            // no op required to make substr ending in b
+            prefix.push(*prefix.last().unwrap_or(&0));
+        } else {
+            // flip length of i to end in b
+            prefix.push(i as i64 + prefix.last().unwrap_or(&0));
         }
+        prev = b;
+    }
+    let mut suffix = vec![0_i64];
+    prev = s.as_bytes()[n - 1];
+    for (i, b) in s.bytes().rev().enumerate().skip(1) {
+        if prev == b {
+            suffix.push(*suffix.last().unwrap_or(&0));
+        } else {
+            suffix.push(i as i64 + suffix.last().unwrap_or(&0));
+        }
+        prev = b;
+    }
+    suffix.reverse();
+    let mut res = i64::MAX;
+    for (p, s) in prefix.into_iter().zip(suffix) {
+        res = res.min(p + s);
     }
     res
 }
@@ -63,7 +68,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(minimum_cost("0011"), 2);
+        assert_eq!(minimum_cost("010101"), 9);
+    }
 
     #[test]
     fn test() {}
