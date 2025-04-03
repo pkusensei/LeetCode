4462e62e4b6a6ce4acb8d5249b4a23f1da4e90f1
Zx Wang <ZW_08@outlook.com>
2025-04-03 12:29:59 -0400

    2709 dsu.union during sieve

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,32 +2,80 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashSet;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_strength(mut nums: Vec<i32>) -> i64 {
-    let n = nums.len();
-    nums.sort_unstable();
-    let mut res = 0;
-    let mut idx = 0;
-    while 1 + idx < n && nums[idx] < 0 && nums[1 + idx] < 0 {
-        if res == 0 {
-            res = 1;
+pub fn can_traverse_all_pairs(nums: &[i32]) -> bool {
+    if nums.len() < 2 {
+        return true;
+    }
+    let mut set = HashSet::new();
+    let mut max = 0;
+    for &num in nums.iter() {
+        if num == 1 {
+            return false;
         }
-        res *= i64::from(nums[idx] * nums[1 + idx]);
-        idx += 2;
+        set.insert(num);
+        max = max.max(num);
     }
-    while idx < n && nums[idx] <= 0 {
-        idx += 1;
-    }
-    while let Some(&v) = nums.get(idx) {
-        if res == 0 {
-            res = 1;
+    let mut sieve = vec![true; 1 + max as usize];
+    sieve[0..2].copy_from_slice(&[false; 2]);
+    let mut dsu = DSU::new(1 + max as usize);
+    for p in 2..=max as usize {
+        if sieve[p] {
+            for val in (p + p..=max as usize).step_by(p) {
+                sieve[val] = false;
+                if set.contains(&(val as i32)) {
+                    dsu.union(p, val);
+                }
+            }
         }
-        res *= i64::from(v);
-        idx += 1;
     }
-    if res == 0 { nums[n - 1] as i64 } else { res }
+    let root = dsu.find(nums[0] as usize);
+    for &num in nums.iter() {
+        if dsu.find(num as usize) != root {
+            return false;
+        }
+    }
+    true
+}
+
+struct DSU {
+    parent: Vec<usize>,
+    rank: Vec<i32>,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, v: usize) -> usize {
+        if self.parent[v] != v {
+            self.parent[v] = self.find(self.parent[v]);
+        }
+        self.parent[v]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return;
+        }
+        match self.rank[rx].cmp(&self.rank[ry]) {
+            std::cmp::Ordering::Less => self.parent[rx] = ry,
+            std::cmp::Ordering::Equal => {
+                self.parent[ry] = rx;
+                self.rank[rx] += 1;
+            }
+            std::cmp::Ordering::Greater => self.parent[ry] = rx,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -60,7 +108,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert!(can_traverse_all_pairs(&[2, 3, 6]));
+        assert!(!can_traverse_all_pairs(&[3, 9, 5]));
+        assert!(can_traverse_all_pairs(&[4, 3, 12, 8]));
+    }
 
     #[test]
     fn test() {}
