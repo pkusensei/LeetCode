feb577bbcb498e0bda83bfbfc9e39f28bc956076
pkusensei <ZW_08@outlook.com>
2025-04-04 13:32:00 -0400

    2719 digit dp ???

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,71 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn matrix_sum_queries(n: i32, queries: &[[i32; 3]]) -> i64 {
-    use std::collections::HashSet;
-    let n = n as usize;
-    let mut res = 0;
-    let mut row_set = HashSet::new();
-    let mut col_set = HashSet::new();
-    for q in queries.iter().rev() {
-        let [t, idx, val] = q[..] else { unreachable!() };
-        let idx = idx as usize;
-        let val = i64::from(val);
-        if t == 0 && row_set.insert(idx) {
-            res += val * (n - col_set.len()) as i64;
-        } else if t == 1 && col_set.insert(idx) {
-            res += val * (n - row_set.len()) as i64;
+pub fn count(num1: String, num2: String, min_sum: i32, max_sum: i32) -> i32 {
+    let (n1, n2) = (num1.len(), num2.len());
+    let mut v = num1.into_bytes();
+    for b in v.iter_mut().rev() {
+        if *b == b'0' {
+            *b = b'9';
+        } else {
+            *b -= 1;
+            break;
         }
+    }
+    let a = dfs(
+        &v,
+        min_sum,
+        max_sum,
+        0,
+        0,
+        true,
+        &mut vec![vec![-1; 450]; n1],
+    );
+    let b = dfs(
+        num2.as_bytes(),
+        min_sum,
+        max_sum,
+        0,
+        0,
+        true,
+        &mut vec![vec![-1; 450]; n2],
+    );
+    (b - a).rem_euclid(MOD)
+}
+
+const MOD: i32 = 1_000_000_007;
+
+fn dfs(
+    max: &[u8],
+    min_sum: i32,
+    max_sum: i32,
+    idx: usize,
+    sum: i32,
+    limit: bool,
+    memo: &mut [Vec<i32>],
+) -> i32 {
+    if idx >= max.len() {
+        return i32::from((min_sum..=max_sum).contains(&sum));
+    }
+    if !limit && memo[idx][sum as usize] > -1 {
+        return memo[idx][sum as usize];
+    }
+    let mut res = 0;
+    let upper = if limit { i32::from(max[idx] - b'0') } else { 9 };
+    for d in 0..=upper {
+        res += dfs(
+            max,
+            min_sum,
+            max_sum,
+            1 + idx,
+            sum + d,
+            limit && d == upper,
+            memo,
+        );
+        res %= MOD;
+    }
+    if !limit {
+        memo[idx][sum as usize] = res;
     }
     res
 }
@@ -55,16 +105,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            matrix_sum_queries(3, &[[0, 0, 1], [1, 2, 2], [0, 2, 3], [1, 0, 4]]),
-            23
-        );
-        assert_eq!(
-            matrix_sum_queries(3, &[[0, 0, 4], [0, 1, 2], [1, 0, 1], [0, 2, 3], [1, 2, 1]]),
-            17
-        );
+        assert_eq!(count("1".into(), "12".into(), 1, 8), 11);
+        assert_eq!(count("1".into(), "5".into(), 1, 5), 5);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            count("4179205230".into(), "7748704426".into(), 8, 46),
+            883045899
+        );
+    }
 }
