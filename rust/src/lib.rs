75a36e8fb5262451ba0062445b69c88d8e5a792a
pkusensei <ZW_08@outlook.com>
2025-04-06 09:41:26 -0400

    2747 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,36 +2,37 @@ mod dsu;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
+use std::collections::HashMap;
 
-pub fn minimize_concatenated_length(words: &[&str]) -> i32 {
-    let s = words[0].as_bytes();
-    let n = s.len();
-    n as i32 + dfs(words, 1, s[0], s[n - 1], &mut HashMap::new())
-}
-
-fn dfs(
-    words: &[&str],
-    idx: usize,
-    first: u8,
-    last: u8,
-    memo: &mut HashMap<(usize, u8, u8), i32>,
-) -> i32 {
-    if idx >= words.len() {
-        return 0;
+pub fn count_servers(n: i32, mut logs: Vec<[i32; 2]>, x: i32, queries: Vec<i32>) -> Vec<i32> {
+    logs.sort_unstable_by_key(|v| v[1]); // [id, time]
+    let qn = queries.len();
+    let qis = (0..qn)
+        .sorted_unstable_by_key(|&i| queries[i])
+        .collect_vec();
+    let mut res = vec![0; qn];
+    let [mut left, mut right] = [0, 0];
+    let mut map = HashMap::new();
+    for qi in qis {
+        let start = queries[qi] - x;
+        let end = queries[qi];
+        while logs.get(right).is_some_and(|v| v[1] <= end) {
+            *map.entry(logs[right][0]).or_insert(0) += 1;
+            right += 1;
+        }
+        while logs.get(left).is_some_and(|v| v[1] < start) {
+            let id = logs[left][0];
+            *map.entry(id).or_insert(0) -= 1;
+            left += 1;
+            if map[&id] == 0 {
+                map.remove(&id);
+            }
+        }
+        res[qi] = n - map.len() as i32;
     }
-    if let Some(&v) = memo.get(&(idx, first, last)) {
-        return v;
-    }
-    let s = words[idx].as_bytes();
-    let n = s.len();
-    let prepend = n as i32 - i32::from(s[n - 1] == first) + dfs(words, 1 + idx, s[0], last, memo);
-    let append = n as i32 - i32::from(s[0] == last) + dfs(words, 1 + idx, first, s[n - 1], memo);
-    let res = prepend.min(append);
-    memo.insert((idx, first, last), res);
     res
 }
 
@@ -66,13 +67,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimize_concatenated_length(&["aa", "ab", "bc"]), 4);
-        assert_eq!(minimize_concatenated_length(&["ab", "b"]), 2);
-        assert_eq!(minimize_concatenated_length(&["aaa", "c", "aba"]), 6);
+        assert_eq!(
+            count_servers(3, vec![[1, 3], [2, 6], [1, 5]], 5, vec![10, 11]),
+            [1, 2]
+        );
+        assert_eq!(
+            count_servers(3, vec![[2, 4], [2, 1], [1, 2], [3, 1]], 2, vec![3, 4]),
+            [0, 1]
+        );
     }
 
     #[test]
-    fn test() {
-        assert_eq!(minimize_concatenated_length(&["a", "cba", "a"]), 3);
-    }
+    fn test() {}
 }
