5597f1aad75789a6042019b218ee565e3bbb4bf2
pkusensei <ZW_08@outlook.com>
2025-04-06 11:09:23 -0400

    2751 keep popping stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_good_subarray_splits(nums: Vec<i32>) -> i32 {
-    let idx: Vec<_> = nums
-        .iter()
+pub fn survived_robots_healths(positions: &[i32], healths: &[i32], directions: &str) -> Vec<i32> {
+    use itertools::{Itertools, izip};
+    // (id, pos, health, dir)
+    let robs = izip!(positions.iter(), healths.iter(), directions.bytes())
         .enumerate()
-        .filter_map(|(i, &v)| if v == 1 { Some(i) } else { None })
-        .collect();
-    if idx.is_empty() {
-        return 0;
+        .map(|(i, (&p, &h, b))| (i, p, h, b))
+        .sorted_unstable_by_key(|t| t.1)
+        .collect_vec();
+    // (id, health, dir)
+    let mut stack: Vec<(usize, i32, u8)> = vec![];
+    'outer: for rob in robs {
+        let mut curr = (rob.0, rob.2, rob.3);
+        while stack.last().is_some_and(|&v| v.2 == b'R' && curr.2 == b'L') {
+            let v = stack.pop().unwrap();
+            match v.1.cmp(&curr.1) {
+                std::cmp::Ordering::Less => curr.1 -= 1,
+                std::cmp::Ordering::Equal => continue 'outer,
+                std::cmp::Ordering::Greater => {
+                    curr = v;
+                    curr.1 -= 1
+                }
+            }
+        }
+        stack.push(curr);
     }
-    idx.windows(2)
-        .fold(1, |acc, w| acc * (w[1] - w[0]) % 1_000_000_007) as i32
+    stack.sort_unstable_by_key(|v| v.0);
+    stack.into_iter().map(|v| v.1).collect()
 }
 
 #[cfg(test)]
@@ -48,8 +64,26 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            survived_robots_healths(&[5, 4, 3, 2, 1], &[2, 17, 9, 15, 10], "RRRRR"),
+            [2, 17, 9, 15, 10]
+        );
+        assert_eq!(
+            survived_robots_healths(&[3, 5, 2, 6], &[10, 10, 15, 12], "RLRL"),
+            [14]
+        );
+        assert_eq!(
+            survived_robots_healths(&[1, 2, 5, 6], &[10, 10, 11, 11], "RLRL"),
+            []
+        );
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            survived_robots_healths(&[11, 44, 16], &[1, 20, 17], "RLR"),
+            [18]
+        );
+    }
 }
