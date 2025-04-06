1e4f7765cc17c7deb49f6df80408f02c0a0762ce
pkusensei <ZW_08@outlook.com>
2025-04-06 15:40:29 -0400

    2763 left and right

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,49 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_prime_pairs(n: i32) -> Vec<Vec<i32>> {
-    let n = n as usize;
-    let mut sieve = vec![true; 1 + n];
-    sieve[..2].fill(false);
-    for p in 2..=n {
-        if sieve[p] {
-            for i in (2 * p..=n).step_by(p) {
-                sieve[i] = false;
+pub fn sum_imbalance_numbers(nums: &[i32]) -> i32 {
+    use std::collections::BTreeSet;
+    let mut res = 0;
+    for (i1, &a) in nums.iter().enumerate() {
+        let mut map = BTreeSet::from([a]);
+        let mut curr = 0;
+        for &b in nums.iter().skip(1 + i1) {
+            match (map.range(..=b).next_back(), map.range(b..).next()) {
+                (Some(&left), None) => curr += i32::from(b - left > 1),
+                (None, Some(&right)) => curr += i32::from(right - b > 1),
+                (Some(&left), Some(&right)) if b == left || b == right => (),
+                (Some(&left), Some(&right)) => match (b - left > 1, right - b > 1) {
+                    (true, true) => curr += 1,
+                    (true, false) | (false, true) => (),
+                    (false, false) => curr -= 1,
+                },
+                _ => (),
             }
+            res += curr;
+            map.insert(b);
         }
     }
-    let mut res = vec![];
-    for a in 2..=n / 2 {
-        if sieve[a] && sieve[n - a] {
-            res.push(vec![a as i32, (n - a) as i32]);
+    res
+}
+
+pub fn with_vec(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let mut res = 0;
+    for (i1, &a) in nums.iter().enumerate() {
+        let mut seen = vec![false; 2 + n];
+        let mut curr = 0;
+        seen[a as usize] = true;
+        for &b in nums.iter().skip(1 + i1) {
+            let b = b as usize;
+            if !seen[b] {
+                seen[b] = true;
+                if seen[b - 1] && seen[b + 1] {
+                    curr -= 1;
+                } else if !seen[b - 1] && !seen[b + 1] {
+                    curr += 1;
+                }
+            }
+            res += curr;
         }
     }
     res
@@ -55,8 +83,17 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(sum_imbalance_numbers(&[2, 3, 1, 4]), 3);
+        assert_eq!(sum_imbalance_numbers(&[1, 3, 3, 3, 5]), 8);
+
+        assert_eq!(with_vec(&[2, 3, 1, 4]), 3);
+        assert_eq!(with_vec(&[1, 3, 3, 3, 5]), 8);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(sum_imbalance_numbers(&[1, 3, 2]), 1);
+        assert_eq!(with_vec(&[1, 3, 2]), 1);
+    }
 }
