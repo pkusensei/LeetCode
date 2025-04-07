d3aa2daf247e3e3247421e5b92ab34c033bc39f6
pkusensei <ZW_08@outlook.com>
2025-04-07 15:08:26 -0400

    2788 one-liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,49 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_ways(n: i32, x: i32) -> i32 {
-    let n = n as usize;
-    let mut dp = vec![vec![-1; 1 + n]; 1 + n];
-    dfs(n, x as _, 1, &mut dp)
-}
-
-fn dfs(n: usize, x: u32, start: usize, memo: &mut [Vec<i32>]) -> i32 {
-    if n == 0 {
-        return 1;
-    }
-    if memo[n][start] > -1 {
-        return memo[n][start];
-    }
-    let mut res = 0;
-    let mut base = start;
-    while base.pow(x) <= n {
-        res += dfs(n - base.pow(x), x, 1 + base, memo);
-        res %= 1_000_000_007;
-        base += 1;
-    }
-    memo[n][start] = res;
-    res
-}
-
-pub fn tabulation(n: i32, x: i32) -> i32 {
-    let n = n as usize;
-    let x = x as u32;
-    let mut base = 0;
-    let mut v = 1_usize;
-    while v.pow(x) <= n {
-        base = v;
-        v += 1;
-    }
-    let mut dp = vec![0; 1 + n];
-    dp[0] = 1;
-    for i1 in 1..=base {
-        let p = i1.pow(x);
-        for i2 in (p..=n).rev() {
-            dp[i2] += dp[i2 - p];
-            dp[i2] %= 1_000_000_007;
-        }
-    }
-    dp[n]
+pub fn split_words_by_separator(words: Vec<String>, separator: char) -> Vec<String> {
+    words
+        .iter()
+        .flat_map(|word| {
+            word.split(separator).filter_map(|s| {
+                if !s.is_empty() {
+                    Some(s.to_string())
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -80,13 +50,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_ways(10, 2), 1);
-        assert_eq!(number_of_ways(4, 1), 2);
-
-        assert_eq!(tabulation(10, 2), 1);
-        assert_eq!(tabulation(4, 1), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
