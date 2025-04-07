3ed4906ca4ab5a060e125e7ce5cfea1b6371db61
pkusensei <ZW_08@outlook.com>
2025-04-07 11:46:41 -0400

    2781 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,15 +2,38 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashSet;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_of_squares(nums: Vec<i32>) -> i32 {
-    let n = nums.len();
-    (1..)
-        .zip(nums)
-        .filter_map(|(i, v)| if n % i == 0 { Some(v.pow(2)) } else { None })
-        .sum()
+pub fn longest_valid_substring(word: &str, forbidden: &[&str]) -> i32 {
+    let (s, n) = (word.as_bytes(), word.len());
+    let set: HashSet<_> = forbidden.iter().map(|s| s.as_bytes()).collect();
+    let mut res = 0;
+    let mut left = 0;
+    for right in 0..n {
+        let start = left.max(right.saturating_sub(10));
+        for i in start..=right {
+            if is_valid(&set, &s[i..=right]) {
+                break;
+            } else {
+                left = 1 + i;
+            }
+        }
+        res = res.max(right + 1 - left);
+    }
+    res as i32
+}
+
+fn is_valid(set: &HashSet<&[u8]>, s: &[u8]) -> bool {
+    let n = s.len();
+    for i in (0..n).rev() {
+        if set.contains(&s[i..]) {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -43,7 +66,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(longest_valid_substring("cbaaaabc", &["aaa", "cb"]), 4);
+        assert_eq!(longest_valid_substring("leetcode", &["de", "le", "e"]), 4);
+    }
 
     #[test]
     fn test() {}
