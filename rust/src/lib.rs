3c18c54de5fe1e875bcc2c553d0acf350163d585
pkusensei <ZW_08@outlook.com>
2025-04-07 21:22:15 -0400

    2791 O(N^2) -> O(N)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-    let mut seen = std::collections::HashSet::new();
-    let mut len = nums.len();
-    for (idx, &num) in nums.iter().enumerate().rev() {
-        if seen.insert(num) {
-            len = idx;
-        } else {
-            break;
+pub fn count_palindrome_paths(parent: &[i32], s: &str) -> i64 {
+    let n = parent.len();
+    let adj = parent
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (i, &p)| {
+            if p >= 0 {
+                acc[p as usize].push(i);
+            }
+            acc
+        });
+    let mut masks = vec![0; n];
+    dfs(&adj, s.as_bytes(), 0, 0, &mut masks);
+    let mut res = 0;
+    let mut seen = std::collections::HashMap::new();
+    for &mask in masks[1..].iter() {
+        res += i64::from(mask.count_ones() <= 1);
+        for bit in 0..26 {
+            let xor = mask ^ (1 << bit);
+            res += seen.get(&xor).unwrap_or(&0);
         }
+        let v = seen.entry(mask).or_insert(0);
+        res += *v;
+        *v += 1;
     }
-    (len / 3) as i32 + i32::from(len % 3 > 0)
+    res
+}
+
+fn dfs(adj: &[Vec<usize>], s: &[u8], node: usize, mut mask: i32, masks: &mut [i32]) {
+    if node > 0 {
+        mask ^= 1 << (s[node] - b'a');
+        masks[node] = mask;
+    }
+    for &next in adj[node].iter() {
+        dfs(adj, s, next, mask, masks);
+    }
 }
 
 #[cfg(test)]
@@ -48,7 +73,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_palindrome_paths(&[-1, 0, 0, 1, 1, 2], &"acaabc"), 8);
+        assert_eq!(count_palindrome_paths(&[-1, 0, 0, 0, 0], &"aaaaa"), 10);
+    }
 
     #[test]
     fn test() {}
