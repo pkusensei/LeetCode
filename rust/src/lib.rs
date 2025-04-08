2b7e2e8ee19fb9a64b9de86cd893289fb82fc144
Zx Wang <ZW_08@outlook.com>
2025-04-08 10:30:45 -0400

    2800 try all combos

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,26 +5,56 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
-    use itertools::Itertools;
-    use std::collections::HashMap;
-    let n = nums.len();
-    let count = nums.iter().unique().count();
-    let mut map = HashMap::new();
-    let mut left = 0;
-    let mut res = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        *map.entry(num).or_insert(0) += 1;
-        while map.len() == count {
-            res += n - right;
-            *map.entry(nums[left]).or_insert(0) -= 1;
-            if map[&nums[left]] == 0 {
-                map.remove(&nums[left]);
-            }
-            left += 1;
+pub fn minimum_string(a: &str, b: &str, c: &str) -> String {
+    use itertools::{Itertools, chain};
+    let res = chain!(a.bytes(), b.bytes(), c.bytes()).collect_vec();
+    let s = [a.as_bytes(), b.as_bytes(), c.as_bytes()]
+        .iter()
+        .permutations(3)
+        .map(|v| {
+            let [a, b, c] = v[..] else { unreachable!() };
+            let v = build(a, b, c);
+            dbg!(v)
+        })
+        .min_by(|a, b| a.len().cmp(&b.len()).then(a.cmp(b)))
+        .unwrap_or(res);
+    String::from_utf8(s).unwrap()
+}
+
+fn build(a: &[u8], b: &[u8], c: &[u8]) -> Vec<u8> {
+    let mut res = a.to_vec();
+    if !is_sub(&res, b) {
+        append(&mut res, b);
+    }
+    if !is_sub(&res, c) {
+        append(&mut res, c);
+    }
+    res
+}
+
+fn is_sub(mut hay: &[u8], needle: &[u8]) -> bool {
+    while !hay.is_empty() {
+        if hay.starts_with(needle) {
+            return true;
+        }
+        hay = &hay[1..];
+    }
+    false
+}
+
+fn append(res: &mut Vec<u8>, b: &[u8]) {
+    let n = b.len();
+    let mut flag = false;
+    for i in (0..n).rev() {
+        if res.ends_with(&b[..=i]) {
+            res.extend_from_slice(&b[1 + i..]);
+            flag = true;
+            break;
         }
     }
-    res as i32
+    if !flag {
+        res.extend_from_slice(b);
+    }
 }
 
 #[cfg(test)]
@@ -57,8 +87,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(minimum_string("abc", "bca", "aaa"), "aaabca");
+        assert_eq!(minimum_string("ab", "ba", "aba"), "aba");
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(minimum_string("cac", "b", "a"), "bcac")
+    }
 }
