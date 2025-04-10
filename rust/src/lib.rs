eb90c888da19ad1bd6069b8a93859c6963fa1c6f
Zx Wang <ZW_08@outlook.com>
2025-04-10 09:57:42 -0400

    2999 fck digit dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,18 +5,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_sum(n: i32, k: i32) -> i32 {
-    use std::collections::HashSet;
-    let mut set = HashSet::new();
-    let mut num = 1;
-    while set.len() < n as usize {
-        while num < k && set.contains(&(k - num)) {
-            num += 1;
-        }
-        set.insert(num);
-        num += 1;
+pub fn number_of_powerful_int(start: i64, finish: i64, limit: i32, s: &str) -> i64 {
+    less_than(finish, limit, &s) - less_than(start - 1, limit, &s)
+}
+
+fn less_than(num: i64, limit: i32, s: &str) -> i64 {
+    let num = num.to_string();
+    let mut memo = vec![[-1; 2]; num.len()];
+    dfs(&num, limit, s, 0, 1, &mut memo)
+}
+
+fn dfs(num: &str, limit: i32, s: &str, idx: usize, tight: usize, memo: &mut [[i64; 2]]) -> i64 {
+    let n = num.len();
+    if idx >= n {
+        return 1;
     }
-    set.into_iter().sum()
+    if n < s.len() {
+        return 0;
+    }
+    if memo[idx][tight] > -1 {
+        return memo[idx][tight];
+    }
+    let len = n - s.len();
+    let num_d = i32::from(num.as_bytes()[idx] - b'0');
+    let upper = if tight > 0 { num_d.min(limit) } else { limit };
+    let mut res = 0;
+    if idx >= len {
+        match upper.cmp(&i32::from(s.as_bytes()[idx - len] - b'0')) {
+            std::cmp::Ordering::Less => return 0,
+            std::cmp::Ordering::Greater => return 1,
+            std::cmp::Ordering::Equal => res += dfs(num, limit, s, 1 + idx, tight & 1, memo),
+        }
+    } else {
+        for d in 0..=upper {
+            let next_tight = tight & usize::from(d == num_d);
+            res += dfs(num, limit, s, 1 + idx, next_tight, memo);
+        }
+    }
+    memo[idx][tight] = res;
+    res
 }
 
 #[cfg(test)]
@@ -49,7 +76,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_powerful_int(1, 6000, 4, "124"), 5);
+        assert_eq!(number_of_powerful_int(15, 215, 6, "10"), 2);
+        assert_eq!(number_of_powerful_int(1000, 2000, 4, "3000"), 0);
+    }
 
     #[test]
     fn test() {}
