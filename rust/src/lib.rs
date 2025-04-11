e9faeed74c1b59842eab9c9bc9dcece79710cf06
Zx Wang <ZW_08@outlook.com>
2025-04-11 10:00:05 -0400

    2836 not binary lifting??

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
-    (low..=high).filter(|&v| check(v)).count() as i32
-}
-
-fn check(num: i32) -> bool {
-    if (1 + num.ilog10()) & 1 == 1 {
-        return false;
+pub fn get_max_function_value(receiver: &[i32], k: i64) -> i64 {
+    let n = receiver.len();
+    let k = k as usize;
+    let mut edges = vec![];
+    // [node, score]
+    let mut prev: Vec<_> = receiver.iter().map(|&v| [v as usize; 2]).collect();
+    let mut dist = 2;
+    while dist <= k {
+        let next = prev
+            .iter()
+            .map(|&[node, score]| {
+                let [next, next_score] = prev[node];
+                [next, score + next_score]
+            })
+            .collect();
+        edges.push(prev);
+        prev = next;
+        dist <<= 1;
     }
-    let s = num.to_string();
-    let n = s.len();
-    let a: u8 = s[..n / 2].bytes().map(|b| b - b'0').sum();
-    let b: u8 = s[n / 2..].bytes().map(|b| b - b'0').sum();
-    a == b
+    edges.push(prev);
+    let mut res = 0;
+    for mut node in 0..n {
+        let mut score = node;
+        let mut b = 0;
+        while (1 << b) <= k {
+            if (1 << b) & k > 0 {
+                let [next, s] = edges[b][node];
+                node = next;
+                score += s;
+            }
+            b += 1;
+        }
+        res = res.max(score)
+    }
+    res as i64
 }
 
 #[cfg(test)]
@@ -50,7 +72,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(get_max_function_value(&[2, 0, 1], 4), 6);
+        assert_eq!(get_max_function_value(&[1, 1, 1, 2, 3], 3), 10);
+    }
 
     #[test]
     fn test() {}
