54d0816f64a1cf9b313cde5223b8c047146de81f
pkusensei <ZW_08@outlook.com>
2025-04-11 12:21:51 -0400

    2844 brute-force

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,43 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_k_subsequences_with_max_beauty(s: &str, k: i32) -> i32 {
-    use itertools::Itertools;
-    use std::cmp::Reverse;
-    const MOD: usize = 1_000_000_007;
-    let count = s
-        .bytes()
-        .fold([0; 26], |mut acc, b| {
-            acc[usize::from(b - b'a')] += 1;
-            acc
-        })
-        .into_iter()
-        .filter(|&v| v > 0)
-        .sorted_unstable_by_key(|&v| Reverse(v))
-        .collect_vec();
-    let n = count.len();
-    let mut k = k as usize;
-    if !(1..=n).contains(&k) {
-        return 0;
+pub fn minimum_operations(num: String) -> i32 {
+    let n = num.len();
+    let mut zeros = 0;
+    let mut res = n;
+    for (i1, a) in num.bytes().enumerate() {
+        if b"0257".contains(&a) {
+            zeros += i32::from(a == b'0');
+            for b in num.bytes().skip(1 + i1) {
+                match a {
+                    b'0' | b'5' if b == b'0' => res = res.min(n - i1 - 2),
+                    b'2' | b'7' if b == b'5' => res = res.min(n - i1 - 2),
+                    _ => (),
+                }
+            }
+        }
     }
-    let mut freq = count.as_slice();
-    let mut res = 1;
-    while let Some(&f) = freq.get(0) {
-        let mut repeat = freq.iter().filter(|&&v| v == f).count();
-        if repeat > k {
-            let m = repeat - k;
-            let comb = (1..=k).fold(1, |acc, v| acc * (m + v) / v);
-            res = (res * comb) % MOD;
-            repeat = k;
-        }
-        for _ in 0..repeat {
-            res = res * f % MOD;
-        }
-        if repeat >= k {
-            break;
-        }
-        freq = &freq[repeat..];
-        k -= repeat;
+    if zeros == 1 {
+        res = res.min(n-1);
     }
     res as i32
 }
@@ -76,10 +57,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_k_subsequences_with_max_beauty("bcca", 2), 4);
-        assert_eq!(count_k_subsequences_with_max_beauty("abbcd", 4), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
