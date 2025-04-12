5f9d1f00f69494a0149e78dfd8e8ca240a0f2bdb
pkusensei <ZW_08@outlook.com>
2025-04-12 19:12:52 -0400

    2864

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,41 +5,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_sum(nums: &[i32]) -> i64 {
-    use std::collections::HashMap;
-    let n = nums.len();
-    let sieve = primes(n);
-    let mut map = HashMap::new();
-    for (mut i, &val) in (1..).zip(nums.iter()) {
-        let mut product = 1;
-        for &p in sieve.iter() {
-            while i % (p * p) == 0 {
-                i /= p * p;
-            }
-            if i % p == 0 {
-                product *= p;
-            }
-        }
-        *map.entry(product).or_insert(0) += i64::from(val);
-    }
-    map.into_values().max().unwrap()
-}
-
-fn primes(n: usize) -> Vec<i32> {
-    let mut sieve = vec![true; 1 + n];
-    sieve[..2].fill(false);
-    for p in 2..=n {
-        if sieve[p] {
-            for val in (2 * p..=n).step_by(p) {
-                sieve[val] = false;
-            }
-        }
-    }
-    sieve
-        .iter()
-        .enumerate()
-        .filter_map(|(i, &v)| if v { Some(i as i32) } else { None })
-        .collect()
+pub fn maximum_odd_binary_number(s: String) -> String {
+    let n = s.len();
+    let mut s = s.into_bytes();
+    let ones = s.iter().filter(|&&v| v == b'1').count();
+    s.fill(b'0');
+    s[n - 1] = b'1';
+    s[..ones - 1].fill(b'1');
+    String::from_utf8(s).unwrap()
 }
 
 #[cfg(test)]
@@ -72,10 +45,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(maximum_sum(&[8, 7, 3, 5, 7, 2, 4, 9]), 16);
-        assert_eq!(maximum_sum(&[8, 10, 3, 8, 1, 13, 7, 9, 4]), 20);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
