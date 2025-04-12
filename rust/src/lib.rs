290aed3082203255fad5d4964632c1b0946e1fe1
pkusensei <ZW_08@outlook.com>
2025-04-12 11:00:05 -0400

    2855 cyclic

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,56 +5,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_ways(s: &str, t: &str, k: i64) -> i32 {
-    const MOD: i64 = 1_000_000_007;
-    let n = t.len();
-    let mut s = s.as_bytes().to_vec();
-    s.extend_from_within(..n - 1);
-    let pos = kmp(&s, t.as_bytes());
-    let mut f_k = [0, 0];
-    f_k[1] = (mod_pow(n as i64 - 1, k, MOD) + ((k & 1) * 2 - 1)).rem_euclid(MOD)
-        * mod_pow(n as i64, MOD - 2, MOD);
-    f_k[0] = (f_k[1] - ((k & 1) * 2 - 1)).rem_euclid(MOD);
-    let mut res = 0;
-    for p in pos {
-        if p == 0 {
-            res += f_k[0]
-        } else {
-            res += f_k[1]
+pub fn minimum_right_shifts(nums: Vec<i32>) -> i32 {
+    let Some(start) = nums.windows(2).position(|w| w[0] > w[1]) else {
+        return 0;
+    };
+    let n = nums.len();
+    let mut i = (1 + start) % n;
+    while i != start {
+        if nums[i] > nums[(i + 1) % n] {
+            return -1;
         }
-        res %= MOD;
+        i = (1 + i) % n;
     }
-    res as i32
-}
-
-fn kmp(s: &[u8], t: &[u8]) -> Vec<usize> {
-    let n = t.len();
-    let mut f = vec![0; n];
-    for i1 in 1..n {
-        let mut i2 = f[i1 - 1];
-        while i2 > 0 && t[i2] != t[i1] {
-            i2 = f[i2 - 1];
-        }
-        if i2 == 0 && t[0] != t[i1] {
-            f[i1] = 0;
-        } else {
-            f[i1] = 1 + i2;
-        }
-    }
-    let mut i2 = 0;
-    let mut res = vec![];
-    for (i1, &byte) in s.iter().enumerate() {
-        while i2 >= n || (i2 > 0 && byte != t[i2]) {
-            i2 = f[i2 - 1];
-        }
-        if byte == t[i2] {
-            i2 += 1
-        }
-        if i2 == n {
-            res.push(i1 + 1 - n);
-        }
-    }
-    res
+    (n - start - 1) as i32
 }
 
 #[cfg(test)]
@@ -87,10 +50,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_ways("abcd", "cdab", 2), 2);
-        assert_eq!(number_of_ways("ababab", "ababab", 1), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
