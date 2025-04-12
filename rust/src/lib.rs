c856a8b058ae50076d8ed0341f86465cd27f7254
pkusensei <ZW_08@outlook.com>
2025-04-12 13:32:50 -0400

    2858 reroot dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,20 +5,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_pairs(coordinates: &[[i32; 2]], k: i32) -> i32 {
-    let mut map = std::collections::HashMap::new();
+pub fn min_edge_reversals(n: i32, edges: &[[i32; 2]]) -> Vec<i32> {
+    let n = n as usize;
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push((b, 1));
+        acc[b].push((a, -1));
+        acc
+    });
+    let mut res = vec![0; n];
+    res[0] = dfs(&adj, 0, n);
+    reroot(&adj, 0, n, &mut res);
+    res
+}
+
+fn dfs(adj: &[Vec<(usize, i8)>], node: usize, prev: usize) -> i32 {
     let mut res = 0;
-    for c in coordinates.iter() {
-        let [x1, y1] = c[..] else { unreachable!() };
-        for x in 0..=k {
-            let y = k - x;
-            let x2 = x ^ x1;
-            let y2 = y ^ y1;
-            res += map.get(&[x2, y2]).unwrap_or(&0);
+    for &(next, val) in adj[node].iter() {
+        if prev == next {
+            continue;
         }
-        *map.entry([x1, y1]).or_insert(0) += 1;
+        res += i32::from(val == -1) + dfs(adj, next, node);
     }
     res
+}
+
+fn reroot(adj: &[Vec<(usize, i8)>], node: usize, prev: usize, res: &mut [i32]) {
+    let curr = res[node];
+    for &(next, val) in adj[node].iter() {
+        if prev == next {
+            continue;
+        }
+        // if val==-1 {curr-1}
+        // else {curr+1}
+        res[next] = curr + i32::from(val);
+        reroot(adj, next, node, res);
+    }
 }
 
 #[cfg(test)]
@@ -52,11 +74,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(count_pairs(&[[1, 2], [4, 2], [1, 3], [5, 2]], 5), 2);
         assert_eq!(
-            count_pairs(&[[1, 3], [1, 3], [1, 3], [1, 3], [1, 3]], 0),
-            10
+            min_edge_reversals(4, &[[2, 0], [2, 1], [1, 3]]),
+            [1, 1, 0, 2]
         );
+        assert_eq!(min_edge_reversals(3, &[[1, 2], [2, 0]]), [2, 0, 1]);
     }
 
     #[test]
