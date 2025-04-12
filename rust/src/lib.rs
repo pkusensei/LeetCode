73fcf014fd208e1e0ab16acada4d058665e0641b
pkusensei <ZW_08@outlook.com>
2025-04-12 14:46:11 -0400

    2860 what

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,42 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_edge_reversals(n: i32, edges: &[[i32; 2]]) -> Vec<i32> {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push((b, 1));
-        acc[b].push((a, -1));
-        acc
-    });
-    let mut res = vec![0; n];
-    res[0] = dfs(&adj, 0, n);
-    reroot(&adj, 0, n, &mut res);
-    res
-}
-
-fn dfs(adj: &[Vec<(usize, i8)>], node: usize, prev: usize) -> i32 {
-    let mut res = 0;
-    for &(next, val) in adj[node].iter() {
-        if prev == next {
-            continue;
-        }
-        res += i32::from(val == -1) + dfs(adj, next, node);
+pub fn count_ways(mut nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    nums.sort_unstable();
+    let mut res = i32::from(nums[0] > 0);
+    for (i, &val) in nums.iter().enumerate() {
+        res +=
+            i32::from((1 + i) as i32 > val && nums.get(1 + i).is_some_and(|&v| v > (1 + i) as i32));
     }
+    res += i32::from(nums[n - 1] < n as i32);
     res
-}
-
-fn reroot(adj: &[Vec<(usize, i8)>], node: usize, prev: usize, res: &mut [i32]) {
-    let curr = res[node];
-    for &(next, val) in adj[node].iter() {
-        if prev == next {
-            continue;
-        }
-        // if val==-1 {curr-1}
-        // else {curr+1}
-        res[next] = curr + i32::from(val);
-        reroot(adj, next, node, res);
-    }
 }
 
 #[cfg(test)]
@@ -74,13 +48,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            min_edge_reversals(4, &[[2, 0], [2, 1], [1, 3]]),
-            [1, 1, 0, 2]
-        );
-        assert_eq!(min_edge_reversals(3, &[[1, 2], [2, 0]]), [2, 0, 1]);
+        assert_eq!(count_ways(vec![1, 1]), 2);
+        assert_eq!(count_ways(vec![6, 0, 3, 3, 6, 7, 2, 7]), 3);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(count_ways(vec![1, 1, 0, 1]), 1);
+    }
 }
