3bf4d8239d9a2623134da23850aabf4009ffa62a
pkusensei <ZW_08@outlook.com>
2025-04-13 16:16:26 -0400

    2872 dfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,17 +7,31 @@ namespace Solution;
 
 public class Solution
 {
-    public bool CanMakeSubsequence(string str1, string str2)
+    public int MaxKDivisibleComponents(int n, int[][] edges, int[] values, int k)
     {
-        int i = 0;
-        foreach (var c in str1)
+        List<List<int>> adj = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
+        foreach (var edge in edges)
         {
-            if (c == str2[i] || c + 1 == str2[i] || (c == 'z' && str2[i] == 'a'))
-            {
-                i += 1;
-            }
-            if (i == str2.Length) { break; }
+            adj[edge[0]].Add(edge[1]);
+            adj[edge[1]].Add(edge[0]);
         }
-        return i == str2.Length;
+        int res = 1;
+        Dfs(0, -1);
+        return res;
+
+        long Dfs(int node, int prev)
+        {
+            long sum = values[node];
+            foreach (var next in adj[node])
+            {
+                if (next != prev)
+                {
+                    var s = Dfs(next, node);
+                    if (s % k == 0) { res += 1; }
+                    else { sum += s; }
+                }
+            }
+            return sum;
+        }
     }
 }
