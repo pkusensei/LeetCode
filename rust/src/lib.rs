234c497e933ee159331a4e71269f5dfc17f7ac54
pkusensei <ZW_08@outlook.com>
2025-04-13 18:49:42 -0400

    2875 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,22 +5,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_subarrays(nums: Vec<i32>) -> i32 {
-    let bitand = nums.iter().fold(i32::MAX, |acc, &num| acc & num);
-    if bitand > 0 {
-        1
-    } else {
-        let mut curr = i32::MAX;
-        let mut res = 0;
-        for &num in nums.iter() {
-            curr &= num;
-            if curr == 0 {
-                res += 1;
-                curr = i32::MAX;
-            }
-        }
-        res
+pub fn min_size_subarray(nums: &[i32], target: i32) -> i32 {
+    use std::collections::HashMap;
+    let k = i64::from(target);
+    let n = nums.len();
+    let sum: i64 = nums.iter().map(|&v| i64::from(v)).sum();
+    let rem = k % sum;
+    let len = (k / sum) as i32 * n as i32;
+    if rem == 0 {
+        return len;
     }
+    let mut res = i32::MAX;
+    let mut prefix = HashMap::new();
+    let mut curr = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        curr += i64::from(num);
+        if let Some(&left) = prefix.get(&(curr - rem)) {
+            res = res.min((idx - left) as i32 + len);
+        }
+        let right_sum = sum - curr;
+        if let Some(&left) = prefix.get(&(rem - right_sum)) {
+            res = res.min((1 + left + n - idx - 1) as i32 + len);
+        }
+        prefix.insert(curr, idx);
+    }
+    if res == i32::MAX { -1 } else { res }
 }
 
 #[cfg(test)]
@@ -53,8 +62,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_size_subarray(&[1, 2, 3], 5), 2);
+        assert_eq!(min_size_subarray(&[1, 1, 1, 2, 3], 4), 2);
+        assert_eq!(min_size_subarray(&[2, 4, 6, 8], 3), -1);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(min_size_subarray(&[2, 1, 5, 7, 7, 1, 6, 3], 39), 9);
+    }
 }
