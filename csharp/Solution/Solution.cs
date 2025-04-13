c2f78b119199b86fcdc5631fc2205c7cde271ace
pkusensei <ZW_08@outlook.com>
2025-04-13 16:30:16 -0400

    2874 greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,31 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxKDivisibleComponents(int n, int[][] edges, int[] values, int k)
+    public long MaximumTripletValue(int[] nums)
     {
-        List<List<int>> adj = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
-        foreach (var edge in edges)
+        long maxi = 0;
+        long delta = 0;
+        long res = 0;
+        foreach (var num in nums)
         {
-            adj[edge[0]].Add(edge[1]);
-            adj[edge[1]].Add(edge[0]);
+            res = Math.Max(res, delta * num);
+            delta = Math.Max(delta, maxi - num);
+            maxi = Math.Max(maxi, num);
         }
-        int res = 1;
-        Dfs(0, -1);
         return res;
-
-        long Dfs(int node, int prev)
-        {
-            long sum = values[node];
-            foreach (var next in adj[node])
-            {
-                if (next != prev)
-                {
-                    var s = Dfs(next, node);
-                    if (s % k == 0) { res += 1; }
-                    else { sum += s; }
-                }
-            }
-            return sum;
-        }
     }
 }
