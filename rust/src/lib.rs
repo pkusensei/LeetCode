409425e0aceba06bee4020014e048b87f51ba0e2
pkusensei <ZW_08@outlook.com>
2025-04-14 08:45:58 -0400

    2896 dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,14 +5,35 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_processing_time(mut processor_time: Vec<i32>, mut tasks: Vec<i32>) -> i32 {
-    processor_time.sort_unstable_by(|a, b| b.cmp(a));
-    tasks.sort_unstable();
-    let mut res = 0;
-    for (i, p) in processor_time.iter().enumerate() {
-        res = res.max(tasks[i * 4 + 3] + p);
+pub fn min_operations(s1: &str, s2: &str, x: i32) -> i32 {
+    let diff: Vec<i32> = s1
+        .bytes()
+        .zip(s2.bytes())
+        .enumerate()
+        .filter_map(|(i, (b1, b2))| if b1 != b2 { Some(i as i32) } else { None })
+        .collect();
+    let n = diff.len();
+    if n & 1 == 1 {
+        return -1;
     }
-    res
+    dfs(&diff, f64::from(x) / 2.0, 0, &mut vec![-1.0; n]) as i32
+}
+
+fn dfs(diff: &[i32], cost: f64, idx: usize, memo: &mut [f64]) -> f64 {
+    let n = diff.len();
+    if idx >= n {
+        return 0.0;
+    }
+    if idx == n - 1 {
+        return cost;
+    }
+    if memo[idx] > -1.0 {
+        return memo[idx];
+    }
+    let op1 = cost + dfs(diff, cost, 1 + idx, memo);
+    let op2 = f64::from(diff[1 + idx] - diff[idx]) + dfs(diff, cost, 2 + idx, memo);
+    memo[idx] = op1.min(op2);
+    memo[idx]
 }
 
 #[cfg(test)]
@@ -45,7 +66,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_operations("1100011000", "0101001010", 2), 4);
+        assert_eq!(min_operations("10110", "00011", 4), -1)
+    }
 
     #[test]
     fn test() {}
