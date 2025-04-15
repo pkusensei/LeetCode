54e7569c28db01685b4bd5a266d975795c99c96d
Zx Wang <ZW_08@outlook.com>
2025-04-15 16:01:22 -0400

    2916 segment tree, and it's lazy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,44 +2,74 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn length_of_longest_subsequence(nums: &[i32], target: i32) -> i32 {
+pub fn sum_counts(nums: &[i32]) -> i32 {
     let n = nums.len();
-    let mut memo = vec![vec![None; 1 + target as usize]; n];
-    dfs(nums, target, 0, &mut memo).unwrap_or(-1)
+    let mut last_pos = HashMap::new();
+    let mut tree = SegmentTree::new(n);
+    let mut res = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        let start = last_pos.get(&num).map(|v| v + 1).unwrap_or(0);
+        let end = idx;
+        tree.add_one(0, 0, n - 1, start, end);
+        res += tree.square[0];
+        res %= MOD;
+        last_pos.insert(num, idx);
+    }
+    res as i32
 }
 
-fn dfs(nums: &[i32], target: i32, idx: usize, memo: &mut [Vec<Option<i32>>]) -> Option<i32> {
-    if target <= 0 {
-        return if target == 0 { Some(0) } else { None };
-    }
-    if idx >= nums.len() {
-        return None;
-    }
-    if let Some(v) = memo[idx][target as usize] {
-        return if v == -1 { None } else { Some(v) };
-    }
-    let skip = dfs(nums, target, 1 + idx, memo);
-    let take = dfs(nums, target - nums[idx], 1 + idx, memo).map(|v| 1 + v);
-    let res = skip.max(take);
-    memo[idx][target as usize] = if res.is_none() { Some(-1) } else { res };
-    res
+const MOD: i64 = 1_000_000_007;
+
+struct SegmentTree {
+    lazy: Vec<i64>,
+    sum: Vec<i64>,
+    square: Vec<i64>,
 }
 
-pub fn tabulation(nums: &[i32], target: i32) -> i32 {
-    let mut dp = vec![i32::MIN; 1 + target as usize];
-    dp[0] = 0;
-    for &num in nums.iter() {
-        for i in (num..=target).rev() {
-            dp[i as usize] = dp[i as usize].max(1 + dp[(i - num) as usize]);
+impl SegmentTree {
+    fn new(n: usize) -> Self {
+        Self {
+            lazy: vec![0; 4 * n],
+            sum: vec![0; 4 * n],
+            square: vec![0; 4 * n],
         }
     }
-    if dp[target as usize] < 0 {
-        -1
-    } else {
-        dp[target as usize]
+
+    fn update(&mut self, idx: usize, left: usize, right: usize) {
+        if left < right {
+            self.lazy[2 * idx + 1] += self.lazy[idx];
+            self.lazy[2 * idx + 2] += self.lazy[idx];
+        }
+        let gap = 1 + right - left;
+        let sum = self.sum[idx] + gap as i64 * self.lazy[idx];
+        let square = self.square[idx]
+            + 2 * self.lazy[idx] * self.sum[idx]
+            + self.lazy[idx].pow(2) * gap as i64;
+        self.sum[idx] = sum % MOD;
+        self.square[idx] = square % MOD;
+        self.lazy[idx] = 0;
+    }
+
+    fn add_one(&mut self, idx: usize, left: usize, right: usize, x: usize, y: usize) {
+        self.update(idx, left, right);
+        if right < x || left > y {
+            return;
+        }
+        if x <= left && right <= y {
+            self.lazy[idx] = 1;
+            self.update(idx, left, right);
+            return;
+        }
+        let mid = left.midpoint(right);
+        self.add_one(2 * idx + 1, left, mid, x, y);
+        self.add_one(2 * idx + 2, 1 + mid, right, x, y);
+        self.sum[idx] = (self.sum[2 * idx + 2] + self.sum[2 * idx + 1]) % MOD;
+        self.square[idx] = (self.square[2 * idx + 2] + self.square[2 * idx + 1]) % MOD;
     }
 }
 
@@ -74,13 +104,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(length_of_longest_subsequence(&[1, 2, 3, 4, 5], 9), 3);
-        assert_eq!(length_of_longest_subsequence(&[4, 1, 3, 2, 1, 5], 7), 4);
-        assert_eq!(length_of_longest_subsequence(&[1, 1, 5, 4, 5], 3), -1);
-
-        assert_eq!(tabulation(&[1, 2, 3, 4, 5], 9), 3);
-        assert_eq!(tabulation(&[4, 1, 3, 2, 1, 5], 7), 4);
-        assert_eq!(tabulation(&[1, 1, 5, 4, 5], 3), -1);
+        assert_eq!(sum_counts(&[1, 2, 1]), 15);
+        assert_eq!(sum_counts(&[2, 2]), 3);
     }
 
     #[test]
