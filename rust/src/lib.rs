a67bb7fadd433c0fb27bae8c2697c79fc551476b
pkusensei <ZW_08@outlook.com>
2025-04-16 15:51:12 -0400

    2928 dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,39 +2,29 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_balanced_subsequence_sum(nums: &[i64]) -> i64 {
-    use std::collections::BTreeMap;
-    let arr: Vec<_> = nums
-        .iter()
-        .enumerate()
-        .map(|(i, &num)| i64::from(num) - i as i64)
-        .collect();
-    // nums[i]-i => max_sum up to i
-    let mut map = BTreeMap::new();
-    let mut res = i64::MIN;
-    for (&num, &val) in nums.iter().zip(arr.iter()) {
-        let mut curr_sum = i64::from(num);
-        if curr_sum <= 0 {
-            res = res.max(curr_sum); // negative => record only single number
-            continue;
-        }
-        if let Some((_, &prev)) = map.range(..=val).next_back() {
-            curr_sum += prev; // max prev_sum up until i
-        }
-        let del: Vec<_> = map
-            .range(val..)
-            .take_while(|(_, v)| **v < curr_sum)
-            .map(|(&k, _)| k)
-            .collect(); // monotonic map
-        for k in del {
-            map.remove(&k);
-        }
-        map.insert(val, curr_sum);
-        res = res.max(curr_sum);
+pub fn distribute_candies(n: i32, limit: i32) -> i32 {
+        dfs(n, limit, 3, &mut HashMap::new())
+}
+
+fn dfs(n: i32, limit: i32, c: i32, memo: &mut HashMap<[i32; 2], i32>) -> i32 {
+    if c == 0 {
+        return i32::from(n == 0);
     }
+    if n < 0 {
+        return 0;
+    }
+    if let Some(&v) = memo.get(&[n, c]) {
+        return v;
+    }
+    let res = (0..=limit.min(n))
+        .map(|v| dfs(n - v, limit, c - 1, memo))
+        .sum();
+    memo.insert([n, c], res);
     res
 }
 
@@ -69,9 +59,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_balanced_subsequence_sum(&[3, 3, 5, 6]), 14);
-        assert_eq!(max_balanced_subsequence_sum(&[5, -1, -3, 8]), 13);
-        assert_eq!(max_balanced_subsequence_sum(&[-2, -1]), -1);
+        assert_eq!(distribute_candies(5, 2), 3);
+        assert_eq!(distribute_candies(3, 3), 10);
     }
 
     #[test]
