664c2651f5004338fb9ba57e5ccacc02b6307765
pkusensei <ZW_08@outlook.com>
2025-04-15 20:12:48 -0400

    2918 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,22 +5,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_k_or(nums: Vec<i32>, k: i32) -> i32 {
-    let mut freq = [0; 32];
-    for &num in nums.iter() {
-        for bit in 0..32 {
-            if num & (1 << bit) > 0 {
-                freq[bit] += 1;
-            }
-        }
+pub fn min_sum(nums1: &[i32], nums2: &[i32]) -> i64 {
+    let [mut zero1, mut sum1] = f(&nums1);
+    let [mut zero2, mut sum2] = f(&nums2);
+    if zero1 < zero2 {
+        std::mem::swap(&mut zero1, &mut zero2);
+        std::mem::swap(&mut sum1, &mut sum2);
     }
-    let mut res = 0;
-    for (bit, &v) in freq.iter().enumerate() {
-        if v >= k {
-            res |= 1 << bit;
-        }
+    // zero1 >= zero2
+    if zero1 == 0 {
+        if sum1 == sum2 { sum1 } else { -1 }
+    } else if zero2 == 0 {
+        if sum1 + zero1 <= sum2 { sum2 } else { -1 }
+    } else {
+        (sum1 + zero1).max(sum2 + zero2)
     }
-    res
+}
+
+fn f(nums: &[i32]) -> [i64; 2] {
+    let mut zeros = 0;
+    let mut sum = 0;
+    for &num in nums {
+        sum += i64::from(num);
+        zeros += i64::from(num == 0);
+    }
+    [zeros, sum]
 }
 
 #[cfg(test)]
@@ -53,7 +62,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_sum(&[3, 2, 0, 1, 0], &[6, 5, 0]), 12);
+    }
 
     #[test]
     fn test() {}
