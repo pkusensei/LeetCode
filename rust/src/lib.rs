c509bc41288f57867f404716ceb2222102d9cf43
Zx Wang <ZW_08@outlook.com>
2025-04-17 12:07:41 -0400

    2937

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,51 +5,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_strong_pair_xor(mut nums: Vec<i32>) -> i32 {
-    nums.sort_unstable();
-    let mut res = 0;
-    for (i, &num) in nums.iter().enumerate() {
-        let mut left = i;
-        let mut right = nums.partition_point(|&v| v <= 2 * num);
-        for bit in (0..=20).rev() {
-            if left >= right {
-                break;
-            }
-            let [nl, nr] = if num & (1 << bit) > 0 {
-                // bit is set in [left]
-                // confine search within range where it's unset
-                [left, binary_search(&nums, left, right, 1 << bit)]
-            } else {
-                // Or search in all numbers that has it set
-                [binary_search(&nums, left, right, 1 << bit), right]
-            };
-            if nl != nr {
-                left = nl;
-                right = nr;
-            }
-        }
-        res = res.max(num ^ nums[left]);
+pub fn find_minimum_operations(s1: String, s2: String, s3: String) -> i32 {
+    use itertools::izip;
+    let len = izip!(s1.bytes(), s2.bytes(), s3.bytes())
+        .take_while(|(a, b, c)| a == b && b == c)
+        .count();
+    if len == 0 {
+        -1
+    } else {
+        (s1.len() - len + s2.len() - len + s3.len() - len) as i32
     }
-    res
-}
-
-// Find the min num with set bit in mask
-fn binary_search(nums: &[i32], mut left: usize, mut right: usize, mask: i32) -> usize {
-    if nums[left] & mask > 0 {
-        return left;
-    }
-    if right.checked_sub(1).is_some_and(|i| nums[i] & mask == 0) {
-        return right;
-    }
-    while left < right {
-        let mid = left.midpoint(right);
-        if nums[mid] & mask > 0 {
-            right = mid;
-        } else {
-            left = 1 + mid;
-        }
-    }
-    left
 }
 
 #[cfg(test)]
@@ -82,11 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(maximum_strong_pair_xor(vec![1, 2, 3, 4, 5]), 7);
-        assert_eq!(maximum_strong_pair_xor(vec![10, 100]), 0);
-        assert_eq!(maximum_strong_pair_xor(vec![500, 520, 2500, 3000]), 1020);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
