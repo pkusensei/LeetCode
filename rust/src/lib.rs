acf0a1d86e8f0209ae3d6ac62ba6a8b98a9c42d5
pkusensei <ZW_08@outlook.com>
2025-04-18 19:21:14 -0400

    2949 prefix count map

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn beautiful_substrings(s: String, k: i32) -> i32 {
-    use std::collections::HashSet;
-    let vowels = HashSet::from([b'a', b'e', b'i', b'o', b'u']);
-    let (s, n) = (s.as_bytes(), s.len());
+pub fn beautiful_substrings(s: &str, k: i32) -> i32 {
+    use std::collections::HashMap;
+    const V: [u8; 5] = [b'a', b'e', b'i', b'o', b'u'];
+    let root = (1..=k).find(|&v| v * v % k == 0).unwrap_or(k);
     let mut res = 0;
-    for left in 0..n - 1 {
-        let [mut vs, mut cs] = [0, 0];
-        for right in left..n {
-            if vowels.contains(&s[right]) {
-                vs += 1
-            } else {
-                cs += 1
-            }
-            res += i32::from(right - left > 0 && vs == cs && vs * cs % k == 0);
+    let mut v = 0;
+    let mut v_minus_c = 0;
+    let mut map = HashMap::from([([0, 0], 1)]);
+    for b in s.bytes() {
+        if V.contains(&b) {
+            v = (1 + v) % root; // count**2%k=0
+            v_minus_c += 1; // v_count==c_count
+        } else {
+            v_minus_c -= 1;
         }
+        let count = map.entry([v, v_minus_c]).or_insert(0);
+        res += *count;
+        *count += 1;
     }
     res
 }
@@ -54,7 +57,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(beautiful_substrings("baeyh", 2), 2);
+        assert_eq!(beautiful_substrings("abba", 1), 3);
+        assert_eq!(beautiful_substrings("bcdf", 1), 0);
+    }
 
     #[test]
     fn test() {}
