ff2657721d3fcf1217349a4c9231517c51852715
pkusensei <ZW_08@outlook.com>
2025-04-18 15:06:01 -0400

    2945 speed up dp with binary search/partition point

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_coins(prices: &[i32]) -> i32 {
-    let n = prices.len();
-    let mut dp = vec![i32::MAX; 1 + n];
-    dp[n] = 0;
-    for left in (0..n).rev() {
-        for right in 1 + left..=(2 + 2 * left).min(n) {
-            dp[left] = dp[left].min(prices[left] + dp[right]);
-        }
+pub fn find_maximum_length(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let mut vals = vec![i64::MAX; 1 + n];
+    vals[0] = 0;
+    let prefix = nums.iter().fold(vec![0], |mut acc, &v| {
+        acc.push(i64::from(v) + acc.last().unwrap_or(&0));
+        acc
+    });
+    let mut dp = vec![0; 1 + n];
+    let mut prev_idx = vec![0; 2 + n];
+    let mut left = 0;
+    for right in 1..=n {
+        left = left.max(prev_idx[right]);
+        dp[right] = 1 + dp[left];
+        // Find first v in prefix_sum
+        // such that v-prefix[right] >= prefix[right]-prefix[left]
+        // i.e sum(right..=v)>=sum(left..=right)
+        let i = prefix.partition_point(|&v| v < 2 * prefix[right] - prefix[left]);
+        prev_idx[i] = right;
     }
-    dp[0]
+    dp[n]
 }
 
 #[cfg(test)]
@@ -48,11 +59,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimum_coins(&[3, 1, 2]), 4);
-        assert_eq!(minimum_coins(&[1, 10, 1, 1]), 2);
-        assert_eq!(minimum_coins(&[26, 18, 6, 12, 49, 7, 45, 45]), 39);
+        assert_eq!(find_maximum_length(&[5, 2, 2]), 1);
+        assert_eq!(find_maximum_length(&[1, 2, 3, 4]), 4);
+        assert_eq!(find_maximum_length(&[4, 3, 2, 6]), 3);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            find_maximum_length(&[336, 78, 256, 976, 976, 764, 370, 46]),
+            4
+        );
+    }
 }
