3cb5506bac71d64d857adac081c5e392a549013a
pkusensei <ZW_08@outlook.com>
2025-04-19 13:09:49 -0400

    2957 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,49 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_sequence(n: i32, sick: &[i32]) -> i32 {
-    const MOD: i64 = 1_000_000_007;
-    let mut fact = vec![0; 1 + n as usize];
-    fact[..2].fill(1);
-    for v in 2..=n {
-        fact[v as usize] = i64::from(v) * fact[v as usize - 1] % MOD;
-    }
-    let mut s = 0;
-    let mut len_fact = 1;
-    let mut k = 0;
-    if sick[0] > 0 {
-        let seg = i64::from(sick[0]); // left end
-        s += seg;
-        len_fact = (len_fact * fact[seg as usize]) % MOD;
-    }
-    for w in sick.windows(2) {
-        if w[1] - w[0] > 1 {
-            let seg = i64::from(w[1] - w[0] - 1);
-            s += seg;
-            len_fact = (len_fact * fact[seg as usize]) % MOD;
-            k += seg - 1;
+pub fn remove_almost_equal_characters(word: &str) -> i32 {
+    let (s, n) = (word.as_bytes(), word.len());
+    let mut res = 0;
+    let mut idx = 1;
+    while idx < n {
+        if s[idx - 1].abs_diff(s[idx]) <= 1 {
+            res += 1;
+            idx += 1;
         }
+        idx += 1;
     }
-    if let Some(&v) = sick.last() {
-        if n - v > 1 {
-            let seg = i64::from(n - v - 1); // right end
-            s += seg;
-            len_fact = (len_fact * fact[seg as usize]) % MOD;
-        }
-    }
-    let res = (fact[s as usize] * mod_pow(len_fact, MOD - 2, MOD)) % MOD * mod_pow(2, k, MOD) % MOD;
-    res as i32
-}
-
-const fn mod_pow(base: i64, exp: i64, m: i64) -> i64 {
-    if exp == 0 {
-        return 1;
-    };
-    if exp & 1 == 0 {
-        mod_pow(base * base % m, exp >> 1, m)
-    } else {
-        base * mod_pow(base * base % m, exp >> 1, m) % m
-    }
+    res
 }
 
 #[cfg(test)]
@@ -81,8 +50,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(number_of_sequence(5, &[0, 4]), 4);
-        assert_eq!(number_of_sequence(4, &[1]), 3);
+        assert_eq!(remove_almost_equal_characters("aaaaa"), 2);
+        assert_eq!(remove_almost_equal_characters("abddez"), 2);
+        assert_eq!(remove_almost_equal_characters("zyxyxyz"), 3);
     }
 
     #[test]
