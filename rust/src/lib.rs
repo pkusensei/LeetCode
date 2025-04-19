a9fd41ecd75d33ba5c9c297d52f89242f9211301
pkusensei <ZW_08@outlook.com>
2025-04-19 16:38:35 -0400

    2963 find and merge intervals

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,21 +5,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_subarrays(nums: &[i32], k: i32) -> i64 {
-    let max = nums.iter().copied().max().unwrap();
-    let mut count = 0;
-    let mut left = 0;
-    let mut res = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        count += i32::from(num == max);
-        while count >= k {
-            count -= i32::from(nums[left] == max);
-            left += 1;
-        }
-        res += right + 1 - left;
+pub fn number_of_good_partitions(nums: &[i32]) -> i32 {
+    use itertools::Itertools;
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
+    for (idx, &num) in nums.iter().enumerate() {
+        let v = map.entry(num).or_insert([idx, idx]);
+        v[1] = idx;
     }
-    let n = nums.len();
-    (n * (1 + n) / 2 - res) as i64
+    let spans = map.into_values().sorted_unstable().collect_vec();
+    let mut count = 1;
+    let mut end = spans[0][1];
+    for &[s, e] in &spans[1..] {
+        count += i64::from(s > end);
+        end = end.max(e);
+    }
+    mod_pow(2, count - 1, 1_000_000_007) as i32
+}
+
+const fn mod_pow(b: i64, e: i64, m: i64) -> i64 {
+    if e == 0 {
+        return 1;
+    }
+    if e & 1 == 0 {
+        mod_pow(b * b % m, e >> 1, m)
+    } else {
+        mod_pow(b * b % m, e >> 1, m) * b % m
+    }
 }
 
 #[cfg(test)]
@@ -52,7 +64,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_good_partitions(&[1, 2, 3, 4]), 8);
+        assert_eq!(number_of_good_partitions(&[1, 1, 1, 1]), 1);
+        assert_eq!(number_of_good_partitions(&[1, 2, 1, 3]), 2);
+    }
 
     #[test]
     fn test() {}
