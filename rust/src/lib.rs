7246d47b8ec6ff9f18348dc0d68c15ccde199bd6
pkusensei <ZW_08@outlook.com>
2025-04-18 20:07:19 -0400

    2952 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,24 +5,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn beautiful_substrings(s: &str, k: i32) -> i32 {
-    use std::collections::HashMap;
-    const V: [u8; 5] = [b'a', b'e', b'i', b'o', b'u'];
-    let root = (1..=k).find(|&v| v * v % k == 0).unwrap_or(k);
+pub fn minimum_added_coins(mut coins: Vec<i32>, target: i32) -> i32 {
+    coins.sort_unstable();
+    let mut curr = 1;
+    let mut idx = 0;
     let mut res = 0;
-    let mut v = 0;
-    let mut v_minus_c = 0;
-    let mut map = HashMap::from([([0, 0], 1)]);
-    for b in s.bytes() {
-        if V.contains(&b) {
-            v = (1 + v) % root; // count**2%k=0
-            v_minus_c += 1; // v_count==c_count
+    while curr <= target {
+        // Suppose v is reachable,
+        // Adding v makes all [v..=2v] reachable
+        if coins.get(idx).is_some_and(|&v| v <= curr) {
+            curr += coins[idx];
+            idx += 1
         } else {
-            v_minus_c -= 1;
+            res += 1;
+            curr *= 2;
         }
-        let count = map.entry([v, v_minus_c]).or_insert(0);
-        res += *count;
-        *count += 1;
     }
     res
 }
@@ -58,9 +55,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(beautiful_substrings("baeyh", 2), 2);
-        assert_eq!(beautiful_substrings("abba", 1), 3);
-        assert_eq!(beautiful_substrings("bcdf", 1), 0);
+        assert_eq!(minimum_added_coins(vec![1, 4, 10], 19), 2);
+        assert_eq!(minimum_added_coins(vec![1, 4, 10, 5, 7, 19], 19), 1);
+        assert_eq!(minimum_added_coins(vec![1, 1, 1], 20), 3);
     }
 
     #[test]
