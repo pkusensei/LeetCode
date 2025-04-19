a7d0a775f1154188b912dc7773159cdd1e7f717c
pkusensei <ZW_08@outlook.com>
2025-04-19 14:55:42 -0400

    2959 brute-force floyd warshall

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,19 +5,50 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_subarray_length(nums: &[i32], k: i32) -> i32 {
+pub fn number_of_sets(n: i32, max_distance: i32, roads: &[[i32; 3]]) -> i32 {
+    let n = n as usize;
+    let adj = roads.iter().fold(vec![vec![i32::MAX; n]; n], |mut acc, r| {
+        let [a, b] = [0, 1].map(|i| r[i] as usize);
+        acc[a][b] = acc[a][b].min(r[2]);
+        acc[b][a] = acc[b][a].min(r[2]);
+        acc
+    });
     let mut res = 0;
-    let mut map = std::collections::HashMap::new();
-    let mut left = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        *map.entry(num).or_insert(0) += 1;
-        while map[&num] > k {
-            map.entry(nums[left]).and_modify(|v| *v -= 1);
-            left += 1;
-        }
-        res = res.max(right - left + 1);
+    for mask in 0..(1 << n) {
+        res += i32::from(check(&adj, max_distance, mask));
     }
-    res as i32
+    res
+}
+
+fn check(adj: &[Vec<i32>], max_distance: i32, mask: i32) -> bool {
+    let mut mat = adj.to_vec();
+    let n = adj.len();
+    for mid in 0..n {
+        if (mask >> mid) & 1 == 1 {
+            continue;
+        }
+        for src in 0..n {
+            if (mask >> src) & 1 == 1 {
+                continue;
+            }
+            for desc in 0..n {
+                if (mask >> desc) & 1 == 1 {
+                    continue;
+                }
+                if mat[src][mid] < i32::MAX && mat[mid][desc] < i32::MAX {
+                    mat[src][desc] = mat[src][desc].min(mat[src][mid] + mat[mid][desc]);
+                }
+            }
+        }
+    }
+    for a in 0..n {
+        for b in 0..n {
+            if a != b && (mask >> a) & 1 == 0 && (mask >> b) & 1 == 0 && mat[a][b] > max_distance {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -51,9 +82,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_subarray_length(&[1, 2, 3, 1, 2, 3, 1, 2], 2), 6);
-        assert_eq!(max_subarray_length(&[1, 2, 1, 2, 1, 2, 1, 2], 1), 2);
-        assert_eq!(max_subarray_length(&[5, 5, 5, 5, 5, 5, 5], 4), 4);
+        assert_eq!(
+            number_of_sets(3, 5, &[[0, 1, 2], [1, 2, 10], [0, 2, 10]]),
+            5
+        );
+        assert_eq!(
+            number_of_sets(3, 5, &[[0, 1, 20], [0, 1, 10], [1, 2, 2], [0, 2, 2]]),
+            7
+        );
+        assert_eq!(number_of_sets(1, 10, &[]), 2);
     }
 
     #[test]
