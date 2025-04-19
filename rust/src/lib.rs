26e6a787fc02f5f0f8ad6d85443874e863ec7f14
pkusensei <ZW_08@outlook.com>
2025-04-19 15:20:20 -0400

    2961 mod_pow

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,14 +5,32 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_tested_devices(battery_percentages: Vec<i32>) -> i32 {
-    let mut res = 0;
-    for &num in &battery_percentages {
-        if num > res {
-            res += 1;
-        }
+pub fn get_good_indices(variables: Vec<Vec<i32>>, target: i32) -> Vec<i32> {
+    variables
+        .iter()
+        .enumerate()
+        .filter_map(|(i, v)| {
+            let [a, b, c, m] = v[..] else { unreachable!() };
+            let x = mod_pow(a.into(), b.into(), 10);
+            let y = mod_pow(x.into(), c.into(), m.into());
+            if y == i64::from(target) {
+                Some(i as i32)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+const fn mod_pow(base: i64, exp: i64, m: i64) -> i64 {
+    if exp == 0 {
+        return 1;
     }
-    res
+    if exp & 1 == 0 {
+        mod_pow(base * base % m, exp >> 1, m)
+    } else {
+        base * mod_pow(base * base % m, exp >> 1, m) % m
+    }
 }
 
 #[cfg(test)]
