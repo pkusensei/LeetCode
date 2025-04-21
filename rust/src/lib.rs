c0d42b2e88f81de98ff8961d450c5ab6342f3678
pkusensei <ZW_08@outlook.com>
2025-04-20 20:56:15 -0400

    2977 trie and floyd warshall are too slow? and imiting search space to valid substr lengths

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,41 +2,87 @@ mod dsu;
 mod helper;
 mod trie;
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap, HashSet},
+};
+
 #[allow(unused_imports)]
 use helper::*;
+use itertools::{chain, izip};
 
 pub fn minimum_cost(
-    source: String,
-    target: String,
-    original: Vec<char>,
-    changed: Vec<char>,
-    cost: Vec<i32>,
+    source: &str,
+    target: &str,
+    original: &[&str],
+    changed: &[&str],
+    cost: &[i32],
 ) -> i64 {
-    use itertools::izip;
-    let mut mat = [[i64::MAX; 26]; 26];
-    for (a, b, c) in izip!(original, changed, cost) {
-        let [i1, i2] = [a, b].map(|v| usize::from(v as u8 - b'a'));
-        mat[i1][i2] = mat[i1][i2].min(i64::from(c));
+    let mut id_map = HashMap::new();
+    let mut id = 0;
+    for s in chain!(original.iter(), changed.iter()) {
+        let v = id_map.entry(s.as_bytes()).or_insert(id);
+        if *v == id {
+            id += 1;
+        }
     }
-    for mid in 0..26 {
-        for a in 0..26 {
-            for b in 0..26 {
-                if mat[a][mid] < i64::MAX && mat[mid][b] < i64::MAX {
-                    mat[a][b] = mat[a][b].min(mat[a][mid] + mat[mid][b]);
+    let cn = id_map.len();
+        let mut graph = vec![vec![]; cn];
+        let mut lengths = HashSet::new();
+        for (a, b, &c) in izip!(original.iter(), changed.iter(), cost.iter()) {
+            let i1 = id_map[&a.as_bytes()];
+            let i2 = id_map[&b.as_bytes()];
+            graph[i1].push((i2, i64::from(c)));
+            lengths.insert(a.len());
+        }
+
+        let sn = source.len();
+        let mut dp = vec![i64::MAX; 1 + sn];
+        dp[0] = 0;
+        let mut seen = HashMap::new();
+        for len in 1..=sn {
+            if source.as_bytes()[len - 1] == target.as_bytes()[len - 1] {
+                dp[len] = dp[len].min(dp[len - 1])
+            }
+            for &val in &lengths {
+                if len < val {
+                    continue;
+                }
+                let left = len - val;
+                let src = source[left..len].as_bytes();
+                let tgt = target[left..len].as_bytes();
+                if let Some((&i1, &i2)) = id_map.get(&src).zip(id_map.get(&tgt)) {
+                    let key = [i1, i2];
+                    let cost = *seen.entry(key).or_insert_with(|| dijkstra(&graph, i1, i2));
+                    if cost < i64::MAX {
+                        dp[len] = dp[len].min(dp[left].saturating_add(cost));
+                    }
                 }
             }
         }
-    }
-    let mut res = 0;
-    for (a, b) in izip!(source.bytes(), target.bytes()).filter(|(a, b)| a != b) {
-        let [i1, i2] = [a, b].map(|v| usize::from(v - b'a'));
-        if mat[i1][i2] < i64::MAX {
-            res += mat[i1][i2]
-        } else {
-            return -1;
+    if dp[sn] == i64::MAX { -1 } else { dp[sn] }
+}
+
+fn dijkstra(graph: &[Vec<(usize, i64)>], start: usize, goal: usize) -> i64 {
+    let mut dists = vec![i64::MAX; graph.len()];
+    let mut heap = BinaryHeap::from([(Reverse(0), start)]);
+    dists[start] = 0;
+    while let Some((Reverse(dist), node)) = heap.pop() {
+        if node == goal {
+            return dist;
+        }
+        if dist > dists[node] {
+            continue;
+        }
+        for &(next, d) in &graph[node] {
+            let nc = d + dist;
+            if nc < dists[next] {
+                dists[next] = nc;
+                heap.push((Reverse(nc), next));
+            }
         }
     }
-    res
+    i64::MAX
 }
 
 #[cfg(test)]
@@ -69,7 +115,38 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            minimum_cost(
+                "abcd",
+                "acbe",
+                &["a", "b", "c", "c", "e", "d"],
+                &["b", "c", "b", "e", "b", "e"],
+                &[2, 5, 5, 1, 2, 20]
+            ),
+            28
+        );
+        assert_eq!(
+            minimum_cost(
+                "abcdefgh",
+                "acdeeghh",
+                &["bcd", "fgh", "thh"],
+                &["cde", "thh", "ghh"],
+                &[1, 3, 5]
+            ),
+            9
+        );
+        assert_eq!(
+            minimum_cost(
+                "abcdefgh",
+                "addddddd",
+                &["bcd", "defgh"],
+                &["ddd", "ddddd"],
+                &[100, 1578]
+            ),
+            -1
+        );
+    }
 
     #[test]
     fn test() {}
