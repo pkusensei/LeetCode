b2d734db868eed1d2493a7e82ac9a37e72e68978
Zx Wang <ZW_08@outlook.com>
2025-04-22 15:15:34 -0400

    3008 kmp and two ptrs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,39 +5,65 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_maximum_number(k: i64, x: i32) -> i64 {
-    let mut left = 1;
-    let mut right = 10i64.pow(15);
-    while left < right {
-        let mid = left + (right + 1 - left) / 2;
-        if sum(x as u32, mid) > k {
-            right = mid - 1;
-        } else {
-            left = mid;
+pub fn beautiful_indices(s: &str, a: &str, b: &str, k: i32) -> Vec<i32> {
+    let lps_a = kmp(a.as_bytes());
+    let lps_b = kmp(b.as_bytes());
+    let match_a = search(s.as_bytes(), a.as_bytes(), &lps_a);
+    let match_b = search(s.as_bytes(), b.as_bytes(), &lps_b);
+    let mut res = vec![];
+    let mut ia = 0;
+    let mut ib = 0;
+    while let Some(&va) = match_a.get(ia) {
+        while let Some(&vb) = match_b.get(ib) {
+            if va.abs_diff(vb) <= k as usize {
+                res.push(va as i32);
+                break;
+            } else if match_b
+                .get(1 + ib)
+                .is_some_and(|&next_val| next_val.abs_diff(va) < vb.abs_diff(va))
+            {
+                ib += 1;
+            } else {
+                break;
+            }
+        }
+        ia += 1;
+    }
+    res
+}
+
+fn kmp(s: &[u8]) -> Vec<usize> {
+    let n = s.len();
+    let mut lps = vec![0; n];
+    let mut j = 0;
+    for i in 1..n {
+        while j > 0 && s[i] != s[j] {
+            j = lps[j - 1];
+        }
+        if s[i] == s[j] {
+            j += 1;
+        }
+        lps[i] = j;
+    }
+    lps
+}
+
+fn search(s: &[u8], pat: &[u8], lps: &[usize]) -> Vec<usize> {
+    let mut res = vec![];
+    let mut j = 0;
+    for (i, &b) in s.iter().enumerate() {
+        while j > 0 && b != pat[j] {
+            j = lps[j - 1];
+        }
+        if b == pat[j] {
+            j += 1;
+        }
+        if j == pat.len() {
+            res.push(1 + i - j);
+            j = lps[j - 1];
         }
     }
-    left
-}
-
-fn sum(x: u32, mid: i64) -> i64 {
-    let mut total = 0;
-    let mut bit = x - 1;
-    while (1 << bit) <= mid {
-        total += count(mid, bit);
-        bit += x;
-    }
-    total
-}
-
-fn count(n: i64, bit: u32) -> i64 {
-    let group_size = 1i64 << (bit + 1);
-    let ones_in_full_group = 1i64 << bit;
-    let full_groups = n / group_size;
-    let remaining = n % group_size;
-    let extra = (remaining - ones_in_full_group + 1)
-        .max(0)
-        .min(ones_in_full_group);
-    full_groups * ones_in_full_group + extra
+    res
 }
 
 #[cfg(test)]
@@ -71,8 +97,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(find_maximum_number(9, 1), 6);
-        assert_eq!(find_maximum_number(7, 2), 9);
+        assert_eq!(
+            beautiful_indices("isawsquirrelnearmysquirrelhouseohmy", "my", "squirrel", 15),
+            [16, 33]
+        );
+        assert_eq!(beautiful_indices("abcd", "a", "a", 4), [0]);
     }
 
     #[test]
