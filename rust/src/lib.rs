27a96270bc0624254cba7517a0ed57da2fbe73c9
pkusensei <ZW_08@outlook.com>
2025-04-23 15:20:30 -0400

    3020 1**n==1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,37 +5,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_of_pairs(n: i32, x: i32, y: i32) -> Vec<i64> {
-    let [x, y] = [x.min(y) as usize, x.max(y) as usize];
-    let n = n as usize;
-    let mut diff = vec![0i64; n];
-    // This i is the house
-    for i in 1..=n {
-        // Every i can go left or right
-        diff[0] += 2;
-        // From here on, i is distance k
-        // To go left to 1, take either
-        // 1) direct route i-1
-        // 2) shortcut i->y->x->1
-        //    2.1) Since x<=y, i->x->y->1 is always an longer path
-        // Any path longer is invalid; discount it from diff array
-        diff[(i - 1).min(i.abs_diff(y) + x)] -= 1;
-        // Similarly, go right to n
-        diff[(n - i).min(i.abs_diff(x) + n + 1 - y)] -= 1;
-        // Possible shortcuts
-        diff[i.abs_diff(x).min(i.abs_diff(y) + 1)] += 1;
-        diff[(i.abs_diff(x) + 1).min(i.abs_diff(y))] += 1;
-        // min dist to x or y
-        // it's 0 if x<=i<=y
-        let min_dist = x.saturating_sub(i) + i.saturating_sub(y);
-        // Reduce 2 counts when crossing x.midpoint(y)
-        diff[min_dist + (y - x + 0) / 2] -= 1;
-        diff[min_dist + (y - x + 1) / 2] -= 1;
+pub fn maximum_length(nums: &[i32]) -> i32 {
+    use itertools::Itertools;
+    let freq = nums.iter().copied().counts();
+    let mut res = freq
+        .get(&1)
+        .map(|&v| if v & 1 == 1 { v } else { v - 1 })
+        .unwrap_or(1);
+    for (&k, &v) in freq.iter() {
+        if k == 1 || v == 1 {
+            continue;
+        }
+        let mut num = k;
+        let mut curr = 0;
+        while let Some(&v) = freq.get(&num) {
+            if v > 1 {
+                curr += 2;
+                let Some(next) = num.checked_mul(num) else {
+                    break;
+                };
+                num = next;
+            } else {
+                curr += 1;
+                break;
+            }
+        }
+        if curr & 1 == 0 {
+            curr -= 1;
+        }
+        res = res.max(curr)
     }
-    for i in 1..n {
-        diff[i] += diff[i - 1]
-    }
-    diff
+    res as i32
 }
 
 #[cfg(test)]
@@ -69,9 +69,8 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(count_of_pairs(3, 1, 3), [6, 0, 0]);
-        assert_eq!(count_of_pairs(5, 2, 4), [10, 8, 2, 0, 0]);
-        assert_eq!(count_of_pairs(4, 1, 1), [6, 4, 2, 0]);
+        assert_eq!(maximum_length(&[5, 4, 1, 2, 2]), 3);
+        assert_eq!(maximum_length(&[1, 3, 2, 4]), 1);
     }
 
     #[test]
