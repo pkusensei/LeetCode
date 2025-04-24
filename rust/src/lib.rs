702f806b512506ea65416ecd4121947568e18d83
pkusensei <ZW_08@outlook.com>
2025-04-23 20:19:07 -0400

    3025 sort and keep record of max_y

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,23 +5,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_or_after_operations(nums: Vec<i32>, k: i32) -> i32 {
+pub fn number_of_pairs(mut points: Vec<[i32; 2]>) -> i32 {
+    points.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then(b[1].cmp(&a[1])));
     let mut res = 0;
-    let mut mask = 0;
-    for bit in (0..31).rev() {
-        mask |= 1 << bit;
-        let mut count = k;
-        let mut and = mask;
-        for &num in &nums {
-            and &= num;
-            if and | res == res {
-                and = mask;
-            } else {
-                count -= 1;
+    for (i, p1) in points.iter().enumerate() {
+        let y1 = p1[1];
+        let mut max_y = i32::MIN;
+        for p2 in points.iter().skip(1 + i) {
+            let y2 = p2[1];
+            if y1 >= y2 && y2 > max_y {
+                res += 1;
+                max_y = y2;
             }
-        }
-        if count < 0 {
-            res |= 1 << bit;
         }
     }
     res
@@ -58,16 +53,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_or_after_operations(vec![3, 5, 3, 2, 7], 2), 3);
-        assert_eq!(min_or_after_operations(vec![7, 3, 15, 14, 2, 8], 4), 2);
-        assert_eq!(
-            min_or_after_operations(vec![10, 7, 10, 3, 9, 14, 9, 4], 1),
-            15
-        );
+        assert_eq!(number_of_pairs(vec![[1, 1], [2, 2], [3, 3]]), 0);
+        assert_eq!(number_of_pairs(vec![[6, 2], [4, 4], [2, 6]]), 2);
+        assert_eq!(number_of_pairs(vec![[3, 1], [1, 3], [1, 1]]), 2);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(min_or_after_operations(vec![39, 62, 35, 11, 28, 32], 3), 38);
-    }
+    fn test() {}
 }
