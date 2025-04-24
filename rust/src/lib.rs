07f21951780e21fd66ae3672ee14ec7226b36090
Zx Wang <ZW_08@outlook.com>
2025-04-24 10:15:03 -0400

    3030 brute-force all 3x3's

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,15 +5,49 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_time_to_initial_state(word: &str, k: i32) -> i32 {
-    let (s, n) = (word.as_bytes(), word.len());
-    let k = k as usize;
-    for idx in (k..n).step_by(k) {
-        if s.starts_with(&s[idx..]) {
-            return (idx / k) as i32;
+pub fn result_grid(image: &[&[i32]], threshold: i32) -> Vec<Vec<i32>> {
+    let [rows, cols] = get_dimensions(image);
+    let [mut sum, mut freq] = [0, 1].map(|_| vec![vec![0; cols]; rows]);
+    for r in 0..=rows - 3 {
+        for c in 0..=cols - 3 {
+            let Some(v) = sum_grid(image, r, c, threshold as u32) else {
+                continue;
+            };
+            for row in r..r + 3 {
+                for col in c..c + 3 {
+                    sum[row][col] += v / 9;
+                    freq[row][col] += 1;
+                }
+            }
         }
     }
-    n.div_ceil(k) as i32
+    let mut res = vec![vec![0; cols]; rows];
+    for (r, row) in res.iter_mut().enumerate() {
+        for (c, v) in row.iter_mut().enumerate() {
+            if freq[r][c] == 0 {
+                *v = image[r][c];
+            } else {
+                *v = sum[r][c] / freq[r][c];
+            }
+        }
+    }
+    res
+}
+
+fn sum_grid(image: &[&[i32]], up: usize, left: usize, thr: u32) -> Option<i32> {
+    let mut sum = 0;
+    for r in up..up + 3 {
+        for c in left..left + 3 {
+            if r > up && image[r - 1][c].abs_diff(image[r][c]) > thr {
+                return None;
+            }
+            if c > left && image[r][c - 1].abs_diff(image[r][c]) > thr {
+                return None;
+            }
+            sum += image[r][c];
+        }
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -47,9 +81,21 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimum_time_to_initial_state("abacaba", 3), 2);
-        assert_eq!(minimum_time_to_initial_state("abacaba", 4), 1);
-        assert_eq!(minimum_time_to_initial_state("abcbabcd", 2), 4);
+        assert_eq!(
+            result_grid(&[&[5, 6, 7, 10], &[8, 9, 10, 10], &[11, 12, 13, 10]], 3),
+            [[9, 9, 9, 9], [9, 9, 9, 9], [9, 9, 9, 9]]
+        );
+        assert_eq!(
+            result_grid(
+                &[&[10, 20, 30], &[15, 25, 35], &[20, 30, 40], &[25, 35, 45]],
+                12
+            ),
+            [[25, 25, 25], [27, 27, 27], [27, 27, 27], [30, 30, 30]]
+        );
+        assert_eq!(
+            result_grid(&[&[5, 6, 7], &[8, 9, 10], &[11, 12, 13]], 1),
+            [[5, 6, 7], [8, 9, 10], [11, 12, 13]]
+        );
     }
 
     #[test]
