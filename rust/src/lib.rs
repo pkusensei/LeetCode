61598fd9268bfe362579c4db4c447216fe95745c
pkusensei <ZW_08@outlook.com>
2025-04-25 19:58:58 -0400

    3071

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,25 +5,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_submatrices(grid: Vec<Vec<i32>>, k: i32) -> i32 {
-    let mut prefix = vec![];
-    for row in grid.iter() {
-        let mut curr = row.iter().fold(vec![], |mut acc, &v| {
-            acc.push(i64::from(v) + acc.last().unwrap_or(&0));
-            acc
-        });
-        if let Some(up) = prefix.last() {
-            for (v, old) in curr.iter_mut().zip(up) {
-                *v += old
+pub fn minimum_operations_to_write_y(grid: &[&[i32]]) -> i32 {
+    let n = grid.len();
+    let [mut fin, mut fout] = [vec![0; 3], vec![0; 3]];
+    let [mut cin, mut cout] = [0, 0];
+    for (r, row) in grid.iter().enumerate() {
+        for (c, &v) in row.iter().enumerate() {
+            if in_y(n, r, c) {
+                fin[v as usize] += 1;
+                cin += 1;
+            } else {
+                fout[v as usize] += 1;
+                cout += 1;
             }
         }
-        prefix.push(curr);
     }
-    prefix
-        .iter()
-        .flatten()
-        .filter(|&&v| v <= i64::from(k))
-        .count() as i32
+    let mut res = (n as i32).pow(2);
+    for y in 0..3 {
+        let mut curr = cin - fin[y];
+        let mut max = 0;
+        for out in (0..3).filter(|&v| v != y) {
+            max = max.max(fout[out]);
+        }
+        curr += cout - max;
+        res = res.min(curr);
+    }
+    res
+}
+
+const fn in_y(n: usize, r: usize, c: usize) -> bool {
+    if r <= n / 2 {
+        r == c || r + c + 1 == n
+    } else {
+        c == n / 2
+    }
 }
 
 #[cfg(test)]
@@ -56,7 +71,12 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            minimum_operations_to_write_y(&[&[1, 2, 2], &[1, 1, 0], &[0, 1, 0]]),
+            3
+        );
+    }
 
     #[test]
     fn test() {}
