7fa2347ea328ad31d9088f09ef02e31caf9327df
pkusensei <ZW_08@outlook.com>
2025-04-26 18:46:37 -0400

    3081 heap and sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,40 +6,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn unmarked_sum_array(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i64> {
+pub fn minimize_string_value(s: String) -> String {
     use std::{cmp::Reverse, collections::BinaryHeap};
-    let mut sum: i64 = nums.iter().map(|&v| i64::from(v)).sum();
-    let mut seen = vec![false; nums.len()];
-    let mut heap: BinaryHeap<_> = nums
-        .iter()
-        .enumerate()
-        .map(|(i, &v)| Reverse((v, i)))
-        .collect();
-    let mut res = vec![];
-    for q in queries {
-        let [i, mut k] = q[..] else {
-            unreachable!();
-        };
-        if !seen[i as usize] {
-            sum -= i64::from(nums[i as usize]);
-            seen[i as usize] = true
+    let mut freq = [0; 26];
+    let mut count = 0;
+    for b in s.bytes() {
+        if b == b'?' {
+            count += 1
+        } else {
+            freq[usize::from(b - b'a')] += 1;
         }
-        if k > 0 {
-            while let Some(Reverse((v, i))) = heap.pop() {
-                if seen[i] {
-                    continue;
-                }
-                seen[i] = true;
-                sum -= i64::from(v);
-                k -= 1;
-                if k <= 0 {
-                    break;
-                }
-            }
-        }
-        res.push(sum);
     }
-    res
+    let mut heap: BinaryHeap<_> = freq
+        .into_iter()
+        .enumerate()
+        .map(|(i, f)| Reverse((f, i as u8 + b'a')))
+        .collect();
+    let mut chs = Vec::with_capacity(count);
+    for _ in 0..count {
+        let Reverse((f, b)) = heap.pop().unwrap();
+        chs.push(b);
+        heap.push(Reverse((1 + f, b)));
+    }
+    chs.sort_unstable();
+    chs.reverse();
+    let mut s = s.into_bytes();
+    for v in s.iter_mut() {
+        if *v != b'?' {
+            continue;
+        }
+        *v = chs.pop().unwrap();
+    }
+    String::from_utf8(s).unwrap()
 }
 
 #[cfg(test)]
@@ -75,5 +73,10 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            minimize_string_value("abcdefghijklmnopqrstuvwxy??".into()),
+            "abcdefghijklmnopqrstuvwxyaz"
+        );
+    }
 }
