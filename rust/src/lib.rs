c1f63e6fe36a682ff26087280c1de9879279298a
pkusensei <ZW_08@outlook.com>
2025-04-27 08:16:47 -0400

    3083

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,53 +6,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_of_power(nums: &[i32], k: i32) -> i32 {
-    let n = nums.len();
-    dfs(nums, k, 0, &mut vec![vec![-1; 1 + k as usize]; n]) as i32
-}
-
-const M: i64 = 1_000_000_007;
-
-fn dfs(nums: &[i32], k: i32, idx: usize, memo: &mut [Vec<i64>]) -> i64 {
-    if k == 0 {
-        return mod_pow(2, (nums.len() - idx) as _, M);
+pub fn is_substring_present(s: String) -> bool {
+    let n = s.len();
+    if n < 2 {
+        return false;
     }
-    if k < 0 || idx >= nums.len() {
-        return 0;
-    }
-    if memo[idx][k as usize] > -1 {
-        return memo[idx][k as usize];
-    }
-    let skip = dfs(nums, k, 1 + idx, memo);
-    let take = dfs(nums, k - nums[idx], 1 + idx, memo);
-    memo[idx][k as usize] = (take + 2 * skip) % M;
-    memo[idx][k as usize]
-}
-
-const fn mod_pow(base: i64, exp: i64, m: i64) -> i64 {
-    if exp == 0 {
-        return 1;
-    }
-    if exp & 1 == 0 {
-        mod_pow(base * base % m, exp >> 1, m)
-    } else {
-        mod_pow(base * base % m, exp >> 1, m) * base % m
-    }
-}
-
-pub fn bottom_up(nums: &[i32], k: i32) -> i32 {
-    let mut dp = vec![0; 1 + k as usize];
-    dp[0] = 1;
-    for &num in nums {
-        for i in (0..=k).rev() {
-            if i < num {
-                dp[i as usize] = 2 * dp[i as usize] % M;
-            } else {
-                dp[i as usize] = (2 * dp[i as usize] + dp[(i - num) as usize]) % M;
+    let s = s.as_bytes();
+    for i1 in 0..n - 1 {
+        for i2 in (1 + i1..n).rev() {
+            if s[i1] == s[i2] && s[1 + i1] == s[i2 - 1] {
+                return true;
             }
         }
     }
-    dp[k as usize] as i32
+    false
 }
 
 #[cfg(test)]
@@ -85,15 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(sum_of_power(&[1, 2, 3], 3), 6);
-        assert_eq!(sum_of_power(&[2, 3, 3], 5), 4);
-        assert_eq!(sum_of_power(&[1, 2, 3], 7), 0);
-
-        assert_eq!(bottom_up(&[1, 2, 3], 3), 6);
-        assert_eq!(bottom_up(&[2, 3, 3], 5), 4);
-        assert_eq!(bottom_up(&[1, 2, 3], 7), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
