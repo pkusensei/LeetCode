6ecef9e443c7bb5dd74e3bb57abbe12fb0cad8c9
pkusensei <ZW_08@outlook.com>
2025-04-27 07:58:55 -0400

    3082 dfs/memo; dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,38 +6,53 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimize_string_value(s: String) -> String {
-    use std::{cmp::Reverse, collections::BinaryHeap};
-    let mut freq = [0; 26];
-    let mut count = 0;
-    for b in s.bytes() {
-        if b == b'?' {
-            count += 1
-        } else {
-            freq[usize::from(b - b'a')] += 1;
+pub fn sum_of_power(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    dfs(nums, k, 0, &mut vec![vec![-1; 1 + k as usize]; n]) as i32
+}
+
+const M: i64 = 1_000_000_007;
+
+fn dfs(nums: &[i32], k: i32, idx: usize, memo: &mut [Vec<i64>]) -> i64 {
+    if k == 0 {
+        return mod_pow(2, (nums.len() - idx) as _, M);
+    }
+    if k < 0 || idx >= nums.len() {
+        return 0;
+    }
+    if memo[idx][k as usize] > -1 {
+        return memo[idx][k as usize];
+    }
+    let skip = dfs(nums, k, 1 + idx, memo);
+    let take = dfs(nums, k - nums[idx], 1 + idx, memo);
+    memo[idx][k as usize] = (take + 2 * skip) % M;
+    memo[idx][k as usize]
+}
+
+const fn mod_pow(base: i64, exp: i64, m: i64) -> i64 {
+    if exp == 0 {
+        return 1;
+    }
+    if exp & 1 == 0 {
+        mod_pow(base * base % m, exp >> 1, m)
+    } else {
+        mod_pow(base * base % m, exp >> 1, m) * base % m
+    }
+}
+
+pub fn bottom_up(nums: &[i32], k: i32) -> i32 {
+    let mut dp = vec![0; 1 + k as usize];
+    dp[0] = 1;
+    for &num in nums {
+        for i in (0..=k).rev() {
+            if i < num {
+                dp[i as usize] = 2 * dp[i as usize] % M;
+            } else {
+                dp[i as usize] = (2 * dp[i as usize] + dp[(i - num) as usize]) % M;
+            }
         }
     }
-    let mut heap: BinaryHeap<_> = freq
-        .into_iter()
-        .enumerate()
-        .map(|(i, f)| Reverse((f, i as u8 + b'a')))
-        .collect();
-    let mut chs = Vec::with_capacity(count);
-    for _ in 0..count {
-        let Reverse((f, b)) = heap.pop().unwrap();
-        chs.push(b);
-        heap.push(Reverse((1 + f, b)));
-    }
-    chs.sort_unstable();
-    chs.reverse();
-    let mut s = s.into_bytes();
-    for v in s.iter_mut() {
-        if *v != b'?' {
-            continue;
-        }
-        *v = chs.pop().unwrap();
-    }
-    String::from_utf8(s).unwrap()
+    dp[k as usize] as i32
 }
 
 #[cfg(test)]
@@ -70,13 +85,16 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(sum_of_power(&[1, 2, 3], 3), 6);
+        assert_eq!(sum_of_power(&[2, 3, 3], 5), 4);
+        assert_eq!(sum_of_power(&[1, 2, 3], 7), 0);
+
+        assert_eq!(bottom_up(&[1, 2, 3], 3), 6);
+        assert_eq!(bottom_up(&[2, 3, 3], 5), 4);
+        assert_eq!(bottom_up(&[1, 2, 3], 7), 0);
+    }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            minimize_string_value("abcdefghijklmnopqrstuvwxy??".into()),
-            "abcdefghijklmnopqrstuvwxyaz"
-        );
-    }
+    fn test() {}
 }
