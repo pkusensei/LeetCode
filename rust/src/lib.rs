f0df32ffcde4e9d507940db925c563426741dc0c
pkusensei <ZW_08@outlook.com>
2025-04-28 17:04:23 -0400

    3112 dijkstra with a twist

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,21 +3,37 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+};
+
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
 
-pub fn min_rectangles_to_cover_points(points: Vec<Vec<i32>>, w: i32) -> i32 {
-    if points.len() < 2 {
-        return 1;
-    }
-    let nums = points.iter().map(|p| p[0]).sorted_unstable().collect_vec();
-    let mut prev = nums[0];
-    let mut res = 1;
-    for &num in &nums[1..] {
-        if num - prev > w {
-            prev = num;
-            res += 1;
+pub fn minimum_time(n: i32, edges: Vec<Vec<i32>>, disappear: Vec<i32>) -> Vec<i32> {
+    let n = n as usize;
+    let adj = edges.iter().fold(vec![HashMap::new(); n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        let v = acc[a].entry(b).or_insert(e[2]);
+        *v = (*v).min(e[2]);
+        let v = acc[b].entry(a).or_insert(e[2]);
+        *v = (*v).min(e[2]);
+        acc
+    });
+    let mut res = vec![-1; n];
+    res[0] = 0;
+    let mut heap = BinaryHeap::from([(Reverse(0), 0)]);
+    while let Some((Reverse(cost), node)) = heap.pop() {
+        if cost > res[node] {
+            continue;
+        }
+        for (&next, &c) in adj[node].iter() {
+            let nc = cost + c;
+            if nc < disappear[next] && (res[next] == -1 || res[next] > nc) {
+                res[next] = nc;
+                heap.push((Reverse(nc), next));
+            }
         }
     }
     res
