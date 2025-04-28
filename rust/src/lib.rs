28aa7bdb23c63259443810fb3784b749ff78b09c
pkusensei <ZW_08@outlook.com>
2025-04-28 15:19:51 -0400

    3107

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,27 +6,35 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_smallest_string(s: String, mut k: i32) -> String {
-        let mut s = s.into_bytes();
-        for v in s.iter_mut() {
-            for b in b'a'..=b'z' {
-                let d = dist(*v, b);
-                if d <= k {
-                    k -= d;
-                    *v = b;
+pub fn min_operations_to_make_median_k(mut nums: Vec<i32>, k: i32) -> i64 {
+    let n = nums.len();
+    nums.sort_unstable();
+    let med = nums[n / 2];
+    match med.cmp(&k) {
+        std::cmp::Ordering::Less => {
+            let mut res = 0;
+            for &num in &nums[n / 2..] {
+                if num < k {
+                    res += i64::from(k - num);
+                } else {
                     break;
                 }
             }
-            if k == 0 {
-                break;
-            }
+            res
         }
-        String::from_utf8(s).unwrap()
-}
-
-fn dist(b1: u8, b2: u8) -> i32 {
-    let d = i32::from(b1.abs_diff(b2));
-    d.min(26 - d)
+        std::cmp::Ordering::Equal => 0,
+        std::cmp::Ordering::Greater => {
+            let mut res = 0;
+            for &num in nums[..=n / 2].iter().rev() {
+                if num > k {
+                    res += i64::from(num - k);
+                } else {
+                    break;
+                }
+            }
+            res
+        }
+    }
 }
 
 #[cfg(test)]
