cdd63d132f2f8aacc1cb2723b6fc9b9531501af3
pkusensei <ZW_08@outlook.com>
2025-04-28 11:28:28 -0400

    3098 dfs/memo on min diff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,37 +3,46 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
-    let mut freq = [0; 32];
-    let mut res = 200_001;
-    let mut left = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        for (bit, v) in freq.iter_mut().enumerate() {
-            if num & (1 << bit) > 0 {
-                *v += 1;
-            }
-        }
-        while left <= right
-            && k <= freq.iter().enumerate().fold(
-                0,
-                |acc, (bit, &f)| {
-                    if f > 0 { acc | (1 << bit) } else { acc }
-                },
-            )
-        {
-            res = res.min(1 + right - left);
-            for (bit, v) in freq.iter_mut().enumerate() {
-                if nums[left] & (1 << bit) > 0 {
-                    *v -= 1;
-                }
-            }
-            left += 1;
-        }
+pub fn sum_of_powers(mut nums: Vec<i32>, k: i32) -> i32 {
+    let n = nums.len();
+    nums.sort_unstable();
+    dfs(&nums, k as usize, 0, n, i32::MAX, &mut HashMap::new())
+}
+
+fn dfs(
+    nums: &[i32],
+    k: usize,
+    idx: usize,
+    prev: usize,
+    min_d: i32,
+    memo: &mut HashMap<(usize, usize, usize, i32), i32>,
+) -> i32 {
+    let n = nums.len();
+    if k == 0 {
+        return min_d;
     }
-    if res < 200_001 { res as i32 } else { -1 }
+    if n - idx < k {
+        return 0;
+    }
+    let key = (k, idx, prev, min_d);
+    if let Some(&v) = memo.get(&key) {
+        return v;
+    }
+    let curr = if prev == n {
+        min_d
+    } else {
+        min_d.min(nums[idx] - nums[prev])
+    };
+    let skip = dfs(nums, k, 1 + idx, prev, min_d, memo);
+    let take = dfs(nums, k - 1, 1 + idx, idx, curr, memo);
+    let res = (skip + take) % 1_000_000_007;
+    memo.insert(key, res);
+    res
 }
 
 #[cfg(test)]
@@ -66,7 +75,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(sum_of_powers(vec![1, 2, 3, 4], 3), 4);
+        assert_eq!(sum_of_powers(vec![2, 2], 2), 0);
+        assert_eq!(sum_of_powers(vec![4, 3, -1], 2), 10);
+    }
 
     #[test]
     fn test() {}
