15fb7d94a0d7e0e93940b9d70af1bc0c9dc88043
pkusensei <ZW_08@outlook.com>
2025-04-28 14:49:33 -0400

    3106

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,45 +6,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_distance(points: &[[i32; 2]]) -> i32 {
-    use std::collections::BTreeMap;
-    let mut coords = vec![];
-    let mut a_map = BTreeMap::new();
-    let mut b_map = BTreeMap::new();
-    for [a, b] in points.iter().map(|p| {
-        let [x, y] = p[..] else { unreachable!() };
-        [x - y, x + y]
-    }) {
-        coords.push([a, b]);
-        *a_map.entry(a).or_insert(0) += 1;
-        *b_map.entry(b).or_insert(0) += 1;
-    }
-    let mut res = i32::MAX;
-    let dec = |v: &mut i32| *v -= 1;
-    let inc = |v: &mut i32| *v += 1;
-    for [a, b] in coords {
-        a_map.entry(a).and_modify(dec);
-        b_map.entry(b).and_modify(dec);
-        let [a_min, b_min] = [&a_map, &b_map].map(|m| {
-            m.iter()
-                .skip_while(|(_, v)| **v == 0)
-                .next()
-                .map(|(k, _)| *k)
-                .unwrap_or(0)
-        });
-        let [a_max, b_max] = [&a_map, &b_map].map(|m| {
-            m.iter()
-                .rev()
-                .skip_while(|(_, v)| **v == 0)
-                .next()
-                .map(|(k, _)| *k)
-                .unwrap_or(0)
-        });
-        res = res.min((a_max - a_min).max(b_max - b_min));
-        a_map.entry(a).and_modify(inc);
-        b_map.entry(b).and_modify(inc);
-    }
-    res
+pub fn get_smallest_string(s: String, mut k: i32) -> String {
+        let mut s = s.into_bytes();
+        for v in s.iter_mut() {
+            for b in b'a'..=b'z' {
+                let d = dist(*v, b);
+                if d <= k {
+                    k -= d;
+                    *v = b;
+                    break;
+                }
+            }
+            if k == 0 {
+                break;
+            }
+        }
+        String::from_utf8(s).unwrap()
+}
+
+fn dist(b1: u8, b2: u8) -> i32 {
+    let d = i32::from(b1.abs_diff(b2));
+    d.min(26 - d)
 }
 
 #[cfg(test)]
@@ -77,10 +59,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(minimum_distance(&[[3, 10], [5, 15], [10, 2], [4, 4]]), 12);
-        assert_eq!(minimum_distance(&[[1, 1], [1, 1], [1, 1]]), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
