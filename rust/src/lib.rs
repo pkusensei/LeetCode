0b102ed8b3e8bf3d4300f5c9af95fb79bd381590
pkusensei <ZW_08@outlook.com>
2025-04-27 22:02:04 -0400

    3093 trie

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,40 +6,58 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn most_frequent_i_ds(nums: &[i32], freq: &[i32]) -> Vec<i64> {
-    let max_val = *nums.iter().max().unwrap();
-    let mut tree = SegmentTree::new(1 + max_val as usize);
+pub fn string_indices(words_container: Vec<String>, words_query: Vec<String>) -> Vec<i32> {
     let mut res = vec![];
-    for (&num, &f) in nums.iter().zip(freq.iter()) {
-        tree.update(0, 0, max_val as usize, num as usize, f.into());
-        res.push(tree.tree[0]);
+    for q in words_query.iter() {
+        let mut trie = Trie::default();
+        trie.insert(q.bytes().rev());
+        let mut maxi = 0;
+        let mut max_suf = 0;
+        let mut len = usize::MAX;
+        for (i, s) in words_container.iter().enumerate() {
+            let temp = trie.find(s.bytes().rev());
+            if temp > max_suf {
+                max_suf = temp;
+                len = s.len();
+                maxi = i;
+            } else if temp == max_suf && s.len() < len {
+                len = s.len();
+                maxi = i;
+            }
+        }
+        res.push(maxi as i32);
     }
     res
 }
 
-struct SegmentTree {
-    tree: Vec<i64>,
+#[derive(Default)]
+struct Trie {
+    nodes: [Option<Box<Trie>>; 26],
+    len: usize,
 }
 
-impl SegmentTree {
-    fn new(n: usize) -> Self {
-        Self {
-            tree: vec![0; 4 * n],
+impl Trie {
+    fn insert(&mut self, it: impl Iterator<Item = u8>) {
+        let mut curr = self;
+        for (i, b) in it.enumerate() {
+            let idx = usize::from(b - b'a');
+            curr = curr.nodes[idx].get_or_insert_default();
+            curr.len = 1 + i;
         }
     }
 
-    fn update(&mut self, node: usize, left: usize, right: usize, idx: usize, delta: i64) {
-        if left == right {
-            self.tree[node] += delta;
-            return;
+    fn find(&self, it: impl Iterator<Item = u8>) -> usize {
+        let mut curr = self;
+        let mut res = curr.len;
+        for b in it {
+            let idx = usize::from(b - b'a');
+            let Some(ref v) = curr.nodes[idx] else {
+                break;
+            };
+            curr = v;
+            res = curr.len;
         }
-        let mid = left.midpoint(right);
-        if idx <= mid {
-            self.update(2 * node + 1, left, mid, idx, delta);
-        } else {
-            self.update(2 * node + 2, 1 + mid, right, idx, delta);
-        }
-        self.tree[node] = self.tree[2 * node + 1].max(self.tree[2 * node + 2]);
+        res
     }
 }
 
@@ -73,31 +91,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            most_frequent_i_ds(&[2, 3, 2, 1], &[3, 2, -3, 1]),
-            [3, 3, 2, 2]
-        );
-        assert_eq!(most_frequent_i_ds(&[5, 5, 3], &[2, -2, 1]), [2, 0, 1]);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(
-            most_frequent_i_ds(
-                &[
-                    18, 3, 2, 19, 14, 14, 17, 2, 15, 16, 10, 12, 5, 19, 17, 9, 16, 14, 14, 17, 13,
-                    1, 7, 13, 13, 14, 2, 19, 4, 8, 15, 18, 11, 14, 11, 6, 11, 5
-                ],
-                &[
-                    3, 3, 5, 4, 1, -1, 3, 5, 2, 2, 4, 1, 4, -2, -3, 3, -2, 2, 4, 1, 1, 1, 5, -1, 2,
-                    4, 4, -2, 2, 5, 1, 2, 3, -7, 2, 3, -3, 3
-                ]
-            ),
-            [
-                3, 3, 5, 5, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-                10, 10, 10, 10, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14
-            ]
-        );
-    }
+    fn test() {}
 }
