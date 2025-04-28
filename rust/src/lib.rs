48efd6c448e642fa2fbe5913dad24d78e3dbc6ad
pkusensei <ZW_08@outlook.com>
2025-04-28 11:35:17 -0400

    3100

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,46 +3,18 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_of_powers(mut nums: Vec<i32>, k: i32) -> i32 {
-    let n = nums.len();
-    nums.sort_unstable();
-    dfs(&nums, k as usize, 0, n, i32::MAX, &mut HashMap::new())
-}
-
-fn dfs(
-    nums: &[i32],
-    k: usize,
-    idx: usize,
-    prev: usize,
-    min_d: i32,
-    memo: &mut HashMap<(usize, usize, usize, i32), i32>,
-) -> i32 {
-    let n = nums.len();
-    if k == 0 {
-        return min_d;
+pub const fn max_bottles_drunk(mut num_bottles: i32, mut num_exchange: i32) -> i32 {
+    let mut res = 0;
+    while num_bottles >= num_exchange {
+        res += num_exchange;
+        num_bottles -= num_exchange;
+        num_bottles += 1;
+        num_exchange += 1
     }
-    if n - idx < k {
-        return 0;
-    }
-    let key = (k, idx, prev, min_d);
-    if let Some(&v) = memo.get(&key) {
-        return v;
-    }
-    let curr = if prev == n {
-        min_d
-    } else {
-        min_d.min(nums[idx] - nums[prev])
-    };
-    let skip = dfs(nums, k, 1 + idx, prev, min_d, memo);
-    let take = dfs(nums, k - 1, 1 + idx, idx, curr, memo);
-    let res = (skip + take) % 1_000_000_007;
-    memo.insert(key, res);
-    res
+    res + num_bottles
 }
 
 #[cfg(test)]
@@ -75,11 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(sum_of_powers(vec![1, 2, 3, 4], 3), 4);
-        assert_eq!(sum_of_powers(vec![2, 2], 2), 0);
-        assert_eq!(sum_of_powers(vec![4, 3, -1], 2), 10);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
