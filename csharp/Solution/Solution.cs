d5553fa3159aea6eedccbd1c85de2b12cb0090bd
pkusensei <ZW_08@outlook.com>
2025-04-28 20:17:37 -0400

    2962

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,69 +8,24 @@ namespace Solution;
 
 public class Solution
 {
-    public int[] MinimumCost(int n, int[][] edges, int[][] query)
+    public long CountSubarrays(int[] nums, int k)
     {
-        var dsu = new DSU(n);
-        foreach (var item in edges)
+        int max = nums.Max();
+        int left = 0;
+        long res = 0;
+        int count = 0;
+        for (int right = 0; right < nums.Length; right += 1)
         {
-            dsu.Union(item[0], item[1], item[2]);
+            count += nums[right] == max ? 1 : 0;
+            while (count > k)
+            {
+                count -= nums[left] == max ? 1 : 0;
+                left += 1;
+            }
+            res += right + 1 - left;
         }
-        return [.. query.Select(q =>
-        {
-            int x = dsu.Find(q[0]);
-            int y = dsu.Find(q[1]);
-            return x == y ? dsu.Vals[x] : -1;
-        })];
+        long n = nums.Length;
+        return n * (1 + n) / 2 - res;
     }
 }
 
-class DSU
-{
-    public int[] Parent { get; }
-    public int[] Rank { get; }
-    public int[] Vals { get; }
-
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)];
-        Rank = new int[n];
-        Vals = new int[n];
-        Array.Fill(Vals, int.MaxValue);
-    }
-
-    public int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public void Union(int x, int y, int val)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry)
-        {
-            Vals[rx] &= val;
-            return;
-        }
-        if (Rank[rx] < Rank[ry])
-        {
-            Parent[rx] = ry;
-            Vals[ry] &= val;
-            Vals[ry] &= Vals[rx];
-        }
-        else if (Rank[rx] == Rank[ry])
-        {
-            Parent[ry] = rx;
-            Rank[rx] += 1;
-            Vals[rx] &= val;
-            Vals[rx] &= Vals[ry];
-        }
-        else
-        {
-            Parent[ry] = rx;
-            Vals[rx] &= val;
-            Vals[rx] &= Vals[ry];
-        }
-    }
-}
