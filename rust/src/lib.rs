5b2c908c9dc5342b17498e8c5794b5950eb1a4cd
pkusensei <ZW_08@outlook.com>
2025-04-28 21:28:08 -0400

    3113 mono stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,38 +3,22 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, HashMap},
-};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_time(n: i32, edges: Vec<Vec<i32>>, disappear: Vec<i32>) -> Vec<i32> {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![HashMap::new(); n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        let v = acc[a].entry(b).or_insert(e[2]);
-        *v = (*v).min(e[2]);
-        let v = acc[b].entry(a).or_insert(e[2]);
-        *v = (*v).min(e[2]);
-        acc
-    });
-    let mut res = vec![-1; n];
-    res[0] = 0;
-    let mut heap = BinaryHeap::from([(Reverse(0), 0)]);
-    while let Some((Reverse(cost), node)) = heap.pop() {
-        if cost > res[node] {
-            continue;
+pub fn number_of_subarrays(nums: &[i32]) -> i64 {
+    let mut st: Vec<(i32, i64)> = vec![];
+    let mut res = 0;
+    for &num in nums.iter() {
+        while st.last().is_some_and(|&(v, _count)| v < num) {
+            st.pop();
         }
-        for (&next, &c) in adj[node].iter() {
-            let nc = cost + c;
-            if nc < disappear[next] && (res[next] == -1 || res[next] > nc) {
-                res[next] = nc;
-                heap.push((Reverse(nc), next));
-            }
+        if st.last().is_none_or(|&(v, _)| v > num) {
+            st.push((num, 1));
+        } else if let Some((_, count)) = st.last_mut() {
+            *count += 1;
         }
+        res += st.last().map(|&(_, count)| count).unwrap_or(0);
     }
     res
 }
@@ -69,7 +53,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_subarrays(&[1, 4, 3, 3, 2]), 6);
+        assert_eq!(number_of_subarrays(&[3, 3, 3]), 6);
+        assert_eq!(number_of_subarrays(&[1]), 1);
+    }
 
     #[test]
     fn test() {}
