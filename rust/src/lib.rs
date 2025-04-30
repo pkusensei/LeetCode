73fde0b4280757c16cba59401b9366be68fc12bf
pkusensei <ZW_08@outlook.com>
2025-04-30 15:48:10 -0400

    3134 binary search and sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_added_integer(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> i32 {
-    nums1.sort_unstable();
-    nums2.sort_unstable();
-    let mut res = i32::MAX;
-    for start in 0..=2 {
-        let delta = nums2[0] - nums1[start];
-        let mut skip = start;
-        let mut i2 = 0;
-        for v1 in nums1[start..].iter() {
-            if nums2.get(i2).is_some_and(|&v| v == v1 + delta) {
-                i2 += 1
+use itertools::Itertools;
+use std::collections::HashMap;
+
+pub fn median_of_uniqueness_array(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let size = n * (1 + n) / 2;
+    let median = (size + 1) / 2;
+    let mut left = 1;
+    let mut right = nums.iter().unique().count();
+    while left < right {
+        let mid = left.midpoint(right);
+        if count(nums, mid) < median {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    left as i32
+}
+
+fn count(nums: &[i32], unique: usize) -> usize {
+    let mut res = 0;
+    let mut left = 0;
+    let mut map = HashMap::new();
+    for (right, &num) in nums.iter().enumerate() {
+        *map.entry(num).or_insert(0) += 1;
+        while map.len() > unique {
+            let v = map.entry(nums[left]).or_insert(0);
+            if *v == 1 {
+                map.remove(&nums[left]);
             } else {
-                skip += 1
+                *v -= 1;
             }
+            left += 1;
         }
-        if skip == 2 {
-            res = res.min(delta);
-        }
+        res += right + 1 - left
     }
     res
 }
@@ -59,17 +77,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            minimum_added_integer(vec![4, 20, 16, 12, 8], vec![14, 18, 10]),
-            -2
-        );
+        assert_eq!(median_of_uniqueness_array(&[1, 2, 3]), 1);
+        assert_eq!(median_of_uniqueness_array(&[3, 4, 3, 4, 5]), 2);
+        assert_eq!(median_of_uniqueness_array(&[4, 3, 5, 4]), 2);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            minimum_added_integer(vec![7, 2, 6, 8, 7], vec![7, 6, 5]),
-            -1
-        );
-    }
+    fn test() {}
 }
