79716b28a344cf7e110d4e1a21d348927d795729
Zx Wang <ZW_08@outlook.com>
2025-04-30 11:49:12 -0400

    3132 sort and two ptrs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,75 +6,26 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_stable_arrays(zero: i32, one: i32, limit: i32) -> i32 {
-    // dp[zero][one][0 or 1]
-    let [zero, one, limit] = [zero, one, limit].map(|v| v as usize);
-    let mut dp = vec![vec![[0_i64; 2]; 1 + one]; 1 + zero];
-    for ze in 0..=zero.min(limit) {
-        dp[ze][0][0] = 1;
-    }
-    for on in 0..=one.min(limit) {
-        dp[0][on][1] = 1;
-    }
-    for ze in 1..=zero {
-        for on in 1..=one {
-            dp[ze][on][0] = (dp[ze - 1][on][0] + dp[ze - 1][on][1]
-                - ze.checked_sub(limit + 1)
-                    .map(|prev| dp[prev][on][1])
-                    .unwrap_or(0))
-            .rem_euclid(M);
-            dp[ze][on][1] = (dp[ze][on - 1][1] + dp[ze][on - 1][0]
-                - on.checked_sub(1 + limit)
-                    .map(|prev| dp[ze][prev][0])
-                    .unwrap_or(0))
-            .rem_euclid(M)
+pub fn minimum_added_integer(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> i32 {
+    nums1.sort_unstable();
+    nums2.sort_unstable();
+    let mut res = i32::MAX;
+    for start in 0..=2 {
+        let delta = nums2[0] - nums1[start];
+        let mut skip = start;
+        let mut i2 = 0;
+        for v1 in nums1[start..].iter() {
+            if nums2.get(i2).is_some_and(|&v| v == v1 + delta) {
+                i2 += 1
+            } else {
+                skip += 1
+            }
+        }
+        if skip == 2 {
+            res = res.min(delta);
         }
     }
-    ((dp[zero][one][0] + dp[zero][one][1]) % M) as i32
-}
-
-const M: i64 = 1_000_000_007;
-
-pub fn top_down(zero: i32, one: i32, limit: i32) -> i32 {
-    let mut memo =
-        vec![vec![vec![vec![-1; 1 + limit as usize]; 3]; 1 + one as usize]; 1 + zero as usize];
-    dfs(zero, one, limit, 2, 0, &mut memo) as i32
-}
-
-fn dfs(
-    num_zero: i32,
-    num_one: i32,
-    limit: i32,
-    prev: i32,
-    count: i32,
-    memo: &mut [Vec<Vec<Vec<i64>>>],
-) -> i64 {
-    if num_zero == 0 && num_one == 0 {
-        return i64::from(count <= limit);
-    }
-    if num_zero < 0 || num_one < 0 || count > limit {
-        return 0;
-    }
-    if memo[num_zero as usize][num_one as usize][prev as usize][count as usize] > -1 {
-        return memo[num_zero as usize][num_one as usize][prev as usize][count as usize];
-    }
-    let res = dfs(
-        num_zero - 1,
-        num_one,
-        limit,
-        0,
-        if prev == 0 { 1 + count } else { 1 },
-        memo,
-    ) + dfs(
-        num_zero,
-        num_one - 1,
-        limit,
-        1,
-        if prev == 1 { 1 + count } else { 1 },
-        memo,
-    );
-    memo[num_zero as usize][num_one as usize][prev as usize][count as usize] = res % M;
-    memo[num_zero as usize][num_one as usize][prev as usize][count as usize]
+    res
 }
 
 #[cfg(test)]
@@ -108,15 +59,17 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(number_of_stable_arrays(1, 1, 2), 2);
-        assert_eq!(number_of_stable_arrays(1, 2, 1), 1);
-        assert_eq!(number_of_stable_arrays(3, 3, 2), 14);
-
-        assert_eq!(top_down(1, 1, 2), 2);
-        assert_eq!(top_down(1, 2, 1), 1);
-        assert_eq!(top_down(3, 3, 2), 14);
+        assert_eq!(
+            minimum_added_integer(vec![4, 20, 16, 12, 8], vec![14, 18, 10]),
+            -2
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            minimum_added_integer(vec![7, 2, 6, 8, 7], vec![7, 6, 5]),
+            -1
+        );
+    }
 }
