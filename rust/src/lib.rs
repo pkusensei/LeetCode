b5294f1ef080569e6056f808495aacae896c468a
Zx Wang <ZW_08@outlook.com>
2025-04-30 10:40:45 -0400

    3129 dfs/4d memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,47 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_right_triangles(grid: Vec<Vec<i32>>) -> i64 {
-    let cols = grid[0].len();
-    let row_sums: Vec<_> = grid
-        .iter()
-        .map(|r| r.iter().map(|&v| i64::from(v)).sum::<i64>())
-        .collect();
-    let col_sums: Vec<_> = (0..cols)
-        .map(|c| grid.iter().map(|r| i64::from(r[c])).sum::<i64>())
-        .collect();
-    let mut res = 0;
-    for (r, row) in grid.iter().enumerate() {
-        for (c, &v) in row.iter().enumerate() {
-            if v == 1 {
-                res += (row_sums[r] - 1) * (col_sums[c] - 1);
-            }
-        }
+pub fn number_of_stable_arrays(zero: i32, one: i32, limit: i32) -> i32 {
+    let mut memo =
+        vec![vec![vec![vec![-1; 1 + limit as usize]; 3]; 1 + one as usize]; 1 + zero as usize];
+    dfs(zero, one, limit, 2, 0, &mut memo)
+}
+
+fn dfs(
+    num_zero: i32,
+    num_one: i32,
+    limit: i32,
+    prev: i32,
+    count: i32,
+    memo: &mut [Vec<Vec<Vec<i32>>>],
+) -> i32 {
+    const M: i32 = 1_000_000_007;
+    if num_zero == 0 && num_one == 0 {
+        return i32::from(count <= limit);
     }
-    res
+    if num_zero < 0 || num_one < 0 || count > limit {
+        return 0;
+    }
+    if memo[num_zero as usize][num_one as usize][prev as usize][count as usize] > -1 {
+        return memo[num_zero as usize][num_one as usize][prev as usize][count as usize];
+    }
+    let res = dfs(
+        num_zero - 1,
+        num_one,
+        limit,
+        0,
+        if prev == 0 { 1 + count } else { 1 },
+        memo,
+    ) + dfs(
+        num_zero,
+        num_one - 1,
+        limit,
+        1,
+        if prev == 1 { 1 + count } else { 1 },
+        memo,
+    );
+    memo[num_zero as usize][num_one as usize][prev as usize][count as usize] = res % M;
+    memo[num_zero as usize][num_one as usize][prev as usize][count as usize]
 }
 
 #[cfg(test)]
@@ -56,7 +79,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_stable_arrays(1, 1, 2), 2);
+        assert_eq!(number_of_stable_arrays(1, 2, 1), 1);
+        assert_eq!(number_of_stable_arrays(3, 3, 2), 14);
+    }
 
     #[test]
     fn test() {}
