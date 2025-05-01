a3b5d012d519cb15eb1707819f524a483a47c458
pkusensei <ZW_08@outlook.com>
2025-04-30 22:15:01 -0400

    2071 why is there no deque in C#

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,24 +8,54 @@ namespace Solution;
 
 public class Solution
 {
-    public long MinEnd(int n, int x)
+    public int MaxTaskAssign(int[] tasks, int[] workers, int pills, int strength)
     {
-        var sb = new StringBuilder(Convert.ToString(x, 2), 64);
-        var val = Convert.ToString(n - 1, 2);
-        int i2 = val.Length - 1;
-        for (int i1 = sb.Length - 1; i1 >= 0; i1 -= 1)
+        Array.Sort(tasks);
+        Array.Sort(workers, (a, b) => b.CompareTo(a));
+        int left = 0;
+        int right = tasks.Length;
+        while (left < right)
         {
-            if (sb[i1] == '1') { continue; }
-            sb[i1] = val[i2];
-            i2 -= 1;
-            if (i2 < 0) { break; }
+            int mid = left + (right + 1 - left) / 2;
+            if (Check(mid, pills)) { left = mid; }
+            else { right = mid - 1; }
         }
-        while (i2 >= 0)
+        return left;
+
+        bool Check(int mid, int pills)
         {
-            sb.Insert(0, val[i2]);
-            i2 -= 1;
+            if (mid > workers.Length) { return false; }
+            int taski = 0;
+            List<int> deque = new(mid);
+            // worker[i] goes from mid-1 to 0; least to most
+            for (int i = mid - 1; i >= 0; i -= 1)
+            {
+                if (deque.Count == 0 && taski < mid)
+                {
+                    deque.Insert(0, tasks[taski]);
+                    taski += 1;
+                }
+                if (deque.Last() <= workers[i])
+                {
+                    deque.RemoveAt(deque.Count - 1); // worker[i] finished last in deque
+                }
+                else
+                {
+                    if (pills == 0) { return false; }
+                    if (deque.Last() > workers[i] + strength) { return false; }
+                    // Add all thats possible with worker[i] and pill
+                    while (taski < mid && tasks[taski] <= workers[i] + strength)
+                    {
+                        deque.Insert(0, tasks[taski]);
+                        taski += 1;
+                    }
+                    // worker[i] finishes first in queue with pill
+                    deque.RemoveAt(0);
+                    pills -= 1;
+                }
+            }
+            return true;
         }
-        return Convert.ToInt64(sb.ToString(), 2);
     }
 }
 
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(3, 4, 6)]
-    [DataRow(2, 7, 15)]
-    [DataRow(1, 4, 4)]
-    public void TestMethod1(int n, int x, int exp)
+    [DataRow(new[] { 3, 2, 1 }, new[] { 0, 3, 3 }, 1, 1, 3)]
+    [DataRow(new[] { 5, 4 }, new[] { 0, 0, 0 }, 1, 5, 1)]
+    [DataRow(new[] { 10, 15, 30 }, new[] { 0, 10, 10, 10, 10 }, 3, 10, 2)]
+    public void TestMethod1(int[] tasks, int[] workers, int pills, int strength, int exp)
     {
-        Assert.AreEqual(exp, sol.MinEnd(n, x));
+        Assert.AreEqual(exp, sol.MaxTaskAssign(tasks, workers, pills, strength));
     }
 
     [TestMethod]
