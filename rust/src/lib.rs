45a04563bb7b6b5c0ad35b46346858b889a223fd
pkusensei <ZW_08@outlook.com>
2025-05-01 11:41:38 -0400

    3143 BTreeMap ftw

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,19 +6,32 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-    pub fn satisfies_conditions(grid: Vec<Vec<i32>>) -> bool {
-        let [rows, cols] = get_dimensions(&grid);
-        for r in 0..rows {
-            for c in 0..cols {
-                if (r < rows - 1 && grid[r][c] != grid[1 + r][c])
-                    || (c < cols - 1 && grid[r][c] == grid[r][1 + c])
-                {
-                    return false;
-                }
-            }
-        }
-        true
+pub fn max_points_inside_square(points: &[[i32; 2]], s: &str) -> i32 {
+    use itertools::{Itertools, izip};
+    use std::collections::BTreeMap;
+    let arr = izip!(points.iter(), s.bytes())
+        .map(|(p, b)| {
+            let [x, y] = p[..] else { unreachable!() };
+            (x.abs().max(y.abs()), b - b'a')
+        })
+        .sorted_unstable_by_key(|&(dist, _tag)| dist)
+        .collect_vec();
+    let mut map = BTreeMap::new();
+    let mut mask: i32 = 0;
+    for (i, &(dist, tag)) in arr.iter().enumerate() {
+        mask |= 1 << tag;
+        map.insert(dist, (mask, 1 + i as u32));
     }
+    let mut res = 0;
+    for &(mask, count) in map.values() {
+        if mask.count_ones() == count {
+            res = res.max(count)
+        } else {
+            break;
+        }
+    }
+    res as i32
+}
 
 #[cfg(test)]
 mod tests {
@@ -50,7 +63,20 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            max_points_inside_square(&[[2, 2], [-1, -2], [-4, 4], [-3, 1], [3, -3]], "abdca"),
+            2
+        );
+        assert_eq!(
+            max_points_inside_square(&[[1, 1], [-2, -2], [-2, 2]], "abb"),
+            1
+        );
+        assert_eq!(
+            max_points_inside_square(&[[1, 1], [-1, -1], [2, -2]], "ccd"),
+            0
+        );
+    }
 
     #[test]
     fn test() {}
