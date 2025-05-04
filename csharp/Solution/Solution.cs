394d27c781c991220aef4f03c4f0493575253a56
pkusensei <ZW_08@outlook.com>
2025-05-03 20:29:09 -0400

    1128

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,30 +8,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinDominoRotations(int[] tops, int[] bottoms)
+    public int NumEquivDominoPairs(int[][] dominoes)
     {
-        if (tops.Length != bottoms.Length) { return -1; }
-        int a = Solve(tops[0]);
-        int b = Solve(bottoms[0]);
-        return (a == -1, b == -1) switch
+        Dictionary<(int, int), int> dict = [];
+        int res = 0;
+        foreach (var item in dominoes)
         {
-            (true, true) => -1,
-            (true, false) => b,
-            (false, true) => a,
-            _ => Math.Min(a, b)
-        };
-
-        int Solve(int target)
-        {
-            int count1 = 0;
-            int count2 = 0;
-            foreach (var (a, b) in tops.Zip(bottoms))
-            {
-                if (target != a && target != b) { return -1; }
-                else if (target == a && target != b) { count2 += 1; }
-                else if (target != a && target == b) { count1 += 1; }
-            }
-            return Math.Min(count1, count2);
+            int a = item.Min();
+            int b = item.Max();
+            if (dict.TryGetValue((a, b), out var v)) { res += v; }
+            if (!dict.TryAdd((a, b), 1)) { dict[(a, b)] += 1; }
         }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,8 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 2, 1, 2, 4, 2, 2 }, new[] { 5, 2, 6, 2, 3, 2 }, 2)]
-    [DataRow(new[] { 3, 5, 1, 2, 3 }, new[] { 3, 6, 3, 3, 4 }, -1)]
-    [DataRow(new[] { 1, 2, 1, 1, 1, 2, 2, 2 }, new[] { 2, 1, 2, 2, 2, 2, 2, 2 }, 1)]
-    public void TestMethod1(int[] top, int[] bottom, int exp)
+    public void TestMethod1()
     {
-        Assert.AreEqual(exp, sol.MinDominoRotations(top, bottom));
     }
 
     [TestMethod]
