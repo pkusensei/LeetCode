e92e26bd8ee18d1b9a3c5b72bd55c375a1c7d099
pkusensei <ZW_08@outlook.com>
2025-05-04 13:04:45 -0400

    3158 bitmask

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,29 +3,18 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn ways_to_reach_stair(k: i32) -> i32 {
-    dfs(k, 1, 0, true, &mut HashMap::new())
-}
-
-fn dfs(k: i32, curr: i32, jump: u32, dec: bool, memo: &mut HashMap<(i32, u32, bool), i32>) -> i32 {
-    if curr > 1 + k {
-        return 0;
+pub fn duplicate_numbers_xor(nums: Vec<i32>) -> i32 {
+    let mut res = 0;
+    let mut mask = 0_i64;
+    for &num in &nums {
+        if mask & (1 << num) > 0 {
+            res ^= num;
+        }
+        mask |= 1 << num
     }
-    let key = (curr, jump, dec);
-    if let Some(&v) = memo.get(&key) {
-        return v;
-    }
-    let mut res = i32::from(k == curr);
-    res += dfs(k, curr + 2_i32.pow(jump), 1 + jump, true, memo);
-    if dec && curr > 0 {
-        res += dfs(k, curr - 1, jump, false, memo);
-    }
-    memo.insert(key, res);
     res
 }
 
@@ -59,10 +48,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(ways_to_reach_stair(0), 2);
-        assert_eq!(ways_to_reach_stair(1), 4);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
