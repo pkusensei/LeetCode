dc16a0aa57ff2d7f4579663bc1615c615c2cb8cd
pkusensei <ZW_08@outlook.com>
2025-05-05 10:30:35 -0400

    3163

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,93 +6,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_results(queries: &[&[i32]]) -> Vec<bool> {
-    let max = queries.iter().map(|q| q[1] as usize).max().unwrap_or(0);
-    let mut tree = SegmentTree::new(2 + max);
-    let mut res = vec![];
-    for q in queries.iter() {
-        if q[0] == 1 {
-            tree.insert(0, 0, tree.n - 1, q[1] as usize);
-        } else {
-            let [x, sz] = [1, 2].map(|i| q[i] as usize);
-            let state = tree.query(0, 0, tree.n - 1, 0, x);
-            if let Some([a, b]) = state.obs {
-                let diff = state.diff.max(a).max(x.saturating_sub(b));
-                res.push(sz <= diff);
-            } else {
-                res.push(sz <= 1 + x);
-            }
+pub fn compressed_string(word: String) -> String {
+    let mut res = String::new();
+    for ch in word.as_bytes().chunk_by(|a, b| a == b) {
+        for _ in 0..ch.len() / 9 {
+            res.push('9');
+            res.push(char::from(ch[0]));
+        }
+        if ch.len() % 9 > 0 {
+            res.push_str(&format!("{}{}", ch.len() % 9, char::from(ch[0])));
         }
     }
     res
-}
-
-#[derive(Clone, Copy, Default)]
-struct State {
-    obs: Option<[usize; 2]>,
-    diff: usize,
-}
-
-impl State {
-    fn merge(self, other: State) -> Self {
-        match (self.obs, other.obs) {
-            (None, _) => other,
-            (Some(_), None) => self,
-            (Some([a1, a2]), Some([b1, b2])) => {
-                let diff = self.diff.max(other.diff).max(b1 - a2);
-                Self {
-                    obs: Some([a1, b2]),
-                    diff,
-                }
-            }
-        }
-    }
-}
-
-struct SegmentTree {
-    tree: Vec<State>,
-    n: usize,
-}
-
-impl SegmentTree {
-    fn new(n: usize) -> Self {
-        let mut s = Self {
-            tree: vec![Default::default(); 4 * n],
-            n,
-        };
-        s.insert(0, 0, n - 1, 0);
-        s
-    }
-
-    fn insert(&mut self, node: usize, left: usize, right: usize, idx: usize) {
-        if left == right {
-            self.tree[node] = State {
-                obs: Some([idx; 2]),
-                diff: 0,
-            };
-            return;
-        }
-        let mid = left.midpoint(right);
-        if idx <= mid {
-            self.insert(2 * node + 1, left, mid, idx);
-        } else {
-            self.insert(2 * node + 2, 1 + mid, right, idx);
-        }
-        self.tree[node] = self.tree[2 * node + 1].merge(self.tree[2 * node + 2]);
-    }
-
-    fn query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> State {
-        if qr < left || right < ql {
-            return Default::default();
-        }
-        if ql <= left && right <= qr {
-            return self.tree[node];
-        }
-        let mid = left.midpoint(right);
-        let a = self.query(2 * node + 1, left, mid, ql, qr);
-        let b = self.query(2 * node + 2, 1 + mid, right, ql, qr);
-        a.merge(b)
-    }
 }
 
 #[cfg(test)]
@@ -125,16 +50,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            get_results(&[&[1, 2], &[2, 3, 3], &[2, 3, 1], &[2, 2, 2]]),
-            [false, true, true]
-        );
-        assert_eq!(
-            get_results(&[&[1, 7], &[2, 7, 6], &[1, 2], &[2, 7, 5], &[2, 7, 6]]),
-            [true, true, false]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
