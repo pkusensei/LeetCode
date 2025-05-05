50dc797e3aca29885117c4ad6b86cc16d129bfa9
pkusensei <ZW_08@outlook.com>
2025-05-04 21:15:16 -0400

    790 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,17 +8,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int NumEquivDominoPairs(int[][] dominoes)
+    public int NumTilings(int n)
     {
-        Dictionary<(int, int), int> dict = [];
-        int res = 0;
-        foreach (var item in dominoes)
+        const int M = 1_000_000_007;
+        Span<int> dp = stackalloc int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for (int i = 4; i <= n; i++)
         {
-            int a = item.Min();
-            int b = item.Max();
-            if (dict.TryGetValue((a, b), out var v)) { res += v; }
-            if (!dict.TryAdd((a, b), 1)) { dict[(a, b)] += 1; }
+            dp[i] = (2 * dp[i - 1] % M + dp[i - 3]) % M;
         }
-        return res;
+        return dp[n];
     }
 }
