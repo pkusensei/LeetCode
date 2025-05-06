ba3c5ab910b4030f110e633453d393f8812b1d8f
pkusensei <ZW_08@outlook.com>
2025-05-05 20:33:48 -0400

    1920

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,17 +8,8 @@ namespace Solution;
 
 public class Solution
 {
-    public int NumTilings(int n)
+    public int[] BuildArray(int[] nums)
     {
-        const int M = 1_000_000_007;
-        Span<int> dp = stackalloc int[1001];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
-        for (int i = 4; i <= n; i++)
-        {
-            dp[i] = (2 * dp[i - 1] % M + dp[i - 3]) % M;
-        }
-        return dp[n];
+        return nums.Select(v => nums[v]).ToArray();
     }
 }
