eb2c1aaab9ee2b721ccc3748886d7581834f3567
pkusensei <ZW_08@outlook.com>
2025-05-07 10:08:48 -0400

    3184 2sum but with modulos

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,33 +6,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_total_reward(mut reward_values: Vec<i32>) -> i32 {
-    reward_values.sort_unstable();
-    reward_values.dedup();
-    // let mut set = std::collections::HashSet::from([0]);
-    // for num in reward_values {
-    //     for score in set.clone() {
-    //         if score < num {
-    //             set.insert(num + score);
-    //         }
-    //     }
-    // }
-    // set.into_iter().max().unwrap()
-    let n = reward_values.len();
-    let max = reward_values[n - 1];
-    let mut dp = vec![0; max as usize];
-    for (idx, &val) in reward_values.iter().enumerate() {
-        let temp = val as usize; // With this value
-        if idx.checked_sub(1).is_none_or(|v| v != temp) {
-            // Find all previous processed values and their subset sums
-            let limit = temp.min(max as usize - temp);
-            for left in 0..limit {
-                let prev = dp[left] as usize;
-                dp[temp + prev] = (temp + prev) as i32;
-            }
+pub fn count_complete_day_pairs(hours: Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
+    let mut res = 0;
+    for &num in &hours {
+        let rem = num % 24;
+        if rem == 0 {
+            res += map.get(&0).unwrap_or(&0);
+        } else {
+            res += map.get(&(24 - rem)).unwrap_or(&0);
         }
+        *map.entry(num % 24).or_insert(0) += 1;
     }
-    max + dp.iter().max().unwrap()
+    res
 }
 
 #[cfg(test)]
@@ -65,13 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_total_reward(vec![1, 1, 3, 3]), 4);
-        assert_eq!(max_total_reward(vec![1, 6, 4, 3, 2]), 11);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(max_total_reward(vec![6, 13, 9, 19]), 34);
-    }
+    fn test() {}
 }
