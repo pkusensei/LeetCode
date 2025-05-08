8897d0b2f9bde3d834685aeb1eb668bb7a1379da
pkusensei <ZW_08@outlook.com>
2025-05-07 20:25:18 -0400

    3193 hard dp...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,15 +6,47 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(nums: Vec<i32>) -> i32 {
-    let mut res = 0;
-    for &num in &nums {
-        // num == res&1
-        if (num == 0 && res & 1 == 0) || (num == 1 && res & 1 == 1) {
-            res += 1;
+pub fn number_of_permutations(n: i32, requirements: &[[i32; 2]]) -> i32 {
+    const M: i64 = 1_000_000_007;
+    let n = n as usize;
+    let mut max_end = 0;
+    let mut max_cnt = 0;
+    let mut req = vec![None; n];
+    for r in requirements.iter() {
+        let [end, cnt] = r[..] else { unreachable!() };
+        if end == 0 && cnt > 0 {
+            return 0;
         }
+        req[end as usize] = Some(cnt as usize);
+        max_end = max_end.max(end as usize);
+        max_cnt = max_cnt.max(cnt as usize);
     }
-    res
+    let mut dp = vec![0; 1 + max_cnt];
+    dp[0] = 1;
+    for idx in 0..=max_end {
+        let mut curr = vec![0; 1 + max_cnt];
+        if let Some(c) = req[idx] {
+            curr[c] = dp[c.saturating_sub(idx)..=c]
+                .iter()
+                .fold(0, |acc, v| (acc + v) % M);
+        } else {
+            for c in 0..=max_cnt {
+                curr[c] = dp[c];
+                if let Some(prev) = c.checked_sub(1) {
+                    curr[c] = (curr[c] + curr[prev]) % M;
+                }
+                if let Some(prev) = c.checked_sub(1 + idx) {
+                    curr[c] = (curr[c] - dp[prev]).rem_euclid(M);
+                }
+            }
+        }
+        dp = curr;
+    }
+    let mut res = dp.iter().fold(0, |acc, v| (acc + v) % M);
+    for i in 1 + max_end..n {
+        res = res * (1 + i) as i64 % M;
+    }
+    res as i32
 }
 
 #[cfg(test)]
@@ -47,7 +79,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_permutations(3, &[[2, 2], [0, 0]]), 2);
+        assert_eq!(number_of_permutations(3, &[[2, 2], [1, 1], [0, 0]]), 1);
+        assert_eq!(number_of_permutations(2, &[[0, 0], [1, 0]]), 1);
+    }
 
     #[test]
     fn test() {}
