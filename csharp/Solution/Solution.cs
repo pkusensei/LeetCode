08bf41bf86370e1fa82051c30c26c987dda4e3ae
pkusensei <ZW_08@outlook.com>
2025-05-09 10:47:40 -0400

    3208

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,55 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinimumDiameterAfterMerge(int[][] edges1, int[][] edges2)
+    public int NumberOfAlternatingGroups(int[] colors, int k)
     {
-        var tree1 = Build(edges1);
-        var tree2 = Build(edges2);
-        int d1 = TopoSort(tree1);
-        int d2 = TopoSort(tree2);
-        int half1 = d1 / 2 + (d1 & 1);
-        int half2 = d2 / 2 + (d2 & 1);
-        return Math.Max(1 + half1 + half2, Math.Max(d1, d2));
-
-        static int TopoSort(List<List<int>> tree)
+        int curr = 1;
+        int res = 0;
+        int n = colors.Length;
+        for (int i = 1; i < n + k - 1; i++)
         {
-            int nodes = tree.Count;
-            int[] degs = new int[nodes];
-            Queue<int> queue = [];
-            for (int i = 0; i < nodes; i++)
-            {
-                degs[i] = tree[i].Count;
-                if (degs[i] == 1) { queue.Enqueue(i); }
-            }
-            int layers = 0;
-            while (nodes > 2)
-            {
-                int sz = queue.Count;
-                nodes -= sz;
-                layers += 1;
-                for (int _ = 0; _ < sz; _++)
-                {
-                    int front = queue.Dequeue();
-                    foreach (var item in tree[front])
-                    {
-                        degs[item] -= 1;
-                        if (degs[item] == 1) { queue.Enqueue(item); }
-                    }
-                }
-            }
-            return 2 * layers + nodes / 2;
+            if (colors[i % n] != colors[(i - 1) % n]) { curr += 1; }
+            else { curr = 1; }
+            res += curr >= k ? 1 : 0;
         }
-
-        static List<List<int>> Build(int[][] edges)
-        {
-            int n = 1 + edges.Length;
-            List<List<int>> res = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
-            foreach (var e in edges)
-            {
-                res[e[0]].Add(e[1]);
-                res[e[1]].Add(e[0]);
-            }
-            return res;
-        }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,17 +12,11 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        int[][] a = [[0, 1], [0, 2], [0, 3]];
-        int[][] b = [[0, 1]];
-        Assert.AreEqual(3, sol.MinimumDiameterAfterMerge(a, b));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        int[][] a = [[0, 1], [0, 2], [0, 3], [2, 4], [2, 5], [3, 6], [2, 7]];
-        int[][] b = [[0, 1], [0, 2], [0, 3], [2, 4], [2, 5], [3, 6], [2, 7]];
-        Assert.AreEqual(5, sol.MinimumDiameterAfterMerge(a, b));
     }
 
     [TestMethod]
