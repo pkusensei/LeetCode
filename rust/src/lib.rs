02c26a8ac2bc0935946e59a944cc2c9a652cabf8
pkusensei <ZW_08@outlook.com>
2025-05-10 14:41:51 -0400

    3224 greedy and try 2 most frequent diffs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,33 +6,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_cost(
-    _m: i32,
-    _n: i32,
-    mut horizontal_cut: Vec<i32>,
-    mut vertical_cut: Vec<i32>,
-) -> i64 {
-    horizontal_cut.sort_unstable();
-    vertical_cut.sort_unstable();
-    let mut h_count = 1;
-    let mut v_count = 1;
-    let mut res = 0;
-    while let Some((&hor, &ver)) = horizontal_cut.last().zip(vertical_cut.last()) {
-        if hor > ver {
-            horizontal_cut.pop();
-            res += i64::from(hor * v_count);
-            h_count += 1;
-        } else {
-            vertical_cut.pop();
-            res += i64::from(ver * h_count);
-            v_count += 1;
+pub fn min_changes(nums: Vec<i32>, k: i32) -> i32 {
+    let n = nums.len();
+    let mut freq = vec![0; 1 + k as usize];
+    for left in 0..n / 2 {
+        let right = n - left - 1;
+        let v = nums[left].abs_diff(nums[right]);
+        freq[v as usize] += 1;
+    }
+    let [mut maxf1, mut maxf2] = [0, 0];
+    let [mut x1, mut x2] = [0, 0];
+    for (x, &f) in freq.iter().enumerate() {
+        if f > maxf1 {
+            (maxf2, x2) = (maxf1, x1);
+            (maxf1, x1) = (f, x);
+        } else if f > maxf2 {
+            (maxf2, x2) = (f, x);
         }
     }
-    while let Some(h) = horizontal_cut.pop() {
-        res += i64::from(h * v_count);
-    }
-    while let Some(v) = vertical_cut.pop() {
-        res += i64::from(v * h_count);
+    solve(&nums, k, x1 as i32).min(solve(&nums, k, x2 as i32))
+}
+
+fn solve(nums: &[i32], k: i32, x: i32) -> i32 {
+    let n = nums.len();
+    let mut res = 0;
+    for left in 0..n / 2 {
+        let right = n - left - 1;
+        let a = nums[left].min(nums[right]);
+        let b = nums[left].max(nums[right]);
+        match (b - a).cmp(&x) {
+            std::cmp::Ordering::Less => {
+                if (0..=k).contains(&(a + x)) || (0..=k).contains(&(b - x)) {
+                    res += 1;
+                } else {
+                    res += 2;
+                }
+            }
+            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Greater => res += 1,
+        }
     }
     res
 }
@@ -68,10 +80,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimum_cost(3, 2, vec![1, 3], vec![5]), 13);
-        assert_eq!(minimum_cost(2, 2, vec![7], vec![4]), 15);
+        assert_eq!(min_changes(vec![0, 1, 2, 3, 3, 6, 5, 4], 6), 2);
+        assert_eq!(min_changes(vec![1, 0, 1, 2, 4, 3], 4), 2);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            min_changes(vec![3, 1, 7, 7, 8, 7, 0, 5, 8, 0, 6, 7, 0, 2, 6, 6], 8),
+            6
+        );
+    }
 }
