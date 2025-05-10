b3a5b73f016a327a04edf5dc8cee10bd2aae0f13
Zx Wang <ZW_08@outlook.com>
2025-05-10 09:43:19 -0400

    3218 greedy+dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,17 +5,76 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
 
-pub fn get_smallest_string(s: String) -> String {
-    let mut s = s.into_bytes();
-    let n = s.len();
-    for i in 0..n - 1 {
-        if s[i] & 1 == s[1 + i] & 1 && s[i] > s[1 + i] {
-            s.swap(i, 1 + i);
-            break;
-        }
+pub fn minimum_cost(m: i32, n: i32, horizontal_cut: &[i32], vertical_cut: &[i32]) -> i32 {
+    let [hors, vers] = [&horizontal_cut, &vertical_cut].map(|v| {
+        v.iter()
+            .copied()
+            .enumerate()
+            .sorted_unstable_by_key(|&(_i, v)| v)
+            .map(|(i, v)| (1 + i, v))
+            .collect_vec()
+    });
+    let ymax = m as usize;
+    let xmax = n as usize;
+    let mut memo = vec![vec![vec![vec![-1; 1 + xmax]; 1 + xmax]; 1 + ymax]; 1 + ymax];
+    dfs([0, ymax], [0, xmax], hors, vers, &mut memo)
+}
+
+fn dfs(
+    ys: [usize; 2],
+    xs: [usize; 2],
+    hors: Vec<(usize, i32)>,
+    vers: Vec<(usize, i32)>,
+    memo: &mut [Vec<Vec<Vec<i32>>>],
+) -> i32 {
+    if memo[ys[0]][ys[1]][xs[0]][xs[1]] > -1 {
+        return memo[ys[0]][ys[1]][xs[0]][xs[1]];
     }
-    String::from_utf8(s).unwrap()
+    let res = if ys[1] - ys[0] == 1 {
+        if xs[1] - xs[0] == 1 {
+            return 0;
+        }
+        let (pos, cost, v1, v2) = cut(xs, vers);
+        cost + dfs(ys, [xs[0], pos], hors.clone(), v1, memo) + dfs(ys, [pos, xs[1]], hors, v2, memo)
+    } else if xs[1] - xs[0] == 1 {
+        let (pos, cost, v1, v2) = cut(ys, hors);
+        cost + dfs([ys[0], pos], xs, v1, vers.clone(), memo) + dfs([pos, ys[1]], xs, v2, vers, memo)
+    } else {
+        let (pos1, cost1, v1, v2) = cut(xs, vers.clone());
+        let curr = cost1
+            + dfs(ys, [xs[0], pos1], hors.clone(), v1, memo)
+            + dfs(ys, [pos1, xs[1]], hors.clone(), v2, memo);
+        let (pos2, cost2, v11, v22) = cut(ys, hors);
+        curr.min(
+            cost2
+                + dfs([ys[0], pos2], xs, v11, vers.clone(), memo)
+                + dfs([pos2, ys[1]], xs, v22, vers, memo),
+        )
+    };
+    memo[ys[0]][ys[1]][xs[0]][xs[1]] = res;
+    res
+}
+
+fn cut(
+    [c1, c2]: [usize; 2],
+    mut arr: Vec<(usize, i32)>,
+) -> (usize, i32, Vec<(usize, i32)>, Vec<(usize, i32)>) {
+    let Some((pos, cost)) = arr.pop() else {
+        unreachable!()
+    };
+    let v1 = arr
+        .iter()
+        .copied()
+        .filter(|(c, _)| (c1..pos).contains(c))
+        .collect_vec();
+    let v2 = arr
+        .iter()
+        .copied()
+        .filter(|(c, _)| (pos..c2).contains(c))
+        .collect_vec();
+    (pos, cost, v1, v2)
 }
 
 #[cfg(test)]
@@ -48,7 +107,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(minimum_cost(3, 2, &[1, 3], &[5]), 13);
+        assert_eq!(minimum_cost(2, 2, &[7], &[4]), 15);
+    }
 
     #[test]
     fn test() {}
