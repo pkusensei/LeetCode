c924955f6195c0beca0c996d52cf11cbd8461961
pkusensei <ZW_08@outlook.com>
2025-05-12 08:19:48 -0400

    3229 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,20 +6,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_operations(s: String) -> i32 {
+pub fn minimum_operations(nums: &[i32], target: &[i32]) -> i64 {
     let mut res = 0;
-    let mut one = 0;
-    let mut fresh = false;
-    for b in s.bytes() {
-        if b == b'1' {
-            one += 1;
-            fresh = true;
-        } else {
-            if fresh {
-                res += one;
-            }
-            fresh = false
+    let mut prev_sign = 0;
+    let mut prev_abs = 0;
+    for delta in nums
+        .iter()
+        .zip(target.iter())
+        .map(|(a, b)| i64::from(a - b))
+    {
+        if prev_sign * delta < 0 {
+            prev_abs = 0; // end greedy streak
         }
+        res += (delta.abs() - prev_abs).max(0);
+        prev_abs = delta.abs();
+        prev_sign = delta.signum();
     }
     res
 }
@@ -54,7 +55,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(minimum_operations(&[3, 5, 1, 2], &[4, 6, 2, 4]), 2);
+        assert_eq!(minimum_operations(&[1, 3, 2], &[2, 1, 4]), 5);
+    }
 
     #[test]
     fn test() {}
