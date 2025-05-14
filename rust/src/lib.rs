d8e7f117ef06cfd8174cda110f85cb2cb3eda011
pkusensei <ZW_08@outlook.com>
2025-05-14 09:17:50 -0400

    3337 matrix exponentiation

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,36 +6,63 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn shortest_distance_after_queries(n: i32, queries: &[[i32; 2]]) -> Vec<i32> {
-    use std::collections::BTreeMap;
-    let mut dist = n - 1;
-    let mut map = BTreeMap::<i32, i32>::new();
-    let mut res = vec![];
-    for q in queries.iter() {
-        let [a, b] = q[..] else {
-            continue;
-        };
-        if map.range(..=a).next_back().is_some_and(|(_k, &v)| b <= v) {
-            // [1..4] in map
-            // [2..3] is covered
-            res.push(dist);
-            continue;
+pub fn length_after_transformations(s: &str, t: i32, nums: &[i32]) -> i32 {
+    let freq = s.bytes().fold([0; 26], |mut acc, b| {
+        acc[usize::from(b - b'a')] += 1;
+        acc
+    });
+    let mut mat = Mat::default();
+    for (idx, &num) in nums.iter().enumerate() {
+        for val in 1..=num {
+            mat[(idx + val as usize) % N][idx] += 1; // mat[to][from]
         }
-        // [2..3] in map
-        // [1..4] is being added
-        let del: Vec<_> = map
-            .range(a..)
-            .filter_map(|(&k, &v)| if v <= b { Some(k) } else { None })
-            .collect();
-        for k in del {
-            let span = map.remove(&k).map(|v| v - k).unwrap_or(0);
-            dist += span - 1;
+    }
+    mat = mat_pow(&mat, t);
+    let mut res = [0; N];
+    for i in 0..N {
+        for j in 0..N {
+            res[i] += mat[i][j] * freq[j] % M;
+            res[i] %= M;
         }
-        map.insert(a, b);
-        dist += 1 - (b - a);
-        res.push(dist);
+    }
+    res.iter().fold(0, |acc, v| (acc + v) % M) as i32
+}
+
+const N: usize = 26;
+const M: i64 = 1_000_000_007;
+type Mat = [[i64; N]; N];
+
+fn mat_mul(a: &Mat, b: &Mat) -> Mat {
+    let mut res = Mat::default();
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
+                res[i][j] += a[i][k] * b[k][j] % M;
+                res[i][j] %= M;
+            }
+        }
     }
     res
+}
+
+fn mat_pow(mat: &Mat, power: i32) -> Mat {
+    let mut res = Mat::default();
+    for i in 0..N {
+        res[i][i] = 1; // identity matrix
+    }
+    if power == 0 {
+        return res;
+    }
+    if power == 1 {
+        return *mat;
+    }
+    let half = mat_pow(mat, power >> 1);
+    let squared = mat_mul(&half, &half);
+    if power & 1 == 0 {
+        squared
+    } else {
+        mat_mul(&squared, mat)
+    }
 }
 
 #[cfg(test)]
@@ -70,12 +97,24 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            shortest_distance_after_queries(5, &[[2, 4], [0, 2], [0, 4]]),
-            [3, 2, 1]
+            length_after_transformations(
+                "abcyy",
+                2,
+                &[
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2
+                ]
+            ),
+            7
         );
         assert_eq!(
-            shortest_distance_after_queries(4, &[[0, 3], [0, 2]]),
-            [1, 1]
+            length_after_transformations(
+                "azbk",
+                1,
+                &[
+                    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+                ]
+            ),
+            8
         );
     }
 
