2aad81c121b7431397d7760ac00c9eeadae111b9
pkusensei <ZW_08@outlook.com>
2025-05-14 20:40:55 -0400

    3251 dp with prefix sum optimization

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,8 +7,33 @@ mod trie;
 use helper::*;
 
 pub fn count_of_pairs(nums: Vec<i32>) -> i32 {
+    const M: i32 = 1_000_000_007;
     let n = nums.len();
-    dfs(&nums, 0, 0, i32::MAX, &mut vec![[-1; 51]; n])
+    let max = *nums.iter().max().unwrap_or(&0);
+    let mut dp = vec![1; 1 + max as usize];
+    // arr2[i-1]>=arr2[i]
+    // nums[i-1]-arr1[i-1] >= nums[i]-arr1[i]
+    // arr1[i-1] + nums[i]-nums[i-1] <= arr1[i]
+    // arr1[i-1] + d <= arr1[i]
+    // Everything smaller than val1-d can reach val1
+    // curr[val1] = dp[val1-d] + dp[val1-d-1]+..+dp[0]
+    // By extension, val1-1 can be reached by...
+    // curr[val1-1] = dp[val1-d-1] +..+dp[0]
+    // curr[val1]-curr[val-1]=dp[val1-d]
+    for idx in 1..n {
+        let mut curr = vec![0; 1 + max as usize];
+        let diff = (nums[idx] - nums[idx - 1]).max(0);
+        for val1 in diff..=nums[idx] {
+            curr[val1 as usize] =
+                if val1 > 0 { curr[val1 as usize - 1] } else { 0 } + dp[(val1 - diff) as usize];
+            curr[val1 as usize] %= M;
+        }
+        dp = curr;
+    }
+    dp[..=nums[n - 1] as usize]
+        .iter()
+        .fold(0, |acc, v| (acc + v) % M)
+    // dfs(&nums, 0, 0, i32::MAX, &mut vec![[-1; 51]; n])
 }
 
 fn dfs(nums: &[i32], idx: usize, prev1: i32, prev2: i32, memo: &mut [[i32; 51]]) -> i32 {
