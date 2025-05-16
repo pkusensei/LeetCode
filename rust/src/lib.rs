4faa72c685b1bc81c8e5131542fdbbff3e9de41e
Zx Wang <ZW_08@outlook.com>
2025-05-16 10:36:06 -0400

    3265 2sum; padding zeros

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,42 +3,41 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::{HashMap, HashSet};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_k_constraint_substrings(s: &str, k: i32, queries: &[[i32; 2]]) -> Vec<i64> {
-    let (s, n) = (s.as_bytes(), s.len());
-    let mut left = 0;
-    let mut count = [0, 0];
-    let mut left_right = vec![n; n];
-    let mut prefix = vec![];
-    for (right, &b) in s.iter().enumerate() {
-        count[usize::from(b - b'0')] += 1;
-        while count[0] > k && count[1] > k {
-            // first invalid idx for left
-            // i.e half open [left, right)
-            left_right[left] = right;
-            count[usize::from(s[left] - b'0')] -= 1;
-            left += 1;
+pub fn count_pairs(nums: Vec<i32>) -> i32 {
+    let mut res = 0;
+    let mut map = HashMap::new();
+    let max_len = nums.iter().map(|v| 1 + v.ilog10()).max().unwrap_or(0) as usize;
+    for &num in nums.iter() {
+        for val in one_swap(num, max_len) {
+            res += map.get(&val).unwrap_or(&0)
         }
-        // right-left+1 = count of valid substring
-        // prefix[right+1] = prefix sum of all substrs up until right
-        prefix.push(right - left + 1 + prefix.last().unwrap_or(&0));
+        *map.entry(num).or_insert(0) += 1;
     }
-    let mut res = vec![];
-    for q in queries.iter() {
-        let [left, right] = [0, 1].map(|i| q[i] as usize);
-        let valid_from_left = (1 + right).min(left_right[left]);
-        // All substrs in [left, valid_from_left)
-        let left_count = (1 + valid_from_left - left) * (valid_from_left - left) / 2;
-        // All substrs in [right_end, right]
-        let right_count = prefix[right]
-            - if valid_from_left > 0 {
-                prefix[valid_from_left - 1]
-            } else {
-                0
-            };
-        res.push((left_count + right_count) as i64);
+    res
+}
+
+fn one_swap(mut num: i32, n: usize) -> HashSet<i32> {
+    let mut res = HashSet::from([num]);
+    let mut ds = vec![];
+    while num > 0 {
+        ds.push(num % 10);
+        num /= 10;
+    }
+    while ds.len() < n {
+        ds.push(0);
+    }
+    ds.reverse();
+    for a in 0..n {
+        for b in 1 + a..n {
+            ds.swap(a, b);
+            res.insert(ds.iter().fold(0, |acc, v| acc * 10 + v));
+            ds.swap(a, b);
+        }
     }
     res
 }
@@ -73,13 +72,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_k_constraint_substrings("0001111", 2, &[[0, 6]]), [26]);
-        assert_eq!(
-            count_k_constraint_substrings("010101", 1, &[[0, 5], [1, 4], [2, 3]]),
-            [15, 9, 3]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
