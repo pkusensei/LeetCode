5b35fb4789762e54bbdaa315c832b7eec49bb461
Zx Wang <ZW_08@outlook.com>
2025-05-16 09:55:23 -0400

    3261 prefix sum on sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,72 +5,42 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::chain;
-use std::iter::{once, repeat_n};
 
-pub fn largest_palindrome(n: i32, k: i32) -> String {
-    let n = n as usize;
-    match k {
-        1 | 3 | 9 => repeat_n('9', n).collect(),
-        5 => build('5', n, 2),
-        2 => build('8', n, 2),
-        4 => build('8', n, 4),
-        8 => build('8', n, 6),
-        6 => {
-            if n <= 2 {
-                repeat_n('6', n).collect()
-            } else if n & 1 == 1 {
-                let block = n / 2 - 1;
-                chain!(
-                    once('8'),
-                    repeat_n('9', block),
-                    once('8'),
-                    repeat_n('9', block),
-                    once('8')
-                )
-                .collect()
-            } else {
-                let block = n / 2 - 2;
-                chain!(
-                    once('8'),
-                    repeat_n('9', block),
-                    repeat_n('7', 2),
-                    repeat_n('9', block),
-                    once('8')
-                )
-                .collect()
+pub fn count_k_constraint_substrings(s: &str, k: i32, queries: &[[i32; 2]]) -> Vec<i64> {
+        let (s, n) = (s.as_bytes(), s.len());
+        let mut left = 0;
+        let mut count = [0, 0];
+        let mut left_right = vec![n; n];
+        let mut prefix = vec![];
+        for (right, &b) in s.iter().enumerate() {
+            count[usize::from(b - b'0')] += 1;
+            while count[0] > k && count[1] > k {
+                // first invalid idx for left
+                // i.e half open [left, right)
+                left_right[left] = right;
+                count[usize::from(s[left] - b'0')] -= 1;
+                left += 1;
             }
+            // right-left+1 = count of valid substring
+            // prefix[right+1] = prefix sum of all substrs up until right
+            prefix.push(right - left + 1 + prefix.last().unwrap_or(&0));
         }
-        _ => {
-            // dreaded 7
-            if n <= 2 {
-                repeat_n('7', n).collect()
-            } else {
-                const M: [&str; 12] =
-                    ["5", "77", "7", "", "4", "44", "6", "44", "4", "", "7", "77"];
-                let mid = M[(n + 9) % 12]; // (n-3)%12
-                let len = n.saturating_sub(mid.len()) / 2;
-                let mut res = String::with_capacity(n);
-                res.extend(repeat_n('9', len));
-                res.push_str(mid);
-                res.extend(repeat_n('9', len));
-                res
-            }
+        let mut res = vec![];
+        for q in queries.iter() {
+            let [left, right] = [0, 1].map(|i| q[i] as usize);
+            let valid_from_left = (1 + right).min(left_right[left]);
+            // All substrs in [left, valid_from_left)
+            let left_count = (1 + valid_from_left - left) * (valid_from_left - left) / 2;
+            // All substrs in [right_end, right]
+            let right_count = prefix[right]
+                - if valid_from_left > 0 {
+                    prefix[valid_from_left - 1]
+                } else {
+                    0
+                };
+            res.push((left_count + right_count) as i64);
         }
-    }
-}
-
-fn build(d: char, n: usize, min_len: usize) -> String {
-    if n <= min_len {
-        repeat_n(d, n).collect()
-    } else {
-        chain!(
-            repeat_n(d, min_len / 2),
-            repeat_n('9', n - min_len),
-            repeat_n(d, min_len / 2)
-        )
-        .collect()
-    }
+        res
 }
 
 #[cfg(test)]
@@ -104,7 +74,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(largest_palindrome(5, 6), "89898");
+        assert_eq!(count_k_constraint_substrings("0001111", 2, &[[0, 6]]), [26]);
+        assert_eq!(
+            count_k_constraint_substrings("010101", 1, &[[0, 5], [1, 4], [2, 3]]),
+            [15, 9, 3]
+        );
     }
 
     #[test]
