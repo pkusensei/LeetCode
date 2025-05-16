cce98931261626d0180592d95e790fd52affc81b
pkusensei <ZW_08@outlook.com>
2025-05-16 17:38:48 -0400

    3271 one-liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,25 +6,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
-    let [a, b, c] = [num1, num2, num3].map(f);
-    let mut res = 0;
-    for pow in 0..a.len().min(b.len()).min(c.len()) {
-        let ad = *a.get(pow).unwrap_or(&9);
-        let bd = *b.get(pow).unwrap_or(&9);
-        let cd = *c.get(pow).unwrap_or(&9);
-        res += ad.min(bd).min(cd) * 10i32.pow(pow as _);
-    }
-    res
-}
-
-fn f(mut num: i32) -> Vec<i32> {
-    let mut ds = Vec::with_capacity(4);
-    while num > 0 {
-        ds.push(num % 10);
-        num /= 10;
-    }
-    ds
+pub fn string_hash(s: String, k: i32) -> String {
+    s.as_bytes()
+        .chunks(k as usize)
+        .map(|ch| {
+            let sum: u32 = ch.iter().map(|b| u32::from(b - b'a')).sum();
+            char::from((sum % 26) as u8 + b'a')
+        })
+        .collect()
 }
 
 #[cfg(test)]
