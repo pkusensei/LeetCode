0cd4637c1315a87cf16afd808dffdaee4baf323d
pkusensei <ZW_08@outlook.com>
2025-05-17 16:33:30 -0400

    3280

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,27 +6,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_subarray_xor(nums: &[i32], queries: &[[i32; 2]]) -> Vec<i32> {
-    let n = nums.len();
-    let mut memo = vec![vec![None; n]; n];
-    queries
-        .iter()
-        .map(|q| dfs(nums, q[0] as usize, q[1] as usize, &mut memo)[1])
-        .collect()
-}
-
-fn dfs(nums: &[i32], left: usize, right: usize, memo: &mut [Vec<Option<[i32; 2]>>]) -> [i32; 2] {
-    if left == right {
-        return [nums[left]; 2];
-    }
-    if let Some(v) = memo[left][right] {
-        return v;
-    }
-    let [xor1, max1] = dfs(nums, left, right - 1, memo);
-    let [xor2, max2] = dfs(nums, 1 + left, right, memo);
-    let res = [xor1 ^ xor2, (xor1 ^ xor2).max(max1).max(max2)];
-    memo[left][right] = Some(res);
-    res
+pub fn convert_date_to_binary(date: String) -> String {
+    date.split_terminator('-')
+        .map(|s| {
+            let v: i32 = s.parse().unwrap();
+            format!("{:b}", v)
+        })
+        .collect::<Vec<_>>()
+        .join("-")
 }
 
 #[cfg(test)]
@@ -59,19 +46,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            maximum_subarray_xor(&[2, 8, 4, 32, 16, 1], &[[0, 2], [1, 4], [0, 5]]),
-            [12, 60, 60]
-        );
-        assert_eq!(
-            maximum_subarray_xor(
-                &[0, 7, 3, 2, 8, 5, 1],
-                &[[0, 3], [1, 5], [2, 4], [2, 6], [5, 6]]
-            ),
-            [7, 14, 11, 14, 5]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
