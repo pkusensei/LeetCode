ad47bec484eeeac14718e7a1928f598e4cd19b35
pkusensei <ZW_08@outlook.com>
2025-05-18 09:00:15 -0400

    3280 binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,14 +6,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn convert_date_to_binary(date: String) -> String {
-    date.split_terminator('-')
-        .map(|s| {
-            let v: i32 = s.parse().unwrap();
-            format!("{:b}", v)
-        })
-        .collect::<Vec<_>>()
-        .join("-")
+pub fn max_possible_score(mut start: Vec<i32>, d: i32) -> i32 {
+    start.sort_unstable();
+    let mut left = 0;
+    let mut right = i32::MAX - 1;
+    while left < right {
+        let mid = left + (right - left + 1) / 2;
+        if chech(&start, d, mid) {
+            left = mid
+        } else {
+            right = mid - 1
+        }
+    }
+    left
+}
+
+fn chech(nums: &[i32], d: i32, mid: i32) -> bool {
+    let mut prev = nums[0];
+    for &num in &nums[1..] {
+        let curr = (prev + mid).max(num);
+        if curr > num + d {
+            return false;
+        }
+        prev = curr;
+    }
+    true
 }
 
 #[cfg(test)]
@@ -46,8 +63,16 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_possible_score(vec![2, 6, 13, 13], 5), 5);
+        assert_eq!(max_possible_score(vec![6, 0, 3], 2), 4);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            max_possible_score(vec![1000000000, 0], 1000000000),
+            2000000000
+        );
+    }
 }
