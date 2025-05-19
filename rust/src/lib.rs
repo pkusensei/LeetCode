9f9e274a738a11941bb3b5323255e3a6efbfb88e
pkusensei <ZW_08@outlook.com>
2025-05-19 10:21:27 -0400

    3288 two lis

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,46 +6,34 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_value(mut nums: Vec<i32>, k: i32) -> i32 {
-    let n = nums.len();
-    let k = k as usize;
-    let [mut memo1, mut memo2] = [0, 1].map(|_| vec![vec![[None; 128]; 1 + k]; n]);
-    dfs(&nums, k, 0, 0, 0, &mut memo1);
-    nums.reverse();
-    dfs(&nums, k, 0, 0, 0, &mut memo2);
-    let mut res = 0;
-    for idx in k..=n - k {
-        for or1 in 0..128 {
-            for or2 in 0..128 {
-                if let Some((true, true)) = memo1[idx][k][or1].zip(memo2[n - idx][k][or2]) {
-                    res = res.max(or1 ^ or2);
-                }
-            }
+pub fn max_path_length(coordinates: &[[i32; 2]], k: i32) -> i32 {
+    let [kx, ky] = [0, 1].map(|i| coordinates[k as usize][i]);
+    let [mut left, mut right] = [vec![], vec![]];
+    for c in coordinates.iter() {
+        let [x, y] = c[..] else { unreachable!() };
+        if x < kx && y < ky {
+            left.push([x, y]);
+        } else if x > kx && y > ky {
+            right.push([x, y]);
         }
     }
-    res as i32
+    let func = |a: &[i32; 2], b: &[i32; 2]| a[0].cmp(&b[0]).then(b[1].cmp(&a[1]));
+    left.sort_unstable_by(func);
+    right.sort_unstable_by(func);
+    (lis(&left) + lis(&right) + 1) as i32
 }
 
-fn dfs(
-    nums: &[i32],
-    k: usize,
-    idx: usize,
-    len: usize,
-    orv: usize,
-    memo: &mut [Vec<[Option<bool>; 128]>],
-) -> bool {
-    if idx >= nums.len() {
-        return len == k;
+fn lis(nums: &[[i32; 2]]) -> usize {
+    let mut arr = vec![];
+    for &[_, y] in nums {
+        let i = arr.partition_point(|&v| v < y);
+        if i == arr.len() {
+            arr.push(y);
+        } else {
+            arr[i] = y; // useless?
+        }
     }
-    if let Some(v) = memo[idx][len][orv] {
-        return v;
-    }
-    let mut res = dfs(nums, k, 1 + idx, len, orv, memo); // skip
-    if len < k {
-        res |= dfs(nums, k, 1 + idx, 1 + len, orv | nums[idx] as usize, memo);
-    }
-    memo[idx][len][orv] = Some(res);
-    res
+    arr.len()
 }
 
 #[cfg(test)]
@@ -79,10 +67,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_value(vec![2, 6, 7], 1), 5);
-        assert_eq!(max_value(vec![4, 2, 5, 6, 7], 2), 2);
+        assert_eq!(
+            max_path_length(&[[3, 1], [2, 2], [4, 1], [0, 0], [5, 3]], 1),
+            3
+        );
+        assert_eq!(max_path_length(&[[2, 1], [7, 0], [5, 6]], 2), 2);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(max_path_length(&[[2, 1], [5, 4], [9, 8]], 0), 3);
+    }
 }
