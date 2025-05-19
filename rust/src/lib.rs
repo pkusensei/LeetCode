8b829e91252ba1d352a97a00429fd4d3c10f4f01
pkusensei <ZW_08@outlook.com>
2025-05-19 10:46:09 -0400

    3290 dfs/memo pick-unpick

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,15 +6,25 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
-    let mut mask = 0_i128;
-    let mut res = Vec::with_capacity(2);
-    for num in nums {
-        if mask & (1 << num) > 0 {
-            res.push(num);
-        }
-        mask |= 1 << num;
+pub fn max_score(a: &[i32], b: &[i32]) -> i64 {
+    let n = b.len();
+    dfs(a, b, 0, 0, &mut vec![vec![None; n]; 4])
+}
+
+fn dfs(a: &[i32], b: &[i32], i1: usize, i2: usize, memo: &mut [Vec<Option<i64>>]) -> i64 {
+    if i1 >= a.len() {
+        return 0;
     }
+    if i2 >= b.len() {
+        return i64::MIN / 2;
+    }
+    if let Some(v) = memo[i1][i2] {
+        return v;
+    }
+    let take = i64::from(a[i1]) * i64::from(b[i2]) + dfs(a, b, 1 + i1, 1 + i2, memo);
+    let skip = dfs(a, b, i1, 1 + i2, memo);
+    let res = take.max(skip);
+    memo[i1][i2] = Some(res);
     res
 }
 
@@ -48,7 +58,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_score(&[3, 2, 5, 6], &[2, -6, 4, -5, -3, 2, -7]), 26);
+        assert_eq!(max_score(&[-1, 4, 5, -2], &[-5, -1, -3, -2, -4]), -1);
+    }
 
     #[test]
     fn test() {}
