3f001c50d8c0e82e22fa69e920d9e9c21ec605eb
pkusensei <ZW_08@outlook.com>
2025-05-20 20:41:45 -0400

    73 set zeros in place

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,15 +7,52 @@ namespace Solution;
 
 public class Solution
 {
-    public string TriangleType(int[] nums)
+    public void SetZeroes(int[][] matrix)
     {
-        Array.Sort(nums);
-        if (nums[0] + nums[1] <= nums[2]) { return "none"; }
-        return nums.Distinct().Count() switch
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        bool first_row = false;
+        bool first_col = false;
+        for (int r = 0; r < rows; r++)
         {
-            1 => "equilateral",
-            2 => "isosceles",
-            _ => "scalene",
-        };
+            for (int c = 0; c < cols; c++)
+            {
+                if (matrix[r][c] == 0)
+                {
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
+                    first_row |= r == 0;
+                    first_col |= c == 0;
+                }
+            }
+        }
+        for (int c = 1; c < cols; c++)
+        {
+            if (matrix[0][c] == 0)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        for (int r = 1; r < rows; r++)
+        {
+            if (matrix[r][0] == 0)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        if (first_row) { Array.Fill(matrix[0], 0); }
+        if (first_col)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                matrix[r][0] = 0;
+            }
+        }
     }
 }
