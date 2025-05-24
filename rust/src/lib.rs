0041ff9432a6ce6f0f240767539821f1f170e92f
pkusensei <ZW_08@outlook.com>
2025-05-24 16:35:07 -0400

    3330

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,67 +3,15 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::iter::once;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_answer(parent: &[i32], s: &str) -> Vec<bool> {
-    let n = parent.len();
-    let adj = parent
-        .iter()
-        .enumerate()
-        .fold(vec![vec![]; n], |mut acc, (i, &v)| {
-            if i > 0 {
-                acc[v as usize].push(i);
-            }
-            acc
-        });
-    let mut traversed = vec![b'#'; 1 + 2 * n];
-    let mut subs = vec![[0, 0]; n];
-    dfs(&adj, s.as_bytes(), &mut traversed, 0, &mut 0, &mut subs);
-    let d = manacher_odd(traversed);
-    let mut res = Vec::with_capacity(n);
-    for [left, right] in subs {
-        let len = right + 1 - left;
-        res.push(d[left + right + 1] - 1 >= len);
-    }
-    res
-}
-
-fn dfs(
-    adj: &[Vec<usize>],
-    s: &[u8],
-    traversed: &mut [u8],
-    node: usize,
-    count: &mut usize,
-    subs: &mut [[usize; 2]],
-) {
-    let left = *count;
-    for &next in &adj[node] {
-        dfs(adj, s, traversed, next, count, subs);
-    }
-    traversed[1 + 2 * (*count)] = s[node];
-    subs[node] = [left, *count];
-    *count += 1;
-}
-
-fn manacher_odd(s: Vec<u8>) -> Vec<usize> {
-    let n = s.len();
-    let s: Vec<u8> = once(b'$').chain(s).chain(once(b'^')).collect();
-    let mut p = vec![0; 2 + n];
-    let [mut left, mut right] = [1_usize, 1];
-    for idx in 1..=n {
-        p[idx] = right.saturating_sub(idx).min(p[left + right - idx]);
-        while s[idx - p[idx]] == s[idx + p[idx]] {
-            p[idx] += 1;
-        }
-        if idx + p[idx] > right {
-            left = idx - p[idx];
-            right = idx + p[idx];
-        }
-    }
-    p[1..n].to_vec()
+pub fn possible_string_count(word: String) -> i32 {
+    word.as_bytes()
+        .chunk_by(|a, b| a == b)
+        .map(|w| w.len() as i32 - 1)
+        .sum::<i32>()
+        + 1
 }
 
 #[cfg(test)]
@@ -96,12 +44,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            find_answer(&[-1, 0, 0, 1, 1, 2], "aababa"),
-            [true, true, false, true, true, true]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
