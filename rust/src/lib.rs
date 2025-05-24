c78e1fff1ad5eb4b83ded7e27683195b8e78dbbd
pkusensei <ZW_08@outlook.com>
2025-05-24 17:37:12 -0400

    3331 dfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,12 +6,56 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn possible_string_count(word: String) -> i32 {
-    word.as_bytes()
-        .chunk_by(|a, b| a == b)
-        .map(|w| w.len() as i32 - 1)
-        .sum::<i32>()
-        + 1
+pub fn find_subtree_sizes(mut parent: Vec<i32>, s: &str) -> Vec<i32> {
+    let n = parent.len();
+    let mut adj = build(&parent);
+    dfs1(&adj, s.as_bytes(), 0, &mut [None; 26], &mut parent);
+    adj = build(&parent);
+    let mut res = vec![0; n];
+    dfs2(&adj, 0, &mut res);
+    res
+}
+
+fn build(parent: &[i32]) -> Vec<Vec<usize>> {
+    let n = parent.len();
+    let adj = parent
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (i, &v)| {
+            if i > 0 {
+                acc[v as usize].push(i);
+            }
+            acc
+        });
+    adj
+}
+
+fn dfs1(
+    adj: &[Vec<usize>],
+    s: &[u8],
+    node: usize,
+    last: &mut [Option<i32>; 26],
+    parent: &mut [i32],
+) {
+    let idx = usize::from(s[node] - b'a');
+    let old = last[idx];
+    if let Some(v) = last[idx] {
+        parent[node] = v
+    }
+    last[idx] = Some(node as i32);
+    for &next in &adj[node] {
+        dfs1(adj, s, next, last, parent);
+    }
+    last[idx] = old;
+}
+
+fn dfs2(adj: &[Vec<usize>], node: usize, res: &mut [i32]) -> i32 {
+    let mut sz = 1;
+    for &next in &adj[node] {
+        sz += dfs2(adj, next, res);
+    }
+    res[node] = sz;
+    sz
 }
 
 #[cfg(test)]
@@ -44,7 +88,12 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            find_subtree_sizes(vec![-1, 0, 4, 0, 1], "abbba"),
+            [5, 2, 1, 1, 1]
+        );
+    }
 
     #[test]
     fn test() {}
