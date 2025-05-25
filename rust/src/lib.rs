b542e3efc815f450ee17805556d6b4dac2c03559
pkusensei <ZW_08@outlook.com>
2025-05-25 15:02:31 -0400

    3334 prefix and suffix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,57 +6,58 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn possible_string_count(word: &str, k: i32) -> i32 {
-    const M: i64 = 1_000_000_007;
-    let mut nums = vec![];
-    let mut prod = 1;
-    for len in word
-        .as_bytes()
-        .chunk_by(|a, b| a == b)
-        .map(|ch| ch.len() as i64)
-    {
-        nums.push(len);
-        prod = prod * len % M;
-    }
-    let (n, k) = (nums.len(), k as usize);
-    if k <= n {
-        return prod as i32;
-    }
-    let mut dp = vec![0; k];
-    dp[0] = 1;
-    for idx in 1..=n {
-        let prefix = dp.into_iter().fold(vec![0], |mut acc, v| {
-            acc.push((v + acc.last().unwrap_or(&0)) % M);
-            acc
-        });
-        dp = vec![0; k];
-        for len in idx..k {
-            let prev = len - (nums[idx - 1] as usize).min(len + 1 - idx);
-            dp[len] = (prefix[len] - prefix[prev]).rem_euclid(M);
+pub fn max_score(nums: Vec<i32>) -> i64 {
+    let n = nums.len();
+    let mut pre_gcd = Vec::with_capacity(n);
+    let mut pre_lcm = Vec::with_capacity(n);
+    let mut gcd_ = i64::from(nums[0]);
+    let mut lcm_ = gcd_;
+    let mut res = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        gcd_ = gcd(num.into(), gcd_);
+        lcm_ = lcm(num.into(), lcm_);
+        pre_gcd.push(gcd_);
+        pre_lcm.push(lcm_);
+        if idx == n - 2 {
+            res = res.max(gcd_ * lcm_);
         }
     }
-    let less_than_k = dp.iter().fold(0, |acc, v| (acc + v) % M);
-    (prod - less_than_k).rem_euclid(M) as i32
+    res = res.max(lcm_ * gcd_);
+    let mut suf_gcd = Vec::with_capacity(n);
+    let mut suf_lcm = Vec::with_capacity(n);
+    gcd_ = i64::from(nums[n - 1]);
+    lcm_ = gcd_;
+    for (idx, &num) in nums.iter().rev().enumerate() {
+        gcd_ = gcd(num.into(), gcd_);
+        lcm_ = lcm(num.into(), lcm_);
+        suf_gcd.push(gcd_);
+        suf_lcm.push(lcm_);
+        if idx == n - 2 {
+            res = res.max(gcd_ * lcm_);
+        }
+    }
+    suf_gcd.reverse();
+    suf_lcm.reverse();
+    res = res.max(lcm_ * gcd_);
+    for idx in 1..n - 1 {
+        let left_gcd = pre_gcd[idx - 1];
+        let left_lcm = pre_lcm[idx - 1];
+        let right_gcd = suf_gcd[1 + idx];
+        let right_lcm = suf_lcm[1 + idx];
+        gcd_ = gcd(left_gcd, right_gcd);
+        lcm_ = lcm(left_lcm, right_lcm);
+        res = res.max(lcm_ * gcd_);
+    }
+    res
 }
 
-// fn dfs(nums: &[usize], k: usize, idx: usize, memo: &mut [Vec<Option<usize>>]) -> usize {
-//     if k == 0 {
-//         return nums[idx..].iter().fold(1, |acc, v| acc * v % M);
-//     }
-//     if idx >= nums.len() {
-//         return 0;
-//     }
-//     if let Some(v) = memo[idx][k] {
-//         return v;
-//     }
-//     let mut res = 0;
-//     for curr in 1..=nums[idx] {
-//         res += dfs(nums, k.saturating_sub(curr), 1 + idx, memo);
-//         res %= M
-//     }
-//     memo[idx][k] = Some(res);
-//     res
-// }
+const fn gcd(a: i64, b: i64) -> i64 {
+    if a == 0 { b } else { gcd(b % a, a) }
+}
+
+const fn lcm(a: i64, b: i64) -> i64 {
+    a / gcd(a, b) * b
+}
 
 #[cfg(test)]
 mod tests {
@@ -88,11 +89,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(possible_string_count("aabbccdd", 7), 5);
-        assert_eq!(possible_string_count("aabbccdd", 8), 1);
-        assert_eq!(possible_string_count("aaabbb", 3), 8);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
