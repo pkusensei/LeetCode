7c9ef2aba5b5b1564a8ae5edf19bc38f552554bc
pkusensei <ZW_08@outlook.com>
2025-05-25 16:52:12 -0400

    3336 dfs/memo; knapsack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,60 +3,54 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_score(nums: Vec<i32>) -> i64 {
-    let n = nums.len();
-    let mut pre_gcd = Vec::with_capacity(n);
-    let mut pre_lcm = Vec::with_capacity(n);
-    let mut gcd_ = i64::from(nums[0]);
-    let mut lcm_ = gcd_;
-    let mut res = 0;
-    for (idx, &num) in nums.iter().enumerate() {
-        gcd_ = gcd(num.into(), gcd_);
-        lcm_ = lcm(num.into(), lcm_);
-        pre_gcd.push(gcd_);
-        pre_lcm.push(lcm_);
-        if idx == n - 2 {
-            res = res.max(gcd_ * lcm_);
-        }
+pub fn subsequence_pair_count(nums: &[i32]) -> i32 {
+    dfs(nums, 0, 0, 0, &mut HashMap::new())
+}
+
+fn dfs(
+    nums: &[i32],
+    idx: usize,
+    gcd1: i32,
+    gcd2: i32,
+    memo: &mut HashMap<(usize, i32, i32), i32>,
+) -> i32 {
+    const M: i32 = 1_000_000_007;
+    if idx >= nums.len() {
+        return i32::from(gcd1 > 0 && gcd1 == gcd2);
     }
-    res = res.max(lcm_ * gcd_);
-    let mut suf_gcd = Vec::with_capacity(n);
-    let mut suf_lcm = Vec::with_capacity(n);
-    gcd_ = i64::from(nums[n - 1]);
-    lcm_ = gcd_;
-    for (idx, &num) in nums.iter().rev().enumerate() {
-        gcd_ = gcd(num.into(), gcd_);
-        lcm_ = lcm(num.into(), lcm_);
-        suf_gcd.push(gcd_);
-        suf_lcm.push(lcm_);
-        if idx == n - 2 {
-            res = res.max(gcd_ * lcm_);
-        }
+    let k = (idx, gcd1, gcd2);
+    if let Some(&v) = memo.get(&k) {
+        return v;
     }
-    suf_gcd.reverse();
-    suf_lcm.reverse();
-    res = res.max(lcm_ * gcd_);
-    for idx in 1..n - 1 {
-        let left_gcd = pre_gcd[idx - 1];
-        let left_lcm = pre_lcm[idx - 1];
-        let right_gcd = suf_gcd[1 + idx];
-        let right_lcm = suf_lcm[1 + idx];
-        gcd_ = gcd(left_gcd, right_gcd);
-        lcm_ = lcm(left_lcm, right_lcm);
-        res = res.max(lcm_ * gcd_);
-    }
+    let val = nums[idx];
+    let mut res = (dfs(nums, 1 + idx, gcd1, gcd2, memo)
+        + dfs(
+            nums,
+            1 + idx,
+            if gcd1 > 0 { gcd(gcd1, val) } else { val },
+            gcd2,
+            memo,
+        ))
+        % M
+        + dfs(
+            nums,
+            1 + idx,
+            gcd1,
+            if gcd2 > 0 { gcd(gcd2, val) } else { val },
+            memo,
+        );
+    res %= M;
+    memo.insert(k, res);
     res
 }
 
-const fn gcd(a: i64, b: i64) -> i64 {
+const fn gcd(a: i32, b: i32) -> i32 {
     if a == 0 { b } else { gcd(b % a, a) }
-}
-
-const fn lcm(a: i64, b: i64) -> i64 {
-    a / gcd(a, b) * b
 }
 
 #[cfg(test)]
@@ -89,7 +83,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(subsequence_pair_count(&[1, 2, 3, 4]), 10);
+        assert_eq!(subsequence_pair_count(&[10, 20, 30]), 2);
+    }
 
     #[test]
     fn test() {}
