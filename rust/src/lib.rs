152e8f1de0535d9fd3f3fe1248d2c332914cf445
pkusensei <ZW_08@outlook.com>
2025-05-28 18:45:01 -0400

    3371 num = (sum without num)/2

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,37 +6,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximize_sum_of_weights(edges: &[[i32; 3]], k: i32) -> i64 {
-    let n = 1 + edges.len();
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push((b, i64::from(e[2])));
-        acc[b].push((a, i64::from(e[2])));
-        acc
-    });
-    dfs(&adj, k as usize, 0, n)[1]
-}
-
-// sum with [at most k-1 children, at most k]
-fn dfs(adj: &[Vec<(usize, i64)>], k: usize, node: usize, prev: usize) -> [i64; 2] {
-    let mut res = 0;
-    let mut diff = vec![];
-    for &(next, w) in &adj[node] {
-        if next == prev {
+pub fn get_largest_outlier(nums: Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+    let mut freq = HashMap::new();
+    let mut sum = 0;
+    for &num in &nums {
+        *freq.entry(num).or_insert(0) += 1;
+        sum += num;
+    }
+    let mut res = i32::MIN;
+    for &num in &nums {
+        let curr = sum - num;
+        if curr & 1 == 1 {
             continue;
         }
-        let [a, b] = dfs(adj, k, next, node);
-        res += b; // Assume subtree with k children -- no connection
-        // For each child, what if current `w` is picked?
-        // i.e try find a "upgrade" with this edge
-        diff.push((a + w - b).max(0));
+        let cand = curr / 2;
+        if (cand == num && freq.get(&cand).is_some_and(|&v| v >= 2))
+            || (cand != num && freq.get(&cand).is_some_and(|&v| v > 0))
+        {
+            res = res.max(num);
+        }
     }
-    if diff.len() >= k {
-        diff.select_nth_unstable_by(k - 1, |a, b| b.cmp(a)); // big first
-    }
-    res += diff.iter().take(k - 1).sum::<i64>();
-    let res2 = res + diff.get(k - 1).unwrap_or(&0);
-    [res, res2]
+    res
 }
 
 #[cfg(test)]
@@ -69,26 +60,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            maximize_sum_of_weights(&[[0, 1, 4], [0, 2, 2], [2, 3, 12], [2, 4, 6]], 2),
-            22
-        );
-        assert_eq!(
-            maximize_sum_of_weights(
-                &[
-                    [0, 1, 5],
-                    [1, 2, 10],
-                    [0, 3, 15],
-                    [3, 4, 20],
-                    [3, 5, 5],
-                    [0, 6, 10]
-                ],
-                3
-            ),
-            65
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
