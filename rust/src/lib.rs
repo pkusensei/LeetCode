038e8560a42b9eeb47d42398523b8751fab483f0
pkusensei <ZW_08@outlook.com>
2025-05-28 15:37:44 -0400

    3364 prefix sum and sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,56 +6,39 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_collected_fruits(fruits: &[&[i32]]) -> i32 {
-    let n = fruits.len();
-    let mut memo = vec![vec![-1; n]; n];
-    let mut res = dfs1(&fruits, 0, n - 1, &mut memo) + dfs2(&fruits, n - 1, 0, &mut memo);
-    res += (0..n).map(|i| fruits[i][i]).sum::<i32>();
-    res
-}
-
-fn dfs1(grid: &[&[i32]], r: usize, c: usize, memo: &mut [Vec<i32>]) -> i32 {
-    let n = grid.len();
-    if r == n - 1 && c == n - 1 {
-        return 0;
-    }
-    if r >= c {
-        return 0;
-    }
-    if memo[r][c] > -1 {
-        return memo[r][c];
-    }
-    let mut res = 0;
-    for nc in [c.saturating_sub(1), c, 1 + c] {
-        if nc < n {
-            res = res.max(dfs1(grid, 1 + r, nc, memo));
+pub fn minimum_sum_subarray(nums: Vec<i32>, l: i32, r: i32) -> i32 {
+    let n = nums.len();
+    let [l, r] = [l, r].map(|v| v as usize);
+    let prefix = nums
+        .iter()
+        .take(r)
+        .fold(Vec::with_capacity(r), |mut acc, &v| {
+            acc.push(v + acc.last().unwrap_or(&0));
+            acc
+        });
+    let mut res = None::<i32>;
+    for len in l..=r {
+        let mut curr = prefix[len - 1];
+        if curr > 0 {
+            if let Some(v) = res {
+                res = Some(v.min(curr));
+            } else {
+                res = Some(curr)
+            }
+        }
+        for i in len..n {
+            curr += nums[i];
+            curr -= nums[i - len];
+            if curr > 0 {
+                if let Some(v) = res {
+                    res = Some(v.min(curr));
+                } else {
+                    res = Some(curr)
+                }
+            }
         }
     }
-    res += grid[r][c];
-    memo[r][c] = res;
-    res
-}
-
-fn dfs2(grid: &[&[i32]], r: usize, c: usize, memo: &mut [Vec<i32>]) -> i32 {
-    let n = grid.len();
-    if r == n - 1 && c == n - 1 {
-        return 0;
-    }
-    if c >= r {
-        return 0;
-    }
-    if memo[r][c] > -1 {
-        return memo[r][c];
-    }
-    let mut res = 0;
-    for nr in [r.saturating_sub(1), r, 1 + r] {
-        if nr < n {
-            res = res.max(dfs2(grid, nr, 1 + c, memo));
-        }
-    }
-    res += grid[r][c];
-    memo[r][c] = res;
-    res
+    res.unwrap_or(-1)
 }
 
 #[cfg(test)]
@@ -88,29 +71,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            max_collected_fruits(&[
-                &[1, 2, 3, 4],
-                &[5, 6, 8, 7],
-                &[9, 10, 11, 12],
-                &[13, 14, 15, 16]
-            ]),
-            100
-        );
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(
-            max_collected_fruits(&[
-                &[8, 5, 0, 17, 15],
-                &[6, 0, 15, 6, 0],
-                &[7, 19, 16, 8, 18],
-                &[11, 3, 2, 12, 13],
-                &[17, 15, 15, 4, 6]
-            ]),
-            145
-        );
-    }
+    fn test() {}
 }
