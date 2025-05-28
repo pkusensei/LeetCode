3d3f3061191ae3a236a4a1176a8e334283048305
pkusensei <ZW_08@outlook.com>
2025-05-27 20:59:06 -0400

    3372 bfs all nodes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,61 +6,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_difference(nums: &[i32]) -> i32 {
-    let mut min = i32::MAX;
-    let mut max = 0;
-    let mut max_gap = 0;
-    for w in nums.windows(2) {
-        let a = w[0].min(w[1]);
-        let b = w[0].max(w[1]);
-        if a == -1 && b > -1 {
-            min = min.min(b);
-            max = max.max(b);
-        } else {
-            max_gap = max_gap.max(b - a);
-        }
-    }
-    let mut left = max_gap;
-    let mut right = (max - min + 1) / 2;
-    while left < right {
-        let mid = left + (right - left) / 2;
-        if check(nums, min + mid, max - mid, mid) {
-            right = mid;
-        } else {
-            left = 1 + mid;
-        }
-    }
-    left
+pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
+    let t1 = build(&edges1, k);
+    let t2 = build(&edges2, k - 1).into_iter().max().unwrap_or(0);
+    t1.into_iter().map(|v| v + t2).collect()
 }
 
-fn check(nums: &[i32], x: i32, y: i32, d: i32) -> bool {
-    let mut count = 0; // minus ones
-    let mut prev = 0_i32;
-    for &num in nums {
-        if num == -1 {
-            if prev > 0 && prev.abs_diff(x).min(prev.abs_diff(y)) > d as u32 {
-                return false; // abs(prev-curr) must be <= d
+fn build(edges: &[Vec<i32>], k: i32) -> Vec<i32> {
+    use std::collections::VecDeque;
+    let n = 1 + edges.len();
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push(b);
+        acc[b].push(a);
+        acc
+    });
+    let mut counts = Vec::with_capacity(n);
+    for start in 0..n {
+        let mut queue = VecDeque::from([(start, 0)]);
+        let mut seen = vec![false; n];
+        seen[start] = true;
+        let mut count = 0;
+        while let Some((node, dist)) = queue.pop_front() {
+            if dist > k {
+                continue;
             }
             count += 1;
-        } else {
-            if count > 0 {
-                let diff = if prev > 0 {
-                    std::cmp::min(
-                        prev.abs_diff(x).max(num.abs_diff(x)),
-                        prev.abs_diff(y).max(num.abs_diff(y)),
-                    )
-                } else {
-                    num.abs_diff(x).min(num.abs_diff(y))
-                };
-                if diff > d as u32 && (count == 1 || y - x > d) {
-                    return false;
+            for &next in &adj[node] {
+                if !seen[next] {
+                    seen[next] = true;
+                    queue.push_back((next, 1 + dist));
                 }
             }
-            prev = num;
-            count = 0;
         }
+        counts.push(count);
     }
-    true
+    counts
 }
 
 #[cfg(test)]
@@ -93,10 +74,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_difference(&[1, 2, -1, 10, 8]), 4);
-        assert_eq!(min_difference(&[-1, 10, -1, 8]), 1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
