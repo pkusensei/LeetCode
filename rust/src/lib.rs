ad1b78b4e8d8971169280ed899a88f622319b1d5
pkusensei <ZW_08@outlook.com>
2025-05-28 20:21:51 -0400

    3373 coloring nodes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,28 +6,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_largest_outlier(nums: Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-    let mut freq = HashMap::new();
-    let mut sum = 0;
-    for &num in &nums {
-        *freq.entry(num).or_insert(0) += 1;
-        sum += num;
-    }
-    let mut res = i32::MIN;
-    for &num in &nums {
-        let curr = sum - num;
-        if curr & 1 == 1 {
-            continue;
-        }
-        let cand = curr / 2;
-        if (cand == num && freq.get(&cand).is_some_and(|&v| v >= 2))
-            || (cand != num && freq.get(&cand).is_some_and(|&v| v > 0))
-        {
-            res = res.max(num);
-        }
+pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>) -> Vec<i32> {
+    let (colors1, count1) = build(&edges1);
+    let (_, count2) = build(&edges2);
+    let mut res = vec![];
+    for c in colors1 {
+        res.push(count1[usize::from(c)] + count2[0].max(count2[1]));
     }
     res
+}
+
+fn build(edges: &[Vec<i32>]) -> (Vec<bool>, [i32; 2]) {
+    let n = 1 + edges.len();
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push(b);
+        acc[b].push(a);
+        acc
+    });
+    let mut colors = vec![false; n];
+    let count = dfs(&adj, 0, n, false, &mut colors);
+    (colors, count)
+}
+
+fn dfs(adj: &[Vec<usize>], node: usize, prev: usize, color: bool, colors: &mut [bool]) -> [i32; 2] {
+    colors[node] = color;
+    let mut count = [0, 0];
+    count[usize::from(color)] += 1;
+    for &next in &adj[node] {
+        if next != prev {
+            let [a, b] = dfs(adj, next, node, !color, colors);
+            count[0] += a;
+            count[1] += b;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
