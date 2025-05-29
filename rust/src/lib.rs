acd2a7a18ba30f69a3c082beeecd14f0fa00a16f
pkusensei <ZW_08@outlook.com>
2025-05-29 19:38:42 -0400

    3388 lcp backwards

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,55 +5,33 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::{chain, izip};
-use std::collections::{HashMap, VecDeque};
 
-pub fn max_amount(
-    initial_currency: String,
-    pairs1: Vec<Vec<String>>,
-    rates1: Vec<f64>,
-    pairs2: Vec<Vec<String>>,
-    rates2: Vec<f64>,
-) -> f64 {
-    let mut str_id = HashMap::new();
-    str_id.insert(initial_currency.as_str(), 0);
-    for s in chain!(pairs1.iter(), pairs2.iter()).flatten() {
-        let id = str_id.len();
-        str_id.entry(s.as_str()).or_insert(id);
-    }
-    let mat1 = build(&pairs1, &rates1, &str_id);
-    let mat2 = build(&pairs2, &rates2, &str_id);
-    let mut seen = bfs(mat1, HashMap::from([(0, 1.0)]));
-    seen = bfs(mat2, seen);
-    seen[&0]
-}
-
-fn bfs(mat1: Vec<Vec<f64>>, mut seen: HashMap<usize, f64>) -> HashMap<usize, f64> {
-    let mut queue: VecDeque<_> = seen.iter().map(|(k, v)| (*k, *v)).collect();
-    while let Some((node, num)) = queue.pop_front() {
-        for (next, v) in mat1[node].iter().enumerate() {
-            let next_num = v * num;
-            if seen.get(&next).is_none_or(|&v| v < next_num) {
-                seen.insert(next, next_num);
-                queue.push_back((next, next_num));
+pub fn beautiful_splits(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let mut lcp = vec![vec![0; 1 + n]; 1 + n];
+    for i1 in (0..n).rev() {
+        for i2 in i1..n {
+            if nums[i1] == nums[i2] {
+                lcp[i1][i2] = 1 + lcp[1 + i1][1 + i2];
             }
         }
     }
-    seen
-}
-
-fn build(pairs: &[Vec<String>], rates: &[f64], str_id: &HashMap<&str, usize>) -> Vec<Vec<f64>> {
-    let n = str_id.len();
-    let mut mat = vec![vec![0.0; n]; n];
-    for i in 0..n {
-        mat[i][i] = 1.0;
+    let mut res = 0;
+    for i1 in 1..n {
+        for i2 in 1 + i1..n {
+            // split between nums1 and nums2, i.e [0..i1) and [i1..i2)
+            // This length must be >= i1, len(nums1)
+            // Or
+            // split between nums2 and nums3, i.e [i1..i2) and [i2..n)
+            // length must be >= (i2-i1), len(nums2)
+            if lcp[0][i1].min(i1).min(i2 - i1) >= i1
+                || lcp[i1][i2].min(i2 - i1).min(n - i2) >= i2 - i1
+            {
+                res += 1;
+            }
+        }
     }
-    for (p, &r) in izip!(pairs.iter(), rates.iter()) {
-        let [a, b] = [0, 1].map(|i| str_id[p[i].as_str()]);
-        mat[a][b] = r;
-        mat[b][a] = 1.0 / r;
-    }
-    mat
+    res
 }
 
 #[cfg(test)]
@@ -86,7 +64,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(beautiful_splits(&[1, 1, 2, 1]), 2);
+        assert_eq!(beautiful_splits(&[1, 2, 3, 4]), 0);
+    }
 
     #[test]
     fn test() {}
