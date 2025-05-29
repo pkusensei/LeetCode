f9c6d2172a58440f5e19ffe1499bd957d29b0add
pkusensei <ZW_08@outlook.com>
2025-05-29 07:54:26 -0400

    3376 dfs/memo bitmask

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,41 +6,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>) -> Vec<i32> {
-    let (colors1, count1) = build(&edges1);
-    let (_, count2) = build(&edges2);
-    let mut res = vec![];
-    for c in colors1 {
-        res.push(count1[usize::from(c)] + count2[0].max(count2[1]));
+pub fn find_minimum_time(strength: &[i32], k: i32) -> i32 {
+    let n = strength.len();
+    let mut memo = vec![vec![-1; 1 << n]; 2 + n];
+    dfs(strength, k, 1, 0, &mut memo)
+}
+
+fn dfs(nums: &[i32], k: i32, x: i32, mask: usize, memo: &mut [Vec<i32>]) -> i32 {
+    if nums.len() == mask.count_ones() as usize {
+        return 0;
     }
-    res
-}
-
-fn build(edges: &[Vec<i32>]) -> (Vec<bool>, [i32; 2]) {
-    let n = 1 + edges.len();
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push(b);
-        acc[b].push(a);
-        acc
-    });
-    let mut colors = vec![false; n];
-    let count = dfs(&adj, 0, n, false, &mut colors);
-    (colors, count)
-}
-
-fn dfs(adj: &[Vec<usize>], node: usize, prev: usize, color: bool, colors: &mut [bool]) -> [i32; 2] {
-    colors[node] = color;
-    let mut count = [0, 0];
-    count[usize::from(color)] += 1;
-    for &next in &adj[node] {
-        if next != prev {
-            let [a, b] = dfs(adj, next, node, !color, colors);
-            count[0] += a;
-            count[1] += b;
+    let depth = (x / k) as usize;
+    if memo[depth][mask] > -1 {
+        return memo[depth][mask];
+    }
+    let mut res = i32::MAX;
+    for (i, &num) in nums.iter().enumerate() {
+        if (mask >> i) & 1 == 0 {
+            res = res.min((num + x - 1) / x + dfs(nums, k, x + k, mask | (1 << i), memo));
         }
     }
-    count
+    memo[depth][mask] = res;
+    res
 }
 
 #[cfg(test)]
@@ -73,7 +60,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(find_minimum_time(&[3, 4, 1], 1), 4);
+    }
 
     #[test]
     fn test() {}
