1f084f5ab79072e66699df8918596b206d55364e
Zx Wang <ZW_08@outlook.com>
2025-05-29 09:27:00 -0400

    3377 dijkstra

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,29 +6,78 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_minimum_time(strength: &[i32], k: i32) -> i32 {
-    let n = strength.len();
-    let mut memo = vec![vec![-1; 1 << n]; 2 + n];
-    dfs(strength, k, 1, 0, &mut memo)
-}
-
-fn dfs(nums: &[i32], k: i32, x: i32, mask: usize, memo: &mut [Vec<i32>]) -> i32 {
-    if nums.len() == mask.count_ones() as usize {
-        return 0;
+pub fn min_operations(n: i32, m: i32) -> i32 {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+    let [n, m] = [n, m].map(|v| v as usize);
+    if SIEVE[n] || SIEVE[m] {
+        return -1;
     }
-    let depth = (x / k) as usize;
-    if memo[depth][mask] > -1 {
-        return memo[depth][mask];
-    }
-    let mut res = i32::MAX;
-    for (i, &num) in nums.iter().enumerate() {
-        if (mask >> i) & 1 == 0 {
-            res = res.min((num + x - 1) / x + dfs(nums, k, x + k, mask | (1 << i), memo));
+    let mut heap = BinaryHeap::from([(Reverse(n), n)]);
+    let mut seen = [false; N];
+    seen[n] = true;
+    while let Some((Reverse(cost), num)) = heap.pop() {
+        if num == m {
+            return cost as i32;
+        }
+        for next in step(num) {
+            if !seen[next] {
+                seen[next] = true;
+                heap.push((Reverse(cost + next), next));
+            }
         }
     }
-    memo[depth][mask] = res;
+    -1
+}
+
+fn step(mut num: usize) -> Vec<usize> {
+    let mut ds = vec![];
+    while num > 0 {
+        ds.push(num % 10);
+        num /= 10;
+    }
+    ds.reverse();
+    let n = ds.len();
+    let mut res = vec![];
+    for i in 0..n {
+        if ds[i] < 9 {
+            ds[i] += 1;
+            check(&ds, &mut res);
+            ds[i] -= 1;
+        }
+        if (i == 0 && ds[i] > 1) || (i > 0 && ds[i] > 0) {
+            ds[i] -= 1;
+            check(&ds, &mut res);
+            ds[i] += 1;
+        }
+    }
     res
 }
+
+fn check(ds: &[usize], res: &mut Vec<usize>) {
+    let v = ds.iter().fold(0, |acc, d| acc * 10 + d);
+    if !SIEVE[v] {
+        res.push(v);
+    }
+}
+
+const N: usize = 10_000;
+const SIEVE: [bool; N] = {
+    let mut sieve = [true; N];
+    sieve[0] = false;
+    sieve[1] = false;
+    let mut p = 2;
+    while p < N {
+        if sieve[p] {
+            let mut val = p * p;
+            while val < N {
+                sieve[val] = false;
+                val += p;
+            }
+        }
+        p += 1;
+    }
+    sieve
+};
 
 #[cfg(test)]
 mod tests {
@@ -61,7 +110,7 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(find_minimum_time(&[3, 4, 1], 1), 4);
+        assert_eq!(min_operations(10, 12), 85);
     }
 
     #[test]
