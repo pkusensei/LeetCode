142fafd2cc96db2cdd36fc3975f5f78f1e990328
pkusensei <ZW_08@outlook.com>
2025-05-30 19:44:20 -0400

    3402

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,34 +6,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_length(s: &str, num_ops: i32) -> i32 {
-    let mut left = 1;
-    let mut right = s.len();
-    while left < right {
-        let mid = left.midpoint(right);
-        if check(s.as_bytes(), num_ops as usize, mid) {
-            right = mid;
-        } else {
-            left = 1 + mid;
+pub fn minimum_operations(grid: Vec<Vec<i32>>) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let mut res = 0;
+    for c in 0..cols {
+        let mut prev = grid[0][c];
+        for r in 1..rows {
+            res += (prev + 1 - grid[r][c]).max(0);
+            prev = (prev + 1).max(grid[r][c]);
         }
     }
-    left as i32
-}
-
-fn check(s: &[u8], ops: usize, mid: usize) -> bool {
-    if mid == 1 {
-        let sum: usize = s
-            .iter()
-            .enumerate()
-            .map(|(i, &b)| (i & 1 ^ usize::from(b & 1)))
-            .sum();
-        sum.min(s.len() - sum) <= ops
-    } else {
-        s.chunk_by(|a, b| a == b)
-            .map(|ch| ch.len() / (1 + mid))
-            .sum::<usize>()
-            <= ops
-    }
+    res
 }
 
 #[cfg(test)]
@@ -66,14 +49,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_length("000001", 1), 2);
-        assert_eq!(min_length("0000", 2), 1);
-        assert_eq!(min_length("0101", 0), 1);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(min_length("1001", 1), 2);
-    }
+    fn test() {}
 }
