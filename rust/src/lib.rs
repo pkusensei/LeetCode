ea401d79234349b46a0554cc677e87a6014d03cd
pkusensei <ZW_08@outlook.com>
2025-06-01 19:17:37 -0400

    3407 find and rfind

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,34 +6,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_good_arrays(n: i32, m: i32, k: i32) -> i32 {
-    let [n, m, k] = [n, m, k].map(i64::from);
-    let comb = n_choose_k(n - 1, k);
-    (comb * mod_pow(m - 1, n - k - 1, MOD) % MOD * m % MOD) as i32
-}
-
-const MOD: i64 = 1_000_000_007;
-
-fn n_choose_k(n: i64, k: i64) -> i64 {
-    if k > n {
-        return 0;
-    }
-    let k = k.min(n - k);
-    let f = |acc, v| acc * v % MOD;
-    let numerator = (n + 1 - k..=n).fold(1, f);
-    let denominator = (1..=k).fold(1, f);
-    numerator * mod_pow(denominator, MOD - 2, MOD) % MOD
-}
-
-const fn mod_pow(b: i64, exp: i64, m: i64) -> i64 {
-    if exp == 0 {
-        return 1;
-    }
-    if exp & 1 == 0 {
-        mod_pow(b * b % m, exp >> 1, m)
-    } else {
-        mod_pow(b * b % m, exp >> 1, m) * b % m
-    }
+pub fn has_match(s: String, p: String) -> bool {
+    let Some((a, b)) = p.split_once('*') else {
+        return false;
+    };
+    let (Some(i1), Some(i2)) = (s.find(a), s.rfind(b)) else {
+        return false;
+    };
+    i1 + a.len() <= i2
 }
 
 #[cfg(test)]
@@ -66,11 +46,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_good_arrays(3, 2, 1), 4);
-        assert_eq!(count_good_arrays(4, 2, 2), 6);
-        assert_eq!(count_good_arrays(5, 2, 0), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
