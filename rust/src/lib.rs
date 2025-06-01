390205c2f3853a47dde66d4baad3961bf157485d
pkusensei <ZW_08@outlook.com>
2025-06-01 15:11:53 -0400

    3404 2sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,21 +3,35 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn answer_string(word: String, num_friends: i32) -> String {
-    if num_friends <= 1 {
-        return word;
+// p*r == q*s
+// p/q == s/r
+pub fn number_of_subsequences(nums: &[i32]) -> i64 {
+    let n = nums.len();
+    let mut res = 0;
+    let mut map: HashMap<[i32; 2], i64> = HashMap::new();
+    for r in 4..n - 2 {
+        let q = r - 2;
+        for p in 0..q - 1 {
+            let [a, b] = [p, q].map(|i| nums[i]);
+            let gcd_ = gcd(a, b);
+            *map.entry([a / gcd_, b / gcd_]).or_insert(0) += 1;
+        }
+        for s in 2 + r..n {
+            let [a, b] = [s, r].map(|i| nums[i]);
+            let gcd_ = gcd(a, b);
+            res += map.get(&[a / gcd_, b / gcd_]).unwrap_or(&0);
+        }
     }
-    let (s, n) = (word.as_bytes(), word.len());
-    let len = n + 1 - num_friends as usize;
-    let mut res = "".as_bytes();
-    for idx in 0..n {
-        let curr = &s[idx..(idx + len).min(n)];
-        res = res.max(curr);
-    }
-    String::from_utf8(res.to_vec()).unwrap()
+    res as i64
+}
+
+const fn gcd(a: i32, b: i32) -> i32 {
+    if a == 0 { b } else { gcd(b % a, a) }
 }
 
 #[cfg(test)]
@@ -50,7 +64,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(number_of_subsequences(&[1, 2, 3, 4, 3, 6, 1]), 1);
+    }
 
     #[test]
     fn test() {}
