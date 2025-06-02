1844482634564f3b172ffdc5fc726d0a03b5afee
pkusensei <ZW_08@outlook.com>
2025-06-02 13:10:36 -0400

    3412 26 stacks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,34 +6,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_length(nums: &[i32]) -> i32 {
-    const MAX: i64 = 3_628_800;
-    let mut res = 1;
-    'outer: for (left, &num) in nums.iter().enumerate() {
-        let [mut prod, mut gcd_, mut lcm_] = [i64::from(num); 3];
-        let mut count = 1;
-        for &num in &nums[1 + left..] {
-            prod *= i64::from(num);
-            if prod > MAX {
-                continue 'outer;
-            }
-            gcd_ = gcd(gcd_, num.into());
-            lcm_ = lcm(lcm_, num.into());
-            if prod == gcd_ * lcm_ {
-                count += 1;
-                res = res.max(count);
-            }
+pub fn calculate_score(s: String) -> i64 {
+    let mut sts = [const { vec![] }; 26];
+    let mut res = 0;
+    for (idx, b) in s.bytes().enumerate() {
+        let bi = usize::from(b - b'a');
+        if let Some(prev) = sts[25 - bi].pop() {
+            res += idx - prev;
+        } else {
+            sts[bi].push(idx);
         }
     }
-    res
-}
-
-const fn gcd(a: i64, b: i64) -> i64 {
-    if a == 0 { b } else { gcd(b % a, a) }
-}
-
-const fn lcm(a: i64, b: i64) -> i64 {
-    a / gcd(a, b) * b
+    res as i64
 }
 
 #[cfg(test)]
