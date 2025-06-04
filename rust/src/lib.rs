c6597ccab0d0ebb5e5adc35d4251813e4ca3ccc7
pkusensei <ZW_08@outlook.com>
2025-06-04 14:40:22 -0400

    3425 prefix on the fly with dfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,23 +3,62 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost(mut arr: Vec<i32>, mut brr: Vec<i32>, k: i64) -> i64 {
-    let num1: i64 = arr
-        .iter()
-        .zip(&brr)
-        .map(|(a, b)| i64::from(a.abs_diff(*b)))
-        .sum();
-    arr.sort_unstable();
-    brr.sort_unstable();
-    let num2 = k + arr
-        .iter()
-        .zip(&brr)
-        .map(|(a, b)| i64::from(a.abs_diff(*b)))
-        .sum::<i64>();
-    num1.min(num2)
+pub fn longest_special_path(edges: &[[i32; 3]], nums: &[i32]) -> Vec<i32> {
+    let n = nums.len();
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push((b, e[2]));
+        acc[b].push((a, e[2]));
+        acc
+    });
+    let mut res = [0, 1];
+    dfs(
+        &adj,
+        &nums,
+        0,
+        n,
+        0,
+        1,
+        &mut HashMap::new(),
+        &mut vec![0],
+        &mut res,
+    );
+    res.to_vec()
+}
+
+fn dfs(
+    adj: &[Vec<(usize, i32)>],
+    nums: &[i32],
+    node: usize,
+    prev: usize,
+    mut left: usize,
+    depth: usize,
+    depths: &mut HashMap<i32, usize>,
+    prefix: &mut Vec<i32>,
+    res: &mut [i32; 2],
+) {
+    let prev_depth = depths.insert(nums[node], depth).unwrap_or(0);
+    left = left.max(prev_depth);
+    let val = prefix.last().unwrap_or(&0) - prefix[left];
+    let count = (depth - left) as i32;
+    if val > res[0] {
+        *res = [val, count];
+    } else if val == res[0] {
+        res[1] = res[1].min(count)
+    }
+    for &(next, w) in &adj[node] {
+        if next != prev {
+            prefix.push(w + prefix.last().unwrap_or(&0));
+            dfs(adj, nums, next, node, left, 1 + depth, depths, prefix, res);
+            prefix.pop();
+        }
+    }
+    depths.insert(nums[node], prev_depth);
 }
 
 #[cfg(test)]
@@ -52,8 +91,26 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(longest_special_path(&[[1, 0, 8]], &[2, 2]), [0, 1]);
+        assert_eq!(
+            longest_special_path(
+                &[[0, 1, 2], [1, 2, 3], [1, 3, 5], [1, 4, 4], [2, 5, 6]],
+                &[2, 1, 2, 1, 3, 1]
+            ),
+            [6, 2]
+        );
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            longest_special_path(&[[1, 0, 7], [1, 2, 4]], &[1, 1, 3]),
+            [4, 2]
+        );
+        assert_eq!(
+            longest_special_path(&[[1, 0, 5], [2, 1, 3]], &[3, 1, 1]),
+            [5, 2]
+        );
+    }
 }
