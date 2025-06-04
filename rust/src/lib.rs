8f61019ee801cf222af1c26f1074f6c712366a7a
pkusensei <ZW_08@outlook.com>
2025-06-04 17:40:25 -0400

    3427 prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,37 +6,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn distance_sum(m: i32, n: i32, k: i32) -> i32 {
-    const M: i64 = 1_000_000_007;
-    let [m, n, k] = [m, n, k].map(i64::from);
-    let nn = n * m - 2;
-    let kk = k - 2;
-    let kk = kk.min(nn - kk);
-    let f = |acc, v| acc * v % M;
-    let nom = (nn - kk + 1..=nn).fold(1, f);
-    let den = (1..=kk).fold(1, f);
-    let factor = nom * mod_pow(den, M - 2, M) % M;
-    let mut dist = 0;
-    for d in 1..m {
-        dist += d * (m - d) % M * n % M * n % M;
-        dist %= M;
-    }
-    for d in 1..n {
-        dist += d * (n - d) % M * m % M * m % M;
-        dist %= M;
-    }
-    (dist * factor % M) as i32
-}
-
-const fn mod_pow(b: i64, exp: i64, m: i64) -> i64 {
-    if exp == 0 {
-        return 1;
-    }
-    if exp & 1 == 0 {
-        mod_pow(b * b % m, exp >> 1, m)
-    } else {
-        mod_pow(b * b % m, exp >> 1, m) * b % m
-    }
+pub fn subarray_sum(nums: Vec<i32>) -> i32 {
+    let prefix = nums.iter().fold(vec![0], |mut acc, v| {
+        acc.push(v + acc.last().unwrap_or(&0));
+        acc
+    });
+    nums.iter()
+        .enumerate()
+        .map(|(i, &v)| prefix[1 + i] - prefix[i.saturating_sub(v as usize)])
+        .sum()
 }
 
 #[cfg(test)]
@@ -69,13 +47,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(distance_sum(2, 2, 2), 8);
-        assert_eq!(distance_sum(1, 4, 3), 20);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(distance_sum(50, 100, 1299), 995516149);
-    }
+    fn test() {}
 }
