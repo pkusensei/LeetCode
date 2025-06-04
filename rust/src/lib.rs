23d62af64a49d389deba41d993b2b3a260e4a8b2
pkusensei <ZW_08@outlook.com>
2025-06-04 11:10:59 -0400

    3420 mono increasing queue; scan backwards

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,35 +6,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_max_weight(n: i32, edges: Vec<Vec<i32>>, _threshold: i32) -> i32 {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[b].push((a, e[2]));
-        acc
-    });
-    let mut left = 1;
-    let mut right = 1_000_001;
-    while left < right {
-        let mid = left + (right - left) / 2;
-        let mut seen = vec![false; n];
-        dfs(&adj, mid, 0, &mut seen);
-        if seen.into_iter().all(|v| v) {
-            right = mid;
-        } else {
-            left = 1 + mid;
+pub fn count_non_decreasing_subarrays(nums: &[i32], k: i32) -> i64 {
+    let k = i64::from(k);
+    let mut right = nums.len() - 1;
+    let mut queue = std::collections::VecDeque::from([(nums[right], 1)]);
+    let mut cost = 0;
+    let mut res = 1;
+    for (left, &num) in nums.iter().enumerate().rev().skip(1) {
+        let mut curr_count = 1;
+        // "Flatten" the back half of deque to be >= this num
+        while queue.back().is_some_and(|&(v, _)| v < num) {
+            let Some((last, count)) = queue.pop_back() else {
+                break;
+            };
+            cost += i64::from(num - last) * count;
+            curr_count += count;
         }
-    }
-    if left < 1_000_001 { left } else { -1 }
-}
-
-fn dfs(adj: &[Vec<(usize, i32)>], mid: i32, node: usize, seen: &mut [bool]) {
-    seen[node] = true;
-    for &(next, w) in &adj[node] {
-        if !seen[next] && w <= mid {
-            dfs(adj, mid, next, seen);
+        queue.push_back((num, curr_count));
+        while cost > k {
+            // Pop smaller (to the right of array) off queue
+            let Some((front, mut count)) = queue.pop_front() else {
+                break;
+            };
+            cost -= i64::from(front - nums[right]);
+            right -= 1;
+            count -= 1;
+            if count > 0 {
+                queue.push_front((front, count));
+            }
         }
+        res += right + 1 - left;
     }
+    res as i64
 }
 
 #[cfg(test)]
@@ -67,8 +70,16 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_non_decreasing_subarrays(&[6, 3, 1, 2, 4, 4], 7), 17);
+        assert_eq!(count_non_decreasing_subarrays(&[6, 3, 1, 3, 6], 4), 12);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            count_non_decreasing_subarrays(&[1000000000, 1, 1, 1, 1], 1000000000),
+            12
+        );
+    }
 }
