ed7686c7b679f60e94895149f2bdf0119a185fbc
pkusensei <ZW_08@outlook.com>
2025-06-04 11:23:39 -0400

    3424 non-sort vs k+sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,38 +6,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_non_decreasing_subarrays(nums: &[i32], k: i32) -> i64 {
-    let k = i64::from(k);
-    let mut right = nums.len() - 1;
-    let mut queue = std::collections::VecDeque::from([(nums[right], 1)]);
-    let mut cost = 0;
-    let mut res = 1;
-    for (left, &num) in nums.iter().enumerate().rev().skip(1) {
-        let mut curr_count = 1;
-        // "Flatten" the back half of deque to be >= this num
-        while queue.back().is_some_and(|&(v, _)| v < num) {
-            let Some((last, count)) = queue.pop_back() else {
-                break;
-            };
-            cost += i64::from(num - last) * count;
-            curr_count += count;
-        }
-        queue.push_back((num, curr_count));
-        while cost > k {
-            // Pop smaller (to the right of array) off queue
-            let Some((front, mut count)) = queue.pop_front() else {
-                break;
-            };
-            cost -= i64::from(front - nums[right]);
-            right -= 1;
-            count -= 1;
-            if count > 0 {
-                queue.push_front((front, count));
-            }
-        }
-        res += right + 1 - left;
-    }
-    res as i64
+pub fn min_cost(mut arr: Vec<i32>, mut brr: Vec<i32>, k: i64) -> i64 {
+    let num1: i64 = arr
+        .iter()
+        .zip(&brr)
+        .map(|(a, b)| i64::from(a.abs_diff(*b)))
+        .sum();
+    arr.sort_unstable();
+    brr.sort_unstable();
+    let num2 = k + arr
+        .iter()
+        .zip(&brr)
+        .map(|(a, b)| i64::from(a.abs_diff(*b)))
+        .sum::<i64>();
+    num1.min(num2)
 }
 
 #[cfg(test)]
@@ -70,16 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_non_decreasing_subarrays(&[6, 3, 1, 2, 4, 4], 7), 17);
-        assert_eq!(count_non_decreasing_subarrays(&[6, 3, 1, 3, 6], 4), 12);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(
-            count_non_decreasing_subarrays(&[1000000000, 1, 1, 1, 1], 1000000000),
-            12
-        );
-    }
+    fn test() {}
 }
