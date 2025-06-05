b64b746694ec26ca6bb2e89ae0cef462ed710db0
pkusensei <ZW_08@outlook.com>
2025-06-05 15:27:44 -0400

    3433 totally contrived

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,10 +6,51 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_partitions(nums: Vec<i32>) -> i32 {
-    let n = nums.len();
-    let sum: i32 = nums.iter().sum();
-    if sum & 1 == 1 { 0 } else { n as i32 - 1 }
+pub fn count_mentions(number_of_users: i32, mut events: Vec<Vec<String>>) -> Vec<i32> {
+    use std::{
+        cmp::Reverse,
+        collections::{BinaryHeap, HashSet},
+    };
+    let n = number_of_users as usize;
+    let mut on: HashSet<_> = (0..n).collect();
+    let mut off = BinaryHeap::new();
+    events.sort_unstable_by(|a, b| {
+        let [v1, v2] = [a, b].map(|v| v[1].parse::<i32>().unwrap_or(100_000));
+        v1.cmp(&v2).then(b[0].cmp(&a[0]))
+    });
+    let mut res = vec![0; n];
+    let mut all = 0;
+    for e in events {
+        let time = e[1].parse::<i32>().unwrap_or(100_000);
+        while off.peek().is_some_and(|&(Reverse(t), _)| t <= time) {
+            let (_, id) = off.pop().unwrap();
+            on.insert(id);
+        }
+        if e[0] == "MESSAGE" {
+            match e[2].as_str() {
+                "ALL" => all += 1,
+                "HERE" => {
+                    for &i in &on {
+                        res[i] += 1;
+                    }
+                }
+                _ => {
+                    for s in e[2].split_ascii_whitespace() {
+                        let id = s[2..].parse::<usize>().unwrap_or(0);
+                        res[id] += 1;
+                    }
+                }
+            }
+        } else {
+            let id = e[2].parse::<usize>().unwrap_or(100);
+            on.remove(&id);
+            off.push((Reverse(time + 60), id));
+        }
+    }
+    for v in res.iter_mut() {
+        *v += all;
+    }
+    res
 }
 
 #[cfg(test)]
