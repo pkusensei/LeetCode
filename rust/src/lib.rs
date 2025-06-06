78c7855250d465fcb6d62e9e7c08436fe5bc1864
pkusensei <ZW_08@outlook.com>
2025-06-06 17:07:12 -0400

    3435 brute force

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,29 +6,77 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_frequency(nums: &[i32], k: i32) -> i32 {
-    use std::collections::HashMap;
-    let freq = nums.iter().fold(HashMap::new(), |mut acc, &v| {
-        *acc.entry(v).or_insert(0) += 1;
-        acc
-    });
-    let mut res = 0;
-    for &target in freq.keys() {
-        let mut curr = 0;
-        for &num in nums.iter() {
-            if num == k {
-                curr -= 1; // lose this native `k`
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn supersequences(words: &[&str]) -> Vec<Vec<i32>> {
+    let mut nodes = HashSet::new();
+    let mut edges = vec![];
+    for s in words.iter().map(|s| s.as_bytes()) {
+        let [a, b] = [0, 1].map(|i| usize::from(s[i] - b'a'));
+        nodes.extend([a, b]);
+        edges.push([a, b]);
+    }
+    let mut res = vec![];
+    for d in find_doubles(&edges, &nodes) {
+        let mut curr = vec![0; 26];
+        for &i in &nodes {
+            curr[i] = 1;
+        }
+        for i in d {
+            curr[i] = 2;
+        }
+        res.push(curr);
+    }
+    res
+}
+
+fn find_doubles(edges: &[[usize; 2]], nodes: &HashSet<usize>) -> Vec<Vec<usize>> {
+    let n = nodes.len();
+    let mut res = vec![];
+    for len in 0..=n {
+        for doubles in nodes.iter().copied().combinations(len) {
+            let mut adj = [const { vec![] }; 26];
+            for &[a, b] in edges {
+                if !doubles.contains(&a) && !doubles.contains(&b) {
+                    adj[a].push(b);
+                }
             }
-            if num == target {
-                curr += 1; // target -> k
+            if !has_cycle(&adj) {
+                res.push(doubles);
             }
-            if curr < 0 {
-                curr = 0; // reset subarray
-            }
-            res = res.max(curr);
+        }
+        if !res.is_empty() {
+            break;
         }
     }
-    res + freq.get(&k).unwrap_or(&0)
+    res
+}
+
+fn has_cycle(adj: &[Vec<usize>; 26]) -> bool {
+    let mut colors = [Color::New; 26];
+    (0..26).any(|node| dfs(adj, node, &mut colors))
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum Color {
+    New,
+    Curr,
+    Done,
+}
+
+fn dfs(adj: &[Vec<usize>; 26], node: usize, colors: &mut [Color; 26]) -> bool {
+    if colors[node] != Color::New {
+        return colors[node] == Color::Curr;
+    }
+    colors[node] = Color::Curr;
+    for &next in &adj[node] {
+        if dfs(adj, next, colors) {
+            return true;
+        }
+    }
+    colors[node] = Color::Done;
+    false
 }
 
 #[cfg(test)]
@@ -62,8 +110,29 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_frequency(&[1, 2, 3, 4, 5, 6], 1), 2);
-        assert_eq!(max_frequency(&[10, 2, 3, 4, 5, 5, 4, 3, 2, 2], 10), 4);
+        assert_eq!(
+            supersequences(&["ab", "ba"]),
+            [
+                [
+                    2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                ],
+                [
+                    1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                ],
+            ]
+        );
+        assert_eq!(
+            supersequences(&["aa", "ac"]),
+            [[
+                2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]]
+        );
+        assert_eq!(
+            supersequences(&["aa", "bb", "cc"]),
+            [[
+                2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]]
+        );
     }
 
     #[test]
