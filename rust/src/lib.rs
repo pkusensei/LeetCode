2f3bee83875919ddfb636d0395388f75aa2443d5
pkusensei <ZW_08@outlook.com>
2025-06-07 20:22:54 -0400

    386 dfs; eagerly expand number

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,22 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_distance(s: String, k: i32) -> i32 {
-    let mut res = 0;
-    for dir in [b"NE", b"NW", b"SE", b"SW"] {
-        let mut curr = 0;
-        let mut k_ = k;
-        for b in s.bytes() {
-            if dir.contains(&b) {
-                curr += 1;
-            } else if k_ > 0 {
-                curr += 1;
-                k_ -= 1;
-            } else {
-                curr -= 1;
-            }
-            res = res.max(curr);
-        }
+pub fn lexical_order(n: i32) -> Vec<i32> {
+    let mut res = Vec::with_capacity(n as usize);
+    for d in 1..=9 {
+        dfs(n, d, &mut res);
     }
     res
+}
+
+fn dfs(n: i32, curr: i32, res: &mut Vec<i32>) {
+    if n < curr {
+        return;
+    }
+    res.push(curr);
+    for d in 0..=9 {
+        dfs(n, curr * 10 + d, res);
+    }
 }
 
 #[cfg(test)]
@@ -56,7 +54,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            lexical_order(13),
+            [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
+        );
+        assert_eq!(lexical_order(2), [1, 2]);
+    }
 
     #[test]
     fn test() {}
