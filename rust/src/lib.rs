ab07e5d5afe16ea2b7593cf50eb45ef4e14190d3
pkusensei <ZW_08@outlook.com>
2025-06-08 19:12:13 -0400

    3444 try all bitmasks on all nums

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,22 +6,48 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn lexical_order(n: i32) -> Vec<i32> {
-    let mut res = Vec::with_capacity(n as usize);
-    for d in 1..=9 {
-        dfs(n, d, &mut res);
+pub fn minimum_increments(nums: &[i32], target: &[i32]) -> i32 {
+    let n = target.len();
+    let mut mask_lcms = vec![1; 1 << n];
+    for mask in 1..1 << n {
+        let mut curr = 1;
+        for (i, &num) in target.iter().enumerate() {
+            if (mask >> i) & 1 == 1 {
+                curr = lcm(curr, num.into());
+            }
+        }
+        mask_lcms[mask] = curr;
     }
-    res
+    let mut dp = vec![i64::MAX; 1 << n];
+    dp[0] = 0;
+    for &num in nums.iter() {
+        let num = i64::from(num);
+        let mut mask_costs = vec![0; 1 << n];
+        for (mask, &lcm_) in mask_lcms.iter().enumerate().skip(1) {
+            let rem = num % lcm_;
+            mask_costs[mask] = if rem == 0 { 0 } else { lcm_ - rem };
+        }
+        let mut curr = dp.clone();
+        for prev in 0..1 << n {
+            if dp[prev] == i64::MAX {
+                continue;
+            }
+            for (mask, &cost) in mask_costs.iter().enumerate() {
+                let new_mask = prev | mask;
+                curr[new_mask] = curr[new_mask].min(dp[prev] + cost);
+            }
+        }
+        dp = curr;
+    }
+    let res = dp[(1 << n) - 1];
+    if res < i64::MAX { res as i32 } else { -1 }
 }
 
-fn dfs(n: i32, curr: i32, res: &mut Vec<i32>) {
-    if n < curr {
-        return;
+const fn lcm(a: i64, b: i64) -> i64 {
+    const fn gcd(a: i64, b: i64) -> i64 {
+        if a == 0 { b } else { gcd(b % a, a) }
     }
-    res.push(curr);
-    for d in 0..=9 {
-        dfs(n, curr * 10 + d, res);
-    }
+    a / gcd(a, b) * b
 }
 
 #[cfg(test)]
@@ -55,11 +81,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            lexical_order(13),
-            [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
-        );
-        assert_eq!(lexical_order(2), [1, 2]);
+        assert_eq!(minimum_increments(&[1, 2, 3], &[4]), 1);
+        assert_eq!(minimum_increments(&[8, 4], &[10, 5]), 2);
+        assert_eq!(minimum_increments(&[7, 9, 10], &[7]), 0);
     }
 
     #[test]
