97c4c182e2b99509785c5300823ac4993c5d8ac0
pkusensei <ZW_08@outlook.com>
2025-06-09 18:03:15 -0400

    3448 dp one by one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,57 +3,24 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn assign_elements(groups: &[i32], elements: &[i32]) -> Vec<i32> {
-    let map = elements
-        .iter()
-        .enumerate()
-        .fold(HashMap::new(), |mut acc, (i, &v)| {
-            acc.entry(v).or_insert(i as i32);
-            acc
-        });
-    groups
-        .iter()
-        .map(|&num| find(num, &map).unwrap_or(-1))
-        .collect()
-}
-
-fn find(num: i32, map: &HashMap<i32, i32>) -> Option<i32> {
-    let mut res = i32::MAX;
-    for v in 1..=num.isqrt() {
-        if num % v > 0 {
-            continue;
-        }
-        if let Some(&i) = map.get(&v) {
-            res = res.min(i)
-        }
-        if let Some(&i) = map.get(&(num / v)) {
-            res = res.min(i)
-        }
-    }
-    if res == i32::MAX { None } else { Some(res) }
-}
-
-pub fn with_sieve(groups: &[i32], elements: &[i32]) -> Vec<i32> {
-    let max = *groups.iter().max().unwrap();
-    let mut sieve = vec![-1; 1 + max as usize];
-    for (idx, &v) in elements.iter().enumerate() {
-        if v > max || sieve[v as usize] > -1 {
-            continue;
-        }
-        let mut curr = v;
-        while curr <= max {
-            if sieve[curr as usize] == -1 {
-                sieve[curr as usize] = idx as i32;
+pub fn count_substrings(s: &str) -> i64 {
+    let mut dp = [[0; 10]; 10];
+    let mut res = 0;
+    for digit in s.bytes().map(|b| usize::from(b - b'0')) {
+        let mut curr = [[0; 10]; 10];
+        for den in 1..10 {
+            for rem in 0..10 {
+                curr[den][(rem * 10 + digit) % den] += dp[den][rem]
             }
-            curr += v;
+            curr[den][digit % den] += 1; // start new substr
         }
+        res += curr[digit][0];
+        dp = curr;
     }
-    groups.iter().map(|&v| sieve[v as usize]).collect()
+    res
 }
 
 #[cfg(test)]
@@ -87,13 +54,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(assign_elements(&[8, 4, 3, 2, 4], &[4, 2]), [0, 0, -1, 1, 0]);
-        assert_eq!(assign_elements(&[2, 3, 5, 7], &[5, 3, 3]), [-1, 1, 0, -1]);
-        assert_eq!(assign_elements(&[10, 21, 30, 41], &[2, 1]), [0, 1, 0, 1]);
-
-        assert_eq!(with_sieve(&[8, 4, 3, 2, 4], &[4, 2]), [0, 0, -1, 1, 0]);
-        assert_eq!(with_sieve(&[2, 3, 5, 7], &[5, 3, 3]), [-1, 1, 0, -1]);
-        assert_eq!(with_sieve(&[10, 21, 30, 41], &[2, 1]), [0, 1, 0, 1]);
+        assert_eq!(count_substrings("1010101010"), 25);
+        assert_eq!(count_substrings("12936"), 11);
+        assert_eq!(count_substrings("5701283"), 18);
     }
 
     #[test]
