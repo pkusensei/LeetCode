a4a9836572ac446eb133f849f4b133c421e0700c
pkusensei <ZW_08@outlook.com>
2025-06-10 09:14:03 -0400

    3452

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,41 +6,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_score(points: &[i32], m: i32) -> i64 {
-    let mut left = 0;
-    let mut right = 10_i64.pow(15);
-    while left < right {
-        let mid = left + (right - left + 1) / 2;
-        if check(points, m.into(), mid) {
-            left = mid;
-        } else {
-            right = mid - 1;
-        }
-    }
-    left
-}
-
-fn check(points: &[i32], m: i64, min: i64) -> bool {
-    let [mut moves, mut prev] = [0, 0];
-    for (idx, num) in points.iter().map(|&v| i64::from(v)).enumerate() {
-        // number of increments to reach `min`
-        let need = (min + num - 1) / num - prev;
-        if need > 0 {
-            // back and forth, so that
-            // [idx] takes `need` moves
-            // [1+idx] takes `need-` moves
-            moves += 2 * need - 1;
-            prev = need - 1; // moves already spent on [1+idx]
-        } else if 1 + idx < points.len() {
-            // Current [idx] already satisfied, slide forward
-            moves += 1;
-            prev = 0;
-        }
-        if moves > m {
-            return false;
-        }
-    }
-    true
+pub fn sum_of_good_numbers(nums: Vec<i32>, k: i32) -> i32 {
+    let k = k as usize;
+    nums.iter()
+        .enumerate()
+        .filter_map(|(idx, &num)| {
+            if idx.checked_sub(k).is_none_or(|i| nums[i] < num)
+                && nums.get(idx + k).is_none_or(|&v| v < num)
+            {
+                Some(num)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -73,10 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_score(&[2, 4], 3), 4);
-        assert_eq!(max_score(&[1, 2, 3], 5), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
