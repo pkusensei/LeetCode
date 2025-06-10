d8051c0e1cb3f117ccef0ab8ca760ad1a4b6dbb4
pkusensei <ZW_08@outlook.com>
2025-06-10 09:43:27 -0400

    3453 binary search on f64

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,20 +6,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_of_good_numbers(nums: Vec<i32>, k: i32) -> i32 {
-    let k = k as usize;
-    nums.iter()
-        .enumerate()
-        .filter_map(|(idx, &num)| {
-            if idx.checked_sub(k).is_none_or(|i| nums[i] < num)
-                && nums.get(idx + k).is_none_or(|&v| v < num)
-            {
-                Some(num)
-            } else {
-                None
-            }
-        })
-        .sum()
+pub fn separate_squares(squares: Vec<Vec<i32>>) -> f64 {
+    let mut left = 0.0;
+    let mut right = f64::from(i32::MAX);
+    while (right - left) > EP {
+        let mid = (left + right) / 2.0;
+        if diff(&squares, mid) > 0.0 {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    left
+}
+
+const EP: f64 = 1e-5;
+
+fn diff(squares: &[Vec<i32>], mid: f64) -> f64 {
+    let [mut top, mut bot] = [0.0; 2];
+    for sq in squares {
+        let [y, side] = [1, 2].map(|i| f64::from(sq[i]));
+        if mid <= y {
+            top += side.powi(2);
+        } else if mid >= y + side {
+            bot += side.powi(2);
+        } else {
+            top += side * (y + side - mid);
+            bot += side * (mid - y);
+        }
+    }
+    top - bot
 }
 
 #[cfg(test)]
@@ -37,10 +53,10 @@ mod tests {
         }};
     }
 
-    const _EP: f64 = 1e-5;
     #[allow(unused_macros)]
     macro_rules! float_eq {
         ($a:expr, $b:expr) => {{
+            const _EP: f64 = 1e-5;
             let (left, right) = ($a, $b);
             assert!(
                 (left - right).abs() <= _EP,
@@ -52,7 +68,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        float_eq!(separate_squares(vec![vec![0, 0, 1], vec![2, 2, 1]]), 1.0);
+        float_eq!(
+            separate_squares(vec![vec![0, 0, 2], vec![1, 1, 1]]),
+            1.16667
+        );
+    }
 
     #[test]
     fn test() {}
