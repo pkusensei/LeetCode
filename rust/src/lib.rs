7b10f01ada66022cacae0c4977372c01cc60107a
pkusensei <ZW_08@outlook.com>
2025-06-11 11:02:14 -0400

    3463 didn't do it at all; and thanks Claude

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,23 +6,62 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_sum(mut grid: Vec<Vec<i32>>, limits: Vec<i32>, k: i32) -> i64 {
-    use std::{cmp::Reverse, collections::BinaryHeap};
-    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
-    for (row, limit) in grid.iter_mut().zip(limits) {
-        if limit > 0 {
-            row.select_nth_unstable_by_key(limit as usize - 1, |&v| Reverse(v));
-            heap.extend(&row[..limit as usize]);
-        }
+pub fn has_same_digits(s: &str) -> bool {
+    let n = s.len();
+    let [mut s1, mut s2] = [0, 0];
+    for (idx, w) in s.as_bytes().windows(2).enumerate() {
+        let val = bin_mod10(n - 2, idx); // C(n-2, idx) %10
+        let [d1, d2] = [0, 1].map(|i| usize::from(w[i] - b'0'));
+        s1 = (s1 + val * d1) % 10;
+        s2 = (s2 + val * d2) % 10;
     }
-    let mut res = 0;
-    for _ in 0..k {
-        let Some(v) = heap.pop().map(i64::from) else {
-            break;
-        };
-        res += v;
+    s1 == s2
+}
+
+fn bin_mod10(n: usize, k: usize) -> usize {
+    let mod2 = bin_mod2(n, k);
+    let mod5 = bin_mod5(n, k);
+    (0..10)
+        .find(|&i| i % 2 == mod2 && i % 5 == mod5)
+        .unwrap_or(0)
+}
+
+const fn bin_mod2(mut n: usize, mut k: usize) -> usize {
+    while k > 0 {
+        if k & 1 > n & 1 {
+            return 0;
+        }
+        n >>= 1;
+        k >>= 1;
+    }
+    1
+}
+
+fn bin_mod5(mut n: usize, mut k: usize) -> usize {
+    let mut res = 1;
+    while n > 0 || k > 0 {
+        let nd = n % 5;
+        let kd = k % 5;
+        if kd > nd {
+            return 0;
+        }
+        res = res * bin_small(nd, kd) % 5;
+        n /= 5;
+        k /= 5;
     }
     res
+}
+
+fn bin_small(n: usize, k: usize) -> usize {
+    // [0!, 1!, 2!, 3!, 4!] %5
+    const FACT: [usize; 5] = [1, 1, 2, 1, 4];
+    if k > n {
+        return 0;
+    }
+    let numerator = FACT[n];
+    let denominator = FACT[k] * FACT[n - k] % 5;
+    let den_inv = (0..5).find(|&i| (denominator * i) % 5 == 1).unwrap_or(0);
+    numerator * den_inv % 5
 }
 
 #[cfg(test)]
@@ -55,7 +94,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert!(has_same_digits("3902"));
+        assert!(!has_same_digits("34789"));
+    }
 
     #[test]
     fn test() {}
