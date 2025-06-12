a11b210d5f5934c9a1a181d8a94ad8d447843089
pkusensei <ZW_08@outlook.com>
2025-06-12 10:30:00 -0400

    3470 overflows and perms...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,27 +6,81 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost(nums: &[i32]) -> i32 {
-    let n = nums.len();
-    dfs(&nums, 1, 0, &mut vec![vec![-1; n]; n])
+pub fn permute(n: i32, k: i64) -> Vec<i32> {
+    let n = n as usize;
+    let mut res = Vec::with_capacity(n);
+    let type_ = if n & 1 == 1 { Some(1) } else { None };
+    construct(
+        n,
+        k as _,
+        type_,
+        &mut vec![false; 1 + n],
+        &mut vec![false; 1 + n],
+        &mut res,
+    );
+    res
 }
 
-fn dfs(nums: &[i32], idx: usize, prev: usize, memo: &mut [Vec<i32>]) -> i32 {
-    let n = nums.len();
-    if idx >= n {
-        return nums[prev];
+fn construct(
+    n: usize,
+    mut k: usize,
+    type_: Option<usize>,
+    seen_odd: &mut [bool],
+    seen_even: &mut [bool],
+    res: &mut Vec<i32>,
+) {
+    let count_odd = seen_odd
+        .iter()
+        .enumerate()
+        .filter(|&(i, &v)| i & 1 == 1 && !v)
+        .count();
+    let count_even = seen_even
+        .iter()
+        .enumerate()
+        .filter(|&(i, &v)| i > 0 && i & 1 == 0 && !v)
+        .count();
+    if count_odd + count_even == 0 {
+        return;
     }
-    if idx == n - 1 {
-        return nums[prev].max(nums[idx]);
+    let next_perm = if let Some(t) = type_ {
+        count(count_odd - t, count_even - (1 - t))
+    } else {
+        count(count_odd, count_even - 1)
+    };
+    for num in 1..=n {
+        if type_.is_none_or(|t| t == num & 1) {
+            if (num & 1 == 1 && seen_odd[num]) || (num & 1 == 0 && seen_even[num]) {
+                continue;
+            }
+            if next_perm.is_some_and(|v| v < k) {
+                k -= next_perm.unwrap();
+            } else {
+                if num & 1 == 1 {
+                    seen_odd[num] = true;
+                } else {
+                    seen_even[num] = true;
+                }
+                res.push(num as i32);
+                construct(n, k, Some(1 - (num & 1)), seen_odd, seen_even, res);
+                break;
+            }
+        }
     }
-    if memo[idx][prev] > -1 {
-        return memo[idx][prev];
-    }
-    let a = nums[prev].max(nums[idx]) + dfs(nums, 2 + idx, 1 + idx, memo);
-    let b = nums[prev].max(nums[1 + idx]) + dfs(nums, 2 + idx, idx, memo);
-    let c = nums[idx].max(nums[1 + idx]) + dfs(nums, 2 + idx, prev, memo);
-    memo[idx][prev] = a.min(b).min(c);
-    memo[idx][prev]
+}
+
+fn count(odd: usize, even: usize) -> Option<usize> {
+    const F: [usize; 21] = {
+        let mut f = [1; 21];
+        let mut i = 1;
+        while i < 21 {
+            f[i] = i * f[i - 1];
+            i += 1;
+        }
+        f
+    };
+    let fodd = F.get(odd);
+    let feven = F.get(even);
+    fodd.zip(feven).and_then(|(a, b)| a.checked_mul(*b))
 }
 
 #[cfg(test)]
@@ -60,8 +114,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_cost(&[6, 2, 8, 4]), 12);
-        assert_eq!(min_cost(&[2, 1, 3, 3]), 5);
+        assert_eq!(permute(4, 6), [3, 4, 1, 2]);
+        assert_eq!(permute(3, 2), [3, 2, 1]);
+        assert_eq!(permute(2, 3), []);
     }
 
     #[test]
