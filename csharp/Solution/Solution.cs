a155eb6ce0e193c93910ec597d26a02e61d6a921
pkusensei <ZW_08@outlook.com>
2025-06-12 20:41:55 -0400

    2616 binary search

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,39 +7,31 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxDifference(string s, int k)
+    public int MinimizeMax(int[] nums, int p)
     {
-        const int INF = int.MaxValue / 2;
-        int res = int.MinValue;
-        foreach (var a in "01234")
+        Array.Sort(nums);
+        int left = 0;
+        int right = 1_000_000_001;
+        while (left < right)
         {
-            foreach (var b in "01234")
+            int mid = left + (right - left) / 2;
+            if (Count(mid) >= p) { right = mid; }
+            else { left = 1 + mid; }
+        }
+        return left;
+
+        int Count(int mid)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Length - 1; i += 1)
             {
-                if (a == b) { continue; }
-                List<int> prefa = [0];
-                List<int> prefb = [0];
-                Dictionary<(int, int), int> seen = [];
-                int left = 0;
-                for (int idx = 0; idx < s.Length; idx += 1)
+                if (nums[1 + i] - nums[i] <= mid)
                 {
-                    prefa.Add(prefa.Last());
-                    prefb.Add(prefb.Last());
-                    if (s[idx] == a) { prefa[1 + idx] += 1; }
-                    if (s[idx] == b) { prefb[1 + idx] += 1; }
-                    while (left + k - 1 <= idx && prefa[left] < prefa[1 + idx] && prefb[left] < prefb[1 + idx])
-                    {
-                        var key = (prefa[left] & 1, prefb[left] & 1);
-                        int diff = prefa[left] - prefb[left];
-                        if (!seen.TryAdd(key, diff)) { seen[key] = Math.Min(seen[key], diff); }
-                        left += 1;
-                    }
-                    var key_ = (1 - (prefa[1 + idx] & 1), prefb[1 + idx] & 1);
-                    int diff_ = prefa[1 + idx] - prefb[1 + idx];
-                    int prev = seen.GetValueOrDefault(key_, INF);
-                    res = Math.Max(res, diff_ - prev);
+                    res += 1;
+                    i += 1;
                 }
             }
+            return res;
         }
-        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,11 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow("12233", 4, -1)]
-    [DataRow("1122211", 3, 1)]
-    [DataRow("110", 3, -1)]
-    public void TestMethod1(string s, int k, int exp)
+    [DataRow(new[] { 10, 1, 2, 7, 1, 3 }, 2, 1)]
+    [DataRow(new[] { 4, 2, 1, 2 }, 1, 0)]
+    public void TestMethod1(int[] s, int p, int exp)
     {
-        Assert.AreEqual(exp, sol.MaxDifference(s, k));
+        Assert.AreEqual(exp, sol.MinimizeMax(s, p));
     }
 
     [TestMethod]
