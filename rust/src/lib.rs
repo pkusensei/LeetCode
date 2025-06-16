e1560299ea16d668517533b52b9a26b2fab4f7d7
pkusensei <ZW_08@outlook.com>
2025-06-16 17:43:44 -0400

    3504 precompute and dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,25 +5,45 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::chain;
 
 pub fn longest_palindrome(s: String, t: String) -> i32 {
-    let (s1, n1) = (s.as_bytes(), s.len());
-    let (s2, n2) = (t.as_bytes(), t.len());
-    let mut res = 1;
-    for a in 0..n1 {
-        for b in a..=n1 {
-            for c in 0..n2 {
-                for d in c..=n2 {
-                    let it = chain!(&s1[a..b], &s2[c..d]);
-                    if is_palindrome(it.clone()) {
-                        res = res.max(it.count());
-                    }
-                }
+    use itertools::{Itertools, chain};
+    let s1 = s.into_bytes();
+    let s2 = t.into_bytes().into_iter().rev().collect_vec();
+    let pal1 = get_pals(&s1);
+    let pal2 = get_pals(&s2);
+    let (n1, n2) = (s1.len(), s2.len());
+    let mut res = chain!(pal1.iter(), pal2.iter()).copied().max().unwrap_or(1);
+    let mut dp = vec![vec![0; 1 + n2]; 1 + n1];
+    for i1 in 1..=n1 {
+        for i2 in 1..=n2 {
+            if s1[i1 - 1] == s2[i2 - 1] {
+                dp[i1][i2] = dp[i1][i2].max(1 + dp[i1 - 1][i2 - 1]);
+                res = res
+                    .max(2 * dp[i1][i2] + pal1.get(i1).unwrap_or(&0))
+                    .max(2 * dp[i1][i2] + pal2.get(i2).unwrap_or(&0));
             }
         }
     }
     res as i32
+}
+
+fn get_pals(s: &[u8]) -> Vec<usize> {
+    let n = s.len();
+    let mut res = vec![0; n];
+    for i in 0..n {
+        expand(s, i, i, &mut res);
+        expand(s, i, 1 + i, &mut res);
+    }
+    res
+}
+
+fn expand(s: &[u8], mut left: usize, mut right: usize, pals: &mut [usize]) {
+    while s.get(left).zip(s.get(right)).is_some_and(|(a, b)| a == b) {
+        pals[left] = pals[left].max(right + 1 - left);
+        left = left.wrapping_sub(1);
+        right += 1;
+    }
 }
 
 #[cfg(test)]
@@ -56,7 +76,12 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(longest_palindrome("a".into(), "a".into()), 2);
+        assert_eq!(longest_palindrome("abc".into(), "def".into()), 1);
+        assert_eq!(longest_palindrome("b".into(), "aaaa".into()), 4);
+        assert_eq!(longest_palindrome("abcde".into(), "ecdba".into()), 5);
+    }
 
     #[test]
     fn test() {}
