ecec5512529e8a9f836b8252db8933410622b782
pkusensei <ZW_08@outlook.com>
2025-06-17 10:17:57 -0400

    3508 hashmap and hashset and deque

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,100 +6,70 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(nums: &[i32], x: i32, k: i32) -> i64 {
-    let [x, k] = [x, k].map(|v| v as usize);
-    let ops = get_min_ops(&nums, x);
-    dfs(&ops, x, k, 0, &mut vec![vec![-1; ops.len()]; 1 + k])
+use std::collections::{HashMap, HashSet, VecDeque};
+
+struct Router {
+    set: HashSet<Packet>,
+    queue: VecDeque<Packet>,
+    dst_times: HashMap<i32, VecDeque<Packet>>,
+    n: usize,
 }
 
-fn dfs(ops: &[i64], x: usize, k: usize, idx: usize, memo: &mut [Vec<i64>]) -> i64 {
-    if k == 0 {
-        return 0;
+impl Router {
+    fn new(memoryLimit: i32) -> Self {
+        let n = memoryLimit as usize;
+        Self {
+            set: HashSet::with_capacity(n),
+            queue: VecDeque::with_capacity(n),
+            dst_times: HashMap::with_capacity(n),
+            n,
+        }
     }
-    if idx >= ops.len() {
-        return i64::MAX >> 1;
+
+    fn add_packet(&mut self, source: i32, destination: i32, timestamp: i32) -> bool {
+        let p = Packet {
+            time: timestamp,
+            src: source,
+            dst: destination,
+        };
+        if self.set.contains(&p) {
+            return false;
+        }
+        if self.queue.len() == self.n {
+            _ = self.forward_packet();
+        }
+        self.dst_times.entry(destination).or_default().push_back(p);
+        self.queue.push_back(p);
+        self.set.insert(p)
     }
-    if memo[k][idx] > -1 {
-        return memo[k][idx];
+
+    fn forward_packet(&mut self) -> Vec<i32> {
+        let Some(p) = self.queue.pop_front() else {
+            return vec![];
+        };
+        self.set.remove(&p);
+        let packets = self.dst_times.entry(p.dst).or_default();
+        if packets.front().is_some_and(|&v| v == p) {
+            packets.pop_front();
+        }
+        vec![p.src, p.dst, p.time]
     }
-    let skip = dfs(ops, x, k, 1 + idx, memo);
-    let take = ops[idx] + dfs(ops, x, k - 1, idx + x, memo);
-    memo[k][idx] = skip.min(take);
-    memo[k][idx]
+
+    fn get_count(&self, destination: i32, start_time: i32, end_time: i32) -> i32 {
+        let Some(packets) = self.dst_times.get(&destination) else {
+            return 0;
+        };
+        let i1 = packets.partition_point(|p| p.time < start_time);
+        let i2 = packets.partition_point(|p| p.time <= end_time);
+        (i2 - i1) as i32
+    }
 }
 
-// smh O(x) sum is too slow
-fn get_min_ops(nums: &[i32], x: usize) -> Vec<i64> {
-    use std::collections::BTreeMap;
-    let n = nums.len();
-    let mut ops = Vec::with_capacity(n - x + 1);
-    let [mut small, mut big] = [const { BTreeMap::new() }; 2];
-    let [mut small_count, mut big_count] = [0, 0];
-    let [mut small_sum, mut big_sum] = [0, 0];
-    for (idx, &right) in nums.iter().enumerate() {
-        let right = i64::from(right);
-        if small.last_key_value().is_none_or(|(&k, _)| k >= right) {
-            *small.entry(right).or_insert(0) += 1;
-            small_count += 1;
-            small_sum += right;
-        } else {
-            *big.entry(right).or_insert(0) += 1;
-            big_count += 1;
-            big_sum += right;
-        }
-        if idx >= x {
-            let left = i64::from(nums[idx - x]);
-            if let Some(v) = small.get_mut(&left) {
-                *v -= 1;
-                small_count -= 1;
-                small_sum -= left;
-                if *v == 0 {
-                    small.remove(&left);
-                }
-            } else if let Some(v) = big.get_mut(&left) {
-                *v -= 1;
-                big_count -= 1;
-                big_sum -= left;
-                if *v == 0 {
-                    big.remove(&left);
-                }
-            }
-        }
-        while small_count < big_count {
-            let Some((&k, v)) = big.iter_mut().next() else {
-                break;
-            };
-            *small.entry(k).or_insert(0) += 1;
-            small_count += 1;
-            small_sum += k;
-            *v -= 1;
-            big_count -= 1;
-            big_sum -= k;
-            if *v == 0 {
-                big.remove(&k);
-            }
-        }
-        while small_count > 1 + big_count {
-            let Some((&k, v)) = small.iter_mut().last() else {
-                break;
-            };
-            *big.entry(k).or_insert(0) += 1;
-            big_count += 1;
-            big_sum += k;
-            *v -= 1;
-            small_count -= 1;
-            small_sum -= k;
-            if *v == 0 {
-                small.remove(&k);
-            }
-        }
-        if idx >= x - 1 {
-            let med = small.keys().last().unwrap();
-            let curr = med * small_count - small_sum + big_sum - med * big_count;
-            ops.push(curr);
-        }
-    }
-    ops
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Packet {
+    time: i32,
+    src: i32,
+    dst: i32,
 }
 
 #[cfg(test)]
@@ -132,10 +102,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_operations(&[5, -2, 1, 3, 7, 3, 6, 4, -1], 3, 2), 8);
-        assert_eq!(min_operations(&[9, -2, -2, -2, 1, 5], 2, 2), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
