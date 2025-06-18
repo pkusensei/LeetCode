db094d46eabb6506da99bc9d019c9c2cda966c33
pkusensei <ZW_08@outlook.com>
2025-06-18 16:07:24 -0400

    3519 digit dp; conver to base x is ...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,68 +6,78 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn smallest_palindrome(s: &str, k: i32) -> String {
-    use itertools::Itertools;
-    let n = s.len();
-    let mut k = i64::from(k);
-    let freq = s.bytes().fold([0; 26], |mut acc, b| {
-        acc[usize::from(b - b'a')] += 1;
-        acc
-    });
-    let mut res = Vec::with_capacity((1 + n) / 2);
-    let mut fhalf = freq.map(|f| f / 2);
-    while res.len() < n / 2 {
-        let mut pushed = false;
-        for cand in 0..26 {
-            if fhalf[cand] > 0 {
-                fhalf[cand] -= 1;
-                let v = next_perm(&fhalf);
-                if v < k {
-                    k -= v;
-                    fhalf[cand] += 1;
-                } else {
-                    res.push(cand as u8 + b'a');
-                    pushed = true;
-                    break;
-                }
-            }
-        }
-        if !pushed {
-            return "".into();
+pub fn count_numbers(l: &str, r: &str, b: i32) -> i32 {
+    let mut s1 = parse(&l, b.into());
+    for v in s1.iter_mut().rev() {
+        if *v == 0 {
+            *v = b as u8 - 1;
+        } else {
+            *v -= 1;
+            break;
         }
     }
-    let rev = res.iter().copied().rev().collect_vec();
-    for (idx, f) in freq.iter().enumerate() {
-        if f & 1 == 1 {
-            res.push(idx as u8 + b'a');
-        }
-    }
-    res.extend(rev);
-    String::from_utf8(res).unwrap()
+    let s2 = parse(&r, b.into());
+    let a = solve(&s1, b as u8);
+    let b = solve(&s2, b as u8);
+    (b - a).rem_euclid(M)
 }
 
-fn next_perm(fhalf: &[i32; 26]) -> i64 {
-    let mut sum = 0;
-    let mut denoms = vec![];
-    for &f in fhalf {
-        if f > 0 {
-            let f = i64::from(f);
-            sum += f;
-            denoms.extend(2..=f);
+const M: i32 = 1_000_000_007;
+
+fn parse(s: &str, b: i64) -> Vec<u8> {
+    let mut curr = vec![0];
+    for d in s.bytes().map(|ch| ch - b'0') {
+        let mut carry = 0;
+        for i in curr.iter_mut() {
+            let prod = (*i) * 10 + carry;
+            *i = prod % b;
+            carry = prod / b;
+        }
+        while carry > 0 {
+            curr.push(carry % b);
+            carry /= b;
+        }
+        carry = i64::from(d);
+        for i in curr.iter_mut() {
+            let sum = *i + carry;
+            *i = sum % b;
+            carry = sum / b;
+            if carry < 1 {
+                break;
+            }
+        }
+        while carry > 0 {
+            curr.push(carry % b);
+            carry /= b;
         }
     }
-    let mut res = 1;
-    let mut idx = 0;
-    for i in 2..=sum {
-        res *= i;
-        while denoms.get(idx).is_some_and(|d| res % d == 0) {
-            res /= denoms[idx];
-            idx += 1;
-        }
-        if res > 1_000_000 {
-            return res;
-        }
+    let mut res: Vec<u8> = curr.iter().rev().map(|&v| v as u8).collect();
+    if res.is_empty() {
+        res = vec![0]
     }
+    res
+}
+
+fn solve(ds: &[u8], b: u8) -> i32 {
+    let n = ds.len();
+    dfs(ds, b, 0, true, 0, &mut vec![[[-1; 10]; 2]; n])
+}
+
+fn dfs(nums: &[u8], b: u8, idx: usize, tight: bool, prev: u8, memo: &mut [[[i32; 10]; 2]]) -> i32 {
+    if idx >= nums.len() {
+        return 1;
+    }
+    if memo[idx][usize::from(tight)][usize::from(prev)] > -1 {
+        return memo[idx][usize::from(tight)][usize::from(prev)];
+    }
+    let upper = if tight { nums[idx] } else { b - 1 };
+    let mut res = 0;
+    for d in prev..=upper {
+        let next_tight = tight && d == upper;
+        res += dfs(nums, b, 1 + idx, next_tight, d, memo);
+        res %= M;
+    }
+    memo[idx][usize::from(tight)][usize::from(prev)] = res;
     res
 }
 
@@ -102,13 +112,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(smallest_palindrome("aa", 2), "");
-        assert_eq!(smallest_palindrome("abba", 2), "baab");
-        assert_eq!(smallest_palindrome("bacab", 1), "abcba");
+        assert_eq!(count_numbers("23", "28", 8), 3);
+        assert_eq!(count_numbers("2", "7", 2), 2);
     }
 
     #[test]
     fn test() {
-        assert_eq!(smallest_palindrome("gnllllng", 6), "llgnngll");
+        assert_eq!(
+            count_numbers("3017011", "6014684453239676968884889861183768278549", 4),
+            52032
+        );
     }
 }
