347b1c002b000982cdd09c19aff587403638e74b
pkusensei <ZW_08@outlook.com>
2025-06-19 21:14:09 -0400

    3533 dfs/memo and reconstruct with memo table

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,23 +6,53 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn path_existence_queries(
-    n: i32,
-    nums: Vec<i32>,
-    max_diff: i32,
-    queries: Vec<Vec<i32>>,
-) -> Vec<bool> {
-    let n = n as usize;
-    let mut root: Vec<_> = (0..n).collect();
-    for (i, w) in nums.windows(2).enumerate() {
-        if w[1] - w[0] <= max_diff {
-            root[i + 1] = root[i];
+pub fn concatenated_divisibility(mut nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let n = nums.len();
+    nums.sort_unstable();
+    let mut memo = vec![vec![None; 1 + k as usize]; 1 << n];
+    if dfs(&nums, k, 0, 0, &mut memo) {
+        build(&nums, &mut memo, k, 0, 0)
+    } else {
+        vec![]
+    }
+}
+
+fn dfs(nums: &[i32], k: i32, rem: i32, mask: usize, memo: &mut [Vec<Option<bool>>]) -> bool {
+    let n = nums.len();
+    if n == mask.count_ones() as usize {
+        return rem == 0;
+    }
+    if let Some(v) = memo[mask][rem as usize] {
+        return v;
+    }
+    let mut res = false;
+    for (i, &num) in nums.iter().enumerate() {
+        if (mask >> i) & 1 == 0 {
+            let width = 1 + num.ilog10();
+            let nrem = (rem * 10i32.pow(width) % k + num) % k;
+            if dfs(nums, k, nrem, mask | (1 << i), memo) {
+                res = true;
+                break;
+            }
         }
     }
-    queries
-        .iter()
-        .map(|q| root[q[0] as usize] == root[q[1] as usize])
-        .collect()
+    memo[mask][rem as usize] = Some(res);
+    res
+}
+
+fn build(nums: &[i32], memo: &mut [Vec<Option<bool>>], k: i32, rem: i32, mask: usize) -> Vec<i32> {
+    for (i, &num) in nums.iter().enumerate() {
+        if (mask >> i) & 1 == 0 {
+            let width = 1 + num.ilog10();
+            let nrem = (rem * 10i32.pow(width) % k + num) % k;
+            if dfs(nums, k, nrem, mask | (1 << i), memo) {
+                let mut res = vec![num];
+                res.extend(build(nums, memo, k, nrem, mask | (1 << i)));
+                return res;
+            }
+        }
+    }
+    vec![]
 }
 
 #[cfg(test)]
@@ -55,7 +85,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(concatenated_divisibility(vec![3, 12, 45], 5), [3, 12, 45]);
+        assert_eq!(concatenated_divisibility(vec![10, 5], 10), [5, 10]);
+        assert_eq!(concatenated_divisibility(vec![1, 2, 3], 5), []);
+    }
 
     #[test]
     fn test() {}
