f3c10535ad59e6d0df6bb21e3c5637c7824079b5
Zx Wang <ZW_08@outlook.com>
2025-06-20 10:14:36 -0400

    3534 binary lifting... wth

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,53 +6,62 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn concatenated_divisibility(mut nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let n = nums.len();
-    nums.sort_unstable();
-    let mut memo = vec![vec![None; 1 + k as usize]; 1 << n];
-    if dfs(&nums, k, 0, 0, &mut memo) {
-        build(&nums, &mut memo, k, 0, 0)
-    } else {
-        vec![]
+pub fn path_existence_queries(
+    n: i32,
+    nums: &[i32],
+    max_diff: i32,
+    queries: &[[i32; 2]],
+) -> Vec<i32> {
+    use itertools::Itertools;
+    let n = n as usize;
+    let arr = nums
+        .iter()
+        .enumerate()
+        .map(|(i, &v)| (v, i))
+        .sorted_unstable()
+        .collect_vec();
+    let mut indices = vec![0; n];
+    for (ai, &(_, ni)) in arr.iter().enumerate() {
+        indices[ni] = ai;
     }
-}
-
-fn dfs(nums: &[i32], k: i32, rem: i32, mask: usize, memo: &mut [Vec<Option<bool>>]) -> bool {
-    let n = nums.len();
-    if n == mask.count_ones() as usize {
-        return rem == 0;
-    }
-    if let Some(v) = memo[mask][rem as usize] {
-        return v;
-    }
-    let mut res = false;
-    for (i, &num) in nums.iter().enumerate() {
-        if (mask >> i) & 1 == 0 {
-            let width = 1 + num.ilog10();
-            let nrem = (rem * 10i32.pow(width) % k + num) % k;
-            if dfs(nums, k, nrem, mask | (1 << i), memo) {
-                res = true;
-                break;
-            }
+    let h = 2 + n.ilog2() as usize;
+    let mut jumps = vec![vec![0; h]; n];
+    let mut left = 0;
+    for right in 0..n {
+        while arr[right].0 - arr[left].0 > max_diff {
+            jumps[left][0] = right - 1;
+            left += 1;
         }
     }
-    memo[mask][rem as usize] = Some(res);
+    while left < n {
+        jumps[left][0] = n - 1;
+        left += 1;
+    }
+    for right in 1..h {
+        for left in 0..n {
+            jumps[left][right] = jumps[jumps[left][right - 1]][right - 1];
+        }
+    }
+    let mut res = vec![];
+    for q in queries.iter() {
+        let [_a, _b] = [0, 1].map(|i| q[i] as usize);
+        if _a == _b {
+            res.push(0);
+            continue;
+        }
+        let a = indices[_a].min(indices[_b]);
+        let b = indices[_a].max(indices[_b]);
+        let mut curr = a;
+        let mut steps = 0;
+        for j in (0..h).rev() {
+            if jumps[curr][j] < b {
+                curr = jumps[curr][j];
+                steps += 1 << j;
+            }
+        }
+        res.push(if jumps[curr][0] >= b { 1 + steps } else { -1 });
+    }
     res
-}
-
-fn build(nums: &[i32], memo: &mut [Vec<Option<bool>>], k: i32, rem: i32, mask: usize) -> Vec<i32> {
-    for (i, &num) in nums.iter().enumerate() {
-        if (mask >> i) & 1 == 0 {
-            let width = 1 + num.ilog10();
-            let nrem = (rem * 10i32.pow(width) % k + num) % k;
-            if dfs(nums, k, nrem, mask | (1 << i), memo) {
-                let mut res = vec![num];
-                res.extend(build(nums, memo, k, nrem, mask | (1 << i)));
-                return res;
-            }
-        }
-    }
-    vec![]
 }
 
 #[cfg(test)]
@@ -86,11 +95,25 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(concatenated_divisibility(vec![3, 12, 45], 5), [3, 12, 45]);
-        assert_eq!(concatenated_divisibility(vec![10, 5], 10), [5, 10]);
-        assert_eq!(concatenated_divisibility(vec![1, 2, 3], 5), []);
+        assert_eq!(
+            path_existence_queries(5, &[1, 8, 3, 4, 2], 3, &[[0, 3], [2, 4]]),
+            [1, 1]
+        );
+        assert_eq!(
+            path_existence_queries(5, &[5, 3, 1, 9, 10], 2, &[[0, 1], [0, 2], [2, 3], [4, 3]]),
+            [1, 2, -1, 1]
+        );
+        assert_eq!(
+            path_existence_queries(3, &[3, 6, 1], 1, &[[0, 0], [0, 1], [1, 2]]),
+            [0, -1, -1]
+        );
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            path_existence_queries(3, &[7, 4, 7], 0, &[[2, 0], [1, 1], [2, 1], [2, 2]]),
+            [1, 0, -1, 0]
+        );
+    }
 }
