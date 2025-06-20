724efd9a06b80ca867afb2226c6576f706545e45
Zx Wang <ZW_08@outlook.com>
2025-06-20 10:21:07 -0400

    3536

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,62 +6,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn path_existence_queries(
-    n: i32,
-    nums: &[i32],
-    max_diff: i32,
-    queries: &[[i32; 2]],
-) -> Vec<i32> {
-    use itertools::Itertools;
-    let n = n as usize;
-    let arr = nums
-        .iter()
-        .enumerate()
-        .map(|(i, &v)| (v, i))
-        .sorted_unstable()
-        .collect_vec();
-    let mut indices = vec![0; n];
-    for (ai, &(_, ni)) in arr.iter().enumerate() {
-        indices[ni] = ai;
-    }
-    let h = 2 + n.ilog2() as usize;
-    let mut jumps = vec![vec![0; h]; n];
-    let mut left = 0;
-    for right in 0..n {
-        while arr[right].0 - arr[left].0 > max_diff {
-            jumps[left][0] = right - 1;
-            left += 1;
+pub fn max_product(mut n: i32) -> i32 {
+    let mut max1 = 0;
+    let mut max2 = 0;
+    while n > 0 {
+        let d = n % 10;
+        n /= 10;
+        if d >= max1 {
+            max2 = max1;
+            max1 = d;
+        } else if d > max2 {
+            max2 = d;
         }
     }
-    while left < n {
-        jumps[left][0] = n - 1;
-        left += 1;
-    }
-    for right in 1..h {
-        for left in 0..n {
-            jumps[left][right] = jumps[jumps[left][right - 1]][right - 1];
-        }
-    }
-    let mut res = vec![];
-    for q in queries.iter() {
-        let [_a, _b] = [0, 1].map(|i| q[i] as usize);
-        if _a == _b {
-            res.push(0);
-            continue;
-        }
-        let a = indices[_a].min(indices[_b]);
-        let b = indices[_a].max(indices[_b]);
-        let mut curr = a;
-        let mut steps = 0;
-        for j in (0..h).rev() {
-            if jumps[curr][j] < b {
-                curr = jumps[curr][j];
-                steps += 1 << j;
-            }
-        }
-        res.push(if jumps[curr][0] >= b { 1 + steps } else { -1 });
-    }
-    res
+    max1 * max2
 }
 
 #[cfg(test)]
@@ -94,26 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            path_existence_queries(5, &[1, 8, 3, 4, 2], 3, &[[0, 3], [2, 4]]),
-            [1, 1]
-        );
-        assert_eq!(
-            path_existence_queries(5, &[5, 3, 1, 9, 10], 2, &[[0, 1], [0, 2], [2, 3], [4, 3]]),
-            [1, 2, -1, 1]
-        );
-        assert_eq!(
-            path_existence_queries(3, &[3, 6, 1], 1, &[[0, 0], [0, 1], [1, 2]]),
-            [0, -1, -1]
-        );
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(
-            path_existence_queries(3, &[7, 4, 7], 0, &[[2, 0], [1, 1], [2, 1], [2, 2]]),
-            [1, 0, -1, 0]
-        );
-    }
+    fn test() {}
 }
