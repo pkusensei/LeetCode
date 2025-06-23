0729bd9449cee077d40e1262b6b36d4fda2b6ecb
pkusensei <ZW_08@outlook.com>
2025-06-22 21:26:26 -0400

    2081 brute force

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,29 +7,49 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinimumDeletions(string word, int k)
+    public long KMirror(int k, int n)
     {
-        Span<int> _freq = stackalloc int[26];
-        foreach (var item in word)
+        long res = 0;
+        List<int> s = [0];
+        for (int i = 0; i < n; i++)
         {
-            _freq[item - 'a'] += 1;
-        }
-        List<int> freq = [];
-        for (int i = 0; i < 26; i++)
-        {
-            if (_freq[i] > 0) { freq.Add(_freq[i]); }
-        }
-        freq.Sort();
-        int res = 0; // resulting length
-        for (int i = 0; i < freq.Count; i++)
-        {
-            int curr = 0;
-            foreach (var f in freq[i..])
+            while (true)
             {
-                curr += Math.Min(freq[i] + k, f);
+                s = Build(s, k);
+                long num = s.Aggregate(0L, (acc, v) => acc * k + v);
+                if (Check(num.ToString()))
+                {
+                    res += num;
+                    break;
+                }
             }
-            res = Math.Max(res, curr);
         }
-        return word.Length - res;
+        return res;
+
+        static List<int> Build(List<int> s, int k)
+        {
+            int n = s.Count;
+            int mid = n / 2;
+            for (int i1 = mid; i1 < n; i1++)
+            {
+                if (s[i1] + 1 < k)
+                {
+                    s[i1] += 1;
+                    s[n - 1 - i1] = s[i1];
+                    for (int i2 = mid; i2 < i1; i2++)
+                    {
+                        s[i2] = 0;
+                        s[n - 1 - i2] = 0;
+                    }
+                    return s;
+                }
+            }
+            List<int> res = [1];
+            res.AddRange(Enumerable.Range(0, n - 1).Select(_ => 0));
+            res.Add(1);
+            return res;
+        }
+
+        static bool Check(string s) => s.SequenceEqual(s.Reverse());
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(2, 5, 25)]
+    [DataRow(3, 7, 499)]
+    [DataRow(7, 17, 20379000)]
+    public void TestMethod1(int k, int n, int exp)
     {
+        Assert.AreEqual(exp, sol.KMirror(k, n));
     }
 
     [TestMethod]
