838db46034fe4f7ea205d980a8c4e7efa9918653
Zx Wang <ZW_08@outlook.com>
2025-06-24 14:19:30 -0400

    3556

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,53 +7,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-use binary_lifting::BinaryLifting;
-
-pub fn minimum_weight(edges: &[[i32; 3]], queries: &[[i32; 3]]) -> Vec<i32> {
-    let n = 1 + edges.len();
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push((b, e[2]));
-        acc[b].push((a, e[2]));
-        acc
-    });
-    let max_log = 1 + n.ilog2() as usize;
-    let mut jump = BinaryLifting::new(n, max_log);
-    let mut dist = vec![0; n];
-    dfs(&adj, 0, n, &mut dist, &mut jump);
-    jump.build();
-    queries
-        .iter()
-        .map(|q| {
-            let [s1, s2, d] = [0, 1, 2].map(|i| q[i] as usize);
-            (distance(s1, s2, &jump, &dist)
-                + distance(s1, d, &jump, &dist)
-                + distance(s2, d, &jump, &dist))
-                / 2
-        })
-        .collect()
-}
-
-fn dfs(
-    adj: &[Vec<(usize, i32)>],
-    node: usize,
-    prev: usize,
-    dist: &mut [i32],
-    jump: &mut BinaryLifting,
-) {
-    for &(next, w) in &adj[node] {
-        if next != prev {
-            jump.up[next][0] = node;
-            jump.depth[next] = 1 + jump.depth[node];
-            dist[next] = w + dist[node];
-            dfs(adj, next, node, dist, jump);
+pub fn sum_of_largest_primes(s: &str) -> i64 {
+    use std::collections::BTreeSet;
+    let n = s.len();
+    let mut seen = BTreeSet::new();
+    for a in 0..n {
+        for b in 1 + a..=n {
+            let Ok(v) = s[a..b].parse() else {
+                continue;
+            };
+            if is_prime(v) {
+                seen.insert(v);
+            }
         }
     }
+    let mut res = 0;
+    for _ in 0..3 {
+        let Some(v) = seen.pop_last() else {
+            break;
+        };
+        res += v;
+    }
+    res
 }
 
-fn distance(node1: usize, node2: usize, jump: &BinaryLifting, dist: &[i32]) -> i32 {
-    let lca_ = jump.lca(node1, node2);
-    dist[node1] + dist[node2] - 2 * dist[lca_]
+fn is_prime(n: i64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    for p in 2..=n.isqrt() {
+        if n % p == 0 {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -87,24 +74,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            minimum_weight(
-                &[[0, 1, 2], [1, 2, 3], [1, 3, 5], [1, 4, 4], [2, 5, 6]],
-                &[[2, 3, 4], [0, 2, 5]]
-            ),
-            [12, 11]
-        );
-        assert_eq!(minimum_weight(&[[1, 0, 8], [0, 2, 7]], &[[0, 1, 2]]), [15])
+        assert_eq!(sum_of_largest_primes("12234"), 1469);
+        assert_eq!(sum_of_largest_primes("111"), 11);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            minimum_weight(
-                &[[0, 3, 3], [1, 2, 8], [2, 3, 4]],
-                &[[2, 1, 0], [2, 1, 3], [2, 3, 1], [1, 2, 0]]
-            ),
-            [15, 12, 12, 15]
-        );
-    }
+    fn test() {}
 }
