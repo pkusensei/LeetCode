942473a5205c6c31000095448a2135d7ebb2cc86
Zx Wang <ZW_08@outlook.com>
2025-06-24 09:54:24 -0400

    3548 transpose and reverse matrix

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,9 +6,59 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_score(n: i32, edges: &[[i32; 2]]) -> i64 {
-    let n = i64::from(n);
-    (((2 * n + 3) * n - 11) * n + 6) / 6 + 2 * i64::from(edges.len() == n as usize)
+pub fn can_partition_grid(mut grid: Vec<Vec<i32>>) -> bool {
+    let sum = grid
+        .iter()
+        .flat_map(|r| r.iter().map(|&v| i64::from(v)))
+        .sum();
+    if check(&grid, sum) {
+        return true;
+    }
+    let mut t = transpose(&grid);
+    if check(&t, sum) {
+        return true;
+    }
+    grid.reverse();
+    t.reverse();
+    if check(&grid, sum) || check(&t, sum) {
+        return true;
+    }
+    false
+}
+
+fn transpose(grid: &[Vec<i32>]) -> Vec<Vec<i32>> {
+    let [rows, cols] = get_dimensions(grid);
+    let mut res = vec![vec![0; rows]; cols];
+    for r in 0..rows {
+        for c in 0..cols {
+            res[c][r] = grid[r][c];
+        }
+    }
+    res
+}
+
+fn check(grid: &[Vec<i32>], sum: i64) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    let mut top = 0;
+    for (r, row) in grid.iter().enumerate() {
+        for v in row.iter().map(|&v| i64::from(v)) {
+            top += v;
+            seen.insert(v);
+        }
+        let bot = sum - top;
+        let d = top - bot;
+        if d == 0
+            || d == i64::from(grid[0][0])
+            || d == i64::from(*grid[0].last().unwrap())
+            || d == i64::from(row[0])
+        {
+            return true;
+        }
+        if grid[0].len() > 1 && r > 0 && seen.contains(&d) {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -42,15 +92,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            max_score(6, &[[0, 3], [4, 5], [2, 0], [1, 3], [2, 4], [1, 5]]),
-            82
-        );
+        assert!(can_partition_grid(vec![vec![1, 4], vec![2, 3]]));
+        assert!(can_partition_grid(vec![vec![1, 2], vec![3, 4]]));
+        assert!(!can_partition_grid(vec![vec![1, 2, 4], vec![2, 3, 5]]));
+        assert!(!can_partition_grid(vec![vec![4, 1, 8], vec![3, 2, 6]]));
     }
 
     #[test]
     fn test() {
-        //  3 - 0 - 4 - 1 - 2
-        assert_eq!(max_score(5, &[[2, 1], [1, 4], [4, 0], [0, 3]]), 46);
+        assert!(can_partition_grid(vec![vec![5, 5, 6, 2, 2, 2]]))
     }
 }
