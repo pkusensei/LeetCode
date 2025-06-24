0b074317e1c72bb1c0b2106c78395538e679f41c
Zx Wang <ZW_08@outlook.com>
2025-06-24 15:25:19 -0400

    3558 2^(max_depth-1)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,35 +7,39 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_substrings(word: &str) -> i32 {
-    let mut freq = [const { vec![] }; 26];
-    for (i, b) in word.bytes().map(|b| usize::from(b - b'a')).enumerate() {
-        freq[b].push(i);
-    }
-    let mut spans = vec![];
-    for row in freq {
-        if row.len() < 2 {
-            continue;
-        }
-        for &left in &row {
-            let i = row.partition_point(|&v| v <= left || v - left < 3);
-            if let Some(&right) = row.get(i) {
-                spans.push([left, right]);
-            } else {
-                break;
-            }
-        }
-    }
-    spans.sort_unstable_by_key(|&[_, e]| e);
+pub fn assign_edge_weights(edges: &[[i32; 2]]) -> i32 {
+    let n = 1 + edges.len();
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize - 1);
+        acc[a].push(b);
+        acc[b].push(a);
+        acc
+    });
+    let depth = dfs(&adj, 0, n);
+    mod_pow(2, depth - 2) as i32
+}
+
+const M: i64 = 1_000_000_007;
+
+fn dfs(adj: &[Vec<usize>], node: usize, prev: usize) -> i64 {
     let mut res = 0;
-    let mut last = None;
-    for [start, end] in spans {
-        if last.is_none_or(|v| v < start) {
-            res += 1;
-            last = Some(end);
+    for &next in &adj[node] {
+        if next != prev {
+            res = res.max(dfs(adj, next, node))
         }
     }
-    res
+    1 + res
+}
+
+const fn mod_pow(b: i64, exp: i64) -> i64 {
+    if exp == 0 {
+        return 1;
+    }
+    if exp & 1 == 0 {
+        mod_pow(b * b % M, exp >> 1)
+    } else {
+        mod_pow(b * b % M, exp >> 1) * b % M
+    }
 }
 
 #[cfg(test)]
@@ -69,12 +73,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_substrings("abcdeafdef"), 2);
-        assert_eq!(max_substrings("bcdaaaab"), 1);
+        assert_eq!(assign_edge_weights(&[[1, 2], [1, 3], [3, 4], [3, 5]]), 2);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(max_substrings("abcceaddba"), 1);
-    }
+    fn test() {}
 }
