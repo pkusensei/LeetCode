eee7f3dd41129d94dde39e5cd6964f2d7b451d10
pkusensei <ZW_08@outlook.com>
2025-06-25 21:45:39 -0400

    2311 greedy backwards

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,47 +7,19 @@ namespace Solution;
 
 public class Solution
 {
-    public long KthSmallestProduct(int[] nums1, int[] nums2, long k)
+    public int LongestSubsequence(string s, int k)
     {
-        (var a1, var a2) = Prep(nums1);
-        (var b1, var b2) = Prep(nums2);
-        long neg_count = (long)a1.Count * b2.Count + (long)a2.Count * b1.Count;
-        if (k <= neg_count) { (b1, b2) = (b2, b1); }
-        int sign = k > neg_count ? 1 : -1;
-        k = k > neg_count ? k - neg_count : neg_count + 1 - k;
-        long left = 0;
-        long right = 10_000_000_001;
-        while (left < right)
+        int val = 1;
+        int res = 0;
+        for (int i = s.Length - 1; i >= 0; i -= 1)
         {
-            var mid = left + (right - left) / 2;
-            if (Count(a1, b1, mid) + Count(a2, b2, mid) >= k) { right = mid; }
-            else { left = 1 + mid; }
-        }
-        return sign * left;
-
-        static long Count(IList<long> arr1, IList<long> arr2, long mid)
-        {
-            long res = 0;
-            int i2 = arr2.Count;
-            foreach (var v1 in arr1)
+            if (s[i] == '0' || val <= k)
             {
-                while (i2 > 0 && v1 * arr2[i2 - 1] > mid) { i2 -= 1; }
-                res += i2;
+                k -= val * (s[i] - '0');
+                res += 1;
             }
-            return res;
+            if (val <= k) { val *= 2; }
         }
-
-        static (List<long>, List<long>) Prep(int[] nums)
-        {
-            List<long> v1 = [];
-            List<long> v2 = [];
-            foreach (var num in nums)
-            {
-                if (num < 0) { v1.Add(-num); }
-                else { v2.Add(num); }
-            }
-            v1.Reverse();
-            return (v1, v2);
-        }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,8 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 2, 5 }, new[] { 3, 4 }, 2, 8)]
-    [DataRow(new[] { -4, -2, 0, 3 }, new[] { 2, 4 }, 6, 0)]
-    [DataRow(new[] { -2, -1, 0, 1, 2 }, new[] { -3, -1, 2, 4, 5 }, 3, -6)]
-    public void TestMethod1(int[] a, int[] b, int k, int exp)
+    public void TestMethod1()
     {
-        Assert.AreEqual(exp, sol.KthSmallestProduct(a, b, k));
     }
 
     [TestMethod]
