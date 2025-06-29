4f6041e5f33d672f7c7207d096bd834aff3146a7
pkusensei <ZW_08@outlook.com>
2025-06-29 17:42:47 -0400

    3578 can never learn mono queue...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,30 +4,76 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::{BTreeMap, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn can_make_equal(nums: &[i32], k: i32) -> bool {
-    solve(nums, k, 1) || solve(nums, k, -1)
+const M: i32 = 1_000_000_007;
+
+pub fn count_partitions(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let mut dp = vec![0; 1 + n];
+    dp[0] = 1;
+    let mut window = 0;
+    let mut map = BTreeMap::new();
+    let mut left = 0;
+    for (right, &num) in nums.iter().enumerate() {
+        window = (window + dp[right]) % M;
+        *map.entry(num).or_insert(0) += 1;
+        while map
+            .keys()
+            .next()
+            .zip(map.keys().last())
+            .is_some_and(|(a, b)| b - a > k)
+        {
+            window = (window - dp[left]).rem_euclid(M);
+            let v = map.entry(nums[left]).or_insert(0);
+            *v -= 1;
+            if *v == 0 {
+                map.remove(&nums[left]);
+            }
+            left += 1;
+        }
+        dp[1 + right] = window;
+    }
+    dp[n]
 }
 
-fn solve(nums: &[i32], mut k: i32, target: i32) -> bool {
-    use itertools::Itertools;
-    let arr = nums
-        .iter()
-        .enumerate()
-        .filter_map(|(i, &v)| if v != target { Some(i) } else { None })
-        .collect_vec();
-    if arr.len() & 1 == 1 {
-        return false;
-    }
-    for ch in arr.chunks(2) {
-        k -= (ch[1] - ch[0]) as i32;
-        if k < 0 {
-            return false;
+pub fn with_deque(nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let mut dp = vec![0; 1 + n];
+    dp[0] = 1;
+    let mut acc: i32 = 1;
+    let mut left = 0;
+    let [mut minq, mut maxq] = [const { VecDeque::new() }; 2];
+    for (right, &num) in nums.iter().enumerate() {
+        while maxq.back().is_some_and(|(_, v)| *v < num) {
+            maxq.pop_back();
         }
+        maxq.push_back((right, num));
+        while minq.back().is_some_and(|(_, v)| *v > num) {
+            minq.pop_back();
+        }
+        minq.push_back((right, num));
+        while maxq
+            .front()
+            .zip(minq.front())
+            .is_some_and(|((_, a), (_, b))| a - b > k)
+        {
+            acc = (acc - dp[left]).rem_euclid(M);
+            left += 1;
+            if minq.front().is_some_and(|(i, _)| *i < left) {
+                minq.pop_front();
+            }
+            if maxq.front().is_some_and(|(i, _)| *i < left) {
+                maxq.pop_front();
+            }
+        }
+        dp[1 + right] = acc;
+        acc = (acc + dp[1 + right]) % M;
     }
-    k >= 0
+    dp[n]
 }
 
 #[cfg(test)]
@@ -60,7 +106,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_partitions(&[3, 3, 4], 0), 2);
+        assert_eq!(count_partitions(&[9, 4, 1, 3, 7], 4), 6);
+
+        assert_eq!(with_deque(&[3, 3, 4], 0), 2);
+        assert_eq!(with_deque(&[9, 4, 1, 3, 7], 4), 6);
+    }
 
     #[test]
     fn test() {}
