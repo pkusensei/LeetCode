91a0ecd3f37e03a3a3d5839406fa5ea1f56efd69
pkusensei <ZW_08@outlook.com>
2025-07-01 18:54:35 -0400

    3593 two dfs passes

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,23 +7,53 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_coins(mut nums: Vec<i32>) -> Vec<i32> {
-    let n = nums.len();
-    nums.insert(0, 1);
-    let mut res = vec![];
-    for c in 1..=n {
-        if nums[c] == 0 {
-            continue;
+pub fn min_increase(n: i32, edges: &[[i32; 2]], cost: &[i32]) -> i32 {
+    let n = n as usize;
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push(b);
+        acc[b].push(a);
+        acc
+    });
+    let mut max_until = vec![0; n];
+    let max = dfs_max(&adj, cost, 0, n, &mut max_until);
+    dfs_inc(&adj, cost, &max_until, 0, n, max, &mut vec![0; n])
+}
+
+fn dfs_max(
+    adj: &[Vec<usize>],
+    cost: &[i32],
+    node: usize,
+    prev: usize,
+    max_until: &mut [i64],
+) -> i64 {
+    let mut res = 0;
+    for &next in &adj[node] {
+        if next != prev {
+            res = res.max(dfs_max(adj, cost, next, node, max_until))
         }
-        if nums[c] > 1 {
-            return vec![];
-        }
-        res.push(c as i32);
-        for s in (c..=n).rev() {
-            nums[s] -= nums[s - c];
-            if nums[s] < 0 {
-                return vec![];
-            }
+    }
+    res += i64::from(cost[node]);
+    max_until[node] = res;
+    res
+}
+
+fn dfs_inc(
+    adj: &[Vec<usize>],
+    cost: &[i32],
+    max_until: &[i64],
+    node: usize,
+    prev: usize,
+    target: i64,
+    inc: &mut [i64],
+) -> i32 {
+    inc[node] = target - max_until[node];
+    let ntarget = target - i64::from(cost[node]);
+    let mut res = 0;
+    for &next in &adj[node] {
+        if next != prev {
+            res += dfs_inc(adj, cost, max_until, next, node, ntarget, inc);
+            res += i32::from(inc[node] != inc[next]);
         }
     }
     res
@@ -60,9 +90,12 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(find_coins(vec![0, 1, 0, 2, 0, 3, 0, 4, 0, 5]), [2, 4, 6]);
-        assert_eq!(find_coins(vec![1, 2, 2, 3, 4]), [1, 2, 5]);
-        assert_eq!(find_coins(vec![1, 2, 3, 4, 15]), []);
+        assert_eq!(min_increase(3, &[[0, 1], [0, 2]], &[2, 1, 3]), 1);
+        assert_eq!(min_increase(3, &[[0, 1], [1, 2]], &[5, 1, 4]), 0);
+        assert_eq!(
+            min_increase(5, &[[0, 4], [0, 1], [1, 2], [1, 3]], &[3, 4, 1, 1, 7]),
+            1
+        )
     }
 
     #[test]
