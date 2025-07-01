2c583950777246f42cdd0b349f85f59370c1e47a
pkusensei <ZW_08@outlook.com>
2025-07-01 12:22:07 -0400

    3591 one liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,55 +4,22 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::collections::BTreeSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn kth_smallest(par: &[i32], vals: &[i32], queries: &[[i32; 2]]) -> Vec<i32> {
-    let n = par.len();
-    let adj = par
-        .iter()
-        .enumerate()
-        .fold(vec![vec![]; n], |mut acc, (i, &v)| {
-            if v >= 0 {
-                acc[v as usize].push(i);
-            }
+pub fn check_prime_frequency(nums: Vec<i32>) -> bool {
+    use std::collections::HashMap;
+    const P: [i32; 25] = [
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+        97,
+    ];
+    nums.into_iter()
+        .fold(HashMap::new(), |mut acc, n| {
+            *acc.entry(n).or_insert(0) += 1;
             acc
-        });
-    let mut qs = vec![vec![]; n];
-    for (i, q) in queries.iter().enumerate() {
-        let [node, k] = [0, 1].map(|i| q[i] as usize);
-        qs[node].push((k - 1, i));
-    }
-    let mut res = vec![0; queries.len()];
-    dfs(&adj, &vals, 0, 0, &qs, &mut res);
-    res
-}
-
-fn dfs(
-    adj: &[Vec<usize>],
-    vals: &[i32],
-    node: usize,
-    mut path: i32,
-    qmap: &[Vec<(usize, usize)>],
-    res: &mut [i32],
-) -> BTreeSet<i32> {
-    path ^= vals[node];
-    let mut set = BTreeSet::from([path]);
-    for &next in &adj[node] {
-        let mut nset = dfs(adj, vals, next, path, qmap, res);
-        if nset.len() > set.len() {
-            nset.extend(set);
-            set = nset
-        } else {
-            set.extend(nset);
-        }
-    }
-    for &(k, i) in &qmap[node] {
-        res[i] = *set.iter().nth(k).unwrap_or(&-1);
-    }
-    set
+        })
+        .into_values()
+        .any(|v| P.contains(&v))
 }
 
 #[cfg(test)]
@@ -85,16 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            kth_smallest(&[-1, 0, 0], &[1, 1, 1], &[[0, 1], [0, 2], [0, 3]]),
-            [0, 1, -1]
-        );
-        assert_eq!(
-            kth_smallest(&[-1, 0, 1], &[5, 2, 7], &[[0, 1], [1, 2], [1, 3], [2, 1]]),
-            [0, 7, -1, 0]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
