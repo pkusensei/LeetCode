ace7344c9d68a5e66da631a839d82f15d6d8f713
pkusensei <ZW_08@outlook.com>
2025-07-01 21:58:09 -0400

    3333 no thanks

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,21 +8,40 @@ namespace Solution;
 
 public class Solution
 {
-    public int PossibleStringCount(string word)
+    public int PossibleStringCount(string word, int k)
     {
-        char prev = ' ';
-        int res = 1;
-        int curr = 0;
-        foreach (var item in word)
+        const long M = 1_000_000_007;
+        List<int> nums = [];
+        int curr = 1;
+        for (int i = 1; i < word.Length; i++)
         {
-            if (item != prev)
+            if (word[i] != word[i - 1])
             {
-                res += Math.Max(0, curr - 1);
+                nums.Add(curr);
                 curr = 0;
             }
-            prev = item;
             curr += 1;
         }
-        return res + curr - 1;
+        nums.Add(curr);
+        long prod = nums.Aggregate(1L, (acc, v) => acc * v % M);
+        if (k <= nums.Count) { return (int)prod; }
+        long[] dp = new long[k];
+        dp[0] = 1;
+        for (int idx = 1; idx <= nums.Count; idx++)
+        {
+            List<long> prefix = [0];
+            foreach (var item in dp)
+            {
+                prefix.Add((item + prefix.Last()) % M);
+            }
+            dp = new long[k];
+            for (int len = idx; len < k; len++)
+            {
+                int last = len - Math.Min(nums[idx - 1], len + 1 - idx);
+                dp[len] = (prefix[len] - prefix[last] + M) % M;
+            }
+        }
+        long lessThanK = dp.Aggregate(0L, (acc, v) => (acc + v) % M);
+        return (int)((prod - lessThanK + M) % M);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow("aabbccdd", 7, 5)]
+    [DataRow("aabbccdd", 8, 1)]
+    [DataRow("aaabbb", 3, 8)]
+    public void TestMethod1(string s, int k, int exp)
     {
+        Assert.AreEqual(exp, sol.PossibleStringCount(s, k));
     }
 
     [TestMethod]
