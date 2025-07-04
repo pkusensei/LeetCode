caf905175c01bd93af134d5ed842e94a92bbf9e9
pkusensei <ZW_08@outlook.com>
2025-07-04 10:10:48 -0400

    07 checked keyword in C#

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,41 +7,31 @@ namespace Solution;
 
 public class Solution
 {
-    public string LongestPalindrome(string s)
+    public int Reverse(int x)
     {
-        int n = s.Length;
-        bool[,] dp = new bool[n, n];
-        ReadOnlySpan<char> res = s.AsSpan(0, 1);
-        for (int i = 0; i < n; i++)
+        bool is_neg = x < 0;
+        if (is_neg) { x = -x; }
+        List<int> ds = [];
+        while (x > 0)
         {
-            dp[i, i] = true;
-            if (i < n - 1 && s[i] == s[1 + i])
-            {
-                dp[i, 1 + i] = true;
-                if (res.Length < 2) { res = s.AsSpan(i, 2); }
-            }
+            ds.Add(x % 10);
+            x /= 10;
         }
-        for (int mid = 0; mid < n; mid++)
+        try
         {
-            int left = mid - 1;
-            int right = 1 + mid;
-            while (0 <= left && right < n && s[left] == s[right] && dp[left + 1, right - 1])
+            int res = 0;
+            checked
             {
-                dp[left, right] = true;
-                if (res.Length < right + 1 - left) { res = s.AsSpan(left, right + 1 - left); }
-                left -= 1;
-                right += 1;
+                foreach (var d in ds)
+                {
+                    res = 10 * res + d;
+                }
             }
-            left = mid - 1;
-            right = 2 + mid;
-            while (0 <= left && right < n && s[left] == s[right] && dp[left + 1, right - 1])
-            {
-                dp[left, right] = true;
-                if (res.Length < right + 1 - left) { res = s.AsSpan(left, right + 1 - left); }
-                left -= 1;
-                right += 1;
-            }
+            return is_neg ? -res : res;
         }
-        return new(res);
+        catch (OverflowException)
+        {
+            return 0;
+        }
     }
 }
