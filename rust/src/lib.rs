e6838661d15d14a116b6ab9f5c7310c757336b2e
pkusensei <ZW_08@outlook.com>
2025-07-06 19:57:48 -0400

    3602

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,81 +7,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_median_sorted_arrays(nums1: &[i32], nums2: &[i32]) -> f64 {
-    fn dfs(
-        nums1: &[i32],
-        nums2: &[i32],
-        k: i32,
-        [start1, end1]: [i32; 2],
-        [start2, end2]: [i32; 2],
-    ) -> f64 {
-        if end1 < start1 {
-            return f64::from(nums2[(k - start1) as usize]);
-        }
-        if end2 < start2 {
-            return f64::from(nums1[(k - start2) as usize]);
-        }
-        let i1 = (start1 + end1) / 2;
-        let i2 = (start2 + end2) / 2;
-        if i1 + i2 < k {
-            if nums1[i1 as usize] > nums2[i2 as usize] {
-                dfs(nums1, nums2, k, [start1, end1], [1 + i2, end2])
-            } else {
-                dfs(nums1, nums2, k, [1 + i1, end1], [start2, end2])
-            }
-        } else {
-            if nums1[i1 as usize] > nums2[i2 as usize] {
-                dfs(nums1, nums2, k, [start1, i1 - 1], [start2, end2])
-            } else {
-                dfs(nums1, nums2, k, [start1, end1], [start2, i2 - 1])
-            }
-        }
-    }
-
-    let (n1, n2) = (nums1.len() as i32, nums2.len() as i32);
-    let n = n1 + n2;
-    if n & 1 == 1 {
-        dfs(nums1, nums2, n / 2, [0, n1 - 1], [0, n2 - 1])
-    } else {
-        (dfs(nums1, nums2, n / 2, [0, n1 - 1], [0, n2 - 1])
-            + dfs(nums1, nums2, n / 2 - 1, [0, n1 - 1], [0, n2 - 1]))
-            / 2.0
-    }
+pub fn concat_hex36(n: i32) -> String {
+    let mut a = convert(n.pow(2), 16);
+    a.extend(convert(n.pow(3), 36));
+    String::from_utf8(a).unwrap()
 }
 
-pub fn with_binary_search(nums1: &[i32], nums2: &[i32]) -> f64 {
-    if nums1.len() > nums2.len() {
-        return with_binary_search(nums2, nums1);
+fn convert(mut num: i32, base: i32) -> Vec<u8> {
+    let mut res = vec![];
+    while num > 0 {
+        let d = (num % base) as u8;
+        num /= base;
+        res.push(if d < 10 { b'0' + d } else { b'A' + d - 10 });
     }
-    let (n1, n2) = (nums1.len(), nums2.len());
-    let mut left = 0;
-    let mut right = n1;
-    while left <= right {
-        let partition_a = left.midpoint(right);
-        let partition_b = (1 + n1 + n2) / 2 - partition_a;
-        let max_left_a = partition_a
-            .checked_sub(1)
-            .map(|i| nums1[i])
-            .unwrap_or(i32::MIN);
-        let min_right_a = *nums1.get(partition_a).unwrap_or(&i32::MAX);
-        let max_left_b = partition_b
-            .checked_sub(1)
-            .map(|i| nums2[i])
-            .unwrap_or(i32::MIN);
-        let min_right_b = *nums2.get(partition_b).unwrap_or(&i32::MAX);
-        if max_left_a <= min_right_b && max_left_b <= min_right_a {
-            if (n1 + n2) & 1 == 0 {
-                return f64::from(max_left_a.max(max_left_b) + min_right_a.min(min_right_b)) / 2.0;
-            } else {
-                return max_left_a.max(max_left_b).into();
-            }
-        } else if max_left_a > max_left_b {
-            right = partition_a - 1;
-        } else {
-            left = 1 + partition_a;
-        }
-    }
-    0.0
+    res.reverse();
+    res
 }
 
 #[cfg(test)]
@@ -114,13 +54,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(find_median_sorted_arrays(&[1, 3], &[2]), 2.0);
-        assert_eq!(find_median_sorted_arrays(&[1, 2], &[3, 4]), 2.5);
-
-        assert_eq!(with_binary_search(&[1, 3], &[2]), 2.0);
-        assert_eq!(with_binary_search(&[1, 2], &[3, 4]), 2.5);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
