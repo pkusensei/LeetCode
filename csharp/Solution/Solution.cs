599873cd4d4ba5433d15abcadfd3b34e31808f54
pkusensei <ZW_08@outlook.com>
2025-07-05 20:33:10 -0400

    1865 counting

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,95 +5,42 @@ using static Solution.Utils;
 
 namespace Solution;
 
-public class Solution
+public class FindSumPairs
 {
-    public IList<IList<int>> ThreeSum(int[] nums)
+    int[] Nums2 { get; init; }
+    Dictionary<int, int> Dict1 { get; init; }
+    Dictionary<int, int> Dict2 { get; init; }
+
+    public FindSumPairs(int[] nums1, int[] nums2)
     {
-        Array.Sort(nums);
-        List<IList<int>> res = [];
-        for (int i = 0; i < nums.Length; i++)
+        Nums2 = nums2;
+        Dict1 = [];
+        Dict2 = [];
+        foreach (var item in nums1)
         {
-            if (nums[i] > 0) { break; }
-            if (i > 0 && nums[i] == nums[i - 1]) { continue; }
-            int left = 1 + i;
-            int right = nums.Length - 1;
-            while (left < right)
-            {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum < 0) { left += 1; }
-                else if (sum > 0) { right -= 1; }
-                else
-                {
-                    res.Add([nums[i], nums[left], nums[right]]);
-                    left += 1;
-                    while (left < right && nums[left] == nums[left - 1]) { left += 1; }
-                }
-            }
+            if (!Dict1.TryAdd(item, 1)) { Dict1[item] += 1; }
         }
-        return res;
+        foreach (var item in nums2)
+        {
+            if (!Dict2.TryAdd(item, 1)) { Dict2[item] += 1; }
+        }
     }
 
-    public int ThreeSumClosest(int[] nums, int target)
+    public void Add(int index, int val)
     {
-        Array.Sort(nums);
-        int res = nums[..3].Sum();
-        for (int i = 0; i < nums.Length - 2; i++)
-        {
-            int left = 1 + i;
-            int right = nums.Length - 1;
-            while (left < right)
-            {
-                int curr = nums[i] + nums[left] + nums[right];
-                if (Math.Abs(curr - target) < Math.Abs(res - target)) { res = curr; }
-                if (curr > target) { right -= 1; }
-                else if (curr < target) { left += 1; }
-                else { return res; }
-            }
-        }
-        return res;
+        int oldval = Nums2[index];
+        Dict2[oldval] -= 1;
+        Nums2[index] += val;
+        if (!Dict2.TryAdd(Nums2[index], 1)) { Dict2[Nums2[index]] += 1; }
     }
 
-    public IList<IList<int>> FourSum(int[] nums, int target)
+    public int Count(int tot)
     {
-        Array.Sort(nums);
-        return XSum(nums, target, 4);
-
-        static List<IList<int>> XSum(Span<int> nums, long target, int k)
+        int res = 0;
+        foreach (var (n1, v1) in Dict1)
         {
-            List<IList<int>> res = [];
-            if (nums.Length < 1) { return res; }
-            if (k == 2) { return TwoSum(nums, target); }
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (i == 0 || nums[i - 1] != nums[i])
-                {
-                    foreach (var item in XSum(nums[(1 + i)..], target - nums[i], k - 1))
-                    {
-                        res.Add([nums[i], .. item]);
-                    }
-                }
-            }
-            return res;
+            res += v1 * Dict2.GetValueOrDefault(tot - n1, 0);
         }
-
-        static List<IList<int>> TwoSum(Span<int> nums, long target)
-        {
-            List<IList<int>> res = [];
-            int left = 0;
-            int right = nums.Length - 1;
-            while (left < right)
-            {
-                long curr = nums[left] + nums[right];
-                if (curr < target || (left > 0 && nums[left] == nums[left - 1])) { left += 1; }
-                else if (curr > target) { right -= 1; }
-                else
-                {
-                    res.Add([nums[left], nums[right]]);
-                    left += 1;
-                    right -= 1;
-                }
-            }
-            return res;
-        }
+        return res;
     }
 }
