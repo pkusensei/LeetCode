b512b4a20f326a7ab72b53f8799766bf6895aa09
Zx Wang <ZW_08@outlook.com>
2025-07-08 10:20:13 -0400

    29 shift left until it's bigger

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,23 +7,21 @@ namespace Solution;
 
 public class Solution
 {
-    public int StrStr(string haystack, string needle)
+    public int Divide(int dividend, int divisor)
     {
-        int[] lps = new int[needle.Length];
-        int len = 0;
-        for (int i = 1; i < needle.Length; i++)
+        if (divisor == 0 || dividend == int.MinValue && divisor == -1) { return int.MaxValue; }
+        int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+        int res = 0;
+        long top = Math.Abs((long)dividend);
+        long bot = Math.Abs((long)divisor);
+        while (top >= bot)
         {
-            while (len > 0 && needle[i] != needle[len]) { len = lps[len - 1]; }
-            if (needle[i] == needle[len]) { len += 1; }
-            lps[i] = len;
+            int shift = 0;
+            while (top >= (bot << shift)) { shift += 1; }
+            shift -= 1; // Offset last shift to ensure top >= bot
+            res += 1 << shift;
+            top -= bot << shift;
         }
-        len = 0;
-        for (int i = 0; i < haystack.Length; i += 1)
-        {
-            while (len > 0 && haystack[i] != needle[len]) { len = lps[len - 1]; }
-            if (needle[len] == haystack[i]) { len += 1; }
-            if (len == needle.Length) { return i - len + 1; }
-        }
-        return -1;
+        return res * sign;
     }
 }
