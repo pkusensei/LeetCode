6589cd2f27f6abfc8434562d56a720f13d96423f
Zx Wang <ZW_08@outlook.com>
2025-07-08 12:11:29 -0400

    31 next perm

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,48 +7,32 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<int> FindSubstring(string s, string[] words)
+    public void NextPermutation(int[] nums)
     {
-        int word_len = words[0].Length;
-        int window_len = words.Length * word_len;
-        if (s.Length < window_len) { return []; }
-        Dictionary<string, int> _freq = [];
-        var freq = _freq.GetAlternateLookup<ReadOnlySpan<char>>();
-        foreach (var item in words)
+        int left = -1;
+        for (int i = nums.Length - 2; i >= 0; i -= 1)
         {
-            if (!freq.TryAdd(item, 1)) { freq[item] += 1; }
-        }
-        List<int> res = [];
-        for (int i = 0; i < word_len; i++)
-        {
-            Dictionary<string, int> _window = [];
-            var window = _window.GetAlternateLookup<ReadOnlySpan<char>>();
-            int left = i;
-            int count = 0;
-            for (int right = i; right + word_len <= s.Length; right += word_len)
+            if (nums[i] < nums[1 + i])
             {
-                var word = s.AsSpan(right, word_len);
-                if (freq.ContainsKey(word))
-                {
-                    if (!window.TryAdd(word, 1)) { window[word] += 1; }
-                    count += 1;
-                    while (window[word] > freq[word])
-                    {
-                        var left_span = s.AsSpan(left, word_len);
-                        window[left_span] -= 1;
-                        left += word_len;
-                        count -= 1;
-                    }
-                    if (count == words.Length) { res.Add(left); }
-                }
-                else
-                {
-                    _window.Clear();
-                    count = 0;
-                    left = right + word_len;
-                }
+                left = i;
+                break;
             }
         }
-        return res;
+        if (left == -1)
+        {
+            Array.Sort(nums);
+            return;
+        }
+        int right = -1;
+        for (int i = nums.Length - 1; i > left; i -= 1)
+        {
+            if (nums[left] < nums[i])
+            {
+                right = i;
+                break;
+            }
+        }
+        if (right != -1) { (nums[left], nums[right]) = (nums[right], nums[left]); }
+        Array.Reverse(nums, 1 + left, nums.Length - left - 1);
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(new[] { 1, 2, 3 }, new[] { 1, 3, 2 })]
+    [DataRow(new[] { 1, 3, 2 }, new[] { 2, 1, 3 })]
+    public void TestMethod1(int[] nums, int[] exp)
     {
+        sol.NextPermutation(nums);
+        Assert.IsTrue(nums.SequenceEqual(exp));
     }
 
     [TestMethod]
