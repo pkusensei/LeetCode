db6c67cd5947e29a1c91c26a0e693702d5f23c69
pkusensei <ZW_08@outlook.com>
2025-07-08 21:10:27 -0400

    3439 sliding window of size 1+k

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,54 +8,24 @@ namespace Solution;
 
 public class Solution
 {
-    public void SolveSudoku(char[][] board)
+    public int MaxFreeTime(int eventTime, int k, int[] startTime, int[] endTime)
     {
-        Backtrack();
-
-        bool Backtrack()
+        List<int> gaps = [];
+        int end = 0;
+        foreach (var (s, e) in startTime.Zip(endTime))
         {
-            for (int r = 0; r < 9; r++)
-            {
-                for (int c = 0; c < 9; c++)
-                {
-                    if (board[r][c] == '.')
-                    {
-                        for (int ch = '1'; ch <= '9'; ch += 1)
-                        {
-                            if (Check(r, c, (char)ch))
-                            {
-                                board[r][c] = (char)ch;
-                                if (Backtrack()) { return true; }
-                                else { board[r][c] = '.'; }
-                            }
-                        }
-                        return false;
-                    }
-                }
-            }
-            return true;
+            gaps.Add(s - end);
+            end = e;
         }
-
-        bool Check(int r, int c, char ch)
+        gaps.Add(eventTime - end);
+        int res = 0;
+        int curr = 0;
+        for (int i = 0; i < gaps.Count; i++)
         {
-            for (int rr = 0; rr < 9; rr++)
-            {
-                if (board[rr][c] == ch) { return false; }
-            }
-            for (int cc = 0; cc < 9; cc++)
-            {
-                if (board[r][cc] == ch) { return false; }
-            }
-            int start_r = r / 3 * 3;
-            int start_c = c / 3 * 3;
-            for (int dr = 0; dr < 3; dr++)
-            {
-                for (int dc = 0; dc < 3; dc++)
-                {
-                    if (board[start_r + dr][start_c + dc] == ch) { return false; }
-                }
-            }
-            return true;
+            curr += gaps[i];
+            if (i >= 1 + k) { curr -= gaps[i - 1 - k]; }
+            res = Math.Max(res, curr);
         }
+        return res;
     }
 }
