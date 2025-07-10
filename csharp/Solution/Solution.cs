023e6b1f7e93ce266963d7c8e7dbb84b70a8c4c7
Zx Wang <ZW_08@outlook.com>
2025-07-10 09:34:40 -0400

    46 backtrack; swap i with all idx>=i

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,25 +7,21 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxFreeTime(int eventTime, int[] startTime, int[] endTime)
+    public IList<IList<int>> Permute(int[] nums)
     {
-        int n = startTime.Length;
-        int res = 0;
-        int t1 = 0;
-        int t2 = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int left1 = i == 0 ? 0 : endTime[i - 1];
-            int right1 = i == n - 1 ? eventTime : startTime[i + 1];
-            if (endTime[i] - startTime[i] <= t1) { res = Math.Max(res, right1 - left1); }
-            t1 = Math.Max(t1, startTime[i] - (i == 0 ? 0 : endTime[i - 1]));
-            res = Math.Max(res, right1 - left1 - (endTime[i] - startTime[i]));
-
-            int left2 = i == n - 1 ? 0 : endTime[n - i - 2];
-            int right2 = i == 0 ? eventTime : startTime[n - i];
-            if (endTime[n - i - 1] - startTime[n - i - 1] <= t2) { res = Math.Max(res, right2 - left2); }
-            t2 = Math.Max(t2, (i == 0 ? eventTime : startTime[n - i]) - endTime[n - i - 1]);
-        }
+        List<IList<int>> res = [];
+        Backtrack(nums);
         return res;
+
+        void Backtrack(Span<int> span)
+        {
+            if (span.IsEmpty) { res.Add([.. nums]); }
+            for (int i = 0; i < span.Length; i++)
+            {
+                (span[0], span[i]) = (span[i], span[0]);
+                Backtrack(span[1..]);
+                (span[0], span[i]) = (span[i], span[0]);
+            }
+        }
     }
 }
