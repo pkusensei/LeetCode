1c4f041844f29f2200b7157fa3c26c7c84be6aec
pkusensei <ZW_08@outlook.com>
2025-07-10 19:18:22 -0400

    65 the most annoying

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using Solution.LList;
 using Solution.Tree;
 using static Solution.Utils;
@@ -9,29 +10,39 @@ public class Solution
 {
     public bool IsNumber(string s)
     {
-        int exp_idx = s.LastIndexOfAny(['e', 'E']);
-        if (exp_idx == 0) { return false; }
-        if (exp_idx > 0 && s[(1 + exp_idx)..].Any(c => !char.IsAsciiDigit(c))) { return false; }
-
-        string left = exp_idx > 0 ? s[..exp_idx] : s;
-        int dot_idx = left.LastIndexOf('.');
-        int start_idx = 0;
-        while (left[start_idx] == '+' || left[start_idx] == '-')
+        s = s.Trim();
+        int n = s.Length;
+        char prev = '#';
+        bool seen_e = false;
+        bool seen_dot = false;
+        bool seen_sign = false;
+        for (int i = 0; i < n; i++)
         {
-            start_idx += 1;
-        }
-        if (start_idx > 1) { return false; }
-        if (dot_idx < 0 && left[start_idx..].Any(c => !char.IsAsciiDigit(c))) { return false; }
-        if (dot_idx >= 0)
-        {
-            if (left.Length < 2) { return false; }
-            if (dot_idx > start_idx && left[start_idx..dot_idx].Any(c => !char.IsAsciiDigit(c)))
+            switch (s[i])
             {
-                return false;
-            }
-            if (left[(1 + dot_idx)..].Length > 0 && left[(1 + dot_idx)..].Any(c => !char.IsAsciiDigit(c)))
-            {
-                return false;
+                case char c when '0' <= c && c <= '9': prev = c; break;
+                case '+':
+                case '-':
+                    if ((prev != 'e' && prev != '#') || seen_sign || i == n - 1) { return false; }
+                    prev = s[i];
+                    seen_sign = true;
+                    break;
+                case 'e':
+                case 'E':
+                    if (seen_e || !char.IsAsciiDigit(prev) || i == n - 1) { return false; }
+                    prev = char.ToLower(s[i]);
+                    seen_e = true;
+                    seen_sign = false;
+                    break;
+                case '.':
+                    if (seen_dot || seen_e) { return false; }
+                    if (i == 0 && !(i < n - 1 && char.IsAsciiDigit(s[1 + i]))) { return false; }
+                    if (prev != '#' && !char.IsAsciiDigit(prev) && prev != '+' && prev != '-') { return false; }
+                    if ((prev == '+' || prev == '-') && !(i < n - 1 && char.IsAsciiDigit(s[1 + i]))) { return false; }
+                    seen_dot = true;
+                    seen_sign = false;
+                    break;
+                default: return false;
             }
         }
         return true;
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,6 +12,9 @@ public class UnitTest
     [TestMethod]
     [DataRow("0", true)]
     [DataRow(".", false)]
+    [DataRow("-1E+3", true)]
+    [DataRow(".1", true)]
+    [DataRow("+.8", true)]
     public void TestMethod1(string s, bool exp)
     {
         Assert.AreEqual(exp, sol.IsNumber(s));
