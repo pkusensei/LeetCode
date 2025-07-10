edd0e85ad7102e3335c6a77917aad119c0696d84
pkusensei <ZW_08@outlook.com>
2025-07-09 21:57:33 -0400

    3440 BTreeMaps feel better than this solution

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,18 +7,25 @@ namespace Solution;
 
 public class Solution
 {
-    public int Jump(int[] nums)
+    public int MaxFreeTime(int eventTime, int[] startTime, int[] endTime)
     {
-        int[] dp = new int[nums.Length];
-        Array.Fill(dp, int.MaxValue);
-        dp[0] = 0;
-        for (int i = 0; i < nums.Length; i++)
+        int n = startTime.Length;
+        int res = 0;
+        int t1 = 0;
+        int t2 = 0;
+        for (int i = 0; i < n; i++)
         {
-            for (int d = 1; d <= nums[i] && i + d < nums.Length; d++)
-            {
-                dp[i + d] = Math.Min(dp[i + d], 1 + dp[i]);
-            }
+            int left1 = i == 0 ? 0 : endTime[i - 1];
+            int right1 = i == n - 1 ? eventTime : startTime[i + 1];
+            if (endTime[i] - startTime[i] <= t1) { res = Math.Max(res, right1 - left1); }
+            t1 = Math.Max(t1, startTime[i] - (i == 0 ? 0 : endTime[i - 1]));
+            res = Math.Max(res, right1 - left1 - (endTime[i] - startTime[i]));
+
+            int left2 = i == n - 1 ? 0 : endTime[n - i - 2];
+            int right2 = i == 0 ? eventTime : startTime[n - i];
+            if (endTime[n - i - 1] - startTime[n - i - 1] <= t2) { res = Math.Max(res, right2 - left2); }
+            t2 = Math.Max(t2, (i == 0 ? eventTime : startTime[n - i]) - endTime[n - i - 1]);
         }
-        return dp.Last();
+        return res;
     }
 }
