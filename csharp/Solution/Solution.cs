4df7aed169593c63b771ccc76d408fb2d35ca9df
Zx Wang <ZW_08@outlook.com>
2025-07-10 12:08:59 -0400

    57 insert and merge intervals

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,26 +7,23 @@ namespace Solution;
 
 public class Solution
 {
-    public int[][] Merge(int[][] intervals)
+    public int[][] Insert(int[][] intervals, int[] newInterval)
     {
-        Array.Sort(intervals, (a, b) =>
-        {
-            if (a[0] == b[0]) { return a[1].CompareTo(b[1]); }
-            return a[0].CompareTo(b[0]);
-        });
         List<int[]> res = [];
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        foreach (var item in intervals.Skip(1))
+        int start = newInterval[0];
+        int end = newInterval[1];
+        foreach (var item in intervals)
         {
-            if (end < item[0])
+            if (end < item[0] || item[1] < start)
             {
-                res.Add([start, end]);
-                start = item[0];
+                res.Add(item);
+                continue;
             }
+            start = int.Min(start, item[0]);
             end = int.Max(end, item[1]);
         }
         res.Add([start, end]);
+        res.Sort((a, b) => a[0].CompareTo(b[0]));
         return [.. res];
     }
 }
