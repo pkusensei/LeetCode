e235a8cc8ffc785ac3af75c39b3d55a24e4e2840
Zx Wang <ZW_08@outlook.com>
2025-07-10 11:37:10 -0400

    53 refreshing on Kadane's

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,43 +7,15 @@ namespace Solution;
 
 public class Solution
 {
-    public int TotalNQueens(int n)
+    public int MaxSubArray(int[] nums)
     {
-        if (n == 1) { return 1; }
-        if (n == 2 || n == 3) { return 0; }
-        int res = 0;
-        Backtrack(0, []);
+        int res = int.MinValue;
+        int curr = 0;
+        foreach (var num in nums)
+        {
+            curr = int.Max(num, curr + num);
+            res = int.Max(res, curr);
+        }
         return res;
-
-        void Backtrack(int row, List<int> cols)
-        {
-            if (row == n)
-            {
-                res += 1;
-                return;
-            }
-            for (int col = 0; col < n; col++)
-            {
-                if (Check(cols, col))
-                {
-                    cols.Add(col);
-                    Backtrack(1 + row, cols);
-                    cols.RemoveAt(cols.Count - 1);
-                }
-            }
-        }
-
-        static bool Check(List<int> cols, int col)
-        {
-            for (int row = 0; row < cols.Count; row += 1)
-            {
-                if (col == cols[row]
-                    || Math.Abs(row - cols.Count) == Math.Abs(col - cols[row]))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
