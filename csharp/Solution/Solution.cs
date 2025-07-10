9d39bb98fc642e497a76358b3266c66740ccb09b
Zx Wang <ZW_08@outlook.com>
2025-07-10 14:59:34 -0400

    63 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,22 +7,25 @@ namespace Solution;
 
 public class Solution
 {
-    public int UniquePaths(int m, int n)
+    public int UniquePathsWithObstacles(int[][] obstacleGrid)
     {
+        int m = obstacleGrid.Length;
+        int n = obstacleGrid[0].Length;
         int[,] dp = new int[m, n];
-        for (int c = 0; c < n; c++)
+        dp[0, 0] = 1 - obstacleGrid[0][0];
+        for (int c = 1; c < n; c++)
         {
-            dp[0, c] = 1;
+            dp[0, c] = obstacleGrid[0][c] == 1 ? 0 : dp[0, c - 1];
         }
-        for (int r = 0; r < m; r++)
+        for (int r = 1; r < m; r++)
         {
-            dp[r, 0] = 1;
+            dp[r, 0] = obstacleGrid[r][0] == 1 ? 0 : dp[r - 1, 0];
         }
         for (int r = 1; r < m; r++)
         {
             for (int c = 1; c < n; c++)
             {
-                dp[r, c] = dp[r - 1, c] + dp[r, c - 1];
+                dp[r, c] = obstacleGrid[r][c] == 1 ? 0 : dp[r - 1, c] + dp[r, c - 1];
             }
         }
         return dp[m - 1, n - 1];
