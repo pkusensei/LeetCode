6e796deff719df7e887348800f37028aa071a4a9
Zx Wang <ZW_08@outlook.com>
2025-07-10 15:12:17 -0400

    64 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,27 +7,38 @@ namespace Solution;
 
 public class Solution
 {
-    public int UniquePathsWithObstacles(int[][] obstacleGrid)
+    public int MinPathSum(int[][] grid)
     {
-        int m = obstacleGrid.Length;
-        int n = obstacleGrid[0].Length;
-        int[,] dp = new int[m, n];
-        dp[0, 0] = 1 - obstacleGrid[0][0];
-        for (int c = 1; c < n; c++)
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        int[,] dp = new int[rows, cols];
+        for (int r = 0; r < rows; r++)
         {
-            dp[0, c] = obstacleGrid[0][c] == 1 ? 0 : dp[0, c - 1];
-        }
-        for (int r = 1; r < m; r++)
-        {
-            dp[r, 0] = obstacleGrid[r][0] == 1 ? 0 : dp[r - 1, 0];
-        }
-        for (int r = 1; r < m; r++)
-        {
-            for (int c = 1; c < n; c++)
+            for (int c = 0; c < cols; c++)
             {
-                dp[r, c] = obstacleGrid[r][c] == 1 ? 0 : dp[r - 1, c] + dp[r, c - 1];
+                dp[r, c] = int.MaxValue;
             }
         }
-        return dp[m - 1, n - 1];
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                if (r == 0 && c == 0) { dp[0, 0] = grid[0][0]; }
+                else if (r > 0 && c > 0)
+                {
+                    dp[r, c] = Math.Min(dp[r, c],
+                                    Math.Min(grid[r][c] + dp[r - 1, c], grid[r][c] + dp[r, c - 1]));
+                }
+                else if (r > 0)
+                {
+                    dp[r, c] = grid[r][c] + dp[r - 1, c];
+                }
+                else
+                {
+                    dp[r, c] = grid[r][c] + dp[r, c - 1];
+                }
+            }
+        }
+        return dp[rows - 1, cols - 1];
     }
 }
