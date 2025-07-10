13e6ff243b3666a3be07f1138c54dd729639a3d0
Zx Wang <ZW_08@outlook.com>
2025-07-10 13:01:57 -0400

    60 subtree size and factorial

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,23 +7,28 @@ namespace Solution;
 
 public class Solution
 {
-    public int[][] Insert(int[][] intervals, int[] newInterval)
+    static int[] F => [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+
+    public string GetPermutation(int n, int k)
     {
-        List<int[]> res = [];
-        int start = newInterval[0];
-        int end = newInterval[1];
-        foreach (var item in intervals)
+        List<char> cands = [.. Enumerable.Range(0, n).Select(i => (char)(i + '1'))];
+        List<char> res = [];
+        Dfs(res, cands, k);
+        return string.Concat(res);
+
+        static void Dfs(List<char> res, List<char> cands, int k)
         {
-            if (end < item[0] || item[1] < start)
+            if (cands.Count == 0) { return; }
+            int subtree = F[cands.Count - 1]; // e.g n=9, subtree=(8!)
+            int idx = 0;
+            while (k > subtree)
             {
-                res.Add(item);
-                continue;
+                k -= subtree;
+                idx += 1;
             }
-            start = int.Min(start, item[0]);
-            end = int.Max(end, item[1]);
+            res.Add(cands[idx]);
+            cands.RemoveAt(idx);
+            Dfs(res, cands, k);
         }
-        res.Add([start, end]);
-        res.Sort((a, b) => a[0].CompareTo(b[0]));
-        return [.. res];
     }
 }
