c5dc2c967afec109aa9f765489c25d445654b271
pkusensei <ZW_08@outlook.com>
2025-07-11 08:43:48 -0400

    70 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,44 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int MostBooked(int n, int[][] meetings)
+    public int ClimbStairs(int n)
     {
-        Array.Sort(meetings, (a, b) =>
+        if (n <= 1) { return 1; }
+        int dp0 = 1;
+        int dp1 = 1;
+        int dp2 = 0;
+        for (int i = 2; i <= n; i++)
         {
-            if (a[0] == b[0]) { return a[1].CompareTo(b[1]); }
-            return a[0].CompareTo(b[0]);
-        });
-        PriorityQueue<int, int> free = new();
-        for (int i = 0; i < n; i++)
-        {
-            free.Enqueue(i, i);
+            dp2 = dp0 + dp1;
+            (dp0, dp1) = (dp1, dp2);
         }
-        PriorityQueue<int, (long end, int id)> inuse = new();
-        int[] rooms = [.. Enumerable.Range(0, n).Select(_ => 0)];
-        foreach (var meet in meetings)
-        {
-            int room;
-            while (inuse.TryPeek(out room, out var prio) && prio.end <= meet[0])
-            {
-                inuse.Dequeue();
-                free.Enqueue(room, room);
-            }
-            if (free.TryDequeue(out room, out _))
-            {
-                inuse.Enqueue(room, (meet[1], room));
-            }
-            else
-            {
-                inuse.TryDequeue(out room, out var prio);
-                inuse.Enqueue(room, (prio.end + meet[1] - meet[0], room));
-            }
-            rooms[room] += 1;
-        }
-        int res = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (rooms[i] > rooms[res]) { res = i; }
-        }
-        return res;
+        return dp2;
     }
 }
