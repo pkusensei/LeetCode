58ba431dc627882719621dbcbd801d64c8c0e18f
pkusensei <ZW_08@outlook.com>
2025-07-11 22:09:01 -0400

    1900 this the only solution I could understand realy...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,21 +7,61 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    use std::collections::HashMap;
-    fn count(s: &str) -> [u8; 26] {
-        s.bytes().fold([0; 26], |mut acc, b| {
-            acc[usize::from(b - b'a')] += 1;
-            acc
-        })
+pub fn earliest_and_latest(n: i32, first_player: i32, second_player: i32) -> Vec<i32> {
+    let mut res = [i32::MAX, i32::MIN];
+    dfs(n, first_player, second_player, 0, 1, n, 1, &mut res);
+    res.to_vec()
+}
+
+fn dfs(
+    n: i32,
+    first: i32,
+    second: i32,
+    mask: i32,
+    mut left: i32,
+    mut right: i32,
+    round: i32,
+    res: &mut [i32; 2],
+) {
+    while left < right && (mask >> left) & 1 == 1 {
+        left += 1; // skip eliminated ones
     }
-    strs.into_iter()
-        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, s| {
-            acc.entry(count(&s)).or_default().push(s);
-            acc
-        })
-        .into_values()
-        .collect()
+    while left < right && (mask >> right) & 1 == 1 {
+        right -= 1;
+    }
+    if left >= right {
+        // end of round
+        dfs(n, first, second, mask, 1, n, 1 + round, res);
+    } else if left == first && right == second {
+        res[0] = res[0].min(round);
+        res[1] = res[1].max(round);
+    } else {
+        // could be eliminated
+        if left != first && left != second {
+            dfs(
+                n,
+                first,
+                second,
+                mask | (1 << left),
+                left + 1,
+                right - 1,
+                round,
+                res,
+            );
+        }
+        if right != first && right != second {
+            dfs(
+                n,
+                first,
+                second,
+                mask | (1 << right),
+                left + 1,
+                right - 1,
+                round,
+                res,
+            );
+        }
+    }
 }
 
 #[cfg(test)]
@@ -54,7 +94,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(earliest_and_latest(11, 2, 4), [3, 4]);
+        assert_eq!(earliest_and_latest(5, 1, 5), [1, 1]);
+    }
 
     #[test]
     fn test() {}
