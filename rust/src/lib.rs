51c2a6bcce0e43b8e2d725aa5edafb682f0d7a26
pkusensei <ZW_08@outlook.com>
2025-07-13 11:07:04 -0400

    3615 dfs/memo + bitmask

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,38 +7,60 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn process_str(s: &str, mut k: i64) -> char {
-    let mut len = 0_i64;
-    for b in s.bytes() {
-        match b {
-            b'*' => len = (len - 1).max(0),
-            b'#' => len *= 2,
-            b'%' => (),
-            _ => len += 1,
-        }
+pub fn max_len(n: i32, edges: &[[i32; 2]], label: &str) -> i32 {
+    let n = n as usize;
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        acc[a].push(b);
+        acc[b].push(a);
+        acc
+    });
+    let mut memo = vec![vec![vec![-1; n]; n]; 1 << n];
+    dfs(&adj, label.as_bytes(), 0, 0, 0, &mut memo)
+}
+
+fn dfs(
+    adj: &[Vec<usize>],
+    s: &[u8],
+    mask: usize,
+    left: usize,
+    right: usize,
+    memo: &mut [Vec<Vec<i32>>],
+) -> i32 {
+    let n = adj.len();
+    if memo[mask][left][right] > -1 {
+        return memo[mask][left][right];
     }
-    if k >= len {
-        return '.';
-    }
-    for ch in s.chars().rev() {
-        match ch {
-            '*' => len += 1,
-            '#' => {
-                len /= 2;
-                if k >= len {
-                    k -= len;
+    let mut res = 0;
+    if mask == 0 {
+        for mid in 0..n {
+            res = res.max(1 + dfs(adj, s, 1 << mid, mid, mid, memo));
+            for &next in &adj[mid] {
+                if s[mid] == s[next] {
+                    res = res.max(2 + dfs(adj, s, (1 << mid) | (1 << next), mid, next, memo))
                 }
             }
-            '%' => k = len - k - 1,
-            _ => {
-                len -= 1;
-                if k == len {
-                    return ch;
+        }
+    } else {
+        for &next1 in adj[left].iter().filter(|&&v| (mask >> v) & 1 == 0) {
+            for &next2 in adj[right].iter().filter(|&&v| (mask >> v) & 1 == 0) {
+                if next1 != next2 && s[next1] == s[next2] {
+                    res = res.max(
+                        2 + dfs(
+                            adj,
+                            s,
+                            mask | (1 << next1) | (1 << next2),
+                            next1,
+                            next2,
+                            memo,
+                        ),
+                    );
                 }
             }
         }
     }
-    '.'
+    memo[mask][left][right] = res;
+    res
 }
 
 #[cfg(test)]
@@ -72,12 +94,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(process_str("a#b%*", 1), 'a');
-        assert_eq!(process_str("cd%#*#", 3), 'd');
+        assert_eq!(max_len(3, &[[0, 1], [1, 2]], "aba"), 3);
+        assert_eq!(max_len(3, &[[0, 1], [0, 2]], "abc"), 1);
+        assert_eq!(max_len(4, &[[0, 2], [0, 3], [3, 1]], "bbac"), 3);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(process_str("jio#*g", 1), 'i');
-    }
+    fn test() {}
 }
