2c7760600203e871a6946cb5c43c1ce3a1b3a8ce
Zx Wang <ZW_08@outlook.com>
2025-07-15 09:48:18 -0400

    101 recursive and stack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,23 +7,39 @@ namespace Solution;
 
 public class Solution
 {
-    TreeNode prev = new(int.MinValue);
-    TreeNode n1 = null;
-    TreeNode n2 = null;
-
-    public void RecoverTree(TreeNode root)
+    public bool IsSymmetric(TreeNode root)
     {
-        Dfs(root);
-        (n1.val, n2.val) = (n2.val, n1.val);
+        if (root is null) { return true; }
+        return Dfs(root.left, root.right);
+
+        static bool Dfs(TreeNode left, TreeNode right)
+        {
+            if (left is null && right is null) { return true; }
+            if (left is null ^ right is null) { return false; }
+            return left.val == right.val
+                   && Dfs(left.left, right.right)
+                   && Dfs(left.right, right.left);
+        }
     }
 
-    void Dfs(TreeNode node)
+    public bool WithStack(TreeNode root)
     {
-        if (node is null) { return; }
-        Dfs(node.left);
-        if (n1 is null && prev.val > node.val) { n1 = prev; }
-        if (n1 is not null && prev.val > node.val) { n2 = node; }
-        prev = node;
-        Dfs(node.right);
+        if (root is null) { return true; }
+        Stack<TreeNode> st = [];
+        st.Push(root.left);
+        st.Push(root.right);
+        while (st.Count > 1)
+        {
+            var n1 = st.Pop();
+            var n2 = st.Pop();
+            if (n1 is null && n2 is null) { continue; }
+            if (n1 is null ^ n2 is null) { return false; }
+            if (n1.val != n2.val) { return false; }
+            st.Push(n1.left);
+            st.Push(n2.right);
+            st.Push(n1.right);
+            st.Push(n2.left);
+        }
+        return true;
     }
 }
