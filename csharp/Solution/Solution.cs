b485ae7d15fc69d43cab0302e1590fe6f7c1e4b4
Zx Wang <ZW_08@outlook.com>
2025-07-15 09:28:13 -0400

    99 inorder

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,21 +7,23 @@ namespace Solution;
 
 public class Solution
 {
-    public bool IsValid(string word)
+    TreeNode prev = new(int.MinValue);
+    TreeNode n1 = null;
+    TreeNode n2 = null;
+
+    public void RecoverTree(TreeNode root)
     {
-        if (word.Length < 3) { return false; }
-        bool vow = false;
-        bool con = false;
-        foreach (var c in word)
-        {
-            if (char.IsAsciiDigit(c)) { continue; }
-            if (char.IsAsciiLetter(c))
-            {
-                if ("aeiou".Contains(char.ToLower(c))) { vow = true; }
-                else { con = true; }
-            }
-            else { return false; }
-        }
-        return vow && con;
+        Dfs(root);
+        (n1.val, n2.val) = (n2.val, n1.val);
+    }
+
+    void Dfs(TreeNode node)
+    {
+        if (node is null) { return; }
+        Dfs(node.left);
+        if (n1 is null && prev.val > node.val) { n1 = prev; }
+        if (n1 is not null && prev.val > node.val) { n2 = node; }
+        prev = node;
+        Dfs(node.right);
     }
 }
