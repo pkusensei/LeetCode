185fd31f3d67c59da54f9319c0f0844d7e65c58c
pkusensei <ZW_08@outlook.com>
2025-07-16 08:38:10 -0400

    3201 greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,24 +7,29 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxPathSum(TreeNode root)
+    public int MaximumLength(int[] nums)
     {
-        int res = int.MinValue;
-        Dfs(root);
-        return res;
-
-        int Dfs(TreeNode node)
+        int even = 0;
+        int odd = 0;
+        int even_odd = 0;
+        int odd_even = 0;
+        foreach (var num in nums)
         {
-            if (node is null) { return 0; }
-            int v1 = node.val;
-            int left = Dfs(node.left);
-            int right = Dfs(node.right);
-            int v2 = node.val + left;
-            int v3 = node.val + right;
-            int v4 = node.val + left + right; // trimmed when recursing back up
-            int val = int.Max(int.Max(v1, v2), int.Max(v3, v4));
-            res = int.Max(res, val);
-            return int.Max(v1, int.Max(v2, v3));
+            int next_even_odd = even_odd;
+            int next_odd_even = odd_even;
+            if ((num & 1) == 0)
+            {
+                even += 1;
+                next_odd_even = 1 + even_odd;
+            }
+            else
+            {
+                odd += 1;
+                next_even_odd = 1 + odd_even;
+            }
+            even_odd = next_even_odd;
+            odd_even = next_odd_even;
         }
+        return int.Max(int.Max(even, odd), int.Max(even_odd, odd_even));
     }
 }
