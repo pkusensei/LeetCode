9b8ce9591d2e0431334eeefa9433f9936bbb9dd8
Zx Wang <ZW_08@outlook.com>
2025-07-17 14:46:35 -0400

    145 morris traversal

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -24,4 +24,35 @@ public class Solution
         }
         return res;
     }
+
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        if (root is null) { return []; }
+        List<int> res = [];
+        Stack<TreeNode> st = [];
+        var curr = root;
+        TreeNode prev = null;
+        while (curr is not null || st.Count > 0)
+        {
+            if (curr is not null)
+            {
+                st.Push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                var top = st.Peek();
+                if (top.right is not null && prev != top.right)
+                {
+                    curr = top.right;
+                }
+                else
+                {
+                    res.Add(top.val);
+                    prev = st.Pop();
+                }
+            }
+        }
+        return res;
+    }
 }
