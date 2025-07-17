aeab6d1db59e5014f7e04433b7d956585717a6fa
Zx Wang <ZW_08@outlook.com>
2025-07-17 12:00:51 -0400

    142 cycle start in linked list

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,70 +7,29 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<string> WordBreak(string s, IList<string> wordDict)
+    public ListNode DetectCycle(ListNode head)
     {
-        Trie tr = new();
-        foreach (var item in wordDict)
+        if (head is null) { return null; }
+        bool cycle = false;
+        var slow = head;
+        var fast = head;
+        while (fast is not null && fast.next is not null)
         {
-            tr.Insert(item);
-        }
-        List<List<int>> res = [];
-        tr.Dfs(s, 0, [], res);
-        return res.Select(v =>
-        {
-            int prev = 0;
-            StringBuilder sb = new();
-            foreach (var end in v)
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
             {
-                sb.Append(s[prev..end]);
-                sb.Append(' ');
-                prev = end;
-            }
-            sb.Remove(sb.Length - 1, 1);
-            return sb.ToString();
-        }).ToList();
-    }
-}
-
-class Trie
-{
-    public Trie()
-    {
-        Nodes = new Trie[26];
-        IsEnd = false;
-    }
-
-    public Trie[] Nodes { get; }
-    public bool IsEnd { get; private set; }
-
-    public void Insert(ReadOnlySpan<char> s)
-    {
-        var curr = this;
-        for (int i = 0; i < s.Length; i++)
-        {
-            curr = curr.Nodes[s[i] - 'a'] ??= new();
-        }
-        curr.IsEnd = true;
-    }
-
-    public void Dfs(ReadOnlySpan<char> s, int idx, List<int> indices, List<List<int>> res)
-    {
-        if (idx == s.Length)
-        {
-            res.Add([.. indices]);
-            return;
-        }
-        var curr = this;
-        for (int i = idx; i < s.Length; i++)
-        {
-            curr = curr.Nodes[s[i] - 'a'];
-            if (curr is null) { break; }
-            if (curr.IsEnd)
-            {
-                indices.Add(1 + i);
-                Dfs(s, 1 + i, indices, res);
-                indices.RemoveAt(indices.Count - 1);
+                cycle = true;
+                break;
             }
         }
+        if (!cycle) { return null; }
+        slow = head;
+        while (slow != fast)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
