68f0848d61f6ea8046c35fa6d67a3263af990cf9
pkusensei <ZW_08@outlook.com>
2025-07-19 10:43:27 -0400

    169 M*** voting algo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,31 +7,23 @@ namespace Solution;
 
 public class Solution
 {
-    public string FractionToDecimal(int numerator, int denominator)
+    public int MajorityElement(int[] nums)
     {
-        if (numerator == 0) { return "0"; }
-        StringBuilder sb = new();
-        if (numerator < 0 ^ denominator < 0) { sb.Append('-'); }
-        long num = long.Abs(numerator);
-        long den = long.Abs(denominator);
-        sb.Append(num / den);
-        num %= den;
-        if (num == 0) { return sb.ToString(); }
-        sb.Append('.');
-        Dictionary<long, int> seen = [];
-        while (num > 0)
+        int res = 0;
+        int count = 0;
+        foreach (var num in nums)
         {
-            if (seen.TryGetValue(num, out var idx))
+            if (num == res) { count += 1; }
+            else
             {
-                sb.Insert(idx, '(');
-                sb.Append(')');
-                break;
+                count -= 1;
+                if (count <= 0)
+                {
+                    res = num;
+                    count = 1;
+                }
             }
-            seen.Add(num, sb.Length);
-            num *= 10;
-            sb.Append(num / den);
-            num %= den;
         }
-        return sb.ToString();
+        return res;
     }
 }
