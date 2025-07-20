777638ca65857c36cf4efd5f943cf8f86036f003
Zx Wang <ZW_08@outlook.com>
2025-07-20 10:26:25 -0400

    3621 digit dp on bits

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,61 +4,56 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_max_path_score(edges: &[[i32; 3]], online: &[bool], k: i64) -> i32 {
-    let n = online.len();
-    let mut adj = vec![vec![]; n];
-    let mut max_e = 0;
-    for e in edges.iter() {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        if online[a] && online[b] {
-            adj[a].push((b, e[2]));
-            max_e = max_e.max(e[2]);
-        }
-    }
-    let mut left = 0;
-    let mut right = max_e + 1;
-    let mut reached = false;
-    while left < right {
-        let mid = left + (right - left + 1) / 2;
-        if check(&adj, k, mid) {
-            left = mid;
-            reached = true;
-        } else {
-            right = mid - 1;
-        }
-    }
-    if reached { left } else { -1 }
+pub fn popcount_depth(n: i64, k: i32) -> i64 {
+    let bit_width = 1 + n.ilog2() as usize;
+    let mut memo = vec![[[-1; 2]; 64]; bit_width];
+    dfs(n, k, bit_width - 1, 0, 1, &mut memo) - i64::from(k == 1)
 }
 
-fn check(adj: &[Vec<(usize, i32)>], k: i64, mid: i32) -> bool {
-    let n = adj.len();
-    let mut heap = BinaryHeap::from([(Reverse(0), 0)]);
-    let mut dists = vec![1 + k; n];
-    dists[0] = 0;
-    while let Some((Reverse(total), node)) = heap.pop() {
-        if node == n - 1 && total <= k {
-            return true;
+fn dfs(n: i64, k: i32, idx: usize, ones: usize, tight: usize, memo: &mut [[[i64; 2]; 64]]) -> i64 {
+    if memo[idx][ones][tight] > -1 {
+        return memo[idx][ones][tight];
+    }
+    let max_bit = if tight == 1 { (n >> idx) & 1 } else { 1 };
+    let mut res = 0;
+    if idx == 0 {
+        for bit in 0..=max_bit {
+            let curr_ones = ones + usize::from(bit == 1);
+            res += i64::from(DEPTH[curr_ones] == k);
         }
-        if total > dists[node] {
-            continue;
-        }
-        for &(next, cost) in &adj[node] {
-            if cost < mid {
-                continue;
-            }
-            let ntotal = total + i64::from(cost);
-            if ntotal < dists[next] {
-                dists[next] = ntotal;
-                heap.push((Reverse(ntotal), next));
-            }
+    } else {
+        for bit in 0..=max_bit {
+            let curr_ones = ones + usize::from(bit == 1);
+            let ntight = usize::from(tight > 0 && bit == max_bit);
+            res += dfs(n, k, idx - 1, curr_ones, ntight, memo)
         }
     }
-    false
+    memo[idx][ones][tight] = res;
+    res
+}
+
+const DEPTH: [i32; 64] = {
+    let mut depth = [-1; 64];
+    let mut x = 0;
+    while x < 64 {
+        precompute(x, &mut depth);
+        x += 1;
+    }
+    depth
+};
+
+const fn precompute(x: usize, depth: &mut [i32; 64]) -> i32 {
+    if x < 2 {
+        depth[x] = x as i32;
+    }
+    if depth[x] > -1 {
+        return depth[x];
+    }
+    depth[x] = 1 + precompute(x.count_ones() as _, depth);
+    depth[x]
 }
 
 #[cfg(test)]
@@ -92,33 +87,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            find_max_path_score(
-                &[[0, 1, 5], [1, 3, 10], [0, 2, 3], [2, 3, 4]],
-                &[true; 4],
-                10
-            ),
-            3
-        );
-        assert_eq!(
-            find_max_path_score(
-                &[
-                    [0, 1, 7],
-                    [1, 4, 5],
-                    [0, 2, 6],
-                    [2, 3, 6],
-                    [3, 4, 2],
-                    [2, 4, 6]
-                ],
-                &[true, true, true, false, true],
-                12
-            ),
-            6
-        );
+        assert_eq!(popcount_depth(4, 1), 2);
+        assert_eq!(popcount_depth(7, 2), 3);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(find_max_path_score(&[[0, 1, 8]], &[true, true], 11), 8);
-    }
+    fn test() {}
 }
