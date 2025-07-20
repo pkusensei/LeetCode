fddccd9c1031660576c17d063b1f61f118b0c088
pkusensei <ZW_08@outlook.com>
2025-07-20 18:45:57 -0400

    3625 way less appeasing than 3624

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,122 +7,52 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn popcount_depth(nums: Vec<i64>, queries: Vec<Vec<i64>>) -> Vec<i32> {
-    let mut tree = SegTree::new(&nums);
-    let mut res = vec![];
-    for q in queries {
-        if q[0] == 1 {
-            res.push(tree.query(q[1] as usize, q[2] as usize, q[3] as usize));
-        } else {
-            tree.update(q[1] as usize, q[2]);
+pub fn count_trapezoids(points: Vec<Vec<i32>>) -> i32 {
+    use itertools::Itertools;
+    use std::collections::HashMap;
+    let mut slopes = HashMap::new();
+    let mut lines = HashMap::new();
+    let mut mids = HashMap::new();
+    let mut midlines = HashMap::new();
+    for [p1, p2] in points.iter().array_combinations() {
+        let [x1, y1] = p1[..] else { unreachable!() };
+        let [x2, y2] = p2[..] else { unreachable!() };
+        let mut dx = x2 - x1;
+        let mut dy = y2 - y1;
+        let gcd_ = gcd(dx.abs(), dy.abs());
+        dx /= gcd_;
+        dy /= gcd_;
+        if dx < 0 || (dx == 0 && dy < 0) {
+            dx *= -1;
+            dy *= -1;
         }
+        let intercept = dx * y1 - dy * x1;
+        *slopes.entry([dx, dy]).or_insert(0) += 1;
+        *lines.entry([dx, dy, intercept]).or_insert(0) += 1;
+        *mids.entry([x1 + x2, y1 + y2]).or_insert(0) += 1;
+        *midlines
+            .entry([x1 + x2, y1 + y2, dx, dy, intercept])
+            .or_insert(0) += 1;
     }
-    res
+    // count_all(slopes) - count_all(segments on each line)
+    // Then to subtract parallelograms
+    // count_all(line with specific length) - count_all(specific length on line)
+    let res =
+        sum_up(slopes.into_values()) - sum_up(lines.into_values()) - sum_up(mids.into_values())
+            + sum_up(midlines.into_values());
+    res as i32
 }
 
-struct SegTree {
-    tree: Vec<[i32; 6]>,
-    n: usize,
+fn sum_up(it: impl Iterator<Item = usize>) -> usize {
+    it.map(n_choose_2).sum()
 }
 
-impl SegTree {
-    fn new(nums: &[i64]) -> Self {
-        let n = nums.len();
-        let mut s = Self {
-            tree: vec![[0; 6]; 4 * n],
-            n,
-        };
-        s.build(1, 0, n - 1, nums);
-        s
-    }
-
-    fn build(&mut self, node: usize, left: usize, right: usize, nums: &[i64]) {
-        if left == right {
-            let val = nums[left]; // bits(val)==1 but depth(val)==0
-            let k = DEPTH[val.count_ones() as usize] as usize - usize::from(val == 1);
-            self.tree[node][k] = 1;
-            return;
-        }
-        let mid = left.midpoint(right);
-        self.build(2 * node, left, mid, nums);
-        self.build(2 * node + 1, 1 + mid, right, nums);
-        self.merge(node);
-    }
-
-    fn update(&mut self, idx: usize, val: i64) {
-        self._update(1, 0, self.n - 1, idx, val);
-    }
-
-    fn _update(&mut self, node: usize, left: usize, right: usize, idx: usize, val: i64) {
-        if left == right {
-            let k = DEPTH[val.count_ones() as usize] as usize - usize::from(val == 1);
-            self.tree[node].fill(0);
-            self.tree[node][k] = 1;
-            return;
-        }
-        let mid = left.midpoint(right);
-        if idx <= mid {
-            self._update(2 * node, left, mid, idx, val);
-        } else {
-            self._update(2 * node + 1, 1 + mid, right, idx, val);
-        }
-        self.merge(node);
-    }
-
-    fn query(&self, ql: usize, qr: usize, k: usize) -> i32 {
-        self._query(1, 0, self.n - 1, ql, qr, k)
-    }
-
-    fn _query(
-        &self,
-        node: usize,
-        left: usize,
-        right: usize,
-        ql: usize,
-        qr: usize,
-        k: usize,
-    ) -> i32 {
-        if qr < left || right < ql {
-            return 0;
-        }
-        if ql <= left && right <= qr {
-            return self.tree[node][k];
-        }
-        let mid = left.midpoint(right);
-        self._query(2 * node, left, mid, ql, qr, k)
-            + self._query(2 * node + 1, 1 + mid, right, ql, qr, k)
-    }
-
-    fn merge(&mut self, node: usize) {
-        let left = self.tree[2 * node];
-        let right = self.tree[2 * node + 1];
-        let mut curr = [0; 6];
-        for (v, (a, b)) in curr.iter_mut().zip(left.iter().zip(right)) {
-            *v = a + b;
-        }
-        self.tree[node] = curr;
-    }
+const fn n_choose_2(n: usize) -> usize {
+    n * (n - 1) / 2
 }
 
-const DEPTH: [i8; 64] = {
-    let mut depth = [-1; 64];
-    let mut x = 0;
-    while x < 64 {
-        precompute(x, &mut depth);
-        x += 1;
-    }
-    depth
-};
-
-const fn precompute(x: usize, depth: &mut [i8; 64]) -> i8 {
-    if x < 2 {
-        depth[x] = (x & 1) as _;
-    }
-    if depth[x] > -1 {
-        return depth[x];
-    }
-    depth[x] = 1 + precompute(x.count_ones() as usize, depth);
-    depth[x]
+const fn gcd(a: i32, b: i32) -> i32 {
+    if a == 0 { b } else { gcd(b % a, a) }
 }
 
 #[cfg(test)]
