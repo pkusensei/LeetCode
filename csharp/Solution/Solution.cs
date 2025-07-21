29d99e04d283a87e71aba021881b2bece250bf20
pkusensei <ZW_08@outlook.com>
2025-07-20 20:54:22 -0400

    1957

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,83 +7,14 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<IList<string>> DeleteDuplicateFolder(IList<IList<string>> paths)
-    {
-        Trie tr = new("");
-        foreach (var p in paths)
-        {
-            tr.Add(p);
-        }
-        Dictionary<string, Trie> seen = [];
-        tr.Mark(seen);
-        List<IList<string>> res = [];
-        foreach (var v in tr.Nodes.Values)
-        {
-            v.Build([], res);
-        }
-        return res;
-    }
-}
-
-internal class Trie
-{
-    public string Name { get; }
-    public SortedList<string, Trie> Nodes { get; }
-    public bool Del { get; set; }
-
-    public Trie(string name)
-    {
-        Name = name;
-        Nodes = [];
-        Del = false;
-    }
-
-    public void Add(IList<string> paths)
-    {
-        var curr = this;
-        foreach (var p in paths)
-        {
-            if (!curr.Nodes.TryGetValue(p, out var node))
-            {
-                node = new(p);
-                curr.Nodes.Add(p, node);
-            }
-            curr = node;
-        }
-    }
-
-    public string Mark(IDictionary<string, Trie> seen)
+    public string MakeFancyString(string s)
     {
         StringBuilder sb = new();
-        foreach (var v in Nodes.Values)
+        foreach (var ch in s)
         {
-            sb.Append(v.Mark(seen));
+            if (sb.Length >= 2 && sb[^1] == ch && sb[^2] == ch) { continue; }
+            else { sb.Append(ch); }
         }
-        var s = sb.ToString();
-        if (s.Length > 0)
-        {
-            if (seen.TryGetValue(s, out var node))
-            {
-                node.Del = true;
-                Del = true;
-            }
-            else
-            {
-                seen.Add(s, this);
-            }
-        }
-        return $"({Name}{s})";
-    }
-
-    public void Build(List<string> curr, List<IList<string>> res)
-    {
-        if (Del) { return; }
-        curr.Add(Name);
-        res.Add([.. curr]);
-        foreach (var v in Nodes.Values)
-        {
-            v.Build(curr, res);
-        }
-        curr.RemoveAt(curr.Count - 1);
+        return sb.ToString();
     }
 }
