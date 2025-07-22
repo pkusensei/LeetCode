81cbe8faf80bac9ebaaf19ef9374b58aa0c8bbcf
Zx Wang <ZW_08@outlook.com>
2025-07-22 12:05:28 -0400

    220 bucket sort and sliding window

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,30 +7,29 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<IList<int>> GetSkyline(int[][] buildings)
+    public bool ContainsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff)
     {
-        List<(int x, int y)> arr = [];
-        foreach (var b in buildings)
+        Dictionary<int, int> window = [];
+        for (int i = 0; i < nums.Length; i++)
         {
-            arr.Add((b[0], -b[2]));
-            arr.Add((b[1], b[2]));
-        }
-        arr.Sort();
-        SortedList<int, int> dict = [];
-        List<IList<int>> res = [];
-        int prev = 0;
-        foreach (var (x, y) in arr)
-        {
-            if (y < 0 && !dict.TryAdd(-y, 1)) { dict[-y] += 1; }
-            else if (y > 0)
+            int buc = BucNum(nums[i], valueDiff);
+            if (!window.TryAdd(buc, nums[i])) { return true; }
+            if (window.TryGetValue(buc - 1, out var v) && nums[i] - v <= valueDiff) { return true; }
+            if (window.TryGetValue(buc + 1, out v) && v - nums[i] <= valueDiff) { return true; }
+            if (window.Count > indexDiff)
             {
-                dict[y] -= 1;
-                if (dict[y] == 0) { dict.Remove(y); }
+                int num = nums[i - indexDiff];
+                buc = BucNum(num, valueDiff);
+                window.Remove(buc);
             }
-            var height = dict.Keys.LastOrDefault();
-            if (height != prev) { res.Add([x, height]); }
-            prev = height;
         }
-        return res;
+        return false;
+
+        static int BucNum(int num, int diff)
+        {
+            int b = num / (1 + diff);
+            if (num < 0) { b -= 1; }
+            return b;
+        }
     }
 }
