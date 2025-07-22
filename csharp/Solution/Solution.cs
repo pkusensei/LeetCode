cb4ae0eea72b5aada41aafb0a05227d29d75ab86
Zx Wang <ZW_08@outlook.com>
2025-07-22 10:26:10 -0400

    216 bitmask

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,17 +7,37 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindKthLargest(int[] nums, int k)
+    public IList<IList<int>> CombinationSum3(int k, int n)
     {
-        if (nums.Length == 0) { return 0; }
-        int pivot = nums[^k];
-        int[] left = [.. nums.Where(v => v > pivot)];
-        int[] mid = [.. nums.Where(v => v == pivot)];
-        int[] right = [.. nums.Where(v => v < pivot)];
-        int n1 = left.Length;
-        int n2 = mid.Length;
-        if (k <= n1) { return FindKthLargest(left, k); }
-        else if (n1 + n2 < k) { return FindKthLargest(right, k - n1 - n2); }
-        else return mid[0];
+        List<IList<int>> res = [];
+        for (int mask = 0; mask < 1 << 10; mask++)
+        {
+            if ((mask & 1) == 0 && BitCount(mask) == k)
+            {
+                List<int> curr = [];
+                int sum = 0;
+                for (int bit = 1; bit <= 9; bit++)
+                {
+                    if (((mask >> bit) & 1) == 1)
+                    {
+                        sum += bit;
+                        curr.Add(bit);
+                    }
+                }
+                if (sum == n) { res.Add(curr); }
+            }
+        }
+        return res;
+
+        static int BitCount(int x)
+        {
+            int res = 0;
+            while (x != 0)
+            {
+                x &= x - 1;
+                res += 1;
+            }
+            return res;
+        }
     }
 }
