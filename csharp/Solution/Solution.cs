cf59dd146181d1ce1f15c424c9f480fffaa238a6
Zx Wang <ZW_08@outlook.com>
2025-07-22 15:19:45 -0400

    224 stack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,13 +7,41 @@ namespace Solution;
 
 public class Solution
 {
-    public int ComputeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+    public int Calculate(string s)
     {
-        int cx1 = int.Max(ax1, bx1);
-        int cx2 = int.Min(ax2, bx2);
-        int cy1 = int.Max(ay1, by1);
-        int cy2 = int.Min(ay2, by2);
-        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1)
-            - int.Max(cx2 - cx1, 0) * int.Max(cy2 - cy1, 0);
+        Stack<(int num, int sign)> st = [];
+        int num = 0;
+        int sign = 1;
+        for (int i = 0; i < s.Length; i++)
+        {
+            switch (s[i])
+            {
+                case '-':
+                    sign *= -1;
+                    break;
+                case '(':
+                    st.Push((num, sign));
+                    num = 0;
+                    sign = 1;
+                    break;
+                case ')':
+                    var item = st.Pop();
+                    num = item.num + item.sign * num;
+                    break;
+                case char c when char.IsAsciiDigit(c):
+                    int curr = c - '0';
+                    while (1 + i < s.Length && char.IsAsciiDigit(s[1 + i]))
+                    {
+                        i += 1;
+                        curr = curr * 10 + (s[i] - '0');
+                    }
+                    num += curr * sign;
+                    sign = 1;
+                    break;
+                case '+':
+                default: break;
+            }
+        }
+        return num;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(2, "1 + 1")]
+    [DataRow(3, " 2-1 + 2 ")]
+    [DataRow(23, "(1+(4+5+2)-3)+(6+8)")]
+    public void TestMethod1(int exp, string s)
     {
+        Assert.AreEqual(exp, sol.Calculate(s));
     }
 
     [TestMethod]
