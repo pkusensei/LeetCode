681629096dc7c04b778bcf111a1620c8a45b04f1
Zx Wang <ZW_08@outlook.com>
2025-07-22 11:03:55 -0400

    218 line sweep

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,37 +7,30 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<IList<int>> CombinationSum3(int k, int n)
+    public IList<IList<int>> GetSkyline(int[][] buildings)
     {
-        List<IList<int>> res = [];
-        for (int mask = 0; mask < 1 << 10; mask++)
+        List<(int x, int y)> arr = [];
+        foreach (var b in buildings)
         {
-            if ((mask & 1) == 0 && BitCount(mask) == k)
+            arr.Add((b[0], -b[2]));
+            arr.Add((b[1], b[2]));
+        }
+        arr.Sort();
+        SortedDictionary<int, int> dict = [];
+        List<IList<int>> res = [];
+        int prev = 0;
+        foreach (var (x, y) in arr)
+        {
+            if (y < 0 && !dict.TryAdd(-y, 1)) { dict[-y] += 1; }
+            else if (y > 0)
             {
-                List<int> curr = [];
-                int sum = 0;
-                for (int bit = 1; bit <= 9; bit++)
-                {
-                    if (((mask >> bit) & 1) == 1)
-                    {
-                        sum += bit;
-                        curr.Add(bit);
-                    }
-                }
-                if (sum == n) { res.Add(curr); }
+                dict[y] -= 1;
+                if (dict[y] == 0) { dict.Remove(y); }
             }
+            var height = dict.Keys.LastOrDefault();
+            if (height != prev) { res.Add([x, height]); }
+            prev = height;
         }
         return res;
-
-        static int BitCount(int x)
-        {
-            int res = 0;
-            while (x != 0)
-            {
-                x &= x - 1;
-                res += 1;
-            }
-            return res;
-        }
     }
 }
