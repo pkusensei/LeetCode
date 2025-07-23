2564cdd4be54b87fb01ca527ff0495e3ea8eabda
pkusensei <ZW_08@outlook.com>
2025-07-23 14:15:39 -0400

    236 binary tree and lca

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,7 +7,7 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    public TreeNode LowestCommonAncestor_BST(TreeNode root, TreeNode p, TreeNode q)
     {
         int min = int.Min(p.val, q.val);
         int max = int.Max(p.val, q.val);
@@ -20,5 +20,15 @@ public class Solution
             if (val < min) { return Dfs(node.right, min, max); }
             return Dfs(node.left, min, max);
         }
+    }
+
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root is null) { return null; }
+        if (root == p || root == q) { return root; }
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        if (left is not null && right is not null) { return root; }
+        else { return left ?? right; }
     }
 }
