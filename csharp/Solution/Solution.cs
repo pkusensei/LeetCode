ba40948576651155395ccad06d57521cf348419e
Zx Wang <ZW_08@outlook.com>
2025-07-24 13:05:13 -0400

    273 tedious

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,11 +8,62 @@ namespace Solution;
 
 public class Solution
 {
-    public int MissingNumber(int[] nums)
+    static readonly string[] DIGITS = ["Zero", "One", "Two", "Three", "Four",
+                                       "Five", "Six", "Seven", "Eight", "Nine",
+                                       "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                                       "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+    static readonly string[] TENS = ["Ten", "Twenty", "Thirty", "Forty",
+                                     "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+    static readonly string[] ORDERS = ["Thousand", "Million", "Billion"];
+
+    public string NumberToWords(int num)
     {
-        int n = nums.Length;
-        int xor = Enumerable.Range(0, 1 + n).Aggregate((a, b) => a ^ b);
-        int curr = nums.Aggregate((a, b) => a ^ b);
-        return xor ^ curr;
+        if (num == 0) { return DIGITS[0]; }
+        int order = 0;
+        List<string> res = [];
+        while (num > 0)
+        {
+            int curr = num % 1000;
+            if (curr > 0) { res.Add(Solve(curr, order)); }
+            order += 1;
+            num /= 1000;
+        }
+        return string.Join(' ', res.AsEnumerable().Reverse().ToArray());
+
+        static string Solve(int num, int order)
+        {
+            if (num == 0) { return string.Empty; }
+            StringBuilder sb = new();
+            int hun = num / 100;
+            if (hun > 0)
+            {
+                sb.Append(DIGITS[hun]);
+                sb.Append(" Hundred");
+            }
+            int tens = num % 100;
+            if (1 <= tens && tens <= 19)
+            {
+                if (sb.Length > 0) { sb.Append(' '); }
+                sb.Append(DIGITS[tens]);
+            }
+            else if (tens > 0)
+            {
+                if (sb.Length > 0) { sb.Append(' '); }
+                int single = tens % 10;
+                tens /= 10;
+                sb.Append(TENS[tens - 1]);
+                if (single > 0)
+                {
+                    sb.Append(' ');
+                    sb.Append(DIGITS[single]);
+                }
+            }
+            if (order > 0)
+            {
+                sb.Append(' ');
+                sb.Append(ORDERS[order - 1]);
+            }
+            return sb.ToString();
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,10 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven", 1234567)]
+    public void TestMethod1(string exp, int num)
     {
+        Assert.AreEqual(exp, sol.NumberToWords(num));
     }
 
     [TestMethod]
