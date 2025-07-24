e8e576e24525f60fec8b8667a56694498f54da57
pkusensei <ZW_08@outlook.com>
2025-07-23 21:51:15 -0400

    2322 this partion logic...

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,28 +7,45 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode LowestCommonAncestor_BST(TreeNode root, TreeNode p, TreeNode q)
+    public int MinimumScore(int[] nums, int[][] edges)
     {
-        int min = int.Min(p.val, q.val);
-        int max = int.Max(p.val, q.val);
-        return Dfs(root, min, max);
-
-        static TreeNode Dfs(TreeNode node, int min, int max)
+        int n = nums.Length;
+        List<int>[] adj = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
+        foreach (var e in edges)
         {
-            int val = node.val;
-            if (min <= val && val <= max) { return node; }
-            if (val < min) { return Dfs(node.right, min, max); }
-            return Dfs(node.left, min, max);
+            adj[e[0]].Add(e[1]);
+            adj[e[1]].Add(e[0]);
         }
-    }
+        int[] xors = new int[n];
+        int[] last = new int[n];
+        int node_id = 0;
+        int total_xor = Dfs(0, -1);
+        int res = int.MaxValue;
+        for (int i1 = 1; i1 < n; i1++)
+        {
+            for (int i2 = 1 + i1; i2 < n; i2++)
+            {
+                int v1 = i2 < last[i1] ? total_xor ^ xors[i1] : total_xor ^ xors[i1] ^ xors[i2];
+                int v2 = i2 < last[i1] ? xors[i1] ^ xors[i2] : xors[i1];
+                int min = int.Min(xors[i2], int.Min(v1, v2));
+                int max = int.Max(xors[i2], int.Max(v1, v2));
+                res = int.Min(res, max - min);
+            }
+        }
+        return res;
 
-    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
-    {
-        if (root is null) { return null; }
-        if (root == p || root == q) { return root; }
-        var left = LowestCommonAncestor(root.left, p, q);
-        var right = LowestCommonAncestor(root.right, p, q);
-        if (left is not null && right is not null) { return root; }
-        else { return left ?? right; }
+        int Dfs(int node, int prev)
+        {
+            int curr_val = nums[node];
+            int curr_id = node_id;
+            node_id += 1;
+            foreach (var next in adj[node])
+            {
+                if (next != prev) { curr_val ^= Dfs(next, node); }
+            }
+            last[curr_id] = node_id;
+            xors[curr_id] = curr_val;
+            return curr_val;
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,11 +12,17 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
+        int[] nums = [1, 5, 5, 4, 11];
+        int[][] edges = [[0, 1], [1, 2], [1, 3], [3, 4]];
+        Assert.AreEqual(9, sol.MinimumScore(nums, edges));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        int[] nums = [5, 5, 2, 4, 4, 2];
+        int[][] edges = [[0, 1], [1, 2], [5, 2], [4, 3], [1, 3]];
+        Assert.AreEqual(0, sol.MinimumScore(nums, edges));
     }
 
     [TestMethod]
