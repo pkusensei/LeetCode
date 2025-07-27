9d126a5b270ddaa76eade79859778b21c9b5137c
Zx Wang <ZW_08@outlook.com>
2025-07-27 09:57:37 -0400

    3629 idea is easy but tle hell

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,37 +4,82 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::{HashMap, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn num_of_subsequences(s: &str) -> i64 {
-    let n = s.len();
-    if n < 2 {
-        return 0;
-    }
-    let [mut prefix_l, mut suffix_t] = [vec![], vec![]];
-    for b in s.bytes() {
-        prefix_l.push(i64::from(b == b'L'));
-        suffix_t.push(i64::from(b == b'T'));
-    }
-    for i in 1..n {
-        prefix_l[i] += prefix_l[i - 1];
-    }
-    for i in (0..n - 1).rev() {
-        suffix_t[i] += suffix_t[1 + i];
-    }
-    let mut base = 0;
-    let [mut add_l, mut add_c, mut add_t] = [0; 3];
-    for (i, b) in s.bytes().enumerate() {
-        if b == b'C' {
-            base += prefix_l[i] * suffix_t[i];
-            add_l += suffix_t[i];
-            add_t += prefix_l[i];
-        } else {
-            add_c = add_c.max(prefix_l[i] * suffix_t[i]);
+pub fn min_jumps(nums: &[i32]) -> i32 {
+    let max = *nums.iter().max().unwrap_or(&1_000_000);
+    let sieve = get_sieve(max);
+    let mut buckets = HashMap::<_, Vec<_>>::new();
+    for (idx, &num) in nums.iter().enumerate() {
+        for p in precompute(num, &sieve) {
+            buckets.entry(p).or_default().push(idx);
         }
     }
-    base + add_l.max(add_c).max(add_t)
+    let n = nums.len();
+    let mut queue = VecDeque::from([(0, 0)]);
+    let mut seen = vec![false; n];
+    seen[0] = true;
+    while let Some((node, step)) = queue.pop_front() {
+        if node == n - 1 {
+            return step;
+        }
+        if node > 0 && !seen[node - 1] {
+            seen[node - 1] = true;
+            queue.push_back((node - 1, 1 + step));
+        }
+        if node < n - 1 && !seen[1 + node] {
+            seen[1 + node] = true;
+            queue.push_back((1 + node, 1 + step));
+        }
+        if sieve[nums[node] as usize] {
+            let Some(buc) = buckets.get(&nums[node]) else {
+                continue;
+            };
+            for &next in buc {
+                if !seen[next] {
+                    seen[next] = true;
+                    queue.push_back((next, 1 + step));
+                }
+            }
+            buckets.remove(&nums[node]);
+        }
+    }
+    -1
+}
+
+fn precompute(mut num: i32, sieve: &[bool]) -> Vec<i32> {
+    let mut p = 2;
+    let mut primes = vec![];
+    while p * p <= num {
+        if sieve[p as usize] && num % p == 0 {
+            primes.push(p);
+            while num % p == 0 {
+                num /= p;
+            }
+        }
+        p += 1;
+    }
+    if num > 0 {
+        primes.push(num);
+    }
+    primes
+}
+
+fn get_sieve(n: i32) -> Vec<bool> {
+    let n = 1 + n as usize;
+    let mut sieve = vec![true; n];
+    sieve[..2].fill(false);
+    for p in 2..=n.isqrt() {
+        if sieve[p] {
+            for val in (p * p..n).step_by(p) {
+                sieve[val] = false;
+            }
+        }
+    }
+    sieve
 }
 
 #[cfg(test)]
@@ -67,12 +112,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(num_of_subsequences("LMCT"), 2);
-        assert_eq!(num_of_subsequences("LCCT"), 4);
-        assert_eq!(num_of_subsequences("L"), 0);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(min_jumps(&[5, 2, 20, 1, 15]), 1);
+    }
 }
