9050612eead3af81da8f0070080754f3d615420c
pkusensei <ZW_08@outlook.com>
2025-07-28 10:41:41 -0400

    313 heap and dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,20 +7,60 @@ namespace Solution;
 
 public class Solution
 {
-    public int CountMaxOrSubsets(int[] nums)
+    public int NthSuperUglyNumber(int n, int[] primes)
     {
-        int n = nums.Length;
-        int total = nums.Aggregate((acc, v) => acc | v);
-        int res = 0;
-        for (int mask = 1; mask < 1 << n; mask++)
+        HashSet<int> seen = [];
+        PriorityQueue<int, int> pq = new();
+        pq.Enqueue(1, 1);
+        while (pq.TryDequeue(out int num, out _))
         {
-            int curr = 0;
-            for (int i = 0; i < n; i++)
+            if (seen.Add(num))
             {
-                if (((mask >> i) & 1) == 1) { curr |= nums[i]; }
+                n -= 1;
+                if (n == 0) { return num; }
+                try
+                {
+                    checked
+                    {
+                        foreach (var p in primes)
+                        {
+                            pq.Enqueue(p * num, p * num);
+                        }
+                    }
+                }
+                catch (OverflowException) { }
             }
-            if (curr == total) { res += 1; }
         }
-        return res;
+        return -1;
+    }
+
+    public int WithDp(int n, int[] primes)
+    {
+        if (n <= 1) { return 1; }
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        // Each ptr corresponds to one prime.
+        // ptrs[i] is the index in `ugly` array that primes[i] should multiply
+        int[] ptrs = new int[primes.Length];
+        // Each prime with multiplication produces these candidates
+        // init as prime*ugly[0]
+        long[] values = [.. primes];
+        for (int idx = 1; idx < n; idx++)
+        {
+            long curr_min = values.Min();
+            ugly[idx] = (int)curr_min;
+            for (int i = 0; i < primes.Length; i++)
+            {
+                // This candidate has be "used"
+                if (values[i] == curr_min)
+                {
+                    // Its ptr has to be updated to next in ugly
+                    ptrs[i] += 1;
+                    // update new candidate
+                    values[i] = (long)ugly[ptrs[i]] * primes[i];
+                }
+            }
+        }
+        return ugly[n - 1];
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,12 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(32, 12, new[] { 2, 7, 13, 19 })]
+    [DataRow(1, 1, new[] { 2, 3, 5 })]
+    public void TestMethod1(int exp, int n, int[] p)
     {
+        Assert.AreEqual(exp, sol.NthSuperUglyNumber(n, p));
+        Assert.AreEqual(exp, sol.WithDp(n, p));
     }
 
     [TestMethod]
