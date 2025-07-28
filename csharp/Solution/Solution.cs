5e0f92f0a9bc539c003b27fcf247e969afef5190
pkusensei <ZW_08@outlook.com>
2025-07-28 17:26:10 -0400

    321 why is there no lexico compare in C#

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,28 +7,75 @@ namespace Solution;
 
 public class Solution
 {
-    public string RemoveDuplicateLetters(string s)
+    public int[] MaxNumber(int[] nums1, int[] nums2, int k)
     {
-        Span<int> last_idx = stackalloc int[26];
-        last_idx.Fill(-1);
-        for (int i = 0; i < s.Length; i++)
+        int[] res = new int[k];
+        for (int k1 = 0; k1 <= nums1.Length; k1++)
         {
-            last_idx[s[i] - 'a'] = i;
-        }
-        Stack<char> st = [];
-        int mask = 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            int ci = s[i] - 'a';
-            if (((mask >> ci) & 1) == 1) { continue; }
-            while (st.TryPeek(out var top) && top > s[i] & last_idx[top - 'a'] > i)
+            int k2 = int.Min(k - k1, nums2.Length);
+            if (k1 + k2 != k) { continue; }
+            var curr = Merge(Build(nums1, k1), Build(nums2, k2));
+            if (curr.Zip(res, (a, b) => a.CompareTo(b)).FirstOrDefault(c => c != 0) > 0)
             {
-                st.Pop();
-                mask ^= 1 << (top - 'a');
+                res = curr;
             }
-            st.Push(s[i]);
-            mask |= 1 << ci;
         }
-        return string.Concat(st.Reverse());
+        return res;
+
+        static int[] Build(ReadOnlySpan<int> nums, int k)
+        {
+            if (k <= 0) { return []; }
+            Stack<int> st = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (st.TryPeek(out int top) && top < nums[i] && nums.Length - i > k - st.Count)
+                {
+                    st.Pop();
+                }
+                if (st.Count < k) { st.Push(nums[i]); }
+            }
+            return [.. st.Reverse()];
+        }
+
+        static int[] Merge(ReadOnlySpan<int> n1, ReadOnlySpan<int> n2)
+        {
+            List<int> res = new(n1.Length + n2.Length);
+            while (!n1.IsEmpty && !n2.IsEmpty)
+            {
+                if (Compare(n1, n2) >= 0)
+                {
+                    res.Add(n1[0]);
+                    n1 = n1[1..];
+                }
+                else
+                {
+                    res.Add(n2[0]);
+                    n2 = n2[1..];
+                }
+            }
+            res.AddRange(n1);
+            res.AddRange(n2);
+            return [.. res];
+        }
+
+        static int Compare(ReadOnlySpan<int> n1, ReadOnlySpan<int> n2)
+        {
+            int i1 = 0, i2 = 0;
+            for (; i1 < n1.Length && i2 < n2.Length; i1 += 1, i2 += 1)
+            {
+                if (n1[i1] != n2[i2]) { return n1[i1].CompareTo(n2[i2]); }
+            }
+            while (i1 < n1.Length)
+            {
+                if (n1[i1] != n2[0]) { return n1[i1].CompareTo(n2[0]); }
+                i1++;
+            }
+            while (i2 < n2.Length)
+            {
+                if (n1[0] != n2[i2]) { return n1[0].CompareTo(n2[i2]); }
+                i2++;
+            }
+            return 0;
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,11 +10,14 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow("abc", "bcabc")]
-    [DataRow("acdb", "cbacdcbc")]
-    public void TestMethod1(string exp, string s)
+    [DataRow(new[] { 9, 8, 6, 5, 3 }, new[] { 3, 4, 6, 5 }, new[] { 9, 1, 2, 5, 8, 3 }, 5)]
+    [DataRow(new[] { 6, 7, 6, 0, 4 }, new[] { 6, 7 }, new[] { 6, 0, 4 }, 5)]
+    [DataRow(new[] { 9, 8, 9 }, new[] { 3, 9 }, new[] { 8, 9 }, 3)]
+    [DataRow(new[] { 6, 3, 9, 2, 2, 5, 2, 1, 4, 4, 5, 7, 8, 9, 3, 1, 6, 9, 7, 0, 5, 6, 0 },
+             new[] { 6, 3, 9, 0, 5, 6 }, new[] { 2, 2, 5, 2, 1, 4, 4, 5, 7, 8, 9, 3, 1, 6, 9, 7, 0 }, 23)]
+    public void TestMethod1(int[] exp, int[] n1, int[] n2, int k)
     {
-        Assert.AreEqual(exp, sol.RemoveDuplicateLetters(s));
+        Assert.IsTrue(exp.SequenceEqual(sol.MaxNumber(n1, n2, k)));
     }
 
     [TestMethod]
