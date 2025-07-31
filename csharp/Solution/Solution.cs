40a0db2eb964ea2edd17da5da5c6efa4d3d83229
pkusensei <ZW_08@outlook.com>
2025-07-31 17:54:30 -0400

    363 2d prefix sum; kadanes and sliding window

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,16 +7,77 @@ namespace Solution;
 
 public class Solution
 {
-    public int CountNumbersWithUniqueDigits(int n)
+    public int MaxSumSubmatrix(int[][] matrix, int k)
     {
-        if (n < 1) { return 1; }
-        int res = 9;
-        int v = 9;
-        for (int _ = 1; _ < n; _++)
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+        int[,] prefix = new int[1 + rows, 1 + cols];
+        for (int r = 0; r < rows; r++)
         {
-            res *= v;
-            v -= 1;
+            int curr = 0;
+            for (int c = 0; c < cols; c++)
+            {
+                curr += matrix[r][c];
+                prefix[1 + r, 1 + c] = curr + prefix[r, 1 + c];
+            }
         }
-        return res + CountNumbersWithUniqueDigits(n - 1);
+        int res = int.MinValue;
+        for (int r1 = 0; r1 < rows; r1++)
+        {
+            for (int c1 = 0; c1 < cols; c1++)
+            {
+                for (int r2 = r1; r2 < rows; r2++)
+                {
+                    for (int c2 = c1; c2 < cols; c2++)
+                    {
+                        int curr = prefix[1 + r2, 1 + c2] + prefix[r1, c1]
+                                   - prefix[1 + r2, c1] - prefix[r1, 1 + c2];
+                        if (curr <= k) { res = int.Max(res, curr); }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public int WithKadanes(int[][] mat, int k)
+    {
+        int rows = mat.Length;
+        int cols = mat[0].Length;
+        int res = int.MinValue;
+        for (int top = 0; top < rows; top++)
+        {
+            int[] row_sum = new int[cols];
+            for (int bot = top; bot < rows; bot++)
+            {
+                int kadane = 0;
+                int max_kadane = int.MinValue;
+                for (int i = 0; i < cols; i++)
+                {
+                    row_sum[i] += mat[bot][i];
+                    kadane = int.Max(row_sum[i], kadane + row_sum[i]);
+                    max_kadane = int.Max(max_kadane, kadane);
+                }
+                if (max_kadane <= k)
+                {
+                    res = int.Max(res, max_kadane);
+                    continue;
+                }
+                SortedSet<int> set = [0];
+                int running_sum = 0;
+                foreach (var num in row_sum)
+                {
+                    running_sum += num;
+                    var temp = set.GetViewBetween(running_sum - k, int.MaxValue);
+                    if (temp.Count > 0)
+                    {
+                        res = int.Max(res, running_sum - temp.Min);
+                    }
+                    set.Add(running_sum);
+                }
+            }
+        }
+        return res;
+
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -7,16 +7,22 @@ namespace Tests;
 [TestClass]
 public class UnitTest
 {
-    // readonly Solution.Solution sol = new();
+    readonly Solution.Solution sol = new();
 
     [TestMethod]
     public void TestMethod1()
     {
+        int[][] m = [[1, 0, 1], [0, -2, 3]];
+        Assert.AreEqual(2, sol.MaxSumSubmatrix(m, 2));
+        Assert.AreEqual(2, sol.WithKadanes(m, 2));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
+        int[][] m = [[2, 2, -1]];
+        Assert.AreEqual(3, sol.MaxSumSubmatrix(m, 3));
+        Assert.AreEqual(3, sol.WithKadanes(m, 3));
     }
 
     [TestMethod]
