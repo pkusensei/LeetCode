2d805e27a0bec88ce27afd20108bdb4bf2cee556
Zx Wang <ZW_08@outlook.com>
2025-07-31 11:34:19 -0400

    352 merge intervals and BTReeMap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,65 +7,36 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn palindrome_pairs(words: &[&str]) -> Vec<Vec<i32>> {
-    let mut trie = Trie::default();
-    for (i, s) in words.iter().enumerate() {
-        trie.insert(s.as_bytes(), i);
-    }
-    let mut res = vec![];
-    for (i, s) in words.iter().enumerate() {
-        trie.search(s.as_bytes(), i, &mut res);
-    }
-    res
+use std::collections::BTreeMap;
+struct SummaryRanges {
+    map: BTreeMap<i32, i32>,
 }
 
-#[derive(Default)]
-struct Trie {
-    nodes: [Option<Box<Trie>>; 26],
-    idx: Option<usize>,
-    indices: Vec<usize>,
-}
-
-impl Trie {
-    fn insert(&mut self, s: &[u8], idx: usize) {
-        let mut curr = self;
-        for (right, &b) in s.iter().enumerate().rev() {
-            let node = curr.nodes[usize::from(b - b'a')].get_or_insert_default();
-            if is_palindrome(&s[..=right]) {
-                curr.indices.push(idx); // From this point to the start, it is palin
-            }
-            curr = node;
+impl SummaryRanges {
+    fn new() -> Self {
+        Self {
+            map: BTreeMap::new(),
         }
-        // records this node is a word's start(since loop backwards)
-        curr.idx = Some(idx);
     }
 
-    fn search(&self, s: &[u8], idx: usize, res: &mut Vec<Vec<i32>>) {
-        let mut curr = self;
-        for (si, &b) in s.iter().enumerate() {
-            if let Some(curr_idx) = curr.idx
-                && curr_idx != idx
-                && is_palindrome(&s[si..])
-            {
-                // Found a word (curr.idx is Some) and the rest of queried s is palindrome
-                res.push(vec![idx as i32, curr_idx as _]);
-            }
-            let Some(v) = curr.nodes[usize::from(b - b'a')].as_ref() else {
-                return;
-            };
-            curr = v;
-        }
-        if let Some(curr_idx) = curr.idx
-            && curr_idx != idx
+    fn add_num(&mut self, value: i32) {
+        if let Some((&left_start, &left_end)) = self.map.range(..=value).next_back()
+            && left_end + 1 >= value
         {
-            // Check this node
-            res.push(vec![idx as i32, curr_idx as _]);
-        }
-        for &i in &curr.indices {
-            if i != idx {
-                res.push(vec![idx as i32, i as i32]);
+            if let Some(end) = self.map.remove(&(1 + value)) {
+                self.map.insert(left_start, end);
+            } else {
+                self.map.insert(left_start, left_end.max(value));
             }
+        } else if let Some(end) = self.map.remove(&(1 + value)) {
+            self.map.insert(value, end);
+        } else {
+            self.map.insert(value, value);
         }
+    }
+
+    fn get_intervals(&self) -> Vec<Vec<i32>> {
+        self.map.iter().map(|(k, v)| vec![*k, *v]).collect()
     }
 }
 
@@ -100,19 +71,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        sort_eq!(
-            palindrome_pairs(&["abcd", "dcba", "lls", "s", "sssll"]),
-            [[0, 1], [1, 0], [3, 2], [2, 4]]
-        );
-        sort_eq!(palindrome_pairs(&["bat", "tab", "cat"]), [[0, 1], [1, 0]]);
-        sort_eq!(palindrome_pairs(&["a", ""]), [[0, 1], [1, 0]]);
+        let mut sr = SummaryRanges::new();
+        sr.add_num(6);
+        sr.add_num(8);
+        assert_eq!(sr.get_intervals(), [[6, 6], [8, 8]]);
+        sr.add_num(7);
+        assert_eq!(sr.get_intervals(), [[6, 8]]);
+        sr.add_num(6);
+        assert_eq!(sr.get_intervals(), [[6, 8]]);
     }
 
     #[test]
-    fn test() {
-        sort_eq!(
-            palindrome_pairs(&["a", "abc", "aba", ""]),
-            [[0, 3], [3, 0], [2, 3], [3, 2]]
-        );
-    }
+    fn test() {}
 }
