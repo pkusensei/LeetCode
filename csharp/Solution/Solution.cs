5bf31ebffbac77c0ad8029f799898992dc41e9fb
pkusensei <ZW_08@outlook.com>
2025-08-01 13:05:02 -0400

    373 rare to see C# code shorter than rust

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,15 +7,27 @@ namespace Solution;
 
 public class Solution
 {
-    public int GetSum(int a, int b)
+    public IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
     {
-        while (b != 0)
+        int n1 = nums1.Length;
+        int n2 = nums2.Length;
+        List<IList<int>> res = new(k);
+        PriorityQueue<(int, int), int> pq = new();
+        pq.Enqueue((0, 0), nums1[0] + nums2[0]);
+        HashSet<(int, int)> seen = [(0, 0)];
+        while (res.Count < k && pq.TryDequeue(out var item, out _))
         {
-            int sum_no_carry = a ^ b;
-            int carry = a & b;
-            a = sum_no_carry;
-            b = carry << 1;
+            (int i1, int i2) = item;
+            res.Add([nums1[i1], nums2[i2]]);
+            if (i1 + 1 < n1 && seen.Add((1 + i1, i2)))
+            {
+                pq.Enqueue((1 + i1, i2), nums1[1 + i1] + nums2[i2]);
+            }
+            if (1 + i2 < n2 && seen.Add((i1, 1 + i2)))
+            {
+                pq.Enqueue((i1, 1 + i2), nums1[i1] + nums2[1 + i2]);
+            }
         }
-        return a;
+        return res;
     }
 }
