28c815209b51dd061b2cf36de7bf740e43f4a469
pkusensei <ZW_08@outlook.com>
2025-08-01 21:08:58 -0400

    2561 to swap a and b, do it directly vs find a min value and swap (a, min) (b, min)

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,31 +7,32 @@ namespace Solution;
 
 public class Solution
 {
-    public int GetMoneyAmount(int n)
+    public long MinCost(int[] basket1, int[] basket2)
     {
-        int[,] memo = new int[1 + n, 1 + n];
-        for (int i = 0; i <= n; i++)
+        Dictionary<int, int> freq = [];
+        int min_num = int.MaxValue;
+        foreach (var num in basket1)
         {
-            for (int j = 0; j <= n; j++)
-            {
-                memo[i, j] = -1;
-            }
+            if (!freq.TryAdd(num, 1)) { freq[num] += 1; }
+            min_num = int.Min(min_num, num);
         }
-        return Dfs(1, n);
-
-        int Dfs(int left, int right)
+        foreach (var num in basket2)
         {
-            if (left >= right) { return 0; } // Found!
-            if (memo[left, right] > -1) { return memo[left, right]; }
-            int res = int.MaxValue;
-            for (int i = left; i <= right; i++)
-            {
-                // Goes on either branch, pick the big one
-                int curr = i + int.Max(Dfs(left, i - 1), Dfs(1 + i, right));
-                res = int.Min(res, curr);
-            }
-            memo[left, right] = res;
-            return res;
+            if (!freq.TryAdd(num, -1)) { freq[num] -= 1; }
+            min_num = int.Min(min_num, num);
         }
+        List<int> diff = [];
+        foreach (var (num, f) in freq)
+        {
+            if ((f & 1) == 1) { return -1; }
+            if (f != 0) { diff.AddRange(Enumerable.Repeat(num, int.Abs(f) / 2)); }
+        }
+        diff.Sort();
+        long res = 0;
+        foreach (var num in diff[..(diff.Count / 2)])
+        {
+            res += long.Min(num, 2 * min_num);
+        }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,10 +10,11 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(16, 10)]
-    public void TestMethod1(int exp, int n)
+    [DataRow(1, new[] { 4, 2, 2, 2 }, new[] { 1, 4, 1, 2 })]
+    [DataRow(-1, new[] { 2, 3, 4, 1 }, new[] { 3, 2, 5, 1 })]
+    public void TestMethod1(int exp, int[] a, int[] b)
     {
-        Assert.AreEqual(exp, sol.GetMoneyAmount(n));
+        Assert.AreEqual(exp, sol.MinCost(a, b));
     }
 
     [TestMethod]
