1271740442dd286ea67d7473f0c0b6c6d47a84f8
pkusensei <ZW_08@outlook.com>
2025-08-02 21:12:42 -0400

    2106 sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,33 +7,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn super_pow(a: i32, b: &[i32]) -> i32 {
-    const M: i32 = 1337;
-    // Effectively insert 0 in front of b
-    let mut res = 1;
-    for &d in b.iter() {
-        // Suppose b=[d1, d2, ..]
-        // res = pow(a, b) = pow(a, 10*d1 + d2)
-        // pow(a, 10*d1) * pow(a, d2)
-        // pow(pow(a, d1), 10) * pow(a, d2)
-        // res *= pow(a, d1), then res = pow(res, 10);
-        // res *= pow(a, d2)
-        res = mod_pow(res, 10, M);
-        res = res * mod_pow(a % M, d, M);
-        res %= M;
+pub fn max_total_fruits(fruits: &[[i32; 2]], start_pos: i32, k: i32) -> i32 {
+    let i = fruits.partition_point(|f| f[0] < start_pos - k);
+    let [mut left, mut right] = [i, i];
+    let mut res = 0;
+    let mut curr = 0;
+    while let Some(left_f) = fruits.get(left) {
+        let left_pos = left_f[0].min(start_pos);
+        let left_steps = start_pos - left_pos;
+        // walk left first:  2*left_steps + right_steps = k
+        // walk right first: 2*right_steps + left_steps = k
+        let right_steps = (k - 2 * left_steps).max((k - left_steps) / 2);
+        let right_pos = start_pos + right_steps;
+        while let Some(f) = fruits.get(right)
+            && f[0] <= right_pos
+        {
+            curr += f[1];
+            right += 1;
+        }
+        res = res.max(curr);
+        curr -= left_f[1];
+        if left_f[0] > start_pos {
+            // [left] is already to the right of start
+            // i.e goes right straight
+            // Now, what happens if this is the first [left] in loop?
+            // Left point is computed by min([left], start)
+            // When start<[left] it is start => goes right straight
+            break;
+        }
+        left += 1;
     }
     res
-}
-
-const fn mod_pow(b: i32, exp: i32, m: i32) -> i32 {
-    if exp == 0 {
-        return 1;
-    }
-    if exp & 1 == 0 {
-        mod_pow(b * b % m, exp >> 1, m)
-    } else {
-        mod_pow(b * b % m, exp >> 1, m) * b % m
-    }
 }
 
 #[cfg(test)]
@@ -66,10 +70,15 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_total_fruits(&[[2, 8], [6, 3], [8, 6]], 5, 4), 9);
+        assert_eq!(
+            max_total_fruits(&[[0, 9], [4, 1], [5, 7], [6, 2], [7, 4], [10, 9]], 5, 4),
+            14
+        );
+        assert_eq!(max_total_fruits(&[[0, 3], [6, 4], [8, 5]], 3, 2), 0);
+    }
 
     #[test]
-    fn test() {
-        assert_eq!(super_pow(2147483647, &[2, 0, 0]), 1198);
-    }
+    fn test() {}
 }
