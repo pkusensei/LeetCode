0bed0ea75102c9ab551385e508fbf4f90c8143e9
Zx Wang <ZW_08@outlook.com>
2025-08-03 10:26:05 -0400

    3636 Mo's method

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,39 +7,64 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_time(s: String, order: Vec<i32>, k: i32) -> i32 {
-    let n = s.len();
-    let mut s = s.into_bytes();
-    let mut left = 0;
-    let mut right = n;
-    while left < right {
-        let mid = left + (right - left) / 2;
-        if count(&mut s, &order, mid) >= k as usize {
-            right = mid;
-        } else {
-            left = 1 + mid;
-        }
-    }
-    if left < n { left as i32 } else { -1 }
-}
+use itertools::Itertools;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
-fn count(s: &mut [u8], order: &[i32], mid: usize) -> usize {
-    let n = s.len();
-    for &i in &order[..=mid] {
-        s[i as usize] = b'*';
-    }
-    let v = s
-        .split(|&b| b == b'*')
-        .map(|w| {
-            let v = w.len();
-            v * (1 + v) / 2
-        })
-        .sum::<usize>();
-    let res = n * (1 + n) / 2 - v;
-    for &i in &order[..=mid] {
-        s[i as usize] = b'#';
+// Took from https://pages.di.unipi.it/rossano/blog/2023/mosalgorithm/
+pub fn subarray_majority(nums: &[i32], queries: &[[i32; 3]]) -> Vec<i32> {
+    let n = nums.len();
+    let qn = queries.len();
+    let sqrt = 1 + n.isqrt() as i32;
+    let mut res = vec![-1; qn];
+    let mut track = Tracker::default();
+    let mut curr_left = 0;
+    let mut curr_right = 0; // exclusive
+    for qi in (0..qn).sorted_unstable_by_key(|&i| [queries[i][0] / sqrt, queries[i][1]]) {
+        let [left, right] = [0, 1].map(|i| queries[qi][i] as usize);
+        while curr_left > left {
+            curr_left -= 1;
+            track.update(nums[curr_left], 1);
+        }
+        while curr_right <= right {
+            track.update(nums[curr_right], 1);
+            curr_right += 1;
+        }
+        while curr_left < left {
+            track.update(nums[curr_left], -1);
+            curr_left += 1;
+        }
+        while curr_right > 1 + right {
+            curr_right -= 1;
+            track.update(nums[curr_right], -1);
+        }
+        res[qi] = track.find(queries[qi][2]).unwrap_or(-1);
     }
     res
+}
+
+#[derive(Default)]
+struct Tracker {
+    num_freq: HashMap<i32, i32>,
+    freq_nums: BTreeMap<i32, BTreeSet<i32>>,
+}
+
+impl Tracker {
+    fn update(&mut self, num: i32, delta: i32) {
+        let f = self.num_freq.entry(num).or_insert(0);
+        *f += delta;
+        let old = *f - delta;
+        self.freq_nums.entry(old).or_default().remove(&num);
+        self.freq_nums.entry(old + delta).or_default().insert(num);
+    }
+
+    fn find(&self, th: i32) -> Option<i32> {
+        for (_, set) in self.freq_nums.range(th..).rev() {
+            if let Some(&v) = set.first() {
+                return Some(v);
+            }
+        }
+        None
+    }
 }
 
 #[cfg(test)]
@@ -72,7 +97,19 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            subarray_majority(&[1, 1, 2, 2, 1, 1], &[[0, 5, 4], [0, 3, 3], [2, 3, 2]]),
+            [1, -1, 2]
+        );
+        assert_eq!(
+            subarray_majority(
+                &[3, 2, 3, 2, 3, 2, 3],
+                &[[0, 6, 4], [1, 5, 2], [2, 4, 1], [3, 3, 1]]
+            ),
+            [3, 2, 3, 2]
+        );
+    }
 
     #[test]
     fn test() {}
