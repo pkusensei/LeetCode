1ecdcc849fcf21e4de83df827fc720bf86bdd3f8
Zx Wang <ZW_08@outlook.com>
2025-08-03 08:45:23 -0400

    3633 brute force

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,35 +7,37 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_total_fruits(fruits: &[[i32; 2]], start_pos: i32, k: i32) -> i32 {
-    let i = fruits.partition_point(|f| f[0] < start_pos - k);
-    let [mut left, mut right] = [i, i];
-    let mut res = 0;
-    let mut curr = 0;
-    while let Some(left_f) = fruits.get(left) {
-        let left_pos = left_f[0].min(start_pos);
-        let left_steps = start_pos - left_pos;
-        // walk left first:  2*left_steps + right_steps = k
-        // walk right first: 2*right_steps + left_steps = k
-        let right_steps = (k - 2 * left_steps).max((k - left_steps) / 2);
-        let right_pos = start_pos + right_steps;
-        while let Some(f) = fruits.get(right)
-            && f[0] <= right_pos
-        {
-            curr += f[1];
-            right += 1;
-        }
-        res = res.max(curr);
-        curr -= left_f[1];
-        if left_f[0] > start_pos {
-            // [left] is already to the right of start
-            // i.e goes right straight
-            // Now, what happens if this is the first [left] in loop?
-            // Left point is computed by min([left], start)
-            // When start<[left] it is start => goes right straight
-            break;
-        }
-        left += 1;
+pub fn earliest_finish_time(
+    land_start_time: Vec<i32>,
+    land_duration: Vec<i32>,
+    water_start_time: Vec<i32>,
+    water_duration: Vec<i32>,
+) -> i32 {
+    f(
+        &land_start_time,
+        &land_duration,
+        &water_start_time,
+        &water_duration,
+    )
+    .min(f(
+        &water_start_time,
+        &water_duration,
+        &land_start_time,
+        &land_duration,
+    ))
+}
+
+fn f(start1: &[i32], dur1: &[i32], start2: &[i32], dur2: &[i32]) -> i32 {
+    let earliest = start1
+        .iter()
+        .zip(dur1)
+        .map(|(s, d)| s + d)
+        .min()
+        .unwrap_or(0);
+    let mut res = i32::MAX;
+    for (&s, &d) in start2.iter().zip(dur2) {
+        let curr = s.max(earliest) + d;
+        res = res.min(curr);
     }
     res
 }
@@ -70,14 +72,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_total_fruits(&[[2, 8], [6, 3], [8, 6]], 5, 4), 9);
-        assert_eq!(
-            max_total_fruits(&[[0, 9], [4, 1], [5, 7], [6, 2], [7, 4], [10, 9]], 5, 4),
-            14
-        );
-        assert_eq!(max_total_fruits(&[[0, 3], [6, 4], [8, 5]], 3, 2), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
