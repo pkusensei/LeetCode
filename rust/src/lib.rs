b838408ff8e66003901e21cf20a3411510911af1
Zx Wang <ZW_08@outlook.com>
2025-08-03 11:35:46 -0400

    3640 dp one by one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,64 +7,34 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-use itertools::Itertools;
-use std::collections::{BTreeMap, BTreeSet, HashMap};
-
-// Took from https://pages.di.unipi.it/rossano/blog/2023/mosalgorithm/
-pub fn subarray_majority(nums: &[i32], queries: &[[i32; 3]]) -> Vec<i32> {
+pub fn max_sum_trionic(nums: &[i32]) -> i64 {
+    const MIN: i64 = i64::MIN / 2;
     let n = nums.len();
-    let qn = queries.len();
-    let sqrt = 1 + n.isqrt() as i32;
-    let mut res = vec![-1; qn];
-    let mut track = Tracker::default();
-    let mut curr_left = 0;
-    let mut curr_right = 0; // exclusive
-    for qi in (0..qn).sorted_unstable_by_key(|&i| [queries[i][0] / sqrt, queries[i][1]]) {
-        let [left, right] = [0, 1].map(|i| queries[qi][i] as usize);
-        while curr_left > left {
-            curr_left -= 1;
-            track.update(nums[curr_left], 1);
-        }
-        while curr_right <= right {
-            track.update(nums[curr_right], 1);
-            curr_right += 1;
-        }
-        while curr_left < left {
-            track.update(nums[curr_left], -1);
-            curr_left += 1;
-        }
-        while curr_right > 1 + right {
-            curr_right -= 1;
-            track.update(nums[curr_right], -1);
-        }
-        res[qi] = track.find(queries[qi][2]).unwrap_or(-1);
-    }
-    res
-}
-
-#[derive(Default)]
-struct Tracker {
-    num_freq: HashMap<i32, i32>,
-    freq_nums: BTreeMap<i32, BTreeSet<i32>>,
-}
-
-impl Tracker {
-    fn update(&mut self, num: i32, delta: i32) {
-        let f = self.num_freq.entry(num).or_insert(0);
-        *f += delta;
-        let old = *f - delta;
-        self.freq_nums.entry(old).or_default().remove(&num);
-        self.freq_nums.entry(old + delta).or_default().insert(num);
-    }
-
-    fn find(&self, th: i32) -> Option<i32> {
-        for (_, set) in self.freq_nums.range(th..).rev() {
-            if let Some(&v) = set.first() {
-                return Some(v);
+    // start, inc, dec, inc
+    let [mut dp0, mut dp1, mut dp2, mut dp3] = [0; 4].map(|_| vec![MIN; n]);
+    dp0[0] = i64::from(nums[0]);
+    for i in 1..n {
+        let val = i64::from(nums[i]);
+        match nums[i - 1].cmp(&nums[i]) {
+            std::cmp::Ordering::Less => {
+                // start from [i-1] or [i]
+                dp0[i] = val.max(dp0[i - 1] + val);
+                // continue inc or initiate anew
+                dp1[i] = val + dp1[i - 1].max(dp0[i - 1]);
+                // Similarly, continue inc or initiate anew from dec
+                dp3[i] = val + dp3[i - 1].max(dp2[i - 1]);
+            }
+            std::cmp::Ordering::Equal => {
+                dp0[i] = val; // Have to start anew
+            }
+            std::cmp::Ordering::Greater => {
+                // Start anew since subarr always begins with inc order
+                dp0[i] = val;
+                dp2[i] = val + dp1[i - 1].max(dp2[i - 1]);
             }
         }
-        None
     }
+    *dp3[3..].iter().max().unwrap()
 }
 
 #[cfg(test)]
@@ -98,19 +68,16 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            subarray_majority(&[1, 1, 2, 2, 1, 1], &[[0, 5, 4], [0, 3, 3], [2, 3, 2]]),
-            [1, -1, 2]
-        );
-        assert_eq!(
-            subarray_majority(
-                &[3, 2, 3, 2, 3, 2, 3],
-                &[[0, 6, 4], [1, 5, 2], [2, 4, 1], [3, 3, 1]]
-            ),
-            [3, 2, 3, 2]
-        );
+        assert_eq!(max_sum_trionic(&[0, -2, -1, -3, 0, 2, -1]), -4);
+        assert_eq!(max_sum_trionic(&[1, 4, 2, 7]), 14);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(max_sum_trionic(&[35, 941, 281, 713, -160, 996]), 1970);
+        assert_eq!(
+            max_sum_trionic(&[-754, 167, -172, 202, 735, -941, 992]),
+            988
+        );
+    }
 }
