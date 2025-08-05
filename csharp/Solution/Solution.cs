6201566107eaf803fedcfe79425430d39b4f358e
Zx Wang <ZW_08@outlook.com>
2025-08-05 15:05:11 -0400

    399 bfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,17 +7,54 @@ namespace Solution;
 
 public class Solution
 {
-    public int IntegerReplacement(int n)
+    public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
     {
-        int res = 0;
-        long val = n;
-        while (val != 1)
+        Dictionary<string, int> str_ids = [];
+        foreach (var s in equations.SelectMany(e => e))
         {
-            if ((val & 1) == 0) { val /= 2; }
-            else if (val == 3 || (val - 1) % 4 == 0) { val -= 1; }
-            else { val += 1; }
-            res += 1;
+            str_ids.TryAdd(s, str_ids.Count);
         }
-        return res;
+        int n = str_ids.Count;
+        List<(int node, double w)>[] adj = [.. Enumerable.Range(0, n).Select(_ => new List<(int node, double w)>())];
+        foreach (var (eq, v) in equations.Zip(values))
+        {
+            int n1 = str_ids[eq[0]];
+            int n2 = str_ids[eq[1]];
+            adj[n1].Add((n2, v));
+            adj[n2].Add((n1, 1 / v));
+        }
+        List<double> res = new(queries.Count);
+        foreach (var q in queries)
+        {
+            if (!str_ids.TryGetValue(q[0], out int n1) || !str_ids.TryGetValue(q[1], out int n2))
+            {
+                res.Add(-1.0);
+                continue;
+            }
+            res.Add(Bfs(n1, n2));
+        }
+        return [.. res];
+
+        double Bfs(int n1, int n2)
+        {
+            Queue<(int, double)> queue = [];
+            queue.Enqueue((n1, 1.0));
+            Span<bool> seen = stackalloc bool[n];
+            seen[n1] = true;
+            while (queue.TryDequeue(out var item))
+            {
+                (int node, double w) = item;
+                if (node == n2) { return w; }
+                foreach (var (next, w1) in adj[node])
+                {
+                    if (!seen[next])
+                    {
+                        seen[next] = true;
+                        queue.Enqueue((next, w * w1));
+                    }
+                }
+            }
+            return -1.0;
+        }
     }
 }
