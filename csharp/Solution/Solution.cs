5e698893bed592b8381c1fbd4f3aebd70c751c2b
pkusensei <ZW_08@outlook.com>
2025-08-06 08:57:59 -0400

    400 count chunks of numbers

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,54 +7,20 @@ namespace Solution;
 
 public class Solution
 {
-    public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+    public int FindNthDigit(int n)
     {
-        Dictionary<string, int> str_ids = [];
-        foreach (var s in equations.SelectMany(e => e))
+        long n_ = n;
+        long step = 1;
+        long count = 9;
+        long start = 1;
+        while (n_ > step * count)
         {
-            str_ids.TryAdd(s, str_ids.Count);
+            n_ -= step * count;
+            step += 1;
+            count *= 10;
+            start *= 10;
         }
-        int n = str_ids.Count;
-        List<(int node, double w)>[] adj = [.. Enumerable.Range(0, n).Select(_ => new List<(int node, double w)>())];
-        foreach (var (eq, v) in equations.Zip(values))
-        {
-            int n1 = str_ids[eq[0]];
-            int n2 = str_ids[eq[1]];
-            adj[n1].Add((n2, v));
-            adj[n2].Add((n1, 1 / v));
-        }
-        List<double> res = new(queries.Count);
-        foreach (var q in queries)
-        {
-            if (!str_ids.TryGetValue(q[0], out int n1) || !str_ids.TryGetValue(q[1], out int n2))
-            {
-                res.Add(-1.0);
-                continue;
-            }
-            res.Add(Bfs(n1, n2));
-        }
-        return [.. res];
-
-        double Bfs(int n1, int n2)
-        {
-            Queue<(int, double)> queue = [];
-            queue.Enqueue((n1, 1.0));
-            Span<bool> seen = stackalloc bool[n];
-            seen[n1] = true;
-            while (queue.TryDequeue(out var item))
-            {
-                (int node, double w) = item;
-                if (node == n2) { return w; }
-                foreach (var (next, w1) in adj[node])
-                {
-                    if (!seen[next])
-                    {
-                        seen[next] = true;
-                        queue.Enqueue((next, w * w1));
-                    }
-                }
-            }
-            return -1.0;
-        }
+        string num = (start + (n_ - 1) / step).ToString();
+        return num[(int)((n_ - 1) % step)] - '0';
     }
 }
