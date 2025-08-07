70455466e9762c32a3dcbfe5a5125380bb5e9cd6
Zx Wang <ZW_08@outlook.com>
2025-08-07 12:10:39 -0400

    417 bfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,51 +7,56 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxCollectedFruits(int[][] fruits)
+    public IList<IList<int>> PacificAtlantic(int[][] heights)
     {
-        int n = fruits.Length;
-        int[,] memo = new int[n, n];
-        int res = 0;
-        for (int i = 0; i < n; i++)
+        int rows = heights.Length;
+        int cols = heights[0].Length;
+        HashSet<(int, int)> a = [];
+        Queue<(int, int)> queue = [];
+        for (int i = 0; i < cols; i++)
         {
-            res += fruits[i][i];
-            for (int j = 0; j < n; j++)
-            {
-                memo[i, j] = -1;
-            }
+            a.Add((0, i));
+            queue.Enqueue((0, i));
         }
-        return res + TopRight(0, n - 1) + BottomLeft(n - 1, 0);
-
-        int TopRight(int row, int col)
+        for (int i = 1; i < rows; i++)
         {
-            if (row < 0 || n <= row || col < 0 || n <= col) { return 0; }
-            if (row == n - 1 && col == n - 1) { return 0; }
-            if (row >= col) { return 0; }
-            if (memo[row, col] > -1) { return memo[row, col]; }
-            int curr = 0;
-            foreach (var nc in new[] { col - 1, col, 1 + col })
-            {
-                curr = int.Max(curr, TopRight(1 + row, nc));
-            }
-            curr += fruits[row][col];
-            memo[row, col] = curr;
-            return curr;
+            a.Add((i, 0));
+            queue.Enqueue((i, 0));
         }
-
-        int BottomLeft(int row, int col)
+        a = Bfs(queue, a);
+        queue.Clear();
+        HashSet<(int, int)> b = [];
+        for (int i = 0; i < cols; i++)
         {
-            if (row < 0 || n <= row || col < 0 || n <= col) { return 0; }
-            if (row == n - 1 && col == n - 1) { return 0; }
-            if (col >= row) { return 0; }
-            if (memo[row, col] > -1) { return memo[row, col]; }
-            int curr = 0;
-            foreach (var nr in new[] { row - 1, row, 1 + row })
+            b.Add((rows - 1, i));
+            queue.Enqueue((rows - 1, i));
+        }
+        for (int i = 0; i < rows - 1; i++)
+        {
+            b.Add((i, cols - 1));
+            queue.Enqueue((i, cols - 1));
+        }
+        b = Bfs(queue, b);
+        return [.. a.Intersect(b).Select(v => new[] { v.Item1, v.Item2 })];
+
+        HashSet<(int, int)> Bfs(Queue<(int, int)> queue, HashSet<(int, int)> set)
+        {
+            Span<(int, int)> deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+            while (queue.TryDequeue(out var item))
             {
-                curr = int.Max(curr, BottomLeft(nr, 1 + col));
+                (int r, int c) = item;
+                foreach (var (dr, dc) in deltas)
+                {
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if (0 <= nr && nr < rows && 0 <= nc && nc < cols
+                    && heights[nr][nc] >= heights[r][c] && set.Add((nr, nc)))
+                    {
+                        queue.Enqueue((nr, nc));
+                    }
+                }
             }
-            curr += fruits[row][col];
-            memo[row, col] = curr;
-            return curr;
+            return set;
         }
     }
 }
