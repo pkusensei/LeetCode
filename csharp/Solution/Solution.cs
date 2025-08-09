d7a3cd35639bd3e7dacf955b5b6c6406209575cc
pkusensei <ZW_08@outlook.com>
2025-08-09 18:38:55 -0400

    437 dfs; prefix sum and backtrack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,20 +7,41 @@ namespace Solution;
 
 public class Solution
 {
-    public int EraseOverlapIntervals(int[][] intervals)
+    public int PathSum(TreeNode root, int targetSum)
     {
-        Array.Sort(intervals, (a, b) =>
+        if (root is null) { return 0; }
+        return Dfs(root, targetSum) + PathSum(root.left, targetSum) + PathSum(root.right, targetSum);
+
+        static int Dfs(TreeNode node, long target)
         {
-            if (a[1] == b[1]) { return a[0].CompareTo(b[0]); }
-            return a[1].CompareTo(b[1]);
-        });
-        int prev = int.MinValue;
-        int res = 0;
-        foreach (var item in intervals)
-        {
-            if (prev > item[0]) { res += 1; }
-            else { prev = int.Max(prev, item[1]); }
+            if (node is null) { return 0; }
+            target -= node.val;
+            int res = target == 0 ? 1 : 0;
+            return res + Dfs(node.left, target) + Dfs(node.right, target);
         }
-        return res;
+    }
+
+    public int WithPrefixSum(TreeNode root, int targetSum)
+    {
+        Dictionary<int, int> prefix = new() { [0] = 1 };
+        return Backtrack(root, 0, targetSum);
+
+        int Backtrack(TreeNode node, int lastSum, int targetSum)
+        {
+            if (node is null) { return 0; }
+            int curr = lastSum + node.val;
+            if ((lastSum ^ curr) < 0 && (node.val ^ curr) < 0)
+            {
+                return 0; // checks overflow
+            }
+            int pathCount = prefix.GetValueOrDefault(curr - targetSum, 0);
+            prefix[curr] = prefix.GetValueOrDefault(curr, 0) + 1;
+
+            int leftPaths = Backtrack(node.left, curr, targetSum);
+            int rightPaths = Backtrack(node.right, curr, targetSum);
+
+            prefix[curr] -= 1;
+            return pathCount + leftPaths + rightPaths;
+        }
     }
 }
