55ec591b8707d9b7c22a9c6f66c2f8d255c1da14
pkusensei <ZW_08@outlook.com>
2025-08-10 20:10:01 -0400

    2438

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,31 +7,21 @@ namespace Solution;
 
 public class Solution
 {
-    public Solution()
+    public int[] ProductQueries(int n, int[][] queries)
     {
-        Valid = new int[32][];
+        List<int> ps = [];
         for (int p = 0; p < 32; p++)
         {
-            Valid[p] = Count((int)Math.Pow(2, p));
+            if (((n >> p) & 1) == 1) { ps.Add((int)Math.Pow(2, p)); }
         }
-    }
-
-    public bool ReorderedPowerOf2(int n)
-    {
-        var curr = Count(n);
-        return Valid.Any(v => v.SequenceEqual(curr));
-    }
-
-    int[][] Valid { get; }
-
-    private static int[] Count(int num)
-    {
-        string s = num.ToString();
-        int[] curr = new int[10];
-        foreach (var c in s)
+        return queries.Select(q =>
         {
-            curr[c - '0'] += 1;
-        }
-        return curr;
+            long res = 1;
+            for (int i = q[0]; i <= q[1]; i++)
+            {
+                res = res * ps[i] % 1_000_000_007;
+            }
+            return (int)res;
+        }).ToArray();
     }
 }
