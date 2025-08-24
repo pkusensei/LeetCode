a7b60ba48096aab44745f3d9e5745e4ca5c02a9c
pkusensei <ZW_08@outlook.com>
2025-08-24 16:09:40 -0400

    3661 clean up

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,45 +7,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-use itertools::{Itertools, izip};
-use std::collections::HashMap;
-
 pub fn max_walls(robots: &[i32], distance: &[i32], mut walls: Vec<i32>) -> i32 {
+    use itertools::{Itertools, izip};
     walls.sort_unstable();
     // [rob, dist]
     let robs = izip!(robots.iter(), distance.iter())
         .map(|(&r, &d)| [r, d])
         .sorted_unstable()
         .collect_vec();
-    dfs(&robs, &walls, 0, 0, &mut HashMap::new())
+    let n = robs.len();
+    dfs(&robs, &walls, 0, 0, 0, &mut vec![[-1; 2]; n])
 }
 
 fn dfs(
     robs: &[[i32; 2]],
     walls: &[i32],
     idx: usize,
-    prev: i32,
-    memo: &mut HashMap<(usize, i32), i32>,
+    prev_dir: usize,
+    prev_end: i32,
+    memo: &mut [[i32; 2]],
 ) -> i32 {
     let Some(&[pos, dist]) = robs.get(idx) else {
         return 0;
     };
-    if let Some(&v) = memo.get(&(idx, prev)) {
-        return v;
+    if memo[idx][prev_dir] > -1 {
+        return memo[idx][prev_dir];
     }
-    let left = (1 + prev).max(pos - dist);
+    let left = (1 + prev_end).max(pos - dist);
     let i1 = walls.partition_point(|&v| v < left);
     let i2 = walls.partition_point(|&v| v <= pos);
-    let v1 = (i2 - i1) as i32 + dfs(robs, walls, 1 + idx, pos, memo);
+    let v1 = (i2 - i1) as i32 + dfs(robs, walls, 1 + idx, 0, pos, memo);
     let right = robs
         .get(1 + idx)
         .map(|&v| (v[0] - 1).min(pos + dist))
         .unwrap_or(pos + dist);
     let i3 = walls.partition_point(|&v| v < pos);
     let i4 = walls.partition_point(|&v| v <= right);
-    let v2 = (i4 - i3) as i32 + dfs(robs, walls, 1 + idx, right, memo);
+    let v2 = (i4 - i3) as i32 + dfs(robs, walls, 1 + idx, 1, right, memo);
     let res = v1.max(v2);
-    memo.insert((idx, prev), res);
+    memo[idx][prev_dir] = res;
     res
 }
 
