2b1a2cc8a98904128d52df4904f23ced31a87330
pkusensei <ZW_08@outlook.com>
2025-08-27 10:39:05 -0400

    476 32-leading zeros

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,22 +7,9 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindRadius(int[] houses, int[] heaters)
+    public int FindComplement(int num)
     {
-        Array.Sort(heaters);
-        int n = heaters.Length;
-        int res = 0;
-        foreach (var house in houses)
-        {
-            int idx = Array.BinarySearch(heaters, house);
-            if (idx < 0)
-            {
-                idx = ~idx;
-                if (idx == 0) { res = int.Max(res, heaters[0] - house); }
-                else if (idx == n) { res = int.Max(res, house - heaters[n - 1]); }
-                else { res = int.Max(res, int.Min(house - heaters[idx - 1], heaters[idx] - house)); }
-            }
-        }
-        return res;
+        int zeros = int.LeadingZeroCount(num);
+        return ((1 << (32 - zeros)) - 1) ^ num;
     }
 }
