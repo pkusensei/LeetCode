bd0ee80f0b0a4d00449fb446aff4cc8410c355bd
pkusensei <ZW_08@outlook.com>
2025-08-27 13:10:09 -0400

    478 radius*sqrt(rand())

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,9 +7,23 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindComplement(int num)
+    public Solution(double radius, double x_center, double y_center)
     {
-        int zeros = int.LeadingZeroCount(num);
-        return ((1 << (32 - zeros)) - 1) ^ num;
+        Radius = radius;
+        X = x_center;
+        Y = y_center;
+        Rng = new Random();
+    }
+
+    public double Radius { get; }
+    public double X { get; }
+    public double Y { get; }
+    public Random Rng { get; }
+
+    public double[] RandPoint()
+    {
+        double angle = Rng.NextDouble() * double.Pi * 2;
+        double r = Radius * double.Sqrt(Rng.NextDouble());
+        return [X + r * double.Cos(angle), Y + r * double.Sin(angle)];
     }
 }
