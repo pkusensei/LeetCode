b605ac58a4c98b2b47f3bad36aa6bcce1048a659
pkusensei <ZW_08@outlook.com>
2025-08-28 21:08:51 -0400

    3021 reading...

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,34 +7,10 @@ namespace Solution;
 
 public class Solution
 {
-    public int LargestPalindrome(int n)
+    public long FlowerGame(int n, int m)
     {
-        long up = (long)Math.Pow(10, n) - 1;
-        long down = (long)Math.Pow(10, n - 1);
-        for (long i = up; i >= down; i -= 1)
-        {
-            string s = i.ToString();
-            StringBuilder sb = new(s);
-            sb.Append([.. s.Reverse()]);
-            if (long.TryParse(sb.ToString(), out var v) && Check(v))
-            {
-                return (int)(v % 1337);
-            }
-        }
-        return 9;
-
-        bool Check(long num)
-        {
-            for (long i = up; i >= down; i -= 1)
-            {
-                if (i * i < num) { break; }
-                if (num % i == 0)
-                {
-                    long val = num / i;
-                    if (down <= val && val <= up) { return true; }
-                }
-            }
-            return false;
-        }
+        long nn = n;
+        long mm = m;
+        return (nn + 1) / 2 * (mm / 2) + nn / 2 * ((mm + 1) / 2);
     }
 }
