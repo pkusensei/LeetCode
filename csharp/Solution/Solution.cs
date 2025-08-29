b5e2b054e6bad0a42c2ab516ad75762e3851f584
pkusensei <ZW_08@outlook.com>
2025-08-29 19:01:34 -0400

    480 two heaps and lazy deletion

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,10 +7,70 @@ namespace Solution;
 
 public class Solution
 {
-    public long FlowerGame(int n, int m)
+    public double[] MedianSlidingWindow(int[] nums, int k)
     {
-        long nn = n;
-        long mm = m;
-        return (nn + 1) / 2 * (mm / 2) + nn / 2 * ((mm + 1) / 2);
+        // upper half
+        PriorityQueue<int, int> min_heap = new();
+        // lower half
+        PriorityQueue<int, int> max_heap = new(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+        Dictionary<int, int> freq = [];
+        int n = nums.Length;
+        List<double> res = new(n - k);
+        foreach (var num in nums[..k])
+        {
+            max_heap.Enqueue(num, num);
+            min_heap.Enqueue(max_heap.Peek(), max_heap.Dequeue()); // Params eval-ed in order
+            if (min_heap.Count > max_heap.Count)
+            {
+                max_heap.Enqueue(min_heap.Peek(), min_heap.Dequeue());
+            }
+        }
+        double med = GetMedian();
+        res.Add(med);
+        for (int i = k; i < n; i++)
+        {
+            int prev = nums[i - k];
+            // Lazy deletion
+            if (!freq.TryAdd(prev, 1)) { freq[prev] += 1; }
+            // Remove from lower half vs from upper half
+            int balance = prev <= med ? -1 : 1;
+            if (nums[i] <= med)
+            {
+                balance += 1; // add to lower half
+                max_heap.Enqueue(nums[i], nums[i]);
+            }
+            else
+            {
+                balance -= 1;
+                min_heap.Enqueue(nums[i], nums[i]);
+            }
+            if (balance < 0) // balance==-2 <== upper half has too many numbers
+            {
+                max_heap.Enqueue(min_heap.Peek(), min_heap.Dequeue());
+            }
+            else if (balance > 0) // balance==2
+            {
+                min_heap.Enqueue(max_heap.Peek(), max_heap.Dequeue());
+            }
+            while (max_heap.TryPeek(out int top, out _) && freq.TryGetValue(top, out var f) && f > 0)
+            {
+                freq[top] -= 1;
+                max_heap.Dequeue();
+            }
+            while (min_heap.TryPeek(out int top, out _) && freq.TryGetValue(top, out var f) && f > 0)
+            {
+                freq[top] -= 1;
+                min_heap.Dequeue();
+            }
+            med = GetMedian();
+            res.Add(med);
+        }
+        return [.. res];
+
+        double GetMedian()
+        {
+            if ((k & 1) == 1) { return max_heap.Peek(); }
+            else { return ((double)max_heap.Peek() + min_heap.Peek()) / 2; }
+        }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -7,11 +7,14 @@ namespace Tests;
 [TestClass]
 public class UnitTest
 {
-    // readonly Solution.Solution sol = new();
+    readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(new[] { 1.0, -1.0, -1.0, 3.0, 5.0, 6.0 }, new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3)]
+    [DataRow(new[] { 2.0, 3.0, 3.0, 3.0, 2.0, 3.0, 2.0 }, new[] { 1, 2, 3, 4, 2, 3, 1, 4, 2 }, 3)]
+    public void TestMethod1(double[] exp, int[] nums, int k)
     {
+        Assert.IsTrue(exp.SequenceEqual(sol.MedianSlidingWindow(nums, k)));
     }
 
     [TestMethod]
