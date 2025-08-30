23c3b78c200de61fb0cb294af2d1633bdd2c7a95
pkusensei <ZW_08@outlook.com>
2025-08-30 13:59:57 -0400

    483 binary search

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,16 +7,32 @@ namespace Solution;
 
 public class Solution
 {
-    public int MagicalString(int n)
+    public string SmallestGoodBase(string n)
     {
-        List<int> s = new(1 + n) { 1, 2, 2 };
-        int i = 2;
-        while (s.Count < n)
+        // n  = k^0 + k^1 + .. + k^(p-1)
+        // n > k^(p-1) ==> k<n^(1/(p-1))
+        // kn =       k^1 + .. + k^(p-1) + k^p
+        // (k-1)n = k^p - 1
+        // n = (k^p - 1)/(k-1) ==> n <= k^p - 1 ==> (n+1)^(1/p)<=k
+        if (!long.TryParse(n, out var num)) { return n; }
+        int max_p = (int)Math.Log2(1 + num);
+        for (int p = max_p; p > 2; p -= 1)
         {
-            int item = 3 - s.Last();
-            s.AddRange(Enumerable.Range(0, s[i]).Select(_ => item));
-            i += 1;
+            long left = (long)Math.Pow(1 + num, 1.0 / p);
+            long right = (long)Math.Pow(num, 1.0 / (p - 1));
+            while (left <= right)
+            {
+                long mid = left + (right - left) / 2;
+                long acc = 0;
+                for (int i = 0; i < p; i++)
+                {
+                    acc = acc * mid + 1;
+                }
+                if (num == acc) { return mid.ToString(); }
+                else if (acc < num) { left = 1 + mid; }
+                else { right = mid - 1; }
+            }
         }
-        return s.Take(n).Count(v => v == 1);
+        return (num - 1).ToString();
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,11 +10,13 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(3, 6)]
-    [DataRow(1, 1)]
-    public void TestMethod1(int exp, int n)
+    [DataRow("3", "13")]
+    [DataRow("8", "4681")]
+    [DataRow("999999999999999999", "1000000000000000000")]
+    [DataRow("686286299", "470988884881403701")]
+    public void TestMethod1(string exp, string n)
     {
-        Assert.AreEqual(exp, sol.MagicalString(n));
+        Assert.AreEqual(exp, sol.SmallestGoodBase(n));
     }
 
     [TestMethod]
