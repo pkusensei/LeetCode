23dcd82d8dc79f326544fb8497e20b41922c1d85
pkusensei <ZW_08@outlook.com>
2025-08-31 08:57:43 -0400

    3663 biwe_conq164 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,67 +7,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
-    let mut masks = Masks::default();
-    for (r, row) in board.iter().enumerate() {
-        for (c, &ch) in row.iter().enumerate() {
-            if let Some(v) = ch.to_digit(10) {
-                masks.add(r, c, v);
-            }
+pub fn get_least_frequent_digit(mut n: i32) -> i32 {
+    let mut freq = [0; 10];
+    while n > 0 {
+        freq[(n % 10) as usize] += 1;
+        n /= 10;
+    }
+    let mut f = i32::MAX;
+    let mut res = 9;
+    for (i, v) in freq.into_iter().enumerate() {
+        if v > 0 && v < f {
+            f = v;
+            res = i as i32
         }
     }
-    backtrack(board, &mut masks);
-}
-
-const N: usize = 9;
-
-fn backtrack(board: &mut Vec<Vec<char>>, masks: &mut Masks) -> bool {
-    for r in 0..N {
-        for c in 0..N {
-            if board[r][c] == '.' {
-                for num in 1..=9 {
-                    if masks.check(r, c, num) {
-                        board[r][c] = char::from_digit(num, 10).unwrap();
-                        masks.add(r, c, num);
-                        if backtrack(board, masks) {
-                            return true;
-                        }
-                        board[r][c] = '.';
-                        masks.remove(r, c, num);
-                    }
-                }
-                return false;
-            }
-        }
-    }
-    true
-}
-
-#[derive(Default)]
-struct Masks {
-    row_masks: [u16; N],
-    col_masks: [u16; N],
-    box_masks: [[u16; 3]; 3],
-}
-
-impl Masks {
-    fn add(&mut self, r: usize, c: usize, v: u32) {
-        self.row_masks[r] |= 1 << v;
-        self.col_masks[c] |= 1 << v;
-        self.box_masks[r / 3][c / 3] |= 1 << v;
-    }
-
-    fn check(&self, r: usize, c: usize, v: u32) -> bool {
-        (self.row_masks[r] >> v) & 1 == 0
-            && (self.col_masks[c] >> v) & 1 == 0
-            && (self.box_masks[r / 3][c / 3] >> v) & 1 == 0
-    }
-
-    fn remove(&mut self, r: usize, c: usize, v: u32) {
-        self.row_masks[r] ^= 1 << v;
-        self.col_masks[c] ^= 1 << v;
-        self.box_masks[r / 3][c / 3] ^= 1 << v;
-    }
+    res
 }
 
 #[cfg(test)]
@@ -100,7 +54,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(get_least_frequent_digit(1553322), 1);
+        assert_eq!(get_least_frequent_digit(723344511), 2);
+    }
 
     #[test]
     fn test() {}
