618fa3ef3d68ec7835a3262efd1e0e10c0cedf43
pkusensei <ZW_08@outlook.com>
2025-08-30 20:46:15 -0400

    37 backtrack and bitmask

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,46 +7,67 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_walls(robots: &[i32], distance: &[i32], mut walls: Vec<i32>) -> i32 {
-    use itertools::{Itertools, izip};
-    walls.sort_unstable();
-    // [rob, dist]
-    let robs = izip!(robots.iter(), distance.iter())
-        .map(|(&r, &d)| [r, d])
-        .sorted_unstable()
-        .collect_vec();
-    let n = robs.len();
-    dfs(&robs, &walls, 0, 0, 0, &mut vec![[-1; 2]; n])
+pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+    let mut masks = Masks::default();
+    for (r, row) in board.iter().enumerate() {
+        for (c, &ch) in row.iter().enumerate() {
+            if let Some(v) = ch.to_digit(10) {
+                masks.add(r, c, v);
+            }
+        }
+    }
+    backtrack(board, &mut masks);
 }
 
-fn dfs(
-    robs: &[[i32; 2]],
-    walls: &[i32],
-    idx: usize,
-    prev_dir: usize,
-    prev_end: i32,
-    memo: &mut [[i32; 2]],
-) -> i32 {
-    let Some(&[pos, dist]) = robs.get(idx) else {
-        return 0;
-    };
-    if memo[idx][prev_dir] > -1 {
-        return memo[idx][prev_dir];
+const N: usize = 9;
+
+fn backtrack(board: &mut Vec<Vec<char>>, masks: &mut Masks) -> bool {
+    for r in 0..N {
+        for c in 0..N {
+            if board[r][c] == '.' {
+                for num in 1..=9 {
+                    if masks.check(r, c, num) {
+                        board[r][c] = char::from_digit(num, 10).unwrap();
+                        masks.add(r, c, num);
+                        if backtrack(board, masks) {
+                            return true;
+                        }
+                        board[r][c] = '.';
+                        masks.remove(r, c, num);
+                    }
+                }
+                return false;
+            }
+        }
     }
-    let left = (1 + prev_end).max(pos - dist);
-    let i1 = walls.partition_point(|&v| v < left);
-    let i2 = walls.partition_point(|&v| v <= pos);
-    let v1 = (i2 - i1) as i32 + dfs(robs, walls, 1 + idx, 0, pos, memo);
-    let right = robs
-        .get(1 + idx)
-        .map(|&v| (v[0] - 1).min(pos + dist))
-        .unwrap_or(pos + dist);
-    let i3 = walls.partition_point(|&v| v < pos);
-    let i4 = walls.partition_point(|&v| v <= right);
-    let v2 = (i4 - i3) as i32 + dfs(robs, walls, 1 + idx, 1, right, memo);
-    let res = v1.max(v2);
-    memo[idx][prev_dir] = res;
-    res
+    true
+}
+
+#[derive(Default)]
+struct Masks {
+    row_masks: [u16; N],
+    col_masks: [u16; N],
+    box_masks: [[u16; 3]; 3],
+}
+
+impl Masks {
+    fn add(&mut self, r: usize, c: usize, v: u32) {
+        self.row_masks[r] |= 1 << v;
+        self.col_masks[c] |= 1 << v;
+        self.box_masks[r / 3][c / 3] |= 1 << v;
+    }
+
+    fn check(&self, r: usize, c: usize, v: u32) -> bool {
+        (self.row_masks[r] >> v) & 1 == 0
+            && (self.col_masks[c] >> v) & 1 == 0
+            && (self.box_masks[r / 3][c / 3] >> v) & 1 == 0
+    }
+
+    fn remove(&mut self, r: usize, c: usize, v: u32) {
+        self.row_masks[r] ^= 1 << v;
+        self.col_masks[c] ^= 1 << v;
+        self.box_masks[r / 3][c / 3] ^= 1 << v;
+    }
 }
 
 #[cfg(test)]
@@ -79,11 +100,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_walls(&[4], &[3], vec![1, 10]), 1);
-        assert_eq!(max_walls(&[10, 2], &[5, 1], vec![5, 2, 7]), 3);
-        assert_eq!(max_walls(&[1, 2], &[100, 1], vec![10]), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
