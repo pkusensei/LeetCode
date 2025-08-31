3255dfc055477e87901b0eed2e55ea8a367e4e0e
pkusensei <ZW_08@outlook.com>
2025-08-31 14:47:07 -0400

    3668 week_cont465 q1; binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,36 +7,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_operations(s: &str, k: i32) -> i32 {
-    use std::collections::{BTreeSet, VecDeque};
-    let n = s.len() as i32;
-    let zeros = s.bytes().filter(|&v| v == b'0').count() as i32;
-    let mut queue = VecDeque::from([(zeros, 0)]);
-    let mut odd_set: BTreeSet<_> = (0..=n).filter(|&v| v != zeros && v & 1 == 1).collect();
-    let mut even_set: BTreeSet<_> = (0..=n).filter(|&v| v != zeros && v & 1 == 0).collect();
-    while let Some((zeros, step)) = queue.pop_front() {
-        if zeros == 0 {
-            return step;
-        }
-        let min_ones = (k - zeros).max(0);
-        let max_ones = k.min(n - zeros);
-        let lower = zeros + 2 * min_ones - k;
-        let upper = zeros + 2 * max_ones - k;
-        let set = if lower & 1 == 1 {
-            &mut odd_set
-        } else {
-            &mut even_set
-        };
-        let mut del = vec![];
-        for &v in set.range(lower..=upper) {
-            queue.push_back((v, 1 + step));
-            del.push(v);
-        }
-        for v in del {
-            set.remove(&v);
+pub fn recover_order(order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![];
+    for num in order {
+        if friends.binary_search(&num).is_ok() {
+            res.push(num);
         }
     }
-    -1
+    res
 }
 
 #[cfg(test)]
@@ -69,11 +47,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_operations("110", 1), 1);
-        assert_eq!(min_operations("0101", 3), 2);
-        assert_eq!(min_operations("101", 2), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
