4e64df009ffc46f61c6d943fabf6959d33cacf4a
Zx Wang <ZW_08@outlook.com>
2025-09-02 14:22:09 -0400

    497 random over area

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,14 +7,37 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindPoisonedDuration(int[] timeSeries, int duration)
+    public Solution(int[][] rects)
     {
-        if (duration == 0 || timeSeries.Length == 0) { return 0; }
-        int res = 0;
-        for (int i = 0; i < timeSeries.Length - 1; i++)
+        N = rects.Length;
+        Rects = new(N);
+        Prefix = new(N);
+        Rng = new();
+        foreach (var r in rects)
         {
-            res += int.Min(timeSeries[1 + i] - timeSeries[i], duration);
+            Rect curr = new(r[0], r[1], r[2], r[3]);
+            Rects.Add(curr);
+            Prefix.Add(curr.Dots + Prefix.LastOrDefault());
         }
-        return res + duration;
     }
+
+    int N { get; }
+    List<Rect> Rects { get; }
+    List<long> Prefix { get; }
+    Random Rng { get; }
+
+    public int[] Pick()
+    {
+        long all_dots = Prefix[^1];
+        long count = Rng.NextInt64(1, 1 + all_dots);
+        int i = Prefix.BinarySearch(count);
+        if (i < 0) { i = ~i; }
+        return Rects[i].Point(Rng);
+    }
+}
+
+readonly record struct Rect(int X1, int Y1, int X2, int Y2)
+{
+    public readonly long Dots => (X2 - X1 + 1L) * (Y2 - Y1 + 1L);
+    public readonly int[] Point(Random rng) => [rng.Next(X1, 1 + X2), rng.Next(Y1, 1 + Y2)];
 }
