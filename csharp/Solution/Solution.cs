0c315b624f43e87d317ced53073779d6d5d41a62
Zx Wang <ZW_08@outlook.com>
2025-09-02 13:33:13 -0400

    495

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,45 +7,14 @@ namespace Solution;
 
 public class Solution
 {
-    public int ReversePairs(int[] nums)
+    public int FindPoisonedDuration(int[] timeSeries, int duration)
     {
-        return MergeSort(nums);
-
-        static int MergeSort(Span<int> nums)
+        if (duration == 0 || timeSeries.Length == 0) { return 0; }
+        int res = 0;
+        for (int i = 0; i < timeSeries.Length - 1; i++)
         {
-            if (nums.Length < 2) { return 0; }
-            int mid = nums.Length / 2;
-            int res = MergeSort(nums[..mid]) + MergeSort(nums[mid..]);
-            int left = 0;
-            int right = mid;
-            for (; left < mid; left++)
-            {
-                for (; right < nums.Length && nums[left] > 2L * nums[right]; right++) { }
-                res += right - mid;
-            }
-            left = 0;
-            right = mid;
-            List<int> vals = new(nums.Length);
-            while (left < mid && right < nums.Length)
-            {
-                if (nums[left] <= nums[right])
-                {
-                    vals.Add(nums[left]);
-                    left += 1;
-                }
-                else
-                {
-                    vals.Add(nums[right]);
-                    right += 1;
-                }
-            }
-            vals.AddRange(nums[left..mid]);
-            vals.AddRange(nums[right..]);
-            for (int i = 0; i < nums.Length; i++)
-            {
-                nums[i] = vals[i];
-            }
-            return res;
+            res += int.Min(timeSeries[1 + i] - timeSeries[i], duration);
         }
+        return res + duration;
     }
 }
