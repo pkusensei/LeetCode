0887deee717ed94c2bdeeb08d61d209388b91b74
Zx Wang <ZW_08@outlook.com>
2025-09-02 11:24:06 -0400

    491 backtrack, bitmask, and skip dups

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,65 +7,35 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindMinStep(string board, string hand)
+    public IList<IList<int>> FindSubsequences(int[] nums)
     {
-        List<byte> board_ = Compress(board);
-        List<byte> hand_ = Compress(hand);
-        hand_.Sort();
-        Queue<(List<byte> board, List<byte> hand, int step)> queue = [];
-        queue.Enqueue((board_, hand_, 0));
-        HashSet<(List<byte>, List<byte>)> seen = [(board_, hand_)];
-        while (queue.TryDequeue(out var item))
+        List<IList<int>> res = [];
+        Backtrack(0, 0, 0);
+        return res;
+
+        void Backtrack(int idx, int prev, int mask)
         {
-            if (item.board.Count == 0) { return item.step; }
-            for (int bi = 0; bi < item.board.Count; bi++)
+            if (idx >= nums.Length)
             {
-                for (int hi = 0; hi < item.hand.Count; hi++)
+                if (int.PopCount(mask) >= 2)
                 {
-                    if (bi > 0 && item.board[bi - 1] == item.hand[hi]) { continue; }
-                    if (hi > 0 && item.hand[hi - 1] == item.hand[hi]) { continue; }
-                    if (item.board[bi] == item.hand[hi]
-                        || (bi > 0 && item.board[bi - 1] == item.board[bi]))
+                    List<int> curr = new(int.PopCount(mask));
+                    for (int bit = 0; bit < nums.Length; bit++)
                     {
-                        List<byte> nboard = [.. item.board];
-                        List<byte> nhand = [.. item.hand];
-                        nboard.Insert(bi, item.hand[hi]);
-                        nhand.RemoveAt(hi);
-                        nboard = Process(nboard, bi);
-                        if (seen.Add((nboard, nhand)))
-                        {
-                            queue.Enqueue((nboard, nhand, 1 + item.step));
-                        }
+                        if (((mask >> bit) & 1) == 1) { curr.Add(nums[bit]); }
                     }
+                    res.Add(curr);
                 }
+                return;
             }
-        }
-        return -1;
-
-        static List<byte> Process(List<byte> s, int i)
-        {
-            if (i >= s.Count) { return s; }
-            int left = i;
-            for (; left > 0 && s[left - 1] == s[i]; left -= 1) { }
-            int right = i;
-            for (; 1 + right < s.Count && s[1 + right] == s[i]; right += 1) { }
-            int count = right - left + 1;
-            if (count >= 3)
+            if (mask == 0 || prev <= nums[idx])
             {
-                s.RemoveRange(left, count);
-                return Process(s, left);
+                Backtrack(1 + idx, nums[idx], mask | (1 << idx));
             }
-            return s;
-        }
-
-        static List<byte> Compress(ReadOnlySpan<char> s)
-        {
-            List<byte> res = new(s.Length);
-            foreach (var c in s)
+            if (mask == 0 || prev != nums[idx])
             {
-                res.Add((byte)(c - 'A'));
+                Backtrack(1 + idx, prev, mask);
             }
-            return res;
         }
     }
 }
