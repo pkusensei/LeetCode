b8b7152003044ad748a9de25543b3d36402baeb0
pkusensei <ZW_08@outlook.com>
2025-09-03 11:55:31 -0400

    514 dfs/memo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,23 +7,45 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindBottomLeftValue(TreeNode root)
+    public int FindRotateSteps(string ring, string key)
     {
-        int max_depth = -1;
-        int res = 0;
-        Dfs(root, 0);
-        return res;
-
-        void Dfs(TreeNode node, int depth)
+        int rn = ring.Length;
+        int kn = key.Length;
+        int[,] memo = new int[rn, kn];
+        for (int i1 = 0; i1 < rn; i1++)
         {
-            if (node is null) { return; }
-            if (depth > max_depth)
+            for (int i2 = 0; i2 < kn; i2++)
             {
-                max_depth = depth;
-                res = node.val;
+                memo[i1, i2] = -1;
             }
-            Dfs(node.left, 1 + depth);
-            Dfs(node.right, 1 + depth);
+        }
+        return Dfs(0, 0);
+
+        int Dfs(int ri, int ki)
+        {
+            if (ki >= key.Length) { return 0; }
+            if (memo[ri, ki] > -1) { return memo[ri, ki]; }
+            int res;
+            if (ring[ri] == key[ki]) { res = 1 + Dfs(ri, 1 + ki); }
+            else
+            {
+                int step = 0;
+                int curr = ri;
+                for (; ring[curr] != key[ki]; curr = (1 + curr) % rn)
+                {
+                    step += 1;
+                }
+                res = 1 + step + Dfs(curr, 1 + ki);
+                step = 0;
+                curr = ri;
+                for (; ring[curr] != key[ki]; curr = (curr + rn - 1) % rn)
+                {
+                    step += 1;
+                }
+                res = int.Min(res, 1 + step + Dfs(curr, 1 + ki));
+            }
+            memo[ri, ki] = res;
+            return res;
         }
     }
 }
