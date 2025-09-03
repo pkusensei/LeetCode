b4bed8e65bb43a9469e9fd4efa1d6aa51737c8b2
pkusensei <ZW_08@outlook.com>
2025-09-03 09:26:24 -0400

    509 fib in const context

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,57 +7,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn reverse_pairs(nums: &[i32]) -> i32 {
-    use itertools::Itertools;
-    use std::collections::BTreeMap;
-    let map: BTreeMap<i64, usize> = nums
-        .iter()
-        .copied()
-        .sorted_unstable_by(|a, b| b.cmp(a)) // Big number gets smaller idx
-        .dedup()
-        .enumerate()
-        .map(|(i, v)| (i64::from(v), 1 + i)) // +1 for Fenwick Tree
-        .collect();
-    dbg!(&map);
-    let mut ft = FenwickTree::new(map.len());
-    let mut res = 0;
-    for &num in nums.iter() {
-        if let Some((_, &i)) = map.range(1 + 2 * i64::from(num)..).next() {
-            res += ft.query(i);
-        };
-        ft.update(map[&i64::from(num)], 1);
-    }
-    res
+pub fn fib(n: i32) -> i32 {
+    FIB[n as usize]
 }
 
-struct FenwickTree {
-    tree: Vec<i32>,
-}
-
-impl FenwickTree {
-    fn new(n: usize) -> Self {
-        Self {
-            tree: vec![0; 1 + n],
-        }
+const N: usize = 31;
+const FIB: [i32; N] = {
+    let mut f = [0; N];
+    f[1] = 1;
+    let mut i = 2;
+    while i < N {
+        f[i] = f[i - 1] + f[i - 2];
+        i += 1;
     }
-
-    fn update(&mut self, mut idx: usize, val: i32) {
-        let sz = self.tree.len();
-        while idx < sz {
-            self.tree[idx] += val;
-            idx += idx & idx.wrapping_neg();
-        }
-    }
-
-    fn query(&self, mut idx: usize) -> i32 {
-        let mut res = 0;
-        while idx > 0 {
-            res += self.tree[idx];
-            idx -= idx & idx.wrapping_neg();
-        }
-        res
-    }
-}
+    f
+};
 
 #[cfg(test)]
 mod tests {
@@ -89,9 +53,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(reverse_pairs(&[1, 3, 2, 3, 1]), 2);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
