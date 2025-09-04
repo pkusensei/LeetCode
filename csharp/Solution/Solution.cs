b263d4b2e660d162ed6c99baabcc53e3e594be86
Zx Wang <ZW_08@outlook.com>
2025-09-04 14:01:38 -0400

    525 treat 0 as -1

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,19 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public bool CheckSubarraySum(int[] nums, int k)
+    public int FindMaxLength(int[] nums)
     {
-        Dictionary<int, int> seen = new() { [0] = -1 };
         int prefix = 0;
+        int res = 0;
+        Dictionary<int, int> seen = new() { [0] = -1 };
         for (int i = 0; i < nums.Length; i++)
         {
-            prefix = (prefix + nums[i]) % k;
-            if (seen.TryGetValue(prefix, out var prev) && i - prev >= 2)
-            {
-                return true;
-            }
+            prefix += nums[i] > 0 ? 1 : -1;
+            if (seen.TryGetValue(prefix, out var prev)) { res = int.Max(res, i - prev); }
             seen.TryAdd(prefix, i);
         }
-        return false;
+        return res;
     }
 }
