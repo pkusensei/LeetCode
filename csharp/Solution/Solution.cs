7d7b66760e4e313ffc81e45de4d4c8ff641f5d5c
Zx Wang <ZW_08@outlook.com>
2025-09-04 10:42:29 -0400

    517 flow/bottleneck; two pass

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,52 +7,26 @@ namespace Solution;
 
 public class Solution
 {
-    public int LongestPalindromeSubseq(string s)
+    public int FindMinMoves(int[] machines)
     {
-        int n = s.Length;
-        int[,] dp = new int[n, n];
-        for (int left = n - 1; left >= 0; left -= 1)
+        int n = machines.Length;
+        int sum = machines.Sum();
+        if (sum % n > 0) { return -1; }
+        int ave = sum / n;
+        int prefix = 0;
+        int res = 0;
+        for (int i = 0; i < n; i++)
         {
-            dp[left, left] = 1;
-            for (int right = 1 + left; right < n; right++)
-            {
-                if (s[left] == s[right])
-                {
-                    dp[left, right] = 2 + dp[1 + left, right - 1];
-                }
-                else
-                {
-                    dp[left, right] = int.Max(dp[1 + left, right], dp[left, right - 1]);
-                }
-            }
+            res = int.Max(res, machines[i] - ave);
+            prefix += machines[i] - ave;
+            res = int.Max(res, prefix);
         }
-        return dp[0, n - 1];
-        int[,] memo = new int[n, n];
-        for (int i1 = 0; i1 < n; i1++)
+        int suffix = 0;
+        for (int i = n - 1; i >= 0; i -= 1)
         {
-            for (int i2 = 0; i2 < n; i2++)
-            {
-                memo[i1, i2] = -1;
-            }
+            suffix += machines[i] - ave;
+            res = int.Max(res, suffix);
         }
-        // return Dfs(0, n - 1);
-
-        int Dfs(int left, int right)
-        {
-            if (left > right) { return 0; }
-            if (left == right) { return 1; }
-            if (memo[left, right] > -1) { return memo[left, right]; }
-            int res;
-            if (s[left] == s[right])
-            {
-                res = 2 + Dfs(1 + left, right - 1);
-            }
-            else
-            {
-                res = int.Max(Dfs(left + 1, right), Dfs(left, right - 1));
-            }
-            memo[left, right] = res;
-            return res;
-        }
+        return res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,13 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(3, new[] { 1, 0, 5 })]
+    [DataRow(2, new[] { 0, 3, 0 })]
+    [DataRow(-1, new[] { 0, 2, 0 })]
+    [DataRow(8, new[] { 0, 0, 11, 5 })]
+    public void TestMethod1(int exp, int[] nums)
     {
+        Assert.AreEqual(exp, sol.FindMinMoves(nums));
     }
 
     [TestMethod]
