dc4650b153f92f625762016a8cc2eb4997596454
Zx Wang <ZW_08@outlook.com>
2025-09-04 11:28:36 -0400

    518 process each coin in order

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,26 +7,20 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindMinMoves(int[] machines)
+    public int Change(int amount, int[] coins)
     {
-        int n = machines.Length;
-        int sum = machines.Sum();
-        if (sum % n > 0) { return -1; }
-        int ave = sum / n;
-        int prefix = 0;
-        int res = 0;
-        for (int i = 0; i < n; i++)
+        long[] dp = new long[1 + amount];
+        dp[0] = 1;
+        foreach (var c in coins)
         {
-            res = int.Max(res, machines[i] - ave);
-            prefix += machines[i] - ave;
-            res = int.Max(res, prefix);
+            for (int i = 0; i <= amount; i++)
+            {
+                if (c + i <= amount)
+                {
+                    dp[c + i] += dp[i];
+                }
+            }
         }
-        int suffix = 0;
-        for (int i = n - 1; i >= 0; i -= 1)
-        {
-            suffix += machines[i] - ave;
-            res = int.Max(res, suffix);
-        }
-        return res;
+        return (int)dp[^1];
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,13 +10,8 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(3, new[] { 1, 0, 5 })]
-    [DataRow(2, new[] { 0, 3, 0 })]
-    [DataRow(-1, new[] { 0, 2, 0 })]
-    [DataRow(8, new[] { 0, 0, 11, 5 })]
-    public void TestMethod1(int exp, int[] nums)
+    public void TestMethod1()
     {
-        Assert.AreEqual(exp, sol.FindMinMoves(nums));
     }
 
     [TestMethod]
