51170372efa84690ac93dc4ec98433680e50ce9f
pkusensei <ZW_08@outlook.com>
2025-09-04 19:52:54 -0400

    528 0 is not reachable

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,38 +7,25 @@ namespace Solution;
 
 public class Solution
 {
-    public int CountArrangement(int n)
+    public Solution(int[] w)
     {
-        int sz = 1 << n;
-        int[] dp = new int[sz];
-        dp[0] = 1;
-        for (int mask = 1; mask < sz; mask++)
+        Prefix = new int[w.Length];
+        Rng = new();
+        int prev = 0;
+        for (int i = 0; i < w.Length; i++)
         {
-            int pos = int.PopCount(mask);
-            for (int bit = 0; bit < n; bit++)
-            {
-                if (((mask >> bit) & 1) == 1 && (pos % (1 + bit) == 0 || (1 + bit) % pos == 0))
-                {
-                    dp[mask] += dp[mask ^ (1 << bit)];
-                }
-            }
+            Prefix[i] = prev + w[i];
+            prev = Prefix[i];
         }
-        return dp[^1];
-        return Dfs(n, 0, 1);
+    }
 
-        static int Dfs(int n, int mask, int depth)
-        {
-            if (int.PopCount(mask) == n) { return 1; }
-            int res = 0;
-            for (int bit = 0; bit < n; bit++)
-            {
-                if (((mask >> bit) & 1) == 0
-                    && ((1 + bit) % depth == 0 || depth % (1 + bit) == 0))
-                {
-                    res += Dfs(n, mask | (1 << bit), 1 + depth);
-                }
-            }
-            return res;
-        }
+    int[] Prefix { get; }
+    Random Rng { get; }
+
+    public int PickIndex()
+    {
+        int val = Rng.Next(1, 1 + Prefix[^1]);
+        int i = Array.BinarySearch(Prefix, val);
+        return i < 0 ? ~i : i;
     }
 }
