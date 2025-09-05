1fb74534590ea9732f08b1aceaa276642cc9b970
pkusensei <ZW_08@outlook.com>
2025-09-05 10:57:38 -0400

    538

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,26 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public string ComplexNumberMultiply(string num1, string num2)
+    public TreeNode ConvertBST(TreeNode root)
     {
-        (int a1, int b1) = Parse(num1);
-        (int a2, int b2) = Parse(num2);
-        int a = a1 * a2 - b1 * b2;
-        int b = a1 * b2 + a2 * b1;
-        return $"{a}+{b}i";
+        Dfs(root, 0);
+        return root;
 
-        // a + bi
-        static (int a, int b) Parse(string s)
+        static int Dfs(TreeNode node, int suffix)
         {
-            Span<int> vals = stackalloc int[2];
-            int i = 0;
-            foreach (var item in s.Split('+'))
-            {
-                if (i == 1) { vals[i] = int.Parse(item.AsSpan()[..(item.Length - 1)]); }
-                else { vals[i] = int.Parse(item); }
-                i += 1;
-            }
-            return (vals[0], vals[1]);
+            if (node is null) { return suffix; }
+            suffix = Dfs(node.right, suffix);
+            node.val += suffix;
+            return Dfs(node.left, node.val);
         }
     }
 }
