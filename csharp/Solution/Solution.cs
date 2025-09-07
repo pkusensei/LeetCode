c737e0e58f48824b4258dcd2f12b2534e7e83949
pkusensei <ZW_08@outlook.com>
2025-09-06 20:07:17 -0400

    552 dfs/memo; dp count two phases

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,48 +7,61 @@ namespace Solution;
 
 public class Solution
 {
-    public int RemoveBoxes(int[] boxes)
+    public int CheckRecord(int n)
     {
-        int n = boxes.Length;
-        int[,,] memo = new int[n, n, n];
-        for (int a = 0; a < n; a++)
+        const int M = 1_000_000_007;
+        int[,,] memo = new int[1 + n, 2, 3];
+        for (int p = 0; p <= n; p++)
         {
-            for (int b = 0; b < n; b++)
+            for (int a = 0; a < 2; a++)
             {
-                for (int c = 0; c < n; c++)
+                for (int l_ = 0; l_ < 3; l_++)
                 {
-                    memo[a, b, c] = -1;
+                    memo[p, a, l_] = -1;
                 }
             }
         }
-        return Dfs(0, n - 1, 0);
+        return Dfs(n, 1, 2);
 
-        int Dfs(int left, int right, int streak)
+        int Dfs(int n, int a, int streak)
         {
-            if (left > right) { return 0; }
-            if (memo[left, right, streak] > -1) { return memo[left, right, streak]; }
-            int curr_streak = streak;
-            int left_end = left;
-            while (left_end <= right && boxes[left_end] == boxes[left])
-            {
-                left_end += 1;
-                curr_streak += 1; // Greedily accumulate streak
-            }
-            // Option 1) Cut at end of streak
-            int res = curr_streak * curr_streak + Dfs(left_end, right, 0);
-            for (int i = left_end; i <= right; i++)
-            {
-                if (boxes[i] == boxes[left])
-                {
-                    // Option 2) Continue this streak with window to the right
-                    // ..1,1,2,2,1,1.. is seen as 
-                    // 2,2 and ..1,1,1,1..
-                    int curr = Dfs(left_end, i - 1, 0) + Dfs(i, right, curr_streak);
-                    res = int.Max(res, curr);
-                }
-            }
-            memo[left, right, streak] = res;
+            if (a < 0 || streak < 0) { return 0; }
+            if (n == 0) { return 1; }
+            if (memo[n, a, streak] > -1) { return memo[n, a, streak]; }
+            int res = (Dfs(n - 1, a, 2) + Dfs(n - 1, a - 1, 2)) % M + Dfs(n - 1, a, streak - 1);
+            res %= M;
+            memo[n, a, streak] = res;
             return res;
         }
+    }
+
+    public int WithTwoPhases(int n)
+    {
+        const int M = 1_000_000_007;
+        long[] dp = new long[n + 1];
+
+        // Phase 1) No absences
+        dp[0] = 1;
+        if (n >= 1) dp[1] = 2;
+        if (n >= 2) dp[2] = 4; // PP PL LP LL
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % M;
+            // To build seq of length i:
+            // Add P to any [i-1]
+            // Add PL to any [i-2]
+            // Add PLL to any [i-3]
+        }
+        long res = dp[n]; // Accumulates counts with no absence
+
+        // Phase 2) With one absence
+        for (int i = 0; i < n; i++)
+        {
+            // [0..i) => dp[i]
+            // [i]=A
+            // [1+i..] => dp[n-i-1]
+            res = (res + dp[i] * dp[n - i - 1]) % M;
+        }
+        return (int)res;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,10 +10,14 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(23, new[] { 1, 3, 2, 2, 2, 3, 4, 3, 1 })]
-    public void TestMethod1(int exp, int[] b)
+    [DataRow(8, 2)]
+    [DataRow(3, 1)]
+    [DataRow(19, 3)]
+    [DataRow(183236316, 10101)]
+    public void TestMethod1(int exp, int n)
     {
-        Assert.AreEqual(exp, sol.RemoveBoxes(b));
+        Assert.AreEqual(exp, sol.CheckRecord(n));
+        Assert.AreEqual(exp, sol.WithTwoPhases(n));
     }
 
     [TestMethod]
