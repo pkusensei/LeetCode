037fdf86596840ad2f066e5981428c3598ead589
pkusensei <ZW_08@outlook.com>
2025-09-10 20:12:42 -0400

    2785

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,73 +7,21 @@ namespace Solution;
 
 public class Solution
 {
-    public int[][] OuterTrees(int[][] points)
+    public string SortVowels(string s)
     {
-        int n = points.Length;
-        if (n <= 1) { return points; }
-        Point[] arr = [.. points.Select(v => new Point(v[0], v[1]))];
-        Array.Sort(arr, (a, b) => a.X == b.X ? a.Y.CompareTo(b.Y) : a.X.CompareTo(b.X));
-        List<Point> lower = [];
-        foreach (var p in arr)
+        char[] V = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        char[] vows = [.. s.Where(c => V.Contains(c)).Order()];
+        int i = 0;
+        StringBuilder sb = new();
+        foreach (var c in s)
         {
-            while (lower.Count >= 2 && Point.Cross(lower[^2], lower[^1], p) < 0)
+            if (V.Contains(c))
             {
-                lower.RemoveAt(lower.Count - 1);
+                sb.Append(vows[i]);
+                i += 1;
             }
-            lower.Add(p);
+            else { sb.Append(c); }
         }
-        List<Point> upper = [];
-        foreach (var p in arr.Reverse())
-        {
-            while (upper.Count >= 2 && Point.Cross(upper[^2], upper[^1], p) < 0)
-            {
-                upper.RemoveAt(upper.Count - 1);
-            }
-            upper.Add(p);
-        }
-        return [.. lower.SkipLast(1).Union(upper.SkipLast(1))
-                    .Select(p => new[] { p.X, p.Y })];
+        return sb.ToString();
     }
-}
-
-readonly record struct Point(int X, int Y) : IComparable<Point>
-{
-    // Find the orientation of two vectors, i.e rotate OA to OB.
-    // >0 counter-clockwise,
-    // <0 clockwise,
-    // =0  collinear
-    public static int Cross(Point ori, Point a, Point b)
-        => (a.X - ori.X) * (b.Y - ori.Y) - (a.Y - ori.Y) * (b.X - ori.X);
-
-    public static Point[] ConvexHull(Point[] points)
-    {
-        int n = points.Length;
-        if (n <= 1) { return points; }
-        Array.Sort(points);
-        List<Point> lower = [];
-        foreach (var p in points)
-        {
-            // We want counter-clockwise turns.
-            // If the last three points make a clockwise turn (cross product < 0),
-            // remove the middle point.
-            while (lower.Count >= 2 && Cross(lower[^2], lower[^1], p) < 0)
-            {
-                lower.RemoveAt(lower.Count - 1);
-            }
-            lower.Add(p);
-        }
-        List<Point> upper = [];
-        foreach (var p in points.Reverse())
-        {
-            while (upper.Count >= 2 && Cross(upper[^2], upper[^1], p) < 0)
-            {
-                upper.RemoveAt(upper.Count - 1);
-            }
-            upper.Add(p);
-        }
-        return [.. lower.SkipLast(1).Union(upper.SkipLast(1))];
-    }
-
-    public int CompareTo(Point other)
-        => X == other.X ? Y.CompareTo(other.Y) : X.CompareTo(other.X);
 }
