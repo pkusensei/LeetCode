466813b7770642ff6cab5385ddf6d845e3d251a7
Zx Wang <ZW_08@outlook.com>
2025-09-11 10:52:01 -0400

    598

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,24 +7,9 @@ namespace Solution;
 
 public class Solution
 {
-    public bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4)
+    public int MaxCount(int m, int n, int[][] ops)
     {
-        Point[] points = [.. new[] { p1, p2, p3, p4 }.Select(v => new Point(v))];
-        HashSet<int> s = [];
-        for (int i = 0; i < 4; i++)
-        {
-            for (int i2 = 1 + i; i2 < 4; i2++)
-            {
-                s.Add(points[i].Dist(points[i2]));
-            }
-        }
-        return s.Count == 2 && s.All(v => v > 0);
+        if (ops is null || ops.Length == 0) { return m * n; }
+        return ops.Min(v => v[0]) * ops.Min(v => v[1]);
     }
-}
-
-readonly record struct Point(int X, int Y)
-{
-    public Point(int[] v) : this(v[0], v[1]) { }
-    public int Dist(Point other)
-        => (int)(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
 }
