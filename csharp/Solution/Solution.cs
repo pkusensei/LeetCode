c5a8a12543e2ce479b9ecdee6c0c56bd0c6118c8
pkusensei <ZW_08@outlook.com>
2025-09-12 08:41:31 -0400

    606 dfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,5 +7,29 @@ namespace Solution;
 
 public class Solution
 {
-    public bool DoesAliceWin(string s) => s.Any(v => "aeiou".Contains(v));
+    public string Tree2str(TreeNode root)
+    {
+        List<string> res = [];
+        Dfs(root);
+        return string.Join("", res);
+
+        void Dfs(TreeNode node)
+        {
+            if (node is null) { return; }
+            res.Add(node.val.ToString());
+            res.Add("(");
+            Dfs(node.left);
+            res.Add(")");
+            res.Add("(");
+            Dfs(node.right);
+            res.Add(")");
+            for (int _ = 0; _ < 2; _++)
+            {
+                if (res[^2] == "(" && res[^1] == ")")
+                {
+                    res.RemoveRange(res.Count - 2, 2);
+                }
+            }
+        }
+    }
 }
