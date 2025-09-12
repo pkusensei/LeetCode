9e7b5a4af3c482ade9fa8a2cd0108d3c1d175426
pkusensei <ZW_08@outlook.com>
2025-09-12 09:37:28 -0400

    611 sort and two ptrs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,29 +7,28 @@ namespace Solution;
 
 public class Solution
 {
-    public string Tree2str(TreeNode root)
+    public int TriangleNumber(int[] nums)
     {
-        List<string> res = [];
-        Dfs(root);
-        return string.Join("", res);
-
-        void Dfs(TreeNode node)
+        int n = nums.Length;
+        Array.Sort(nums);
+        int res = 0;
+        for (int i = n - 1; i >= 2; i -= 1)
         {
-            if (node is null) { return; }
-            res.Add(node.val.ToString());
-            res.Add("(");
-            Dfs(node.left);
-            res.Add(")");
-            res.Add("(");
-            Dfs(node.right);
-            res.Add(")");
-            for (int _ = 0; _ < 2; _++)
+            int left = 0;
+            int right = i - 1;
+            while (left < right)
             {
-                if (res[^2] == "(" && res[^1] == ")")
+                if (nums[left] + nums[right] > nums[i])
                 {
-                    res.RemoveRange(res.Count - 2, 2);
+                    res += right - left;
+                    right -= 1;
+                }
+                else
+                {
+                    left += 1;
                 }
             }
         }
+        return res;
     }
 }
