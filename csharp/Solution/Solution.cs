604aea476249937bf02c7808398bf8c526dacfd1
pkusensei <ZW_08@outlook.com>
2025-09-12 21:05:49 -0400

    3541

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,9 +7,21 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaximumProduct(int[] nums)
+    public int MaxFreqSum(string s)
     {
-        Array.Sort(nums);
-        return int.Max(nums[^3] * nums[^2] * nums[^1], nums[0] * nums[1] * nums[^1]);
+        Span<int> freq = stackalloc int[26];
+        foreach (var ch in s)
+        {
+            freq[ch - 'a'] += 1;
+        }
+        int v = 0;
+        int c = 0;
+        for (int i = 0; i < 26; i += 1)
+        {
+            int curr = freq[i];
+            if ("aeiou".Contains((char)(i + 'a'))) { v = int.Max(v, curr); }
+            else { c = int.Max(c, curr); }
+        }
+        return v + c;
     }
 }
