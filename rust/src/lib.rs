6ea65dc779965a001d1add65fad22176627495c8
pkusensei <ZW_08@outlook.com>
2025-09-14 15:51:14 -0400

    3686 week_cont467 q4; dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,15 +7,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_k_distinct(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    use std::collections::BTreeSet;
-    nums.iter()
-        .copied()
-        .collect::<BTreeSet<_>>()
-        .into_iter()
-        .rev()
-        .take(k as usize)
-        .collect()
+pub fn count_stable_subsequences(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    dfs(nums, 0, 2, 2, &mut vec![[[-1; 3]; 3]; n]) - 1
+}
+
+const M: i32 = 1_000_000_007;
+
+fn dfs(nums: &[i32], idx: usize, prev1: usize, prev2: usize, memo: &mut [[[i32; 3]; 3]]) -> i32 {
+    if idx >= nums.len() {
+        return 1;
+    }
+    if memo[idx][prev1][prev2] > -1 {
+        return memo[idx][prev1][prev2];
+    }
+    // skip
+    let mut res = dfs(nums, 1 + idx, prev1, prev2, memo);
+    let parity = (nums[idx] & 1) as usize;
+    if prev1 == prev2 {
+        if prev1 == 2 || prev2 != parity {
+            res += dfs(nums, 1 + idx, prev2, parity, memo);
+        }
+    } else {
+        res += dfs(nums, 1 + idx, prev2, parity, memo);
+    }
+    res %= M;
+    memo[idx][prev1][prev2] = res;
+    res
 }
 
 #[cfg(test)]
