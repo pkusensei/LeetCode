82bdc2946050f0b9903f999a639d84f9816ea16c
pkusensei <ZW_08@outlook.com>
2025-09-15 17:02:33 -0400

    630 priority queue

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,13 +7,28 @@ namespace Solution;
 
 public class Solution
 {
-    public int CanBeTypedWords(string text, string brokenLetters)
+    public int ScheduleCourse(int[][] courses)
     {
-        int res = 0;
-        foreach (var item in text.Split(' '))
+        Array.Sort(courses, (a, b) => a[1].CompareTo(b[1]));
+        int end = 0;
+        PriorityQueue<int, int> pq = new();
+        foreach (var c in courses)
         {
-            if (!item.Intersect(brokenLetters).Any()) { res += 1; }
+            if (end + c[0] <= c[1])
+            {
+                // this duration is applicable
+                end += c[0];
+                pq.Enqueue(c[0], -c[0]);
+            }
+            else if (pq.TryPeek(out int dur, out _) && dur > c[0])
+            {
+                // or is shorter ==> swap to get earlier end time
+                pq.Dequeue();
+                end -= dur;
+                end += c[0];
+                pq.Enqueue(c[0], -c[0]);
+            }
         }
-        return res;
+        return pq.Count;
     }
 }
