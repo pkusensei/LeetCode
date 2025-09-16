83fd4a7a8968afac2c71b362288cc5ef5f44afa5
Zx Wang <ZW_08@outlook.com>
2025-09-16 13:57:01 -0400

    633

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,38 +7,17 @@ namespace Solution;
 
 public class Solution
 {
-    public int[] SmallestRange(IList<IList<int>> nums)
+    public bool JudgeSquareSum(int c)
     {
-        int k = nums.Count;
-        int min = int.MaxValue;
-        int max = int.MinValue;
-        PriorityQueue<(int val, int arr, int i), int> min_heap = new();
-        int[] freq = new int[k];
-        for (int arr = 0; arr < k; arr++)
+        long a = 0;
+        long b = (long)Math.Sqrt(c);
+        while (a <= b)
         {
-            min = int.Min(min, nums[arr][0]);
-            max = int.Max(max, nums[arr][0]);
-            freq[arr] = nums[arr].Count;
-            int val = nums[arr][0];
-            min_heap.Enqueue((val, arr, 0), val);
+            long sum = a * a + b * b;
+            if (sum > c) { b -= 1; }
+            else if (sum < c) { a += 1; }
+            else { return true; }
         }
-        int right = max;
-        while (min_heap.TryDequeue(out var item, out _))
-        {
-            (int left, int arr, int i) = item;
-            if (right - left < max - min)
-            {
-                min = left;
-                max = right;
-            }
-            if (1 + i < nums[arr].Count)
-            {
-                int val = nums[arr][1 + i];
-                min_heap.Enqueue((val, arr, 1 + i), val);
-                right = int.Max(right, val);
-            }
-            else { break; }
-        }
-        return [min, max];
+        return false;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,15 +12,11 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        int[][] n = [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]];
-        Assert.IsTrue(new[] { 20, 24 }.SequenceEqual(sol.SmallestRange(n)));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        int[][] n = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
-        Assert.IsTrue(new[] { 1, 1 }.SequenceEqual(sol.SmallestRange(n)));
     }
 
     [TestMethod]
