53cad3e04a10b376711bbc629fe9b7db9f4f702c
Zx Wang <ZW_08@outlook.com>
2025-09-16 13:01:14 -0400

    632 priority queue

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -7,22 +7,38 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<int> ReplaceNonCoprimes(int[] nums)
+    public int[] SmallestRange(IList<IList<int>> nums)
     {
-        Stack<int> st = [];
-        foreach (var num in nums)
+        int k = nums.Count;
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        PriorityQueue<(int val, int arr, int i), int> min_heap = new();
+        int[] freq = new int[k];
+        for (int arr = 0; arr < k; arr++)
         {
-            int curr = num;
-            while (st.TryPeek(out var top) && GCD(top, curr) > 1)
-            {
-                st.Pop();
-                curr = LCM(top, curr);
-            }
-            st.Push(curr);
+            min = int.Min(min, nums[arr][0]);
+            max = int.Max(max, nums[arr][0]);
+            freq[arr] = nums[arr].Count;
+            int val = nums[arr][0];
+            min_heap.Enqueue((val, arr, 0), val);
         }
-        return [.. st.Reverse()];
-
-        static int GCD(int a, int b) => a == 0 ? b : GCD(b % a, a);
-        static int LCM(int a, int b) => a / GCD(a, b) * b;
+        int right = max;
+        while (min_heap.TryDequeue(out var item, out _))
+        {
+            (int left, int arr, int i) = item;
+            if (right - left < max - min)
+            {
+                min = left;
+                max = right;
+            }
+            if (1 + i < nums[arr].Count)
+            {
+                int val = nums[arr][1 + i];
+                min_heap.Enqueue((val, arr, 1 + i), val);
+                right = int.Max(right, val);
+            }
+            else { break; }
+        }
+        return [min, max];
     }
 }
