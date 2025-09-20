fba1ee1b2ea7215b7fb2f0efdcce6938bd9c6a00
pkusensei <ZW_08@outlook.com>
2025-09-19 20:46:07 -0400

    3508 Queue<T>.BinarySearch??

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -5,59 +5,72 @@ using static Solution.Utils;
 
 namespace Solution;
 
-public class Solution
+public class Router
 {
-    public string SolveEquation(string equation)
+    public Router(int memoryLimit)
     {
-        int xfactor = 0;
-        int num = 0;
-        int sign = 1;
-        int is_left = 1;
-        int curr_num = 0;
-        for (int i = 0; i < equation.Length; i++)
+        Cap = memoryLimit;
+        Set = [];
+        Fifo = [];
+        DstTimes = [];
+    }
+
+    int Cap { get; }
+    HashSet<Packet> Set { get; }
+    Queue<Packet> Fifo { get; }
+    Dictionary<int, List<int>> DstTimes { get; }
+
+    public bool AddPacket(int source, int destination, int timestamp)
+    {
+        Packet p = new(source, destination, timestamp);
+        if (!Set.Add(p)) { return false; }
+        if (Fifo.Count == Cap) { ForwardPacket(); }
+        Fifo.Enqueue(p);
+        if (!DstTimes.TryAdd(destination, [timestamp])) { DstTimes[destination].Add(timestamp); }
+        return true;
+    }
+
+    public int[] ForwardPacket()
+    {
+        if (!Fifo.TryDequeue(out var pac)) { return []; }
+        Set.Remove(pac);
+        DstTimes[pac.Dst].RemoveAt(0);
+        return [pac.Src, pac.Dst, pac.Time];
+    }
+
+    public int GetCount(int destination, int startTime, int endTime)
+    {
+        if (!DstTimes.TryGetValue(destination, out var list) || list.Count == 0)
         {
-            switch (equation[i])
+            return 0;
+        }
+        int start = list.BinarySearch(startTime);
+        if (start >= 0)
+        {
+            while (start >= 0 && list[start] == startTime)
             {
-                case 'x':
-                    if (i > 0 && equation[i - 1] == '0') { break; }
-                    if (curr_num == 0)
-                    {
-                        xfactor += sign;
-                    }
-                    else
-                    {
-                        xfactor += sign * curr_num;
-                        curr_num = 0;
-                        sign = is_left;
-                    }
-                    break;
-                case '+':
-                    num += sign * curr_num;
-                    sign = is_left;
-                    curr_num = 0;
-                    break;
-                case '-':
-                    num += sign * curr_num;
-                    sign = -1 * is_left;
-                    curr_num = 0;
-                    break;
-                case '=':
-                    num += sign * curr_num;
-                    is_left = -1;
-                    sign = is_left;
-                    curr_num = 0;
-                    break;
-                default:
-                    curr_num = 10 * curr_num + equation[i] - '0';
-                    break;
+                start -= 1;
+            }
+            start += 1;
+        }
+        else
+        {
+            start = ~start;
+        }
+        int end = list.BinarySearch(endTime);
+        if (end >= 0)
+        {
+            while (end < list.Count && list[end] == endTime)
+            {
+                end += 1;
             }
         }
-        num += sign * curr_num;
-        return (xfactor == 0, num == 0) switch
+        else
         {
-            (true, true) => "Infinite solutions",
-            (true, false) => "No solution",
-            _ => $"x={-num / xfactor}"
-        };
+            end = ~end;
+        }
+        return end - start;
     }
 }
+
+readonly record struct Packet(int Src, int Dst, int Time);
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -7,18 +7,11 @@ namespace Tests;
 [TestClass]
 public class UnitTest
 {
-    readonly Solution.Solution sol = new();
+    // readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow("x=2", "x+5-3+x=6+x-2")]
-    [DataRow("Infinite solutions", "x=x")]
-    [DataRow("x=0", "2x=x")]
-    [DataRow("x=1", "-x=-1")]
-    [DataRow("x=-2", "2=-x")]
-    [DataRow("Infinite solutions", "0x=0")]
-    public void TestMethod1(string exp, string s)
+    public void TestMethod1()
     {
-        Assert.AreEqual(exp, sol.SolveEquation(s));
     }
 
     [TestMethod]
