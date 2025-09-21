2a944fde14a6e19a3828af00645e0ce2dda10f55
pkusensei <ZW_08@outlook.com>
2025-09-21 09:36:59 -0400

    3690 week_cont468 q3; bfs and bitmask

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,16 +4,72 @@ mod fenwick_tree;
 mod helper;
 mod trie;
 
+use std::collections::{HashMap, HashSet, VecDeque};
+
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
 
-pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
-    let [mut min, mut max] = [i32::MAX, i32::MIN];
-    for num in nums {
-        min = min.min(num);
-        max = max.max(num);
+pub fn min_split_merge(nums1: &[i32], nums2: &[i32]) -> i32 {
+    let map = nums1.iter().fold(HashMap::new(), |mut acc, &v| {
+        let n = acc.len() as i32;
+        acc.entry(v).or_insert(n);
+        acc
+    });
+    let start = to_bits(&map, nums1);
+    let target = to_bits(&map, nums2);
+    let mut queue = VecDeque::from([(start, 0)]);
+    let mut seen = HashSet::from([start]);
+    while let Some((curr, step)) = queue.pop_front() {
+        if curr == target {
+            return step;
+        }
+        for next in generate(nums1.len(), curr, &mut seen) {
+            queue.push_back((next, 1 + step));
+        }
     }
-    i64::from(k) * i64::from(max - min)
+    -1
+}
+
+const WIDTH: i32 = 3; // 0b111
+
+fn generate(n: usize, mut bits: i32, seen: &mut HashSet<i32>) -> Vec<i32> {
+    let mut nums = Vec::with_capacity(n);
+    while nums.len() < n {
+        nums.push(bits & 0b111);
+        bits >>= WIDTH;
+    }
+    nums.reverse();
+    let mut res = vec![];
+    for left in 0..n {
+        for right in left..n {
+            let mut curr = nums[..left]
+                .iter()
+                .chain(&nums[1 + right..])
+                .copied()
+                .collect_vec();
+            let sub = &nums[left..=right];
+            for i in 0..curr.len() {
+                let mut temp = curr.clone();
+                temp.splice(i..i, sub.iter().copied());
+                let mask = temp.iter().fold(0, |acc, v| (acc << WIDTH) | v);
+                if seen.insert(mask) {
+                    res.push(mask);
+                }
+            }
+            curr.extend_from_slice(sub);
+            let mask = curr.iter().fold(0, |acc, v| (acc << WIDTH) | v);
+            if seen.insert(mask) {
+                res.push(mask);
+            }
+        }
+    }
+    res
+}
+
+// len(nums)<=6
+fn to_bits(map: &HashMap<i32, i32>, nums: &[i32]) -> i32 {
+    nums.iter().fold(0, |acc, v| (acc << WIDTH) | map[v])
 }
 
 #[cfg(test)]
@@ -46,8 +102,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_split_merge(&[3, 1, 2], &[1, 2, 3]), 1);
+        assert_eq!(min_split_merge(&[1, 1, 2, 3, 4, 5], &[5, 4, 3, 2, 1, 1]), 3)
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(min_split_merge(&[-17, -31], &[-31, -17]), 1);
+    }
 }
