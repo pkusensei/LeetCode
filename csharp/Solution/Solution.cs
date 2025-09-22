8e3258cfa36a2579aef2e66d802c4cc0eb7debb6
pkusensei <ZW_08@outlook.com>
2025-09-21 20:16:30 -0400

    3005

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -6,65 +6,12 @@ using static Solution.Utils;
 
 namespace Solution;
 
-public class MovieRentingSystem
+public class Solution
 {
-    public MovieRentingSystem(int n, int[][] entries)
+    public int MaxFrequencyElements(int[] nums)
     {
-        InStore = [];
-        Rented = [];
-        Dictionary<(int, int), int> lookup = [];
-        foreach (var item in entries)
-        {
-            int shop = item[0];
-            int movie = item[1];
-            int price = item[2];
-            InStore.TryAdd(movie, []);
-            InStore[movie].Add((price, shop));
-            lookup.Add((shop, movie), price);
-        }
-        Lookup = lookup.ToFrozenDictionary();
-    }
-
-    // movie_id - (price, shop)
-    Dictionary<int, SortedSet<(int price, int shop)>> InStore = [];
-    SortedSet<Movie> Rented { get; }
-    FrozenDictionary<(int shop, int movie), int> Lookup { get; }
-
-    public IList<int> Search(int movie)
-    {
-        List<int> res = [];
-        if (!InStore.TryGetValue(movie, out var set)) { return res; }
-        foreach (var (_, shop) in set)
-        {
-            res.Add(shop);
-            if (res.Count == 5) { break; }
-        }
-        return res;
-    }
-
-    public void Rent(int shop, int movie)
-    {
-        int price = Lookup[(shop, movie)];
-        InStore[movie].Remove((price, shop));
-        Rented.Add(new(shop, movie, price));
-    }
-
-    public void Drop(int shop, int movie)
-    {
-        int price = Lookup[(shop, movie)];
-        InStore[movie].Add((price, shop));
-        Rented.Remove(new(shop, movie, price));
-    }
-
-    public IList<IList<int>> Report() => [.. Rented.Take(5).Select(m => (IList<int>)[m.Shop, m.Id])];
-
-    readonly record struct Movie(int Shop, int Id, int Price) : IComparable<Movie>
-    {
-        public int CompareTo(Movie other)
-        {
-            if (Price != other.Price) { return Price.CompareTo(other.Price); }
-            else if (Shop != other.Shop) { return Shop.CompareTo(other.Shop); }
-            else { return Id.CompareTo(other.Id); }
-        }
+        Dictionary<int, int> freq = nums.CountBy(x => x).ToDictionary(p => p.Key, p => p.Value);
+        int max = freq.Values.Max();
+        return freq.Values.Where(v => v == max).Sum();
     }
 }
