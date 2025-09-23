0c9f8f3abea8fab68a33f482f0e1b506f5a20047
pkusensei <ZW_08@outlook.com>
2025-09-23 09:59:25 -0400

    648 trie

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,19 +8,47 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindLongestChain(int[][] pairs)
+    public string ReplaceWords(IList<string> dictionary, string sentence)
     {
-        Array.Sort(pairs, (a, b) => a[1].CompareTo(b[1]));
-        int end = -1001;
-        int res = 0;
-        foreach (var item in pairs)
+        Trie tr = new();
+        foreach (var w in dictionary)
         {
-            if (end < item[0])
-            {
-                res += 1;
-                end = item[1];
-            }
+            tr.Insert(w);
         }
-        return res;
+        return string.Join(' ', sentence.Split(' ').Select(s => tr.Find(s)));
+    }
+
+    class Trie
+    {
+        public Trie()
+        {
+            Nodes = new Trie[26];
+            IsEnd = false;
+        }
+
+        Trie[] Nodes { get; }
+        bool IsEnd { get; set; }
+
+        public void Insert(ReadOnlySpan<char> s)
+        {
+            var curr = this;
+            foreach (var ch in s)
+            {
+                curr = curr.Nodes[ch - 'a'] ??= new();
+            }
+            curr.IsEnd = true;
+        }
+
+        public string Find(ReadOnlySpan<char> s)
+        {
+            var curr = this;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (curr is null) { break; }
+                if (curr.IsEnd) { return new(s[..i]); }
+                curr = curr.Nodes[s[i] - 'a'];
+            }
+            return new(s);
+        }
     }
 }
