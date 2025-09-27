1aed620b18e92eeaf6f23704a77edb7a802ddc11
pkusensei <ZW_08@outlook.com>
2025-09-26 20:49:55 -0400

    812 shoelace

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,27 +8,35 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindSecondMinimumValue(TreeNode root)
+    public double LargestTriangleArea(int[][] points)
     {
-        long min1 = long.MaxValue;
-        long min2 = long.MaxValue;
-        Dfs(root);
-        return min2 > int.MaxValue ? -1 : (int)min2;
-
-        void Dfs(TreeNode node)
+        int n = points.Length;
+        double res = 0.0;
+        Span<double> x = stackalloc double[3];
+        Span<double> y = stackalloc double[3];
+        for (int i0 = 0; i0 < n; i0++)
         {
-            if (node is null || node.val >= min2) { return; }
-            if (node.val < min1)
+            x[0] = points[i0][0];
+            y[0] = points[i0][1];
+            for (int i1 = 1 + i0; i1 < n; i1++)
             {
-                min2 = min1;
-                min1 = node.val;
+                x[1] = points[i1][0];
+                y[1] = points[i1][1];
+                for (int i2 = 1 + i1; i2 < n; i2++)
+                {
+                    x[2] = points[i2][0];
+                    y[2] = points[i2][1];
+                    double curr = 0.0;
+                    for (int p = 0; p < 3; p++)
+                    {
+                        curr += x[p] * y[(1 + p) % 3] - x[(1 + p) % 3] * y[p];
+                    }
+                    curr = 0.5 * double.Abs(curr);
+                    res = double.Max(res, curr);
+                }
             }
-            else if (min1 < node.val)
-            {
-                min2 = node.val;
-            }
-            Dfs(node.left);
-            Dfs(node.right);
+
         }
+        return res;
     }
 }
