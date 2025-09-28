06da1272eb067f6c1f03614aa0748686adf08a43
pkusensei <ZW_08@outlook.com>
2025-09-28 08:59:19 -0400

    3697 week_cont469 q1; reading...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,69 +8,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_alternating_sum(nums: &[i32], swaps: &[[i32; 2]]) -> i64 {
-    use std::collections::HashMap;
-    let n = nums.len();
-    let mut dsu = DSU::new(n);
-    for s in swaps {
-        dsu.union(s[0] as usize, s[1] as usize);
+pub fn decimal_representation(mut n: i32) -> Vec<i32> {
+    let mut res = vec![];
+    let mut p = 0;
+    while n > 0 {
+        if n % 10 > 0 {
+            res.push((n % 10) * 10_i32.pow(p));
+        }
+        p += 1;
+        n /= 10;
     }
-    let mut groups = HashMap::<_, Pack>::new();
-    for (idx, &num) in nums.iter().enumerate() {
-        let root = dsu.find(idx);
-        let pack = groups.entry(root).or_default();
-        pack.odd += idx & 1;
-        pack.nums.push(num.into());
-    }
-    let mut res = 0;
-    for mut pack in groups.into_values() {
-        pack.nums.sort_unstable();
-        res +=
-            pack.nums[pack.odd..].iter().sum::<i64>() - pack.nums[..pack.odd].iter().sum::<i64>();
-    }
+    res.reverse();
     res
-}
-
-#[derive(Default)]
-struct Pack {
-    nums: Vec<i64>,
-    odd: usize,
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    rank: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            rank: vec![0; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v]);
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            return;
-        }
-        match self.rank[rx].cmp(&self.rank[ry]) {
-            std::cmp::Ordering::Less => self.parent[rx] = ry,
-            std::cmp::Ordering::Equal => {
-                self.rank[rx] += 1;
-                self.parent[ry] = rx;
-            }
-            std::cmp::Ordering::Greater => self.parent[ry] = rx,
-        }
-    }
 }
 
 #[cfg(test)]
@@ -103,14 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_alternating_sum(&[1, 2, 3], &[[0, 2], [1, 2]]), 4);
-        assert_eq!(max_alternating_sum(&[1, 2, 3], &[[1, 2]]), 2);
-        assert_eq!(
-            max_alternating_sum(&[1, 1000000000, 1, 1000000000, 1, 1000000000], &[]),
-            -2999999997
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
