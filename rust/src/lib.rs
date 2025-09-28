dbdc9c6779a3edf04318e459a54d047e7376fa31
pkusensei <ZW_08@outlook.com>
2025-09-28 09:13:18 -0400

    3698 week_cont469 q2; prefix sum; pref inc and suc dec

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,18 +8,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn decimal_representation(mut n: i32) -> Vec<i32> {
-    let mut res = vec![];
-    let mut p = 0;
-    while n > 0 {
-        if n % 10 > 0 {
-            res.push((n % 10) * 10_i32.pow(p));
+pub fn split_array(nums: &[i32]) -> i64 {
+    let n = nums.len();
+    let [mut pref, mut suf] = [vec![false; n], vec![false; n]];
+    pref[0] = true;
+    for (i, w) in nums.windows(2).enumerate() {
+        pref[1 + i] = pref[i] && (w[0] < w[1]);
+        if !pref[1 + i] {
+            break;
         }
-        p += 1;
-        n /= 10;
     }
-    res.reverse();
-    res
+    suf[n - 1] = true;
+    for (i, w) in nums.windows(2).enumerate().rev() {
+        suf[i] = suf[1 + i] && (w[0] > w[1]);
+        if !suf[i] {
+            break;
+        }
+    }
+    let sum = nums.iter().map(|&v| i64::from(v)).sum::<i64>();
+    let mut left = 0;
+    let mut res = i64::MAX;
+    for (i, &num) in nums[..n - 1].iter().enumerate() {
+        left += i64::from(num);
+        if pref[i] && suf[1 + i] {
+            res = res.min((sum - 2 * left).abs());
+        }
+    }
+    if res == i64::MAX { -1 } else { res }
 }
 
 #[cfg(test)]
@@ -52,7 +67,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(split_array(&[1, 3, 2]), 2);
+        assert_eq!(split_array(&[1, 2, 4, 3]), 4);
+        assert_eq!(split_array(&[3, 1, 2]), -1);
+    }
 
     #[test]
     fn test() {}
