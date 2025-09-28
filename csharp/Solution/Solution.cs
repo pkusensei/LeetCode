21e6c20fc460633b2f492fa6d5d8ba04bdde43ff
pkusensei <ZW_08@outlook.com>
2025-09-27 20:12:51 -0400

    976 sort and greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,35 +8,16 @@ namespace Solution;
 
 public class Solution
 {
-    public double LargestTriangleArea(int[][] points)
+    public int LargestPerimeter(int[] nums)
     {
-        int n = points.Length;
-        double res = 0.0;
-        Span<double> x = stackalloc double[3];
-        Span<double> y = stackalloc double[3];
-        for (int i0 = 0; i0 < n; i0++)
+        Array.Sort(nums, (a, b) => b.CompareTo(a));
+        for (int i = 0; i < nums.Length - 3; i++)
         {
-            x[0] = points[i0][0];
-            y[0] = points[i0][1];
-            for (int i1 = 1 + i0; i1 < n; i1++)
+            if (nums[i] < nums[1 + i] + nums[2 + i])
             {
-                x[1] = points[i1][0];
-                y[1] = points[i1][1];
-                for (int i2 = 1 + i1; i2 < n; i2++)
-                {
-                    x[2] = points[i2][0];
-                    y[2] = points[i2][1];
-                    double curr = 0.0;
-                    for (int p = 0; p < 3; p++)
-                    {
-                        curr += x[p] * y[(1 + p) % 3] - x[(1 + p) % 3] * y[p];
-                    }
-                    curr = 0.5 * double.Abs(curr);
-                    res = double.Max(res, curr);
-                }
+                return nums[i..(3 + i)].Sum();
             }
-
         }
-        return res;
+        return 0;
     }
 }
