afcb8bca6bdcfb85d4b12358c052218847b3c0c0
pkusensei <ZW_08@outlook.com>
2025-10-05 09:03:04 -0400

    3701 weekly_cont470 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,43 +6,15 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::{HashSet, VecDeque};
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn pacific_atlantic(heights: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let [rows, cols] = get_dimensions(&heights);
-    let mut pset = HashSet::new();
-    let mut aset = HashSet::new();
-    for r in 0..rows {
-        bfs(&heights, &mut pset, [r, 0]);
-        bfs(&heights, &mut aset, [r, cols - 1]);
+pub fn alternating_sum(nums: Vec<i32>) -> i32 {
+    let mut res = 0;
+    for (i, &v) in nums.iter().enumerate() {
+        if i & 1 == 0 { res += v } else { res -= v }
     }
-    for c in 0..cols {
-        bfs(&heights, &mut pset, [0, c]);
-        bfs(&heights, &mut aset, [rows - 1, c]);
-    }
-    pset.intersection(&aset)
-        .map(|v| vec![v[0] as i32, v[1] as i32])
-        .collect()
-}
-
-fn bfs(grid: &[Vec<i32>], seen: &mut HashSet<Coord>, start: Coord) {
-    if !seen.insert(start) {
-        return;
-    }
-    let mut queue = VecDeque::from([start]);
-    while let Some([r, c]) = queue.pop_front() {
-        for [nr, nc] in neighbors([r, c]) {
-            if let Some(&v) = grid.get(nr).and_then(|row| row.get(nc))
-                && v >= grid[r][c]
-                && seen.insert([nr, nc])
-            {
-                queue.push_back([nr, nc]);
-            }
-        }
-    }
+    res
 }
 
 #[cfg(test)]
