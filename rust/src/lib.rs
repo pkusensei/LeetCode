9f9fd48f1dd51c357d87e72386d966cfb035d3bf
pkusensei <ZW_08@outlook.com>
2025-10-11 10:54:13 -0400

    biweek_cont167 q3; binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,22 +9,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_subarray(nums: &[i32]) -> i32 {
-    let mut res = 2;
-    let n = nums.len();
-    let mut left = 0;
-    let mut right = 1;
-    while 1 + right < n {
-        while 1 + right < n
-            && i64::from(nums[right - 1]) + i64::from(nums[right]) == i64::from(nums[1 + right])
-        {
-            right += 1;
-        }
-        right += 1;
-        res = res.max(right - left);
-        left = right - 1;
+struct ExamTracker {
+    data: Vec<(i32, i64)>,
+}
+
+impl ExamTracker {
+    fn new() -> Self {
+        Self { data: Vec::new() }
     }
-    res as i32
+
+    fn record(&mut self, time: i32, score: i32) {
+        self.data.push((time, i64::from(score)));
+    }
+
+    fn total_score(&self, start_time: i32, end_time: i32) -> i64 {
+        let a = self.data.partition_point(|v| v.0 < start_time);
+        let b = self.data.partition_point(|v| v.0 <= end_time);
+        self.data[a..b].iter().map(|v| v.1).sum()
+    }
 }
 
 #[cfg(test)]
@@ -58,9 +60,14 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(longest_subarray(&[1, 1, 1, 1, 2, 3, 5, 1]), 5);
-        assert_eq!(longest_subarray(&[5, 2, 7, 9, 16]), 5);
-        assert_eq!(longest_subarray(&[1000000000, 1000000000, 1000000000]), 2);
+        let mut examTracker = ExamTracker::new();
+        examTracker.record(1, 98); // Alice takes a new exam at time 1, scoring 98.
+        assert_eq!(examTracker.total_score(1, 1), 98); // Between time 1 and time 1, Alice took 1 exam at time 1, scoring 98. The total score is 98.
+        examTracker.record(5, 99); // Alice takes a new exam at time 5, scoring 99.
+        assert_eq!(examTracker.total_score(1, 3), 98); // Between time 1 and time 3, Alice took 1 exam at time 1, scoring 98. The total score is 98.
+        assert_eq!(examTracker.total_score(1, 5), 197); // Between time 1 and time 5, Alice took 2 exams at time 1 and 5, scoring 98 and 99. The total score is 98 + 99 = 197.
+        assert_eq!(examTracker.total_score(3, 4), 0); // Alice did not take any exam between time 3 and time 4. Therefore, the answer is 0.
+        assert_eq!(examTracker.total_score(2, 5), 99);
     }
 
     #[test]
