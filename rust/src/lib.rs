98eecbe195b0687b5489657ebb3cba674e6d06dc
pkusensei <ZW_08@outlook.com>
2025-10-13 13:32:50 -0400

    689 dp; sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,64 +6,86 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::sync::LazyLock;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_of_ancestors(n: i32, edges: Vec<Vec<i32>>, nums: Vec<i32>) -> i64 {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push(b);
-        acc[b].push(a);
-        acc
-    });
-    let facts: Vec<i32> = nums.iter().map(|&v| factors(v)).collect();
-    let max = *facts.iter().max().unwrap_or(&1);
-    dfs(&adj, &facts, 0, n, &mut vec![0; 1 + max as usize])
-}
-
-fn dfs(adj: &[Vec<usize>], facts: &[i32], node: usize, prev: usize, freq: &mut [i64]) -> i64 {
-    let f = facts[node];
-    let mut res = freq[f as usize];
-    freq[f as usize] += 1;
-    for &next in &adj[node] {
-        if next != prev {
-            res += dfs(adj, facts, next, node, freq);
+pub fn max_sum_of_three_subarrays(nums: &[i32], k: i32) -> [usize; 3] {
+    let k = k as usize;
+    let mut sums = vec![];
+    let mut window = 0;
+    for (i, &num) in nums.iter().enumerate() {
+        window += num;
+        if i >= k {
+            window -= nums[i - k];
+        }
+        if i >= k - 1 {
+            sums.push(window);
         }
     }
-    freq[f as usize] -= 1;
+    let mut max_i = 0;
+    let mut left = vec![];
+    for (i, &v) in sums.iter().enumerate() {
+        if v > sums[max_i] {
+            max_i = i;
+        }
+        left.push(max_i);
+    }
+    let mut right = vec![];
+    max_i = sums.len() - 1;
+    for (i, &v) in sums.iter().enumerate().rev() {
+        // smallest idx
+        if v >= sums[max_i] {
+            max_i = i;
+        }
+        right.push(max_i);
+    }
+    right.reverse();
+    let mut max_sum = 0;
+    let mut res = [0, 0, 0];
+    for mid in k..sums.len() - k {
+        let curr = sums[mid] + sums[left[mid - k]] + sums[right[mid + k]];
+        if curr > max_sum {
+            max_sum = curr;
+            res = [left[mid - k], mid, right[mid + k]];
+        }
+    }
     res
 }
 
-fn factors(mut num: i32) -> i32 {
-    for &p in S.iter() {
-        while num >= p.pow(2) && num % p.pow(2) == 0 {
-            num /= p.pow(2);
+pub fn with_three_windows(nums: &[i32], k: i32) -> [usize; 3] {
+    let n = nums.len();
+    let k = k as usize;
+    let mut best_seq1 = [0];
+    let mut best_seq2 = [0, k];
+    let mut best_seq3 = [0, k, 2 * k];
+    let mut sum1: i32 = nums[..k].iter().sum();
+    let mut sum2: i32 = nums[k..2 * k].iter().sum();
+    let mut sum3: i32 = nums[2 * k..3 * k].iter().sum();
+    let mut max_sum1 = sum1;
+    let mut max_sum2 = sum1 + sum2;
+    let mut max_sum3 = sum1 + sum2 + sum3;
+    let mut i1 = 1;
+    let mut i2 = 1 + k;
+    for i3 in 1 + 2 * k..=n - k {
+        sum1 += nums[i1 + k - 1] - nums[i1 - 1];
+        sum2 += nums[i2 + k - 1] - nums[i2 - 1];
+        sum3 += nums[i3 + k - 1] - nums[i3 - 1];
+        if sum1 > max_sum1 {
+            max_sum1 = sum1;
+            best_seq1 = [i1];
         }
-    }
-    num
-}
-
-static S: LazyLock<Vec<i32>> = LazyLock::new(sieve);
-
-fn sieve() -> Vec<i32> {
-    const MAX: usize = 100_000_usize.isqrt();
-    let mut res = vec![true; 1 + MAX];
-    res[..2].fill(false);
-    res[2] = true;
-    for p in 2..=MAX.isqrt() {
-        if res[p] {
-            for val in (p * p..=MAX).step_by(p) {
-                res[val] = false;
-            }
+        if sum2 + max_sum1 > max_sum2 {
+            max_sum2 = sum2 + max_sum1;
+            best_seq2 = [best_seq1[0], i2];
         }
+        if sum3 + max_sum2 > max_sum3 {
+            max_sum3 = sum3 + max_sum2;
+            best_seq3 = [best_seq2[0], best_seq2[1], i3];
+        }
+        i1 += 1;
+        i2 += 1;
     }
-    res.into_iter()
-        .enumerate()
-        .filter_map(|(p, v)| if v { Some(p as i32) } else { None })
-        .collect()
+    best_seq3
 }
 
 #[cfg(test)]
@@ -96,7 +118,22 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            max_sum_of_three_subarrays(&[1, 2, 1, 2, 6, 7, 5, 1], 2),
+            [0, 3, 5]
+        );
+        assert_eq!(
+            max_sum_of_three_subarrays(&[1, 2, 1, 2, 1, 2, 1, 2, 1], 2),
+            [0, 2, 4]
+        );
+
+        assert_eq!(with_three_windows(&[1, 2, 1, 2, 6, 7, 5, 1], 2), [0, 3, 5]);
+        assert_eq!(
+            with_three_windows(&[1, 2, 1, 2, 1, 2, 1, 2, 1], 2),
+            [0, 2, 4]
+        );
+    }
 
     #[test]
     fn test() {}
