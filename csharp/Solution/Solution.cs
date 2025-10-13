2c2d9b837bb8e8d0496ba75f0885094ecd1eb93a
pkusensei <ZW_08@outlook.com>
2025-10-13 09:42:19 -0400

    688 dfs/memo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,16 +8,35 @@ namespace Solution;
 
 public class Solution
 {
-    public int LongestUnivaluePath(TreeNode root)
+    public double KnightProbability(int n, int k, int row, int column)
     {
-        if (root is null) { return 0; }
-        return int.Max(Dfs(root.left, root.val) + Dfs(root.right, root.val),
-                    int.Max(LongestUnivaluePath(root.left), LongestUnivaluePath(root.right)));
-
-        static int Dfs(TreeNode node, int val)
+        double[,,] memo = new double[1 + k, n, n];
+        for (int i1 = 0; i1 <= k; i1++)
         {
-            if (node is null || node.val != val) { return 0; }
-            return 1 + int.Max(Dfs(node.left, val), Dfs(node.right, val));
+            for (int r = 0; r < n; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    memo[i1, r, c] = -1.0;
+                }
+            }
+        }
+        return Dfs(k, row, column, 1.0);
+
+        double Dfs(int k, int row, int col, double f)
+        {
+            if (row < 0 || n <= row || col < 0 || n <= col) { return 0.0; }
+            if (k == 0) { return f; }
+            if (memo[k, row, col] > -1.0) { return memo[k, row, col]; }
+            Span<(int, int)> deltas = [(-2, -1), (-1, -2), (2, 1), (1, 2),
+                                        (2, -1), (-1,2), (-2, 1), (1, -2)];
+            double res = 0.0;
+            foreach (var (dr, dc) in deltas)
+            {
+                res += Dfs(k - 1, row + dr, col + dc, f / 8.0);
+            }
+            memo[k, row, col] = res;
+            return res;
         }
     }
 }
