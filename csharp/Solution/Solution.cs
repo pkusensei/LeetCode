d18416c86aa98e8fec01d5e234d28f5ad4b58ffc
pkusensei <ZW_08@outlook.com>
2025-10-15 10:39:18 -0400

    695 dsu

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,24 +8,74 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxIncreasingSubarrays(IList<int> nums)
+    public int MaxAreaOfIsland(int[][] grid)
     {
-        int n = nums.Count;
-        int res = 0;
-        int left = 0;
-        int prev = 0;
-        for (int right = 1; right < n; right++)
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        DSU dsu = new(rows * cols);
+        for (int r = 0; r < rows; r++)
         {
-            if (nums[right - 1] < nums[right]) { continue; }
-            int a = (right - left) / 2;
-            int b = int.Min(prev, right - left);
-            res = int.Max(res, int.Max(a, b));
-            prev = right - left;
-            left = right;
+            for (int c = 0; c < cols; c++)
+            {
+                if (grid[r][c] == 1)
+                {
+                    dsu.Size[r * cols + c] = 1;
+                }
+            }
         }
-        int aa = (n - left) / 2;
-        int bb = int.Min(prev, n - left);
-        res = int.Max(res, int.Max(aa, bb));
-        return res;
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                if (grid[r][c] == 1)
+                {
+                    foreach (var (dr, dc) in new[] { (-1, 0), (0, -1), (1, 0), (0, 1) })
+                    {
+                        int nr = r + dr;
+                        int nc = c + dc;
+                        if (0 <= nr && nr < rows && 0 <= nc && nc < cols && grid[nr][nc] == 1)
+                        {
+                            dsu.Union(r * cols + c, nr * cols + nc);
+                        }
+                    }
+                }
+            }
+        }
+        return dsu.Size.Max();
+    }
+}
+
+readonly struct DSU
+{
+    public DSU(int n)
+    {
+        Parent = [.. Enumerable.Range(0, n)];
+        Size = new int[n];
+    }
+
+    public int[] Parent { get; }
+    public int[] Size { get; }
+
+    public int Find(int v)
+    {
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Size[rx] < Size[ry])
+        {
+            Size[ry] += Size[rx];
+            Parent[rx] = ry;
+        }
+        else
+        {
+            Size[rx] += Size[ry];
+            Parent[ry] = rx;
+        }
     }
 }
