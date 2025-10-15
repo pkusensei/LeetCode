18e2ad38095ee819b700c1e04bc263b94157c984
pkusensei <ZW_08@outlook.com>
2025-10-15 11:09:12 -0400

    696

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,74 +8,22 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxAreaOfIsland(int[][] grid)
+    public int CountBinarySubstrings(string s)
     {
-        int rows = grid.Length;
-        int cols = grid[0].Length;
-        DSU dsu = new(rows * cols);
-        for (int r = 0; r < rows; r++)
+        int n = s.Length;
+        List<int> arr = [];
+        for (int left = 0; left < n;)
         {
-            for (int c = 0; c < cols; c++)
-            {
-                if (grid[r][c] == 1)
-                {
-                    dsu.Size[r * cols + c] = 1;
-                }
-            }
+            int right = left;
+            for (; right < n && s[right] == s[left]; right += 1) { }
+            arr.Add(right - left);
+            left = right;
         }
-        for (int r = 0; r < rows; r++)
+        int res = 0;
+        for (int i = 0; i < arr.Count - 1; i++)
         {
-            for (int c = 0; c < cols; c++)
-            {
-                if (grid[r][c] == 1)
-                {
-                    foreach (var (dr, dc) in new[] { (-1, 0), (0, -1), (1, 0), (0, 1) })
-                    {
-                        int nr = r + dr;
-                        int nc = c + dc;
-                        if (0 <= nr && nr < rows && 0 <= nc && nc < cols && grid[nr][nc] == 1)
-                        {
-                            dsu.Union(r * cols + c, nr * cols + nc);
-                        }
-                    }
-                }
-            }
+            res += int.Min(arr[i], arr[1 + i]);
         }
-        return dsu.Size.Max();
-    }
-}
-
-readonly struct DSU
-{
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)];
-        Size = new int[n];
-    }
-
-    public int[] Parent { get; }
-    public int[] Size { get; }
-
-    public int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public void Union(int x, int y)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry) { return; }
-        if (Size[rx] < Size[ry])
-        {
-            Size[ry] += Size[rx];
-            Parent[rx] = ry;
-        }
-        else
-        {
-            Size[rx] += Size[ry];
-            Parent[ry] = rx;
-        }
+        return res;
     }
 }
