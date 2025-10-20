bd735201716038f03502269615a84b72b67566af
pkusensei <ZW_08@outlook.com>
2025-10-20 10:39:21 -0400

    712 lcs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,101 +9,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_balanced(nums: &[i32]) -> i32 {
-    use std::collections::HashMap;
-    let n = nums.len();
-    let mut prev = HashMap::with_capacity(n);
-    let mut st = SegTree::new(n);
-    let mut res = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        let sign = if num & 1 == 1 { 1 } else { -1 };
-        if let Some(&left) = prev.get(&num) {
-            // remove old impact to ensure distinct
-            st.update(0, left, -sign);
-        }
-        st.update(0, right, sign);
-        prev.insert(num, right);
-        // With this number counted in, there is a balanced left point
-        if let Some(left) = st.query()
-            && left <= right
-        {
-            res = res.max(right + 1 - left);
-        }
-    }
-    res as i32
-}
-
-struct SegTree {
-    min_tree: Vec<i32>,
-    max_tree: Vec<i32>,
-    lazy: Vec<i32>,
-    n: usize,
-}
-
-impl SegTree {
-    fn new(n: usize) -> Self {
-        Self {
-            min_tree: vec![0; 4 * n],
-            max_tree: vec![0; 4 * n],
-            lazy: vec![0; 4 * n],
-            n,
-        }
-    }
-
-    /// find leftmost zero
-    fn query(&mut self) -> Option<usize> {
-        self._query(1, 0, self.n - 1)
-    }
-
-    fn _query(&mut self, node: usize, left: usize, right: usize) -> Option<usize> {
-        self.push(node, left, right); // push any pending updates
-        if self.min_tree[node] > 0 || self.max_tree[node] < 0 {
-            return None;
-        }
-        if left == right {
-            return if self.min_tree[node] == 0 {
-                Some(left)
+pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
+    let n1 = s1.len();
+    let n2 = s2.len();
+    let mut dp = vec![vec![0; 1 + n2]; 1 + n1];
+    for (i1, b1) in s1.bytes().enumerate() {
+        for (i2, b2) in s2.bytes().enumerate() {
+            dp[1 + i1][1 + i2] = if b1 == b2 {
+                b1 as i32 + dp[i1][i2]
             } else {
-                None
+                dp[1 + i1][i2].max(dp[i1][1 + i2])
             };
         }
-        let mid = left.midpoint(right);
-        self._query(2 * node, left, mid)
-            .or_else(|| self._query(2 * node + 1, 1 + mid, right))
     }
-
-    fn update(&mut self, ql: usize, qr: usize, val: i32) {
-        self._update(1, 0, self.n - 1, ql, qr, val);
-    }
-
-    fn _update(&mut self, node: usize, left: usize, right: usize, ql: usize, qr: usize, val: i32) {
-        self.push(node, left, right); // push any pending updates
-        if left > right || qr < left || right < ql {
-            return;
-        }
-        if ql <= left && right <= qr {
-            self.lazy[node] += val; // cache local update on this range
-            self.push(node, left, right);
-            return; // recursion stops here!
-        }
-        let mid = left.midpoint(right);
-        self._update(2 * node, left, mid, ql, qr, val);
-        self._update(2 * node + 1, 1 + mid, right, ql, qr, val);
-        self.min_tree[node] = self.min_tree[2 * node].min(self.min_tree[2 * node + 1]);
-        self.max_tree[node] = self.max_tree[2 * node].max(self.max_tree[2 * node + 1]);
-    }
-
-    fn push(&mut self, node: usize, left: usize, right: usize) {
-        if self.lazy[node] != 0 {
-            self.min_tree[node] += self.lazy[node];
-            self.max_tree[node] += self.lazy[node];
-            if left < right {
-                self.lazy[2 * node] += self.lazy[node];
-                self.lazy[2 * node + 1] += self.lazy[node];
-            }
-            self.lazy[node] = 0;
-        }
-    }
+    s1.bytes().chain(s2.bytes()).map(|b| b as i32).sum::<i32>() - 2 * dp[n1][n2]
 }
 
 #[cfg(test)]
@@ -136,11 +55,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(longest_balanced(&[2, 5, 4, 3]), 4);
-        assert_eq!(longest_balanced(&[3, 2, 2, 5, 4]), 5);
-        assert_eq!(longest_balanced(&[1, 2, 3, 2]), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
