b9869047f6f3d8f616bf0f49baee1e9162235e8b
Zx Wang <ZW_08@outlook.com>
2025-10-21 14:15:09 -0400

    715 BTreeMap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,15 +9,61 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
-    let mut buy = -prices[0];
-    let mut sell = 0;
-    for price in &prices[1..] {
-        let temp = buy;
-        buy = buy.max(sell - price);
-        sell = sell.max(temp + price - fee)
+use std::collections::BTreeMap;
+struct RangeModule {
+    data: BTreeMap<i32, i32>,
+}
+
+impl RangeModule {
+    fn new() -> Self {
+        Self {
+            data: BTreeMap::new(),
+        }
     }
-    buy.max(sell)
+
+    fn add_range(&mut self, mut left: i32, mut right: i32) {
+        if let Some(p) = self.data.range(..=left).next_back()
+            && left <= *p.1
+        {
+            left = *p.0;
+        }
+        if let Some(p) = self.data.range(..=right).next_back()
+            && right < *p.1
+        {
+            right = *p.1;
+        }
+        let del: Vec<_> = self.data.range(left..right).map(|p| *p.0).collect();
+        for k in del {
+            self.data.remove(&k);
+        }
+        self.data.insert(left, right);
+    }
+
+    fn query_range(&self, left: i32, right: i32) -> bool {
+        self.data
+            .range(..=left)
+            .next_back()
+            .is_some_and(|p| right <= *p.1)
+    }
+
+    fn remove_range(&mut self, left: i32, right: i32) {
+        if let Some(p) = self.data.range(..=right).next_back()
+            && right < *p.1
+        {
+            let val = *p.1;
+            self.data.insert(right, val);
+        }
+        if let Some(p) = self.data.range(..=left).next_back()
+            && left < *p.1
+        {
+            let key = *p.0;
+            self.data.insert(key, left);
+        }
+        let del: Vec<_> = self.data.range(left..right).map(|p| *p.0).collect();
+        for k in del {
+            self.data.remove(&k);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -50,7 +96,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        let mut rm = RangeModule::new();
+        rm.add_range(10, 20);
+        rm.remove_range(14, 16);
+        assert!(rm.query_range(10, 14)); // return True,(Every number in [10, 14) is being tracked)
+        assert!(!rm.query_range(13, 15)); // return False,(Numbers like 14, 14.03, 14.17 in [13, 15) are not being tracked)
+        assert!(rm.query_range(16, 17));
+    }
 
     #[test]
     fn test() {}
