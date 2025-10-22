d9740d603549c9e597a1f83aff67d90ced3dd20f
pkusensei <ZW_08@outlook.com>
2025-10-22 11:27:14 -0400

    721 dsu

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,42 +8,68 @@ namespace Solution;
 
 public class Solution
 {
-    public string LongestWord(string[] words)
+    public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts)
     {
-        Array.Sort(words, (a, b) =>
+        int n = accounts.Count;
+        DSU dsu = new(n);
+        for (int a = 0; a < n; a++)
         {
-            if (a.Length == b.Length) { return a.CompareTo(b); }
-            return a.Length.CompareTo(b.Length);
-        });
-        string res = "";
-        Trie trie = new();
-        foreach (var s in words)
+            for (int b = 1 + a; b < n; b++)
+            {
+                if (accounts[a].Skip(1).Intersect(accounts[b].Skip(1)).Any())
+                {
+                    dsu.Union(a, b);
+                }
+            }
+        }
+        Dictionary<int, HashSet<string>> roots = [];
+        for (int i = 0; i < n; i++)
         {
-            if (trie.Insert(s) && s.Length > res.Length) { res = s; }
+            int root = dsu.Find(i);
+            if (!roots.TryAdd(root, [.. accounts[i].Skip(1)]))
+            {
+                roots[root].UnionWith(accounts[i].Skip(1));
+            }
+        }
+        List<IList<string>> res = new(roots.Count);
+        foreach (var (k, v) in roots)
+        {
+            List<string> curr = [accounts[k][0]];
+            curr.AddRange(v.Order(Comparer<string>.Create(string.CompareOrdinal)));
+            res.Add(curr);
         }
         return res;
     }
 }
 
-internal sealed class Trie
+readonly struct DSU
 {
-    public Trie()
+    public DSU(int n)
     {
-        Nodes = new Trie[26];
+        Parent = [.. Enumerable.Range(0, n)];
+        Rank = new int[n];
     }
 
-    Trie[] Nodes { get; }
+    public int[] Parent { get; }
+    public int[] Rank { get; }
 
-    public bool Insert(ReadOnlySpan<char> s)
+    public int Find(int v)
     {
-        var curr = this;
-        for (int i = 0; i < s.Length; i++)
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Rank[rx] < Rank[ry]) { Parent[rx] = ry; }
+        else if (Rank[rx] == Rank[ry])
         {
-            int idx = s[i] - 'a';
-            if (curr.Nodes[idx] is not null) { curr = curr.Nodes[idx]; }
-            else if (i == s.Length - 1) { curr = curr.Nodes[idx] = new(); }
-            else { return false; }
+            Parent[ry] = rx;
+            Rank[rx] += 1;
         }
-        return true;
+        else { Parent[ry] = rx; }
     }
 }
