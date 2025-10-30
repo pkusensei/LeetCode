bb6f83032a2443cd5119688a353e8226ceb48be5
ZW <ZW_08@outlook.com>
2025-10-30 11:28:47 -0400

    739 mono stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,31 +9,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn monotone_increasing_digits(n: i32) -> i32 {
-    let s = n.to_string();
-    let len = s.len();
-    let mut res = vec![];
-    let mut prev = 0;
-    for (idx, d) in s.bytes().map(|b| i32::from(b - b'0')).enumerate() {
-        if prev <= d {
-            res.push(d);
-            prev = d;
-        } else {
-            let mut last = prev - 1;
-            let mut count = len - idx;
-            while let Some(&top) = res.last()
-                && top > last
-            {
-                res.pop();
-                last = top - 1;
-                count += 1;
-            }
-            res.push(last);
-            res.extend(std::iter::repeat_n(9, count - 1));
-            break;
+pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+    let n = temperatures.len();
+    let mut res = vec![0; n];
+    let mut st = vec![];
+    for (i, &num) in temperatures.iter().enumerate() {
+        while let Some(&top) = st.last()
+            && temperatures[top] < num
+        {
+            st.pop();
+            res[top] = i.abs_diff(top) as i32;
         }
+        st.push(i);
     }
-    res.into_iter().fold(0, |acc, d| acc * 10 + d)
+    res
 }
 
 #[cfg(test)]
@@ -66,11 +55,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(monotone_increasing_digits(10), 9);
-        assert_eq!(monotone_increasing_digits(1234), 1234);
-        assert_eq!(monotone_increasing_digits(332), 299);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
