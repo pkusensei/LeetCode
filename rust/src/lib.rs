0fc7f8479537d77b7546dda0dfe5c218c98321d0
ZW <ZW_08@outlook.com>
2025-10-31 11:52:26 -0400

    741 dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,28 +9,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
-    let n = nums.len() as i32 - 2;
-    let mut xor = 0;
-    for &num in &nums {
-        xor ^= num;
+pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
+    let n = grid.len();
+    let mut memo = vec![vec![vec![M; n]; n]; n];
+    dfs(&grid, [0; 4], &mut memo).max(0)
+}
+
+const M: i32 = -2501;
+
+fn dfs(grid: &[Vec<i32>], pos: [usize; 4], memo: &mut [Vec<Vec<i32>>]) -> i32 {
+    let n = grid.len();
+    let [r1, c1, r2, c2] = pos;
+    if !pos.iter().all(|c| (0..n).contains(c)) || grid[r1][c1] == -1 || grid[r2][c2] == -1 {
+        return M;
     }
-    for i in 0..n {
-        xor ^= i;
+    if r1 == n - 1 && c1 == n - 1 {
+        return grid[c1][c1];
     }
-    let low_bit = xor & (-xor);
-    let [mut a, mut b] = [0, 0];
-    for num in nums {
-        if num & low_bit == 0 {
-            a ^= num
-        } else {
-            b ^= num
-        }
+    if memo[r1][c1][r2] > M {
+        return memo[r1][c1][r2];
     }
-    for i in 0..n {
-        if i & low_bit == 0 { a ^= i } else { b ^= i }
+    let mut res = grid[r1][c1];
+    if r1 != r2 && c1 != c2 {
+        res += grid[r2][c2];
     }
-    [a, b].into()
+    res += [
+        dfs(grid, [1 + r1, c1, 1 + r2, c2], memo),
+        dfs(grid, [r1, 1 + c1, r2, 1 + c2], memo),
+        dfs(grid, [1 + r1, c1, r2, 1 + c2], memo),
+        dfs(grid, [r1, 1 + c1, 1 + r2, c2], memo),
+    ]
+    .into_iter()
+    .max()
+    .unwrap_or(0);
+    memo[r1][c1][r2] = res;
+    res
 }
 
 #[cfg(test)]
@@ -63,12 +76,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(delete_and_earn(vec![2, 2, 3, 3, 3, 4]), 9);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(delete_and_earn(vec![1, 6, 3, 3, 8, 4, 8, 10, 1, 3]), 43);
-    }
+    fn test() {}
 }
