466c82cdb1a9a326b8847fe82ed52a3ed5a3afa5
ZW <ZW_08@outlook.com>
2025-10-31 20:15:04 -0400

    3217

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,69 +8,16 @@ namespace Solution;
 
 public class Solution
 {
-    public string CountOfAtoms(string formula)
+    public ListNode ModifiedList(int[] nums, ListNode head)
     {
-        var res = Parse(formula, 0).dict;
-        StringBuilder sb = new();
-        foreach (var kv in res.OrderBy(kv => kv.Key))
+        HashSet<int> set = [.. nums];
+        ListNode dummy = new(-1, head);
+        var curr = dummy;
+        while (curr.next is not null)
         {
-            sb.Append(kv.Key);
-            if (kv.Value > 1) { sb.Append(kv.Value); }
+            if (set.Contains(curr.next.val)) { curr.next = curr.next.next; }
+            else { curr = curr.next; }
         }
-        return sb.ToString();
-
-        static (int end, Dictionary<string, int> dict) Parse(ReadOnlySpan<char> s, int idx)
-        {
-            Dictionary<string, int> res = [];
-            var lookup = res.GetAlternateLookup<ReadOnlySpan<char>>();
-            int prev = idx;
-            bool done = false;
-            while (!done && idx < s.Length)
-            {
-                switch (s[idx])
-                {
-                    case char ch when char.IsAsciiLetterUpper(ch):
-                        idx += 1;
-                        while (idx < s.Length && char.IsAsciiLetterLower(s[idx]))
-                        {
-                            idx += 1;
-                        }
-                        var name = s[prev..idx];
-                        var temp = Count(s, idx);
-                        idx = temp.i;
-                        if (!lookup.TryAdd(name, temp.c)) { lookup[name] += temp.c; }
-                        prev = idx;
-                        break;
-                    case '(':
-                        var (end, dict) = Parse(s, 1 + idx);
-                        idx = end;
-                        temp = Count(s, idx);
-                        idx = temp.i;
-                        prev = idx;
-                        foreach (var (k, v) in dict)
-                        {
-                            if (!lookup.TryAdd(k, temp.c * v)) { lookup[k] += temp.c * v; }
-                        }
-                        break;
-                    default:
-                        done = true;
-                        idx += 1; // skip ')'
-                        break; // do not break while loop
-                }
-            }
-            return (idx, res);
-        }
-
-        static (int c, int i) Count(ReadOnlySpan<char> s, int idx)
-        {
-            int count = 0;
-            while (idx < s.Length && char.IsAsciiDigit(s[idx]))
-            {
-                count = 10 * count + s[idx] - '0';
-                idx += 1;
-            }
-            if (count == 0) { count = 1; }
-            return (count, idx);
-        }
+        return dummy.next;
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,12 +10,8 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow("H2O", "H2O")]
-    [DataRow("H2MgO2", "Mg(OH)2")]
-    [DataRow("K4N2O14S4", "K4(ON(SO3)2)2")]
-    public void TestMethod1(string exp, string s)
+    public void TestMethod1()
     {
-        Assert.AreEqual(exp, sol.CountOfAtoms(s));
     }
 
     [TestMethod]
