3db2a78d0448b4dfa51654ce5f8a8b33c8d58dfa
ZW <ZW_08@outlook.com>
2025-11-01 09:34:34 -0400

    745 trie

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,18 +9,66 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn next_greatest_letter(letters: Vec<char>, target: char) -> char {
-    let mut left = 0;
-    let mut right = letters.len();
-    while left < right {
-        let mid = left.midpoint(right);
-        if letters[mid] <= target {
-            left = 1 + mid;
-        } else {
-            right = mid;
+use itertools::chain;
+use std::iter::once;
+
+struct WordFilter {
+    trie: Trie,
+}
+
+impl WordFilter {
+    fn new(words: Vec<String>) -> Self {
+        let mut trie = Trie::default();
+        for (idx, s) in words.iter().enumerate() {
+            let n = s.len();
+            for i in 0..n {
+                trie.add(chain!(s.bytes().skip(i), once(1 + b'z'), s.bytes()), idx);
+            }
+        }
+        Self { trie }
+    }
+
+    fn f(&self, pref: String, suff: String) -> i32 {
+        self.trie
+            .find(chain!(suff.bytes(), once(1 + b'z'), pref.bytes()))
+    }
+}
+
+struct Trie {
+    nodes: [Option<Box<Trie>>; 27],
+    idx: i32,
+}
+
+impl Default for Trie {
+    fn default() -> Self {
+        Self {
+            nodes: Default::default(),
+            idx: -1,
         }
     }
-    letters.get(left).copied().unwrap_or(letters[0])
+}
+
+impl Trie {
+    fn add(&mut self, s: impl Iterator<Item = u8>, idx: usize) {
+        let mut curr = self;
+        for b in s {
+            let i = usize::from(b - b'a');
+            curr = curr.nodes[i].get_or_insert_default();
+            curr.idx = idx as i32
+        }
+    }
+
+    fn find(&self, s: impl Iterator<Item = u8>) -> i32 {
+        let mut curr = self;
+        for b in s {
+            let i = usize::from(b - b'a');
+            let Some(v) = curr.nodes[i].as_ref() else {
+                return -1;
+            };
+            curr = v;
+        }
+        curr.idx
+    }
 }
 
 #[cfg(test)]
