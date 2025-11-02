dd4688902cd4216c372c4c122696658fb4a93790
ZW <ZW_08@outlook.com>
2025-11-02 08:35:03 -0500

    3731 week_cont474 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,40 +9,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_unguarded(m: i32, n: i32, guards: Vec<Vec<i32>>, walls: Vec<Vec<i32>>) -> i32 {
-    let [rows, cols] = [m, n].map(|v| v as usize);
-    let mut grid = vec![vec![0; cols]; rows];
-    for w in walls.iter().chain(&guards) {
-        grid[w[0] as usize][w[1] as usize] = 3;
+pub fn find_missing_elements(mut nums: Vec<i32>) -> Vec<i32> {
+    nums.sort_unstable();
+    let min = nums[0];
+    let mut res = vec![];
+    let mut curr = min;
+    for num in nums {
+        while curr < num {
+            res.push(curr);
+            curr += 1;
+        }
+        curr += 1;
     }
-    for g in guards {
-        let [row, col] = [0, 1].map(|i| g[i] as usize);
-        for r in 1 + row..rows {
-            if grid[r][col] > 2 {
-                break;
-            }
-            grid[r][col] += 1;
-        }
-        for r in (0..row).rev() {
-            if grid[r][col] > 2 {
-                break;
-            }
-            grid[r][col] += 1;
-        }
-        for c in 1 + col..cols {
-            if grid[row][c] > 2 {
-                break;
-            }
-            grid[row][c] += 1;
-        }
-        for c in (0..col).rev() {
-            if grid[row][c] > 2 {
-                break;
-            }
-            grid[row][c] += 1;
-        }
-    }
-    grid.into_iter().flatten().filter(|&v| v == 0).count() as i32
+    res
 }
 
 #[cfg(test)]
