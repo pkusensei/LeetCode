7f19d21f78625a06ca7cc4ddfb712b7e05967d18
ZW <ZW_08@outlook.com>
2025-11-01 21:55:18 -0400

    2257 not fun at all

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,18 +9,40 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-    let n = cost.len();
-    let mut dp = vec![i32::MAX; 1 + n];
-    dp[..2].fill(0);
-    for i in 0..n {
-        for d in 1..=2 {
-            if i + d <= n {
-                dp[i + d] = dp[i + d].min(cost[i] + dp[i]);
+pub fn count_unguarded(m: i32, n: i32, guards: Vec<Vec<i32>>, walls: Vec<Vec<i32>>) -> i32 {
+    let [rows, cols] = [m, n].map(|v| v as usize);
+    let mut grid = vec![vec![0; cols]; rows];
+    for w in walls.iter().chain(&guards) {
+        grid[w[0] as usize][w[1] as usize] = 3;
+    }
+    for g in guards {
+        let [row, col] = [0, 1].map(|i| g[i] as usize);
+        for r in 1 + row..rows {
+            if grid[r][col] > 2 {
+                break;
             }
+            grid[r][col] += 1;
+        }
+        for r in (0..row).rev() {
+            if grid[r][col] > 2 {
+                break;
+            }
+            grid[r][col] += 1;
+        }
+        for c in 1 + col..cols {
+            if grid[row][c] > 2 {
+                break;
+            }
+            grid[row][c] += 1;
+        }
+        for c in (0..col).rev() {
+            if grid[row][c] > 2 {
+                break;
+            }
+            grid[row][c] += 1;
         }
     }
-    dp[n]
+    grid.into_iter().flatten().filter(|&v| v == 0).count() as i32
 }
 
 #[cfg(test)]
