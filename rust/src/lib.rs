9c567668cb6e682cb84115a61e4a5be320a6b537
ZW <ZW_08@outlook.com>
2025-11-05 09:43:27 -0500

    754 no thanks

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,68 +9,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_x_sum(nums: &[i32], k: i32, x: i32) -> Vec<i64> {
-    use std::collections::{BTreeSet, HashMap};
-    let [k, x] = [k, x].map(|v| v as usize);
-    let mut res = vec![];
-    let mut freq = HashMap::new();
-    let mut big = BTreeSet::new();
-    let mut small = BTreeSet::new();
+pub const fn reach_number(target: i32) -> i32 {
+    let mut num = 0;
     let mut sum = 0;
-    for (idx, &num) in nums.iter().enumerate() {
-        let f = *freq.get(&num).unwrap_or(&0);
-        let item = Item { f: 1 + f, num };
-        if f > 0 {
-            let a = Item { f, ..item };
-            if !small.remove(&a) {
-                big.remove(&a);
-                sum -= a.sum();
-            }
-        }
-        freq.insert(item.num, item.f);
-        big.insert(item);
-        sum += item.sum();
-        if big.len() > x {
-            let a = big.pop_first().unwrap();
-            sum -= a.sum();
-            small.insert(a);
-        }
-        if idx >= k {
-            let left = nums[idx - k];
-            let f = *freq.get(&left).unwrap_or(&0);
-            let a = Item { f, num: left };
-            if !small.remove(&a) {
-                big.remove(&a);
-                sum -= a.sum();
-            }
-            let a = Item { f: f - 1, ..a };
-            freq.insert(a.num, a.f);
-            if a.f > 0 {
-                small.insert(a);
-            }
-            if big.len() < x {
-                if let Some(a) = small.pop_last() {
-                    big.insert(a);
-                    sum += a.sum();
-                }
-            }
-        }
-        if idx >= k - 1 {
-            res.push(sum);
-        }
+    while sum < target.abs() {
+        num += 1;
+        sum += num;
     }
-    res
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Item {
-    f: i32,
-    num: i32,
-}
-
-impl Item {
-    fn sum(&self) -> i64 {
-        i64::from(self.f) * i64::from(self.num)
+    let delta = sum - target.abs();
+    if delta & 1 == 0 {
+        num
+    } else if num & 1 == 1 {
+        1 + num
+    } else {
+        2 + num
     }
 }
 
@@ -104,13 +56,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(find_x_sum(&[1, 1, 2, 2, 3, 4, 2, 3], 6, 2), [6, 10, 12]);
-        assert_eq!(find_x_sum(&[3, 8, 7, 8, 7, 5], 2, 2), [11, 15, 15, 15, 12]);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(find_x_sum(&[8, 2, 2], 2, 2), [10, 4]);
-    }
+    fn test() {}
 }
