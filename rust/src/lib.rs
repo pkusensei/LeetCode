c392ce6bd5bb09a4037f14422131ba5cf175fe28
ZW <ZW_08@outlook.com>
2025-11-04 20:47:20 -0500

    3321

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,31 +6,71 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn crack_safe(n: i32, k: i32) -> String {
-    if n == 1 && k == 1 {
-        return "0".into();
-    }
-    let n = n as usize;
-    let start = vec![b'0'; n - 1];
+pub fn find_x_sum(nums: &[i32], k: i32, x: i32) -> Vec<i64> {
+    use std::collections::{BTreeSet, HashMap};
+    let [k, x] = [k, x].map(|v| v as usize);
     let mut res = vec![];
-    dfs(&start, k, &mut HashSet::new(), &mut res);
-    res.extend(start);
-    String::from_utf8(res).unwrap()
+    let mut freq = HashMap::new();
+    let mut big = BTreeSet::new();
+    let mut small = BTreeSet::new();
+    let mut sum = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        let f = *freq.get(&num).unwrap_or(&0);
+        let item = Item { f: 1 + f, num };
+        if f > 0 {
+            let a = Item { f, ..item };
+            if !small.remove(&a) {
+                big.remove(&a);
+                sum -= a.sum();
+            }
+        }
+        freq.insert(item.num, item.f);
+        big.insert(item);
+        sum += item.sum();
+        if big.len() > x {
+            let a = big.pop_first().unwrap();
+            sum -= a.sum();
+            small.insert(a);
+        }
+        if idx >= k {
+            let left = nums[idx - k];
+            let f = *freq.get(&left).unwrap_or(&0);
+            let a = Item { f, num: left };
+            if !small.remove(&a) {
+                big.remove(&a);
+                sum -= a.sum();
+            }
+            let a = Item { f: f - 1, ..a };
+            freq.insert(a.num, a.f);
+            if a.f > 0 {
+                small.insert(a);
+            }
+            if big.len() < x {
+                if let Some(a) = small.pop_last() {
+                    big.insert(a);
+                    sum += a.sum();
+                }
+            }
+        }
+        if idx >= k - 1 {
+            res.push(sum);
+        }
+    }
+    res
 }
 
-fn dfs(node: &[u8], k: i32, seen: &mut HashSet<Vec<u8>>, res: &mut Vec<u8>) {
-    for i in 0..k {
-        let mut next = node.to_vec();
-        next.push(i as u8 + b'0');
-        if seen.insert(next.clone()) {
-            dfs(&next[1..], k, seen, res);
-            res.push(i as u8 + b'0');
-        }
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Item {
+    f: i32,
+    num: i32,
+}
+
+impl Item {
+    fn sum(&self) -> i64 {
+        i64::from(self.f) * i64::from(self.num)
     }
 }
 
@@ -64,8 +104,13 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(find_x_sum(&[1, 1, 2, 2, 3, 4, 2, 3], 6, 2), [6, 10, 12]);
+        assert_eq!(find_x_sum(&[3, 8, 7, 8, 7, 5], 2, 2), [11, 15, 15, 15, 12]);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(find_x_sum(&[8, 2, 2], 2, 2), [10, 4]);
+    }
 }
