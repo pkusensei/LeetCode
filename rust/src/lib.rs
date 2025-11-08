e8b9dd186ea852db62aeea10a369a372c40b7a45
ZW <ZW_08@outlook.com>
2025-11-08 09:58:26 -0500

    biwe_cont169 q2; fenwick tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,9 +9,52 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_moves(nums: Vec<i32>) -> i32 {
-    let max = nums.iter().max().unwrap();
-    nums.iter().map(|v| max - v).sum()
+pub fn count_majority_subarrays(nums: &[i32], target: i32) -> i32 {
+    let n = nums.len();
+    let mut res = 0;
+    let mut ft = FenwickTree::new(n);
+    for (right, &num) in nums.iter().enumerate() {
+        if num == target {
+            ft.update(right, 1);
+        }
+        for left in 0..=right {
+            let f = ft.query(right) - if left > 0 { ft.query(left - 1) } else { 0 };
+            res += i32::from((2 * f) as usize > right + 1 - left)
+        }
+    }
+    res
+}
+
+struct FenwickTree {
+    tree: Vec<i32>,
+    n: usize,
+}
+
+impl FenwickTree {
+    fn new(n: usize) -> Self {
+        Self {
+            tree: vec![0; 1 + n],
+            n,
+        }
+    }
+
+    fn update(&mut self, mut idx: usize, val: i32) {
+        idx += 1;
+        while idx <= self.n {
+            self.tree[idx] += val;
+            idx += idx & idx.wrapping_neg();
+        }
+    }
+
+    fn query(&self, mut idx: usize) -> i32 {
+        idx += 1;
+        let mut res = 0;
+        while idx > 0 {
+            res += self.tree[idx];
+            idx -= idx & idx.wrapping_neg();
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -44,7 +87,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_majority_subarrays(&[1, 2, 2, 3], 2), 5);
+        assert_eq!(count_majority_subarrays(&[1, 1, 1, 1], 1), 10);
+    }
 
     #[test]
     fn test() {}
