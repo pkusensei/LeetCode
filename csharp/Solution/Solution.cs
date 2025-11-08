88b193a06b45ece91022dc6a563c78e9ce01d195
ZW <ZW_08@outlook.com>
2025-11-07 21:04:57 -0500

    1611 hard dfs/memo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,62 +8,24 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinSwapsCouples(int[] row)
+    public int MinimumOneBitOperations(int n)
     {
-        int n = row.Length / 2;
-        DSU dsu = new(n);
-        int[] seats = [.. Enumerable.Repeat(-1, n)];
-        for (int i = 0; i < row.Length; i++)
-        {
-            int group = row[i] / 2;
-            if (seats[group] < 0) { seats[group] = i / 2; }
-            else { dsu.Union(seats[group], i / 2); }
-        }
-        int res = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int root = dsu.Find(i);
-            if (seats[root] >= 0)
-            {
-                res += dsu.Size[root] - 1;
-                seats[root] = -1;
-            }
-        }
-        return res;
-    }
-}
+        // 100 101 111 110 010 011 001 000
+        // 4               2       1
+        Dictionary<int, int> memo = Enumerable.Range(0, 30)
+            .Select(p => ((int)Math.Pow(2, p), (int)Math.Pow(2, 1 + p) - 1))
+            .ToDictionary(kv => kv.Item1, kv => kv.Item2);
+        return Dfs(n);
 
-readonly struct DSU
-{
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)];
-        Size = [.. Enumerable.Repeat(1, n)];
-    }
-
-    public int[] Parent { get; }
-    public int[] Size { get; }
-
-    public int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public void Union(int x, int y)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry) { return; }
-        if (Size[rx] < Size[ry])
+        int Dfs(int x)
         {
-            Parent[rx] = ry;
-            Size[ry] += Size[rx];
-        }
-        else
-        {
-            Parent[ry] = rx;
-            Size[rx] += Size[ry];
+            if (x <= 1) { return x; }
+            if (memo.TryGetValue(x, out var v)) { return v; }
+            int p = int.Log2(x);
+            int res = Dfs((int)Math.Pow(2, p)) - Dfs(x - (int)Math.Pow(2, p));
+            memo.TryAdd(x, res);
+            return res;
         }
     }
 }
+
