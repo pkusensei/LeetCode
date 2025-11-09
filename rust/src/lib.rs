0a3a62ad22cab40d53f214bfd7075bcf44b8700e
ZW <ZW_08@outlook.com>
2025-11-09 09:10:52 -0500

    3742 week_cont475 q3; dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,28 +9,57 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_distance(nums: Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-    let map = nums
-        .iter()
-        .enumerate()
-        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, (i, &v)| {
-            acc.entry(v).or_default().push(i);
-            acc
-        });
-    let mut res = None;
-    for v in map.values() {
-        if let Some(curr) = v
-            .windows(3)
-            .map(|w| w[0].abs_diff(w[1]) + w[1].abs_diff(w[2]) + w[2].abs_diff(w[0]))
-            .min()
-            .map(|x| x as i32)
-        {
-            let r = res.get_or_insert(curr);
-            *r = (*r).min(curr)
-        }
+pub fn max_path_score(grid: Vec<Vec<i32>>, k: i32) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let k = k as usize;
+    dfs(
+        &grid,
+        k,
+        0,
+        0,
+        0,
+        &mut vec![vec![vec![None; 1 + k]; cols]; rows],
+    )
+    .unwrap_or(-1)
+}
+
+fn dfs(
+    grid: &[Vec<i32>],
+    k: usize,
+    row: usize,
+    col: usize,
+    cost: usize,
+    memo: &mut [Vec<Vec<Option<i32>>>],
+) -> Option<i32> {
+    let [rows, cols] = get_dimensions(grid);
+    if row >= rows || col >= cols {
+        return None;
     }
-    res.unwrap_or(-1)
+    if let Some(v) = memo[row][col][cost] {
+        return if v < 0 { None } else { Some(v) };
+    }
+    let ncost = cost + usize::from(grid[row][col] > 0);
+    if ncost > k {
+        return None;
+    }
+    let curr = grid[row][col];
+    if row == rows - 1 && col == cols - 1 {
+        return Some(curr);
+    }
+    if let Some(res) = match (
+        dfs(grid, k, 1 + row, col, ncost, memo),
+        dfs(grid, k, row, 1 + col, ncost, memo),
+    ) {
+        (None, None) => None,
+        (None, Some(v)) | (Some(v), None) => Some(curr + v),
+        (Some(a), Some(b)) => Some(curr + a.max(b)),
+    } {
+        memo[row][col][cost] = Some(res);
+        Some(res)
+    } else {
+        memo[row][col][cost] = Some(-1);
+        None
+    }
 }
 
 #[cfg(test)]
@@ -63,7 +92,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_path_score(vec![vec![0, 1], vec![2, 0]], 1), 2);
+        assert_eq!(max_path_score(vec![vec![0, 1], vec![1, 2]], 1), -1);
+    }
 
     #[test]
     fn test() {}
