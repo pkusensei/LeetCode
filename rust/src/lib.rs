e42dc5a258ae01cbc5432b6e0719ef6cf84ecd95
ZW <ZW_08@outlook.com>
2025-11-15 19:07:23 -0500

    1513

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,31 +9,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn number_of_substrings(s: &str) -> i32 {
-    let s = s.as_bytes();
-    let n = s.len();
-    let mut left_zero = vec![-1; 1 + n];
-    for i in 0..n {
-        left_zero[1 + i] = if i == 0 || s[i - 1] == b'0' {
-            i as i32
-        } else {
-            left_zero[i]
-        };
-    }
+pub fn num_sub(s: String) -> i32 {
+    const M: i32 = 1_000_000_007;
     let mut res = 0;
-    for (right, &b) in s.iter().enumerate() {
-        let mut f0 = if b == b'0' { 1 } else { 0 };
-        let mut end = 1 + right as i32;
-        while end > 0 && f0 * f0 <= n {
-            let f1 = (1 + right) as i32 - left_zero[end as usize] - f0 as i32;
-            if (f0 * f0) as i32 <= f1 {
-                res += (end as i32 - left_zero[end as usize]).min(1 + f1 - (f0 * f0) as i32);
-            }
-            end = left_zero[end as usize];
-            f0 += 1;
+    let mut curr = 0;
+    for b in s.bytes() {
+        if b == b'1' {
+            curr = (1 + curr) % M;
+        } else {
+            curr = 0;
         }
+        res = (res + curr) % M;
     }
-    res as i32
+    res
 }
 
 #[cfg(test)]
@@ -66,10 +54,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(number_of_substrings("00011"), 5);
-        assert_eq!(number_of_substrings("101101"), 16);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
