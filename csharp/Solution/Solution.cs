2f164530304defd6999d6b6463eabd0246976314
Zx Wang <ZW_08@outlook.com>
2025-11-19 19:20:34 -0500

    757 sort, greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,22 +8,35 @@ namespace Solution;
 
 public class Solution
 {
-    public bool IsOneBitCharacter(int[] bits)
+    public int IntersectionSizeTwo(int[][] intervals)
     {
-        bool prev_zero = false;
-        for (int i = 0; i < bits.Length; i++)
+        Array.Sort(intervals, (a, b) =>
         {
-            if (bits[i] == 0)
+            if (a[1] == b[1]) { return b[0].CompareTo(a[0]); }
+            return a[1].CompareTo(b[1]);
+        });
+        int res = 2;
+        int right = intervals[0][1];
+        int left = right - 1;
+        foreach (var item in intervals[1..])
+        {
+            int a = item[0];
+            int b = item[1];
+            if (a <= left) { }
+            else if (a <= right)
             {
-                prev_zero = true;
+                left = right;
+                right = b;
+                res += 1;
             }
             else
             {
-                prev_zero = false;
-                i += 1;
+                right = b;
+                left = right - 1;
+                res += 2;
             }
         }
-        return prev_zero;
+        return res;
     }
 }
 
