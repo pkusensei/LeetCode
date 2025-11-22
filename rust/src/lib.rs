05c422f050e2da6fb24e891e8ad793570dffa664
ZW <ZW_08@outlook.com>
2025-11-22 12:03:27 -0500

    3753 biwe_cont170 q4; digit dp
    Look back for valid triplet;
    look forward to propagate valid counts

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,29 +6,54 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn lex_smallest_negated_perm(n: i32, target: i64) -> Vec<i32> {
-    let n = i64::from(n);
-    let sum = (1 + n) * n / 2;
-    let t_abs = target.abs();
-    let delta = sum - t_abs;
-    if delta < 0 || delta & 1 == 1 {
-        return vec![];
+pub fn total_waviness(num1: i64, num2: i64) -> i64 {
+    let [s1, s2] = [num1 - 1, num2].map(|v| v.to_string().into_bytes());
+    let a = dfs(&s1, true, false, b'0', b'0', &mut HashMap::new())[0];
+    let b = dfs(&s2, true, false, b'0', b'0', &mut HashMap::new())[0];
+    b - a
+}
+
+// [waviness, count of numbers]
+fn dfs(
+    s: &[u8],
+    tight: bool,
+    start: bool,
+    left: u8,
+    mid: u8,
+    memo: &mut HashMap<(usize, bool, bool, u8, u8), [i64; 2]>,
+) -> [i64; 2] {
+    if s.is_empty() {
+        return [0, 1]; // [no more waviness, one number]
     }
-    let mut res = Vec::with_capacity(n as usize);
-    let mut delta = sum - target;
-    for num in (1..=n).rev() {
-        if delta > 0 && 2 * num <= delta {
-            res.push(-num);
-            delta -= 2 * num;
-        } else {
-            res.push(num);
+    let k = (s.len(), tight, start, left, mid);
+    if let Some(&v) = memo.get(&k) {
+        return v;
+    }
+    let upper = if tight { s[0] } else { b'9' };
+    let mut res = [0; 2];
+    for digit in b'0'..=upper {
+        let ntight = tight && digit == upper;
+        let nstart = start || mid > b'0';
+        let [sub_wav, sub_count] = dfs(&s[1..], ntight, nstart, mid, digit, memo);
+        res[0] += sub_wav;
+        res[1] += sub_count;
+        if start && nstart {
+            // This peak/valey propagates to all sub numbers
+            if left > mid && digit > mid {
+                res[0] += sub_count;
+            }
+            if left < mid && digit < mid {
+                res[0] += sub_count;
+            }
         }
     }
-    res.sort_unstable();
-    res.into_iter().map(|v| v as i32).collect()
+    memo.insert(k, res);
+    res
 }
 
 #[cfg(test)]
@@ -62,12 +87,11 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(lex_smallest_negated_perm(3, 0), [-3, 1, 2]);
-        assert_eq!(lex_smallest_negated_perm(3, -2), [-3, -1, 2])
+        assert_eq!(total_waviness(4848, 4848), 2);
+        assert_eq!(total_waviness(120, 130), 3);
+        assert_eq!(total_waviness(198, 202), 3);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(lex_smallest_negated_perm(4, -4), [-4, -3, 1, 2]);
-    }
+    fn test() {}
 }
