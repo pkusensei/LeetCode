24e796f5a00f5ce39b8b52b5b0b68aa4123ce894
ZW <ZW_08@outlook.com>
2025-11-23 09:49:30 -0500

    3756 week_cont477 q3; dancing around prefix, mod_pow

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,27 +9,52 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_balanced_subarray(nums: Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-    // (xor, even_odd) - idx
-    let mut prefix = HashMap::from([((0, 0), -1)]);
-    let mut curr_xor = 0;
-    let mut curr_even_odd = 0;
-    let mut res = 0;
-    for (idx, &num) in nums.iter().enumerate() {
-        curr_xor ^= num;
-        if num & 1 == 1 {
-            curr_even_odd -= 1
-        } else {
-            curr_even_odd += 1
+pub fn sum_and_multiply(s: &str, queries: &[[i32; 2]]) -> Vec<i32> {
+    let n = s.len();
+    let mut pref_sum = Vec::with_capacity(n);
+    let mut pref_x = Vec::with_capacity(n);
+    let mut pref_pow = Vec::with_capacity(n);
+    let mut curr_val = 0;
+    for b in s.bytes() {
+        let d = i64::from(b - b'0');
+        pref_sum.push(d + pref_sum.last().unwrap_or(&0));
+        pref_pow.push(i64::from(d > 0) + pref_pow.last().unwrap_or(&0));
+        curr_val *= if d == 0 { 1 } else { 10 };
+        curr_val %= M;
+        curr_val += d;
+        curr_val %= M;
+        pref_x.push(curr_val);
+    }
+    let mut res = vec![];
+    for q in queries {
+        let [left, right] = [0, 1].map(|i| q[i] as usize);
+        let dsum = pref_sum[right] - if left > 0 { pref_sum[left - 1] } else { 0 };
+        if dsum == 0 {
+            res.push(0);
+            continue;
         }
-        if let Some(prev) = prefix.get(&(curr_xor, curr_even_odd)) {
-            res = res.max(idx as i32 - prev)
-        } else {
-            prefix.insert((curr_xor, curr_even_odd), idx as i32);
-        }
+        let x = pref_x[right]
+            - if left > 0 {
+                pref_x[left - 1] * mod_pow(10, pref_pow[right] - pref_pow[left - 1], M) % M
+            } else {
+                0
+            };
+        res.push((x.rem_euclid(M) * dsum % M) as i32);
     }
     res
+}
+
+const M: i64 = 1_000_000_007;
+
+const fn mod_pow(b: i64, exp: i64, m: i64) -> i64 {
+    if exp == 0 {
+        return 1;
+    }
+    if exp & 1 == 1 {
+        mod_pow(b * b % m, exp >> 1, m) * b % m
+    } else {
+        mod_pow(b * b % m, exp >> 1, m)
+    }
 }
 
 #[cfg(test)]
@@ -62,7 +87,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            sum_and_multiply("10203004", &[[0, 7], [1, 3], [4, 6]]),
+            [12340, 4, 9]
+        );
+        assert_eq!(sum_and_multiply("1000", &[[0, 3], [1, 1]]), [1, 0]);
+        assert_eq!(sum_and_multiply("9876543210", &[[0, 9]]), [444444137]);
+    }
 
     #[test]
     fn test() {}
