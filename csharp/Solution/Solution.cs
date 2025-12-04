f8a4bf8710132f90bf397b2e7d129f6e8b521a13
Zx Wang <ZW_08@outlook.com>
2025-12-03 19:14:41 -0500

    2211 only LR clash

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,37 +8,11 @@ namespace Solution;
 
 public class Solution
 {
-    public int BestRotation(int[] nums)
+    public int CountCollisions(string directions)
     {
-        int n = nums.Length;
-        int score = 0;
-        int[] diff = new int[1 + n];
-        for (int i = 0; i < n; i++)
-        {
-            int num = nums[i];
-            if (num <= i)
-            {
-                score += 1;
-                diff[1 + i] += 1;
-                diff[1 + i - num] -= 1;
-            }
-            else
-            {
-                diff[1 + i] += 1;
-                diff[1 + i + n - num] -= 1;
-            }
-        }
-        int max = 0;
-        int res = 0;
-        for (int rotate = 0; rotate < n; rotate++)
-        {
-            score += diff[rotate];
-            if (max < score)
-            {
-                res = rotate;
-                max = score;
-            }
-        }
-        return res;
+        int left = directions.IndexOfAny(['R', 'S']);
+        int right = directions.LastIndexOfAny(['L', 'S']);
+        if (left == -1 || right == -1 || left > right) { return 0; }
+        return directions[left..(1 + right)].Count(c => c != 'S');
     }
 }
