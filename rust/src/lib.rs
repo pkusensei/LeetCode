9a34958670284618720fc111e6db0e1c0a3c71dd
ZW <ZW_08@outlook.com>
2025-12-06 09:48:49 -0500

    biwe_cont171 q1; dumb

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,95 +6,62 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::sync::LazyLock;
+
 #[allow(unused_imports)]
 use helper::*;
 
-const M: i32 = 1_000_000_007;
-
-pub fn count_partitions(nums: Vec<i32>, k: i32) -> i32 {
-    use std::collections::BTreeMap;
-    let n = nums.len();
-    let mut map = BTreeMap::new();
-    let mut left = 0;
-    let mut dp = vec![0; 1 + n];
-    dp[0] = 1;
-    let mut curr = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        *map.entry(num).or_insert(0) += 1;
-        curr = (curr + dp[right]) % M;
-        while let (Some(a), Some(b)) = (map.keys().next(), map.keys().next_back())
-            && b - a > k
+pub fn complete_prime(num: i32) -> bool {
+    let s = num.to_string();
+    let n = s.len();
+    for i in 0..n {
+        if let Ok(pref) = s[..i].parse::<i32>()
+            && !is_prime(pref)
         {
-            let v = map.entry(nums[left]).or_insert(0);
-            *v -= 1;
-            if *v == 0 {
-                map.remove(&nums[left]);
-            }
-            curr = (curr - dp[left]).rem_euclid(M);
-            left += 1;
+            return false;
         }
-        dp[1 + right] = curr;
-    }
-    dp[n]
-}
-
-pub fn with_deque(nums: Vec<i32>, k: i32) -> i32 {
-    use std::collections::VecDeque;
-
-    let n = nums.len();
-    let mut dp = vec![0; 1 + n];
-    dp[0] = 1;
-    let mut curr = 0;
-    let mut minq = VecDeque::new();
-    let mut maxq = VecDeque::new();
-    let mut left = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        while minq.back().is_some_and(|&v| nums[v] >= num) {
-            minq.pop_back();
-        }
-        minq.push_back(right);
-        while maxq.back().is_some_and(|&v| nums[v] <= num) {
-            maxq.pop_back();
-        }
-        maxq.push_back(right);
-        curr = (curr + dp[right]) % M;
-        while let (Some(&a), Some(&b)) = (minq.front(), maxq.front())
-            && nums[b] - nums[a] > k
+        if let Ok(suf) = s[i..].parse::<i32>()
+            && !is_prime(suf)
         {
-            if a == left {
-                minq.pop_front();
-            }
-            if b == left {
-                maxq.pop_front();
-            }
-            curr = (curr - dp[left]).rem_euclid(M);
-            left += 1;
-        }
-        dp[1 + right] = curr;
-    }
-    dp[n]
-}
-
-fn find_window_max(nums: &[i32], k: usize) -> Vec<i32> {
-    use std::collections::VecDeque;
-
-    let n = nums.len();
-    let mut deque = VecDeque::with_capacity(k);
-    let mut res = Vec::with_capacity(1 + n - k);
-    for (right, &num) in nums.iter().enumerate() {
-        while deque.back().is_some_and(|&v| nums[v] <= num) {
-            deque.pop_back();
-        }
-        deque.push_back(right);
-        while deque.front().is_some_and(|v| right - v > k) {
-            deque.pop_front();
-        }
-        if right >= k - 1 {
-            res.push(nums[*deque.front().unwrap()]);
+            return false;
         }
     }
-    res
+    true
 }
+
+fn is_prime(num: i32) -> bool {
+    if num < 2 {
+        return false;
+    }
+    for &p in S.iter() {
+        if num < p {
+            break;
+        }
+        if num % p == 0 && num > p {
+            return false;
+        }
+    }
+    true
+}
+
+static S: LazyLock<Vec<i32>> = LazyLock::new(|| {
+    const MAX: usize = 31_623; // or some big number
+    let mut res = vec![true; 1 + MAX]; // assume all are prime
+    res[..2].fill(false); // 0 and 1 are not prime
+    res[2] = true;
+    for p in 2..=MAX {
+        if res[p] {
+            // For this prime, all of its multiples are non-prime
+            for val in (p * p..=MAX).step_by(p) {
+                res[val] = false;
+            }
+        }
+    }
+    res.into_iter()
+        .enumerate()
+        .filter_map(|(p, v)| if v { Some(p as i32) } else { None })
+        .collect()
+});
 
 #[cfg(test)]
 mod tests {
@@ -127,10 +94,7 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            find_window_max(&[1, 3, -1, -3, 5, 3, 6, 7], 3),
-            [3, 3, 5, 5, 6, 7]
-        );
+        assert!(!complete_prime(39));
     }
 
     #[test]
