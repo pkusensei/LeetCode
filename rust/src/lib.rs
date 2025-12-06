3a35a0887669fd232b47902902de4e745837208a
ZW <ZW_08@outlook.com>
2025-12-06 11:52:53 -0500

    3768 biwe_cont171 q4; couldn't think of fenwick tree...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,25 +9,65 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_points(technique1: Vec<i32>, technique2: Vec<i32>, k: i32) -> i64 {
-    use itertools::{Itertools, izip};
-    use std::collections::HashSet;
-    let deltas: HashSet<_> = izip!(&technique1, &technique2)
+pub fn min_inversion_count(nums: &[i32], k: i32) -> i64 {
+    use itertools::Itertools;
+    use std::collections::HashMap;
+    let num_idx: HashMap<_, _> = nums
+        .iter()
+        .copied()
+        .sorted_unstable()
+        .dedup()
         .enumerate()
-        .map(|(idx, (a, b))| (a - b, idx))
-        .sorted_unstable_by(|a, b| b.0.cmp(&a.0))
-        .take(k as usize)
-        .map(|(_v, i)| i)
+        .map(|(i, v)| (v, 1 + i))
         .collect();
-    let mut res = 0;
-    for (idx, (&a, &b)) in izip!(&technique1, &technique2).enumerate() {
-        if deltas.contains(&idx) {
-            res += i64::from(a);
-        } else {
-            res += i64::from(a.max(b))
+    let mut res = i64::MAX;
+    let n = num_idx.len();
+    let k = k as usize;
+    let mut ft = FenwickTree::new(n);
+    let mut curr = 0;
+    for (right, num) in nums.iter().enumerate() {
+        if right >= k {
+            let left = num_idx[&nums[right - k]];
+            ft.update(left, -1);
+            curr -= ft.query(left - 1);
+        }
+        curr += right.min(k - 1) as i64 - ft.query(num_idx[num]);
+        ft.update(num_idx[num], 1);
+        if right >= k - 1 {
+            res = res.min(curr);
         }
     }
     res
+}
+
+struct FenwickTree {
+    tree: Vec<i64>,
+    n: usize, // Semantic size
+}
+
+impl FenwickTree {
+    fn new(n: usize) -> Self {
+        Self {
+            tree: vec![0; 1 + n],
+            n,
+        }
+    }
+
+    fn update(&mut self, mut idx: usize, val: i64) {
+        while idx <= self.n {
+            self.tree[idx] += val;
+            idx += idx & idx.wrapping_neg();
+        }
+    }
+
+    fn query(&self, mut idx: usize) -> i64 {
+        let mut res = 0;
+        while idx > 0 {
+            res += self.tree[idx];
+            idx -= idx & idx.wrapping_neg();
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -60,7 +100,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(min_inversion_count(&[3, 1, 2, 5, 4], 3), 0);
+        assert_eq!(min_inversion_count(&[5, 3, 2, 1], 4), 6);
+    }
 
     #[test]
     fn test() {}
