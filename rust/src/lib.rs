bfecc46af53326306ebbc0a5c458357061827860
ZW <ZW_08@outlook.com>
2025-12-07 10:10:42 -0500

    3771 week_cont479 q3; prefix and binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,35 +9,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_prime(n: i32) -> i32 {
-    if n <= 1 {
-        return 0;
-    }
-    let mut sieve = vec![true; 1 + n as usize]; // assume all are prime
-    sieve[..2].fill(false); // 0 and 1 are not prime
-    sieve[2] = true;
-    for p in 2..=n as usize {
-        if sieve[p] {
-            // For this prime, all of its multiples are non-prime
-            for val in (p * p..=n as usize).step_by(p) {
-                sieve[val] = false;
-            }
-        }
-    }
-    let mut prefix = 0;
+pub fn total_score(hp: i32, damage: &[i32], requirement: &[i32]) -> i64 {
+    use itertools::izip;
+    let n = damage.len();
+    let mut prefix = Vec::with_capacity(1 + n);
+    prefix.push(0);
+    // for d in damage.iter() {
+    //     prefix.push(i64::from(*d) + prefix.last().unwrap_or(&0));
+    // }
+    // hp - sum(damage[left..=right]) >= req
+    // hp - (pref[1+right]-pref[left]) >= req
+    // pref[left] >= req - hp + pref[1+right]
     let mut res = 0;
-    for (p, &is_prime) in sieve.iter().enumerate() {
-        if is_prime {
-            prefix += p;
-        }
-        if prefix > n as usize {
-            break;
-        }
-        if sieve[prefix] {
-            res = res.max(prefix)
+    for (right, (d, req)) in izip!(damage.iter(), requirement.iter()).enumerate() {
+        prefix.push(i64::from(*d) + prefix.last().unwrap_or(&0));
+        let req = i64::from(*req);
+        let need = prefix[1 + right] + req - i64::from(hp);
+        let left = prefix.partition_point(|&v| v < need);
+        if left <= right {
+            res += 1 + right - left;
         }
     }
-    res as i32
+    res as i64
 }
 
 #[cfg(test)]
@@ -70,8 +63,21 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(total_score(11, &[3, 6, 7], &[4, 2, 5]), 3);
+        assert_eq!(total_score(2, &[10000, 1], &[1, 1]), 1);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            total_score(
+                6,
+                &[1, 10000, 1, 10000, 1, 10000],
+                &[1, 10000, 1, 10000, 1, 10000]
+            ),
+            3
+        );
+        assert_eq!(total_score(1, &[1], &[2]), 0);
+    }
 }
