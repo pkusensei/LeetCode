5a6e0983696e998f9830d741d03b13c0b313db39
Zx Wang <ZW_08@outlook.com>
2025-12-08 17:34:19 -0500

    802 topo sort

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,60 +8,42 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinSwap(int[] nums1, int[] nums2)
+    public IList<int> EventualSafeNodes(int[][] graph)
     {
-        int n = nums1.Length;
-        List<int> skip = new(n) { 0 };
-        List<int> swap = new(n) { 1 };
-        for (int i = 1; i < n; i++)
-        {
-            skip.Add(n);
-            swap.Add(n);
-            if (nums1[i - 1] < nums1[i] && nums2[i - 1] < nums2[i])
-            {
-                // Already in order, same skip ops
-                skip[i] = skip[i - 1];
-                // But if `i-1` is swapped, `i` must be swapped
-                swap[i] = 1 + swap[i - 1];
-            }
-            if (nums1[i - 1] < nums2[i] && nums2[i - 1] < nums1[i])
-            {
-                // swap `i-1` but skip `i`
-                skip[i] = int.Min(skip[i], swap[i - 1]);
-                // skip `i-1` but swap `i`
-                swap[i] = int.Min(swap[i], 1 + skip[i - 1]);
-            }
-
-        }
-        return int.Min(skip[^1], swap[^1]);
-    }
-
-    public int TopDown(int[] nums1, int[] nums2)
-    {
-        int n = nums1.Length;
-        int[,] memo = new int[n, 2];
+        int n = graph.Length;
+        int[] outdegs = new int[n];
+        List<int>[] incoming = [.. Enumerable.Range(0, n).Select(_ => new List<int>())];
         for (int i = 0; i < n; i++)
         {
-            memo[i, 0] = -1;
-            memo[i, 1] = -1;
+            outdegs[i] = graph[i].Length;
+            foreach (var node in graph[i])
+            {
+                incoming[node].Add(i);
+            }
         }
-        return Dfs(0, 0, -1, -1);
-
-        int Dfs(int i, int swap, int preva, int prevb)
+        Queue<int> queue = [];
+        List<int> res = [];
+        for (int i = 0; i < n; i++)
         {
-            if (i >= n) { return 0; }
-            if (memo[i, swap] > -1) { return memo[i, swap]; }
-            int res = n;
-            if (preva < nums1[i] && prevb < nums2[i])
+            if (outdegs[i] == 0)
             {
-                res = Dfs(1 + i, 0, nums1[i], nums2[i]);
+                res.Add(i);
+                queue.Enqueue(i);
             }
-            if (preva < nums2[i] && prevb < nums1[i])
-            {
-                res = int.Min(res, 1 + Dfs(1 + i, 1, nums2[i], nums1[i]));
-            }
-            memo[i, swap] = res;
-            return res;
         }
+        while (queue.TryDequeue(out int node))
+        {
+            foreach (var item in incoming[node])
+            {
+                outdegs[item] -= 1;
+                if (outdegs[item] == 0)
+                {
+                    res.Add(item);
+                    queue.Enqueue(item);
+                }
+            }
+        }
+        res.Sort();
+        return res;
     }
 }
