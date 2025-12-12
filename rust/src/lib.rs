72469b08172e2be8fe6d524130648583c73b3dec
ZW <ZW_08@outlook.com>
2025-12-11 20:15:52 -0500

    3433 sorting; all this reading tho

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,26 +9,49 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_covered_buildings(n: i32, buildings: Vec<Vec<i32>>) -> i32 {
-    let n = n as usize;
-    let mut max_row = vec![0; 1 + n];
-    let mut min_row = vec![1 + n; 1 + n];
-    let mut max_col = vec![0; 1 + n];
-    let mut min_col = vec![1 + n; 1 + n];
-    for b in &buildings {
-        let [x, y] = b[..] else { unreachable!() };
-        max_row[y as usize] = max_row[y as usize].max(x as usize);
-        min_row[y as usize] = min_row[y as usize].min(x as usize);
-        max_col[x as usize] = max_col[x as usize].max(y as usize);
-        min_col[x as usize] = min_col[x as usize].min(y as usize);
-    }
-    let mut res = 0;
-    for b in buildings {
-        let [x, y] = b[..] else { unreachable!() };
-        if (1 + min_row[y as usize]..max_row[y as usize]).contains(&(x as usize))
-            && (1 + min_col[x as usize]..max_col[x as usize]).contains(&(y as usize))
-        {
-            res += 1;
+pub fn count_mentions(number_of_users: i32, events: &[[&str; 3]]) -> Vec<i32> {
+    use itertools::Itertools;
+    use std::collections::HashMap;
+    let n = number_of_users as usize;
+    let mut res = vec![0; n];
+    let mut offline = HashMap::new();
+    for e in events
+        .iter()
+        .map(|e| {
+            let kind = i32::from(e[0] == "MESSAGE"); // "OFFLINE"=0
+            let time = e[1].parse::<i32>().unwrap();
+            (time, kind, e[2])
+        })
+        .sorted_unstable()
+    {
+        offline.retain(|_, v| *v > e.0);
+        if e.1 == 0 {
+            let id = e.2.parse::<usize>().unwrap();
+            // Could already be offline?
+            *offline.entry(id).or_insert(e.0) += 60;
+        } else {
+            match e.2 {
+                "ALL" => {
+                    for v in res.iter_mut() {
+                        *v += 1;
+                    }
+                }
+                "HERE" => {
+                    for i in 0..n {
+                        if !offline.contains_key(&i) {
+                            res[i] += 1;
+                        }
+                    }
+                }
+                _ => {
+                    for id in
+                        e.2.split_ascii_whitespace()
+                            .map(|s| s[2..].parse::<usize>().unwrap())
+                    {
+                        res[id] += 1;
+                    }
+                }
+            };
         }
     }
     res
@@ -64,8 +87,33 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            count_mentions(
+                2,
+                &[
+                    ["MESSAGE", "10", "id1 id0"],
+                    ["OFFLINE", "11", "0"],
+                    ["MESSAGE", "71", "HERE"]
+                ]
+            ),
+            [2, 2]
+        );
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            count_mentions(
+                3,
+                &[
+                    ["MESSAGE", "1", "id0 id1"],
+                    ["MESSAGE", "5", "id2"],
+                    ["MESSAGE", "6", "ALL"],
+                    ["OFFLINE", "5", "2"]
+                ]
+            ),
+            [2, 2, 2]
+        );
+    }
 }
