c52c1ea609d3568125400bde44abdd0fd921855f
ZW <ZW_08@outlook.com>
2025-12-17 10:10:50 -0500

    3573 knapsack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,61 +9,51 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_profit(
-    n: i32,
-    present: &[i32],
-    future: &[i32],
-    hierarchy: &[[i32; 2]],
-    budget: i32,
-) -> i32 {
-    let n = n as usize;
-    let adj = hierarchy.iter().fold(vec![vec![]; n], |mut acc, e| {
-        acc[e[0] as usize - 1].push(e[1] as usize - 1);
-        acc
-    });
-    let [a, _] = dfs(&adj, &present, &future, budget as usize, 0, n);
-    a.into_iter().max().unwrap_or_default()
-}
-
-fn dfs(
-    adj: &[Vec<usize>],
-    present: &[i32],
-    future: &[i32],
-    budget: usize,
-    node: usize,
-    prev: usize,
-) -> [Vec<i32>; 2] {
-    let mut full = vec![0; 1 + budget];
-    let mut half = vec![0; 1 + budget];
-    for &next in &adj[node] {
-        if next != prev {
-            let [a, b] = dfs(adj, present, future, budget, next, node);
-            full = merge(&full, &a);
-            half = merge(&half, &b);
+pub fn knapsack(prices: &[i32], k: i32) -> i64 {
+    let k = k as usize;
+    let mut normal = vec![0; 1 + k];
+    let mut buy = vec![i64::MIN >> 2; 1 + k];
+    let mut short = vec![0; 1 + k];
+    for num in prices.iter().map(|&v| i64::from(v)) {
+        for i in (1..=k).rev() {
+            normal[i] = normal[i].max(buy[i] + num).max(short[i] - num);
+            buy[i] = buy[i].max(normal[i - 1] - num);
+            short[i] = short[i].max(normal[i - 1] + num);
         }
     }
-    let mut res_full = full.clone();
-    let mut res_half = full.clone();
-    let mut cost = present[node] as usize;
-    for b in cost..=budget {
-        res_full[b] = res_full[b].max(half[b - cost] + future[node] - cost as i32);
-    }
-    cost /= 2;
-    for b in cost..=budget {
-        res_half[b] = res_half[b].max(half[b - cost] + future[node] - cost as i32);
-    }
-    [res_full, res_half]
+    normal[k]
 }
 
-fn merge(a: &[i32], b: &[i32]) -> Vec<i32> {
-    let n = a.len();
-    let mut res = vec![0; n];
-    for (i1, v1) in a.iter().enumerate() {
-        for (i2, v2) in b[..n - i1].iter().enumerate() {
-            res[i1 + i2] = res[i1 + i2].max(v1 + v2);
-        }
+pub fn maximum_profit(prices: &[i32], k: i32) -> i64 {
+    let n = prices.len();
+    let k = k as usize;
+    let mut memo = vec![vec![[[-1; 2]; 2]; 1 + k]; 1 + n];
+    dfs(&prices, k, 0, 0, &mut memo)
+}
+
+fn dfs(nums: &[i32], k: usize, type_: usize, run: usize, memo: &mut [Vec<[[i64; 2]; 2]>]) -> i64 {
+    if k == 0 || nums.is_empty() {
+        return if run == 0 { 0 } else { i64::MIN >> 4 };
     }
-    res
+    let n = nums.len();
+    if memo[n][k][type_][run] > -1 {
+        return memo[n][k][type_][run];
+    }
+    let skip = dfs(&nums[1..], k, type_, run, memo);
+    let num = i64::from(nums[0]);
+    let take = if run == 0 {
+        let buy = -num + dfs(&nums[1..], k, 0, 1 - run, memo);
+        let short = num + dfs(&nums[1..], k, 1, 1 - run, memo);
+        buy.max(short)
+    } else {
+        if type_ == 0 {
+            num + dfs(&nums[1..], k - 1, type_, 1 - run, memo)
+        } else {
+            -num + dfs(&nums[1..], k - 1, type_, 1 - run, memo)
+        }
+    };
+    memo[n][k][type_][run] = skip.max(take);
+    memo[n][k][type_][run]
 }
 
 #[cfg(test)]
@@ -97,23 +87,13 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_profit(2, &[1, 2], &[4, 3], &[[1, 2]], 3), 5);
-        assert_eq!(max_profit(2, &[3, 4], &[5, 8], &[[1, 2]], 4), 4);
-        assert_eq!(
-            max_profit(3, &[4, 6, 8], &[7, 9, 11], &[[1, 2], [1, 3]], 10),
-            10
-        );
-        assert_eq!(
-            max_profit(3, &[5, 2, 3], &[8, 5, 6], &[[1, 2], [2, 3]], 7),
-            12
-        );
+        assert_eq!(maximum_profit(&[1, 7, 9, 8, 2], 2), 14);
+        assert_eq!(maximum_profit(&[12, 16, 19, 19, 8, 1, 19, 13, 9], 3), 36);
+
+        assert_eq!(knapsack(&[1, 7, 9, 8, 2], 2), 14);
+        assert_eq!(knapsack(&[12, 16, 19, 19, 8, 1, 19, 13, 9], 3), 36);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(
-            max_profit(3, &[42, 27, 32], &[46, 8, 17], &[[1, 2], [2, 3]], 93),
-            4
-        );
-    }
+    fn test() {}
 }
