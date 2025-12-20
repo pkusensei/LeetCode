33c598dbba2b9c806459aba238ee6a7517591398
ZW <ZW_08@outlook.com>
2025-12-20 10:51:32 -0500

    bwc172 q2; track 2 maximums for each remainder

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,36 +9,69 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_score(nums: Vec<i32>, s: String) -> i64 {
-    use itertools::izip;
-    use std::collections::BinaryHeap;
-    let mut res = 0;
-    let mut heap = BinaryHeap::new();
-    for (&num, b) in izip!(&nums, s.bytes()) {
-        heap.push(num);
-        if b == b'1' {
-            res += i64::from(heap.pop().unwrap_or_default());
-        }
+pub fn last_integer(n: i64) -> i64 {
+    dfs(n, n, true)
+}
+
+fn dfs(n: i64, count: i64, dir: bool) -> i64 {
+    if count > 2 {
+        dfs(n, (1 + count) / 2, !dir)
+    } else {
+        if dir { 1 } else { n }
     }
-    res
 }
 
 // q2
 pub fn maximum_sum(mut nums: Vec<i32>) -> i32 {
-    nums.sort_unstable_by(|a, b| b.cmp(a));
     let mut res = 0;
-    let mut rems = [None::<i32>; 3];
-    for (i, &a) in nums.iter().enumerate() {
-        let rem = (3 - a % 3) % 3;
-        if let Some(v) = rems[rem as usize] {
-            res = res.max(v + a);
-        }
-        if rems.iter().any(|v| v.is_none()) {
-            for b in &nums[..i] {
-                let rem = (a + b) % 3;
-                let v = rems[rem as usize].get_or_insert(a + b);
-                *v = (*v).max(a + b);
+    let mut rem0 = [0; 2];
+    let mut rem1 = [0; 2];
+    let mut rem2 = [0; 2];
+    for &num in nums.iter() {
+        match num % 3 {
+            0 => {
+                if rem0.iter().all(|&v| v > 0) {
+                    res = res.max(num + rem0[0] + rem0[1]);
+                }
+                if rem1[0] > 0 && rem2[0] > 0 {
+                    res = res.max(num + rem1[0] + rem2[0]);
+                }
+                if num > rem0[0] {
+                    rem0[1] = rem0[0];
+                    rem0[0] = num;
+                } else if num > rem0[1] {
+                    rem0[1] = num;
+                }
             }
+            1 => {
+                if rem1.iter().all(|&v| v > 0) {
+                    res = res.max(num + rem1[0] + rem1[1]);
+                }
+                if rem0[0] > 0 && rem2[0] > 0 {
+                    res = res.max(num + rem0[0] + rem2[0]);
+                }
+                if num > rem1[0] {
+                    rem1[1] = rem1[0];
+                    rem1[0] = num;
+                } else if num > rem1[1] {
+                    rem1[1] = num;
+                }
+            }
+            2 => {
+                if rem2.iter().all(|&v| v > 0) {
+                    res = res.max(num + rem2[0] + rem2[1]);
+                }
+                if rem0[0] > 0 && rem1[0] > 0 {
+                    res = res.max(num + rem0[0] + rem1[0]);
+                }
+                if num > rem2[0] {
+                    rem2[1] = rem2[0];
+                    rem2[0] = num;
+                } else if num > rem2[1] {
+                    rem2[1] = num;
+                }
+            }
+            _ => unreachable!(),
         }
     }
     res
@@ -75,6 +108,9 @@ mod tests {
 
     #[test]
     fn basics() {
+        // assert_eq!(last_integer(5), 1);
+        // assert_eq!(last_integer(9), 9);
+
         assert_eq!(maximum_sum(vec![4, 2, 3, 1]), 9);
         assert_eq!(maximum_sum(vec![2, 1, 5]), 0);
     }
