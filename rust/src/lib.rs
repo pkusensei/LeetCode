e27457fa6cd933592daee1b5a3c71123b418ea05
ZW <ZW_08@outlook.com>
2025-12-20 11:22:18 -0500

    3782 bwc172 q4; recursion, track start and end

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -10,71 +10,22 @@ mod trie;
 use helper::*;
 
 pub fn last_integer(n: i64) -> i64 {
-    dfs(n, n, true)
+    dfs(1, n, n, true, 0)
 }
 
-fn dfs(n: i64, count: i64, dir: bool) -> i64 {
-    if count > 2 {
-        dfs(n, (1 + count) / 2, !dir)
-    } else {
-        if dir { 1 } else { n }
-    }
-}
-
-// q2
-pub fn maximum_sum(mut nums: Vec<i32>) -> i32 {
-    let mut res = 0;
-    let mut rem0 = [0; 2];
-    let mut rem1 = [0; 2];
-    let mut rem2 = [0; 2];
-    for &num in nums.iter() {
-        match num % 3 {
-            0 => {
-                if rem0.iter().all(|&v| v > 0) {
-                    res = res.max(num + rem0[0] + rem0[1]);
-                }
-                if rem1[0] > 0 && rem2[0] > 0 {
-                    res = res.max(num + rem1[0] + rem2[0]);
-                }
-                if num > rem0[0] {
-                    rem0[1] = rem0[0];
-                    rem0[0] = num;
-                } else if num > rem0[1] {
-                    rem0[1] = num;
-                }
-            }
-            1 => {
-                if rem1.iter().all(|&v| v > 0) {
-                    res = res.max(num + rem1[0] + rem1[1]);
-                }
-                if rem0[0] > 0 && rem2[0] > 0 {
-                    res = res.max(num + rem0[0] + rem2[0]);
-                }
-                if num > rem1[0] {
-                    rem1[1] = rem1[0];
-                    rem1[0] = num;
-                } else if num > rem1[1] {
-                    rem1[1] = num;
-                }
-            }
-            2 => {
-                if rem2.iter().all(|&v| v > 0) {
-                    res = res.max(num + rem2[0] + rem2[1]);
-                }
-                if rem0[0] > 0 && rem1[0] > 0 {
-                    res = res.max(num + rem0[0] + rem1[0]);
-                }
-                if num > rem2[0] {
-                    rem2[1] = rem2[0];
-                    rem2[0] = num;
-                } else if num > rem2[1] {
-                    rem2[1] = num;
-                }
-            }
+const fn dfs(start: i64, end: i64, count: i64, dir: bool, pow: u32) -> i64 {
+    if count > 1 {
+        let (a, b) = match (count & 1, dir) {
+            (1, true) => (start, end),
+            (0, true) => (start, end - 2_i64.pow(pow)),
+            (1, false) => (start, end),
+            (0, false) => (start + 2_i64.pow(pow), end),
             _ => unreachable!(),
-        }
+        };
+        dfs(a, b, (1 + count) / 2, !dir, 1 + pow)
+    } else {
+        start
     }
-    res
 }
 
 #[cfg(test)]
@@ -108,11 +59,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        // assert_eq!(last_integer(5), 1);
-        // assert_eq!(last_integer(9), 9);
-
-        assert_eq!(maximum_sum(vec![4, 2, 3, 1]), 9);
-        assert_eq!(maximum_sum(vec![2, 1, 5]), 0);
+        assert_eq!(last_integer(5), 1);
+        assert_eq!(last_integer(8), 3);
+        assert_eq!(last_integer(9), 9);
     }
 
     #[test]
