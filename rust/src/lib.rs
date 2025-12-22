556d18f9309ff57683c63c7aeadec5da63b27e0f
ZW <ZW_08@outlook.com>
2025-12-22 08:27:19 -0500

    960 lis on multi-rows

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,47 +6,23 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn interaction_costs(n: i32, edges: &[[i32; 2]], group: &[i32]) -> i64 {
-    let n = n as usize;
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        acc[a].push(b);
-        acc[b].push(a);
-        acc
-    });
-    let group_freq = group.iter().fold(HashMap::new(), |mut acc, &g| {
-        *acc.entry(g).or_insert(0) += 1;
-        acc
-    });
-    dfs(&adj, &group, &group_freq, 0, n).0
-}
-
-// group-freq
-fn dfs(
-    adj: &[Vec<usize>],
-    group: &[i32],
-    gf: &HashMap<i32, i64>,
-    node: usize,
-    prev: usize,
-) -> (i64, HashMap<i32, i64>) {
-    let mut freq = HashMap::from([(group[node], 1)]);
-    let mut res = 0;
-    for &next in &adj[node] {
-        if next != prev {
-            let subtree = dfs(adj, group, gf, next, node);
-            res += subtree.0;
-            for (k, v) in subtree.1 {
-                res += v * (gf[&k] - v);
-                *freq.entry(k).or_insert(0) += v;
+pub fn min_deletion_size(strs: Vec<String>) -> i32 {
+    let n = strs[0].len();
+    let mut lis = vec![1; n];
+    for right in 1..n {
+        for left in 0..right {
+            if strs.iter().all(|s| {
+                let s = s.as_bytes();
+                s[left] <= s[right]
+            }) {
+                lis[right] = lis[right].max(1 + lis[left]);
             }
         }
     }
-    (res, freq)
+    n as i32 - lis.iter().max().unwrap_or(&1)
 }
 
 #[cfg(test)]
@@ -79,15 +55,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(interaction_costs(3, &[[0, 1], [1, 2]], &[1, 1, 1]), 4);
-        assert_eq!(interaction_costs(3, &[[0, 1], [1, 2]], &[3, 2, 3]), 2);
-        assert_eq!(
-            interaction_costs(4, &[[0, 1], [0, 2], [0, 3]], &[1, 1, 4, 4]),
-            3
-        );
-        assert_eq!(interaction_costs(2, &[[0, 1]], &[9, 8]), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
