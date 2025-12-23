3db361442bb6b20d088d907e11f05a70261a9636
ZW <ZW_08@outlook.com>
2025-12-22 19:42:19 -0500

    2054 sort, heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,20 +9,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_deletion_size(strs: Vec<String>) -> i32 {
-    let n = strs[0].len();
-    let mut lis = vec![1; n];
-    for right in 1..n {
-        for left in 0..right {
-            if strs.iter().all(|s| {
-                let s = s.as_bytes();
-                s[left] <= s[right]
-            }) {
-                lis[right] = lis[right].max(1 + lis[left]);
-            }
+pub fn max_two_events(mut events: Vec<[i32; 3]>) -> i32 {
+    use std::{cmp::Reverse, collections::BinaryHeap};
+
+    events.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then(a[1].cmp(&b[1])));
+    let mut heap: BinaryHeap<(Reverse<i32>, i32)> = BinaryHeap::new();
+    let mut res = 0;
+    let mut prev_max = 0;
+    for e in events {
+        let [start, end, val] = e[..] else {
+            unreachable!()
+        };
+        while let Some((Reverse(prev_e), prev_v)) = heap.peek()
+            && *prev_e < start
+        {
+            prev_max = prev_max.max(*prev_v);
+            heap.pop();
         }
+        res = res.max(prev_max + val);
+        heap.push((Reverse(end), val));
     }
-    n as i32 - lis.iter().max().unwrap_or(&1)
+    res
 }
 
 #[cfg(test)]
@@ -58,5 +65,33 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            max_two_events(vec![
+                [28, 81, 48],
+                [27, 90, 94],
+                [97, 99, 79],
+                [5, 35, 81],
+                [65, 94, 84],
+                [65, 83, 58],
+                [94, 94, 31],
+                [39, 52, 73]
+            ]),
+            173
+        );
+        assert_eq!(
+            max_two_events(vec![
+                [22, 44, 9],
+                [93, 96, 48],
+                [56, 90, 3],
+                [80, 92, 45],
+                [63, 73, 69],
+                [73, 96, 33],
+                [11, 23, 84],
+                [59, 72, 29],
+                [89, 100, 46]
+            ]),
+            153
+        );
+    }
 }
