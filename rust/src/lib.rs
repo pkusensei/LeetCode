4c5ccb5c0f9986da05408dc2031a9bbedc2961eb
ZW <ZW_08@outlook.com>
2025-12-28 09:37:47 -0500

    3791 wc482 q4; digit dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,23 +9,48 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_all_one_multiple(k: i32) -> i32 {
-    use std::collections::HashSet;
-    if k & 1 == 0 {
-        return -1;
+pub fn count_balanced(low: i64, high: i64) -> i64 {
+    digit_dp(high) - digit_dp(low - 1)
+}
+
+fn digit_dp(num: i64) -> i64 {
+    let s: Vec<_> = num
+        .to_string()
+        .into_bytes()
+        .into_iter()
+        .map(|b| u16::from(b - b'0'))
+        .collect();
+    let n = s.len();
+    let mut memo = vec![vec![vec![vec![-1; 9 * n]; 9 * n]; 2]; n];
+    dfs(&s, 0, true, 0, 0, &mut memo)
+}
+
+fn dfs(
+    s: &[u16],
+    idx: usize,
+    tight: bool,
+    sum_odd: u16,
+    sum_even: u16,
+    memo: &mut [Vec<Vec<Vec<i64>>>],
+) -> i64 {
+    if idx >= s.len() {
+        return i64::from(sum_odd == sum_even && sum_odd > 0);
     }
-    let mut num = 1;
-    let mut res = 1;
-    let mut seen = HashSet::new();
-    while seen.insert(num) {
-        if num % k == 0 {
-            return res;
+    if memo[idx][usize::from(tight)][usize::from(sum_odd)][usize::from(sum_even)] > -1 {
+        return memo[idx][usize::from(tight)][usize::from(sum_odd)][usize::from(sum_even)];
+    }
+    let upper = if tight { s[idx] } else { 9 };
+    let mut res = 0;
+    for d in 0..=upper {
+        let ntight = tight && d == upper;
+        res += if idx & 1 == 1 {
+            dfs(s, 1 + idx, ntight, d + sum_odd, sum_even, memo)
         } else {
-            num = num * 10 % k + 1;
-            res += 1;
-        }
+            dfs(s, 1 + idx, ntight, sum_odd, d + sum_even, memo)
+        };
     }
-    -1
+    memo[idx][usize::from(tight)][usize::from(sum_odd)][usize::from(sum_even)] = res;
+    res
 }
 
 #[cfg(test)]
@@ -59,10 +84,15 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(min_all_one_multiple(3), 3);
-        assert_eq!(min_all_one_multiple(7), 6);
+        assert_eq!(count_balanced(1, 100), 9);
+        assert_eq!(count_balanced(120, 129), 1);
     }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            count_balanced(810591751771967, 823082845685977),
+            289395497430
+        );
+    }
 }
