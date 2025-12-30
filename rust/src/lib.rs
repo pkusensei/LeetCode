0ea229ccb1a17b91bd4046f4602edb696c29d897
ZW <ZW_08@outlook.com>
2025-12-29 19:21:31 -0500

    840

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,41 +8,45 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
 
-pub fn pyramid_transition(bottom: &str, allowed: &[&str]) -> bool {
-    let rules = allowed.iter().fold([[0; 6]; 6], |mut acc, s| {
-        let [a, b, c] = s.as_bytes()[..] else {
-            unreachable!()
-        };
-        let a = usize::from(a - b'A');
-        let b = usize::from(b - b'A');
-        acc[a][b] |= 1 << (c - b'A');
-        acc
-    });
-    let line = bottom.bytes().map(|b| b - b'A').collect_vec();
-    backtrack(&line, &rules, &mut vec![])
+pub fn num_magic_squares_inside(grid: Vec<Vec<i32>>) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    if rows < 3 || cols < 3 {
+        return 0;
+    }
+    let mut res = 0;
+    for r in 0..=rows - 3 {
+        for c in 0..=cols - 3 {
+            if check(&grid, r, c) {
+                res += 1;
+            }
+        }
+    }
+    res
 }
 
-fn backtrack(line: &[u8], rules: &[[u8; 6]; 6], curr: &mut Vec<u8>) -> bool {
-    if line.len() <= 1 {
-        if curr.is_empty() {
-            return true;
-        }
-        return backtrack(curr, rules, &mut vec![]);
-    }
-    let a = usize::from(line[0]);
-    let b = usize::from(line[1]);
-    for bit in 0..6 {
-        if rules[a][b] & (1 << bit) > 0 {
-            curr.push(bit as u8);
-            if backtrack(&line[1..], rules, curr) {
-                return true;
+fn check(grid: &[Vec<i32>], row: usize, col: usize) -> bool {
+    let mut freq = [0; 9];
+    for r in &grid[row..row + 3] {
+        for &v in &r[col..col + 3] {
+            if !(1..=9).contains(&v) {
+                return false;
             }
-            curr.pop();
+            freq[v as usize - 1] += 1;
         }
     }
-    false
+    if freq.into_iter().any(|n| n != 1) {
+        return false;
+    }
+    const SUM: i32 = 15;
+    grid[row][col..col + 3].iter().sum::<i32>() == SUM
+        && grid[row + 1][col..col + 3].iter().sum::<i32>() == SUM
+        && grid[row + 2][col..col + 3].iter().sum::<i32>() == SUM
+        && grid[row][col] + grid[row + 1][col] + grid[row + 2][col] == SUM
+        && grid[row][col + 1] + grid[row + 1][col + 1] + grid[row + 2][col + 1] == SUM
+        && grid[row][col + 2] + grid[row + 1][col + 2] + grid[row + 2][col + 2] == SUM
+        && grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2] == SUM
+        && grid[row][col + 2] + grid[row + 1][col + 1] + grid[row + 2][col] == SUM
 }
 
 #[cfg(test)]
@@ -75,9 +79,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert!(pyramid_transition("BCD", &["BCC", "CDE", "CEA", "FFF"]))
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
