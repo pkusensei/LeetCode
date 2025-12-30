328f0e518a08059a29be90eb2414dfa5e2221efd
ZW <ZW_08@outlook.com>
2025-12-30 18:49:47 -0500

    814 postorder

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,27 +8,12 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxIncreaseKeepingSkyline(int[][] grid)
+    public TreeNode PruneTree(TreeNode root)
     {
-        int n = grid.Length;
-        int[] rowmax = new int[n];
-        int[] colmax = new int[n];
-        for (int r = 0; r < n; r++)
-        {
-            for (int c = 0; c < n; c++)
-            {
-                rowmax[r] = int.Max(rowmax[r], grid[r][c]);
-                colmax[c] = int.Max(colmax[c], grid[r][c]);
-            }
-        }
-        int res = 0;
-        for (int r = 0; r < n; r++)
-        {
-            for (int c = 0; c < n; c++)
-            {
-                res += int.Min(rowmax[r], colmax[c]) - grid[r][c];
-            }
-        }
-        return res;
+        if (root is null) { return null; }
+        root.left = PruneTree(root.left);
+        root.right = PruneTree(root.right);
+        if (root.left is null & root.right is null && root.val == 0) { return null; }
+        return root;
     }
 }
