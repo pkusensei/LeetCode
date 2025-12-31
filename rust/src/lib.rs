837cee682951735e1be7bc0e5fbf563d41269879
ZW <ZW_08@outlook.com>
2025-12-31 09:35:19 -0500

    815 bfs with a twist

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,70 +9,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn latest_day_to_cross(row: i32, col: i32, cells: &[[i32; 2]]) -> i32 {
-    let [row, col] = [row, col].map(|v| v as usize);
-    let n = row * col;
-    let mut dsu = DSU::new(2 + n);
-    let id_of = |r, c| r * col + c;
-    for c in 0..col {
-        dsu.union(n, c); // top tow
-        dsu.union(1 + n, id_of(row - 1, c)); // bottom row
+pub fn num_buses_to_destination(routes: Vec<Vec<i32>>, source: i32, target: i32) -> i32 {
+    use std::collections::{HashMap, HashSet, VecDeque};
+    if source == target {
+        return 0;
     }
-    let mut land = vec![false; n];
-    for (idx, cell) in cells.iter().enumerate().rev() {
-        let [r, c] = {
-            let [r, c] = cell[..] else { unreachable!() };
-            [(r - 1) as usize, c as usize - 1]
-        };
-        let id = id_of(r, c);
-        land[id] = true;
-        for [nr, nc] in neighbors([r, c]) {
-            let nid = id_of(nr, nc);
-            if nr < row && nc < col && land[nid] {
-                dsu.union(id, nid);
+    let stop_buses =
+        routes
+            .iter()
+            .enumerate()
+            .fold(HashMap::<_, Vec<usize>>::new(), |mut acc, (i, r)| {
+                for &stop in r {
+                    acc.entry(stop).or_default().push(i);
+                }
+                acc
+            });
+    let Some(src_buses) = stop_buses.get(&source) else {
+        return -1;
+    };
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+    for &b in src_buses {
+        queue.push_back((b, 1));
+        seen.insert(b);
+    }
+    while let Some((bus, num)) = queue.pop_front() {
+        for &stop in &routes[bus] {
+            if stop == target {
+                return num;
             }
-        }
-        if dsu.find(n) == dsu.find(1 + n) {
-            return idx as i32;
+            if let Some(v) = stop_buses.get(&stop) {
+                for &next in v {
+                    if seen.insert(next) {
+                        queue.push_back((next, 1 + num));
+                    }
+                }
+            }
         }
     }
     -1
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    rank: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            rank: vec![0; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v])
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            return;
-        }
-        match self.rank[rx].cmp(&self.rank[ry]) {
-            std::cmp::Ordering::Less => self.parent[rx] = ry,
-            std::cmp::Ordering::Equal => {
-                self.parent[ry] = rx;
-                self.rank[rx] += 1;
-            }
-            std::cmp::Ordering::Greater => self.parent[ry] = rx,
-        }
-    }
 }
 
 #[cfg(test)]
