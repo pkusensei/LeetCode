ad02031dd2952726172206d502093c5315e99144
Zx Wang <ZW_08@outlook.com>
2026-01-05 19:01:44 -0500

    827 dsu

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,22 +8,84 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
+    public int LargestIsland(int[][] grid)
     {
-        (int d, int p)[] arr = [.. difficulty.Zip(profit).Order().Select(v => (v.First, v.Second))];
-        int max_p = 0;
-        int res = 0;
-        int i = 0;
-        Array.Sort(worker);
-        foreach (var item in worker)
+        int n = grid.Length;
+        DSU dsu = new(n * n);
+        for (int r = 0; r < n; r++)
         {
-            while (i < arr.Length && arr[i].d <= item)
+            for (int c = 0; c < n; c++)
             {
-                max_p = int.Max(max_p, arr[i].p);
-                i += 1;
+                if (grid[r][c] == 1)
+                {
+                    if (1 + r < n && grid[1 + r][c] == 1)
+                    {
+                        dsu.Union(r * n + c, (1 + r) * n + c);
+                    }
+                    if (1 + c < n && grid[r][1 + c] == 1)
+                    {
+                        dsu.Union(r * n + c, r * n + 1 + c);
+                    }
+                }
             }
-            res += max_p;
+        }
+        ReadOnlySpan<(int dr, int dc)> D = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        int res = dsu.Size.Max();
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                if (grid[r][c] == 1) { continue; }
+                HashSet<int> roots = [];
+                foreach (var (dr, dc) in D)
+                {
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if (0 <= nr && nr < n && 0 <= nc && nc < n && grid[nr][nc] == 1)
+                    {
+                        int root = dsu.Find(nr * n + nc);
+                        roots.Add(root);
+                    }
+                }
+                int curr = 1 + roots.Sum(r => dsu.Size[r]);
+                res = int.Max(res, curr);
+            }
         }
         return res;
+    }
+}
+
+struct DSU
+{
+    public DSU(int n)
+    {
+        Parent = [.. Enumerable.Range(0, n)];
+        Size = [.. Enumerable.Repeat(1, n)];
+    }
+
+    public int[] Parent;
+    public int[] Size;
+
+    public int Find(int v)
+    {
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Size[rx] < Size[ry])
+        {
+            Parent[rx] = ry;
+            Size[ry] += Size[rx];
+        }
+        else
+        {
+            Parent[ry] = rx;
+            Size[rx] += Size[ry];
+        }
     }
 }
