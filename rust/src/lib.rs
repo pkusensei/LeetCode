635408abbe6c176a0ac0ac9cd4b26a26870b9ae6
ZW <ZW_08@outlook.com>
2026-01-15 19:50:17 -0500

    2975 brute foece

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,21 +9,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximize_square_hole_area(
-    _n: i32,
-    _m: i32,
-    mut h_bars: Vec<i32>,
-    mut v_bars: Vec<i32>,
-) -> i32 {
-    h_bars.sort_unstable();
-    v_bars.sort_unstable();
-    let [hmax, vmax] = [&h_bars, &v_bars].map(|v| {
-        v.chunk_by(|a, b| 1 + a == *b)
-            .flat_map(|ch| ch.last().zip(ch.first()).map(|(y, x)| y - x))
-            .max()
-            .unwrap_or_default()
-    });
-    (2 + hmax.min(vmax)).pow(2)
+pub fn maximize_square_area(m: i32, n: i32, mut h_fences: Vec<i32>, mut v_fences: Vec<i32>) -> i32 {
+    use std::collections::HashSet;
+    const M: i64 = 1_000_000_007;
+    h_fences.extend([1, m]);
+    let mut hnums = HashSet::new();
+    for (i, a) in h_fences.iter().enumerate() {
+        for b in h_fences.iter().skip(1 + i) {
+            hnums.insert(b.abs_diff(*a));
+        }
+    }
+    v_fences.extend([1, n]);
+    let mut res = 0;
+    for (i, a) in v_fences.iter().enumerate() {
+        for b in v_fences.iter().skip(1 + i) {
+            let v = a.abs_diff(*b);
+            if hnums.contains(&v) {
+                res = res.max(v);
+            }
+        }
+    }
+    if res == 0 {
+        -1
+    } else {
+        (i64::from(res).pow(2) % M) as i32
+    }
 }
 
 #[cfg(test)]
