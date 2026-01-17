f667c60d7770c001d2396e41c31e63c5813d27e9
ZW <ZW_08@outlook.com>
2026-01-17 12:13:39 -0500

    3812 bwc174 q4; dfs, greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,29 +9,53 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn alternating_xor(nums: &[i32], target1: i32, target2: i32) -> i32 {
-    use std::collections::HashMap;
-    const M: i32 = 1_000_000_007;
-    let mut pref_xor = 0;
-    let mut map1 = HashMap::new(); // ends in target1
-    let mut map2 = HashMap::new(); // ends in target2
-    let [mut dp1, mut dp2] = [0, 0];
-    for num in nums {
-        pref_xor ^= num;
-        let temp1 = pref_xor ^ target1;
-        let temp2 = pref_xor ^ target2;
-        let mut curr1 = *map2.get(&temp1).unwrap_or(&0);
-        let curr2 = *map1.get(&temp2).unwrap_or(&0);
-        if pref_xor == target1 {
-            curr1 += 1;
-        }
-        let v = map1.entry(pref_xor).or_insert(0);
-        *v = (*v + curr1) % M;
-        let v = map2.entry(pref_xor).or_insert(0);
-        *v = (*v + curr2) % M;
-        [dp1, dp2] = [curr1, curr2]
+pub fn minimum_flips(n: i32, edges: &[[i32; 2]], start: String, target: String) -> Vec<i32> {
+    use itertools::{Itertools, izip};
+    let n = n as usize;
+    let adj = edges
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (i, e)| {
+            let [a, b] = [0, 1].map(|i| e[i] as usize);
+            acc[a].push((b, i));
+            acc[b].push((a, i));
+            acc
+        });
+    let mut flip = izip!(start.bytes(), target.bytes())
+        .map(|(a, b)| a != b)
+        .collect_vec();
+    let mut res = vec![];
+    if dfs(&adj, 0, n, n, &mut flip, &mut res) {
+        res.sort_unstable();
+        res
+    } else {
+        vec![-1]
     }
-    (dp1 + dp2) % M
+}
+
+fn dfs(
+    adj: &[Vec<(usize, usize)>],
+    node: usize,
+    prev: usize,
+    edge_idx: usize,
+    flip: &mut [bool],
+    res: &mut Vec<i32>,
+) -> bool {
+    for &(next, ei) in &adj[node] {
+        if next != prev && !dfs(adj, next, node, ei, flip, res) {
+            return false;
+        }
+    }
+    if flip[node] {
+        flip[node] = false;
+        if let Some(v) = flip.get_mut(prev) {
+            *v = !(*v);
+            res.push(edge_idx as i32);
+        } else {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -64,16 +88,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(alternating_xor(&[2, 3, 1, 4], 1, 5), 1);
-        assert_eq!(alternating_xor(&[1, 0, 0], 1, 0), 3);
-        assert_eq!(alternating_xor(&[7], 1, 7), 0);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(alternating_xor(&[22867, 87890, 22867], 22867, 87890), 1);
-        assert_eq!(alternating_xor(&[12], 12, 1), 1);
-        assert_eq!(alternating_xor(&[33646, 79267, 0], 33646, 79267), 1);
-    }
+    fn test() {}
 }
