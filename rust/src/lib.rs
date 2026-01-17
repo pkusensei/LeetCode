57b886182a72d035a1b23d7789e981b1fc55e1b7
ZW <ZW_08@outlook.com>
2026-01-17 09:36:07 -0500

    bwc174 q1; one-liner

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_square_area(bottom_left: Vec<Vec<i32>>, top_right: Vec<Vec<i32>>) -> i64 {
-    use itertools::{Itertools, izip};
-    let mut res = 0;
-    for [a, b] in izip!(bottom_left.iter(), top_right.iter()).array_combinations::<2>() {
-        let [ax1, ay1] = a.0[..] else { unreachable!() };
-        let [ax2, ay2] = a.1[..] else { unreachable!() };
-        let [bx1, by1] = b.0[..] else { unreachable!() };
-        let [bx2, by2] = b.1[..] else { unreachable!() };
-        let dx = ax2.min(bx2) - ax1.max(bx1);
-        let dy = ay2.min(by2) - ay1.max(by1);
-        res = res.max(dx.min(dy).max(0));
-    }
-    i64::from(res).pow(2)
+pub fn best_tower(towers: Vec<Vec<i32>>, center: Vec<i32>, radius: i32) -> Vec<i32> {
+    use itertools::Itertools;
+    use std::cmp::Reverse;
+    towers
+        .iter()
+        .filter(|t| {
+            let [tx, ty] = t[..2] else { unreachable!() };
+            let [cx, cy] = center[..] else { unreachable!() };
+            tx.abs_diff(cx) + ty.abs_diff(cy) <= radius as u32
+        })
+        .sorted_unstable_by_key(|t| (Reverse(t[2]), t[0], t[1]))
+        .next()
+        .map(|t| t[..2].to_vec())
+        .unwrap_or(vec![-1, -1])
 }
 
 #[cfg(test)]
