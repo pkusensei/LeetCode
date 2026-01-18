1d0fd1c48458321ff0ce8ba70b838b6e89a987f9
ZW <ZW_08@outlook.com>
2026-01-17 19:58:37 -0500

    1895; brute force, prefix sum; such a boring and annoying one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,53 +9,53 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_flips(n: i32, edges: &[[i32; 2]], start: String, target: String) -> Vec<i32> {
-    use itertools::{Itertools, izip};
-    let n = n as usize;
-    let adj = edges
-        .iter()
-        .enumerate()
-        .fold(vec![vec![]; n], |mut acc, (i, e)| {
-            let [a, b] = [0, 1].map(|i| e[i] as usize);
-            acc[a].push((b, i));
-            acc[b].push((a, i));
+pub fn largest_magic_square(grid: &[&[i32]]) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let mut row_sums = Vec::with_capacity(rows);
+    for row in grid.iter() {
+        let curr = row.iter().fold(Vec::with_capacity(cols), |mut acc, v| {
+            acc.push(v + acc.last().unwrap_or(&0));
             acc
         });
-    let mut flip = izip!(start.bytes(), target.bytes())
-        .map(|(a, b)| a != b)
-        .collect_vec();
-    let mut res = vec![];
-    if dfs(&adj, 0, n, n, &mut flip, &mut res) {
-        res.sort_unstable();
-        res
-    } else {
-        vec![-1]
+        row_sums.push(curr);
     }
-}
-
-fn dfs(
-    adj: &[Vec<(usize, usize)>],
-    node: usize,
-    prev: usize,
-    edge_idx: usize,
-    flip: &mut [bool],
-    res: &mut Vec<i32>,
-) -> bool {
-    for &(next, ei) in &adj[node] {
-        if next != prev && !dfs(adj, next, node, ei, flip, res) {
-            return false;
+    let mut col_sums = vec![vec![0; cols]; rows];
+    for c in 0..cols {
+        col_sums[0][c] = grid[0][c];
+        for r in 1..rows {
+            col_sums[r][c] = col_sums[r - 1][c] + grid[r][c];
         }
     }
-    if flip[node] {
-        flip[node] = false;
-        if let Some(v) = flip.get_mut(prev) {
-            *v = !(*v);
-            res.push(edge_idx as i32);
-        } else {
-            return false;
+    for len in (2..=rows.min(cols)).rev() {
+        for r in 0..=(rows - len) {
+            'out: for c in 0..=(cols - len) {
+                let sum = row_sums[r][c + len - 1] - if c > 0 { row_sums[r][c - 1] } else { 0 };
+                for rr in 1 + r..r + len {
+                    let curr =
+                        row_sums[rr][c + len - 1] - if c > 0 { row_sums[rr][c - 1] } else { 0 };
+                    if curr != sum {
+                        continue 'out;
+                    }
+                }
+                for cc in c..c + len {
+                    let curr =
+                        col_sums[r + len - 1][cc] - if r > 0 { col_sums[r - 1][cc] } else { 0 };
+                    if curr != sum {
+                        continue 'out;
+                    }
+                }
+                let [mut d1, mut d2] = [0, 0];
+                for i in 0..len {
+                    d1 += grid[r + i][c + i];
+                    d2 += grid[r + i][c + len - 1 - i];
+                }
+                if d1 == sum && d2 == sum {
+                    return len as i32;
+                }
+            }
         }
     }
-    true
+    1
 }
 
 #[cfg(test)]
@@ -91,5 +91,15 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            largest_magic_square(&[
+                &[1, 9, 3, 5, 5, 8, 1, 6, 9],
+                &[4, 1, 1, 6, 8, 3, 5, 7, 6],
+                &[9, 8, 4, 7, 2, 4, 9, 2, 7],
+                &[1, 9, 8, 10, 5, 10, 1, 6, 3]
+            ]),
+            3
+        );
+    }
 }
