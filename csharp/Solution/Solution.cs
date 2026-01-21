082074671449636b6078f430f45780d1b3ad681c
Zx Wang <ZW_08@outlook.com>
2026-01-21 17:30:02 -0500

    838 two pass greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,35 +8,37 @@ namespace Solution;
 
 public class Solution
 {
-    public double SeparateSquares(int[][] squares)
+public string PushDominoes(string dominoes)
+{
+    int n = dominoes.Length;
+    int[] arr = new int[n];
+    int curr = 0;
+    for (int i = 0; i < n; i++)
     {
-        const double E = 1e-5;
-        double left = double.MaxValue;
-        double right = double.MinValue;
-        double area = 0.0;
-        foreach (var sq in squares)
+        curr = dominoes[i] switch
         {
-            left = double.Min(left, sq[1]);
-            right = double.Max(right, sq[1] + sq[2]);
-            area += double.Pow(sq[2], 2);
-        }
-        while (right - left > E)
-        {
-            double mid = (left + right) / 2;
-            if (Check(mid)) { left = mid; }
-            else { right = mid; }
-        }
-        return left;
-
-        bool Check(double mid)
-        {
-            double bot = 0;
-            foreach (var sq in squares)
-            {
-                if (mid >= sq[1] + sq[2]) { bot += double.Pow(sq[2], 2); }
-                else if (mid >= sq[1]) { bot += (mid - sq[1]) * sq[2]; }
-            }
-            return area > 2 * bot;
-        }
+            'R' => n,
+            'L' => 0,
+            _ => int.Max(0, curr - 1)
+        };
+        arr[i] = curr;
     }
+    curr = 0;
+    for (int i = n - 1; i >= 0; i -= 1)
+    {
+        curr = dominoes[i] switch
+        {
+            'L' => n,
+            'R' => 0,
+            _ => int.Max(0, curr - 1)
+        };
+        arr[i] -= curr;
+    }
+    return new([.. arr.Select(v => v switch
+    {
+        > 0 => 'R',
+        < 0 => 'L',
+        _ => '.'
+    })]);
+}
 }
