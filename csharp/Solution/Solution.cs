04a8c49a5d3b823336e59e51c9304ce594555951
Zx Wang <ZW_08@outlook.com>
2026-01-21 18:59:02 -0500

    839 this needs unique strings

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,37 +8,86 @@ namespace Solution;
 
 public class Solution
 {
-public string PushDominoes(string dominoes)
-{
-    int n = dominoes.Length;
-    int[] arr = new int[n];
-    int curr = 0;
-    for (int i = 0; i < n; i++)
+    public int NumSimilarGroups(string[] strs)
     {
-        curr = dominoes[i] switch
+        int n = strs.Length;
+        FrozenDictionary<string, int> dict
+            = strs.Select((s, i) => (s, i)).ToFrozenDictionary(v => v.s, v => v.i);
+        DSU dsu = new(n);
+        for (int idx = 0; idx < n; idx++)
         {
-            'R' => n,
-            'L' => 0,
-            _ => int.Max(0, curr - 1)
-        };
-        arr[i] = curr;
-    }
-    curr = 0;
-    for (int i = n - 1; i >= 0; i -= 1)
-    {
-        curr = dominoes[i] switch
+            if (dsu.GetSize(idx) > 1) { continue; }
+            Queue<int> queue = new();
+            queue.Enqueue(idx);
+            bool[] seen = new bool[n];
+            seen[idx] = true;
+            while (queue.TryDequeue(out var si))
+            {
+                foreach (var item in Next(strs[si]))
+                {
+                    if (dict.TryGetValue(item, out var next) && !seen[next])
+                    {
+                        seen[next] = true;
+                        dsu.Union(idx, next);
+                        queue.Enqueue(next);
+                    }
+                }
+            }
+        }
+        return Enumerable.Range(0, n).Select(i => dsu.Find(i)).Distinct().Count();
+
+        static IEnumerable<string> Next(string s)
         {
-            'L' => n,
-            'R' => 0,
-            _ => int.Max(0, curr - 1)
-        };
-        arr[i] -= curr;
+            int n = s.Length;
+            for (int i1 = 0; i1 < n; i1++)
+            {
+                for (int i2 = 0; i2 < n; i2++)
+                {
+                    if (s[i1] != s[i2])
+                    {
+                        char[] arr = s.ToArray();
+                        (arr[i1], arr[i2]) = (arr[i2], arr[i1]);
+                        yield return new(arr);
+                    }
+                }
+            }
+        }
     }
-    return new([.. arr.Select(v => v switch
-    {
-        > 0 => 'R',
-        < 0 => 'L',
-        _ => '.'
-    })]);
 }
+
+struct DSU
+{
+    public DSU(int n)
+    {
+        Parent = [.. Enumerable.Range(0, n)];
+        Size = [.. Enumerable.Repeat(1, n)];
+    }
+
+    public int[] Parent { get; }
+    public int[] Size { get; }
+
+    public int Find(int v)
+    {
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Size[rx] < Size[ry])
+        {
+            Parent[rx] = ry;
+            Size[ry] += Size[rx];
+        }
+        else
+        {
+            Parent[ry] = rx;
+            Size[rx] += Size[ry];
+        }
+    }
+
+    public int GetSize(int v) => Size[Find(v)];
 }
