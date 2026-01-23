bf041052e67f004f5c1eb291e290f411035fc00b
ZW <ZW_08@outlook.com>
2026-01-23 17:33:46 -0500

    846 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,30 +9,33 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_mountain(arr: Vec<i32>) -> i32 {
-    let n = arr.len();
-    let mut res = 0;
-    let mut idx = 0;
-    while idx < n {
-        let start = idx;
-        while arr.get(1 + idx).is_some_and(|&v| v > arr[idx]) {
-            idx += 1;
-        }
-        if start == idx {
-            idx += 1;
-            continue;
-        }
-        let mid = idx;
-        while arr.get(1 + idx).is_some_and(|&v| v < arr[idx]) {
-            idx += 1;
-        }
-        if mid == idx {
-            idx += 1;
-            continue;
-        }
-        res = res.max(idx + 1 - start);
+pub fn is_n_straight_hand(mut hand: Vec<i32>, group_size: i32) -> bool {
+    use itertools::Itertools;
+    use std::cmp::Reverse;
+    let n = hand.len();
+    let sz = group_size as usize;
+    if n % sz > 0 {
+        return false;
     }
-    res as i32
+    let mut freq = hand.iter().copied().counts();
+    hand.sort_unstable_by_key(|&v| Reverse(v));
+    while let Some(last) = hand.pop() {
+        match freq.get(&last) {
+            None | Some(&0) => continue,
+            Some(&f) => {
+                for d in 0..group_size {
+                    if let Some(v) = freq.get_mut(&(last + d))
+                        && *v >= f
+                    {
+                        *v -= f;
+                    } else {
+                        return false;
+                    };
+                }
+            }
+        }
+    }
+    true
 }
 
 #[cfg(test)]
