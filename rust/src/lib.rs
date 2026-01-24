f9090d70093b292287f5347d4513160dfd7a2144
ZW <ZW_08@outlook.com>
2026-01-24 16:10:08 -0500

    848 suffix sum, rev

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,29 +9,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn shortest_path_length(graph: Vec<Vec<i32>>) -> i32 {
-    use std::collections::VecDeque;
-    let n = graph.len();
-    let mut seen = vec![vec![false; n]; 1 << n];
-    let mut queue = VecDeque::new();
-    for i in 0..n {
-        let mask = 1 << i;
-        queue.push_back((mask, i, 0));
-        seen[mask][i] = true;
+pub fn shifting_letters(s: String, shifts: Vec<i32>) -> String {
+    let mut s = s.into_bytes();
+    let mut curr = 0;
+    for (b, sh) in s.iter_mut().zip(shifts).rev() {
+        curr = (curr + sh) % 26;
+        *b = ((*b - b'a') + curr as u8) % 26 + b'a';
     }
-    while let Some((mask, node, step)) = queue.pop_front() {
-        if mask == (1 << n) - 1 {
-            return step;
-        }
-        for &next in &graph[node] {
-            let nmask = mask | (1 << next);
-            if !seen[nmask][next as usize] {
-                seen[nmask][next as usize] = true;
-                queue.push_back((nmask, next as usize, 1 + step));
-            }
-        }
-    }
-    -1
+    String::from_utf8(s).unwrap()
 }
 
 #[cfg(test)]
