dbb4c998059ed79e36031d0bb706291d3406e2b2
ZW <ZW_08@outlook.com>
2026-01-27 21:07:47 -0500

    3651 dp on dijkstra

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,39 +6,64 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, BinaryHeap},
+};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn loud_and_rich(richer: &[[i32; 2]], quiet: &[i32]) -> Vec<i32> {
-    use std::collections::VecDeque;
-    let n = quiet.len();
-    let mut adj = vec![vec![]; n];
-    let mut indegs = vec![0; n];
-    for e in richer.iter() {
-        let [a, b] = [0, 1].map(|i| e[i] as usize);
-        adj[a].push(b);
-        indegs[b] += 1;
-    }
-    // (curr_node, curr_min_quiet)
-    let mut queue: VecDeque<_> = indegs
+pub fn min_cost(grid: &[&[i32]], k: i32) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let map = grid
         .iter()
         .enumerate()
-        .filter_map(|(i, &v)| if v == 0 { Some((i, i)) } else { None })
-        .collect();
-    let mut res: Vec<_> = (0..n).collect();
-    while let Some((node, min_node)) = queue.pop_front() {
-        res[node] = min_node;
-        for &next in &adj[node] {
-            indegs[next] -= 1;
-            if quiet[min_node] <= quiet[res[next]] {
-                res[next] = min_node;
-            }
-            if indegs[next] == 0 {
-                queue.push_back((next, res[next]));
+        .flat_map(|(r, row)| row.iter().enumerate().map(move |(c, &v)| (r, c, v)))
+        .fold(BTreeMap::<_, Vec<_>>::new(), |mut acc, (r, c, v)| {
+            acc.entry(v).or_default().push([r, c]);
+            acc
+        });
+    let k = k as usize;
+    let mut dists = vec![vec![vec![i32::MAX >> 1; 1 + k]; cols]; rows];
+    dists[0][0][k] = 0;
+    let mut heap = BinaryHeap::from([(Reverse(0), 0, 0, k)]);
+    let mut max_expanded = vec![-1; k];
+    while let Some((Reverse(cost), r, c, rem_k)) = heap.pop() {
+        if cost > dists[r][c][rem_k] {
+            continue;
+        }
+        if r == rows - 1 && c == cols - 1 {
+            return cost;
+        }
+        for [nr, nc] in [[1 + r, c], [r, 1 + c]] {
+            if let Some(v) = grid.get(nr).and_then(|row| row.get(nc)) {
+                let ncost = cost + v;
+                if ncost < dists[nr][nc][rem_k] {
+                    dists[nr][nc][rem_k] = ncost;
+                    heap.push((Reverse(ncost), nr, nc, rem_k));
+                }
             }
         }
+        // Tracks max grid value before reaching `nk` teleports
+        // Suppose this cell has value `v1`, we could jump to all `v<=v1`
+        // Later when seeing `v2<=v1`, no jump is useful (at same `nk`)
+        if let Some(nk) = rem_k.checked_sub(1)
+            && max_expanded[nk] < grid[r][c]
+        {
+            let start = 1 + max_expanded[nk];
+            for (_, pos) in map.range(start..=grid[r][c]) {
+                for &[nr, nc] in pos {
+                    if cost < dists[nr][nc][nk] {
+                        dists[nr][nc][nk] = cost;
+                        heap.push((Reverse(cost), nr, nc, nk));
+                    }
+                }
+            }
+            max_expanded[nk] = grid[r][c];
+        }
     }
-    res.iter().map(|&v| v as i32).collect()
+    -1
 }
 
 #[cfg(test)]
@@ -72,12 +97,24 @@ mod tests {
 
     #[test]
     fn basics() {
+        assert_eq!(min_cost(&[&[1, 3, 3], &[2, 5, 4], &[4, 3, 5]], 2), 7);
+        assert_eq!(min_cost(&[&[1, 2], &[2, 3], &[3, 4]], 1), 9);
         assert_eq!(
-            loud_and_rich(
-                &[[1, 0], [2, 1], [3, 1], [3, 7], [4, 3], [5, 3], [6, 3]],
-                &[3, 2, 5, 4, 6, 1, 7, 0]
+            min_cost(
+                &[
+                    &[3, 0, 0],
+                    &[3, 1, 2],
+                    &[4, 4, 3],
+                    &[5, 4, 6],
+                    &[8, 6, 4],
+                    &[9, 8, 6],
+                    &[11, 8, 8],
+                    &[11, 7, 12],
+                    &[12, 10, 9]
+                ],
+                6
             ),
-            [5, 5, 2, 5, 4, 5, 6, 7]
+            23
         );
     }
 
