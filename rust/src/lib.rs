5dd409607b0500e99c2193a1e4098b4a7be4eb65
ZW <ZW_08@outlook.com>
2026-01-30 16:48:55 -0500

    858 lcm

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,30 +9,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn mincost_to_hire_workers(quality: Vec<i32>, wage: Vec<i32>, k: i32) -> f64 {
-    use itertools::{Itertools, izip};
-    use std::collections::BinaryHeap;
-    let arr = izip!(quality.iter(), wage.iter())
-        .map(|(&qu, &wa)| (qu, f64::from(wa) / f64::from(qu)))
-        .sorted_unstable_by(|a, b| a.1.total_cmp(&b.1))
-        .collect_vec();
-    let mut sum = 0;
-    let mut res = f64::MAX;
-    let mut heap = BinaryHeap::new();
-    // low rate of wage/quality
-    // and low sum(quality)
-    for curr in arr {
-        sum += curr.0;
-        heap.push(curr.0);
-        if heap.len() > k as usize {
-            let top = heap.pop().unwrap();
-            sum -= top;
-        }
-        if heap.len() == k as usize {
-            res = res.min(f64::from(sum) * curr.1);
-        }
+pub fn mirror_reflection(x: i32, y: i32) -> i32 {
+    let val = lcm(x, y);
+    let [a, b] = [x, y].map(|v| val / v);
+    match [a & 1, b & 1] {
+        [1, 1] => 1,
+        [0, 1] => 0,
+        _ => 2,
     }
-    res
+}
+
+// `x` is side length.
+// `y` is the height where beams hits right wall for first time.
+// lcm(x, y) is the length beams travels before it hits corner.
+// lcm/y is even => beam hits corner on left wall => #2
+// lcm/x is odd => hits #1
+
+const fn gcd(a: i32, b: i32) -> i32 {
+    if a == 0 { b } else { gcd(b % a, a) }
+}
+
+const fn lcm(a: i32, b: i32) -> i32 {
+    a / gcd(a, b) * b
 }
 
 #[cfg(test)]
