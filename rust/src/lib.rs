8fc1f8b9f92420444dd32255ca9be75413591c37
ZW <ZW_08@outlook.com>
2026-02-01 10:16:51 -0500

    wc487 q2; wtf

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,58 +9,9 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-use std::collections::VecDeque;
-#[derive(Default)]
-struct RideSharingSystem {
-    r_queue: VecDeque<i32>,
-    d_queue: VecDeque<i32>,
-    seen: Vec<bool>,
-    enroute: Vec<bool>,
-    cancel: Vec<bool>,
-}
-
-impl RideSharingSystem {
-    fn new() -> Self {
-        Self {
-            seen: vec![false; 1001],
-            enroute: vec![false; 1001],
-            cancel: vec![false; 1001],
-            ..Default::default()
-        }
-    }
-
-    fn add_rider(&mut self, rider_id: i32) {
-        self.seen[rider_id as usize] = true;
-        self.r_queue.push_back(rider_id);
-    }
-
-    fn add_driver(&mut self, driver_id: i32) {
-        self.d_queue.push_back(driver_id);
-    }
-
-    fn match_driver_with_rider(&mut self) -> Vec<i32> {
-        while let Some(r) = self.r_queue.front()
-            && self.cancel[*r as usize]
-        {
-            self.r_queue.pop_front();
-        }
-        if let Some(&r) = self.r_queue.front()
-            && let Some(&d) = self.d_queue.front()
-        {
-            self.r_queue.pop_front();
-            self.d_queue.pop_front();
-            self.enroute[r as usize] = true;
-            return vec![d, r];
-        }
-        vec![-1, -1]
-    }
-
-    fn cancel_rider(&mut self, rider_id: i32) {
-        let id = rider_id as usize;
-        if self.seen[id] && !self.enroute[id] {
-            self.cancel[id] = true;
-        }
-    }
+pub fn final_element(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    nums[0].max(nums[n - 1])
 }
 
 #[cfg(test)]
@@ -93,8 +44,16 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(final_element(&[4, 16, 3, 9]), 9);
+        assert_eq!(final_element(&[1, 5, 2]), 2);
+        assert_eq!(final_element(&[3, 7]), 7);
+        assert_eq!(final_element(&[1, 17, 12, 9]), 9);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(final_element(&[22, 26, 16, 3, 20]), 22);
+        assert_eq!(final_element(&[22, 6, 24, 15, 4]), 22);
+    }
 }
