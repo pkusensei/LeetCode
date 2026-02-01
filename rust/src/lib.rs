d5fbfcf60a3341f39a29baaa9c00f25623f5478b
ZW <ZW_08@outlook.com>
2026-02-01 09:24:58 -0500

    wc487 q3; deque

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,14 +9,58 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub const fn count_monobit(n: i32) -> i32 {
-    let mut p = 0;
-    let mut res = 0;
-    while (1 << p) - 1 <= n {
-        res += 1;
-        p += 1;
+use std::collections::VecDeque;
+#[derive(Default)]
+struct RideSharingSystem {
+    r_queue: VecDeque<i32>,
+    d_queue: VecDeque<i32>,
+    seen: Vec<bool>,
+    enroute: Vec<bool>,
+    cancel: Vec<bool>,
+}
+
+impl RideSharingSystem {
+    fn new() -> Self {
+        Self {
+            seen: vec![false; 1001],
+            enroute: vec![false; 1001],
+            cancel: vec![false; 1001],
+            ..Default::default()
+        }
     }
-    res
+
+    fn add_rider(&mut self, rider_id: i32) {
+        self.seen[rider_id as usize] = true;
+        self.r_queue.push_back(rider_id);
+    }
+
+    fn add_driver(&mut self, driver_id: i32) {
+        self.d_queue.push_back(driver_id);
+    }
+
+    fn match_driver_with_rider(&mut self) -> Vec<i32> {
+        while let Some(r) = self.r_queue.front()
+            && self.cancel[*r as usize]
+        {
+            self.r_queue.pop_front();
+        }
+        if let Some(&r) = self.r_queue.front()
+            && let Some(&d) = self.d_queue.front()
+        {
+            self.r_queue.pop_front();
+            self.d_queue.pop_front();
+            self.enroute[r as usize] = true;
+            return vec![d, r];
+        }
+        vec![-1, -1]
+    }
+
+    fn cancel_rider(&mut self, rider_id: i32) {
+        let id = rider_id as usize;
+        if self.seen[id] && !self.enroute[id] {
+            self.cancel[id] = true;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -49,10 +93,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_monobit(0), 1);
-        assert_eq!(count_monobit(4), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
