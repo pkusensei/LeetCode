7d679993c348716586015e9d7adff69a9fcce7f6
ZW <ZW_08@outlook.com>
2026-02-08 08:45:56 -0500

    3833 wc488 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,76 +9,14 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn with_stack(s: &str) -> i32 {
+pub fn dominant_indices(nums: &[i32]) -> i32 {
     let mut res = 0;
-    let mut st = vec![];
-    for b in s.bytes() {
-        if b == b'a' && st.last().is_some_and(|v| *v == b'b') {
-            st.pop();
-            res += 1;
-        } else {
-            st.push(b);
-        }
-    }
-    res
-}
-
-pub fn with_prefix_suffix(s: &str) -> i32 {
-    let mut count_a = 0;
-    let mut suf_a = vec![];
-    // count of 'a' after this idx
-    for b in s.bytes().rev() {
-        suf_a.push(count_a);
-        count_a += i32::from(b == b'a');
-    }
-    suf_a.reverse();
-    let mut res = s.len() as i32;
-    let mut count_b = 0;
-    for (b, count_a) in s.bytes().zip(suf_a) {
-        res = res.min(count_a + count_b);
-        count_b += i32::from(b == b'b');
-    }
-    res
-}
-
-pub fn with_prefix_suffix_opt(s: &str) -> i32 {
-    let mut count_a = s.bytes().filter(|&b| b == b'a').count() as i32;
-    let mut res = s.len() as i32;
-    let mut count_b = 0;
-    for b in s.bytes() {
-        // 'a' to the right
-        count_a -= i32::from(b == b'a');
-        res = res.min(count_a + count_b);
-        // 'b' to the left
-        count_b += i32::from(b == b'b');
-    }
-    res
-}
-
-pub fn with_dp(s: &str) -> i32 {
-    let n = s.len();
-    let mut dp = vec![0; 1 + n];
-    let mut count_b = 0;
-    for (i, b) in s.bytes().enumerate() {
-        if b == b'b' {
-            dp[1 + i] = dp[i];
-            count_b += 1;
-        } else {
-            dp[1 + i] = (1 + dp[i]).min(count_b);
-        }
-    }
-    dp[n]
-}
-
-pub fn with_dp_opt(s: &str) -> i32 {
-    let mut res = 0;
-    let mut count_b = 0;
-    for b in s.bytes() {
-        if b == b'b' {
-            count_b += 1;
-        } else {
-            res = (1 + res).min(count_b);
-        }
+    let mut sum = f64::from(*nums.last().unwrap());
+    let mut count = 1.0;
+    for &num in nums.iter().rev().skip(1) {
+        res += i32::from(f64::from(num) > sum / count);
+        sum += f64::from(num);
+        count += 1.0;
     }
     res
 }
@@ -114,20 +52,7 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(with_stack("aababbab"), 2);
-        assert_eq!(with_stack("bbaaaaabb"), 2);
-
-        assert_eq!(with_prefix_suffix("aababbab"), 2);
-        assert_eq!(with_prefix_suffix("bbaaaaabb"), 2);
-
-        assert_eq!(with_prefix_suffix_opt("aababbab"), 2);
-        assert_eq!(with_prefix_suffix_opt("bbaaaaabb"), 2);
-
-        assert_eq!(with_dp("aababbab"), 2);
-        assert_eq!(with_dp("bbaaaaabb"), 2);
-
-        assert_eq!(with_dp_opt("aababbab"), 2);
-        assert_eq!(with_dp_opt("bbaaaaabb"), 2);
+        assert_eq!(dominant_indices(&[5, 4, 3]), 2);
     }
 
     #[test]
