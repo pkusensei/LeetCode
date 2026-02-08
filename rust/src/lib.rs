9cd0dd48b4f83434ef84d9f4e90fc0e4be0a2515
ZW <ZW_08@outlook.com>
2026-02-08 09:36:30 -0500

    3835 wc488 q3; mono deque

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,59 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn merge_adjacent(nums: Vec<i32>) -> Vec<i64> {
-    let mut st = vec![];
-    for num in nums.iter() {
-        let mut num = i64::from(*num);
-        while let Some(&top) = st.last()
-            && top == num
+use std::collections::VecDeque;
+
+pub fn count_subarrays(nums: &[i32], k: i64) -> i64 {
+    let mut left = 0;
+    let mut res = 0;
+    let [mut minq, mut maxq] = [const { VecDeque::new() }; 2];
+    for (right, &num) in nums.iter().enumerate() {
+        while let Some((i, _val)) = minq.front()
+            && *i < left
         {
-            num *= 2;
-            st.pop();
+            minq.pop_front();
         }
-        st.push(num);
+        while let Some(&(_, val)) = minq.back()
+            && val > num
+        {
+            minq.pop_back();
+        }
+        minq.push_back((right, num));
+        while let Some((i, _val)) = maxq.front()
+            && *i < left
+        {
+            minq.pop_front();
+        }
+        while let Some(&(_, val)) = maxq.back()
+            && val < num
+        {
+            maxq.pop_back();
+        }
+        maxq.push_back((right, num));
+        while f(&minq, &maxq, right - left + 1) > k {
+            if let Some((i, _)) = minq.front()
+                && *i == left
+            {
+                minq.pop_front();
+            }
+            if let Some((i, _)) = maxq.front()
+                && *i == left
+            {
+                maxq.pop_front();
+            }
+            left += 1;
+        }
+        let len = right - left + 1;
+        res += len;
     }
-    st
+    res as _
+}
+
+fn f(minq: &VecDeque<(usize, i32)>, maxq: &VecDeque<(usize, i32)>, len: usize) -> i64 {
+    let (Some(a), Some(b)) = (minq.front(), maxq.front()) else {
+        return 0;
+    };
+    i64::from(b.1 - a.1) * len as i64
 }
 
 #[cfg(test)]
@@ -54,7 +94,11 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_subarrays(&[1, 3, 2], 4), 5);
+        assert_eq!(count_subarrays(&[5; 4], 0), 10);
+        assert_eq!(count_subarrays(&[1, 2, 3], 0), 3);
+    }
 
     #[test]
     fn test() {}
