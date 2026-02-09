73ea1068737b0a94da87cbdb31ff58841ea297d4
Zx Wang <ZW_08@outlook.com>
2026-02-09 17:57:07 -0500

    870

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,20 +8,28 @@ namespace Solution;
 
 public class Solution
 {
-    public int BinaryGap(int n)
+    public int[] AdvantageCount(int[] nums1, int[] nums2)
     {
-        int prev = -1;
-        int res = 0;
-        int i = 0;
-        while (n > 0)
+        int n = nums1.Length;
+        Array.Sort(nums1);
+        int[] ids = [.. Enumerable.Range(0, n)];
+        Array.Sort(ids, (a, b) => nums2[a].CompareTo(nums2[b]));
+        int[] res = new int[n];
+        Array.Fill(res, -1);
+        Stack<int> st = [];
+        int i2 = 0;
+        foreach (var num in nums1)
         {
-            if ((n & 1) == 1)
+            if (num > nums2[ids[i2]])
             {
-                if (prev > -1) { res = int.Max(res, i - prev); }
-                prev = i;
+                res[ids[i2]] = num;
+                i2 += 1;
             }
-            n >>= 1;
-            i += 1;
+            else { st.Push(num); }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (res[i] == -1) { res[i] = st.Pop(); }
         }
         return res;
     }
