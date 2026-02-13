ead79a018d9ab9100f90090be1d7da3b8c6fd269
ZW <ZW_08@outlook.com>
2026-02-12 20:41:41 -0500

    3714 prefix soup

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,60 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
 
 pub fn longest_balanced(s: String) -> i32 {
-    let n = s.len();
-    let s = s.as_bytes();
-    let mut res = 1;
-    for left in 0..n {
-        let mut f = [0; 26];
-        for right in left..n {
-            f[usize::from(s[right] - b'a')] += 1;
-            if right + 1 - left > res && f.iter().filter(|&&v| v > 0).all_equal() {
-                res = right + 1 - left;
-            }
-        }
+    // uniform
+    let mut res = s
+        .as_bytes()
+        .chunk_by(|a, b| a == b)
+        .map(|w| w.len())
+        .max()
+        .unwrap_or(1);
+    let mut seen = HashMap::from([([0, 0], 0)]);
+    let mut curr = [0; 3];
+    let mut prefix = Vec::from([curr]);
+    for (idx, b) in s.bytes().enumerate() {
+        curr[usize::from(b - b'a')] += 1;
+        prefix.push(curr);
+        // [a-b, b-c]
+        let diff = [curr[0] - curr[1], curr[1] - curr[2]];
+        // +1 to offset seen[[0,0]]=0
+        let left = *seen.entry(diff).or_insert(1 + idx);
+        res = res.max(1 + idx - left);
     }
-    res as i32
+    res.max(f2(&prefix, 0, 1))
+        .max(f2(&prefix, 0, 2))
+        .max(f2(&prefix, 1, 2)) as i32
+}
+
+fn f2(prefix: &[[i32; 3]], i0: usize, i1: usize) -> usize {
+    let n = prefix.len();
+    let i2 = 3 - i0 - i1;
+    let mut res = 1;
+    let mut discard = 0;
+    let mut left = 1;
+    while left < n {
+        while left < n && prefix[left][i2] > discard {
+            discard = prefix[left][i2];
+            left += 1;
+        }
+        let mut right = left;
+        let mut seen = HashMap::from([(0, left - 1)]);
+        while right < n && prefix[right][i2] == discard {
+            let left_f = prefix[left - 1];
+            let f = prefix[right];
+            let diff = (f[i1] - f[i0]) - (left_f[i1] - left_f[i0]);
+            let v = *seen.entry(diff).or_insert(right);
+            res = res.max(right - v);
+            right += 1;
+        }
+        left = right;
+    }
+    res
 }
 
 #[cfg(test)]
