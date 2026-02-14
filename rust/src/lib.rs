83b4ca264af6f69e1bedf48f7a8ca86994a5119e
ZW <ZW_08@outlook.com>
2026-02-14 09:49:30 -0500

    bwc176 q3; dfs/memo, dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,18 +9,56 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn prefix_connected(words: Vec<String>, k: i32) -> i32 {
-    use std::collections::HashMap;
-    let k = k as usize;
-    let mut map = HashMap::new();
-    for w in words.iter() {
-        if w.len() < k {
-            continue;
-        }
-        let p = &w[..k];
-        *map.entry(p).or_insert(0) += 1;
+pub fn with_dp_opt(nums: &[i32], colors: &[i32]) -> i64 {
+    let [mut dp1, mut dp2] = [0, 0];
+    for (i, val) in nums.iter().enumerate() {
+        let val = i64::from(*val);
+        // dp1, dp2, curr
+        let curr = if i > 0 && colors[i] == colors[i - 1] {
+            dp2.max(val + dp1)
+        } else {
+            val + dp2
+        };
+        dp1 = dp2;
+        dp2 = curr;
     }
-    map.iter().filter(|(k, v)| **v > 1).count() as i32
+    dp2
+}
+
+pub fn with_dp(nums: &[i32], colors: &[i32]) -> i64 {
+    let n = nums.len();
+    let mut dp = vec![0; n];
+    for (i, &num) in nums.iter().enumerate().rev() {
+        let val = i64::from(num);
+        dp[i] = if colors.get(1 + i).is_some_and(|&c| c == colors[i]) {
+            dp[1 + i].max(val + dp.get(2 + i).unwrap_or(&0))
+        } else {
+            val + dp.get(1 + i).unwrap_or(&0)
+        };
+    }
+    dp[0]
+}
+
+pub fn rob(nums: &[i32], colors: &[i32]) -> i64 {
+    let n = nums.len();
+    dfs(&nums, &colors, 0, &mut vec![-1; n])
+}
+
+fn dfs(nums: &[i32], colors: &[i32], idx: usize, memo: &mut [i64]) -> i64 {
+    if idx >= nums.len() {
+        return 0;
+    }
+    if memo[idx] > -1 {
+        return memo[idx];
+    }
+    let skip = dfs(nums, colors, 1 + idx, memo);
+    let take = if colors.get(1 + idx).is_some_and(|&c| c == colors[idx]) {
+        i64::from(nums[idx]) + dfs(nums, colors, 2 + idx, memo)
+    } else {
+        i64::from(nums[idx]) + dfs(nums, colors, 1 + idx, memo)
+    };
+    memo[idx] = skip.max(take);
+    memo[idx]
 }
 
 #[cfg(test)]
