5e6e39afed187dfc34d8f8959c6bad1277608217
ZW <ZW_08@outlook.com>
2026-02-15 09:27:48 -0500

    3844 wc489 q3; dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,27 +9,56 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn first_unique_freq(nums: Vec<i32>) -> i32 {
-    use std::collections::HashMap;
-    let freq = nums.iter().fold(vec![0; 100_001], |mut acc, &num| {
-        acc[num as usize] += 1;
-        acc
-    });
-    let map = freq
-        .iter()
-        .enumerate()
-        .filter_map(|(num, &f)| if f > 0 { Some((num, f)) } else { None })
-        .fold(HashMap::<_, Vec<_>>::new(), |mut acc, (num, f)| {
-            acc.entry(f).or_default().push(num);
-            acc
-        });
-    for num in nums {
-        let f = freq[num as usize];
-        if map[&f].len() == 1 {
-            return num;
+pub fn almost_palindromic(s: &str) -> i32 {
+    let (s, n) = (s.as_bytes(), s.len());
+    if n < 2 {
+        return n as i32;
+    }
+    let mut memo = vec![vec![[-1; 2]; n]; n];
+    let res = (0..n).map(|i| f(s, i, i, 0, &mut memo)).max().unwrap_or(2);
+    (0..n - 1)
+        .filter_map(|i| {
+            if s[i] == s[1 + i] {
+                Some(f(s, i, 1 + i, 0, &mut memo))
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap_or(2)
+        .max(res)
+}
+
+fn f(
+    s: &[u8],
+    mut left: usize,
+    mut right: usize,
+    skipped: usize,
+    memo: &mut [Vec<[i32; 2]>],
+) -> i32 {
+    let n = s.len();
+    if memo[left][right][skipped] > -1 {
+        return memo[left][right][skipped];
+    }
+    let mut res = (1 + right - left) as i32;
+    while let Some(ll) = left.checked_sub(1)
+        && 1 + right < n
+        && s[ll] == s[1 + right]
+    {
+        res += 2;
+        left -= 1;
+        right += 1;
+    }
+    if skipped == 0 {
+        if left > 0 {
+            res = res.max(f(s, left - 1, right, 1, memo));
+        }
+        if right < n - 1 {
+            res = res.max(f(s, left, 1 + right, 1, memo));
         }
     }
-    -1
+    memo[left][right][skipped] = res;
+    res
 }
 
 #[cfg(test)]
@@ -62,8 +91,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(almost_palindromic("abca"), 4);
+        assert_eq!(almost_palindromic("abba"), 4);
+        assert_eq!(almost_palindromic("zzabba"), 5);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(almost_palindromic("abc"), 2);
+    }
 }
