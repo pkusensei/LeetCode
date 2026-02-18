aba87cbf1fcf6732ce47b094b1d85062467a245f
Zx Wang <ZW_08@outlook.com>
2026-02-18 17:40:10 -0500

    879 knapsack, dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,18 +8,80 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinEatingSpeed(int[] piles, int h)
+    public int ProfitableSchemes(int n, int minProfit, int[] group, int[] profit)
     {
-        int left = 1;
-        int right = piles.Max();
-        while (left < right)
+        const int M = 1_000_000_007;
+        int len = group.Length;
+        int[,] dp = new int[1 + n, 1 + minProfit];
+        for (int i = 0; i <= n; i++)
         {
-            int mid = left + (right - left) / 2;
-            if (Eat(mid) > h) { left = 1 + mid; }
-            else { right = mid; }
+            dp[i, 0] = 1;
         }
-        return left;
+        foreach (var (gro, pro) in group.Zip(profit))
+        {
+            for (int c = n; c >= gro; c -= 1)
+            {
+                for (int p = minProfit; p >= 0; p -= 1)
+                {
+                    dp[c, p] = (dp[c, p] + dp[c - gro, int.Max(0, p - pro)]) % M;
+                }
+            }
+        }
+        return dp[n, minProfit];
+        // int[,] prev = new int[1 + n, 1 + minProfit];
+        // for (int i = 0; i <= n; i++)
+        // {
+        //     prev[i, 0] = 1;
+        // }
+        // foreach (var (gro, pro) in group.Zip(profit))
+        // {
+        //     int[,] curr = new int[1 + n, 1 + minProfit];
+        //     for (int c = 0; c <= n; c++)
+        //     {
+        //         for (int p = 0; p <= minProfit; p++)
+        //         {
+        //             curr[c, p] = prev[c, p];
+        //             if (c - gro >= 0)
+        //             {
+        //                 curr[c, p] += prev[c - gro, int.Max(0, p - pro)];
+        //                 curr[c, p] %= M;
+        //             }
+        //         }
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[n, minProfit];
 
-        int Eat(int mid) => piles.Sum(v => (v + mid - 1) / mid);
+        // int[,,] dp = new int[1 + len, 1 + n, 1 + minProfit];
+        // for (int i = 0; i <= n; i++)
+        // {
+        //     dp[0, i, 0] = 1;
+        // }
+        // for (int idx = 0; idx < len; idx += 1)
+        // {
+        //     for (int c = 0; c <= n; c++)
+        //     {
+        //         for (int p = 0; p <= minProfit; p++)
+        //         {
+        //             dp[1 + idx, c, p] = dp[idx, c, p];
+        //             if (c - group[idx] >= 0)
+        //             {
+        //                 dp[1 + idx, c, p] += dp[idx, c - group[idx], int.Max(0, p - profit[idx])];
+        //                 dp[1 + idx, c, p] %= M;
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp[len, n, minProfit];
+
+        // return Dfs(0, n, minProfit);
+
+        // int Dfs(int idx, int count, int min_profit)
+        // {
+        //     if (idx >= len) { return min_profit <= 0 ? 1 : 0; }
+        //     if (count < 0) { return 0; }
+        //     return Dfs(1 + idx, count, min_profit)
+        //         + Dfs(1 + idx, count - group[idx], min_profit - profit[idx]);
+        // }
     }
 }
