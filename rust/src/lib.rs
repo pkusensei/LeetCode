5b5a9b6f05beeb6b4526a00cdb8962c3f4fa6a70
ZW <ZW_08@outlook.com>
2026-02-25 11:14:29 -0500

    895 binary heap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,9 +9,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sort_by_bits(mut arr: Vec<i32>) -> Vec<i32> {
-    arr.sort_unstable_by(|a, b| a.count_ones().cmp(&b.count_ones()).then(a.cmp(b)));
-    arr
+use std::collections::{BinaryHeap, HashMap};
+
+#[derive(Default)]
+struct FreqStack {
+    id: u32,
+    map: HashMap<i32, i32>,            // val - freq
+    heap: BinaryHeap<(i32, u32, i32)>, // (freq, id, val)
+}
+
+impl FreqStack {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn push(&mut self, val: i32) {
+        let id = self.id;
+        self.id += 1;
+        let f = self.map.entry(val).or_default();
+        *f += 1;
+        self.heap.push((*f, id, val));
+    }
+
+    fn pop(&mut self) -> i32 {
+        while let Some(top) = self.heap.peek()
+            && self.map.get(&top.2).is_none_or(|v| top.0 != *v)
+        {
+            self.heap.pop();
+        }
+        let top = self.heap.pop().unwrap_or_default();
+        let v = self.map.get_mut(&top.2).unwrap();
+        *v -= 1;
+        if *v == 0 {
+            self.map.remove(&top.2);
+        }
+        top.2
+    }
 }
 
 #[cfg(test)]
