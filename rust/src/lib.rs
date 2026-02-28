a740a985c675a0dce4ab4f773cdbdf0a7384981a
ZW <ZW_08@outlook.com>
2026-02-28 11:57:44 -0500

    3855 bwc177 q4; geometric seq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,36 +9,41 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn make_parity_alternating(nums: Vec<i32>) -> Vec<i32> {
-    let a = f(&nums, 0);
-    let b = f(&nums, 1);
-    a.min(b)
+pub fn sum_of_numbers(l: i32, r: i32, k: i32) -> i32 {
+    let [l, r, k] = [l, r, k].map(i64::from);
+    let count = r + 1 - l;
+    let sum = (l + r) * count / 2;
+    // total_number = pow(count, k)
+    // For each digit, d_count = pow(count, k)/count
+    let d_count = mod_pow(count, k - 1);
+    // For each digit position, digit_sum = sum * d_count * pow(10, p)
+    // total_sum = (sum * d_count)* (pow(10, 0)+pow(10, 1)+...pow(10, k-1))
+    //           = sum * d_count * (pow(10, k) - 1) / 9
+    let pow10 = mod_pow(10, k);
+    let gsum = (pow10 - 1) * mod_pow(9, M - 2) % M;
+    (sum * d_count % M * gsum % M) as i32
 }
 
-fn f(nums: &[i32], mut parity: i32) -> Vec<i32> {
-    let mut res = Vec::with_capacity(nums.len());
-    let mut min = *nums.iter().min().unwrap();
-    let mut max = *nums.iter().max().unwrap();
-    let mut count = 0;
-    for &num in nums.iter() {
-        if num & 1 == parity {
-            res.push(num);
-            min = min.min(num);
-            max = max.max(num);
-        } else {
-            if num <= min {
-                res.push(1 + num);
-            } else if num >= max {
-                res.push(num - 1);
-            }
-            count += 1;
-        }
-        parity ^= 1;
+const M: i64 = 1_000_000_007;
+
+const fn mod_pow(b: i64, p: i64) -> i64 {
+    if p == 0 {
+        return 1;
     }
-    min = *res.iter().min().unwrap();
-    max = *res.iter().max().unwrap();
-    vec![count, max - min]
+    if p & 1 == 0 {
+        mod_pow(b * b % M, p >> 1)
+    } else {
+        mod_pow(b * b % M, p >> 1) * b % M
+    }
 }
+
+// pow 0,1,2
+// 1 1 1
+// 1 1 2
+// 1 2 1
+// 1 2 2
+// 2 1 1
+// 2 2 1
 
 #[cfg(test)]
 mod tests {
@@ -70,8 +75,14 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(sum_of_numbers(1, 2, 2), 66);
+        assert_eq!(sum_of_numbers(0, 1, 3), 444);
+        assert_eq!(sum_of_numbers(5, 5, 10), 555555520);
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(sum_of_numbers(5, 5, 8299924), 170388343);
+    }
 }
