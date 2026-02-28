923a54a813beee65721fcb719e0f27e5c44566c3
ZW <ZW_08@outlook.com>
2026-02-28 09:36:16 -0500

    bwc177 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,45 +9,18 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn solve(n: i32) -> i32 {
-    (1..=n).fold(0, |acc, num| {
-        ((acc << (32 - num.leading_zeros())) + i64::from(num)) % M
-    }) as i32
-}
-
-pub fn concatenated_binary(n: i32) -> i32 {
-    let mut res = 1;
-    for num in 2..=n {
-        let width = 1 + num.ilog2();
-        res = res * mod_pow(2, width as _) % M;
-        res = (res + f(num.into())) % M
+pub fn min_distinct_freq_pair(mut nums: Vec<i32>) -> Vec<i32> {
+    use itertools::Itertools;
+    nums.sort_unstable();
+    let chunks = nums.chunk_by(|a, b| a == b).collect_vec();
+    for (i, ch1) in chunks.iter().enumerate() {
+        for ch2 in chunks.iter().skip(1 + i) {
+            if ch1.len() != ch2.len() {
+                return vec![ch1[0], ch2[0]];
+            }
+        }
     }
-    res as i32
-}
-
-const M: i64 = 1_000_000_007;
-
-const fn f(mut num: i64) -> i64 {
-    let mut res = 0;
-    let mut pow = 0;
-    while num > 0 {
-        let bit = num & 1;
-        num >>= 1;
-        res = (res + bit * mod_pow(2, pow)) % M;
-        pow += 1;
-    }
-    res
-}
-
-const fn mod_pow(b: i64, p: i64) -> i64 {
-    if p == 0 {
-        return 1;
-    }
-    if p & 1 == 0 {
-        mod_pow(b * b % M, p >> 1)
-    } else {
-        mod_pow(b * b % M, p >> 1) * b % M
-    }
+    vec![-1, -1]
 }
 
 #[cfg(test)]
