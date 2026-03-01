3823a215d962e08833c82a07ffc47c4b47bea43c
ZW <ZW_08@outlook.com>
2026-03-01 08:42:54 -0500

    3857 wc491 q2; recursion

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,13 +9,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn trim_trailing_vowels(s: String) -> String {
-    let mut s = s.into_bytes();
-    while s.last().is_some_and(|v| b"aeiou".contains(v)) {
-        s.pop();
-    }
-    String::from_utf8(s).unwrap()
+pub fn min_cost(n: i32) -> i32 {
+    f(n)
 }
+
+const fn f(n: i32) -> i32 {
+    if n <= 2 {
+        return n - 1;
+    }
+    if n == 3 {
+        return 3;
+    }
+    let a = n / 2;
+    let b = n - n / 2;
+    a * b + f(a) + f(b)
+}
+
+// 4
+// 2 2
+// 1 1 1 1
+//
+// 5
+// 2 3        6
+// 1 1 1 2    3
+//      1 1   1
+// 1 4        4
 
 #[cfg(test)]
 mod tests {
