66f8e83460aceff1b50d2dc941cda1d37eb79f51
ZW <ZW_08@outlook.com>
2026-03-01 12:06:38 -0500

    3859 dual sliding window

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,26 +9,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_or(grid: &[&[i32]]) -> i32 {
+pub fn count_subarrays(nums: &[i32], k: i32, m: i32) -> i64 {
+    use std::collections::HashMap;
     let mut res = 0;
-    // Greedily ignore these bits
-    let mut discard = 0;
-    'out: for bit in (0..=17).rev() {
-        // Attempt to turn this bit off
-        let temp = discard | (1 << bit);
-        for row in grid.iter() {
-            // Test against all previous ignored bits
-            if row.iter().all(|&v| v & temp != 0) {
-                // Fail to turn it of
-                // Record it in `res`
-                res |= 1 << bit;
-                continue 'out;
+    let [mut unique_map, mut valid_map] = [HashMap::new(), HashMap::new()];
+    let [mut u_left, mut v_left] = [0, 0];
+    let mut valid = 0;
+    for (_right, &num) in nums.iter().enumerate() {
+        // Window [u_left..=right] has k elements
+        *unique_map.entry(num).or_insert(0) += 1;
+        while unique_map.len() > k as usize {
+            let f = unique_map.entry(nums[u_left]).or_insert(0);
+            *f -= 1;
+            if *f == 0 {
+                unique_map.remove(&nums[u_left]);
             }
+            u_left += 1;
         }
-        // Turn this `bit` off
-        discard |= 1 << bit;
+
+        let f = valid_map.entry(num).or_insert(0);
+        *f += 1;
+        if *f == m {
+            valid += 1; // cross the valid boundary
+        }
+        // Window [v_left-1 ..=right] has k "valid" numbers
+        while valid >= k {
+            let f = valid_map.entry(nums[v_left]).or_insert(0);
+            if *f == m {
+                valid -= 1; // cross the valid boundary
+            }
+            *f -= 1;
+            if *f == 0 {
+                valid_map.remove(&nums[v_left]);
+            }
+            v_left += 1;
+        }
+        // [u_left..vleft] are left boundaries for valid window
+        res += v_left.saturating_sub(u_left);
     }
-    res
+    res as i64
 }
 
 #[cfg(test)]
@@ -62,13 +81,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(minimum_or(&[&[1, 5], &[2, 4]]), 3);
-        assert_eq!(minimum_or(&[&[3, 5], &[6, 4]]), 5);
-        assert_eq!(minimum_or(&[&[7, 9, 8]]), 7);
+        assert_eq!(count_subarrays(&[1, 2, 1, 2, 2], 2, 2), 2);
+        assert_eq!(count_subarrays(&[3, 1, 2, 4], 2, 1), 3);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(minimum_or(&[&[14, 7]]), 7);
-    }
+    fn test() {}
 }
