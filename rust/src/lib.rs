147978b2bf7fbe764850527fbb389acce80620ba
ZW <ZW_08@outlook.com>
2026-03-01 20:14:13 -0500

    1536 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,46 +8,27 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
 
-pub fn count_subarrays(nums: &[i32], k: i32, m: i32) -> i64 {
-    use std::collections::HashMap;
+pub fn min_swaps(grid: Vec<Vec<i32>>) -> i32 {
+    let n = grid.len();
+    let mut zeros = grid
+        .iter()
+        .map(|row| row.iter().rev().take_while(|&&v| v == 0).count())
+        .collect_vec();
     let mut res = 0;
-    let [mut unique_map, mut valid_map] = [HashMap::new(), HashMap::new()];
-    let [mut u_left, mut v_left] = [0, 0];
-    let mut valid = 0;
-    for (_right, &num) in nums.iter().enumerate() {
-        // Window [u_left..=right] has k elements
-        *unique_map.entry(num).or_insert(0) += 1;
-        while unique_map.len() > k as usize {
-            let f = unique_map.entry(nums[u_left]).or_insert(0);
-            *f -= 1;
-            if *f == 0 {
-                unique_map.remove(&nums[u_left]);
-            }
-            u_left += 1;
+    for left in 0..n {
+        let target = n - left - 1;
+        if zeros[left] >= target {
+            continue;
         }
-
-        let f = valid_map.entry(num).or_insert(0);
-        *f += 1;
-        if *f == m {
-            valid += 1; // cross the valid boundary
-        }
-        // Window [v_left-1 ..=right] has k "valid" numbers
-        while valid >= k {
-            let f = valid_map.entry(nums[v_left]).or_insert(0);
-            if *f == m {
-                valid -= 1; // cross the valid boundary
-            }
-            *f -= 1;
-            if *f == 0 {
-                valid_map.remove(&nums[v_left]);
-            }
-            v_left += 1;
-        }
-        // [u_left..vleft] are left boundaries for valid window
-        res += v_left.saturating_sub(u_left);
+        let Some(right) = zeros[1 + left..].iter().position(|&v| v >= target) else {
+            return -1;
+        };
+        res += 1 + right;
+        zeros[left..=(1 + left + right)].rotate_right(1);
     }
-    res as i64
+    res as i32
 }
 
 #[cfg(test)]
@@ -80,10 +61,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(count_subarrays(&[1, 2, 1, 2, 2], 2, 2), 2);
-        assert_eq!(count_subarrays(&[3, 1, 2, 4], 2, 1), 3);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
