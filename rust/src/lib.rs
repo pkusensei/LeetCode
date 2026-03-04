7fa0143cf9cfe7a38a44092b98837fc761f74470
ZW <ZW_08@outlook.com>
2026-03-04 12:35:46 -0500

    910

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,33 +9,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn sum_subarray_mins(arr: Vec<i32>) -> i32 {
-    const M: i64 = 1_000_000_007;
-    let mut st = Vec::<(usize, i64)>::new();
-    // sum(subarr_min) at `i`
-    let mut dp = vec![0; arr.len()];
-    for (idx, &num) in arr.iter().enumerate() {
-        let num = i64::from(num);
-        while let Some(top) = st.last()
-            && top.1 > num
-        {
-            st.pop();
-        }
-        if let Some(top) = st.last() {
-            // For all subarr ending on `idx`, i.e left..=idx
-            // Since [top]<=[idx]
-            // if left<=top.0, min(subarr) = [top], stored in dp[top]
-            // if top.0<left, min(subarr)=[idx], sum = (idx-top.0)*[idx]
-            // Thus all subarrs ending on `idx` is
-            let curr = (idx - top.0) as i64 * num % M;
-            dp[idx] = (curr + dp[top.0]) % M;
-        } else {
-            // For all 0..=idx, min(subarr) = [idx]
-            dp[idx] = (1 + idx) as i64 * num % M;
-        }
-        st.push((idx, num));
+pub fn smallest_range_ii(mut nums: Vec<i32>, k: i32) -> i32 {
+    let n = nums.len();
+    nums.sort_unstable();
+    let mut res = nums[n - 1] - nums[0];
+    for i in 1..n {
+        let min = (nums[0] + k).min(nums[i] - k);
+        let max = (nums[i - 1] + k).max(nums[n - 1] - k);
+        res = res.min(max - min);
     }
-    dp.iter().fold(0, |acc, v| (acc + v) % M) as i32
+    res
 }
 
 #[cfg(test)]
