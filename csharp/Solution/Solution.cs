879109e4b5c1149de8e7ca356c70744bd774b73a
Zx Wang <ZW_08@outlook.com>
2026-03-11 18:28:29 -0400

    926 prefix and suffix array

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,74 +8,20 @@ namespace Solution;
 
 public class Solution
 {
-    public int MinMalwareSpread(int[][] graph, int[] initial)
+    public int MinFlipsMonoIncr(string s)
     {
-        int n = graph.Length;
-        DSU dsu = new(n);
-        for (int i1 = 0; i1 < n; i1++)
+        int n = s.Length;
+        List<int> pref1 = new(n);
+        foreach (var item in s)
         {
-            for (int i2 = 1 + i1; i2 < n; i2++)
-            {
-                if (graph[i1][i2] == 1) { dsu.Union(i1, i2); }
-            }
+            pref1.Add((item == '1' ? 1 : 0) + pref1.LastOrDefault());
         }
-        Dictionary<int, List<int>> roots = [];
-        foreach (int i in initial)
+        List<int> suf0 = new(n);
+        foreach (var item in s.Reverse())
         {
-            int root = dsu.Find(i);
-            if (!roots.TryAdd(root, [i])) { roots[root].Add(i); }
+            suf0.Add((item == '0' ? 1 : 0) + suf0.LastOrDefault());
         }
-        int res = n;
-        int max_size = 0;
-        foreach (var (root, nodes) in roots)
-        {
-            if (nodes.Count > 1) { continue; }
-            int size = dsu.Size[root];
-            if (size > max_size)
-            {
-                max_size = size;
-                res = nodes[0];
-            }
-            else if (size == max_size)
-            {
-                res = int.Min(res, nodes[0]);
-            }
-        }
-        return res == n ? initial.Min() : res;
-    }
-}
-
-readonly struct DSU
-{
-    public int[] Parent { get; }
-    public int[] Size { get; }
-
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)];
-        Size = [.. Enumerable.Repeat(1, n)];
-    }
-
-    public readonly int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public readonly void Union(int x, int y)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry) { return; }
-        if (Size[rx] < Size[ry])
-        {
-            Size[ry] += Size[rx];
-            Parent[rx] = ry;
-        }
-        else
-        {
-            Size[rx] += Size[ry];
-            Parent[ry] = rx;
-        }
+        suf0.Reverse();
+        return pref1.Zip(suf0).Select(p => p.First + p.Second - 1).Min();
     }
 }
