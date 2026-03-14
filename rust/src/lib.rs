31df9c2734fdb478ac2b86ee04988c057aa0f193
ZW <ZW_08@outlook.com>
2026-03-13 20:40:33 -0400

    1415 backtrack, or walk down the tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,38 +9,66 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn with_slide(nums: &[i32], goal: i32) -> i32 {
-    at_most(nums, goal) - at_most(nums, goal - 1)
+pub fn solve(n: i32, mut k: i32) -> String {
+    k -= 1;
+    let mut res = vec![];
+    let mut subtree = 1 << (n - 1);
+    if k >= 3 * subtree {
+        return "".to_owned();
+    }
+    let mut val = match k / subtree {
+        0 => b'a',
+        1 => b'b',
+        _ => b'c',
+    };
+    res.push(val);
+    k %= subtree;
+    while res.len() < n as usize {
+        subtree >>= 1;
+        val = if k / subtree == 0 {
+            match *res.last().unwrap() {
+                b'a' => b'b',
+                b'b' => b'a',
+                b'c' => b'a',
+                _ => unreachable!(),
+            }
+        } else {
+            match *res.last().unwrap() {
+                b'a' => b'c',
+                b'b' => b'c',
+                b'c' => b'b',
+                _ => unreachable!(),
+            }
+        };
+        res.push(val);
+        k %= subtree;
+    }
+    String::from_utf8(res).unwrap()
 }
 
-fn at_most(nums: &[i32], goal: i32) -> i32 {
-    let mut res = 0;
-    let mut left = 0;
-    let mut sum = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        sum += num;
-        while left <= right && sum > goal {
-            sum -= nums[left];
-            left += 1;
-        }
-        res += 1 + right - left;
-    }
-    res as i32
+pub fn get_happy_string(n: i32, mut k: i32) -> String {
+    let n = n as usize;
+    let mut res = Vec::with_capacity(n);
+    backtrack(n, &mut k, &mut res);
+    String::from_utf8(res).unwrap()
 }
 
-pub fn num_subarrays_with_sum(nums: Vec<i32>, goal: i32) -> i32 {
-    use std::collections::HashMap;
-    let mut map = HashMap::from([(0, 1)]);
-    let mut res = 0;
-    let mut sum = 0;
-    for &num in nums.iter() {
-        sum += num;
-        if let Some(f) = map.get(&(sum - goal)) {
-            res += f;
-        }
-        *map.entry(sum).or_insert(0) += 1;
+fn backtrack(n: usize, k: &mut i32, res: &mut Vec<u8>) -> bool {
+    if res.len() == n {
+        *k -= 1;
+        return *k == 0;
     }
-    res
+    for &b in b"abc" {
+        if res.last().is_some_and(|&v| v == b) {
+            continue;
+        }
+        res.push(b);
+        if backtrack(n, k, res) {
+            return true;
+        }
+        res.pop();
+    }
+    false
 }
 
 #[cfg(test)]
