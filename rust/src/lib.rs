e298a72d075a01a8d8f3c18bceacd2b6277ad0e8
ZW <ZW_08@outlook.com>
2026-03-14 10:32:47 -0400

    bwc178 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,66 +9,15 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn solve(n: i32, mut k: i32) -> String {
-    k -= 1;
-    let mut res = vec![];
-    let mut subtree = 1 << (n - 1);
-    if k >= 3 * subtree {
-        return "".to_owned();
-    }
-    let mut val = match k / subtree {
-        0 => b'a',
-        1 => b'b',
-        _ => b'c',
-    };
-    res.push(val);
-    k %= subtree;
-    while res.len() < n as usize {
-        subtree >>= 1;
-        val = if k / subtree == 0 {
-            match *res.last().unwrap() {
-                b'a' => b'b',
-                b'b' => b'a',
-                b'c' => b'a',
-                _ => unreachable!(),
-            }
-        } else {
-            match *res.last().unwrap() {
-                b'a' => b'c',
-                b'b' => b'c',
-                b'c' => b'b',
-                _ => unreachable!(),
-            }
-        };
-        res.push(val);
-        k %= subtree;
-    }
-    String::from_utf8(res).unwrap()
-}
-
-pub fn get_happy_string(n: i32, mut k: i32) -> String {
-    let n = n as usize;
-    let mut res = Vec::with_capacity(n);
-    backtrack(n, &mut k, &mut res);
-    String::from_utf8(res).unwrap()
-}
-
-fn backtrack(n: usize, k: &mut i32, res: &mut Vec<u8>) -> bool {
-    if res.len() == n {
-        *k -= 1;
-        return *k == 0;
-    }
-    for &b in b"abc" {
-        if res.last().is_some_and(|&v| v == b) {
-            continue;
+pub fn first_unique_even(nums: Vec<i32>) -> i32 {
+    use itertools::Itertools;
+    let map = nums.iter().copied().filter(|&v| v & 1 == 0).counts();
+    for &num in nums.iter() {
+        if map.get(&num).is_some_and(|&f| f == 1) {
+            return num;
         }
-        res.push(b);
-        if backtrack(n, k, res) {
-            return true;
-        }
-        res.pop();
     }
-    false
+    -1
 }
 
 #[cfg(test)]
