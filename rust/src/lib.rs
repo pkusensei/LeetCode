769c5eb5077c4107a66a11e2ea109e59ca0957de
ZW <ZW_08@outlook.com>
2026-03-19 20:36:01 -0400

    3567

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,29 +9,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-// Suppose s = "aba"
-// idx => dp[1+idx]
-// idx = 1, previous seqs are [], "a"
-//          Without 'b' => [], "a"
-//          With 'b' => "b", "ab"
-// idx = 2, [], "a", "b", "ab"
-//          "a", "aa", "ba", "aba"
-//          Remove dupe "a" => Remove any seq ending with 'a', dp[index('a')]
-pub fn distinct_subseq_ii(s: &str) -> i32 {
-    const M: i32 = 1_000_000_007;
-    let n = s.len();
-    let mut prev = [None::<usize>; 26];
-    let mut dp = vec![0_i32; 1 + n];
-    dp[0] = 1; // empty seq
-    for (idx, b) in s.bytes().enumerate() {
-        let bi = usize::from(b - b'a');
-        dp[1 + idx] = 2 * dp[idx] % M;
-        if let Some(v) = prev[bi] {
-            dp[1 + idx] = (dp[1 + idx] - dp[v]).rem_euclid(M);
-        }
-        prev[bi] = Some(idx);
+pub fn min_abs_diff(grid: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+    let k = k as usize;
+    let [rows, cols] = get_dimensions(&grid);
+    if k == 0 {
+        return vec![vec![0; cols]; rows];
     }
-    (dp[n] - 1).rem_euclid(M)
+    let mut res = vec![];
+    for r in 0..=rows - k {
+        let mut curr = vec![];
+        for c in 0..=cols - k {
+            let mut mat = vec![];
+            for i in 0..k {
+                mat.extend_from_slice(&grid[r + i][c..c + k]);
+            }
+            mat.sort_unstable();
+            mat.dedup();
+            curr.push(mat.windows(2).map(|w| w[1] - w[0]).min().unwrap_or(0));
+        }
+        res.push(curr);
+    }
+    res
 }
 
 #[cfg(test)]
