caeb67eacfd1763857192eb1ada6989b420262f2
ZW <ZW_08@outlook.com>
2026-03-22 09:06:32 -0400

    wc494 q2; greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -10,6 +10,33 @@ mod trie;
 use helper::*;
 
 pub fn uniform_array(nums1: Vec<i32>) -> bool {
+    let [mut min_odd, mut min_even] = [i32::MAX; 2];
+    for &num in nums1.iter() {
+        if num & 1 == 1 && min_odd > num {
+            min_odd = num;
+        }
+        if num & 1 == 0 && min_even > num {
+            min_even = num;
+        }
+    }
+    let min = min_odd.min(min_even);
+    let par = min & 1;
+    for &num in nums1.iter() {
+        if num & 1 == par {
+            continue;
+        }
+        if par == 1 {
+            // num is even
+            if num < min_odd {
+                return false;
+            }
+        } else {
+            // num is odd
+            if num - min_odd < 2 {
+                return false;
+            }
+        }
+    }
     true
 }
 
