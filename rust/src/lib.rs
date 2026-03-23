07af28b675c860b0d03d5acd5650aebab5f8d01c
ZW <ZW_08@outlook.com>
2026-03-22 20:24:44 -0400

    1594 dp on max and min

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,42 +6,37 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-fn build_subseqs(nums: &[i32]) -> Vec<i32> {
-    fn f_acc(acc: i32, num: i32) -> i32 {
-        todo!() // stub
-    }
-
-    let n = nums.len();
-    let full_mask = 1 << n;
-    let mut res = Vec::with_capacity(full_mask);
-    res.push(0); // identity
-    for mask in 1..full_mask {
-        let lsb = mask & mask.wrapping_neg();
-        let curr = f_acc(res[mask ^ lsb], nums[lsb.ilog2() as usize]);
-        res.push(curr);
-    }
-    res
-}
-
-pub fn min_removals(nums: &[i32], target: i32) -> i32 {
-    // (sub_or, min_len)
-    let mut prev = HashMap::from([(0, 0)]);
-    for &num in nums.iter() {
-        let mut curr = prev.clone();
-        for (v, len) in prev {
-            let val = v ^ num;
-            let temp = curr.entry(val).or_insert(1 + len);
-            *temp = (*temp).min(1 + len);
+pub fn max_product_path(grid: Vec<Vec<i32>>) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let mut dp_max = vec![vec![i64::MIN; cols]; rows];
+    let mut dp_min = vec![vec![i64::MAX; cols]; rows];
+    dp_max[0][0] = grid[0][0].into();
+    dp_min[0][0] = grid[0][0].into();
+    for r in 0..rows {
+        for c in 0..cols {
+            let curr = i64::from(grid[r][c]);
+            if r > 0 {
+                dp_max[r][c] = dp_max[r][c]
+                    .max(curr * dp_max[r - 1][c])
+                    .max(curr * dp_min[r - 1][c]);
+                dp_min[r][c] = dp_min[r][c]
+                    .min(curr * dp_max[r - 1][c])
+                    .min(curr * dp_min[r - 1][c]);
+            }
+            if c > 0 {
+                dp_max[r][c] = dp_max[r][c]
+                    .max(curr * dp_max[r][c - 1])
+                    .max(curr * dp_min[r][c - 1]);
+                dp_min[r][c] = dp_min[r][c]
+                    .min(curr * dp_max[r][c - 1])
+                    .min(curr * dp_min[r][c - 1]);
+            }
         }
-        prev = curr
     }
-    let xor = nums.iter().fold(target, |acc, v| acc ^ v);
-    *prev.get(&xor).unwrap_or(&-1)
+    (dp_max[rows - 1][cols - 1] % 1_000_000_007).max(-1) as i32
 }
 
 #[cfg(test)]
@@ -74,9 +69,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_removals(&[7], 7), 0);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
