0b97f568f0381ea2b8bbde950bf7b3b6f32c1bad
Zx Wang <ZW_08@outlook.com>
2026-03-25 17:16:15 -0400

    948 sort, greedy, two ptrs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,62 +8,35 @@ namespace Solution;
 
 public class Solution
 {
-    public int RemoveStones(int[][] stones)
+    public int BagOfTokensScore(int[] tokens, int power)
     {
-        int n = stones.Length;
-        int[] x_i = [.. Enumerable.Repeat(10_001, 10_001)];
-        int[] y_i = [.. Enumerable.Repeat(10_001, 10_001)];
-        DSU dsu = new(n);
-        for (int i = 0; i < n; i++)
+        int n = tokens.Length;
+        if (n == 0) { return 0; }
+        Array.Sort(tokens);
+        int left = 0;
+        int right = n - 1;
+        int score = 0;
+        int res = 0;
+        while (left <= right)
         {
-            int x = stones[i][0];
-            int y = stones[i][1];
-            x_i[x] = int.Min(i, x_i[x]);
-            dsu.Union(x_i[x], i);
-            y_i[y] = int.Min(i, y_i[y]);
-            dsu.Union(y_i[y], i);
+            bool moved = false;
+            while (left <= right && power >= tokens[left])
+            {
+                power -= tokens[left];
+                score += 1;
+                left += 1;
+                moved = true;
+            }
+            res = int.Max(res, score);
+            if (left <= right && score > 0)
+            {
+                score -= 1;
+                power += tokens[right];
+                right -= 1;
+                moved = true;
+            }
+            if (!moved) { break; }
         }
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            int root = dsu.Find(i);
-            arr[root] = dsu.Size[root];
-        }
-        return arr.Select(v => int.Max(v - 1, 0)).Sum();
-    }
-}
-
-readonly struct DSU
-{
-    public DSU(int n)
-    {
-        Parent = [.. Enumerable.Range(0, n)];
-        Size = [.. Enumerable.Repeat(1, n)];
-    }
-
-    public int[] Parent { get; }
-    public int[] Size { get; }
-
-    public int Find(int v)
-    {
-        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
-        return Parent[v];
-    }
-
-    public void Union(int x, int y)
-    {
-        int rx = Find(x);
-        int ry = Find(y);
-        if (rx == ry) { return; }
-        if (Size[rx] < Size[ry])
-        {
-            Size[ry] += Size[rx];
-            Parent[rx] = ry;
-        }
-        else
-        {
-            Size[rx] += Size[ry];
-            Parent[ry] = rx;
-        }
+        return res;
     }
 }
