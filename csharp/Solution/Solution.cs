ee0a7d00a14233b1a727449e989f7b4328153826
Zx Wang <ZW_08@outlook.com>
2026-03-25 16:50:27 -0400

    947 dsu

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,20 +8,62 @@ namespace Solution;
 
 public class Solution
 {
-    public bool ValidateStackSequences(int[] pushed, int[] popped)
+    public int RemoveStones(int[][] stones)
     {
-        int n = pushed.Length;
-        Stack<int> st = [];
-        int i = 0;
-        foreach (var item in pushed)
+        int n = stones.Length;
+        int[] x_i = [.. Enumerable.Repeat(10_001, 10_001)];
+        int[] y_i = [.. Enumerable.Repeat(10_001, 10_001)];
+        DSU dsu = new(n);
+        for (int i = 0; i < n; i++)
         {
-            st.Push(item);
-            while (st.TryPeek(out int top) && top == popped[i])
-            {
-                st.Pop();
-                i += 1;
-            }
+            int x = stones[i][0];
+            int y = stones[i][1];
+            x_i[x] = int.Min(i, x_i[x]);
+            dsu.Union(x_i[x], i);
+            y_i[y] = int.Min(i, y_i[y]);
+            dsu.Union(y_i[y], i);
         }
-        return st.Count == 0 && i == n;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int root = dsu.Find(i);
+            arr[root] = dsu.Size[root];
+        }
+        return arr.Select(v => int.Max(v - 1, 0)).Sum();
+    }
+}
+
+readonly struct DSU
+{
+    public DSU(int n)
+    {
+        Parent = [.. Enumerable.Range(0, n)];
+        Size = [.. Enumerable.Repeat(1, n)];
+    }
+
+    public int[] Parent { get; }
+    public int[] Size { get; }
+
+    public int Find(int v)
+    {
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Size[rx] < Size[ry])
+        {
+            Size[ry] += Size[rx];
+            Parent[rx] = ry;
+        }
+        else
+        {
+            Size[rx] += Size[ry];
+            Parent[ry] = rx;
+        }
     }
 }
