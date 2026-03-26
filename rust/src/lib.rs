7f2e5bab28109166b6deafedd9777c9806388cbf
ZW <ZW_08@outlook.com>
2026-03-26 15:30:01 -0400

    959 dsu, expand to 4

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,20 +9,74 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn tallest_billboard(rods: &[i32]) -> i32 {
-    use std::collections::HashMap;
-    let mut prev = HashMap::from([(0, 0)]);
-    for &num in rods.iter() {
-        let mut curr = prev.clone(); // skip current
-        for (diff, v) in prev {
-            let temp = curr.entry(diff + num).or_insert(v + num);
-            *temp = (*temp).max(v + num);
-            let temp = curr.entry((diff - num).abs()).or_insert(v + num);
-            *temp = (*temp).max(v + num);
+//   0
+// 1 x 2
+//   3
+pub fn regions_by_slashes(grid: Vec<String>) -> i32 {
+    use itertools::Itertools;
+    let n = grid.len();
+    let mut dsu = DSU::new(4 * n * n);
+    for (r, s) in grid.iter().enumerate() {
+        for (c, b) in s.bytes().enumerate() {
+            let i = (r * n + c) * 4;
+            match b {
+                b'/' => {
+                    dsu.union(i, i + 1);
+                    dsu.union(i + 2, i + 3);
+                }
+                b'\\' => {
+                    dsu.union(i, i + 2);
+                    dsu.union(i + 1, i + 3);
+                }
+                _ => {
+                    dsu.union(i, i + 1);
+                    dsu.union(i, i + 2);
+                    dsu.union(i, i + 3);
+                }
+            }
+            if r > 0 {
+                dsu.union(((r - 1) * n + c) * 4 + 3, i);
+            }
+            if c > 0 {
+                dsu.union((r * n + c - 1) * 4 + 2, i + 1);
+            }
         }
-        prev = curr;
     }
-    prev.get(&0).map(|v| v / 2).unwrap_or(0)
+    (0..4 * n * n).map(|v| dsu.find(v)).unique().count() as i32
+}
+
+struct DSU {
+    parent: Vec<usize>,
+    rank: Vec<i32>,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, v: usize) -> usize {
+        if self.parent[v] != v {
+            self.parent[v] = self.find(self.parent[v])
+        }
+        self.parent[v]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return;
+        }
+        if self.rank[rx] < self.rank[ry] {
+            self.parent[rx] = ry
+        } else {
+            self.rank[rx] += 1;
+            self.parent[ry] = rx;
+        }
+    }
 }
 
 #[cfg(test)]
