391eb356e16c3f15f001be46221285699a27e498
ZW <ZW_08@outlook.com>
2026-04-01 20:45:37 -0400

    3418 3d dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,43 +9,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn survived_robots_healths(
-    positions: Vec<i32>,
-    healths: Vec<i32>,
-    directions: String,
-) -> Vec<i32> {
-    use itertools::{Itertools, izip};
-    // (pos, health, dir, idx)
-    let robs = izip!(positions.iter(), healths.iter(), directions.bytes())
-        .enumerate()
-        .map(|(i, (p, h, b))| (*p, *h, b, i))
-        .sorted_unstable()
-        .collect_vec();
-    // (pos, health, dir, idx)
-    let mut st = Vec::<(i32, i32, u8, usize)>::new();
-    'out: for rob in robs {
-        let mut rob = rob;
-        while let Some(top) = st.last()
-            && top.2 == b'R'
-            && rob.2 == b'L'
-        {
-            let mut top = *top;
-            st.pop();
-            match top.1.cmp(&rob.1) {
-                std::cmp::Ordering::Less => rob.1 -= 1,
-                std::cmp::Ordering::Equal => continue 'out,
-                std::cmp::Ordering::Greater => {
-                    top.1 -= 1;
-                    rob = top;
+pub fn maximum_amount(coins: Vec<Vec<i32>>) -> i32 {
+    let [rows, cols] = get_dimensions(&coins);
+    let mut dp = vec![vec![[i32::MIN >> 1; 3]; cols]; rows];
+    dp[0][0][0] = coins[0][0];
+    dp[0][0][1..].fill(coins[0][0].max(0));
+    for r in 0..rows {
+        for c in 0..cols {
+            let v = coins[r][c];
+            for i in 0..3 {
+                if r > 0 {
+                    dp[r][c][i] = dp[r][c][i].max(v + dp[r - 1][c][i]);
+                    if i > 0 {
+                        dp[r][c][i] = dp[r][c][i].max(dp[r - 1][c][i - 1]);
+                    }
                 }
-            };
+                if c > 0 {
+                    dp[r][c][i] = dp[r][c][i].max(v + dp[r][c - 1][i]);
+                    if i > 0 {
+                        dp[r][c][i] = dp[r][c][i].max(dp[r][c - 1][i - 1]);
+                    }
+                }
+            }
         }
-        st.push(rob);
     }
-    st.into_iter()
-        .sorted_unstable_by_key(|rob| rob.3)
-        .map(|r| r.1)
-        .collect()
+    *dp[rows - 1][cols - 1].iter().max().unwrap()
 }
 
 #[cfg(test)]
