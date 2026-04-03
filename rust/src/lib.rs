edc13a5716c9c3289a6f992c3afcdd91b258a180
ZW <ZW_08@outlook.com>
2026-04-02 20:57:28 -0400

    3661 dp one by one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,31 +9,55 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_amount(coins: Vec<Vec<i32>>) -> i32 {
-    let [rows, cols] = get_dimensions(&coins);
-    let mut dp = vec![vec![[i32::MIN >> 1; 3]; cols]; rows];
-    dp[0][0][0] = coins[0][0];
-    dp[0][0][1..].fill(coins[0][0].max(0));
-    for r in 0..rows {
-        for c in 0..cols {
-            let v = coins[r][c];
-            for i in 0..3 {
-                if r > 0 {
-                    dp[r][c][i] = dp[r][c][i].max(v + dp[r - 1][c][i]);
-                    if i > 0 {
-                        dp[r][c][i] = dp[r][c][i].max(dp[r - 1][c][i - 1]);
-                    }
-                }
-                if c > 0 {
-                    dp[r][c][i] = dp[r][c][i].max(v + dp[r][c - 1][i]);
-                    if i > 0 {
-                        dp[r][c][i] = dp[r][c][i].max(dp[r][c - 1][i - 1]);
-                    }
-                }
-            }
-        }
+pub fn max_walls(robots: Vec<i32>, distance: Vec<i32>, mut walls: Vec<i32>) -> i32 {
+    use itertools::{Itertools, izip};
+    let n = robots.len();
+    // (pos, dist)
+    let robs = izip!(robots.iter().copied(), distance.iter().copied())
+        .sorted_unstable()
+        .collect_vec();
+    walls.sort_unstable();
+    // dp[i][0] turn left
+    // dp[i][1] turn right
+    let mut prev_left = find(&walls, robs[0].0 - robs[0].1, robs[0].0);
+    let mut prev_right = find(
+        &walls,
+        robs[0].0,
+        (robs[0].0 + robs[0].1).min(robs.get(1).map(|r| r.0 - 1).unwrap_or(i32::MAX >> 1)),
+    );
+    for i in 1..n {
+        let (pos, dist) = robs[i];
+        let curr_left = (prev_left + find(&walls, (pos - dist).max(1 + robs[i - 1].0), pos)).max(
+            prev_right
+                + find(
+                    &walls,
+                    (pos - dist).max(robs[i - 1].0 + robs[i - 1].1 + 1),
+                    pos,
+                ),
+        );
+        let curr_right = (prev_left
+            + find(
+                &walls,
+                pos,
+                (pos + dist).min(robs.get(1 + i).map(|r| r.0 - 1).unwrap_or(i32::MAX >> 1)),
+            ))
+        .max(
+            prev_right
+                + find(
+                    &walls,
+                    pos,
+                    (pos + dist).min(robs.get(1 + i).map(|r| r.0 - 1).unwrap_or(i32::MAX >> 1)),
+                ),
+        );
+        (prev_left, prev_right) = (curr_left, curr_right);
     }
-    *dp[rows - 1][cols - 1].iter().max().unwrap()
+    prev_left.max(prev_right)
+}
+
+fn find(walls: &[i32], left: i32, right: i32) -> i32 {
+    let a = walls.partition_point(|&v| v < left);
+    let b = walls.partition_point(|&v| v <= right);
+    (b - a) as i32
 }
 
 #[cfg(test)]
