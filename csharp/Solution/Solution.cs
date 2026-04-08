1d8963d253063b9dd4cd6bae08da6977eba8d5a6
Zx Wang <ZW_08@outlook.com>
2026-04-08 17:28:16 -0400

    967 bfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,15 +8,30 @@ namespace Solution;
 
 public class Solution
 {
-    public bool IsUnivalTree(TreeNode root)
+    public int[] NumsSameConsecDiff(int n, int k)
     {
-        return Dfs(root, null);
-        
-        static bool Dfs(TreeNode node, int? val)
+        Queue<(int val, int len)> queue = [];
+        List<int> res = [];
+        for (int i = 1; i < 10; i++)
         {
-            if (node is null) { return true; }
-            val ??= node.val;
-            return node.val == val.Value && Dfs(node.left, val) && Dfs(node.right, val);
+            queue.Enqueue((i, 1));
         }
+        while (queue.TryDequeue(out var item))
+        {
+            if (item.len == n)
+            {
+                res.Add(item.val);
+                continue;
+            }
+            int prev = item.val % 10;
+            for (int d = 0; d < 10; d++)
+            {
+                if (int.Abs(prev - d) == k)
+                {
+                    queue.Enqueue((10 * item.val + d, 1 + item.len));
+                }
+            }
+        }
+        return [.. res];
     }
 }
