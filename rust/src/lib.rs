3ebac51d6430c4e729ff618a95ae4ebf78087206
ZW <ZW_08@outlook.com>
2026-04-08 21:29:21 -0400

    3655 sqrt decompositon

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,18 +9,62 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn xor_after_queries(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
-    const M: i64 = 1_000_000_007;
-    let mut nums: Vec<_> = nums.iter().map(|&v| i64::from(v)).collect();
+pub fn xor_after_queries(nums: &[i32], queries: &[[i32; 4]]) -> i32 {
+    let n = nums.len();
+    let root = 1 + n.isqrt();
+    let mut nums: Vec<_> = nums.iter().map(|v| i64::from(*v)).collect();
+    // diffs[k] diff array with step size k
+    let mut diffs = vec![vec![]; root];
     for q in queries {
         let [l, r, k, v] = q[..] else { unreachable!() };
-        let mut idx = l;
-        while idx <= r {
-            nums[idx as usize] = (nums[idx as usize] * i64::from(v)) % M;
-            idx += k;
+        let [left, right, k] = [l, r, k].map(|v| v as usize);
+        let v = i64::from(v);
+        if k < root {
+            let group = {
+                if diffs[k].is_empty() {
+                    diffs[k] = vec![1; n]; // init here to skip never-seen `k`s
+                }
+                &mut diffs[k]
+            };
+            // [left] *= v
+            group[left] = group[left] * v % M;
+            let right = right + k - (right - left) % k;
+            // [right] /= v
+            if let Some(v_) = group.get_mut(right) {
+                *v_ = (*v_) * mod_pow(v, M - 2) % M;
+            }
+        } else {
+            for idx in (left..=right).step_by(k) {
+                nums[idx] = nums[idx] * v % M;
+            }
+        }
+    }
+    for (k, diff) in diffs.iter().enumerate() {
+        if diff.is_empty() {
+            continue;
+        }
+        for start in 0..k {
+            let mut prefix = 1;
+            for i in (start..n).step_by(k) {
+                prefix = prefix * diff[i] % M;
+                nums[i] = nums[i] * prefix % M;
+            }
         }
     }
     nums.iter().fold(0, |acc, v| acc ^ v) as i32
+}
+
+const M: i64 = 1_000_000_007;
+
+const fn mod_pow(b: i64, exp: i64) -> i64 {
+    if exp == 0 {
+        return 1;
+    }
+    if exp & 1 == 0 {
+        mod_pow(b * b % M, exp >> 1)
+    } else {
+        mod_pow(b * b % M, exp >> 1) * b % M
+    }
 }
 
 #[cfg(test)]
