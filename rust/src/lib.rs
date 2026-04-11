8909e18de7d25958c3b7e8817954156500547ddb
ZW <ZW_08@outlook.com>
2026-04-11 10:46:42 -0400

    bwc180 q3; binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,19 +6,46 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::sync::LazyLock;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_digit_occurrences(nums: Vec<i32>, digit: i32) -> i32 {
-    nums.iter()
-        .map(|num| {
-            num.to_string()
-                .bytes()
-                .filter(|b| i32::from(b - b'0') == digit)
-                .count() as i32
-        })
-        .sum()
+pub fn min_operations(nums: Vec<i32>) -> i32 {
+    let mut res = 0;
+    for (idx, &num) in nums.iter().enumerate() {
+        if idx & 1 == 0 {
+            let i = P.partition_point(|&v| v < num);
+            res += P[i] - num;
+        } else {
+            if num == 2 {
+                res += 2
+            } else if P.binary_search(&num).is_ok() {
+                res += 1
+            }
+        }
+    }
+    res
 }
+
+const MAX: usize = 100_003;
+
+static P: LazyLock<Vec<i32>> = LazyLock::new(|| {
+    let mut sieve = vec![true; 1 + MAX];
+    sieve[..2].fill(false);
+    for p in 2..=MAX {
+        if sieve[p] {
+            for val in (p * p..=MAX).step_by(p) {
+                sieve[val] = false;
+            }
+        }
+    }
+    sieve
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &b)| if b { Some(i as i32) } else { None })
+        .collect()
+});
 
 #[cfg(test)]
 mod tests {
