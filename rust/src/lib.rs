305581ffd9b9164126633c4af655786d2bf994dd
ZW <ZW_08@outlook.com>
2026-04-12 20:09:55 -0400

    1848

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,38 +6,21 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_balanced(s: String) -> i32 {
-    let n = s.len();
-    let total1 = s.bytes().map(|b| i32::from(b - b'0')).sum::<i32>();
-    let total0 = n as i32 - total1;
-    let limit = 2 * total0.min(total1); // max possible length
-    // (sum, prev_indices)
-    let mut prefix = HashMap::from([(0, vec![-1])]);
-    let mut sum = 0;
-    let mut res = 0;
-    // 1 1 0 0 1
-    // 1 2 1 0 1
-    for (idx, b) in s.bytes().enumerate() {
-        sum += if b == b'1' { 1 } else { -1 };
-        if let Some(p) = prefix.get(&sum) {
-            res = res.max(idx as i32 - p[0]);
-        }
-        for target in [sum - 2, sum + 2] {
-            if let Some(prev) = prefix.get(&target) {
-                let i = prev.partition_point(|&v| v < idx as i32 - limit);
-                if i < prev.len() {
-                    res = res.max(idx as i32 - prev[i]);
-                }
+pub fn get_min_distance(nums: Vec<i32>, target: i32, start: i32) -> i32 {
+    nums.iter()
+        .enumerate()
+        .filter_map(|(i, &num)| {
+            if target == num {
+                Some((i as i32 - start).abs())
+            } else {
+                None
             }
-        }
-        prefix.entry(sum).or_default().push(idx as i32);
-    }
-    res.min(limit)
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
