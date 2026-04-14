8f0aff99017c21c730512b55e367896791e73881
ZW <ZW_08@outlook.com>
2026-04-14 11:10:24 -0400

    2463 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,21 +6,62 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::iter::repeat_n;
+
 #[allow(unused_imports)]
 use helper::*;
+use itertools::Itertools;
 
-pub fn get_min_distance(nums: Vec<i32>, target: i32, start: i32) -> i32 {
-    nums.iter()
-        .enumerate()
-        .filter_map(|(i, &num)| {
-            if target == num {
-                Some((i as i32 - start).abs())
-            } else {
-                None
-            }
-        })
-        .min()
-        .unwrap()
+pub fn minimum_total_distance(mut robot: Vec<i32>, mut factory: Vec<[i32; 2]>) -> i64 {
+    robot.sort_unstable();
+    factory.sort_unstable();
+    let facts = factory
+        .iter()
+        .flat_map(|f| repeat_n(f[0], f[1] as usize))
+        .collect_vec();
+    let (nr, nf) = (robot.len(), facts.len());
+    let mut dp = vec![vec![i64::MAX >> 2; 1 + nf]; 1 + nr];
+    // dp[nr].fill(0); // no robot left
+    // for i1 in (0..nr).rev() {
+    //     for i2 in (i1..nf).rev() {
+    //         dp[i1][i2] =
+    //             dp[i1][1 + i2].min(i64::from(robot[i1].abs_diff(facts[i2])) + dp[1 + i1][1 + i2]);
+    //     }
+    // }
+    // dp[0][0]
+    dp[0].fill(0);
+    for i1 in 0..nr {
+        for i2 in i1..nf {
+            dp[1 + i1][1 + i2] =
+                dp[1 + i1][i2].min(i64::from(robot[i1].abs_diff(facts[i2])) + dp[i1][i2]);
+        }
+    }
+    dp[nr][nf]
+    // dfs(&robot, &facts, nr, nf)
+}
+
+fn dfs1(robot: &[i32], facts: &[i32], i1: usize, i2: usize) -> i64 {
+    if i1 == robot.len() {
+        return 0;
+    }
+    if i2 == facts.len() {
+        return i64::MAX >> 2;
+    }
+    let skip = dfs(robot, facts, i1, i2 + 1);
+    let take = i64::from(robot[i1].abs_diff(facts[i2])) + dfs(robot, facts, i1 + 1, i2 + 1);
+    skip.min(take)
+}
+
+fn dfs(robot: &[i32], facts: &[i32], i1: usize, i2: usize) -> i64 {
+    if i1 == 0 {
+        return 0;
+    }
+    if i2 == 0 {
+        return i64::MAX >> 2;
+    }
+    let skip = dfs(robot, facts, i1, i2 - 1);
+    let take = i64::from(robot[i1 - 1].abs_diff(facts[i2 - 1])) + dfs(robot, facts, i1 - 1, i2 - 1);
+    skip.min(take)
 }
 
 #[cfg(test)]
@@ -56,5 +97,13 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            minimum_total_distance(
+                vec![9, 11, 99, 101],
+                vec![[10, 1], [7, 1], [14, 1], [100, 1], [96, 1], [103, 1]]
+            ),
+            6
+        );
+    }
 }
