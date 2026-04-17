b2c019bb82e2e711c32658f9226368638bde8c14
ZW <ZW_08@outlook.com>
2026-04-17 11:28:06 -0400

    974

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,69 +9,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn is_rational_equal(s: String, t: String) -> bool {
-    let Some((a, b)) = Rational::new(&s).zip(Rational::new(&t)) else {
-        return false;
-    };
-    a == b
-}
-
-#[derive(PartialEq, Eq)]
-struct Rational {
-    int: i32,
-    dec: Vec<u8>,
-}
-
-impl Rational {
-    fn new(s: &str) -> Option<Rational> {
-        if let Some((a, b)) = s.split_once('.') {
-            let int = a.parse().ok()?;
-            if b.is_empty() {
-                return Some(Self {
-                    int,
-                    dec: vec![b'0'; 8],
-                });
-            }
-            let mut it = b.split(['(', ')']);
-            let mut dec = it.next().map(|v| v.as_bytes().to_vec())?;
-            if let Some(repeat) = it.next() {
-                if repeat.bytes().all(|b| b == b'9') {
-                    if let Some(i) = dec.iter().rposition(|&b| b != b'9') {
-                        dec[i] += 1;
-                        dec[1 + i..].fill(b'0');
-                        while dec.len() < 8 {
-                            dec.push(b'0');
-                        }
-                        return Some(Self { int, dec });
-                    } else {
-                        return Some(Self {
-                            int: 1 + int,
-                            dec: vec![b'0'; 8],
-                        });
-                    }
-                }
-                for b in repeat.bytes().cycle() {
-                    if dec.len() < 8 {
-                        dec.push(b);
-                    } else {
-                        break;
-                    }
-                }
-                Some(Self { int, dec })
-            } else {
-                while dec.len() < 8 {
-                    dec.push(b'0');
-                }
-                Some(Self { int, dec })
-            }
-        } else {
-            let int = s.parse().ok()?;
-            Some(Self {
-                int,
-                dec: vec![b'0'; 8],
-            })
-        }
+pub fn subarrays_div_by_k(nums: Vec<i32>, k: i32) -> i32 {
+    use std::collections::HashMap;
+    let mut map = HashMap::from([(0, 1)]);
+    let mut sum = 0;
+    let mut res = 0;
+    for num in nums.iter() {
+        sum += num;
+        res += map.get(&sum.rem_euclid(k)).unwrap_or(&0);
+        *map.entry(sum.rem_euclid(k)).or_insert(0) += 1;
     }
+    res
 }
 
 #[cfg(test)]
