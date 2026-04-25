f219f1e49165c42e6c9e4677bf01ede26e010b4c
ZW <ZW_08@outlook.com>
2026-04-25 15:53:16 -0400

    bwc181 q4; binary search, prefix sum

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,55 +6,55 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::VecDeque;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn even_sum_subgraphs(nums: &[i32], edges: &[[i32; 2]]) -> i32 {
-    let n = nums.len();
-    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
-        acc[e[0] as usize].push(e[1] as usize);
-        acc[e[1] as usize].push(e[0] as usize);
+pub fn kth_remaining_integer(nums: &[i32], queries: &[[i32; 3]]) -> Vec<i32> {
+    let pref_even = nums.iter().fold(vec![], |mut acc, v| {
+        acc.push(i32::from(v & 1 == 0) + acc.last().unwrap_or(&0));
         acc
     });
-    let full = 1 << n;
-    let mut res = 0;
-    for mask in 1..full {
-        res += i32::from(f(&nums, &adj, mask))
+    // let mut next_even = vec![];
+    // let mut temp = None;
+    // for &num in nums.iter().rev() {
+    //     if num & 1 == 0 {
+    //         temp = Some(num)
+    //     }
+    //     next_even.push(temp);
+    // }
+    // next_even.reverse();
+    let mut res = vec![];
+    for q in queries.iter() {
+        let [left, right] = [0, 1].map(|i| q[i] as usize);
+        let k = q[2];
+        // let count = pref_even[right] - if left > 0 { pref_even[left - 1] } else { 0 };
+        // if count == 0 || next_even[left].is_some_and(|v| v > 2 * k) {
+        //     res.push(2 * k);
+        //     continue;
+        // }
+        let mut low = 1;
+        let mut high = i32::MAX >> 1;
+        while low < high {
+            let mid = low + (high - low) / 2;
+            if f(&nums, &pref_even, left, right, k, mid) {
+                high = mid
+            } else {
+                low = 1 + mid;
+            }
+        }
+        res.push(2 * low);
     }
     res
 }
 
-fn f(nums: &[i32], adj: &[Vec<usize>], mask: i32) -> bool {
-    let n = nums.len();
-    let mut queue = VecDeque::new();
-    let mut sum = 0;
-    let mut curr = 0;
-    for bit in 0..n {
-        if mask & (1 << bit) > 0 {
-            sum ^= nums[bit];
-            if queue.is_empty() {
-                curr |= 1 << bit;
-                queue.push_back(bit);
-            }
-        }
+fn f(nums: &[i32], pref_even: &[i32], left: usize, right: usize, k: i32, mid: i32) -> bool {
+    let i = nums.partition_point(|&v| v <= 2 * mid);
+    let mut even_count = 0;
+    if i > left {
+        let right = right.min(i - 1);
+        even_count = pref_even[right] - if left > 0 { pref_even[left - 1] } else { 0 };
     }
-    if sum & 1 == 1 {
-        return false;
-    }
-    while let Some(node) = queue.pop_front() {
-        if curr == mask as usize {
-            break;
-        }
-        for &next in &adj[node] {
-            if mask & (1 << next) > 0 && curr & (1 << next) == 0 {
-                curr |= 1 << next;
-                queue.push_back(next);
-            }
-        }
-    }
-    curr == mask as usize
+    mid - even_count >= k
 }
 
 #[cfg(test)]
@@ -88,7 +88,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(even_sum_subgraphs(&[1, 0, 1], &[[0, 1], [1, 2]]), 2);
+        assert_eq!(
+            kth_remaining_integer(&[1, 4, 7], &[[0, 2, 1], [1, 1, 2], [0, 0, 3]]),
+            [2, 6, 6]
+        );
     }
 
     #[test]
