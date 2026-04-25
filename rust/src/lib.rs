2e9d4e4cc7b18098d85e20cb9eabae6b5ad9496b
ZW <ZW_08@outlook.com>
2026-04-24 22:00:43 -0400

    3464 binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,17 +9,51 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn furthest_distance_from_origin(moves: String) -> i32 {
-    let mut res = 0_i32;
-    let mut count = 0;
-    for b in moves.bytes() {
-        match b {
-            b'L' => res += 1,
-            b'R' => res -= 1,
-            _ => count += 1,
+pub fn max_distance(side: i32, points: &[[i32; 2]], k: i32) -> i32 {
+    let side = i64::from(side);
+    let mut arr = vec![];
+    for p in points.iter() {
+        let [x, y] = [0, 1].map(|i| i64::from(p[i]));
+        if x == 0 {
+            arr.push(y); // left
+        } else if y == side {
+            arr.push(side + x); // top
+        } else if x == side {
+            arr.push(3 * side - y); // right
+        } else {
+            arr.push(4 * side - x); // bottom
         }
     }
-    res.abs() + count
+    arr.sort_unstable();
+    let mut left = 0;
+    let mut right = side;
+    while left < right {
+        let mid = left + (right - left + 1) / 2;
+        if f(&arr, side, k, mid) {
+            left = mid
+        } else {
+            right = mid - 1
+        }
+    }
+    left as i32
+}
+
+fn f(arr: &[i64], side: i64, k: i32, mid: i64) -> bool {
+    let total = 4 * side;
+    'out: for &start in arr {
+        // For each start, this is the farthest point on "1D" arr
+        let end = total + start - mid;
+        let mut curr = start;
+        for _ in 1..k {
+            let i = arr.partition_point(|&v| v < curr + mid);
+            if arr.get(i).is_none_or(|&v| v > end) {
+                continue 'out; // Impossible `start`; try next one
+            }
+            curr = arr[i];
+        }
+        return true;
+    }
+    false
 }
 
 #[cfg(test)]
