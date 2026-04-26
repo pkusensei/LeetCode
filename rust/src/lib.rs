6e581a9f4de6d1fb527d0fe870ffafd5eb83c857
ZW <ZW_08@outlook.com>
2026-04-26 08:40:49 -0400

    wc499 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,59 +9,27 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn contains_cycle(grid: Vec<Vec<char>>) -> bool {
-    let [rows, cols] = get_dimensions(&grid);
-    let mut dsu = DSU::new(rows * cols);
-    for (r, row) in grid.iter().enumerate() {
-        for (c, &val) in row.iter().enumerate() {
-            for [nr, nc] in [[r + 1, c], [r, 1 + c]] {
-                if grid
-                    .get(nr)
-                    .is_some_and(|rr| rr.get(nc).is_some_and(|&v| v == val))
-                    && dsu.union(r * cols + c, nr * cols + nc)
-                {
-                    return true;
-                }
-            }
+pub fn find_valid_elements(nums: &[i32]) -> Vec<i32> {
+    let n = nums.len();
+    let mut pref_max = nums.to_vec();
+    for i in 1..n {
+        pref_max[i] = pref_max[i].max(pref_max[i - 1])
+    }
+    let mut suf_max = nums.to_vec();
+    for i in (0..n - 1).rev() {
+        suf_max[i] = suf_max[i].max(suf_max[1 + i]);
+    }
+    let mut res = vec![];
+    for (i, &num) in nums.iter().enumerate() {
+        if i == 0 || i == n - 1 {
+            res.push(num);
+            continue;
+        }
+        if num > pref_max[i - 1] || num > suf_max[1 + i] {
+            res.push(num);
         }
     }
-    false
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    size: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            size: vec![1; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v])
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) -> bool {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            return self.size[rx] >= 4;
-        }
-        if self.size[rx] < self.size[ry] {
-            self.parent[rx] = ry;
-            self.size[ry] += self.size[rx];
-        } else {
-            self.parent[ry] = rx;
-            self.size[rx] += self.size[ry];
-        }
-        false
-    }
+    res
 }
 
 #[cfg(test)]
@@ -94,7 +62,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(find_valid_elements(&[2, 2, 1, 1, 2]), [2, 2]);
+    }
 
     #[test]
     fn test() {}
