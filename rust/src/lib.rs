5c42ffcb2e9bbe7ca98c75773988aef90f3f3c75
ZW <ZW_08@outlook.com>
2026-04-25 20:19:57 -0400

    1559 dsu

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,52 +9,59 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn kth_remaining_integer(nums: &[i32], queries: &[[i32; 3]]) -> Vec<i32> {
-    let pref_even = nums.iter().fold(vec![], |mut acc, v| {
-        acc.push(i32::from(v & 1 == 0) + acc.last().unwrap_or(&0));
-        acc
-    });
-    // let mut next_even = vec![];
-    // let mut temp = None;
-    // for &num in nums.iter().rev() {
-    //     if num & 1 == 0 {
-    //         temp = Some(num)
-    //     }
-    //     next_even.push(temp);
-    // }
-    // next_even.reverse();
-    let mut res = vec![];
-    for q in queries.iter() {
-        let [left, right] = [0, 1].map(|i| q[i] as usize);
-        let k = q[2];
-        // let count = pref_even[right] - if left > 0 { pref_even[left - 1] } else { 0 };
-        // if count == 0 || next_even[left].is_some_and(|v| v > 2 * k) {
-        //     res.push(2 * k);
-        //     continue;
-        // }
-        let mut low = 1;
-        let mut high = i32::MAX >> 1;
-        while low < high {
-            let mid = low + (high - low) / 2;
-            if f(&nums, &pref_even, left, right, k, mid) {
-                high = mid
-            } else {
-                low = 1 + mid;
+pub fn contains_cycle(grid: Vec<Vec<char>>) -> bool {
+    let [rows, cols] = get_dimensions(&grid);
+    let mut dsu = DSU::new(rows * cols);
+    for (r, row) in grid.iter().enumerate() {
+        for (c, &val) in row.iter().enumerate() {
+            for [nr, nc] in [[r + 1, c], [r, 1 + c]] {
+                if grid
+                    .get(nr)
+                    .is_some_and(|rr| rr.get(nc).is_some_and(|&v| v == val))
+                    && dsu.union(r * cols + c, nr * cols + nc)
+                {
+                    return true;
+                }
             }
         }
-        res.push(2 * low);
     }
-    res
+    false
 }
 
-fn f(nums: &[i32], pref_even: &[i32], left: usize, right: usize, k: i32, mid: i32) -> bool {
-    let i = nums.partition_point(|&v| v <= 2 * mid);
-    let mut even_count = 0;
-    if i > left {
-        let right = right.min(i - 1);
-        even_count = pref_even[right] - if left > 0 { pref_even[left - 1] } else { 0 };
+struct DSU {
+    parent: Vec<usize>,
+    size: Vec<i32>,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+        }
     }
-    mid - even_count >= k
+
+    fn find(&mut self, v: usize) -> usize {
+        if self.parent[v] != v {
+            self.parent[v] = self.find(self.parent[v])
+        }
+        self.parent[v]
+    }
+
+    fn union(&mut self, x: usize, y: usize) -> bool {
+        let [rx, ry] = [x, y].map(|v| self.find(v));
+        if rx == ry {
+            return self.size[rx] >= 4;
+        }
+        if self.size[rx] < self.size[ry] {
+            self.parent[rx] = ry;
+            self.size[ry] += self.size[rx];
+        } else {
+            self.parent[ry] = rx;
+            self.size[rx] += self.size[ry];
+        }
+        false
+    }
 }
 
 #[cfg(test)]
@@ -87,12 +94,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            kth_remaining_integer(&[1, 4, 7], &[[0, 2, 1], [1, 1, 2], [0, 0, 3]]),
-            [2, 6, 6]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
