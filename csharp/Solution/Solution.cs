c6012fa82beec051ad361c837a4949c5d4ed664d
Zx Wang <ZW_08@outlook.com>
2026-04-29 17:58:52 -0400

    975 mono stack, dfs/memo

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,29 +8,71 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+    public int OddEvenJumps(int[] arr)
     {
-        List<int> res = [];
-        return Dfs(root, voyage) ? res : [-1];
-
-        bool Dfs(TreeNode node, ReadOnlySpan<int> arr)
+        int n = arr.Length;
+        List<(int num, int idx)> sorted = new(n);
+        for (int i = 0; i < n; i++)
         {
-            if (node is null) { return arr.IsEmpty; }
-            if (arr.IsEmpty || node.val != arr[0]) { return false; }
-            if (node.left is null) { return Dfs(node.right, arr[1..]); }
-            if (node.right is null) { return Dfs(node.left, arr[1..]); }
-            if (arr.Length < 2) { return false; }
-            if (arr[1] == node.left.val)
+            sorted.Add((arr[i], i));
+        }
+        sorted.Sort();
+        Stack<int> st = [];
+        int[] odd_jumps = new int[n];
+        Array.Fill(odd_jumps, -1);
+        foreach (var (_, idx) in sorted)
+        {
+            while (st.TryPeek(out var top) && top < idx)
             {
-                int right = arr.IndexOf(node.right.val);
-                return right >= 2 && Dfs(node.left, arr[1..right]) && Dfs(node.right, arr[right..]);
+                st.Pop();
+                odd_jumps[top] = idx;
+            }
+            st.Push(idx);
+        }
+        sorted.Sort((a, b) => a.num == b.num ? a.idx.CompareTo(b.idx) : b.num.CompareTo(a.num));
+        st.Clear();
+        int[] even_jumps = new int[n];
+        Array.Fill(even_jumps, -1);
+        foreach (var (_, idx) in sorted)
+        {
+            while (st.TryPeek(out var top) && top < idx)
+            {
+                st.Pop();
+                even_jumps[top] = idx;
+            }
+            st.Push(idx);
+        }
+        int[,] memo = new int[n, 2];
+        for (int i = 0; i < n; i++)
+        {
+            for (int odd = 0; odd < 2; odd++)
+            {
+                memo[i, odd] = -1;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res += Dfs(i, 1);
+        }
+        return res;
+
+        int Dfs(int idx, int odd)
+        {
+            if (idx == n - 1) { return 1; }
+            if (idx == -1) { return 0; }
+            if (memo[idx, odd] > -1) { return memo[idx, odd]; }
+            int res = 0;
+            if (odd == 1)
+            {
+                res = Dfs(odd_jumps[idx], odd ^ 1);
             }
             else
             {
-                res.Add(node.val);
-                int left = arr.IndexOf(node.left.val);
-                return left >= 2 && Dfs(node.right, arr[1..left]) && Dfs(node.left, arr[left..]);
+                res = Dfs(even_jumps[idx], odd ^ 1);
             }
+            memo[idx, odd] = res;
+            return res;
         }
     }
 }
