457b07befd3b1e386b459552901cdb5bc4a5a30f
ZW <ZW_08@outlook.com>
2026-04-30 12:43:46 -0400

    3225 prefix sum, dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,29 +9,112 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_path_score(grid: Vec<Vec<i32>>, k: i32) -> i32 {
-    let [rows, cols] = get_dimensions(&grid);
-    let k = k as usize;
-    let mut dp = vec![vec![vec![-1; 1 + k]; cols]; rows];
-    dp[0][0][0] = 0;
-    for r in 0..rows {
-        for c in 0..cols {
-            let cost = usize::from(grid[r][c] > 0);
-            for kk in 0..=k {
-                if kk + cost <= k {
-                    if r > 0 && dp[r - 1][c][kk] >= 0 {
-                        dp[r][c][kk + cost] =
-                            dp[r][c][kk + cost].max(grid[r][c] + dp[r - 1][c][kk]);
-                    }
-                    if c > 0 && dp[r][c - 1][kk] >= 0 {
-                        dp[r][c][kk + cost] =
-                            dp[r][c][kk + cost].max(grid[r][c] + dp[r][c - 1][kk]);
+pub fn maximum_score(grid: Vec<Vec<i32>>) -> i64 {
+    let n = grid.len();
+    let mut prefix = vec![vec![0; n]; 1 + n];
+    for c in 0..n {
+        for r in 1..=n {
+            prefix[r][c] = prefix[r - 1][c] + i64::from(grid[r - 1][c]);
+        }
+    }
+    let mut prev = vec![[0; 2]; 1 + n];
+    for col in (0..n).rev() {
+        let mut curr = vec![[0; 2]; 1 + n];
+        for last_h in 0..=n {
+            for h in 0..=n {
+                for taken in 0..2 {
+                    if h <= last_h {
+                        let val = prefix[last_h][col] - prefix[h][col];
+                        curr[last_h][taken] =
+                            curr[last_h][taken].max(val + prev[h][1]).max(prev[h][0]);
+                    } else if taken == 1 {
+                        curr[last_h][taken] = curr[last_h][taken].max(prev[h][0]);
+                    } else {
+                        let val = if col > 0 {
+                            prefix[h][col - 1] - prefix[last_h][col - 1]
+                        } else {
+                            0
+                        };
+                        curr[last_h][taken] = curr[last_h][taken].max(val + prev[h][0]);
                     }
                 }
             }
         }
+        prev = curr;
     }
-    *dp[rows - 1][cols - 1].iter().max().unwrap()
+    prev[0][0]
+    // let mut dp = vec![vec![[0; 2]; 1 + n]; 1 + n];
+    // for col in (0..n).rev() {
+    //     for last_h in 0..=n {
+    //         for h in 0..=n {
+    //             for taken in 0..2 {
+    //                 if h <= last_h {
+    //                     let curr = prefix[last_h][col] - prefix[h][col];
+    //                     dp[col][last_h][taken] = dp[col][last_h][taken]
+    //                         .max(curr + dp[1 + col][h][1])
+    //                         .max(dp[1 + col][h][0]);
+    //                 } else {
+    //                     if taken == 1 {
+    //                         dp[col][last_h][taken] = dp[col][last_h][taken].max(dp[1 + col][h][0]);
+    //                     } else {
+    //                         let curr = if col > 0 {
+    //                             prefix[h][col - 1] - prefix[last_h][col - 1]
+    //                         } else {
+    //                             0
+    //                         };
+    //                         dp[col][last_h][taken] =
+    //                             dp[col][last_h][taken].max(curr + dp[1 + col][h][0]);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // dp[0][0][0]
+    // let mut memo = vec![vec![[-1; 2]; 1 + n]; n];
+    // dfs(&prefix, 0, 0, false, &mut memo)
+}
+
+fn dfs(
+    prefix: &[Vec<i64>],
+    idx: usize,
+    last_h: usize,
+    taken: bool,
+    memo: &mut [Vec<[i64; 2]>],
+) -> i64 {
+    let n = prefix.len() - 1;
+    if idx >= n {
+        return 0;
+    }
+    if memo[idx][last_h][usize::from(taken)] > -1 {
+        return memo[idx][last_h][usize::from(taken)];
+    }
+    let mut res = 0;
+    for h in 0..=n {
+        if h <= last_h {
+            let curr = prefix[last_h][idx] - prefix[h][idx];
+            // Take [h..=last_h] points on current col
+            res = res.max(curr + dfs(prefix, 1 + idx, h, true, memo));
+            // Skip these points
+            res = res.max(dfs(prefix, 1 + idx, h, false, memo));
+        } else {
+            // h > last_h
+            // No point on current col is available
+            if !taken {
+                // These points are on prev col
+                // They are only available if not taken previously
+                let mut curr = 0;
+                if idx > 0 {
+                    curr = prefix[h][idx - 1] - prefix[last_h][idx - 1];
+                }
+                res = res.max(curr + dfs(prefix, 1 + idx, h, false, memo))
+            } else {
+                res = res.max(dfs(prefix, 1 + idx, h, false, memo))
+            }
+        }
+    }
+    memo[idx][last_h][usize::from(taken)] = res;
+    res
 }
 
 #[cfg(test)]
