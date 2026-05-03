0954612d54cb6b2ff772e629f25fa5150e3560de
ZW <ZW_08@outlook.com>
2026-05-03 08:35:34 -0400

    wc500 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,41 +9,21 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn rotate_string(s: String, goal: String) -> bool {
-    s.len() == goal.len() && search(format!("{s}{s}").as_bytes(), goal.as_bytes())
-}
-
-fn search(hay: &[u8], needle: &[u8]) -> bool {
-    let lps = compute_lps(needle);
-    let mut len = 0;
-    for &val in hay {
-        while len > 0 && needle[len] != val {
-            len = lps[len - 1]
-        }
-        if needle.get(len).is_some_and(|&b| b == val) {
-            len += 1;
-        }
-        if len == needle.len() {
-            break;
+pub fn count_opposite_parity(nums: Vec<i32>) -> Vec<i32> {
+    let mut odd = 0;
+    let mut even = 0;
+    let mut res = vec![];
+    for &num in nums.iter().rev() {
+        if num & 1 == 1 {
+            odd += 1;
+            res.push(even);
+        } else {
+            even += 1;
+            res.push(odd);
         }
     }
-    len == needle.len()
-}
-
-fn compute_lps(needle: &[u8]) -> Vec<usize> {
-    let n = needle.len();
-    let mut lps = vec![0; n];
-    let mut len = 0;
-    for idx in 1..n {
-        while len > 0 && needle[idx] != needle[len] {
-            len = lps[len - 1];
-        }
-        if needle[idx] == needle[len] {
-            len += 1
-        }
-        lps[idx] = len;
-    }
-    lps
+    res.reverse();
+    res
 }
 
 #[cfg(test)]
@@ -76,11 +56,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(compute_lps(b"ababb"), [0, 0, 1, 2, 0]);
-        assert_eq!(compute_lps(b"ababc"), [0, 0, 1, 2, 0]);
-        assert_eq!(compute_lps(b"abab"), [0, 0, 1, 2])
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
