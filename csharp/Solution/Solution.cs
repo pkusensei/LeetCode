a90593a46e544d777ee67de1a7deb16cb7df8a0b
ZW <ZW_08@outlook.com>
2026-05-04 20:45:28 -0400

    61 all off-by-1's

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,89 +8,27 @@ namespace Solution;
 
 public class Solution
 {
-    public int OddEvenJumps(int[] arr)
+    public ListNode RotateRight(ListNode head, int k)
     {
-        int n = arr.Length;
-        List<(int num, int idx)> sorted = new(n);
-        for (int i = 0; i < n; i++)
+        if (head is null || head.next is null || k == 0) { return head; }
+        int len = 1;
+        ListNode tail = head;
+        while (tail.next is not null)
         {
-            sorted.Add((arr[i], i));
+            tail = tail.next;
+            len += 1;
         }
-        sorted.Sort();
-        Stack<int> st = [];
-        int[] odd_jumps = new int[n];
-        Array.Fill(odd_jumps, -1);
-        foreach (var (_, idx) in sorted)
+        k %= len;
+        if (k == 0) { return head; }
+        int left_len = len - k;
+        ListNode new_tail = head;
+        for (int _ = 0; _ < left_len - 1; _++)
         {
-            while (st.TryPeek(out var top) && top < idx)
-            {
-                st.Pop();
-                odd_jumps[top] = idx;
-            }
-            st.Push(idx);
+            new_tail = new_tail.next;
         }
-        sorted.Sort((a, b) => a.num == b.num ? a.idx.CompareTo(b.idx) : b.num.CompareTo(a.num));
-        st.Clear();
-        int[] even_jumps = new int[n];
-        Array.Fill(even_jumps, -1);
-        foreach (var (_, idx) in sorted)
-        {
-            while (st.TryPeek(out var top) && top < idx)
-            {
-                st.Pop();
-                even_jumps[top] = idx;
-            }
-            st.Push(idx);
-        }
-        int[,] dp = new int[n, 2];
-        for (int i = 0; i < n; i++)
-        {
-            dp[i, 1] = 1;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            if (odd_jumps[i] >= 0)
-            {
-                dp[odd_jumps[i], 0] += dp[i, 1];
-            }
-            if (even_jumps[i] >= 0)
-            {
-                dp[even_jumps[i], 1] += dp[i, 0];
-            }
-        }
-        return dp[n - 1, 1] + dp[n - 1, 0];
-
-        int res = 0;
-        int[,] memo = new int[n, 2];
-        for (int i = 0; i < n; i++)
-        {
-            for (int odd = 0; odd < 2; odd++)
-            {
-                memo[i, odd] = -1;
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            res += Dfs(i, 1);
-        }
-        return res;
-
-        int Dfs(int idx, int odd)
-        {
-            if (idx == n - 1) { return 1; }
-            if (idx == -1) { return 0; }
-            if (memo[idx, odd] > -1) { return memo[idx, odd]; }
-            int res = 0;
-            if (odd == 1)
-            {
-                res = Dfs(odd_jumps[idx], odd ^ 1);
-            }
-            else
-            {
-                res = Dfs(even_jumps[idx], odd ^ 1);
-            }
-            memo[idx, odd] = res;
-            return res;
-        }
+        ListNode new_head = new_tail.next;
+        new_tail.next = null;
+        tail.next = head;
+        return new_head;
     }
 }
