d03e71d69e743a1f6f179931cbfd975e983415d7
ZW <ZW_08@outlook.com>
2026-05-06 10:14:05 -0400

    981

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,41 +6,36 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn unique_paths_iii(mut grid: Vec<Vec<i32>>) -> i32 {
-    let [rows, cols] = get_dimensions(&grid);
-    let mut start = [0, 0];
-    let mut ban = 0;
-    for (r, row) in grid.iter().enumerate() {
-        for (c, &v) in row.iter().enumerate() {
-            match v {
-                1 => start = [r, c],
-                -1 => ban += 1,
-                _ => (),
-            }
-        }
-    }
-    dfs(&mut grid, start[0], start[1], (rows * cols) as i32 - ban)
+struct TimeMap {
+    map: HashMap<String, Vec<(i32, String)>>,
 }
 
-fn dfs(grid: &mut [Vec<i32>], row: usize, col: usize, count: i32) -> i32 {
-    if grid[row][col] == 2 {
-        return (count == 1).into();
-    }
-    let mut res = 0;
-    grid[row][col] = -1;
-    for [nr, nc] in neighbors([row, col]) {
-        if grid
-            .get(nr)
-            .is_some_and(|r| r.get(nc).is_some_and(|&v| v >= 0))
-        {
-            res += dfs(grid, nr, nc, count - 1)
+impl TimeMap {
+    fn new() -> Self {
+        Self {
+            map: HashMap::new(),
         }
     }
-    grid[row][col] = 0;
-    res
+
+    fn set(&mut self, key: String, value: String, timestamp: i32) {
+        self.map.entry(key).or_default().push((timestamp, value));
+    }
+
+    fn get(&self, key: String, timestamp: i32) -> String {
+        let Some(v) = self.map.get(&key) else {
+            return "".to_string();
+        };
+        let i = v.partition_point(|v| v.0 <= timestamp);
+        if i == 0 {
+            return "".to_string();
+        }
+        v[i - 1].1.to_owned()
+    }
 }
 
 #[cfg(test)]
