444192c7eba42a94943d68b9d4819c61dc0d49fb
ZW <ZW_08@outlook.com>
2026-05-07 12:03:35 -0400

    986 two ptrs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,73 +9,24 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_value(nums: Vec<i32>) -> Vec<i32> {
-    let n = nums.len();
-    let mut pref_max = nums.to_vec();
-    for i in 1..n {
-        pref_max[i] = pref_max[i].max(pref_max[i - 1])
-    }
-    let mut suf_min = nums.to_vec();
-    for i in (0..n - 1).rev() {
-        suf_min[i] = suf_min[i].min(suf_min[1 + i]);
-    }
-    let mut res = Vec::with_capacity(n);
-    let mut start = 0;
-    for i in 0..n - 1 {
-        if pref_max[i] <= suf_min[1 + i] {
-            for _ in start..=i {
-                res.push(pref_max[i]);
-            }
-            start = 1 + i;
+pub fn interval_intersection(first_list: &[[i32; 2]], second_list: &[[i32; 2]]) -> Vec<Vec<i32>> {
+    let n1 = first_list.len();
+    let n2 = second_list.len();
+    let [mut i1, mut i2] = [0, 0];
+    let mut res = vec![];
+    while i1 < n1 && i2 < n2 {
+        let a = first_list[i1][0].max(second_list[i2][0]);
+        let b = first_list[i1][1].min(second_list[i2][1]);
+        if a <= b {
+            res.push(vec![a, b]);
         }
-    }
-    for _ in start..n {
-        res.push(pref_max[n - 1]);
+        if first_list[i1][1] <= second_list[i2][1] {
+            i1 += 1
+        } else {
+            i2 += 1
+        }
     }
     res
-    // let mut dsu = DSU::new(nums);
-    // for i in 0..n - 1 {
-    //     if pref_max[i] > suf_min[1 + i] {
-    //         dsu.union(i, 1 + i);
-    //     }
-    // }
-    // (0..n)
-    //     .map(|i| {
-    //         let root = dsu.find(i);
-    //         dsu.nums[root]
-    //     })
-    //     .collect()
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    nums: Vec<i32>,
-}
-
-impl DSU {
-    fn new(nums: Vec<i32>) -> Self {
-        let n = nums.len();
-        Self {
-            parent: (0..n).collect(),
-            nums,
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v]);
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if self.nums[rx] >= self.nums[ry] {
-            self.parent[ry] = rx
-        } else {
-            self.parent[rx] = ry;
-        }
-    }
 }
 
 #[cfg(test)]
@@ -108,7 +59,15 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            interval_intersection(
+                &[[0, 2], [5, 10], [13, 23], [24, 25]],
+                &[[1, 5], [8, 12], [15, 24], [25, 26]]
+            ),
+            [[1, 2], [5, 5], [8, 10], [15, 23], [24, 24], [25, 25]]
+        );
+    }
 
     #[test]
     fn test() {}
