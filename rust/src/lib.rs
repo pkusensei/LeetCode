88cf4d97455c0f2f13d6412710566c8f989fe5bb
ZW <ZW_08@outlook.com>
2026-05-09 12:15:34 -0400

    bwc182 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,41 +9,20 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn rotate_grid(mut grid: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
-    let [rows, cols] = get_dimensions(&grid);
-    let layers = rows.min(cols) / 2;
-    for lay in 0..layers {
-        let mut curr = grid[lay][lay..cols - lay - 1].to_vec();
-        for r in lay..rows - lay - 1 {
-            curr.push(grid[r][cols - lay - 1]);
+pub fn score_validator(events: Vec<String>) -> Vec<i32> {
+    let [mut score, mut counter] = [0, 0];
+    for e in events.iter() {
+        match e.as_str() {
+            "0" | "1" | "2" | "3" | "4" | "6" => score += e.parse::<i32>().unwrap_or(0),
+            "W" => counter += 1,
+            "WD" | "NB" => score += 1,
+            _ => (),
         }
-        for c in (1 + lay..cols - lay).rev() {
-            curr.push(grid[rows - lay - 1][c]);
-        }
-        for r in (1 + lay..rows - lay).rev() {
-            curr.push(grid[r][lay]);
-        }
-        let k = k as usize % curr.len();
-        curr.rotate_left(k);
-        let mut idx = 0;
-        for c in lay..cols - lay - 1 {
-            grid[lay][c] = curr[idx];
-            idx += 1;
-        }
-        for r in lay..(rows - lay - 1) {
-            grid[r][cols - lay - 1] = curr[idx];
-            idx += 1;
-        }
-        for c in (1 + lay..cols - lay).rev() {
-            grid[rows - lay - 1][c] = curr[idx];
-            idx += 1;
-        }
-        for r in (1 + lay..rows - lay).rev() {
-            grid[r][lay] = curr[idx];
-            idx += 1;
+        if counter == 10 {
+            break;
         }
     }
-    grid
+    [score, counter].to_vec()
 }
 
 #[cfg(test)]
