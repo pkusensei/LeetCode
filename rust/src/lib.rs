84c35b702a35425e47fd9c9d012ab2409952aa93
ZW <ZW_08@outlook.com>
2026-05-12 11:17:57 -0400

    3924 0-1 bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,107 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn minimum_effort(mut tasks: Vec<[i32; 2]>) -> i32 {
-    // [actual, start]
-    // sort_by(Reverse(start - actual))
-    // sort_by(actual - start)
-    tasks.sort_unstable_by_key(|v| v[0] - v[1]);
-    let mut curr = tasks[0][1];
-    let mut res = curr;
-    for t in &tasks {
-        let [actual, start] = t[..] else {
-            unreachable!()
-        };
-        res += (start - curr).max(0);
-        curr = curr.max(start) - actual;
+pub fn minimum_threshold(n: i32, edges: &[[i32; 3]], source: i32, target: i32, k: i32) -> i32 {
+    let n = n as usize;
+    let mut adj = vec![vec![]; n];
+    for e in edges {
+        let [a, b] = [0, 1].map(|i| e[i] as usize);
+        adj[a].push((b, e[2]));
+        adj[b].push((a, e[2]));
     }
-    res
+    let mut left = 0;
+    let mut right = 1_000_000_001;
+    let mut possible = false;
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if bfs(&adj, source as usize, target as usize, k, mid) {
+            possible = true;
+            right = mid
+        } else {
+            left = 1 + mid
+        }
+    }
+    if possible { left } else { -1 }
+}
+
+pub fn bfs_01(adj: &[Vec<(usize, i32)>], start: usize, goal: usize) -> i32 {
+    let n = adj.len();
+    let mut queue = VecDeque::from([(start, 0)]);
+    let mut dists = vec![i32::MAX >> 1; n];
+    dists[start] = 0;
+    while let Some((node, dist)) = queue.pop_front() {
+        if node == goal {
+            return dist;
+        }
+        for &(next, d) in &adj[node] {
+            let nd = dist + d;
+            if dists[next] > nd {
+                dists[next] = nd;
+                if d == 0 {
+                    queue.push_front((next, nd));
+                } else {
+                    queue.push_back((next, nd));
+                }
+            }
+        }
+    }
+    // This could be sentinel value
+    dists[goal]
+}
+
+fn bfs(adj: &[Vec<(usize, i32)>], source: usize, target: usize, k: i32, thr: i32) -> bool {
+    let n = adj.len();
+    let mut queue = VecDeque::from([(source, 0)]);
+    let mut costs = vec![1 + k; n];
+    costs[source] = 0;
+    while let Some((node, cost)) = queue.pop_front() {
+        if node == target {
+            return cost <= k;
+        }
+        for &(next, w) in &adj[node] {
+            let nc = cost + i32::from(w > thr);
+            if costs[next] > nc {
+                costs[next] = nc;
+                if w > thr {
+                    queue.push_back((next, nc));
+                } else {
+                    queue.push_front((next, nc));
+                }
+            }
+        }
+    }
+    costs[target] <= k
+}
+
+fn dijkstra(adj: &[Vec<(usize, i32)>], source: usize, target: usize, k: i32, thr: i32) -> bool {
+    let n = adj.len();
+    let mut dists = vec![i32::MAX >> 1; n];
+    dists[source] = 0;
+    let mut heap = BinaryHeap::from([(Reverse(0), source)]);
+    while let Some((Reverse(dist), node)) = heap.pop() {
+        if node == target {
+            return dist <= k;
+        }
+        if dist > dists[node] {
+            continue;
+        }
+        for &(next, w) in adj[node].iter() {
+            let nd = dist + i32::from(w > thr);
+            if dists[next] > nd {
+                dists[next] = nd;
+                heap.push((Reverse(nd), next));
+            }
+        }
+    }
+    dists[target] <= k
 }
 
 #[cfg(test)]
@@ -58,8 +141,14 @@ mod tests {
     #[test]
     fn basics() {
         assert_eq!(
-            minimum_effort(vec![[1, 3], [2, 4], [10, 11], [10, 12], [8, 9]]),
-            32
+            minimum_threshold(
+                6,
+                &[[0, 1, 5], [1, 2, 3], [3, 4, 4], [4, 5, 1], [1, 4, 2]],
+                0,
+                3,
+                1
+            ),
+            4
         );
     }
 
