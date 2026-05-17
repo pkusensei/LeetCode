c8f9ec4001a033794b950c68243fe013a4f4da67
ZW <ZW_08@outlook.com>
2026-05-17 10:19:04 -0400

    wc502 q4; binary search, rolling hash

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,101 +6,41 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
 
-pub fn count_local_maximums(matrix: &[&[i32]]) -> i32 {
-    let [rows, cols] = get_dimensions(&matrix);
-    let trees = matrix.iter().map(|row| SegTree::new(row)).collect_vec();
-    let mut res = 0;
-    for (r, row) in matrix.iter().enumerate() {
-        for (c, &val) in row.iter().enumerate() {
-            if val > 0
-                && val
-                    == trees[r].query(
-                        c.saturating_sub(val as usize),
-                        (c + val as usize).min(cols - 1),
-                    )
-            {
-                let mut flag = true;
-                for d in 0..val {
-                    let ql = if d == val - 1 {
-                        c.saturating_sub(val as usize - 1)
-                    } else {
-                        c.saturating_sub(val as usize)
-                    };
-                    let qr = if d == val - 1 {
-                        (c + val as usize - 1).min(cols - 1)
-                    } else {
-                        (c + val as usize).min(cols - 1)
-                    };
-                    if r >= 1 + d as usize && val < trees[r - 1 - d as usize].query(ql, qr) {
-                        flag = false;
-                        break;
-                    }
-                    if (r + 1 + d as usize) < rows && val < trees[r + 1 + d as usize].query(ql, qr)
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if flag {
-                    res += 1;
-                }
-            }
+pub fn smallest_unique_subarray(nums: &[i32]) -> i32 {
+    let mut left = 1;
+    let mut right = nums.len();
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if check(&nums, mid) {
+            right = mid;
+        } else {
+            left = 1 + mid;
         }
     }
-    res
+    left as i32
 }
 
-struct SegTree {
-    tree: Vec<i32>,
-    n: usize,
-}
-
-impl SegTree {
-    fn new(row: &[i32]) -> Self {
-        let n = row.len();
-        let mut s = Self {
-            tree: vec![0; 4 * n],
-            n,
-        };
-        s.build(1, 0, n - 1, row);
-        s
+fn check(nums: &[i32], mid: usize) -> bool {
+    const M: i64 = 1_000_000_007;
+    const BASE: i64 = 215215;
+    let mut freq = HashMap::new();
+    let mut val = 0;
+    let mut pow = 1;
+    for &num in &nums[..mid] {
+        val = (val * BASE + i64::from(num)) % M;
+        pow = pow * BASE % M;
     }
-
-    fn build(&mut self, node: usize, left: usize, right: usize, row: &[i32]) {
-        if left == right {
-            self.tree[node] = row[left];
-            return;
-        }
-        let mid = left.midpoint(right);
-        self.build(2 * node, left, mid, row);
-        self.build(2 * node + 1, 1 + mid, right, row);
-        self.tree[node] = self.tree[2 * node].max(self.tree[2 * node + 1])
+    freq.insert(val, 1);
+    for i in mid..nums.len() {
+        val = (val * BASE + i64::from(nums[i]) - i64::from(nums[i - mid]) * pow % M).rem_euclid(M);
+        *freq.entry(val).or_insert(0) += 1;
     }
-
-    fn query(&self, ql: usize, qr: usize) -> i32 {
-        self._query(1, 0, self.n - 1, ql, qr)
-    }
-
-    fn _query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> i32 {
-        if qr < left || right < ql {
-            return 0;
-        }
-        if ql <= left && right <= qr {
-            return self.tree[node];
-        }
-        let mid = left.midpoint(right);
-        self._query(2 * node, left, mid, ql, qr).max(self._query(
-            2 * node + 1,
-            1 + mid,
-            right,
-            ql,
-            qr,
-        ))
-    }
+    freq.into_values().any(|f| f == 1)
 }
 
 #[cfg(test)]
@@ -134,22 +74,10 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            count_local_maximums(&[
-                &[0, 0, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 2, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0],
-                &[0, 0, 0, 0, 0, 0, 0]
-            ]),
-            1
-        );
+        assert_eq!(smallest_unique_subarray(&[3, 3, 3]), 3);
+        assert_eq!(smallest_unique_subarray(&[1, 1, 2, 2, 1]), 2);
     }
 
     #[test]
-    fn test() {
-        assert_eq!(count_local_maximums(&[&[0, 2, 1], &[1, 1, 0]]), 2)
-    }
+    fn test() {}
 }
