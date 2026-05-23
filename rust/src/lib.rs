40ccf489d8828b4bb19f77c63c45df429630fdbd
ZW <ZW_08@outlook.com>
2026-05-23 17:39:34 -0400

    bwc183 q4; tree dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,37 +9,46 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_score(grid: Vec<Vec<i32>>) -> i32 {
-    let [rows, cols] = get_dimensions(&grid);
-    let single = grid[1..rows - 1].iter().flat_map(|r| &r[1..cols - 1]).max();
-    let mut res = single.copied().unwrap_or(i32::MIN >> 1);
-    for row in grid.iter() {
-        let mut min = 0;
-        let mut prefix = vec![];
-        for (i, &num) in row.iter().enumerate() {
-            prefix.push(num + prefix.last().unwrap_or(&0));
-            if i >= 2 {
-                min = min.min(prefix[i - 2]);
+pub fn count_valid_subsets(parent: &[i32], nums: &[i32], k: i32) -> i32 {
+    let n = nums.len();
+    let adj = parent
+        .iter()
+        .enumerate()
+        .fold(vec![vec![]; n], |mut acc, (node, &p)| {
+            if p >= 0 {
+                acc[p as usize].push(node);
             }
-            if i >= 1 {
-                res = res.max(prefix[i] - min);
+            acc
+        });
+    let [skip, take] = dfs(&adj, &nums, k, 0);
+    // remove empty subset
+    (skip[0] + take[0] - 1).rem_euclid(M) as i32
+}
+
+const M: i64 = 1_000_000_007;
+fn dfs(adj: &[Vec<usize>], nums: &[i32], k: i32, node: usize) -> [Vec<i64>; 2] {
+    let num = (nums[node] % k) as usize;
+    let mut skip = vec![0; k as usize];
+    let mut take = vec![0; k as usize];
+    skip[0] = 1;
+    take[num] = 1;
+    for &next in &adj[node] {
+        let mut temp_skip = vec![0; k as usize];
+        let mut temp_take = vec![0; k as usize];
+        let [next_skip, next_take] = dfs(adj, nums, k, next);
+        let k = k as usize;
+        for a in 0..k {
+            for b in 0..k {
+                let rem = (a + b) % k;
+                let count = (next_skip[b] + next_take[b]) % M;
+                temp_skip[rem] = (temp_skip[rem] + skip[a] * count % M) % M;
+                temp_take[rem] = (temp_take[rem] + take[a] * next_skip[b] % M) % M
             }
         }
+        skip = temp_skip;
+        take = temp_take;
     }
-    for c in 0..cols {
-        let mut min = 0;
-        let mut prefix = vec![];
-        for r in 0..rows {
-            prefix.push(grid[r][c] + prefix.last().unwrap_or(&0));
-            if r >= 2 {
-                min = min.min(prefix[r - 2]);
-            }
-            if r >= 1 {
-                res = res.max(prefix[r] - min);
-            }
-        }
-    }
-    res
+    [skip, take]
 }
 
 #[cfg(test)]
@@ -72,7 +81,10 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(count_valid_subsets(&[-1, 0, 1], &[1, 2, 3], 3), 1);
+        assert_eq!(count_valid_subsets(&[-1, 0, 0, 0], &[2, 1, 2, 1], 3), 2);
+    }
 
     #[test]
     fn test() {}
