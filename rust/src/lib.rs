612618994601be2f130a83b5ec06b7f083938372
ZW <ZW_08@outlook.com>
2026-05-28 10:59:21 -0400

    1003 stack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,43 +9,17 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn grid_illumination(_n: i32, lamps: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    use std::collections::{HashMap, HashSet};
-    let mut set = HashSet::new();
-    let [mut rows, mut cols, mut diag1, mut diag2] = [0; 4].map(|_| HashMap::new());
-    for v in lamps {
-        let [r, c] = v[..] else { unreachable!() };
-        if set.insert([r, c]) {
-            *rows.entry(r).or_insert(0) += 1;
-            *cols.entry(c).or_insert(0) += 1;
-            // diag /
-            *diag1.entry(r + c).or_insert(0) += 1;
-            // diag \
-            *diag2.entry(r - c).or_insert(0) += 1;
+pub fn is_valid(s: &str) -> bool {
+    let mut st = vec![];
+    for b in s.bytes() {
+        if b == b'c' && st.ends_with(b"ab") {
+            st.pop();
+            st.pop();
+        } else {
+            st.push(b);
         }
     }
-    let mut res = vec![];
-    for q in queries {
-        let [r, c] = q[..] else { unreachable!() };
-        let is_on = rows.get(&r).is_some_and(|&v| v > 0)
-            || cols.get(&c).is_some_and(|&v| v > 0)
-            || diag1.get(&(r + c)).is_some_and(|&v| v > 0)
-            || diag2.get(&(r - c)).is_some_and(|&v| v > 0);
-        res.push(i32::from(is_on));
-        for [dr, dc] in ALL_DIRS.into_iter().chain([[0, 0]]) {
-            let nr = r + dr;
-            let nc = c + dc;
-            if set.remove(&[nr, nc]) {
-                *rows.entry(nr).or_insert(0) -= 1;
-                *cols.entry(nc).or_insert(0) -= 1;
-                // diag /
-                *diag1.entry(nr + nc).or_insert(0) -= 1;
-                // diag \
-                *diag2.entry(nr - nc).or_insert(0) -= 1;
-            }
-        }
-    }
-    res
+    st.is_empty()
 }
 
 #[cfg(test)]
@@ -78,7 +52,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert!(!is_valid("abacbcabcc"));
+    }
 
     #[test]
     fn test() {}
