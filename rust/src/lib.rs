e577dfdac7b7ee458c0e950521d2fb1d1d72e6e9
ZW <ZW_08@outlook.com>
2026-05-29 20:55:21 -0400

    3161 segment tree

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,104 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
-    let mut res = 0;
-    let mut left = 0;
-    let mut zeros = 0;
-    for (right, &num) in nums.iter().enumerate() {
-        zeros += i32::from(num == 0);
-        while zeros > k {
-            zeros -= i32::from(nums[left] == 0);
-            left += 1;
+pub fn get_results(queries: Vec<Vec<i32>>) -> Vec<bool> {
+    // 1 for zero/origin, another 1 for M
+    let mut tree = SegTree::new(1 + M);
+    let mut res = vec![];
+    for q in queries {
+        if q[0] == 1 {
+            tree.update(q[1] as usize);
+        } else {
+            if let Some(node) = tree.query(0, q[1] as usize) {
+                let len = node.len.max(node.start).max(q[1] as usize - node.end);
+                res.push(q[2] <= len as i32);
+            } else {
+                res.push(q[2] <= q[1]);
+            }
         }
-        res = res.max(1 + right - left)
     }
-    res as i32
+    res
+}
+
+const M: usize = 50_000;
+
+#[derive(Clone, Copy, Default)]
+struct Node {
+    start: usize,
+    end: usize,
+    len: usize,
+}
+
+impl Node {
+    fn merge(a: Option<Self>, b: Option<Self>) -> Option<Self> {
+        match [a, b] {
+            [_, None] => a,
+            [None, _] => b,
+            [Some(a), Some(b)] => {
+                let len = a.len.max(b.len).max(b.start - a.end);
+                Some(Self {
+                    start: a.start,
+                    end: b.end,
+                    len,
+                })
+            }
+        }
+    }
+}
+
+struct SegTree {
+    tree: Vec<Option<Node>>,
+    n: usize,
+}
+
+impl SegTree {
+    fn new(n: usize) -> Self {
+        let mut s = Self {
+            tree: vec![None; 4 * n],
+            n,
+        };
+        s._update(1, 0, n - 1, 0);
+        s
+    }
+
+    fn update(&mut self, idx: usize) {
+        self._update(1, 0, self.n - 1, idx);
+    }
+
+    fn _update(&mut self, node: usize, left: usize, right: usize, idx: usize) {
+        if left == right {
+            self.tree[node] = Some(Node {
+                start: idx,
+                end: idx,
+                len: 0,
+            });
+            return;
+        }
+        let mid = left.midpoint(right);
+        if idx <= mid {
+            self._update(2 * node, left, mid, idx);
+        } else {
+            self._update(2 * node + 1, 1 + mid, right, idx);
+        }
+        self.tree[node] = Node::merge(self.tree[2 * node], self.tree[2 * node + 1]);
+    }
+
+    fn query(&self, ql: usize, qr: usize) -> Option<Node> {
+        self._query(1, 0, self.n - 1, ql, qr)
+    }
+
+    fn _query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> Option<Node> {
+        if qr < left || right < ql {
+            return None;
+        }
+        if ql <= left && right <= qr {
+            return self.tree[node];
+        }
+        let mid = left.midpoint(right);
+        let a = self._query(2 * node, left, mid, ql, qr);
+        let b = self._query(2 * node + 1, 1 + mid, right, ql, qr);
+        Node::merge(a, b)
+    }
 }
 
 #[cfg(test)]
