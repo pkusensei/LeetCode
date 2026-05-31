51948b6e0b7dbefecb1fc12c843a327eb50fb407
ZW <ZW_08@outlook.com>
2026-05-31 15:06:29 -0400

    wc504 q4; heck of a greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,44 +9,45 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_sale_items(items: Vec<[i32; 2]>, mut budget: i32) -> i32 {
-    use itertools::Itertools;
-    use std::collections::HashMap;
-    let mut min_price = i32::MAX;
-    // Preprocessing and count factors
-    let mut max_factor = 1;
-    let mut factors = HashMap::new();
-    for item in items.iter() {
-        let [fac, p] = item[..] else { unreachable!() };
-        *factors.entry(fac).or_insert(0) += 1;
-        min_price = min_price.min(p);
-        max_factor = max_factor.max(fac);
-    }
-    // For each factor, add count(factor_multiple)
-    let mut freq = HashMap::new();
-    for &fac in factors.keys() {
-        for p in (fac..=max_factor).step_by(fac as usize) {
-            *freq.entry(fac).or_insert(0) += factors.get(&p).unwrap_or(&0);
-        }
-    }
-    // Each item contributes 2*min(budget/price, freq[factor]-1)
-    // budget/price => most amount to buy
-    // freq[factor]-1 => possible free copies
-    //      -1 to remove item itself
-    // filter() remove item too expensive or non-existent
-    let arr = items
+pub fn maximum_mex(nums: &[i32]) -> Vec<i32> {
+    let n = nums.len();
+    let max = *nums.iter().max().unwrap();
+    let mut freq = nums
         .iter()
-        .map(|v| (v[1], freq[&v[0]] - 1))
-        .filter(|v| v.0 < 2 * min_price && v.1 > 0)
-        .sorted_unstable();
-    let mut res = 0;
-    for item in arr {
-        let (p, f) = item;
-        let curr = f.min(budget / p);
-        res += 2 * curr;
-        budget -= curr * p;
+        .fold(vec![0; 2 + max as usize], |mut acc, &num| {
+            acc[num as usize] += 1;
+            acc
+        });
+    // We attempt to make this "mex"
+    let mut target = 0;
+    // Track which number still survives
+    let mut mex_present = vec![false; 2 + max as usize];
+    let mut res = vec![];
+    for &num in nums.iter() {
+        // Prior to consider `num`
+        // Any present number in 0..target is not mex
+        while mex_present[target] {
+            target += 1;
+        }
+        // Found candidate
+        if freq[target] == 0 {
+            res.push(target as i32);
+            // Remove all impact
+            mex_present[..target].fill(false);
+            target = 0;
+        }
+        // Add this number
+        mex_present[num as usize] = true;
+        // "Truncate" nums
+        freq[num as usize] -= 1;
     }
-    res + budget / min_price
+    while mex_present[target] {
+        target += 1;
+    }
+    if res.len() < n {
+        res.push(target as i32);
+    }
+    res
 }
 
 #[cfg(test)]
@@ -80,11 +81,9 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(maximum_sale_items(vec![[1, 6], [2, 4], [3, 5]], 19), 5);
-        assert_eq!(
-            maximum_sale_items(vec![[2, 8], [1, 10], [6, 6], [4, 12], [5, 20], [5, 17]], 35),
-            7
-        )
+        assert_eq!(maximum_mex(&[3, 1]), [0, 0]);
+        assert_eq!(maximum_mex(&[0, 1, 0]), [2, 1]);
+        assert_eq!(maximum_mex(&[1, 0, 2]), [3]);
     }
 
     #[test]
