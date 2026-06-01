a6bdd2be3f09286e97b95b7640cb4a4c914e8f9a
ZW <ZW_08@outlook.com>
2026-05-31 20:14:18 -0400

    2144 greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,34 +9,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_mex(nums: &[i32]) -> Vec<i32> {
-    use std::collections::BTreeSet;
-    let n = nums.len();
-    let mut suf_mex = Vec::with_capacity(n);
-    // All numbers not in `nums`
-    let mut set: BTreeSet<_> = (0..=n as i32).collect();
-    for &num in nums.iter().rev() {
-        set.remove(&num); // Remove candidate
-        suf_mex.push(*set.first().unwrap());
-    }
-    suf_mex.reverse();
-    let mut mex = suf_mex[0];
-    // These are required to reach `mex`
-    // Put `mex` in for easier `first()` call
-    let mut filler: BTreeSet<_> = (0..=mex).collect();
-    let mut res = vec![];
-    for (idx, &num) in nums.iter().enumerate() {
-        filler.remove(&num);
-        let v = *filler.first().unwrap();
-        // Found all <=mex
-        if v >= mex {
-            res.push(v);
-            filler.clear();
-            if let Some(v) = suf_mex.get(1 + idx) {
-                mex = *v;
-                filler.extend(0..=mex);
-            }
-        }
+pub fn minimum_cost(mut cost: Vec<i32>) -> i32 {
+    cost.sort_unstable();
+    let mut res = 0;
+    while let Some(a) = cost.pop() {
+        res += a;
+        let Some(b) = cost.pop() else {
+            break;
+        };
+        res += b;
+        cost.pop();
     }
     res
 }
@@ -71,11 +53,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(maximum_mex(&[3, 1]), [0, 0]);
-        assert_eq!(maximum_mex(&[0, 1, 0]), [2, 1]);
-        assert_eq!(maximum_mex(&[1, 0, 2]), [3]);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
