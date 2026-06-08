ff897de92583148b064ccb6d51d6ebd332ec2eda
ZW <ZW_08@outlook.com>
2026-06-07 20:16:10 -0400

    2161

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,107 +6,23 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::VecDeque;
+use std::iter::repeat_n;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_sum(nums: &[i32], m: i32, l: i32, r: i32) -> i64 {
-    let [l, r] = [l, r].map(|v| v as usize);
-    let prefix = nums.iter().fold(vec![0], |mut acc, &v| {
-        acc.push(i64::from(v) + acc.last().unwrap_or(&0));
-        acc
-    });
-    let max_single = single(&prefix, l, r);
-    let (free_val, free_count) = solve(&prefix, l, r, 0);
-    if free_val <= 0 {
-        return max_single;
-    }
-    if free_count <= m {
-        return free_val;
-    }
-    let mut low = 0;
-    let mut high = 1 + max_single;
-    while low < high {
-        let mid = low + (high - low + 1) / 2;
-        let (_, count) = solve(&prefix, l, r, mid);
-        if count >= m {
-            low = mid;
-        } else {
-            high = mid - 1;
+pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
+    let [mut res, mut big] = [const { vec![] }; 2];
+    let mut count = 0;
+    for &num in nums.iter() {
+        match num.cmp(&pivot) {
+            std::cmp::Ordering::Less => res.push(num),
+            std::cmp::Ordering::Equal => count += 1,
+            std::cmp::Ordering::Greater => big.push(num),
         }
     }
-    let (val, _) = solve(&prefix, l, r, low);
-    val + i64::from(m) * low
-}
-
-// (max_sum, count of subarrs)
-fn solve(prefix: &[i64], l: usize, r: usize, penalty: i64) -> (i64, i32) {
-    let n = prefix.len() - 1;
-    let mut dp_val = vec![0; 1 + n];
-    let mut dp_count = vec![0; 1 + n];
-    let mut queue = VecDeque::new();
-    let mut left = 0;
-    for right in l..=n {
-        // skip
-        dp_val[right] = dp_val[right - 1];
-        dp_count[right] = dp_count[right - 1];
-
-        while left <= right - l {
-            let val = dp_val[left] - prefix[left];
-            let count = dp_count[left];
-            while let Some(&(back_val, back_count, _)) = queue.back()
-                && (back_val < val || (back_val == val && back_count <= count))
-            {
-                queue.pop_back();
-            }
-            while let Some(&(_, _, i)) = queue.front()
-                && i < right.saturating_sub(r)
-            {
-                queue.pop_front();
-            }
-            queue.push_back((val, count, left));
-            left += 1;
-        }
-        // `queue` is ensured to have element
-        let Some(&(val, count, _)) = queue.front() else {
-            unreachable!();
-        };
-        // take
-        let curr_val = val + prefix[right] - penalty;
-        let curr_count = 1 + count;
-        if curr_val >= dp_val[right] {
-            dp_val[right] = curr_val;
-            dp_count[right] = dp_count[right].max(curr_count);
-        }
-    }
-    (dp_val[n], dp_count[n])
-}
-
-fn single(prefix: &[i64], l: usize, r: usize) -> i64 {
-    let n = prefix.len() - 1;
-    let mut res = i64::MIN >> 2;
-    let mut queue = VecDeque::<(usize, i64)>::new();
-    let mut left = 0;
-    for right in l..=n {
-        while left <= right - l {
-            let num = prefix[left];
-            while let Some(&(_, val)) = queue.back()
-                && val >= num
-            {
-                queue.pop_back(); // we want min(prefix[left])
-            }
-            while let Some(&(i, _)) = queue.front()
-                && i < right.saturating_sub(r)
-            {
-                queue.pop_front();
-            }
-            queue.push_back((left, num));
-            left += 1;
-        }
-        // `queue` is ensured to have element
-        res = res.max(prefix[right] - queue.front().map(|&(_, v)| v).unwrap_or(0));
-    }
+    res.extend(repeat_n(pivot, count));
+    res.extend(big);
     res
 }
 
@@ -140,12 +56,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(maximum_sum(&[-1, 7, -4], 1, 2, 3), 6);
-        assert_eq!(maximum_sum(&[4, 1, -5, 2], 2, 1, 3), 7);
-        assert_eq!(maximum_sum(&[1, 0, 3, 4], 2, 1, 2), 8);
-        assert_eq!(maximum_sum(&[-3, -4, -1], 2, 1, 2), -1);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
