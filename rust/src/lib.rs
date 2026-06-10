0766ab01306be13203fffad2b0b2619d6e70768c
ZW <ZW_08@outlook.com>
2026-06-10 15:31:48 -0400

    3691 sparse table

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -13,14 +13,10 @@ use helper::*;
 
 pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
     let n = nums.len();
-    let max_tree = SegTree::new(&nums, |a, b| a.max(b), i32::MIN >> 2);
-    let min_tree = SegTree::new(&nums, |a, b| a.min(b), i32::MAX >> 2);
+    let st = RMQ::new(&nums);
     let mut res = 0;
     let mut heap: BinaryHeap<_> = (0..n)
-        .map(|left| {
-            let val = max_tree.query(left, n - 1) - min_tree.query(left, n - 1);
-            (val, left, n - 1)
-        })
+        .map(|left| (st.query(left, n - 1), left, n - 1))
         .collect();
     for _ in 0..k {
         let Some((val, left, right)) = heap.pop() else {
@@ -28,60 +24,99 @@ pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
         };
         res += i64::from(val);
         if left < right {
-            let val = max_tree.query(left, right - 1) - min_tree.query(left, right - 1);
-            heap.push((val, left, right - 1));
+            heap.push((st.query(left, right - 1), left, right - 1));
         }
     }
     res
 }
 
+type RMQ = SparseTable;
+
+struct SparseTable {
+    st_max: Vec<Vec<i32>>,
+    st_min: Vec<Vec<i32>>,
+}
+
+impl SparseTable {
+    fn new(nums: &[i32]) -> Self {
+        let n = nums.len();
+        let logn = 32 - (n as u32).leading_zeros() as usize;
+        let mut st_max = vec![vec![0; logn]; n];
+        let mut st_min = vec![vec![0; logn]; n];
+        for (i, &num) in nums.iter().enumerate() {
+            st_max[i][0] = num;
+            st_min[i][0] = num;
+        }
+        for col in 1..logn {
+            for row in 0..n {
+                if row + (1 << col) > n {
+                    break;
+                }
+                st_max[row][col] =
+                    st_max[row][col - 1].max(st_max[row + (1 << (col - 1))][col - 1]);
+                st_min[row][col] =
+                    st_min[row][col - 1].min(st_min[row + (1 << (col - 1))][col - 1]);
+            }
+        }
+        Self { st_max, st_min }
+    }
+
+    fn query(&self, left: usize, right: usize) -> i32 {
+        let col = 31 - ((right + 1 - left) as u32).leading_zeros() as usize;
+        let max = self.st_max[left][col].max(self.st_max[right - (1 << col) + 1][col]);
+        let min = self.st_min[left][col].min(self.st_min[right - (1 << col) + 1][col]);
+        max - min
+    }
+}
+
 struct SegTree {
-    tree: Vec<i32>,
+    max_tree: Vec<i32>,
+    min_tree: Vec<i32>,
     n: usize,
-    f: fn(i32, i32) -> i32,
-    identity: i32,
 }
 
 impl SegTree {
-    fn new(nums: &[i32], f: fn(i32, i32) -> i32, identity: i32) -> Self {
+    fn new(nums: &[i32]) -> Self {
         let n = nums.len();
         let mut s = Self {
-            tree: vec![0; 4 * n],
+            max_tree: vec![0; 4 * n],
+            min_tree: vec![0; 4 * n],
             n,
-            f,
-            identity,
         };
         s.build(nums, 1, 0, n - 1);
         s
     }
 
-    fn build(&mut self, nums: &[i32], node: usize, left: usize, right: usize) -> i32 {
+    fn build(&mut self, nums: &[i32], node: usize, left: usize, right: usize) -> [i32; 2] {
         if left == right {
-            self.tree[node] = nums[left].into();
-            return self.tree[node];
+            self.max_tree[node] = nums[left];
+            self.min_tree[node] = nums[left];
+            return [nums[left]; 2];
         }
         let mid = left.midpoint(right);
         let a = self.build(nums, 2 * node, left, mid);
         let b = self.build(nums, 2 * node + 1, 1 + mid, right);
-        self.tree[node] = (self.f)(a, b);
-        self.tree[node]
+        self.max_tree[node] = a[0].max(b[0]);
+        self.min_tree[node] = a[1].min(b[1]);
+        [self.max_tree[node], self.min_tree[node]]
     }
 
     fn query(&self, ql: usize, qr: usize) -> i32 {
-        self._query(1, 0, self.n - 1, ql, qr)
+        let [max, min] = self._query(1, 0, self.n - 1, ql, qr);
+        max - min
     }
 
-    fn _query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> i32 {
+    fn _query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> [i32; 2] {
         if right < ql || qr < left {
-            return self.identity;
+            return [i32::MIN, i32::MAX];
         }
         if ql <= left && right <= qr {
-            return self.tree[node];
+            return [self.max_tree[node], self.min_tree[node]];
         }
         let mid = left.midpoint(right);
         let a = self._query(2 * node, left, mid, ql, qr);
         let b = self._query(2 * node + 1, 1 + mid, right, ql, qr);
-        (self.f)(a, b)
+        [a[0].max(b[0]), a[1].min(b[1])]
     }
 }
 
