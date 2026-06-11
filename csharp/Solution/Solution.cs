6fa0ddd4abb922c9b429bab46c4690e9146b315a
Zx Wang <ZW_08@outlook.com>
2026-06-11 14:31:29 -0400

    1014 greedy

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,31 +8,16 @@ namespace Solution;
 
 public class Solution
 {
-    public TreeNode CreateBinaryTree(int[][] descriptions)
+    public int MaxScoreSightseeingPair(int[] values)
     {
-        Dictionary<int, TreeNode> dict = [];
-        HashSet<int> children = [];
-        foreach (var item in descriptions)
+        int res = 0;
+        int prev = values[0] + 0;
+        for (int i = 1; i < values.Length; i++)
         {
-            int parent = item[0];
-            int child = item[1];
-            bool is_left = item[2] == 1;
-            if (!dict.TryGetValue(parent, out TreeNode parent_node))
-            {
-                parent_node = new(parent);
-            }
-            if (!dict.TryGetValue(child, out var child_node))
-            {
-                child_node = new(child);
-            }
-            if (is_left) { parent_node.left = child_node; }
-            else { parent_node.right = child_node; }
-            dict.TryAdd(parent, parent_node);
-            dict.TryAdd(child, child_node);
-            children.Add(child);
+            res = int.Max(res, prev + values[i] - i);
+            prev = int.Max(prev, values[i] + i);
         }
-        int root = dict.Keys.Except(children).Single();
-        return dict[root];
+        return res;
     }
 }
 
