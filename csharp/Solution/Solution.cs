ea76835618887ad59aa6bcc143d5d8f085f93f9f
Zx Wang <ZW_08@outlook.com>
2026-06-12 14:00:11 -0400

    1026

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,21 +8,22 @@ namespace Solution;
 
 public class Solution
 {
-    public int VideoStitching(int[][] clips, int time)
+    public int MaxAncestorDiff(TreeNode root)
     {
-        int n = clips.Length;
-        Array.Sort(clips, (a, b) => a[0] == b[0] ? a[1].CompareTo(b[1]) : a[0].CompareTo(b[0]));
-        int[] dp = new int[1 + time];
-        Array.Fill(dp, 1 + n);
-        dp[0] = 0;
-        foreach (var item in clips)
+        int res = 0;
+        Dfs(root.left, root.val, root.val);
+        Dfs(root.right, root.val, root.val);
+        return res;
+
+        void Dfs(TreeNode node, int min, int max)
         {
-            for (int i = 1 + item[0]; i <= int.Min(time, item[1]); i++)
-            {
-                dp[i] = int.Min(dp[i], 1 + dp[item[0]]);
-            }
+            if (node is null) { return; }
+            int a = int.Abs(min - node.val);
+            int b = int.Abs(max - node.val);
+            res = int.Max(res, int.Max(a, b));
+            Dfs(node.left, int.Min(min, node.val), int.Max(max, node.val));
+            Dfs(node.right, int.Min(min, node.val), int.Max(max, node.val));
         }
-        return dp[^1] > n ? -1 : dp[^1];
     }
 }
 
