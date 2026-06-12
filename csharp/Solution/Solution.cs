92ed4c2bd79f27b76ab3be1aa32f4173a5ff4622
Zx Wang <ZW_08@outlook.com>
2026-06-12 14:49:28 -0400

    1027 lis dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,22 +8,27 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaxAncestorDiff(TreeNode root)
+    public int LongestArithSeqLength(int[] nums)
     {
-        int res = 0;
-        Dfs(root.left, root.val, root.val);
-        Dfs(root.right, root.val, root.val);
-        return res;
-
-        void Dfs(TreeNode node, int min, int max)
+        int n = nums.Length;
+        int res = 2;
+        List<Dictionary<int, int>> dp = [];
+        for (int right = 0; right < n; right++)
         {
-            if (node is null) { return; }
-            int a = int.Abs(min - node.val);
-            int b = int.Abs(max - node.val);
-            res = int.Max(res, int.Max(a, b));
-            Dfs(node.left, int.Min(min, node.val), int.Max(max, node.val));
-            Dfs(node.right, int.Min(min, node.val), int.Max(max, node.val));
+            Dictionary<int, int> curr = [];
+            for (int left = 0; left < right; left++)
+            {
+                int d = nums[right] - nums[left];
+                curr.TryAdd(d, 2);
+                if (dp[left].TryGetValue(d, out var count))
+                {
+                    curr[d] = int.Max(curr[d], 1 + count);
+                    res = int.Max(res, 1 + count);
+                }
+            }
+            dp.Add(curr);
         }
+        return res;
     }
 }
 
