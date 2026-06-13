ca2519f5ec276ba1592dc2aea47c5fa1e6cf2172
Zx Wang <ZW_08@outlook.com>
2026-06-13 09:23:19 -0400

    1031 prefix sum/max, sliding window; off-by-1's

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,12 +8,40 @@ namespace Solution;
 
 public class Solution
 {
-    public int TwoCitySchedCost(int[][] costs)
+    public int MaxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen)
     {
-        int n = costs.Length / 2;
-        // By selecting x[0], the net impact on budget x[0]-x[1]
-        Array.Sort(costs, (a, b) => (a[0] - a[1]).CompareTo(b[0] - b[1]));
-        return costs[..n].Select(v => v[0]).Sum() + costs[n..].Select(v => v[1]).Sum();
+        return int.Max(Solve(firstLen, secondLen), Solve(secondLen, firstLen));
+
+        int Solve(int len1, int len2)
+        {
+            int n = nums.Length;
+            List<int> prefix = new(n);
+            int running = 0;
+            for (int i = 0; i < n; i++)
+            {
+                running += nums[i];
+                if (i >= len1) { running -= nums[i - len1]; }
+                prefix.Add(running);
+                if (i > 0) { prefix[i] = int.Max(prefix[i], prefix[i - 1]); }
+            }
+            running = 0;
+            List<int> suffix = new(n);
+            for (int i = n - 1; i >= 0; i -= 1)
+            {
+                running += nums[i];
+                if (i + len2 < n) { running -= nums[i + len2]; }
+                suffix.Add(running);
+                if (suffix.Count >= 2) { suffix[^1] = int.Max(suffix[^1], suffix[^2]); }
+            }
+            suffix.Reverse();
+            int res = 0;
+            for (int i = len1 - 1; i + 1 + len2 <= n; i++)
+            {
+                int val = prefix[i] + suffix[1 + i];
+                res = int.Max(res, val);
+            }
+            return res;
+        }
     }
 }
 
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -10,8 +10,11 @@ public class UnitTest
     readonly Solution.Solution sol = new();
 
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(2, new[] { 1, 0, 1 }, 1, 1)]
+    [DataRow(4, new[] { 1, 0, 3 }, 1, 2)]
+    public void TestMethod1(int exp, int[] nums, int a, int b)
     {
+        Assert.AreEqual(exp, sol.MaxSumTwoNoOverlap(nums, a, b));
     }
 
     [TestMethod]
