40328654817233ec9e0d53250d7f9cb49ae56dbf
ZW <ZW_08@outlook.com>
2026-06-16 20:44:05 -0400

    3614

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,39 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn process_str(s: String) -> String {
-    let mut res = vec![];
+pub fn process_str(s: String, k: i64) -> char {
+    let mut len = 0_i64;
     for b in s.bytes() {
         match b {
-            b'*' => {
-                res.pop();
-            }
-            b'#' => res.extend_from_within(..),
-            b'%' => res.reverse(),
-            _ => res.push(b),
+            b'*' => len = (len - 1).max(0),
+            b'#' => len *= 2,
+            b'%' => (),
+            _ => len += 1,
         }
     }
-    String::from_utf8(res).unwrap()
+    if k >= len {
+        return '.';
+    }
+    let mut k = k;
+    for b in s.bytes().rev() {
+        match b {
+            b'*' => len += 1,
+            b'#' => {
+                len /= 2;
+                if k >= len {
+                    k -= len;
+                }
+            }
+            b'%' => k = len - 1 - k,
+            _ => {
+                len -= 1;
+                if k == len {
+                    return b.into();
+                }
+            }
+        }
+    }
+    '.'
 }
 
 #[cfg(test)]
