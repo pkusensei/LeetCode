18aab1c3ccaa85e9a744c9152ac1dfddc32bd05d
ZW <ZW_08@outlook.com>
2026-06-21 06:47:40 -0400

    wc507 q1; greedy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,46 +9,19 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn good_integers(l: i64, r: i64, k: i32) -> i64 {
-    f(r, k as u8) - f(l - 1, k as u8)
-}
-
-fn f(num: i64, k: u8) -> i64 {
-    let s = num.to_string().into_bytes();
-    let n = s.len();
-    let mut memo = vec![[[[-1; 10]; 2]; 2]; n];
-    dfs(&s, k, 0, true, true, b'0', &mut memo)
-}
-
-fn dfs(
-    s: &[u8],
-    k: u8,
-    idx: usize,
-    tight: bool,
-    leading: bool,
-    prev: u8,
-    memo: &mut [[[[i64; 10]; 2]; 2]],
-) -> i64 {
-    if idx >= s.len() {
-        return (!leading).into();
-    }
-    if memo[idx][usize::from(tight)][usize::from(leading)][usize::from(prev - b'0')] > -1 {
-        return memo[idx][usize::from(tight)][usize::from(leading)][usize::from(prev - b'0')];
-    }
-    let upper = if tight { s[idx] } else { b'9' };
-    let mut res = 0;
-    for d in b'0'..=upper {
-        let ntight = tight && d == upper;
-        // This slot is still zero
-        let nleading = leading && d == b'0';
-        // leading represents `prev > 0`
-        // !leading means prev already started number
-        if leading || d.abs_diff(prev) <= k {
-            res += dfs(s, k, 1 + idx, ntight, nleading, d, memo);
+pub fn max_distance(moves: String) -> i32 {
+    let mut free = 0;
+    let [mut x, mut y] = [0_i32, 0];
+    for b in moves.bytes() {
+        match b {
+            b'U' => y += 1,
+            b'D' => y -= 1,
+            b'L' => x -= 1,
+            b'R' => x += 1,
+            _ => free += 1,
         }
     }
-    memo[idx][usize::from(tight)][usize::from(leading)][usize::from(prev - b'0')] = res;
-    res
+    x.abs() + y.abs() + free
 }
 
 #[cfg(test)]
@@ -81,13 +54,8 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(good_integers(10, 15, 1), 3);
-        assert_eq!(good_integers(201, 204, 2), 2);
-    }
+    fn basics() {}
 
     #[test]
-    fn test() {
-        assert_eq!(good_integers(15, 147, 7), 126);
-    }
+    fn test() {}
 }
