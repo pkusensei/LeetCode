4a503388468875e0edb6ecd24ae0f00c1d60d82b
ZW <ZW_08@outlook.com>
2026-06-22 20:53:24 -0400

    3699 dp one by one

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,26 +9,28 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn prev_perm_opt1(mut arr: Vec<i32>) -> Vec<i32> {
-    let n = arr.len();
-    let mut left = n;
-    for i in (0..n - 1).rev() {
-        if arr[i] > arr[1 + i] {
-            left = i;
-            break;
+pub fn zig_zag_arrays(n: i32, l: i32, r: i32) -> i32 {
+    const M: i32 = 1_000_000_007;
+
+    let width = (1 + r - l) as usize;
+    let mut dp_inc = vec![1; width];
+    let mut dp_dec = vec![1; width];
+    for _ in 1..n {
+        let mut curr_inc = vec![0; width];
+        let mut curr_dec = vec![0; width];
+        for i in 1..width {
+            curr_inc[i] = (curr_inc[i - 1] + dp_dec[i - 1]) % M;
         }
-    }
-    if left == n {
-        return arr;
-    }
-    let mut right = 1 + left;
-    for i in 1 + left..n {
-        if arr[i] < arr[left] && arr[right] < arr[i] {
-            right = i;
+        for i in (0..width - 1).rev() {
+            curr_dec[i] = (curr_dec[1 + i] + dp_inc[1 + i]) % M;
         }
+        dp_inc = curr_inc;
+        dp_dec = curr_dec;
     }
-    arr.swap(left, right);
-    arr
+    dp_inc
+        .into_iter()
+        .chain(dp_dec)
+        .fold(0, |acc, v| (acc + v) % M)
 }
 
 #[cfg(test)]
