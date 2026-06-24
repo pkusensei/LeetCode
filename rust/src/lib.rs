c252024bb64e7399ecc80d1c506f9881223d7b00
ZW <ZW_08@outlook.com>
2026-06-23 20:28:58 -0400

    3700 matrix exponentiation

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,15 +9,60 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_equal_rows_after_flips(matrix: Vec<Vec<i32>>) -> i32 {
-    use std::collections::HashMap;
-    let mut map = HashMap::new();
-    for row in matrix {
-        let flip = row.iter().map(|v| 1 - v).collect();
-        *map.entry(row).or_insert(0) += 1;
-        *map.entry(flip).or_insert(0) += 1;
+pub fn zig_zag_arrays(n: i32, l: i32, r: i32) -> i32 {
+    let width = (1 + r - l) as usize;
+
+    // [..width] inc half
+    // [width..] dec half
+    let mut state = vec![vec![1]; 2 * width];
+    let mut transition = vec![vec![0; 2 * width]; 2 * width];
+    for prev in 0..width {
+        for curr in 0..prev {
+            // prev > curr
+            transition[prev][curr + width] = 1;
+        }
+        for curr in 1 + prev..width {
+            // prev < curr
+            transition[prev + width][curr] = 1;
+        }
     }
-    map.into_values().max().unwrap_or(1)
+    transition = mat_pow(transition, n - 1);
+    state = mat_mul(&transition, &state);
+    state.iter().fold(0, |acc, v| (acc + v[0]) % M) as i32
+}
+
+const M: i64 = 1_000_000_007;
+
+fn mat_mul(a: &[Vec<i64>], b: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let m = a.len();
+    let n = a[0].len();
+    let p = b[0].len();
+    let mut res = vec![vec![0; p]; m];
+    for i1 in 0..m {
+        for i2 in 0..p {
+            for i3 in 0..n {
+                res[i1][i2] += a[i1][i3] * b[i3][i2];
+                res[i1][i2] %= M;
+            }
+        }
+    }
+    res
+}
+
+fn mat_pow(mut mat: Vec<Vec<i64>>, mut pow: i32) -> Vec<Vec<i64>> {
+    let n = mat.len();
+    let mut res = vec![vec![0; n]; n];
+    for i in 0..n {
+        res[i][i] = 1;
+    }
+    while pow > 0 {
+        if pow & 1 == 1 {
+            res = mat_mul(&res, &mat);
+        }
+        mat = mat_mul(&mat, &mat);
+        pow >>= 1;
+    }
+    res
 }
 
 #[cfg(test)]
