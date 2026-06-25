9f3161bf0c87b182352ba488eb8d1397b2655523
Zx Wang <ZW_08@outlook.com>
2026-06-25 13:27:48 -0400

    1073

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,32 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_majority_subarrays(nums: Vec<i32>, target: i32) -> i32 {
-    let n = nums.len();
-    let mut res = 0;
-    for left in 0..n {
-        let mut count = 0;
-        for right in left..n {
-            count += usize::from(nums[right] == target);
-            if 2 * count > 1 + right - left {
-                res += 1
-            }
+pub fn add_negabinary(arr1: Vec<i32>, arr2: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![];
+    let mut i1 = arr1.len();
+    let mut i2 = arr2.len();
+    let mut carry = 0;
+    while i1 > 0 || i2 > 0 {
+        if i1 > 0 {
+            carry += arr1[i1 - 1];
+            i1 -= 1;
         }
+        if i2 > 0 {
+            carry += arr2[i2 - 1];
+            i2 -= 1;
+        }
+        res.push(carry & 1);
+        carry = -(carry >> 1);
     }
-    res
+    while carry != 0 {
+        res.push(carry & 1);
+        carry = -(carry >> 1);
+    }
+    while res.last().is_some_and(|&v| v == 0) {
+        res.pop();
+    }
+    res.reverse();
+    if res.is_empty() { vec![0] } else { res }
 }
 
 #[cfg(test)]
