5a084901a899d56070942a53491da2cded058303
Zx Wang <ZW_08@outlook.com>
2026-06-25 14:21:45 -0400

    1079 backtrack

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,35 +8,28 @@ namespace Solution;
 
 public class Solution
 {
-    public int NumSubmatrixSumTarget(int[][] matrix, int target)
+    public int NumTilePossibilities(string tiles)
     {
-        int rows = matrix.Length;
-        int cols = matrix[0].Length;
-        int[,] prefix = new int[rows, cols];
-        for (int r = 0; r < rows; r++)
+        HashSet<string> set = [];
+        StringBuilder sb = new();
+        Backtrack(0);
+        return set.Count - 1;
+
+        void Backtrack(int idx)
         {
-            for (int c = 0; c < cols; c++)
+            if (idx >= tiles.Length)
             {
-                prefix[r, c] = matrix[r][c];
-                if (c > 0) { prefix[r, c] += prefix[r, c - 1]; }
+                set.Add(sb.ToString());
+                return;
+            }
+            Backtrack(1 + idx);
+            int n = sb.Length;
+            for (int i = 0; i <= n; i++)
+            {
+                sb.Insert(i, tiles[idx]);
+                Backtrack(1 + idx);
+                sb.Remove(i, 1);
             }
         }
-        int res = 0;
-        for (int left = 0; left < cols; left++)
-        {
-            for (int right = left; right < cols; right++)
-            {
-                Dictionary<int, int> dict = new() { [0] = 1 };
-                int curr = 0;
-                for (int r = 0; r < rows; r++)
-                {
-                    curr += prefix[r, right];
-                    if (left > 0) { curr -= prefix[r, left - 1]; }
-                    if (dict.TryGetValue(curr - target, out var v)) { res += v; }
-                    if (!dict.TryAdd(curr, 1)) { dict[curr] += 1; }
-                }
-            }
-        }
-        return res;
     }
 }
