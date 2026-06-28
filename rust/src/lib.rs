e0ab2b3c19093326f7e890705d365dca34b3f465
ZW <ZW_08@outlook.com>
2026-06-28 07:27:41 -0400

    wc508 q4; dijkstra

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,22 +6,50 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn max_subarray_sum(nums: &[i32], k: i32) -> i64 {
-    let k = i64::from(k);
-    let mut res = i64::MIN;
-    let [mut prev_noop, mut prev_mul, mut prev_div, mut prev_past] = [0; 4];
-    for num in nums.iter().map(|&v| i64::from(v)) {
-        let curr_noop = num.max(num + prev_noop);
-        let curr_mul = (num * k).max(num * k + prev_noop.max(prev_mul));
-        let curr_div = (num / k).max(num / k + prev_noop.max(prev_div));
-        let curr_past = num.max(num + prev_mul.max(prev_div).max(prev_past));
-        res = res.max(curr_mul).max(curr_div).max(curr_past);
-        [prev_noop, prev_mul, prev_div, prev_past] = [curr_noop, curr_mul, curr_div, curr_past];
+pub fn min_time_max_power(
+    n: i32,
+    edges: Vec<Vec<i32>>,
+    power: i32,
+    cost: Vec<i32>,
+    source: i32,
+    target: i32,
+) -> Vec<i64> {
+    let [n, power, source, target] = [n, power, source, target].map(|v| v as usize);
+    let adj = edges.iter().fold(vec![vec![]; n], |mut acc, e| {
+        acc[e[0] as usize].push((e[1] as usize, e[2]));
+        acc
+    });
+    let mut heap = BinaryHeap::from([(Reverse(0), power, source)]);
+    let mut seen = vec![vec![i64::MAX >> 1; 1 + power]; n];
+    seen[source].fill(0);
+    let mut res = [None, None];
+    while let Some((Reverse(dist), power, node)) = heap.pop() {
+        if node == target {
+            let v = res[0].get_or_insert(dist);
+            if dist <= *v {
+                *v = dist;
+                res[1] = res[1].max(Some(power as i64));
+            }
+            continue;
+        }
+        if dist > seen[node][power] || power < cost[node] as usize {
+            continue;
+        }
+        for &(next, t) in &adj[node] {
+            let ndist = i64::from(t) + dist;
+            let np = power - cost[node] as usize;
+            if ndist < seen[next][np] {
+                seen[next][np] = ndist;
+                heap.push((Reverse(ndist), np, next));
+            }
+        }
     }
-    res
+    res.map(|v| v.unwrap_or(-1)).into()
 }
 
 #[cfg(test)]
@@ -54,9 +82,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(max_subarray_sum(&[1, -2, 3, 4, -5], 2), 14);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
