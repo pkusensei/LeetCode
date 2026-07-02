301678a6de4178ef329137944e0340f46e7600aa
ZW <ZW_08@outlook.com>
2026-07-01 20:45:01 -0400

    3286 0-1 bfs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,59 +11,30 @@ use std::collections::VecDeque;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_safeness_factor(grid: &[&[i32]]) -> i32 {
-    let n = grid.len();
-    let mut safe = vec![vec![i32::MAX; n]; n];
-    let mut queue: VecDeque<_> = VecDeque::new();
-    for (r, row) in grid.iter().enumerate() {
-        for (c, &v) in row.iter().enumerate() {
-            if v == 1 {
-                queue.push_back((r, c, 0));
-                safe[r][c] = 0;
-            }
-        }
-    }
-    while let Some((r, c, v)) = queue.pop_front() {
-        for [nr, nc] in neighbors([r, c]) {
-            if nr < n && nc < n && safe[nr][nc] > 1 + v {
-                safe[nr][nc] = 1 + v;
-                queue.push_back((nr, nc, 1 + v));
-            }
-        }
-    }
-    let mut left = 0;
-    let mut right = 2 * n as i32;
-    while left < right {
-        let mid = left + (1 + right - left) / 2;
-        if check(&safe, mid) {
-            left = mid;
-        } else {
-            right = mid - 1;
-        }
-    }
-    left
-}
-
-fn check(safe: &[Vec<i32>], mid: i32) -> bool {
-    let n = safe.len();
-    if safe[0][0] < mid || safe[n - 1][n - 1] < mid {
-        return false;
-    }
-    let mut queue = VecDeque::from([[0, 0]]);
-    let mut seen = vec![vec![false; n]; n];
-    seen[0][0] = true;
-    while let Some([r, c]) = queue.pop_front() {
-        if r == n - 1 && c == n - 1 {
+pub fn find_safe_walk(grid: Vec<Vec<i32>>, health: i32) -> bool {
+    let [rows, cols] = get_dimensions(&grid);
+    let mut queue = VecDeque::from([(0, 0, health - grid[0][0])]);
+    let mut seen = vec![vec![0; cols]; rows];
+    seen[0][0] = health - grid[0][0];
+    while let Some((r, c, h)) = queue.pop_front() {
+        if r == rows - 1 && c == cols - 1 {
             break;
         }
         for [nr, nc] in neighbors([r, c]) {
-            if nr < n && nc < n && !seen[nr][nc] && safe[nr][nc] >= mid {
-                seen[nr][nc] = true;
-                queue.push_back([nr, nc]);
+            if nr < rows && nc < cols {
+                let next = h - grid[nr][nc];
+                if seen[nr][nc] < next {
+                    seen[nr][nc] = next;
+                    if next == h {
+                        queue.push_front((nr, nc, next));
+                    } else {
+                        queue.push_back((nr, nc, next));
+                    }
+                }
             }
         }
     }
-    seen[n - 1][n - 1]
+    seen[rows - 1][cols - 1] > 0
 }
 
 #[cfg(test)]
@@ -96,16 +67,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            maximum_safeness_factor(&[&[0, 0, 1], &[0, 0, 0], &[0, 0, 0]]),
-            2
-        );
-        assert_eq!(
-            maximum_safeness_factor(&[&[1, 0, 0], &[0, 0, 0], &[0, 0, 1]]),
-            0
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
