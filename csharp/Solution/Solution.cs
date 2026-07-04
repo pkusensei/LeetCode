9dad2b8a7d5ab6b5ed4bcab66ad52518f8e2409a
Zx Wang <ZW_08@outlook.com>
2026-07-04 14:50:05 -0400

    1110 dfs

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -9,18 +9,29 @@ namespace Solution;
 
 public class Solution
 {
-    public int[] CorpFlightBookings(int[][] bookings, int n)
+    public IList<TreeNode> DelNodes(TreeNode root, int[] to_delete)
     {
-        int[] diff = new int[n];
-        foreach (var item in bookings)
+        List<TreeNode> res = [];
+        HashSet<int> set = [.. to_delete];
+        var v = Dfs(root);
+        if (v is not null) { res.Add(v); }
+        return res;
+
+        TreeNode Dfs(TreeNode node)
         {
-            diff[item[0] - 1] += item[2];
-            if (item[1] < n) { diff[item[1]] -= item[2]; }
+            if (node is null) { return null; }
+            node.left = Dfs(node.left);
+            node.right = Dfs(node.right);
+            if (set.Contains(node.val))
+            {
+                if (node.left is not null) { res.Add(node.left); }
+                if (node.right is not null) { res.Add(node.right); }
+                return null;
+            }
+            else
+            {
+                return node;
+            }
         }
-        for (int i = 0; i < n - 1; i++)
-        {
-            diff[1 + i] += diff[i];
-        }
-        return diff;
     }
 }
