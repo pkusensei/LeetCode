e26ab5ef19f5afed0359194b3e5c34a05a83a92f
Zx Wang <ZW_08@outlook.com>
2026-07-03 20:24:44 -0400

    2492 dsu

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,49 +8,53 @@ namespace Solution;
 
 public class Solution
 {
-    public int FindMaxPathScore(int[][] edges, bool[] online, long k)
+    public int MinScore(int n, int[][] roads)
     {
-        int n = online.Length;
-        List<(int next, int cost)>[] adj = [.. Enumerable.Range(0, n).Select(_ => new List<(int, int)>())];
-        int right = 0;
-        foreach (var item in edges)
+        DSU dsu = new(n);
+        foreach (var item in roads)
         {
-            if (online[item[0]] && online[item[1]])
+            dsu.Union(item[0] - 1, item[1] - 1);
+        }
+        int res = int.MaxValue;
+        foreach (var item in roads)
+        {
+            if (dsu.Find(0) == dsu.Find(item[0] - 1))
             {
-                adj[item[0]].Add((item[1], item[2]));
-                right = int.Max(right, item[2]);
+                res = int.Min(res, item[2]);
             }
         }
-        int left = -1;
-        while (left < right)
-        {
-            int mid = left + (1 + right - left) / 2;
-            if (Check(mid)) { left = mid; }
-            else { right = mid - 1; }
-        }
-        return left;
+        return res;
+    }
+}
 
-        bool Check(int mid)
+public readonly struct DSU
+{
+    public DSU(int n)
+    {
+        Parent = [.. Enumerable.Range(0, n)];
+        Rank = new int[n];
+    }
+
+    public int[] Parent { get; }
+    public int[] Rank { get; }
+
+    public int Find(int v)
+    {
+        if (Parent[v] != v) { Parent[v] = Find(Parent[v]); }
+        return Parent[v];
+    }
+
+    public void Union(int x, int y)
+    {
+        int rx = Find(x);
+        int ry = Find(y);
+        if (rx == ry) { return; }
+        if (Rank[rx] < Rank[ry]) { Parent[rx] = ry; }
+        else if (Rank[rx] > Rank[ry]) { Parent[ry] = rx; }
+        else
         {
-            PriorityQueue<int, long> pq = new();
-            pq.Enqueue(0, 0);
-            long[] dists = [.. Enumerable.Range(0, n).Select(_ => long.MaxValue >> 1)];
-            dists[0] = 0;
-            while (pq.TryDequeue(out var node, out long dist))
-            {
-                if (node == n - 1) { break; }
-                if (dist > dists[node]) { continue; }
-                foreach (var (next, cost) in adj[node])
-                {
-                    long ncost = cost + dist;
-                    if (cost >= mid && ncost < dists[next])
-                    {
-                        dists[next] = ncost;
-                        pq.Enqueue(next, ncost);
-                    }
-                }
-            }
-            return dists[^1] <= k;
+            Rank[rx] += 1;
+            Parent[ry] = rx;
         }
     }
 }
--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -12,25 +12,16 @@ public class UnitTest
     [TestMethod]
     public void TestMethod1()
     {
-        int[][] e = [[0, 1, 5], [1, 3, 10], [0, 2, 3], [2, 3, 4]];
-        bool[] online = [true, true, true, true];
-        Assert.AreEqual(3, sol.FindMaxPathScore(e, online, 10));
     }
 
     [TestMethod]
     public void TestMethod2()
     {
-        int[][] e = [[0, 1, 8]];
-        bool[] online = [true, true];
-        Assert.AreEqual(8, sol.FindMaxPathScore(e, online, 11));
     }
 
     [TestMethod]
     public void TestMethod3()
     {
-        int[][] e = [[0, 1, 0], [0, 2, 7], [1, 3, 9], [0, 4, 7], [2, 4, 9], [3, 4, 2], [0, 3, 5], [2, 3, 3], [1, 4, 6], [1, 2, 0]];
-        bool[] online = [true, true, true, true, true];
-        Assert.AreEqual(0, sol.FindMaxPathScore(e, online, 5));
     }
 
     [TestMethod]
