2958df8d0aa554c6f9bee1c9dd8b653496a26e6e
Zx Wang <ZW_08@outlook.com>
2026-07-04 13:44:48 -0400

    1105 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -8,18 +8,26 @@ namespace Solution;
 
 public class Solution
 {
-    public IList<int> PathInZigZagTree(int label)
+    public int MinHeightShelves(int[][] books, int shelfWidth)
     {
-        if (label <= 1) { return [1]; }
-        int pow = Math.ILogB(label);
-        int prev_opposite = label / 2;
-        int prev_start = (int)Math.Pow(2, pow - 1);
-        int prev_end = (int)Math.Pow(2, pow) - 1;
-        int diff = prev_opposite - prev_start;
-        int distance = prev_end - prev_start;
-        int v = prev_start + (distance - diff);
-        var res = PathInZigZagTree(v);
-        res.Add(label);
-        return res;
+        int n = books.Length;
+        int[] dp = new int[1 + n];
+        Array.Fill(dp, int.MaxValue >> 1, 1, n);
+        for (int right = 0; right < n; right++)
+        {
+            int thick = books[right][0];
+            int height = books[right][1];
+            dp[1 + right] = int.Min(dp[1 + right], dp[right] + height);
+
+            int curr_thick = thick;
+            for (int left = right - 1; left >= 0; left -= 1)
+            {
+                curr_thick += books[left][0];
+                if (curr_thick > shelfWidth) { break; }
+                height = int.Max(height, books[left][1]);
+                dp[1 + right] = int.Min(dp[1 + right], height + dp[left]);
+            }
+        }
+        return dp[n];
     }
 }
