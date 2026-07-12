45b060d133afc7498a0a1879aafb807f480d1a18
ZW <ZW_08@outlook.com>
2026-07-11 20:13:49 -0400

    1331 sort

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,64 +9,23 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-    let n = n as usize;
-    let mut dsu = DSU::new(n);
-    for e in edges {
-        dsu.union(e[0] as usize, e[1] as usize);
-    }
-    let mut seen = 0_i64;
-    let mut res = 0;
-    for i in 0..n {
-        let root = dsu.find(i);
-        if seen & (1 << root) == 0 {
-            seen |= 1 << root;
-            let a = dsu.size[root];
-            let b = dsu.edge[root];
-            res += i32::from(a * (a - 1) / 2 == b);
-        }
+pub fn array_rank_transform(arr: Vec<i32>) -> Vec<i32> {
+    use itertools::Itertools;
+    let vals = arr
+        .iter()
+        .enumerate()
+        .map(|(i, &v)| (v, i))
+        .sorted_unstable()
+        .collect_vec();
+    let mut res = vec![0; arr.len()];
+    let mut rank = 0;
+    let mut prev = i32::MIN;
+    for (num, i) in vals {
+        rank += i32::from(num > prev);
+        res[i] = rank;
+        prev = num;
     }
     res
-}
-
-struct DSU {
-    parent: Vec<usize>,
-    size: Vec<i32>,
-    edge: Vec<i32>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-            size: vec![1; n],
-            edge: vec![0; n],
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v])
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        if rx == ry {
-            self.edge[rx] += 1;
-            return;
-        }
-        if self.size[rx] < self.size[ry] {
-            self.parent[rx] = ry;
-            self.size[ry] += self.size[rx];
-            self.edge[ry] += 1 + self.edge[rx]
-        } else {
-            self.parent[ry] = rx;
-            self.size[rx] += self.size[ry];
-            self.edge[rx] += 1 + self.edge[ry];
-        }
-    }
 }
 
 #[cfg(test)]
