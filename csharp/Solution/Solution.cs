18ba8d3b9381a5eeed9c6fd59c0260c972dfd540
Zx Wang <ZW_08@outlook.com>
2026-07-14 15:40:13 -0400

    1125 dp, bitmask

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -9,18 +9,39 @@ namespace Solution;
 
 public class Solution
 {
-    public int LongestWPI(int[] hours)
+    public int[] SmallestSufficientTeam(string[] req_skills, IList<IList<string>> people)
     {
-        int prefix = 0;
-        Dictionary<int, int> dict = [];
-        int res = 0;
-        for (int i = 0; i < hours.Length; i += 1)
+        int n = req_skills.Length;
+        Dictionary<string, int> str_i = [];
+        for (int i = 0; i < n; i++)
         {
-            prefix += hours[i] > 8 ? 1 : -1;
-            if (prefix > 0) { res = int.Max(res, 1 + i); }
-            if (dict.TryGetValue(prefix - 1, out var left)) { res = int.Max(res, 1 + i - left); }
-            dict.TryAdd(prefix, i);
+            str_i[req_skills[i]] = i;
         }
-        return res;
+        int[] people_mask = people.Select(list =>
+        {
+            int mask = 0;
+            foreach (var item in list)
+            {
+                mask |= 1 << str_i[item];
+            }
+            return mask;
+        }).ToArray();
+        List<int>[] dp = new List<int>[1 << n];
+        dp[0] = [];
+        for (int mask = 0; mask < (1 << n) - 1; mask++)
+        {
+            if (dp[mask] is null) { continue; }
+            for (int i = 0; i < people_mask.Length; i++)
+            {
+                int nmask = mask | people_mask[i];
+                if (int.PopCount(nmask) > int.PopCount(mask) &&
+                (dp[nmask] is null || dp[nmask].Count > 1 + dp[mask].Count))
+                {
+                    dp[nmask] = [.. dp[mask]];
+                    dp[nmask].Add(i);
+                }
+            }
+        }
+        return [.. dp[^1]];
     }
 }
