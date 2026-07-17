c2c107de8bd16cc128658489e15bd71edb2c72fc
ZW <ZW_08@outlook.com>
2026-07-16 21:32:17 -0400

    3312 inclusion exclusion

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,57 +9,31 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn subsequence_pair_count(nums: Vec<i32>) -> i32 {
-    let n = nums.len();
-    let mut memo = vec![[[-1; 201]; 201]; n];
-    dfs(&nums, 0, 0, 0, &mut memo)
-}
-
-pub fn bottom_up(nums: &[i32]) -> i32 {
-    const fn gcd(a: usize, b: usize) -> usize {
-        if a == 0 { return b } else { gcd(b % a, a) }
-    }
-
-    let n = nums.len();
-    let mut dp = [[0; 201]; 201];
-    dp[0][0] = 1;
-    for &num in nums.iter() {
-        let mut curr = [[0; 201]; 201];
-        let num = num as usize;
-        for a in 0..201 {
-            let gcd1 = gcd(a, num);
-            for b in 0..201 {
-                if dp[a][b] == 0 {
-                    continue;
-                }
-                let gcd2 = gcd(b, num);
-                curr[a][b] = (curr[a][b] + dp[a][b]) % M;
-                curr[gcd1][b] = (curr[gcd1][b] + dp[a][b]) % M;
-                curr[a][gcd2] = (curr[a][gcd2] + dp[a][b]) % M;
-            }
+pub fn gcd_values(nums: Vec<i32>, queries: Vec<i64>) -> Vec<i32> {
+    let max = *nums.iter().max().unwrap() as usize;
+    let freq = nums.iter().fold(vec![0; 1 + max], |mut acc, &num| {
+        acc[num as usize] += 1;
+        acc
+    });
+    let mut gcd_freq = vec![0_i64; 1 + max];
+    for div in (1..=max).rev() {
+        let mut count = 0;
+        for v in (div..=max).step_by(div) {
+            count += i64::from(freq[v]);
         }
-        dp = curr;
+        gcd_freq[div] += count * (count - 1) / 2;
+        for v in (2 * div..=max).step_by(div) {
+            gcd_freq[div] -= gcd_freq[v];
+        }
     }
-    (1..201).map(|i| dp[i][i]).fold(0, |acc, v| (acc + v) % M)
-}
-
-const M: i32 = 1_000_000_007;
-fn dfs(nums: &[i32], idx: usize, gcd1: i32, gcd2: i32, memo: &mut [[[i32; 201]; 201]]) -> i32 {
-    const fn gcd(a: i32, b: i32) -> i32 {
-        if a == 0 { return b } else { gcd(b % a, a) }
+    for i in 1..=max {
+        gcd_freq[i] += gcd_freq[i - 1];
     }
-
-    if idx >= nums.len() {
-        return i32::from(gcd1 == gcd2 && gcd1 > 0);
+    let mut res = vec![];
+    for &q in queries.iter() {
+        let i = gcd_freq.partition_point(|&v| v <= q);
+        res.push(i as i32);
     }
-    if memo[idx][gcd1 as usize][gcd2 as usize] > -1 {
-        return memo[idx][gcd1 as usize][gcd2 as usize];
-    }
-    let skip = dfs(nums, 1 + idx, gcd1, gcd2, memo);
-    let take1 = dfs(nums, 1 + idx, gcd(gcd1, nums[idx]), gcd2, memo);
-    let take2 = dfs(nums, 1 + idx, gcd1, gcd(gcd2, nums[idx]), memo);
-    let res = ((skip + take1) % M + take2) % M;
-    memo[idx][gcd1 as usize][gcd2 as usize] = res;
     res
 }
 
