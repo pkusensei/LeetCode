02a0e8a7fe0e7532380dc3ac327f754c799b334e
Zx Wang <ZW_08@outlook.com>
2026-07-22 16:57:06 -0400

    1140 dp

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -9,20 +9,50 @@ namespace Solution;
 
 public class Solution
 {
-    public int CountDominantNodes(TreeNode root)
+    public int StoneGameII(int[] piles)
     {
-        int res = 0;
-        Dfs(root);
-        return res;
-
-        int Dfs(TreeNode node)
+        int n = piles.Length;
+        if (n <= 2) { return piles.Sum(); }
+        int[] suffix = [.. piles];
+        for (int i = n - 2; i >= 0; i -= 1)
         {
-            if (node is null) { return 0; }
-            int left = Dfs(node.left);
-            int right = Dfs(node.right);
-            int curr = int.Max(node.val, int.Max(left, right));
-            if (curr == node.val) { res += 1; }
-            return curr;
+            suffix[i] += suffix[1 + i];
+        }
+        int[,] dp = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int m = 1; m < n; m++)
+            {
+                if (i + 2 * m >= n) { dp[i, m] = suffix[i]; }
+            }
+        }
+        for (int idx = n - 1; idx >= 0; idx -= 1)
+        {
+            for (int m = 1; m < n; m += 1)
+            {
+                for (int x = 1; idx + x < n && x <= 2 * m; x++)
+                {
+                    int oppo = dp[idx + x, int.Max(x, m)];
+                    if (oppo > 0) { dp[idx, m] = int.Max(dp[idx, m], suffix[idx] - oppo); }
+                }
+            }
+        }
+        return dp[0, 1];
+        int[,] memo = new int[n, n];
+        return Dfs(0, 1);
+
+        int Dfs(int idx, int m)
+        {
+            if (idx + 2 * m >= n) { return suffix[idx]; }
+            if (memo[idx, m] > 0) { return memo[idx, m]; }
+            int res = 0;
+            for (int x = 1; x <= 2 * m; x++)
+            {
+                int oppo = Dfs(idx + x, int.Max(x, m));
+                res = int.Max(res, suffix[idx] - oppo);
+            }
+            memo[idx, m] = res;
+            return memo[idx, m];
         }
     }
 }
