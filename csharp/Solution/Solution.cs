d22ad8ce426150df64271e2ec231da6cac56743a
ZW <ZW_08@outlook.com>
2026-07-23 16:43:26 -0400

    1144

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -9,50 +9,19 @@ namespace Solution;
 
 public class Solution
 {
-    public int StoneGameII(int[] piles)
+    public int MovesToMakeZigzag(int[] nums)
     {
-        int n = piles.Length;
-        if (n <= 2) { return piles.Sum(); }
-        int[] suffix = [.. piles];
-        for (int i = n - 2; i >= 0; i -= 1)
-        {
-            suffix[i] += suffix[1 + i];
-        }
-        int[,] dp = new int[n, n];
+        int n = nums.Length;
+        int a = 0;
+        int b = 0;
         for (int i = 0; i < n; i++)
         {
-            for (int m = 1; m < n; m++)
-            {
-                if (i + 2 * m >= n) { dp[i, m] = suffix[i]; }
-            }
+            int left = i > 0 ? nums[i - 1] : 1001;
+            int right = 1 + i < n ? nums[1 + i] : 1001;
+            int curr = nums[i] - (int.Min(left, right) - 1);
+            curr = int.Max(curr, 0);
+            if ((i & 1) == 0) { a += curr; } else { b += curr; }
         }
-        for (int idx = n - 1; idx >= 0; idx -= 1)
-        {
-            for (int m = 1; m < n; m += 1)
-            {
-                for (int x = 1; idx + x < n && x <= 2 * m; x++)
-                {
-                    int oppo = dp[idx + x, int.Max(x, m)];
-                    if (oppo > 0) { dp[idx, m] = int.Max(dp[idx, m], suffix[idx] - oppo); }
-                }
-            }
-        }
-        return dp[0, 1];
-        int[,] memo = new int[n, n];
-        return Dfs(0, 1);
-
-        int Dfs(int idx, int m)
-        {
-            if (idx + 2 * m >= n) { return suffix[idx]; }
-            if (memo[idx, m] > 0) { return memo[idx, m]; }
-            int res = 0;
-            for (int x = 1; x <= 2 * m; x++)
-            {
-                int oppo = Dfs(idx + x, int.Max(x, m));
-                res = int.Max(res, suffix[idx] - oppo);
-            }
-            memo[idx, m] = res;
-            return memo[idx, m];
-        }
+        return int.Min(a, b);
     }
 }
