b1fe1c0af4faf9e642ab4919ca2f52d1792bf8cc
ZW <ZW_08@outlook.com>
2026-07-28 21:10:56 -0400

    3518 ahh

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,99 +6,69 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use helper::*;
 
-struct MajorityChecker {
-    arr: Vec<i32>,
-    map: HashMap<i32, Vec<usize>>,
-    buckets: Vec<Option<i32>>,
-}
-
-impl MajorityChecker {
-    fn new(arr: Vec<i32>) -> Self {
-        let map = arr
-            .iter()
-            .enumerate()
-            .fold(HashMap::<_, Vec<_>>::new(), |mut acc, (i, &v)| {
-                acc.entry(v).or_default().push(i);
-                acc
-            });
-        let n = arr.len();
-        let b = n.isqrt();
-        let buckets = arr.chunks(b).map(vote).collect();
-        Self { arr, map, buckets }
-    }
-
-    fn query(&self, left: i32, right: i32, threshold: i32) -> i32 {
-        let [left, right] = [left, right].map(|v| v as usize);
-        let b = self.arr.len().isqrt();
-        let [buc_left, buc_right] = [left, right].map(|v| v / b);
-        if buc_left == buc_right {
-            // same bucket
-            if let Some(v) = vote(&self.arr[left..=right])
-                && self.count(v, left, right) >= threshold
-            {
-                return v;
+pub fn smallest_palindrome(s: String, k: i32) -> String {
+    let n = s.len();
+    let mut k = i64::from(k);
+    let mut freq = s.bytes().take(n / 2).fold([0; 26], |mut acc, b| {
+        acc[usize::from(b - b'a')] += 1;
+        acc
+    });
+    let mut res = Vec::with_capacity(n);
+    for idx in 0..n / 2 {
+        for cand in 0..26 {
+            if freq[cand] == 0 {
+                continue;
             }
-            return -1;
-        }
-        // left partial
-        let rr = (1 + buc_left) * b - 1;
-        if let Some(v) = vote(&self.arr[left..=rr])
-            && self.count(v, left, right) >= threshold
-        {
-            return v;
-        }
-        // right partial
-        let ll = b * buc_right;
-        if let Some(v) = vote(&self.arr[ll..=right])
-            && self.count(v, left, right) >= threshold
-        {
-            return v;
-        }
-        for i in 1 + buc_left..buc_right {
-            if let Some(v) = self.buckets[i]
-                && self.count(v, left, right) >= threshold
-            {
-                return v;
+            freq[cand] -= 1;
+            let ways = count_perm(&freq, (n / 2 - idx - 1) as i64, k);
+            if ways < k {
+                freq[cand] += 1;
+                k -= ways;
+            } else {
+                res.push(cand as u8 + b'a');
+                break;
             }
         }
-        -1
     }
-
-    fn count(&self, num: i32, left: usize, right: usize) -> i32 {
-        let Some(arr) = self.map.get(&num) else {
-            return 0;
-        };
-        let a = arr.partition_point(|&v| v < left);
-        let b = arr.partition_point(|&v| v <= right);
-        (b - a) as i32
+    if res.len() < n / 2 {
+        return "".to_string();
     }
+    if n & 1 == 1 {
+        res.push(s.as_bytes()[n / 2]);
+    }
+    res.extend_from_within(..n / 2);
+    res[(1 + n) / 2..].reverse();
+    String::from_utf8(res).unwrap()
 }
 
-fn vote(nums: &[i32]) -> Option<i32> {
-    let mut counter = 0;
-    let mut major = 0;
-    for &num in nums.iter() {
-        if counter == 0 {
-            major = num;
-            counter = 0;
+fn count_perm(freq: &[i64; 26], mut rem: i64, max: i64) -> i64 {
+    let mut res = 1;
+    for &f in freq {
+        if f == 0 {
+            continue;
         }
-        if num == major {
-            counter += 1;
-        } else {
-            counter -= 1;
+        res *= n_choose_k(rem, f, max);
+        if res > max {
+            break;
+        }
+        rem -= f;
+    }
+    res
+}
+
+fn n_choose_k(n: i64, k: i64, max: i64) -> i64 {
+    let k = k.min(n - k);
+    let mut res = 1;
+    for i in 1..=k {
+        res = res * (n - i + 1) / i;
+        if res > max {
+            return 1 + max;
         }
     }
-    let freq = nums.iter().filter(|&&v| v == major).count();
-    if 2 * freq > nums.len() {
-        Some(major)
-    } else {
-        None
-    }
+    res
 }
 
 #[cfg(test)]
@@ -135,8 +105,5 @@ mod tests {
     fn basics() {}
 
     #[test]
-    fn test() {
-        let m = MajorityChecker::new(vec![2, 2, 1, 2, 1, 2, 2, 1, 1, 2]);
-        assert_eq!(-1, m.query(0, 5, 6));
-    }
+    fn test() {}
 }
