e7f7e2789dc9c8d6768cd4da82060fca2332a2e6
ZW <ZW_08@outlook.com>
2026-08-01 12:58:44 -0400

    bwc q3; diff array, binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,14 +9,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_valid_prefixes(s: String) -> i32 {
-    let mut res = 0;
-    let mut prefix = 0_i32;
-    for b in s.bytes() {
-        prefix += if b == b'1' { 1 } else { -1 };
-        res += i32::from(prefix.abs() <= 1);
+pub fn min_initial_strength(monsters: &[i32], boosts: &[[i32; 3]]) -> i64 {
+    use itertools::izip;
+    let n = monsters.len();
+    let mut diff = vec![0; n];
+    for b in boosts {
+        let [left, right, v] = b[..] else {
+            unreachable!()
+        };
+        let v = i64::from(v);
+        diff[left as usize] += v;
+        if (1 + right as usize) < n {
+            diff[1 + right as usize] -= v;
+        }
     }
-    res
+    let mut left = 0;
+    let mut right: i64 = monsters.iter().map(|&v| i64::from(v)).sum();
+    'out: while left < right {
+        let mid = left + (right - left) / 2;
+        let mut curr = mid;
+        let mut prefix = 0;
+        for (&mon, &d) in izip!(monsters.iter(), diff.iter()) {
+            let mon = i64::from(mon);
+            prefix += d;
+            if mon > curr + prefix {
+                left = 1 + mid;
+                continue 'out;
+            }
+            curr = (curr - mon).max(0);
+        }
+        right = mid;
+    }
+    left
 }
 
 #[cfg(test)]
@@ -50,8 +74,27 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            min_initial_strength(&[5, 10, 15], &[[1, 2, 10], [1, 2, 5]]),
+            5
+        );
+    }
 
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(
+            min_initial_strength(
+                &[738260286],
+                &[
+                    [0, 0, 779345931],
+                    [0, 0, 322031493],
+                    [0, 0, 512569541],
+                    [0, 0, 621512189],
+                    [0, 0, 322982288]
+                ]
+            ),
+            0
+        );
+    }
 }
