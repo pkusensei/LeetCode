5c80c4c62cf9c7d30ac28d30b0d4775093728d65
ZW <ZW_08@outlook.com>
2026-08-02 20:44:24 -0400

    1406 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,64 +8,37 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
-use std::{cmp::Reverse, collections::HashMap};
 
-// even/odd <= a/b
-// even*b <= odd*a
-pub fn count_ratio_subarrays(nums: Vec<i32>, a: i32, b: i32) -> i64 {
-    let prefix = nums.iter().fold(vec![0], |mut acc, v| {
-        let curr = if v & 1 == 0 { -b } else { a };
-        acc.push(i64::from(curr) + acc.last().unwrap_or(&0));
-        acc
-    });
-    let map = prefix
-        .iter()
-        .sorted_unstable()
-        .fold(HashMap::new(), |mut acc, &v| {
-            let i = acc.len();
-            acc.entry(v).or_insert(i);
-            acc
-        });
-    let n = map.len();
-    let mut ft = FenwickTree::new(1 + n);
-    let mut res = 0;
-    for v in prefix.iter() {
-        let i = map[v];
-        res += ft.query(1 + i);
-        ft.update(1 + i, 1);
+pub fn stone_game_iii(stone_value: &[i32]) -> String {
+    let n = stone_value.len();
+    let mut dp = vec![i32::MIN; n];
+    // Could push in dp[n] = 0;
+    for idx in (0..n).rev() {
+        for next in idx + 1..=(idx + 3).min(n) {
+            dp[idx] = dp[idx]
+                .max(stone_value[idx..next].iter().sum::<i32>() - dp.get(next).unwrap_or(&0));
+        }
+    }
+    match dp[0].cmp(&0) {
+        // match dfs(&stone_value, 0).cmp(&0) {
+        std::cmp::Ordering::Less => "Bob",
+        std::cmp::Ordering::Equal => "Tie",
+        std::cmp::Ordering::Greater => "Alice",
+    }
+    .to_string()
+}
+
+fn dfs(nums: &[i32], idx: usize) -> i32 {
+    let n = nums.len();
+    if idx >= n {
+        return 0;
+    }
+    let mut res = i32::MIN;
+    for i in idx + 1..=(idx + 3).min(n) {
+        let curr = nums[idx..i].iter().sum::<i32>() - dfs(nums, i);
+        res = res.max(curr)
     }
     res
-}
-
-struct FenwickTree {
-    tree: Vec<i64>,
-    n: usize,
-}
-
-impl FenwickTree {
-    fn new(n: usize) -> Self {
-        Self {
-            tree: vec![0; 1 + n],
-            n,
-        }
-    }
-
-    fn update(&mut self, mut idx: usize, val: i64) {
-        while idx <= self.n {
-            self.tree[idx] += val;
-            idx += idx & idx.wrapping_neg();
-        }
-    }
-
-    fn query(&self, mut idx: usize) -> i64 {
-        let mut res = 0;
-        while idx > 0 {
-            res += self.tree[idx];
-            idx -= idx & idx.wrapping_neg();
-        }
-        res
-    }
 }
 
 #[cfg(test)]
@@ -99,7 +72,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(stone_game_iii(&[1, 2, 3, 6]), "Tie")
+    }
 
     #[test]
     fn test() {}
