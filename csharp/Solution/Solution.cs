e024789c7fd199c4af067ecff546e9fd3b16db4d
Zx Wang <ZW_08@outlook.com>
2026-08-05 16:33:25 -0400

    1187 dp, binary search

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -9,21 +9,50 @@ namespace Solution;
 
 public class Solution
 {
-    public int MaximumSum(int[] arr)
+    public int MakeArrayIncreasing(int[] arr1, int[] arr2)
     {
-        int no_delete = 0;
-        int with_delete = 0;
-        int res = int.MinValue;
-        bool started = false;
-        foreach (var item in arr)
+        Array.Sort(arr2);
+        int n1 = arr1.Length;
+        int n2 = arr2.Length;
+        Dictionary<int, int> dp = new() { [-1] = 0 };
+        foreach (var item in arr1)
         {
-            if (started) { with_delete = int.Max(item + with_delete, no_delete); }
-            no_delete = int.Max(item, item + no_delete);
-            int curr = started ? int.Max(no_delete, with_delete) : no_delete;
-            res = int.Max(res, curr);
-            started = true;
+            if (dp.Count == 0) { break; }
+            Dictionary<int, int> curr = [];
+            foreach (var (prev, val) in dp)
+            {
+                if (prev < item)
+                {
+                    int v = curr.GetValueOrDefault(item, val);
+                    curr[item] = int.Min(v, val);
+                }
+                int i2 = arr2.BinarySearch(1 + prev);
+                if (i2 < 0) { i2 = ~i2; }
+                if (i2 < n2)
+                {
+                    int v = curr.GetValueOrDefault(arr2[i2], 1 + val);
+                    curr[arr1[i2]] = int.Min(v, 1 + val);
+                }
+            }
+            dp = curr;
         }
-        return res;
+        return dp.Count == 0 ? -1 : dp.Values.Min();
+        int res = Dfs(0, -1);
+        return res < (int.MaxValue >> 1) ? res : -1;
+
+        int Dfs(int idx, int prev)
+        {
+            if (idx >= n1) { return 0; }
+            int res = int.MaxValue >> 1;
+            if (prev < arr1[idx]) { res = Dfs(1 + idx, arr1[idx]); }
+            int i2 = arr2.BinarySearch(1 + prev);
+            if (i2 < 0) { i2 = ~i2; }
+            if (i2 < n2)
+            {
+                int curr = 1 + Dfs(1 + idx, arr2[i2]);
+                res = int.Min(res, curr);
+            }
+            return res;
+        }
     }
 }
-
