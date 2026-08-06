dcfaf8ebe0796e6e13551c23b658d4a5cbe76a4b
Zx Wang <ZW_08@outlook.com>
2026-08-06 15:37:58 -0400

    1191 kadane

--- csharp/Solution/Solution.cs
+++ csharp/Solution/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Frozen;
 using System.Linq.Expressions;
+using System.Net.Sockets;
 using System.Text;
 using Solution.LList;
 using Solution.Tree;
@@ -9,24 +10,36 @@ namespace Solution;
 
 public class Solution
 {
-    public string ReverseParentheses(string s)
+    public int KConcatenationMaxSum(int[] arr, int k)
     {
-        Stack<int> st = [];
-        List<char> res = [];
-        foreach (var c in s)
+        const long M = 1_000_000_007;
+        int n = arr.Length;
+        long curr = 0;
+        long total_max = long.MinValue;
+        foreach (var item in arr)
         {
-            switch (c)
-            {
-                case '(':
-                    st.Push(res.Count); break;
-                case ')':
-                    int top = st.Pop();
-                    res.Reverse(top, res.Count - top);
-                    break;
-                default:
-                    res.Add(c); break;
-            }
+            curr = long.Max(item, item + curr);
+            total_max = long.Max(total_max, curr);
         }
-        return string.Concat(res);
+        long res = long.Max(total_max, 0);
+        if (k == 1) { return (int)(res % M); }
+        long sum = 0;
+        long pref_max = long.MinValue;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            pref_max = long.Max(pref_max, sum);
+        }
+        sum = 0;
+        long suf_max = long.MinValue;
+        for (int i = n - 1; i >= 0; i -= 1)
+        {
+            sum += arr[i];
+            suf_max = long.Max(suf_max, sum);
+        }
+        long v = long.Max(sum * k, sum * (k - 2) + pref_max + suf_max);
+        v = long.Max(v, pref_max + suf_max);
+        res = long.Max(res, v);
+        return (int)(res % M);
     }
 }
