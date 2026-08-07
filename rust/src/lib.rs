78bae3cb1976fb59522e4626317fcd855b757e08
ZW <ZW_08@outlook.com>
2026-08-06 22:13:28 -0400

    3348 greedy, backtrack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,41 +6,70 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::VecDeque;
+use std::iter::repeat_n;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn remaining_methods(n: i32, k: i32, invocations: Vec<Vec<i32>>) -> Vec<i32> {
-    let [n, k] = [n, k].map(|v| v as usize);
-    let mut in_degs = vec![0; n];
-    let mut adj = vec![vec![]; n];
-    for inv in invocations.iter() {
-        let [a, b] = [0, 1].map(|i| inv[i] as usize);
-        in_degs[b] += 1;
-        adj[a].push(b);
+pub fn smallest_number(num: String, t: i64) -> String {
+    if !check(t) {
+        return "-1".to_string();
     }
-    let mut queue = VecDeque::from([k]);
-    let mut sus = vec![false; n];
-    sus[k] = true;
-    while let Some(node) = queue.pop_front() {
-        for &next in &adj[node] {
-            in_degs[next] -= 1;
-            if !sus[next] {
-                sus[next] = true;
-                queue.push_back(next);
+    let (s, n) = (num.as_bytes(), num.len());
+    let mut remaining = vec![t; 1 + n];
+    let mut zero = n - 1;
+    for (idx, &b) in s.iter().enumerate() {
+        let v = i64::from(b - b'0');
+        if v == 0 {
+            zero = idx;
+            break;
+        }
+        remaining[1 + idx] = remaining[idx] / gcd(remaining[idx], v);
+    }
+    if remaining[n] == 1 {
+        return num;
+    }
+    for idx in (0..=zero).rev() {
+        let requirement = remaining[idx];
+        let len = n - 1 - idx;
+        let start = i64::from(1 + s[idx] - b'0');
+        for d in start..=9 {
+            let suffix = fill_in(requirement / gcd(requirement, d), len);
+            if suffix.len() <= len {
+                let mut res = s[..idx].to_vec();
+                res.push(d as u8 + b'0');
+                res.extend(suffix);
+                return String::from_utf8(res).unwrap();
             }
         }
     }
-    let mut res = vec![];
-    for (i, &v) in sus.iter().enumerate() {
-        if v && in_degs[i] > 0 {
-            return (0..n as i32).collect();
-        } else if !v {
-            res.push(i as i32);
+    String::from_utf8(fill_in(t, 1 + n)).unwrap()
+}
+
+fn fill_in(mut requirement: i64, len: usize) -> Vec<u8> {
+    let mut res = Vec::with_capacity(len);
+    for d in (2..=9).rev() {
+        while requirement % d == 0 {
+            res.push(d as u8 + b'0');
+            requirement /= d;
         }
     }
+    res.extend(repeat_n(b'1', len.saturating_sub(res.len())));
+    res.reverse();
     res
+}
+
+fn check(mut t: i64) -> bool {
+    for p in [2, 3, 5, 7] {
+        while t % p == 0 {
+            t /= p;
+        }
+    }
+    t == 1
+}
+
+const fn gcd(a: i64, b: i64) -> i64 {
+    if a == 0 { b } else { gcd(b % a, a) }
 }
 
 #[cfg(test)]
