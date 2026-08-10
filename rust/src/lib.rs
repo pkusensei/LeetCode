0638cbae1090df73f67121d498eaeeab273548c0
ZW <ZW_08@outlook.com>
2026-08-09 21:19:59 -0400

    1510 dp

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,115 +9,38 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn count_of_peaks(mut nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i64> {
-    let n = nums.len();
-    let mut st = SegTree::new(n);
-    for i in 1..n - 1 {
-        if nums[i - 1] < nums[i] && nums[i] > nums[1 + i] {
-            st.update(i, 1);
-        }
-    }
-    let mut res = vec![];
-    for q in queries.iter() {
-        if q[0] == 1 {
-            res.push(st.query(q[1], q[2]));
-        } else {
-            let idx = q[1] as usize;
-            nums[idx] = q[2];
-            for i in 1.max(idx.saturating_sub(1))..=(idx + 1).min(n - 2) {
-                let v = nums[i - 1] < nums[i] && nums[i] > nums[1 + i];
-                st.update(i, v.into());
+pub fn winner_square_game(n: i32) -> bool {
+    let n = n as usize;
+    let mut dp = vec![false; 1 + n];
+    for right in 1..=n {
+        let mut left = 1_usize;
+        while left.pow(2) <= right {
+            if !dp[right - left.pow(2)] {
+                dp[right] = true;
+                break;
             }
+            left += 1
         }
     }
-    res
+    dp[n]
+    // dfs(n)
 }
 
-struct SegTree {
-    tree: Vec<Node>,
-    n: usize,
+fn dfs(n: i32) -> bool {
+    if is_square(n) {
+        return true;
+    }
+    for i in 1..=n.isqrt() {
+        if !dfs(n - i.pow(2)) {
+            return true;
+        }
+    }
+    false
 }
 
-impl SegTree {
-    fn new(n: usize) -> Self {
-        Self {
-            tree: vec![Node::default(); 4 * n],
-            n,
-        }
-    }
-
-    fn update(&mut self, idx: usize, val: i64) {
-        self._update(1, 0, self.n - 1, idx, val);
-    }
-
-    fn _update(&mut self, node: usize, left: usize, right: usize, idx: usize, val: i64) {
-        if left == right {
-            self.tree[node] = Node {
-                count: val,
-                left_peak: left as i64,
-                right_peak: left as i64,
-                peak_free: 0,
-            };
-            return;
-        }
-        let mid = left.midpoint(right);
-        if idx <= mid {
-            self._update(2 * node, left, mid, idx, val);
-        } else {
-            self._update(1 + 2 * node, 1 + mid, right, idx, val);
-        }
-        self.tree[node] = Self::merge(self.tree[2 * node], self.tree[1 + 2 * node]);
-    }
-
-    fn query(&self, ql: i32, qr: i32) -> i64 {
-        let node = self._query(1, 0, self.n - 1, 1 + ql as usize, qr as usize - 1);
-        if node.count == 0 {
-            return 0;
-        }
-        let [ql, qr] = [ql, qr].map(i64::from);
-        let len = qr - ql;
-        let left = node.left_peak;
-        let right = node.right_peak;
-        (len.pow(2) - node.peak_free - (left - ql).pow(2) - (qr - right).pow(2)) / 2
-    }
-
-    fn _query(&self, node: usize, left: usize, right: usize, ql: usize, qr: usize) -> Node {
-        if qr < left || right < ql {
-            return Node::default();
-        }
-        if ql <= left && right <= qr {
-            return self.tree[node];
-        }
-        let mid = left.midpoint(right);
-        Self::merge(
-            self._query(2 * node, left, mid, ql, qr),
-            self._query(1 + 2 * node, 1 + mid, right, ql, qr),
-        )
-    }
-
-    fn merge(a: Node, b: Node) -> Node {
-        if a.count == 0 {
-            return b;
-        }
-        if b.count == 0 {
-            return a;
-        }
-        let gap = b.left_peak - a.right_peak;
-        Node {
-            count: a.count + b.count,
-            left_peak: a.left_peak,
-            right_peak: b.right_peak,
-            peak_free: a.peak_free + b.peak_free + gap.pow(2),
-        }
-    }
-}
-
-#[derive(Clone, Copy, Default)]
-struct Node {
-    count: i64,
-    left_peak: i64,
-    right_peak: i64,
-    peak_free: i64,
+const fn is_square(n: i32) -> bool {
+    let r = n.isqrt();
+    r.pow(2) == n
 }
 
 #[cfg(test)]
