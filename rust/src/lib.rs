7846e37f6daf19e9ca9ab80465ab06d9d4d07b49
ZW <ZW_08@outlook.com>
2026-08-17 09:37:54 -0400

    1563 dp, binary search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,10 +9,42 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
+pub fn final_opt(stone_value: &[i32]) -> i32 {
+    let n = stone_value.len();
+    let mut dp = vec![vec![0; n]; n];
+    let mut max_left = dp.clone();
+    let mut max_right = dp.clone();
+    for (left, &val) in stone_value.iter().enumerate().rev() {
+        max_left[left][left] = val;
+        max_right[left][left] = val;
+        let mut total = val;
+        let mut sum_left = 0;
+        let mut i = left as i32 - 1;
+        for right in (1 + left)..n {
+            total += stone_value[right];
+            while 1 + i < right as i32 && (sum_left + stone_value[(1 + i) as usize]) * 2 <= total {
+                sum_left += stone_value[(1 + i) as usize];
+                i += 1;
+            }
+            if left as i32 <= i {
+                dp[left][right] = dp[left][right].max(max_left[left][i as usize]);
+            }
+            if 1 + i < right as i32 {
+                dp[left][right] = dp[left][right].max(max_right[(2 + i) as usize][right]);
+            }
+            if 2 * sum_left == total {
+                dp[left][right] = dp[left][right].max(max_right[(1 + i) as usize][right]);
+            }
+            max_left[left][right] = max_left[left][right - 1].max(total + dp[left][right]);
+            max_right[left][right] = max_right[1 + left][right].max(total + dp[left][right]);
+        }
+    }
+    dp[0][n - 1]
+}
+
 pub fn with_binary_search(stone_value: &[i32]) -> i32 {
     let n = stone_value.len();
-    // len(prefix) = 1+n
-    let prefix = stone_value.iter().fold(vec![0], |mut acc, v| {
+    let prefix = stone_value.iter().fold(vec![], |mut acc, v| {
         acc.push(v + acc.last().unwrap_or(&0));
         acc
     });
@@ -26,10 +58,11 @@ pub fn with_binary_search(stone_value: &[i32]) -> i32 {
     for len in 2..=n {
         for left in 0..=n - len {
             let right = left + len - 1;
-            let sum = prefix[1 + right] - prefix[left];
-            let i = search(&prefix, left, right);
-            // let i = prefix.partition_point(|v| v - prefix[left] < prefix[1 + right] - v) - 1;
-            let left_half = prefix[1 + i] - prefix[left];
+            let drop_left = if left > 0 { prefix[left - 1] } else { 0 };
+            let sum = prefix[right] - drop_left;
+            // let i = search(&prefix, left, right);
+            let i = prefix.partition_point(|v| v - drop_left < prefix[right] - v);
+            let left_half = prefix[i] - drop_left;
             if left_half * 2 == sum {
                 dp[left][right] = max_left[left][i].max(max_right[1 + i][right]);
             } else {
