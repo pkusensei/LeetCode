99bfae923a7bdda87e8adb5fe0cc0ce0dae35432
ZW <ZW_08@outlook.com>
2026-08-23 07:57:07 -0400

    wc516 q3; sieve, edge cases...

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,24 +6,67 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
+use std::{collections::HashMap, sync::LazyLock};
+
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn find_disappeared_numbers(mut nums: Vec<i32>, lower: i32, upper: i32) -> Vec<Vec<i32>> {
-    nums.sort_unstable();
-    let mut res = vec![];
-    let mut prev = lower;
-    for &num in nums.iter() {
-        if prev <= num - 1 && prev <= upper {
-            res.push(vec![prev, (num - 1).min(upper)]);
+pub fn longest_subarray(nums: &[i32], k: i32) -> i32 {
+    let k = k as usize;
+    let mut map = HashMap::new();
+    let mut left = 0;
+    let mut res = 0;
+    let mut seen = vec![];
+    for (right, &num) in nums.iter().enumerate() {
+        let num = num as usize;
+        let mut curr = vec![];
+        for p in 1..=num.isqrt() {
+            if num % p == 0 {
+                if P[p] {
+                    curr.push(p);
+                    *map.entry(p).or_insert(0) += 1;
+                }
+                if p * p != num && P[num / p] {
+                    curr.push(num / p);
+                    *map.entry(num / p).or_insert(0) += 1;
+                }
+            }
         }
-        prev = (1 + num).max(prev);
+        if curr.len() > k {
+            map.clear();
+            left = 1 + right;
+            seen.push(vec![]);
+            continue;
+        }
+        seen.push(curr);
+        while map.len() > k && left <= right {
+            for &p in &seen[left] {
+                let v = map.entry(p).or_insert(0);
+                *v -= 1;
+                if *v == 0 {
+                    map.remove(&p);
+                }
+            }
+            left += 1;
+        }
+        res = res.max(1 + right - left);
     }
-    if prev <= upper {
-        res.push(vec![prev, upper]);
-    }
-    res
+    res as i32
 }
+
+static P: LazyLock<Vec<bool>> = LazyLock::new(|| {
+    const M: usize = 100_000;
+    let mut sieve = vec![true; 1 + M];
+    sieve[..2].fill(false);
+    for p in 2..=M.isqrt() {
+        if sieve[p] {
+            for val in (p * p..=M).step_by(p) {
+                sieve[val] = false;
+            }
+        }
+    }
+    sieve
+});
 
 #[cfg(test)]
 mod tests {
@@ -57,10 +100,7 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(
-            find_disappeared_numbers(vec![3, 9, 7], 1, 12),
-            [[1, 2], [4, 6], [8, 8], [10, 12]]
-        );
+        assert_eq!(longest_subarray(&[7, 6, 10, 12, 11], 3), 3);
     }
 
     #[test]
