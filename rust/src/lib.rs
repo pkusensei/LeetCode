7e4a23935a7874b35c63643734efad7ce9e4049c
ZW <ZW_08@outlook.com>
2026-08-27 20:36:55 -0400

    3734 backtrack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,45 +6,70 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::iter::repeat_n;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn lex_greater_permutation(s: String, target: String) -> String {
-    let mut freq = s.bytes().fold([0; 26], |mut acc, b| {
+pub fn lex_palindromic_permutation(s: &str, target: &str) -> String {
+    let freq = s.bytes().fold([0; 26], |mut acc, b| {
         acc[usize::from(b - b'a')] += 1;
         acc
     });
-    if let Some(mut v) = dfs(&mut freq, target.as_bytes(), false) {
-        v.reverse();
+    let mut single = None;
+    for (i, f) in freq.iter().enumerate() {
+        if f & 1 == 1 {
+            if single.is_some() {
+                return "".to_string();
+            }
+            single = Some(i as u8 + b'a');
+        }
+    }
+    let mut half = freq.map(|v| v / 2);
+    if let Some(v) = dfs(&mut half, 0, false, single, target.as_bytes(), &mut vec![]) {
         String::from_utf8(v).unwrap()
     } else {
         "".to_string()
     }
 }
 
-fn dfs(freq: &mut [i32; 26], t: &[u8], bigger: bool) -> Option<Vec<u8>> {
-    if bigger {
-        let mut res = vec![];
-        for (i, &f) in freq.iter().enumerate().rev() {
-            res.extend(repeat_n(i as u8 + b'a', f as usize));
-        }
-        return Some(res);
-    }
-    if t.is_empty() {
-        return None;
-    }
-    let head = usize::from(t[0] - b'a');
-    for curr in head..26 {
-        if freq[curr] > 0 {
-            freq[curr] -= 1;
-            if let Some(mut v) = dfs(freq, &t[1..], curr > head) {
-                v.push(curr as u8 + b'a');
-                return Some(v);
-            } else {
-                freq[curr] += 1;
+fn dfs(
+    freq: &mut [i32],
+    idx: usize,
+    bigger: bool,
+    single: Option<u8>,
+    target: &[u8],
+    curr: &mut Vec<u8>,
+) -> Option<Vec<u8>> {
+    use std::iter::repeat_n;
+
+    let n = target.len();
+    if bigger || idx >= n / 2 {
+        let mut temp = curr.clone();
+        if idx < n / 2 {
+            for (i, &f) in freq.iter().enumerate() {
+                temp.extend(repeat_n(i as u8 + b'a', f as usize));
             }
+        }
+        if let Some(v) = single {
+            temp.push(v);
+        }
+        temp.extend_from_within(..n / 2);
+        temp[(1 + n) / 2..].reverse();
+        return if bigger || temp.as_slice() > target {
+            Some(temp)
+        } else {
+            None
+        };
+    }
+    let low = usize::from(target[idx] - b'a');
+    for i in low..26 {
+        if freq[i] > 0 {
+            freq[i] -= 1;
+            curr.push(i as u8 + b'a');
+            if let Some(v) = dfs(freq, 1 + idx, i > low, single, target, curr) {
+                return Some(v);
+            }
+            curr.pop();
+            freq[i] += 1;
         }
     }
     None
@@ -81,7 +106,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(lex_palindromic_permutation("baba", "abba"), "baab")
+    }
 
     #[test]
     fn test() {}
