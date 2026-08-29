668e8805c5abf38dc94cf78d5421ce339f777767
ZW <ZW_08@outlook.com>
2026-08-29 12:25:36 -0400

    bwc190 q1; 3 cases

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,53 +8,16 @@ mod trie;
 
 #[allow(unused_imports)]
 use helper::*;
-use itertools::Itertools;
-use std::collections::HashMap;
 
-pub fn lexicographically_smallest_array(nums: &[i32], limit: i32) -> Vec<i32> {
-    let n = nums.len();
-    let sorted = (0..n).sorted_by_key(|&i| nums[i]).collect_vec();
-    let mut dsu = DSU::new(n);
-    for w in sorted.windows(2) {
-        let [a, b] = w[..] else { unreachable!() };
-        if nums[b] - nums[a] <= limit {
-            dsu.union(a, b);
-        }
-    }
-    let mut groups = HashMap::<usize, Vec<_>>::new();
-    for i in sorted {
-        groups.entry(dsu.find(i)).or_default().push(i);
-    }
-    let mut res = vec![0; n];
-    for i in (0..n).rev() {
-        let root = dsu.find(i);
-        let group = groups.get_mut(&root).unwrap();
-        res[i] = nums[group.pop().unwrap()];
-    }
-    res
-}
-
-struct DSU {
-    parent: Vec<usize>,
-}
-
-impl DSU {
-    fn new(n: usize) -> Self {
-        Self {
-            parent: (0..n).collect(),
-        }
-    }
-
-    fn find(&mut self, v: usize) -> usize {
-        if self.parent[v] != v {
-            self.parent[v] = self.find(self.parent[v])
-        }
-        self.parent[v]
-    }
-
-    fn union(&mut self, x: usize, y: usize) {
-        let [rx, ry] = [x, y].map(|v| self.find(v));
-        self.parent[ry] = rx;
+pub fn min_bishop_moves(source: Vec<i32>, target: Vec<i32>) -> i32 {
+    let [sx, sy] = source[..] else { unreachable!() };
+    let [tx, ty] = target[..] else { unreachable!() };
+    if (sx + sy) & 1 != (tx + ty) & 1 {
+        -1
+    } else if sx + sy == tx + ty || sx - sy == tx - ty {
+        1
+    } else {
+        2
     }
 }
 
@@ -89,12 +52,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(
-            lexicographically_smallest_array(&[1, 5, 3, 9, 8], 2),
-            [1, 3, 5, 8, 9]
-        );
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
