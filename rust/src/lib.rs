87564b0c751b00e0eb46f56811a7f52ce6de7287
ZW <ZW_08@outlook.com>
2026-08-29 13:16:07 -0400

    bwc190 q4; focus on cliifs only

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,20 +9,73 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_string(nums: Vec<i32>) -> Vec<String> {
-    nums.iter()
-        .map(|&x| {
-            let mut x = x;
-            let mut s = vec![];
-            for p in (0..26).rev() {
-                while x >= 1 << p {
-                    s.push(p + b'a');
-                    x -= 1 << p;
-                }
-            }
-            String::from_utf8(s).unwrap()
-        })
-        .collect()
+pub fn max_valid_splits(nums: &[i32]) -> i32 {
+    let n = nums.len();
+    let prefix = nums.iter().fold(Vec::with_capacity(n), f);
+    let mut suffix = nums.iter().rev().fold(Vec::with_capacity(n), f);
+    suffix.reverse();
+    let mut res = (0..n - 1).filter(|&i| prefix[i] == suffix[1 + i]).count() as i32;
+    for i in 0..n - 1 {
+        if prefix[i] != prefix[1 + i] {
+            res = res.max(solve(&nums, 1 + i));
+        }
+    }
+    for i in (0..n - 1).rev() {
+        if suffix[i] != suffix[1 + i] {
+            res = res.max(solve(&nums, i));
+        }
+    }
+    res
+}
+
+fn f(mut acc: Vec<i32>, v: &i32) -> Vec<i32> {
+    if let Some(&x) = acc.last() {
+        acc.push(gcd(x, *v));
+    } else {
+        acc.push(*v);
+    }
+    acc
+}
+
+fn solve(nums: &[i32], skip: usize) -> i32 {
+    let n = nums.len();
+    let mut prefix = Vec::with_capacity(n);
+    for (i, &num) in nums.iter().enumerate() {
+        if i == skip {
+            continue;
+        }
+        if let Some(&x) = prefix.last() {
+            prefix.push(gcd(x, num));
+        } else {
+            prefix.push(num);
+        }
+    }
+    let mut suffix = Vec::with_capacity(n);
+    for (i, &num) in nums.iter().enumerate().rev() {
+        if i == skip {
+            continue;
+        }
+        if let Some(&x) = suffix.last() {
+            suffix.push(gcd(x, num));
+        } else {
+            suffix.push(num);
+        }
+    }
+    suffix.reverse();
+    let mut res = 0;
+    for i in 0..n - 2 {
+        if prefix[i] == suffix[1 + i] {
+            res += 1
+        }
+    }
+    res
+}
+
+const fn gcd(mut a: i32, mut b: i32) -> i32 {
+    while a != 0 {
+        (a, b) = (b % a, a)
+    }
+    b
 }
 
 #[cfg(test)]
@@ -56,7 +109,9 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(max_valid_splits(&[10, 30, 15, 10]), 2);
+    }
 
     #[test]
     fn test() {}
