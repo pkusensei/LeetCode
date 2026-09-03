26cc124b2071b30245f2c99156b53e05019afd25
Zx Wang <ZW_08@outlook.com>
2026-09-03 15:08:00 -0400

    1206

--- csharp/Tests/UnitTest.cs
+++ csharp/Tests/UnitTest.cs
@@ -7,16 +7,11 @@ namespace Tests;
 [TestClass]
 public class UnitTest
 {
-    readonly Solution.Solution sol = new();
+    // readonly Solution.Solution sol = new();
 
     [TestMethod]
-    [DataRow(new[] { 5, 3, 1, 2, 5, 1, 2 }, new[] { 1, 3 })]
-    [DataRow(new[] { 2, 2, 1, 3 }, new[] { -1, -1 })]
-    public void TestMethod1(int[] nums, int[] exp)
-    {
-        var a = ListNode.Make(nums);
-        Assert.IsTrue(sol.NodesBetweenCriticalPoints(a).SequenceEqual(exp));
-    }
+    public void TestMethod1()
+    {}
 
     [TestMethod]
     public void TestMethod2()
