aec78daee8115344afec7fcbf785ededf3772fd5
ZW <ZW_08@outlook.com>
2026-09-08 19:57:47 -0400

    4046 dfs/memo

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -56,6 +56,45 @@ const fn check(rows: usize, cols: usize, r: i32, c: i32) -> Option<[usize; 2]> {
 const DIDX: [usize; 4] = [0, 1, 2, 3];
 const DIRS: [[i32; 2]; 4] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 
+pub fn with_dp(grid: &[&[i32]], k: i32) -> i32 {
+    let [rows, cols] = get_dimensions(&grid);
+    let k = k as usize;
+    let mut memo = vec![vec![vec![[-1; 4]; 1 + k]; cols]; rows];
+    let res = dfs(&grid, 0, 0, k, 1, &mut memo).min(dfs(&grid, 0, 0, k, 3, &mut memo));
+    if res < i32::MAX >> 1 { res } else { -1 }
+}
+
+fn dfs(
+    grid: &[&[i32]],
+    r: usize,
+    c: usize,
+    k: usize,
+    dir: usize,
+    memo: &mut [Vec<Vec<[i32; 4]>>],
+) -> i32 {
+    let [rows, cols] = get_dimensions(grid);
+    if r == rows - 1 && c == cols - 1 {
+        return grid[r][c];
+    }
+    if memo[r][c][k][dir] > -1 {
+        return memo[r][c][k][dir];
+    }
+    let mut res = i32::MAX >> 1;
+    for (d, [dr, dc]) in DIRS.iter().enumerate() {
+        if d == dir || k > 0 {
+            let nr = r as i32 + dr;
+            let nc = c as i32 + dc;
+            if let Some([nr, nc]) = check(rows, cols, nr, nc) {
+                let nk = k - usize::from(d != dir);
+                res = res.min(dfs(grid, nr, nc, nk, d, memo));
+            }
+        }
+    }
+    res += grid[r][c];
+    memo[r][c][k][dir] = res;
+    res
+}
+
 #[cfg(test)]
 mod tests {
 
