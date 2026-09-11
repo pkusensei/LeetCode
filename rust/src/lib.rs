822b2490101b3c9363f698c9cd8ded899805ba70
ZW <ZW_08@outlook.com>
2026-09-10 20:20:15 -0400

    3483 backtrack

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,93 +6,35 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::HashSet;
 
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn min_cost(grid: &[&[i32]], k: i32) -> i32 {
-    let [rows, cols] = get_dimensions(&grid);
-    let k = k as usize;
-    let mut queue = BinaryHeap::new();
-    let mut max_costs = vec![vec![vec![[i32::MAX >> 1; 4]; 1 + k]; cols]; rows];
-    queue.push((Reverse(grid[0][0]), 0, 0, k, 1));
-    max_costs[0][0][k][1] = grid[0][0];
-    queue.push((Reverse(grid[0][0]), 0, 0, k, 3));
-    max_costs[0][0][k][3] = grid[0][0];
-    while let Some((Reverse(cost), r, c, turn, dir)) = queue.pop() {
-        if r == rows - 1 && c == cols - 1 {
-            return cost;
-        }
-        if cost > max_costs[r][c][turn][dir] {
-            continue;
-        }
-        for (&didx, [dr, dc]) in DIDX.iter().zip(DIRS) {
-            if didx == dir || turn > 0 {
-                let nr = r as i32 + dr;
-                let nc = c as i32 + dc;
-                if let Some([nr, nc]) = check(rows, cols, nr, nc) {
-                    let ncost = grid[nr][nc] + cost;
-                    let nturn = turn - usize::from(didx != dir);
-                    if max_costs[nr][nc][nturn][didx] > ncost {
-                        max_costs[nr][nc][nturn][didx] = ncost;
-                        queue.push((Reverse(ncost), nr, nc, nturn, didx));
-                    }
-                }
-            }
-        }
-    }
-    -1
+pub fn total_numbers(digits: Vec<i32>) -> i32 {
+    let mut freq = digits.iter().fold([0; 10], |mut acc, &v| {
+        acc[v as usize] += 1;
+        acc
+    });
+    let mut seen = HashSet::new();
+    dfs(&mut freq, 0, 0, &mut seen);
+    seen.len() as i32
 }
 
-const fn check(rows: usize, cols: usize, r: i32, c: i32) -> Option<[usize; 2]> {
-    if 0 <= r && r < rows as i32 && 0 <= c && c < cols as i32 {
-        Some([r as usize, c as usize])
-    } else {
-        None
+fn dfs(freq: &mut [i32; 10], curr: i32, len: i32, seen: &mut HashSet<i32>) {
+    if len == 3 {
+        if curr >= 100 && (curr & 1) == 0 {
+            seen.insert(curr);
+        }
+        return;
     }
-}
-
-const DIDX: [usize; 4] = [0, 1, 2, 3];
-const DIRS: [[i32; 2]; 4] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-
-pub fn with_dp(grid: &[&[i32]], k: i32) -> i32 {
-    let [rows, cols] = get_dimensions(&grid);
-    let k = k as usize;
-    let mut memo = vec![vec![vec![[-1; 4]; 1 + k]; cols]; rows];
-    let res = dfs(&grid, 0, 0, k, 1, &mut memo).min(dfs(&grid, 0, 0, k, 3, &mut memo));
-    if res < i32::MAX >> 1 { res } else { -1 }
-}
-
-fn dfs(
-    grid: &[&[i32]],
-    r: usize,
-    c: usize,
-    k: usize,
-    dir: usize,
-    memo: &mut [Vec<Vec<[i32; 4]>>],
-) -> i32 {
-    let [rows, cols] = get_dimensions(grid);
-    if r == rows - 1 && c == cols - 1 {
-        return grid[r][c];
-    }
-    if memo[r][c][k][dir] > -1 {
-        return memo[r][c][k][dir];
-    }
-    let mut res = i32::MAX >> 1;
-    for (d, [dr, dc]) in DIRS.iter().enumerate() {
-        if d == dir || k > 0 {
-            let nr = r as i32 + dr;
-            let nc = c as i32 + dc;
-            if let Some([nr, nc]) = check(rows, cols, nr, nc) {
-                let nk = k - usize::from(d != dir);
-                res = res.min(dfs(grid, nr, nc, nk, d, memo));
-            }
+    for i in 0..10 {
+        if freq[i] > 0 {
+            freq[i] -= 1;
+            dfs(freq, curr * 10 + i as i32, 1 + len, seen);
+            freq[i] += 1;
         }
     }
-    res += grid[r][c];
-    memo[r][c][k][dir] = res;
-    res
 }
 
 #[cfg(test)]
@@ -126,9 +68,7 @@ mod tests {
     }
 
     #[test]
-    fn basics() {
-        assert_eq!(min_cost(&[&[2, 7, 3], &[1, 4, 5]], 1), 12);
-    }
+    fn basics() {}
 
     #[test]
     fn test() {}
