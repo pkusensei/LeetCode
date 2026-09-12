921abc7fe6bd0d63beefd3b89ef0ce8b02723831
ZW <ZW_08@outlook.com>
2026-09-11 21:38:04 -0400

    3414 dp, partition_point

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,35 +6,45 @@ mod matrix;
 mod seg_tree;
 mod trie;
 
-use std::collections::HashSet;
-
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn total_numbers(digits: Vec<i32>) -> i32 {
-    let mut freq = digits.iter().fold([0; 10], |mut acc, &v| {
-        acc[v as usize] += 1;
-        acc
-    });
-    let mut seen = HashSet::new();
-    dfs(&mut freq, 0, 0, &mut seen);
-    seen.len() as i32
-}
-
-fn dfs(freq: &mut [i32; 10], curr: i32, len: i32, seen: &mut HashSet<i32>) {
-    if len == 3 {
-        if curr >= 100 && (curr & 1) == 0 {
-            seen.insert(curr);
+pub fn maximum_weight(intervals: Vec<Vec<i32>>) -> Vec<i32> {
+    let n = intervals.len();
+    // idx, left, right, weight
+    let mut arr: Vec<_> = intervals
+        .iter()
+        .enumerate()
+        .map(|(i, interval)| (i, interval[0], interval[1], i64::from(interval[2])))
+        .collect();
+    arr.sort_unstable_by(|a, b| a.2.cmp(&b.2).then_with(|| a.1.cmp(&b.1)));
+    let mut dp_w = vec![[0; 5]; 1 + n];
+    let mut dp_idx = vec![vec![Vec::<i32>::new(); 5]; 1 + n];
+    for (arr_i, &(int_i, left, _right, weight)) in arr.iter().enumerate() {
+        // forces dp array to be of length `1+n`
+        // because prev=0 does not guarantee `0` is valid option
+        // `1+n` turns it into always valid in dp array
+        let prev = arr[..arr_i].partition_point(|v| v.2 < left);
+        for len in 1..=4 {
+            let skip = dp_w[arr_i][len];
+            let take = dp_w[prev][len - 1] + weight;
+            if skip > take {
+                dp_w[1 + arr_i][len] = skip;
+                dp_idx[1 + arr_i][len] = dp_idx[arr_i][len].clone();
+            } else {
+                dp_w[1 + arr_i][len] = take;
+                let mut curr = dp_idx[prev][len - 1].clone();
+                curr.push(int_i as i32);
+                curr.sort_unstable();
+                let skip_idx = &dp_idx[arr_i][len];
+                if skip == take && skip_idx < &curr {
+                    curr = dp_idx[arr_i][len].clone();
+                }
+                dp_idx[1 + arr_i][len] = curr;
+            }
         }
-        return;
     }
-    for i in 0..10 {
-        if freq[i] > 0 {
-            freq[i] -= 1;
-            dfs(freq, curr * 10 + i as i32, 1 + len, seen);
-            freq[i] += 1;
-        }
-    }
+    dp_idx[n][4].clone()
 }
 
 #[cfg(test)]
