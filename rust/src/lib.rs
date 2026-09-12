66289becd4e2d00a7eae5425e8b587f5f0c607f4
ZW <ZW_08@outlook.com>
2026-09-12 12:11:57 -0400

    bwc191 q1

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,42 +9,16 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn maximum_weight(intervals: Vec<Vec<i32>>) -> Vec<i32> {
-    let n = intervals.len();
-    // idx, left, right, weight
-    let mut arr: Vec<_> = intervals
-        .iter()
-        .enumerate()
-        .map(|(i, interval)| (i, interval[0], interval[1], i64::from(interval[2])))
-        .collect();
-    arr.sort_unstable_by(|a, b| a.2.cmp(&b.2).then_with(|| a.1.cmp(&b.1)));
-    let mut dp_w = vec![[0; 5]; 1 + n];
-    let mut dp_idx = vec![vec![Vec::<i32>::new(); 5]; 1 + n];
-    for (arr_i, &(int_i, left, _right, weight)) in arr.iter().enumerate() {
-        // forces dp array to be of length `1+n`
-        // because prev=0 does not guarantee `0` is valid option
-        // `1+n` turns it into always valid in dp array
-        let prev = arr[..arr_i].partition_point(|v| v.2 < left);
-        for len in 1..=4 {
-            let skip = dp_w[arr_i][len];
-            let take = dp_w[prev][len - 1] + weight;
-            if skip > take {
-                dp_w[1 + arr_i][len] = skip;
-                dp_idx[1 + arr_i][len] = dp_idx[arr_i][len].clone();
-            } else {
-                dp_w[1 + arr_i][len] = take;
-                let mut curr = dp_idx[prev][len - 1].clone();
-                curr.push(int_i as i32);
-                curr.sort_unstable();
-                let skip_idx = &dp_idx[arr_i][len];
-                if skip == take && skip_idx < &curr {
-                    curr = dp_idx[arr_i][len].clone();
-                }
-                dp_idx[1 + arr_i][len] = curr;
-            }
-        }
+pub fn count_special_integers(nums: Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
+    for (i, &num) in nums.iter().enumerate() {
+        let v = map.entry(num).or_insert(vec![]);
+        v.push(i);
     }
-    dp_idx[n][4].clone()
+    map.into_values()
+        .filter(|v| v.len() == 3 && v[1] - v[0] == v[2] - v[1])
+        .count() as i32
 }
 
 #[cfg(test)]
