050c532a1b178d91b69a08cc30ffa5f4e0ef5293
ZW <ZW_08@outlook.com>
2026-09-13 13:33:23 -0400

    wc519 q1; annoying

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,35 +9,25 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn largest_overlap(img1: Vec<Vec<i32>>, img2: Vec<Vec<i32>>) -> i32 {
-    use itertools::Itertools;
-    use std::collections::HashMap;
-    let a = img1
-        .iter()
-        .enumerate()
-        .flat_map(|(r, row)| {
-            row.iter()
-                .enumerate()
-                .map(move |(c, &v)| (r as i32, c as i32, v))
-        })
-        .filter(|v| v.2 == 1)
-        .collect_vec();
-    let b = img2
-        .iter()
-        .enumerate()
-        .flat_map(|(r, row)| {
-            row.iter()
-                .enumerate()
-                .map(move |(c, &v)| (r as i32, c as i32, v))
-        })
-        .filter(|v| v.2 == 1);
-    let mut map = HashMap::new();
-    for (r2, c2, _) in b {
-        for (r1, c1, _) in &a {
-            *map.entry([r1 - r2, c1 - c2]).or_insert(0) += 1;
+pub fn cyclic_shift(
+    n: i32,
+    mut grid: Vec<Vec<i32>>,
+    row_shift: &[i32],
+    col_shift: &[i32],
+) -> Vec<Vec<i32>> {
+    let n = n as usize;
+    for (r, &v) in row_shift.iter().enumerate() {
+        let v = v as usize % n;
+        grid[r].rotate_left(v);
+    }
+    for (c, &v) in col_shift.iter().enumerate() {
+        let v = v as usize % n;
+        let a: Vec<i32> = (0..n).map(|i| grid[i][c]).collect();
+        for i in 0..n {
+            grid[(i + n - v) % n][c] = a[i];
         }
     }
-    map.into_values().max().unwrap_or(0)
+    grid
 }
 
 #[cfg(test)]
@@ -71,7 +61,21 @@ mod tests {
     }
 
     #[test]
-    fn basics() {}
+    fn basics() {
+        assert_eq!(
+            cyclic_shift(
+                3,
+                vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]],
+                &[1, 2, 0],
+                &[2, 2, 1]
+            ),
+            [[7, 8, 5], [2, 3, 9], [6, 4, 1]]
+        );
+        assert_eq!(
+            cyclic_shift(2, vec![vec![1, 2], vec![3, 4]], &[1, 0], &[0, 1]),
+            [[2, 4], [3, 1]]
+        )
+    }
 
     #[test]
     fn test() {}
