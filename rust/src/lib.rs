aa4918d2759fe10ed30d6fdbeccebc5f7b423cd0
ZW <ZW_08@outlook.com>
2026-09-17 21:11:23 -0400

    1520 intervals, mono stack or sorting

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,19 +9,49 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-pub fn longest_subsequence(arr: Vec<i32>, difference: i32) -> i32 {
-        use std::collections::HashMap;
-        let mut map = HashMap::new();
-        let mut res = 1;
-        for &num in arr.iter() {
-            if let Some(v) = map.get(&(num - difference)) {
-                res = res.max(1 + v);
-                map.insert(num, 1 + v);
-            } else {
-                map.insert(num, 1);
-            }
+pub fn max_num_of_substrings(s: String) -> Vec<String> {
+    let [mut first, mut last] = [[None; 26]; 2];
+    for (i, b) in s.bytes().enumerate() {
+        let bi = usize::from(b - b'a');
+        first[bi].get_or_insert(i);
+        last[bi] = Some(i);
+    }
+    let mut st: Vec<[usize; 2]> = vec![];
+    for (idx, b) in s.bytes().enumerate() {
+        let bi = usize::from(b - b'a');
+        if first[bi] != Some(idx) {
+            continue;
         }
-        res
+        let Some(right) = expand(&first, &last, s.as_bytes(), idx, last[bi].unwrap()) else {
+            continue;
+        };
+        while let Some(&[a, b]) = st.last()
+            && (a < idx && right < b)
+        {
+            st.pop();
+        }
+        st.push([idx, right]);
+    }
+    st.iter().map(|&[a, b]| s[a..=b].to_owned()).collect()
+}
+
+fn expand(
+    first: &[Option<usize>; 26],
+    last: &[Option<usize>; 26],
+    s: &[u8],
+    left: usize,
+    mut right: usize,
+) -> Option<usize> {
+    let mut idx = 1 + left;
+    while idx < right {
+        let bi = usize::from(s[idx] - b'a');
+        if first[bi].is_some_and(|v| v < left) {
+            return None; // This letter starts before `left
+        }
+        right = right.max(last[bi].unwrap());
+        idx += 1;
+    }
+    Some(right)
 }
 
 #[cfg(test)]
