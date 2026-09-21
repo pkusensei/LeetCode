d632df77a1dc251b38b34dc2fae7c55211104472
ZW <ZW_08@outlook.com>
2026-09-20 20:09:53 -0400

    4059 in place partition

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,28 +9,73 @@ mod trie;
 #[allow(unused_imports)]
 use helper::*;
 
-// a - b + c - d
-// (a c d b) a - c + d - b
-// (a b d c) a - b + d - c
-// (a c b d) a - c + b - d
-pub fn max_value(nums: &[i32]) -> i64 {
-    let mut pulse = 0;
-    let mut max_even = 0;
-    let mut max_odd = i64::MIN >> 2;
-    let mut min = i64::MAX;
-    for (i, &num) in nums.iter().enumerate() {
-        if i & 1 == 0 {
-            pulse += i64::from(num);
-            // length is odd
-            min = min.min(pulse - max_odd);
-            max_odd = max_odd.max(pulse);
-        } else {
-            pulse -= i64::from(num);
-            min = min.min(pulse - max_even);
-            max_even = max_even.max(pulse);
-        };
+pub fn iterative(nums: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![0; 15];
+    let mut done = [false; 15];
+    let mut groups = vec![nums];
+    for bit in (0..15).rev() {
+        let mut next = vec![];
+        // Updated `done` could impact
+        // further processing of `groups` in the same outer loop
+        // if done[bit] {continue;}
+        for g in groups {
+            if done[bit] {
+                next.push(g);
+                continue;
+            }
+            let [mut left, mut right] = [const { vec![] }; 2];
+            for num in g {
+                if num & (1 << bit) > 0 {
+                    left.push(num);
+                } else {
+                    right.push(num);
+                }
+            }
+            if !left.is_empty() {
+                res[14 - bit] += left.len() as i32;
+                next.push(left);
+            }
+            if !right.is_empty() {
+                done[bit] = true;
+                next.push(right);
+            }
+        }
+        groups = next;
     }
-    pulse.max(pulse - 2 * min)
+    res
+}
+
+pub fn largest_power(nums: &[i32]) -> Vec<i32> {
+    let mut res = vec![0; 15];
+    let mut done = [false; 15];
+    dfs(&nums, 14, &mut res, &mut done);
+    res
+}
+
+fn dfs(nums: &[i32], bit: usize, res: &mut [i32], done: &mut [bool; 15]) {
+    if bit >= 15 {
+        return;
+    }
+    if done[bit] {
+        dfs(nums, bit.wrapping_sub(1), res, done);
+        return;
+    }
+    let [mut left, mut right] = [const { vec![] }; 2];
+    for &num in nums {
+        if num & (1 << bit) > 0 {
+            left.push(num);
+        } else {
+            right.push(num);
+        }
+    }
+    if !left.is_empty() {
+        res[14 - bit] += left.len() as i32;
+        dfs(&left, bit.wrapping_sub(1), res, done);
+    }
+    if !right.is_empty() {
+        done[bit] = true;
+        dfs(&right, bit.wrapping_sub(1), res, done);
+    }
 }
 
 #[cfg(test)]
@@ -65,7 +110,23 @@ mod tests {
 
     #[test]
     fn basics() {
-        assert_eq!(max_value(&[9, 7]), 2);
+        assert_eq!(
+            iterative(vec![5, 2]),
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]
+        );
+        assert_eq!(
+            iterative(vec![7, 5]),
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2]
+        );
+
+        assert_eq!(
+            largest_power(&[5, 2]),
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]
+        );
+        assert_eq!(
+            largest_power(&[7, 5]),
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2]
+        );
     }
 
     #[test]
